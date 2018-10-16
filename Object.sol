fba574c8942c2fd7f22544c05aaf44a0c9dc2493
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:05:51 +0700

    add contract Object

--- Object.sol
+++ Object.sol
@@ -1,9 +1,196 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Object at 0x9a100db3bdbd69600e949a9bc650ebf336811afd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Object at 0x353cb767a07972647a653fdfe36cd310abf85a26
 */
 pragma solidity ^0.4.18;
 
+// zeppelin-solidity: 1.5.0
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+}
 
 /**
  * @title Ownable
@@ -16,6 +203,7 @@ contract Ownable {
 
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
@@ -24,6 +212,7 @@ contract Ownable {
     owner = msg.sender;
   }
 
+
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -31,6 +220,7 @@ contract Ownable {
     require(msg.sender == owner);
     _;
   }
+
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
@@ -44,340 +234,57 @@ contract Ownable {
 
 }
 
-contract BuildingStatus is Ownable {
-  /* Observer contract  */
-  address public observer;
+contract Object is StandardToken, Ownable {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+    bool public mintingFinished = false;
 
-  /* Crowdsale contract */
-  address public crowdsale;
+    event Burn(address indexed burner, uint value);
+    event Mint(address indexed to, uint amount);
+    event MintFinished();
 
-  enum statusEnum {
-      crowdsale,
-      refund,
-      preparation_works,
-      building_permit,
-      design_technical_documentation,
-      utilities_outsite,
-      construction_residential,
-      frame20,
-      frame40,
-      frame60,
-      frame80,
-      frame100,
-      stage1,
-      stage2,
-      stage3,
-      stage4,
-      stage5,
-      facades20,
-      facades40,
-      facades60,
-      facades80,
-      facades100,
-      engineering,
-      finishing,
-      construction_parking,
-      civil_works,
-      engineering_further,
-      commisioning_project,
-      completed
-  }
-
-  modifier notCompleted() {
-      require(status != statusEnum.completed);
-      _;
-  }
-
-  modifier onlyObserver() {
-    require(msg.sender == observer || msg.sender == owner || msg.sender == address(this));
-    _;
-  }
-
-  modifier onlyCrowdsale() {
-    require(msg.sender == crowdsale || msg.sender == owner || msg.sender == address(this));
-    _;
-  }
-
-  statusEnum public status;
-
-  event StatusChanged(statusEnum newStatus);
-
-  function setStatus(statusEnum newStatus) onlyCrowdsale  public {
-      status = newStatus;
-      StatusChanged(newStatus);
-  }
-
-  function changeStage(uint8 stage) public onlyObserver {
-      if (stage==1) status = statusEnum.stage1;
-      if (stage==2) status = statusEnum.stage2;
-      if (stage==3) status = statusEnum.stage3;
-      if (stage==4) status = statusEnum.stage4;
-      if (stage==5) status = statusEnum.stage5;
-  }
- 
-}
-
-/*
- * Manager that stores permitted addresses 
- */
-contract PermissionManager is Ownable {
-    mapping (address => bool) permittedAddresses;
-
-    function addAddress(address newAddress) public onlyOwner {
-        permittedAddresses[newAddress] = true;
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
     }
 
-    function removeAddress(address remAddress) public onlyOwner {
-        permittedAddresses[remAddress] = false;
+    function Object(string _name, string _symbol) public {
+        name = _name;
+        symbol = _symbol;
     }
 
-    function isPermitted(address pAddress) public view returns(bool) {
-        if (permittedAddresses[pAddress]) {
-            return true;
-        }
-        return false;
-    }
-}
-
-contract ERC223Interface {
-  uint public totalSupply;
-  function balanceOf(address who) public view returns (uint);
-  function allowedAddressesOf(address who) public view returns (bool);
-  function getTotalSupply() public view returns (uint);
-
-  function transfer(address to, uint value) public returns (bool ok);
-  function transfer(address to, uint value, bytes data) public returns (bool ok);
-  function transfer(address to, uint value, bytes data, string custom_fallback) public returns (bool ok);
-
-  event Transfer(address indexed from, address indexed to, uint value, bytes data);
-  event TransferContract(address indexed from, address indexed to, uint value, bytes data);
-}
-
-/**
- * @title Building Object contract.
- * @author Vladimir Kovalchuk
- */
-contract Object is BuildingStatus {
-
-  /* Name of an object */
-  string public name;
-
-  /* Gross building area */
-  uint32 public gba;
-
-  /* Gress sale area */
-  uint32 public gla;
-
-  /* Parking space */
-  uint32 public parking;
-
-  /* Type of the building */
-  enum unitEnum {appartment, residential}
-
-  unitEnum public unit;
-
-  /* Developer of an object */
-  string public developer;
-
-  /* Leed */
-  string public leed;
-
-  /* Location of an object */
-  string public location;
-
-  /* start date of a project */
-  uint public constructionStart;
-
-  /* end of construction of an object */
-  uint public constructionEnd;
-  // unt sqm
-  uint public untsqm;
-
-  /* report of completion */
-  string public report;
-
-  event ConstructionDateChanged(uint constructStart, uint constructEnd);
-  event PropertyChanged(uint32 gba, uint32 gla, uint32 parking, unitEnum unit, string developer,
-    string leed, string location, uint constructionStart, uint constructionEnd);
-
-  event HoldChanged(address newHold);
-  event ObserverChanged(address newObserver);
-  event CrowdsaleChanged(address newCrowdsale);
-  event TokenChanged(address newCrowdsale);
-
-  /* ERC223 Unity token */
-  ERC223Interface public token;
-
-  /* Hold contract */
-  address public hold;
-
-  /* Permission manager contract */
-  PermissionManager public permissionManager;
-
-  modifier onlyPermitted() {
-    require(permissionManager.isPermitted(msg.sender) || msg.sender == owner || msg.sender == address(this));
-    _;
-  }
-
-  event Completed(string report);
-
-  /* Constructor of an object */
-  function Object(string iName, uint32 iGBA, uint32 iGSA, uint32 iParking, unitEnum iUnit,
-    string iDeveloper, string iLeed, string iLocation, uint iStartDate, uint iEndDate, uint UNTSQM,
-    address iToken, address iCrowdsale, address iObserver, address iHold, address pManager) public {
-      name = iName;
-      gba = iGBA;
-      gla = iGSA;
-      parking = iParking;
-      unit = iUnit;
-      developer = iDeveloper;
-      leed = iLeed;
-      location = iLocation;
-      untsqm = UNTSQM;
-      constructionStart = iStartDate;
-      constructionEnd = iEndDate;
-
-      token = ERC223Interface(iToken);
-      crowdsale = iCrowdsale;
-      observer = iObserver;
-      hold = iHold;
-      permissionManager = PermissionManager(pManager);
-  }
-
-  function setPermissionManager(address _permadr) public onlyOwner {
-    require(_permadr != 0x0);
-    permissionManager = PermissionManager(_permadr);
-  }
-
-  /*
-   * Public setters area
-   */
-  function setGBA(uint32 newGBA) public onlyPermitted notCompleted {
-    gba = newGBA;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setGLA(uint32 newGLA) public onlyPermitted notCompleted {
-    gla = newGLA;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setParking(uint32 newParking) public onlyPermitted notCompleted {
-    parking = newParking;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setUnit(unitEnum newUnit) public onlyPermitted notCompleted {
-    unit = newUnit;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setDeveloper(string newDeveloper) public onlyPermitted notCompleted {
-    developer = newDeveloper;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setLeed(string newLeed) public onlyPermitted notCompleted {
-    leed = newLeed;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setLocation(string newLocation) public onlyPermitted notCompleted {
-    location = newLocation;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setStartDate(uint newStartDate) public onlyPermitted notCompleted {
-    constructionStart = newStartDate;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setEndDate(uint newEndDate) public onlyPermitted notCompleted {
-    constructionEnd = newEndDate;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-
-  function setName(string _name) public onlyPermitted notCompleted {
-    name = _name;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-  
-  function setUntsqm(uint _untsqm) public onlyPermitted notCompleted {
-    untsqm = _untsqm;
-    PropertyChanged(gba, gla, parking, unit, developer, leed, location, constructionStart, constructionEnd);
-  }
-
-  function setObserver(address _observer) public onlyOwner {
-    require(_observer != 0x0);
-    observer = _observer;
-    ObserverChanged(_observer);
-  }
-
-  function setToken(address _token) public onlyOwner {
-    require(_token != 0x0);
-    token = ERC223Interface(_token);
-    TokenChanged(_token);
-  }
-
-  function setHold(address _hold) public onlyOwner {
-    require(_hold != 0x0);
-    hold = _hold;
-    HoldChanged(_hold);
-  }
-
-  function setCrowdsale(address _crowdsale) public onlyOwner {
-    require(_crowdsale != 0x0);
-    crowdsale = _crowdsale;
-    CrowdsaleChanged(_crowdsale);
-  }
-
-  function getTotalSupply() public view returns (uint) {
-    return token.getTotalSupply();
-  }
-
-  function getUNTSQM() public view returns (uint) {
-    return untsqm;
-  }
-
-  function setProperty(string property, string typeArg, uint intVal, string strVal) public onlyObserver {
-    string memory set = "set";
-    string memory s = "(";
-    string memory s2 = ")";
-    bytes memory _ba = bytes(set);
-    bytes memory _bb = bytes(property);
-    bytes memory _t = bytes(typeArg);
-    bytes memory _s = bytes(s);
-    bytes memory _s2 = bytes(s2);
-    string memory ab = new string(_ba.length + _bb.length + 1 + _t.length + 1);
-    bytes memory babcde = bytes(ab);
-    uint k = 0;
-
-    for (uint i = 0; i < _ba.length; i++) {
-      babcde[k++] = _ba[i];
-    }
-    for (i = 0; i < _bb.length; i++) {
-      babcde[k++] = _bb[i];
-    }
-    babcde[k++] = _s[0];
-
-    for (i = 0; i < _t.length; i++) {
-      babcde[k++] = _t[i];
+    function burn(uint _value) public {
+        require(_value <= balances[msg.sender]);
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
     }
 
-    babcde[k++] = _s2[0];
-    if (intVal == 0) {
-      assert(this.call(bytes4(keccak256(string(babcde))), strVal));
-    } else {
-      assert(this.call(bytes4(keccak256(string(babcde))), intVal));
+    function mint(address _to, uint _amount) onlyOwner canMint public returns(bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
+        return true;
     }
-  }
 
-  function completeStatus(string newReport) public onlyOwner notCompleted {
-    status = statusEnum.completed;
-    report = newReport;
-    Completed(report);
-  }
+    function finishMinting() onlyOwner canMint public returns(bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
 
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+        require(_value % (1 ether) == 0); // require whole token transfers
 
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
