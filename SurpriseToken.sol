a2c53506383e8d3725a53ef0165224748cb45645
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:57:08 +0700

    add contract SurpriseToken

--- SurpriseToken.sol
+++ SurpriseToken.sol
@@ -1,8 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SurpriseToken at 0xd9A72fEc8683db0666769D841d6D127F350B4418
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SurpriseToken at 0xCFf7426140d5b84297af981EeCB92824d1135d8c
 */
 pragma solidity ^0.4.18;
-
 
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -10,7 +9,7 @@ library SafeMath {
       return 0;
     }
     uint256 c = a * b;
-    require(c / a == b);
+    assert(c / a == b);
     return c;
   }
 
@@ -20,18 +19,16 @@ library SafeMath {
   }
 
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
+    assert(b <= a);
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
-    require(c >= a);
+    assert(c >= a);
     return c;
   }
 }
-
-
 contract Ownable {
   address public owner;
 
@@ -59,26 +56,29 @@ contract SurpriseToken is Ownable{
     
     using SafeMath for uint256;
     
-    string public constant name       = "SURPRISE";
-    string public constant symbol     = "SPS";
-    uint32 public constant decimals   = 18;
-    uint256 public totalSupply        = 208932000 ether;
+    uint256 public totalSupply = 208932000 ether;
     uint256 public currentTotalSupply = 0;
-    uint256 startBalance              = 276 ether;
+    
+    string public constant name = "SURPRISE";
+    string public constant symbol = "SPS";
+    uint32 public constant decimals = 18;
+    
+    uint256 startBalance = 276 ether;
     
     mapping(address => bool) touched;
+    
     mapping(address => uint256) balances;
     mapping (address => mapping (address => uint256)) internal allowed;
     
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     
-
+    
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
-
+            
         if( !touched[msg.sender] && currentTotalSupply < totalSupply ){
-            balances[msg.sender] = balances[msg.sender].add( startBalance );
+            balances[msg.sender]+= startBalance;
             touched[msg.sender] = true;
             currentTotalSupply = currentTotalSupply.add( startBalance );
         }
@@ -92,7 +92,6 @@ contract SurpriseToken is Ownable{
         return true;
     }
   
-
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
         
@@ -100,7 +99,7 @@ contract SurpriseToken is Ownable{
         
         if( !touched[_from] && currentTotalSupply < totalSupply ){
             touched[_from] = true;
-            balances[_from] = balances[_from].add( startBalance );
+            balances[_from]+= startBalance;
             currentTotalSupply = currentTotalSupply.add( startBalance );
         }
         
@@ -113,25 +112,21 @@ contract SurpriseToken is Ownable{
         return true;
     }
 
-
     function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-
     function allowance(address _owner, address _spender) public view returns (uint256) {
         return allowed[_owner][_spender];
      }
-
 
     function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
         Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
-
 
     function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
         uint oldValue = allowed[msg.sender][_spender];
@@ -144,21 +139,16 @@ contract SurpriseToken is Ownable{
         return true;
      }
     
-
     function getBalance(address _a) internal constant returns(uint256)
     {
-        if( currentTotalSupply < totalSupply ){
-            if( touched[_a] )
-                return balances[_a];
-            else
-                return balances[_a].add( startBalance );
-        } else {
+        if( touched[_a] )
             return balances[_a];
-        }
+        else
+            return balances[_a].add( startBalance );
     }
     
-
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return getBalance( _owner );
     }
+
 }
