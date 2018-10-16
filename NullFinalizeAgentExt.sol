e2b96fa69ce107760f7d652b990461c1dc958146
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:17:01 +0700

    add contract NullFinalizeAgentExt

--- NullFinalizeAgentExt.sol
+++ NullFinalizeAgentExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgentExt at 0xd80d5B9a08F0Bf0058fF470877EbA73562DC48f9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgentExt at 0xfCFf69f30A8046E1E49eAB611071577668AF2E35
 */
 // Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 pragma solidity ^0.4.11;
