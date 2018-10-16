2605668dce2c04ffec9d49818ee4b3479eaec6fe
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:41:41 +0700

    add contract Burner

--- Burner.sol
+++ Burner.sol
@@ -1,12 +1,46 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Burner at 0xf603c4161c388b77249486f9cb6f1741abda6a9b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Burner at 0x02f63293d2028a6de37f1d9545e66246a0a45911
 */
-pragma solidity ^0.4.15;
+pragma solidity ^ 0.4.11;
+
+
+contract Dragon {
+    function burnDragons( uint256 _amount );
+}
+
 
 contract Burner {
+    
 
-    function tokenFallback(address /* _from */, uint /* _value */, bytes /* _data */) returns (bool result) {
-        return true;
+    address public dragon;
+    uint256 public DragonsBurned;
+    
+    
+    modifier onlyDragon() {
+        if (msg.sender != dragon) {
+            throw;
+        }
+        _;
+    }
+    
+    function Burner () {
+        
+        dragon = 0x814F67fA286f7572B041D041b1D99b432c9155Ee; // Hardcode Dragon address
+        
     }
 
+
+
+    function dragonHandler( uint256 _amount ) onlyDragon {
+        
+        Dragon drag = Dragon ( dragon );
+        drag.burnDragons ( _amount );
+        DragonsBurned += _amount;
+    
+        
+    }   
+ 
+ 
+    
+    
 }
