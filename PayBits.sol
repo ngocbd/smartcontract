902fa77ae2a5deefdd3f5cd6e403f9767c4b1397
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:59:08 +0700

    add contract PayBits

--- PayBits.sol
+++ PayBits.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PayBits at 0xcdd0cdb09eea84a693e892cf02ee9fd246137585
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PayBits at 0x53e3c9d46fa3f0a3aecd1887f2281a1407f23a32
 */
 pragma solidity ^0.4.16;
 
@@ -13,8 +13,7 @@ pragma solidity ^0.4.16;
      event Transfer(address indexed _from, address indexed _to, uint256 _value);
      event Approval(address indexed _owner, address indexed _spender, uint256 _value);
  }
- 
-  // PayBits is ERC20 Token
+  
   contract PayBits is ERC20 {
      string public constant symbol = "PYB";
      string public constant name = "PayBits";
@@ -28,7 +27,7 @@ pragma solidity ^0.4.16;
   
      mapping(address => mapping (address => uint256)) allowed;
      
-        // Total Supply of Token will be 21 Million only
+  
      function PayBits() {
          owner = msg.sender;
          balances[owner] = 21000000 * 10**18;
@@ -39,45 +38,12 @@ pragma solidity ^0.4.16;
         _;
     }
      
-     // To Distribute AirDrops for Applicants of Round 1
-     function AirDropPayBitsR1(address[] addresses) onlyOwner {
-         for (uint i = 0; i < addresses.length; i++) {
-             balances[owner] -= 400000000000000000000;
-             balances[addresses[i]] += 400000000000000000000;
-             Transfer(owner, addresses[i], 400000000000000000000);
-         }
-     }
-      // To Distribute AirDrops for Applicants of Round 2
-      function AirDropPayBitsR2(address[] addresses) onlyOwner {
-         for (uint i = 0; i < addresses.length; i++) {
-             balances[owner] -= 300000000000000000000;
-             balances[addresses[i]] += 300000000000000000000;
-             Transfer(owner, addresses[i], 300000000000000000000);
-         }
-     }
-      // To Distribute AirDrops for Applicants of Round 3
-     function AirDropPayBitsR3(address[] addresses) onlyOwner {
-         for (uint i = 0; i < addresses.length; i++) {
-             balances[owner] -= 200000000000000000000;
-             balances[addresses[i]] += 200000000000000000000;
-             Transfer(owner, addresses[i], 200000000000000000000);
-         }
-     }
-     
-     // To Distribute AirDrops of Remaining Token To Bounty (Press Release Post, Article submission, Blog submission, Social Sharing Etc)
-     function AirDropPayBitsBounty(address[] addresses) onlyOwner {
-         for (uint i = 0; i < addresses.length; i++) {
-             balances[owner] -= 100000000000000000000;
-             balances[addresses[i]] += 100000000000000000000;
-             Transfer(owner, addresses[i], 100000000000000000000);
-         }
-     }
-        // Total Supply of Coin will be 21 Million only 
+  
      function totalSupply() constant returns (uint256 totalSupply) {
          totalSupply = _totalSupply;
      }
   
-        // For Future Development 20% of token will be used
+
      function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
      }
