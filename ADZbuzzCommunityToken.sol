a61f6a3aa554b24e030cfd646164bf90038deefb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:30:31 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x62ce5a01fa9c6ede017af7bc4621d98b9c21b71c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x2f5d94f962991b5b0068932489d18db2c487b8ce
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT256018' token contract
+    // 'ACT297427' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT256018
-    // Name        : ADZbuzz Blackberry.com Community Token
+    // Symbol      : ACT297427
+    // Name        : ADZbuzz Crypto-analyse.org Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT256018";
-            name = "ADZbuzz Blackberry.com Community Token";
+            symbol = "ACT297427";
+            name = "ADZbuzz Crypto-analyse.org Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
