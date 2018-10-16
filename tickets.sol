dafde45a28e35f0efd82d965f08c7b53596fbd50
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:09:56 +0700

    add contract tickets

--- tickets.sol
+++ tickets.sol
@@ -1,180 +1,205 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Tickets at 0x2b0067e8a59a66a4fbf370b70d21133400b04ac2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract tickets at 0x802216857760c60d78f34b7f7d7ec9cc6af3bfed
 */
-contract Tickets {
+pragma solidity ^0.4.16;
 
-    string public name = "Kraftwerk";
-    string public symbol = "KKT";
-    uint8 public decimals = 0;
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    address[1000] public holders;
-    mapping(uint256 => bool) public usedTickets;
-    mapping(uint256 => string) public additionalInfo;
-    mapping(address => uint[16]) public seatsList;
-    mapping(address => uint256) public balanceOf;
-    address[30000] public booking;
-    mapping(address => uint[16]) public bookingList;
-    mapping(address => uint256) public amountOfBooked;
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-    address Manager;
-    address ManagerForRate;
-    address Company;
-    address nullAddress = 0x0;
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    uint public limitPerHolder = 16;
-    uint public seatsCount = 1000;
-    uint scaleMultiplier = 1000000000000000000; 
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
-    uint public Rate_Eth = 298;
-    uint public Ticket_Price = 15*scaleMultiplier/Rate_Eth;
+    contract owned {
+        address public owner;
 
-    modifier managerOnly { require(msg.sender == Manager); _; }
-    modifier rateManagerOnly { require(msg.sender == ManagerForRate); _; }
-
-    event LogAllocateTicket(uint256 _seatID, address _buyer, string _infoString);
-    event LogTransfer(address _holder, address _receiver, uint256 _seatID, string _infoStringt);
-    event LogRedeemTicket(uint _seatID, address _holder, string _infoString);
-    event LogBookTicket(uint _seatID, address _buyer,string _infoString);
-    event LogCancelReservation(address _buyer, uint _seatID);
-
-
-
-    function Tickets(address _ManagerForRate,  address _Manager, address _Company) {
-        ManagerForRate = _ManagerForRate;
-        Manager = _Manager;
-        Company = _Company;
-    }
-
-    function setRate(uint _RateEth) external rateManagerOnly {
-       Rate_Eth = _RateEth;
-       Ticket_Price = 15*scaleMultiplier/Rate_Eth;
-    }
-
-
-    function allocateTicket(uint256 seatID, address buyer, string infoString) external managerOnly {
-        require(seatID > 0 && seatID < seatsCount);
-        require(holders[seatID] == nullAddress);
-        require(balanceOf[buyer] < limitPerHolder);
-        require(booking[seatID] == nullAddress);
-        createTicket(seatID, buyer);
-        additionalInfo[seatID] = infoString;
-        LogAllocateTicket(seatID, buyer, infoString);
-    }
-
-    function createTicket(uint256 seatID, address buyer) internal {
-        uint i = 0;
-        for(i = 0; i < limitPerHolder; i++)
-        {
-            if(seatsList[buyer][i] == 0)
-            {
-                break;
-            }
+        function owned() {
+            owner = msg.sender;
         }
-        holders[seatID] = buyer;
-        balanceOf[buyer] += 1;
-        seatsList[buyer][i] = seatID;
-    }
 
-    function redeemTicket(uint seatID, address holder) external managerOnly{
-        require(seatID > 0 && seatID < seatsCount);
-        require(usedTickets[seatID] == false);
-        require(holders[seatID] == holder);
-        usedTickets[seatID] = true;
-        string infoString = additionalInfo[seatID];
-        LogRedeemTicket(seatID, holder, infoString);
-    }
-
-    function transfer(address holder, address receiver, uint256 seatID) external managerOnly{
-        require(seatID > 0 && seatID < seatsCount);
-        require(holders[seatID] == holder);
-        require(balanceOf[receiver] < limitPerHolder);
-        require(holder != receiver);
-        uint i = 0;
-        holders[seatID] = receiver;
-        balanceOf[holder] -= 1;
-        if(receiver != nullAddress)
-        {
-            for(i = 0; i < limitPerHolder; i++)
-              {
-                  if(seatsList[receiver][i] == 0)
-                  {
-                     break;
-                  }
-            }
-            balanceOf[receiver] += 1;
-            seatsList[receiver][i] = seatID;
+        modifier onlyOwner {
+            require(msg.sender == owner);
+            _;
         }
-        for(i = 0; i < limitPerHolder; i++)
-        {
-            if(seatsList[holder][i] == seatID)
-            {
-                seatsList[holder][i] = 0;
-            }
+
+        function transferOwnership(address newOwner) onlyOwner {
+            owner = newOwner;
         }
-        string infoString = additionalInfo[seatID];
-        LogTransfer(holder, receiver, seatID, infoString);
     }
 
-    function bookTicket(uint256 seatID, address buyer, string infoString) external managerOnly{
-        require(seatID > 0 && seatID < seatsCount);
-        require(holders[seatID] == nullAddress);
-        require(booking[seatID] == nullAddress);
-        require(balanceOf[buyer] + amountOfBooked[buyer] < limitPerHolder);
-        uint i = 0;
-        booking[seatID] = buyer;
-        amountOfBooked[buyer] += 1;
-        while(bookingList[buyer][i] != 0) {
-            i++;
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+
+contract tickets is owned {
+
+	using SafeMath for uint256;
+	
+    // Public variables of the token
+    string public name = "tickets";
+    string public symbol = "TKT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 10^24;
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+	
+    function tickets(){
+     balanceOf[msg.sender] = totalSupply;
+    }
+
+	function mintToken(address target, uint256 mintedAmount) onlyOwner {
+		balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        Transfer(0, owner, mintedAmount);
+        Transfer(owner, target, mintedAmount);
+    }
+	
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
         }
-        bookingList[buyer][i] = seatID;
-        additionalInfo[seatID] = infoString;
-        LogBookTicket(seatID, buyer, infoString);
     }
 
-    function cancelReservation(address buyer, uint256 seatID) external managerOnly{
-        require(booking[seatID] == buyer);
-        uint i = 0;
-        while(i < limitPerHolder) {
-            if (seatID == bookingList[buyer][i]){
-              booking[seatID] = nullAddress;
-              bookingList[buyer][i] = 0;
-              break;
-            }
-            i++;
-        }
-        amountOfBooked[buyer] -= 1;
-        LogCancelReservation(buyer, seatID);
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
     }
 
-
-    function() payable {
-        require(amountOfBooked[msg.sender] != 0);
-        require(balanceOf[msg.sender] + amountOfBooked[msg.sender] <= limitPerHolder);
-        require(msg.value >= Ticket_Price * amountOfBooked[msg.sender]);
-        makePayment(msg.sender);
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
     }
-
-    function makePayment(address buyer) internal {
-        uint i = 0;
-        uint seatID;
-        string infoString;
-        while(i < limitPerHolder) {
-            if(bookingList[buyer][i] != 0) {
-              seatID = bookingList[buyer][i];
-              bookingList[buyer][i] = 0;
-              booking[seatID] = nullAddress;
-              createTicket(seatID, buyer);
-              infoString = additionalInfo[seatID];
-              LogAllocateTicket(seatID, msg.sender, infoString);
-            }
-            i++;
-        }
-        amountOfBooked[buyer] = 0;
-    }
-
-    function withdrawEther(uint256 _value) external managerOnly{
-       Company.transfer(_value);
-    }
-
-
+    
+    function distributeToken(address[] addresses, uint256 _value) onlyOwner {
+     for (uint i = 0; i < addresses.length; i++) {
+         balanceOf[owner] -= _value;
+         balanceOf[addresses[i]] += _value;
+         Transfer(owner, addresses[i], _value);
+     }
+}
 }
