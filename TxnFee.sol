d85d3de57a3ce61fa6b109451f89ecfa99419e77
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:31:15 +0700

    add contract TxnFee

--- TxnFee.sol
+++ TxnFee.sol
@@ -1,21 +1,29 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TxnFee at 0xc6fec2f3d7fa123671f2af8698e845bd4aecc09d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TxnFee at 0x19ea6a8616065038c81c695f218e1b206fbcbd6a
 */
 pragma solidity ^0.4.20;
 
 contract TxnFee {
     address public owner;
     address public primary_wallet;
+    address public thirty_wallet;
+    address public ten_wallet;
     
-    constructor (address main_wallet) public {
+    constructor (address main_wallet, address first, address second) public {
         owner = msg.sender;
         primary_wallet = main_wallet;
+        thirty_wallet = first;
+        ten_wallet = second;
     }
     
     event Contribution (address investor, uint256 eth_paid);
     
     function () public payable {
         emit Contribution(msg.sender, msg.value);
-        primary_wallet.transfer(msg.value);
+        uint256 thirty_value = msg.value * 3 / 10;
+        uint256 ten_value = msg.value * 1 / 10;
+        thirty_wallet.transfer(thirty_value);
+        ten_wallet.transfer(ten_value);
+        primary_wallet.transfer(msg.value - (thirty_value + ten_value));
     }
 }
