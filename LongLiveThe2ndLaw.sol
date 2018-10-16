e123efb7f9d7b24ccc7fdb563fa5e68d34ffc55b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:00:18 +0700

    add contract LongLiveThe2ndLaw

---
+++ LongLiveThe2ndLaw.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LongLiveThe2ndLaw at 0xf690f0ee16bb8f67c4f8a8d9e116f77cf3acf166
+*/
+pragma solidity ^0.4.6;
+contract LongLiveThe2ndLaw {
+    string deathToUnitarity = "The strong version of the 2nd Law of Thermodynamics, that the entropy of an isolated system always increases, when combined with the Schrodinger Equation, implies that the imaginary part of the Hamiltonian is positive.  This implies that all non-relativistic masses have a positive imaginary component.  The weak version of the 2nd Law of Thermodynamics, that the entropy of an isolated system never decreases, implies that the imaginary parts of a Hamiltonian and of non-relativistic masses are non-negative";
+    
+    function output() returns (string) {
+        return deathToUnitarity;
+    }
+}
