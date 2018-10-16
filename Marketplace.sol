4830c611bbc64b3a1de8f7881ce372b197dc0d1e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:45:20 +0700

    add contract Marketplace

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,7 +1,55 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Marketplace at 0x4ce6405331b0cf16f3b7ac4037acad7247055ef2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Marketplace at 0xcd2de7fd530269feaf6075c8bca940526fd7c4de
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.21;
+
+// File: zeppelin-solidity/contracts/math/SafeMath.sol
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 // File: zeppelin-solidity/contracts/ownership/Ownable.sol
 
@@ -45,106 +93,212 @@ contract Ownable {
 
 }
 
-// File: zeppelin-solidity/contracts/token/ERC721/ERC721.sol
+// File: contracts/marketplace/Marketplace.sol
 
 /**
- * @title ERC721 interface
- * @dev see https://github.com/ethereum/eips/issues/721
+ * @title Interface for contracts conforming to ERC-20
  */
-contract ERC721 {
-  event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-  event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
-
-  function balanceOf(address _owner) public view returns (uint256 _balance);
-  function ownerOf(uint256 _tokenId) public view returns (address _owner);
-  function transfer(address _to, uint256 _tokenId) public;
-  function approve(address _to, uint256 _tokenId) public;
-  function takeOwnership(uint256 _tokenId) public;
+contract ERC20Interface {
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 }
 
-// File: contracts/Marketplace.sol
+/**
+ * @title Interface for contracts conforming to ERC-721
+ */
+contract ERC721Interface {
+    function ownerOf(uint256 assetId) public view returns (address);
+    function safeTransferFrom(address from, address to, uint256 assetId) public;
+    function isAuthorized(address operator, uint256 assetId) public view returns (bool);
+}
 
 contract Marketplace is Ownable {
-    ERC721 public nft;
+    using SafeMath for uint256;
 
-    mapping (uint256 => Listing) public listings;
+    ERC20Interface public acceptedToken;
+    ERC721Interface public nonFungibleRegistry;
 
-    uint256 public minListingSeconds;
-    uint256 public maxListingSeconds;
-
-    struct Listing {
+    struct Auction {
+        // Auction ID
+        bytes32 id;
+        // Owner of the NFT
         address seller;
-        uint256 startingPrice;
-        uint256 minimumPrice;
-        uint256 createdAt;
-        uint256 durationSeconds;
+        // Price (in wei) for the published item
+        uint256 price;
+        // Time when this sale ends
+        uint256 expiresAt;
     }
 
-    event TokenListed(uint256 indexed _tokenId, uint256 _startingPrice, uint256 _minimumPrice, uint256 _durationSeconds, address _seller);
-    event TokenUnlisted(uint256 indexed _tokenId, address _unlister);
-    event TokenSold(uint256 indexed _tokenId, uint256 _price, uint256 _paidAmount, address indexed _seller, address _buyer);
+    mapping (uint256 => Auction) public auctionByAssetId;
 
-    modifier nftOnly() {
-        require(msg.sender == address(nft));
-        _;
+    uint256 public ownerCutPercentage;
+    uint256 public publicationFeeInWei;
+
+    /* EVENTS */
+    event AuctionCreated(
+        bytes32 id,
+        uint256 indexed assetId,
+        address indexed seller, 
+        uint256 priceInWei, 
+        uint256 expiresAt
+    );
+    event AuctionSuccessful(
+        bytes32 id,
+        uint256 indexed assetId, 
+        address indexed seller, 
+        uint256 totalPrice, 
+        address indexed winner
+    );
+    event AuctionCancelled(
+        bytes32 id,
+        uint256 indexed assetId, 
+        address indexed seller
+    );
+
+    event ChangedPublicationFee(uint256 publicationFee);
+    event ChangedOwnerCut(uint256 ownerCut);
+
+
+    /**
+     * @dev Constructor for this contract.
+     * @param _acceptedToken - Address of the ERC20 accepted for this marketplace
+     * @param _nonFungibleRegistry - Address of the ERC721 registry contract.
+     */
+    function Marketplace(address _acceptedToken, address _nonFungibleRegistry) public {
+        acceptedToken = ERC20Interface(_acceptedToken);
+        nonFungibleRegistry = ERC721Interface(_nonFungibleRegistry);
     }
 
-    function Marketplace(ERC721 _nft, uint256 _minListingSeconds, uint256 _maxListingSeconds) public {
-        nft = _nft;
-        minListingSeconds = _minListingSeconds;
-        maxListingSeconds = _maxListingSeconds;
+    /**
+     * @dev Sets the publication fee that's charged to users to publish items
+     * @param publicationFee - Fee amount in wei this contract charges to publish an item
+     */
+    function setPublicationFee(uint256 publicationFee) onlyOwner public {
+        publicationFeeInWei = publicationFee;
+
+        ChangedPublicationFee(publicationFeeInWei);
     }
 
-    function list(address _tokenSeller, uint256 _tokenId, uint256 _startingPrice, uint256 _minimumPrice, uint256 _durationSeconds) public nftOnly {
-        require(_durationSeconds >= minListingSeconds && _durationSeconds <= maxListingSeconds);
-        require(_startingPrice >= _minimumPrice);
-        require(! listingActive(_tokenId));
-        listings[_tokenId] = Listing(_tokenSeller, _startingPrice, _minimumPrice, now, _durationSeconds);
-        nft.takeOwnership(_tokenId);
-        TokenListed(_tokenId, _startingPrice, _minimumPrice, _durationSeconds, _tokenSeller);
+    /**
+     * @dev Sets the share cut for the owner of the contract that's
+     *  charged to the seller on a successful sale.
+     * @param ownerCut - Share amount, from 0 to 100
+     */
+    function setOwnerCut(uint8 ownerCut) onlyOwner public {
+        require(ownerCut < 100);
+
+        ownerCutPercentage = ownerCut;
+
+        ChangedOwnerCut(ownerCutPercentage);
     }
 
-    function unlist(address _caller, uint256 _tokenId) public nftOnly {
-        address _seller = listings[_tokenId].seller;
-        // Allow owner to unlist (via nft) for when it's time to shut this down
-        require(_seller == _caller || address(owner) == _caller);
-        nft.transfer(_seller, _tokenId);
-        delete listings[_tokenId];
-        TokenUnlisted(_tokenId, _caller);
+    /**
+     * @dev Cancel an already published order
+     * @param assetId - ID of the published NFT
+     * @param priceInWei - Price in Wei for the supported coin.
+     * @param expiresAt - Duration of the auction (in hours)
+     */
+    function createOrder(uint256 assetId, uint256 priceInWei, uint256 expiresAt) public {
+        address assetOwner = nonFungibleRegistry.ownerOf(assetId);
+        require(msg.sender == assetOwner);
+        require(nonFungibleRegistry.isAuthorized(address(this), assetId));
+        require(priceInWei > 0);
+        require(expiresAt > now.add(1 minutes));
+
+        bytes32 auctionId = keccak256(
+            block.timestamp, 
+            assetOwner,
+            assetId, 
+            priceInWei
+        );
+
+        auctionByAssetId[assetId] = Auction({
+            id: auctionId,
+            seller: assetOwner,
+            price: priceInWei,
+            expiresAt: expiresAt
+        });
+
+        // Check if there's a publication fee and
+        // transfer the amount to marketplace owner.
+        if (publicationFeeInWei > 0) {
+            require(acceptedToken.transferFrom(
+                msg.sender,
+                owner,
+                publicationFeeInWei
+            ));
+        }
+
+        AuctionCreated(
+            auctionId,
+            assetId, 
+            assetOwner,
+            priceInWei, 
+            expiresAt
+        );
     }
 
-    function purchase(address _caller, uint256 _tokenId, uint256 _totalPaid) public payable nftOnly {
-        Listing memory _listing = listings[_tokenId];
-        address _seller = _listing.seller;
+    /**
+     * @dev Cancel an already published order
+     *  can only be canceled by seller or the contract owner.
+     * @param assetId - ID of the published NFT
+     */
+    function cancelOrder(uint256 assetId) public {
+        require(auctionByAssetId[assetId].seller == msg.sender || msg.sender == owner);
 
-        require(_caller != _seller); // Doesn't make sense for someone to buy/sell their own token.
-        require(listingActive(_tokenId));
+        bytes32 auctionId = auctionByAssetId[assetId].id;
+        address auctionSeller = auctionByAssetId[assetId].seller;
+        delete auctionByAssetId[assetId];
 
-        uint256 _price = currentPrice(_tokenId);
-        require(_totalPaid >= _price);
-
-        delete listings[_tokenId];
-
-        nft.transfer(_caller, _tokenId);
-        _seller.transfer(msg.value);
-        TokenSold(_tokenId, _price, _totalPaid, _seller, _caller);
+        AuctionCancelled(auctionId, assetId, auctionSeller);
     }
 
-    function currentPrice(uint256 _tokenId) public view returns (uint256) {
-        Listing memory listing = listings[_tokenId];
-        require(now >= listing.createdAt);
+    /**
+     * @dev Executes the sale for a published NTF
+     * @param assetId - ID of the published NFT
+     */
+    function executeOrder(uint256 assetId, uint256 price) public {
+        address seller = auctionByAssetId[assetId].seller;
 
-        uint256 _deadline = listing.createdAt + listing.durationSeconds;
-        require(now <= _deadline);
+        require(seller != address(0));
+        require(seller != msg.sender);
+        require(auctionByAssetId[assetId].price == price);
+        require(now < auctionByAssetId[assetId].expiresAt);
 
-        uint256 _elapsedTime = now - listing.createdAt;
-        uint256 _progress = (_elapsedTime * 100) / listing.durationSeconds;
-        uint256 _delta = listing.startingPrice - listing.minimumPrice;
-        return listing.startingPrice - ((_delta * _progress) / 100);
+        require(seller == nonFungibleRegistry.ownerOf(assetId));
+
+        uint saleShareAmount = 0;
+
+        if (ownerCutPercentage > 0) {
+
+            // Calculate sale share
+            saleShareAmount = price.mul(ownerCutPercentage).div(100);
+
+            // Transfer share amount for marketplace Owner.
+            acceptedToken.transferFrom(
+                msg.sender,
+                owner,
+                saleShareAmount
+            );
+        }
+
+        // Transfer sale amount to seller
+        acceptedToken.transferFrom(
+            msg.sender,
+            seller,
+            price.sub(saleShareAmount)
+        );
+
+        // Transfer asset owner
+        nonFungibleRegistry.safeTransferFrom(
+            seller,
+            msg.sender,
+            assetId
+        );
+
+
+        bytes32 auctionId = auctionByAssetId[assetId].id;
+        delete auctionByAssetId[assetId];
+
+        AuctionSuccessful(auctionId, assetId, seller, price, msg.sender);
     }
-
-    function listingActive(uint256 _tokenId) internal view returns (bool) {
-        Listing memory listing = listings[_tokenId];
-        return listing.createdAt + listing.durationSeconds >= now && now >= listing.createdAt;
-    }
-}
+ }
