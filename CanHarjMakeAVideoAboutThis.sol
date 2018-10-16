3e18e216a67ddd1ec27dc746f7aa9f11b988b4b9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:03:46 +0700

    add contract CanHarjMakeAVideoAboutThis

---
+++ CanHarjMakeAVideoAboutThis.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CanHarjMakeAVideoAboutThis at 0x25a260907fee6892de31f8c6b80c60d430e2fd00
+*/
+pragma solidity ^0.4.21;
+
+
+
+contract CanHarjMakeAVideoAboutThis{
+
+}
