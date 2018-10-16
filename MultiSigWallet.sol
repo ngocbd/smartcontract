2ce55ce65923eba11a2fc4b410b924e0b4a7c308
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:43:44 +0700

    add contract MultiSigWallet

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -1,384 +1,16 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWallet at 0x9311f7ee5f48a807d598cdd68f43cd4e73ffa747
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWallet at 0x78fb2c7276011d392226a9e75298424424294d0a
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.18;
 
-// ----------------------------------------------------------------------------------------------
-// GTO Token by GTO Limited.
-// An ERC20 standard
-//
-// author: GTO Team
-// Contact: datwhnguyen@gmail.com
-
-contract ERC20Interface {
-    // Get the total token supply
-    function totalSupply() public constant returns (uint256 _totalSupply);
- 
-    // Get the account balance of another account with address _owner
-    function balanceOf(address _owner) public constant returns (uint256 balance);
- 
-    // Send _value amount of tokens to address _to
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    
-    // transfer _value amount of token approved by address _from
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-
-    // approve an address with _value amount of tokens
-    function approve(address _spender, uint256 _value) public returns (bool success);
-
-    // get remaining token approved by _owner to _spender
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-  
-    // Triggered when tokens are transferred.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
- 
-    // Triggered whenever approve(address _spender, uint256 _value) is called.
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
- 
-contract GTO is ERC20Interface {
-    uint8 public constant decimals = 5;
-
-    string public constant symbol = "GTO";
-    string public constant name = "GTO";
-
-    bool public _selling = false;//initial not selling
-    uint256 public _totalSupply = 10 ** 14; // total supply is 10^14 unit, equivalent to 10^9 GTO
-    uint256 public _originalBuyPrice = 45 * 10**7; // original buy 1ETH = 4500 GTO = 45 * 10**7 unit
-
-    // Owner of this contract
-    address public owner;
- 
-    // Balances GTO for each account
-    mapping(address => uint256) private balances;
-    
-    // Owner of account approves the transfer of an amount to another account
-    mapping(address => mapping (address => uint256)) private allowed;
-
-    // List of approved investors
-    mapping(address => bool) private approvedInvestorList;
-    
-    // mapping Deposit
-    mapping(address => uint256) private deposit;
-    
-    // buyers buy token deposit
-    address[] private buyers;
-    
-    // icoPercent
-    uint8 public _icoPercent = 10;
-    
-    // _icoSupply is the avalable unit. Initially, it is _totalSupply
-    uint256 public _icoSupply = _totalSupply * _icoPercent / 100;
-    
-    // minimum buy 0.3 ETH
-    uint256 public _minimumBuy = 3 * 10 ** 17;
-    
-    // maximum buy 30 ETH
-    uint256 public _maximumBuy = 30 * 10 ** 18;
-    
-    /**
-     * Functions with this modifier can only be executed by the owner
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    /**
-     * Functions with this modifier check on sale status
-     * Only allow sale if _selling is on
-     */
-    modifier onSale() {
-        require(_selling && (_icoSupply > 0) );
-        _;
-    }
-    
-    /**
-     * Functions with this modifier check the validity of address is investor
-     */
-    modifier validInvestor() {
-        require(approvedInvestorList[msg.sender]);
-        _;
-    }
-    
-    /**
-     * Functions with this modifier check the validity of msg value
-     * value must greater than equal minimumBuyPrice
-     * total deposit must less than equal maximumBuyPrice
-     */
-    modifier validValue(){
-        // require value >= _minimumBuy AND total deposit of msg.sender <= maximumBuyPrice
-        require ( (msg.value >= _minimumBuy) &&
-                ( (deposit[msg.sender] + msg.value) <= _maximumBuy) );
-        _;
-    }
-    
-    /**
-     * Functions with this modifier check the validity of range [a, b] <= [0, buyers.length-1]
-     */
-    modifier validRange(uint256 a, uint256 b){
-        require ( (a>=0 && a<=b) &&
-                  (b<buyers.length) );
-        _;
-    }
-
-    /// @dev Fallback function allows to buy ether.
-    function()
-        public
-        payable {
-        buyGifto();
-    }
-    
-    /// @dev buy function allows to buy ether. for using optional data
-    function buyGifto()
-        public
-        payable
-        onSale
-        validValue
-        validInvestor {
-        // check the first buy => push to Array
-        if (deposit[msg.sender] == 0){
-            // add new buyer to List
-            buyers.push(msg.sender);
-        }
-        // increase amount deposit of buyer
-        deposit[msg.sender] += msg.value;
-        // transfer value directly for owner
-        owner.transfer(msg.value);
-    }
-
-    /// @dev Constructor
-    function GTO() 
-        public {
-        owner = msg.sender;
-        // buyers = new address[](1);
-        balances[owner] = _totalSupply;
-        Transfer(0x0, owner, _totalSupply);
-    }
-    
-    /// @dev Gets totalSupply
-    /// @return Total supply
-    function totalSupply()
-        public 
-        constant 
-        returns (uint256) {
-        return _totalSupply;
-    }
-    
-    /// @dev Enables sale 
-    function turnOnSale() onlyOwner 
-        public {
-        _selling = true;
-    }
-
-    /// @dev Disables sale
-    function turnOffSale() onlyOwner 
-        public {
-        _selling = false;
-    }
-    
-    /// @dev set new icoPercent
-    /// @param newIcoPercent new value of icoPercent
-    function setIcoPercent(uint8 newIcoPercent)
-        public 
-        onlyOwner {
-        _icoPercent = newIcoPercent;
-        _icoSupply = _totalSupply * _icoPercent / 100;
-    }
-    
-    /// @dev set new _maximumBuy
-    /// @param newMaximumBuy new value of _maximumBuy
-    function setMaximumBuy(uint256 newMaximumBuy)
-        public 
-        onlyOwner {
-        _maximumBuy = newMaximumBuy;
-    }
-
-    /// @dev Updates buy price (owner ONLY)
-    /// @param newBuyPrice New buy price (in unit)
-    function setBuyPrice(uint256 newBuyPrice) 
-        onlyOwner 
-        public {
-        require(newBuyPrice>0);
-        _originalBuyPrice = newBuyPrice; // 3000 GTO = 3000 00000 unit
-        // control _maximumBuy_USD = 10,000 USD, GTO price is 0.1USD
-        // maximumBuy_Gifto = 100,000 GTO = 100,000,00000 unit
-        // 3000 GTO = 1ETH => maximumETH = 100,000,00000 / _originalBuyPrice
-        // 100,000,00000/3000 0000 ~ 33ETH => change to wei
-        _maximumBuy = 10**18 * 10000000000 /_originalBuyPrice;
-    }
-        
-    /// @dev Gets account's balance
-    /// @param _addr Address of the account
-    /// @return Account balance
-    function balanceOf(address _addr) 
-        public
-        constant 
-        returns (uint256) {
-        return balances[_addr];
-    }
-    
-    /// @dev check address is approved investor
-    /// @param _addr address
-    function isApprovedInvestor(address _addr)
-        public
-        constant
-        returns (bool) {
-        return approvedInvestorList[_addr];
-    }
-    
-    /// @dev get investors deposited
-    function getBuyers()
-    public
-    constant
-    returns(address[]){
-        return buyers;
-    }
-    
-    /// @dev get ETH deposit
-    /// @param _addr address get deposit
-    /// @return amount deposit of an buyer
-    function getDeposit(address _addr)
-        public
-        constant
-        returns(uint256){
-        return deposit[_addr];
-    }
-    
-    /// @dev Adds list of new investors to the investors list and approve all
-    /// @param newInvestorList Array of new investors addresses to be added
-    function addInvestorList(address[] newInvestorList)
-        onlyOwner
-        public {
-        for (uint256 i = 0; i < newInvestorList.length; i++){
-            approvedInvestorList[newInvestorList[i]] = true;
-        }
-    }
-
-    /// @dev Removes list of investors from list
-    /// @param investorList Array of addresses of investors to be removed
-    function removeInvestorList(address[] investorList)
-        onlyOwner
-        public {
-        for (uint256 i = 0; i < investorList.length; i++){
-            approvedInvestorList[investorList[i]] = false;
-        }
-    }
-    
-    /// @dev delivery token for buyer
-    /// @param a start point
-    /// @param b end point
-    function deliveryToken(uint256 a, uint256 b)
-        public
-        onlyOwner
-        validRange(a, b) {
-        // make sure balances owner greater than _icoSupply
-        require(balances[owner] >= _icoSupply);
-        for (uint256 i = a; i <= b; i++){
-            if(approvedInvestorList[buyers[i]]){
-                // compute amount token of each buyer
-                uint256 requestedUnits = (deposit[buyers[i]] * _originalBuyPrice) / 10**18;
-                
-                //check requestedUnits > _icoSupply
-                if(requestedUnits <= _icoSupply && requestedUnits > 0 ){
-                    // prepare transfer data
-                    balances[owner] -= requestedUnits;
-                    balances[buyers[i]] += requestedUnits;
-                    _icoSupply -= requestedUnits;
-                    
-                    // submit transfer
-                    Transfer(owner, buyers[i], requestedUnits);
-                    
-                    // reset deposit of buyer
-                    deposit[buyers[i]] = 0;
-                }
-            }
-        }
-    }
- 
-    /// @dev Transfers the balance from Multisig wallet to an account
-    /// @param _to Recipient address
-    /// @param _amount Transfered amount in unit
-    /// @return Transfer status
-    function transfer(address _to, uint256 _amount)
-        public 
-        returns (bool) {
-        // if sender's balance has enough unit and amount >= 0, 
-        //      and the sum is not overflow,
-        // then do transfer 
-        if ( (balances[msg.sender] >= _amount) &&
-             (_amount >= 0) && 
-             (balances[_to] + _amount > balances[_to]) ) {  
-
-            balances[msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(msg.sender, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
-     
-    // Send _value amount of tokens from address _from to address _to
-    // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-    // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-    // fees in sub-currencies; the command should fail unless the _from account has
-    // deliberately authorized the sender of the message via some mechanism; we propose
-    // these standardized APIs for approval:
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    )
-    public
-    returns (bool success) {
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
-            balances[_from] -= _amount;
-            allowed[_from][msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(_from, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) 
-        public
-        returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-    
-    // get allowance
-    function allowance(address _owner, address _spender) 
-        public
-        constant 
-        returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-    
-    /// @dev Withdraws Ether in contract (Owner only)
-    /// @return Status of withdrawal
-    function withdraw() onlyOwner 
-        public 
-        returns (bool) {
-        return owner.send(this.balance);
-    }
-}
 
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
 contract MultiSigWallet {
 
-    uint constant public MAX_OWNER_COUNT = 50;
-
+    /*
+     *  Events
+     */
     event Confirmation(address indexed sender, uint indexed transactionId);
     event Revocation(address indexed sender, uint indexed transactionId);
     event Submission(uint indexed transactionId);
@@ -388,15 +20,21 @@ contract MultiSigWallet {
     event OwnerAddition(address indexed owner);
     event OwnerRemoval(address indexed owner);
     event RequirementChange(uint required);
-    event CoinCreation(address coin);
 
+    /*
+     *  Constants
+     */
+    uint constant public MAX_OWNER_COUNT = 50;
+
+    /*
+     *  Storage
+     */
     mapping (uint => Transaction) public transactions;
     mapping (uint => mapping (address => bool)) public confirmations;
     mapping (address => bool) public isOwner;
     address[] public owners;
     uint public required;
     uint public transactionCount;
-    bool flag = true;
 
     struct Transaction {
         address destination;
@@ -405,51 +43,54 @@ contract MultiSigWallet {
         bool executed;
     }
 
+    /*
+     *  Modifiers
+     */
     modifier onlyWallet() {
         if (msg.sender != address(this))
-            revert();
+            throw;
         _;
     }
 
     modifier ownerDoesNotExist(address owner) {
         if (isOwner[owner])
-            revert();
+            throw;
         _;
     }
 
     modifier ownerExists(address owner) {
         if (!isOwner[owner])
-            revert();
+            throw;
         _;
     }
 
     modifier transactionExists(uint transactionId) {
         if (transactions[transactionId].destination == 0)
-            revert();
+            throw;
         _;
     }
 
     modifier confirmed(uint transactionId, address owner) {
         if (!confirmations[transactionId][owner])
-            revert();
+            throw;
         _;
     }
 
     modifier notConfirmed(uint transactionId, address owner) {
         if (confirmations[transactionId][owner])
-            revert();
+            throw;
         _;
     }
 
     modifier notExecuted(uint transactionId) {
         if (transactions[transactionId].executed)
-            revert();
+            throw;
         _;
     }
 
     modifier notNull(address _address) {
         if (_address == 0)
-            revert();
+            throw;
         _;
     }
 
@@ -458,7 +99,7 @@ contract MultiSigWallet {
             || _required > ownerCount
             || _required == 0
             || ownerCount == 0)
-            revert();
+            throw;
         _;
     }
 
@@ -482,7 +123,7 @@ contract MultiSigWallet {
     {
         for (uint i=0; i<_owners.length; i++) {
             if (isOwner[_owners[i]] || _owners[i] == 0)
-                revert();
+                throw;
             isOwner[_owners[i]] = true;
         }
         owners = _owners;
@@ -524,7 +165,7 @@ contract MultiSigWallet {
 
     /// @dev Allows to replace an owner with a new owner. Transaction has to be sent by wallet.
     /// @param owner Address of owner to be replaced.
-    /// @param owner Address of new owner.
+    /// @param newOwner Address of new owner.
     function replaceOwner(address owner, address newOwner)
         public
         onlyWallet
@@ -595,6 +236,8 @@ contract MultiSigWallet {
     /// @param transactionId Transaction ID.
     function executeTransaction(uint transactionId)
         public
+        ownerExists(msg.sender)
+        confirmed(transactionId, msg.sender)
         notExecuted(transactionId)
     {
         if (isConfirmed(transactionId)) {
@@ -736,15 +379,5 @@ contract MultiSigWallet {
         _transactionIds = new uint[](to - from);
         for (i=from; i<to; i++)
             _transactionIds[i - from] = transactionIdsTemp[i];
-    }
-    
-    /// @dev Create new coin.
-    function createCoin()
-        external
-        onlyWallet
-    {
-        require(flag == true);
-        CoinCreation(new GTO());
-        flag = false;
     }
 }
