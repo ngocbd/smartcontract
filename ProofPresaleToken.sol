476c4c22cebae0360a104bc7423c42d971f4526b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:27:54 +0700

    add contract ProofPresaleToken

--- ProofPresaleToken.sol
+++ ProofPresaleToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofPresaleToken at 0xbF43a143881De80700f63a7da29819d1b0A7d027
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofPresaleToken at 0x2469f31a34fcaac0debf73806ce39b2388874b13
 */
 pragma solidity ^0.4.11;
 
@@ -11,14 +11,14 @@ contract ERC20 {
 
   uint256 public totalSupply;
 
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  function allowance(address owner, address spender) constant returns (uint256);
+  function balanceOf(address _owner) constant returns (uint256);
+  function transfer(address _to, uint256 _value) returns (bool);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool);
+  function approve(address _spender, uint256 _value) returns (bool);
+  function allowance(address _owner, address _spender) constant returns (uint256);
   
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 }
 
@@ -111,9 +111,9 @@ contract ProofPresaleToken is ERC20, Ownable {
   mapping(address => uint) balances;
   mapping (address => mapping (address => uint)) allowed;
 
-  string public name = "Proof Presale Token";
-  string public symbol = "PROOFP";
-  uint8 public decimals = 18;
+  string public constant name = "Proof Presale Token";
+  string public constant symbol = "PPT";
+  uint8 public constant decimals = 18;
   bool public mintingFinished = false;
 
   event Mint(address indexed to, uint256 amount);
@@ -127,6 +127,10 @@ contract ProofPresaleToken is ERC20, Ownable {
     revert();
   }
 
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+    
   function transfer(address _to, uint _value) returns (bool success) {
 
     balances[msg.sender] = balances[msg.sender].sub(_value);
@@ -145,10 +149,6 @@ contract ProofPresaleToken is ERC20, Ownable {
 
     Transfer(_from, _to, _value);
     return true;
-  }
-
-  function balanceOf(address _owner) constant returns (uint balance) {
-    return balances[_owner];
   }
 
   function approve(address _spender, uint _value) returns (bool success) {
