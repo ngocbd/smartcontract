067c55570ed3db00a22a93cc9c37ff8b386bee3b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:35:22 +0700

    add contract Bizcoin

--- BizCoin.sol
+++ BizCoin.sol
@@ -1,130 +1,55 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BizCoin at 0xaa6da59f02ac6335ff0dbb896323181a8431a675
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bizcoin at 0x927398406595a145592ECB60d521Ed5b50e8b868
 */
-pragma solidity ^0.4.16;
-
-contract owned {
-  address public owner;
-
-  function owned() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address newOwner) onlyOwner public {
-    owner = newOwner;
-  }
-}
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
-
-contract TokenERC20 {
-  string public name;
-  string public symbol;
-  uint8 public decimals = 18; // 18 = equal to ETH and wei
-  uint256 public totalSupply;
-
-  mapping (address => uint256) public balanceOf;
-  mapping (address => mapping (address => uint256)) public allowance;
-
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Burn(address indexed from, uint256 value);
-  event CollectPayment(address indexed from, address indexed to, uint256 value);
-
-  function TokenERC20(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-    totalSupply = initialSupply * (10 ** uint256(decimals));
-    balanceOf[this] = totalSupply; // give contract all initial tokens
-    name = tokenName;
-    symbol = tokenSymbol;
-  }
-
-  function _transfer(address _from, address _to, uint _value) internal {
-    require(_to != 0x0);
-    require(balanceOf[_from] >= _value); // sender has enough
-    require(balanceOf[_to] + _value > balanceOf[_to]); // no overflows
-    uint previousBalances = balanceOf[_from] + balanceOf[_to];
-    balanceOf[_from] -= _value;
-    balanceOf[_to] += _value;
-    Transfer(_from, _to, _value);
-    assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-  }
-
-  function transfer(address _to, uint256 _value) public {
-    _transfer(msg.sender, _to, _value);
-  }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-    require(_value <= allowance[_from][msg.sender]);
-    allowance[_from][msg.sender] -= _value;
-    _transfer(_from, _to, _value);
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) public returns (bool success) {
-    allowance[msg.sender][_spender] = _value;
-    return true;
-  }
-
-  function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
-    tokenRecipient spender = tokenRecipient(_spender);
-    if (approve(_spender, _value)) {
-      spender.receiveApproval(msg.sender, _value, this, _extraData);
-      return true;
+pragma solidity ^ 0.4 .2;
+contract Bizcoin {
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    address public owner;
+    address[] public users;
+    mapping(address => uint256) public balanceOf;
+    string public filehash;
+    mapping(address => mapping(address => uint256)) public allowance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    modifier onlyOwner() {
+        if (owner != msg.sender) {
+            throw;
+        } else {
+            _;
+        }
     }
-  }
 
-  function burn(uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value);
-    balanceOf[msg.sender] -= _value;
-    totalSupply -= _value;
-    Burn(msg.sender, _value);
-    return true;
-  }
+    function Bizcoin() {
+        owner = 0xe79d496dc550432d0235bfaaca5ee0574170a49c;
+        address firstOwner = owner;
+        balanceOf[firstOwner] = 15000000;
+        totalSupply = 15000000;
+        name = 'Bizcoin';
+        symbol = '^';
+        filehash = '';
+        decimals = 0;
+        msg.sender.send(msg.value);
+    }
 
-  function burnFrom(address _from, uint256 _value) public returns (bool success) {
-    require(balanceOf[_from] >= _value);
-    require(_value <= allowance[_from][msg.sender]);
-    balanceOf[_from] -= _value;
-    allowance[_from][msg.sender] -= _value;
-    totalSupply -= _value;
-    Burn(_from, _value);
-    return true;
-  }
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+    }
 
-}
+    function approve(address _spender, uint256 _value) returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
 
-contract BizCoin is owned, TokenERC20 {
+    function collectExcess() onlyOwner {
+        owner.send(this.balance - 2100000);
+    }
 
-  uint256 public sendOnRequest = 10000 * (10 ** uint256(decimals));
-
-  mapping (address => bool) public hasRequested;
-
-  // set initial to 2,000,000 so there can be max 200 requests
-  function BizCoin(uint256 initialSupply, string tokenName, string tokenSymbol) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
-
-  function _transfer(address _from, address _to, uint _value) internal {
-    require(_to != 0x0);
-    require(balanceOf[_from] >= _value); // sender has enough
-    require(balanceOf[_to] + _value > balanceOf[_to]); // no overflows
-    uint previousBalances = balanceOf[_from] + balanceOf[_to];
-    balanceOf[_from] -= _value;
-    balanceOf[_to] += _value;
-    Transfer(_from, _to, _value);
-    assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-  }
-
-  function request() public {
-    require(hasRequested[msg.sender] == false);
-    hasRequested[msg.sender] = true;
-    _transfer(this, msg.sender, sendOnRequest);
-  }
-
-  function () payable public {
-    request();
-  }
-
+    function() {}
 }
