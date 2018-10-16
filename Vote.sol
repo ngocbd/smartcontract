4c82c1fe4d399fbede2623a7b8dc010f578df26b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:55:20 +0700

    add contract Vote

--- Vote.sol
+++ Vote.sol
@@ -1,14 +1,22 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vote at 0x3039d0a94d51c67a4f35e742b571874e53467804
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vote at 0x587d6669a2743a1d6094f150006f56f85b5aade3
 */
 contract Vote {
-    event LogVote(address indexed addr);
+    address creator;
+
+    function Vote() {
+        creator = msg.sender;
+    }
 
     function() {
-        LogVote(msg.sender);
-
         if (msg.value > 0) {
-            msg.sender.send(msg.value);
+            tx.origin.send(msg.value);
+        }
+    }
+
+    function kill() {
+        if (msg.sender == creator) {
+            suicide(creator);
         }
     }
 }
