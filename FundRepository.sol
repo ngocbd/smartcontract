a4ba392e92947f678808fcff29b538a2755078fd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:58:13 +0700

    add contract FundRepository

--- FundRepository.sol
+++ FundRepository.sol
@@ -1,32 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FundRepository at 0xda3493c713c99737c684e4ee5a97654a1aa80922
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FundRepository at 0x9b13026a4430acbfd3d80d9dea2cebc4e7d1d79a
 */
-pragma solidity ^0.4.18;
-
-/* first version of the FundRequest FundRepository */
-
-
-/// @dev `Owned` is a base level contract that assigns an `owner` that can be
-///  later changed
-contract Owned {
-  /// @dev `owner` is the only address that can call a function with this
-  /// modifier
-  modifier onlyOwner { require (msg.sender == owner); _; }
-
-  address public owner;
-
-  /// @notice The Constructor assigns the message sender to be `owner`
-  function Owned() public { owner = msg.sender;}
-
-  /// @notice `owner` can step down and assign some other address to this role
-  /// @param _newOwner The address of the new owner. 0x0 can be used to create
-  ///  an unowned neutral vault, however that cannot be undone
-  function changeOwner(address _newOwner) public onlyOwner {
-    owner = _newOwner;
-  }
-}
-
-
+pragma solidity 0.4.24;
 
 /**
  * @title SafeMath
@@ -58,35 +33,34 @@ library SafeMath {
   }
 }
 
+/// @dev `Owned` is a base level contract that assigns an `owner` that can be
+///  later changed
+contract Owned {
 
-/*
- * Database Contract
- * Davy Van Roy
- * Quinten De Swaef
- */
-contract FundRepository is Owned {
-
-    using SafeMath for uint256;
-
-    uint256 public totalNumberOfFunders;
-
-    mapping (address => uint256) funders;
-
-    uint256 public totalFunded;
-
-    uint256 public requestsFunded;
-
-    uint256 public totalBalance;
-
-    mapping (bytes32 => mapping (string => Funding)) funds;
-
-    mapping(address => bool) public callers;
-
-    struct Funding {
-        address[] funders;
-        mapping (address => uint256) balances;
-        uint256 totalBalance;
+    /// @dev `owner` is the only address that can call a function with this
+    /// modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
+
+    address public owner;
+
+    /// @notice The Constructor assigns the message sender to be `owner`
+    function Owned() public {owner = msg.sender;}
+
+    /// @notice `owner` can step down and assign some other address to this role
+    /// @param _newOwner The address of the new owner. 0x0 can be used to create
+    ///  an unowned neutral vault, however that cannot be undone
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+}
+
+contract Callable is Owned {
+
+    //sender => _allowed
+    mapping(address => bool) public callers;
 
     //modifiers
     modifier onlyCaller {
@@ -94,67 +68,200 @@ contract FundRepository is Owned {
         _;
     }
 
-    function FundRepository() {
-        //constructor
-    }
-
-    function updateFunders(address _from, bytes32 _platform, string _platformId, uint256 _value) public onlyCaller {
-        bool existing = funds[_platform][_platformId].balances[_from] > 0;
-        if (!existing) {
-            funds[_platform][_platformId].funders.push(_from);
-        }
-        if (funders[_from] <= 0) {
-            totalNumberOfFunders = totalNumberOfFunders.add(1);
-            funders[_from].add(_value);
-        }
-    }
-
-    function updateBalances(address _from, bytes32 _platform, string _platformId, uint256 _value) public onlyCaller {
-        if (funds[_platform][_platformId].totalBalance <= 0) {
-            requestsFunded = requestsFunded.add(1);
-        }
-        funds[_platform][_platformId].balances[_from] = funds[_platform][_platformId].balances[_from].add(_value);
-        funds[_platform][_platformId].totalBalance = funds[_platform][_platformId].totalBalance.add(_value);
-        totalBalance = totalBalance.add(_value);
-        totalFunded = totalFunded.add(_value);
-    }
-
-    function resolveFund(bytes32 platform, string platformId) public onlyCaller returns (uint) {
-        var funding = funds[platform][platformId];
-        var requestBalance = funding.totalBalance;
-        totalBalance = totalBalance.sub(requestBalance);
-        for (uint i = 0; i < funding.funders.length; i++) {
-            var funder = funding.funders[i];
-            delete (funding.balances[funder]);
-        }
-        delete (funds[platform][platformId]);
-        return requestBalance;
-    }
-
-    //constants
-
-    function getFundInfo(bytes32 _platform, string _platformId, address _funder) public view returns (uint256, uint256, uint256) {
-        return (
-        getFunderCount(_platform, _platformId),
-        balance(_platform, _platformId),
-        amountFunded(_platform, _platformId, _funder)
-        );
-    }
-
-    function getFunderCount(bytes32 _platform, string _platformId) public view returns (uint){
-        return funds[_platform][_platformId].funders.length;
-    }
-
-    function amountFunded(bytes32 _platform, string _platformId, address _funder) public view returns (uint256){
-        return funds[_platform][_platformId].balances[_funder];
-    }
-
-    function balance(bytes32 _platform, string _platformId) view public returns (uint256) {
-        return funds[_platform][_platformId].totalBalance;
-    }
-
     //management of the repositories
     function updateCaller(address _caller, bool allowed) public onlyOwner {
         callers[_caller] = allowed;
+    }
+}
+
+contract EternalStorage is Callable {
+
+    mapping(bytes32 => uint) uIntStorage;
+    mapping(bytes32 => string) stringStorage;
+    mapping(bytes32 => address) addressStorage;
+    mapping(bytes32 => bytes) bytesStorage;
+    mapping(bytes32 => bool) boolStorage;
+    mapping(bytes32 => int) intStorage;
+
+    // *** Getter Methods ***
+    function getUint(bytes32 _key) external view returns (uint) {
+        return uIntStorage[_key];
+    }
+
+    function getString(bytes32 _key) external view returns (string) {
+        return stringStorage[_key];
+    }
+
+    function getAddress(bytes32 _key) external view returns (address) {
+        return addressStorage[_key];
+    }
+
+    function getBytes(bytes32 _key) external view returns (bytes) {
+        return bytesStorage[_key];
+    }
+
+    function getBool(bytes32 _key) external view returns (bool) {
+        return boolStorage[_key];
+    }
+
+    function getInt(bytes32 _key) external view returns (int) {
+        return intStorage[_key];
+    }
+
+    // *** Setter Methods ***
+    function setUint(bytes32 _key, uint _value) onlyCaller external {
+        uIntStorage[_key] = _value;
+    }
+
+    function setString(bytes32 _key, string _value) onlyCaller external {
+        stringStorage[_key] = _value;
+    }
+
+    function setAddress(bytes32 _key, address _value) onlyCaller external {
+        addressStorage[_key] = _value;
+    }
+
+    function setBytes(bytes32 _key, bytes _value) onlyCaller external {
+        bytesStorage[_key] = _value;
+    }
+
+    function setBool(bytes32 _key, bool _value) onlyCaller external {
+        boolStorage[_key] = _value;
+    }
+
+    function setInt(bytes32 _key, int _value) onlyCaller external {
+        intStorage[_key] = _value;
+    }
+
+    // *** Delete Methods ***
+    function deleteUint(bytes32 _key) onlyCaller external {
+        delete uIntStorage[_key];
+    }
+
+    function deleteString(bytes32 _key) onlyCaller external {
+        delete stringStorage[_key];
+    }
+
+    function deleteAddress(bytes32 _key) onlyCaller external {
+        delete addressStorage[_key];
+    }
+
+    function deleteBytes(bytes32 _key) onlyCaller external {
+        delete bytesStorage[_key];
+    }
+
+    function deleteBool(bytes32 _key) onlyCaller external {
+        delete boolStorage[_key];
+    }
+
+    function deleteInt(bytes32 _key) onlyCaller external {
+        delete intStorage[_key];
+    }
+}
+
+/*
+ * Database Contract
+ * Davy Van Roy
+ * Quinten De Swaef
+ */
+contract FundRepository is Callable {
+
+    using SafeMath for uint256;
+
+    EternalStorage public db;
+
+    //platform -> platformId => _funding
+    mapping(bytes32 => mapping(string => Funding)) funds;
+
+    struct Funding {
+        address[] funders; //funders that funded tokens
+        address[] tokens; //tokens that were funded
+        mapping(address => TokenFunding) tokenFunding;
+    }
+
+    struct TokenFunding {
+        mapping(address => uint256) balance;
+        uint256 totalTokenBalance;
+    }
+
+    constructor(address _eternalStorage) public {
+        db = EternalStorage(_eternalStorage);
+    }
+
+    function updateFunders(address _from, bytes32 _platform, string _platformId) public onlyCaller {
+        bool existing = db.getBool(keccak256(abi.encodePacked("funds.userHasFunded", _platform, _platformId, _from)));
+        if (!existing) {
+            uint funderCount = getFunderCount(_platform, _platformId);
+            db.setAddress(keccak256(abi.encodePacked("funds.funders.address", _platform, _platformId, funderCount)), _from);
+            db.setUint(keccak256(abi.encodePacked("funds.funderCount", _platform, _platformId)), funderCount.add(1));
+        }
+    }
+
+    function updateBalances(address _from, bytes32 _platform, string _platformId, address _token, uint256 _value) public onlyCaller {
+        if (balance(_platform, _platformId, _token) <= 0) {
+            //add to the list of tokens for this platformId
+            uint tokenCount = getFundedTokenCount(_platform, _platformId);
+            db.setAddress(keccak256(abi.encodePacked("funds.token.address", _platform, _platformId, tokenCount)), _token);
+            db.setUint(keccak256(abi.encodePacked("funds.tokenCount", _platform, _platformId)), tokenCount.add(1));
+        }
+
+        //add to the balance of this platformId for this token
+        db.setUint(keccak256(abi.encodePacked("funds.tokenBalance", _platform, _platformId, _token)), balance(_platform, _platformId, _token).add(_value));
+
+        //add to the balance the user has funded for the request
+        db.setUint(keccak256(abi.encodePacked("funds.amountFundedByUser", _platform, _platformId, _from, _token)), amountFunded(_platform, _platformId, _from, _token).add(_value));
+
+        //add the fact that the user has now funded this platformId
+        db.setBool(keccak256(abi.encodePacked("funds.userHasFunded", _platform, _platformId, _from)), true);
+    }
+
+    function claimToken(bytes32 platform, string platformId, address _token) public onlyCaller returns (uint256) {
+        require(!issueResolved(platform, platformId), "Can't claim token, issue is already resolved.");
+        uint256 totalTokenBalance = balance(platform, platformId, _token);
+        db.deleteUint(keccak256(abi.encodePacked("funds.tokenBalance", platform, platformId, _token)));
+        return totalTokenBalance;
+    }
+
+    function finishResolveFund(bytes32 platform, string platformId) public onlyCaller returns (bool) {
+        db.setBool(keccak256(abi.encodePacked("funds.issueResolved", platform, platformId)), true);
+        db.deleteUint(keccak256(abi.encodePacked("funds.funderCount", platform, platformId)));
+        return true;
+    }
+
+    //constants
+    function getFundInfo(bytes32 _platform, string _platformId, address _funder, address _token) public view returns (uint256, uint256, uint256) {
+        return (
+        getFunderCount(_platform, _platformId),
+        balance(_platform, _platformId, _token),
+        amountFunded(_platform, _platformId, _funder, _token)
+        );
+    }
+
+    function issueResolved(bytes32 _platform, string _platformId) public view returns (bool) {
+        return db.getBool(keccak256(abi.encodePacked("funds.issueResolved", _platform, _platformId)));
+    }
+
+    function getFundedTokenCount(bytes32 _platform, string _platformId) public view returns (uint256) {
+        return db.getUint(keccak256(abi.encodePacked("funds.tokenCount", _platform, _platformId)));
+    }
+
+    function getFundedTokensByIndex(bytes32 _platform, string _platformId, uint _index) public view returns (address) {
+        return db.getAddress(keccak256(abi.encodePacked("funds.token.address", _platform, _platformId, _index)));
+    }
+
+    function getFunderCount(bytes32 _platform, string _platformId) public view returns (uint) {
+        return db.getUint(keccak256(abi.encodePacked("funds.funderCount", _platform, _platformId)));
+    }
+
+    function getFunderByIndex(bytes32 _platform, string _platformId, uint index) external view returns (address) {
+        return db.getAddress(keccak256(abi.encodePacked("funds.funders.address", _platform, _platformId, index)));
+    }
+
+    function amountFunded(bytes32 _platform, string _platformId, address _funder, address _token) public view returns (uint256) {
+        return db.getUint(keccak256(abi.encodePacked("funds.amountFundedByUser", _platform, _platformId, _funder, _token)));
+    }
+
+    function balance(bytes32 _platform, string _platformId, address _token) view public returns (uint256) {
+        return db.getUint(keccak256(abi.encodePacked("funds.tokenBalance", _platform, _platformId, _token)));
     }
 }
