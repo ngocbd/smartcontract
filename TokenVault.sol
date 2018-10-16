687ef9f107a2bf5638e38dc7997e8000093bbaa8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:47:00 +0700

    add contract TokenVault

--- TokenVault.sol
+++ TokenVault.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenVault at 0x98831e9ca2874f2b6454d0a263d248444ea0705e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenVault at 0xa071fa153805f2db25874c998ebe058df33adcbd
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
