409852e2713c193d8e4b806100aac3725b31d5a3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:44:45 +0700

    add contract TheLoveContract

---
+++ TheLoveContract.sol
@@ -0,0 +1,12 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TheLoveContract at 0xded4adeb1cfbc2cda51c7824c15c35b70491e3a4
+*/
+pragma solidity 0.4.19;
+
+contract TheLoveContract {
+    
+    bytes16 public loveEvent = "Valentine's Day";
+    bytes6 public loveName = "Negar";
+    string public loveMessage = "Every morning I wake up, your thoughts come up in my mind and I think solely of you and I realize how much I love you, Negar. You're someone I wish to have in my arms. I yearn for the time when I'll be able to hold you tight in my arms. I wish to come to you soon and be with you for ever. I love you so much, Negar.";
+
+}
