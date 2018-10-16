70e6f337d7d2088621655922a5884c224f8e70dd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:44:26 +0700

    add contract Airdropper

--- Airdropper.sol
+++ Airdropper.sol
@@ -1,140 +1,30 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x05d90ae5e319d1747f9f79fea0af125a968cf475
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0xe6137325d896a5faad6f362d70a404445ddff77c
 */
-pragma solidity 0.4.18;
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-    /**
-     * @dev Multiplies two numbers, throws on overflow.
-     */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    /**
-     * @dev Integer division of two numbers, truncating the quotient.
-     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-     * @dev Adds two numbers, throws on overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+pragma solidity ^0.4.24;
+contract ERC20 {
+    uint public totalSupply;
+    function balanceOf(address _owner) constant public returns (uint balance);
+    function transfer(address _to, uint _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
+    function approve(address _spender, uint _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint remaining);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    function Ownable() public {
-        owner = msg.sender;
+contract Airdropper
+{
+  function multisend(address _tokenAddr, address[] addr, uint256[] values) public
+  {
+    require(addr.length == values.length && addr.length > 0);
+    uint256 i=0;
+    while(i < addr.length)
+    {
+      require(addr[i] != address(0));
+      require(values[i] > 0);
+      require(ERC20(_tokenAddr).transferFrom(msg.sender, addr[i], values[i]));
+      i++;
     }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-}
-
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-/**
- * @title Airdropper
- * @author C&B
- */
-contract Airdropper is Ownable {
-    using SafeMath for uint;
-
-    ERC20Basic public token;
-    uint public multiplier;
-
-    /// @dev constructor
-    /// @param tokenAddress Address of token contract
-    function Airdropper(address tokenAddress, uint decimals) public {
-        require(decimals <= 77);  // 10**77 < 2**256-1 < 10**78
-
-        token = ERC20Basic(tokenAddress);
-        multiplier = 10**decimals;
-    }
-
-    /// @dev Airdrops some tokens to some accounts.
-    /// @param dests List of account addresses.
-    /// @param values List of token amounts.
-    function airdrop(address[] dests, uint[] values) public onlyOwner {
-        require(dests.length == values.length);
-
-        for (uint256 i = 0; i < dests.length; i++) {
-            token.transfer(dests[i], values[i].mul(multiplier));
-        }
-    }
-
-    /// @dev Return all remaining tokens back to owner.
-    function returnTokens() public onlyOwner {
-        token.transfer(owner, token.balanceOf(this));
-    }
+  }
 }
