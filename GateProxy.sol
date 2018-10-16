0c37ca4bc85871b489d4055df7c62dfb45cb1cf8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:12:06 +0700

    add contract GateProxy

--- GateProxy.sol
+++ GateProxy.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GateProxy at 0x4bfad6fb23edcfa65bb8b8bf7a33499766404f52
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GateProxy at 0x6817178368e30a0cd92662442a966b428e8f2d49
 */
 pragma solidity ^0.4.0;
 contract theCyberGatekeeper {
@@ -7,7 +7,7 @@ contract theCyberGatekeeper {
 }
 
 contract GateProxy {
-    function enter(bytes32 _passcode, bytes8 _gateKey, uint16 _gas) public {
-        theCyberGatekeeper(0x44919b8026f38D70437A8eB3BE47B06aB1c3E4Bf).enter.gas(_gas)(_passcode, _gateKey);
+    function enter(bytes32 _passcode, bytes8 _gateKey) public {
+        theCyberGatekeeper(0x44919b8026f38D70437A8eB3BE47B06aB1c3E4Bf).enter.gas(81910)(_passcode, _gateKey);
     }
 }
