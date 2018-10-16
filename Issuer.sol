6cfb7cba3900e40ada25d0986ef8504e41eec18e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:45:33 +0700

    add contract Issuer

--- Issuer.sol
+++ Issuer.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Issuer at 0xd7fdc3818a99df8c1aeaded49c3a22b8fd970a5e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Issuer at 0x6ddca767d731f57d90a516dd751f2aa9282b37d2
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
