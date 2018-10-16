cd66d9bc96fa58611b0891e10323168033bb598d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:58:22 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,355 +1,228 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x06441deaf11d60d77e5e42d4f644c64ca05c2fce
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xca8ce7370e981637945cda630ace6f5994d7efd6
 */
-pragma solidity ^0.4.23;
-pragma experimental "v0.5.0";
-/*
-  This file is part of The Colony Network.
+pragma solidity ^0.4.15;
 
-  The Colony Network is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  The Colony Network is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with The Colony Network. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
-
-
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
-contract DSAuthority {
-    function canCall(
-        address src, address dst, bytes4 sig
-    ) public view returns (bool);
-}
-
-contract DSAuthEvents {
-    event LogSetAuthority (address indexed authority);
-    event LogSetOwner     (address indexed owner);
-}
-
-contract DSAuth is DSAuthEvents {
-    DSAuthority  public  authority;
-    address      public  owner;
-
-    constructor() public {
+contract Token {
+    
+    mapping (address => uint256) public balanceOf;
+    mapping (uint256 => address) public addresses;
+    mapping (address => bool) public addressExists;
+    mapping (address => uint256) public addressIndex;
+    mapping(address => mapping (address => uint256)) allowed;
+    uint256 public numberOfAddress = 0;
+    
+    string public physicalString;
+    string public cryptoString;
+    
+    bool public isSecured;
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    bool public canMintBurn;
+    uint256 public txnTax;
+    uint256 public holdingTax;
+    //In Weeks, on Fridays
+    uint256 public holdingTaxInterval;
+    uint256 public lastHoldingTax;
+    uint256 public holdingTaxDecimals = 2;
+    bool public isPrivate;
+    
+    address public owner;
+    
+    function Token(string n, string a, uint256 totalSupplyToUse, bool isSecured, bool cMB, string physical, string crypto, uint256 txnTaxToUse, uint256 holdingTaxToUse, uint256 holdingTaxIntervalToUse, bool isPrivateToUse) {
+        name = n;
+        symbol = a;
+        totalSupply = totalSupplyToUse;
+        balanceOf[msg.sender] = totalSupplyToUse;
+        isSecured = isSecured;
+        physicalString = physical;
+        cryptoString = crypto;
+        canMintBurn = cMB;
         owner = msg.sender;
-        emit LogSetOwner(msg.sender);
+        txnTax = txnTaxToUse;
+        holdingTax = holdingTaxToUse;
+        holdingTaxInterval = holdingTaxIntervalToUse;
+        if(holdingTaxInterval!=0) {
+            lastHoldingTax = now;
+            while(getHour(lastHoldingTax)!=21) {
+                lastHoldingTax -= 1 hours;
+            }
+            while(getWeekday(lastHoldingTax)!=5) {
+                lastHoldingTax -= 1 days;
+            }
+            lastHoldingTax -= getMinute(lastHoldingTax) * (1 minutes) + getSecond(lastHoldingTax) * (1 seconds);
+        }
+        isPrivate = isPrivateToUse;
+        
+        addAddress(owner);
     }
-
-    function setOwner(address owner_)
-        public
-        auth
-    {
-        owner = owner_;
-        emit LogSetOwner(owner);
-    }
-
-    function setAuthority(DSAuthority authority_)
-        public
-        auth
-    {
-        authority = authority_;
-        emit LogSetAuthority(authority);
-    }
-
-    modifier auth {
-        require(isAuthorized(msg.sender, msg.sig));
-        _;
-    }
-
-    function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
-        if (src == address(this)) {
-            return true;
-        } else if (src == owner) {
-            return true;
-        } else if (authority == DSAuthority(0)) {
+    
+    function transfer(address _to, uint256 _value) payable returns (bool success) {
+        chargeHoldingTax();
+        if (balanceOf[msg.sender] < _value) return false;
+        if (balanceOf[_to] + _value < balanceOf[_to]) return false;
+        if (msg.sender != owner && _to != owner && txnTax != 0) {
+            if(!owner.send(txnTax)) {
+                return false;
+            }
+        }
+        if(isPrivate && msg.sender != owner && !addressExists[_to]) {
             return false;
+        }
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        addAddress(_to);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function transferFrom(
+         address _from,
+         address _to,
+         uint256 _amount
+     ) payable returns (bool success) {
+        if (_from != owner && _to != owner && txnTax != 0) {
+            if(!owner.send(txnTax)) {
+                return false;
+            }
+        }
+        if(isPrivate && _from != owner && !addressExists[_to]) {
+            return false;
+        }
+        if (balanceOf[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+            && _amount > 0
+            && balanceOf[_to] + _amount > balanceOf[_to]) {
+            balanceOf[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            balanceOf[_to] += _amount;
+            Transfer(_from, _to, _amount);
+            return true;
         } else {
-            return authority.canCall(src, this, sig);
+            return false;
         }
     }
-}
-/// base.sol -- basic ERC20 implementation
-
-// Copyright (C) 2015, 2016, 2017  DappHub, LLC
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
-/// erc20.sol -- API for the ERC20 token standard
-
-// See <https://github.com/ethereum/EIPs/issues/20>.
-
-// This file likely does not meet the threshold of originality
-// required for copyright to apply.  As a result, this is free and
-// unencumbered software belonging to the public domain.
-
-
-
-contract ERC20Events {
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
-}
-
-contract ERC20 is ERC20Events {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address guy) public view returns (uint);
-    function allowance(address src, address guy) public view returns (uint);
-
-    function approve(address guy, uint wad) public returns (bool);
-    function transfer(address dst, uint wad) public returns (bool);
-    function transferFrom(
-        address src, address dst, uint wad
-    ) public returns (bool);
-}
-/// math.sol -- mixin for inline numerical wizardry
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
-contract DSMath {
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x);
+     
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
     }
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x);
+    
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
-    function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x);
+    
+    function changeTxnTax(uint256 _newValue) {
+        if(msg.sender != owner) throw;
+        txnTax = _newValue;
     }
-
-    function min(uint x, uint y) internal pure returns (uint z) {
-        return x <= y ? x : y;
+    
+    function mint(uint256 _value) {
+        if(canMintBurn && msg.sender == owner) {
+            if (balanceOf[msg.sender] + _value < balanceOf[msg.sender]) throw;
+            balanceOf[msg.sender] += _value;
+            totalSupply += _value;
+            Transfer(0, msg.sender, _value);
+        }
     }
-    function max(uint x, uint y) internal pure returns (uint z) {
-        return x >= y ? x : y;
+    
+    function burn(uint256 _value) {
+        if(canMintBurn && msg.sender == owner) {
+            if (balanceOf[msg.sender] < _value) throw;
+            balanceOf[msg.sender] -= _value;
+            totalSupply -= _value;
+            Transfer(msg.sender, 0, _value);
+        }
     }
-    function imin(int x, int y) internal pure returns (int z) {
-        return x <= y ? x : y;
-    }
-    function imax(int x, int y) internal pure returns (int z) {
-        return x >= y ? x : y;
-    }
-
-    uint constant WAD = 10 ** 18;
-    uint constant RAY = 10 ** 27;
-
-    function wmul(uint x, uint y) internal pure returns (uint z) {
-        z = add(mul(x, y), WAD / 2) / WAD;
-    }
-    function rmul(uint x, uint y) internal pure returns (uint z) {
-        z = add(mul(x, y), RAY / 2) / RAY;
-    }
-    function wdiv(uint x, uint y) internal pure returns (uint z) {
-        z = add(mul(x, WAD), y / 2) / y;
-    }
-    function rdiv(uint x, uint y) internal pure returns (uint z) {
-        z = add(mul(x, RAY), y / 2) / y;
-    }
-
-    // This famous algorithm is called "exponentiation by squaring"
-    // and calculates x^n with x as fixed-point and n as regular unsigned.
-    //
-    // It's O(log n), instead of O(n) for naive repeated multiplication.
-    //
-    // These facts are why it works:
-    //
-    //  If n is even, then x^n = (x^2)^(n/2).
-    //  If n is odd,  then x^n = x * x^(n-1),
-    //   and applying the equation for even x gives
-    //    x^n = x * (x^2)^((n-1) / 2).
-    //
-    //  Also, EVM division is flooring and
-    //    floor[(n-1) / 2] = floor[n / 2].
-    //
-    function rpow(uint x, uint n) internal pure returns (uint z) {
-        z = n % 2 != 0 ? x : RAY;
-
-        for (n /= 2; n != 0; n /= 2) {
-            x = rmul(x, x);
-
-            if (n % 2 != 0) {
-                z = rmul(z, x);
+    
+    function chargeHoldingTax() {
+        if(holdingTaxInterval!=0) {
+            uint256 dateDif = now - lastHoldingTax;
+            bool changed = false;
+            while(dateDif >= holdingTaxInterval * (1 weeks)) {
+                changed=true;
+                dateDif -= holdingTaxInterval * (1 weeks);
+                for(uint256 i = 0;i<numberOfAddress;i++) {
+                    if(addresses[i]!=owner) {
+                        uint256 amtOfTaxToPay = ((balanceOf[addresses[i]]) * holdingTax)  / (10**holdingTaxDecimals)/ (10**holdingTaxDecimals);
+                        balanceOf[addresses[i]] -= amtOfTaxToPay;
+                        balanceOf[owner] += amtOfTaxToPay;
+                    }
+                }
+            }
+            if(changed) {
+                lastHoldingTax = now;
+                while(getHour(lastHoldingTax)!=21) {
+                    lastHoldingTax -= 1 hours;
+                }
+                while(getWeekday(lastHoldingTax)!=5) {
+                    lastHoldingTax -= 1 days;
+                }
+                lastHoldingTax -= getMinute(lastHoldingTax) * (1 minutes) + getSecond(lastHoldingTax) * (1 seconds);
             }
         }
     }
-}
-
-contract DSTokenBase is ERC20, DSMath {
-    uint256                                            _supply;
-    mapping (address => uint256)                       _balances;
-    mapping (address => mapping (address => uint256))  _approvals;
-
-    constructor(uint supply) public {
-        _balances[msg.sender] = supply;
-        _supply = supply;
+    
+    function changeHoldingTax(uint256 _newValue) {
+        if(msg.sender != owner) throw;
+        holdingTax = _newValue;
     }
-
-    function totalSupply() public view returns (uint) {
-        return _supply;
+    
+    function changeHoldingTaxInterval(uint256 _newValue) {
+        if(msg.sender != owner) throw;
+        holdingTaxInterval = _newValue;
     }
-    function balanceOf(address src) public view returns (uint) {
-        return _balances[src];
-    }
-    function allowance(address src, address guy) public view returns (uint) {
-        return _approvals[src][guy];
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
-    }
-
-    function transferFrom(address src, address dst, uint wad)
-        public
-        returns (bool)
-    {
-        if (src != msg.sender) {
-            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+    
+    function addAddress (address addr) private {
+        if(!addressExists[addr]) {
+            addressIndex[addr] = numberOfAddress;
+            addresses[numberOfAddress++] = addr;
+            addressExists[addr] = true;
         }
-
-        _balances[src] = sub(_balances[src], wad);
-        _balances[dst] = add(_balances[dst], wad);
-
-        emit Transfer(src, dst, wad);
-
-        return true;
+    }
+    
+    function addAddressManual (address addr) {
+        if(msg.sender == owner && isPrivate) {
+            addAddress(addr);
+        } else {
+            throw;
+        }
+    }
+    
+    function removeAddress (address addr) private {
+        if(addressExists[addr]) {
+            numberOfAddress--;
+            addresses[addressIndex[addr]] = 0x0;
+            addressExists[addr] = false;
+        }
+    }
+    
+    function removeAddressManual (address addr) {
+        if(msg.sender == owner && isPrivate) {
+            removeAddress(addr);
+        } else {
+            throw;
+        }
+    }
+    
+    function getWeekday(uint timestamp) returns (uint8) {
+            return uint8((timestamp / 86400 + 4) % 7);
+    }
+    
+    function getHour(uint timestamp) returns (uint8) {
+            return uint8((timestamp / 60 / 60) % 24);
     }
 
-    function approve(address guy, uint wad) public returns (bool) {
-        _approvals[msg.sender][guy] = wad;
-
-        emit Approval(msg.sender, guy, wad);
-
-        return true;
+    function getMinute(uint timestamp) returns (uint8) {
+            return uint8((timestamp / 60) % 60);
     }
-}
-/*
-  This file is part of The Colony Network.
 
-  The Colony Network is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  The Colony Network is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with The Colony Network. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
-
-
-
-
-
-contract ERC20Extended is ERC20 {
-  event Mint(address indexed guy, uint wad);
-  event Burn(address indexed guy, uint wad);
-
-  function mint(uint wad) public;
-  
-  function burn(uint wad) public;
-}
-
-
-contract Token is DSTokenBase(0), DSAuth, ERC20Extended {
-  uint8 public decimals;
-  string public symbol;
-  string public name;
-
-  bool public locked;
-
-  modifier unlocked {
-    if (locked) {
-      require(isAuthorized(msg.sender, msg.sig));
+    function getSecond(uint timestamp) returns (uint8) {
+            return uint8(timestamp % 60);
     }
-    _;
-  }
 
-  constructor(string _name, string _symbol, uint8 _decimals) public {
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
-    locked = true;
-  }
-
-  function transferFrom(address src, address dst, uint wad) public 
-  unlocked
-  returns (bool)
-  {
-    return super.transferFrom(src, dst, wad);
-  }
-
-  function mint(uint wad) public
-  auth
-  {
-    _balances[msg.sender] = add(_balances[msg.sender], wad);
-    _supply = add(_supply, wad);
-
-    emit Mint(msg.sender, wad);
-  }
-
-  function burn(uint wad) public {
-    _balances[msg.sender] = sub(_balances[msg.sender], wad);
-    _supply = sub(_supply, wad);
-
-    emit Burn(msg.sender, wad);
-  }
-
-  function unlock() public
-  auth
-  {
-    locked = false;
-  }
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
