0d5c7d7046b5250b1a44656e5b4ad830a0c788e9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:03:26 +0700

    add contract POSC_launches_in_3_minutes

---
+++ POSC_launches_in_3_minutes.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract POSC_launches_in_3_minutes at 0xadc01fbb5477f429b6684b9565070155d781bb13
+*/
+pragma solidity ^0.4.21;
+
+
+
+contract POSC_launches_in_3_minutes{
+
+}
