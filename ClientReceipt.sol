54339d6a80a49efd1dd3f349e957462f02fb0480
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:50:21 +0700

    add contract ClientReceipt

---
+++ ClientReceipt.sol
@@ -0,0 +1,24 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ClientReceipt at 0x16e98489427c86094e67b72d70743783afdd82c3
+*/
+pragma solidity ^0.4.18;
+//Copyright 2018 MiningRigRentals.com
+contract ClientReceipt {
+    event Deposit(address indexed _to, bytes32 indexed _id, uint _value);
+    address public owner;
+    function ClientReceipt() {
+        owner = msg.sender;
+    }
+    function deposit(bytes32 _id) public payable {
+        Deposit(this, _id, msg.value);
+        if(msg.value > 0) {
+            owner.transfer(msg.value);
+        }
+    }
+    function () public payable { 
+        Deposit(this, 0, msg.value);
+        if(msg.value > 0) {
+            owner.transfer(msg.value);
+        }
+    }
+}
