65163dc953fffd23f5d921e9d0c0a1fc5828bcec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:01:28 +0700

    add contract CoinMarketAlert

--- CoinMarketAlert.sol
+++ CoinMarketAlert.sol
@@ -1,9 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinMarketAlert at 0x54c6acad21fcda13f318268335b10d7b809da327
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinMarketAlert at 0xdf05f539d535b0d8b62145b9d5ffeeda6ef720c4
 */
 pragma solidity 0.4.18;
 
-// implement safemath as a library
 library SafeMath {
 
   // We use `pure` bbecause it promises that the value for the function depends ONLY
@@ -125,7 +124,7 @@ contract CoinMarketAlert is Administration {
     mapping (address => uint256) public balances;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approve(address indexed _owner, address indexed _spender, uint256 _amount);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
     event MintTokens(address indexed _minter, uint256 _amountMinted, bool indexed Minted);
     event FreezeTransfers(address indexed _freezer, bool indexed _frozen);
     event ThawTransfers(address indexed _thawer, bool indexed _thawed);
@@ -288,7 +287,7 @@ contract CoinMarketAlert is Administration {
         require(transferCheck(_owner, _receiver, _amount));
         balances[_owner] = balances[_owner].sub(_amount);
         balances[_receiver] = balances[_receiver].add(_amount);
-        allowance[_owner][_receiver] = allowance[_owner][_receiver].sub(_amount);
+        allowance[_owner][msg.sender] = allowance[_owner][msg.sender].sub(_amount);
         Transfer(_owner, _receiver, _amount);
         return true;
     }
@@ -303,7 +302,7 @@ contract CoinMarketAlert is Administration {
         require(_amount > 0);
         require(balances[msg.sender] > 0);
         allowance[msg.sender][_spender] = _amount;
-        Approve(msg.sender, _spender, _amount);
+        Approval(msg.sender, _spender, _amount);
         return true;
     }
 
