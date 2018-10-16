5ab5a6bbbae5fd5675983a8af5828c087e7091c4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:06:13 +0700

    add contract MultiSigWallet

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -1,22 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWallet at 0x371d305f254ae991dbd9eeb55dc0b7f80e75f57e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWallet at 0x771226ea2e5eb2f0d4f373d0f42a63a3aa81cc3b
 */
-pragma solidity ^0.4.15;
-contract IToken {
-    function executeSettingsChange(
-        uint amount, 
-        uint partInvestor,
-        uint partProject, 
-        uint partFounders, 
-        uint blocksPerStage, 
-        uint partInvestorIncreasePerStage,
-        uint maxStages
-    );
-}
+/**
+ * Originally from https://github.com/ConsenSys/MultiSigWallet
+ */
 
 
+
+/// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
+/// @author Stefan George - <stefan.george@consensys.net>
 contract MultiSigWallet {
+
     uint constant public MAX_OWNER_COUNT = 50;
+
     event Confirmation(address indexed sender, uint indexed transactionId);
     event Revocation(address indexed sender, uint indexed transactionId);
     event Submission(uint indexed transactionId);
@@ -26,81 +22,82 @@ contract MultiSigWallet {
     event OwnerAddition(address indexed owner);
     event OwnerRemoval(address indexed owner);
     event RequirementChange(uint required);
+
     mapping (uint => Transaction) public transactions;
     mapping (uint => mapping (address => bool)) public confirmations;
     mapping (address => bool) public isOwner;
     address[] public owners;
     uint public required;
     uint public transactionCount;
-    
-    IToken public token;
-    struct SettingsRequest 
-    {
-        uint amount;
-        uint partInvestor;
-        uint partProject;
-        uint partFounders;
-        uint blocksPerStage;
-        uint partInvestorIncreasePerStage;
-        uint maxStages;
-        bool executed;
-        mapping(address => bool) confirmations;
-    }
-    uint settingsRequestsCount = 0;
-    mapping(uint => SettingsRequest) settingsRequests;
+
     struct Transaction {
         address destination;
         uint value;
         bytes data;
         bool executed;
     }
-    address owner;
+
     modifier onlyWallet() {
-        require(msg.sender == address(this));
+        if (msg.sender != address(this))
+            throw;
         _;
     }
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+
     modifier ownerDoesNotExist(address owner) {
-        require(!isOwner[owner]);
+        if (isOwner[owner])
+            throw;
         _;
     }
+
     modifier ownerExists(address owner) {
-        require(isOwner[owner]);
+        if (!isOwner[owner])
+            throw;
         _;
     }
+
     modifier transactionExists(uint transactionId) {
-        require(transactions[transactionId].destination != 0);
+        if (transactions[transactionId].destination == 0)
+            throw;
         _;
     }
+
     modifier confirmed(uint transactionId, address owner) {
-        require(confirmations[transactionId][owner]);
+        if (!confirmations[transactionId][owner])
+            throw;
         _;
     }
+
     modifier notConfirmed(uint transactionId, address owner) {
-        require(!confirmations[transactionId][owner]);
+        if (confirmations[transactionId][owner])
+            throw;
         _;
     }
+
     modifier notExecuted(uint transactionId) {
-        require(!transactions[transactionId].executed);
+        if (transactions[transactionId].executed)
+            throw;
         _;
     }
+
     modifier notNull(address _address) {
-        require(_address != 0);
+        if (_address == 0)
+            throw;
         _;
     }
+
     modifier validRequirement(uint ownerCount, uint _required) {
-        require(ownerCount < MAX_OWNER_COUNT
-            && _required <= ownerCount
-            && _required != 0
-            && ownerCount != 0);
+        if (   ownerCount > MAX_OWNER_COUNT
+            || _required > ownerCount
+            || _required == 0
+            || ownerCount == 0)
+            throw;
         _;
     }
 
     /// @dev Fallback function allows to deposit ether.
-    function() public payable {
+    function()
+        payable
+    {
         if (msg.value > 0)
             Deposit(msg.sender, msg.value);
     }
@@ -116,122 +113,12 @@ contract MultiSigWallet {
         validRequirement(_owners.length, _required)
     {
         for (uint i=0; i<_owners.length; i++) {
-            require(!isOwner[_owners[i]] && _owners[i] != 0);
+            if (isOwner[_owners[i]] || _owners[i] == 0)
+                throw;
             isOwner[_owners[i]] = true;
         }
         owners = _owners;
         required = _required;
-        owner = msg.sender;
-    }
-
-    function setToken(address _token)
-    public
-    onlyOwner
-    {
-        require(token == address(0));
-        
-        token = IToken(_token);
-    }
-
-    //---------------- TGE SETTINGS -----------
-    /// @dev Sends request to change settings
-    /// @return Transaction ID
-    function tgeSettingsChangeRequest(
-        uint amount, 
-        uint partInvestor,
-        uint partProject, 
-        uint partFounders, 
-        uint blocksPerStage, 
-        uint partInvestorIncreasePerStage,
-        uint maxStages
-    ) 
-    public
-    ownerExists(msg.sender)
-    returns (uint _txIndex) 
-    {
-        assert(amount*partInvestor*partProject*blocksPerStage*partInvestorIncreasePerStage*maxStages != 0); //asserting no parameter is zero except partFounders
-        _txIndex = settingsRequestsCount;
-        settingsRequests[_txIndex] = SettingsRequest({
-            amount: amount,
-            partInvestor: partInvestor,
-            partProject: partProject,
-            partFounders: partFounders,
-            blocksPerStage: blocksPerStage,
-            partInvestorIncreasePerStage: partInvestorIncreasePerStage,
-            maxStages: maxStages,
-            executed: false
-        });
-        settingsRequestsCount++;
-        return _txIndex;
-    }
-    /// @dev Allows an owner to confirm a change settings request.
-    /// @param _txIndex Transaction ID.
-    function confirmSettingsChange(uint _txIndex) 
-    public
-    ownerExists(msg.sender) 
-    returns(bool success)
-    {
-        require(settingsRequests[_txIndex].executed == false);
-
-        settingsRequests[_txIndex].confirmations[msg.sender] = true;
-        if(isConfirmedSettingsRequest(_txIndex)){
-            SettingsRequest storage request = settingsRequests[_txIndex];
-            request.executed = true;
-            IToken(token).executeSettingsChange(
-                request.amount, 
-                request.partInvestor,
-                request.partProject,
-                request.partFounders,
-                request.blocksPerStage,
-                request.partInvestorIncreasePerStage,
-                request.maxStages
-            );
-            return true;
-        } else {
-            return false;
-        }
-    }
-    function isConfirmedSettingsRequest(uint transactionId)
-    public
-    constant
-    returns (bool)
-    {
-        uint count = 0;
-        for (uint i = 0; i < owners.length; i++) {
-            if (settingsRequests[transactionId].confirmations[owners[i]])
-                count += 1;
-            if (count == required)
-                return true;
-        }
-        return false;
-    }
-
-    function getSettingChangeConfirmationCount(uint _txIndex)
-        public
-        constant
-        returns (uint count)
-    {
-        for (uint i=0; i<owners.length; i++)
-            if (settingsRequests[_txIndex].confirmations[owners[i]])
-                count += 1;
-    }
-
-    /// @dev Shows what settings were requested in a settings change request
-    function viewSettingsChange(uint _txIndex) 
-    public
-    constant
-    ownerExists(msg.sender)  
-    returns (uint amount, uint partInvestor, uint partProject, uint partFounders, uint blocksPerStage, uint partInvestorIncreasePerStage, uint maxStages) {
-        SettingsRequest storage request = settingsRequests[_txIndex];
-        return (
-            request.amount,
-            request.partInvestor, 
-            request.partProject,
-            request.partFounders,
-            request.blocksPerStage,
-            request.partInvestorIncreasePerStage,
-            request.maxStages
-        );
     }
 
     /// @dev Allows to add a new owner. Transaction has to be sent by wallet.
@@ -247,6 +134,7 @@ contract MultiSigWallet {
         owners.push(owner);
         OwnerAddition(owner);
     }
+
     /// @dev Allows to remove an owner. Transaction has to be sent by wallet.
     /// @param owner Address of owner.
     function removeOwner(address owner)
@@ -265,6 +153,7 @@ contract MultiSigWallet {
             changeRequirement(owners.length);
         OwnerRemoval(owner);
     }
+
     /// @dev Allows to replace an owner with a new owner. Transaction has to be sent by wallet.
     /// @param owner Address of owner to be replaced.
     /// @param owner Address of new owner.
@@ -284,6 +173,7 @@ contract MultiSigWallet {
         OwnerRemoval(owner);
         OwnerAddition(newOwner);
     }
+
     /// @dev Allows to change the number of required confirmations. Transaction has to be sent by wallet.
     /// @param _required Number of required confirmations.
     function changeRequirement(uint _required)
@@ -294,6 +184,7 @@ contract MultiSigWallet {
         required = _required;
         RequirementChange(_required);
     }
+
     /// @dev Allows an owner to submit and confirm a transaction.
     /// @param destination Transaction target address.
     /// @param value Transaction ether value.
@@ -301,42 +192,12 @@ contract MultiSigWallet {
     /// @return Returns transaction ID.
     function submitTransaction(address destination, uint value, bytes data)
         public
-        ownerExists(msg.sender)
-        notNull(destination)
         returns (uint transactionId)
     {
         transactionId = addTransaction(destination, value, data);
         confirmTransaction(transactionId);
     }
 
-    function setFinishedTx(address destination)
-        public
-        ownerExists(msg.sender)
-        notNull(destination)
-        returns(uint transactionId)
-    {
-        transactionId = addTransaction(destination, 0, hex"64f65cc0");
-        confirmTransaction(transactionId);
-    }
-
-    function setLiveTx(address destination)
-        public
-        ownerExists(msg.sender)
-        notNull(destination)
-        returns(uint transactionId)
-    {
-        transactionId = addTransaction(destination, 0, hex"9d0714b2");
-        confirmTransaction(transactionId);
-    }
-
-    function setFreezeTx(address destination)
-        ownerExists(msg.sender)
-        notNull(destination)
-        returns(uint transactionId)
-    {
-        transactionId = addTransaction(destination, 0, hex"2c8cbe40");
-        confirmTransaction(transactionId);
-    }
     /// @dev Allows an owner to confirm a transaction.
     /// @param transactionId Transaction ID.
     function confirmTransaction(uint transactionId)
@@ -349,6 +210,7 @@ contract MultiSigWallet {
         Confirmation(msg.sender, transactionId);
         executeTransaction(transactionId);
     }
+
     /// @dev Allows an owner to revoke a confirmation for a transaction.
     /// @param transactionId Transaction ID.
     function revokeConfirmation(uint transactionId)
@@ -360,6 +222,7 @@ contract MultiSigWallet {
         confirmations[transactionId][msg.sender] = false;
         Revocation(msg.sender, transactionId);
     }
+
     /// @dev Allows anyone to execute a confirmed transaction.
     /// @param transactionId Transaction ID.
     function executeTransaction(uint transactionId)
@@ -377,6 +240,7 @@ contract MultiSigWallet {
             }
         }
     }
+
     /// @dev Returns the confirmation status of a transaction.
     /// @param transactionId Transaction ID.
     /// @return Confirmation status.
@@ -392,8 +256,8 @@ contract MultiSigWallet {
             if (count == required)
                 return true;
         }
-        return false;
     }
+
     /*
      * Internal functions
      */
@@ -404,6 +268,7 @@ contract MultiSigWallet {
     /// @return Returns transaction ID.
     function addTransaction(address destination, uint value, bytes data)
         internal
+        notNull(destination)
         returns (uint transactionId)
     {
         transactionId = transactionCount;
@@ -493,7 +358,7 @@ contract MultiSigWallet {
         uint[] memory transactionIdsTemp = new uint[](transactionCount);
         uint count = 0;
         uint i;
-        for (i=from; i<transactionCount; i++)
+        for (i=0; i<transactionCount; i++)
             if (   pending && !transactions[i].executed
                 || executed && transactions[i].executed)
             {
