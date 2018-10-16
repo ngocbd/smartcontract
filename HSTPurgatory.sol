be469d40d94a9fe8e2f5aa9d152df9663c880022
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:09:35 +0700

    add contract HSTPurgatory

---
+++ HSTPurgatory.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HSTPurgatory at 0x85f1b0178c5c0710aa9da3c4409c731aae0a944a
+*/
+pragma solidity ^0.4.18;
+
+// This is the HST token purgatory, where Horizon State
+// Transferrs tokens that are locked forever.
+contract HSTPurgatory {
+    
+    function() public payable {
+        revert();
+    }
+
+}
