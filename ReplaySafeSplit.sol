72efab7d100d972b7cc340c2c507886236622baf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:56:35 +0700

    add contract ReplaySafeSplit

--- ReplaySafeSplit.sol
+++ ReplaySafeSplit.sol
@@ -1,25 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0x341e790174e3a4d35b65fdc067b6b5634a61caea
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0xE94b04a0FeD112f3664e45adb2B8915693dD5FF3
 */
 contract AmIOnTheFork {
     function forked() constant returns(bool);
 }
 
-/**
- * 
- */
 contract ReplaySafeSplit {
     // Fork oracle to use
     AmIOnTheFork amIOnTheFork = AmIOnTheFork(0x2bd2326c993dfaef84f696526064ff22eba5b362);
 
+    event e(address a);
+	
     // Splits the funds into 2 addresses
     function split(address targetFork, address targetNoFork) returns(bool) {
         if (amIOnTheFork.forked() && targetFork.send(msg.value)) {
+			e(targetFork);
             return true;
         } else if (!amIOnTheFork.forked() && targetNoFork.send(msg.value)) {
+			e(targetNoFork);		
             return true;
         }
-        return false;
+        throw; // don't accept value transfer, otherwise it would be trapped.
     }
 
     // Reject value transfers.
