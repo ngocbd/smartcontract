370af7b734bf0897bbd770c6f8ebebcdf1541038
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:05:48 +0700

    add contract Maurel

---
+++ Maurel.sol
@@ -0,0 +1,52 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Maurel at 0xb44A2acD38F7Ee6aE965AA8d77f4f14849770203
+*/
+pragma solidity ^0.4.2;  
+
+contract Maurel {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Maurel() {
+
+         initialSupply = 5000;
+         name ="Maurel";
+        decimals = 0;
+         symbol = "VM";
+        
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+                                   
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
+    }
+
+   
+
+    
+
+   
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
