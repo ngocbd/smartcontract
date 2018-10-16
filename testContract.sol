0f7a09c0c6128444ee50bdd0f4b77db208e49b6c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:12:47 +0700

    add contract TestContract

--- testContract.sol
+++ testContract.sol
@@ -1,9 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestContract at 0x8A4299721D2b10d2a511112889B72927B4413CBC
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestContract at 0x03ab37e5754f55c965101ba1623b3bc91e819b28
 */
-pragma solidity 0.4.16;
-
-
+pragma solidity ^0.4.0;
 contract TestContract {
-    string public constant VERSION = "0.2.0-test-0.4.16";
+    string name;
+    function getName() public constant returns (string){
+        return name;
+    }
+    function setName(string newName) public {
+        name = newName;
+    }
 }
