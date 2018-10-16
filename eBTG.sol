079681acfbdfd0343e4954cc1a03c155f4509083
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:23:33 +0700

    add contract eBTG

--- eBTG.sol
+++ eBTG.sol
@@ -1,82 +1,89 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EBTG at 0x44e6d9ae9053a16e9311fd9702291c5516804359
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract eBTG at 0xc51c938c4d513780c66c722a41c197d3a89fa9a8
 */
-pragma solidity ^ 0.4 .9;
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+pragma solidity ^0.4.4;
 
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
+    contract ERC20 {
+     function totalSupply() constant returns (uint256 totalSupply);
+     function balanceOf(address _owner) constant returns (uint256 balance);
+     function transfer(address _to, uint256 _value) returns (bool success);
+     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+     function approve(address _spender, uint256 _value) returns (bool success);
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+ }
+  
+  contract eBTG is ERC20 {
+     string public constant symbol = "eBTG";
+     string public constant name = "eBGOLD";
+     uint8 public constant decimals = 8;
+     uint256 _totalSupply = 21000000 * 10**8;
+     
 
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+     address public owner;
+  
+     mapping(address => uint256) balances;
+  
+     mapping(address => mapping (address => uint256)) allowed;
+     
+  
+     function eBTG() {
+         owner = msg.sender;
+         balances[owner] = 21000000 * 10**8;
+     }
+     
+     
+  
+     function totalSupply() constant returns (uint256 totalSupply) {
+         totalSupply = _totalSupply;
+     }
+  
 
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-contract EBTG {
-    using SafeMath
-    for uint256;
-    mapping(address => mapping(address => uint256)) allowed;
-    mapping(address => uint256) balances;
-    uint256 public totalSupply;
-    uint256 public decimals;
-    address public owner;
-    bytes32 public symbol;
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed spender, uint256 value);
-
-    function EBTG() {
-        totalSupply = 21000000;
-        symbol = 'eBTG';
-        owner = 0xe3191ff06d4c6d760e2c55f0f0e4d4137855d3bd;
-        balances[owner] = totalSupply;
-        decimals = 0;
-    }
-
-    function balanceOf(address _owner) constant returns(uint256 balance) {
+     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
-    }
-
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    function transfer(address _to, uint256 _value) returns(bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
-        var _allowance = allowed[_from][msg.sender];
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value) returns(bool) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function() {
-        revert();
+     }
+ 
+     function transfer(address _to, uint256 _amount) returns (bool success) {
+         if (balances[msg.sender] >= _amount 
+            && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[msg.sender] -= _amount;
+             balances[_to] += _amount;
+             Transfer(msg.sender, _to, _amount);
+            return true;
+         } else {
+             return false;
+         }
+     }
+     
+     
+     function transferFrom(
+         address _from,
+         address _to,
+         uint256 _amount
+     ) returns (bool success) {
+         if (balances[_from] >= _amount
+             && allowed[_from][msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[_from] -= _amount;
+             allowed[_from][msg.sender] -= _amount;
+             balances[_to] += _amount;
+             Transfer(_from, _to, _amount);
+             return true;
+         } else {
+            return false;
+         }
+     }
+ 
+     function approve(address _spender, uint256 _amount) returns (bool success) {
+         allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+         return true;
+     }
+  
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+         return allowed[_owner][_spender];
     }
 }
