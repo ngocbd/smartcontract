2f43f9be6b7faa4af1eb27131e5ef6cdda9dca00
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:54:59 +0700

    add contract LastIsMe

--- LastIsMe.sol
+++ LastIsMe.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LastIsMe at 0xCB3Ae1fcb86d208D688dA935A463aecff8EAeb41
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LastIsMe at 0x33A683a7Af55942220e4eFEC514dA11B5dc47996
 */
 // Last is me! Lottery paying the last player
 //
