9090c0d1616192ab95ecfa02888864ca6457ca94
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:31:47 +0700

    add contract OrderChain

--- OrderChain.sol
+++ OrderChain.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OrderChain at 0xdffc8d5468d926e1558b2ad21065ded12920f62a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OrderChain at 0xaa1623f45f1f3830d543501e244c80f40cb43676
 */
 pragma solidity ^0.4.4;
 
@@ -38,8 +38,10 @@ contract Token {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
+    
 }
+
+
 
 contract StandardToken is Token {
 
@@ -87,7 +89,14 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract OrderChain is StandardToken { // CHANGE THIS. Update the contract name.
+
+//name this contract whatever you'd like
+contract OrderChain is StandardToken {
+
+    function () {
+        //if ether is sent to this address, send it back.
+        throw;
+    }
 
     /* Public variables of the token */
 
@@ -97,40 +106,24 @@ contract OrderChain is StandardToken { // CHANGE THIS. Update the contract name.
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   // Token Name
-    uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
-    string public symbol;                 // An identifier: eg SBX, XPR etc..
-    string public version = 'H1.0'; 
-    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
-    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
-    address public fundsWallet;           // Where should the raised ETH go?
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
-    // This is a constructor function 
-    // which means the following function name has to match the contract name declared above
-    function HashnodeTestCoin() {
-        balances[msg.sender] = 100000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "OrderChain";                                   // Set the name for display purposes (CHANGE THIS)
-        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "ORD";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 77;                                      // Set the price of your token for the ICO (CHANGE THIS)
-        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
-    }
+//
+// CHANGE THESE VALUES FOR YOUR TOKEN
+//
 
-    function() payable{
-        totalEthInWei = totalEthInWei + msg.value;
-        uint256 amount = msg.value * unitsOneEthCanBuy;
-        if (balances[fundsWallet] < amount) {
-            return;
-        }
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of OrderChain
 
-        balances[fundsWallet] = balances[fundsWallet] - amount;
-        balances[msg.sender] = balances[msg.sender] + amount;
-
-        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
-
-        //Transfer ether to fundsWallet
-        fundsWallet.transfer(msg.value);                               
+    function OrderChain(
+        ) {
+        balances[msg.sender] = 100000000;               // Give the creator all initial tokens (100000 for example)
+        name = "OrderChain";                                   // Set the name for display purposes
+        decimals = 18;                            // Amount of decimals for display purposes
+        symbol = "ORD";                               // Set the symbol for display purposes
+		totalSupply = 1000000000 * 10 ** uint(decimals);                        // Update total supply (100000 for example)
     }
 
     /* Approves and then calls the receiving contract */
