b20d930f475ec97fd9a326d75302461fe4b46ed9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:57:07 +0700

    add contract ERC20BatchTransfer

--- ERC20BatchTransfer.sol
+++ ERC20BatchTransfer.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20BatchTransfer at 0x5b0462bb50a0c4b051e26a01689f45f6ed0ffaab
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20BatchTransfer at 0x35330bd1c1a64ee4ba0c914fca9249654003ea27
 */
 pragma solidity ^0.4.18;
 
@@ -10,33 +10,45 @@ pragma solidity ^0.4.18;
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 {
-  function totalSupply() constant public returns (uint);
+    function totalSupply() constant public returns (uint);
 
-  function balanceOf(address who) constant public returns (uint256);
+    function balanceOf(address who) constant public returns (uint256);
 
-  function transfer(address to, uint256 value) public returns (bool);
+    function transfer(address to, uint256 value) public returns (bool);
 
-  function allowance(address owner, address spender) public constant returns (uint256);
+    function allowance(address owner, address spender) public constant returns (uint256);
 
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
 
-  function approve(address spender, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
 
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
 
 // File: contracts/ERC20BatchTransfer.sol
-
+/**
+ * @author Davy Van Roy
+ */
 contract ERC20BatchTransfer {
 
-  function batchTransfer(address _tokenAddress, address[] _beneficiaries, uint256 _amount) public {
-    require(_amount > 0);
-    ERC20 tokenContract = ERC20(_tokenAddress);
-    for (uint b = 0; b < _beneficiaries.length; b++) {
-      require(tokenContract.transferFrom(msg.sender, _beneficiaries[b], _amount));
+    function batchTransferFixedAmount(address _tokenAddress, address[] _beneficiaries, uint256 _amount) public {
+        require(_amount > 0);
+        ERC20 tokenContract = ERC20(_tokenAddress);
+        for (uint b = 0; b < _beneficiaries.length; b++) {
+            require(tokenContract.transferFrom(msg.sender, _beneficiaries[b], _amount));
+        }
     }
-  }
+
+    function batchTransfer(address _tokenAddress, address[] _beneficiaries, uint256[] _amounts) public {
+        require(_beneficiaries.length == _amounts.length);
+        ERC20 tokenContract = ERC20(_tokenAddress);
+        for (uint b = 0; b < _beneficiaries.length; b++) {
+            if (_amounts[b] > 0) {
+                require(tokenContract.transferFrom(msg.sender, _beneficiaries[b], _amounts[b]));
+            }
+        }
+    }
 
 }
