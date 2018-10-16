812e55f756e37a21c10db3fb4b6da12def964015
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:58:57 +0700

    add contract DragonDistributions

--- DragonDistributions.sol
+++ DragonDistributions.sol
@@ -1,11 +1,11 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DragonDistributions at 0xb08fc1db58ef5e15a6f97d8f63b2bb4a14b9df37
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DragonDistributions at 0x32a44d9D49a05F9049CD7F8eb0E3D19C90DD2421
 */
 pragma solidity ^ 0.4.11;
 
 
 contract Dragon {
-    function transfer( address _to, uint256 _amount );
+    function transfer( address _to, uint256 _amount )returns(bool ok);
 }
 
 
@@ -14,39 +14,70 @@ contract DragonDistributions {
 
     address public dragon;
     uint256 public clock;
+    address public prime;
+    address public film;
+    address public northadvisor;
+    address public southadvisor;
+    
+    uint public filmamount;
+    bool public filmpaid;
+    
     mapping ( address => uint256 ) public  balanceOf;
     mapping ( address => bool ) public  distributionOne;
     mapping ( address => bool ) public  distributionTwo;
     mapping ( address => bool ) public  distributionThree;
     mapping ( address => bool ) public  advisors;
    
+    uint256 public awardAmount       =  45000000000000;
+    uint256 public awardAmountPrime  = 100000000000000;
     
     
     
-    modifier onlyDragon() {
-        if (msg.sender != dragon) {
+    
+    
+    function DragonDistributions () {
+        
+        dragon = 0x814F67fA286f7572B041D041b1D99b432c9155Ee; // Hard code Dragon address
+        prime =  0x243098c1e16973c7e3e969c289c5b87808e359c1; // prime Advisor Address
+        film =   0xdFCf69C8FeD25F5150Db719BAd4EfAb64F628d31;// filmmaker address
+        
+        northadvisor = 0x74Fc8fA4F99b6c19C250E4Fc6952051a95F6060D;
+        southadvisor = 0xCC3c6A89B5b8a054f21bCEff58B6429447cd8e5E;
+        
+        clock = now;
+        
+        filmamount = 2500000000000;
+        
+        balanceOf[ film ] = awardAmount + filmamount; // award amount plus film maker
+        balanceOf[ northadvisor ] = awardAmount;
+        balanceOf[ southadvisor ] = awardAmount;
+        
+        balanceOf[ prime ] = awardAmountPrime;
+        
+        advisors [ film ] = true;
+        advisors [ northadvisor ] = true;
+        advisors [ southadvisor ] = true;
+        
+        filmpaid = false;
+        
+        
+        
+        
+    }
+    
+     modifier onlyPrime() {
+        if (msg.sender != prime) {
             throw;
         }
         _;
     }
-    
-    function DragonDistributions () {
-        
-        dragon = 0x1d1CF6cD3fE91fe4d1533BA3E0b7758DFb59aa1f;
-        clock = now;
-        
-        balanceOf[ 0xdFCf69C8FeD25F5150Db719BAd4EfAb64F628d31 ] = 45000000000000;
-        balanceOf[ 0x74Fc8fA4F99b6c19C250E4Fc6952051a95F6060D ] = 45000000000000;
-        balanceOf[ 0xCC3c6A89B5b8a054f21bCEff58B6429447cd8e5E ] = 45000000000000;
-        
-        advisors [ 0xdFCf69C8FeD25F5150Db719BAd4EfAb64F628d31 ] = true;
-        advisors [ 0x74Fc8fA4F99b6c19C250E4Fc6952051a95F6060D ] = true;
-        advisors [ 0xCC3c6A89B5b8a054f21bCEff58B6429447cd8e5E ] = true;
-        
-        
-        
-    }
 
+    modifier onlyFilm() {
+        if ( msg.sender != film ) {
+            throw;
+        }
+        _;
+    }
 
 
     function withdrawDragons()
@@ -64,7 +95,7 @@ contract DragonDistributions {
             
         }
         
-        if ( distributionTwo[ msg.sender ] == false && now > clock + 80 days ){
+        if ( distributionTwo[ msg.sender ] == false && now > clock + 22 days  ){
             
             
             distributionTwo[ msg.sender ] = true;
@@ -73,7 +104,7 @@ contract DragonDistributions {
             
         }
         
-        if ( distributionThree[ msg.sender ] == false && now > clock + 445 days ){
+        if ( distributionThree[ msg.sender ] == false && now > clock + 387 days ){
             distributionThree[ msg.sender ] = true;
             total += 15000000000000;
             balanceOf[ msg.sender ] -= 15000000000000; 
@@ -81,11 +112,30 @@ contract DragonDistributions {
         }
         
         
-        
-        
         drg.transfer ( msg.sender, total);
+        
+        
     } 
- 
     
+    
+    function withdrawDragonsPrime() onlyPrime {
+        
+         uint _amount = balanceOf[ prime ];
+         balanceOf[ prime ] = 0; 
+         Dragon drg = Dragon ( dragon );
+         drg.transfer ( prime , _amount );
+ 
+    }
+    
+    function withdrawDragonsFilm() onlyFilm {
+        
+        if ( filmpaid == true ) throw;
+         filmpaid = true;
+         uint _amount = filmamount;
+         balanceOf[ film ] -= filmamount; 
+         Dragon drg = Dragon ( dragon );
+         drg.transfer ( film , _amount );
+ 
+    }
     
 }
