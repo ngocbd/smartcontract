d6d81a2f34f36de580a4469ce5fd8b5ea16faeab
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:41:13 +0700

    add contract BtradeWhiteList

---
+++ BtradeWhiteList.sol
@@ -0,0 +1,56 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BtradeWhiteList at 0x839d4bc00744a64af316000c31ecbf26fc07727a
+*/
+pragma solidity ^0.4.24;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  
+    function Ownable() public {
+        owner = msg.sender;
+    }
+    
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0)); 
+        owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
+    }
+}
+
+contract BtradeWhiteList {
+	mapping(address => bool) public whiteList;
+	
+	function BtradeWhiteList() public {
+	
+	}
+	
+	function register(address _address) public {
+        whiteList[msg.sender] = true;
+    }
+
+    function unregister(address _address) public {
+        whiteList[msg.sender] = false;
+    }
+
+    function isRegistered(address _address) public view returns (bool registered) {
+        return whiteList[_address];
+    }
+}
