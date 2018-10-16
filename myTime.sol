6f3decc045751b1480360e4fab7f4e63b50b7cde
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:17:37 +0700

    add contract myTime

---
+++ myTime.sol
@@ -0,0 +1,43 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract myTime at 0xfe489b05dCceEfBbACC7efa27Afe0f22032Aef03
+*/
+pragma solidity ^0.4.11;
+
+contract mortal
+{
+    address owner;
+
+    function mortal() { owner = msg.sender; }
+    function kill() { if(msg.sender == owner) selfdestruct(owner); }
+}
+
+contract myTime is mortal
+{
+    uint deployTime;
+
+    /* Constructor */
+    function myTime() public
+    {
+        deployTime = block.timestamp;
+    }
+
+    function getBlockNumber() constant returns (uint)
+    {
+        return block.number;
+    }
+
+    function getDeployTime() constant returns (uint)
+    {
+        return deployTime;
+    }
+
+    function getBlockTime() constant returns (uint)
+    {
+        return block.timestamp;
+    }
+
+    function getNowTime() constant returns (uint)
+    {
+        return now;
+    }
+}
