b5d784d033c11dc9330ba85b53d094f889a483c1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:11:02 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,294 +1,362 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x9c838f01bc4dc62ec17e1cebc82a356af60b7983
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x1C6C8e0C6B93b81c0aB3cA4E16EAc96f02175F73
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
+
+
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); 
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); 
+    return c;
+  }
 
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  
 }
 
 contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() { require(msg.sender == owner); _; }
-
-    function Ownable() {
-        owner = msg.sender;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
-
-contract Pausable is Ownable {
-    bool public paused = false;
-
-    event Pause();
-    event Unpause();
-
-    modifier whenNotPaused() { require(!paused); _; }
-    modifier whenPaused() { require(paused); _; }
-
-    function pause() onlyOwner whenNotPaused {
-        paused = true;
-        Pause();
-    }
     
-    function unpause() onlyOwner whenPaused {
-        paused = false;
-        Unpause();
-    }
+  address public owner;
+
+  
+  function Ownable() {
+    owner = msg.sender;
+  }
+  
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 }
 
-contract ERC20 {
-    uint256 public totalSupply;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function balanceOf(address who) constant returns (uint256);
-    function transfer(address to, uint256 value) returns (bool);
-    function transferFrom(address from, address to, uint256 value) returns (bool);
-    function allowance(address owner, address spender) constant returns (uint256);
-    function approve(address spender, uint256 value) returns (bool);
-}
-
-contract StandardToken is ERC20 {
+contract Crowdsale is Ownable {
+    
     using SafeMath for uint256;
+    
+    address public multisig;
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
+   
 
-    function balanceOf(address _owner) constant returns(uint256 balance) {
-        return balances[_owner];
+   
+
+   
+ ERC20 public token;
+    uint public startTime;
+    
+  
+    uint public endTime;
+
+    uint256 public hardcap;
+
+    uint public rate;
+    
+    uint public bonusPercent;
+    
+  
+    
+  
+  uint256 public tokensSold = 0;
+
+ 
+  uint256 public weiRaised = 0;
+  
+  
+  uint public investorCount = 0;
+    
+  mapping (address => uint256) public investedAmountOf;
+
+ 
+  mapping (address => uint256) public tokenAmountOf;
+ 
+
+  
+  struct Promo {
+        uint bonus;
+        uint EndTime;
     }
-
-    function transfer(address _to, uint256 _value) returns(bool success) {
-        require(_to != address(0));
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-
-        Transfer(msg.sender, _to, _value);
-
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool success) {
-        require(_to != address(0));
-
-        var _allowance = allowed[_from][msg.sender];
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-
-        Transfer(_from, _to, _value);
-
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    function approve(address _spender, uint256 _value) returns(bool success) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-        allowed[msg.sender][_spender] = _value;
-
-        Approval(msg.sender, _spender, _value);
-
-        return true;
-    }
-
-    function increaseApproval(address _spender, uint _addedValue) returns(bool success) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-
-        return true;
-    }
-
-    function decreaseApproval(address _spender, uint _subtractedValue) returns(bool success) {
-        uint oldValue = allowed[msg.sender][_spender];
-
-        if(_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        
-        return true;
-    }
-}
-
-contract BurnableToken is StandardToken {
-    event Burn(address indexed burner, uint256 value);
-
-    function burn(uint256 _value) public {
-        require(_value > 0);
-
-        address burner = msg.sender;
-
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-
-        Burn(burner, _value);
-    }
-}
-
-contract MintableToken is StandardToken, Ownable {
-    event Mint(address indexed to, uint256 amount);
-    event MintFinished();
-
-    bool public mintingFinished = false;
-    uint public MAX_SUPPLY;
-
-    modifier canMint() { require(!mintingFinished); _; }
-
-    function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool success) {
-        require(totalSupply.add(_amount) <= MAX_SUPPLY);
-
-        totalSupply = totalSupply.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
-
-        Mint(_to, _amount);
-        Transfer(address(0), _to, _amount);
-
-        return true;
-    }
-
-    function finishMinting() onlyOwner public returns(bool success) {
-        mintingFinished = true;
-
-        MintFinished();
-
-        return true;
-    }
-}
-
-/*
-    ICO S Token
-    - ??????? ??????? ?????????? (????? 85 600 000 ???????, ?????? ??????????? ?? ????? ICO ? PreICO)
-    - ???? ?????? ?????????????: 1 ETH = 1250 ???????
-    - ?????? ?? PreICO: +40% ?????? 3 ???, +30% ? 4 ?? 6 ????, +20% ? 7 ?? 9 ???? 
-    - ??????????? ? ???????????? ????? ???????: 0.001 ETH ? 10000 ETH
-    - ??????? ?? ??????? 42 800 000 (50%)
-    - 42 800 000 (50%) ??????? ?????????? ??????? ?? ????? ???????? ??????
-    - ???????? ?? ??????? ??????? ?????????? ???????????
-    - Crowdsale ????????? ?? ???????
-    - ???????? Crowdsale ?????????? ? ??????? ??????? `withdraw()`: ?????????? ??????? ?????????? ???????????
-    - ????? ?????????? ICO ? PreICO ???????? ?????? ??????? `finishMinting()` ? ?????? ????? ??????? ?????? ???????
-*/
-
-contract Token is BurnableToken, MintableToken {
-    string public name = "S Token";
-    string public symbol = "SKK";
-    uint256 public decimals = 18;
-
-    function Token() {
-        MAX_SUPPLY = 85600000 * 1 ether;                                            // Maximum amount tokens
-        mint(0x17D0b1A81f186bfA186b5841F21FC3207Be2Af7C, 42800000 * 1 ether);       // Command mint
-    }
-}
-
-contract Crowdsale is Pausable {
-    using SafeMath for uint;
-
-    Token public token;
-    address public beneficiary = 0x17D0b1A81f186bfA186b5841F21FC3207Be2Af7C;        // Beneficiary
-
-    uint public collectedWei;
-    uint public tokensSold;
-
-    uint public tokensForSale = 42800000 * 1 ether;                                 // Amount tokens for sale
-    uint public priceTokenWei = 1 ether / 1250;
-
-    uint public startTime = 1513252800;                                             // Date start   14.12.2017 12:00 +0
-    uint public endTime = 1514030400;                                               // Date end     23.12.2017 12:00 +0
-    bool public crowdsaleFinished = false;
-
-    event NewContribution(address indexed holder, uint256 tokenAmount, uint256 etherAmount);
-    event Withdraw();
-
+ 
+ mapping (address => Promo) PromoList;
+ mapping (uint=>uint) amountBonus;
+ uint public level_1_amount=50 ether;
+ uint public level_2_amount=100 ether;
+ uint public level_3_amount=250 ether;
+ uint public level_4_amount=500 ether;
+ uint public level_5_amount=1000 ether;
+ uint public level_6_amount=100000 ether;
+ uint public level_7_amount=1000000 ether;
+ uint public level_8_amount=1000000 ether;
+ uint public level_9_amount=1000000 ether;
+ uint public level_1_percent=20;
+ uint public level_2_percent=25;
+ uint public level_3_percent=30;
+ uint public level_4_percent=35;
+ uint public level_5_percent=40;
+ uint public level_6_percent=40;
+ uint public level_7_percent=40;
+ uint public level_8_percent=40;
+ uint public level_9_percent=40;
+ bool public canExchange=true;
     function Crowdsale() {
-        token = new Token();
+        multisig =0x7c27f68b0d5afffb668da3e046adfba6ea1f6bc3;
+     
+       bonusPercent=130;
+        rate =5000;
+        startTime =1510704000;
+        endTime=1513382399;
+     
+        hardcap = 1000000000000000;
+        token=ERC20(0x292317a267adfb97d1b4e3ffd04f9da399cf973b);
+        
     }
 
-    function() payable {
-        purchase();
+
+
+  
+    function setEndTime(uint _endTime) public onlyOwner{
+         require(_endTime>=now&&_endTime>=startTime);
+        endTime=_endTime;
     }
     
-    function purchase() whenNotPaused payable {
-        require(!crowdsaleFinished);
-        require(now >= startTime && now < endTime);
-        require(tokensSold < tokensForSale);
-        require(msg.value >= 0.08 * 1 ether && msg.value <= 100 * 1 ether);
+     function setHardcap(uint256 _hardcap) public onlyOwner{
+       
+        hardcap=_hardcap;
+    }
+    
+   function setPromo(address _address,uint _amount,uint _endtime) public onlyOwner{
+       
+       PromoList[_address].bonus=_amount;
+        PromoList[_address].EndTime=_endtime;
+    }
+     function resetAmountBonuses() public onlyOwner
+     {
+ level_1_amount=0;
+ level_2_amount=0;
+ level_3_amount=0;
+ level_4_amount=0;
+ level_5_amount=0;
+ level_6_amount=0;
+ level_7_amount=0;
+ level_8_amount=0;
+ level_9_amount=0;
+ level_1_percent=0;
+ level_2_percent=0;
+ level_3_percent=0;
+ level_4_percent=0;
+ level_5_percent=0;
+ level_6_percent=0;
+ level_7_percent=0;
+ level_8_percent=0;
+ level_9_percent=0;
+    }
+     function setAmountBonuses(uint _level,uint _amount,uint _percent) public onlyOwner
+     {
+         if (_level==1) 
+         {
+           level_1_amount=(_amount).mul(1 ether);
+          level_1_percent=_percent;
+         }
+        else if (_level==2) 
+         {
+           level_2_amount=_amount.mul(1 ether);
+          level_2_percent=_percent;
+         }
+       else  if (_level==3) 
+         {
+           level_3_amount=_amount.mul(1 ether);
+          level_3_percent=_percent;
+         }
+      else   if (_level==4) 
+         {
+           level_4_amount=_amount.mul(1 ether);
+          level_4_percent=_percent;
+         }
+      else   if (_level==5) 
+         {
+           level_5_amount=_amount.mul(1 ether);
+          level_5_percent=_percent;
+         }
+     else    if (_level==6) 
+         {
+           level_6_amount=_amount.mul(1 ether);
+          level_6_percent=_percent;
+         }
+       else  if (_level==7) 
+         {
+           level_7_amount=_amount.mul(1 ether);
+          level_7_percent=_percent;
+         }
+      else   if (_level==8) 
+         {
+           level_8_amount=_amount.mul(1 ether);
+          level_8_percent=_percent;
+         }
+       else  if (_level==9) 
+         {
+           level_9_amount=_amount.mul(1 ether);
+          level_9_percent=_percent;
+         }
+     }
+ 
+    
 
-        uint sum = msg.value;
-        uint price = priceTokenWei.mul(100).div(
-            now < startTime + 3 days ? 140
-                : (now < startTime + 6 days ? 130
-                    : (now < startTime + 9 days ? 120 : 100))
-        );
-        uint amount = sum.div(price).mul(1 ether);
-        uint retSum = 0;
+    
+    
+    
+    modifier saleIsOn(){
+         require(now > startTime && now <= endTime);
+         _;
+    }
+    
+    modifier isUnderHardCap() {
+   
+       require(tokensSold <= hardcap);
+        _;
+    }
+    
+    modifier isCanExchange(){
+       require(canExchange);
+       _;
+       }
+   
+   function calcToken()
+      
+        returns (uint256)
+    {
+         uint bonus;
+        uint256  tokens=0;
+         bonus=bonusPercent;
+       if (PromoList[msg.sender].EndTime >=now)
+        {
+           bonus += PromoList[msg.sender].bonus; 
+        }
+       
         
-        if(tokensSold.add(amount) > tokensForSale) {
-            uint retAmount = tokensSold.add(amount).sub(tokensForSale);
-            retSum = retAmount.mul(price).div(1 ether);
+           
+            if (msg.value>=level_1_amount && msg.value<level_2_amount )
+            {
+            bonus+=level_1_percent;
+            }
+            else
+             if (msg.value>=level_2_amount && msg.value<level_3_amount )
+            {
+            bonus+=level_2_percent;
+            }
+             else
+             if (msg.value>=level_3_amount && msg.value<level_4_amount )
+            {
+            bonus+=level_3_percent;
+            }
+             else
+             if (msg.value>=level_4_amount && msg.value<level_5_amount )
+            {
+            bonus+=level_4_percent;
+            }
+             else
+             if (msg.value>=level_5_amount && msg.value<level_6_amount )
+            {
+            bonus+=level_5_percent;
+            }
+         else
+             if (msg.value>=level_6_amount && msg.value<level_7_amount )
+            {
+            bonus+=level_6_percent;
+            }
+            else
+             if (msg.value>=level_7_amount && msg.value<level_8_amount )
+            {
+            bonus+=level_7_percent;
+            }
+             else
+             if (msg.value>=level_8_amount && msg.value<level_9_amount )
+            {
+            bonus+=level_8_percent;
+            }
+       else
+             if (msg.value>=level_9_amount)
+            {
+            bonus+=level_9_percent;
+            }
+             uint256 multiplier = 10 **6;
+         tokens = multiplier.mul(msg.value).div(1 ether).mul(rate).div(100).mul(bonus);
+        
+        
+       
+        return tokens;
+    }
+       function exchange() public isCanExchange {
+     // address myAdrress=this;
+     ERC20  oldToken=ERC20(0x12a35383cA24ceb44cdcBBecbEb7baCcB5F3754A);
+    ERC20   newToken=ERC20(0x292317a267AdFb97d1b4E3Ffd04f9Da399cf973b);
+       
 
-            amount = amount.sub(retAmount);
-            sum = sum.sub(retSum);
+     uint  oldTokenAmount=oldToken.balanceOf(msg.sender);
+     //oldToken.approve(myAdrress,oldTokenAmount);
+      oldToken.transferFrom(msg.sender,0x0a6d9df476577C0D4A24EB50220fad007e444db8,oldTokenAmount);
+ newToken.transferFrom(0x0a6d9df476577C0D4A24EB50220fad007e444db8,msg.sender,oldTokenAmount*105/40);
+    
+       
+   }
+    function createTokens() payable saleIsOn isUnderHardCap {
+        
+      
+      
+      uint256 tokens=calcToken();
+        
+         
+        assert (tokens >= 10000);
+    
+        
+       
+       token.transferFrom(0x0a6d9df476577C0D4A24EB50220fad007e444db8,msg.sender, tokens);
+        if(investedAmountOf[msg.sender] == 0) {
+      
+       investorCount++;
         }
-
-        tokensSold = tokensSold.add(amount);
-        collectedWei = collectedWei.add(sum);
-
-        beneficiary.transfer(sum);
-        token.mint(msg.sender, amount);
-
-        if(retSum > 0) {
-            msg.sender.transfer(retSum);
-        }
-
-        NewContribution(msg.sender, amount, sum);
+        investedAmountOf[msg.sender] = investedAmountOf[msg.sender].add(msg.value);
+        tokenAmountOf[msg.sender] = tokenAmountOf[msg.sender].add(tokens);
+        
+        weiRaised = weiRaised.add(msg.value);
+    tokensSold = tokensSold.add(tokens);  
+    
+     multisig.transfer(msg.value);
     }
 
-    function withdraw() onlyOwner {
-        require(!crowdsaleFinished);
-        
-        token.transferOwnership(beneficiary);
-        crowdsaleFinished = true;
-
-        Withdraw();
+    function() external payable {
+        createTokens();
     }
+    
 }
