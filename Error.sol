a06d929b7a7936315199c481c8db72fdc426176b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:25:44 +0700

    add contract Error

---
+++ Error.sol
@@ -0,0 +1,50 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Error at 0xc7787c3b001284d712d86d60e247ab7bb25806c7
+*/
+contract Error {
+  
+    string public standard = '0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
+
+    
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+  
+    
+    function Error() {
+
+         initialSupply = 1000000000;
+         name ="Error";
+        decimals = 1;
+         symbol = "404";
+        
+        balanceOf[msg.sender] = initialSupply;              
+        totalSupply = initialSupply;                        
+                                   
+    }
+
+    
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; 
+        balanceOf[msg.sender] -= _value;                     
+        balanceOf[_to] += _value;                            
+      
+    }
+
+   
+
+    
+
+   
+
+    
+    function () {
+        throw;    
+    }
+}
