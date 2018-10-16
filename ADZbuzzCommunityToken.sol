fba23a6c193677bafa0c145c27e6afdee18be231
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:31:49 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x42070392de7b35c80fce511548c1c6913eefdec5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xae8d4ee446cbb1e99e2eeff77ba37b0404941ae4
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT221251' token contract
+    // 'ACT221823' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT221251
-    // Name        : ADZbuzz Fashionista.com Community Token
+    // Symbol      : ACT221823
+    // Name        : ADZbuzz Quicksprout.com Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT221251";
-            name = "ADZbuzz Fashionista.com Community Token";
+            symbol = "ACT221823";
+            name = "ADZbuzz Quicksprout.com Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
