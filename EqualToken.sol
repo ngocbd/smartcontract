cf8beb2cd40002230656728fc614bf7d22177d5a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:08:56 +0700

    add contract EqualToken

--- EqualToken.sol
+++ EqualToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EqualToken at 0x5824f275dab2c59b8972a1fda45ff404c9a703e3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EqualToken at 0x47dd62d4d075dead71d0e00299fc56a2d747bebb
 */
 pragma solidity ^0.4.18;
 
@@ -40,10 +40,30 @@ contract Token {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     event Burn(address indexed from, uint256 value);
+    event UpdateToken(address _newtoken);
     
+    // Function to set balances from new Token
+    function setBalance(address _to,uint256 _value) external ;
+    
+    // Function to set allowed from new Token
+    function setAllowed(address _spender,address _to,uint256 _value) external;
+    
+    // Function to set total supply from new Token.
+    function setTotalSupply(uint256 _value) external;
+    
+    function getDecimals() constant returns (uint256 decimals);
+    
+    function eventTransfer(address _from, address  _to, uint256 _value) external;
+    function eventApproval(address _owner, address  _spender, uint256 _value) external;
+    function eventBurn(address from, uint256 value) external;
 }
 
-
+contract NewToken{
+    
+    function transfer(address _sender,address _to,uint256 value) returns (bool);
+    function transferFrom(address _sender,address from,address _to,uint256 value) returns (bool);
+    function approve(address _sender,address _spender, uint256 _value) returns (bool success);
+}
 
 library SafeMath {
   function mul(uint256 a, uint256 b) internal constant returns (uint256) {
@@ -81,38 +101,84 @@ library SafeMath {
 
 contract StandardToken is Token {
     using SafeMath for uint256;
+    address newToken=0x0;
+    
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public _totalSupply=0;
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    mapping(address=>bool) internal withoutFee;
-    uint256 internal maxFee;
+    // there is 3 level. 1 - inbound tx, 2 - outbount tx, 3 - all tx;
+    mapping(uint8 =>mapping(address=>bool)) internal whitelist;
+    mapping(address=>uint8) internal whitelistModerator;
+    
+    uint256 public maxFee;
+    uint256 public feePercantage;
+    address public _owner;
+    
+    modifier onlyOwner {
+        require(msg.sender == _owner);
+        _;
+    }
+
+    modifier canModifyWhitelistIn {
+        require(whitelistModerator[msg.sender]==1 || whitelistModerator[msg.sender]==3);
+        _;
+    }
+    
+    modifier canModifyWhitelistOut {
+        require(whitelistModerator[msg.sender]==2 || whitelistModerator[msg.sender]==3);
+        _;
+    }
+    
+    modifier canModifyWhitelist {
+        require(whitelistModerator[msg.sender]==3);
+        _;
+    }
+    
+    modifier onlyNewToken {
+        require(msg.sender==newToken);
+        _;
+    }
     
     function transfer(address _to, uint256 _value) returns (bool success) {
+        if(newToken!=0x0){
+            return NewToken(newToken).transfer(msg.sender,_to,_value);
+        }
         uint256 fee=getFee(_value);
-        if (balances[msg.sender].add(fee) >= _value && _value > 0) {
+        uint256 valueWithFee=_value;
+         if(withFee(msg.sender,_to)){
+            valueWithFee=valueWithFee.add(fee);
+        }
+        if (balances[msg.sender] >= valueWithFee && _value > 0) {
             //Do Transfer
             doTransfer(msg.sender,_to,_value,fee);
             return true;
         }  else { return false; }
     }
     
+    function withFee(address _from,address _to) private returns(bool){
+        return !whitelist[2][_from] && !whitelist[1][_to] && !whitelist[3][_to] && !whitelist[3][_from];
+    }
+    
     function getFee(uint256 _value) private returns (uint256){
-        uint256 onePercentOfValue=_value.onePercent();
+        uint256 feeOfValue=_value.onePercent().mul(feePercantage);
         uint256 fee=uint256(maxFee).power(decimals);
          // Check if 1% burn fee exceeds maxfee
         // If so then hard cap for burn fee is maxfee
-        if (onePercentOfValue>= fee) {
+        if (feeOfValue>= fee) {
             return fee;
         // If 1% burn fee is less than maxfee
         // then use 1% burn fee
         } 
-        if (onePercentOfValue < fee) {
-            return onePercentOfValue;
+        if (feeOfValue < fee) {
+            return feeOfValue;
         }
     }
     function doTransfer(address _from,address _to,uint256 _value,uint256 fee) internal {
             balances[_from] =balances[_from].sub(_value);
             balances[_to] = balances[_to].add(_value);
             Transfer(_from, _to, _value);
-            if(!withoutFee[_from]){
+            if(withFee(_from,_to)) {
                 doBurn(_from,fee);
             }
     }
@@ -125,21 +191,31 @@ contract StandardToken is Token {
         return true;
     }
     
-    function burn(address _from,uint256 _value) public returns (bool success) {
+    function burn(address _from,uint256 _value) onlyOwner public returns (bool success) {
         return doBurn(_from,_value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        uint256 fee=getFee(_value);  
+        if(newToken!=0x0){
+            return NewToken(newToken).transferFrom(msg.sender,_from,_to,_value);
+        }
+        uint256 fee=getFee(_value);
         uint256 valueWithFee=_value;
-        if(!withoutFee[_from]){
+        if(withFee(_from,_to)){
             valueWithFee=valueWithFee.add(fee);
         }
-        if (balances[_from] >= valueWithFee && allowed[_from][msg.sender] >= valueWithFee && _value > 0 ) {
+        if (balances[_from] >= valueWithFee && 
+            (allowed[_from][msg.sender] >= valueWithFee || allowed[_from][msg.sender] == _value) &&
+            _value > 0 ) {
             doTransfer(_from,_to,_value,fee);
-            allowed[_from][msg.sender] =allowed[_from][msg.sender].sub(valueWithFee);
+            if(allowed[_from][msg.sender] == _value){
+                allowed[_from][msg.sender] =allowed[_from][msg.sender].sub(_value);
+            }
+            else{
+                allowed[_from][msg.sender] =allowed[_from][msg.sender].sub(valueWithFee);
+            }
             return true;
         } else { return false; }
     }
@@ -149,9 +225,16 @@ contract StandardToken is Token {
     }
 
     function approve(address _spender, uint256 _value) returns (bool success) {
-        uint256 fee=getFee(_value);  
-        allowed[msg.sender][_spender] = _value.add(fee);
-        Approval(msg.sender, _spender, _value.add(fee));
+        if(newToken!=0x0){
+            return NewToken(newToken).approve(msg.sender,_spender,_value);
+        }
+        uint256 valueWithFee=_value;
+        if(withFee(_spender,0x0)){
+            uint256 fee=getFee(_value);  
+            valueWithFee=valueWithFee.add(fee);
+        }
+        allowed[msg.sender][_spender] = valueWithFee;
+        Approval(msg.sender, _spender, valueWithFee);
         return true;
     }
 
@@ -163,9 +246,31 @@ contract StandardToken is Token {
         return _totalSupply;
     }
     
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public _totalSupply;
+    function setTotalSupply(uint256 _value) onlyNewToken external {
+        _totalSupply=_value;
+    }
+    
+    function setBalance(address _to,uint256 _value) onlyNewToken external {
+        balances[_to]=_value;
+    }
+    
+    function setAllowed(address _spender,address _to,uint256 _value) onlyNewToken external {
+        allowed[_to][_spender]=_value;
+    }
+    function getDecimals() constant returns (uint256 decimals){
+        return decimals;
+    }
+    
+    function eventTransfer(address _from, address  _to, uint256 _value) onlyNewToken external{
+        Transfer(_from,_to,_value);
+    }
+    
+    function eventApproval(address _owner, address  _spender, uint256 _value)onlyNewToken external{
+        Approval(_owner,_spender,_value);
+    }
+    function eventBurn(address from, uint256 value)onlyNewToken external{
+        Burn(from,value);
+    }
 }
 
 
@@ -177,7 +282,7 @@ contract EqualToken is StandardToken {
     }
 
     /* Public variables of the token */
-
+    
     /*
     NOTE:
     The following variables are OPTIONAL vanities. One does not have to include them.
@@ -187,50 +292,105 @@ contract EqualToken is StandardToken {
     string public name;                   //fancy name: eg Simon Bucks
     string public symbol;                 //An identifier: eg SBX
     string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-    address private _owner;
+    address public oldToken=0x0;    
     // Fee info
     string public feeInfo = "Each operation costs 1% of the transaction amount, but not more than 250 tokens.";
 
     function EqualToken() {
-        _totalSupply = 800000000000000000000000000; 
         _owner=msg.sender;
-        balances[msg.sender] =_totalSupply;
-
-        // Airdrop Allocation
-        allocate(0xeeEb0f191c4E2ee96E1399937a5241fA1E9f9A6e,20); // Airdrop Round One
-        allocate(0x3CF9d0484790a1a24cfb1A51f8BEf39b6F1322d6,20); // Airdrop Round Two
-        allocate(0x64d1832cf8879A903af86E60fF9ed549648B2Bda,5); // Airdrop Round Three
-
-        // Adoption Allocation
-        allocate(0x0b6cFbc459efF6B12238380AC2A26926896eaFA2,35); // Seed Offerings
-
-        // Internal Allocation
-        allocate(0xaBf029361BeCB2bE58011AD874C6eAbCD4c84D09,20); // Team 
-
-        maxFee=250; // max fee for transfer
+        whitelistModerator[msg.sender]=3;
+        whitelist[3][msg.sender]=true;
         
-        name = "EQUAL Token";                      // Set the name for display purposes
+        
+        maxFee=250; // max fee for transfer
+        feePercantage=1; // fee in percents
+        
+        name = "EQUAL";                      // Set the name for display purposes
         decimals = 18;                            // Amount of decimals for display purposes
         symbol = "EQL";                          // Set the symbol for display purposes
     }
 
+    function setOldToken(address _oldToken) onlyOwner public{
+        require(oldToken==0x0);
+        oldToken=_oldToken;
+        Token token=Token(_oldToken);
+        _totalSupply=token.totalSupply();
+        balances[msg.sender] =_totalSupply;
+        Transfer(0x0,msg.sender,_totalSupply);
+    }
+    
+    // Redistibute new token with same balances;
+    function redistribute(address[] holders) onlyOwner public{
+        require(oldToken!=0x0);
+        Token token=Token(oldToken);
+        for(uint256 i=0;i<holders.length;++i){
+            address _to=holders[i];
+            if(balances[_to]==0){
+                uint256 balance=token.balanceOf(_to);
+                balances[_to]=balance;
+                balances[msg.sender]=balances[msg.sender].sub(balance);
+                Transfer(msg.sender,_to,balance);
+            }
+        }
+    }
+    
     function allocate(address _address,uint256 percent) private{
         uint256 bal=_totalSupply.onePercent().mul(percent);
         //balances[_address]=bal;
-        withoutFee[_address]=true;
+        whitelist[3][_address]=true;
         doTransfer(msg.sender,_address,bal,0);
     }
    
-    function setWithoutFee(address _address,bool _withoutFee) public {
-        require(_owner==msg.sender);
-        withoutFee[_address]=_withoutFee;
+    // Set address access to inbound whitelist. 
+    function setWhitelistIn(address _address,bool _value) canModifyWhitelistIn public{
+        setWhitelistValue(_address,_value,1);
+    }
+    
+    // Set address access to outbound whitelist. 
+    function setWhitelistOut(address _address,bool _value) canModifyWhitelistOut public{
+        setWhitelistValue(_address,_value,2);
+    }
+    
+    // Set address access to inbound and outbound whitelist. 
+    function setWhitelist(address _address,bool _value) canModifyWhitelist public{
+        setWhitelistValue(_address,_value,3);
+    }
+    
+    function setWhitelistValue(address _address,bool _withoutFee,uint8 _type) internal {
+        whitelist[_type][_address]=_withoutFee;
+    }
+    
+    // Set address of moderator whitelist
+    // _level can be: 0 -not moderator, 1 -inbound,2 - outbound, 3 -all
+    function setWhitelistModerator(address _address,uint8 _level) onlyOwner public {
+        whitelistModerator[_address]=_level;
+    }
+    
+    //Set max fee value
+    function setMaxFee(uint256 newFee) onlyOwner public {
+        maxFee=newFee;
+    }
+    
+    //Set fee percent value
+    function setFeePercent(uint256 newFee) onlyOwner public {
+        feePercantage=newFee;
+    }
+    
+    //Set fee info
+    function setFeeInfo(string newFeeInfo) onlyOwner public {
+       feeInfo=newFeeInfo;
+    }
+    
+    function setNewToken(address _newtoken) onlyOwner public{
+        newToken=_newtoken;
+        UpdateToken(_newtoken);
     }
     
     /* Approves and then calls the receiving contract */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
+        if(!approve(_spender,_value)){
+            return false;
+        }
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
         //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
