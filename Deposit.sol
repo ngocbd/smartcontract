733307fbf367ea9c39e71d345864b2860a04ac73
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:11:43 +0700

    add contract Deposit

--- Deposit.sol
+++ Deposit.sol
@@ -1,7 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Deposit at 0x1235b9042f7fe167f09450eaffdc07efcc3acb38
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Deposit at 0xA16CDcbA1d6Cb6874ff9fd8a6c8b82a3f834f512
 */
-pragma solidity ^0.4.7;
+// Copyright (C) 2017  The Halo Platform by Scott Morrison
+//
+// This is free software and you are welcome to redistribute it under certain conditions.
+// ABSOLUTELY NO WARRANTY; for details visit: https://www.gnu.org/licenses/gpl-2.0.html
+//
+pragma solidity ^0.4.13;
 
 contract ForeignToken {
     function balanceOf(address who) constant public returns (uint256);
@@ -9,10 +14,8 @@ contract ForeignToken {
 }
 
 contract Owned {
-    bool public locked = true;
     address public Owner = msg.sender;
-    modifier onlyOwner { if (msg.sender == Owner || !locked) _; }
-    function lock(bool flag) onlyOwner { locked = flag; }
+    modifier onlyOwner { if (msg.sender == Owner) _; }
 }
 
 contract Deposit is Owned {
@@ -32,20 +35,20 @@ contract Deposit is Owned {
     }
 
     function deposit() payable {
-        if (msg.value >= 0.1 ether) {
+        if (msg.value >= 1 ether) {
             Deposits[msg.sender] += msg.value;
             Deposit(msg.value);
         }
     }
 
-    function kill() payable {
+    function kill() {
         if (this.balance == 0)
             selfdestruct(msg.sender);
     }
     
     function withdraw(uint amount) payable onlyOwner {
         if (Deposits[msg.sender] > 0 && amount <= Deposits[msg.sender]) {
-            msg.sender.send(amount);
+            msg.sender.transfer(amount);
             Withdraw(amount);
         }
     }
