2baafa2bed43ea1b836eee593eefa50d70b201d3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:10 +0700

    add contract Switch

--- Switch.sol
+++ Switch.sol
@@ -1,20 +1,21 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0x69644dad199c1973ae660ddbd86bfec1109c06a7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0x7bF4BA18cdfBD4286dC7e93497BFA62B3d6Ec739
 */
 pragma solidity ^0.4.8;
 contract Switch 
 {
-    uint256 public blink_block;
+    bool public state=false;
+    uint256 public blinc_block;
     uint256 public on_block;
     address public owner;
 
     function Switch(){
         owner=msg.sender;
-        on_block=block.number;
-        blink_block=block.number;
+        on_block==block.number;
+        blinc_block=block.number;
     }
-    function blink() payable {
-        if(msg.value>0)blink_block=block.number;
+    function blinc() payable {
+        if(msg.value>0)blinc_block=block.number;
     }
     function () payable {
         if(msg.value>0)on_block=block.number;
