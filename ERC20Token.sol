76defc37cfea9853f9dbd15db59f5faac58ff629
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:00:12 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xad4c0ccc3cb30b6b7e04b234c7198349fea0a7d5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x52c00d4d7c5e488bc4447f9ccb009a0a6ba446c9
 */
 pragma solidity ^0.4.4;
 
@@ -98,32 +98,20 @@ contract ERC20Token is StandardToken {
         throw;
     }
 
-    /* Public variables of the token */
+    
+    string public name;              
+    uint8 public decimals;               
+    string public symbol;                 
+    string public version = 'H1.0';
 
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
-
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 1000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 1000000;                        // Update total supply (100000 for example)
-        name = "DutchCCcoin";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "DutchCCcoin";                               // Set the symbol for display purposes
+        balances[msg.sender] = 49000000000000000000000000;                
+        totalSupply = 49000000000000000000000000;                       
+        name = "Colombia Coin";                                    
+        decimals = 18;                            
+        symbol = "COLC";                              
     }
 
     /* Approves and then calls the receiving contract */
