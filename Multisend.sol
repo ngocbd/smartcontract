6dfaeb4d89bbd2d0dac0490b71c7a770ce0859a3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 06:00:49 +0700

    add contract MultiSend

--- Multisend.sol
+++ Multisend.sol
@@ -1,78 +1,43 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Multisend at 0xebe9320e07079e54d630c5eac9d7cdbb8f6f5508
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSend at 0xcc16643aa020fc23011c209e7d3f1abf2240ead1
 */
-contract Ownable {
-  address public owner;
+pragma solidity ^0.4.18;
 
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    if (msg.sender != owner) {
-      throw;
-    }
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-
-}
-
-
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
 contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-
-contract Multisend is Ownable {
-    
-    function withdraw() onlyOwner {
-        msg.sender.transfer(this.balance);
+contract MultiSend {
+  function multiSend(address _token, address[] addresses, uint amount) public {
+    ERC20 token = ERC20(_token);
+    for(uint i = 0; i < addresses.length; i++) {
+      require(token.transferFrom(msg.sender, addresses[i], amount));
     }
-    
-    function send(address _tokenAddr, address dest, uint value)
-    onlyOwner
-    {
-      ERC20(_tokenAddr).transfer(dest, value);
+  }
+  function multiSendEth(address[] addresses) public payable {
+    uint256 amount = msg.value / addresses.length;
+    for(uint i = 0; i < addresses.length; i++) {
+      addresses[i].transfer(amount);
     }
-    
-    function multisend(address _tokenAddr, address[] dests, uint256[] values)
-    onlyOwner
-      returns (uint256) {
-        uint256 i = 0;
-        while (i < dests.length) {
-           ERC20(_tokenAddr).transfer(dests[i], values[i]);
-           i += 1;
-        }
-        return (i);
-    }
+  }
 }
