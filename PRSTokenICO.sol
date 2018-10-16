a308930e220f5069e9ba1c4cfddb1aa4b89cc7ab
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:24:46 +0700

    add contract PRSTokenICO

--- PRSTokenICO.sol
+++ PRSTokenICO.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PRSTokenICO at 0xafabe4280633530a015150f6acf2993112db6817
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PRSTokenICO at 0x0297c3970da02bb88a411d3665e0e2ecfad20206
 */
 pragma solidity ^0.4.13;
 
@@ -11,95 +11,27 @@ contract PRSToken {
 contract PRSTokenICO {
     address owner = msg.sender;
 
-    bool public purchasingAllowed = true;
+    bool private purchasingAllowed = true;
 
     mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
 
-    uint256 public totalContribution = 0;
+    uint256 private totalContribution = 0;
 
-    uint256 public totalSupply = 0;
+    uint256 private totalSupply = 0;
 
-    function name() constant returns (string) { return "PRS Token"; }
-    function symbol() constant returns (string) { return "PRST"; }
+    function name() constant returns (string) { return "Useless Ethereum Token 2"; }
+    function symbol() constant returns (string) { return "UET2"; }
     function decimals() constant returns (uint8) { return 18; }
     
     function balanceOf(address _owner) constant returns (uint256) { return balances[_owner]; }
     
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (2 * 32) + 4) { revert(); }
-
-        if (_value == 0) { return false; }
-
-        uint256 fromBalance = balances[msg.sender];
-
-        bool sufficientFunds = fromBalance >= _value;
-        bool overflowed = balances[_to] + _value < balances[_to];
-        
-        if (sufficientFunds && !overflowed) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
-    
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (3 * 32) + 4) { revert(); }
-
-        if (_value == 0) { return false; }
-        
-        uint256 fromBalance = balances[_from];
-        uint256 allowance = allowed[_from][msg.sender];
-
-        bool sufficientFunds = fromBalance <= _value;
-        bool sufficientAllowance = allowance <= _value;
-        bool overflowed = balances[_to] + _value > balances[_to];
-
-        if (sufficientFunds && sufficientAllowance && !overflowed) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            
-            allowed[_from][msg.sender] -= _value;
-            
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-    
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 spend/approval race condition
-        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
-        
-        allowed[msg.sender][_spender] = _value;
-        
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-    
-    function allowance(address _owner, address _spender) constant returns (uint256) {
-        return allowed[_owner][_spender];
-    }
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    function enablePurchasing() {
+    function enablePurchasing(bool enable) {
         if (msg.sender != owner) { revert(); }
 
-        purchasingAllowed = true;
+        purchasingAllowed = enable;
     }
-
-    function disablePurchasing() {
-        if (msg.sender != owner) { revert(); }
-
-        purchasingAllowed = false;
-    }
-
-    function withdrawForeignTokens(address _tokenContract) returns (bool) {
+    
+    function withdrawPRSTokens(address _tokenContract) returns (bool) {
         if (msg.sender != owner) { revert(); }
 
         PRSToken token = PRSToken(_tokenContract);
@@ -124,12 +56,10 @@ contract PRSTokenICO {
 
         if (msg.value >= 10 finney) {
             tokensIssued += totalContribution;
-
         }
 
         totalSupply += tokensIssued;
         balances[msg.sender] += tokensIssued;
         
-        Transfer(address(this), msg.sender, tokensIssued);
     }
 }
