f24b131da3aeb4fac2d2085798e23a9835e02043
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:37:22 +0700

    add contract SafeMath

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,56 +1,28 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0x647d792d620a664a2a55e2452693b6ad32aa794d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0xfd672e2adbcdab7d0835d089af64b02c2627ffed
 */
 library SafeMath {
-  function mul(uint256 a, uint256 b) constant public returns (uint256) {
+  function mul(uint256 a, uint256 b) constant returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) constant public returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
+  function div(uint256 a, uint256 b) constant returns (uint256) {
+    assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b) constant public returns (uint256) {
+  function sub(uint256 a, uint256 b) constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) constant public returns (uint256) {
+  function add(uint256 a, uint256 b) constant returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
   }
-}
-
-
-contract Ownable {
-  address public owner;
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    if(msg.sender == owner){
-      _;
-    }
-    else{
-      revert();
-    }
-  }
-
 }
