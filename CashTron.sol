b84339b525703b1c699970b79639fef094779e89
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:54:14 +0700

    add contract CashTron

--- CashTron.sol
+++ CashTron.sol
@@ -1,9 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CashTron at 0xde6d0b12b940cdb84618c6bd47157dffd268fd6a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CashTron at 0x851f621d70464f84d7d308f29b9252e5d7db00d6
 */
 //--------------------------------------------------------------------------------
-     // CashTron Currency Token "PHPC" (Is Not Bank Legal Money)
-     // USDC/EURC/AUDC/CADC/NZDC/RUBC/CNYC/SGDC/#PHPC/IDRC/MYRC/THBC/WNDC/BDTC
+     // CashTron Currency Token "VNDC" (Is Not Bank Legal Money)
+     // USDC/AUDC/CADC/NZDC/RUBC/CNYC/SGDC/PHPC/IDRC/MYRC/THBC/#VNDC
      // CashTron @ 2018 CashTron.io  CashTron.co
      //--------------------------------------------------------------------------------
 
@@ -232,9 +232,9 @@ contract CashTron is BurnableToken {
     using SafeMath for uint256;
 
     string public constant name = "CashTron";
-    string public constant symbol = "PHPC";
-    uint8 public constant decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 13000000000;
+    string public constant symbol = "VNDC";
+    uint8 public constant decimals = 8;
+    uint256 public constant INITIAL_SUPPLY = 500000000000;
 
     function CashTron() public {
         totalSupply = INITIAL_SUPPLY.mul(10 ** uint256(decimals));
