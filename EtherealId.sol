ebf4a30a11f4a9bb010b18c76225dd06aa70d8a3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:01:53 +0700

    add contract EtherealId

--- EtherealId.sol
+++ EtherealId.sol
@@ -1,11 +1,11 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherealId at 0xe9e4afe548256ba90ee7d63f24eac11250f67450
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherealId at 0xdaab8a09aed3b98df4fffef85e78cd6f513de7d0
 */
 pragma solidity ^0.4.18;
 
 contract EtherealId{
      string public constant CONTRACT_NAME = "EtherealId";
-    string public constant CONTRACT_VERSION = "B";
+    string public constant CONTRACT_VERSION = "A";
     mapping (address => bool) private IsAuthority;
 	address private Creator;
 	address private Owner;
@@ -94,21 +94,6 @@ contract EtherealId{
     function VerifiedInfoHash(bytes32 hash) public view notBlocked returns(bool)     {
         return VerifiedInfoHashes[hash];
     }
-    
-	//this is the fallback
-    event RecievedEth(address indexed _from, uint256 _value);
-	function () payable public {
-		RecievedEth(msg.sender, msg.value);		
-	}
 	
-	event TransferedEth(address indexed _to, uint256 _value);
-	function TransferEth(address _to, uint256 _value) public onlyOwner{
-	    require(this.balance >= _value);
-	    
-        if(_value >0)
-		{
-			_to.transfer(_value);
-			TransferedEth(_to, _value);
-		}   
-	}
+	
 }
