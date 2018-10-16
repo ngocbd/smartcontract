b6b99fb28bd7ff901d6292c18cabdb19cdc8afd7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:21:09 +0700

    add contract Distributor

--- Distributor.sol
+++ Distributor.sol
@@ -1,78 +1,78 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distributor at 0xc36bdd9a0435f1532499c6a13fcb720bca8bde79
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distributor at 0x07eeba7dbd5e260a1ca887879b9ea29db1c034b3
 */
 pragma solidity ^0.4.17;
 
 contract Owned {
-    address public owner;
-    address public newOwner;
+   address public owner;
+   address public newOwner;
 
-    /**
-     * Events
-     */
-    event ChangedOwner(address indexed new_owner);
+   /**
+    * Events
+    */
+   event ChangedOwner(address indexed new_owner);
 
-    /**
-     * Functionality
-     */
+   /**
+    * Functionality
+    */
 
-    function Owned() {
-        owner = msg.sender;
-    }
+   function Owned() {
+       owner = msg.sender;
+   }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+   modifier onlyOwner() {
+       require(msg.sender == owner);
+       _;
+   }
 
-    function changeOwner(address _newOwner) onlyOwner external {
-        newOwner = _newOwner;
-    }
+   function changeOwner(address _newOwner) onlyOwner external {
+       newOwner = _newOwner;
+   }
 
-    function acceptOwnership() external {
-        if (msg.sender == newOwner) {
-            owner = newOwner;
-            newOwner = 0x0;
-            ChangedOwner(owner);
-        }
-    }
+   function acceptOwnership() external {
+       if (msg.sender == newOwner) {
+           owner = newOwner;
+           newOwner = 0x0;
+           ChangedOwner(owner);
+       }
+   }
 }
 
 contract IOwned {
-    function owner() returns (address);
-    function changeOwner(address);
-    function acceptOwnership();
+   function owner() returns (address);
+   function changeOwner(address);
+   function acceptOwnership();
 }
 
 // interface with what we need to withdraw
 contract Withdrawable {
-	function withdrawTo(address) returns (bool);
+       function withdrawTo(address) returns (bool);
 }
 
-// responsible for 
+// responsible for
 contract Distributor is Owned {
 
-	uint256 public nonce;
-	Withdrawable public w;
+       uint256 public nonce;
+       Withdrawable public w;
 
-	event BatchComplete(uint256 nonce);
+       event BatchComplete(uint256 nonce);
 
-	event Complete();
+       event Complete();
 
-	function setWithdrawable(address w_addr) onlyOwner {
-		w = Withdrawable(w_addr);
-	}
-	
-	function distribute(address[] addrs) onlyOwner {
-		for (uint256 i = 0; i <  addrs.length; i++) {
-			w.withdrawTo(addrs[i]);
-		}
-		BatchComplete(nonce);
-		nonce = nonce + 1;
-	}
+       function setWithdrawable(address w_addr) onlyOwner {
+               w = Withdrawable(w_addr);
+       }
 
-	function complete() onlyOwner {
-		nonce = 0;
-		Complete();
-	}
+       function distribute(address[] addrs) onlyOwner {
+               for (uint256 i = 0; i <  addrs.length; i++) {
+                       w.withdrawTo(addrs[i]);
+               }
+               BatchComplete(nonce);
+               nonce = nonce + 1;
+       }
+
+       function complete() onlyOwner {
+               nonce = 0;
+               Complete();
+       }
 }
