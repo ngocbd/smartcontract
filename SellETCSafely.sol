53a955645fbc2b4e96de902b0495dbe0e949ff29
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:45:31 +0700

    add contract SellETCSafely

--- SellETCSafely.sol
+++ SellETCSafely.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SellETCSafely at 0xc8d9890df1ff2e87be05e9edab3cca26f054b611
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SellETCSafely at 0x5f0d0c4c159970fda5adc93a6b7f17706fd3255c
 */
 contract AmIOnTheFork {
     function forked() constant returns(bool);
@@ -8,15 +8,15 @@ contract AmIOnTheFork {
 contract SellETCSafely {
     // fork oracle to use
     AmIOnTheFork amIOnTheFork = AmIOnTheFork(0x2bd2326c993dfaef84f696526064ff22eba5b362);
-
+    
     // recipient of the 1 % fee on the ETC side
     address feeRecipient = 0x46a1e8814af10Ef6F1a8449dA0EC72a59B29EA54;
-
+    
     function split(address ethDestination, address etcDestination) {
         if (amIOnTheFork.forked()) {
             // The following happens on the forked chain:
             // 100 % is forwarded to the provided destination for ETH
-            ethDestination.call.value(msg.value)();
+            ethDestination.call.value(msg.value);
         } else {
             // The following happens on the classic chain:
             // 1 % is forwarded to the fee recipient
@@ -26,7 +26,7 @@ contract SellETCSafely {
             etcDestination.call.value(msg.value - fee)();
         }
     }
-
+    
     function () {
         throw;  // do not accept value transfers
     }
