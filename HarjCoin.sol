adfdc125e1b85d455daeb4a357a589710709a34d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:02:59 +0700

    add contract HarjCoin

---
+++ HarjCoin.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HarjCoin at 0x95563e9e5700e728061059b5f52fc91b2cb5850c
+*/
+pragma solidity ^0.4.21;
+
+// NICE ETHERSCAN SNIPING MATE
+
+
+contract HarjCoin{
+
+}
