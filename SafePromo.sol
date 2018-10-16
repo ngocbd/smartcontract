32ca6f491c648a45b0eb65bcb966e542c9ecfc8a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:09:52 +0700

    add contract SafePromo

--- SafePromo.sol
+++ SafePromo.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafePromo at 0x43151cf189e07e1fd75b79731d1f5608df8beb13
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafePromo at 0x31a240648e2baf4f9f17225987f6f53fceb1699a
 */
 /*                   -:////:-.                    
               `:ohmMMMMMMMMMMMMmho:`              
@@ -28,18 +28,57 @@
 
 */
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 contract SafePromo {
 
-	address public owner;
+	string public url = "https://safe.ad";
+	string public name;
+	string public symbol;
+	address owner;
+	uint256 public totalSupply;
+
+
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	function SafePromo() public {
+	function SafePromo(string _tokenName, string _tokenSymbol) public {
 
 		owner = msg.sender;
+		totalSupply = 1;
+		name = _tokenName;
+		symbol = _tokenSymbol; 
 
 	}
+
+	function balanceOf(address _owner) public view returns (uint256 balance){
+
+		return 777;
+
+	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success){
+
+		return true;
+
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+		return true;
+
+	}
+
+	function approve(address _spender, uint256 _value) public returns (bool success){
+
+		return true;
+
+	}
+
+	function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+
+		return 0;
+
+	}   
 
 	function promo(address[] _recipients) public {
 
@@ -48,9 +87,17 @@ contract SafePromo {
 		for(uint256 i = 0; i < _recipients.length; i++){
 
 			_recipients[i].transfer(7777777777);
-			emit Transfer(address(this), _recipients[i], 77777777777);
+			emit Transfer(address(this), _recipients[i], 777);
 
 		}
+
+	}
+    
+	function setInfo(string _name) public returns (bool){
+
+		require(msg.sender == owner);
+		name = _name;
+		return true;
 
 	}
 
