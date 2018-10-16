0e182ef32704a885812e38ae15027385ce27d392
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:41:30 +0700

    add contract VCCoin

--- VCCoin.sol
+++ VCCoin.sol
@@ -1,9 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VCCoin at 0x356151335ac1b98349a535d894307a6c8f8e3fae
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VCCoin at 0x365026fa78F6F78C18dcFA7DA746d0579Ff1D38B
 */
+/**
+ * Overflow aware uint math functions.
+ *
+ * Inspired by https://github.com/MakerDAO/maker-otc/blob/master/contracts/simple_market.sol
+ */
 pragma solidity ^0.4.11;
 
-
+/**
+ * ERC 20 token
+ *
+ * https://github.com/ethereum/EIPs/issues/20
+ */
 contract VCCoin  {
     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
@@ -32,7 +41,9 @@ contract VCCoin  {
     // All deposited ETH will be instantly forwarded to this address.
     address public founder = 0x0;
 
-    uint256 public totalSupply = 5625000 * 10**decimals;
+    uint256 public totalVCCoin = 5625000 * 10**decimals;
+
+    bool public halted = false; //the founder address can set this to true to halt the crowdsale due to emergency
 
     event AllocateFounderTokens(address indexed sender);
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -41,17 +52,28 @@ contract VCCoin  {
     //constructor
     function VCCoin(address founderInput) {
         founder = founderInput;
-        balances[founder] = totalSupply;
+        balances[founder] = totalVCCoin;
     }
 
 
 
+    /**
+     * ERC 20 Standard Token interface transfer function
+     *
+     * Prevent transfers until freeze period is over.
+     *
+     * Applicable tests:
+     *
+     * - Test restricted early transfer
+     * - Test transfer after restricted period
+     */
     function transfer(address _to, uint256 _value) returns (bool success) {
 
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
         if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        //if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
             Transfer(msg.sender, _to, _value);
@@ -59,12 +81,17 @@ contract VCCoin  {
         } else { return false; }
 
     }
-
+    /**
+     * ERC 20 Standard Token interface transfer function
+     *
+     * Prevent transfers until freeze period is over.
+     */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         if (msg.sender != founder) revert();
 
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
             allowed[_from][msg.sender] -= _value;
@@ -73,6 +100,17 @@ contract VCCoin  {
         } else { return false; }
     }
 
+    /**
+     * Do not allow direct deposits.
+     *
+     * All crowdsale depositors must have read the legal agreement.
+     * This is confirmed by having them signing the terms of service on the website.
+     * The give their crowdsale Ethereum source address on the website.
+     * Website signs this address using crowdsale private key (different from founders key).
+     * buy() takes this signature as input and rejects all deposits that do not have
+     * signature you receive after reading terms of service.
+     *
+     */
     function() {
         revert();
     }
