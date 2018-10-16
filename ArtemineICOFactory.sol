59386d299277433546b2f2b9f04e9c437fb46c9f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:58:55 +0700

    add contract ArtemineICOFactory

---
+++ ArtemineICOFactory.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ArtemineICOFactory at 0xa918185d042016e2d87c5cb0c7d216b062f79559
+*/
+pragma solidity 0.4.19;
+
+contract ArtemineICOFactory {
+    /* Constructor */
+    function ArtemineICOFactory() {
+        //Any tokens sent to this address will be destroyed
+    }
+}
