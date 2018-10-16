81b09c2e9f360865c112d2ad02ff16b07ed50e38
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:00:45 +0700

    add contract AnalyticProxy

---
+++ AnalyticProxy.sol
@@ -0,0 +1,31 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AnalyticProxy at 0x689169d5c51ee19f277cb85e881f04c3c823cbca
+*/
+/*
+ * @title Mixin contract which supports different payment channels and provides analytical per-channel data.
+ * @author Eenae
+ */
+contract InvestmentAnalytics {
+    function iaInvestedBy(address investor) external payable;
+}
+
+
+/*
+ * @title This is proxy for analytics. Target contract can be found at field m_analytics (see "read contract").
+ * @author Eenae
+
+ * FIXME after fix of truffle issue #560: refactor to a separate contract file which uses InvestmentAnalytics interface
+ */
+contract AnalyticProxy {
+
+    function AnalyticProxy() {
+        m_analytics = InvestmentAnalytics(msg.sender);
+    }
+
+    /// @notice forward payment to analytics-capable contract
+    function() payable {
+        m_analytics.iaInvestedBy.value(msg.value)(msg.sender);
+    }
+
+    InvestmentAnalytics public m_analytics;
+}
