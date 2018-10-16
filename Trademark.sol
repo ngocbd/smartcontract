10f2731cddf37866c516531129fd149a028fc45c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:45:07 +0700

    add contract Trademark

---
+++ Trademark.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Trademark at 0x509F5Ca1C64F0F0A63E5C28aDB3398a3c3ec13cf
+*/
+contract Trademark {
+function getTrademark() constant returns (string) {
+return "Global Net Encryption Investment Security Service (GNEISS) Software";
+}
+}
