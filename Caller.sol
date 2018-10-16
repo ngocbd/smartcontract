482abd9805d4b12b9fe2c6e223675dbe0db085c9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:52:02 +0700

    add contract Caller

--- Caller.sol
+++ Caller.sol
@@ -1,23 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Caller at 0x580f18c090914EB9eDbb3d6A810C9789a91c7ba4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Caller at 0x0cbe050f75bc8f8c2d6c0d249fea125fd6e1acc9
 */
-pragma solidity >=0.4.13;
-
-contract Emitter {
-    event Emit(uint x);
-    function emit(uint x) {
-        Emit(x);
-    }
-}
+pragma solidity ^0.4.10;
 
 contract Caller {
-    address emitter;
-    function setEmitter(address e) {
-        if (emitter == 0x0) {
-            emitter = e;
-        }
-    }
-    function callEmitter(uint x) {
-        Emitter(emitter).emit(x);
+    function callAddress(address a) {
+        a.call();
     }
 }
