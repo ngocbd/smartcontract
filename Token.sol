7a8587d69c6b7f0fae53261c998422b9206c32a5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:28:15 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,147 +1,252 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x0a783b98fdac271efd3804b129a5af15e88093d5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xc38c848cc0f618d60846a853e42da32ea85fccf0
 */
-/*
-The goico_kasko2go Contract is free software: you can redistribute it and/or
-modify it under the terms of the GNU lesser General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The goico_kasko2go Contract is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU lesser General Public License for more details.
-
-You should have received a copy of the GNU lesser General Public License
-along with the goico_kasko2go Contract. If not, see <http://www.gnu.org/licenses/>.
-
-@author Ilya Svirin <i.svirin@prover.io>
-*/
-
-pragma solidity ^0.4.19;
-
-contract owned {
-
+pragma solidity ^0.4.21;
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns(uint256) {
+        if(a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns(uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns(uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+    function add(uint256 a, uint256 b) internal pure returns(uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+contract Ownable {
     address public owner;
-    address public candidate;
-
-    constructor() public {
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    modifier onlyOwner() { require(msg.sender == owner); _; }
+    function Ownable() public {
         owner = msg.sender;
     }
-
-    modifier onlyOwner {
-        require(owner == msg.sender);
-        _;
-    }
-
-    function changeOwner(address _owner) onlyOwner public {
-        candidate = _owner;
-    }
-
-    function confirmOwner() public {
-        require(candidate == msg.sender);
-        owner = candidate;
-        delete candidate;
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(this));
+        owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
     }
 }
 
-contract BaseERC20 {
-    function balanceOf(address who) public constant returns (uint);
-    function transfer(address to, uint value) public;
+contract ERC20 {
+    uint256 public totalSupply;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function balanceOf(address who) public view returns(uint256);
+    function transfer(address to, uint256 value) public returns(bool);
+    function transferFrom(address from, address to, uint256 value) public returns(bool);
+    function allowance(address owner, address spender) public view returns(uint256);
+    function approve(address spender, uint256 value) public returns(bool);
 }
 
-contract Token is owned {
+contract StandardToken is ERC20 {
+    using SafeMath for uint256;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    function StandardToken(string _name, string _symbol, uint8 _decimals) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+}
 
-    string  public standard = 'Token 0.1';
-    string  public name     = '_K2G';
-    string  public symbol   = '_K2G';
-    uint8   public decimals = 8;
+function balanceOf(address _owner) public view returns(uint256 balance) {
+        return balances[_owner];
+}
 
-    uint                      public totalSupply;
-    mapping (address => uint) public balanceOf;
-
-    uint                      public numberOfInvestors;
-    mapping (address => bool) public investors;
-    mapping (address => uint) public depositedCPT;
-    mapping (address => uint) public depositedWei;
-
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    enum State {
-        NotStarted,
-        Started,
-        Finished
-    }
-
-    address public backend;
-    address public cryptaurToken = 0x88d50B466BE55222019D71F9E8fAe17f5f45FCA1;
-    uint    public tokenPriceInWei;
-    State   public state;
-
-    event Mint(address indexed minter, uint tokens, bytes32 originalTxHash);
-
-    constructor() public owned() {}
-
-    function startCrowdsale() public onlyOwner {
-        require(state==State.NotStarted);
-        state=State.Started;
-    }
-
-    function finishCrowdsale() public onlyOwner {
-        require(state==State.Started);
-        state=State.Finished;
-    }
-
-    function changeBackend(address _backend) public onlyOwner {
-        backend = _backend;
-    }
-
-    function setTokenPriceInWei(uint _tokenPriceInWei) public {
-        require(msg.sender == owner || msg.sender == backend);
-        tokenPriceInWei = _tokenPriceInWei;
-    }
-
-    function () payable public {
-        require(state==State.Started);
-        uint tokens = msg.value / tokenPriceInWei * 100000000;
-        require(balanceOf[msg.sender] + tokens > balanceOf[msg.sender]); // overflow
-        require(tokens > 0);
-        depositedWei[msg.sender]+=msg.value;
-        balanceOf[msg.sender] += tokens;
-        if (!investors[msg.sender]) {
-            investors[msg.sender] = true;
-            ++numberOfInvestors;
+function transfer(address _to, uint256 _value) public returns(bool) {
+        require(_to != address(this));
+        require(_value <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+}
+function multiTransfer(address[] _to, uint256[] _value) public returns(bool) {
+        require(_to.length == _value.length);
+        for(uint i = 0; i < _to.length; i++) {
+            transfer(_to[i], _value[i]);
         }
-        emit Transfer(this, msg.sender, tokens);
-        totalSupply += tokens;
+        return true;
+}
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
+        require(_to != address(this));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 
-    function depositCPT(address _who, uint _valueCPT, bytes32 _originalTxHash) public {
-        require(msg.sender == backend || msg.sender == owner);
-        require(state==State.Started);
-        // decimals in K2G and PROOF are the same and equal 8
-        uint tokens = (_valueCPT * 10000) / 238894; // 1 K2G = 23,8894 CPT
-        depositedCPT[_who]+=_valueCPT;
-        require(balanceOf[_who] + tokens > balanceOf[_who]); // overflow
-        require(tokens > 0);
-        balanceOf[_who] += tokens;
-        totalSupply += tokens;
-        if (!investors[_who]) {
-            investors[_who] = true;
-            ++numberOfInvestors;
+    function allowance(address _owner, address _spender) public view returns(uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function approve(address _spender, uint256 _value) public returns(bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns(bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns(bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if(_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
-        emit Transfer(this, _who, tokens);
-        emit Mint(_who, tokens, _originalTxHash);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+}
+
+contract MintableToken is StandardToken, Ownable {
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
+    bool public mintingFinished = false;
+    modifier canMint(){require(!mintingFinished); _;}
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        emit Transfer(address(this), _to, _amount);
+        return true;
+    }
+    function finishMinting() onlyOwner canMint public returns(bool) {
+        mintingFinished = true;
+        emit MintFinished();
+        return true;
+    }
+}
+
+contract CappedToken is MintableToken {
+    uint256 public cap;
+
+    function CappedToken(uint256 _cap) public {
+        require(_cap > 0);
+        cap = _cap;
     }
 
-    function withdraw() public onlyOwner {
-        require(msg.sender.call.gas(3000000).value(address(this).balance)());
-        uint balance = BaseERC20(cryptaurToken).balanceOf(this);
-        BaseERC20(cryptaurToken).transfer(msg.sender, balance);
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
+        require(totalSupply.add(_amount) <= cap);
+
+        return super.mint(_to, _amount);
+    }
+}
+
+contract BurnableToken is StandardToken {
+    event Burn(address indexed burner, uint256 value);
+
+    function burn(uint256 _value) public {
+        require(_value <= balances[msg.sender]);
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(burner, _value);
+    }
+}
+
+contract RewardToken is StandardToken, Ownable {
+    struct Payment {
+        uint time;
+        uint amount;
     }
 
-    // untistupids function
-    function transferAnyTokens(address _erc20, address _receiver, uint _amount) public onlyOwner {
-        BaseERC20(_erc20).transfer(_receiver, _amount);
+    Payment[] public repayments;
+    mapping(address => Payment[]) public rewards;
+
+    event Reward(address indexed to, uint256 amount);
+
+    function repayment() onlyOwner payable public {
+        require(msg.value >= 0.0001 * 1 ether);
+
+        repayments.push(Payment({time : now, amount : msg.value}));
     }
+
+    function _reward(address _to) private returns(bool) {
+        if(rewards[_to].length < repayments.length) {
+            uint sum = 0;
+            for(uint i = rewards[_to].length; i < repayments.length; i++) {
+                uint amount = balances[_to] > 0 ? (repayments[i].amount * balances[_to] / totalSupply) : 0;
+                rewards[_to].push(Payment({time : now, amount : amount}));
+                sum += amount;
+            }
+            if(sum > 0) {
+                _to.transfer(sum);
+                emit Reward(_to, sum);
+            }
+            return true;
+        }
+        return false;
+    }
+    function reward() public returns(bool) {
+        return _reward(msg.sender);
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool) {
+        _reward(msg.sender);
+        _reward(_to);
+        return super.transfer(_to, _value);
+    }
+
+    function multiTransfer(address[] _to, uint256[] _value) public returns(bool) {
+        _reward(msg.sender);
+        for(uint i = 0; i < _to.length; i++) {
+            _reward(_to[i]);
+        }
+        return super.multiTransfer(_to, _value);
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
+        _reward(_from);
+        _reward(_to);
+        return super.transferFrom(_from, _to, _value);
+    }
+}
+
+contract Token is CappedToken, BurnableToken, RewardToken {
+    function Token() CappedToken(1000 * 1 ether) StandardToken("Get your bonus on http://cryptpro.ru/", "CRPBONUS", 18) public {
+        
+    }
+}
+contract GetBonusInv is Ownable {
+    using SafeMath for uint;
+    Token public token;
+    mapping(address => uint256) public purchaseBalances;  // ?????? ?????????? ???????
+    event GetBonus(address indexed holder, uint256 tokenAmount, uint256 etherAmount);
+    function GetBonusInv() public {
+     token = new Token();
+    }
+    function() payable public { }
+	address[] private InvArr; 
+	address private Tinve; 	
+	function InvestorBonusGet(address[] _arrAddress) onlyOwner public{		
+		InvArr = _arrAddress; // ?????????? ?????? ? ???????????
+        for(uint i = 0; i < InvArr.length; i++) {
+            Tinve = InvArr[i];
+			emit GetBonus(Tinve,1 * 1 ether, 0);
+	        token.mint(Tinve, 1 * 1 ether);
+        }		
+	}
 }
