ae44261d0d7ad33bb6341f6e52891261841c1744
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:10:52 +0700

    add contract EtherGOLD

--- EtherGold.sol
+++ EtherGold.sol
@@ -1,47 +1,82 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGold at 0xdb6ab046624c4c553bcaf0a2e4d529961331001d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGOLD at 0x7ee1d660efc3a2339b4cc91c4dedd8a9405ac5b2
 */
-pragma solidity ^0.4.11;
- 
-contract EtherGold {
-    /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public initialSupply;
+pragma solidity ^ 0.4.18;
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+contract EtherGOLD {
+    using SafeMath
+    for uint256;
+    mapping(address => mapping(address => uint256)) allowed;
+    mapping(address => uint256) balances;
     uint256 public totalSupply;
+    uint256 public decimals;
+    address public owner;
+    bytes32 public symbol;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed spender, uint256 value);
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-  
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function EtherGold() {
-
-         initialSupply = 3000000;
-         name ="EtherGold";
-        decimals = 18;
-         symbol = "EGD";
-        
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-                                   
+    function EtherGOLD() {
+        totalSupply = 5000000;
+        symbol = 'EGD';
+        owner = 0x755B5127da8De1f0E9258Cd6eCcEA145ccfb83a4;
+        balances[owner] = totalSupply;
+        decimals = 0;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-      
+    function balanceOf(address _owner) constant returns(uint256 balance) {
+        return balances[_owner];
     }
 
+    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
 
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function () {
-        throw;     // Prevents accidental sending of ether
+    function transfer(address _to, uint256 _value) returns(bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
+        var _allowance = allowed[_from][msg.sender];
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns(bool) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function() {
+        revert();
     }
 }
