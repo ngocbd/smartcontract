f3b74e25582375adeaee9b77224ec3efc6ed3ed4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:31:17 +0700

    add contract EDOGE

--- EDOGE.sol
+++ EDOGE.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EDOGE at 0x1dbd7059247baf317b11c9e36308fa8582867d4b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EDOGE at 0xb6ca1b4c1e511ce1345bde831ea4f4518401c7c5
 */
 pragma solidity ^0.4.17;
 
@@ -9,27 +9,19 @@ pragma solidity ^0.4.17;
 contract ERC223 {
     uint public totalSupply;
 
-    // ERC223 and ERC20 functions and events
     function balanceOf(address who) public constant returns (uint);
-    function totalSupply() constant public returns (uint256 _supply);
-    function transfer(address to, uint value) public returns (bool ok);
 
-    // ERC20 functions and events
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-
-    // ERC223 functions and events
     function name() constant public returns (string _name);
     function symbol() constant public returns (string _symbol);
     function decimals() constant public returns (uint8 _decimals);
+    function totalSupply() constant public returns (uint256 _supply);
 
+    function transfer(address to, uint value) public returns (bool ok);
     function transfer(address to, uint value, bytes data) public returns (bool ok);
     function transfer(address to, uint value, bytes data, string customFallback) public returns (bool ok);
     event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
-
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
 
 /**
