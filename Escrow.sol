4c60d027059cb5eacfa761f96390c3c6f4160c84
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:25 +0700

    add contract Escrow

--- Escrow.sol
+++ Escrow.sol
@@ -1,24 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Escrow at 0x665f2d9e0ea20573255ecddb2d597bf94ef30784
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Escrow at 0x58b71665b05b7952472fb753541cd2cbd1020fab
 */
+pragma solidity ^0.4.0;
 contract Escrow {
     address buyer;
     address seller;
-    address arbitrator;
-
+    address arbiter;
+    
     function Escrow() payable {
-       seller = 0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f;
-       buyer = msg.sender;
-       arbitrator = 0xabad6ec946eff02b22e4050b3209da87380b3cbd;
+        seller = 0x1db3439a222c519ab44bb1144fc28167b4fa6ee6;
+        arbiter = 0xd8da6bf26964af9d7eed9e03e53415d37aa96045;
+        buyer = msg.sender;
     }
     
     function finalize() {
-        if (msg.sender == buyer || msg.sender == arbitrator)
-            seller.send(this.balance);
+        if (msg.sender == buyer || msg.sender == arbiter)
+            seller.send(msg.value);
     }
     
     function refund() {
-        if (msg.sender == seller || msg.sender == arbitrator)
-            buyer.send(this.balance);
+        if (msg.sender == seller || msg.sender == arbiter)
+            buyer.send(msg.value);
     }
 }
