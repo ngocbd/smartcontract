70c5a309ea7bc1e70f3c0535d295f345d8bdc409
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:46:41 +0700

    add contract KyberReserve

--- KyberReserve.sol
+++ KyberReserve.sol
@@ -1,144 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KyberReserve at 0xd836715f1c6a249237feb47fa4d4186591da0f22
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KyberReserve at 0x668d6003399D2D3Bad858553EAF4B9477fF0962D
 */
 pragma solidity 0.4.18;
-
-interface ConversionRatesInterface {
-
-    function recordImbalance(
-        ERC20 token,
-        int buyAmount,
-        uint rateUpdateBlock,
-        uint currentBlock
-    )
-        public;
-
-    function getRate(ERC20 token, uint currentBlockNumber, bool buy, uint qty) public view returns(uint);
-}
-
-contract PermissionGroups {
-
-    address public admin;
-    address public pendingAdmin;
-    mapping(address=>bool) internal operators;
-    mapping(address=>bool) internal alerters;
-    address[] internal operatorsGroup;
-    address[] internal alertersGroup;
-
-    function PermissionGroups() public {
-        admin = msg.sender;
-    }
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin);
-        _;
-    }
-
-    modifier onlyOperator() {
-        require(operators[msg.sender]);
-        _;
-    }
-
-    modifier onlyAlerter() {
-        require(alerters[msg.sender]);
-        _;
-    }
-
-    function getOperators () external view returns(address[]) {
-        return operatorsGroup;
-    }
-
-    function getAlerters () external view returns(address[]) {
-        return alertersGroup;
-    }
-
-    event TransferAdminPending(address pendingAdmin);
-
-    /**
-     * @dev Allows the current admin to set the pendingAdmin address.
-     * @param newAdmin The address to transfer ownership to.
-     */
-    function transferAdmin(address newAdmin) public onlyAdmin {
-        require(newAdmin != address(0));
-        TransferAdminPending(pendingAdmin);
-        pendingAdmin = newAdmin;
-    }
-
-    event AdminClaimed( address newAdmin, address previousAdmin);
-
-    /**
-     * @dev Allows the pendingAdmin address to finalize the change admin process.
-     */
-    function claimAdmin() public {
-        require(pendingAdmin == msg.sender);
-        AdminClaimed(pendingAdmin, admin);
-        admin = pendingAdmin;
-        pendingAdmin = address(0);
-    }
-
-    event AlerterAdded (address newAlerter, bool isAdd);
-
-    function addAlerter(address newAlerter) public onlyAdmin {
-        require(!alerters[newAlerter]); // prevent duplicates.
-        AlerterAdded(newAlerter, true);
-        alerters[newAlerter] = true;
-        alertersGroup.push(newAlerter);
-    }
-
-    function removeAlerter (address alerter) public onlyAdmin {
-        require(alerters[alerter]);
-        alerters[alerter] = false;
-
-        for (uint i = 0; i < alertersGroup.length; ++i) {
-            if (alertersGroup[i] == alerter) {
-                alertersGroup[i] = alertersGroup[alertersGroup.length - 1];
-                alertersGroup.length--;
-                AlerterAdded(alerter, false);
-                break;
-            }
-        }
-    }
-
-    event OperatorAdded(address newOperator, bool isAdd);
-
-    function addOperator(address newOperator) public onlyAdmin {
-        require(!operators[newOperator]); // prevent duplicates.
-        OperatorAdded(newOperator, true);
-        operators[newOperator] = true;
-        operatorsGroup.push(newOperator);
-    }
-
-    function removeOperator (address operator) public onlyAdmin {
-        require(operators[operator]);
-        operators[operator] = false;
-
-        for (uint i = 0; i < operatorsGroup.length; ++i) {
-            if (operatorsGroup[i] == operator) {
-                operatorsGroup[i] = operatorsGroup[operatorsGroup.length - 1];
-                operatorsGroup.length -= 1;
-                OperatorAdded(operator, false);
-                break;
-            }
-        }
-    }
-}
-
-interface KyberReserveInterface {
-
-    function trade(
-        ERC20 srcToken,
-        uint srcAmount,
-        ERC20 destToken,
-        address destAddress,
-        uint conversionRate,
-        bool validate
-    )
-        public
-        payable
-        returns(bool);
-
-    function getConversionRate(ERC20 src, ERC20 dest, uint srcQty, uint blockNumber) public view returns(uint);
-}
 
 contract Utils {
 
@@ -197,6 +60,170 @@ contract Utils {
         }
         return (numerator + denominator - 1) / denominator; //avoid rounding down errors
     }
+}
+
+contract PermissionGroups {
+
+    address public admin;
+    address public pendingAdmin;
+    mapping(address=>bool) internal operators;
+    mapping(address=>bool) internal alerters;
+    address[] internal operatorsGroup;
+    address[] internal alertersGroup;
+    uint constant internal MAX_GROUP_SIZE = 50;
+
+    function PermissionGroups() public {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    modifier onlyOperator() {
+        require(operators[msg.sender]);
+        _;
+    }
+
+    modifier onlyAlerter() {
+        require(alerters[msg.sender]);
+        _;
+    }
+
+    function getOperators () external view returns(address[]) {
+        return operatorsGroup;
+    }
+
+    function getAlerters () external view returns(address[]) {
+        return alertersGroup;
+    }
+
+    event TransferAdminPending(address pendingAdmin);
+
+    /**
+     * @dev Allows the current admin to set the pendingAdmin address.
+     * @param newAdmin The address to transfer ownership to.
+     */
+    function transferAdmin(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0));
+        TransferAdminPending(pendingAdmin);
+        pendingAdmin = newAdmin;
+    }
+
+    /**
+     * @dev Allows the current admin to set the admin in one tx. Useful initial deployment.
+     * @param newAdmin The address to transfer ownership to.
+     */
+    function transferAdminQuickly(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0));
+        TransferAdminPending(newAdmin);
+        AdminClaimed(newAdmin, admin);
+        admin = newAdmin;
+    }
+
+    event AdminClaimed( address newAdmin, address previousAdmin);
+
+    /**
+     * @dev Allows the pendingAdmin address to finalize the change admin process.
+     */
+    function claimAdmin() public {
+        require(pendingAdmin == msg.sender);
+        AdminClaimed(pendingAdmin, admin);
+        admin = pendingAdmin;
+        pendingAdmin = address(0);
+    }
+
+    event AlerterAdded (address newAlerter, bool isAdd);
+
+    function addAlerter(address newAlerter) public onlyAdmin {
+        require(!alerters[newAlerter]); // prevent duplicates.
+        require(alertersGroup.length < MAX_GROUP_SIZE);
+
+        AlerterAdded(newAlerter, true);
+        alerters[newAlerter] = true;
+        alertersGroup.push(newAlerter);
+    }
+
+    function removeAlerter (address alerter) public onlyAdmin {
+        require(alerters[alerter]);
+        alerters[alerter] = false;
+
+        for (uint i = 0; i < alertersGroup.length; ++i) {
+            if (alertersGroup[i] == alerter) {
+                alertersGroup[i] = alertersGroup[alertersGroup.length - 1];
+                alertersGroup.length--;
+                AlerterAdded(alerter, false);
+                break;
+            }
+        }
+    }
+
+    event OperatorAdded(address newOperator, bool isAdd);
+
+    function addOperator(address newOperator) public onlyAdmin {
+        require(!operators[newOperator]); // prevent duplicates.
+        require(operatorsGroup.length < MAX_GROUP_SIZE);
+
+        OperatorAdded(newOperator, true);
+        operators[newOperator] = true;
+        operatorsGroup.push(newOperator);
+    }
+
+    function removeOperator (address operator) public onlyAdmin {
+        require(operators[operator]);
+        operators[operator] = false;
+
+        for (uint i = 0; i < operatorsGroup.length; ++i) {
+            if (operatorsGroup[i] == operator) {
+                operatorsGroup[i] = operatorsGroup[operatorsGroup.length - 1];
+                operatorsGroup.length -= 1;
+                OperatorAdded(operator, false);
+                break;
+            }
+        }
+    }
+}
+
+interface ConversionRatesInterface {
+
+    function recordImbalance(
+        ERC20 token,
+        int buyAmount,
+        uint rateUpdateBlock,
+        uint currentBlock
+    )
+        public;
+
+    function getRate(ERC20 token, uint currentBlockNumber, bool buy, uint qty) public view returns(uint);
+}
+
+interface ERC20 {
+    function totalSupply() public view returns (uint supply);
+    function balanceOf(address _owner) public view returns (uint balance);
+    function transfer(address _to, uint _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
+    function approve(address _spender, uint _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint remaining);
+    function decimals() public view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+interface KyberReserveInterface {
+
+    function trade(
+        ERC20 srcToken,
+        uint srcAmount,
+        ERC20 destToken,
+        address destAddress,
+        uint conversionRate,
+        bool validate
+    )
+        public
+        payable
+        returns(bool);
+
+    function getConversionRate(ERC20 src, ERC20 dest, uint srcQty, uint blockNumber) public view returns(uint);
 }
 
 interface SanityRatesInterface {
@@ -455,15 +482,4 @@ contract KyberReserve is KyberReserveInterface, Withdrawable, Utils {
 
         return true;
     }
-}
-
-interface ERC20 {
-    function totalSupply() public view returns (uint supply);
-    function balanceOf(address _owner) public view returns (uint balance);
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
-    function decimals() public view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
