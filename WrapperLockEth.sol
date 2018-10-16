cff29376a5f40758fbb7cdc1b31c311e1e6677dc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:49:01 +0700

    add contract WrapperLockEth

--- WrapperLockEth.sol
+++ WrapperLockEth.sol
@@ -1,7 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WrapperLockEth at 0x991f0e5c5775ca9dd02e30e67f545c36cff3690e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WrapperLockEth at 0x9fb2fd4bce3061b826bf1a0b53b737c6f3e0e586
 */
-pragma solidity 0.4.19;
+pragma solidity ^0.4.22;
+
+
+
 /**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
@@ -13,11 +16,70 @@ contract ERC20Basic {
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  uint256 totalSupply_;
+
+  /**
+  * @dev total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
+
   /**
   * @dev Multiplies two numbers, throws on overflow.
   */
@@ -29,6 +91,7 @@ library SafeMath {
     assert(c / a == b);
     return c;
   }
+
   /**
   * @dev Integer division of two numbers, truncating the quotient.
   */
@@ -38,6 +101,7 @@ library SafeMath {
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
+
   /**
   * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
@@ -45,6 +109,7 @@ library SafeMath {
     assert(b <= a);
     return a - b;
   }
+
   /**
   * @dev Adds two numbers, throws on overflow.
   */
@@ -54,43 +119,8 @@ library SafeMath {
     return c;
   }
 }
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances.
- */
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-  mapping(address => uint256) balances;
-  uint256 totalSupply_;
-  /**
-  * @dev total number of tokens in existence
-  */
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-    // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-}
+
+
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -98,7 +128,11 @@ contract BasicToken is ERC20Basic {
  */
 contract Ownable {
   address public owner;
+
+
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
@@ -106,6 +140,7 @@ contract Ownable {
   function Ownable() public {
     owner = msg.sender;
   }
+
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -113,6 +148,7 @@ contract Ownable {
     require(msg.sender == owner);
     _;
   }
+
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
@@ -122,20 +158,29 @@ contract Ownable {
     OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
+
 }
+
 /*
+
 Copyright Will Harborne (Ethfinex) 2017
+
 */
+
 contract WrapperLockEth is BasicToken, Ownable {
     using SafeMath for uint256;
+
     address public TRANSFER_PROXY;
     mapping (address => bool) private isSigner;
+
     string public name;
     string public symbol;
     uint public decimals;
     address public originalToken = 0x00;
+
     mapping (address => uint) public depositLock;
     mapping (address => uint256) public balances;
+
     function WrapperLockEth(string _name, string _symbol, uint _decimals, address _transferProxy) {
         TRANSFER_PROXY = _transferProxy;
         name = _name;
@@ -143,6 +188,7 @@ contract WrapperLockEth is BasicToken, Ownable {
         decimals = _decimals;
         isSigner[msg.sender] = true;
     }
+
     function deposit(uint _value, uint _forTime) public payable returns (bool success) {
         require(_forTime >= 1);
         require(now + _forTime * 1 hours >= depositLock[msg.sender]);
@@ -150,6 +196,7 @@ contract WrapperLockEth is BasicToken, Ownable {
         depositLock[msg.sender] = now + _forTime * 1 hours;
         return true;
     }
+
     function withdraw(
         uint8 v,
         bytes32 r,
@@ -173,9 +220,11 @@ contract WrapperLockEth is BasicToken, Ownable {
         }
         return true;
     }
+
     function transfer(address _to, uint256 _value) public returns (bool) {
         return false;
     }
+
     function transferFrom(address _from, address _to, uint _value) public {
         require(_to == owner || _from == owner);
         assert(msg.sender == TRANSFER_PROXY);
@@ -183,14 +232,17 @@ contract WrapperLockEth is BasicToken, Ownable {
         balances[_from] = balances[_from].sub(_value);
         Transfer(_from, _to, _value);
     }
+
     function allowance(address _owner, address _spender) public constant returns (uint) {
         if (_spender == TRANSFER_PROXY) {
             return 2**256 - 1;
         }
     }
+
     function balanceOf(address _owner) public constant returns (uint256) {
         return balances[_owner];
     }
+
     function isValidSignature(
         bytes32 hash,
         uint8 v,
@@ -207,10 +259,12 @@ contract WrapperLockEth is BasicToken, Ownable {
             s
         )];
     }
+
     function addSigner(address _newSigner) public {
         require(isSigner[msg.sender]);
         isSigner[_newSigner] = true;
     }
+
     function keccak(address _sender, address _wrapper, uint _validTill) public constant returns(bytes32) {
         return keccak256(_sender, _wrapper, _validTill);
     }
