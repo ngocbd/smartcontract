9c61cd16133bc40ce0a49adda2fc3a0f179cb820
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:14:15 +0700

    add contract StandardToken

--- StandardToken.sol
+++ StandardToken.sol
@@ -1,176 +1,167 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract StandardToken at 0xf49cdd50ad408d387d611f88a647179c3de3492b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract StandardToken at 0x5f2c5cfeb3e752c32a9d8224854fe0b6e523d78e
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.23;
 
-library SafeMath
-{
-	function mul(uint a, uint b) internal pure returns (uint)
-	{
-		if (a == 0)
-		{
-			return 0;
-		}
-		uint c = a * b;
-		assert(c / a == b);
-		return c;
-	}
-
-	function div(uint a, uint b) internal pure returns (uint)
-	{
-		uint c = a / b;
-		return c;
-	}
-
-	function sub(uint a, uint b) internal pure returns (uint)
-	{
-		assert(b <= a);
-		return a - b;
-	}
-
-	function add(uint a, uint b) internal pure returns (uint)
-	{
-		uint c = a + b;
-		assert(c >= a);
-		return c;
-	}
+interface ERC20 {
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-interface ERC20
-{
-	function balanceOf(address who) public view returns (uint);
-	function transfer(address to, uint value) public returns (bool);
-	function allowance(address owner, address spender) public view returns (uint);
-	function transferFrom(address from, address to, uint value) public returns (bool);
-	function approve(address spender, uint value) public returns (bool);
-	event Transfer(address indexed from, address indexed to, uint value);
-	event Approval(address indexed owner, address indexed spender, uint value);
+interface ERC223 {
+    function transfer(address to, uint value, bytes data) public;
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
 }
 
-interface ERC223
-{
-	function transfer(address to, uint value, bytes data) public;
-	event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+contract ERC223ReceivingContract { 
+    function tokenFallback(address _from, uint _value, bytes _data) public;
 }
 
-contract ERC223ReceivingContract
-{
-	function tokenFallback(address _from, uint _value, bytes _data) public;
+/**
+* @title SafeMath
+* @dev Math operations with safety checks that throw on error
+*/
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+    
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+    
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+    
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
-contract StandardToken is ERC20, ERC223
-{
-	using SafeMath for uint;
+contract StandardToken is ERC20, ERC223 {
+    using SafeMath for uint;
+    
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint256 internal _totalSupply;
+    
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    
+    constructor(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
+        _symbol = symbol;
+        _name = name;
+        _decimals = decimals;
+        _totalSupply = totalSupply;
+        balances[msg.sender] = totalSupply;
+    }
+    
+    function name() public view returns (string) {
+        return _name;
+    }
+    
+    function symbol() public view returns (string) {
+        return _symbol;
+    }
+    
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+    
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+        balances[_to] = SafeMath.add(balances[_to], _value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        
+        balances[_from] = SafeMath.sub(balances[_from], _value);
+        balances[_to] = SafeMath.add(balances[_to], _value);
+        allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+    
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+    
+    
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+    
+    function transfer(address _to, uint _value, bytes _data) public {
+        require(_value > 0 );
+        if(isContract(_to)) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value, _data);
+    }
+    
+    function isContract(address _addr) private returns (bool is_contract) {
+        uint length;
+        assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+        }
+        return (length>0);
+    }
 
-	string public name;
-	string public symbol;
-	uint8 public decimals;
-	uint public totalSupply;
 
-	mapping (address => uint) balances;
-	mapping (address => mapping (address => uint)) allowed;
-
-	function StandardToken(string _name, string _symbol, uint8 _decimals, uint _totalSupply, address _admin) public
-	{
-		name = _name;
-		symbol = _symbol;
-		decimals = _decimals;
-		totalSupply = _totalSupply * 10 ** uint(_decimals);
-		balances[_admin] = totalSupply;
-	}
-
-	function tokenFallback(address _from, uint _value, bytes _data)
-	{
-	    revert();
-	}
-	
-	function () //revert any ether sent to this contract
-	{
-		revert();
-	}
-	
-	function balanceOf(address _owner) public view returns (uint balance)
-	{
-		return balances[_owner];
-	}
-
-	function transfer(address _to, uint _value) public returns (bool)
-	{
-		require(_to != address(0));
-		require(_value <= balances[msg.sender]);
-		balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
-		balances[_to] = SafeMath.add(balances[_to], _value);
-		Transfer(msg.sender, _to, _value);
-		return true;
-	}
-
-	function transferFrom(address _from, address _to, uint _value) public returns (bool)
-	{
-		require(_to != address(0));
-		require(_value <= balances[_from]);
-		require(_value <= allowed[_from][msg.sender]);
-
-		balances[_from] = SafeMath.sub(balances[_from], _value);
-		balances[_to] = SafeMath.add(balances[_to], _value);
-		allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
-		Transfer(_from, _to, _value);
-		return true;
-	}
-
-	function approve(address _spender, uint _value) public returns (bool)
-	{
-		allowed[msg.sender][_spender] = _value;
-		Approval(msg.sender, _spender, _value);
-		return true;
-	}
-
-	function allowance(address _owner, address _spender) public view returns (uint)
-	{
-		return allowed[_owner][_spender];
-	}
-
-	function increaseApproval(address _spender, uint _addedValue) public returns (bool)
-	{
-		allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
-		Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-		return true;
-	}
-
-	function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool)
-	{
-		uint oldValue = allowed[msg.sender][_spender];
-		if (_subtractedValue > oldValue)
-		{
-			allowed[msg.sender][_spender] = 0;
-		}
-		else
-		{
-			allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
-		}
-		Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-		return true;
-	}
-	
-	function transfer(address _to, uint _value, bytes _data) public
-	{
-		require(_value > 0 );
-		if(isContract(_to))
-		{
-			ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-			receiver.tokenFallback(msg.sender, _value, _data);
-		}
-		balances[msg.sender] = balances[msg.sender].sub(_value);
-		balances[_to] = balances[_to].add(_value);
-		Transfer(msg.sender, _to, _value, _data);
-	}
-		
-	function isContract(address _addr) private returns (bool is_contract)
-	{
-		uint length;
-		assembly
-		{
-			length := extcodesize(_addr)
-		}
-		return (length>0);
-	}
 }
