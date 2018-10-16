4e61d68697e9fc8a84d5d7f1ddd627b1510b50b9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:00:42 +0700

    add contract CrypteriumToken

--- CrypteriumToken.sol
+++ CrypteriumToken.sol
@@ -1,125 +1,188 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrypteriumToken at 0x62aea49c09e5f7944b10d5f05fdc6b7cb15c00b1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrypteriumToken at 0x80a7e048f37a50500351c204cb407766fa3bae7f
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.16;
 
-
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a / b;
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-
-contract Token {
-
-    function totalSupply() constant returns (uint256 supply) {}
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
     
-}
+    address public owner;
 
-
-
-contract StandardToken is Token {
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    function Ownable() public {
+        owner = msg.sender;
+    }
     
-    using SafeMath for uint256;
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != address(0));      
+        owner = newOwner;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+}
+
+contract CrypteriumToken is Ownable {
+    
+    uint256 public totalSupply;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+    
+    string public constant name = "CrypteriumToken";
+    string public constant symbol = "CRPT";
+    uint32 public constant decimals = 18;
+
+    uint constant restrictedPercent = 30; //should never be set above 100
+    address constant restricted = 0x1d907C982B0B093b5173574FAbe7965181522c7B;
+    uint constant start = 1509458400;
+    uint constant period = 87;
+    uint256 public constant hardcap = 300000000 * 1 ether;
+    
+    bool public transferAllowed = false;
+    bool public mintingFinished = false;
+    
+    modifier whenTransferAllowed() {
+        if(msg.sender != owner){
+            require(transferAllowed);
+        }
+        _;
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    modifier saleIsOn() {
+        require(now > start && now < start + period * 1 days);
+        _;
+    }
+    
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
+  
+    function transfer(address _to, uint256 _value) whenTransferAllowed public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+        
+        balances[msg.sender] = balances[msg.sender] - _value;
+        balances[_to] = balances[_to] + _value;
+        //assert(balances[_to] >= _value); no need to check, since mint has limited hardcap
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant public returns (uint256 balance) {
         return balances[_owner];
     }
+    
+    function transferFrom(address _from, address _to, uint256 _value) whenTransferAllowed public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        
+        balances[_from] = balances[_from] - _value;
+        balances[_to] = balances[_to] + _value;
+        //assert(balances[_to] >= _value); no need to check, since mint has limited hardcap
+        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        //NOTE: To prevent attack vectors like the one discussed here: 
+        //https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729,
+        //clients SHOULD make sure to create user interfaces in such a way 
+        //that they set the allowance first to 0 before setting it to another value for the same spender. 
+    
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
-
-contract CrypteriumToken is StandardToken {
-
-    function () {
-        throw;
+   
+    function allowTransfer() onlyOwner public {
+        transferAllowed = true;
     }
-
-
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;                 
-    string public version = '1.0';     
-
-
-    function CrypteriumToken(
-        ) {
-        balances[msg.sender] = 100000000000000000000000000; 
-        totalSupply = 100000000000000000000000000;  
-        name = "Crypterium";   
-        decimals = 18; 
-        symbol = "CRYPT";  
-    }
-
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+    
+    function mint(address _to, uint256 _value) onlyOwner saleIsOn canMint public returns (bool) {
+        require(_to != address(0));
+        
+        uint restrictedTokens = _value * restrictedPercent / (100 - restrictedPercent);
+        uint _amount = _value + restrictedTokens;
+        assert(_amount >= _value);
+        
+        if(_amount + totalSupply <= hardcap){
+        
+            totalSupply = totalSupply + _amount;
+            
+            assert(totalSupply >= _amount);
+            
+            balances[msg.sender] = balances[msg.sender] + _amount;
+            assert(balances[msg.sender] >= _amount);
+            Mint(msg.sender, _amount);
+        
+            transfer(_to, _value);
+            transfer(restricted, restrictedTokens);
+        }
         return true;
     }
+
+    function finishMinting() onlyOwner public returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
+    
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public returns (bool) {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+        balances[msg.sender] = balances[msg.sender] - _value;
+        totalSupply = totalSupply - _value;
+        Burn(msg.sender, _value);
+        return true;
+    }
+    
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        balances[_from] = balances[_from] - _value;
+        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
+        totalSupply = totalSupply - _value;
+        Burn(_from, _value);
+        return true;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    event Mint(address indexed to, uint256 amount);
+
+    event MintFinished();
+
+    event Burn(address indexed burner, uint256 value);
+
 }
