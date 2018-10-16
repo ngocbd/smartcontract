56bef74b6c2b219891c8a6731beac3f86887b6fd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:08:01 +0700

    add contract MultiSend

--- Multisend.sol
+++ Multisend.sol
@@ -1,37 +1,55 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Multisend at 0x15e745a2e3b677ec6421291b212d3917b3738dee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSend at 0xa63864e4b6eb2838ea5cc8f1dace9909d38c364a
 */
-contract Multisend {
-    mapping(address => uint) public balances;
-    mapping(address => uint) public nonces;
+pragma solidity ^0.4.19;
 
+/* Function required from STCn main contract */
+contract ERC20Token {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
     
-    function send(address[] addrs, uint[] amounts, uint nonce) {
-        if(addrs.length != amounts.length || nonce != nonces[msg.sender]) throw;
-        uint val = msg.value;
-        
-        for(uint i = 0; i<addrs.length; i++){
-            if(val < amounts[i]) throw;
-            
-            if(!addrs[i].send(amounts[i])){
-                balances[addrs[i]] += amounts[i];
-            }
-            val -= amounts[i];
+}
+
+contract MultiSend {
+    ERC20Token public _STCnContract;
+    address public _multiSendOwner;
+    uint256 public tokensApproved;
+    
+    function MultiSend () {
+        address c = 0x420C42cE1370c0Ec3ca87D9Be64A7002E78e6709; // set STCn contract address
+        _STCnContract = ERC20Token(c); 
+        _multiSendOwner = msg.sender;
+        tokensApproved = 0; // set to 0 first as allowance to contract can't be set yet
+    }
+    
+    /* Before first sending, make sure to allow this contract spending from token contract with function approve(address _spender, uint256 _value)
+    ** and to update tokensApproved with function updateTokensApproved () */
+    
+    function dropCoinsSingle(address[] dests, uint256 tokens) {
+        require(msg.sender == _multiSendOwner && tokensApproved >= (dests.length * tokens));
+        uint256 i = 0;
+        while (i < dests.length) {
+            _STCnContract.transferFrom(_multiSendOwner, dests[i], tokens);
+            i += 1;
         }
-        
-        if(!msg.sender.send(val)){
-            balances[msg.sender] += val;
+        updateTokensApproved();
+    }
+    
+    /* Be careful to this function to be sure you approved enough before you send as contract can't check first total amount in array
+    ** If not enough amount is approved, transaction will fail */
+    
+    function dropCoinsMulti(address[] dests, uint256[] tokens) {
+        require(msg.sender == _multiSendOwner);
+        uint256 i = 0;
+        while (i < dests.length) {
+            _STCnContract.transferFrom(_multiSendOwner, dests[i], tokens[i]);
+            i += 1;
         }
-        nonces[msg.sender]++;
+        updateTokensApproved();
     }
     
-    function withdraw(){
-        uint balance = balances[msg.sender];
-        balances[msg.sender] = 0;
-        if(!msg.sender.send(balance)) throw;
+    function updateTokensApproved () {
+        tokensApproved = _STCnContract.allowance(_multiSendOwner, this);
     }
     
-    function(){
-        withdraw();
-    }
 }
