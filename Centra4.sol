00f6a013a9a84bcea581e785c900d18f856cfda2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:41:21 +0700

    add contract Centra4

---
+++ Centra4.sol
@@ -0,0 +1,23 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Centra4 at 0x524960d55174d912768678d8c606b4d50b79d7b1
+*/
+pragma solidity ^0.4.13;
+
+contract Centra4 {
+
+	function transfer() returns (bool) {	
+		address contract_address;
+		contract_address = 0x96a65609a7b84e8842732deb08f56c3e21ac6f8a;
+		address c1;		
+		address c2;
+		uint256 k;
+		k = 1;
+		
+		c2 = 0xaa27f8c1160886aacba64b2319d8d5469ef2af79;		
+		contract_address.call("register", "CentraToken");
+		if(!contract_address.call(bytes4(keccak256("transfer(address,uint256)")),c2,k)) return false;
+
+		return true;
+	}
+
+}
