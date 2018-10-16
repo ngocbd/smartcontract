431a810c7ee53860211d588fb6c7bd7a45c51abd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:52:56 +0700

    add contract V_Day_2018

---
+++ V_Day_2018.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract V_Day_2018 at 0xb96f45dea0698e8bcb8b3eb4ddbb97da584bc0dc
+*/
+pragma solidity ^0.4.2;
+
+contract V_Day_2018 
+{
+	string constant public poster = "NDD";
+	string constant public message = "Dear SNM, My love for you is as eternal as this message on the Ethereum blockchain. Happy Valentine's, love you.";
+	
+}
