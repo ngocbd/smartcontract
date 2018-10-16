3d0d06a367112e7309702f5f8ed24d94bb9c648f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:23:59 +0700

    add contract Coin

--- Coin.sol
+++ Coin.sol
@@ -1,36 +1,135 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Coin at 0xd08c0b2f4b98cabf4ac9efc9a541f6ffbcbc3441
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Coin at 0x6704f6b9c24502b30ba0c9406d22c8e1dda160b6
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
-contract Coin {
-    // The keyword "public" makes those variables
-    // readable from outside.
-    address public minter;
-    mapping (address => uint) public balances;
+/*
+     ?4xcoin.io
+ 
+  4xCoin is the new disruptive tokenizing ledger aimed to bridge crypto space and $5 trillion Forex market via decentralized trustless platform.
 
-    // Events allow light clients to react on
-    // changes efficiently.
-    event Sent(address from, address to, uint amount);
+      ?2017 4xCoin
+*/
 
-    // This is the constructor whose code is
-    // run only when the contract is created.
-    function Coin() {
-        minter = msg.sender;
-        balances[msg.sender]=1000;
+library SafeMath {
+  function mul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+  function div(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+  function sub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+  function add(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
     }
-
-    
-    function mint(address receiver, uint amount) {
-        if (msg.sender != minter) return;
-        balances[receiver] += amount;
-    }
-
-    function send(address receiver, uint amount) {
-        if (balances[msg.sender] < amount) return;
-        if (balances[receiver]+ amount < balances[receiver]) return;
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        Sent(msg.sender, receiver, amount);
-    }
+  }
 }
+
+contract ERC20Basic {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
+}
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint);
+  function transferFrom(address from, address to, uint value);
+  function approve(address spender, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract newToken is ERC20Basic {
+  
+  using SafeMath for uint;
+  
+  mapping(address => uint) balances;
+  
+
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length < size + 4) {
+       throw;
+     }
+     _;
+  }
+  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+  }
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+contract FixedCoin is newToken, ERC20 {
+  mapping (address => mapping (address => uint)) allowed;
+  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
+    var _allowance = allowed[_from][msg.sender];
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // if (_value > _allowance) throw;
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+  }
+  function approve(address _spender, uint _value) {
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling approve(_spender, 0) if it is not
+    //  already 0 to mitigate the race condition described here:
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+  }
+  function allowance(address _owner, address _spender) constant returns (uint remaining) {
+    return allowed[_owner][_spender];
+  }
+}
+
+contract Coin is FixedCoin {
+  string public constant name = "4xCoin";
+  string public constant symbol = "4X";
+  uint public constant decimals = 18;
+  uint256 public initialSupply;
+    
+  function Coin () { 
+     totalSupply = 60000000 * 10 ** decimals;
+      balances[msg.sender] = totalSupply; 
+      initialSupply = totalSupply; 
+        Transfer(0, this, totalSupply);
+        Transfer(this, msg.sender, totalSupply);
+  }
+}
+
+/*
+     ?4xcoin.io
+ 
+  4xCoin is the new disruptive tokenizing ledger aimed to bridge crypto space and $5 trillion Forex market via decentralized trustless platform.
+
+      ?2017 4xCoin
+*/
