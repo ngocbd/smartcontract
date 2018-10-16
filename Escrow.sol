dbb3f1906be637318774dc3b641861f6b74ca37b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:58 +0700

    add contract Escrow

--- Escrow.sol
+++ Escrow.sol
@@ -1,24 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Escrow at 0x9b27a23006b6612b2dfc840d0a25f4e347121d21
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Escrow at 0x66943100bf0464c1ec43b957a5f72fe2d67a3678
 */
 contract Escrow {
+    
     address seller;
     address buyer;
-    address arbitrator;
+    address arbiter;
     
-    function Escrow(address _seller, address _arbitrator) {
-        seller = _seller;
-        arbitrator = _arbitrator;
+    function Escrow() {
         buyer = msg.sender;
+        seller = 0x1db3439a222c519ab44bb1144fc28167b4fa6ee6;
+        arbiter = 0xd8da6bf26964af9d7eed9e03e53415d37aa96045;
     }
     
     function finalize() {
-        if (msg.sender == buyer || msg.sender == arbitrator)
-            seller.send(this.balance);
+        if (msg.sender != buyer && msg.sender != arbiter) throw;
+        seller.send(this.balance);
     }
     
     function refund() {
-        if (msg.sender == seller || msg.sender == arbitrator)
-            buyer.send(this.balance);
+        if (msg.sender != seller && msg.sender != arbiter) throw;
+        buyer.send(this.balance);        
     }
 }
