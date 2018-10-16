39ae71e01b9e608e0a488d052cd87681e73b78b3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:52:49 +0700

    add contract s_Form004

---
+++ s_Form004.sol
@@ -0,0 +1,56 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract s_Form004 at 0xce6aa4d66f9ccce1f5f21d004ff66f99c9284cb4
+*/
+pragma solidity ^0.4.13;
+ 
+contract s_Form004 {
+    
+    mapping (bytes32 => string) data;
+    
+    address owner;
+    
+    function s_Form004() {
+        owner = msg.sender;
+    }
+    
+    function setDataColla_AA_01(string key, string value) {
+        require(msg.sender == owner);
+        data[sha3(key)] = value;
+    }
+    
+    function getDataColla_AA_01(string key) constant returns(string) {
+        return data[sha3(key)];
+    }
+
+    function setDataColla_AA_02(string key, string value) {
+        require(msg.sender == owner);
+        data[sha3(key)] = value;
+    }
+    
+    function getDataColla_AA_02(string key) constant returns(string) {
+        return data[sha3(key)];
+    }
+    
+    function setDataColla_AB_01(string key, string value) {
+        require(msg.sender == owner);
+        data[sha3(key)] = value;
+    }
+    
+    function getDataColla_AB_01(string key) constant returns(string) {
+        return data[sha3(key)];
+    }    
+
+    function setDataColla_AB_02(string key, string value) {
+        require(msg.sender == owner);
+        data[sha3(key)] = value;
+    }
+    
+    function getDataColla_AB_02(string key) constant returns(string) {
+        return data[sha3(key)];
+    } 
+
+/*
+0xCe6Aa4d66f9CCCE1f5F21D004Ff66F99c9284Cb4
+Liechtensteinischer Sozialversicherung -Rentenversicherung AAD (association autonome et décentralisée/distribuée)
+*/
+}
