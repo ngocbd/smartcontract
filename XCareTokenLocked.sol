9409aa80b170b087f214e9eff737a340f4a4c0a6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:20:58 +0700

    add contract XCareTokenLocked

---
+++ XCareTokenLocked.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XCareTokenLocked at 0x09824e7eb4fd9c1f94b8fd7ace3952668938ab72
+*/
+pragma solidity ^0.4.16;
+
+contract XCareTokenLocked {
+    // no any function to tranfer token
+}
