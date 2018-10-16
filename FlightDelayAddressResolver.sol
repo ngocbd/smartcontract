708114f99c2cc268f05c826679ea6f28a51bfe5c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:19:31 +0700

    add contract FlightDelayAddressResolver

--- FlightDelayAddressResolver.sol
+++ FlightDelayAddressResolver.sol
@@ -1,6 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlightDelayAddressResolver at 0x63338bb37bc3a0d55d2e9505f11e56c613b51494
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlightDelayAddressResolver at 0xa2e4fb662ba999a1e1bac839340c4d34e36e5d19
 */
+// File: contracts/FlightDelayAddressResolver.sol
+
 /*
   Copyright (c) 2015-2016 Oraclize SRL
   Copyright (c) 2016 Oraclize LTD
@@ -15,20 +17,20 @@ contract FlightDelayAddressResolver {
 
     address owner;
 
-    function FlightDelayAddressResolver() {
+    function FlightDelayAddressResolver() public {
         owner = msg.sender;
     }
 
-    function changeOwner(address _owner) {
+    function changeOwner(address _owner) public {
         require(msg.sender == owner);
         owner = _owner;
     }
 
-    function getAddress() constant returns (address _addr) {
+    function getAddress() public constant returns (address _addr) {
         return addr;
     }
 
-    function setAddress(address _addr) {
+    function setAddress(address _addr) public {
         require(msg.sender == owner);
         addr = _addr;
     }
