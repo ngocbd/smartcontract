eb8043a58e75dc9c6af7f62808dc47539820f5f5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 06:53:31 +0700

    add contract MultiTransfer

--- Multitransfer.sol
+++ Multitransfer.sol
@@ -1,10 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0xa676dd063114d1b32c6b9f6dcfcd4bea81081325
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0x7824a08a0fe1b4f2562484dc80ca0f75fb00cdfc
 */
-pragma solidity ^0.4.16;
-
-
-
+pragma solidity ^0.4.23;
 
 /**
  * @title ERC20Basic
@@ -12,99 +9,44 @@ pragma solidity ^0.4.16;
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  uint256 public totalSupply;
+  function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-
-
-
-
-
-
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+contract DetailedERC20Basic is ERC20Basic {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
 }
 
+contract MultiTransfer {
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  function name(address _token) public view returns(string) { return DetailedERC20Basic(_token).name(); }
+  function symbol(address _token) public view returns(string) { return DetailedERC20Basic(_token).symbol(); }
+  function decimals(address _token) public view returns(uint8) { return DetailedERC20Basic(_token).decimals(); }
+  function balanceOf(address _token, address _who) public view returns(uint256) { return DetailedERC20Basic(_token).balanceOf(_who); }
+  function transfer(address _token, address[] _to, uint256[] _value) public returns(bool) {
 
+    require(_to.length != 0);
+    require(_value.length != 0);
+    require(_to.length == _value.length);
 
+    uint256 sum = 0;
 
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-
-
-
-
-
-contract MultiTransfer is Ownable {
-    ERC20 public tkcAddress;
-
-    function MultiTransfer() public {
-
+    for (uint256 i = 0; i < _to.length; i++) {
+      require(_to[i] != address(0));
+      sum += _value[i];
     }
 
-    function setTKC(address tkc) public onlyOwner {
-        require(tkcAddress == address(0));
-        tkcAddress = ERC20(tkc);
+    assert(balanceOf(_token, msg.sender) >= sum);
+
+    for (i = 0; i < _to.length; i++) {
+      require(DetailedERC20Basic(_token).transfer(_to[i], _value[i]));
+      emit Transfer(msg.sender, _to[i], _value[i]);
     }
 
-    function transfer(address[] to, uint[] value) public onlyOwner {
-        require(to.length == value.length);
-
-        for (uint i = 0; i < to.length; i++) {
-            tkcAddress.transferFrom(owner, to[i], value[i]);
-        }
-    }
+    return true;
+  }
 }
