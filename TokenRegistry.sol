01c467493357aa8d5e1557f5a5295f87e2950b4c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:46:22 +0700

    add contract TokenRegistry

--- TokenRegistry.sol
+++ TokenRegistry.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenRegistry at 0x2EfEc84e4C52D1392cA0210b571234AD375C2250
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenRegistry at 0x974e1e639b5a3c5f44909e1959ab786af21b7086
 */
 /*
   Copyright 2017 Loopring Project Ltd (Loopring Foundation).
@@ -95,67 +95,160 @@ contract Claimable is Ownable {
 /// @author Kongliang Zhong - <kongliang@loopring.org>,
 /// @author Daniel Wang - <daniel@loopring.org>.
 contract TokenRegistry is Claimable {
-    address[] public tokens;
-    mapping (address => bool) tokenMap;
-    mapping (string => address) tokenSymbolMap;
-    function registerToken(address _token, string _symbol)
-        external
-        onlyOwner
-    {
-        require(_token != 0x0);
-        require(!isTokenRegisteredBySymbol(_symbol));
-        require(!isTokenRegistered(_token));
-        tokens.push(_token);
-        tokenMap[_token] = true;
-        tokenSymbolMap[_symbol] = _token;
+    address[] public addresses;
+    mapping (address => TokenInfo) addressMap;
+    mapping (string => address) symbolMap;
+    
+    uint8 public constant TOKEN_STANDARD_ERC20   = 0;
+    uint8 public constant TOKEN_STANDARD_ERC223  = 1;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    /// Structs                                                              ///
+    ////////////////////////////////////////////////////////////////////////////
+    struct TokenInfo {
+        uint   pos;      // 0 mens unregistered; if > 0, pos + 1 is the
+                         // token's position in `addresses`.
+        uint8  standard; // ERC20 or ERC223
+        string symbol;   // Symbol of the token
     }
-    function unregisterToken(address _token, string _symbol)
+    
+    ////////////////////////////////////////////////////////////////////////////
+    /// Events                                                               ///
+    ////////////////////////////////////////////////////////////////////////////
+    event TokenRegistered(address addr, string symbol);
+    event TokenUnregistered(address addr, string symbol);
+    
+    ////////////////////////////////////////////////////////////////////////////
+    /// Public Functions                                                     ///
+    ////////////////////////////////////////////////////////////////////////////
+    /// @dev Disable default function.
+    function () payable public {
+        revert();
+    }
+    function registerToken(
+        address addr,
+        string  symbol
+        )
         external
         onlyOwner
     {
-        require(_token != 0x0);
-        require(tokenSymbolMap[_symbol] == _token);
-        delete tokenSymbolMap[_symbol];
-        delete tokenMap[_token];
-        for (uint i = 0; i < tokens.length; i++) {
-            if (tokens[i] == _token) {
-                tokens[i] = tokens[tokens.length - 1];
-                tokens.length --;
-                break;
-            }
+        registerStandardToken(addr, symbol, TOKEN_STANDARD_ERC20);    
+    }
+    function registerStandardToken(
+        address addr,
+        string  symbol,
+        uint8   standard
+        )
+        public
+        onlyOwner
+    {
+        require(0x0 != addr);
+        require(bytes(symbol).length > 0);
+        require(0x0 == symbolMap[symbol]);
+        require(0 == addressMap[addr].pos);
+        require(standard <= TOKEN_STANDARD_ERC223);
+        addresses.push(addr);
+        symbolMap[symbol] = addr;
+        addressMap[addr] = TokenInfo(addresses.length, standard, symbol);
+        TokenRegistered(addr, symbol);      
+    }
+    function unregisterToken(
+        address addr,
+        string  symbol
+        )
+        external
+        onlyOwner
+    {
+        require(addr != 0x0);
+        require(symbolMap[symbol] == addr);
+        delete symbolMap[symbol];
+        
+        uint pos = addressMap[addr].pos;
+        require(pos != 0);
+        delete addressMap[addr];
+        
+        // We will replace the token we need to unregister with the last token
+        // Only the pos of the last token will need to be updated
+        address lastToken = addresses[addresses.length - 1];
+        
+        // Don't do anything if the last token is the one we want to delete
+        if (addr != lastToken) {
+            // Swap with the last token and update the pos
+            addresses[pos - 1] = lastToken;
+            addressMap[lastToken].pos = pos;
         }
+        addresses.length--;
+        TokenUnregistered(addr, symbol);
     }
     function isTokenRegisteredBySymbol(string symbol)
         public
         view
         returns (bool)
     {
-        return tokenSymbolMap[symbol] != 0x0;
+        return symbolMap[symbol] != 0x0;
     }
-    function isTokenRegistered(address _token)
+    function isTokenRegistered(address addr)
         public
         view
         returns (bool)
     {
-        return tokenMap[_token];
+        return addressMap[addr].pos != 0;
     }
-    function areAllTokensRegistered(address[] tokenList)
+    function areAllTokensRegistered(address[] addressList)
         external
         view
         returns (bool)
     {
-        for (uint i = 0; i < tokenList.length; i++) {
-            if (!tokenMap[tokenList[i]]) {
+        for (uint i = 0; i < addressList.length; i++) {
+            if (addressMap[addressList[i]].pos == 0) {
                 return false;
             }
         }
         return true;
+    }
+    
+    function getTokenStandard(address addr)
+        public
+        view
+        returns (uint8)
+    {
+        TokenInfo memory info = addressMap[addr];
+        require(info.pos != 0);
+        return info.standard;
     }
     function getAddressBySymbol(string symbol)
         external
         view
         returns (address)
     {
-        return tokenSymbolMap[symbol];
+        return symbolMap[symbol];
+    }
+    
+    function getTokens(
+        uint start,
+        uint count
+        )
+        public
+        view
+        returns (address[] addressList)
+    {
+        uint num = addresses.length;
+        
+        if (start >= num) {
+            return;
+        }
+        
+        uint end = start + count;
+        if (end > num) {
+            end = num;
+        }
+        if (start == num) {
+            return;
+        }
+        
+        addressList = new address[](end - start);
+        for (uint i = start; i < end; i++) {
+            addressList[i - start] = addresses[i];
+        }
     }
 }
