2a0f664e191ab09719cf2b5f262fba80ffe24e06
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:56:20 +0700

    add contract EvenDistroCrowdsaleLib

--- EvenDistroCrowdsaleLib.sol
+++ EvenDistroCrowdsaleLib.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EvenDistroCrowdsaleLib at 0x3afde2b246a4d7466d6fa938f07625c60ef398eb
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EvenDistroCrowdsaleLib at 0x7a1bc60b7ffad01aceb258e72b4ecec6573ea4f9
 */
 pragma solidity ^0.4.18;
 
@@ -7,8 +7,8 @@ pragma solidity ^0.4.18;
  * @title EvenDistroCrowdsaleLib
  * @author Modular Inc, https://modular.network
  *
- * version 2.1.0
- * Copyright (c) 2017 Majoolr, LLC
+ * version 2.2.1
+ * Copyright (c) 2017 Modular Inc
  * The MIT License (MIT)
  * https://github.com/Modular-Network/ethereum-libraries/blob/master/LICENSE
  *
@@ -70,16 +70,14 @@ library EvenDistroCrowdsaleLib {
   event LogAddressTokenCapChange(uint256 amount, string Msg);
 
   // Logs when the address cap is initially calculated
-  event LogAddressTokenCapCalculated(uint256 saleCap, uint256 numRegistered, uint256 cap, string Msg);
+  event LogAddressTokenCapCalculated(uint256 numRegistered, uint256 cap, string Msg);
 
   /// @dev Called by a crowdsale contract upon creation.
   /// @param self Stored crowdsale from crowdsale contract
   /// @param _owner Address of crowdsale owner
   /// @param _saleData Array of 3 item sets such that, in each 3 element
-  /// set, 1 is timestamp, 2 is price in cents at that time,
+  /// set, 1 is timestamp, 2 is price in tokens/ETH at that time,
   /// 3 is address purchase cap at that time, 0 if no address cap
-  /// @param _fallbackExchangeRate Exchange rate of cents/ETH
-  /// @param _capAmountInCents Total to be raised in cents
   /// @param _endTime Timestamp of sale end time
   /// @param _percentBurn Percentage of extra tokens to burn
   /// @param _staticCap Whether or not the address cap is going to be static
@@ -87,8 +85,6 @@ library EvenDistroCrowdsaleLib {
   function init(EvenDistroCrowdsaleStorage storage self,
                 address _owner,
                 uint256[] _saleData,
-                uint256 _fallbackExchangeRate,
-                uint256 _capAmountInCents,
                 uint256 _endTime,
                 uint8 _percentBurn,
                 uint256 _initialAddressTokenCap,
@@ -98,8 +94,6 @@ library EvenDistroCrowdsaleLib {
   {
   	self.base.init(_owner,
                    _saleData,
-                   _fallbackExchangeRate,
-                   _capAmountInCents,
                    _endTime,
                    _percentBurn,
                    _token);
@@ -213,7 +207,7 @@ library EvenDistroCrowdsaleLib {
     if (self.staticCap)  {
       return false;
     }
-    require(!self.base.rateSet);  // makes sure this can only be called once
+    require(!self.base.tokensSet);  // makes sure this can only be called once
 
     uint256 _baseCap;
     uint256 _calcCap;
@@ -228,7 +222,7 @@ library EvenDistroCrowdsaleLib {
     }
 
     self.addressTokenCap = self.base.saleData[self.base.milestoneTimes[0]][1];
-    LogAddressTokenCapCalculated(self.base.capAmount, self.numRegistered, self.addressTokenCap, "Address cap was Calculated!");
+    LogAddressTokenCapCalculated(self.numRegistered, self.addressTokenCap, "Address cap was Calculated!");
   }
 
   /// @dev utility function for the receivePurchase function. returns the lower number
@@ -276,11 +270,6 @@ library EvenDistroCrowdsaleLib {
     uint256 _remainder; //temp calc holder
     uint256 _allowedWei;  // tells how much more the buyer can contribute up to their cap
     bool err;
-
-    if((self.base.ownerBalance + _amount) > self.base.capAmount){
-      _leftoverWei = (self.base.ownerBalance + _amount) - self.base.capAmount;
-      _amount = _amount - _leftoverWei;
-    }
 
     if(self.addressTokenCap > 0) {
       //_allowedWei represents tokens first, recycle variable to prevent stack depth issues
@@ -336,16 +325,8 @@ library EvenDistroCrowdsaleLib {
 
   /*Functions "inherited" from CrowdsaleLib library*/
 
-  function setTokenExchangeRate(EvenDistroCrowdsaleStorage storage self, uint256 _exchangeRate)
-                                public
-                                returns (bool)
-  {
-    bool ok = calculateAddressTokenCap(self);
-    return self.base.setTokenExchangeRate(_exchangeRate) && ok;
-  }
-
   function setTokens(EvenDistroCrowdsaleStorage storage self) public returns (bool) {
-    bool ok = calculateAddressTokenCap(self);
+    calculateAddressTokenCap(self);
     return self.base.setTokens();
   }
 
@@ -389,18 +370,14 @@ library CrowdsaleLib {
   	address owner;     //owner of the crowdsale
 
   	uint256 tokensPerEth;  //number of tokens received per ether
-  	uint256 capAmount; //Maximum amount to be raised in wei
   	uint256 startTime; //ICO start time, timestamp
   	uint256 endTime; //ICO end time, timestamp automatically calculated
-    uint256 exchangeRate; //cents/ETH exchange rate at the time of the sale
     uint256 ownerBalance; //owner wei Balance
     uint256 startingTokenBalance; //initial amount of tokens for sale
     uint256[] milestoneTimes; //Array of timestamps when token price and address cap changes
     uint8 currentMilestone; //Pointer to the current milestone
-    uint8 tokenDecimals; //stored token decimals for calculation later
     uint8 percentBurn; //percentage of extra tokens to burn
     bool tokensSet; //true if tokens have been prepared for crowdsale
-    bool rateSet; //true if exchange rate has been set
 
     //Maps timestamp to token price and address purchase cap starting at that time
     mapping (uint256 => uint256[2]) saleData;
@@ -436,41 +413,31 @@ library CrowdsaleLib {
   /// @param self Stored crowdsale from crowdsale contract
   /// @param _owner Address of crowdsale owner
   /// @param _saleData Array of 3 item sets such that, in each 3 element
-  /// set, 1 is timestamp, 2 is price in cents at that time,
+  /// set, 1 is timestamp, 2 is price in tokens/eth at that time,
   /// 3 is address token purchase cap at that time, 0 if no address cap
-  /// @param _fallbackExchangeRate Exchange rate of cents/ETH
-  /// @param _capAmountInCents Total to be raised in cents
   /// @param _endTime Timestamp of sale end time
   /// @param _percentBurn Percentage of extra tokens to burn
   /// @param _token Token being sold
   function init(CrowdsaleStorage storage self,
                 address _owner,
                 uint256[] _saleData,
-                uint256 _fallbackExchangeRate,
-                uint256 _capAmountInCents,
                 uint256 _endTime,
                 uint8 _percentBurn,
                 CrowdsaleToken _token)
                 public
   {
-  	require(self.capAmount == 0);
   	require(self.owner == 0);
     require(_saleData.length > 0);
     require((_saleData.length%3) == 0); // ensure saleData is 3-item sets
     require(_saleData[0] > (now + 2 hours));
     require(_endTime > _saleData[0]);
-    require(_capAmountInCents > 0);
     require(_owner > 0);
-    require(_fallbackExchangeRate > 0);
     require(_percentBurn <= 100);
     self.owner = _owner;
-    self.capAmount = ((_capAmountInCents/_fallbackExchangeRate) + 1)*(10**18);
     self.startTime = _saleData[0];
     self.endTime = _endTime;
     self.token = _token;
-    self.tokenDecimals = _token.decimals();
     self.percentBurn = _percentBurn;
-    self.exchangeRate = _fallbackExchangeRate;
 
     uint256 _tempTime;
     for(uint256 i = 0; i < _saleData.length; i += 3){
@@ -583,64 +550,27 @@ library CrowdsaleLib {
 
   /// @dev Function to change the price of the token
   /// @param self Stored crowdsale from crowdsale contract
-  /// @param _newPrice new token price (amount of tokens per ether)
+  /// @param _tokensPerEth new token price (amount of tokens per ether)
   /// @return true if the token price changed successfully
   function changeTokenPrice(CrowdsaleStorage storage self,
-                            uint256 _newPrice)
+                            uint256 _tokensPerEth)
                             internal
                             returns (bool)
   {
-  	require(_newPrice > 0);
+  	require(_tokensPerEth > 0);
 
-    bool err;
-    uint256 result;
-
-    (err, result) = self.exchangeRate.times(10**uint256(self.tokenDecimals));
-    require(!err);
-
-    self.tokensPerEth = result / _newPrice;
+    self.tokensPerEth = _tokensPerEth;
 
     return true;
   }
 
-  /// @dev function that is called three days before the sale to set the token and price
-  /// @param self Stored Crowdsale from crowdsale contract
-  /// @param _exchangeRate  ETH exchange rate expressed in cents/ETH
-  /// @return true if the exchange rate has been set
-  function setTokenExchangeRate(CrowdsaleStorage storage self, uint256 _exchangeRate)
-                                public
-                                returns (bool)
-  {
-    require(msg.sender == self.owner);
-    require((now > (self.startTime - 3 days)) && (now < (self.startTime)));
-    require(!self.rateSet);   // the exchange rate can only be set once!
-    require(self.token.balanceOf(this) > 0);
-    require(_exchangeRate > 0);
-
-    uint256 _capAmountInCents;
-    bool err;
-
-    (err, _capAmountInCents) = self.exchangeRate.times(self.capAmount);
-    require(!err);
-
-    self.exchangeRate = _exchangeRate;
-    self.capAmount = (_capAmountInCents/_exchangeRate) + 1;
-    changeTokenPrice(self,self.saleData[self.milestoneTimes[0]][0]);
-    self.rateSet = true;
-
-    err = !(setTokens(self));
-    require(!err);
-
-    LogNoticeMsg(msg.sender,self.tokensPerEth,"Owner has set the exchange Rate and tokens bought per ETH!");
-    return true;
-  }
-
-  /// @dev fallback function to set tokens if the exchange rate function was not called
+  /// @dev function to set tokens for the sale
   /// @param self Stored Crowdsale from crowdsale contract
   /// @return true if tokens set successfully
   function setTokens(CrowdsaleStorage storage self) public returns (bool) {
-    require((msg.sender == self.owner) || (msg.sender == address(this)));
+    require(msg.sender == self.owner);
     require(!self.tokensSet);
+    require(now < self.endTime);
 
     uint256 _tokenBalance;
 
@@ -910,82 +840,6 @@ library TokenLib {
   }
 }
 
-library BasicMathLib {
-  /// @dev Multiplies two numbers and checks for overflow before returning.
-  /// Does not throw.
-  /// @param a First number
-  /// @param b Second number
-  /// @return err False normally, or true if there is overflow
-  /// @return res The product of a and b, or 0 if there is overflow
-  function times(uint256 a, uint256 b) public view returns (bool err,uint256 res) {
-    assembly{
-      res := mul(a,b)
-      switch or(iszero(b), eq(div(res,b), a))
-      case 0 {
-        err := 1
-        res := 0
-      }
-    }
-  }
-
-  /// @dev Divides two numbers but checks for 0 in the divisor first.
-  /// Does not throw.
-  /// @param a First number
-  /// @param b Second number
-  /// @return err False normally, or true if `b` is 0
-  /// @return res The quotient of a and b, or 0 if `b` is 0
-  function dividedBy(uint256 a, uint256 b) public view returns (bool err,uint256 i) {
-    uint256 res;
-    assembly{
-      switch iszero(b)
-      case 0 {
-        res := div(a,b)
-        let loc := mload(0x40)
-        mstore(add(loc,0x20),res)
-        i := mload(add(loc,0x20))
-      }
-      default {
-        err := 1
-        i := 0
-      }
-    }
-  }
-
-  /// @dev Adds two numbers and checks for overflow before returning.
-  /// Does not throw.
-  /// @param a First number
-  /// @param b Second number
-  /// @return err False normally, or true if there is overflow
-  /// @return res The sum of a and b, or 0 if there is overflow
-  function plus(uint256 a, uint256 b) public view returns (bool err, uint256 res) {
-    assembly{
-      res := add(a,b)
-      switch and(eq(sub(res,b), a), or(gt(res,b),eq(res,b)))
-      case 0 {
-        err := 1
-        res := 0
-      }
-    }
-  }
-
-  /// @dev Subtracts two numbers and checks for underflow before returning.
-  /// Does not throw but rather logs an Err event if there is underflow.
-  /// @param a First number
-  /// @param b Second number
-  /// @return err False normally, or true if there is underflow
-  /// @return res The difference between a and b, or 0 if there is underflow
-  function minus(uint256 a, uint256 b) public view returns (bool err,uint256 res) {
-    assembly{
-      res := sub(a,b)
-      switch eq(and(eq(add(res,b), a), or(lt(res,a), eq(res,a))), 1)
-      case 0 {
-        err := 1
-        res := 0
-      }
-    }
-  }
-}
-
 contract CrowdsaleToken {
   using TokenLib for TokenLib.TokenStorage;
 
@@ -1055,5 +909,81 @@ contract CrowdsaleToken {
 
   function burnToken(uint256 amount) public returns (bool ok) {
     return token.burnToken(amount);
+  }
+}
+
+library BasicMathLib {
+  /// @dev Multiplies two numbers and checks for overflow before returning.
+  /// Does not throw.
+  /// @param a First number
+  /// @param b Second number
+  /// @return err False normally, or true if there is overflow
+  /// @return res The product of a and b, or 0 if there is overflow
+  function times(uint256 a, uint256 b) public pure returns (bool err,uint256 res) {
+    assembly{
+      res := mul(a,b)
+      switch or(iszero(b), eq(div(res,b), a))
+      case 0 {
+        err := 1
+        res := 0
+      }
+    }
+  }
+
+  /// @dev Divides two numbers but checks for 0 in the divisor first.
+  /// Does not throw.
+  /// @param a First number
+  /// @param b Second number
+  /// @return err False normally, or true if `b` is 0
+  /// @return res The quotient of a and b, or 0 if `b` is 0
+  function dividedBy(uint256 a, uint256 b) public pure returns (bool err,uint256 i) {
+    uint256 res;
+    assembly{
+      switch iszero(b)
+      case 0 {
+        res := div(a,b)
+        let loc := mload(0x40)
+        mstore(add(loc,0x20),res)
+        i := mload(add(loc,0x20))
+      }
+      default {
+        err := 1
+        i := 0
+      }
+    }
+  }
+
+  /// @dev Adds two numbers and checks for overflow before returning.
+  /// Does not throw.
+  /// @param a First number
+  /// @param b Second number
+  /// @return err False normally, or true if there is overflow
+  /// @return res The sum of a and b, or 0 if there is overflow
+  function plus(uint256 a, uint256 b) public pure returns (bool err, uint256 res) {
+    assembly{
+      res := add(a,b)
+      switch and(eq(sub(res,b), a), or(gt(res,b),eq(res,b)))
+      case 0 {
+        err := 1
+        res := 0
+      }
+    }
+  }
+
+  /// @dev Subtracts two numbers and checks for underflow before returning.
+  /// Does not throw but rather logs an Err event if there is underflow.
+  /// @param a First number
+  /// @param b Second number
+  /// @return err False normally, or true if there is underflow
+  /// @return res The difference between a and b, or 0 if there is underflow
+  function minus(uint256 a, uint256 b) public pure returns (bool err,uint256 res) {
+    assembly{
+      res := sub(a,b)
+      switch eq(and(eq(add(res,b), a), or(lt(res,a), eq(res,a))), 1)
+      case 0 {
+        err := 1
+        res := 0
+      }
+    }
   }
 }
