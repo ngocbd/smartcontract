7d3765ab7462f12992c7cc06088cf384dfd4968d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:41:12 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x19a4b6cd4586832edfb0125aa8168f5340ca3fed
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x97302e9bc0d5c45cd003b2faaac177645e4a479d
 */
 pragma solidity ^0.4.4;
 
@@ -117,13 +117,12 @@ contract ERC20Token is StandardToken {
 
 //make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
-    function ERC20Token(
-        ) {
-        balances[msg.sender] = 50000000000000000000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 50000000000000000000000000;                        // Update total supply (100000 for example)
-        name = "mars shot token";                                   // Set the name for display purposes
-        decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "XMS";                               // Set the symbol for display purposes
+    function ERC20Token() {
+        balances[msg.sender] = 2100000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 2100000000000000;                        // Update total supply (100000 for example)
+        name = "Independent Future Automation";                      // Set the name for display purposes
+        decimals = 8;                            // Amount of decimals for display purposes
+        symbol = "IFA";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
