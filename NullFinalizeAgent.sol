ea4a64871b3ed1f5355d30f27c0a99da216033e2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:47:36 +0700

    add contract NullFinalizeAgent

--- NullFinalizeAgent.sol
+++ NullFinalizeAgent.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgent at 0xefeddad706024d8747b9f9c3119635f7777cbfb3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgent at 0xe18f9f1b9f07fde8ca4801eb41f16e4b0a91c38f
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
