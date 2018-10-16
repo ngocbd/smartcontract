66b7f6f8a1965246006ac6621043d68eb116fb2a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:32:13 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xf1c7ad4799797cad0127be9e0bac4fbc4bb9f92a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x187be645188bf025947725d6cf1b8618899b2c2a
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT394803' token contract
+    // 'ACT238324' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT394803
-    // Name        : ADZbuzz Playstation.com Community Token
+    // Symbol      : ACT238324
+    // Name        : ADZbuzz Phandroid.com Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT394803";
-            name = "ADZbuzz Playstation.com Community Token";
+            symbol = "ACT238324";
+            name = "ADZbuzz Phandroid.com Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
