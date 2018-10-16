4c8c97740af0b35f71679e9dc4e80e4269028fd6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:18:44 +0700

    add contract Brettmessage

---
+++ Brettmessage.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Brettmessage at 0xe6069f1c9d78ebc232c1726bf4faa727d83ae31d
+*/
+pragma solidity ^0.4.2;
+
+contract Brettmessage 
+{
+	string constant public poster = "BJD";
+	string constant public message = "Posted:2/7/18 Hello!PWK:WI";
+}
