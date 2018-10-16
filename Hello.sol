ac85194030e16f5351a0a0dd1d96d95742c4e811
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:27:15 +0700

    add contract Hello

---
+++ Hello.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Hello at 0xbc8113cbd55993fd6387f404d2062b4019ab89d7
+*/
+pragma solidity ^0.4.11;
+contract Hello {
+
+    function Hello() {
+
+    }
+}
