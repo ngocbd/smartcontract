2939c232a5751c41361007fd2a55fe15b97429c9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 06:45:37 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,108 +1,93 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xe1ee8578fb0bd17824754d32f5fa26b6dac26b9f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xc2b2950fdd01252a720344ef6077491e6e1b10f8
 */
-pragma solidity ^0.4.16;
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
+pragma solidity ^0.4.19;
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a / b;
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+/*  base token */
+contract Token {
+    uint256 public totalSupply;
+    function balanceOf(address _owner) constant public returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public  returns (bool success);
+    function allowance(address _owner, address _spender) constant public  returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
+/*  ERC 20 token */
+contract StandardToken is Token {
 
- contract ERC20Interface {
-     function totalSupply() constant returns (uint256 totalSupply);
-     function balanceOf(address _owner) constant returns (uint256 balance);
-     function transfer(address _to, uint256 _value) returns (bool success);
-     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-     function approve(address _spender, uint256 _value) returns (bool success);
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
- }
-  
- contract MyToken is ERC20Interface {
-      string public constant symbol = "FOD"; 
-      string public constant name = "fodcreate";   
-      uint8 public constant decimals = 18;
-      uint256 _totalSupply = 2000000000000000000000000000; 
-     
-      address public owner;
-  
-      mapping(address => uint256) balances;
-  
-      mapping(address => mapping (address => uint256)) allowed;
-  
-      function MyToken() {
-          owner = msg.sender;
-          balances[owner] = _totalSupply;
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+      if (balances[msg.sender] >= _value && _value > 0) {
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+      } else {
+        return false;
       }
-  
-      function totalSupply() constant returns (uint256 totalSupply) {
-         totalSupply = _totalSupply;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)  public returns (bool success) {
+      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+      } else {
+        return false;
       }
-  
-      // What is the balance of a particular account?
-      function balanceOf(address _owner) constant returns (uint256 balance) {
-         return balances[_owner];
-      }
-   
-      // Transfer the balance from owner's account to another account
-      function transfer(address _to, uint256 _amount) returns (bool success) {
-         if (balances[msg.sender] >= _amount) {
-            balances[msg.sender] = SafeMath.sub(balances[msg.sender],_amount);
-            balances[_to] = SafeMath.add(balances[_to],_amount);
-            
-            return true;
-         }
-         
-         return false;
-      }
-      
-      function transferFrom(
-          address _from,
-          address _to,
-         uint256 _amount
-    ) returns (bool success) {
-         require(_to != address(0));
+    }
+
+    function balanceOf(address _owner) constant public  returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value)  public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant  public returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+}
+
+contract MyToken is StandardToken{
+
+    // metadata
+    string public  name;
+    string public  symbol;
+    uint256 public  decimals;
+    string public version = "1.0";
+    address public owner;
     
-         if (balances[_from] >= _amount) {
-            balances[_from] = SafeMath.sub(balances[_from],_amount);
-            balances[_to] = SafeMath.add(balances[_to],_amount);
-            
-            return true;
-         }
-         
-         return false;
-     }
-  
-     function approve(address _spender, uint256 _amount) returns (bool success) {
-         allowed[msg.sender][_spender] = _amount;
-         Approval(msg.sender, _spender, _amount);
-         return true;
-     }
-  
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-         return allowed[_owner][_spender];
-     }
+    function MyToken (string init_name,string init_symbol,uint256 init_decimals,uint256 init_total,address init_address)  public {
+        name=init_name;
+        symbol=init_symbol;
+        decimals=init_decimals;
+        balances[init_address] = init_total;
+        totalSupply = init_total;
+        owner=msg.sender;
+    }
+    
+    modifier owned(){
+        require(msg.sender==owner);
+        _;
+    }
+    
+    function setName(string new_name) public owned {
+        name = new_name;
+    }
+	
+	 function setSymbol(string new_symbol) public owned {
+        symbol = new_symbol;
+    }
+	
 }
