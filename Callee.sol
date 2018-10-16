ae2b5a18ec48ed99a891b6f74afbe4bc16801ea4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:09:34 +0700

    add contract callee

--- Callee.sol
+++ Callee.sol
@@ -1,12 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Callee at 0x2130fda6b4d6d2d549e54241ada5e2c903b0efee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract callee at 0xe308310359e04c4c95dc0a72c92c3fe35ba1d8c6
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.4;
 
-contract Callee {
-    event ReceivedCall();
+contract callee {
+
+
+    event outputi(uint i);
+    event outputa(address a);
+    event outputb(bytes32 b);
     
-    function () {
-        ReceivedCall();
+    function callee() {
     }
+    
+  function x (address a1, uint i1, address a2, uint i2, bytes32 b1, bytes32 b2) public {
+    outputa(a1);
+    outputi(i1);
+    outputa(a2);
+    outputi(i2);
+    outputb(b1);
+    outputb(b2);
+  }
+
 }
