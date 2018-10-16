c11a18b0c8f1ded4e78ec3eab6053caf60c959a5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:39:06 +0700

    add contract Dragon

--- Dragon.sol
+++ Dragon.sol
@@ -1,223 +1,273 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Dragon at 0x1d1CF6cD3fE91fe4d1533BA3E0b7758DFb59aa1f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Dragon at 0x419c4db4b9e25d6db2ad9691ccb832c8d9fda05e
 */
-pragma solidity ^ 0.4 .11;
+pragma solidity ^0.4.18;
 
+// File: zeppelin-solidity/contracts/math/SafeMath.sol
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-contract tokenRecipient {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
+// File: zeppelin-solidity/contracts/ownership/Ownable.sol
 
-contract ERC20 {
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
 
-    function totalSupply() constant returns(uint _totalSupply);
 
-    function balanceOf(address who) constant returns(uint256);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function transfer(address to, uint value) returns(bool ok);
 
-    function transferFrom(address from, address to, uint value) returns(bool ok);
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
 
-    function approve(address spender, uint value) returns(bool ok);
 
-    function allowance(address owner, address spender) constant returns(uint);
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
 
 }
 
-contract Burner { function dragonHandler( uint256 _amount){} }
- 
-contract Dragon is ERC20 {
+// File: zeppelin-solidity/contracts/token/ERC20Basic.sol
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+// File: zeppelin-solidity/contracts/token/BasicToken.sol
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+// File: zeppelin-solidity/contracts/token/ERC20.sol
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// File: zeppelin-solidity/contracts/token/StandardToken.sol
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) internal allowed;
 
 
-    string public standard = 'DRAGON 1.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-  
-    
-    address public owner;
-    mapping( address => uint256) public balanceOf;
-    mapping( uint => address) public accountIndex;
-    uint accountCount;
-    
-    mapping(address => mapping(address => uint256)) public allowance;
-    address public burner;
-    bool public burnerSet;
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed spender, uint value);
-    event Message ( address a, uint256 amount );
-    event Burn(address indexed from, uint256 value);
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
 
-    
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  /**
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   */
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+}
+
+// File: contracts/Dragon.sol
+
+/**
+ * @title Dragon
+ * @dev DistributableToken contract is based on a simple initial supply token, with an API for the owner to perform bulk distributions.
+ *      transactions to the distributeTokens function should be paginated to avoid gas limits or computational time restrictions. 
+ */
+contract Dragon is StandardToken, Ownable {
+    string public constant name = "Dragon";
+    string public constant symbol = "DRGN";
+    uint8 public constant decimals = 18;
+    uint256 public constant INITIAL_SUPPLY = 433494437 * (10 ** uint256(decimals));
+
+    //prevent duplicate distributions
+    mapping (address => bool) distributionLocks;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
     function Dragon() {
-         
-        uint supply = 50000000000000000; 
-        appendTokenHolders( msg.sender );
-        balanceOf[msg.sender] =  supply;
-        totalSupply = supply; // 
-        name = "DRAGON"; // Set the name for display purposes
-        symbol = "DRG"; // Set the symbol for display purposes
-        decimals = 8; // Amount of decimals for display purposes
-        owner = msg.sender;
-        
-  
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
     }
-    
-    
-    modifier onlyOwner() {
-        if (msg.sender != owner) {
-            throw;
+
+    /**
+     * @dev Distribute tokens to multiple addresses in a single transaction
+     *
+     * @param addresses A list of addresses to distribute to
+     * @param values A corresponding list of amounts to distribute to each address
+     */
+    function anailNathrachOrthaBhaisIsBeathaDoChealDeanaimh(address[] addresses, uint256[] values) onlyOwner public returns (bool success) {
+        require(addresses.length == values.length);
+        for (uint i = 0; i < addresses.length; i++) {
+            require(!distributionLocks[addresses[i]]);
+            transfer(addresses[i], values[i]);
+            distributionLocks[addresses[i]] = true;
         }
-        _;
-    }
-
-     modifier onlyBurner() {
-        if (msg.sender != burner) {
-            throw;
-        }
-        _;
-    }
-    
-    function changeOwnership( address _owner ) onlyOwner {
-        
-        owner = _owner;
-        
-    }
-    
-    function setBurner( address _burner ) onlyOwner {
-        require ( !burnerSet );
-        burner = _burner;
-        burnerSet = true;
-        
-    }
-
-    function burnCheck( address _tocheck , uint256 amount ) internal {
-        
-        if ( _tocheck != burner )return;
-        
-        Burner burn = Burner ( burner );
-        burn.dragonHandler( amount );
-        
-        
-    }
-    
-    function burnDragons ( uint256 _amount ) onlyBurner{
-        
-        
-        burn( _amount );
-        
-        
-    }
-    
-    function balanceOf(address tokenHolder) constant returns(uint256) {
-
-        return balanceOf[tokenHolder];
-    }
-
-    function totalSupply() constant returns(uint256) {
-
-        return totalSupply;
-    }
-
-    function getAccountCount() constant returns(uint256) {
-
-        return accountCount;
-    }
-
-    function getAddress(uint slot) constant returns(address) {
-
-        return accountIndex[slot];
-
-    }
-
-    
-    function appendTokenHolders(address tokenHolder) private {
-
-        if (balanceOf[tokenHolder] == 0) {
-            if ( tokenHolder == burner ) return;
-            accountIndex[accountCount] = tokenHolder;
-            accountCount++;
-        }
-
-    }
-
-    
-    function transfer(address _to, uint256 _value) returns(bool ok) {
-        if (_to == 0x0) throw; 
-        if (balanceOf[msg.sender] < _value) throw; 
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
-        
-        appendTokenHolders(_to);
-        balanceOf[msg.sender] -= _value; 
-        balanceOf[_to] += _value; 
-        Transfer(msg.sender, _to, _value); 
-        burnCheck( _to, _value );
-        
-        return true;
-    }
-    
-    function approve(address _spender, uint256 _value)
-    returns(bool success) {
-        allowance[msg.sender][_spender] = _value;
-        Approval( msg.sender ,_spender, _value);
         return true;
     }
 
- 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-    returns(bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowance[_owner][_spender];
-    }
-
- 
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool success) {
-        if (_to == 0x0) throw;  
-        if (balanceOf[_from] < _value) throw;  
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  
-        if (_value > allowance[_from][msg.sender]) throw; 
-        appendTokenHolders(_to);
-        balanceOf[_from] -= _value; 
-        balanceOf[_to] += _value; 
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-       
-        return true;
-    }
-  
-    function burn(uint256 _value) returns(bool success) {
-        if (balanceOf[msg.sender] < _value) throw; 
-        if( totalSupply -  _value < 2100000000000000) throw;
-        balanceOf[msg.sender] -= _value; 
-        totalSupply -= _value; 
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) returns(bool success) {
-    
-        if (balanceOf[_from] < _value) throw; 
-        if (_value > allowance[_from][msg.sender]) throw; 
-        balanceOf[_from] -= _value; 
-        totalSupply -= _value; 
-        Burn(_from, _value);
-        return true;
-    }
- 
-    
 }
