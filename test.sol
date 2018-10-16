de4e5d33652d20c45f2b281417dba64be1b34ad9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:23:53 +0700

    add contract test

--- test.sol
+++ test.sol
@@ -1,14 +1,90 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0x5485c751c582eaf9e8d0ec47d754ee66a2e6e9eb
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract test at 0x6a2e025f43ca4d0d3c61bdee85a8e37e81880528
 */
 pragma solidity ^0.4.0;
-contract Test {
 
-    function send(address to) public{
-        if (to.call("0xabcdef")) {
-            return;
-        } else {
-            revert();
+contract test{
+    struct SeedComponents{
+        uint component1;
+        uint component2;
+        uint component3;
+        uint component4;
+    }
+    
+    address owner;
+    uint private secretSeed;
+    uint private lastReseed;
+    
+    uint public winnerLuckyNumber = 7;
+        
+    mapping (address => bool) participated;
+
+    event showme(uint luckyNumberOfAddress, uint winnerLuckyNumber, uint n);
+
+    
+    function showNumber() constant returns (uint winnerLuckyNumber){}
+    
+    function test() {
+        owner = msg.sender;
+        reseed(SeedComponents(12345678, 0x12345678, 0xabbaeddaacdc, 0x22222222));
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+  
+    modifier onlyHuman() {
+        require(msg.sender == tx.origin);
+        _;
+    }
+    
+    function participate() payable onlyHuman { 
+        require(msg.value == 0.1 ether);
+        
+        // every address can only win once, obviously
+       require(!participated[msg.sender]);
+         showme(luckyNumberOfAddress(msg.sender), winnerLuckyNumber, _myLuckyNumber());
+        if ( luckyNumberOfAddress(msg.sender) == winnerLuckyNumber)
+        {
+            participated[msg.sender] = true;
+            require(msg.sender.call.value(this.balance)());
         }
+    }
+    
+    function luckyNumberOfAddress(address addr) constant returns(uint n){
+        // 1 in 8 chance
+        n = uint(keccak256(uint(addr), secretSeed)[0]) % 8;
+       
+    }
+    
+    function reseed(SeedComponents components) internal {
+        secretSeed = uint256(keccak256(
+            components.component1,
+            components.component2,
+            components.component3,
+            components.component4
+        ));
+        lastReseed = block.number;
+    }
+    
+    function kill() onlyOwner {
+        suicide(owner);
+    }
+    
+    function forceReseed() onlyOwner{
+        SeedComponents s;
+        s.component1 = uint(msg.sender);
+        s.component2 = uint256(block.blockhash(block.number - 1));
+        s.component3 = block.number * 1337;
+        s.component4 = tx.gasprice * 7;
+        reseed(s);
+    }
+    
+    function () payable {}
+    
+    // DEBUG, DELETE BEFORE DEPLOYMENT!!
+    function _myLuckyNumber() constant returns(uint n){
+        n = luckyNumberOfAddress(msg.sender);
     }
 }
