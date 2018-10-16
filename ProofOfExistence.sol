17e1be7d4592831497d60b2b8965ef696f4cd441
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:32 +0700

    add contract ProofOfExistence

--- ProofOfExistence.sol
+++ ProofOfExistence.sol
@@ -1,22 +1,19 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0x1d6671bad68e2ac15f72502f8a6c3bab42ccafd6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0x1c9207daaa1da36ba34b7e1bc83810dd7390d7c9
 */
 contract ProofOfExistence {
-
   mapping (string => uint) private proofs;
 
-  function notarize(string sha256) {
+  function storeProof(string sha256) {
+    proofs[sha256] = block.timestamp;
+  }
 
-    bytes memory b_hash = bytes(sha256);
-    
-    if ( b_hash.length == 64 ){
-      if ( proofs[sha256] != 0 ){
-        proofs[sha256] = block.timestamp;
-      }
-    }
+  function notarize(string sha256) {
+    storeProof(sha256);
   }
   
-  function verify(string sha256) constant returns (uint) {
+
+  function checkDocument(string sha256) constant returns (uint) {
     return proofs[sha256];
   }
   
