b554c03ca346539aeab8ec8eb362b7b7fb3fa41e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:11:21 +0700

    add contract CoinMarketAlert

--- CoinMarketAlert.sol
+++ CoinMarketAlert.sol
@@ -1,61 +1,103 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinMarketAlert at 0xafedea9dba7d644572aa7c443198c65514faaef2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinMarketAlert at 0x54c6acad21fcda13f318268335b10d7b809da327
 */
-pragma solidity 0.4.16;
+pragma solidity 0.4.18;
 
-// Used for function invoke restriction
-contract Owned {
+// implement safemath as a library
+library SafeMath {
 
-    address public owner; // temporary address
+  // We use `pure` bbecause it promises that the value for the function depends ONLY
+  // on the function arguments
+  function mul(uint256 a, uint256 b) internal pure  returns (uint256) {
+    uint256 c = a * b;
+    require(a == 0 || c / a == b);
+    return c;
+  }
 
-    function Owned() {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+}
+
+contract Administration {
+
+    address     public owner;
+    
+    mapping (address => bool) public moderators;
+    mapping (address => string) privilegeStatus;
+
+    event AddMod(address indexed _invoker, address indexed _newMod, bool indexed _modAdded);
+    event RemoveMod(address indexed _invoker, address indexed _removeMod, bool indexed _modRemoved);
+
+    function Administration() public {
         owner = msg.sender;
     }
 
-    modifier onlyOwner() {
-        if (msg.sender != owner)
-            revert();
-        _; // function code inserted here
+    modifier onlyAdmin() {
+        require(msg.sender == owner || moderators[msg.sender] == true);
+        _;
     }
 
-    function transferOwnership(address _newOwner) onlyOwner returns (bool success) {
-        if (msg.sender != owner)
-            revert();
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner)
+        public
+        onlyOwner
+        returns (bool success)
+    {
         owner = _newOwner;
         return true;
         
     }
+
+    function addModerator(address _newMod)
+        public
+        onlyOwner
+        returns (bool added)
+     {
+        require(_newMod != address(0x0));
+        moderators[_newMod] = true;
+        AddMod(msg.sender, _newMod, true);
+        return true;
+    }
+    
+    function removeModerator(address _removeMod)
+        public
+        onlyOwner
+        returns (bool removed)
+    {
+        require(_removeMod != address(0x0));
+        moderators[_removeMod] = false;
+        RemoveMod(msg.sender, _removeMod, true);
+        return true;
+    }
+
+    function getRoleStatus(address _addr)
+        public
+        view  // We use view as we promise to not change state, but are reading from a state variable
+        returns (string _role)
+    {
+        return privilegeStatus[_addr];
+    }
 }
 
-contract SafeMath {
-
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-        uint256 c = a * b;
-        require(a == 0 || c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-        require(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-        return c;
-    }
-}
-
-
-contract CoinMarketAlert is Owned, SafeMath {
+contract CoinMarketAlert is Administration {
+    using SafeMath for uint256;
 
     address[]   public      userAddresses;
     uint256     public      totalSupply;
@@ -90,19 +132,24 @@ contract CoinMarketAlert is Owned, SafeMath {
     event TokenBurn(address indexed _burner, uint256 _amount, bool indexed _burned);
     event EnableTokenMinting(bool Enabled);
 
-    function CoinMarketAlert() {
+    function CoinMarketAlert()
+        public {
         symbol = "CMA";
         name = "Coin Market Alert";
         decimals = 18;
         // 50 Mil in wei
         totalSupply = 50000000000000000000000000;
-        balances[msg.sender] = add(balances[msg.sender], totalSupply);
+        balances[msg.sender] = 50000000000000000000000000;
         tokenTransfersFrozen = true;
         tokenMintingEnabled = false;
     }
 
     /// @notice Used to launch start the contract
-    function launchContract() onlyOwner returns (bool launched) {
+    function launchContract()
+        public
+        onlyAdmin
+        returns (bool launched)
+    {
         require(!contractLaunched);
         tokenTransfersFrozen = false;
         tokenMintingEnabled = true;
@@ -112,8 +159,11 @@ contract CoinMarketAlert is Owned, SafeMath {
     }
     
     /// @dev keeps a list of addresses that are participating in the site
-    function registerUser(address _user) private returns (bool registered) {
-        usersRegistered = add(usersRegistered, 1);
+    function registerUser(address _user) 
+        private
+        returns (bool registered)
+    {
+        usersRegistered = usersRegistered.add(1);
         AlertCreatorStruct memory acs;
         acs.alertCreator = _user;
         alertCreators.push(acs);
@@ -125,64 +175,86 @@ contract CoinMarketAlert is Owned, SafeMath {
     /// @notice Manual payout for site users
     /// @param _user Ethereum address of the user
     /// @param _amount The mount of CMA tokens in wei to send
-    function singlePayout(address _user, uint256 _amount) onlyOwner returns (bool paid) {
+    function singlePayout(address _user, uint256 _amount)
+        public
+        onlyAdmin
+        returns (bool paid)
+    {
         require(!tokenTransfersFrozen);
         require(_amount > 0);
         require(transferCheck(owner, _user, _amount));
         if (!userRegistered[_user]) {
             registerUser(_user);
         }
-        balances[_user] = add(balances[_user], _amount);
-        balances[owner] = sub(balances[owner], _amount);
+        balances[_user] = balances[_user].add(_amount);
+        balances[owner] = balances[owner].add(_amount);
         Transfer(owner, _user, _amount);
         return true;
     }
 
     /// @dev low-level minting function not accessible externally
-    function tokenMint(address _invoker, uint256 _amount) private returns (bool raised) {
-        require(add(balances[owner], _amount) > balances[owner]);
-        require(add(balances[owner], _amount) > 0);
-        require(add(totalSupply, _amount) > 0);
-        require(add(totalSupply, _amount) > totalSupply);
-        totalSupply = add(totalSupply, _amount);
-        balances[owner] = add(balances[owner], _amount);
+    function tokenMint(address _invoker, uint256 _amount) 
+        private
+        returns (bool raised)
+    {
+        require(balances[owner].add(_amount) > balances[owner]);
+        require(balances[owner].add(_amount) > 0);
+        require(totalSupply.add(_amount) > 0);
+        require(totalSupply.add(_amount) > totalSupply);
+        totalSupply = totalSupply.add(_amount);
+        balances[owner] = balances[owner].add(_amount);
         MintTokens(_invoker, _amount, true);
         return true;
     }
 
     /// @notice Used to mint tokens, only usable by the contract owner
     /// @param _amount The amount of CMA tokens in wei to mint
-    function tokenFactory(uint256 _amount) onlyOwner returns (bool success) {
+    function tokenFactory(uint256 _amount)
+        public
+        onlyAdmin
+        returns (bool success)
+    {
         require(_amount > 0);
         require(tokenMintingEnabled);
-        if (!tokenMint(msg.sender, _amount))
-            revert();
+        require(tokenMint(msg.sender, _amount));
         return true;
     }
 
     /// @notice Used to burn tokens
     /// @param _amount The amount of CMA tokens in wei to burn
-    function tokenBurn(uint256 _amount) onlyOwner returns (bool burned) {
+    function tokenBurn(uint256 _amount)
+        public
+        onlyAdmin
+        returns (bool burned)
+    {
         require(_amount > 0);
         require(_amount < totalSupply);
         require(balances[owner] > _amount);
-        require(sub(balances[owner], _amount) > 0);
-        require(sub(totalSupply, _amount) > 0);
-        balances[owner] = sub(balances[owner], _amount);
-        totalSupply = sub(totalSupply, _amount);
+        require(balances[owner].sub(_amount) >= 0);
+        require(totalSupply.sub(_amount) >= 0);
+        balances[owner] = balances[owner].sub(_amount);
+        totalSupply = totalSupply.sub(_amount);
         TokenBurn(msg.sender, _amount, true);
         return true;
     }
 
     /// @notice Used to freeze token transfers
-    function freezeTransfers() onlyOwner returns (bool frozen) {
+    function freezeTransfers()
+        public
+        onlyAdmin
+        returns (bool frozen)
+    {
         tokenTransfersFrozen = true;
         FreezeTransfers(msg.sender, true);
         return true;
     }
 
     /// @notice Used to thaw token transfers
-    function thawTransfers() onlyOwner returns (bool thawed) {
+    function thawTransfers()
+        public
+        onlyAdmin
+        returns (bool thawed)
+    {
         tokenTransfersFrozen = false;
         ThawTransfers(msg.sender, true);
         return true;
@@ -191,40 +263,43 @@ contract CoinMarketAlert is Owned, SafeMath {
     /// @notice Used to transfer funds
     /// @param _receiver The destination ethereum address
     /// @param _amount The amount of CMA tokens in wei to send
-    function transfer(address _receiver, uint256 _amount) {
+    function transfer(address _receiver, uint256 _amount)
+        public
+        returns (bool _transferred)
+    {
         require(!tokenTransfersFrozen);
-        if (transferCheck(msg.sender, _receiver, _amount)) {
-            balances[msg.sender] = sub(balances[msg.sender], _amount);
-            balances[_receiver] = add(balances[_receiver], _amount);
-            Transfer(msg.sender, _receiver, _amount);
-        } else {
-            // ensure we refund gas costs
-            revert();
-        }
+        require(transferCheck(msg.sender, _receiver, _amount));
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_receiver] = balances[_receiver].add(_amount);
+        Transfer(msg.sender, _receiver, _amount);
+        return true;
     }
 
     /// @notice Used to transfer funds on behalf of one person
     /// @param _owner Person you are allowed to spend funds on behalf of
     /// @param _receiver Person to receive the funds
     /// @param _amount Amoun of CMA tokens in wei to send
-    function transferFrom(address _owner, address _receiver, uint256 _amount) {
+    function transferFrom(address _owner, address _receiver, uint256 _amount)
+        public
+        returns (bool _transferredFrom)
+    {
         require(!tokenTransfersFrozen);
-        require(sub(allowance[_owner][msg.sender], _amount) >= 0);
-        if (transferCheck(_owner, _receiver, _amount)) {
-            balances[_owner] = sub(balances[_owner], _amount);
-            balances[_receiver] = add(balances[_receiver], _amount);
-            allowance[_owner][_receiver] = sub(allowance[_owner][_receiver], _amount);
-            Transfer(_owner, _receiver, _amount);
-        } else {
-            // ensure we refund gas costs
-            revert();
-        }
+        require(allowance[_owner][msg.sender].sub(_amount) >= 0);
+        require(transferCheck(_owner, _receiver, _amount));
+        balances[_owner] = balances[_owner].sub(_amount);
+        balances[_receiver] = balances[_receiver].add(_amount);
+        allowance[_owner][_receiver] = allowance[_owner][_receiver].sub(_amount);
+        Transfer(_owner, _receiver, _amount);
+        return true;
     }
 
     /// @notice Used to approve a third-party to send funds on your behalf
     /// @param _spender The person you are allowing to spend on your behalf
     /// @param _amount The amount of CMA tokens in wei they are allowed to spend
-    function approve(address _spender, uint256 _amount) returns (bool approved) {
+    function approve(address _spender, uint256 _amount)
+        public
+        returns (bool approved)
+    {
         require(_amount > 0);
         require(balances[msg.sender] > 0);
         allowance[msg.sender][_spender] = _amount;
@@ -242,29 +317,40 @@ contract CoinMarketAlert is Owned, SafeMath {
     /// @param _value This is the amount of CMA tokens in wei to send
     function transferCheck(address _sender, address _receiver, uint256 _value) 
         private
-        constant 
+        view
         returns (bool safe) 
     {
         require(_value > 0);
-        // prevents empty receiver
         require(_receiver != address(0));
-        require(sub(balances[_sender], _value) >= 0);
-        require(add(balances[_receiver], _value) > balances[_receiver]);
+        require(balances[_sender].sub(_value) >= 0);
+        require(balances[_receiver].add(_value) > balances[_receiver]);
         return true;
     }
 
     /// @notice Used to retrieve total supply
-    function totalSupply() constant returns (uint256 _totalSupply) {
+    function totalSupply()
+        public
+        view
+        returns (uint256 _totalSupply)
+    {
         return totalSupply;
     }
 
     /// @notice Used to look up balance of a user
-    function balanceOf(address _person) constant returns (uint256 balance) {
+    function balanceOf(address _person)
+        public
+        view
+        returns (uint256 balance)
+    {
         return balances[_person];
     }
 
     /// @notice Used to look up allowance of a user
-    function allowance(address _owner, address _spender) constant returns (uint256 allowed) {
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 allowed)
+    {
         return allowance[_owner][_spender];
     }
 }
