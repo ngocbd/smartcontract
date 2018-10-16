5060752f548f1e30ca4e143808643051fad61cc6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:05:21 +0700

    add contract HumanStandardToken

--- HumanStandardToken.sol
+++ HumanStandardToken.sol
@@ -1,106 +1,90 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HumanStandardToken at 0xdd73f2486aade2502a9d27f7a39c40b68d96bd64
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HumanStandardToken at 0x819f9df405abdadc3ff2e6f9531d87a8c1a5df55
 */
-pragma solidity ^0.4.8;
-contract Token{
-    // token???????public??????getter????????totalSupply().
-    uint256 public totalSupply;
+pragma solidity ^0.4.0;
 
-    /// ????_owner??token??? 
-    function balanceOf(address _owner) constant returns (uint256 balance);
-
-    //??????????_to??????_value?token
-    function transfer(address _to, uint256 _value) returns (bool success);
-
-    //???_from????_to????_value?token??approve??????
-    function transferFrom(address _from, address _to, uint256 _value) returns   
-    (bool success);
-
-    //??????????_spender????????????_value?token
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    //????_spender?????_owner???token???
-    function allowance(address _owner, address _spender) constant returns 
-    (uint256 remaining);
-
-    //????????????? 
+contract Token {
+    function totalSupply() constant returns (uint256 supply) {}
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    //???approve(address _spender, uint256 _value)????????????
-    event Approval(address indexed _owner, address indexed _spender, uint256 
-    _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 contract StandardToken is Token {
     function transfer(address _to, uint256 _value) returns (bool success) {
-        //??totalSupply ??????? (2^256 - 1).
-        //??????????????token??????????????????
-        //require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;//???????????token??_value
-        balances[_to] += _value;//???????token??_value
-        Transfer(msg.sender, _to, _value);//????????
-        return true;
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
     }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) returns 
-    (bool success) {
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= 
-        // _value && balances[_to] + _value > balances[_to]);
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;//??????token??_value
-        balances[_from] -= _value; //????_from??token??_value
-        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
-        Transfer(_from, _to, _value);//????????
-        return true;
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
     }
+
     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-
-    function approve(address _spender, uint256 _value) returns (bool success)   
-    {
+    function approve(address _spender, uint256 _value) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];//??_spender?_owner????token?
+        return allowed[_owner][_spender];
     }
+
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
 }
 
-contract HumanStandardToken is StandardToken { 
-
-    /* Public variables of the token */
-    string public name;                   //??: eg Simon Bucks
-    uint8 public decimals;               //????????How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;               //token??: eg SBX
-    string public version = 'H0.1';    //??
-
-    function HumanStandardToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
-        balances[msg.sender] = _initialAmount; // ??token?????????
-        totalSupply = _initialAmount;         // ??????
-        name = _tokenName;                   // token??
-        decimals = _decimalUnits;           // ????
-        symbol = _tokenSymbol;             // token??
+contract HumanStandardToken is StandardToken {
+    function () { 
+        throw;
     }
 
-    /* Approves and then calls the receiving contract */
-    
+    uint8 public decimals;
+    string public name;
+    string public symbol;
+
+    function HumanStandardToken(
+        uint256 _initialAmount,
+        uint8 _decimalUnits,
+        string _tokenName,
+        string _tokenSymbol
+    ) {
+        decimals = _decimalUnits;                            
+        totalSupply = _initialAmount * 10 ** uint256(decimals);                        
+        balances[msg.sender] = totalSupply;               
+        name = _tokenName;                                   
+        symbol = _tokenSymbol;                               
+    }
+
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) {
+            throw;
+        }
         return true;
     }
-
 }
