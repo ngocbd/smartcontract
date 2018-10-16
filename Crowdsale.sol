8bacde117306ee29db9c1f81e9d23c37f6d68683
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:11:00 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,222 +1,317 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xa214c9c6a617c4b50e040bb798f95174ba14fe42
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x09971fb3ebd22e3afea9f214ddbc85a03f993fe9
 */
-pragma solidity ^0.4.19;
+pragma solidity ^ 0.4.17;
 
-library SafeMath { //standart library for uint
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0 || b == 0){
-        return 0;
+
+library SafeMath {
+    function mul(uint a, uint b) pure internal returns(uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-
-  function pow(uint256 a, uint256 b) internal pure returns (uint256){ //power function
-    if (b == 0){
-      return 1;
+    function sub(uint a, uint b) pure internal returns(uint) {
+        assert(b <= a);
+        return a - b;
     }
-    uint256 c = a**b;
-    assert (c >= a);
-    return c;
-  }
+
+    function add(uint a, uint b) pure internal returns(uint) {
+        uint c = a + b;
+        assert(c >= a && c >= b);
+        return c;
+    }
 }
 
-//standart contract to identify owner
+
+
+
+
 contract Ownable {
+    address public owner;
 
-  address public owner;
-
-  address public newOwner;
-
-  address public techSupport;
-
-  address public newTechSupport;
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  modifier onlyTechSupport() {
-    require(msg.sender == techSupport);
-    _;
-  }
-
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  function transferOwnership(address _newOwner) public onlyOwner {
-    require(_newOwner != address(0));
-    newOwner = _newOwner;
-  }
-
-  function acceptOwnership() public {
-    if (msg.sender == newOwner) {
-      owner = newOwner;
+    function Ownable() public {
+        owner = msg.sender;
     }
-  }
 
-  function transferTechSupport (address _newSupport) public{
-    require (msg.sender == owner || msg.sender == techSupport);
-    newTechSupport = _newSupport;
-  }
-
-  function acceptSupport() public{
-    if(msg.sender == newTechSupport){
-      techSupport = newTechSupport;
+    function transferOwnership(address newOwner) public onlyOwner {
+        if (newOwner != address(0)) 
+            owner = newOwner;
     }
-  }
 
+    function kill() public {
+        if (msg.sender == owner) 
+            selfdestruct(owner);
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender == owner)
+            _;
+    }
 }
 
-//Abstract Token contract
-contract BineuroToken{
-  function setCrowdsaleContract (address) public;
-  function sendCrowdsaleTokens(address, uint256)  public;
-  function burnTokens(address,address, address, uint) public;
-  function getOwner()public view returns(address);
+
+contract Pausable is Ownable {
+    bool public stopped;
+
+    modifier stopInEmergency {
+        if (stopped) {
+            revert();
+        }
+        _;
+    }
+
+    modifier onlyInEmergency {
+        if (!stopped) {
+            revert();
+        }
+        _;
+    }
+
+    // Called by the owner in emergency, triggers stopped state
+    function emergencyStop() external onlyOwner() {
+        stopped = true;
+    }
+
+    // Called by the owner to end of emergency, returns to normal state
+    function release() external onlyOwner() onlyInEmergency {
+        stopped = false;
+    }
 }
 
-//Crowdsale contract
-contract Crowdsale is Ownable{
 
-  using SafeMath for uint;
+// Crowdsale Smart Contract
+// This smart contract collects ETH and in return sends tokens to contributors
+contract Crowdsale is Pausable {
 
-  uint decimals = 3;
-  // Token contract address
-  BineuroToken public token;
+    using SafeMath for uint;
 
-  // Constructor
-  function Crowdsale(address _tokenAddress) public{
-    token = BineuroToken(_tokenAddress);
-    techSupport = msg.sender;
-
-    token.setCrowdsaleContract(this);
-    owner = token.getOwner();
-  }
-
-  address etherDistribution1 = 0x64f89e3CE504f1b15FcD4465b780Fb393ab79187;
-  address etherDistribution2 = 0x320359973d7953FbEf62C4f50960C46D8DBE2425;
-
-  address bountyAddress = 0x7e06828655Ba568Bbe06eD8ce165e4052A6Ea441;
-
-  //Crowdsale variables
-  uint public tokensSold = 0;
-  uint public ethCollected = 0;
-
-  // Buy constants
-  uint minDeposit = (uint)(500).mul((uint)(10).pow(decimals));
-
-  uint tokenPrice = 0.0001 ether;
-
-  // Ico constants
-  uint public icoStart = 1519034400; //02/19/2018 1519034400
-  uint public icoFinish = 1521453600; //03/19/2018 
-
-  uint public maxCap = 27000 ether;
-
-  //Owner can change end date
-  function changeIcoFinish (uint _newDate) public onlyOwner {
-    icoFinish = _newDate;
-  }
-  
-  //check is now ICO
-  function isIco(uint _time) public view returns (bool){
-    if((icoStart <= _time) && (_time < icoFinish)){
-      return true;
+    struct Backer {
+        uint weiReceived; // amount of ETH contributed
+        uint tokensSent; // amount of tokens  sent  
+        bool refunded; // true if user has been refunded       
     }
-    return false;
-  }
 
-  function timeBasedBonus(uint _time) public view returns(uint res) {
-    res = 20;
-    uint timeBuffer = icoStart;
-    for (uint i = 0; i<10; i++){
-      if(_time <= timeBuffer + 7 days){
-        return res;
-      }else{
-        res = res - 2;
-        timeBuffer = timeBuffer + 7 days;
-      }
-      if (res == 0){
-        return (0);
-      }
+    Token public token; // Token contract reference   
+    address public multisig; // Multisig contract that will receive the ETH    
+    address public team; // Address at which the team tokens will be sent        
+    uint public ethReceivedPresale; // Number of ETH received in presale
+    uint public ethReceivedMain; // Number of ETH received in public sale
+    uint public totalTokensSent; // Number of tokens sent to ETH contributors
+    uint public startBlock; // Crowdsale start block
+    uint public endBlock; // Crowdsale end block
+    uint public maxCap; // Maximum number of tokens to sell
+    uint public minCap; // Minimum number of ETH to raise
+    uint public minInvestETH; // Minimum amount to invest   
+    bool public crowdsaleClosed; // Is crowdsale still in progress
+    Step public currentStep;  // to allow for controled steps of the campaign 
+    uint public refundCount;  // number of refunds
+    uint public totalRefunded; // total amount of refunds    
+    uint public tokenPriceWei;  // price of token in wei
+
+    mapping(address => Backer) public backers; //backer list
+    address[] public backersIndex; // to be able to itarate through backers for verification.  
+
+    
+    // @notice to verify if action is not performed out of the campaing range
+    modifier respectTimeFrame() {
+        if ((block.number < startBlock) || (block.number > endBlock)) 
+            revert();
+        _;
     }
-    return res;
-  }
-  
-  function volumeBasedBonus(uint _value)public pure returns(uint res) {
-    if(_value < 5 ether){
-      return 0;
+
+    // @notice to set and determine steps of crowdsale
+    enum Step {
+        Unknown,
+        FundingPreSale,     // presale mode
+        FundingPublicSale,  // public mode
+        Refunding  // in case campaign failed during this step contributors will be able to receive refunds
     }
-    if (_value < 15 ether){
-      return 2;
+
+    // Events
+    event ReceivedETH(address backer, uint amount, uint tokenAmount);
+    event RefundETH(address backer, uint amount);
+
+
+    // Crowdsale  {constructor}
+    // @notice fired when contract is crated. Initilizes all constnat and initial values.
+    function Crowdsale() public {
+        multisig = 0xc15464420aC025077Ba280cBDe51947Fc12583D6; 
+        team = 0xc15464420aC025077Ba280cBDe51947Fc12583D6;                                  
+        minInvestETH = 1 ether/100;
+        startBlock = 0; // Should wait for the call of the function start
+        endBlock = 0; // Should wait for the call of the function start                  
+        tokenPriceWei = 1 ether/8000;
+        maxCap = 30600000e18;         
+        minCap = 900000e18;        
+        totalTokensSent = 1253083e18;  
+        setStep(Step.FundingPreSale);
     }
-    if (_value < 30 ether){
-      return 5;
+
+    // @notice to populate website with status of the sale 
+    function returnWebsiteData() external view returns(uint, uint, uint, uint, uint, uint, uint, uint, Step, bool, bool) {            
+    
+        return (startBlock, endBlock, backersIndex.length, ethReceivedPresale.add(ethReceivedMain), maxCap, minCap, totalTokensSent, tokenPriceWei, currentStep, stopped, crowdsaleClosed);
     }
-    if (_value < 50 ether){
-      return 8;
+
+    // @notice in case refunds are needed, money can be returned to the contract
+    function fundContract() external payable onlyOwner() returns (bool) {
+        return true;
     }
-    return 10;
-  }
-  
-  //fallback function (when investor send ether to contract)
-  function() public payable{
-    require(isIco(now));
-    require(ethCollected.add(msg.value) <= maxCap);
-    require(buy(msg.sender,msg.value, now)); //redirect to func buy
-  }
 
-  //function buy Tokens
-  function buy(address _address, uint _value, uint _time) internal returns (bool){
-    uint tokensForSend = etherToTokens(_value,_time);
+    // @notice Specify address of token contract
+    // @param _tokenAddress {address} address of token contract
+    // @return res {bool}
+    function updateTokenAddress(Token _tokenAddress) external onlyOwner() returns(bool res) {
+        token = _tokenAddress;
+        return true;
+    }
 
-    require (tokensForSend >= minDeposit);
+    // @notice set the step of the campaign 
+    // @param _step {Step}
+    function setStep(Step _step) public onlyOwner() {
+        currentStep = _step;
+        
+        if (currentStep == Step.FundingPreSale) {  // for presale 
+            tokenPriceWei = 1 ether/8000;  
+            minInvestETH = 1 ether/100;                             
+        }else if (currentStep == Step.FundingPublicSale) { // for public sale
+            tokenPriceWei = 1 ether/5000;   
+            minInvestETH = 0;               
+        }            
+    }
 
-    tokensSold = tokensSold.add(tokensForSend);
-    ethCollected = ethCollected.add(_value);
+    // @notice return number of contributors
+    // @return  {uint} number of contributors   
+    function numberOfBackers() external view returns(uint) {
+        return backersIndex.length;
+    }
 
-    token.sendCrowdsaleTokens(_address,tokensForSend);
-    etherDistribution1.transfer(this.balance/2);
-    etherDistribution2.transfer(this.balance);
+    // {fallback function}
+    // @notice It will call internal function which handels allocation of Ether and calculates tokens.
+    function () external payable {           
+        contribute(msg.sender);
+    }
 
-    return true;
-  }
+    // @notice It will be called by owner to start the sale    
+    function start(uint _block) external onlyOwner() {   
 
-  function manualSendTokens (address _address, uint _tokens) public onlyTechSupport {
-    token.sendCrowdsaleTokens(_address, _tokens);
-    tokensSold = tokensSold.add(_tokens);
-  }
+        require(_block < 246528);  // 4.28*60*24*40 days = 246528     
+        startBlock = block.number;
+        endBlock = startBlock.add(_block); 
+    }
 
-  //convert ether to tokens (without decimals)
-  function etherToTokens(uint _value, uint _time) public view returns(uint res) {
-    res = _value.mul((uint)(10).pow(decimals))/(tokenPrice);
-    uint bonus = timeBasedBonus(_time).add(volumeBasedBonus(_value));
-    res = res.add(res.mul(bonus)/100);
-  }
-  
-  bool public isIcoEnded = false;
-  
-  function endIco () public {
-    require(!isIcoEnded);
-    require(msg.sender == owner || msg.sender == techSupport);
-    require(now > icoFinish + 5 days);
-    token.burnTokens(etherDistribution1, etherDistribution2, bountyAddress, tokensSold);
-    isIcoEnded = true;
-  }
+    // @notice Due to changing average of block time
+    // this function will allow on adjusting duration of campaign closer to the end 
+    function adjustDuration(uint _block) external onlyOwner() {
+
+        require(_block < 308160);  // 4.28*60*24*50 days = 308160     
+        require(_block > block.number.sub(startBlock)); // ensure that endBlock is not set in the past
+        endBlock = startBlock.add(_block); 
+    }
+
+    // @notice It will be called by fallback function whenever ether is sent to it
+    // @param  _backer {address} address contributor
+    // @return res {bool} true if transaction was successful
+    function contribute(address _backer) internal stopInEmergency respectTimeFrame returns(bool res) {
+    
+        require(currentStep == Step.FundingPreSale || currentStep == Step.FundingPublicSale); // ensure that this is correct step
+        require(msg.value >= minInvestETH);   // ensure that min contributions amount is met
+          
+        uint tokensToSend = msg.value.mul(1e18) / tokenPriceWei; // calculate amount of tokens to send  (add 18 0s first)     
+        require(totalTokensSent.add(tokensToSend) < maxCap); // Ensure that max cap hasn't been reached  
+            
+        Backer storage backer = backers[_backer];
+    
+        if (backer.weiReceived == 0)      
+            backersIndex.push(_backer);
+           
+        backer.tokensSent = backer.tokensSent.add(tokensToSend); // save contributors tokens to be sent
+        backer.weiReceived = backer.weiReceived.add(msg.value);  // save how much was the contribution
+        totalTokensSent = totalTokensSent.add(tokensToSend);     // update the total amount of tokens sent
+    
+        if (Step.FundingPublicSale == currentStep)  // Update the total Ether recived
+            ethReceivedMain = ethReceivedMain.add(msg.value);
+        else
+            ethReceivedPresale = ethReceivedPresale.add(msg.value);     
+
+        if (!token.transfer(_backer, tokensToSend)) 
+            revert(); // Transfer tokens   
+    
+        multisig.transfer(this.balance);   // transfer funds to multisignature wallet             
+    
+        ReceivedETH(_backer, msg.value, tokensToSend); // Register event
+        return true;
+    }
+
+    // @notice This function will finalize the sale.
+    // It will only execute if predetermined sale time passed or all tokens are sold.
+    // it will fail if minimum cap is not reached
+    function finalize() external onlyOwner() {
+
+        require(!crowdsaleClosed);        
+        // purchasing precise number of tokens might be impractical, thus subtract 1000 tokens so finalizition is possible
+        // near the end 
+        require(block.number >= endBlock || totalTokensSent >= maxCap.sub(1000));                 
+        require(totalTokensSent >= minCap);  // ensure that minimum was reached
+
+        crowdsaleClosed = true;  
+        
+        if (!token.transfer(team, token.balanceOf(this))) // transfer all remaing tokens to team address
+            revert();
+        token.unlock();                      
+    }
+
+    // @notice Failsafe drain
+    function drain() external onlyOwner() {
+        multisig.transfer(this.balance);               
+    }
+
+    // @notice Failsafe token transfer
+    function tokenDrian() external onlyOwner() {
+        if (block.number > endBlock) {
+            if (!token.transfer(team, token.balanceOf(this))) 
+                revert();
+        }
+    }
+    
+    // @notice it will allow contributors to get refund in case campaign failed
+    function refund() external stopInEmergency returns (bool) {
+
+        require(currentStep == Step.Refunding);         
+       
+        require(this.balance > 0);  // contract will hold 0 ether at the end of campaign.                                  
+                                    // contract needs to be funded through fundContract() 
+
+        Backer storage backer = backers[msg.sender];
+
+        require(backer.weiReceived > 0);  // esnure that user has sent contribution
+        require(!backer.refunded);         // ensure that user hasn't been refunded yet
+
+        if (!token.returnTokens(msg.sender, backer.tokensSent)) // transfer tokens
+            revert();
+        backer.refunded = true;  // save refund status to true
+    
+        refundCount++;
+        totalRefunded = totalRefunded.add(backer.weiReceived);
+        msg.sender.transfer(backer.weiReceived);  // send back the contribution 
+        RefundETH(msg.sender, backer.weiReceived);
+        return true;
+    }
+}
+
+
+contract ERC20 {
+    uint public totalSupply;
+   
+    function transfer(address to, uint value) public returns(bool ok);  
+    function balanceOf(address who) public view returns(uint);
+}
+
+
+// The token
+contract Token is ERC20, Ownable {
+
+    function returnTokens(address _member, uint256 _value) public returns(bool);
+    function unlock() public;
 }
