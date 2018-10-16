bf2fbe24b024a6bc728a69cc24dee4cdca46547b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:24:20 +0700

    add contract ReservedTokensFinalizeAgent

--- ReservedTokensFinalizeAgent.sol
+++ ReservedTokensFinalizeAgent.sol
@@ -1,7 +1,6 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReservedTokensFinalizeAgent at 0x1e5c7dfEd1087EF18BE2E0Fd7Dd807C894675430
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReservedTokensFinalizeAgent at 0x3637dB679447EF6850Ce8EBE6b610b1F6c8304Bf
 */
-// Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 pragma solidity ^0.4.11;
 
 
