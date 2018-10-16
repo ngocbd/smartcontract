ced567013cfc4f746c9fc811dae2d543299bef64
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:06:13 +0700

    add contract RelaunchedCrowdsale

--- RelaunchedCrowdsale.sol
+++ RelaunchedCrowdsale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RelaunchedCrowdsale at 0x17ba01ca261a8daca30cd2b4b12c96e62cca359e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RelaunchedCrowdsale at 0xa4b87749b143b13f58922135a99e2d6c48dd32c2
 */
 /*
  * ERC20 interface
