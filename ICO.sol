b0bba108402684df2e592e66f19f50df46fee816
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:38:36 +0700

    add contract ICO

--- ICO.sol
+++ ICO.sol
@@ -1,328 +1,337 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ICO at 0xd0d9e2d407b726994b202a434e01e714b832153e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ICO at 0xF46ede17c0bAb20d87Cd079a2632f71433A407b9
 */
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.11;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
+  function mul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+  function div(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+  function sub(uint a, uint b) internal returns (uint) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
+  function add(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
     assert(c >= a);
     return c;
   }
+
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
+
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
+    }
+  }
 }
 
+contract PreICO {
+  function balanceOf(address _owner) constant returns (uint256);
+  function burnTokens(address _owner);
+}
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
 }
 
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+  function allowance(address owner, address spender) constant returns (uint);
+  function transferFrom(address from, address to, uint value);
+  function approve(address spender, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances. 
- */
 contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
+  using SafeMath for uint;
 
-  mapping(address => uint256) balances;
+  mapping(address => uint) balances;
 
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) returns (bool) {
+  /*
+   * Fix for the ERC20 short address attack  
+   */
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length < size + 4) {
+       throw;
+     }
+     _;
+  }
+
+  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
-    return true;
   }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of. 
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) constant returns (uint256 balance) {
+  function balanceOf(address _owner) constant returns (uint balance) {
     return balances[_owner];
   }
+  
 }
 
+contract StandardToken is BasicToken, ERC20 {
 
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-contract StandardToken is ERC20, BasicToken {
+  mapping (address => mapping (address => uint)) allowed;
 
-  mapping (address => mapping (address => uint256)) allowed;
-
-
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amout of tokens to be transfered
-   */
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+  function transferFrom(address _from, address _to, uint _value) {
     var _allowance = allowed[_from][msg.sender];
 
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-    // require (_value <= _allowance);
+    // if (_value > _allowance) throw;
 
     balances[_to] = balances[_to].add(_value);
     balances[_from] = balances[_from].sub(_value);
     allowed[_from][msg.sender] = _allowance.sub(_value);
     Transfer(_from, _to, _value);
-    return true;
   }
 
-  /**
-   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) returns (bool) {
-
-    // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender, 0)` if it is not
-    //  already 0 to mitigate the race condition described here:
-    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
+  function approve(address _spender, uint _value) {
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
-    return true;
   }
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifing the amount of tokens still avaible for the spender.
-   */
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+  function allowance(address _owner, address _spender) constant returns (uint remaining) {
     return allowed[_owner][_spender];
   }
+
 }
 
+contract ATL is StandardToken {
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
+  string public name = "ATLANT Token";
+  string public symbol = "ATL";
+  uint public decimals = 18;
+  uint constant TOKEN_LIMIT = 150 * 1e6 * 1e18;
 
+  address public ico;
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
+  bool public tokensAreFrozen = true;
+
+  function ATL(address _ico) {
+    ico = _ico;
+  }
+
+  function mint(address _holder, uint _value) external {
+    require(msg.sender == ico);
+    require(_value != 0);
+    require(totalSupply + _value <= TOKEN_LIMIT);
+
+    balances[_holder] += _value;
+    totalSupply += _value;
+    Transfer(0x0, _holder, _value);
+  }
+
+  function unfreeze() external {
+    require(msg.sender == ico);
+    tokensAreFrozen = false;
+  }
+
+  function transfer(address _to, uint _value) public {
+    require(!tokensAreFrozen);
+    super.transfer(_to, _value);
   }
 
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
+  function transferFrom(address _from, address _to, uint _value) public {
+    require(!tokensAreFrozen);
+    super.transferFrom(_from, _to, _value);
   }
 
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
+  function approve(address _spender, uint _value) public {
+    require(!tokensAreFrozen);
+    super.approve(_spender, _value);
+  }
+}
+
+contract ICO {
+
+  uint public constant MIN_TOKEN_PRICE = 425; // min atl per ETH
+  uint public constant TOKENS_FOR_SALE = 103548812 * 1e18;
+  uint public constant ATL_PER_ATP = 2; // Migration rate
+
+  event Buy(address holder, uint atlValue);
+  event ForeignBuy(address holder, uint atlValue, string txHash);
+  event Migrate(address holder, uint atlValue);
+  event RunIco();
+  event PauseIco();
+  event FinishIco(address teamFund, address bountyFund);
+
+  PreICO preICO;
+  ATL public atl;
+
+  address public team;
+  address public tradeRobot;
+  modifier teamOnly { require(msg.sender == team); _; }
+  modifier robotOnly { require(msg.sender == tradeRobot); _; }
+
+  uint public tokensSold = 0;
+
+  enum IcoState { Created, Running, Paused, Finished }
+  IcoState icoState = IcoState.Created;
+
+
+  function ICO(address _team, address _preICO, address _tradeRobot) {
+    atl = new ATL(this);
+    preICO = PreICO(_preICO);
+    team = _team;
+    tradeRobot = _tradeRobot;
+  }
+
+
+  function() external payable {
+    buyFor(msg.sender);
+  }
+
+
+  function buyFor(address _investor) public payable {
+    require(icoState == IcoState.Running);
+    require(msg.value > 0);
+    uint _total = buy(_investor, msg.value * MIN_TOKEN_PRICE);
+    Buy(_investor, _total);
+  }
+
+
+  function getBonus(uint _value, uint _sold)
+    public constant returns (uint)
+  {
+    uint[8] memory _bonusPricePattern = [ 505, 495, 485, 475, 465, 455, 445, uint(435) ];
+    uint _step = TOKENS_FOR_SALE / 10;
+    uint _bonus = 0;
+
+    for (uint8 i = 0; _value > 0 && i < _bonusPricePattern.length; ++i) {
+      uint _min = _step * i;
+      uint _max = _step * (i+1);
+
+      if (_sold >= _min && _sold < _max) {
+        uint bonusedPart = min(_value, _max - _sold);
+        _bonus += bonusedPart * _bonusPricePattern[i] / MIN_TOKEN_PRICE - bonusedPart;
+        _value -= bonusedPart;
+        _sold += bonusedPart;
+      }
     }
-  }
-}
 
-
-
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
-
-  bool public paused = false;
-
-
-  /**
-   * @dev modifier to allow actions only when the contract IS paused
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
+    return _bonus;
   }
 
-  /**
-   * @dev modifier to allow actions only when the contract IS NOT paused
-   */
-  modifier whenPaused {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused returns (bool) {
-    paused = true;
-    Pause();
-    return true;
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused returns (bool) {
-    paused = false;
-    Unpause();
-    return true;
-  }
-}
-
-
-/**
- * Pausable token
- *
- * Simple ERC20 Token example, with pausable token creation
- **/
-
-contract PausableToken is StandardToken, Pausable {
-
-  function transfer(address _to, uint _value) whenNotPaused returns (bool) {
-    return super.transfer(_to, _value);
-  }
-
-  function transferFrom(address _from, address _to, uint _value) whenNotPaused returns (bool) {
-    return super.transferFrom(_from, _to, _value);
-  }
-}
-
-
-contract UNLB is PausableToken {
-
-  string public constant name = "Unolabo";
-  string public constant symbol = "UNLB";
-  uint256 public constant decimals = 18;
-
-  function UNLB() {
-    owner = msg.sender;
-  }
-
-  function mint(address _x, uint _v) public onlyOwner {
-    balances[_x] += _v;
-    totalSupply += _v;
-    Transfer(0x0, _x, _v);
-  }
-}
-
-contract ICO is Pausable {
-
-  uint public constant ICO_START_DATE = /*2017-09-04 12:00:00+8*/ 1504497600;
-  uint public constant ICO_END_DATE   = /*2017-11-21 00:00:00+8*/ 1511193600;
-
-  address public constant admin      = 0xFeC0714C2eE71a486B679d4A3539FA875715e7d8;
-  address public constant teamWallet = 0xf16d5733A31D54e828460AFbf7D60aA803a61C51;
-
-  UNLB public unlb;
-  bool public isFinished = false;
-
-  function ICO() {
-    owner = admin;
-    unlb = new UNLB();
-    unlb.pause();
-  }
-
-  function pricePerWei() public constant returns(uint) {
-    if     (now < /*2017-10-19 00:00:00+8*/ 1508342400) return 650.0 * 1 ether;
-    else if(now < /*2017-10-27 00:00:00+8*/ 1509033600) return 575.0 * 1 ether;
-    else if(now < /*2017-11-05 00:00:00+8*/ 1509811200) return 537.5 * 1 ether;
-    else                                                return 500.0 * 1 ether;
+  function foreignBuy(address _investor, uint _atlValue, string _txHash)
+    external robotOnly
+  {
+    require(icoState == IcoState.Running);
+    require(_atlValue > 0);
+    uint _total = buy(_investor, _atlValue);
+    ForeignBuy(_investor, _total, _txHash);
   }
 
 
-  function() public payable {
-    require(!paused && now >= ICO_START_DATE && now < ICO_END_DATE);
-    uint _tokenVal = (msg.value * pricePerWei()) / 1 ether;
-    unlb.mint(msg.sender, _tokenVal);
+  function setRobot(address _robot) external teamOnly {
+    tradeRobot = _robot;
   }
 
 
-  function finish(address _team, address _fund, address _bounty) external onlyOwner {
-    require(now >= ICO_END_DATE && !isFinished);
-    unlb.unpause();
-    isFinished = true;
-
-    uint _total = unlb.totalSupply() * 100 / (100 - 12 - 10 - 2);
-    unlb.mint(_team,   (_total * 12) / 100);
-    unlb.mint(_fund,   (_total * 10) / 100);
-    unlb.mint(_bounty, (_total *  2) / 100);
+  function migrateSome(address[] _investors) external robotOnly {
+    for (uint i = 0; i < _investors.length; i++)
+      doMigration(_investors[i]);
   }
 
 
-  function withdraw() external onlyOwner {
-    teamWallet.transfer(this.balance);
+  function startIco() external teamOnly {
+    require(icoState == IcoState.Created || icoState == IcoState.Paused);
+    icoState = IcoState.Running;
+    RunIco();
   }
 
-  function transferTokenOwnership(address _a) external onlyOwner {
-    unlb.transferOwnership(_a);
+
+  function pauseIco() external teamOnly {
+    require(icoState == IcoState.Running);
+    icoState = IcoState.Paused;
+    PauseIco();
+  }
+
+
+  function finishIco(
+    address _teamFund,
+    address _bountyFund
+  )
+    external teamOnly
+  {
+    require(icoState == IcoState.Running || icoState == IcoState.Paused);
+
+    atl.mint(_teamFund, 22500000 * 1e18);
+    atl.mint(_bountyFund, 18750000 * 1e18);
+    atl.unfreeze();
+
+    icoState = IcoState.Finished;
+    FinishIco(_teamFund, _bountyFund);
+  }
+
+
+  function withdrawEther(uint _value) external teamOnly {
+    team.transfer(_value);
+  }
+
+
+  function withdrawToken(address _tokenContract, uint _val) external teamOnly
+  {
+    ERC20 _tok = ERC20(_tokenContract);
+    _tok.transfer(team, _val);
+  }
+
+
+  function min(uint a, uint b) internal constant returns (uint) {
+    return a < b ? a : b;
+  }
+
+
+  function buy(address _investor, uint _atlValue) internal returns (uint) {
+    uint _bonus = getBonus(_atlValue, tokensSold);
+    uint _total = _atlValue + _bonus;
+
+    require(tokensSold + _total <= TOKENS_FOR_SALE);
+
+    atl.mint(_investor, _total);
+    tokensSold += _total;
+    return _total;
+  }
+
+
+  function doMigration(address _investor) internal {
+    uint _atpBalance = preICO.balanceOf(_investor);
+    require(_atpBalance > 0);
+
+    preICO.burnTokens(_investor);
+
+    uint _atlValue = _atpBalance * ATL_PER_ATP;
+    atl.mint(_investor, _atlValue);
+
+    Migrate(_investor, _atlValue);
   }
 }
