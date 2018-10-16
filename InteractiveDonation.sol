f0df7f27f9bc925842a7b4514927ce6e377b98c1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:08:18 +0700

    add contract InteractiveDonation

---
+++ InteractiveDonation.sol
@@ -0,0 +1,49 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract InteractiveDonation at 0x9f8c9b1208e55a3c68b668d14c749d6b80519081
+*/
+pragma solidity ^0.4.21;
+
+// Donate all your ethers to 0x7Ec 
+// Made by EtherGuy (etherguy@mail.com)
+// CryptoGaming Discord https://discord.gg/gjrHXFr
+// UI @ htpts://0x7.surge.sh
+
+contract InteractiveDonation{
+    address constant public Donated = 0x7Ec915B8d3FFee3deaAe5Aa90DeF8Ad826d2e110;
+    
+    event Quote(address Sent, string Text, uint256 AmtDonate);
+
+    string public DonatedBanner = "";
+    
+
+    
+    function Donate(string quote) public payable {
+        require(msg.sender != Donated); // GTFO dont donate to yourself
+        
+        emit Quote(msg.sender, quote, msg.value);
+    }
+    
+    function Withdraw() public {
+        if (msg.sender != Donated){
+            emit Quote(msg.sender, "OMG CHEATER ATTEMPTING TO WITHDRAW", 0);
+            return;
+        }
+        address contr = this;
+        msg.sender.transfer(contr.balance);
+    }   
+    
+    function DonatorInteract(string text) public {
+        require(msg.sender == Donated);
+        emit Quote(msg.sender, text, 0);
+    }
+    
+    function DonatorSetBanner(string img) public {
+        require(msg.sender == Donated);
+        DonatedBanner = img;
+    }
+    
+    function() public payable{
+        require(msg.sender != Donated); // Nice cheat but no donating to yourself 
+    }
+    
+}
