1ec2f90c6bb0b2ada793c98a263e767bd7eec2f8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:35:59 +0700

    add contract PumpToken

--- PumpToken.sol
+++ PumpToken.sol
@@ -1,53 +1,34 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PumpToken at 0xd7a51e8e9114849eae481e5887c831248a691acb
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PumpToken at 0xdbe46c0e6526e9068da1626acfddff2386635045
 */
 pragma solidity ^0.4.10;
 
-contract BullishToken {
+contract Token {
     function balanceOf(address _owner) constant returns (uint256);
     function transfer(address _to, uint256 _value) returns (bool);
 }
-contract owned {
-    address public owner;
 
-    function owned() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        if (msg.sender != owner) throw;
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
-    }
-}
-
-contract PumpToken is owned {
-    
+contract PumpToken {
     address owner = msg.sender;
 
-    bool public purchasingAllowed = false;
-   
+    bool public purchasingAllowed = true;
+
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    
+
     uint256 public totalContribution = 0;
-    uint256 public totalBonusTokensIssued = 0;
-    
 
     uint256 public totalSupply = 0;
 
-    function name() constant returns (string) { return "Pump Token"; }
-    function symbol() constant returns (string) { return "PTK"; }
-    function decimals() constant returns (uint8) { return 5; }
+    function name() pure returns (string) { return "Pump Token"; }
+    function symbol() pure returns (string) { return "PUMP"; }
+    function decimals() pure returns (uint8) { return 18; }
     
     function balanceOf(address _owner) constant returns (uint256) { return balances[_owner]; }
     
     function transfer(address _to, uint256 _value) returns (bool success) {
         // mitigates the ERC20 short address attack
-        if(msg.data.length < (2 * 32) + 4) { throw; }
+        if(msg.data.length < (2 * 32) + 4) { return false; }
 
         if (_value == 0) { return false; }
 
@@ -64,10 +45,10 @@ contract PumpToken is owned {
             return true;
         } else { return false; }
     }
-     
+    
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         // mitigates the ERC20 short address attack
-        if(msg.data.length < (3 * 32) + 4) { throw; }
+        if(msg.data.length < (3 * 32) + 4) { return false; }
 
         if (_value == 0) { return false; }
         
@@ -107,55 +88,42 @@ contract PumpToken is owned {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     function enablePurchasing() {
-        if (msg.sender != owner) { throw; }
+        if (msg.sender != owner) { return; }
 
         purchasingAllowed = true;
     }
 
     function disablePurchasing() {
-        if (msg.sender != owner) { throw; }
+        if (msg.sender != owner) { return; }
 
         purchasingAllowed = false;
     }
 
-    function withdrawForeignTokens(address _tokenContract) returns (bool) {
-        if (msg.sender != owner) { throw; }
+    function withdrawTokens(address _tokenContract) returns (bool) {
+        if (msg.sender != owner) { return; }
 
-        BullishToken token = BullishToken(_tokenContract);
+        Token token = Token(_tokenContract);
 
         uint256 amount = token.balanceOf(address(this));
         return token.transfer(owner, amount);
     }
 
-    function getStats() constant returns (uint256, uint256, uint256, bool) {
-        return (totalContribution, totalSupply, totalBonusTokensIssued, purchasingAllowed);
+    function getStats() constant returns (uint256, uint256, bool) {
+        return (totalContribution, totalSupply, purchasingAllowed);
     }
 
     function() payable {
-        if (!purchasingAllowed) { throw; }
+        if (!purchasingAllowed) { return; }
         
         if (msg.value == 0) { return; }
 
         owner.transfer(msg.value);
         totalContribution += msg.value;
 
-        uint256 tokensIssued = (msg.value * 20);
+        uint256 tokensIssued = (msg.value * 100);
 
         if (msg.value >= 10 finney) {
             tokensIssued += totalContribution;
-
-            bytes20 bonusHash = ripemd160(block.coinbase, block.number, block.timestamp);
-            if (bonusHash[0] == 0) {
-                uint8 bonusMultiplier =
-                    ((bonusHash[1] & 0x01 != 0) ? 1 : 0) + ((bonusHash[1] & 0x02 != 0) ? 1 : 0) +
-                    ((bonusHash[1] & 0x04 != 0) ? 1 : 0) + ((bonusHash[1] & 0x08 != 0) ? 1 : 0)
-                    ;
-                
-                uint256 bonusTokensIssued = (msg.value * 100) * bonusMultiplier;
-                tokensIssued += bonusTokensIssued;
-
-                totalBonusTokensIssued += bonusTokensIssued;
-            }
         }
 
         totalSupply += tokensIssued;
@@ -163,11 +131,4 @@ contract PumpToken is owned {
         
         Transfer(address(this), msg.sender, tokensIssued);
     }
-    
-    function mintToken(address target, uint256 mintedAmount) onlyOwner {
-    
-    totalSupply -= mintedAmount;
-    Transfer(0, owner, mintedAmount);
-    Transfer(owner, target, mintedAmount);
-}
 }
