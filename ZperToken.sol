7762ef82d279002d0af3ddcfdfb10f7f798d174d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:46:10 +0700

    add contract ZperToken

--- ZperToken.sol
+++ ZperToken.sol
@@ -1,9 +1,52 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ZperToken at 0x7c539bdeb5e20b084af0722158a1b5613b328c7a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ZperToken at 0xb5b8f5616fe42d5ceca3e87f3fddbdd8f496d760
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
+
+library SafeMath {
+
+/**
+ * @dev Multiplies two numbers, throws on overflow.
+ */
+	function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+		if (a == 0) {
+			return 0;
+		}
+		c = a * b;
+		assert(c / a == b);
+		return c;
+	}
+
+/**
+ * @dev Integer division of two numbers, truncating the quotient.
+ */
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		// assert(b > 0); // Solidity automatically throws when dividing by 0
+		// uint256 c = a / b;
+		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+		return a / b;
+	}
+
+/**
+ * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+ */
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+
+/**
+ * @dev Adds two numbers, throws on overflow.
+ */
+	function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+		c = a + b;
+		assert(c >= a);
+		return c;
+	}
+}
 
 contract ZperToken {
+	using SafeMath for uint256;
 
 	address public owner;
 	uint256 public totalSupply;
@@ -11,6 +54,7 @@ contract ZperToken {
 	string public constant name = "ZperToken";
 	string public constant symbol = "ZPR";
 	uint8 public constant decimals = 18;
+
 
 	mapping (address => uint256) public balances;
 	mapping (address => mapping (address => uint256)) public allowed;
@@ -44,19 +88,24 @@ contract ZperToken {
 	}
 
 	function transfer(address _to, uint256 _value) public returns (bool success) {
-		require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-		balances[msg.sender] -= _value;
-		balances[_to] += _value;
+		require(_to != address(0));
+		require(balances[msg.sender] >= _value);
+
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		
 		emit Transfer(msg.sender, _to, _value);
 		return true;
 	}
 
 	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value
-			   	&& balances[_to] + _value > balances[_to]);
-		balances[_to] += _value;
-		balances[_from] -= _value;
-		allowed[_from][msg.sender] -= _value;
+		require(_to != address(0));
+		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
+
+		balances[_from] = balances[_from].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+
 		emit Transfer(_from, _to, _value);
 		return true;
 	}
@@ -67,6 +116,7 @@ contract ZperToken {
 
 	function approve(address _spender, uint256 _value) public returns (bool success) {
 		allowed[msg.sender][_spender] = _value;
+
 		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
@@ -76,30 +126,39 @@ contract ZperToken {
 	}
 
 	function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
-		require(cap >= totalSupply + _amount);
-		require(totalSupply + _amount > totalSupply && balances[_to] + _amount > balances[_to]);
-		totalSupply += _amount;
-		balances[_to] += _amount;
+		require(_to != address(0));
+		require(cap >= totalSupply.add(_amount));
+
+		totalSupply = totalSupply.add(_amount);
+		balances[_to] = balances[_to].add(_amount);
+
 		emit Mint(_to, _amount);
 		emit Transfer(address(0), _to, _amount);
+
 		return true;
 	}
 
 	function burn(uint256 _value) public returns (bool) {
 		require(_value <= balances[msg.sender]);
-		balances[msg.sender] -= _value;
-		totalSupply -= _value;
+
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		totalSupply = totalSupply.sub(_value);
+
 		emit Burn(msg.sender, _value);
 		emit Transfer(msg.sender, address(0), _value);
+
 		return true;
 	}
 
-	function batchTransfer(address[] _tos, uint256[] _amount) public returns (bool success) {
+	function batchTransfer(address[] _tos, uint256[] _amount) onlyOwner public returns (bool success) {
 		require(_tos.length == _amount.length);
 		uint256 i;
 		uint256 sum = 0;
-		for(i = 0; i < _amount.length; i++)
-			sum += _amount[i];
+
+		for(i = 0; i < _amount.length; i++) {
+			sum = sum.add(_amount[i]);
+			require(_tos[i] != address(0));
+		}
 
 		require(balances[msg.sender] >= sum);
 
