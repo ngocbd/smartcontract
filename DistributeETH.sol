fc2350af47fce115c2870be02d3d2d775110b20e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:33:37 +0700

    add contract DistributeETH

---
+++ DistributeETH.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DistributeETH at 0xd9be732c4b7d25f94376d1f37368422aa2801279
+*/
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+ 
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+
+
+contract DistributeETH is Ownable{
+  
+
+  function distributeFixed(address[] _addrs, uint _amoutToEach) onlyOwner{
+    for(uint i = 0; i < _addrs.length; ++i){
+      if(!_addrs[i].send(_amoutToEach)) throw;
+    }
+  }
+
+  function () payable {}
+
+}
