60873461d9c6bb5116d41e0d668256a6b095387c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:46:41 +0700

    add contract Eater

---
+++ Eater.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Eater at 0x7b9e81ec3591b570aa2c9fbec77f7dda76ac4f6d
+*/
+contract Eater{
+	function(){
+		
+	}
+}
