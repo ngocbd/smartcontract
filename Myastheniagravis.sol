3483560cd02a30683a687f2ea37eda9406af7f79
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:19:44 +0700

    add contract Myastheniagravis

---
+++ Myastheniagravis.sol
@@ -0,0 +1,51 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Myastheniagravis at 0x8D5fF2eFAEeF4c65B3116751BF0b93d583De30fa
+*/
+pragma solidity ^0.4.2;
+
+contract Myastheniagravis {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Myastheniagravis() {
+
+         initialSupply = 1000000;
+         name ="Mysthenia Gravis";
+        decimals = 2;
+         symbol = "MSG";
+        
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        uint256 totalSupply = initialSupply;                        // Update total supply
+                                   
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
+    }
+
+   
+
+    
+
+   
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
