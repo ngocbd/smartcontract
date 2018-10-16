2be0f0e9e465883b5bd7fab91b709374a693ccf8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:40:44 +0700

    add contract FTC

--- FTC.sol
+++ FTC.sol
@@ -1,131 +1,213 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FTC at 0xe6f74dcfa0e20883008d8c16b6d9a329189d0c30
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FTC at 0x19fc30d247c7bd60f42e3794b56df540dd805d0e
 */
-pragma solidity ^0.4.15;
-/* Briantbros Lima Kpy*/
+pragma solidity 0.4.21;
 
 library SafeMath {
-  function mul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-  function div(uint a, uint b) internal returns (uint) {
-    assert(b > 0);
-    uint c = a / b;
-    assert(a == b * c + a % b);
-    return c;
-  }
-  function sub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
-  function add(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c >= a);
-    return c;
-  }
-  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a >= b ? a : b;
-  }
-  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a < b ? a : b;
-  }
-  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a >= b ? a : b;
-  }
-  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a < b ? a : b;
-  }
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
-    }
-  }
-}
-
-contract Ownable {
-    address public owner;
-    function Ownable() {
-        owner = msg.sender;
-    }
-    modifier onlyOwner {
-        if (msg.sender != owner) throw;
-        _;
-    }
-    function transferOwnership(address newOwner) onlyOwner {
-        if (newOwner != address(0)) {
-            owner = newOwner;
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
         }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
 }
 
 contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract Token is ERC20Basic {
-  
-  using SafeMath for uint;
-  
-  mapping(address => uint) balances;
-  
+library SafeERC20 {
+    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+        assert(token.transfer(to, value));
+    }
 
-  modifier onlyPayloadSize(uint size) {
-     if(msg.data.length < size + 4) {
-       throw;
-     }
-     _;
-  }
-  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-  }
-  function balanceOf(address _owner) constant returns (uint balance) {
-    return balances[_owner];
-  }
+    function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
+        assert(token.transferFrom(from, to, value));
+    }
+
+    function safeApprove(ERC20 token, address spender, uint256 value) internal {
+        assert(token.approve(spender, value));
+    }
 }
 
-contract FinTechCoin is Token, ERC20 {
-  mapping (address => mapping (address => uint)) allowed;
-  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
-    var _allowance = allowed[_from][msg.sender];
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    Transfer(_from, _to, _value);
-  }
-  function approve(address _spender, uint _value) {
-    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-  }
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
+contract BasicToken is ERC20Basic {
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+
+    uint256 totalSupply_;
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 }
 
-contract FTC is FinTechCoin, Ownable {
-  string public constant name = "FinTech Coin";
-  string public constant symbol = "FTC";
-  uint public constant decimals = 2;
-  uint256 public initialSupply;
-    
-  function FTC () { 
-     totalSupply = 365000000 * 10 ** decimals;
-      balances[msg.sender] = totalSupply;
-      initialSupply = totalSupply; 
-        Transfer(0, this, totalSupply);
-        Transfer(this, msg.sender, totalSupply);
-  }
+contract StandardToken is ERC20, BasicToken {
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+}
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract HasNoEther is Ownable {
+    function HasNoEther() public payable {
+        require(msg.value == 0);
+    }
+
+    function() external {
+    }
+
+    function reclaimEther() external onlyOwner {
+        assert(owner.send(this.balance));
+    }
+}
+
+contract CanReclaimToken is Ownable {
+    using SafeERC20 for ERC20Basic;
+
+    function reclaimToken(ERC20Basic token) external onlyOwner {
+        uint256 balance = token.balanceOf(this);
+        token.safeTransfer(owner, balance);
+    }
+
+}
+
+contract HasNoTokens is CanReclaimToken {
+    function tokenFallback(address from_, uint256 value_, bytes data_) external {
+        from_;
+        value_;
+        data_;
+        revert();
+    }
+
+}
+
+contract HasNoContracts is Ownable {
+    function reclaimContract(address contractAddr) external onlyOwner {
+        Ownable contractInst = Ownable(contractAddr);
+        contractInst.transferOwnership(owner);
+    }
+}
+
+contract NoOwner is HasNoEther, HasNoTokens, HasNoContracts {
+}
+
+contract FTC is StandardToken, NoOwner {
+    string public constant name = "FTC token";
+    string public constant symbol = "FTC";
+    uint8 public constant decimals = 18;
+
+    function FTC()
+    public
+    {
+        address _account = 0x38e2bF0106da7bE34C8339c7d964B392c43349cE;
+        uint256 _amount = 1000000000 * 1 ether; // 1 000 000 000 * 10e18
+
+        totalSupply_ = _amount;
+        balances[_account] = _amount;
+
+        emit Transfer(address(0), _account, _amount);
+    }
 }
