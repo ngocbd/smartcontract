6f8c102fac43e9fc99cd744ea6ef113a1a7ae3d2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:19:36 +0700

    add contract POWToken

--- PowToken.sol
+++ PowToken.sol
@@ -1,33 +1,36 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PowToken at 0xEb2dA9FAC54284cEA731D1F10bb34EEcB3c00c14
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract POWToken at 0x0f26cfc5fb0fa89c05d86c0333545727aa896a75
 */
-pragma solidity 0.4.13;
-
+pragma solidity ^0.4.18;
+/**
+ * Official POWToken creation Smart Contract from PowerLineUp.io
+ * PowerLineUp is the Standard ERC20 Token for Fantasy Strategy, powered by the Ethereum Network.
+ */
 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
@@ -44,17 +47,6 @@ contract ERC20Basic {
   function balanceOf(address who) public constant returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 /**
@@ -86,10 +78,21 @@ contract BasicToken is ERC20Basic {
   * @param _owner The address to query the the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) public constant returns (uint256 balance) {
+  function balanceOf(address _owner) constant public returns (uint256 balance) {
     return balances[_owner];
   }
 
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 /**
@@ -97,12 +100,10 @@ contract BasicToken is ERC20Basic {
  *
  * @dev Implementation of the basic standard token.
  * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
 contract StandardToken is ERC20, BasicToken {
 
   mapping (address => mapping (address => uint256)) allowed;
-
 
   /**
    * @dev Transfer tokens from one address to another
@@ -113,7 +114,7 @@ contract StandardToken is ERC20, BasicToken {
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
 
-    uint256 _allowance = allowed[_from][msg.sender];
+    var _allowance = allowed[_from][msg.sender];
 
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
     // require (_value <= _allowance);
@@ -127,15 +128,18 @@ contract StandardToken is ERC20, BasicToken {
 
   /**
    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   *
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
   function approve(address _spender, uint256 _value) public returns (bool) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
@@ -155,17 +159,15 @@ contract StandardToken is ERC20, BasicToken {
    * approve should be called when allowed[_spender] == 0. To increment
    * allowed value is better to use this function to avoid 2 calls (and wait until
    * the first transaction is mined)
-   * From MonolithDAO Token.sol
    */
-  function increaseApproval (address _spender, uint _addedValue)
-    returns (bool success) {
+
+  function increaseApproval (address _spender, uint _addedValue)public returns (bool success) {
     allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
     return true;
   }
 
-  function decreaseApproval (address _spender, uint _subtractedValue)
-    returns (bool success) {
+  function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
     uint oldValue = allowed[msg.sender][_spender];
     if (_subtractedValue > oldValue) {
       allowed[msg.sender][_spender] = 0;
@@ -178,14 +180,54 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
-contract PowToken is StandardToken {
-  string public name = "POW Token";
-  string public symbol = "POW";
-  uint256 public decimals = 18;
-  uint256 public INITIAL_SUPPLY = 231802823220 * (10 ** uint256(decimals));
+/**
+ * @title POWToken
+ * POWToken is the Standard ERC20 Token for Fantasy Strategy, powered by the Ethereum Network.
+ */
+contract POWToken is StandardToken {
 
-  function PowToken() {
-  	totalSupply = INITIAL_SUPPLY;
-  	balances[msg.sender] = INITIAL_SUPPLY;
+  string public constant name = "POWToken";
+  string public constant symbol = "POW";
+  uint8 public constant decimals = 18; // 18 decimal to fully comply with ERC20 Standard.
+
+//30 million POWtokens will be distributed via CrowdSale for Early Adopters. 
+//30 million POWtokens will be available only by mining or staking in-game with the previously distributed tokens.
+//All tokens are transferable through the Ethereum Blockchain. 
+//All tokens are redeemable for tournament prizes and ether at regular token prize in-platform. 
+//Since number of tokens is limited, the price of each token will increase as the number of users and demand increases. 
+
+  uint256 public constant totalSupplyWithDecimals = 60000000000000000000000000; //60 million unique tokens for a single distribution and 18 decimal places to fully comply with ERC20 Standard. 
+  
+  uint256 public mineableSupply;                        //Amount of POWTokens stored for in-platform mining, staking and prizes.
+  address public mineableTokenStorageContract;          //Smart Contract to store in-platform tokens.
+  uint256 public openSaleSupply;                        //Amount of POWTokens distributed to Early Adpoters. 
+  address public openDistributionContract;              //Smart Contract to store tokens for Open Distribution (CrowdSale).
+
+  //No tokens for founders. Full-transparency distribution.
+
+  string public tokenCreationDate;
+          
+  /**
+   * @dev Contructor that gives msg.sender all of existing tokens.
+   */
+  function POWToken()public {
+    
+    balances[msg.sender] = totalSupplyWithDecimals;
+
   }
+
+
+// setup contract addresses.
+  function setupFunctionalAddresses(address _mineableTokenStorageContract, address _OpenDistributionContract) public returns (bytes32 response) {
+      
+          mineableSupply = 30000000000000000000000000;                           
+          mineableTokenStorageContract = _mineableTokenStorageContract;
+          openSaleSupply = 30000000000000000000000000;
+          openDistributionContract = _OpenDistributionContract;
+          tokenCreationDate = "December-27-2017";
+          
+          return "Addresses Setup.";
+
+         }
+    
 }
