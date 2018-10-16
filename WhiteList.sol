9c44a6b1de0c719568b29fb2c7e94f2ba9ad958b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:45:01 +0700

    add contract Whitelist

--- WhiteList.sol
+++ WhiteList.sol
@@ -1,8 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Whitelist at 0xd99c17fc653176A32734031Be3000bc82F261EB0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Whitelist at 0x49ac60fcc6990b3105076ecfbf908cc83c8049f8
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.19;
 
+// File: zeppelin-solidity/contracts/ownership/Ownable.sol
 
 /**
  * @title Ownable
@@ -10,10 +11,11 @@ pragma solidity ^0.4.18;
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-
   address public owner;
 
+
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
 
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
@@ -43,50 +45,46 @@ contract Ownable {
 
 }
 
+// File: contracts/Whitelist.sol
 
 /**
-  *  Whitelist contract
-  */
+ * @title Whitelist contract
+ * @dev Whitelist for wallets.
+*/
 contract Whitelist is Ownable {
+    mapping(address => bool) whitelist;
 
-   mapping (address => bool) public whitelist;
-   event Registered(address indexed _addr);
-   event Unregistered(address indexed _addr);
+    uint256 public whitelistLength = 0;
 
-   modifier onlyWhitelisted(address _addr) {
-     require(whitelist[_addr]);
-     _;
-   }
+    /**
+    * @dev Add wallet to whitelist.
+    * @dev Accept request from the owner only.
+    * @param _wallet The address of wallet to add.
+    */
+    function addWallet(address _wallet) public onlyOwner {
+        require(_wallet != address(0));
+        require(!isWhitelisted(_wallet));
+        whitelist[_wallet] = true;
+        whitelistLength++;
+    }
 
-   function isWhitelist(address _addr) public view returns (bool listed) {
-     return whitelist[_addr];
-   }
+    /**
+    * @dev Remove wallet from whitelist.
+    * @dev Accept request from the owner only.
+    * @param _wallet The address of whitelisted wallet to remove.
+    */
+    function removeWallet(address _wallet) public onlyOwner {
+        require(_wallet != address(0));
+        require(isWhitelisted(_wallet));
+        whitelist[_wallet] = false;
+        whitelistLength--;
+    }
 
-   function registerAddress(address _addr) public onlyOwner {
-     require(_addr != address(0) && whitelist[_addr] == false);
-     whitelist[_addr] = true;
-     Registered(_addr);
-   }
-
-   function registerAddresses(address[] _addrs) public onlyOwner {
-     for(uint256 i = 0; i < _addrs.length; i++) {
-       require(_addrs[i] != address(0) && whitelist[_addrs[i]] == false);
-       whitelist[_addrs[i]] = true;
-       Registered(_addrs[i]);
-     }
-   }
-
-   function unregisterAddress(address _addr) public onlyOwner onlyWhitelisted(_addr) {
-       whitelist[_addr] = false;
-       Unregistered(_addr);
-   }
-
-   function unregisterAddresses(address[] _addrs) public onlyOwner {
-     for(uint256 i = 0; i < _addrs.length; i++) {
-       require(whitelist[_addrs[i]]);
-       whitelist[_addrs[i]] = false;
-       Unregistered(_addrs[i]);
-     }
-   }
-
+    /**
+    * @dev Check the specified wallet whether it is in the whitelist.
+    * @param _wallet The address of wallet to check.
+    */
+    function isWhitelisted(address _wallet) public view returns (bool) {
+        return whitelist[_wallet];
+    }
 }
