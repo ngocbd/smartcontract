2ebf5cd14d123f6f2f2a006c846a3cab2c0f4596
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:41:46 +0700

    add contract Presale

--- Presale.sol
+++ Presale.sol
@@ -1,91 +1,227 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x41cECe7e357f332EB9093D13138B02632cF0a3ca
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x18714dba089604fe18a7af33871198fd470ffb3a
 */
-pragma solidity ^0.4.13;
-contract token { 
-    function transfer(address receiver, uint amount);
-    function balanceOf(address _owner) constant returns (uint256 balance); 
-}
+pragma solidity ^0.4.6;
+
+//
+// ==== DISCLAIMER ====
+//
+// ETHEREUM IS STILL AN EXPEREMENTAL TECHNOLOGY.
+// ALTHOUGH THIS SMART CONTRACT WAS CREATED WITH GREAT CARE AND IN THE HOPE OF BEING USEFUL, NO GUARANTEES OF FLAWLESS OPERATION CAN BE GIVEN. 
+// IN PARTICULAR - SUBTILE BUGS, HACKER ATTACKS OR MALFUNCTION OF UNDERLYING TECHNOLOGY CAN CAUSE UNINTENTIONAL BEHAVIOUR. 
+// YOU ARE STRONGLY ENCOURAGED TO STUDY THIS SMART CONTRACT CAREFULLY IN ORDER TO UNDERSTAND POSSIBLE EDGE CASES AND RISKS. 
+// DON'T USE THIS SMART CONTRACT IF YOU HAVE SUBSTANTIAL DOUBTS OR IF YOU DON'T KNOW WHAT YOU ARE DOING.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ====
+//
+//
+// ==== PARANOIA NOTICE ==== 
+// A careful reader will find some additional checks and excessive code, consuming some extra gas. This is intentional. 
+// Even though the contract should work without these parts, they make the code more secure in production and for future refactoring.
+// Also, they show more clearly what we have considered and addressed during development.
+// Discussion is welcome!
+// ====
+//
+
+/// @title GPS Presale, 100% bonus
 
 contract Presale {
-    address public beneficiary;
-    uint public min_fundingGoal; 
-    uint public max_fundingGoal;
-    uint public amountRaised; 
-    uint public deadline; 
-    uint public rate;
-    token public tokenReward;
-    mapping(address => uint256) public balanceOf;
-    bool fundingGoalReached = false;
-    event GoalReached(address beneficiary, uint amountRaised);
-    event FundTransfer(address backer, uint amount, bool isContribution);
-    bool crowdsaleClosed = false;
 
-    /* data structure to hold information about campaign contributors */
+    string public constant VERSION = "0.1.5-GP";
 
-    /*  at initialization, setup the owner */
-    function Presale() {
-        beneficiary = msg.sender;
-        min_fundingGoal = 100 * 1 ether;
-        max_fundingGoal = 2000 * 1 ether;
-        deadline = now + 30 * 1 days;
-        rate = 1500;     // Each ether exchange for 1500.00000000 PAT
-        tokenReward = token(0x990da731331aE62c3e8f3f73cB4ebeb6F082Bc7c);
-    }
+    /* ====== configuration START ====== */
+    uint public constant PRESALE_START  = 4262000; /* approx. MON SEP 11 2017 12:17:13 GMT+0200 (CEST) */
+    uint public constant PRESALE_END    = 4400000; /* approx. TUE OCT 03 2017 05:38:23 GMT+0200 (CEST) */
+    uint public constant WITHDRAWAL_END = 4780000; /* approx. SUN DEC 03 2017 18:25:10 GMT+0100 (CET)  */
 
-    /* The function without name is the default function that is called whenever anyone sends funds to a contract */
-    function () payable {
-        uint amount = msg.value;
-        require (!crowdsaleClosed);
-        require (amountRaised + amount <= max_fundingGoal);
-        require (amount >= 10 * 1 ether);
-        
-        balanceOf[msg.sender] += amount;
-        amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount * rate * 10 ** 8 / 1 ether);
-        FundTransfer(msg.sender, amount, true);
-    }
+    address public constant OWNER = 0xA24AFdBe4A1E8bA8Fa67580186203a60D13083cD;
 
-    modifier afterDeadline() { if (now >= deadline) _; }
+    uint public constant MIN_TOTAL_AMOUNT_TO_RECEIVE_ETH = 200;
+    uint public constant MAX_TOTAL_AMOUNT_TO_RECEIVE_ETH = 900;
+    uint public constant MIN_ACCEPTED_AMOUNT_FINNEY = 1;
 
-    /* checks if the goal or time limit has been reached and ends the campaign */
-    function checkGoalReached() afterDeadline {
-        if (amountRaised >= min_fundingGoal){
-            fundingGoalReached = true;
-            GoalReached(beneficiary, amountRaised);
-        }
-        crowdsaleClosed = true;
-    }
+    /* ====== configuration END ====== */
+
+    string[5] private stateNames = ["BEFORE_START",  "PRESALE_RUNNING", "WITHDRAWAL_RUNNING", "REFUND_RUNNING", "CLOSED" ];
+    enum State { BEFORE_START,  PRESALE_RUNNING, WITHDRAWAL_RUNNING, REFUND_RUNNING, CLOSED }
+
+    uint public total_received_amount;
+    mapping (address => uint) public balances;
+
+    uint private constant MIN_TOTAL_AMOUNT_TO_RECEIVE = MIN_TOTAL_AMOUNT_TO_RECEIVE_ETH * 1 ether;
+    uint private constant MAX_TOTAL_AMOUNT_TO_RECEIVE = MAX_TOTAL_AMOUNT_TO_RECEIVE_ETH * 1 ether;
+    uint private constant MIN_ACCEPTED_AMOUNT = MIN_ACCEPTED_AMOUNT_FINNEY * 1 finney;
+    bool public isAborted = false;
 
 
-    function safeWithdrawal() afterDeadline {
-        if (!fundingGoalReached) {
-            uint amount = balanceOf[msg.sender];
-            balanceOf[msg.sender] = 0;
-            if (amount > 0) {
-                if (msg.sender.send(amount)) {
-                    FundTransfer(msg.sender, amount, false);
-                } else {
-                    balanceOf[msg.sender] = amount;
-                }
-            }
-        }
+    //constructor
+    function Presale () validSetupOnly() { }
 
-        if (fundingGoalReached && beneficiary == msg.sender) {
-            if (beneficiary.send(amountRaised)) {
-                FundTransfer(beneficiary, amountRaised, false);
-            } else {
-                //If we fail to send the funds to beneficiary, unlock funders balance
-                fundingGoalReached = false;
-            }
+    //
+    // ======= interface methods =======
+    //
+
+    //accept payments here
+    function ()
+    payable
+    noReentrancy
+    {
+        State state = currentState();
+        if (state == State.PRESALE_RUNNING) {
+            receiveFunds();
+        } else if (state == State.REFUND_RUNNING) {
+            // any entring call in Refund Phase will cause full refund
+            sendRefund();
+        } else {
+            throw;
         }
     }
 
-    function WithdrawlRemainingPAT() afterDeadline {
-        require(msg.sender == beneficiary);
-        uint remainingAmount = tokenReward.balanceOf(this);
-        remainingAmount = remainingAmount * 99 / 100;
-        tokenReward.transfer(beneficiary, remainingAmount);
-        FundTransfer(beneficiary, remainingAmount, false);
+    function refund() external
+    inState(State.REFUND_RUNNING)
+    noReentrancy
+    {
+        sendRefund();
     }
-}
+
+
+    function withdrawFunds() external
+    inState(State.WITHDRAWAL_RUNNING)
+    onlyOwner
+    noReentrancy
+    {
+        // transfer funds to owner if any
+        if (!OWNER.send(this.balance)) throw;
+    }
+
+    function abort() external
+    inStateBefore(State.REFUND_RUNNING)
+    onlyOwner
+    {
+        isAborted = true;
+    }
+
+    //displays current contract state in human readable form
+    function state()  external constant
+    returns (string)
+    {
+        return stateNames[ uint(currentState()) ];
+    }
+
+
+    //
+    // ======= implementation methods =======
+    //
+
+    function sendRefund() private tokenHoldersOnly {
+        // load balance to refund plus amount currently sent
+        var amount_to_refund = balances[msg.sender] + msg.value;
+        // reset balance
+        balances[msg.sender] = 0;
+        // send refund back to sender
+        if (!msg.sender.send(amount_to_refund)) throw;
+    }
+
+
+    function receiveFunds() private notTooSmallAmountOnly {
+      // no overflow is possible here: nobody have soo much money to spend.
+      if (total_received_amount + msg.value > MAX_TOTAL_AMOUNT_TO_RECEIVE) {
+          // accept amount only and return change
+          var change_to_return = total_received_amount + msg.value - MAX_TOTAL_AMOUNT_TO_RECEIVE;
+          if (!msg.sender.send(change_to_return)) throw;
+
+          var acceptable_remainder = MAX_TOTAL_AMOUNT_TO_RECEIVE - total_received_amount;
+          balances[msg.sender] += acceptable_remainder;
+          total_received_amount += acceptable_remainder;
+      } else {
+          // accept full amount
+          balances[msg.sender] += msg.value;
+          total_received_amount += msg.value;
+      }
+    }
+
+
+    function currentState() private constant returns (State) {
+        if (isAborted) {
+            return this.balance > 0 
+                   ? State.REFUND_RUNNING 
+                   : State.CLOSED;
+        } else if (block.number < PRESALE_START) {
+            return State.BEFORE_START;
+        } else if (block.number <= PRESALE_END && total_received_amount < MAX_TOTAL_AMOUNT_TO_RECEIVE) {
+            return State.PRESALE_RUNNING;
+        } else if (this.balance == 0) {
+            return State.CLOSED;
+        } else if (block.number <= WITHDRAWAL_END && total_received_amount >= MIN_TOTAL_AMOUNT_TO_RECEIVE) {
+            return State.WITHDRAWAL_RUNNING;
+        } else {
+            return State.REFUND_RUNNING;
+        } 
+    }
+
+    //
+    // ============ modifiers ============
+    //
+
+    //fails if state dosn't match
+    modifier inState(State state) {
+        if (state != currentState()) throw;
+        _;
+    }
+
+    //fails if the current state is not before than the given one.
+    modifier inStateBefore(State state) {
+        if (currentState() >= state) throw;
+        _;
+    }
+
+    //fails if something in setup is looking weird
+    modifier validSetupOnly() {
+        if ( OWNER == 0x0 
+            || PRESALE_START == 0 
+            || PRESALE_END == 0 
+            || WITHDRAWAL_END ==0
+            || PRESALE_START <= block.number
+            || PRESALE_START >= PRESALE_END
+            || PRESALE_END   >= WITHDRAWAL_END
+            || MIN_TOTAL_AMOUNT_TO_RECEIVE > MAX_TOTAL_AMOUNT_TO_RECEIVE )
+                throw;
+        _;
+    }
+
+
+    //accepts calls from owner only
+    modifier onlyOwner(){
+        if (msg.sender != OWNER)  throw;
+        _;
+    }
+
+
+    //accepts calls from token holders only
+    modifier tokenHoldersOnly(){
+        if (balances[msg.sender] == 0) throw;
+        _;
+    }
+
+
+    // don`t accept transactions with value less than allowed minimum
+    modifier notTooSmallAmountOnly(){	
+        if (msg.value < MIN_ACCEPTED_AMOUNT) throw;
+        _;
+    }
+
+
+    //prevents reentrancy attacs
+    bool private locked = false;
+    modifier noReentrancy() {
+        if (locked) throw;
+        locked = true;
+        _;
+        locked = false;
+    }
+}//contract
