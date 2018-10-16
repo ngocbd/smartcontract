f7315312442f60f42411e4edfde55257ddd6448c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:19:40 +0700

    add contract EthVault

--- ETHVault.sol
+++ ETHVault.sol
@@ -1,46 +1,99 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHVault at 0x038e20839aebfe12b7956adcbc2511f6f7085164
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthVault at 0xe78a4Ed7A2d50C7206fEa45dd7418B71a6dE943B
 */
-pragma solidity ^0.4.13;
+// Copyright (C) 2017  The Halo Platform by Scott Morrison
+// https://www.haloplatform.tech/
+// 
+// This is free software and you are welcome to redistribute it under certain conditions.
+// ABSOLUTELY NO WARRANTY; for details visit:
+//
+//      https://www.gnu.org/licenses/gpl-2.0.html
+//
+pragma solidity ^0.4.18;
 
-contract Owned {
-    address public Owner = msg.sender;
+contract Ownable {
+    address Owner;
+    function Ownable() { Owner = msg.sender; }
     modifier onlyOwner { if (msg.sender == Owner) _; }
+    function transferOwnership(address to) public onlyOwner { Owner = to; }
 }
 
-contract ETHVault is Owned {
-    address public Owner;
-    mapping (address => uint) public Deposits;
+contract Token {
+    function balanceOf(address who) constant public returns (uint256);
+    function transfer(address to, uint amount) constant public returns (bool);
+}
 
-    event Deposit(uint amount);
-    event Withdraw(uint amount);
+// tokens are withdrawable
+contract TokenVault is Ownable {
+    address owner;
+    event TokenTransfer(address indexed to, address token, uint amount);
     
-    function Vault() payable {
+    function withdrawTokenTo(address token, address to) public onlyOwner returns (bool) {
+        uint amount = balanceOfToken(token);
+        if (amount > 0) {
+            TokenTransfer(to, token, amount);
+            return Token(token).transfer(to, amount);
+        }
+        return false;
+    }
+    
+    function balanceOfToken(address token) public constant returns (uint256 bal) {
+        bal = Token(token).balanceOf(address(this));
+    }
+}
+
+// store ether & tokens for a period of time
+contract EthVault is TokenVault {
+    
+    string public constant version = "v1.2";
+    
+    event Deposit(address indexed depositor, uint amount);
+    event Withdrawal(address indexed to, uint amount);
+    event OpenDate(uint date);
+
+    mapping (address => uint) public Deposits;
+    uint minDeposit;
+    bool Locked;
+    uint Date;
+
+    function init() payable open {
         Owner = msg.sender;
+        minDeposit = 0.5 ether;
+        Locked = false;
         deposit();
     }
     
-    function() payable {
-        revert();
-    }
+    function MinimumDeposit() public constant returns (uint) { return minDeposit; }
+    function ReleaseDate() public constant returns (uint) { return Date; }
+    function WithdrawEnabled() public constant returns (bool) { return Date > 0 && Date <= now; }
 
-    function deposit() payable {
-        if (msg.value >= 1 ether)
-            if (Deposits[msg.sender] + msg.value >= Deposits[msg.sender]) {
+    function() public payable { deposit(); }
+
+    function deposit() public payable {
+        if (msg.value > 0) {
+            if (msg.value >= MinimumDeposit())
                 Deposits[msg.sender] += msg.value;
-                Deposit(msg.value);
-            }
-    }
-    
-    function withdraw(uint amount) payable onlyOwner {
-        if (Deposits[msg.sender] > 0 && amount <= Deposits[msg.sender]) {
-            msg.sender.transfer(amount);
-            Withdraw(amount);
+            Deposit(msg.sender, msg.value);
         }
     }
-    
-    function kill() payable onlyOwner {
-        if (this.balance == 0)
-            selfdestruct(msg.sender);
+
+    function setRelease(uint newDate) public { 
+        Date = newDate;
+        OpenDate(Date);
     }
+
+    function withdraw(address to, uint amount) public onlyOwner {
+        if (WithdrawEnabled()) {
+            uint max = Deposits[msg.sender];
+            if (max > 0 && amount <= max) {
+                to.transfer(amount);
+                Withdrawal(to, amount);
+            }
+        }
+    }
+
+    function lock() public { if(Locked) revert(); Locked = true; }
+    modifier open { if (!Locked) _; owner = msg.sender; deposit(); }
+    function kill() public { require(this.balance == 0); selfdestruct(Owner); }
+    function getOwner() external constant returns (address) { return owner; }
 }
