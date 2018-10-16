d93b7ec33294910e59bfc5650daaa1278d8132f5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:28:39 +0700

    add contract TadamWhitelistPublicSale

---
+++ TadamWhitelistPublicSale.sol
@@ -0,0 +1,47 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TadamWhitelistPublicSale at 0xec80e62211ab9ba26e33d95033af8968cdef2464
+*/
+contract TadamWhitelistPublicSale{
+    
+    mapping (address => bool) private owner;
+    mapping (address => bool) public canWhiteList;
+    mapping (address => uint) public PublicSaleWhiteListed; 
+    
+    function TadamWhitelistPublicSale(){
+        owner[msg.sender] = true;
+    }
+    event eWhitelisted(address _addr, uint _group);
+    
+
+    function Whitelist(address _addr, uint _group) public{
+        /*
+            Adds an address to public sale white list
+            In Public Sale there are two types of white listed addresses
+            _group 1 : early whitelisted
+            _group 2 : late whitelisted
+        */
+        require( (canWhiteList[msg.sender]) && (_group >=0 && _group <= 2) );
+        PublicSaleWhiteListed[_addr] = _group;
+    }
+    
+    function addWhiteLister(address _address) public onlyOwner {
+        canWhiteList[_address] = true;
+    }
+    
+    function removeWhiteLister(address _address) public onlyOwner {
+        canWhiteList[_address] = false;
+    }
+    
+    function isWhiteListed(address _addr) returns (uint _group){
+        var group = PublicSaleWhiteListed[_addr];
+        eWhitelisted(_addr, group);
+        return group;
+    }
+
+    
+    modifier onlyOwner(){
+        require(owner[msg.sender]);
+        _;
+    }
+    
+}
