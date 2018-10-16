3ffc8d201e2b1ce9b22d989fc7a6217f44dc1aa5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:55:50 +0700

    add contract Airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,95 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x3c5bed7eb1481e706fc7f564375211cc5de7a958
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0xce9a5619b3bfb2c11cdffcc15843eab11f6c8280
 */
-pragma solidity ^0.4.16;
-
-contract Ownable {
-
-  address public owner;
-
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));
-    owner = newOwner;
-  }
+contract ERC20 {
+  function transfer(address _recipient, uint256 _value) public returns (bool success);
 }
 
-interface Token {
-  function transfer(address _to, uint256 _value) returns (bool);
-  function balanceOf(address _owner) constant returns (uint256 balance);
-}
-
-contract AirDrop is Ownable {
-
-  Token token;
-
-  event TransferredToken(address indexed to, uint256 value);
-  event FailedTransfer(address indexed to, uint256 value);
-
-  modifier whenDropIsActive() {
-    assert(isActive());
-
-    _;
-  }
-
-  function AirDrop () {
-      address _tokenAddr = 0xCEb99b21d2C9CB017d4fE97E48962A89E579b744;
-      token = Token(_tokenAddr);
-  }
-
-  function isActive() constant returns (bool) {
-    return (
-        tokensAvailable() > 0 // Tokens must be available to send
-    );
-  }
-  //below function can be used when you want to send every recipeint with different number of tokens
-  function sendTokens(address[] dests, uint256[] values) whenDropIsActive onlyOwner external {
-    uint256 i = 0;
-    while (i < dests.length) {
-        uint256 toSend = values[i] * 10**18;
-        sendInternally(dests[i] , toSend, values[i]);
-        i++;
+contract Airdrop {
+  function drop(ERC20 token, address[] recipients, uint256[] values) public {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      token.transfer(recipients[i], values[i]);
     }
-  }
-
-  // this function can be used when you want to send same number of tokens to all the recipients
-  function sendTokensSingleValue(address[] dests, uint256 value) whenDropIsActive onlyOwner external {
-    uint256 i = 0;
-    uint256 toSend = value * 10**18;
-    while (i < dests.length) {
-        sendInternally(dests[i] , toSend, value);
-        i++;
-    }
-  }  
-
-  function sendInternally(address recipient, uint256 tokensToSend, uint256 valueToPresent) internal {
-    if(recipient == address(0)) return;
-
-    if(tokensAvailable() >= tokensToSend) {
-      token.transfer(recipient, tokensToSend);
-      TransferredToken(recipient, valueToPresent);
-    } else {
-      FailedTransfer(recipient, valueToPresent); 
-    }
-  }   
-
-
-  function tokensAvailable() constant returns (uint256) {
-    return token.balanceOf(this);
-  }
-
-  function destroy() onlyOwner {
-    uint256 balance = tokensAvailable();
-    require (balance > 0);
-    token.transfer(owner, balance);
-    selfdestruct(owner);
   }
 }
