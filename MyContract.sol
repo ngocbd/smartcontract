555e08f1493d13db013024bea936c574ccbe55ca
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:24 +0700

    add contract MyContract

---
+++ MyContract.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyContract at 0x7f6d05523908a9688eab8ab8bbc7842e3d453bfb
+*/
+contract MyContract {
+    /* Constructor */
+   
+ function MyContract() {
+var co23actline1 = "2";
+var str2 = "3";
+
+var contractline2="1.1.   Both the Site and the Platform use cookie files. These Cookie Regulations supplement the Site Rules and Regulations and the Rules and Regulations of the Platform.";
+
+    }
+}
