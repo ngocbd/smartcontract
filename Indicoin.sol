68cf2d46046ba3a97ea444e797b44e938cc0e45c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:10:55 +0700

    add contract Indicoin

--- Indicoin.sol
+++ Indicoin.sol
@@ -1,186 +1,190 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Indicoin at 0xa6eccbefa6969afb8d35317d3e8c13f3867872fc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Indicoin at 0x0ffAB58EA5A71CC3cA40217706C3C401407fA4a8
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.11;
 
-/* taking ideas from FirstBlood token */
-contract SafeMath {
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    /* function assert(bool assertion) internal { */
-    /*   if (!assertion) { */
-    /*     throw; */
-    /*   } */
-    /* }      // assert no longer needed once solidity is on 0.4.10 */
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-    function safeAdd(uint256 x, uint256 y) internal returns(uint256) {
-      uint256 z = x + y;
-      assert((z >= x) && (z >= y));
-      return z;
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
+
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
+
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) returns (bool) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifing the amount of tokens still available for the spender.
+   * also adding burn function and event.
+   */
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+  event Burn(address indexed burner, uint256 value);
+  function burn(uint256 _value) public {
+        require(_value > 0);
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
     }
 
-    function safeSubtract(uint256 x, uint256 y) internal returns(uint256) {
-      assert(x >= y);
-      uint256 z = x - y;
-      return z;
-    }
-
-    function safeMult(uint256 x, uint256 y) internal returns(uint256) {
-      uint256 z = x * y;
-      assert((x == 0)||(z/x == y));
-      return z;
-    }
 
 }
 
 
-contract Token {
-    uint256 public totalSupply;
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `StandardToken` functions.
+ */
+contract Indicoin is StandardToken {
 
+  string public constant name = "Indicoin";
+  string public constant symbol = "INDI";
+  uint256 public constant decimals = 18;
 
-/*  ERC 20 token */
-contract StandardToken is Token {
+  uint256 public constant INITIAL_SUPPLY = 1000000000 * 10**18;
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-      if (balances[msg.sender] >= _value && _value > 0) {
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-}
-contract Indicoin is StandardToken, SafeMath {
-
-    // metadata
-    string public constant name = "Indicoin";
-    string public constant symbol = "INDI";
-    uint256 public constant decimals = 18;
-    string public version = "1.0";
-
-    // contracts
-    address public ethFundDeposit;      // deposit address for ETH for Indicoin Developers
-    address public indiFundAndSocialVaultDeposit;      // deposit address for indicoin developers use and social vault 
-    address public bountyDeposit; // deposit address for bounty
-    address public saleDeposit; //deposit address for preSale
-    // crowdsale parameters
-    bool public isFinalized;              // switched to true in operational state
-    uint256 public fundingStartTime;
-    uint256 public fundingEndTime;
-    uint256 public constant indiFundAndSocialVault = 350 * (10**6) * 10**decimals;   // 100m INDI reserved for team use and 250m for social vault
-    uint256 public constant bounty = 50 * (10**6) * 10**decimals; // 50m INDI reserved for bounty
-    uint256 public constant sale = 200 * (10**6) * 10**decimals; 
-    uint256 public constant tokenExchangeRate = 12500; // 12500 INDI tokens per 1 ETH
-    uint256 public constant tokenCreationCap =  1000 * (10**6) * 10**decimals;
-    uint256 public constant tokenCreationMin =  600 * (10**6) * 10**decimals;
-
-
-    // events
-    event LogRefund(address indexed _to, uint256 _value);
-    event CreateINDI(address indexed _to, uint256 _value);
-    
-
-    
-    function Indicoin()
-    {
-      isFinalized = false;                   //controls pre through crowdsale state
-      ethFundDeposit = 0xe16927243587d3293574235314D96B3501fC00b7;
-      indiFundAndSocialVaultDeposit = 0xF83EA33530027A4Fd7F37629E18508E124DFB99D;
-      saleDeposit = 0xC1E5214983d18b80c9Cdd5d2edAC40B7d8ddfCB9;
-      bountyDeposit = 0xB41A19abF814375D89222834aeE3FB264e4b5e77;
-      fundingStartTime = 1507309861;
-      fundingEndTime = 1509580799;
-      
-      totalSupply = indiFundAndSocialVault + bounty + sale;
-      balances[indiFundAndSocialVaultDeposit] = indiFundAndSocialVault; // Deposit Indicoin developers share
-      balances[bountyDeposit] = bounty; //Deposit bounty Share
-      balances[saleDeposit] = sale; //Deposit preSale Share
-      CreateINDI(indiFundAndSocialVaultDeposit, indiFundAndSocialVault);  // logs indicoin developers fund
-      CreateINDI(bountyDeposit, bounty); // logs bounty fund
-      CreateINDI(saleDeposit, sale); // logs preSale fund
-    }
-    
-    
-    /// @dev Accepts ether and creates new INDI tokens.
-    function createTokens() payable external {
-      if (isFinalized) revert();
-      if (now < fundingStartTime) revert();
-      if (now > fundingEndTime) revert();
-      if (msg.value == 0) revert();
-
-      uint256 tokens = safeMult(msg.value, tokenExchangeRate); // check that we're not over totals
-      uint256 checkedSupply = safeAdd(totalSupply, tokens);
-
-      // return money if something goes wrong
-      if (tokenCreationCap < checkedSupply) revert();  // odd fractions won't be found
-
-      totalSupply = checkedSupply;
-      balances[msg.sender] += tokens;  // safeAdd not needed; bad semantics to use here
-      CreateINDI(msg.sender, tokens);  // logs token creation
-    }
-
-    /// @dev Ends the funding period and sends the ETH home
-    function finalize() external {
-      if (isFinalized) revert();
-      if (msg.sender != ethFundDeposit) revert(); // locks finalize to the ultimate ETH owner
-      if(totalSupply < tokenCreationMin) revert();      // have to sell minimum to move to operational
-      if(now <= fundingEndTime && totalSupply != tokenCreationCap) revert();
-      // move to operational
-      isFinalized = true;
-      if(!ethFundDeposit.send(this.balance)) revert();  // send the eth to Indicoin developers
-    }
-
-    /// @dev Allows contributors to recover their ether in the case of a failed funding campaign.
-    function refund() external {
-      if(isFinalized) revert();                       // prevents refund if operational
-      if (now <= fundingEndTime) revert(); // prevents refund until sale period is over
-      if(totalSupply >= tokenCreationMin) revert();  // no refunds if we sold enough
-      if(msg.sender == indiFundAndSocialVaultDeposit) revert();    // Indicoin developers not entitled to a refund
-      uint256 indiVal = balances[msg.sender];
-      if (indiVal == 0) revert();
-      balances[msg.sender] = 0;
-      totalSupply = safeSubtract(totalSupply, indiVal); // extra safe
-      uint256 ethVal = indiVal / tokenExchangeRate;     // should be safe; previous throws covers edges
-      LogRefund(msg.sender, ethVal);               // log it 
-      if (!msg.sender.send(ethVal)) revert();       // if you're using a contract; make sure it works with .send gas limits
-    }
+  /**
+   * @dev Contructor that gives msg.sender all of existing tokens.
+   */
+  function Indicoin() {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
 
 }
