ee1955c58f29da71f350b308669f8f9807f795ff
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:19:57 +0700

    add contract PiggyBank

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -1,92 +1,48 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PiggyBank at 0xbE181DA06D93867af0A3ffaDcEbe345Ec35daE4e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PiggyBank at 0x08573698b2b0aAF07F16395bE3D3530598c0d972
 */
-contract PiggyBank {
+pragma solidity ^0.4.11;
 
-  struct InvestorArray {
-      address etherAddress;
-      uint amount;
-  }
+contract PiggyBank
+{
+    address creator;
+    uint deposits;
+    uint unlockTime;
 
-  InvestorArray[] public investors;
-
-  uint public k = 0;
-  uint public fees;
-  uint public balance = 0;
-  address public owner;
-
-  // simple single-sig function modifier
-  modifier onlyowner { if (msg.sender == owner) _ }
-
-  // this function is executed at initialization and sets the owner of the contract
-  function PiggyBank() {
-    owner = msg.sender;
-  }
-
-  // fallback function - simple transactions trigger this
-  function() {
-    enter();
-  }
-  
-  function enter() {
-    if (msg.value < 50 finney) {
-        msg.sender.send(msg.value);
-        return;
+    /* Constructor */
+    function PiggyBank() public
+    {
+        creator = msg.sender;
+        deposits = 0;
+        unlockTime = now + 10 minutes;
     }
-	
-    uint amount=msg.value;
 
-
-    // add a new participant to array
-    uint total_inv = investors.length;
-    investors.length += 1;
-    investors[total_inv].etherAddress = msg.sender;
-    investors[total_inv].amount = amount;
-    
-    // collect fees and update contract balance
- 
-      fees = amount / 33;             // 3% Fee
-      balance += amount;               // balance update
-
-
-     if (fees != 0) 
-     {
-     	if(balance>fees)
-	{
-      	owner.send(fees);
-      	balance -= fees;                 //balance update
-	}
-     }
- 
-
-   // 4% interest distributed to the investors
-    uint transactionAmount;
-	
-    while (balance > investors[k].amount * 3/100 && k<total_inv)  //exit condition to avoid infinite loop
-    { 
-     
-     if(k%25==0 &&  balance > investors[k].amount * 9/100)
-     {
-      transactionAmount = investors[k].amount * 9/100;  
-      investors[k].etherAddress.send(transactionAmount);
-      balance -= investors[k].amount * 9/100;                      //balance update
-      }
-     else
-     {
-      transactionAmount = investors[k].amount *3/100;  
-      investors[k].etherAddress.send(transactionAmount);
-      balance -= investors[k].amount *3/100;                         //balance update
-      }
-      
-      k += 1;
+    function() payable
+    {
+        deposit();
     }
-    
-    //----------------end enter
-  }
 
+    function deposit() payable returns (uint)
+    {
+        if( msg.value > 0 )
+            deposits = deposits + 1;
 
+        return getNumberOfDeposits();
+    }
 
-  function setOwner(address new_owner) onlyowner {
-      owner = new_owner;
-  }
+    function getNumberOfDeposits() constant returns (uint)
+    {
+        return deposits;
+    }
+
+    function getUnlockTime() constant returns (uint)
+    {
+        return unlockTime;
+    }
+
+    function kill()
+    {
+        if( msg.sender == creator && now >= unlockTime )
+            selfdestruct(creator);
+    }
 }
