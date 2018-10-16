c2ea150914fefc5d3bf24560e3945188188965e6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:37:09 +0700

    add contract LRCMidTermHoldingContract

--- LRCMidTermHoldingContract.sol
+++ LRCMidTermHoldingContract.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LRCMidTermHoldingContract at 0x419cad2516763f39d21fe85d3e99ca32649d3ead
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LRCMidTermHoldingContract at 0x8b34fbc553db3462ad4909f2e5577cc22f63c216
 */
 /*
 
@@ -20,34 +20,43 @@
 */
 pragma solidity ^0.4.11;
 
+
 /**
- * Math operations with safety checks
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-  function mul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint a, uint b) internal returns (uint) {
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint c = a / b;
+    uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint a, uint b) internal returns (uint) {
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
     assert(c >= a);
     return c;
   }
+}
 
+/**
+ * @title Math
+ * @dev Assorted math operations
+ */
+
+library Math {
   function max64(uint64 a, uint64 b) internal constant returns (uint64) {
     return a >= b ? a : b;
   }
@@ -63,52 +72,54 @@ library SafeMath {
   function min256(uint256 a, uint256 b) internal constant returns (uint256) {
     return a < b ? a : b;
   }
-
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
-    }
-  }
 }
 
-contract Token {
+// Abstract contract for the full ERC 20 Token standard
+// https://github.com/ethereum/EIPs/issues/20
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint supply) {}
+contract Token {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint balance) {}
+    function balanceOf(address _owner) constant returns (uint256 balance);
 
-    /// @dev send `_value` token to `_to` from `msg.sender`
+    /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) returns (bool success);
 
-    /// @dev send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 
-    /// @dev `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
+    /// @param _value The amount of tokens to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) returns (bool success);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-    /// Event for a successful transfer.
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-
-    /// Event for a successful Approval.
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 
@@ -117,10 +128,26 @@ contract Token {
 /// For more information, please visit https://loopring.org.
 contract LRCMidTermHoldingContract {
     using SafeMath for uint;
+    using Math for uint;
+
+    // During the first 60 days of deployment, this contract opens for deposit of LRC
+    // in exchange of ETH.
+    uint public constant DEPOSIT_WINDOW                 = 60 days;
+
+    // For each address, its LRC can only be withdrawn between 180 and 270 days after LRC deposit,
+    // which means:
+    //    1) LRC are locked during the first 180 days,
+    //    2) LRC will be sold to the `owner` with the specified `RATE` 270 days after the deposit.
+    uint public constant WITHDRAWAL_DELAY               = 180 days;
+    uint public constant WITHDRAWAL_WINDOW              = 90  days;
+
+    uint public constant MAX_LRC_DEPOSIT_PER_ADDRESS    = 150000 ether; // = 20 ETH * 7500
+
+    // 7500 LRC for 1 ETH. This is the best token sale rate ever.
+    uint public constant RATE       = 7500;
 
     address public lrcTokenAddress  = 0x0;
     address public owner            = 0x0;
-    uint    public rate             = 7500; 
 
     // Some stats
     uint public lrcReceived         = 0;
@@ -128,32 +155,45 @@ contract LRCMidTermHoldingContract {
     uint public ethReceived         = 0;
     uint public ethSent             = 0;
 
-    mapping (address => uint) lrcBalances; // each user's lrc balance
-    
-    /* 
+    uint public depositStartTime    = 0;
+    uint public depositStopTime     = 0;
+
+    bool public closed              = false;
+
+    struct Record {
+        uint lrcAmount;
+        uint timestamp;
+    }
+
+    mapping (address => Record) records;
+
+    /*
      * EVENTS
      */
+    /// Emitted when program starts.
+    event Started(uint _time);
+
     /// Emitted for each sucuessful deposit.
     uint public depositId = 0;
-    event Deposit(uint _depositId, address _addr, uint _ethAmount, uint _lrcAmount);
+    event Deposit(uint _depositId, address indexed _addr, uint _ethAmount, uint _lrcAmount);
 
     /// Emitted for each sucuessful withdrawal.
     uint public withdrawId = 0;
-    event Withdrawal(uint _withdrawId, address _addr, uint _ethAmount, uint _lrcAmount);
+    event Withdrawal(uint _withdrawId, address indexed _addr, uint _ethAmount, uint _lrcAmount);
 
-    /// Emitted when ETH are drained and LRC are drained by owner.
-    event Drained(uint _ethAmount, uint _lrcAmount);
+    /// Emitted when this contract is closed.
+    event Closed(uint _ethAmount, uint _lrcAmount);
 
-    /// Emitted when rate changed by owner.
-    event RateChanged(uint _oldRate, uint _newRate);
+    /// Emitted when ETH are drained.
+    event Drained(uint _ethAmount);
 
-    /// CONSTRUCTOR 
+    /// CONSTRUCTOR
     /// @dev Initialize and start the contract.
     /// @param _lrcTokenAddress LRC ERC20 token address
     /// @param _owner Owner of this contract
     function LRCMidTermHoldingContract(address _lrcTokenAddress, address _owner) {
-        require(_lrcTokenAddress != 0x0);
-        require(_owner != 0x0);
+        require(_lrcTokenAddress != address(0));
+        require(_owner != address(0));
 
         lrcTokenAddress = _lrcTokenAddress;
         owner = _owner;
@@ -162,108 +202,145 @@ contract LRCMidTermHoldingContract {
     /*
      * PUBLIC FUNCTIONS
      */
-    /// @dev Get back ETH to `owner`.
-    /// @param _rate New rate
-    function setRate(uint _rate) public  {
-        require(msg.sender == owner);
-        require(rate > 0);
-        
-        RateChanged(rate, _rate);
-        rate = _rate;
-    }
 
     /// @dev Get back ETH to `owner`.
-    /// @param _ethAmount Amount of ETH to drain back to owner
-    function drain(uint _ethAmount) public payable {
+    /// @param ethAmount Amount of ETH to drain back to owner
+    function drain(uint ethAmount) public payable {
+        require(!closed);
         require(msg.sender == owner);
-        require(_ethAmount >= 0);
-        
-        uint ethAmount = _ethAmount.min256(this.balance);
-        if (ethAmount > 0){
-            require(owner.send(ethAmount));
+
+        uint amount = ethAmount.min256(this.balance);
+        require(amount > 0);
+        owner.transfer(amount);
+
+        Drained(amount);
+    }
+
+    /// @dev Set depositStartTime
+    function start() public {
+        require(msg.sender == owner);
+        require(depositStartTime == 0);
+
+        depositStartTime = now;
+        depositStopTime  = now + DEPOSIT_WINDOW;
+
+        Started(depositStartTime);
+    }
+
+    /// @dev Get all ETH and LRC back to `owner`.
+    function close() public payable {
+        require(!closed);
+        require(msg.sender == owner);
+        require(now > depositStopTime + WITHDRAWAL_DELAY + WITHDRAWAL_WINDOW);
+
+        uint ethAmount = this.balance;
+        if (ethAmount > 0) {
+            owner.transfer(ethAmount);
         }
 
         var lrcToken = Token(lrcTokenAddress);
-        uint lrcAmount = lrcToken.balanceOf(address(this)) - lrcReceived + lrcSent;
-        if (lrcAmount > 0){
+        uint lrcAmount = lrcToken.balanceOf(address(this));
+        if (lrcAmount > 0) {
             require(lrcToken.transfer(owner, lrcAmount));
         }
 
-        Drained(ethAmount, lrcAmount);
+        closed = true;
+        Closed(ethAmount, lrcAmount);
     }
 
     /// @dev This default function allows simple usage.
     function () payable {
+        require(!closed);
+
         if (msg.sender != owner) {
-            if (msg.value == 0) depositLRC();
+            if (now <= depositStopTime) depositLRC();
             else withdrawLRC();
         }
     }
 
-  
+
     /// @dev Deposit LRC for ETH.
     /// If user send x ETH, this method will try to transfer `x * 100 * 6500` LRC from
     /// the user's address and send `x * 100` ETH to the user.
     function depositLRC() payable {
-        require(msg.sender != owner);
+        require(!closed && msg.sender != owner);
+        require(now <= depositStopTime);
         require(msg.value == 0);
 
+        var record = records[msg.sender];
         var lrcToken = Token(lrcTokenAddress);
 
-        uint lrcAmount = this.balance.mul(rate)
+        uint lrcAmount = this.balance.mul(RATE)
             .min256(lrcToken.balanceOf(msg.sender))
-            .min256(lrcToken.allowance(msg.sender, address(this)));
+            .min256(lrcToken.allowance(msg.sender, address(this)))
+            .min256(MAX_LRC_DEPOSIT_PER_ADDRESS - record.lrcAmount);
 
-        uint ethAmount = lrcAmount.div(rate);
+        uint ethAmount = lrcAmount.div(RATE);
+        lrcAmount = ethAmount.mul(RATE);
 
         require(lrcAmount > 0 && ethAmount > 0);
-        require(ethAmount.mul(rate) <= lrcAmount);
 
-        lrcBalances[msg.sender] += lrcAmount;
+        record.lrcAmount += lrcAmount;
+        record.timestamp = now;
+        records[msg.sender] = record;
 
         lrcReceived += lrcAmount;
         ethSent += ethAmount;
 
-        require(lrcToken.transferFrom(msg.sender, address(this), lrcAmount));
-        require(msg.sender.send(ethAmount));
 
         Deposit(
-             depositId++,
-             msg.sender,
-             ethAmount,
-             lrcAmount
-        );      
+                depositId++,
+                msg.sender,
+                ethAmount,
+                lrcAmount
+                );
+        require(lrcToken.transferFrom(msg.sender, address(this), lrcAmount));
+        msg.sender.transfer(ethAmount);
     }
 
     /// @dev Withdrawal LRC with ETH transfer.
     function withdrawLRC() payable {
-        require(msg.sender != owner);
+        require(!closed && msg.sender != owner);
+        require(now > depositStopTime);
         require(msg.value > 0);
 
-        uint lrcAmount = msg.value.mul(rate)
-            .min256(lrcBalances[msg.sender]);
+        var record = records[msg.sender];
+        require(now >= record.timestamp + WITHDRAWAL_DELAY);
+        require(now <= record.timestamp + WITHDRAWAL_DELAY + WITHDRAWAL_WINDOW);
 
-        uint ethAmount = lrcAmount.div(rate);
+        uint ethAmount = msg.value.min256(record.lrcAmount.div(RATE));
+        uint lrcAmount = ethAmount.mul(RATE);
 
-        require(lrcAmount > 0 && ethAmount > 0);
-
-        lrcBalances[msg.sender] -= lrcAmount;
+        record.lrcAmount -= lrcAmount;
+        if (record.lrcAmount == 0) {
+            delete records[msg.sender];
+        } else {
+            records[msg.sender] = record;
+        }
 
         lrcSent += lrcAmount;
         ethReceived += ethAmount;
+
+        Withdrawal(
+                   withdrawId++,
+                   msg.sender,
+                   ethAmount,
+                   lrcAmount
+                   );
 
         require(Token(lrcTokenAddress).transfer(msg.sender, lrcAmount));
 
         uint ethRefund = msg.value - ethAmount;
         if (ethRefund > 0) {
-            require(msg.sender.send(ethRefund));
+            msg.sender.transfer(ethRefund);
         }
+    }
 
-        Withdrawal(
-             withdrawId++,
-             msg.sender,
-             ethAmount,
-             lrcAmount
-        ); 
+    function getLRCAmount(address addr) public constant returns (uint) {
+        return records[addr].lrcAmount;
+    }
+
+    function getTimestamp(address addr) public constant returns (uint) {
+        return records[addr].timestamp;
     }
 }
