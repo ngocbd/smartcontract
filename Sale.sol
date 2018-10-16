c5f48a51fe5aa96142f87ecadf5731f4601bad46
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:43:37 +0700

    add contract Sale

--- Sale.sol
+++ Sale.sol
@@ -1,50 +1,22 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x866eb2108eff3437fe373f6bfff0856910c4e8f7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0xcf221c18372506565a5777be534f354a71e352c9
 */
 pragma solidity ^0.4.24;
 
-interface token {
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-}
-
 contract Sale {
-    address private maintoken = 0x2054a15c6822a722378d13c4e4ea85365e46e50b;
-    address private owner = 0xabc45921642cbe058555361490f49b6321ed6989;
-    address private owner8 = 0xAdCB24E52390924C0cA0de043c53de2149257f83;
-    address private owner6 = 0x8610a40e51454a5bbc6fc3d31874595d7b2cb8f0;                uint256 private sendtoken;
-    uint256 public cost1token = 0.0004 ether;
-    uint256 private ethersum;
-    uint256 private ethersum8;
-    uint256 private ethersum6;                token public tokenReward;
-    
+    address private owner80 = 0xf2b9DA535e8B8eF8aab29956823df7237f1863A3;
+    address private owner20 = 0x29FD9956553b9Ce92e658662b2F73d95CF90A969;
+    uint256 private ether80;
+    uint256 private ether20;
+
     function Sale() public {
-        tokenReward = token(maintoken);
+
     }
     
     function() external payable {
-        sendtoken = (msg.value)/cost1token;
-        if (msg.value >= 5 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*125/100;
-        }
-        if (msg.value >= 10 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*150/100;
-        }
-        if (msg.value >= 15 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*175/100;
-        }
-        if (msg.value >= 20 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*200/100;
-        }
-        tokenReward.transferFrom(owner, msg.sender, sendtoken);
-        
-        ethersum8 = (msg.value)*8/100;
-        ethersum6 = (msg.value)*6/100;    	    	    	    	
-    	    	ethersum = (msg.value)-ethersum8-ethersum6;    	    	    	        
-        owner8.transfer(ethersum8);
-        owner6.transfer(ethersum6);    	    	    	        owner.transfer(ethersum);
+        ether20 = (msg.value)/5;
+        ether80 = (msg.value)-ether20;
+        owner80.transfer(ether80);
+        owner20.transfer(ether20);
     }
 }
