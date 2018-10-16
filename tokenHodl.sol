98375d5e20a4fd540e716950de6f28655828272c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:40:05 +0700

    add contract tokenHodl

---
+++ tokenHodl.sol
@@ -0,0 +1,37 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract tokenHodl at 0x390365cef67dae207db323496d24f643dd3ae581
+*/
+pragma solidity ^0.4.13;
+
+contract ForeignToken {
+    function balanceOf(address _owner) constant returns (uint256);
+    function transfer(address _to, uint256 _value) returns (bool);
+}
+
+
+contract tokenHodl {
+    event Hodl(address indexed hodler, uint indexed amount);
+    event Party(address indexed hodler, uint indexed amount);
+    mapping (address => uint) public hodlers;
+    uint constant partyTime = 1543481761; // Nov 29, 2018
+    function() payable {
+        hodlers[msg.sender] += msg.value;
+        Hodl(msg.sender, msg.value);
+    }
+    function party() {
+        require (block.timestamp > partyTime && hodlers[msg.sender] > 0);
+        uint value = hodlers[msg.sender];
+        hodlers[msg.sender] = 0;
+        msg.sender.transfer(value);
+        Party(msg.sender, value);
+    }
+    function withdrawForeignTokens(address _tokenContract) returns (bool) {
+        if (msg.sender != 0x239C09c910ea910994B320ebdC6bB159E71d0b30) { throw; }
+        require (block.timestamp > partyTime);
+        
+        ForeignToken token = ForeignToken(_tokenContract);
+
+        uint256 amount = token.balanceOf(address(this));
+        return token.transfer(0x239C09c910ea910994B320ebdC6bB159E71d0b30, amount);
+    }
+}
