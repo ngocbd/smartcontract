9d14d8f30a42ee6c2f3292400db38111521db20d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:34:52 +0700

    add contract marketPredictor

---
+++ marketPredictor.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract marketPredictor at 0x46d00b8acef4019b3f2a36820ba47fa733ff8d93
+*/
+pragma solidity ^0.4.16;
+
+contract marketPredictor {
+    
+    string public predictMarket = "beetcoin go AAAWWUUUPP!!!";
+    
+}
