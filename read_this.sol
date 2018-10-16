6a4110d617e1da5c2f9221ae04da754befca9c3c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:03:02 +0700

    add contract read_this

---
+++ read_this.sol
@@ -0,0 +1,29 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract read_this at 0x422afe99da3660387fd40772a237d1e94816c453
+*/
+pragma solidity ^0.4.20;
+
+/*
+                             _       _____   _____       _         
+  _ __     ___   __      __ | |__   |___ /  |___ /      (_)   ___  
+ | '_ \   / _ \  \ \ /\ / / | '_ \    |_ \    |_ \      | |  / _ \ 
+ | |_) | | (_) |  \ V  V /  | | | |  ___) |  ___) |  _  | | | (_) |
+ | .__/   \___/    \_/\_/   |_| |_| |____/  |____/  (_) |_|  \___/ 
+ |_|                                                               
+
+* https://powh33.io 
+
+* ??? 25 ?? ??????????.??? ?? ????? ?????? ?????
+* ONLY 40 ETH in contract
+* Original code POWH, but with 33% DIVIDENDS
+
+* Active community: https://discordapp.com/invite/rcjDFYU
+* Smart contract: https://etherscan.io/token/0x748c6ed3f78f5f06ceb76834f195d3a05b7d628a
+* Twitter: https://twitter.com/POWH33_
+
+* No premined. Owner selled out.
+
+
+*/
+ contract read_this
+ {}
