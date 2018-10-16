b6f3cc39bf7c540a32fcb7fa4b42786c3ab9220c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:36:11 +0700

    add contract ProofOfExistence

--- ProofOfExistence.sol
+++ ProofOfExistence.sol
@@ -1,39 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0x7653d3e923e1b9a6f45de8ce718c12b6e62c3106
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0xfDdD9B2093664411Ec07FAD807A660E3ad9c6ff5
 */
-pragma solidity ^0.4.18;
+contract ProofOfExistence{
+    string public author1= "Davide Carboni";
+    string public author2= "Michele Nati";
+    string public title  = "A blockchain token for community engagement";
+    string public SHA256 = "d9c4208b29ec725280342e5e17baa74920fe142684986ab23f2ae64deced0e4d";
+    string public manual = "cat file.pdf | shasum -a 256";
 
-contract ProofOfExistence {
-
-  event ProofCreated(bytes32 documentHash, uint256 timestamp);
-
-  address public owner = msg.sender;
-
-  mapping (bytes32 => uint256) hashesById;
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  modifier noHashExistsYet(bytes32 documentHash) {
-    require(hashesById[documentHash] == 0);
-    _;
-  }
-
-  function ProofOfExistence() public {
-    owner = msg.sender;
-  }
-
-  function notarizeHash(bytes32 documentHash) onlyOwner public {
-    var timestamp = block.timestamp;
-    hashesById[documentHash] = timestamp;
-    ProofCreated(documentHash, timestamp);
-  }
-
-  function doesProofExist(bytes32 documentHash) public view returns (uint256) {
-    if (hashesById[documentHash] != 0) {
-      return hashesById[documentHash];
-    }
-  }
+    
 }
