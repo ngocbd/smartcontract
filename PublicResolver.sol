6f4c84d68875b4ca84dce9d0eeecbf9c8661a9c9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:01:49 +0700

    add contract PublicResolver

--- PublicResolver.sol
+++ PublicResolver.sol
@@ -1,16 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PublicResolver at 0x5ffc014343cd971b7eb70732021e26c35b744cc4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PublicResolver at 0xd784B7429ed0b2D0Ae9624bCFF1DE8D086f13Aa9
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
-contract AbstractENS {
-    function owner(bytes32 node) constant returns(address);
-    function resolver(bytes32 node) constant returns(address);
-    function ttl(bytes32 node) constant returns(uint64);
-    function setOwner(bytes32 node, address owner);
-    function setSubnodeOwner(bytes32 node, bytes32 label, address owner);
-    function setResolver(bytes32 node, address resolver);
-    function setTTL(bytes32 node, uint64 ttl);
+interface ENS {
 
     // Logged when the owner of a node assigns a new owner to a subnode.
     event NewOwner(bytes32 indexed node, bytes32 indexed label, address owner);
@@ -23,13 +16,24 @@ contract AbstractENS {
 
     // Logged when the TTL of a node changes
     event NewTTL(bytes32 indexed node, uint64 ttl);
+
+
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external returns (bytes32);
+    function setResolver(bytes32 node, address resolver) external;
+    function setOwner(bytes32 node, address owner) external;
+    function setTTL(bytes32 node, uint64 ttl) external;
+    function owner(bytes32 node) external view returns (address);
+    function resolver(bytes32 node) external view returns (address);
+    function ttl(bytes32 node) external view returns (uint64);
 }
+
 
 /**
  * A simple resolver anyone can use; only allows the owner of a node to set its
  * address.
  */
 contract PublicResolver {
+
     bytes4 constant INTERFACE_META_ID = 0x01ffc9a7;
     bytes4 constant ADDR_INTERFACE_ID = 0x3b3b57de;
     bytes4 constant CONTENT_INTERFACE_ID = 0xd8389dc5;
@@ -43,7 +47,7 @@ contract PublicResolver {
     event NameChanged(bytes32 indexed node, string name);
     event ABIChanged(bytes32 indexed node, uint256 indexed contentType);
     event PubkeyChanged(bytes32 indexed node, bytes32 x, bytes32 y);
-    event TextChanged(bytes32 indexed node, string indexed indexedKey, string key);
+    event TextChanged(bytes32 indexed node, string indexedKey, string key);
 
     struct PublicKey {
         bytes32 x;
@@ -59,11 +63,12 @@ contract PublicResolver {
         mapping(uint256=>bytes) abis;
     }
 
-    AbstractENS ens;
-    mapping(bytes32=>Record) records;
+    ENS ens;
+
+    mapping (bytes32 => Record) records;
 
     modifier only_owner(bytes32 node) {
-        if(ens.owner(node) != msg.sender) throw;
+        require(ens.owner(node) == msg.sender);
         _;
     }
 
@@ -71,32 +76,8 @@ contract PublicResolver {
      * Constructor.
      * @param ensAddr The ENS registrar contract.
      */
-    function PublicResolver(AbstractENS ensAddr) {
+    function PublicResolver(ENS ensAddr) public {
         ens = ensAddr;
-    }
-
-    /**
-     * Returns true if the resolver implements the interface specified by the provided hash.
-     * @param interfaceID The ID of the interface to check for.
-     * @return True if the contract implements the requested interface.
-     */
-    function supportsInterface(bytes4 interfaceID) constant returns (bool) {
-        return interfaceID == ADDR_INTERFACE_ID ||
-               interfaceID == CONTENT_INTERFACE_ID ||
-               interfaceID == NAME_INTERFACE_ID ||
-               interfaceID == ABI_INTERFACE_ID ||
-               interfaceID == PUBKEY_INTERFACE_ID ||
-               interfaceID == TEXT_INTERFACE_ID ||
-               interfaceID == INTERFACE_META_ID;
-    }
-
-    /**
-     * Returns the address associated with an ENS node.
-     * @param node The ENS node to query.
-     * @return The associated address.
-     */
-    function addr(bytes32 node) constant returns (address ret) {
-        ret = records[node].addr;
     }
 
     /**
@@ -105,20 +86,9 @@ contract PublicResolver {
      * @param node The node to update.
      * @param addr The address to set.
      */
-    function setAddr(bytes32 node, address addr) only_owner(node) {
+    function setAddr(bytes32 node, address addr) public only_owner(node) {
         records[node].addr = addr;
-        AddrChanged(node, addr);
-    }
-
-    /**
-     * Returns the content hash associated with an ENS node.
-     * Note that this resource type is not standardized, and will likely change
-     * in future to a resource type based on multihash.
-     * @param node The ENS node to query.
-     * @return The associated content hash.
-     */
-    function content(bytes32 node) constant returns (bytes32 ret) {
-        ret = records[node].content;
+        emit AddrChanged(node, addr);
     }
 
     /**
@@ -129,49 +99,20 @@ contract PublicResolver {
      * @param node The node to update.
      * @param hash The content hash to set
      */
-    function setContent(bytes32 node, bytes32 hash) only_owner(node) {
+    function setContent(bytes32 node, bytes32 hash) public only_owner(node) {
         records[node].content = hash;
-        ContentChanged(node, hash);
+        emit ContentChanged(node, hash);
     }
 
-    /**
-     * Returns the name associated with an ENS node, for reverse records.
-     * Defined in EIP181.
-     * @param node The ENS node to query.
-     * @return The associated name.
-     */
-    function name(bytes32 node) constant returns (string ret) {
-        ret = records[node].name;
-    }
-    
     /**
      * Sets the name associated with an ENS node, for reverse records.
      * May only be called by the owner of that node in the ENS registry.
      * @param node The node to update.
      * @param name The name to set.
      */
-    function setName(bytes32 node, string name) only_owner(node) {
+    function setName(bytes32 node, string name) public only_owner(node) {
         records[node].name = name;
-        NameChanged(node, name);
-    }
-
-    /**
-     * Returns the ABI associated with an ENS node.
-     * Defined in EIP205.
-     * @param node The ENS node to query
-     * @param contentTypes A bitwise OR of the ABI formats accepted by the caller.
-     * @return contentType The content type of the return value
-     * @return data The ABI data
-     */
-    function ABI(bytes32 node, uint256 contentTypes) constant returns (uint256 contentType, bytes data) {
-        var record = records[node];
-        for(contentType = 1; contentType <= contentTypes; contentType <<= 1) {
-            if((contentType & contentTypes) != 0 && record.abis[contentType].length > 0) {
-                data = record.abis[contentType];
-                return;
-            }
-        }
-        contentType = 0;
+        emit NameChanged(node, name);
     }
 
     /**
@@ -182,43 +123,23 @@ contract PublicResolver {
      * @param contentType The content type of the ABI
      * @param data The ABI data.
      */
-    function setABI(bytes32 node, uint256 contentType, bytes data) only_owner(node) {
+    function setABI(bytes32 node, uint256 contentType, bytes data) public only_owner(node) {
         // Content types must be powers of 2
-        if(((contentType - 1) & contentType) != 0) throw;
-        
+        require(((contentType - 1) & contentType) == 0);
+
         records[node].abis[contentType] = data;
-        ABIChanged(node, contentType);
+        emit ABIChanged(node, contentType);
     }
-    
-    /**
-     * Returns the SECP256k1 public key associated with an ENS node.
-     * Defined in EIP 619.
-     * @param node The ENS node to query
-     * @return x, y the X and Y coordinates of the curve point for the public key.
-     */
-    function pubkey(bytes32 node) constant returns (bytes32 x, bytes32 y) {
-        return (records[node].pubkey.x, records[node].pubkey.y);
-    }
-    
+
     /**
      * Sets the SECP256k1 public key associated with an ENS node.
      * @param node The ENS node to query
      * @param x the X coordinate of the curve point for the public key.
      * @param y the Y coordinate of the curve point for the public key.
      */
-    function setPubkey(bytes32 node, bytes32 x, bytes32 y) only_owner(node) {
+    function setPubkey(bytes32 node, bytes32 x, bytes32 y) public only_owner(node) {
         records[node].pubkey = PublicKey(x, y);
-        PubkeyChanged(node, x, y);
-    }
-
-    /**
-     * Returns the text data associated with an ENS node and key.
-     * @param node The ENS node to query.
-     * @param key The text data key to query.
-     * @return The associated text data.
-     */
-    function text(bytes32 node, string key) constant returns (string ret) {
-        ret = records[node].text[key];
+        emit PubkeyChanged(node, x, y);
     }
 
     /**
@@ -228,8 +149,92 @@ contract PublicResolver {
      * @param key The key to set.
      * @param value The text data value to set.
      */
-    function setText(bytes32 node, string key, string value) only_owner(node) {
+    function setText(bytes32 node, string key, string value) public only_owner(node) {
         records[node].text[key] = value;
-        TextChanged(node, key, key);
+        emit TextChanged(node, key, key);
+    }
+
+    /**
+     * Returns the text data associated with an ENS node and key.
+     * @param node The ENS node to query.
+     * @param key The text data key to query.
+     * @return The associated text data.
+     */
+    function text(bytes32 node, string key) public view returns (string) {
+        return records[node].text[key];
+    }
+
+    /**
+     * Returns the SECP256k1 public key associated with an ENS node.
+     * Defined in EIP 619.
+     * @param node The ENS node to query
+     * @return x, y the X and Y coordinates of the curve point for the public key.
+     */
+    function pubkey(bytes32 node) public view returns (bytes32 x, bytes32 y) {
+        return (records[node].pubkey.x, records[node].pubkey.y);
+    }
+
+    /**
+     * Returns the ABI associated with an ENS node.
+     * Defined in EIP205.
+     * @param node The ENS node to query
+     * @param contentTypes A bitwise OR of the ABI formats accepted by the caller.
+     * @return contentType The content type of the return value
+     * @return data The ABI data
+     */
+    function ABI(bytes32 node, uint256 contentTypes) public view returns (uint256 contentType, bytes data) {
+        Record storage record = records[node];
+        for (contentType = 1; contentType <= contentTypes; contentType <<= 1) {
+            if ((contentType & contentTypes) != 0 && record.abis[contentType].length > 0) {
+                data = record.abis[contentType];
+                return;
+            }
+        }
+        contentType = 0;
+    }
+
+    /**
+     * Returns the name associated with an ENS node, for reverse records.
+     * Defined in EIP181.
+     * @param node The ENS node to query.
+     * @return The associated name.
+     */
+    function name(bytes32 node) public view returns (string) {
+        return records[node].name;
+    }
+
+    /**
+     * Returns the content hash associated with an ENS node.
+     * Note that this resource type is not standardized, and will likely change
+     * in future to a resource type based on multihash.
+     * @param node The ENS node to query.
+     * @return The associated content hash.
+     */
+    function content(bytes32 node) public view returns (bytes32) {
+        return records[node].content;
+    }
+
+    /**
+     * Returns the address associated with an ENS node.
+     * @param node The ENS node to query.
+     * @return The associated address.
+     */
+    function addr(bytes32 node) public view returns (address) {
+        return records[node].addr;
+    }
+
+    /**
+     * Returns true if the resolver implements the interface specified by the provided hash.
+     * @param interfaceID The ID of the interface to check for.
+     * @return True if the contract implements the requested interface.
+     */
+    function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
+        return interfaceID == ADDR_INTERFACE_ID ||
+        interfaceID == CONTENT_INTERFACE_ID ||
+        interfaceID == NAME_INTERFACE_ID ||
+        interfaceID == ABI_INTERFACE_ID ||
+        interfaceID == PUBKEY_INTERFACE_ID ||
+        interfaceID == TEXT_INTERFACE_ID ||
+        interfaceID == INTERFACE_META_ID;
     }
 }
