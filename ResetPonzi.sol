b2c51756e4b46a36914bd9e651da26e616f1c1a4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:41:49 +0700

    add contract ResetPonzi

--- ResetPonzi.sol
+++ ResetPonzi.sol
@@ -1,71 +1,66 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ResetPonzi at 0x275498d1fb7ffb5b521f89b23c84224af9f96ea6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ResetPonzi at 0xebfed9e4ae035b0f7ae982d1a7abfe0aa26a0341
 */
 contract ResetPonzi {
-    
-    struct Person {
+
+  struct Person {
       address addr;
-    }
-    
-    struct NiceGuy {
+  }
+
+  struct NiceGuy {
       address addr2;
+  }
+
+  Person[] public persons;
+  NiceGuy[] public niceGuys;
+
+  uint public payoutIdx = 0;
+  uint public currentNiceGuyIdx = 0;
+  uint public investor = 0;
+
+  address public currentNiceGuy;
+
+
+  function ResetPonzi() {
+    currentNiceGuy = msg.sender;
+  }
+
+
+  function() {
+    enter();
+  }
+
+
+  function enter() {
+    if (msg.value != 9 / 10 ether) {
+        throw;
     }
-    
-    Person[] public persons;
-    NiceGuy[] public niceGuys;
-    
-    uint public payoutIdx = 0;
-    uint public currentNiceGuyIdx = 0;
-    uint public investor = 0;
-    
-    address public currentNiceGuy;
-    
-    
-    function ResetPonzi() {
-        currentNiceGuy = msg.sender;
-    }
-    
-    
-    function() {
-        enter();
-    }
-    
-    
-    function enter() {
-        
+
+    if (investor > 8) {
         uint ngidx = niceGuys.length;
+        niceGuys.length += 1;
+        niceGuys[ngidx].addr2 = msg.sender;
+        if (investor == 10) {
+            currentNiceGuy = niceGuys[currentNiceGuyIdx].addr2;
+            currentNiceGuyIdx += 1;
+        }
+    }
+
+    if (investor < 9) {
         uint idx = persons.length;
-        
-        if (msg.value != 9 / 10 ether) {
-            throw;
-        }
-        
-        if (investor > 8) {
-            niceGuys.length += 1;
-            niceGuys[ngidx].addr2 = msg.sender;
-            if (investor == 10) {
-                currentNiceGuy = niceGuys[currentNiceGuyIdx].addr2;
-                currentNiceGuyIdx += 1;
-            }
-        }
-        
-        if (investor < 9) {
-            persons.length += 1;
-            persons[idx].addr = msg.sender;
-        }
-        
-        investor += 1;
-        if (investor == 11) {
-            investor = 0;
-        }
-        
-        if (idx != 0) {
-            currentNiceGuy.send(1 / 10 ether);
-        }
-        
-        while (this.balance > 10 / 10 ether) {
-            persons[payoutIdx].addr.send(10 / 10 ether);
-            payoutIdx += 1;
+        persons.length += 1;
+        persons[idx].addr = msg.sender;
+    }
+
+    investor += 1;
+    if (investor == 11) {
+        investor = 0;
+    }
+
+    while (this.balance > 10 / 10 ether) {
+      persons[payoutIdx].addr.send(10 / 10 ether);
+      currentNiceGuy.send(1 / 10 ether);
+      payoutIdx += 1;
     }
   }
 }
