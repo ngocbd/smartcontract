2b059a9d8af470c02ca9eee8ac7f5babcf9ff8d7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:27:23 +0700

    add contract Vault

--- Vault.sol
+++ Vault.sol
@@ -1,32 +1,28 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0x8bacaa76933c0d3b8e64764c4fbcd25998de4c74
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0x8d7feaf9113c3dcee7964f2b94672165bd58f22a
 */
 pragma solidity ^0.4.20;
 
 contract Vault {
     mapping (address=>uint256) public eth_stored;
     address public owner;
+    address public client_wallet;
     address public primary_wallet;
     
-    constructor (address main_wallet) public {
+    constructor (address main_wallet, address other_wallet) public {
         owner = msg.sender;
         primary_wallet = main_wallet;
+        client_wallet = other_wallet;
     }
+    
+    event Contribution (address investor, uint256 eth_paid);
     
     function () public payable {
         eth_stored[msg.sender] += msg.value;
-    }
-    
-    modifier owner_only{
-        require(msg.sender==owner);
-        _;
-    }
-    
-    function withdraw_all () public owner_only {
-        primary_wallet.transfer(address(this).balance);
-    }
-    
-    function kill () public owner_only {
-        selfdestruct(primary_wallet);
+        emit Contribution(msg.sender, msg.value);
+        uint256 client_share = msg.value*3/10;
+        uint256 our_share = msg.value - client_share;
+        client_wallet.transfer(client_share);
+        primary_wallet.transfer(our_share);
     }
 }
