a4ecd2518452d29ec042b499e1367b1cba20bdf1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:06:43 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x4a6c5c683873eabdafa0d80b5f6ccd65865f3a17
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0xbcdc1db0f0cc80919833992ed2b87597a5effa0c
 */
 pragma solidity ^0.4.4;
 
@@ -108,12 +108,12 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function HashnodeTestCoin() {
-        balances[msg.sender] = 1000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "TrieuCoin";                                   // Set the name for display purposes (CHANGE THIS)
-        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "TRIEUCT";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        balances[msg.sender] = 1000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 1000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "Philadelphia Eagles, LII Champions";                                   // Set the name for display purposes (CHANGE THIS)
+        decimals = 0;                                               // Amount of decimals for display purposes (CHANGE THIS)
+        symbol = "PHL";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 200;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
 
