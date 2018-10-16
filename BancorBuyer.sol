0ae012aa6d66e92bf96cada64554fd5d42443125
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:11:17 +0700

    add contract BancorBuyer

--- BancorBuyer.sol
+++ BancorBuyer.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BancorBuyer at 0x6bd33d49d48f76abcd96652e5347e398aa3fda96
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BancorBuyer at 0xceffdb3f1aed635e94a0b565239d4353ae44c744
 */
 pragma solidity ^0.4.11;
 
@@ -18,21 +18,14 @@ contract ERC20 {
   function transfer(address _to, uint _value) returns (bool success);
 }
 
-// Interface to Bancor ICO Contract
-contract CrowdsaleController {
-  function contributeETH() payable returns (uint256 amount);
-}
-
 contract BancorBuyer {
   // Store the amount of ETH deposited or BNT owned by each account.
   mapping (address => uint) public balances;
-  // Reward for first to execute the buy.
-  uint public reward;
   // Track whether the contract has bought the tokens yet.
   bool public bought_tokens;
   // Record the time the contract bought the tokens.
   uint public time_bought;
-
+  
   // The Bancor Token Sale address.
   address sale = 0xBbc79794599b19274850492394004087cBf89710;
   // Bancor Smart Token Contract address.
@@ -51,29 +44,19 @@ contract BancorBuyer {
     msg.sender.transfer(amount);
   }
   
-  // Allow anyone to contribute to the buy executer's reward.
-  function add_reward() payable {
-    // Update reward value to include received amount.
-    reward += msg.value;
-  }
-  
-  // Buys tokens in the crowdsale and rewards the caller, callable by anyone.
+  // Buys tokens in the crowdsale, callable by anyone.
   function buy(){
+    // Transfer all funds to the Bancor crowdsale contract to buy tokens.
+    // Throws if the crowdsale hasn't started yet or has
+    // already completed, preventing loss of funds.
+    sale.transfer(this.balance);
     // Record that the contract has bought the tokens.
     bought_tokens = true;
     // Record the time the contract bought the tokens.
     time_bought = now;
-    // Transfer all the funds (less the caller reward) 
-    // to the Bancor crowdsale contract to buy tokens.
-    // Throws if the crowdsale hasn't started yet or has
-    // already completed, preventing loss of funds.
-    CrowdsaleController(sale).contributeETH.value(this.balance - reward)();
-    // Reward the caller for being the first to execute the buy.
-    msg.sender.transfer(reward);
   }
   
-  // A helper function for the default function, allowing contracts to interact.
-  function default_helper() payable {
+  function () payable {
     // Only allow deposits if the contract hasn't already purchased the tokens.
     if (!bought_tokens) {
       // Update records of deposited ETH to include the received amount.
@@ -97,9 +80,5 @@ contract BancorBuyer {
       // Refund any ETH sent after the contract has already purchased tokens.
       msg.sender.transfer(msg.value);
     }
-  }
-  
-  function () payable {
-    default_helper();
   }
 }
