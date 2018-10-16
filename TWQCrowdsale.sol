b9333cb541eb86ad9d8e29c73ade174a3671a5a2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:47:56 +0700

    add contract TWQCrowdsale

--- TWQCrowdsale.sol
+++ TWQCrowdsale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TWQCrowdsale at 0x9b6754b2fb288a6b6dff59436b1b98e35255a663
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TWQCrowdsale at 0xa7fa7588cd7ebf3d0e128f07da7406dd74e241df
 */
 pragma solidity ^0.4.16;
 
@@ -10,21 +10,24 @@ contract TWQCrowdsale {
     uint256 public token_price;
     mapping (address => uint256) public tokens_backed;
     address public contract_admin;
+    uint256 public start_block;
+    uint256 public end_block;
     
     event FundTransfer(address backer, uint256 amount_paid);
     event Withdrawal(address owner, uint256 total_amount);
-    event CrowdsaleStatus(bool active);
     
-    function TWQCrowdsale (address crowdsale_owner, uint256 set_limit, uint256 price) public {
+    function TWQCrowdsale (address crowdsale_owner, uint256 set_limit, uint256 price, uint256 time_limit) public {
         owner = crowdsale_owner;
         hard_limit = set_limit * 1 ether;
         token_price = price * 100 szabo;
         contract_admin = msg.sender;
+        start_block = block.number;
+        end_block = ((time_limit * 1 hours) / 15 seconds) + start_block;
     }
     
     function () public payable {
-        if (msg.value < 0.01 ether) revert();
-        if (msg.value + amount > hard_limit) revert();
+        if (msg.value < 0.01 ether || msg.value + amount > hard_limit) revert();
+        if (block.number < start_block || block.number > end_block) revert();
         FundTransfer(msg.sender, msg.value);
         amount += msg.value;
         tokens_backed[msg.sender] += msg.value / token_price;
@@ -42,6 +45,26 @@ contract TWQCrowdsale {
     }
     
     function add_hard_limit(uint256 additional_limit) authorized  public {
-        hard_limit += additional_limit * 1 ether;
+        hard_limit += additional_limit * 100 szabo;
+    }
+    
+    function change_start_block(uint256 new_block) authorized public {
+        start_block = new_block;
+    }
+    
+    function extend_end_block(uint256 end_time_period) authorized public {
+        end_block += ((end_time_period * 1 hours) / 15 seconds); 
+    }
+    
+    function shorten_end_block(uint256 end_time_period) authorized public {
+        end_block -= ((end_time_period * 1 hours) / 15 seconds);
+    }
+    
+    function set_end_block(uint256 block_number) authorized public {
+        end_block = block_number;
+    }
+    
+    function end_now() authorized public {
+        end_block = block.number;
     }
 }
