66d361200aa9458971e014d0912f126de6135b7f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:52:58 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,6 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xa89ca0d4ca6c993de3cf7db8f2e7dd8f0ef3ca92
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xffb75756314d9d05cd4c2da94fbdb356b09d455e
 */
+///Indica que el codigo fuente esta escrito para una version de Solidity 0.4.0 o superior.
 pragma solidity ^0.4.4;
 
 contract Token {
@@ -98,20 +99,32 @@ contract ERC20Token is StandardToken {
         throw;
     }
 
-    
-    string public name;              
-    uint8 public decimals;               
-    string public symbol;                 
-    string public version = 'H1.0';
+    /* Public variables of the token */
 
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+//
+// CHANGE THESE VALUES FOR YOUR TOKEN
+//
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 37000000000000000000000000;                
-        totalSupply = 37000000000000000000000000;                       
-        name = "Canada Coin";                                    
-        decimals = 18;                            
-        symbol = "CANC";                              
+        balances[msg.sender] = 40666999;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 40666999;                        // Update total supply (100000 for example)
+        name = "Scrotal Labs";                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "SLAB";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
