16c0da676fb126cbb2d6b511257322d86d154a94
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:42:16 +0700

    add contract PazhukovMaxInETH

---
+++ PazhukovMaxInETH.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PazhukovMaxInETH at 0xa90f15a8323ac98a80629e229ba47f944dfca658
+*/
+pragma solidity ^0.4.18;
+
+contract PazhukovMaxInETH {
+    
+    bytes32 info = "I'm smart contract from p5m.ru!";
+    
+    function getInfo() view public returns (bytes32) {
+        return (info);
+    }
+    
+}
