5b2b7db40ce9b32771d0060b6f5393cbf7206367
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:06:18 +0700

    add contract DaneelBurnTokens

---
+++ DaneelBurnTokens.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DaneelBurnTokens at 0xaa8b7b98a0e12cdda43c221eed7998fb51352442
+*/
+pragma solidity ^0.4.11;
+
+contract DaneelBurnTokens {
+
+}
