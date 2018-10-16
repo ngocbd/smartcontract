eb50483f1add9bb650fcc4c37bed23cec2bd5da2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:34:11 +0700

    add contract Datatrust

--- Datatrust.sol
+++ Datatrust.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Datatrust at 0x5cd38626b95abbe832eefb487df9de4ba75976c5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Datatrust at 0x5b36eb873b5e91c6405407b6d508f670a7c60d4b
 */
 pragma solidity ^0.4.23;
 
@@ -10,18 +10,15 @@ pragma solidity ^0.4.23;
  */
 contract Datatrust {
 
-    // Mapping from Merkle tree root hashes to their anchored state
-    mapping (bytes32 => bool) public anchors;
-
     // Event emitted when saving a new anchor
     event NewAnchor(bytes32 merkleRoot);
-    
+
     /**
      * @dev Save a new anchor for a given Merkle tree root hash
+     * @dev Use events as a form of storage
      * @param _merkleRoot bytes32 hash to anchor
      */
     function saveNewAnchor(bytes32 _merkleRoot) public {
-        anchors[_merkleRoot] = true;
         emit NewAnchor(_merkleRoot);
     }
 }
