0ffbf8b2d07c8f25b1ce6b1fc373761318534b90
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:05:24 +0700

    add contract NewToken

--- NewToken.sol
+++ NewToken.sol
@@ -1,69 +1,75 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NewToken at 0x7a3aa244cb5520cd40876c604953c269044f6599
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NewToken at 0x942e0f9072D5DdB737a6210F08187a1cb435c2dD
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.11;
 
-contract NewToken {
-	uint public totalSupply = 2300000000000000;
-	
-	string public name = "TTInvest";
-	uint8 public decimals = 8;
-	string public symbol = "TTInvest";
-	string public version = "1.0";
+contract ERC20Standard {
+ uint public totalSupply;
+ 
+ string public name;
+ uint8 public decimals;
+ string public symbol;
+ string public version;
+ 
+ mapping (address => uint256) balances;
+ mapping (address => mapping (address => uint)) allowed;
 
-function NewToken(){
-  balances[msg.sender] = 2300000000000000;
-}
+ //Fix for short address attack against ERC20
+ modifier onlyPayloadSize(uint size) {
+  assert(msg.data.length == size + 4);
+  _;
+ } 
 
-	mapping (address => uint256) balances;
-	mapping (address => mapping (address => uint)) allowed;
+ function balanceOf(address _owner) constant returns (uint balance) {
+  return balances[_owner];
+ }
 
-	//Fix for short address attack against ERC20
-	modifier onlyPayloadSize(uint size) {
-		assert(msg.data.length == size + 4);
-		_;
-	} 
-
-	function balanceOf(address _owner) constant returns (uint balance) {
-		return balances[_owner];
-	}
-
-	function transfer(address _recipient, uint _value) onlyPayloadSize(2*32) {
-		require(balances[msg.sender] >= _value && _value > 0);
-	    balances[msg.sender] -= _value;
-	    balances[_recipient] += _value;
-	    Transfer(msg.sender, _recipient, _value);        
+ function transfer(address _recipient, uint _value) onlyPayloadSize(2*32) {
+  require(balances[msg.sender] >= _value && _value > 0);
+     balances[msg.sender] -= _value;
+     balances[_recipient] += _value;
+     Transfer(msg.sender, _recipient, _value);        
     }
 
-	function transferFrom(address _from, address _to, uint _value) {
-		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+ function transferFrom(address _from, address _to, uint _value) {
+  require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
         balances[_to] += _value;
         balances[_from] -= _value;
         allowed[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
     }
 
-	function approve(address _spender, uint _value) {
-		allowed[msg.sender][_spender] = _value;
-		Approval(msg.sender, _spender, _value);
-	}
+ function approve(address _spender, uint _value) {
+  allowed[msg.sender][_spender] = _value;
+  Approval(msg.sender, _spender, _value);
+ }
 
-	function allowance(address _spender, address _owner) constant returns (uint balance) {
-		return allowed[_owner][_spender];
-	}
+ function allowance(address _spender, address _owner) constant returns (uint balance) {
+  return allowed[_owner][_spender];
+ }
 
-	//Event which is triggered to log all transfers to this contract's event log
-	event Transfer(
-		address indexed _from,
-		address indexed _to,
-		uint _value
-		);
-		
-	//Event which is triggered whenever an owner approves a new allowance for a spender.
-	event Approval(
-		address indexed _owner,
-		address indexed _spender,
-		uint _value
-		);
+ //Event which is triggered to log all transfers to this contract's event log
+ event Transfer(
+  address indexed _from,
+  address indexed _to,
+  uint _value
+  );
+  
+ //Event which is triggered whenever an owner approves a new allowance for a spender.
+ event Approval(
+  address indexed _owner,
+  address indexed _spender,
+  uint _value
+  );
 
+}
+contract NewToken is ERC20Standard {
+ function NewToken() {
+  totalSupply = 10000000000000000000000000000;
+  name = "Fext Coin";
+  decimals = 18;
+  symbol = "FEXT";
+  version = "1.0";
+  balances[msg.sender] = totalSupply;
+ }
 }
