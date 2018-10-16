afbdc6c5c12e0bbd3b547701ea0f4fd13266a24b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:32:01 +0700

    add contract DCoin

--- DCoin.sol
+++ DCoin.sol
@@ -1,140 +1,262 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DCoin at 0x36ce26cfec07b4c1ba5a350a57ab19a70bf7af91
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DCoin at 0x64391a1f01c49ae731e258b451afe387f6f57df4
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
 
-contract Token {
-
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a && c>=b);
+    return c;
+  }
 }
 
 
+// source : https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
-//name this contract whatever you'd like
-contract DCoin is StandardToken {
 
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
+contract DCoin is ERC20Interface {
+  using SafeMath for uint;
+
+  // State variables
+  string public name = "D'Coin";
+  string public symbol = 'DCO';
+  uint public decimals = 6;
+  address public owner;
+  uint public maxCoinCap = 200000000 * (10 ** 6);
+  uint public totalSupply;
+  bool public emergencyFreeze;
+  
+  // mappings
+  mapping (address => uint) balances;
+  mapping (address => mapping (address => uint) ) allowed;
+  mapping (address => bool) frozen;
+
+  // events
+  event Mint(address indexed _to, uint indexed _mintedAmount);
+  
+
+  // constructor
+  function DCoin () public {
+    owner = msg.sender;
+  }
+
+  // events
+  event OwnershipTransferred(address indexed _from, address indexed _to);
+  event Burn(address indexed from, uint256 amount);
+  event Freezed(address targetAddress, bool frozen);
+  event EmerygencyFreezed(bool emergencyFreezeStatus);
+  
+
+
+  // Modifiers
+  modifier onlyOwner {
+    require(msg.sender == owner);
+     _;
+  }
+
+  modifier unfreezed(address _account) { 
+    require(!frozen[_account]);
+    _;  
+  }
+  
+  modifier noEmergencyFreeze() { 
+    require(!emergencyFreeze);
+    _; 
+  }
+  
+
+
+  // functions
+
+  // ------------------------------------------------------------------------
+  // Transfer Token
+  // ------------------------------------------------------------------------
+  function transfer(address _to, uint _value) unfreezed(_to) unfreezed(msg.sender) noEmergencyFreeze() public returns (bool success) {
+    require(_to != 0x0);
+    require(balances[msg.sender] >= _value); 
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  // ------------------------------------------------------------------------
+  // Mint Token (Uncapped Minting)
+  // ------------------------------------------------------------------------
+  function mintToken (address _targetAddress, uint256 _mintedAmount) unfreezed(_targetAddress) noEmergencyFreeze() public onlyOwner returns(bool res) {
+    require(_targetAddress != 0x0); // use burn instead
+    require(_mintedAmount != 0);
+    require (totalSupply.add(_mintedAmount) <= maxCoinCap);
+    balances[_targetAddress] = balances[_targetAddress].add(_mintedAmount);
+    totalSupply = totalSupply.add(_mintedAmount);
+    emit Mint(_targetAddress, _mintedAmount);
+    emit Transfer(address(0), _targetAddress, _mintedAmount);
+    return true;
+  }
+
+  // ------------------------------------------------------------------------
+  // Approve others to spend on your behalf
+  // ------------------------------------------------------------------------
+  /* 
+    While changing approval, the allowed must be changed to 0 than then to updated value
+    The smart contract doesn't enforces this due to backward competibility but requires frontend to do the validations
+   */
+  function approve(address _spender, uint _value) unfreezed(_spender) unfreezed(msg.sender) noEmergencyFreeze() public returns (bool success) {
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  // ------------------------------------------------------------------------
+  // Approve and call : If approve returns true, it calls receiveApproval method of contract
+  // ------------------------------------------------------------------------
+  function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success)
+    {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
     }
 
-    /* Public variables of the token */
+  // ------------------------------------------------------------------------
+  // Transferred approved amount from other's account
+  // ------------------------------------------------------------------------
+  function transferFrom(address _from, address _to, uint _value) unfreezed(_to) unfreezed(_from) unfreezed(msg.sender) noEmergencyFreeze() public returns (bool success) {
+    require(_value <= allowed[_from][msg.sender]);
+    require (_value <= balances[_from]);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
 
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
+  // ------------------------------------------------------------------------
+  // Burn (Destroy tokens)
+  // ------------------------------------------------------------------------
+  function burn(uint256 _value) unfreezed(msg.sender) public returns (bool success) {
+    require(balances[msg.sender] >= _value);
+    balances[msg.sender] -= _value;
+    totalSupply -= _value;
+    emit Burn(msg.sender, _value);
+    return true;
+  }
 
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+  // ------------------------------------------------------------------------
+  //               ONLYOWNER METHODS                             
+  // ------------------------------------------------------------------------
 
-    function DCoin(
-        ) {
-        balances[msg.sender] = 1000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 1000000000;                        // Update total supply (100000 for example)
-        name = "dcoin";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "DCO";                               // Set the symbol for display purposes
-    }
 
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+  // ------------------------------------------------------------------------
+  // Transfer Ownership
+  // ------------------------------------------------------------------------
+  function transferOwnership(address _newOwner) public onlyOwner {
+    require(_newOwner != address(0));
+    owner = _newOwner;
+    emit OwnershipTransferred(owner, _newOwner);
+  }
 
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
-    }
+  // ------------------------------------------------------------------------
+  // Freeze account - onlyOwner
+  // ------------------------------------------------------------------------
+  function freezeAccount (address _target, bool _freeze) public onlyOwner returns(bool res) {
+    require(_target != 0x0);
+    frozen[_target] = _freeze;
+    emit Freezed(_target, _freeze);
+    return true;
+  }
+
+  // ------------------------------------------------------------------------
+  // Emerygency freeze - onlyOwner
+  // ------------------------------------------------------------------------
+  function emergencyFreezeAllAccounts (bool _freeze) public onlyOwner returns(bool res) {
+    emergencyFreeze = _freeze;
+    emit EmerygencyFreezed(_freeze);
+    return true;
+  }
+  
+
+  // ------------------------------------------------------------------------
+  //               CONSTANT METHODS
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  // Check Allowance : Constant
+  // ------------------------------------------------------------------------
+  function allowance(address _tokenOwner, address _spender) public constant returns (uint remaining) {
+    return allowed[_tokenOwner][_spender];
+  }
+
+  // ------------------------------------------------------------------------
+  // Check Balance : Constant
+  // ------------------------------------------------------------------------
+  function balanceOf(address _tokenOwner) public constant returns (uint balance) {
+    return balances[_tokenOwner];
+  }
+
+  // ------------------------------------------------------------------------
+  // Total supply : Constant
+  // ------------------------------------------------------------------------
+  function totalSupply() public constant returns (uint) {
+    return totalSupply;
+  }
+
+  // ------------------------------------------------------------------------
+  // Get Freeze Status : Constant
+  // ------------------------------------------------------------------------
+  function isFreezed(address _targetAddress) public constant returns (bool) {
+    return frozen[_targetAddress]; 
+  }
+
+
+
+  // ------------------------------------------------------------------------
+  // Prevents contract from accepting ETH
+  // ------------------------------------------------------------------------
+  function () public payable {
+    revert();
+  }
+
+  // ------------------------------------------------------------------------
+  // Owner can transfer out any accidentally sent ERC20 tokens
+  // ------------------------------------------------------------------------
+  function transferAnyERC20Token(address _tokenAddress, uint _value) public onlyOwner returns (bool success) {
+      return ERC20Interface(_tokenAddress).transfer(owner, _value);
+  }
 }
