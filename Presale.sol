50d9c32fae04a239d2e90c84ea8e36d6b5ebe2fb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:34 +0700

    add contract Presale

--- Presale.sol
+++ Presale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x1b27996479b11abc8a8d0447deb3fb0183788484
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x60f0ee673e413e1c134d1497eeecce8ddd2fdd74
 */
 pragma solidity ^0.4.6;
 
@@ -27,10 +27,9 @@ contract Presale {
 
 	/* ====== configuration START ====== */
 
-	uint public constant PRESALE_START  = 3051585;    /* approx. 23.01.2017 23:45 CET */
-	uint public constant PRESALE_END    = 3055605;    /* approx. 24.01.2017 16:30 CET */
-	uint public constant WITHDRAWAL_END = 3056325;    /* approx. 24.01.2017 19:30 CET */
-
+	uint public constant PRESALE_START  = 3044895;    /* approx. 22.01.2017 21:15 CET */
+	uint public constant PRESALE_END    = 3048975;    /* approx. 23.01.2017 14:15 CET */
+	uint public constant WITHDRAWAL_END = 3049935;    /* approx. 23.01.2017 18:15 CET */
 
 	address public constant OWNER = 0x45d5426471D12b21C3326dD0cF96f6656F7d14b1;
 	
