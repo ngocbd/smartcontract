4c5ffdf7d36e680dd600b6f62e900c01145f076f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:37:51 +0700

    add contract MyAdvancedToken

--- MyAdvancedToken.sol
+++ MyAdvancedToken.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyAdvancedToken at 0x9a6173C1A4739662dC81A7b47F457731a3a67578
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyAdvancedToken at 0xdfbb635d33d85ef5c906c1eb0ceed9b10b81691e
 */
-pragma solidity ^0.4.16; 
+pragma solidity ^0.4.16;
 
 contract owned {
     address public owner;
@@ -20,205 +20,145 @@ contract owned {
     }
 }
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; } 
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract Prete { 
-    // ?? ?? 
-    string public name; 
-    string public symbol; 
-    uint8 public decimals = 18; 
-    // 18?? ? ??? ???? ??? ?? ???? ???? ????. 
-    uint256 public totalSupply; 
+contract TokenERC20 {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 2;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-    // ?? ??? ?? ??? ?????. 
-    mapping (address => uint256) public balanceOf; 
-    mapping (address => mapping (address => uint256)) public allowance; 
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    // ??? ?????? ??????? ???? ?? ???? ????? 
-    event Transfer(address indexed from, address indexed to, uint256 value); 
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    // ??? ?? ????. 
-    event Burn(address indexed from, uint256 value); 
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
 
-    /** 
-     * ??? ?? 
-     * 
-     * ??? ????? ?? ?? ???? ??? ??????. 
-     */ 
-    function Prete( 
-        uint256 initialSupply, 
-        string tokenName, 
-        string tokenSymbol 
-    ) public { 
-        totalSupply = initialSupply * 1 ** uint256(decimals);   // ? ???? ??? ???????. 
-        balanceOf[msg.sender] = totalSupply;                    // ? ??? 
-        name = tokenName;                                       // ?? ?? 
-        symbol = tokenSymbol;                                   // ?? ?? (EX: BTC, ETH, LTC) 
-    } 
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function TokenERC20(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+    }
 
-    /** 
-     * ?? ??, ? ????? ??? ? ????. 
-     */ 
-    function _transfer(address _from, address _to, uint _value) internal { 
-        // Prevent transfer to 0x0 address. Use burn() instead 
-        require(_to != 0x0); 
-        // ??? ?? 
-        require(balanceOf[_from] >= _value); 
-        // ???? ?? 
-        require(balanceOf[_to] + _value > balanceOf[_to]); 
-        // ??? ??? ?? ??? ?????? 
-        uint previousBalances = balanceOf[_from] + balanceOf[_to]; 
-        // ????? ?? 
-        balanceOf[_from] -= _value; 
-        // ?? ???? ?? ?? ??????. 
-        balanceOf[_to] += _value; 
-        Transfer(_from, _to, _value); 
-        // ?? ??? ???? ???? ??? ?? ? ?????. ? ???? ???? ????. 
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances); 
-    } 
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
 
-    /** 
-     * ?? ?? 
-     * @ _to ?? ??? ??? ?? ?? ?? 
-     * @ _value ??? ??? ???. 
-     */ 
-    function transfer(address _to, uint256 _value) public { 
-        _transfer(msg.sender, _to, _value); 
-    } 
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
 
-    /** 
-     * _from  ?? ??? ?? 
-     * _to    ?? ??? ?? 
-     * _value ??? ?? 
-     */ 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) { 
-        require(_value <= allowance[_from][msg.sender]);     // ??? ?? 
-        allowance[_from][msg.sender] -= _value; 
-        _transfer(_from, _to, _value); 
-        return true; 
-    } 
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
 
-    /** 
-     * ?? ??? ?? ??? ?? 
-     * _spender ?? ? ??? ?? 
-     * _value   ??? ? ? ?? ?? ? ??? ?? ?? 
-     */ 
-    function approve(address _spender, uint256 _value) public 
-        returns (bool success) { 
-        allowance[msg.sender][_spender] = _value; 
-        return true; 
-    } 
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
 
-    /** 
-     * ?? ??? ?? ??? ?? ? ?? 
-     * @param _spender   ?? ? ??? ?? 
-     * @param _value     ??? ? ? ?? ?? ? ??? ?? ?? 
-     * @param _extraData ?? ? ???? ?? ?? ?? 
-     */ 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) 
-        public 
-        returns (bool success) { 
-        tokenRecipient spender = tokenRecipient(_spender); 
-        if (approve(_spender, _value)) { 
-            spender.receiveApproval(msg.sender, _value, this, _extraData); 
-            return true; 
-        } 
-    } 
-
-    /** 
-     * ?? ?? 
-     * @param _value ???? ?? 
-     */ 
-    function burn(uint256 _value) public returns (bool success) { 
-        require(balanceOf[msg.sender] >= _value);   // ?? ??? ??? ??? ??????. 
-        balanceOf[msg.sender] -= _value;            // ?????? ???. 
-        totalSupply -= _value;                      // ? ??? ???? 
-        Burn(msg.sender, _value); 
-        return true; 
-    } 
-
-  /** 
-     * ?? ???? ?? ?? 
-     * @param _from ??? ?? 
-     * @param _value ???? ?? 
-     */ 
-    function burnFrom(address _from, uint256 _value) public returns (bool success) { 
-        require(balanceOf[_from] >= _value);                // ?? ??? ??? ? ??????. 
-        require(_value <= allowance[_from][msg.sender]);    // ?? ?? 
-        balanceOf[_from] -= _value;                         // ?? ???? ?? 
-        allowance[_from][msg.sender] -= _value;             // ???? ????? ?? 
-        totalSupply -= _value;                              // ? ??? ???? 
-        Burn(_from, _value); 
-        return true; 
-    } 
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
+    }
 }
 
-contract MyAdvancedToken is owned, Prete {
+/******************************************/
+/*       ADVANCED TOKEN STARTS HERE       */
+/******************************************/
 
-    uint256 public sellPrice;
-    uint256 public buyPrice;
+contract MyAdvancedToken is owned, TokenERC20 {
 
-    mapping (address => bool) public frozenAccount;
-
-    /* ??? ?????? ??????? ???? ?? ???? ?????. */
-    event FrozenFunds(address target, bool frozen);
-
-    /* ??? ????? ?? ?? ???? ??? ??????. */
+    /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyAdvancedToken(
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
-    ) Prete(initialSupply, tokenName, tokenSymbol) public {}
+    ) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
 
-    /* ?? ??, ? ??? ???? ?? ? ? ?? */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // 0x0 ???? ??? ??????. ?? burn () ??
-        require (balanceOf[_from] >= _value);               // ?? ??? ??? ??? ??????.
-        require (balanceOf[_to] + _value > balanceOf[_to]); // ???? ??
-        require(!frozenAccount[_from]);                     // ???? ???? ??? ??????.
-        require(!frozenAccount[_to]);                       // ???? ???? ??? ??????.
-        balanceOf[_from] -= _value;                         // ?????? ???.
-        balanceOf[_to] += _value;                           // ?? ???? ?? ?? ??????.
-        Transfer(_from, _to, _value);
-    }
-
-    /// @notice `mintedAmount`?? ??? ???? `target` ? ????.
-    /// @param target ?? ?? ??
-    /// @param mintedAmount ?? ? ?? ?
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
         balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
         Transfer(0, this, mintedAmount);
         Transfer(this, target, mintedAmount);
-    }
-
-    /// @notice '????' | ??? ???? ??? ??? ??? ????.
-    /// @param target ??? ??
-    /// @param freeze ???? ?? ??
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
-    }
-
-    /// @notice ???? 'newBuyPrice`? ?? ??? ????'newSellPrice`? ?? ??? ????? ??????.
-    /// @param newSellPrice ???? ??? ?? ? ??? ??
-    /// @param newBuyPrice ???? ???? ??? ? ?? ??
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
-    }
-
-    /// @notice ????? ???? ???? ??? ?????.
-    function buy() payable public {
-        uint amount = msg.value / buyPrice;  // ??? ????.
-        _transfer(this, msg.sender, amount); // ??? ???.
-    }
-
-    /// @notice ???? ?? 'amount' ?? ??
-    /// @param amount ?? ? ??? ?
-    function sell(uint256 amount) public {
-        require(this.balance >= amount * sellPrice);      // ???? ??? ????? ??? ??
-        _transfer(msg.sender, this, amount);              // ??? ???.
-        msg.sender.transfer(amount * sellPrice);          // ????? ???? ???. ? ??? ??? ??? ?? ??? ???? ?? ?????.
     }
 }
