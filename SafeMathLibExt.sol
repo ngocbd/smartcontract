3ea9550aa09624f3ec71d4f4e3a06da8f0ad5e77
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:24:50 +0700

    add contract SafeMathLibExt

--- SafeMathLibExt.sol
+++ SafeMathLibExt.sol
@@ -1,19 +1,11 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0xa52FFD38918ac78601876Cb6ec3239E05039a2f9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0x58F03Dc391aa523eF6e940DDF328E123A1E419A3
 */
-// Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
-/**
- * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
- *
- * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
- */
-
 pragma solidity ^0.4.6;
 
 /**
  * Safe unsigned safe math.
  *
- * https://blog.aragon.one/library-driven-development-in-solidity-2bebcaf88736#.750gwtwli
  *
  * Originally from https://raw.githubusercontent.com/AragonOne/zeppelin-solidity/master/contracts/SafeMathLib.sol
  *
