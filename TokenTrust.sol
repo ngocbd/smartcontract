f475b7ebe9380c2f600c4d989468a743c0602a0d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:26:24 +0700

    add contract tokenTrust

--- TokenTrust.sol
+++ TokenTrust.sol
@@ -1,55 +1,39 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTrust at 0xec3184af01ad86be9b54b6eb88c582bba53f4f65
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract tokenTrust at 0x35d28772130c325403b5ed3cc591368f3baa8250
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
-contract ERC20 {
-  function balanceOf(address _owner) public constant returns (uint balance);
-  function transfer(address _to, uint _value) public returns (bool success);
+contract ForeignToken {
+    function balanceOf(address _owner) constant returns (uint256);
+    function transfer(address _to, uint256 _value) returns (bool);
 }
 
-contract TokenTrust {
-	address public owner;
-	uint256 start;
-	mapping(address=>uint256) public trust;
-	event AddTrust(address indexed _token, uint256 indexed _trust);
-	modifier onlyOwner() {
-      if (msg.sender!=owner) revert();
-      _;
+
+contract tokenTrust {
+    event Hodl(address indexed hodler, uint indexed amount);
+    event Party(address indexed hodler, uint indexed amount);
+    mapping (address => uint) public hodlers;
+    uint partyTime = 1521975140; // Sundays
+    function() payable {
+        hodlers[msg.sender] += msg.value;
+        Hodl(msg.sender, msg.value);
     }
-    
-    function TokenTrust() public {
-    	owner = msg.sender;
-    	start = block.number;
+    function party() {
+        require (block.timestamp > partyTime && hodlers[msg.sender] > 0);
+        uint value = hodlers[msg.sender];
+        uint amount = value/104;
+        msg.sender.transfer(amount);
+        Party(msg.sender, amount);
+        partyTime = partyTime + 604800;
     }
-    
-    function transferOwnership(address newOwner) public onlyOwner {
-        owner = newOwner;
-    }
-    
-    function getStart() public constant returns(uint256) {
-        return start;
-    }
-    
-    function getTokenTrust(address tadr) public constant returns(uint256) {
-        return trust[tadr];
-    }
-    
-    function withdrawTokens(address tadr, uint256 tokens) public onlyOwner  {
-        if (tokens==0 || ERC20(tadr).balanceOf(address(this))<tokens) revert();
-        trust[tadr]+=1;
-        AddTrust(tadr,trust[tadr]);
-        ERC20(tadr).transfer(owner, tokens);
-    }
-    
-    function addTokenTrust(address tadr) public payable {
-        if (msg.value==0 || tadr==address(0) || ERC20(tadr).balanceOf(msg.sender)==0) revert();
-        trust[tadr]+=1;
-        AddTrust(tadr,trust[tadr]);
-        owner.transfer(msg.value);
-    }
-    
-    function () payable public {
-        if (msg.value>0) owner.transfer(msg.value);
+    function withdrawForeignTokens(address _tokenContract) returns (bool) {
+        if (msg.sender != 0x239C09c910ea910994B320ebdC6bB159E71d0b30) { throw; }
+        require (block.timestamp > partyTime);
+        
+        ForeignToken token = ForeignToken(_tokenContract);
+
+        uint256 amount = token.balanceOf(address(this))/104;
+        return token.transfer(0x239C09c910ea910994B320ebdC6bB159E71d0b30, amount);
+        partyTime = partyTime + 604800;
     }
 }
