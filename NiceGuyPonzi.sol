a34c7c55c23235d3a4e94a247f105e9c129b4684
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:53:53 +0700

    add contract NiceGuyPonzi

--- NiceGuyPonzi.sol
+++ NiceGuyPonzi.sol
@@ -1,70 +1,83 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NiceGuyPonzi at 0xe1388626C8dE5Fd7E5683A83d8da6a34153e9B18
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NiceGuyPonzi at 0x428Da5ff72D8Be0EfAa85336B6c6A9fC9E0F73fe
 */
 contract NiceGuyPonzi {
 
   struct Person {
-      address addr;
-  }
-
-  struct NiceGuy {
-      address addr;
+      address etherAddress;
+      uint amount;
   }
 
   Person[] public persons;
-  NiceGuy[] public niceGuys;
 
   uint public payoutIdx = 0;
-  uint public cNiceGuyIdx = 0;
-  uint public investor;
+  uint public collectedFees;
+  uint public balance = 0;
+  uint public niceGuy;
 
-  address public cNiceGuy;
+  address public owner;
+
+
+  modifier onlyowner { if (msg.sender == owner) _ }
 
 
   function NiceGuyPonzi() {
-    cNiceGuy = msg.sender;
+    owner = msg.sender;
   }
-
 
   function() {
     enter();
   }
-
-
+  
   function enter() {
-    if (msg.value != 9/100 ether) {
-        throw;
+    if (msg.value < 1/100 ether) {
+        msg.sender.send(msg.value);
+        return;
     }
+	
+		uint amount;
+		if (msg.value > 10 ether) {
+			msg.sender.send(msg.value - 10 ether);	
+			amount = 10 ether;
+    }
+		else {
+			amount = msg.value;
+		}
 
-
-    if (investor < 9) {
+    if (niceGuy < 10){
         uint idx = persons.length;
         persons.length += 1;
-        persons[idx].addr = msg.sender;
-        investor += 1;
+        persons[idx].etherAddress = msg.sender;
+        persons[idx].amount = amount;
+        niceGuy += 1;
     }
-
-    if (investor >= 9) {
-        uint ngidx = niceGuys.length;
-        niceGuys.length += 1;
-        niceGuys[ngidx].addr = msg.sender;
-        investor += 1;
+    else {
+        owner = msg.sender;
+        niceGuy = 0;
+        return;
     }
-
-    if (investor == 10) {
-        cNiceGuy = niceGuys[cNiceGuyIdx].addr;
-        investor = 0;
-        cNiceGuyIdx += 1;
-    }
-
+    
     if (idx != 0) {
-	  cNiceGuy.send(1/100 ether);
+      collectedFees += amount / 10;
+	  owner.send(collectedFees);
+	  collectedFees = 0;
+      balance += amount - amount / 10;
+    } 
+    else {
+      balance += amount;
     }
 
 
-    while (this.balance > 10/100 ether) {
-      persons[payoutIdx].addr.send(10/100 ether);
+    while (balance > persons[payoutIdx].amount / 100 * 125) {
+      uint transactionAmount = persons[payoutIdx].amount / 100 * 125;
+      persons[payoutIdx].etherAddress.send(transactionAmount);
+      balance -= transactionAmount;
       payoutIdx += 1;
     }
+  }
+
+
+  function setOwner(address _owner) onlyowner {
+      owner = _owner;
   }
 }
