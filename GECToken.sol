39b8999add913c6e93075cf55f4e819bc25fca2e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:42:27 +0700

    add contract GECToken

--- GECToken.sol
+++ GECToken.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GECToken at 0x8413428357b3e3842666c36502c3ddef4eb3fc74
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GECToken at 0x4a536c1ce7ad7f6e8d2e59135e17aef5ef4dd4e6
 */
-pragma solidity 0.4.11;
+pragma solidity 0.4.15;
 
 contract ERC20 {
     function totalSupply() constant returns (uint256 totalSupply) {}
@@ -79,81 +79,15 @@ contract GECToken is StandardToken {
     string public symbol = "GEC";
     uint public decimals = 3;
 
-	//INITIALIZATION
-	address public minter; //address that able to mint new tokens
-	uint public icoEndTime; 
-
-	uint illiquidBalance_amount;
-	mapping (uint => address) illiquidBalance_index;
-	mapping (address => uint) public illiquidBalance; //Balance of 'Frozen funds'
-
-	// called by crowdsale contract
-	modifier only_minter {
-		if (msg.sender != minter) throw;
-		_;
-	}
-
-	// Token can be transferred immediately after crowdsale.
-	modifier when_transferable {
-		if (now <= icoEndTime) throw;
-		_;
-	}
-
-	// Can only be called if the `crowdfunder` is allowed to mint tokens. Any
-	// time before `endMintingTime`.
-	modifier when_mintable {
-		if (now > icoEndTime + 10 days) throw;
-		_;
-	}
-
 	// Initialization contract assigns address of crowdfund contract and end time.
-	function GECToken (address _minter, uint _icoEndTime) {
-		minter = _minter;
-		icoEndTime = _icoEndTime;
-	}
-
-	// Create new tokens when called by the crowdfund contract.
-	// Only callable before the end time.
-	function createToken(address _recipient, uint _value)
-		when_mintable
-		only_minter
-		returns (bool o_success)
-	{
-		balances[_recipient] += _value;
-		totalSupply += _value;
-		return true;
-	}
-
-	// Create an illiquidBalance which cannot be traded until admin make it liquid.
-	// Can only be called by crowdfund contract before the end time.
-	function createIlliquidToken(address _recipient, uint _value)
-		when_mintable
-		only_minter
-		returns (bool o_success)
-	{
-		illiquidBalance_index[illiquidBalance_amount] = _recipient;
-		illiquidBalance[_recipient] += _value;
-		illiquidBalance_amount++;
-
-		totalSupply += _value;
-		return true;
-	}
-
-	// Make sender's illiquid balance liquid when called after lockout period.
-	function makeLiquid()
-		only_minter
-	{
-		for (uint i=0; i<illiquidBalance_amount; i++) {
-			address investor = illiquidBalance_index[i];
-			balances[investor] += illiquidBalance[investor];
-			illiquidBalance[investor] = 0;
-		}
+	function GECToken (address _bank, uint _totalSupply) {
+		balances[_bank] += _totalSupply;
+		totalSupply += _totalSupply;
 	}
 
 	// Transfer amount of tokens from sender account to recipient.
 	// Only callable after the crowd fund end date.
 	function transfer(address _recipient, uint _amount)
-		when_transferable
 		returns (bool o_success)
 	{
 		return super.transfer(_recipient, _amount);
@@ -162,7 +96,6 @@ contract GECToken is StandardToken {
 	// Transfer amount of tokens from a specified address to a recipient.
 	// Only callable after the crowd fund end date.
 	function transferFrom(address _from, address _recipient, uint _amount)
-		when_transferable
 		returns (bool o_success)
 	{
 		return super.transferFrom(_from, _recipient, _amount);
