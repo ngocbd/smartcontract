442b1525fa58ab04dbbe3c8d407ded8fe3bb57b6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:09:39 +0700

    add contract COIN

--- Coin.sol
+++ Coin.sol
@@ -1,135 +1,316 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Coin at 0x6704f6b9c24502b30ba0c9406d22c8e1dda160b6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract COIN at 0x4c478ac32725872e30bca49957abc7dbb325cb21
 */
-pragma solidity ^0.4.18;
-
-/*
-     ?4xcoin.io
- 
-  4xCoin is the new disruptive tokenizing ledger aimed to bridge crypto space and $5 trillion Forex market via decentralized trustless platform.
-
-      ?2017 4xCoin
-*/
+pragma solidity ^0.4.13;
 
 library SafeMath {
-  function mul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
+    //?????????? ?????????.
+	//Safe multiplication.
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
-  function div(uint a, uint b) internal returns (uint) {
-    assert(b > 0);
-    uint c = a / b;
-    assert(a == b * c + a % b);
+	//?????????? ???????.
+	//Safe division.
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
-  function sub(uint a, uint b) internal returns (uint) {
+	//?????????? ?????????.
+	//Safe subtraction.
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
-  function add(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
+	//?????????? ????????.
+	//Safe addition.
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
     assert(c >= a);
     return c;
   }
-  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a >= b ? a : b;
+}
+
+contract Ownable {
+  
+  address public owner;
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
   }
-  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a < b ? a : b;
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
-  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a >= b ? a : b;
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));      
+    owner = newOwner;
   }
-  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a < b ? a : b;
-  }
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
+
+}
+
+contract COIN is Ownable {
+    
+    using SafeMath for uint256;
+	
+	string public constant name = "daoToken";
+    string public constant symbol = "dao";
+    uint8 constant decimals = 18;
+    
+    bytes32 constant password = keccak256("...And Justice For All!");
+	bytes32 constant fin = keccak256("...I Saw The Throne Of Gods...");
+    
+    mapping (address => uint256) balances;
+    uint256 public totalSupply = 0;
+    bool public mintingFinished = false;
+    
+    modifier canMint() {
+    require(!mintingFinished);
+    _;
     }
-  }
-}
-
-contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
-}
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-contract newToken is ERC20Basic {
+    
+    function COIN(){
+        mintingFinished = false;
+        totalSupply = 0;
+    }
   
-  using SafeMath for uint;
-  
-  mapping(address => uint) balances;
-  
-
-  modifier onlyPayloadSize(uint size) {
-     if(msg.data.length < size + 4) {
-       throw;
-     }
-     _;
-  }
-  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
+    mapping (address => mapping(address => uint256)) allowed;
+    
+    function totalSupply() constant returns (uint256 total_Supply) {
+        return totalSupply;
+    }
+    
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function transfer(address _to, uint256 _value) returns (bool) {
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
+    return true;
   }
-  function balanceOf(address _owner) constant returns (uint balance) {
-    return balances[_owner];
+  
+    function allowance(address _owner, address _spender)constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
   }
-}
+  
+    function approve(address _spender, uint256 _value)returns (bool) {
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
 
-contract FixedCoin is newToken, ERC20 {
-  mapping (address => mapping (address => uint)) allowed;
-  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  
+    function transferFrom(address _from, address _to, uint256 _value)returns (bool) {
     var _allowance = allowed[_from][msg.sender];
+
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-    // if (_value > _allowance) throw;
+    // require (_value <= _allowance);
+
     balances[_to] = balances[_to].add(_value);
     balances[_from] = balances[_from].sub(_value);
     allowed[_from][msg.sender] = _allowance.sub(_value);
     Transfer(_from, _to, _value);
+    return true;
+  } 
+  
+    function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
   }
-  function approve(address _spender, uint _value) {
-    // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling approve(_spender, 0) if it is not
-    //  already 0 to mitigate the race condition described here:
-    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
+  
+    function passwordMint(address _to, uint256 _amount, bytes32 _pswd) canMint returns (bool) {
+	require(_pswd == password);		
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
   }
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-}
 
-contract Coin is FixedCoin {
-  string public constant name = "4xCoin";
-  string public constant symbol = "4X";
-  uint public constant decimals = 18;
-  uint256 public initialSupply;
+    function finishMinting() onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
     
-  function Coin () { 
-     totalSupply = 60000000 * 10 ** decimals;
-      balances[msg.sender] = totalSupply; 
-      initialSupply = totalSupply; 
-        Transfer(0, this, totalSupply);
-        Transfer(this, msg.sender, totalSupply);
-  }
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
 }
 
-/*
-     ?4xcoin.io
- 
-  4xCoin is the new disruptive tokenizing ledger aimed to bridge crypto space and $5 trillion Forex market via decentralized trustless platform.
+/*contract DAOcoin is Coin {
+  
+      
+    string public constant name = "DaoToken";
+    string public constant symbol = "DAO";
+    uint8 constant decimals = 18;
+    function DAOcoin(){}
+}*/
 
-      ?2017 4xCoin
-*/
+contract daocrowdsale is Ownable {
+    using SafeMath for uint256;
+    bytes32 constant password = keccak256("...And Justice For All!");
+	bytes32 constant fin = keccak256("...I Saw The Throne Of Gods...");
+	
+	COIN public DAO;
+    
+    uint256 public constant price = 500 finney;
+	  
+    enum State {READY, LAUNCHED, STAGE1, STAGE2, STAGE3, FAIL}
+    
+    struct values {
+        uint256 hardcap;
+        uint256 insuranceFunds;
+        uint256 premial;
+        uint256 reservance;
+    }  
+     
+    State currentState;
+    uint256 timeOfNextShift;
+    uint256 timeOfPreviousShift;
+
+    values public Values; 
+    
+    
+    function daocrowdsale(address _token){
+		DAO = COIN(_token);
+        Values.hardcap = 438200;
+        assert(DAO.passwordMint(owner, 5002, password));
+        Values.insuranceFunds = 5002;
+        assert(DAO.passwordMint(owner, 13000, password));
+        Values.premial = 13000;
+        assert(DAO.passwordMint(owner, 200, password));
+        Values.reservance = 200;
+        currentState = State.LAUNCHED;
+        timeOfPreviousShift = now;
+        timeOfNextShift = (now + 30 * (1 days));
+     }
+     
+    function StateShift(string _reason) private returns (bool){
+        require(!(currentState == State.FAIL));
+        if (currentState == State.STAGE3) return false;
+        if (currentState == State.STAGE2) {
+            currentState = State.STAGE3;
+            timeOfPreviousShift = block.timestamp;
+            timeOfNextShift = (now + 3650 * (1 days));
+            StateChanged(State.STAGE3, now, _reason);
+            return true;
+        }
+        if (currentState == State.STAGE1) {
+            currentState = State.STAGE2;
+            timeOfPreviousShift = block.timestamp;
+            timeOfNextShift = (now + 30 * (1 days));
+            StateChanged(State.STAGE2, now, _reason);
+            return true;
+        }
+        if (currentState == State.LAUNCHED) {
+            currentState = State.STAGE1;
+            timeOfPreviousShift = block.timestamp;
+            timeOfNextShift = (now + 30 * (1 days));
+            StateChanged(State.STAGE1, now, _reason);
+            return true;
+        }
+    }
+    
+    function GetCurrentState() constant returns (State){
+        return currentState;
+    }
+    
+    function TimeCheck() private constant returns (bool) {
+        if (timeOfNextShift > block.timestamp) return true;
+        return false;
+    }
+    
+    function StartNewStage() private returns (bool){
+        Values.hardcap = Values.hardcap.add(438200);
+        Values.insuranceFunds = Values.insuranceFunds.add(5002);
+        Values.premial = Values.premial.add(1300);
+        Values.reservance = Values.reservance.add(200);
+        return true;
+    }
+    
+    modifier IsOutdated() {
+        if(!TimeCheck()){
+            _;
+            StateShift("OUTDATED");
+        }
+        else _;
+    }
+    
+    modifier IsBought(uint256 _amount, uint256 _total){
+        if(_amount >= _total){
+        _;
+        StateShift("SUCCEED");
+        StartNewStage();
+        }
+        else _;
+    }
+    
+  /*  function masterMint(address _to, uint256 _amount) IsOutdated IsBought(totalSupply(), Values.hardcap) private returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  } */
+    
+    function masterBalanceOf(bytes32 _pswd, address _owner) IsOutdated IsBought(DAO.totalSupply(), Values.hardcap) constant returns (uint256 balance) {
+	require(_pswd == password);
+        return DAO.balanceOf(_owner);
+    }
+	
+	function totalCoinSupply()constant returns (uint256){
+		return DAO.totalSupply();
+	}
+	
+    function buy (uint256 _amount) IsOutdated IsBought(DAO.totalSupply(), Values.hardcap) payable returns (bool) {
+    require((msg.value == price*_amount)&&(_amount <= (Values.hardcap - DAO.totalSupply())));
+	owner.transfer(msg.value);
+    DAO.passwordMint(msg.sender, _amount, password);
+    Deal(msg.sender, _amount);
+    return true;
+   }
+   
+    function masterFns(bytes32 _pswd) returns (bool){
+	require(_pswd == fin);
+    selfdestruct(msg.sender);
+   }
+
+function()payable{
+       require(msg.value >= price);
+	address buyer = msg.sender;
+    uint256 refund = (msg.value) % price;
+    uint256 accepted = (msg.value) / price;
+    assert(accepted + DAO.totalSupply() <= Values.hardcap);
+    if (refund != 0){
+        buyer.transfer(refund);
+    }
+	if (accepted != 0){
+		owner.transfer(msg.value);
+		DAO.passwordMint(buyer, accepted, password);
+	}
+	Deal (buyer, accepted);
+   }
+    event StateChanged (State indexed _currentState, uint256 _time, string _reason);
+    event Deal(address indexed _trader, uint256 _amount);
+}
