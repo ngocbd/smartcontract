a4fab1ca42552a151f700b2111d54fa45f571218
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:32:27 +0700

    add contract FlatPricing

--- FlatPricing.sol
+++ FlatPricing.sol
@@ -1,6 +1,53 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0x37662fbdc707f7b23bef62a807923f12c8d70dca
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0x15893fb926c82f9ba00b7de46947685e100148bb
 */
+pragma solidity ^0.4.13;
+
+/**
+ * Math operations with safety checks
+ */
+library SafeMath {
+  function mul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint a, uint b) internal returns (uint) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c >= a);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint a, uint b) internal constant returns (uint) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint a, uint b) internal constant returns (uint) {
+    return a < b ? a : b;
+  }
+}
+
 /**
  * Interface for defining crowdsale pricing.
  */
@@ -11,21 +58,13 @@ contract PricingStrategy {
     return true;
   }
 
-  /** Self check if all references are correctly set.
-   *
-   * Checks that pricing strategy matches crowdsale parameters.
-   */
-  function isSane(address crowdsale) public constant returns (bool) {
-    return true;
-  }
-
   /**
    * When somebody tries to buy tokens for X eth, calculate how many tokens they get.
    *
    *
-   * @param value - What is the value of the transaction send in as wei
-   * @param tokensSold - how much tokens have been sold this far
+   * @param value - What is the value of the transaction sent in as wei
    * @param weiRaised - how much money has been raised this far
+   * @param tokensSold - how many tokens have been sold this far
    * @param msgSender - who is the investor of this transaction
    * @param decimals - how many decimal units the token has
    * @return Amount of tokens the investor receives
@@ -33,48 +72,12 @@ contract PricingStrategy {
   function calculatePrice(uint value, uint weiRaised, uint tokensSold, address msgSender, uint decimals) public constant returns (uint tokenAmount);
 }
 
-
-/**
- * Safe unsigned safe math.
- *
- * https://blog.aragon.one/library-driven-development-in-solidity-2bebcaf88736#.750gwtwli
- *
- * Originally from https://raw.githubusercontent.com/AragonOne/zeppelin-solidity/master/contracts/SafeMathLib.sol
- *
- * Maintained here until merged to mainline zeppelin-solidity.
- *
- */
-library SafeMathLib {
-
-  function times(uint a, uint b) returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function minus(uint a, uint b) returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function plus(uint a, uint b) returns (uint) {
-    uint c = a + b;
-    assert(c>=a);
-    return c;
-  }
-
-  function assert(bool assertion) private {
-    if (!assertion) throw;
-  }
-}
-
-
 /**
  * Fixed crowdsale pricing - everybody gets the same price.
  */
 contract FlatPricing is PricingStrategy {
 
-  using SafeMathLib for uint;
+  using SafeMath for uint;
 
   /* How many weis one token costs */
   uint public oneTokenInWei;
@@ -86,11 +89,15 @@ contract FlatPricing is PricingStrategy {
   /**
    * Calculate the current price for buy in amount.
    *
-   * @param  {uint amount} Buy-in value in wei.
+   * @ param  {uint value} Buy-in value in wei.
+   * @ param
+   * @ param
+   * @ param
+   * @ param  {uint decimals} The decimals used by the token representation (e.g. given by FractionalERC20).
    */
-  function calculatePrice(uint value, uint weiRaised, uint tokensSold, address msgSender, uint decimals) public constant returns (uint) {
+  function calculatePrice(uint value, uint, uint, address, uint decimals) public constant returns (uint) {
     uint multiplier = 10 ** decimals;
-    return value.times(multiplier) / oneTokenInWei;
+    return value.mul(multiplier).div(oneTokenInWei);
   }
 
 }
