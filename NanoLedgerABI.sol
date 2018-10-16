f6e0f82936d50ee7943eabae831eb151b4a854fa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:35:39 +0700

    add contract NanoLedgerABI

---
+++ NanoLedgerABI.sol
@@ -0,0 +1,27 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NanoLedgerABI at 0x098c3347cf897d14eb69e47d0b174233cd9f4946
+*/
+pragma solidity ^0.4.21;
+
+contract NanoLedgerABI{
+    
+    struct data{
+        string company;
+        string valid_date;
+    }
+    
+    mapping (uint => data) datas;
+
+    
+    function save(uint256 _id, string _company, string _valid_date) public{
+        datas[_id].company = _company;
+        datas[_id].valid_date = _valid_date;
+    }
+    
+    function readCompany(uint8 _id) view public returns (string){
+       return datas[_id].company;
+    }
+    function readValidDate(uint8 _id) view public returns (string){
+       return datas[_id].valid_date;
+    }
+}
