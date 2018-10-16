e7ddf78f1d65afb5abdcf8d7c092822293e8e4f4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:49:21 +0700

    add contract HOLODECKS

--- HOLODECKS.sol
+++ HOLODECKS.sol
@@ -1,168 +1,350 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HOLODECKS at 0x4df624b49cb604992ef8ebb5250b96ec256c7472
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HOLODECKS at 0x04fd2dc8979d92bba530fd78be5296adc6ed5d43
 */
-pragma solidity ^0.4.16;
-
-contract ERC20 {
-  uint public totalSupply;
-  function balanceOf(address who) constant public returns (uint);
-  function allowance(address owner, address spender) constant public returns (uint);
-  function transfer(address to, uint value) public returns (bool ok);
-  function transferFrom(address from, address to, uint value) public returns (bool ok);
-  function approve(address spender, uint value) public returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-contract Ownable {
-  address public owner;
-
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    if (msg.sender == owner)
-      _;
-  }
-
-  function transferOwnership(address newOwner) onlyOwner public {
-    if (newOwner != address(0)) owner = newOwner;
-  }
-
-}
-
-contract TokenSpender {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
-}
+pragma solidity ^0.4.15;
 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
-contract SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
-    assert(c / a == b);
+    assert(a == 0 || c / a == b);
     return c;
   }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
   }
 }
 
-contract HOLODECKS is ERC20, SafeMath, Ownable {
+contract Token {
 
-  string public name;
-  string public symbol;
-  uint8 public decimals = 18;
-  string public version = 'v0.1'; 
-  uint public initialSupply;
-  uint public totalSupply;
+    /// @return total amount of tokens
+    //function totalSupply() constant returns (uint256 supply);
 
-  mapping(address => uint) balances;
-  mapping (address => mapping (address => uint)) allowed;
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance);
 
-  function HOLODECKS() public {
-    initialSupply = 1500000000 * 10 ** uint256(decimals);
-    totalSupply = initialSupply;
-    balances[msg.sender] = totalSupply;
-    name = 'HOLODECKS';   
-    symbol = 'HDK';
-  }
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success);
 
-  function burn(uint256 _value) public returns (bool){
-    balances[msg.sender] = safeSub(balances[msg.sender], _value) ;
-    totalSupply = safeSub(totalSupply, _value);
-    Transfer(msg.sender, 0x0, _value);
-    return true;
-  }
-  
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balances[_from] >= _value);
-        // Check for overflows
-        require(balances[_to] + _value > balances[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balances[_from] + balances[_to];
-        // Subtract from the sender
-        balances[_from] -= _value;
-        // Add the same to the recipient
-        balances[_to] += _value;
-        Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balances[_from] + balances[_to] == previousBalances);
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+}
+
+
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
     }
 
-  function transfer(address _to, uint _value) public returns (bool) {
-    balances[msg.sender] = safeSub(balances[msg.sender], _value);
-    balances[_to] = safeAdd(balances[_to], _value);
-    Transfer(msg.sender, _to, _value);
-    return true;
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+
+//name this contract whatever you'd like
+contract HOLODECKS is StandardToken {
+
+    function () {
+        //if ether is sent to this address, send it back.
+        revert();
+    }
+
+    /* Public variables of the token */
+
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customize the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+//
+// CHANGE THESE VALUES FOR YOUR TOKEN
+//
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+
+    function HOLODECKS(
+        ) {
+        decimals = 18; 
+        totalSupply = 1500000000 * (10 ** uint256(decimals));                        // Update total supply (100000 for example)
+        balances[msg.sender] = totalSupply;               // Give the creator all initial tokens (100000 for example)
+        name = "HOLODECKS";                                   // Set the name for display purposes
+        symbol = "HDK";                               // Set the symbol for display purposes
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { revert(); }
+        return true;
+    }
+}
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
   }
 
-  function transferFrom(address _from, address _to, uint _value) public returns (bool) {
-    var _allowance = allowed[_from][msg.sender];
-    
-    balances[_to] = safeAdd(balances[_to], _value);
-    balances[_from] = safeSub(balances[_from], _value);
-    allowed[_from][msg.sender] = safeSub(_allowance, _value);
-    Transfer(_from, _to, _value);
-    return true;
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
-  function balanceOf(address _owner) constant public returns (uint balance) {
-    return balances[_owner];
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 
-  function approve(address _spender, uint _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
+}
+
+/**
+ * @title Token 
+ * @dev API interface for interacting with the HOLODECKS contract
+ * /
+ interface Token {
+ function transfer (address _to, uint256 _value) returns (bool);
+ function balanceOf (address_owner) constant returns (uint256 balance);
+}
+
+/**
+ * @title HDK_Crowdsale
+ * @dev HDK_Crowdsale is a base contract for managing a token crowdsale.
+ * Crowdsales have a start and end timestamps, where investors can make
+ * token purchases and the crowdsale will assign them tokens based
+ * on a token per ETH rate. Funds collected are forwarded to a wallet
+ * as they arrive.
+ */
+contract HDK_Crowdsale is Ownable {
+  using SafeMath for uint256;
+
+  // The token being sold
+  HOLODECKS public token;
+
+  // start and end timestamps where investments are allowed (both inclusive)
+
+
+  uint256 public startTime = 1523750400;
+  uint256 public phase_1_Time = 1526342400 ;
+  uint256 public phase_2_Time = 1529020800;
+  uint256 public endTime = 1531612800;
+
+  // Max amount of wei accepted in the crowdsale
+  uint256 public cap;
+  
+  // Min amount of wei an investor can send
+  uint256 public minInvest;
+  
+  
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets. 1 ETH = 10000 HDK
+  uint256 public phase_1_rate = 13000;
+  uint256 public phase_2_rate = 12000;
+  uint256 public phase_3_rate = 11000;
+  
+  
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  mapping (address => uint256) rates;
+
+  function getRate() constant returns (uint256){
+    uint256 current_time = now;
+
+    if(current_time > startTime && current_time < phase_1_Time){
+      return phase_1_rate;
+    }
+    else if(current_time > phase_1_Time && current_time < phase_2_Time){
+      return phase_2_rate;
+    }
+      else if(current_time > phase_2_Time && current_time < endTime){
+      return phase_3_rate;
+    }
+      
   }
 
-  function approveAndCall(address _spender, uint256 _value, bytes _extraData) public{    
-      TokenSpender spender = TokenSpender(_spender);
-      if (approve(_spender, _value)) {
-          spender.receiveApproval(msg.sender, _value, this, _extraData);
-      }
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  function HDK_Crowdsale() {
+    wallet = msg.sender;
+    token = createTokenContract();
+    minInvest = 0.1 * 1 ether;
+    cap = 100000 * 1 ether;
   }
 
-  function allowance(address _owner, address _spender) constant public returns (uint remaining) {
-    return allowed[_owner][_spender];
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific mintable token.
+  function createTokenContract() internal returns (HOLODECKS) {
+    return new HOLODECKS();
+  }
+
+
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) public payable {
+    require(beneficiary != 0x0);
+    require(validPurchase());
+
+    uint256 weiAmount = msg.value;
+
+    // calculate token amount to be created
+    uint256 tokens = weiAmount.mul(getRate());
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    token.transfer(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+    forwardFunds();
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal constant returns (bool) {
+    bool withinPeriod = now >= startTime && now <= endTime;
+    bool nonZeroPurchase = msg.value != 0;
+    return withinPeriod && nonZeroPurchase;
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public constant returns (bool) {
+    return now > endTime;
   }
   
+/**
+ * @notice Terminate contract and refund to owner
+ */
+ function destroy() onlyOwner {
+     // Transfer tokens back to owner
+     uint256 balance = token.balanceOf(this);
+     assert (balance > 0);
+     token.transfer(owner,balance);
+     
+     // There should be no ether in the contract but just in case
+     selfdestruct(owner);
+     
+ }
+
 }
