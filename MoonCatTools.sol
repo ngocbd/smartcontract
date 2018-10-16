124758fa0e28d77d9098617830494055444cdeee
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:28:25 +0700

    add contract MoonCatTools

---
+++ MoonCatTools.sol
@@ -0,0 +1,15 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MoonCatTools at 0x67354c70be127082c5b520086a74c980cf9335f0
+*/
+pragma solidity ^0.4.11;
+
+contract MoonCatTools {
+    bytes32 searchSeed = 0x8363e7eaae8e35b1c2db100a7b0fb9db1bc604a35ce1374d882690d0b1d888e2;
+    
+    function rescueCatHashCheck(bytes32 seed) constant returns (bytes32 catIdHash) {
+        catIdHash = keccak256(seed, searchSeed);
+        if (!(catIdHash[0] | catIdHash[1] | catIdHash[2] == 0x0)) {
+            catIdHash = 0x0;
+        }
+    }
+}
