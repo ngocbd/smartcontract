f988a4710afa6f2e542459771584397232ad6ce1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:43:41 +0700

    add contract thewhalegame

---
+++ thewhalegame.sol
@@ -0,0 +1,77 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract thewhalegame at 0x02B2101903eB6a51518E63e84B785180859FDa9d
+*/
+contract thewhalegame {
+
+  struct Person {
+      address etherAddress;
+      uint amount;
+  }
+
+  Person[] public persons;
+
+  uint public payoutIdx = 0;
+  uint public collectedFees;
+  uint public balance = 0;
+
+  address public owner;
+
+
+  modifier onlyowner { if (msg.sender == owner) _ }
+
+
+  function thewhalegame() {
+    owner = msg.sender;
+  }
+
+  function() {
+    enter();
+  }
+  
+  function enter() {
+    if (msg.value < 5 ether) {
+        msg.sender.send(msg.value);
+        return;
+    }
+	
+		uint amount;
+		if (msg.value > 500 ether) {
+			msg.sender.send(msg.value - 500 ether);	
+			amount = 500 ether;
+    }
+		else {
+			amount = msg.value;
+		}
+
+
+    uint idx = persons.length;
+    persons.length += 1;
+    persons[idx].etherAddress = msg.sender;
+    persons[idx].amount = amount;
+ 
+    
+    if (idx != 0) {
+      collectedFees += amount / 100 * 3;
+	  owner.send(collectedFees);
+	  collectedFees = 0;
+      balance += amount - amount / 100 * 3;
+    } 
+    else {
+      balance += amount;
+    }
+
+
+    while (balance > persons[payoutIdx].amount / 100 * 200) {
+      uint transactionAmount = persons[payoutIdx].amount / 100 * 200;
+      persons[payoutIdx].etherAddress.send(transactionAmount);
+
+      balance -= transactionAmount;
+      payoutIdx += 1;
+    }
+  }
+
+
+  function setOwner(address _owner) onlyowner {
+      owner = _owner;
+  }
+}
