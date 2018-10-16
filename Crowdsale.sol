dd90ca60c051befe7e1d0ee6c06d144c7e4082a3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:44:13 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,324 +1,215 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x0bee1b7287969e1e8afb50eb587e93217ffab3a3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xb5bcd3268dd094ac273f3590a35f52da8677fc55
 */
-pragma solidity ^0.4.18;
-/**
-* @title ICO CONTRACT
-* @dev ERC-20 Token Standard Complian
-*/
+pragma solidity ^0.4.16;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+contract ERC20Basic 
+{
+	uint256 public totalSupply;
+	function balanceOf(address who) constant public returns (uint256);
+	function transfer(address to, uint256 value) public returns (bool);
+	event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract Ownable {
-  address public owner;
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public{
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
+contract ERC20 is ERC20Basic
+{
+	function allowance(address owner, address spender) constant  public returns (uint256);
+	function transferFrom(address from, address to, uint256 value)  public returns (bool);
+	function approve(address spender, uint256 value)  public returns (bool);
+	event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract token {
-
-  function balanceOf(address _owner) public constant returns (uint256 balance);
-  function transfer(address _to, uint256 _value) public returns (bool success);
-
-}
-
-
-contract Crowdsale is Ownable {
-  using SafeMath for uint256;
-  // The token being sold
-  token public token_reward;
-  // start and end timestamps where investments are allowed (both inclusive
-  
-  uint256 public start_time = now; //for testing
-  //uint256 public start_time = 1517846400; //02/05/2018 @ 4:00pm (UTC) or 5 PM (UTC + 1)
-  uint256 public end_Time = 1524355200; // 04/22/2018 @ 12:00am (UTC)
-
-  uint256 public phase_1_remaining_tokens  = 50000000 * (10 ** uint256(8));
-  uint256 public phase_2_remaining_tokens  = 50000000 * (10 ** uint256(8));
-  uint256 public phase_3_remaining_tokens  = 50000000 * (10 ** uint256(8));
-  uint256 public phase_4_remaining_tokens  = 50000000 * (10 ** uint256(8));
-  uint256 public phase_5_remaining_tokens  = 50000000 * (10 ** uint256(8));
-
-  uint256 public phase_1_bonus  = 40;
-  uint256 public phase_2_bonus  = 20;
-  uint256 public phase_3_bonus  = 15;
-  uint256 public phase_4_bonus  = 10;
-  uint256 public phase_5_bonus  = 5;
-
-  uint256 public token_price  = 2;// 2 cents
-
-  // address where funds are collected
-  address public wallet;
-  // Ether to $ price
-  uint256 public eth_to_usd = 1000;
-  // amount of raised money in wei
-  uint256 public weiRaised;
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-  // rate change event
-  event EthToUsdChanged(address indexed owner, uint256 old_eth_to_usd, uint256 new_eth_to_usd);
-  
-  // constructor
-  function Crowdsale(address tokenContractAddress) public{
-    wallet = 0x1aC024482b91fa9AaF22450Ff60680BAd60bF8D3;//wallet where ETH will be transferred
-    token_reward = token(tokenContractAddress);
-  }
-  
- function tokenBalance() constant public returns (uint256){
-    return token_reward.balanceOf(this);
-  }
-
-  function getRate() constant public returns (uint256){
-    return eth_to_usd.mul(100).div(token_price);
-  }
-
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal constant returns (bool) {
-    bool withinPeriod = now >= start_time && now <= end_Time;
-    bool allPhaseFinished = phase_5_remaining_tokens > 0;
-    bool nonZeroPurchase = msg.value != 0;
-    bool minPurchase = eth_to_usd*msg.value >= 100; // minimum purchase $100
-    return withinPeriod && nonZeroPurchase && allPhaseFinished && minPurchase;
-  }
-
-  // @return true if the admin can send tokens manually
-  function validPurchaseForManual() internal constant returns (bool) {
-    bool withinPeriod = now >= start_time && now <= end_Time;
-    bool allPhaseFinished = phase_5_remaining_tokens > 0;
-    return withinPeriod && allPhaseFinished;
-  }
-
-
-  // check token availibility for current phase and max allowed token balance
-  function checkAndUpdateTokenForManual(uint256 _tokens) internal returns (bool){
-    if(phase_1_remaining_tokens > 0){
-      if(_tokens > phase_1_remaining_tokens){
-        uint256 tokens_from_phase_2 = _tokens.sub(phase_1_remaining_tokens);
-        phase_1_remaining_tokens = 0;
-        phase_2_remaining_tokens = phase_2_remaining_tokens.sub(tokens_from_phase_2);
-      }else{
-        phase_1_remaining_tokens = phase_1_remaining_tokens.sub(_tokens);
-      }
-      return true;
-    }else if(phase_2_remaining_tokens > 0){
-      if(_tokens > phase_2_remaining_tokens){
-        uint256 tokens_from_phase_3 = _tokens.sub(phase_2_remaining_tokens);
-        phase_2_remaining_tokens = 0;
-        phase_3_remaining_tokens = phase_3_remaining_tokens.sub(tokens_from_phase_3);
-      }else{
-        phase_2_remaining_tokens = phase_2_remaining_tokens.sub(_tokens);
-      }
-      return true;
-    }else if(phase_3_remaining_tokens > 0){
-      if(_tokens > phase_3_remaining_tokens){
-        uint256 tokens_from_phase_4 = _tokens.sub(phase_3_remaining_tokens);
-        phase_3_remaining_tokens = 0;
-        phase_4_remaining_tokens = phase_4_remaining_tokens.sub(tokens_from_phase_4);
-      }else{
-        phase_3_remaining_tokens = phase_3_remaining_tokens.sub(_tokens);
-      }
-      return true;
-    }else if(phase_4_remaining_tokens > 0){
-      if(_tokens > phase_4_remaining_tokens){
-        uint256 tokens_from_phase_5 = _tokens.sub(phase_4_remaining_tokens);
-        phase_4_remaining_tokens = 0;
-        phase_5_remaining_tokens = phase_5_remaining_tokens.sub(tokens_from_phase_5);
-      }else{
-        phase_4_remaining_tokens = phase_4_remaining_tokens.sub(_tokens);
-      }
-      return true;
-    }else if(phase_5_remaining_tokens > 0){
-      if(_tokens > phase_5_remaining_tokens){
-        return false;
-      }else{
-        phase_5_remaining_tokens = phase_5_remaining_tokens.sub(_tokens);
-       }
-    }else{
-      return false;
-    }
-  }
-
-  // function to transfer token manually
-  function transferManually(uint256 _tokens, address to_address) onlyOwner public returns (bool){
-    require(to_address != 0x0);
-    require(validPurchaseForManual());
-    require(checkAndUpdateTokenForManual(_tokens));
-    token_reward.transfer(to_address, _tokens);
-    return true;
-  }
-
-
-  // check token availibility for current phase and max allowed token balance
-  function transferIfTokenAvailable(uint256 _tokens, uint256 _weiAmount, address _beneficiary) internal returns (bool){
-
-    uint256 total_token_to_transfer = 0;
-    uint256 bonus = 0;
-    if(phase_1_remaining_tokens > 0){
-      if(_tokens > phase_1_remaining_tokens){
-        uint256 tokens_from_phase_2 = _tokens.sub(phase_1_remaining_tokens);
-        bonus = (phase_1_remaining_tokens.mul(phase_1_bonus).div(100)).add(tokens_from_phase_2.mul(phase_2_bonus).div(100));
-        phase_1_remaining_tokens = 0;
-        phase_2_remaining_tokens = phase_2_remaining_tokens.sub(tokens_from_phase_2);
-      }else{
-        phase_1_remaining_tokens = phase_1_remaining_tokens.sub(_tokens);
-        bonus = _tokens.mul(phase_1_bonus).div(100);
-      }
-      total_token_to_transfer = _tokens + bonus;
-    }else if(phase_2_remaining_tokens > 0){
-      if(_tokens > phase_2_remaining_tokens){
-        uint256 tokens_from_phase_3 = _tokens.sub(phase_2_remaining_tokens);
-        bonus = (phase_2_remaining_tokens.mul(phase_2_bonus).div(100)).add(tokens_from_phase_3.mul(phase_3_bonus).div(100));
-        phase_2_remaining_tokens = 0;
-        phase_3_remaining_tokens = phase_3_remaining_tokens.sub(tokens_from_phase_3);
-      }else{
-        phase_2_remaining_tokens = phase_2_remaining_tokens.sub(_tokens);
-        bonus = _tokens.mul(phase_2_bonus).div(100);
-      }
-      total_token_to_transfer = _tokens + bonus;
-    }else if(phase_3_remaining_tokens > 0){
-      if(_tokens > phase_3_remaining_tokens){
-        uint256 tokens_from_phase_4 = _tokens.sub(phase_3_remaining_tokens);
-        bonus = (phase_3_remaining_tokens.mul(phase_3_bonus).div(100)).add(tokens_from_phase_4.mul(phase_4_bonus).div(100));
-        phase_3_remaining_tokens = 0;
-        phase_4_remaining_tokens = phase_4_remaining_tokens.sub(tokens_from_phase_4);
-      }else{
-        phase_3_remaining_tokens = phase_3_remaining_tokens.sub(_tokens);
-        bonus = _tokens.mul(phase_3_bonus).div(100);
-      }
-      total_token_to_transfer = _tokens + bonus;
-    }else if(phase_4_remaining_tokens > 0){
-      if(_tokens > phase_4_remaining_tokens){
-        uint256 tokens_from_phase_5 = _tokens.sub(phase_4_remaining_tokens);
-        bonus = (phase_4_remaining_tokens.mul(phase_4_bonus).div(100)).add(tokens_from_phase_5.mul(phase_5_bonus).div(100));
-        phase_4_remaining_tokens = 0;
-        phase_5_remaining_tokens = phase_5_remaining_tokens.sub(tokens_from_phase_5);
-      }else{
-        phase_4_remaining_tokens = phase_4_remaining_tokens.sub(_tokens);
-        bonus = _tokens.mul(phase_4_bonus).div(100);
-      }
-      total_token_to_transfer = _tokens + bonus;
-    }else if(phase_5_remaining_tokens > 0){
-      if(_tokens > phase_5_remaining_tokens){
-        total_token_to_transfer = 0;
-      }else{
-        phase_5_remaining_tokens = phase_5_remaining_tokens.sub(_tokens);
-        bonus = _tokens.mul(phase_5_bonus).div(100);
-        total_token_to_transfer = _tokens + bonus;
-      }
-    }else{
-      total_token_to_transfer = 0;
-    }
-    if(total_token_to_transfer > 0){
-      token_reward.transfer(_beneficiary, total_token_to_transfer);
-      TokenPurchase(msg.sender, _beneficiary, _weiAmount, total_token_to_transfer);
-      return true;
-    }else{
-      return false;
-    }
+library SafeMath 
+{
     
-  }
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
 
-  // fallback function can be used to buy tokens
-  function () payable public{
-    buyTokens(msg.sender);
-  }
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		// assert(b > 0); // Solidity automatically throws when dividing by 0
+		uint256 c = a / b;
+		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+		return c;
+	}
 
-  // low level token purchase function
-  function buyTokens(address beneficiary) public payable {
-    require(beneficiary != 0x0);
-    require(validPurchase());
-    uint256 weiAmount = msg.value;
-    // calculate token amount to be created
-    uint256 tokens = (weiAmount.mul(getRate())).div(10 ** uint256(10));
-    // Check is there are enough token available for current phase and per person  
-    require(transferIfTokenAvailable(tokens, weiAmount, beneficiary));
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+
+	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
+  
+}
+
+contract BasicToken is ERC20Basic 
+{
     
-    forwardFunds();
-  }
-  
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
-  }
-  
-  // @return true if crowdsale event has ended
-  function hasEnded() public constant returns (bool) {
-    return now > end_Time;
-  }
-  // function to transfer token back to owner
-  function transferBack(uint256 tokens, address to_address) onlyOwner public returns (bool){
-    token_reward.transfer(to_address, tokens);
+	using SafeMath for uint256;
+
+	mapping(address => uint256) balances;
+
+	function transfer(address _to, uint256 _value) public returns (bool) {
+		require(msg.data.length >= (2*32) + 4);     // ???. ???????? ?? ????? ? ???????? ???????
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function balanceOf(address _owner) constant public returns (uint256 balance) {
+		return balances[_owner];
+	}
+}
+
+contract StandardToken is ERC20, BasicToken 
+{
+
+	mapping (address => mapping (address => uint256)) allowed;
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) 
+	{
+		require(msg.data.length >= (3*32) + 4);     // Fix for the ERC20 short address attack
+		var _allowance = allowed[_from][msg.sender];
+		
+		balances[_to] = balances[_to].add(_value);
+		balances[_from] = balances[_from].sub(_value);
+		allowed[_from][msg.sender] = _allowance.sub(_value);
+		Transfer(_from, _to, _value);
     return true;
-  }
-  // function to change rate
-  function changeEth_to_usd(uint256 _eth_to_usd) onlyOwner public returns (bool){
-    EthToUsdChanged(msg.sender, eth_to_usd, _eth_to_usd);
-    eth_to_usd = _eth_to_usd;
-    return true;
-  }
+	}
+
+    function approve(address _spender, uint256 _value) public returns (bool)
+	{
+		require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function allowance(address _owner, address _spender) constant public returns (uint256 remaining) 
+	{
+		return allowed[_owner][_spender];
+	}
+
+}
+
+contract Ownable 
+{
+    address public owner;
+
+	function Ownable()  public
+	{
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner() 
+	{
+		require(msg.sender == owner);
+		_;
+	}
+
+	function transferOwnership(address newOwner)  public onlyOwner
+	{
+		require(newOwner != address(0));      
+		owner = newOwner;
+	}
+}
+
+contract BurnableToken is StandardToken, Ownable 
+{
+    uint256 endIco = 1527854400; // 1 ????
+
+    modifier BurnAll() 
+    { 
+		require(now > endIco && balances[owner] > 0);  
+		_;
+	}
+    
+	function burn()  public BurnAll 
+	{
+		uint256 surplus = balances[owner];
+		totalSupply = totalSupply.sub(1000);
+		balances[owner] = 0;
+		Burn(owner, surplus);
+	}
+	event Burn(address indexed burner, uint indexed value);
+}
+
+contract OSCoinToken is BurnableToken 
+{
+	string public constant name = "OSCoin";   
+	string public constant symbol = "OSC";    
+	uint32 public constant decimals = 18;
+
+	uint256 public INITIAL_SUPPLY = 2000000 * 1 ether;
+
+	function OSCoinToken()  public
+	{
+		totalSupply = INITIAL_SUPPLY;
+		balances[msg.sender] = INITIAL_SUPPLY;
+		
+		allowed[owner][0x740F7A070C283edc1cAd9351A67aD3b513f3136a] = (totalSupply).div(100).mul(11);     // ?????? ? ???????? ????? ??????? 11% from to ???? ?????????? ?????? ????? ??? ????????? OSCoin
+		Approval(owner,0x740F7A070C283edc1cAd9351A67aD3b513f3136a, (totalSupply).div(100).mul(11));     // ???????? ????? ??????? 11% from to          ???? ?????????? ?????? ????? ??? ????????? OSCoin
+	}
+}
+
+contract Crowdsale is Ownable
+{   
+    // ??????? ????? http://i-leon.ru/tools/time   
+        
+    uint256 startPreIco = 1522065600; // 26 ?????
+    uint256 startIco = 1525089600; // 30 ??????
+    uint256 endIco = 1527854400; // 1 ????
+    
+	using SafeMath for uint;    
+	address multisig;
+	uint restrictedPercent;
+	address restricted;
+	OSCoinToken public token = new OSCoinToken();
+	
+	uint period;
+	uint rate;
+	
+	function Crowdsale() public {
+		multisig = 0x83dd3A421C98ea8fd59798bC57B4e2C75Caf9935;      // ????? ???? ????? ????????????? ?????????? Ethereum
+		restricted = 0x83dd3A421C98ea8fd59798bC57B4e2C75Caf9935;    // ????? ???? ????? ????????????? 23% ? ?????? ???? ?????????? ?????? ????? ??? ????????? OSCoin
+		restrictedPercent = 23;
+		rate = 1000000000000000000000;
+	}
+
+	modifier saleIsOn() 
+	{
+		require(now < endIco);
+		_;
+	}
+
+	function createTokens() saleIsOn  public payable {
+		multisig.transfer(msg.value);
+		uint tokens = rate.mul(msg.value).div(1 ether);
+		uint bonusTokens = 0;
+		
+		if((now < startPreIco)) 
+		{ 
+			bonusTokens = tokens.div(2);
+		} else if(now >= startPreIco && now < startIco) {
+			bonusTokens = tokens.div(4);
+		}
+		
+		uint tokensWithBonus = tokens.add(bonusTokens);
+		token.transfer(msg.sender, tokensWithBonus);
+		uint restrictedTokens = tokens.mul(restrictedPercent).div(100 - restrictedPercent);
+		token.transfer(restricted, restrictedTokens);
+	}
+
+	function() external payable 
+	{
+		createTokens();
+	}
 }
