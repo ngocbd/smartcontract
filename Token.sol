a143d9be017aa6314ed9bb9fa00c26b6d4bed6d6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:46:08 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,132 +1,175 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x1d79009f1f1784bf2ce4a8aee658bc6b2a58e9af
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xF3b3Cad094B89392fcE5faFD40bC03b80F2Bc624
 */
-pragma solidity ^0.4.20;
-
-interface ERC20 {
-    function totalSupply() constant returns (uint _totalSupply);
-    function balanceOf(address _owner) constant returns (uint balance);
-    function transfer(address _to, uint _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) returns (bool success);
-    function approve(address _spender, uint _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint remaining);
+pragma solidity ^0.4.15;
+contract Base {
+    modifier only(address allowed) {
+        require(msg.sender == allowed);
+        _;
+    }
+    // *************************************************
+    // *          reentrancy handling                  *
+    // *************************************************
+    uint constant internal L00 = 2 ** 0;
+    uint constant internal L01 = 2 ** 1;
+    uint constant internal L02 = 2 ** 2;
+    uint constant internal L03 = 2 ** 3;
+    uint constant internal L04 = 2 ** 4;
+    uint constant internal L05 = 2 ** 5;
+    uint private bitlocks = 0;
+    modifier noAnyReentrancy {
+        var _locks = bitlocks;
+        require(_locks == 0);
+        bitlocks = uint(-1);
+        _;
+        bitlocks = _locks;
+    }
+}
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+contract Owned is Base {
+    address public owner;
+    address newOwner;
+    function Owned() {
+        owner = msg.sender;
+    }
+    function transferOwnership(address _newOwner) only(owner) {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() only(newOwner) {
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+}
+contract ERC20 is Owned {
     event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
-
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    function transfer(address _to, uint _value) isStartedOnly returns (bool success) {
+        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-
-contract StandardToken is ERC20 {
-
-    using SafeMath for uint256;
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-
-   function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
-
-        // SafeMath.sub will throw if there is not enough balance.
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
+    function transferFrom(address _from, address _to, uint _value) isStartedOnly returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
     }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
+    function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
-
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve_fixed(address _spender, uint _currentValue, uint _value) isStartedOnly returns (bool success) {
+        if(allowed[msg.sender][_spender] == _currentValue){
+            allowed[msg.sender][_spender] = _value;
+            Approval(msg.sender, _spender, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function approve(address _spender, uint _value) isStartedOnly returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
         return allowed[_owner][_spender];
     }
-
-    function totalSupply() public constant returns (uint _totalSupply) {
-        _totalSupply = totalSupply;
+    mapping (address => uint) balances;
+    mapping (address => mapping (address => uint)) allowed;
+    uint public totalSupply;
+    bool    public isStarted = false;
+    modifier isStartedOnly() {
+        require(isStarted);
+        _;
     }
-
-
 }
-
-
-contract Token is StandardToken {
-
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
-    }
-
-    string public name ="Hgcrypto";
+contract Token is ERC20 {
+    using SafeMath for uint;
+    string public name = "PATRON";
+    string public symbol = "PAT";
     uint8 public decimals = 18;
-    string public symbol = "HGC";
-    uint256 public initialSupply = 1280000000;
-
-    function Token(address _receiver) public {
-        require(_receiver != address(0));
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balances[_receiver] = totalSupply;               // Give the receiver all initial tokens
-
+    address public crowdsaleMinter;
+    modifier onlyCrowdsaleMinter(){
+        require(msg.sender == crowdsaleMinter);
+        _;
+    }
+    modifier isNotStartedOnly() {
+        require(!isStarted);
+        _;
+    }
+    function Token(address _crowdsaleMinter){
+        crowdsaleMinter = _crowdsaleMinter;
+    }
+    function start()
+    public
+    onlyCrowdsaleMinter
+    isNotStartedOnly
+    {
+        isStarted = true;
+    }
+    function emergencyStop()
+    public
+    only(owner)
+    {
+        isStarted = false;
+    }
+    //================= Crowdsale Only =================
+    function mint(address _to, uint _amount) public
+    onlyCrowdsaleMinter
+    isNotStartedOnly
+    returns(bool)
+    {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        return true;
     }
 
+    event Burn(address indexed burner, uint256 value);
+
+    function burn(uint256 _value, address _address)
+    onlyCrowdsaleMinter
+    returns (bool)
+    {
+        require(_value <= balances[_address]);
+
+        balances[_address] = balances[_address].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+
+        Burn(_address, _value);
+
+        return true;
+    }
 
 }
