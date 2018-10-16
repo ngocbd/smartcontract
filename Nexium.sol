874df6d6a68d9d493a61f097b9ca06ce01f2ef41
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:44:51 +0700

    add contract Nexium

--- Nexium.sol
+++ Nexium.sol
@@ -1,65 +1,97 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Nexium at 0x1f0b9Db5bb0F2a60F8C8824CEc738C15805dBDec
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Nexium at 0x45e42d659d9f9466cd5df622506033145a9b89bc
 */
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+contract tokenSpender { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 
-    contract Nexium { 
-        /* Public variables of the token */
-        string public name;
-        string public symbol;
-        uint8 public decimals;
+contract Nexium { 
+	
+	
+	/* Public variables of the token */
+	string public name;
+	string public symbol;
+	uint8 public decimals;
+	uint256 public initialSupply;
+	address public burnAddress;
 
-        /* This creates an array with all balances */
-        mapping (address => uint256) public balanceOf;
-        mapping (address => mapping (address => uint)) public allowance;
-        mapping (address => mapping (address => uint)) public spentAllowance;
+	/* This creates an array with all balances */
+	mapping (address => uint) public balanceOf;
+	mapping (address => mapping (address => uint)) public allowance;
 
-        /* This generates a public event on the blockchain that will notify clients */
-        event Transfer(address indexed from, address indexed to, uint256 value);
+	/* This generates a public event on the blockchain that will notify clients */
+	event Transfer(address indexed from, address indexed to, uint value);
+	event Approval(address indexed from, address indexed spender, uint value);
 
-        /* Initializes contract with initial supply tokens to the creator of the contract */
-        function Nexium() {
-            balanceOf[msg.sender] = 100000000000;              // Give the creator all initial tokens                    
-            name = 'Nexium';                                   // Set the name for display purposes     
-            symbol = 'NxC';                               // Set the symbol for display purposes    
-            decimals = 3;                            // Amount of decimals for display purposes        
-        }
+	
+	
+	/* Initializes contract with initial supply tokens to the creator of the contract */
+	function Nexium() {
+		initialSupply = 100000000000;
+		balanceOf[msg.sender] = initialSupply;             // Give the creator all initial tokens                    
+		name = 'Nexium';                                 // Set the name for display purposes     
+		symbol = 'NxC';                               	 // Set the symbol for display purposes    
+		decimals = 3;                           		 // Amount of decimals for display purposes
+		burnAddress = 0x1b32000000000000000000000000000000000000;
+	}
+	
+	function totalSupply() returns(uint){
+		return initialSupply - balanceOf[burnAddress];
+	}
 
-        /* Send coins */
-        function transfer(address _to, uint256 _value) {
-            if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough   
-            if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-            balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-            balanceOf[_to] += _value;                            // Add the same to the recipient            
-            Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-        }
-
-        /* Allow another contract to spend some tokens in your behalf */
-
-        function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-            allowance[msg.sender][_spender] = _value;     
-            tokenRecipient spender = tokenRecipient(_spender);
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-			
+	/* Send coins */
+	function transfer(address _to, uint256 _value) 
+	returns (bool success) {
+		if (balanceOf[msg.sender] >= _value && _value > 0) {
+			balanceOf[msg.sender] -= _value;
+			balanceOf[_to] += _value;
+			Transfer(msg.sender, _to, _value);
 			return true;
-        }
+		} else return false; 
+	}
 
-        /* A contract attempts to get the coins */
+	/* Allow another contract to spend some tokens in your behalf */
 
-        function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-            if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough   
-            if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-            if (spentAllowance[_from][msg.sender] + _value > allowance[_from][msg.sender]) throw;   // Check allowance
-            balanceOf[_from] -= _value;                          // Subtract from the sender
-            balanceOf[_to] += _value;                            // Add the same to the recipient            
-            spentAllowance[_from][msg.sender] += _value;
-            Transfer(msg.sender, _to, _value); 
-			
-			return true;
-        } 
-
-        /* This unnamed function is called whenever someone tries to send ether to it */
-        function () {
-            throw;     // Prevents accidental sending of ether
-        }        
+	
+	
+	function approveAndCall(address _spender,
+							uint256 _value,
+							bytes _extraData)
+	returns (bool success) {
+		allowance[msg.sender][_spender] = _value;     
+		tokenSpender spender = tokenSpender(_spender);
+		spender.receiveApproval(msg.sender, _value, this, _extraData);
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+	
+	
+	
+	/*Allow another adress to use your money but doesn't notify it*/
+	function approve(address _spender, uint256 _value) returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
     }
+
+	
+	
+	/* A contract attempts to get the coins */
+	function transferFrom(address _from,
+						  address _to,
+						  uint256 _value)
+	returns (bool success) {
+		if (balanceOf[_from] >= _value && allowance[_from][msg.sender] >= _value && _value > 0) {
+			balanceOf[_to] += _value;
+			Transfer(_from, _to, _value);
+			balanceOf[_from] -= _value;
+			allowance[_from][msg.sender] -= _value;
+			return true;
+		} else return false; 
+	}
+
+	
+	
+	/* This unnamed function is called whenever someone tries to send ether to it */
+	function () {
+		throw;     // Prevents accidental sending of ether
+	}        
+}
