6fd0a59db57856396b44be0fffc3b9a0e72db7a9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:55:40 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xf9982d56a3bca3e2808c537bba1909baa29f131a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x199c1f173c75948f514fd73dc94d9633d4a99449
 */
 pragma solidity ^0.4.4;
 
@@ -40,6 +40,8 @@ contract Token {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
 }
+
+
 
 contract StandardToken is Token {
 
@@ -117,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 10000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 10000000;                        // Update total supply (100000 for example)
-        name = "Justin Token";                                   // Set the name for display purposes
-        decimals = 1;                            // Amount of decimals for display purposes
-        symbol = "JOWG";                               // Set the symbol for display purposes
+        balances[msg.sender] = 10000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 10000000000000000;                        // Update total supply (100000 for example)
+        name = "CrowdCoin";                                   // Set the name for display purposes
+        decimals = 8;                            // Amount of decimals for display purposes
+        symbol = "CRWD";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
