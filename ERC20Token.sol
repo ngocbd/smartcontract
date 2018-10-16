999022ba17ed37ae6597565eb9d690d2b31729e7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:21:45 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x4395796c4e12fe32129a11b58410752dab56d18b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x5e05456f4616db37e8d12b27e302847609d99b63
 */
 pragma solidity ^0.4.4;
 
@@ -106,7 +106,7 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
+    string public name;                  //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
     string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
@@ -119,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 1000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 1000000000;                        // Update total supply (100000 for example)
-        name = "Easter Coin";                                   // Set the name for display purposes
+        balances[msg.sender] = 100000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 100000;                        // Update total supply (100000 for example)
+        name = "Bling";                                   // Set the name for display purposes
         decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "EAST";                               // Set the symbol for display purposes
+        symbol = "BLNG";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
