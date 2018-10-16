461d4e0a44b8a0d4e030cf4d2131d12c29e1d0b6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:13:31 +0700

    add contract ITO

--- ITO.sol
+++ ITO.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ITO at 0xa1cf7f342c826f5747728295938314ce9c84a9fa
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ITO at 0x67453d62b7ebb51a573f3463ad3d044e8d854944
 */
 pragma solidity ^0.4.18;
 
@@ -852,7 +852,7 @@ contract Configurator is Ownable {
 
     preITO = new PreITO();
 
-    preITO.setWallet(0xa86780383E35De330918D8e4195D671140A60A74);
+    preITO.setWallet(0x1B139Ad79ED5F69ca4545EE9c4F1C774FbEc99Fe);
     preITO.setStart(1526342400);
     preITO.setPeriod(15);
     preITO.setPrice(786700);
@@ -867,7 +867,7 @@ contract Configurator is Ownable {
 
     ito = new ITO();
 
-    ito.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
+    ito.setWallet(0x1B139Ad79ED5F69ca4545EE9c4F1C774FbEc99Fe);
     ito.setStart(1527811200);
     ito.addMilestone(5, 33);
     ito.addMilestone(5, 18);
