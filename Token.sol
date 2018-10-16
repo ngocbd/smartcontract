c131257df6906ec40b517e29e7077b66dfc910fa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:57:47 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,140 +1,177 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x535425c692ab4a98b3b407ad974997a228d7ce59
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x5c70008427a2b2a07702ab59bf191439f652e4ec
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
 
-contract Token {
+library SafeMathMod {// Partial SafeMath Library
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require((c = a - b) < a);
+    }
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require((c = a + b) > a);
+    }
+}
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+contract Token {//is inherently ERC20
+    using SafeMathMod for uint256;
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    /**
+    * @constant name The name of the token
+    * @constant symbol  The symbol used to display the currency
+    * @constant decimals  The number of decimals used to dispay a balance
+    * @variable totalSupply The total number of tokens times 10 times of the number of decimals
+    * @variable presaleAddress  Address of the presale contract
+    * @variable crowdsaleAddress  Address of the crowdsale contract
+    * @variable crowdsaleSuccessful  has there been a successful crowdsale
+    * @constant MAX_UINT256 Magic number for unlimited allowance
+    * @storage balanceOf Holds the balances of all token holders
+    * @storage Approval Holds the allowed balance to be transferable by another address.
+    */
 
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    string constant public name = "Smart City Token";
 
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    string constant public symbol = "SCT";
+
+    uint8 constant public decimals = 18;
+
+    uint256 public totalSupply;
+
+    address public presaleAddress;
+    
+    address public crowdsaleAddress;
+    
+    bool public crowdsaleSuccessful;
+    
+    uint256 constant private MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+    mapping (address => uint256) public balanceOf;
+
+    mapping (address => mapping (address => uint256)) public allowance;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event TransferFrom(address indexed _spender, address indexed _from, address indexed _to, uint256 _value);
+
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
-}
+    event Mint(address indexed _to, uint256 _value, uint256 _totalSupply);
+
+    event Burn(address indexed _from, uint256 _value, uint256 _totalSupply);
 
 
 
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    function Token(address _presaleAddress, address _crowdsaleAddress) public {
+        totalSupply = 0;
+        presaleAddress = _presaleAddress;
+        crowdsaleAddress = _crowdsaleAddress;
     }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
-
-//name this contract whatever you'd like
-contract ERC20Token is StandardToken {
-
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
-    }
-
-    /* Public variables of the token */
-
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
+    
+    /**
+    * @notice send `_value` tokens to `_to` address from `msg.sender`
+    *
+    * @param _to The address of the recipient
+    * @param _value The amount of token to be transferred
+    * @return Whether the transfer was successful
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(crowdsaleSuccessful);
+        /* Ensures that tokens are not sent to address "0x0" */
+        require(_to != address(0));
+        /* SafeMathMOd.sub will throw if there is not enough balance and if the transfer value is 0. */
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        success = true;
+    }
+    
+    /**
+    * @notice send `_value` tokens to `_to` address from `_from` address if allowance allows
+    *
+    * @param _from The address of the sender
+    * @param _to The address of the recipient
+    * @param _value The amount of token to be transferred
+    * @return Whether the transfer was successful
+    */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(crowdsaleSuccessful);
+        /* Ensures that tokens are not sent to address "0x0" */
+        require(_to != address(0));
+        /* Ensures tokens are not sent to this contract */
+        require(_to != address(this));
+        
+        uint256 allowed = allowance[_from][msg.sender];
+        /* Ensures sender has enough available allowance OR sender is balance holder allowing single transsaction send to contracts*/
+        require(_value <= allowed || _from == msg.sender);
 
-//
+        /* Use SafeMathMod to add and subtract from the _to and _from addresses respectively. Prevents under/overflow and 0 transfers */
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
 
-//
-
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
-
-    function ERC20Token(
-        ) {
-        balances[msg.sender] = 100000000000000000000000000;               
-        totalSupply = 100000000000000000000000000;                        
-        name = "LADoken";                                   
-        decimals = 18;                            
-        symbol = "LAD";                               
+        /* Only reduce allowance if not MAX_UINT256 in order to save gas on unlimited allowance */
+        /* Balance holder does not need allowance to send from self. */
+        if (allowed != MAX_UINT256 && _from != msg.sender) {
+            allowance[_from][msg.sender] = allowed.sub(_value);
+        }
+        Transfer(_from, _to, _value);
+        success = true;
     }
 
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
+    /**
+    * @notice approve `_value` tokens for `_spender` address to send from 'msg.sender'
+    *
+    * @param _spender The address of the approved
+    * @param _value The amount of token to be allowed
+    * @return Whether the allowance was successful
+    */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        /* Ensures address "0x0" is not assigned allowance. */
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
+        success = true;
     }
+    
+    /**
+    * @notice mint `_value` tokens into `_to` address possession
+    *
+    * @param _to The address of the recipient
+    * @param _value The amount of token to be minted
+    * @return Whether the minting was successful
+    */
+    function mintTokens(address _to, uint256 _value) external returns(bool success) {
+        require(msg.sender == presaleAddress || msg.sender == crowdsaleAddress);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        totalSupply = totalSupply.add(_value);
+        Mint(_to,  _value, totalSupply);
+        success = true;
+    }
+    
+    /**
+    * @notice burn all tokens assigned to '_address'
+    *
+    * @param _address whose tokens will be burned
+    * @return Whether the burning was successful
+    */
+    function burnAllTokens(address _address) external returns(bool success) {
+        require(msg.sender == crowdsaleAddress);
+        uint256 amount = balanceOf[_address];
+        balanceOf[_address] = 0;
+        totalSupply = totalSupply.sub(amount);
+        Burn(_address,  amount, totalSupply);
+        success = true;
+    }
+
+    /**
+    * @notice set crowdsaleSuccessful to true
+    */
+    function crowdsaleSucceeded() public {
+        require(msg.sender == crowdsaleAddress);
+        crowdsaleSuccessful = true;
+    }
+    
+    // revert on eth transfers to this contract
+    function() public payable {revert();}
 }
