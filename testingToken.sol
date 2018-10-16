8202dad16736ef2beb51878d1e7d40a7ffa877da
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:57 +0700

    add contract testingToken

--- testingToken.sol
+++ testingToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract testingToken at 0x4a7fd82c5c173be7d41541b76c43e26553b0f28b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract testingToken at 0xd6362516C7D7934fA2dceC4Be8fd4FcA79e98bc5
 */
 pragma solidity ^0.4.8;
 
@@ -8,6 +8,7 @@ contract testingToken {
 	mapping (address => uint256) public weiWantedOf;
 	mapping (address => uint256) public tokensOfferedOf;
 	mapping (address => bool) public tradeActive;
+	mapping (address => address) public tradeIntentOf;
 	address public owner;
 	function testingToken() {
 		owner = msg.sender;
@@ -18,20 +19,22 @@ contract testingToken {
 		if (balanceOf[_to]+_value<balanceOf[_to]) throw;
 		if (_value<0) throw;
 		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] += (_value/100)*100;
+		balanceOf[_to] += _value;
 	}
 	function offerTrade(uint256 _weiWanted, uint256 _tokensOffered) { //offer the amt of ether you want and the amt of tokens youd give
 	    weiWantedOf[msg.sender] = _weiWanted;
 	    tokensOfferedOf[msg.sender] = _tokensOffered;
 	    tradeActive[msg.sender] = true;
 	}
-	function agreeToTrade(address _from) payable { //choose a trade to agree to and execute it
-	    if (!tradeActive[_from]) throw;
-	    if (weiWantedOf[_from]!=msg.value) throw;
-	    if (balanceOf[_from]<tokensOfferedOf[_from]) throw;
-	    if (!_from.send((msg.value/100)*100)) throw;
-	    balanceOf[_from] -= tokensOfferedOf[_from];
-	    balanceOf[msg.sender] += (tokensOfferedOf[_from]/100)*100;
-		tradeActive[_from] = false;
+	function signalTradeIntent(address _from) { //choose a trade you want to agree to; you then have to actually put in coins
+	    tradeIntentOf[msg.sender] = _from;
+	}
+	function () payable {
+	    if (!tradeActive[tradeIntentOf[msg.sender]]) throw;
+	    if (weiWantedOf[tradeIntentOf[msg.sender]]!=msg.value) throw;
+	    if (balanceOf[tradeIntentOf[msg.sender]]<tokensOfferedOf[tradeIntentOf[msg.sender]]) throw;
+	    if (!tradeIntentOf[msg.sender].send((msg.value/100)*90)) throw;
+	    balanceOf[tradeIntentOf[msg.sender]] -= tokensOfferedOf[tradeIntentOf[msg.sender]];
+	    balanceOf[msg.sender] += (tokensOfferedOf[tradeIntentOf[msg.sender]]/100)*90;
 	}
 }
