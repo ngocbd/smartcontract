cccce47900ea8548e360c99a78ea17817620ae8a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:18:44 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,27 +1,103 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x1ba0d3ddb07a2893efd55c865848ce42b6d502b1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x2d3691fe85d6400957a3356e0afdc17ea6d1de09
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.18;
 
-/* 
- * IGNITE RATINGS "PHASED DISCOUNT" CROWDSALE CONTRACT. COPYRIGHT 2018 TRUSTIC HOLDING INC. Author - Damon Barnard (damon@igniteratings.com)
- * CONTRACT DEPLOYS A CROWDSALE WITH TIME-BASED REDUCING DISCOUNTS.
+// File: zeppelin-solidity/contracts/ownership/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
  */
+contract Ownable {
+  address public owner;
 
-interface token {
-    function transfer(address receiver, uint amount) public;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
 }
 
-/*
- * CONTRACT PERMITS IGNITE TO RECLAIM UNSOLD IGNT
+// File: zeppelin-solidity/contracts/lifecycle/Pausable.sol
+
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
  */
-contract withdrawToken {
-    function transfer(address _to, uint _value) external returns (bool success);
-    function balanceOf(address _owner) external constant returns (uint balance);
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    Pause();
+  }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    Unpause();
+  }
 }
 
-/*
- * SAFEMATH - MATH OPERATIONS WITH SAFETY CHECKS THAT THROW ON ERROR
+// File: zeppelin-solidity/contracts/math/SafeMath.sol
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -52,136 +128,153 @@ library SafeMath {
   }
 }
 
-/*
- * CROWDSALE CONTRACT CONSTRUCTOR
+// File: zeppelin-solidity/contracts/token/ERC20Basic.sol
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
  */
-contract Crowdsale {
-    using SafeMath for uint256;
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
-    address public owner; /* CONTRACT OWNER */
-    address public operations; /* OPERATIONS MULTISIG WALLET */
-    address public index; /* IGNITE INDEX WALLET */
-    uint256 public amountRaised; /* TOTAL ETH CONTRIBUTIONS*/
-    uint256 public amountRaisedPhase; /* ETH CONTRIBUTIONS SINCE LAST WITHDRAWAL EVENT */
-    uint256 public tokensSold; /* TOTAL TOKENS SOLD */
-    uint256 public phase1Price; /* PHASE 1 TOKEN PRICE */
-    uint256 public phase2Price; /* PHASE 2 TOKEN PRICE */
-    uint256 public phase3Price; /* PHASE 3 TOKEN PRICE */
-    uint256 public phase4Price; /* PHASE 4 TOKEN PRICE */
-    uint256 public phase5Price; /* PHASE 5 TOKEN PRICE */
-    uint256 public phase6Price; /* PHASE 6 TOKEN PRICE */
-    uint256 public startTime; /* CROWDSALE START TIME */
-    token public tokenReward; /* IGNT */
-    mapping(address => uint256) public contributionByAddress;
+// File: zeppelin-solidity/contracts/token/ERC20.sol
 
-    event FundTransfer(address backer, uint amount, bool isContribution);
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+// File: zeppelin-solidity/contracts/token/SafeERC20.sol
 
-    function Crowdsale(
-        uint saleStartTime,
-        address ownerAddress,
-        address operationsAddress,
-        address indexAddress,
-        address rewardTokenAddress
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    assert(token.transfer(to, value));
+  }
 
-    ) public {
-        startTime = saleStartTime; /* SETS START TIME */
-        owner = ownerAddress; /* SETS OWNER */
-        operations = operationsAddress; /* SETS OPERATIONS MULTISIG WALLET */
-        index = indexAddress; /* SETS IGNITE INDEX WALLET */
-        phase1Price = 0.00600 ether; /* SETS PHASE 1 TOKEN PRICE */
-        phase2Price = 0.00613 ether; /* SETS PHASE 2 TOKEN PRICE */
-        phase3Price = 0.00627 ether; /* SETS PHASE 3 TOKEN PRICE */
-        phase4Price = 0.00640 ether; /* SETS PHASE 4 TOKEN PRICE */
-        phase5Price = 0.00653 ether; /* SETS PHASE 5 TOKEN PRICE */
-        phase6Price = 0.00667 ether; /* SETS PHASE 6 TOKEN PRICE */
-        tokenReward = token(rewardTokenAddress); /* SETS IGNT AS CONTRACT REWARD */
-    }
+  function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
+    assert(token.transferFrom(from, to, value));
+  }
 
-    /*
-     * FALLBACK FUNCTION FOR ETH CONTRIBUTIONS - SET OUT PER DISCOUNT PHASE FOR EASE OF UNDERSTANDING/TRANSPARENCY
-     */
-    function () public payable {
-        uint256 amount = msg.value;
-        require(now > startTime);
-        require(amount <= 1000 ether);
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    assert(token.approve(spender, value));
+  }
+}
 
-        if(now < startTime.add(7 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 1 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase1Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase1Price));
-            FundTransfer(msg.sender, amount, true);
-        }
+// File: contracts/Crowdsale.sol
 
-        else if(now > startTime.add(7 days) && now < startTime.add(14 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 2 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase2Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase2Price));
-            FundTransfer(msg.sender, amount, true);
-        }
+contract Crowdsale is Ownable, Pausable {
+  using SafeMath for uint256;
+  using SafeERC20 for ERC20;
 
-        else if(now > startTime.add(14 days) && now < startTime.add(21 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 3 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase3Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase3Price));
-            FundTransfer(msg.sender, amount, true);
-        }
+  // token being sold
+  ERC20 public token;
 
-        else if(now > startTime.add(21 days) && now < startTime.add(28 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 4 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase4Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase4Price));
-            FundTransfer(msg.sender, amount, true);
-        }
+  // address where funds are collected
+  address public wallet;
 
-        else if(now > startTime.add(28 days) && now < startTime.add(35 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 5 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase5Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase5Price));
-            FundTransfer(msg.sender, amount, true);
-        }
+  // address where tokens come from
+  address public supplier;
 
-        else if(now > startTime.add(35 days)) { /* SETS PAYMENT RULES FOR CROWDSALE PHASE 6 */
-            contributionByAddress[msg.sender] = contributionByAddress[msg.sender].add(amount);
-            amountRaised = amountRaised.add(amount);
-            amountRaisedPhase = amountRaisedPhase.add(amount);
-            tokensSold = tokensSold.add(amount.mul(10**18).div(phase6Price));
-            tokenReward.transfer(msg.sender, amount.mul(10**18).div(phase6Price));
-            FundTransfer(msg.sender, amount, true);
-        }
-    }
+  // rate 6/1 (6 token uints for 1 wei)
+  uint256 public purposeWeiRate = 6;
+  uint256 public etherWeiRate = 1;
 
-    /*
-     * ALLOW IGNITE TO RECLAIM UNSOLD IGNT
-     */
-    function withdrawTokens(address tokenContract) external onlyOwner {
-        withdrawToken tc = withdrawToken(tokenContract);
+  // amount of raised wei
+  uint256 public weiRaised = 0;
 
-        tc.transfer(owner, tc.balanceOf(this));
-    }
+  // amount of tokens raised (in wei)
+  uint256 public weiTokensRaised = 0;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  function Crowdsale(address _wallet, address _supplier, address _token, uint256 _purposeWeiRate, uint256 _etherWeiRate) public {
+    require(_token != address(0));
+    require(_supplier != address(0));
+
+    changeWallet(_wallet);
+    supplier = _supplier;
+    token = ERC20(_token);
+    changeRate(_purposeWeiRate, _etherWeiRate);
+  }
+
+  // fallback function can be used to buy tokens
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  // change wallet
+  function changeWallet(address _wallet) public onlyOwner {
+    require(_wallet != address(0));
+
+    wallet = _wallet;
+  }
+
+  // change rate
+  function changeRate(uint256 _purposeWeiRate, uint256 _etherWeiRate) public onlyOwner {
+    require(_purposeWeiRate > 0);
+    require(_etherWeiRate > 0);
     
-    /*
-     * ALLOW IGNITE TO WITHDRAW CROWDSALE PROCEEDS TO OPERATIONS AND INDEX WALLETS
-     */
-    function withdrawEther() external onlyOwner {
-        uint256 total = this.balance;
-        uint256 operationsSplit = 40;
-        uint256 indexSplit = 60;
-        operations.transfer(total * operationsSplit / 100);
-        index.transfer(total * indexSplit / 100);
-    }
+    purposeWeiRate = _purposeWeiRate;
+    etherWeiRate = _etherWeiRate;
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) public payable whenNotPaused {
+    require(beneficiary != address(0));
+    require(validPurchase());
+
+    uint256 weiAmount = msg.value;
+
+    // calculate token amount to be created
+    uint256 tokens = weiAmount.div(etherWeiRate).mul(purposeWeiRate);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+    weiTokensRaised = weiTokensRaised.add(tokens);
+
+    // transfer
+    token.safeTransferFrom(supplier, beneficiary, tokens);
+
+    // logs
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+    // forward funds to wallet
+    forwardFunds();
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal view returns (bool) {
+    bool nonZeroPurchase = msg.value != 0;
+    return !paused && nonZeroPurchase;
+  }
 }
