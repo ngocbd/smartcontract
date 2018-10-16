43bc592b7005d4a7611aee4f46050004d3a16848
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:04:15 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,105 +1,136 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x8385bfdb96c285f63af35bda5ed9ef615eedc3d2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x251c81dd9e05d6c8e00a05381272c6946e2657ec
 */
-pragma solidity ^0.4.11;
+pragma solidity ^ 0.4.15;
 
+/**
+*library name : SafeMath
+*purpose : be the library for the smart contract for the swap between the godz and ether
+*goal : to achieve the secure basic math operations
+*/
+library SafeMath {
+
+  /*function name : mul*/
+  /*purpose : be the funcion for safe multiplicate*/
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    /*assert(a == 0 || c / a == b);*/
+    return c;
+  }
+
+  /*function name : div*/
+  /*purpose : be the funcion for safe division*/
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  /*function name : sub*/
+  /*purpose : be the funcion for safe substract*/
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /*function name : add*/
+  /*purpose : be the funcion for safe sum*/
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+/**
+*contract name : tokenRecipient
+*/
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+
+/**
+*contract name : Token
+*/
 contract Token {
-    string public symbol;
+    /*using the secure math library for basic math operations*/
+    using SafeMath for uint256;
+
+    /* Public variables of the token */
+    string public standard = 'DSCS.GODZ.TOKEN';
     string public name;
+    string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
 
-    // Balances for each account
-    mapping(address => uint256) balances;
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    // Owner of account approves the transfer of an amount to another account
-    mapping(address => mapping (address => uint256)) allowed;
-
+    /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed from, address indexed to, uint256 value);
 
-    //Modifiers
-
-    modifier when_can_transfer(address _from, uint256 _value) {
-        require (balances[_from] >= _value);
-        _;
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Token(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;                  /* Give the creator all initial tokens*/
+        totalSupply = initialSupply;                            /* Update total supply*/
+        name = tokenName;                                       /* Set the name for display purposes*/
+        symbol = tokenSymbol;                                   /* Set the symbol for display purposes*/
+        decimals = decimalUnits;                                /* Amount of decimals for display purposes*/
     }
 
-    modifier when_can_receive(address _recipient, uint256 _value) {
-        require (balances[_recipient] + _value > balances[_recipient]);
-        _;
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (_to == 0x0) revert();                               /* Prevent transfer to 0x0 address. Use burn() instead*/
+        if (balanceOf[msg.sender] < _value) revert();           /* Check if the sender has enough*/
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); /* Check for overflows*/
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);                        /* Subtract from the sender*/
+        balanceOf[_to] = balanceOf[_to].add(_value);                               /* Add the same to the recipient*/
+        Transfer(msg.sender, _to, _value);                      /* Notify anyone listening that this transfer took place*/
     }
 
-    modifier when_is_allowed(address _from, address _delegate, uint256 _value) {
-        require (allowed[_from][_delegate] >= _value);
-        _;
-    }
-
-    // Constructor
-    function Token(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
-        balances[msg.sender] = initialSupply;
-        totalSupply = initialSupply;
-        decimals = decimalUnits;
-        symbol = tokenSymbol;
-        name = tokenName;
-    }
-
-    function totalSupply() constant returns (uint256 _totalSupply) {
-        _totalSupply = totalSupply;
-    }
-
-
-    // What is the balance of a particular account?
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    // Transfer the balance from owner's account to another account
-    function transfer(address _to, uint256 _amount)
-      when_can_transfer(msg.sender, _amount)
-      when_can_receive(_to, _amount)
-      returns (bool success) {
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
-        Transfer(msg.sender, _to, _amount);
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
         return true;
     }
 
-    // Send _value amount of tokens from address _from to address _to
-    // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-    // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-    // fees in sub-currencies; the command should fail unless the _from account has
-    // deliberately authorized the sender of the message via some mechanism:
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    )
-      when_can_transfer(_from, _amount)
-      when_can_receive(_to, _amount)
-      when_is_allowed(_from, msg.sender, _amount)
-      returns (bool success) {
-        allowed[_from][msg.sender] -= _amount;
-        balances[_from] -= _amount;
-        balances[_to] += _amount;
-        Transfer(_from, _to, _amount);
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /* A contract attempts to get the coins but transfer from the origin*/
+    function transferFromOrigin(address _to, uint256 _value)  returns (bool success) {
+        address origin = tx.origin;
+        if (origin == 0x0) revert();
+        if (_to == 0x0) revert();                                /* Prevent transfer to 0x0 address.*/
+        if (balanceOf[origin] < _value) revert();                /* Check if the sender has enough*/
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  /* Check for overflows*/
+        balanceOf[origin] = balanceOf[origin].sub(_value);       /* Subtract from the sender*/
+        balanceOf[_to] = balanceOf[_to].add(_value);             /* Add the same to the recipient*/
         return true;
     }
 
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (_to == 0x0) revert();                                /* Prevent transfer to 0x0 address.*/
+        if (balanceOf[_from] < _value) revert();                 /* Check if the sender has enough*/
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  /* Check for overflows*/
+        if (_value > allowance[_from][msg.sender]) revert();     /* Check allowance*/
+        balanceOf[_from] = balanceOf[_from].sub(_value);                              /* Subtract from the sender*/
+        balanceOf[_to] = balanceOf[_to].add(_value);                                /* Add the same to the recipient*/
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    // Fallback function throws when called.
-    function() {
-      require(true);
-    }
 }
