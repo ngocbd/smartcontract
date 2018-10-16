b33a905e00400e514ea729f94a17cbf6364716fd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:57:53 +0700

    add contract WithdrawDAO

--- WithdrawDAO.sol
+++ WithdrawDAO.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WithdrawDAO at 0x73e25c92d1669af02142f556c1918a1de537ba0e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WithdrawDAO at 0xc13e4ed5e7f8af83663c9e5086de85b1f914a863
 */
-// Refund contract for trust DAO #66
+// Refund contract for trust DAO #65
 
 contract DAO {
     function balanceOf(address addr) returns (uint);
@@ -10,8 +10,8 @@ contract DAO {
 }
 
 contract WithdrawDAO {
-    DAO constant public mainDAO = DAO(0xd164b088bd9108b60d0ca3751da4bceb207b0782);
-    address public trustee = 0x9a9d6a470fc8034085ee8e509623e2f742da6625;
+    DAO constant public mainDAO = DAO(0xa2f1ccba9395d7fcb155bba8bc92db9bafaeade7);
+    address public trustee = 0x208e4a03118380b4f63cf056ecbfe0a241a41b46;
 
     function withdraw(){
         uint balance = mainDAO.balanceOf(msg.sender);
