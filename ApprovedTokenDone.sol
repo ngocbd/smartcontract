5b489fd1e7fe39c51dd0d8139929125c1a46e318
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:56:28 +0700

    add contract ApprovedTokenDone

--- ApprovedTokenDone.sol
+++ ApprovedTokenDone.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ApprovedTokenDone at 0xddf2b3c69070186c5daaa98a440929a1a79e2411
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ApprovedTokenDone at 0xdd1eb182e1b17b2bf937131a8a861e6eeb792f2b
 */
 pragma solidity ^0.4.13;
 
@@ -153,8 +153,8 @@ contract StandardToken is ERC20, BasicToken {
  * Created specifically for TokenDone.io
  */
 contract ApprovedTokenDone is StandardToken {
-	string public name = 'GTA';
-	string public symbol = 'GTA';
+	string public name = 'MINECRAFT';
+	string public symbol = 'MINECRAFT';
 	uint public decimals = 18;
 	uint public initialSupply = 101111101011110010110010;
 	string public publishier = 'TokenDone.io';
