8e05d499a0da4c96724ed1a4237baa3ca7148e36
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:46:57 +0700

    add contract EthereumPrivate

--- EthereumPrivate.sol
+++ EthereumPrivate.sol
@@ -1,101 +1,156 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthereumPrivate at 0xb1caa1bfbd25f16ec5c4c94780d393ed87572b0a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthereumPrivate at 0x5510f1996e210659e135b05d6bf48d6682af23a7
 */
 pragma solidity ^0.4.16;
 
-contract EthereumPrivate{
-    /* This creates an array with all balances */
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+
+contract EthereumPrivate  {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 8;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
+
+    // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    string public name = "EthereumPrivate";
-    string public symbol = "PETH";
-    uint256 public max_supply = 18000000000000;
-    uint256 public unspent_supply = 0;
-    uint256 public spendable_supply = 0;
-    uint256 public circulating_supply = 0;
-    uint256 public decimals = 6;
-    uint256 public reward = 50000000;
-    uint256 public timeOfLastHalving = now;
-    uint public timeOfLastIncrease = now;
-
+    // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed from, uint256 value);
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function howCoin() public {
-      timeOfLastHalving = now;
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function TokenERC20(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = 100000000000000000;  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = "EthereumPrivate";                                   // Set the name for display purposes
+        symbol = "ETHP";                               // Set the symbol for display purposes
     }
 
-    function updateSupply() internal returns (uint256) {
-
-      if (now - timeOfLastHalving >= 2100000 minutes) {
-        reward /= 2;
-        timeOfLastHalving = now;
-      }
-
-      if (now - timeOfLastIncrease >= 150 seconds) {
-        uint256 increaseAmount = ((now - timeOfLastIncrease) / 150 seconds) * reward;
-        spendable_supply += increaseAmount;
-        unspent_supply += increaseAmount;
-        timeOfLastIncrease = now;
-      }
-
-      circulating_supply = spendable_supply - unspent_supply;
-
-      return circulating_supply;
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    /* Send coins */
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
     function transfer(address _to, uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
-        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
-        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
-
-        updateSupply();
-
-        /* Notify anyone listening that the transfer took place */
-        Transfer(msg.sender, _to, _value);
-
-    }
-    /* Mint new coins by sending ether */
-    function mint() public payable {
-        require(balanceOf[msg.sender] + _value >= balanceOf[msg.sender]); // Check for overflows
-        uint256 _value = msg.value / 100000000;
-
-        updateSupply();
-
-        require(unspent_supply - _value <= unspent_supply);
-        unspent_supply -= _value; // Remove from unspent supply
-        balanceOf[msg.sender] += _value; // Add the same to the recipient
-
-        updateSupply();
-
-        /* Notify anyone listening that the minting took place */
-        Mint(msg.sender, _value);
-
+        _transfer(msg.sender, _to, _value);
     }
 
-    function withdraw(uint256 amountToWithdraw) public returns (bool) {
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
 
-        // Balance given in HOW
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
 
-        require(balanceOf[msg.sender] >= amountToWithdraw);
-        require(balanceOf[msg.sender] - amountToWithdraw <= balanceOf[msg.sender]);
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
 
-        // Balance checked in HOW, then converted into Wei
-        balanceOf[msg.sender] -= amountToWithdraw;
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
 
-        // Added back to supply in HOW
-        unspent_supply += amountToWithdraw;
-        // Converted into Wei
-        amountToWithdraw *= 100000000;
-
-        // Transfered in Wei
-        msg.sender.transfer(amountToWithdraw);
-
-        updateSupply();
-
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
         return true;
     }
 }
