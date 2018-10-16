5b9e893c04a5a68f995ea9d2305db48ca867b924
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:14:14 +0700

    add contract BurnContract

---
+++ BurnContract.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BurnContract at 0x0732673feae6ac9899cf078146ebb157ab264099
+*/
+pragma solidity ^0.4.8;
+
+contract BurnContract {
+    
+}
