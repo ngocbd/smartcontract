fc0aa9d1dac7487bb3cf2697bfb51b593886d87f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:44:35 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,85 +1,406 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xb8ffb08a557e40460b0dc1c2c27077a137a4de0a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x715423a818f1f9a85c66d81d2809e0a4dadf07f3
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.18;
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+contract ApproveAndCallReceiver {
+    function receiveApproval(
+    address _from,
+    uint256 _amount,
+    address _token,
+    bytes _data
+    ) public;
+}
 
-contract Token {
-    
-    string public name;
-    string public symbol;
-    uint8 public decimals = 6;
+//normal contract. already compiled as bin
+contract Controlled {
+    modifier onlyController {
+        require(msg.sender == controller);
+        _;
+    }
+    address public controller;
+
+    function Controlled() public {
+        controller = msg.sender;
+    }
+
+    function changeController(address _newController) onlyController public {
+        controller = _newController;
+    }
+}
+
+
+contract ERC20Token {
+
+    /// total amount of tokens
     uint256 public totalSupply;
+    //function totalSupply() public constant returns (uint256 balance);
 
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    // function balanceOf(address _owner) public constant returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    mapping (address => mapping (address => uint256)) public allowance;
+    //function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract TokenI is ERC20Token, Controlled {
+
+    string public name;                //The Token's name: e.g. DigixDAO Tokens
+    uint8 public decimals;             //Number of decimals of the smallest unit
+    string public symbol;              //An identifier: e.g. REP
+
+
+    // ERC20 Methods
+
+    /// @notice `msg.sender` approves `_spender` to send `_amount` tokens on
+    ///  its behalf, and then a function is triggered in the contract that is
+    ///  being approved, `_spender`. This allows users to use their tokens to
+    ///  interact with contracts in one function call instead of two
+    /// @param _spender The address of the contract able to transfer the tokens
+    /// @param _amount The amount of tokens to be approved for transfer
+    /// @return True if the function call was successful
+    function approveAndCall(
+    address _spender,
+    uint256 _amount,
+    bytes _extraData
+    ) public returns (bool success);
+
+
+    // Generate and destroy tokens
+
+    /// @notice Generates `_amount` tokens that are assigned to `_owner`
+    /// @param _owner The address that will be assigned the new tokens
+    /// @param _amount The quantity of tokens generated
+    /// @return True if the tokens are generated correctly
+    function generateTokens(address _owner, uint _amount) public returns (bool);
+
+
+    /// @notice Burns `_amount` tokens from `_owner`
+    /// @param _owner The address that will lose the tokens
+    /// @param _amount The quantity of tokens to burn
+    /// @return True if the tokens are burned correctly
+    function destroyTokens(address _owner, uint _amount) public returns (bool);
+
+}
+
+contract Token is TokenI {
+
+    struct FreezeInfo {
+    address user;
+    uint256 amount;
+    }
+    //Key1: step(????); Key2: user sequence(????)
+    mapping (uint8 => mapping (uint8 => FreezeInfo)) public freezeOf; //?????key ????????????????
+    mapping (uint8 => uint8) public lastFreezeSeq; //??? freezeOf ???key: step; value: sequence
+    mapping (address => uint256) public airdropOf;//????
+
+    address public owner;
+    bool public paused=false;//??????
+    uint256 public minFunding = 1 ether;  //??????
+    uint256 public airdropQty=0;//??????????
+    uint256 public airdropTotalQty=0;//???????????
+    uint256 public tokensPerEther = 9000;//1eth??????(9000,3000,1500)
+    address private vaultAddress;//????ETH???
+    uint256 public totalCollected = 0;//????ETH????
+
+    /* This generates a public event on the blockchain that will notify clients */
+    //event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* This notifies clients about the amount burnt */
     event Burn(address indexed from, uint256 value);
 
+    /* This notifies clients about the amount frozen */
+    event Freeze(address indexed from, uint256 value);
 
-    function Token (
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol) 
-        public {
-        totalSupply = initialSupply * 10 ** uint256(decimals); 
-        balanceOf[msg.sender] = totalSupply;           
-        name = tokenName;                           
-        symbol = tokenSymbol; }
+    /* This notifies clients about the amount unfrozen */
+    event Unfreeze(address indexed from, uint256 value);
 
+    event Payment(address sender, uint256 _ethAmount, uint256 _tokenAmount);
 
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances); }
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Token(
+    uint256 initialSupply,
+    string tokenName,
+    uint8 decimalUnits,
+    string tokenSymbol,
+    address _vaultAddress
+    ) public {
+        require(_vaultAddress != 0);
+        totalSupply = initialSupply * 10 ** uint256(decimalUnits);
+        balanceOf[msg.sender] = totalSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = decimalUnits;
+        owner = msg.sender;
+        vaultAddress=_vaultAddress;
+    }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value); }
+    modifier realUser(address user){
+        if(user == 0x0){
+            revert();
+        }
+        _;
+    }
 
+    modifier moreThanZero(uint256 _value){
+        if (_value <= 0){
+            revert();
+        }
+        _;
+    }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);  
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true; }
+    /// @dev Internal function to determine if an address is a contract
+    /// @param _addr The address being queried
+    /// @return True if `_addr` is a contract
+    function isContract(address _addr) constant internal returns(bool) {
+        uint size;
+        if (_addr == 0) {
+            return false;
+        }
+        assembly {
+        size := extcodesize(_addr)
+        }
+        return size>0;
+    }
 
+    /* Send coins */
+    function transfer(address _to, uint256 _value) realUser(_to) moreThanZero(_value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;                     // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to] + _value;                            // Add the same to the recipient
+        emit Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        return true;
+    }
 
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value) moreThanZero(_value) public
+    returns (bool success) {
         allowance[msg.sender][_spender] = _value;
-        return true; }
+        return true;
+    }
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true; } }
+    /**
+     * @notice `msg.sender` approves `_spender` to send `_amount` tokens on
+     *  its behalf, and then a function is triggered in the contract that is
+     *  being approved, `_spender`. This allows users to use their tokens to
+     *  interact with contracts in one function call instead of two
+     * @param _spender The address of the contract able to transfer the tokens
+     * @param _amount The amount of tokens to be approved for transfer
+     * @return True if the function call was successful
+     */
+    function approveAndCall(address _spender, uint256 _amount, bytes _extraData) public returns (bool success) {
+        require(approve(_spender, _amount));
+        ApproveAndCallReceiver(_spender).receiveApproval(
+        msg.sender,
+        _amount,
+        this,
+        _extraData
+        );
+
+        return true;
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) realUser(_from) realUser(_to) moreThanZero(_value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                 // Check if the sender has enough
+        require(balanceOf[_to] + _value > balanceOf[_to]);  // Check for overflows
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        balanceOf[_from] = balanceOf[_from] - _value;                           // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to] + _value;                             // Add the same to the recipient
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] + _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function transferMulti(address[] _to, uint256[] _value) public returns (uint256 amount){
+        require(_to.length == _value.length);
+        uint8 len = uint8(_to.length);
+        for(uint8 j; j<len; j++){
+            amount += _value[j]*10**uint256(decimals);
+        }
+        require(balanceOf[msg.sender] >= amount);
+        for(uint8 i; i<len; i++){
+            address _toI = _to[i];
+            uint256 _valueI = _value[i]*10**uint256(decimals);
+            balanceOf[_toI] += _valueI;
+            balanceOf[msg.sender] -= _valueI;
+            emit Transfer(msg.sender, _toI, _valueI);
+        }
+    }
+
+    //????
+    function freeze(address _user, uint256 _value, uint8 _step) moreThanZero(_value) onlyController public returns (bool success) {
+        _value=_value*10**uint256(decimals);
+        return _freeze(_user,_value,_step);
+    }
+
+    function _freeze(address _user, uint256 _value, uint8 _step) moreThanZero(_value) private returns (bool success) {
+        //info256("balanceOf[_user]", balanceOf[_user]);
+        require(balanceOf[_user] >= _value);
+        balanceOf[_user] = balanceOf[_user] - _value;
+        freezeOf[_step][lastFreezeSeq[_step]] = FreezeInfo({user:_user, amount:_value});
+        lastFreezeSeq[_step]++;
+        emit Freeze(_user, _value);
+        return true;
+    }
 
 
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;            
-        totalSupply -= _value;                  
-        Burn(msg.sender, _value);
-        return true; }
+    //?????????
+    function unFreeze(uint8 _step) onlyOwner public returns (bool unlockOver) {
+        //_end = length of freezeOf[_step]
+        uint8 _end = lastFreezeSeq[_step];
+        require(_end > 0);
+        unlockOver=false;
+        uint8  _start=0;
+        for(; _end>_start; _end--){
+            FreezeInfo storage fInfo = freezeOf[_step][_end-1];
+            uint256 _amount = fInfo.amount;
+            balanceOf[fInfo.user] += _amount;
+            delete freezeOf[_step][_end-1];
+            lastFreezeSeq[_step]--;
+            emit Unfreeze(fInfo.user, _amount);
+        }
+    }
 
 
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);  
-        balanceOf[_from] -= _value;                         
-        allowance[_from][msg.sender] -= _value;         
-        totalSupply -= _value;                              
-        Burn(_from, _value);
-        return true; }
+    ////////////////
+    // Generate and destroy tokens
+    ////////////////
+
+    /// @notice Generates `_amount` tokens that are assigned to `_owner`
+    /// @param _user The address that will be assigned the new tokens
+    /// @param _amount The quantity of tokens generated
+    /// @return True if the tokens are generated correctly
+    function generateTokens(address _user, uint _amount) onlyController public returns (bool) {
+        _amount=_amount*10**uint256(decimals);
+        return _generateTokens(_user,_amount);
+    }
+
+    function _generateTokens(address _user, uint _amount)  private returns (bool) {
+        require(balanceOf[owner] >= _amount);
+        balanceOf[_user] += _amount;
+        balanceOf[owner] -= _amount;
+        emit Transfer(0, _user, _amount);
+        return true;
+    }
+
+    /// @notice Burns `_amount` tokens from `_owner`
+    /// @param _user The address that will lose the tokens
+    /// @param _amount The quantity of tokens to burn
+    /// @return True if the tokens are burned correctly
+    function destroyTokens(address _user, uint256 _amount) onlyOwner public returns (bool) {
+        _amount=_amount*10**uint256(decimals);
+        return _destroyTokens(_user,_amount);
+    }
+
+    function _destroyTokens(address _user, uint256 _amount) private returns (bool) {
+        require(balanceOf[_user] >= _amount);
+        balanceOf[owner] += _amount;
+        balanceOf[_user] -= _amount;
+        emit Transfer(_user, 0, _amount);
+        emit Burn(_user, _amount);
+        return true;
+    }
+
+
+    function changeOwner(address newOwner) onlyOwner public returns (bool) {
+        balanceOf[newOwner] += balanceOf[owner];
+        balanceOf[owner] = 0;
+        owner = newOwner;
+        return true;
+    }
+
+
+    /**
+     * ??token????,1eth??????
+     */
+    function changeTokensPerEther(uint256 _newRate) onlyController public {
+        tokensPerEther = _newRate;
+    }
+
+    /**
+     * ?????????????
+     */   
+    function changeAirdropQty(uint256 _airdropQty) onlyController public {
+        airdropQty = _airdropQty;
+    }
+
+    /**
+     * ??????
+     */   
+    function changeAirdropTotalQty(uint256 _airdropTotalQty) onlyController public {
+        uint256 _token =_airdropTotalQty*10**uint256(decimals);
+        require(balanceOf[owner] >= _token);
+        airdropTotalQty = _airdropTotalQty;
+    }
+
+        ////////////////
+    // ???????
+    ////////////////
+    function changePaused(bool _paused) onlyController public {
+        paused = _paused;
+    }
+
+    //accept ether
+    function() payable public {
+        require(!paused);
+        address _user=msg.sender;
+        uint256 tokenValue;
+        if(msg.value==0){//??
+            require(airdropQty>0);
+            require(airdropTotalQty>=airdropQty);
+            require(airdropOf[_user]==0);
+            tokenValue=airdropQty*10**uint256(decimals);
+            airdropOf[_user]=tokenValue;
+            airdropTotalQty-=airdropQty;
+            require(_generateTokens(_user, tokenValue));
+            emit Payment(_user, msg.value, tokenValue);
+        }else{
+            require(msg.value >= minFunding);//????
+            require(msg.value % 1 ether==0);//??????eth
+            totalCollected +=msg.value;
+            require(vaultAddress.send(msg.value));//?eth??????
+            tokenValue = (msg.value/1 ether)*(tokensPerEther*10 ** uint256(decimals));
+            require(_generateTokens(_user, tokenValue));
+            //??30%?????????6?????35%?1?????????SPM
+            //
+            require(_freeze(_user, tokenValue*35/100, 0));
+            _freeze(_user, tokenValue*35/100, 1);
+            emit Payment(_user, msg.value, tokenValue);
+        }
+    }
 }
