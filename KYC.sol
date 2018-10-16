b60922433a30ce8e6ed4feb78b52746eb2d117db
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:52:06 +0700

    add contract KYC

--- KYC.sol
+++ KYC.sol
@@ -1,18 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KYC at 0xa96cae028ef41a2a8d7167c4c3645bd99e24daee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KYC at 0xc9e8045616abbdf535fda1fdbfe04b4f42101b2e
 */
 pragma solidity ^0.4.18;
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+
+
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -20,13 +11,20 @@ contract ERC20Basic {
  */
 contract Ownable {
   address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() {
+  function Ownable() public {
     owner = msg.sender;
   }
+
+
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -34,15 +32,23 @@ contract Ownable {
     require(msg.sender == owner);
     _;
   }
+
+
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner {
+  function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
+
 }
+
+
+
+
 /**
  * @title KYC
  * @dev KYC contract handles the white list for ASTCrowdsale contract
@@ -51,21 +57,27 @@ contract Ownable {
  */
 contract KYC is Ownable {
   // check the address is registered for token sale
-  mapping (address => bool) public registeredAddress;
+  // first boolean is true if presale else false
+  // second boolean is true if registered else false
+  mapping (address => mapping (bool => bool)) public registeredAddress;
+
   // check the address is admin of kyc contract
   mapping (address => bool) public admin;
+
   event Registered(address indexed _addr);
   event Unregistered(address indexed _addr);
-  event NewAdmin(address indexed _addr);
-  event ClaimedTokens(address _token, address owner, uint256 balance);
+  event SetAdmin(address indexed _addr);
+
   /**
    * @dev check whether the address is registered for token sale or not.
    * @param _addr address
+   * @param _isPresale bool Whether the address is registered to presale or mainsale
    */
-  modifier onlyRegistered(address _addr) {
-    require(registeredAddress[_addr]);
+  modifier onlyRegistered(address _addr, bool _isPresale) {
+    require(registeredAddress[_addr][_isPresale]);
     _;
   }
+
   /**
    * @dev check whether the msg.sender is admin or not
    */
@@ -73,81 +85,99 @@ contract KYC is Ownable {
     require(admin[msg.sender]);
     _;
   }
-  function KYC() {
+
+  function KYC() public {
     admin[msg.sender] = true;
   }
+
   /**
    * @dev set new admin as admin of KYC contract
    * @param _addr address The address to set as admin of KYC contract
    */
-  function setAdmin(address _addr)
+  function setAdmin(address _addr, bool _value)
     public
     onlyOwner
+    returns (bool)
   {
-    require(_addr != address(0) && admin[_addr] == false);
-    admin[_addr] = true;
-    NewAdmin(_addr);
+    require(_addr != address(0));
+    require(admin[_addr] == !_value);
+
+    admin[_addr] = _value;
+
+    SetAdmin(_addr);
+
+    return true;
   }
+
+  /**
+   * @dev check the address is register
+   * @param _addr address The address to check
+   * @param _isPresale bool Whether the address is registered to presale or mainsale
+   */
+  function isRegistered(address _addr, bool _isPresale)
+    public
+    view
+    returns (bool)
+  {
+    return registeredAddress[_addr][_isPresale];
+  }
+
   /**
    * @dev register the address for token sale
    * @param _addr address The address to register for token sale
+   * @param _isPresale bool Whether register to presale or mainsale
    */
-  function register(address _addr)
+  function register(address _addr, bool _isPresale)
     public
     onlyAdmin
   {
-    require(_addr != address(0) && registeredAddress[_addr] == false);
-    registeredAddress[_addr] = true;
+    require(_addr != address(0) && registeredAddress[_addr][_isPresale] == false);
+
+    registeredAddress[_addr][_isPresale] = true;
+
     Registered(_addr);
   }
+
   /**
    * @dev register the addresses for token sale
    * @param _addrs address[] The addresses to register for token sale
+   * @param _isPresale bool Whether register to presale or mainsale
    */
-  function registerByList(address[] _addrs)
+  function registerByList(address[] _addrs, bool _isPresale)
     public
     onlyAdmin
   {
     for(uint256 i = 0; i < _addrs.length; i++) {
-      require(_addrs[i] != address(0) && registeredAddress[_addrs[i]] == false);
-      registeredAddress[_addrs[i]] = true;
-      Registered(_addrs[i]);
+      register(_addrs[i], _isPresale);
     }
   }
+
   /**
    * @dev unregister the registered address
    * @param _addr address The address to unregister for token sale
+   * @param _isPresale bool Whether unregister to presale or mainsale
    */
-  function unregister(address _addr)
+  function unregister(address _addr, bool _isPresale)
     public
     onlyAdmin
-    onlyRegistered(_addr)
+    onlyRegistered(_addr, _isPresale)
   {
-    registeredAddress[_addr] = false;
+    registeredAddress[_addr][_isPresale] = false;
+
     Unregistered(_addr);
   }
+
   /**
    * @dev unregister the registered addresses
    * @param _addrs address[] The addresses to unregister for token sale
+   * @param _isPresale bool Whether unregister to presale or mainsale
    */
-  function unregisterByList(address[] _addrs)
+  function unregisterByList(address[] _addrs, bool _isPresale)
     public
     onlyAdmin
   {
     for(uint256 i = 0; i < _addrs.length; i++) {
-      require(registeredAddress[_addrs[i]]);
-      registeredAddress[_addrs[i]] = false;
-      Unregistered(_addrs[i]);
+      unregister(_addrs[i], _isPresale);
     }
-  }
-  function claimTokens(address _token) public onlyOwner {
-    if (_token == 0x0) {
-        owner.transfer(this.balance);
-        return;
-    }
-    ERC20Basic token = ERC20Basic(_token);
-    uint256 balance = token.balanceOf(this);
-    token.transfer(owner, balance);
-    ClaimedTokens(_token, owner, balance);
   }
 }
