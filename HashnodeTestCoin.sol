62b7cd5a32d224c00b8a7f6895c3e51e9f30e607
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:07:00 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0xbcdc1db0f0cc80919833992ed2b87597a5effa0c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x6a00b19b2752f3f5de0db60e7007afe749b3f8ad
 */
 pragma solidity ^0.4.4;
 
@@ -108,12 +108,12 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function HashnodeTestCoin() {
-        balances[msg.sender] = 1000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Philadelphia Eagles, LII Champions";                                   // Set the name for display purposes (CHANGE THIS)
-        decimals = 0;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "PHL";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 200;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        balances[msg.sender] = 9000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 9000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "DreamHolderCoin";                                   // Set the name for display purposes (CHANGE THIS)
+        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
+        symbol = "HT3KCoin";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
 
