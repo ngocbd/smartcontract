52d89eac7b62da8dc15c222bb8e4465e59dbf5b4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:58:58 +0700

    add contract CrowdsaleProxy

--- CrowdsaleProxy.sol
+++ CrowdsaleProxy.sol
@@ -1,234 +1,100 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleProxy at 0x0d8d860c117ed7d41a73bd3abb10a8b250c38b1a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleProxy at 0x85ac27e4124863492db18eafbb358102cad8fc96
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.21;
 
-/**
- * ICrowdsale
- *
- * Base crowdsale interface to manage the sale of 
- * an ERC20 token
- *
- * #created 09/09/2017
- * #author Frank Bonnet
- */
-interface ICrowdsale {
-
-    /**
-     * Returns true if the contract is currently in the presale phase
-     *
-     * @return True if in presale phase
-     */
-    function isInPresalePhase() public view returns (bool);
-
-
-    /**
-     * Returns true if the contract is currently in the ended stage
-     *
-     * @return True if ended
-     */
-    function isEnded() public view returns (bool);
-
-
-    /**
-     * Returns true if `_beneficiary` has a balance allocated
-     *
-     * @param _beneficiary The account that the balance is allocated for
-     * @param _releaseDate The date after which the balance can be withdrawn
-     * @return True if there is a balance that belongs to `_beneficiary`
-     */
-    function hasBalance(address _beneficiary, uint _releaseDate) public view returns (bool);
-
-
-    /** 
-     * Get the allocated token balance of `_owner`
-     * 
-     * @param _owner The address from which the allocated token balance will be retrieved
-     * @return The allocated token balance
-     */
-    function balanceOf(address _owner) public view returns (uint);
-
-
-    /** 
-     * Get the allocated eth balance of `_owner`
-     * 
-     * @param _owner The address from which the allocated eth balance will be retrieved
-     * @return The allocated eth balance
-     */
-    function ethBalanceOf(address _owner) public view returns (uint);
-
-
-    /** 
-     * Get invested and refundable balance of `_owner` (only contributions during the ICO phase are registered)
-     * 
-     * @param _owner The address from which the refundable balance will be retrieved
-     * @return The invested refundable balance
-     */
-    function refundableEthBalanceOf(address _owner) public view returns (uint);
-
-
-    /**
-     * Returns the rate and bonus release date
-     *
-     * @param _phase The phase to use while determining the rate
-     * @param _volume The amount wei used to determine what volume multiplier to use
-     * @return The rate used in `_phase` multiplied by the corresponding volume multiplier
-     */
-    function getRate(uint _phase, uint _volume) public view returns (uint);
-
-
-    /**
-     * Convert `_wei` to an amount in tokens using 
-     * the `_rate`
-     *
-     * @param _wei amount of wei to convert
-     * @param _rate rate to use for the conversion
-     * @return Amount in tokens
-     */
-    function toTokens(uint _wei, uint _rate) public view returns (uint);
-
-
-    /**
-     * Receive ether and issue tokens to the sender
-     * 
-     * This function requires that msg.sender is not a contract. This is required because it's 
-     * not possible for a contract to specify a gas amount when calling the (internal) send() 
-     * function. Solidity imposes a maximum amount of gas (2300 gas at the time of writing)
-     * 
-     * Contracts can call the contribute() function instead
-     */
-    function () public payable;
-
-
-    /**
-     * Receive ether and issue tokens to the sender
-     *
-     * @return The accepted ether amount
-     */
-    function contribute() public payable returns (uint);
-
-
-    /**
-     * Receive ether and issue tokens to `_beneficiary`
-     *
-     * @param _beneficiary The account that receives the tokens
-     * @return The accepted ether amount
-     */
-    function contributeFor(address _beneficiary) public payable returns (uint);
-
-
-    /**
-     * Withdraw allocated tokens
-     */
-    function withdrawTokens() public;
-
-
-    /**
-     * Withdraw allocated ether
-     */
-    function withdrawEther() public;
-
-
-    /**
-     * Refund in the case of an unsuccessful crowdsale. The 
-     * crowdsale is considered unsuccessful if minAmount was 
-     * not raised before end of the crowdsale
-     */
-    function refund() public;
+contract CrowdsaleProxyTarget {
+    function isActive() public returns(bool);
+    function initialize(address _owner, address _token, address _familyOwner, address _personalOwner) public;
+    address public token;
 }
 
-
 /**
- * ICrowdsaleProxy
- *
- * #created 23/11/2017
- * #author Frank Bonnet
+ * The CrowdsaleProxy contract which uses crowdsale implementation deployed at
+ * target address. This constuction helps to make Crowdsale code upgradable.
  */
-interface ICrowdsaleProxy {
+contract CrowdsaleProxy {
+    bytes32 constant TARGET_POSITION = keccak256("CrowdsaleProxy.target");
+    bytes32 constant OWNER_POSITION = keccak256("CrowdsaleProxy.owner");
 
-    /**
-     * Receive ether and issue tokens to the sender
-     * 
-     * This function requires that msg.sender is not a contract. This is required because it's 
-     * not possible for a contract to specify a gas amount when calling the (internal) send() 
-     * function. Solidity imposes a maximum amount of gas (2300 gas at the time of writing)
-     * 
-     * Contracts can call the contribute() function instead
-     */
-    function () public payable;
+    event Upgraded(address indexed target);
 
-
-    /**
-     * Receive ether and issue tokens to the sender
-     *
-     * @return The accepted ether amount
-     */
-    function contribute() public payable returns (uint);
-
-
-    /**
-     * Receive ether and issue tokens to `_beneficiary`
-     *
-     * @param _beneficiary The account that receives the tokens
-     * @return The accepted ether amount
-     */
-    function contributeFor(address _beneficiary) public payable returns (uint);
-}
-
-
-/**
- * CrowdsaleProxy
- *
- * #created 22/11/2017
- * #author Frank Bonnet
- */
-contract CrowdsaleProxy is ICrowdsaleProxy {
-
-    address public owner;
-    ICrowdsale public target;
-    
-
-    /**
-     * Deploy proxy
-     *
-     * @param _owner Owner of the proxy
-     * @param _target Target crowdsale
-     */
-    function CrowdsaleProxy(address _owner, address _target) public {
-        target = ICrowdsale(_target);
-        owner = _owner;
+    modifier _onlyProxyOwner() {
+        require(msg.sender == ___proxyOwner());
+        _;
     }
 
+    function CrowdsaleProxy(address _target) public {
+        require(_target != 0x0);
+        bytes32 position = OWNER_POSITION;
+        assembly { sstore(position, caller) }
+        ___setTarget(_target);
+    }
 
-    /**
-     * Receive contribution and forward to the crowdsale
-     * 
-     * This function requires that msg.sender is not a contract. This is required because it's 
-     * not possible for a contract to specify a gas amount when calling the (internal) send() 
-     * function. Solidity imposes a maximum amount of gas (2300 gas at the time of writing)
-     */
+    function ___initialize(address _token, address _familyOwner, address _personalOwner) public {
+        CrowdsaleProxyTarget(this).initialize(msg.sender, _token, _familyOwner, _personalOwner);
+    }
+
     function () public payable {
-        target.contributeFor.value(msg.value)(msg.sender);
+        address _target = ___proxyTarget();
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+            let success := delegatecall(sub(gas, 10000), _target, ptr, calldatasize, 0, 0)
+            let retSz := returndatasize
+            returndatacopy(ptr, 0, retSz)
+
+            switch success
+            case 0 { revert(ptr, retSz) }
+            default { return(ptr, retSz) }
+        }
     }
 
-
-    /**
-     * Receive ether and issue tokens to the sender
-     *
-     * @return The accepted ether amount
-     */
-    function contribute() public payable returns (uint) {
-        target.contributeFor.value(msg.value)(msg.sender);
+    function ___coinAddress() external view returns (address) {
+        return CrowdsaleProxyTarget(this).token();
     }
 
+    function ___isActive() internal returns (bool res) {
+        res = CrowdsaleProxyTarget(this).isActive();
+    }
 
-    /**
-     * Receive ether and issue tokens to `_beneficiary`
-     *
-     * @param _beneficiary The account that receives the tokens
-     * @return The accepted ether amount
-     */
-    function contributeFor(address _beneficiary) public payable returns (uint) {
-        target.contributeFor.value(msg.value)(_beneficiary);
+    function ___proxyOwner() public view returns (address owner) {
+        bytes32 position = OWNER_POSITION;
+        assembly {
+            owner := sload(position)
+        }
+    }
+
+    function ___setProxyOwner(address newOwner) _onlyProxyOwner public {
+        bytes32 position = OWNER_POSITION;
+        assembly {
+            sstore(position, newOwner)
+        }
+    }
+
+    function ___proxyTarget() public view returns (address target) {
+        bytes32 position = TARGET_POSITION;
+        assembly {
+            target := sload(position)
+        }
+    }
+
+    function ___setTarget(address target) internal {
+        bytes32 position = TARGET_POSITION;
+        assembly {
+            sstore(position, target)
+        }
+    }
+
+    function ___upgradeTo(address newTarget) public _onlyProxyOwner {
+        require(!___isActive());
+        require(___proxyTarget() != newTarget);
+        ___setTarget(newTarget);
+        emit Upgraded(___proxyTarget());
+    }
+
+    function ___upgradeToAndCall(address newTarget, bytes data) payable public _onlyProxyOwner {
+        ___upgradeTo(newTarget);
+        require(address(this).call.value(msg.value)(data));
     }
 }
