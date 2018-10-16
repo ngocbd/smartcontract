99097f97fc1c6f0b94d9e013b46e540141efa9fc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:00:13 +0700

    add contract AddressNames

--- AddressNames.sol
+++ AddressNames.sol
@@ -1,23 +1,44 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressNames at 0x3d42f7eb6b97ab66d8d44c725651befe02a70e5e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressNames at 0x2f81a082e11ee3ab3b063bb9605affc8e3121926
 */
 pragma solidity 0.4.4;
 
 contract AddressNames{
 
 	mapping(address => string) addressNames;
+	address[] namedAddresses;
 
 	function setName(string name){
-		if(bytes(name).length >= 3){
-			addressNames[msg.sender] = name;
-		}
+		_setNameToAddress(msg.sender,name);
 	}
 
 	function hasName(address who) constant returns (bool hasAName){
-		hasAName = bytes(addressNames[who]).length != 0;
+		hasAName = _hasName(who);
 	}
 
 	function getName(address who) constant returns (string name){
 		name = addressNames[who];
 	}
+	
+	function getNamedAddresses() constant returns (address[] addresses){
+		addresses = namedAddresses;
+	}
+
+	function _setNameToAddress(address who, string name) internal returns (bool valid){
+		if (bytes(name).length < 3){
+		valid = false;
+		}
+
+		if (!_hasName(who)){
+			namedAddresses.push(who);
+		}
+		addressNames[msg.sender] = name;
+		
+		valid = true;
+	}
+
+	function _hasName(address who) internal returns (bool hasAName){
+		hasAName = bytes(addressNames[who]).length != 0;
+	}
+
 }
