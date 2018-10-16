19866bf108aba009562e8a82d02028a8644769c6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:11:56 +0700

    add contract AirDrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,76 +1,101 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x66ab72953a529a79d5e4f275108a0854a880eecf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0xbcf6c06912f668060dea7d0e2dbbe636e3e548e5
 */
-pragma solidity ^0.4.19;
+/*! airdrop.sol | (c) 2018 BelovITLab LLC | License: MIT */
+//
+//       ???????????                                                                                                                             
+//    ?????        ????                                                                                                                          
+//   ??  ???     ??   ???                                                                                                                        
+//  ??  ?? ??  ??  ?    ??       ???       ???      ???      ?????????    ??    ???       ????????   ????????      ???        ????????  ?????????
+// ??    ????   ????    ??       ????     ????     ?? ??     ???    ???   ??  ???         ??         ??     ??    ?? ??     ???         ???      
+// ??     ??    ??       ?       ??????  ?????    ??   ??    ???   ????   ?????           ???????    ??   ????   ??   ??    ??          ???????? 
+// ??     ???? ????     ??       ???  ???? ???   ?????????   ????????     ??? ???               ??   ???????    ?????????   ???         ???      
+//  ??   ??  ??  ??     ??       ???   ?   ???  ??       ??  ???    ??    ??    ???  ??   ????????   ??        ??       ??   ?????????  ???????? 
+//   ??               ???                                                                                                                        
+//    ?????        ????                                                                                                                          
+//        ??????????                                                                                                                             
 
-/**
- * @title Token
- * @dev Simpler version of ERC20 interface
- */
-contract Token {
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+pragma solidity 0.4.18;
 
 contract Ownable {
-  address public owner;
+    address public owner;
 
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    modifier onlyOwner() { require(msg.sender == owner); _; }
 
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        owner = newOwner;
+        OwnershipTransferred(owner, newOwner);
+    }
 }
 
-contract AirDrop is Ownable {
+contract Withdrawable is Ownable {
+    function withdrawEther(address _to, uint _value) onlyOwner public returns(bool) {
+        require(_to != address(0));
+        require(this.balance >= _value);
 
-  // This declares a state variable that would store the contract address
-  Token public tokenInstance;
+        _to.transfer(_value);
 
-  /*
-    constructor function to set token address
-   */
-  function AirDrop(address _tokenAddress){
-    tokenInstance = Token(_tokenAddress);
-  }
-
-  /*
-    Airdrop function which take up a array of address and amount and call the
-    transfer function to send the token
-   */
-  function doAirDrop(address[] _address, uint256 _amount) onlyOwner public returns (bool) {
-    uint256 count = _address.length;
-    for (uint256 i = 0; i < count; i++)
-    {
-      /* calling transfer function from contract */
-      tokenInstance.transfer(_address [i],_amount);
+        return true;
     }
-  }
+
+    function withdrawTokens(ERC20 _token, address _to, uint _value) onlyOwner public returns(bool) {
+        require(_to != address(0));
+
+        return _token.transfer(_to, _value);
+    }
+}
+
+contract ERC20 {
+    uint256 public totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function balanceOf(address who) public view returns(uint256);
+    function transfer(address to, uint256 value) public returns(bool);
+    function transferFrom(address from, address to, uint256 value) public returns(bool);
+    function allowance(address owner, address spender) public view returns(uint256);
+    function approve(address spender, uint256 value) public returns(bool);
+}
+
+contract AirDrop is Withdrawable {
+    event TransferEther(address indexed to, uint256 value);
+
+    function tokenBalanceOf(ERC20 _token) public view returns(uint256) {
+        return _token.balanceOf(this);
+    }
+
+    function tokenAllowance(ERC20 _token, address spender) public view returns(uint256) {
+        return _token.allowance(this, spender);
+    }
+    
+    function tokenTransfer(ERC20 _token, uint _value, address[] _to) onlyOwner public {
+        require(_token != address(0));
+
+        for(uint i = 0; i < _to.length; i++) {
+            require(_token.transfer(_to[i], _value));
+        }
+    }
+    
+    function tokenTransferFrom(ERC20 _token, address spender, uint _value, address[] _to) onlyOwner public {
+        require(_token != address(0));
+
+        for(uint i = 0; i < _to.length; i++) {
+            require(_token.transferFrom(spender, _to[i], _value));
+        }
+    }
+
+    function etherTransfer(uint _value, address[] _to) onlyOwner payable public {
+        for(uint i = 0; i < _to.length; i++) {
+            _to[i].transfer(_value);
+            TransferEther(_to[i], _value);
+        }
+    }
 }
