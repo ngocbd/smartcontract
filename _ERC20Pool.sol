fa812248489d03b96d7585a5d39542caa603b60d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:04:22 +0700

    add contract _ERC20Pool

--- _ERC20Pool.sol
+++ _ERC20Pool.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract _ERC20Pool at 0x96fd68b56946126d25d8cb6e7df68cb8ed13152a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract _ERC20Pool at 0x77719ee4dc68a71085b26e7792054f9aad32cab0
 */
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.19; 
 /*
 Author: Vox / 0xPool.io
 Description: This smart contract is designed to store mining pool payouts for 
@@ -16,6 +16,8 @@ Description: This smart contract is designed to store mining pool payouts for
 This contract is the 0xBTC (0xBitcoin) payout account for: http://0xpool.io 
 
 Not heard of 0xBitcoin? Head over to http://0xbitcoin.org
+
+May the Qat be with you. 
 */
 
 contract ERC20Interface {
@@ -41,12 +43,16 @@ contract ERC20Interface {
 
 
 contract _ERC20Pool {
+    
+  using SafeMath for uint32;
+
+  // 0xB6eD7644C69416d67B522e20bC294A9a9B405B31 is the 0xBitcoin Smart Contract
   ERC20Interface public tokenContract = ERC20Interface(0xB6eD7644C69416d67B522e20bC294A9a9B405B31);
 
   address public owner = msg.sender;
   uint32 public totalTokenSupply;
-  mapping (address => uint32) minerTokens;
-  mapping (address => uint32) minerTokenPayouts;
+  mapping (address => uint32) public minerTokens;
+  mapping (address => uint32) public minerTokenPayouts;
 
   // Modifier for important owner only functions
   modifier onlyOwner() {
@@ -55,8 +61,8 @@ contract _ERC20Pool {
   }
 
   // Require that the caller actually has tokens to withdraw.
-  modifier hasTokens(address sentFrom) {
-    require(minerTokens[sentFrom] > 0);
+  modifier hasTokens() {
+    require(minerTokens[msg.sender] > 0);
     _;
   }
 
@@ -64,25 +70,81 @@ contract _ERC20Pool {
   function addMinerTokens(uint32 totalTokensInBatch, address[] minerAddress, uint32[] minerRewardTokens) public onlyOwner {
     totalTokenSupply += totalTokensInBatch;
     for (uint i = 0; i < minerAddress.length; i ++) {
-      minerTokens[minerAddress[i]] += minerRewardTokens[i];
+      minerTokens[minerAddress[i]] = minerTokens[minerAddress[i]].add(minerRewardTokens[i]);
     }
   }
-
+  
   // Allow miners to withdraw their earnings from the contract. Update internal accounting.
   function withdraw() public
-    hasTokens(msg.sender) 
+    hasTokens
   {
     uint32 amount = minerTokens[msg.sender];
     minerTokens[msg.sender] = 0;
-    totalTokenSupply -= amount;
-    minerTokenPayouts[msg.sender] += amount;
+    totalTokenSupply = totalTokenSupply.sub(amount);
+    minerTokenPayouts[msg.sender] = minerTokenPayouts[msg.sender].add(amount);
     tokenContract.transfer(msg.sender, amount);
   }
-
-  // Fallback function, Ether sent to this contract will be considered as a donation towards the 
-  // 0xPool & 0xBitcoin projects unless you get in contact with 0xPool.io within 72 hours. 
+  
+  // Fallback function, It's kind of you to send Ether, but we prefer to handle the true currency of
+  // Ethereum here, 0xBitcoin!
   function () public payable {
-    owner.transfer(msg.value);
+    revert();
   }
+  
+  // Allow the owner to retrieve accidentally sent Ethereum
+  function withdrawEther(uint32 amount) public onlyOwner {
+    owner.transfer(amount);
+  }
+  
+  // Allows the owner to transfer any accidentally sent ERC20 Tokens, excluding 0xBitcoin.
+  function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+    if(tokenAddress == 0xB6eD7644C69416d67B522e20bC294A9a9B405B31 ){ 
+        revert(); 
+    }
+    return ERC20Interface(tokenAddress).transfer(owner, tokens);
+  }
+  
+}
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    /**
+     * @dev Multiplies two numbers, throws on overflow.
+     */
+     function mul(uint32 a, uint32 b) internal pure returns (uint32) {
+     if (a == 0) {
+     return 0;
+     }
+     uint32 c = a * b;
+     assert(c / a == b);
+     return c;
+     }
+    /**
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
+     function div(uint32 a, uint32 b) internal pure returns (uint32) {
+     // assert(b > 0); // Solidity automatically throws when dividing by 0
+     uint32 c = a / b;
+     // assert(a == b * c + a % b); // There is no case in which this doesn’t hold
+     return c;
+     }
+    /**
+     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+     */
+     function sub(uint32 a, uint32 b) internal pure returns (uint32) {
+     assert(b <= a);
+     uint32 c = a - b;
+     return c;
+     }
+    /**
+     * @dev Adds two numbers, throws on overflow.
+     */
+     function add(uint32 a, uint32 b) internal pure returns (uint32) {
+     uint32 c = a + b;
+     assert(c >= a);
+     return c;
+     }
 }
