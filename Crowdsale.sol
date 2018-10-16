a393ad2c5d2cd8766c0c5cb1ec9331851c5975ca
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:54:38 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,107 +1,298 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xb25cef763846fe25185de5cf970fdf2ac13fc16d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x63ffad099717cc3fc0d83d001fb94740ae47ee46
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.23;
 
-interface token {
-    function transfer(address receiver, uint amount);
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale,
+ * allowing investors to purchase tokens with ether. This contract implements
+ * such functionality in its most fundamental form and can be extended to provide additional
+ * functionality and/or custom behavior.
+ * The external interface represents the basic interface for purchasing tokens, and conform
+ * the base architecture for crowdsales. They are *not* intended to be modified / overriden.
+ * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
+ * the methods to add functionality. Consider using 'super' where appropiate to concatenate
+ * behavior.
+ */
 contract Crowdsale {
-    address public beneficiary;
-    uint public amountRaised;
-    token public tokenReward;
-    uint256 public soldTokensCounter;
-    uint public price;
-    uint public saleStage = 1;
-    bool public crowdsaleClosed = false;
-    bool public adminVer = false;
-    mapping(address => uint256) public balanceOf;
+  using SafeMath for uint256;
 
-    event FundTransfer(address backer, uint amount, uint price, bool isContribution);
+  // The token being sold
+  ERC20 public token;
 
-    /**
-     * Constrctor function
-     *
-     * Setup the owner
-     */
-    function Crowdsale() {
-        beneficiary = msg.sender;
-        tokenReward = token(0x745Fa4002332C020f6a05B3FE04BCCf060e36dD3);
-    }
+  // Owner address
+  address public owner;
 
-    modifier onlyOwner {
-        require(msg.sender == beneficiary);
-        _;
-    }
+  // Address where funds are collected
+  address public wallet;
 
-    /**
-     * Check ownership
-     */
-    function checkAdmin() onlyOwner {
-        adminVer = true;
-    }
+  // How many token units a buyer gets per wei
+  uint256 public rate;
 
-    /**
-     * Change crowdsale discount stage
-     */
-    function changeStage(uint stage) onlyOwner {
-        saleStage = stage;
-    }
+  // Amount of wei raised
+  uint256 public weiRaised;
 
-    /**
-     * Return unsold tokens to beneficiary address
-     */
-    function getUnsoldTokens(uint val_) onlyOwner {
-        tokenReward.transfer(beneficiary, val_);
-    }
+  modifier onlyOwner() { 
+    require (msg.sender == owner); 
+    _;
+  }
+  
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(
+    address indexed purchaser,
+    address indexed beneficiary,
+    uint256 value,
+    uint256 amount
+  );
 
-    /**
-     * Return unsold tokens to beneficiary address with decimals
-     */
-    function getUnsoldTokensWithDecimals(uint val_, uint dec_) onlyOwner {
-        val_ = val_ * 10 ** dec_;
-        tokenReward.transfer(beneficiary, val_);
-    }
+  /**
+   * @param _rate Number of token units a buyer gets per wei
+   * @param _wallet Address where collected funds will be forwarded to
+   * @param _token Address of the token being sold
+   */
+  constructor(uint256 _rate, address _wallet, ERC20 _token) public {
+    require(_rate > 0);
+    require(_wallet != address(0));
+    require(_token != address(0));
 
-    /**
-     * Close/Open crowdsale
-     */
-    function closeCrowdsale(bool closeType) onlyOwner {
-        crowdsaleClosed = closeType;
-    }
+    owner = msg.sender;
+    rate = _rate;
+    wallet = _wallet;
+    token = _token;
+  }
 
-    /**
-     * Return current token price
-     *
-     * The price depends on `saleStage` and `amountRaised`
-     */
-    function getPrice() returns (uint) {
-        if (saleStage == 4) {
-            return 0.0002000 ether;
-        } else if (saleStage == 3) {
-            return 0.0001667 ether;
-        } else if (saleStage == 2) {
-            return 0.0001429 ether;
-        }
-        return 0.000125 ether;
-    }
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
 
-    /**
-     * Fallback function
-     *
-     * The function without name is the default function that is called whenever anyone sends funds to a contract
-     */
-    function () payable {
-        require(!crowdsaleClosed);                                                         
-        price = getPrice();                                                                //get current token price
-        uint amount = msg.value;                                                           //save users eth value
-        balanceOf[msg.sender] += amount;                                                   //save users eth value in balance list 
-        amountRaised += amount;                                                            //update total amount of crowdsale
-        uint sendTokens = (amount / price) * 10 ** uint256(18);                            //calculate user's tokens
-        tokenReward.transfer(msg.sender, sendTokens);                                      //send tokens to user
-        soldTokensCounter += sendTokens;                                                   //update total sold tokens counter
-        FundTransfer(msg.sender, amount, price, true);                                     //pin transaction data in blockchain
-        if (beneficiary.send(amount)) { FundTransfer(beneficiary, amount, price, false); } //send users amount to beneficiary
-    }
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    _processPurchase(_beneficiary, tokens);
+    emit TokenPurchase(
+      msg.sender,
+      _beneficiary,
+      weiAmount,
+      tokens
+    );
+
+    _forwardFunds();
+
+  }
+
+  // -----------------------------------------
+  // owner only function
+  // -----------------------------------------
+   /**
+   * @dev owner transfer token to specific address, directly
+   * @param _beneficiary Address
+   * @param _tokenAmount to _beneficiary address
+   */
+  function ownerTokenTransfer(address _beneficiary, uint _tokenAmount) onlyOwner {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /*
+   * @dev owner set owner
+   * @param new owner address
+   */ 
+  function ownerSetOwner(address _newOwner) onlyOwner {
+    owner = _newOwner;
+  }
+
+  /*
+   * @dev owner set new wallet
+   * @param new wallet to collect funds
+   */ 
+  function ownerSetWallet(address _newWallet) onlyOwner {
+    wallet = _newWallet;
+  }
+
+  /*
+   * @dev owner set new wallet
+   * @param new wallet to collect funds
+   */ 
+  function ownerSetRate(uint256 _newRate) onlyOwner {
+    rate = _newRate;
+  }
+
+  /*
+   * @dev owner selfdestruct contract ***BE CAREFUL! EMERGENCY ONLY*** 
+   * before self destruct, execute ownerTokenTransfer() to GET TOKEN OUT.
+   */ 
+  function ownerSelfDestruct() onlyOwner {
+    selfdestruct(owner);
+  }
+
+
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+  }
+
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(
+    address _beneficiary,
+    uint256 _tokenAmount
+  )
+    internal
+  {
+    token.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(
+    address _beneficiary,
+    uint256 _tokenAmount
+  )
+    internal
+  {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount)
+    internal view returns (uint256)
+  {
+    return _weiAmount.mul(rate);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
 }
