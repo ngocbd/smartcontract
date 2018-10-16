ed03f06a0baf5fa7bc984ad18fd590bcd562f80f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:11 +0700

    add contract AlarmClockTipFaucet

---
+++ AlarmClockTipFaucet.sol
@@ -0,0 +1,42 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AlarmClockTipFaucet at 0x450dc30a8b1a26e3e5ba6102216276b9c77716bc
+*/
+contract AlarmClockTipFaucet {
+// Alarm Clock 0.8 is on its way, adding time-based scheduling to Ethereum
+
+// This is a contract for tipping the dev for the work leading up to this 0.8 release
+
+// The TipFaucet is open for 10 days, after which the dev can withdraw a clump-sum
+
+address piperMerriam;
+uint timeToPayout;
+
+
+function AlarmClockTipFaucet() {
+    piperMerriam = 0xd3cda913deb6f67967b99d67acdfa1712c293601;
+    timeToPayout = now + 10 days;
+}
+
+modifier isPiper { 
+if (msg.sender != piperMerriam) throw;
+_
+}
+
+modifier isOpen {
+if(block.timestamp > timeToPayout) throw;
+_
+}
+
+modifier canWithdraw {
+if(block.timestamp < timeToPayout) throw;
+_
+}
+
+function() isOpen {
+}
+
+function withdraw() isPiper canWithdraw {
+    msg.sender.send(this.balance);
+}
+
+}
