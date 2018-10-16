cccf98dd17242121e248af1b30d1ad7bf515e606
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:27:34 +0700

    add contract XG4K

--- XG4K.sol
+++ XG4K.sol
@@ -1,28 +1,38 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XG4K at 0x376206e05b41c386a5e4aa9061e7e2d8d41ed073
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XG4K at 0x1ad392cb01caa44a1afdfca5b36434b0eb312420
 */
 pragma solidity ^0.4.4;
+
 /* This currency XG4K/ETH can only be issued by the coiner Xgains4keeps owner of 
 the Equity4keeps programme and can be transferred to anyone or entity.
 */
-contract XG4K {	
-	mapping (address => uint) public balances;
-	function XG4K() {
-		balances[tx.origin] = 100000;
-	}
-	function sendToken(address receiver, uint amount) returns(bool successful){
-		if (balances[msg.sender] < amount) return false;
- 		balances[msg.sender] -= amount;
- 		balances[receiver] += amount;
- 		return false;
- 	}
-} 
-contract coinSpawn{
- 	mapping(uint => XG4K) deployedContracts;
-	uint numContracts;
-	function createCoin() returns(address a){
-		deployedContracts[numContracts] = new XG4K();
-		numContracts++;
-		return deployedContracts[numContracts];
-	}
+
+contract XG4K {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public coiner;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Issue(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function XG4K() public {
+        coiner = msg.sender;
+        balances[msg.sender] = 100000;
+    }
+
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != coiner) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Issue(msg.sender, receiver, amount);
+    }
 }
