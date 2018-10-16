d45bf09835f3bb958bc2ac34a054e47b00d7b02c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:06:02 +0700

    add contract Test

--- test.sol
+++ test.sol
@@ -1,18 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0xdd29e9818ddf08253486006ae0f82efb2f50db2e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0xd2bc942e03dca509ab897896bd5e1ad6bdda97d7
 */
+pragma solidity ^0.4.2;
 contract Test {
+    function uintToBytes(uint v) constant returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
 
-	mapping (uint32 => bytes32) Cert;	
-	
-	function SetCert (uint32 _IndiceIndex, bytes32 _Cert) {
-		if (msg.sender == 0x46b396728e61741D3AbD6Aa5bfC42610997c32C3) {
-			Cert [_IndiceIndex] = _Cert;
-		}
-	}				
-	
-	function GetCert (uint32 _IndiceIndex) returns (bytes32 _Valeur)  {
-		_Valeur = Cert [_IndiceIndex];
-		return _Valeur;
-	}		
 }
