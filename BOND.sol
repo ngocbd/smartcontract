f19e45f955f0fa235ec935fd8431fb533c090449
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:40:17 +0700

    add contract BOND

--- BOND.sol
+++ BOND.sol
@@ -1,235 +1,226 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bond at 0x3a2a7Ad49B34Ad5777806A78110da234381d8863
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BOND at 0x4ee697e33d72bce48c2170bfc009d801933fcb10
 */
-pragma solidity 0.4.16;
+pragma solidity ^0.4.18;
+
+// ----------------------------------------------------------------------------
+// 'INTERNET' token contract
+//
+// Deployed to : 0xED7d4bC3ea331A559b9c2036161DB2AD0933f638
+// Symbol      : BND
+// Name        : BOND
+// Total supply: 85000000000000
+// Decimals    : 5
+//
+// Enjoy.
+//
+// (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence.
+// ----------------------------------------------------------------------------
 
 
-contract Bond {
-    
-    uint public issuerDateMinutes;
-    string public issuerName;
-    string public name;
-    string public description;
-    uint128 public totalAssetUnits;
-    uint128 public totalFiatValue;
-    uint128 public fiatPerAssetUnit;
-    uint128 public interestRate;
-    string public fiatCurrency;
-    uint16 public paymentPeriods;
-
-    address public owner;
-    string bondID; 
-    address public issuer;
-    address public escrowContract;
-    mapping(address => uint128) balances;
-    
-    bool public matured;
-    uint public matured_block_number;
-    uint public matured_timestamp;
-    
-    event TxExecuted(uint32 indexed event_id);
-
-    function Bond(
-        uint _issuerDateMinutes,
-        string _issuerName,
-        string _name,
-        string _description,
-        uint128 _totalAssetUnits,
-        uint128 _totalFiatValue,
-        uint128 _fiatPerAssetUnit,
-        uint128 _interestRate,
-        uint16 _paymentPeriods,
-        string _bondID,
-        string _fiatCurrency,
-        address _escrowContract) {
-            issuerDateMinutes = _issuerDateMinutes;
-            issuerName = _issuerName;
-            name = _name;
-            description = _description;
-            totalAssetUnits = _totalAssetUnits;
-            totalFiatValue = _totalFiatValue;
-            fiatPerAssetUnit = _fiatPerAssetUnit;
-            interestRate = _interestRate;
-            paymentPeriods = _paymentPeriods;
-            fiatCurrency = _fiatCurrency;
-                        
-            owner = msg.sender;
-            bondID = _bondID;
-            escrowContract = _escrowContract;
-            matured = false;
+// ----------------------------------------------------------------------------
+// Safe maths
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
     }
-    
-    modifier onlyOwner() {
-        if(msg.sender == owner) _;
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
     }
-    
-    modifier onlyIssuer() {
-        if(msg.sender == issuer) _;
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
     }
-    
-    function setMatured(uint32 event_id) onlyOwner returns (bool success) {
-        if(matured==false){
-            matured = true;
-            matured_block_number = block.number;
-            matured_timestamp = block.timestamp;
-            TxExecuted(event_id);
-        }        
-        return true;
-    }
-    
-    function checkBalance(address account) constant returns (uint128 _balance) {
-        if(matured)
-            return 0;
-        return balances[account];
-    }
-    
-    function getTotalSupply() constant returns (uint supply) {
-        return totalAssetUnits;
-    }
-    
-    function setIssuer(address _issuer, uint32 event_id) onlyOwner returns (bool success) {
-        if(matured==false && issuer==address(0)){
-            issuer = _issuer;
-            balances[_issuer] = totalAssetUnits;
-            TxExecuted(event_id);
-            return true;
-        }
-        return false;
-    }
-    
-    function getIssuer() constant returns (address _issuer) {
-        return issuer;
-    }
-    
-    struct Transfer {
-        uint128 lockAmount;
-        bytes32 currencyAndBank;
-        address executingBond;
-        address lockFrom;
-        address issuer;
-        uint128 assetAmount;
-        uint128 balancesIssuer;
-        uint32 event_id;
-        bool first;
-        bool second;
-    }
-    mapping (bytes16 => Transfer) public transferBond; 
-    function transfer(uint128 assetAmount, bytes16 lockID, uint32 event_id) onlyIssuer returns (bool success) {
-        if(matured==false){
-            uint128 lockAmount;
-            bytes32 currencyAndBank;
-            address executingBond;
-            address lockFrom;
-            transferBond[lockID].assetAmount = assetAmount;
-            transferBond[lockID].event_id = event_id;
-            Escrow escrow = Escrow(escrowContract);        
-            (lockAmount, currencyAndBank, lockFrom, executingBond) = escrow.lockedMoney(lockID);
-            transferBond[lockID].lockAmount = lockAmount;
-            transferBond[lockID].currencyAndBank = currencyAndBank;
-            transferBond[lockID].executingBond = executingBond;
-            transferBond[lockID].lockFrom = lockFrom;
-            transferBond[lockID].issuer = issuer;
-            transferBond[lockID].balancesIssuer = balances[issuer];
-            transferBond[lockID].first = balances[issuer]>=assetAmount;
-            transferBond[lockID].second = escrow.executeLock(lockID, issuer)==true;        
-            if(transferBond[lockID].first && transferBond[lockID].second){ 
-                balances[lockFrom] += assetAmount;
-                balances[issuer] -= assetAmount;
-                TxExecuted(event_id);
-                return true;
-            }
-        }
-        return false;
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
     }
 }
 
-contract Escrow{
-    
-    function Escrow() {
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+
+// ----------------------------------------------------------------------------
+// Contract function to receive approval and execute function in one call
+//
+// Borrowed from MiniMeToken
+// ----------------------------------------------------------------------------
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    function Owned() public {
         owner = msg.sender;
     }
 
-    mapping (address => mapping (bytes32 => uint128)) public balances;
-    mapping (bytes16 => Lock) public lockedMoney;
-    address public owner;
-    
-    struct Lock {
-        uint128 amount;
-        bytes32 currencyAndBank;
-        address from;
-        address executingBond;
-    }
-    
-    event TxExecuted(uint32 indexed event_id);
-    
-    modifier onlyOwner() {
-        if(msg.sender == owner)
+    modifier onlyOwner {
+        require(msg.sender == owner);
         _;
     }
-    
-    function checkBalance(address acc, string currencyAndBank) constant returns (uint128 balance) {
-        bytes32 cab = sha3(currencyAndBank);
-        return balances[acc][cab];
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
     }
-    
-    function getLocked(bytes16 lockID) returns (uint) {
-        return lockedMoney[lockID].amount;
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
     }
-    
-    function deposit(address to, uint128 amount, string currencyAndBank, uint32 event_id) 
-        onlyOwner returns(bool success) {
-            bytes32 cab = sha3(currencyAndBank);
-            balances[to][cab] += amount;
-            TxExecuted(event_id);
-            return true;
-    } 
-    
-    function withdraw(uint128 amount, string currencyAndBank, uint32 event_id) 
-        returns(bool success) {
-            bytes32 cab = sha3(currencyAndBank);
-            require(balances[msg.sender][cab] >= amount);
-            balances[msg.sender][cab] -= amount;
-            TxExecuted(event_id);
-            return true;
+}
+
+
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals and assisted
+// token transfers
+// ----------------------------------------------------------------------------
+contract BOND is ERC20Interface, Owned, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    function BOND() public {
+        symbol = "BND";
+        name = "BOND";
+        decimals = 5;
+        _totalSupply = 85000000000000;
+        balances[0xED7d4bC3ea331A559b9c2036161DB2AD0933f638] = _totalSupply;
+        Transfer(address(0), 0xED7d4bC3ea331A559b9c2036161DB2AD0933f638, _totalSupply);
     }
-    
-    function lock(uint128 amount, string currencyAndBank, address executingBond, bytes16 lockID, uint32 event_id) 
-        returns(bool success) {   
-            bytes32 cab = sha3(currencyAndBank);
-            require(balances[msg.sender][cab] >= amount);
-            balances[msg.sender][cab] -= amount;
-            lockedMoney[lockID].currencyAndBank = cab;
-            lockedMoney[lockID].amount += amount;
-            lockedMoney[lockID].from = msg.sender;
-            lockedMoney[lockID].executingBond = executingBond;
-            TxExecuted(event_id);
-            return true; 
+
+
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
-    
-    function executeLock(bytes16 lockID, address issuer) returns(bool success) {
-        if(msg.sender == lockedMoney[lockID].executingBond){
-	        balances[issuer][lockedMoney[lockID].currencyAndBank] += lockedMoney[lockID].amount;            
-	        delete lockedMoney[lockID];
-	        return true;
-		}else
-		    return false;
+
+
+    // ------------------------------------------------------------------------
+    // Get the token balance for account tokenOwner
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return balances[tokenOwner];
     }
-    
-    function unlock(bytes16 lockID, uint32 event_id) onlyOwner returns (bool success) {
-        balances[lockedMoney[lockID].from][lockedMoney[lockID].currencyAndBank] +=
-            lockedMoney[lockID].amount;
-        delete lockedMoney[lockID];
-        TxExecuted(event_id);
+
+
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to to account
+    // - Owner's account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        Transfer(msg.sender, to, tokens);
         return true;
     }
-    
-    function pay(address to, uint128 amount, string currencyAndBank, uint32 event_id) 
-        returns (bool success){
-            bytes32 cab = sha3(currencyAndBank);
-            require(balances[msg.sender][cab] >= amount);
-            balances[msg.sender][cab] -= amount;
-            balances[to][cab] += amount;
-            TxExecuted(event_id);
-            return true;
+
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account
+    //
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+    // recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces 
+    // ------------------------------------------------------------------------
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Transfer tokens from the from account to the to account
+    // 
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the from account and
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        Transfer(from, to, tokens);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account. The spender contract function
+    // receiveApproval(...) is then executed
+    // ------------------------------------------------------------------------
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Don't accept ETH
+    // ------------------------------------------------------------------------
+    function () public payable {
+        revert();
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
     }
 }
