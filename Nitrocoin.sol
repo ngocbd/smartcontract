5100e4d96600d6e7671b684f47fea492bd821ef8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:42:54 +0700

    add contract NitroCoin

--- Nitrocoin.sol
+++ Nitrocoin.sol
@@ -1,82 +1,164 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Nitrocoin at 0x4248d5eDC7a30Fd1954C4e4c0262Dc6ee5606D99
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NitroCoin at 0x2f5d5c785953ffdfdb1141c1f1ec462a1bad62e0
 */
-pragma solidity ^ 0.4 .9;
+pragma solidity ^0.4.18;
+
+
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
     }
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+  }
 
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
 
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    return a - b;
+  }
 
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
 }
-contract Nitrocoin {
-    using SafeMath
-    for uint256;
-    mapping(address => mapping(address => uint256)) allowed;
+
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+contract NitroCoin is Ownable{
+    
+    using SafeMath for uint256;
+    
+    string public constant name       = "NitroCoin";
+    string public constant symbol     = "NRC";
+    uint32 public constant decimals   = 18;
+    uint256 public totalSupply        = 25000000 ether;
+    uint256 public currentTotalSupply = 0;
+    uint256 startBalance              = 10 ether;
+    
+    mapping(address => bool) touched;
     mapping(address => uint256) balances;
-    uint256 public totalSupply;
-    uint256 public decimals;
-    address public owner;
-    bytes32 public symbol;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed spender, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
 
-    function Nitrocoin() {
-        totalSupply = 25000000;
-        symbol = 'nrc';
-        owner = 0x1eca6ef43a7561b5ccffaa81a26d28cf6f3b8f6c;
-        balances[owner] = totalSupply;
-        decimals = 0;
-    }
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
 
-    function balanceOf(address _owner) constant returns(uint256 balance) {
-        return balances[_owner];
-    }
-
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    function transfer(address _to, uint256 _value) returns(bool) {
+        if( !touched[msg.sender] && currentTotalSupply < totalSupply ){
+            balances[msg.sender] = balances[msg.sender].add( startBalance );
+            touched[msg.sender] = true;
+            currentTotalSupply = currentTotalSupply.add( startBalance );
+        }
+        
+        require(_value <= balances[msg.sender]);
+        
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
+    
         Transfer(msg.sender, _to, _value);
         return true;
     }
+  
 
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
-        var _allowance = allowed[_from][msg.sender];
-        balances[_to] = balances[_to].add(_value);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        
+        require(_value <= allowed[_from][msg.sender]);
+        
+        if( !touched[_from] && currentTotalSupply < totalSupply ){
+            touched[_from] = true;
+            balances[_from] = balances[_from].add( startBalance );
+            currentTotalSupply = currentTotalSupply.add( startBalance );
+        }
+        
+        require(_value <= balances[_from]);
+        
         balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) returns(bool) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function() {
-        revert();
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+     }
+
+
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+          allowed[msg.sender][_spender] = 0;
+        } else {
+          allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+     }
+    
+
+    function getBalance(address _a) internal constant returns(uint256)
+    {
+        if( currentTotalSupply < totalSupply ){
+            if( touched[_a] )
+                return balances[_a];
+            else
+                return balances[_a].add( startBalance );
+        } else {
+            return balances[_a];
+        }
+    }
+    
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return getBalance( _owner );
     }
 }
