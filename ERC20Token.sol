a661e05401ab9bb5c2fba9f5f1d4d13c816d1821
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:23:10 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xa80d19a184ed658691a63ea42bd174a6d095a682
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x5ef42f054f2173ff032f4e3b9d4f924336242bbd
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.19;
 
 contract Token {
 
@@ -106,24 +106,19 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    string public name;                   
+    uint8 public decimals;                
+    string public symbol;                 
+    string public version = 'H1.0';       
 
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
-
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 1000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 1000000;                        // Update total supply (100000 for example)
-        name = "sugar candy mountain coin";                                   // Set the name for display purposes
-        decimals = 2;                            // Amount of decimals for display purposes
-        symbol = "SCM";                               // Set the symbol for display purposes
+        balances[msg.sender] = 64000000000000000000000000;               
+        totalSupply = 64000000000000000000000000;                        
+        name = "Open Token";                                   
+        decimals = 18;                            
+        symbol = "OPN";                               
     }
 
     /* Approves and then calls the receiving contract */
