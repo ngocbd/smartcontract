29544be05a02d3c7235b42282c3a23552743a2a0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:25:31 +0700

    add contract theegalcoin

---
+++ theegalcoin.sol
@@ -0,0 +1,53 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract theegalcoin at 0x9fed70dbf61c30c2c88890045e7a0b5e7c2b4624
+*/
+pragma solidity ^0.4.0;   
+
+
+contract theegalcoin{
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function theegalcoin() {
+
+         initialSupply = 25000000;
+         name ="theegalcoin";
+        decimals = 2;
+         symbol = "tlc";
+        
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+                                   
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
+    }
+
+   
+
+    
+
+   
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
