953329bf41364a7e908104a6ec2023afca3aa740
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:04:14 +0700

    add contract WavesEthereumSwap

--- WavesEthereumSwap.sol
+++ WavesEthereumSwap.sol
@@ -1,28 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WavesEthereumSwap at 0x3B6d241e1b38776C2eFE944E7012239ed59334c1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WavesEthereumSwap at 0x74Aca0F1a40F7e0F3845DadF52C65060dE7749AF
 */
 pragma solidity ^0.4.8;
 
 // ----------------------------------------------------------------------------------------------
+// The Ripto Bux smart contract - to find out more, join the Incent Slack; http://incentinvites.herokuapp.com/
 // A collaboration between Incent and Bok :)
-// Enjoy. (c) Incent Loyalty Pty Ltd, and Bok Consulting Pty Ltd 2017. The MIT Licence.
+// Enjoy. (c) Incent Loyalty Pty Ltd and Bok Consulting Pty Ltd 2017. The MIT Licence.
 // ----------------------------------------------------------------------------------------------
 
-//config contract
+// Contract configuration
 contract TokenConfig {
-
-    string public constant name = "Incent Coffee Token";
-    string public constant symbol = "INCOF";
-    uint8 public constant decimals = 0;  // 0 decimal places, the same as tokens on Wave
-    uint256 _totalSupply = 824;
-
+    string public constant symbol = "RBX";
+    string public constant name = "Ripto Bux";
+    uint8 public constant decimals = 8;  // 8 decimal places, the same as tokens on Wave
+    uint256 _totalSupply = 100000000000000000;
 }
-
 
 // ERC Token Standard #20 Interface
 // https://github.com/ethereum/EIPs/issues/20
 contract ERC20Interface {
-
     // Get the total token supply
     function totalSupply() constant returns (uint256 totalSupply);
 
@@ -33,15 +30,11 @@ contract ERC20Interface {
     function transfer(address _to, uint256 _value) returns (bool success);
 
     // Send _value amount of tokens from address _from to address _to
-    // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-    // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-    // fees in sub-currencies; the command should fail unless the _from account has
-    // deliberately authorized the sender of the message via some mechanism; we propose
-    // these standardized APIs for approval:
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
     // If this function is called again it overwrites the current allowance with _value.
+    // this function is required for some DEX functionality
     function approve(address _spender, uint256 _value) returns (bool success);
 
     // Returns the amount which _spender is still allowed to withdraw from _owner
@@ -54,8 +47,7 @@ contract ERC20Interface {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract IncentCoffeeToken is ERC20Interface, TokenConfig {
-
+contract RiptoBuxToken is ERC20Interface, TokenConfig {
     // Owner of this contract
     address public owner;
 
@@ -74,8 +66,7 @@ contract IncentCoffeeToken is ERC20Interface, TokenConfig {
     }
 
     // Constructor
-    function IncentCoffeeToken() {
-
+    function RiptoBuxToken() {
         owner = msg.sender;
         balances[owner] = _totalSupply;
     }
@@ -91,13 +82,15 @@ contract IncentCoffeeToken is ERC20Interface, TokenConfig {
 
     // Transfer the balance from owner's account to another account
     function transfer(address _to, uint256 _amount) returns (bool success) {
-        if (balances[msg.sender] >= _amount && _amount > 0) {
+        if (balances[msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
             balances[msg.sender] -= _amount;
             balances[_to] += _amount;
             Transfer(msg.sender, _to, _amount);
             return true;
         } else {
-           return false;
+            return false;
         }
     }
 
@@ -111,15 +104,14 @@ contract IncentCoffeeToken is ERC20Interface, TokenConfig {
         address _from,
         address _to,
         uint256 _amount
-    ) returns (bool success) {
-
+) returns (bool success) {
         if (balances[_from] >= _amount
             && allowed[_from][msg.sender] >= _amount
-            && _amount > 0) {
-
-            balances[_to] += _amount;
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
             balances[_from] -= _amount;
             allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
             Transfer(_from, _to, _amount);
             return true;
         } else {
@@ -138,19 +130,13 @@ contract IncentCoffeeToken is ERC20Interface, TokenConfig {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
-
-
 }
 
-contract WavesEthereumSwap is IncentCoffeeToken {
+contract WavesEthereumSwap is RiptoBuxToken {
+    event WavesTransfer(address indexed _from, string wavesAddress, uint256 amount);
 
- event WavesTransfer(address indexed _from, string wavesAddress, uint256 amount);
-
- function moveToWaves(string wavesAddress, uint256 amount) {
-
-     if (!transfer(owner, amount)) throw;
-     WavesTransfer(msg.sender, wavesAddress, amount);
-
- }
-
+    function moveToWaves(string wavesAddress, uint256 amount) {
+        if (!transfer(owner, amount)) throw;
+        WavesTransfer(msg.sender, wavesAddress, amount);
+    }
 }
