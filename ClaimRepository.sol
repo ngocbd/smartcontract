24bf5a873098c46f869c520c5560f7f35a2e5495
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:58:17 +0700

    add contract ClaimRepository

--- ClaimRepository.sol
+++ ClaimRepository.sol
@@ -1,31 +1,31 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ClaimRepository at 0xe305312618fa07a8a95643f3d79e5950d0b865fd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ClaimRepository at 0x3f5460cc85921d43f14b99699bfc87ab8aa3ab97
 */
-pragma solidity ^0.4.18;
-
-/* first version of the FundRequest ClaimRepository */
+pragma solidity 0.4.24;
 
 /// @dev `Owned` is a base level contract that assigns an `owner` that can be
 ///  later changed
 contract Owned {
-  /// @dev `owner` is the only address that can call a function with this
-  /// modifier
-  modifier onlyOwner { require (msg.sender == owner); _; }
 
-  address public owner;
+    /// @dev `owner` is the only address that can call a function with this
+    /// modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-  /// @notice The Constructor assigns the message sender to be `owner`
-  function Owned() public { owner = msg.sender;}
+    address public owner;
 
-  /// @notice `owner` can step down and assign some other address to this role
-  /// @param _newOwner The address of the new owner. 0x0 can be used to create
-  ///  an unowned neutral vault, however that cannot be undone
-  function changeOwner(address _newOwner) public onlyOwner {
-    owner = _newOwner;
-  }
+    /// @notice The Constructor assigns the message sender to be `owner`
+    function Owned() public {owner = msg.sender;}
+
+    /// @notice `owner` can step down and assign some other address to this role
+    /// @param _newOwner The address of the new owner. 0x0 can be used to create
+    ///  an unowned neutral vault, however that cannot be undone
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
 }
-
-
 
 /**
  * @title SafeMath
@@ -57,16 +57,10 @@ library SafeMath {
   }
 }
 
-contract ClaimRepository is Owned {
-    using SafeMath for uint256;
+contract Callable is Owned {
 
-    mapping (bytes32 => mapping (string => Claim)) claims;
-
+    //sender => _allowed
     mapping(address => bool) public callers;
-
-    uint256 public totalBalanceClaimed;
-    uint256 public totalClaims;
-
 
     //modifiers
     modifier onlyCaller {
@@ -74,27 +68,144 @@ contract ClaimRepository is Owned {
         _;
     }
 
-    struct Claim {
-        address solverAddress;
-        string solver;
-        uint256 requestBalance;
-    }
-
-    function ClaimRepository() {
-        //constructor
-    }
-
-    function addClaim(address _solverAddress, bytes32 _platform, string _platformId, string _solver, uint256 _requestBalance) public onlyCaller returns (bool) {
-        claims[_platform][_platformId].solver = _solver;
-        claims[_platform][_platformId].solverAddress = _solverAddress;
-        claims[_platform][_platformId].requestBalance = _requestBalance;
-        totalBalanceClaimed = totalBalanceClaimed.add(_requestBalance);
-        totalClaims = totalClaims.add(1);
-        return true;
-    }
-
     //management of the repositories
     function updateCaller(address _caller, bool allowed) public onlyOwner {
         callers[_caller] = allowed;
+    }
+}
+
+contract EternalStorage is Callable {
+
+    mapping(bytes32 => uint) uIntStorage;
+    mapping(bytes32 => string) stringStorage;
+    mapping(bytes32 => address) addressStorage;
+    mapping(bytes32 => bytes) bytesStorage;
+    mapping(bytes32 => bool) boolStorage;
+    mapping(bytes32 => int) intStorage;
+
+    // *** Getter Methods ***
+    function getUint(bytes32 _key) external view returns (uint) {
+        return uIntStorage[_key];
+    }
+
+    function getString(bytes32 _key) external view returns (string) {
+        return stringStorage[_key];
+    }
+
+    function getAddress(bytes32 _key) external view returns (address) {
+        return addressStorage[_key];
+    }
+
+    function getBytes(bytes32 _key) external view returns (bytes) {
+        return bytesStorage[_key];
+    }
+
+    function getBool(bytes32 _key) external view returns (bool) {
+        return boolStorage[_key];
+    }
+
+    function getInt(bytes32 _key) external view returns (int) {
+        return intStorage[_key];
+    }
+
+    // *** Setter Methods ***
+    function setUint(bytes32 _key, uint _value) onlyCaller external {
+        uIntStorage[_key] = _value;
+    }
+
+    function setString(bytes32 _key, string _value) onlyCaller external {
+        stringStorage[_key] = _value;
+    }
+
+    function setAddress(bytes32 _key, address _value) onlyCaller external {
+        addressStorage[_key] = _value;
+    }
+
+    function setBytes(bytes32 _key, bytes _value) onlyCaller external {
+        bytesStorage[_key] = _value;
+    }
+
+    function setBool(bytes32 _key, bool _value) onlyCaller external {
+        boolStorage[_key] = _value;
+    }
+
+    function setInt(bytes32 _key, int _value) onlyCaller external {
+        intStorage[_key] = _value;
+    }
+
+    // *** Delete Methods ***
+    function deleteUint(bytes32 _key) onlyCaller external {
+        delete uIntStorage[_key];
+    }
+
+    function deleteString(bytes32 _key) onlyCaller external {
+        delete stringStorage[_key];
+    }
+
+    function deleteAddress(bytes32 _key) onlyCaller external {
+        delete addressStorage[_key];
+    }
+
+    function deleteBytes(bytes32 _key) onlyCaller external {
+        delete bytesStorage[_key];
+    }
+
+    function deleteBool(bytes32 _key) onlyCaller external {
+        delete boolStorage[_key];
+    }
+
+    function deleteInt(bytes32 _key) onlyCaller external {
+        delete intStorage[_key];
+    }
+}
+
+contract ClaimRepository is Callable {
+    using SafeMath for uint256;
+
+    EternalStorage public db;
+
+    constructor(address _eternalStorage) public {
+        //constructor
+        require(_eternalStorage != address(0), "Eternal storage cannot be 0x0");
+        db = EternalStorage(_eternalStorage);
+    }
+
+    function addClaim(address _solverAddress, bytes32 _platform, string _platformId, string _solver, address _token, uint256 _requestBalance) public onlyCaller returns (bool) {
+        if (db.getAddress(keccak256(abi.encodePacked("claims.solver_address", _platform, _platformId))) != address(0)) {
+            require(db.getAddress(keccak256(abi.encodePacked("claims.solver_address", _platform, _platformId))) == _solverAddress, "Adding a claim needs to happen with the same claimer as before");
+        } else {
+            db.setString(keccak256(abi.encodePacked("claims.solver", _platform, _platformId)), _solver);
+            db.setAddress(keccak256(abi.encodePacked("claims.solver_address", _platform, _platformId)), _solverAddress);
+        }
+
+        uint tokenCount = db.getUint(keccak256(abi.encodePacked("claims.tokenCount", _platform, _platformId)));
+        db.setUint(keccak256(abi.encodePacked("claims.tokenCount", _platform, _platformId)), tokenCount.add(1));
+        db.setUint(keccak256(abi.encodePacked("claims.token.amount", _platform, _platformId, _token)), _requestBalance);
+        db.setAddress(keccak256(abi.encodePacked("claims.token.address", _platform, _platformId, tokenCount)), _token);
+        return true;
+    }
+
+    function isClaimed(bytes32 _platform, string _platformId) view external returns (bool claimed) {
+        return db.getAddress(keccak256(abi.encodePacked("claims.solver_address", _platform, _platformId))) != address(0);
+    }
+
+    function getSolverAddress(bytes32 _platform, string _platformId) view external returns (address solverAddress) {
+        return db.getAddress(keccak256(abi.encodePacked("claims.solver_address", _platform, _platformId)));
+    }
+
+    function getSolver(bytes32 _platform, string _platformId) view external returns (string){
+        return db.getString(keccak256(abi.encodePacked("claims.solver", _platform, _platformId)));
+    }
+
+    function getTokenCount(bytes32 _platform, string _platformId) view external returns (uint count) {
+        return db.getUint(keccak256(abi.encodePacked("claims.tokenCount", _platform, _platformId)));
+    }
+
+    function getTokenByIndex(bytes32 _platform, string _platformId, uint _index) view external returns (address token) {
+        return db.getAddress(keccak256(abi.encodePacked("claims.token.address", _platform, _platformId, _index)));
+    }
+
+    function getAmountByToken(bytes32 _platform, string _platformId, address _token) view external returns (uint token) {
+        return db.getUint(keccak256(abi.encodePacked("claims.token.amount", _platform, _platformId, _token)));
     }
 }
