395c50f0a366da1e881b022d5d3e509a74c3e284
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:27:03 +0700

    add contract InvestorWhiteList

--- InvestorWhiteList.sol
+++ InvestorWhiteList.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract InvestorWhiteList at 0xeb11ba96eabdf5d2f540d840260fb3dd5d03f641
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract InvestorWhiteList at 0x1fd6cf63b36e1258ddea7d0accd3838f0fe1aab7
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.20;
 
 /**
  * @title Ownable
@@ -12,14 +12,16 @@ contract Ownable {
   address public owner;
 
 
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() {
+  function Ownable() public {
     owner = msg.sender;
   }
-
 
   /**
    * @dev Throws if called by any account other than the owner.
@@ -29,15 +31,14 @@ contract Ownable {
     _;
   }
 
-
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 
 }
