00309df998ae1b33a3ab16bf1569d4d92ad0b7e0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:19:13 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x63859fb0f95da7e1070d36dc4cbfb57bb7bef58e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xdd941a0495a1f891e220f4a436f5227785be875d
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT238696' token contract
+    // 'ACT156497' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT238696
-    // Name        : ADZbuzz Cbssports.com Community Token
+    // Symbol      : ACT156497
+    // Name        : ADZbuzz Bitcointalk.org Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT238696";
-            name = "ADZbuzz Cbssports.com Community Token";
+            symbol = "ACT156497";
+            name = "ADZbuzz Bitcointalk.org Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
