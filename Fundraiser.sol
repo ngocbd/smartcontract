f199ae524afc9004c381840ea9594ecac084b2f3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:20:09 +0700

    add contract Fundraiser

--- Fundraiser.sol
+++ Fundraiser.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Fundraiser at 0xcE5cdCf3c4b44EA985AeDd0c9C661857e145382c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Fundraiser at 0xB62EF4c58F3997424B0CCeaB28811633201706Bc
 */
 pragma solidity 0.4.11;
 
@@ -7,15 +7,12 @@ contract Fundraiser {
 
   /* State */
 
-  address signer1;
-  address signer2;
-  bool public accept; // are contributions accepted
+  address public signer1;
+  address public signer2;
 
   enum Action {
     None,
-    Withdraw,
-    Close,
-    Open
+    Withdraw
   }
   
   struct Proposal {
@@ -24,44 +21,23 @@ contract Fundraiser {
     uint256 amount;
   }
   
-  Proposal signer1_proposal;
-  Proposal signer2_proposal;
+  Proposal public signer1_proposal;
+  Proposal public signer2_proposal;
 
   /* Constructor, choose signers. Those cannot be changed */
   function Fundraiser(address init_signer1,
                       address init_signer2) {
-    accept = false; // must call Open first
     signer1 = init_signer1;
     signer2 = init_signer2;
     signer1_proposal.action = Action.None;
     signer2_proposal.action = Action.None;
   }
 
-  /* no default action, in case people forget to send their data
-     or in case they use a buggy app that forgets to send the data */
-  function () {
-    throw;
+  /* allow simple send transactions */
+  function () payable {
   }
 
-  /* Entry point for contributors */
-
-  event Deposit (
-                 bytes20 tezos_pk_hash,
-                 uint amount
-                 );
-
-  function Contribute(bytes24 tezos_pkh_and_chksum) payable {
-    // Don't accept contributions if fundraiser closed
-    if (!accept) { throw; }
-    bytes20 tezos_pk_hash = bytes20(tezos_pkh_and_chksum);
-    /* shift left 20 bytes to extract checksum */
-    bytes4 expected_chksum = bytes4(tezos_pkh_and_chksum << 160);
-    bytes4 chksum = bytes4(sha256(sha256(tezos_pk_hash)));
-    /* revert transaction if the checksum cannot be verified */
-    if (chksum != expected_chksum) { throw; }
-    Deposit(tezos_pk_hash, msg.value);
-  }
-
+  
   /* Entry points for signers */
 
   function Withdraw(address proposed_destination,
@@ -82,30 +58,6 @@ contract Fundraiser {
     MaybePerformWithdraw();
   }
 
-  function Close(address proposed_destination) {
-    /* update action */
-    if (msg.sender == signer1) {
-      signer1_proposal.action = Action.Close;
-      signer1_proposal.destination = proposed_destination;
-    } else if (msg.sender == signer2) {
-      signer2_proposal.action = Action.Close;
-      signer2_proposal.destination = proposed_destination;
-    } else { throw; }
-    /* perform action */
-    MaybePerformClose();
-  }
-
-  function Open() {
-    /* update action */
-    if (msg.sender == signer1) {
-      signer1_proposal.action = Action.Open;
-    } else if (msg.sender == signer2) {
-      signer2_proposal.action = Action.Open;
-    } else { throw; }
-    /* perform action */
-    MaybePerformOpen();
-  }
-
   function MaybePerformWithdraw() internal {
     if (signer1_proposal.action == Action.Withdraw
         && signer2_proposal.action == Action.Withdraw
@@ -117,19 +69,4 @@ contract Fundraiser {
     }
   }
 
-  function MaybePerformClose() internal {
-    if (signer1_proposal.action == Action.Close
-        && signer2_proposal.action == Action.Close
-        && signer1_proposal.destination == signer2_proposal.destination) {
-      accept = false;
-      signer1_proposal.destination.transfer(this.balance);
-    }
-  }
-
-  function MaybePerformOpen() internal {
-    if (signer1_proposal.action == Action.Open
-        && signer2_proposal.action == Action.Open) {
-      accept = true;
-    }
-  }
 }
