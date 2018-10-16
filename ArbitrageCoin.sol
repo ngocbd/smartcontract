c3b724f318e4380b108858bbcbcfbec8c3addc85
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:30:14 +0700

    add contract ArbitrageCoin

---
+++ ArbitrageCoin.sol
@@ -0,0 +1,33 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ArbitrageCoin at 0x51c83d97236646fca3239556253032357285c0a1
+*/
+pragma solidity ^0.4.0;
+
+contract ArbitrageCoin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function Coin() public {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, receiver, amount);
+    }
+}
