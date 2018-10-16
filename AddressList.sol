de0b224ab3f2a3e7e7e2283aff1090c677f65b27
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:43:46 +0700

    add contract AddressList

--- AddressList.sol
+++ AddressList.sol
@@ -1,206 +1,89 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressList at 0x6e94097454e55a5e90c71d41efcf36378d053dca
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressList at 0x64e988ae0735c948d1269f87981c619ed5cf72e3
 */
-pragma solidity ^0.4.2;
-/**
- * @dev Double linked list with address items
- */
-library AddressList {
-    struct Data {
-        address head;
-        address tail;
-        uint    length;
-        mapping(address => bool)    isContain;
-        mapping(address => address) nextOf;
-        mapping(address => address) prevOf;
+pragma solidity ^0.4.18;
+
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+contract Claimable is Ownable {
+  address public pendingOwner;
+
+  /**
+   * @dev Modifier throws if called by any account other than the pendingOwner.
+   */
+  modifier onlyPendingOwner() {
+    require(msg.sender == pendingOwner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to set the pendingOwner address.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    pendingOwner = newOwner;
+  }
+
+  /**
+   * @dev Allows the pendingOwner address to finalize the transfer.
+   */
+  function claimOwnership() onlyPendingOwner public {
+    OwnershipTransferred(owner, pendingOwner);
+    owner = pendingOwner;
+    pendingOwner = address(0);
+  }
+}
+
+contract AddressList is Claimable {
+    string public name;
+    mapping (address => bool) public onList;
+
+    function AddressList(string _name, bool nullValue) public {
+        name = _name;
+        onList[0x0] = nullValue;
     }
+    event ChangeWhiteList(address indexed to, bool onList);
 
-    function first(Data storage _data) constant returns (address)
-    { return _data.head; }
-
-    function last(Data storage _data) constant returns (address)
-    { return _data.tail; }
-
-    /**
-     * @dev Chec list for element
-     * @param _data is list storage ref
-     * @param _item is an element
-     * @return `true` when element in list
-     */
-    function contains(Data storage _data, address _item) constant returns (bool)
-    { return _data.isContain[_item]; }
-
-    /**
-     * @dev Next element of list
-     * @param _data is list storage ref
-     * @param _item is current element of list
-     * @return next elemen of list
-     */
-    function next(Data storage _data, address _item) constant returns (address)
-    { return _data.nextOf[_item]; }
-
-    /**
-     * @dev Previous element of list
-     * @param _data is list storage ref
-     * @param _item is current element of list
-     * @return previous element of list 
-     */
-    function prev(Data storage _data, address _item) constant returns (address)
-    { return _data.prevOf[_item]; }
-
-    /**
-     * @dev Append element to end of list
-     * @param _data is list storage ref
-     * @param _item is a new list element  
-     */
-    function append(Data storage _data, address _item)
-    { append(_data, _item, _data.tail); }
-
-    /**
-     * @dev Append element to end of element
-     * @param _data is list storage ref
-     * @param _item is a new list element  
-     * @param _to is a item element before new 
-     * @notice gas usage < 100000
-     */
-    function append(Data storage _data, address _item, address _to) {
-        // Unable to contain double element
-        if (_data.isContain[_item]) throw;
-
-        // Empty list
-        if (_data.head == 0) {
-            _data.head = _data.tail = _item;
-        } else {
-            if (!_data.isContain[_to]) throw;
- 
-            var nextTo = _data.nextOf[_to];
-            if (nextTo != 0) {
-                _data.prevOf[nextTo] = _item;
-            } else {
-                _data.tail = _item;
-            }
-
-            _data.nextOf[_to]    = _item;
-            _data.prevOf[_item]  = _to;
-            _data.nextOf[_item]  = nextTo;
-        }
-        _data.isContain[_item] = true;
-        ++_data.length;
-    }
- 
-    /**
-     * @dev Prepend element to begin of list
-     * @param _data is list storage ref
-     * @param _item is a new list element  
-     */
-    function prepend(Data storage _data, address _item)
-    { prepend(_data, _item, _data.head); }
-
-    /**
-     * @dev Prepend element to element of list
-     * @param _data is list storage ref
-     * @param _item is a new list element  
-     * @param _to is a item element before new 
-     */
-    function prepend(Data storage _data, address _item, address _to) {
-        // Unable to contain double element
-        if (_data.isContain[_item]) throw;
-
-        // Empty list
-        if (_data.head == 0) {
-            _data.head = _data.tail = _item;
-        } else {
-            if (!_data.isContain[_to]) throw;
- 
-            var prevTo = _data.prevOf[_to];
-            if (prevTo != 0) {
-                _data.nextOf[prevTo] = _item;
-            } else {
-                _data.head = _item;
-            }
-
-            _data.prevOf[_item]  = prevTo;
-            _data.nextOf[_item]  = _to;
-            _data.prevOf[_to]    = _item;
-        }
-        _data.isContain[_item] = true;
-        ++_data.length;
-    }
-
-    /**
-     * @dev Remove element from list
-     * @param _data is list storage ref
-     * @param _item is a removed list element
-     */
-    function remove(Data storage _data, address _item) {
-        if (!_data.isContain[_item]) throw;
-
-        var elemPrev = _data.prevOf[_item];
-        var elemNext = _data.nextOf[_item];
-
-        if (elemPrev != 0) {
-            _data.nextOf[elemPrev] = elemNext;
-        } else {
-            _data.head = elemNext;
-        }
-
-        if (elemNext != 0) {
-            _data.prevOf[elemNext] = elemPrev;
-        } else {
-            _data.tail = elemPrev;
-        }
-
-        _data.isContain[_item] = false;
-        --_data.length;
-    }
-
-    /**
-     * @dev Replace element on list
-     * @param _data is list storage ref
-     * @param _from is old element
-     * @param _to is a new element
-     */
-    function replace(Data storage _data, address _from, address _to) {
-        if (!_data.isContain[_from]) throw;
-
-        var elemPrev = _data.prevOf[_from];
-        var elemNext = _data.nextOf[_from];
-
-        if (elemPrev != 0) {
-            _data.nextOf[elemPrev] = _to;
-        } else {
-            _data.head = _to;
-        }
-            
-        if (elemNext != 0) {
-            _data.prevOf[elemNext] = _to;
-        } else {
-            _data.tail = _to;
-        }
-
-        _data.prevOf[_to] = elemPrev;
-        _data.nextOf[_to] = elemNext;
-        _data.isContain[_from] = false;
-    }
-
-    /**
-     * @dev Swap two elements of list
-     * @param _data is list storage ref
-     * @param _a is a first element
-     * @param _b is a second element
-     */
-    function swap(Data storage _data, address _a, address _b) {
-        if (!_data.isContain[_a] || !_data.isContain[_b]) throw; 
-
-        var prevA = _data.prevOf[_a];
-
-        remove(_data, _a);
-        replace(_data, _b, _a);
-
-        if (prevA == 0) {
-            prepend(_data, _b);
-        } else {
-            append(_data, _b, prevA);
+    // Set whether _to is on the list or not. Whether 0x0 is on the list
+    // or not cannot be set here - it is set once and for all by the constructor.
+    function changeList(address _to, bool _onList) onlyOwner public {
+        require(_to != 0x0);
+        if (onList[_to] != _onList) {
+            onList[_to] = _onList;
+            ChangeWhiteList(_to, _onList);
         }
     }
 }
