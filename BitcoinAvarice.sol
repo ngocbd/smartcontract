5edbd4582252ebb7b21b5cb07bb4eb02d941a9e1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:21:22 +0700

    add contract BitcoinAvarice

---
+++ BitcoinAvarice.sol
@@ -0,0 +1,29 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinAvarice at 0x8d2c698650a90c7652d4dc369c4ac7e9094bcae4
+*/
+pragma solidity ^0.4.11;
+
+
+
+/**
+ * @title Bitcoin Avarice
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `StandardToken` functions.
+ */
+contract BitcoinAvarice {
+
+  string public constant name = "Bitcoin Avarice";
+  string public constant symbol = "BTAV";
+  uint8 public constant decimals = 18;
+
+ 
+  uint256 public constant TOTAL_SUPPLY = 21000 * (10 ** uint256(decimals));
+  
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+ 
+   
+    
+  }
