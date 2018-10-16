06270c25e9ccaf998024ea85d12f968b4f334868
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:44:45 +0700

    add contract Thing

---
+++ Thing.sol
@@ -0,0 +1,43 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Thing at 0x4671ebe586199456ca28ac050cc9473cbac829eb
+*/
+contract Mortal {
+        address public owner;
+        function mortal() { owner = msg.sender; }
+        function kill() { if (msg.sender == owner) suicide(owner); }
+}
+
+contract Thing is Mortal {
+        enum Mood { Agree, Disagree, Funny, Sad, Angry, Nothing }
+        // URL of the reaction - optional
+        string public url;
+        // Content of the reaction - optional
+        string public data;
+        // MIME type of the content - optional, default is text/plain
+        string public mimetype;
+        // Mood of the reaction - Mood.Nothing by default
+        Mood public mood;
+        Thing[] public reactions;
+
+        function thing( string _url
+                          , string _data
+                          , Mood _mood
+                          , string _mimetype) {
+                url = _url;
+                data = _data;
+                mimetype = _mimetype;
+                mood = _mood;
+        }
+
+        function react(Thing reaction) {
+                if (msg.sender != reaction.owner()) throw;
+
+                reactions.push(reaction);
+        }
+
+        function withdraw() {
+                if (msg.sender != owner) throw;
+
+                owner.send(this.balance);
+        }
+}
