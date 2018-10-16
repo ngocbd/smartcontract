682bf0ab0a6a1bfb49c568a802a59af7c429daef
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:44 +0700

    add contract Sign

--- Sign.sol
+++ Sign.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sign at 0x07536201e0c4c7751c9cfed79f1bf5dba4772c9e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sign at 0xe64f9f5af35ee8dd53f0c503e55852e169de3b6b
 */
 pragma solidity ^0.4.2;
 contract Sign {
@@ -7,26 +7,68 @@ contract Sign {
 	address public AddAuthority;	
 	mapping (uint32 => bytes32) Cert;	
 	
+	event EventNotarise (address indexed Signer, bytes Donnees_Signature, bytes Donnees_Reste);
+
 	// =============================================
 	
-	function Sign() {
-		AddAuthority = msg.sender;
-		Cert [0] = 0x7a1d671e46f713a33286d4b4215796c8d396fd0e7cedf0b4e01d071df0f1412a;
-		Cert [1] = 0x5705f82396973f8f3861f1c29d7962e3234ff732723e39689ca7e7c030580000;
-	}
+	function Sign() {AddAuthority = msg.sender;}
 
 	function () {throw;} // reverse
 	
 	function destroy() {if (msg.sender == AddAuthority) {selfdestruct(AddAuthority);}}
 	
 	function SetCert (uint32 _IndiceIndex, bytes32 _Cert) {
-		if (msg.sender == AddAuthority) {
-			Cert [_IndiceIndex] = _Cert;
-		}
+		Cert [_IndiceIndex] = _Cert;
 	}				
 	
 	function GetCert (uint32 _IndiceIndex) returns (bytes32 _Valeur)  {
 		_Valeur = Cert [_IndiceIndex];
-		return _Valeur;
 	}		
+	
+
+ 	// ====================================
+
+	function VerifSignature (bytes _Signature, bytes _Reste) returns (bool) {
+		// Vérification de la signature _Signature
+		// _Reste : hash / Signer 
+		// Décompose _Signature
+		bytes32 r;
+		bytes32 s;
+		uint8 v;
+		bytes32 hash;
+		address Signer;
+        assembly {
+            r := mload(add(_Signature, 32))
+            s := mload(add(_Signature, 64))
+            // v := byte(0, mload(add(_Signature, 96)))
+            v := and(mload(add(_Signature, 65)), 255)
+            hash := mload(add(_Reste, 32))
+            Signer := mload(add(_Reste, 52))
+        }		
+		return Signer == ecrecover(hash, v, r, s);
+	}
+	
+	function VerifCert (uint32 _IndiceIndex, bool _log, bytes _Signature, bytes _Reste) returns (uint status) {					
+		status = 0;
+		// Test de la validité de Cert
+		if (Cert [_IndiceIndex] != 0) {
+			status = 1;
+			// Test de la signature
+			if (VerifSignature (_Signature, _Reste)) {
+				// _Reste : hash / Signer / 
+				address Signer;
+				assembly {Signer := mload(add(_Reste, 52))}		
+			} else {
+				// Cert valide mais signature invalide 
+				status = 2;							
+			}		
+			// Log si demandé
+			if (_log) {
+				EventNotarise (Signer, _Signature, _Reste);
+				status = 3;							
+			}
+		}
+		return (status);
+	}
+	
 }
