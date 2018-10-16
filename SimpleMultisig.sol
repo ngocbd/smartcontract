988d303587a9129fb7c754637200f8f06ba64055
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:04:49 +0700

    add contract SimpleMultiSig

--- SimpleMultisig.sol
+++ SimpleMultisig.sol
@@ -1,62 +1,71 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleMultisig at 0x4a412fe6e60949016457897f9170bb00078b89a3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleMultiSig at 0xdcece8aadcbf7e3c6e8ec9e9452f9940b4790352
 */
-pragma solidity ^0.4.4;
+//
+// Simple MultiSig Wallet Contract by Christian Lundkvist
+// https://github.com/christianlundkvist/simple-multisig
+//
+//
+//  Copyright (c) 2017 Christian Lundkvist
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+pragma solidity 0.4.18;
+contract SimpleMultiSig {
 
-// mainnet: 0x4a412fe6e60949016457897f9170bb00078b89a3
+  uint public nonce;                // (only) mutable state
+  uint public threshold;            // immutable state
+  mapping (address => bool) isOwner; // immutable state
+  address[] public ownersArr;        // immutable state
 
-contract SimpleMultisig {
+  function SimpleMultiSig(uint threshold_, address[] owners_) public {
+    require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
 
-  // wallet
-  struct Tx {
-    address founder;
-    address destAddr;
-    uint256 amount;
-    bool active;
-  }
-  
-  mapping (address => bool) public founders;
-  Tx[] public txs;
-
-  function SimpleMultisig() public {
-    founders[0xf8e18E704Fb07282Eec78ADBEC6B584497d0B2e2] = true;
-    founders[0x0c621a12884c4F95B7Af1C46760a1bb7fE85ffaa] = true;
-    founders[0x6fc10338003273a46D7da62a126099998C981572] = true;
-  }
-
-  // contribute function
-  function() public payable {}
-
-  // one of founders can propose destination address for ethers
-  function proposeTx(address destAddr, uint256 amount) public isFounder {
-    txs.push(Tx({
-      founder: msg.sender,
-      destAddr: destAddr,
-      amount: amount,
-      active: true
-    }));
-  }
-
-  // another founder can approve specified tx and send it to destAddr
-  function approveTx(uint8 txIdx) public isFounder {
-    assert(txs[txIdx].founder != msg.sender);
-    assert(txs[txIdx].active);
-
-    txs[txIdx].active = false;
-    txs[txIdx].destAddr.transfer(txs[txIdx].amount);
-  }
-
-  // founder who created tx can cancel it
-  function cancelTx(uint8 txIdx) public {
-    assert(txs[txIdx].founder == msg.sender);
-    assert(txs[txIdx].active);
-
-    txs[txIdx].active = false;
+    address lastAdd = address(0);
+    for (uint i=0; i<owners_.length; i++) {
+      require(owners_[i] > lastAdd);
+      isOwner[owners_[i]] = true;
+      lastAdd = owners_[i];
+    }
+    ownersArr = owners_;
+    threshold = threshold_;
   }
 
-  // check if msg.sender is founder
-  modifier isFounder() {
-    assert(founders[msg.sender]);
-    _;
+  // Note that address recovered from signatures must be strictly increasing
+  function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) public {
+    require(sigR.length == threshold);
+    require(sigR.length == sigS.length && sigR.length == sigV.length);
+
+    // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
+    bytes32 txHash = keccak256(byte(0x19), byte(0), address(this), destination, value, data, nonce);
+
+    address lastAdd = address(0); // cannot have address(0) as an owner
+    for (uint i = 0; i < threshold; i++) {
+      address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
+      require(recovered > lastAdd && isOwner[recovered]);
+      lastAdd = recovered;
+    }
+
+    // If we make it here all signatures are accounted for
+    nonce = nonce + 1;
+    require(destination.call.value(value)(data));
   }
+
+  function () public payable {}
 }
