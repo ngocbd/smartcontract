14a61a0afdb0588f033766961e5b4f9639bb57d1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:52:00 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,78 +1,43 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x3bd5b90c0f13015ec5f292b27b0c4f85d61213a3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x61c0407487349e7c828d0c21d63bfd0ac6640ab5
 */
 pragma solidity ^0.4.13;
 
-contract owned {
-    address public owner;
+contract MyToken 
 
-    function owned() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
-    }
-}
-
-
-contract MyToken is owned {
-
-    string public name;
+{   string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
-
+    
     mapping (address => uint256) public balanceOf;
+    mapping (address => bool) public frozenAccount;
+    event FrozenFunds(address target,bool frozen);
+    event Transfer(address indexed from,address indexed to,uint256 value);
+    
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    function MyToken(uint256 initialSupply,string tokenName,string tokenSymbol,uint8 decimalUnits)
 
-    event Burn(address indexed from, uint256 value);
-
-    function MyToken(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-    ) {
-        balanceOf[msg.sender] = initialSupply;
+    {   balanceOf[msg.sender] = initialSupply;
         totalSupply = initialSupply;
         name = tokenName;
         symbol = tokenSymbol;
         decimals = decimalUnits;
     }
 
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        balanceOf[_from] -= _value;
+    function freezeAccount(address target,bool freeze)
+    
+    {    frozenAccount[target] = freeze;
+         FrozenFunds(target, freeze);
+    }
+
+    function transfer(address _to, uint256 _value)
+
+    {   require(!frozenAccount[msg.sender]);
+        require (balanceOf[msg.sender] >= _value);
+        require (balanceOf[_to] + _value >= balanceOf[_to]);
+        balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-    }
-
-    function transfer(address _to, uint256 _value) {
-        _transfer(msg.sender, _to, _value);
-    }
-
-    function burnToken(address _from, uint256 _value) onlyOwner returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        balanceOf[_from] -= _value;
-        totalSupply -= _value;
-        Burn(_from, _value);
-        return true;
-    }
-
-    function mintToken(address _from, uint256 _value) onlyOwner returns (bool success)  {
-        balanceOf[_from] += _value;
-        totalSupply += _value;
-        Transfer(0, owner, _value);
-        Transfer(owner, _from, _value);
-        return true;
+        Transfer(msg.sender, _to, _value);
     }
 }
