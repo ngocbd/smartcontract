09244a2c999e5a1120b1b7eafc4bd42e9912d428
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:01:53 +0700

    add contract Hypes

--- Hypes.sol
+++ Hypes.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Hypes at 0x96f38b663dafe776da3999fd6e78bcfdce405171
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Hypes at 0x9ed8d6cf06342d3498d96cf72daadd7994e5367d
 */
 pragma solidity ^0.4.18;
 
@@ -49,22 +49,22 @@ contract Hypes {
     require(msg.sender != address(0));
     require(msg.value >= seatPrice);
     uint256 excess = SafeMath.sub(msg.value, seatPrice);
-    hypes[1].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[2].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[3].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[4].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[5].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[6].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[7].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[8].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
-    hypes[9].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 9)));
+    hypes[1].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 2)));
+    hypes[2].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 4)));
+    hypes[3].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 6)));
+    hypes[4].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 8)));
+    hypes[5].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 10)));
+    hypes[6].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 12)));
+    hypes[7].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 14)));
+    hypes[8].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 16)));
+    hypes[9].owner.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 18)));
     hypes[9] = hypes[8]; hypes[8] = hypes[7]; hypes[7] = hypes[6];
     hypes[6] = hypes[5]; hypes[5] = hypes[4]; hypes[4] = hypes[3];
     hypes[3] = hypes[2]; hypes[2] = hypes[1];
     hypes[1] = Hype(msg.sender, seatPrice);
-    ceoAddress.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 19)));
-    NewOne(msg.sender, seatPrice, SafeMath.mul(SafeMath.div(seatPrice, 100), 109));
-    seatPrice = SafeMath.mul(SafeMath.div(seatPrice, 100), 109);
+    ceoAddress.transfer(uint256(SafeMath.mul(SafeMath.div(seatPrice, 100), 10)));
+    NewOne(msg.sender, seatPrice, SafeMath.mul(SafeMath.div(seatPrice, 100), 120));
+    seatPrice = SafeMath.mul(SafeMath.div(seatPrice, 100), 120);
     msg.sender.transfer(excess);
   }
 
