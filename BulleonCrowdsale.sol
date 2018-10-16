a4b38fade0efb35dd624c6bb59b452ce9a02eb22
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 06:30:02 +0700

    add contract BulleonCrowdsale

--- BulleonCrowdsale.sol
+++ BulleonCrowdsale.sol
@@ -1,65 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BulleonCrowdsale at 0x645A90D222D46139d72477F1d41a3c970a2880f4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BulleonCrowdsale at 0x0267ee183beebdb7f931afe41e4e25352195b871
 */
 pragma solidity ^0.4.21;
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-
-
 
 /**
  * @title SafeMath
@@ -106,6 +48,101 @@ library SafeMath {
     return c;
   }
 }
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    assert(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    ERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    assert(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    assert(token.approve(spender, value));
+  }
+}
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
  /* Standard SafeMath implementation from Zeppelin */
 
 
@@ -145,6 +182,33 @@ contract Claimable is Ownable {
     owner = pendingOwner;
     pendingOwner = address(0);
   }
+}
+ /* Standard Claimable implementation from Zeppelin */
+
+
+
+
+
+
+
+/**
+ * @title Contracts that should be able to recover tokens
+ * @author SylTi
+ * @dev This allow a contract to recover any ERC20 token received in a contract by transferring the balance to the contract owner.
+ * This will prevent any accidental loss of tokens.
+ */
+contract CanReclaimToken is Ownable {
+  using SafeERC20 for ERC20Basic;
+
+  /**
+   * @dev Reclaim all ERC20Basic compatible tokens
+   * @param token ERC20Basic The address of the token contract
+   */
+  function reclaimToken(ERC20Basic token) external onlyOwner {
+    uint256 balance = token.balanceOf(this);
+    token.safeTransfer(owner, balance);
+  }
+
 }
  /* Standard Claimable implementation from Zeppelin */
 
@@ -237,21 +301,6 @@ contract BurnableToken is BasicToken {
 
 
 
-
-
-
-
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
 
 
 
@@ -454,12 +503,13 @@ contract PausableToken is StandardToken, Pausable {
 }
  // PausableToken implementation from Zeppelin
  // Claimable implementation from Zeppelin
+ /* Standard Claimable implementation from Zeppelin */
 
 interface CrowdsaleContract {
   function isActive() public view returns(bool);
 }
 
-contract BulleonToken is StandardBurnableToken, PausableToken, Claimable {
+contract BulleonToken is StandardBurnableToken, PausableToken, Claimable, CanReclaimToken {
   /* Additional events */
   event AddedToWhitelist(address wallet);
   event RemoveWhitelist(address wallet);
@@ -543,19 +593,22 @@ contract BulleonToken is StandardBurnableToken, PausableToken, Claimable {
     whitelist[wallet] = false;
     emit RemoveWhitelist(wallet);
   }
+
+  // DELETE IT!
+  function kill() onlyOwner {
+    selfdestruct(owner);
+  }
 }
- /* Bulleon Token Contract */
 
 
-contract BulleonCrowdsale is Claimable {
+
+contract BulleonCrowdsale is Claimable, CanReclaimToken {
     using SafeMath for uint256;
     /* Additionals events */
     event AddedToBlacklist(address wallet);
     event RemovedFromBlacklist(address wallet);
-    event Log(uint256 value);
 
     /* Infomational vars */
-    string public name = "Bulleon Crowdsale";
     string public version = "2.0";
 
     /* ICO params */
@@ -600,8 +653,6 @@ contract BulleonCrowdsale is Claimable {
     uint256 constant BONUS_COEFF = 1000; // Values should be 10x percents, value 1000 = 100%
     mapping(address=>uint256) public investmentsOf; // Investments made by wallet
 
-
-
    /**
     * @dev Returns crowdsale status (if active returns true).
     */
@@ -644,7 +695,7 @@ contract BulleonCrowdsale is Claimable {
         rewardToken = BulleonToken(token);
     }
 
-    function () payable {
+    function () public payable {
         buyTokens(msg.sender);
     }
 
@@ -819,4 +870,5 @@ contract BulleonCrowdsale is Claimable {
       isBlacklisted[wallet] = false;
       emit RemovedFromBlacklist(wallet);
     }
+    
 }
