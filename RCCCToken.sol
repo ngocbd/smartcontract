99f69fdfde40dfa7a5ca7d8ae97ebe7e10ccf83e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:51:18 +0700

    add contract RCCCToken

--- RCCCToken.sol
+++ RCCCToken.sol
@@ -1,131 +1,150 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RCCCToken at 0x2eaa3136ac4ca51526e9704336adb20ed14d06d0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RCCCToken at 0x33bfd20660eeaf952e8d5bc3236e1918701f17d0
 */
-/**
- * Overflow aware uint math functions.
- *
- * Inspired by https://github.com/MakerDAO/maker-otc/blob/master/contracts/simple_market.sol
- */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.15;
 
-/**
- * ERC 20 token
- *
- * https://github.com/ethereum/EIPs/issues/20
- */
-contract RCCCToken  {
-    string public constant name = "RCCC Token";
-    string public constant symbol = "RCCC";
-    uint public constant decimals = 18;
-    uint256 _totalSupply    = 500000000 * 10**decimals;
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-    function totalSupply() constant returns (uint256 supply) {
-        return _totalSupply;
+contract RCCCToken {
+    // Public variables of the token
+    string public name = "RCCC Token";
+    string public symbol = "RCCC";
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function RCCCToken() public {
+        totalSupply = 500000000 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping(address => uint256) balances; //list of balance of each address
-    mapping(address => mapping (address => uint256)) allowed;
-
-    uint public baseStartTime; //All other time spots are calculated based on this time spot.
-
-    address public founder = 0x5e06E608959D68Cf33584BEa9bF6e5CF3975EDe4;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    //constructor
-    function RCCCToken() {
-        balances[founder] = _totalSupply;
-    }
-
-    function setStartTime(uint _startTime) {
-        if (msg.sender!=founder) revert();
-        baseStartTime = _startTime;
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
     }
 
     /**
-     * ERC 20 Standard Token interface transfer function
+     * Set allowance for other address and notify
      *
-     * Prevent transfers until freeze period is over.
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
      *
-     * Applicable tests:
-     *
-     * - Test restricted early transfer
-     * - Test transfer after restricted period
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
      */
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        if (now < baseStartTime) revert();
-
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
             return true;
-        } else {
-            return false;
         }
     }
 
     /**
-     * Change founder address (where ICO ETH is being forwarded).
+     * Destroy tokens
      *
-     * Applicable tests:
+     * Remove `_value` tokens from the system irreversibly
      *
-     * - Test founder change by hacker
-     * - Test founder change
-     * - Test founder token allocation twice
+     * @param _value the amount of money to burn
      */
-    function changeFounder(address newFounder) {
-        if (msg.sender!=founder) revert();
-        founder = newFounder;
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
     }
 
     /**
-     * ERC 20 Standard Token interface transfer function
+     * Destroy tokens from other account
      *
-     * Prevent transfers until freeze period is over.
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
      */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (msg.sender != founder) revert();
-
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
     }
-
-    // forward all eth to founder
-    function() payable {
-        if (!founder.call.value(msg.value)()) revert(); 
-    }
-
-    // only owner can kill
-    function kill() { 
-        if (msg.sender == founder) {
-            suicide(founder); 
-        }
-    }
-
 }
