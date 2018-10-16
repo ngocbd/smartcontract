4fdbc9bf1dbed5dd682d738a599a1cd734e18a25
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:51:39 +0700

    add contract Switch

--- Switch.sol
+++ Switch.sol
@@ -1,26 +1,82 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0xb609e144464e105d717088ea183834bc477bed91
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0xf5a3628805c5e9eb024b9732cd1b61c37c995c59
 */
-pragma solidity ^0.4.8;
-contract Switch 
-{
-    bool public state=false;
-    uint256 public blinc_block;
-    uint256 public on_block;
-    address public owner;
+pragma solidity ^ 0.4 .9;
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
 
-    function Switch(){
-        owner=msg.sender;
-        on_block==block.number;
-        blinc_block=block.number;
+    function div(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a / b;
+        return c;
     }
-    function blinc() payable {
-        if(msg.value>0)blinc_block=block.number;
+
+    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
+        assert(b <= a);
+        return a - b;
     }
-    function () payable {
-        if(msg.value>0)on_block=block.number;
+
+    function add(uint256 a, uint256 b) internal constant returns(uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
-    function kill() {
-    if (msg.sender==owner) selfdestruct(owner);
+}
+contract Switch {
+    using SafeMath
+    for uint256;
+    mapping(address => mapping(address => uint256)) allowed;
+    mapping(address => uint256) balances;
+    uint256 public totalSupply;
+    uint256 public decimals;
+    address public owner;
+    bytes32 public symbol;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed spender, uint256 value);
+
+    function Bithereum() {
+        totalSupply = 1000000000;
+        symbol = '1bit';
+        owner = 0x169b5cED7Ef19401FB7F9e3Afd5A02b8EB4Fc62a;
+        balances[owner] = totalSupply;
+        decimals = 0;
+    }
+
+    function balanceOf(address _owner) constant returns(uint256 balance) {
+        return balances[_owner];
+    }
+
+    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function transfer(address _to, uint256 _value) returns(bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
+        var _allowance = allowed[_from][msg.sender];
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns(bool) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function() {
+        revert();
     }
 }
