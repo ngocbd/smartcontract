40aba63a7de83dae1c4c79a006a96cabc975ed69
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:21:40 +0700

    add contract FlatPricing

--- FlatPricing.sol
+++ FlatPricing.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0xcea47bf4c899265f7fb125705d51ffe4a2bbe555
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0x1b50b6cf84b742cdac99efd55e0f890bbefcccdb
 */
 /**
  * Interface for defining crowdsale pricing.
