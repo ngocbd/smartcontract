232522edb01212be70922101e802933d45b0094f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:21:06 +0700

    add contract Test

--- test.sol
+++ test.sol
@@ -1,25 +1,17 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract test at 0x87d753f3d61bc1f3f525e835e0d6904a5a4a7e16
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0xb1e93423182504646bac08696230d59c5e88743e
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.0;
+contract Test {
+    
+    uint[] array = [1,5];
+    address to = 0x1b60840cBaFBe74DB4B9C7Dd7F1d0822fA9b9591;
 
-contract test
-{
-    event AAA(address indexed sender, uint x);
-    
-    function aaa(bytes data) public
-    {
-        uint x = _bytesToUint(data);
-        AAA(msg.sender, x);
-    }
-    
-    function _bytesToUint(bytes data) internal view returns (uint) {
-        uint num = 0;
-        for(uint i = 0; i < data.length; i++) {
-            num += uint(data[i]);
-            if(i != data.length - 1)
-                num *= 256;
+    function send() public{
+        if (to.call(0xc66ddd68, array)) {
+            return;
+        } else {
+            revert();
         }
-        return num;
     }
 }
