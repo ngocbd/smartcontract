5f2cb7a2eda12a57ec29156867f3cc08b249a6a4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:08:52 +0700

    add contract stopScamHolder

---
+++ stopScamHolder.sol
@@ -0,0 +1,35 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract stopScamHolder at 0xe010d0a52791340db3323b77700817546a062109
+*/
+pragma solidity ^0.4.13;
+
+contract token { 
+    function transfer(address _to, uint256 _value);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+}
+
+contract stopScamHolder {
+    
+    token public sharesTokenAddress;
+    address public owner;
+    uint public endTime = 1530403200;// 1 july 2018
+    uint256 public tokenFree;
+
+modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+}
+
+function stopScamHolder(address _tokenAddress) {
+    sharesTokenAddress = token(_tokenAddress);
+    owner = msg.sender;
+}
+
+function tokensBack() onlyOwner public {
+    if(now > endTime){
+        sharesTokenAddress.transfer(owner, sharesTokenAddress.balanceOf(this));
+    }
+    tokenFree = sharesTokenAddress.balanceOf(this);
+}	
+
+}
