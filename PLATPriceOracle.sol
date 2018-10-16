9605f1f043d28d2be8bb18e02e3fa1f8f8140904
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:06:59 +0700

    add contract PLATPriceOracle

--- PLATPriceOracle.sol
+++ PLATPriceOracle.sol
@@ -1,31 +1,30 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PLATPriceOracle at 0x2339a01f8424d116ff7cf0869c9c37b769ed274f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PLATPriceOracle at 0x3127be52acba38beab6b4b3a406dc04e557c037c
 */
 pragma solidity ^0.4.20;
 
 contract PLATPriceOracle {
 
   mapping (address => bool) admins;
-  
-  // How much Eth you get for 1 PLAT, multiplied by 10^18
-  // Default value is the ICO price, make sure you update
-  uint256 public PLATprice = 12500000000000;
-  
-  event PLATPriceChanged(uint256 newPrice);
-    
-  function PLATPriceOracle() public {
+
+  // How much PLAT you get for 1 ETH, multiplied by 10^18
+  uint256 public ETHPrice = 60000000000000000000000;
+
+  event PriceChanged(uint256 newPrice);
+
+  constructor() public {
     admins[msg.sender] = true;
   }
 
   function updatePrice(uint256 _newPrice) public {
     require(_newPrice > 0);
     require(admins[msg.sender] == true);
-    PLATprice = _newPrice;
-    emit PLATPriceChanged(_newPrice);
+    ETHPrice = _newPrice;
+    emit PriceChanged(_newPrice);
   }
-  
+
   function setAdmin(address _newAdmin, bool _value) public {
     require(admins[msg.sender] == true);
-    admins[_newAdmin] = _value;   
+    admins[_newAdmin] = _value;
   }
 }
