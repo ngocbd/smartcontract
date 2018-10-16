885605a02bffdc9f3cbe57d6ce8a865eed3d55d6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:49:54 +0700

    add contract LndrJPY

---
+++ LndrJPY.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LndrJPY at 0xd7E97C8dB67D0F2CcfeAd3ab9f5F9a3cC727518A
+*/
+pragma solidity 0.4.15;
+
+contract LndrJPY {
+    function allowTransaction(address creditor, address debtor, uint256 amount) public returns (bool) {
+        return true;
+    }
+}
