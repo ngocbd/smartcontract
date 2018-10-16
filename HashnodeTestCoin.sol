becfa2b7e2a14d026a89647e8295f7a079b065a8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:46:56 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x4c9ad100a53380ed6fcceec2751755293790b0c6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x8d15e0034bd347412fa9c262524483268561aeb6
 */
 pragma solidity ^0.4.4;
 
@@ -110,9 +110,9 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     function HashnodeTestCoin() {
         balances[msg.sender] = 100000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 100000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Neo Prime";                                   // Set the name for display purposes (CHANGE THIS)
+        name = "Trx Prime";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "NEOP";                                             // Set the symbol for display purposes (CHANGE THIS)
+        symbol = "TRXP";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
