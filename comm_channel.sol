a4628bbd65d5c86164491db8c3837303258eaa7b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:54:25 +0700

    add contract comm_channel

--- comm_channel.sol
+++ comm_channel.sol
@@ -1,11 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract comm_channel at 0xf236d15aa8164626e3457cd38e69d7f46f95eeb9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract comm_channel at 0x9c9dc2b707a99ae4b6795538ac6bf4f6d4842830
 */
+/* A contract to exchange encrypted messages. Most of the work done on
+   the client side. */
+
 contract comm_channel {
 	
     address owner;
     
-    event content(string datainfo, uint indexed version, string indexed senderKey, string indexed recipientKey, uint amount);
+    event content(string datainfo, string senderKey, string recipientKey, uint amount);
     modifier onlyowner { if (msg.sender == owner) _ }
     
     function comm_channel() public { owner = msg.sender; }
@@ -17,7 +20,7 @@ contract comm_channel {
         owner.send(this.balance);
     }
 
-    function add(string datainfo, uint version, string senderKey, string recipientKey,
+    function add(string datainfo, string senderKey, string recipientKey,
                  address resendTo) {
         
         //try to resend money from message to the address
@@ -27,6 +30,6 @@ contract comm_channel {
         }
         
         //write to blockchain
-        content(datainfo, version, senderKey, recipientKey, msg.value);
+        content(datainfo, senderKey, recipientKey, msg.value);
     }
 }
