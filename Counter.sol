38331d997cfe2ff4e7ba6ee15dcf7fd67b5897a7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:39:36 +0700

    add contract Counter

--- Counter.sol
+++ Counter.sol
@@ -1,21 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Counter at 0xd3f9db6a2ebb18617a2e279b07887e4c6f35b5f1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Counter at 0xa82749c94ab7f921725624fb90e7600216169597
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.19;
 contract Counter {
-    
-    uint total;
-  
-    function add(uint num) public {
-        total = total + num;
-    }
-    
-    function subtract(uint num) public {
-        total = total - num;
-    }
-    
-    function double() public {
-        total = total * 2;
-    }
-  
+    uint public counter;
+    function increment() public { counter++; }
 }
