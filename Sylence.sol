9cdedd96911e5c787d9df179cc93f94670af76b9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:04:08 +0700

    add contract Sylence

--- Sylence.sol
+++ Sylence.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sylence at 0x763465c2114e33886e1574e5994df1bb8cd5f160
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sylence at 0x6c5c0e2496d27f60c486faaaaa9bde58ea1dc386
 */
 pragma solidity ^0.4.8;
 
@@ -14,13 +14,9 @@ contract Sylence {
   address owner;
   function Sylence() { owner = msg.sender; }
 
-  function getPubKeyByHash(string phoneHash) constant returns (
-    string pubKey,
-    uint256 currentIndex
-  ) {
+  function getPubKeyByHash(string phoneHash) constant returns (string pubKey) {
     User u = users[phoneHash];
-    currentIndex = u.pubKeysCount;
-    pubKey = u.pubKeys[currentIndex];
+    pubKey = u.pubKeys[u.pubKeysCount];
   }
 
   function registerNewPubKeyForHash(string phoneHash, string pubKey) {
