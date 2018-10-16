b80ced974b17a5a9ce8cd9d1973e0db5e8cbaf9e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:52:20 +0700

    add contract Bytes32Lib

---
+++ Bytes32Lib.sol
@@ -0,0 +1,40 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bytes32Lib at 0x0fcf5a3884585e14fd2d8d131def18f598dfcc2b
+*/
+/// @title Token Register Contract
+/// @author Kongliang Zhong - <kongliang@loopring.org>,
+/// @author Daniel Wang - <daniel@loopring.org>.
+library Bytes32Lib {
+
+    function xorReduce(
+        bytes32[]   arr,
+        uint        len
+        )
+        public
+        constant
+        returns (bytes32 res) {
+
+        res = arr[0];
+        for (uint i = 1; i < len; i++) {
+            res = _xor(res, arr[i]);
+        }
+    }
+
+    function _xor(
+        bytes32 bs1,
+        bytes32 bs2
+        )
+        public
+        constant
+        returns (bytes32 res) {
+
+        bytes memory temp = new bytes(32);
+        for (uint i = 0; i < 32; i++) {
+            temp[i] = bs1[i] ^ bs2[i];
+        }
+        string memory str = string(temp);
+        assembly {
+            res := mload(add(str, 32))
+        }
+    }
+}
