a372929caaa44e47cc95fd2ef6f81a45cac4a7e3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:02:39 +0700

    add contract Airdropper

--- Airdropper.sol
+++ Airdropper.sol
@@ -1,59 +1,46 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x1d90a6825550feac7406fbc7714b67f329a8d3f1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x9c37948b61ca1ba2c861adf9ede5cc27b7cc1455
 */
-pragma solidity ^0.4.24;
+// 0.4.21+commit.dfe3193c.Emscripten.clang
+pragma solidity ^0.4.21;
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control 
- * functions, this simplifies the implementation of "user permissions". 
- */
-contract Ownable {
+// we assume ERC20 or compatible token with most basic imaginable transfer function
+interface ERC20 {
+  function transfer( address to, uint256 value ) external;
+}
+
+contract owned {
   address public owner;
 
-  function Ownable() {
+  function owned() public {
     owner = msg.sender;
   }
- 
-  modifier onlyOwner() {
-    if (msg.sender != owner) {
-      revert();
-    }
+
+  function changeOwner( address _miner ) public onlyOwner {
+    owner = _miner;
+  }
+
+  modifier onlyOwner {
+    require (msg.sender == owner);
     _;
   }
- 
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
+}
+
+//
+// NOTE: this Airdropper becomes msg.sender for the token transfer and must
+//       already be the holder of enough tokens
+//
+contract Airdropper is owned {
+
+  // NOTE: caller responsible to check ethstats.net for block size limit
+  function airdrop( address tokAddr,
+                    address[] dests,
+                    uint[] quantities ) public onlyOwner returns (uint) {
+
+    for (uint ii = 0; ii < dests.length; ii++) {
+      ERC20(tokAddr).transfer( dests[ii], quantities[ii] );
     }
+
+    return ii;
   }
-
-}
-
-contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
-}
- 
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-contract Airdropper is Ownable {
-
-    function multisend(address _tokenAddr, address[] dests, uint256[] values)
-    onlyOwner
-    returns (uint256) {
-        uint256 i = 0;
-        while (i < dests.length) {
-           ERC20(_tokenAddr).transfer(dests[i], values[i]);
-           i += 1;
-        }
-        return(i);
-    }
 }
