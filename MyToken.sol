a7ffa3aaa24a70fffc08b88d23c8d66633bc58cc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:19:04 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,16 +1,16 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xbda6b8db38a9af49f1e57d6f91e114e7e5d80dd4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x938E6Bfb6B0030ca755ad9f8904200cFDF8da07d
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.11;
 contract MyToken {
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyToken(
-        
+        uint256 initialSupply
         ) {
-        balanceOf[msg.sender] = 210000;              // Give the creator all initial tokens
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
     }
 
     /* Send coins */
