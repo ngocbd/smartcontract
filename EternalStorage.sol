0b0736febf28cfab568ac0d2a8c30883d085faaa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:58:22 +0700

    add contract EternalStorage

--- EternalStorage.sol
+++ EternalStorage.sol
@@ -1,128 +1,130 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EternalStorage at 0x7675de10fe92ec4661438d0b502bcf280898c043
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EternalStorage at 0x0821d33c50d2d2d1381b5dfca34900bacad909a7
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.24;
 
+/// @dev `Owned` is a base level contract that assigns an `owner` that can be
+///  later changed
+contract Owned {
 
-contract Ownable {
-
-    address public owner;
-    address public acceptableAddress;
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    function Ownable() public {
-        owner = msg.sender;
-        acceptableAddress = msg.sender;
-    }
-
-    modifier onlyOwner() {
+    /// @dev `owner` is the only address that can call a function with this
+    /// modifier
+    modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
 
-    modifier onlyAcceptable() {
-        require(msg.sender == acceptableAddress);
+    address public owner;
+
+    /// @notice The Constructor assigns the message sender to be `owner`
+    function Owned() public {owner = msg.sender;}
+
+    /// @notice `owner` can step down and assign some other address to this role
+    /// @param _newOwner The address of the new owner. 0x0 can be used to create
+    ///  an unowned neutral vault, however that cannot be undone
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+}
+
+contract Callable is Owned {
+
+    //sender => _allowed
+    mapping(address => bool) public callers;
+
+    //modifiers
+    modifier onlyCaller {
+        require(callers[msg.sender]);
         _;
     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
+    //management of the repositories
+    function updateCaller(address _caller, bool allowed) public onlyOwner {
+        callers[_caller] = allowed;
     }
-
-    function transferAcceptable(address newAcceptable) public onlyOwner {
-        require(newAcceptable != address(0));
-        OwnershipTransferred(acceptableAddress, newAcceptable);
-        acceptableAddress = newAcceptable;
-    }
-
 }
 
+contract EternalStorage is Callable {
 
-contract EternalStorage is Ownable {
+    mapping(bytes32 => uint) uIntStorage;
+    mapping(bytes32 => string) stringStorage;
+    mapping(bytes32 => address) addressStorage;
+    mapping(bytes32 => bytes) bytesStorage;
+    mapping(bytes32 => bool) boolStorage;
+    mapping(bytes32 => int) intStorage;
 
-    function () public payable {
-        require(msg.sender == acceptableAddress || msg.sender == owner);
+    // *** Getter Methods ***
+    function getUint(bytes32 _key) external view returns (uint) {
+        return uIntStorage[_key];
     }
 
-    mapping(bytes32 => uint) public uintStorage;
-
-    function getUInt(bytes32 record) public view returns (uint) {
-        return uintStorage[record];
+    function getString(bytes32 _key) external view returns (string) {
+        return stringStorage[_key];
     }
 
-    function setUInt(bytes32 record, uint value) public onlyAcceptable {
-        uintStorage[record] = value;
+    function getAddress(bytes32 _key) external view returns (address) {
+        return addressStorage[_key];
     }
 
-    mapping(bytes32 => string) public stringStorage;
-
-    function getString(bytes32 record) public view returns (string) {
-        return stringStorage[record];
+    function getBytes(bytes32 _key) external view returns (bytes) {
+        return bytesStorage[_key];
     }
 
-    function setString(bytes32 record, string value) public onlyAcceptable {
-        stringStorage[record] = value;
+    function getBool(bytes32 _key) external view returns (bool) {
+        return boolStorage[_key];
     }
 
-    mapping(bytes32 => address) public addressStorage;
-
-    function getAdd(bytes32 record) public view returns (address) {
-        return addressStorage[record];
+    function getInt(bytes32 _key) external view returns (int) {
+        return intStorage[_key];
     }
 
-    function setAdd(bytes32 record, address value) public onlyAcceptable {
-        addressStorage[record] = value;
+    // *** Setter Methods ***
+    function setUint(bytes32 _key, uint _value) onlyCaller external {
+        uIntStorage[_key] = _value;
     }
 
-    mapping(bytes32 => bytes) public bytesStorage;
-
-    function getBytes(bytes32 record) public view returns (bytes) {
-        return bytesStorage[record];
+    function setString(bytes32 _key, string _value) onlyCaller external {
+        stringStorage[_key] = _value;
     }
 
-    function setBytes(bytes32 record, bytes value) public onlyAcceptable {
-        bytesStorage[record] = value;
+    function setAddress(bytes32 _key, address _value) onlyCaller external {
+        addressStorage[_key] = _value;
     }
 
-    mapping(bytes32 => bytes32) public bytes32Storage;
-
-    function getBytes32(bytes32 record) public view returns (bytes32) {
-        return bytes32Storage[record];
+    function setBytes(bytes32 _key, bytes _value) onlyCaller external {
+        bytesStorage[_key] = _value;
     }
 
-    function setBytes32(bytes32 record, bytes32 value) public onlyAcceptable {
-        bytes32Storage[record] = value;
+    function setBool(bytes32 _key, bool _value) onlyCaller external {
+        boolStorage[_key] = _value;
     }
 
-    mapping(bytes32 => bool) public booleanStorage;
-
-    function getBool(bytes32 record) public view returns (bool) {
-        return booleanStorage[record];
+    function setInt(bytes32 _key, int _value) onlyCaller external {
+        intStorage[_key] = _value;
     }
 
-    function setBool(bytes32 record, bool value) public  onlyAcceptable {
-        booleanStorage[record] = value;
+    // *** Delete Methods ***
+    function deleteUint(bytes32 _key) onlyCaller external {
+        delete uIntStorage[_key];
     }
 
-    mapping(bytes32 => int) public intStorage;
-
-    function getInt(bytes32 record) public view returns (int) {
-        return intStorage[record];
+    function deleteString(bytes32 _key) onlyCaller external {
+        delete stringStorage[_key];
     }
 
-    function setInt(bytes32 record, int value) public onlyAcceptable {
-        intStorage[record] = value;
+    function deleteAddress(bytes32 _key) onlyCaller external {
+        delete addressStorage[_key];
     }
 
-    function getBalance() public constant returns (uint) {
-        return this.balance;
+    function deleteBytes(bytes32 _key) onlyCaller external {
+        delete bytesStorage[_key];
     }
 
-    function withdraw(address beneficiary) public onlyAcceptable {
-        uint balance = getUInt(keccak256(beneficiary, "balance"));
-        setUInt(keccak256(beneficiary, "balance"), 0);
-        beneficiary.transfer(balance);
+    function deleteBool(bytes32 _key) onlyCaller external {
+        delete boolStorage[_key];
+    }
+
+    function deleteInt(bytes32 _key) onlyCaller external {
+        delete intStorage[_key];
     }
 }
