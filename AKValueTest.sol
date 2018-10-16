ee71e42c2ca039e4c28c70440ee289c7d64ebddd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:43:40 +0700

    add contract AKValueTest

---
+++ AKValueTest.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AKValueTest at 0xc333b0bc21A64A56aB7e59D77b486D4Ee41a50A2
+*/
+pragma solidity ^0.4.16;
+
+contract AKValueTest
+{
+    uint256 public someValue;
+    
+    function setSomeValue(uint256 newValue)
+    {
+        someValue = newValue;
+    }
+}
