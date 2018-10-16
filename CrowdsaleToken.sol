fadbd58ef1c3a91adda91c8110cc9db463805757
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:41:13 +0700

    add contract CrowdsaleToken

--- CrowdsaleToken.sol
+++ CrowdsaleToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleToken at 0xe3f9d921e78097e6ee6dd04725d1d0b34f5bff2f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleToken at 0x08b1d7c6a3b30180338354e98c264c9ca58d0cf9
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
