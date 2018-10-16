4fffbd78f71860a26990329329c834290330d5c5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:35:18 +0700

    add contract Griddeth

---
+++ Griddeth.sol
@@ -0,0 +1,31 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Griddeth at 0xec2c57aa0c77ebb5ff3b85a8ac964087ca295c92
+*/
+pragma solidity ^0.4.18;
+
+
+contract Griddeth {
+    
+  string public constant NAME = "Griddeth";
+
+  uint8[18000] grid8; // 180x100 display
+  
+  // colors = ["#FFFFFF","#E4E4E4","#888888","#222222",
+  // "#FFA7D1","#E50000","#E59500","#A06A42","#E5D900",
+  // "#94E044","#02BE01","#00E5F0","#0083C7","#0000EA",
+  // "#E04AFF","#820080"]
+
+  function getGrid8() public view returns (uint8[18000]) {
+      return grid8;
+  }
+  
+  // No assertion on color < 16 since the frontend will
+  // default to white otherwise.
+  function setColor8(uint256 i, uint8 color) public {
+      grid8[i] = color;
+  }
+  
+  function Griddeth() public {
+  }
+
+}
