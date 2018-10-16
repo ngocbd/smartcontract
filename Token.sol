128ff5bfff8b3933740501873000327cfb3c26c0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:17 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,245 +1,221 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract token at 0x6F47980674550d8c8240e6eaf3B594F562Adc7A9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x2D224e277fDfd273BbE63871Ff69830604B79754
 */
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.0;
+/*
+This vSlice token contract is based on the ERC20 token contract. Additional
+functionality has been integrated:
+* the contract Lockable, which is used as a parent of the Token contract
+* the function mintTokens(), which makes use of the currentSwapRate() and safeToAdd() helpers
+* the function disableTokenSwapLock()
+*/
 
-contract owned {
-    address public owner;
+contract Lockable {
+    uint public numOfCurrentEpoch;
+    uint public creationTime;
+    uint public constant UNLOCKED_TIME = 25 days;
+    uint public constant LOCKED_TIME = 5 days;
+    uint public constant EPOCH_LENGTH = 30 days;
+    bool public lock;
+    bool public tokenSwapLock;
 
-    function owned() {
-        owner = msg.sender;
-    }
+    event Locked();
+    event Unlocked();
 
-    modifier onlyOwner {
-        if (msg.sender != owner) throw;
+    // This modifier should prevent tokens transfers while the tokenswap
+    // is still ongoing
+    modifier isTokenSwapOn {
+        if (tokenSwapLock) throw;
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner {
-        if (newOwner == 0x0000000000000000000000000000000000000000) throw;
-        owner = newOwner;
+    // This modifier checks and, if needed, updates the value of current
+    // token contract epoch, before executing a token transfer of any
+    // kind
+    modifier isNewEpoch {
+        if (numOfCurrentEpoch * EPOCH_LENGTH + creationTime < now ) {
+            numOfCurrentEpoch = (now - creationTime) / EPOCH_LENGTH + 1;
+        }
+        _;
+    }
+
+    // This modifier check whether the contract should be in a locked
+    // or unlocked state, then acts and updates accordingly if
+    // necessary
+    modifier checkLock {
+        if ((creationTime + numOfCurrentEpoch * UNLOCKED_TIME) +
+        (numOfCurrentEpoch - 1) * LOCKED_TIME < now) {
+            // avoids needless lock state change and event spamming
+            if (lock) throw;
+
+            lock = true;
+            Locked();
+            return;
+        }
+        else {
+            // only set to false if in a locked state, to avoid
+            // needless state change and event spam
+            if (lock) {
+                lock = false;
+                Unlocked();
+            }
+        }
+        _;
+    }
+
+    function Lockable() {
+        creationTime = now;
+        numOfCurrentEpoch = 1;
+        tokenSwapLock = true;
     }
 }
 
 
+contract ERC20 {
+    function totalSupply() constant returns (uint);
+    function balanceOf(address who) constant returns (uint);
+    function allowance(address owner, address spender) constant returns (uint);
 
+    function transfer(address to, uint value) returns (bool ok);
+    function transferFrom(address from, address to, uint value) returns (bool ok);
+    function approve(address spender, uint value) returns (bool ok);
 
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
 
+contract Token is ERC20, Lockable {
 
+  mapping( address => uint ) _balances;
+  mapping( address => mapping( address => uint ) ) _approvals;
+  uint _supply;
+  address public walletAddress;
 
+  event TokenMint(address newTokenHolder, uint amountOfTokens);
+  event TokenSwapOver();
 
-/* Dentacoin Contract */
-contract token is owned {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    uint256 public buyPriceEth;
-    uint256 public sellPriceEth;
-    uint256 public minBalanceForAccounts;
-//Public variables of the token
+  modifier onlyFromWallet {
+      if (msg.sender != walletAddress) throw;
+      _;
+  }
 
+  function Token( uint initial_balance, address wallet) {
+    _balances[msg.sender] = initial_balance;
+    _supply = initial_balance;
+    walletAddress = wallet;
+  }
 
-/* Creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+  function totalSupply() constant returns (uint supply) {
+    return _supply;
+  }
 
+  function balanceOf( address who ) constant returns (uint value) {
+    return _balances[who];
+  }
 
-/* Generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+  function allowance(address owner, address spender) constant returns (uint _allowance) {
+    return _approvals[owner][spender];
+  }
 
+  // A helper to notify if overflow occurs
+  function safeToAdd(uint a, uint b) internal returns (bool) {
+    return (a + b >= a && a + b >= b);
+  }
 
-/* Initializes contract with initial supply tokens to the creator of the contract */
-    function token() {
-        totalSupply = 8000000000000;
-        balanceOf[msg.sender] = totalSupply;
-// Give the creator all tokens
-        name = "Dentacoin";
-// Set the name for display purposes
-        symbol = "?";
-// Set the symbol for display purposes
-        decimals = 0;
-// Amount of decimals for display purposes
-        buyPriceEth = 1 finney;
-        sellPriceEth = 1 finney;
-// Sell and buy prices for Dentacoins
-        minBalanceForAccounts = 5 finney;
-// Minimal eth balance of sender and receiver
+  function transfer( address to, uint value)
+    isTokenSwapOn
+    isNewEpoch
+    checkLock
+    returns (bool ok) {
+
+    if( _balances[msg.sender] < value ) {
+        throw;
+    }
+    if( !safeToAdd(_balances[to], value) ) {
+        throw;
     }
 
+    _balances[msg.sender] -= value;
+    _balances[to] += value;
+    Transfer( msg.sender, to, value );
+    return true;
+  }
 
-
-
-/* Constructor parameters */
-    function setEtherPrices(uint256 newBuyPriceEth, uint256 newSellPriceEth) onlyOwner {
-        buyPriceEth = newBuyPriceEth;
-        sellPriceEth = newSellPriceEth;
+  function transferFrom( address from, address to, uint value)
+    isTokenSwapOn
+    isNewEpoch
+    checkLock
+    returns (bool ok) {
+    // if you don't have enough balance, throw
+    if( _balances[from] < value ) {
+        throw;
     }
-
-    function setMinBalance(uint minimumBalanceInWei) onlyOwner {
-     minBalanceForAccounts = minimumBalanceInWei;
+    // if you don't have approval, throw
+    if( _approvals[from][msg.sender] < value ) {
+        throw;
     }
-
-
-
-
-/* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (_value < 1) throw;
-// Prevents drain, spam and overflows
-        address DentacoinAddress = this;
-        if (msg.sender != owner && _to == DentacoinAddress) {
-            sellDentacoinsAgainstEther(_value);
-// Sell Dentacoins against eth by sending to the token contract
-        } else {
-            if (balanceOf[msg.sender] < _value) throw;
-// Check if the sender has enough
-            if (balanceOf[_to] + _value < balanceOf[_to]) throw;
-// Check for overflows
-            balanceOf[msg.sender] -= _value;
-// Subtract from the sender
-            if (msg.sender.balance >= minBalanceForAccounts && _to.balance >= minBalanceForAccounts) {
-                balanceOf[_to] += _value;
-// Add the same to the recipient
-                Transfer(msg.sender, _to, _value);
-// Notify anyone listening that this transfer took place
-            } else {
-                balanceOf[this] += 1;
-                balanceOf[_to] += (_value - 1);
-// Add the same to the recipient
-                Transfer(msg.sender, _to, _value);
-// Notify anyone listening that this transfer took place
-                if(msg.sender.balance < minBalanceForAccounts) {
-                    if(!msg.sender.send(minBalanceForAccounts * 3)) throw;
-// Send minBalance to Sender
-                }
-                if(_to.balance < minBalanceForAccounts) {
-                    if(!_to.send(minBalanceForAccounts)) throw;
-// Send minBalance to Receiver
-                }
-            }
-        }
+    if( !safeToAdd(_balances[to], value) ) {
+        throw;
     }
+    // transfer and return true
+    _approvals[from][msg.sender] -= value;
+    _balances[from] -= value;
+    _balances[to] += value;
+    Transfer( from, to, value );
+    return true;
+  }
 
+  function approve(address spender, uint value)
+    isTokenSwapOn
+    isNewEpoch
+    checkLock
+    returns (bool ok) {
+    _approvals[msg.sender][spender] = value;
+    Approval( msg.sender, spender, value );
+    return true;
+  }
 
+  // The function currentSwapRate() returns the current exchange rate
+  // between vSlice tokens and Ether during the token swap period
+  function currentSwapRate() constant returns(uint) {
+      if (creationTime + 1 weeks > now) {
+          return 130;
+      }
+      else if (creationTime + 2 weeks > now) {
+          return 120;
+      }
+      else if (creationTime + 4 weeks > now) {
+          return 100;
+      }
+      else {
+          return 0;
+      }
+  }
 
+  // The function mintTokens is only usable by the chosen wallet
+  // contract to mint a number of tokens proportional to the
+  // amount of ether sent to the wallet contract. The function
+  // can only be called during the tokenswap period
+  function mintTokens(address newTokenHolder, uint etherAmount)
+    external
+    onlyFromWallet {
 
-/* User buys Dentacoins and pays in Ether */
-    function buyDentacoinsAgainstEther() payable returns (uint amount) {
-        if (buyPriceEth == 0) throw;
-// Avoid buying if not allowed
-        if (msg.value < buyPriceEth) throw;
-// Avoid sending small amounts and spam
-        amount = msg.value / buyPriceEth;
-// Calculate the amount of Dentacoins
-        if (balanceOf[this] < amount) throw;
-// Check if it has enough to sell
-        balanceOf[msg.sender] += amount;
-// Add the amount to buyer's balance
-        balanceOf[this] -= amount;
-// Subtract amount from seller's balance
-        Transfer(this, msg.sender, amount);
-// Execute an event reflecting the change
-        return amount;
-    }
+        uint tokensAmount = currentSwapRate() * etherAmount;
+        if(!safeToAdd(_balances[newTokenHolder],tokensAmount )) throw;
+        if(!safeToAdd(_supply,tokensAmount)) throw;
 
+        _balances[newTokenHolder] += tokensAmount;
+        _supply += tokensAmount;
 
-/* User sells Dentacoins and gets Ether */
-    function sellDentacoinsAgainstEther(uint256 amount) returns (uint revenue) {
-        if (sellPriceEth == 0) throw;
-// Avoid selling
-        if (amount < 1) throw;
-// Avoid spam
-        if (balanceOf[msg.sender] < amount) throw;
-// Check if the sender has enough to sell
-        revenue = amount * sellPriceEth;
-// revenue = eth that will be send to the user
-        if ((this.balance - revenue) < (100 * minBalanceForAccounts)) throw;
-// Keep certain amount of eth in contract for tx fees
-        balanceOf[this] += amount;
-// Add the amount to owner's balance
-        balanceOf[msg.sender] -= amount;
-// Subtract the amount from seller's balance
-        if (!msg.sender.send(revenue)) {
-// Send ether to the seller. It's important
-            throw;
-// To do this last to avoid recursion attacks
-        } else {
-            Transfer(msg.sender, this, amount);
-// Execute an event reflecting on the change
-            return revenue;
-// End function and returns
-        }
-    }
+        TokenMint(newTokenHolder, tokensAmount);
+  }
 
-
-
-
-/* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        tokenRecipient spender = tokenRecipient(_spender);
-        return true;
-    }
-
-
-/* Approve and then comunicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-
-/* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;
-// Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
-// Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;
-// Check allowance
-        balanceOf[_from] -= _value;
-// Subtract from the sender
-        balanceOf[_to] += _value;
-// Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-
-
-
-/* refund To Owner */
-    function refundToOwner (uint256 amountOfEth, uint256 dcn) onlyOwner {
-        uint256 eth = amountOfEth * 1 ether;
-        if (!msg.sender.send(eth)) {
-// Send ether to the owner. It's important
-            throw;
-// To do this last to avoid recursion attacks
-        } else {
-            Transfer(msg.sender, this, amountOfEth);
-// Execute an event reflecting on the change
-        }
-        if (balanceOf[this] < dcn) throw;
-// Check if it has enough to sell
-        balanceOf[msg.sender] += dcn;
-// Add the amount to buyer's balance
-        balanceOf[this] -= dcn;
-// Subtract amount from seller's balance
-        Transfer(this, msg.sender, dcn);
-// Execute an event reflecting the change
-    }
-
-
-/* This unnamed function is called whenever someone tries to send ether to it and sells Dentacoins */
-    function() payable {
-        if (msg.sender != owner) {
-            buyDentacoinsAgainstEther();
-        }
-    }
+  // The function disableTokenSwapLock() is called by the wallet
+  // contract once the token swap has reached its end conditions
+  function disableTokenSwapLock()
+    external
+    onlyFromWallet {
+        tokenSwapLock = false;
+        TokenSwapOver();
+  }
 }
