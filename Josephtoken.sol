6460823400bc6d6f50762ce2cafa867939c68765
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:15:56 +0700

    add contract Josephtoken

---
+++ Josephtoken.sol
@@ -0,0 +1,26 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Josephtoken at 0x63bfc9552ab287a09cd96fe66d2bccfa0502aaa4
+*/
+pragma solidity ^0.4.8;                  //specify compiler version
+//this is a comment!
+
+contract Josephtoken {                     //this is a smart contract!
+    
+    address owner;                       //which account gets the 1000 josephtoken to begin with
+    mapping (address => uint) balances;  //keep track of the number of josephtoken in each account
+    
+    function Josephtoken() public {
+        owner = msg.sender;              //msg.sender is the address of the account that called the function
+                                         //here, msg.sender is the account that deployed the contract
+        balances[owner] = 1000;          //mint the owner 1000 josephtoken and put it in the mapping
+    }
+    
+    function transfer(uint amount, address recipient) public {      //move josephtoken between accounts
+        require(balances[msg.sender] >= amount);
+        require(balances[msg.sender] - amount <= balances[msg.sender]);
+        require(balances[recipient] + amount >= balances[recipient]);
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+        //hmm, how might evil attacker Jennifer try to exploit this function?
+    }
+}
