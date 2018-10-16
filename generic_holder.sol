c554290d13a56c3fca6977f0ab7795e486629d66
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:32:59 +0700

    add contract generic_holder

--- generic_holder.sol
+++ generic_holder.sol
@@ -1,8 +1,6 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract generic_holder at 0x7998b7Fcf30D4aed870635155Cc62Aa55BE96F9A
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract generic_holder at 0xc1dCB60C2eC168C8078064903abb746C566a230F
 */
-pragma solidity ^0.4.15;
-
 contract generic_holder {
     address owner;
     
@@ -23,13 +21,8 @@ contract generic_holder {
     function execute(address _to, uint _value, bytes _data) external onlyowner returns (bool){
         return _to.call.value(_value)(_data);
     }
-
-    function send(address _to, uint _value) external onlyowner returns (bool){
-        return _to.send(_value); 
-    }
     
     function get_owner() constant returns (address) {
         return owner;
     }
-    
 }
