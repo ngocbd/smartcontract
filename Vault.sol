f9870a4f04be6c5ca2d1145bc899d51e8857706d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:51:52 +0700

    add contract Vault

--- Vault.sol
+++ Vault.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0xf9293569e274aedf6b79efa2427a608e874dfb1c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0x5f09f0ee8115a6c55725641c1aff2775675b2466
 */
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.19;
 
 contract Token {
     function balanceOf(address) public constant returns (uint);
@@ -10,12 +10,12 @@ contract Token {
 
 contract Vault {
     Token constant public token = Token(0xa645264C5603E96c3b0B078cdab68733794B0A71);
-    address constant public recipient = 0x0007013D71C0946126d404Fd44b3B9c97F3418e7;
+    address constant public recipient = 0x002AE208AD6064F75Fa78e7bbeF9B12DB850f559;
     // UNIX timestamp
-    uint constant public unlockedAt = 1516788000;
+    uint constant public unlockedAt = 1517571900;
     
     function unlock() public {
-        if (now < unlockedAt) throw;
+        require(now > unlockedAt);
         uint vaultBalance = token.balanceOf(address(this));
         token.transfer(recipient, vaultBalance);
     }
