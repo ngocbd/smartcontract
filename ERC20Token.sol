377925f9dc234936ff865a90b2fc4dddb6ab83f7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:52:38 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x39d6ddeb924cc42356ddafe104318eae0b99f9a7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xd54d64088410d3377472549ca56f90136c0540ba
 */
 pragma solidity ^0.4.4;
 
@@ -106,11 +106,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 12000000000000000;               
-        totalSupply = 12000000000000000;                     
-        name = "AXIMUS";                                   
+        balances[msg.sender] = 800000000000000;               
+        totalSupply = 800000000000000;                     
+        name = "TRENDEX";                                   
         decimals = 8;                            
-        symbol = "AXS";                               
+        symbol = "TDX";                               
     }
 
   
