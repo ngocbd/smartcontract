44a668b2a64bc114c402d4b66da32ac01f7f25ec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:15:20 +0700

    add contract Number

--- Number.sol
+++ Number.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Number at 0xbf915382f51fc659d0a506f7037382edc49667e7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Number at 0xb1c384fab269ac9ad3caacddbc4a211a631947b9
 */
 pragma solidity ^0.4.18;
 contract NumberFactory{
