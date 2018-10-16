00fd919e796b1ec2bd9aaf31d1d42e6484410b16
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:56:46 +0700

    add contract AmIOnTheFork

--- AmIOnTheFork.sol
+++ AmIOnTheFork.sol
@@ -1,15 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AmIOnTheFork at 0x2bd2326c993dfaef84f696526064ff22eba5b362
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AmIOnTheFork at 0xe0b8fce1183e31c995b40460e6cb6712fa929f59
 */
 contract AmIOnTheFork {
     bool public forked = false;
     address constant darkDAO = 0x304a554a310c7e546dfe434669c62820b7d83490;
     // Check the fork condition during creation of the contract.
-    // This function should be called between block 1920000 and 1921200.
-    // Approximately between 2016-07-20 12:00:00 UTC and 2016-07-20 17:00:00 UTC.
+    // This function should be called between block 1920000 and 1930000.
     // After that the status will be locked in.
     function update() {
-        if (block.number >= 1920000 && block.number <= 1921200) {
+        if (block.number >= 1920000 && block.number <= 1930000) {
             forked = darkDAO.balance < 3600000 ether;
         }
     }
