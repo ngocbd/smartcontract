6a49070b21855e537542aeb8492a87bedc7f406c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:17:06 +0700

    add contract DPPBurn

---
+++ DPPBurn.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DPPBurn at 0x655C072286A8ab7B33B16Aa6908CFECDe39E57a1
+*/
+pragma solidity ^0.4.18;
+
+contract DPPBurn {
+    
+}
