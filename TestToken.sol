f0567f3375e62718ad88c6e855ac1033b7663a04
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:20:06 +0700

    add contract TestToken

--- TestToken.sol
+++ TestToken.sol
@@ -1,37 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestToken at 0xd3d20f3f0ededb2a1503dc8858c7c6f37c2b22cf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestToken at 0x40f2c50215e656aff88a7d816aa62569a996c11b
 */
 pragma solidity ^0.4.15;
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
 
 contract Token {
     uint256 public totalSupply;
@@ -40,18 +10,18 @@ contract Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
     function approve(address _spender, uint256 _value) returns (bool success);
     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address _from, address _to, uint256 _value);
+    event Approval(address _owner, address _spender, uint256 _value);
 }
 
 
 /*  ERC 20 token */
 contract StandardToken is Token {
-    using SafeMath for uint256;
+
     function transfer(address _to, uint256 _value) returns (bool success) {
-      if (balances[msg.sender] >= _value) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+      if (balances[msg.sender] >= _value && _value > 0) {
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
         Transfer(msg.sender, _to, _value);
         return true;
       } else {
@@ -60,10 +30,10 @@ contract StandardToken is Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value) {
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
         return true;
       } else {
@@ -75,7 +45,7 @@ contract StandardToken is Token {
         return balances[_owner];
     }
 
-    function approve(address _spender,  uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
@@ -85,37 +55,18 @@ contract StandardToken is Token {
       return allowed[_owner][_spender];
     }
 
-    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
-      allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-      Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-      return true;
-    }
-
-    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
-      uint oldValue = allowed[msg.sender][_spender];
-      if (_subtractedValue > oldValue) {
-        allowed[msg.sender][_spender] = 0;
-      } else {
-        allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-      }
-      Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-      return true;
-    }
-
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
 }
 
 contract TestToken is StandardToken {
-    event Mint(address indexed to, uint256 amount);
-
     uint256 public constant PRICE = 4000;
 
     // metadata
     string public constant name = "Infinite Test Token";
     string public constant symbol = "TEST";
-    uint8 public constant decimals = 18;
-    string public version = "1.1";
+    uint256 public constant decimals = 18;
+    string public version = "1.0";
 
     event CreatedToken();
 
@@ -131,7 +82,6 @@ contract TestToken is StandardToken {
         uint256 tokens = msg.value * PRICE;
         balances[beneficiary] += tokens;
         Transfer(0x0, beneficiary, tokens);
-        Mint(beneficiary, tokens);
         totalSupply += tokens;
         msg.sender.transfer(msg.value);
     }
