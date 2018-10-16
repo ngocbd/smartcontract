5e4c60a8edd6bbf5a5ceaeb7c9ebd5639a3c8523
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:48:33 +0700

    add contract Refund

--- Refund.sol
+++ Refund.sol
@@ -1,51 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Refund at 0x7c27efad27469bfa5369a5bcecd5c4bb07170a65
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Refund at 0xa1089ac6c47575c3b23b92babd85d3c61329847d
 */
-pragma solidity ^0.4.18;
-
-
+pragma solidity ^0.4.16;
 contract Refund {
-
-    address owner;
-
-    function Refund() {
+    address owner = 0x0;
+    function Refund() public payable {
+        // ???????????????
         owner = msg.sender;
     }
     
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
 
-    mapping (address => uint256) public balances;
-
-    function add_addys(address[] _addys, uint256[] _values) onlyOwner {
-        for (uint i = 0; i < _addys.length ; i++) {
-            balances[_addys[i]] += (_values[i]);
-        }
-    }
-
-    function refund() {
-        uint256 eth_to_withdraw = balances[msg.sender];
-        balances[msg.sender] = 0;
-        msg.sender.transfer(eth_to_withdraw);
-    }
-
-    function direct_refunds(address[] _addys, uint256[] _values) onlyOwner {
-        for (uint i = 0; i < _addys.length ; i++) {
-            uint256 to_refund = (_values[i]);
-            balances[_addys[i]] = 0;
-            _addys[i].transfer(to_refund);
-        }
-    }
-
-    function change_specific_addy(address _addy, uint256 _val) onlyOwner {
-        balances[_addy] = (_val);
-    }
-
-    function () payable {}
-
-    function withdraw_ether() onlyOwner {
-        owner.transfer(this.balance);
-    }
 }
