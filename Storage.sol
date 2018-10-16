4d2b9832de2ef4e67f953c5d815841fcef0fa2a1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:21:05 +0700

    add contract Storage

---
+++ Storage.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0x9cc9bf39a84998089050a90087e597c26758685d
+*/
+contract Storage {
+    uint pos0;
+    mapping(address => uint) pos1;
+    function Storage() {
+        pos0 = 1234;
+        pos1[0x1f4e7db8514ec4e99467a8d2ee3a63094a904e7a] = 5678;
+    }
+}
