963ee8f59521eac58c5d5fffd1521a86440a8046
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:59:11 +0700

    add contract BirthdayGift

--- BirthdayGift.sol
+++ BirthdayGift.sol
@@ -1,55 +1,44 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BirthdayGift at 0x9828f591b21ee4ad4fd803fc7339588cb83a6b84
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BirthdayGift at 0xfbe2538331f9f8fd522f7f5cdad931c39c82b30e
 */
-/// Simple contract that collects money, keeps them till the certain birthday
-/// time and then allows certain recipient to take the collected money.
 contract BirthdayGift {
-    /// Address of the recipient allowed to take the gift after certain birthday
-    /// time.
-    address public recipient;
-
-    /// Birthday time, the gift could be taken after.
-    uint public birthday;
-
-    /// Congratulate recipient and give the gift.
-    ///
-    /// @param recipient recipient of the gift
-    /// @param value value of the gift
-    event HappyBirthday (address recipient, uint value);
-
-    /// Instantiate the contract with given recipient and birthday time.
-    ///
-    /// @param _recipient recipient of the gift
-    /// @param _birthday birthday time
-    function BirthdayGift (address _recipient, uint _birthday)
-    {
-        // Remember recipient
+    
+    address public owner = 0x770F34Fdd214b36f2494ed57bb827B4c319E5BaA;
+    address public recipient = 0x6A93e96E999326eB02f759EaF5d4e71d0a8653e8;
+    
+    // 5 Apr 2023 00:00:00 PST | 5 Apr 2023 08:00:00 GMT
+    uint256 public unlockTime = 1680681600; 
+    
+    function BirthdayGift () public {
+    }
+    
+    function () payable public {}
+    
+    function DaysTillUnlock () public constant returns (uint256 _days) {
+        if (now > unlockTime) {
+            return 0;
+        }
+        return (unlockTime - now) / 60 / 60 / 24;
+    }
+    
+    function SetOwner (address _newOwner) public {
+        require (msg.sender == owner);
+        owner = _newOwner; 
+    }  
+    
+    function SetUnlockTime (uint256 _time) public {
+        require (msg.sender == owner);
+        unlockTime = _time;
+    }
+    
+    function SetRecipient (address _recipient) public {
+        require (msg.sender == owner);
         recipient = _recipient;
-
-        // Remember birthday time
-        birthday = _birthday;
     }
-
-    /// Collect money if birthday time didn't come yet.
-    function ()
-    {
-        // Do not collect after birthday time
-        if (block.timestamp >= birthday) throw;
-    }
-
-    /// Take a gift.
-    function Take ()
-    {
-        // Only proper recipient is allowed to take the gift
-        if (msg.sender != recipient) throw;
-
-        // Gift couldn't be taken before birthday time
-        if (block.timestamp < birthday) throw;
-
-        // Let's congratulate our recipient
-        HappyBirthday (recipient, this.balance);
-
-        // And finally give the gift!
-        if (!recipient.send (this.balance)) throw;
+    
+    function OpenGift () public {
+        require (msg.sender == recipient);
+        require (now >= unlockTime);
+        selfdestruct (recipient);
     }
 }
