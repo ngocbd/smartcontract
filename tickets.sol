bb808dfe0129b0f68d2c23db27d466e10f2e83b4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:59:41 +0700

    add contract Tickets

--- tickets.sol
+++ tickets.sol
@@ -1,210 +1,180 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract tickets at 0xd64dce6f3f3e27b7afc5eda41ebab00556eaf946
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Tickets at 0x2b0067e8a59a66a4fbf370b70d21133400b04ac2
 */
-pragma solidity ^0.4.15;
+contract Tickets {
 
-contract tickets {
-    
-    mapping(uint256 => uint256) public ticketPrices;
-    mapping(address => uint256[]) public ticketsOwners;
-    mapping(uint256 => address) public ticketsOwned;
-    mapping(address => uint256) public noOfTicketsOwned;
-    mapping(address => bool) public banned;
-    uint256 noOfSeats;
-    
-    mapping(address => uint256[]) public reservations;
-    mapping(address => uint256) public noOfreservations;
-    mapping(address => uint256) public timeOfreservations;
-    mapping(address => uint256) public priceOfreservations;
-    mapping(uint256 => address) public addressesReserving;
-    uint256 public lowestAddressReserving=0;
-    uint256 public highestAddressReserving=0;
-    
-    mapping(uint256 => uint256[]) public ticketTransfers;
-    mapping(uint256 => uint256) public ticketTransfersPerAmount;
-    uint256 public ticketTransfersAmount = 0;
-    mapping(address => uint256[]) public ticketTransferers;
-    mapping(address => uint256) public ticketTransferersAmount;
-    mapping(address => uint256[]) public ticketTransferees;
-    mapping(address => uint256) public ticketTransfereesAmount;
-    
-    mapping(address => bytes32) public hashes;
-    
-    string public name;
-    
-    uint256 public secondsToHold = 60 * 5 ;
-    
-    address public owner;
-    
-    function tickets(uint256[] ticks, uint256 nOfSeats, string n) {
-        for(uint256 i=0;i<nOfSeats;i++) {
-            ticketPrices[i] = ticks[i];
-        }
-        noOfSeats = nOfSeats;
-        name = n;
-        owner = msg.sender;
+    string public name = "Kraftwerk";
+    string public symbol = "KKT";
+    uint8 public decimals = 0;
+
+    address[1000] public holders;
+    mapping(uint256 => bool) public usedTickets;
+    mapping(uint256 => string) public additionalInfo;
+    mapping(address => uint[16]) public seatsList;
+    mapping(address => uint256) public balanceOf;
+    address[30000] public booking;
+    mapping(address => uint[16]) public bookingList;
+    mapping(address => uint256) public amountOfBooked;
+
+    address Manager;
+    address ManagerForRate;
+    address Company;
+    address nullAddress = 0x0;
+
+    uint public limitPerHolder = 16;
+    uint public seatsCount = 1000;
+    uint scaleMultiplier = 1000000000000000000; 
+
+    uint public Rate_Eth = 298;
+    uint public Ticket_Price = 15*scaleMultiplier/Rate_Eth;
+
+    modifier managerOnly { require(msg.sender == Manager); _; }
+    modifier rateManagerOnly { require(msg.sender == ManagerForRate); _; }
+
+    event LogAllocateTicket(uint256 _seatID, address _buyer, string _infoString);
+    event LogTransfer(address _holder, address _receiver, uint256 _seatID, string _infoStringt);
+    event LogRedeemTicket(uint _seatID, address _holder, string _infoString);
+    event LogBookTicket(uint _seatID, address _buyer,string _infoString);
+    event LogCancelReservation(address _buyer, uint _seatID);
+
+
+
+    function Tickets(address _ManagerForRate,  address _Manager, address _Company) {
+        ManagerForRate = _ManagerForRate;
+        Manager = _Manager;
+        Company = _Company;
     }
-    
-    function reserveSeats(uint256[] seats, uint256 nOfSeats) {
-        if(noOfreservations[msg.sender] != 0 && !banned[msg.sender]) {
-            revert();
-        }
-        resetReservationsInternal();
-        uint256 price = 0;
-        for(uint256 i=0;i<nOfSeats;i++) {
-            if(ticketsOwned[seats[i]] != 0x0) {
-                revert();
-            }
-            reservations[msg.sender].push(seats[i]);
-            price += ticketPrices[seats[i]];
-            ticketsOwned[seats[i]] = msg.sender;
-        }
-        noOfreservations[msg.sender] = nOfSeats;
-        timeOfreservations[msg.sender] = now;
-        priceOfreservations[msg.sender] = price;
-        noOfTicketsOwned[msg.sender]++;
-        highestAddressReserving++;
-        Reserved(msg.sender, seats);
+
+    function setRate(uint _RateEth) external rateManagerOnly {
+       Rate_Eth = _RateEth;
+       Ticket_Price = 15*scaleMultiplier/Rate_Eth;
     }
-    
-    function resetReservations(address requester, bool resetOwn) {
-        if(noOfreservations[requester] == 0) {
-            throw;
-        }
-        for(uint256 i=0;i<noOfreservations[requester] && resetOwn;i++) {
-            ticketsOwned[reservations[requester][i]] = 0x0;
-            noOfTicketsOwned[msg.sender]--;
-        }
-        reservations[requester] = new uint256[](0);
-        noOfreservations[requester] = 0;
-        timeOfreservations[requester] = 0;
-        priceOfreservations[requester] = 0;
+
+
+    function allocateTicket(uint256 seatID, address buyer, string infoString) external managerOnly {
+        require(seatID > 0 && seatID < seatsCount);
+        require(holders[seatID] == nullAddress);
+        require(balanceOf[buyer] < limitPerHolder);
+        require(booking[seatID] == nullAddress);
+        createTicket(seatID, buyer);
+        additionalInfo[seatID] = infoString;
+        LogAllocateTicket(seatID, buyer, infoString);
     }
-    
-    function resetReservationsInternal() private {
-        bool pastTheLowest = false;
-        bool stop = false;
-        for(uint256 i=lowestAddressReserving;i<highestAddressReserving && !stop;i++) {
-            if(timeOfreservations[addressesReserving[i]] != 0) {
-                pastTheLowest = true;
-                if(now - timeOfreservations[addressesReserving[i]] > secondsToHold) {
-                    resetReservations(addressesReserving[i], true);
-                } else {
-                    stop = true;
-                }
-            }
-            if(timeOfreservations[addressesReserving[i]] == 0 && !pastTheLowest) {
-                lowestAddressReserving = i;
-            }
-            
-        }
-    }
-    
-    function revokeTickets(address revokee, bool payback) payable {
-        if(msg.sender == owner) {
-            banned[revokee] = true;
-            uint256 price = 0;
-            for(uint256 i=0;i<noOfTicketsOwned[revokee];i++) {
-                ticketsOwned[ticketsOwners[revokee][i]] = 0x0;
-                price+=ticketPrices[ticketsOwners[revokee][i]];
-            }
-            ticketsOwners[revokee] = new uint256[](0);
-            noOfTicketsOwned[revokee] = 0;
-            if(payback) {
-                revokee.send(price);
-            }
-            Banned(revokee, payback);
-        }
-    }
-    
-    function InvokeTransfer(address transferee, uint256[] ticks, uint256 amount) {
-        if(amount>0 && getTransfer(msg.sender,transferee) != 100000000000000000) {
-            for(uint256 i=0;i<amount;i++) {
-                ticketTransfers[ticketTransfersAmount].push(ticks[i]);
-            }
-            ticketTransferers[msg.sender][ticketTransferersAmount[msg.sender]++] = ticketTransfersAmount;
-            ticketTransferees[transferee][ticketTransfereesAmount[transferee]++] = ticketTransfersAmount;
-            ticketTransfersPerAmount[ticketTransfersAmount] = amount;
-            TransferStarted(msg.sender, transferee, ticks, ticketTransfersAmount++);
-        } else {
-            revert();
-        }
-    }
-    
-    function removeTransfer(uint256 transferID) {
-        bool transferer = false;
-        for(uint256 i=0;i<ticketTransferersAmount[msg.sender] && !transferer;i++) {
-            if(ticketTransferers[msg.sender][i] == transferID) {
-                transferer = true;
+
+    function createTicket(uint256 seatID, address buyer) internal {
+        uint i = 0;
+        for(i = 0; i < limitPerHolder; i++)
+        {
+            if(seatsList[buyer][i] == 0)
+            {
+                break;
             }
         }
-        if(transferer) {
-            ticketTransfers[transferID] = new uint256[](0);
-        } else {
-            revert();
-        }
+        holders[seatID] = buyer;
+        balanceOf[buyer] += 1;
+        seatsList[buyer][i] = seatID;
     }
-    
-    function finishTransfer(uint256 transferID) payable {
-        bool transferee = false;
-        for(uint256 j=0;j<ticketTransfereesAmount[msg.sender] && !transferee;j++) {
-            if(ticketTransferees[msg.sender][j] == transferID) {
-                transferee = true;
+
+    function redeemTicket(uint seatID, address holder) external managerOnly{
+        require(seatID > 0 && seatID < seatsCount);
+        require(usedTickets[seatID] == false);
+        require(holders[seatID] == holder);
+        usedTickets[seatID] = true;
+        string infoString = additionalInfo[seatID];
+        LogRedeemTicket(seatID, holder, infoString);
+    }
+
+    function transfer(address holder, address receiver, uint256 seatID) external managerOnly{
+        require(seatID > 0 && seatID < seatsCount);
+        require(holders[seatID] == holder);
+        require(balanceOf[receiver] < limitPerHolder);
+        require(holder != receiver);
+        uint i = 0;
+        holders[seatID] = receiver;
+        balanceOf[holder] -= 1;
+        if(receiver != nullAddress)
+        {
+            for(i = 0; i < limitPerHolder; i++)
+              {
+                  if(seatsList[receiver][i] == 0)
+                  {
+                     break;
+                  }
+            }
+            balanceOf[receiver] += 1;
+            seatsList[receiver][i] = seatID;
+        }
+        for(i = 0; i < limitPerHolder; i++)
+        {
+            if(seatsList[holder][i] == seatID)
+            {
+                seatsList[holder][i] = 0;
             }
         }
-        if(!transferee) {
-            revert();
+        string infoString = additionalInfo[seatID];
+        LogTransfer(holder, receiver, seatID, infoString);
+    }
+
+    function bookTicket(uint256 seatID, address buyer, string infoString) external managerOnly{
+        require(seatID > 0 && seatID < seatsCount);
+        require(holders[seatID] == nullAddress);
+        require(booking[seatID] == nullAddress);
+        require(balanceOf[buyer] + amountOfBooked[buyer] < limitPerHolder);
+        uint i = 0;
+        booking[seatID] = buyer;
+        amountOfBooked[buyer] += 1;
+        while(bookingList[buyer][i] != 0) {
+            i++;
         }
-        uint256 price = 0;
-        for(uint256 i=0;i<ticketTransfersPerAmount[transferID];i++) {
-            price += ticketPrices[ticketTransfers[transferID][i]];
-        }
-        if(msg.value == price) {
-            for(i=0;i<ticketTransfersPerAmount[transferID];i++) {
-                ticketsOwned[ticketTransfers[transferID][i]] = msg.sender;
+        bookingList[buyer][i] = seatID;
+        additionalInfo[seatID] = infoString;
+        LogBookTicket(seatID, buyer, infoString);
+    }
+
+    function cancelReservation(address buyer, uint256 seatID) external managerOnly{
+        require(booking[seatID] == buyer);
+        uint i = 0;
+        while(i < limitPerHolder) {
+            if (seatID == bookingList[buyer][i]){
+              booking[seatID] = nullAddress;
+              bookingList[buyer][i] = 0;
+              break;
             }
-            Transferred(transferID);
-        } else {
-            revert();
+            i++;
         }
+        amountOfBooked[buyer] -= 1;
+        LogCancelReservation(buyer, seatID);
     }
-    
-    function getTransfer(address transferer, address transferee) returns (uint256) {
-        for(uint256 i=0;i<ticketTransferersAmount[transferer];i++) {
-            for(uint256 j=0;j<ticketTransfereesAmount[transferee];j++) {
-                if(ticketTransferers[transferer][i] == ticketTransferees[transferee][j]) {
-                    return ticketTransferees[transferee][j];
-                }
-            }
-        }
-        return 100000000000000000;
-    }
-    
-    function setHash(bytes32 hash) {
-        hashes[msg.sender] = hash;
-    }
-    
-    function checkHash(address a, string password) constant returns (bool) {
-        return hashes[a]!="" && hashes[a] == sha3(password);
-    }
-    
+
+
     function() payable {
-        if(msg.value == priceOfreservations[msg.sender] && !banned[msg.sender]) {
-            for(uint256 i=0;i<noOfreservations[msg.sender];i++) {
-                ticketsOwners[msg.sender].push(reservations[msg.sender][i]);
-            }
-            resetReservations(msg.sender, false);
-            owner.send(msg.value);
-            Confirmed(msg.sender);
-        } else {
-            revert();
-        }
+        require(amountOfBooked[msg.sender] != 0);
+        require(balanceOf[msg.sender] + amountOfBooked[msg.sender] <= limitPerHolder);
+        require(msg.value >= Ticket_Price * amountOfBooked[msg.sender]);
+        makePayment(msg.sender);
     }
-    
-    event Reserved(address indexed _to, uint256[] _tickets);
-    event Confirmed(address indexed _to);
-    event TransferStarted(address indexed _from, address indexed _to, uint256[] _tickets, uint256 _transferID);
-    event Transferred(uint256 _transferID);
-    event Banned(address indexed _banned, bool payback);
-    
+
+    function makePayment(address buyer) internal {
+        uint i = 0;
+        uint seatID;
+        string infoString;
+        while(i < limitPerHolder) {
+            if(bookingList[buyer][i] != 0) {
+              seatID = bookingList[buyer][i];
+              bookingList[buyer][i] = 0;
+              booking[seatID] = nullAddress;
+              createTicket(seatID, buyer);
+              infoString = additionalInfo[seatID];
+              LogAllocateTicket(seatID, msg.sender, infoString);
+            }
+            i++;
+        }
+        amountOfBooked[buyer] = 0;
+    }
+
+    function withdrawEther(uint256 _value) external managerOnly{
+       Company.transfer(_value);
+    }
+
+
 }
