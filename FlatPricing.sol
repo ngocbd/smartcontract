2696abdc62bb379dd31c1e489ae1be6e7ef9a3c2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:47:41 +0700

    add contract FlatPricing

--- FlatPricing.sol
+++ FlatPricing.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0x4ba4e61241fe471c5ce76cc2ec582b9da7d5376e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricing at 0x95eb44f1e346c761e1031b2658fe614384fbeb57
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
