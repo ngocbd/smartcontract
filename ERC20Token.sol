7c9694818f2f083f30d50260d06c2e38eaf2d857
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:56:42 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x944d5a32977997a055f2fe13ed61b7950169b0d8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xe640d8c3d7e101e3d6f44bc5901e04f57ec90433
 */
 pragma solidity ^0.4.4;
 
@@ -119,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 7539200000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 7539200000;                        // Update total supply (100000 for example)
-        name = "MAHBOOB";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "BOOB";                               // Set the symbol for display purposes
+        balances[msg.sender] = 2800000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 2800000000000000;                        // Update total supply (100000 for example)
+        name = "ANDCoin28";                                   // Set the name for display purposes
+        decimals = 8;                            // Amount of decimals for display purposes
+        symbol = "ANDC";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
