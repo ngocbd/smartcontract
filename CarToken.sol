546d76b6ca36d68dd32a25082e4938da1153df16
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:20:54 +0700

    add contract CARToken

--- CarToken.sol
+++ CarToken.sol
@@ -1,206 +1,203 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CarToken at 0xf4194a1046aaab13f43cc08f2a91157a2e065b02
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CARToken at 0x423e4322cdda29156b49a17dfbd2acc4b280600d
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.11;
 
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+contract SafeMath {
+    function safeMul(uint a, uint b) internal returns (uint) {
+        uint c = a * b;
+        require(a == 0 || c / a == b);
+        return c;
+    }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    function safeSub(uint a, uint b) internal returns (uint) {
+        require(b <= a);
+        return a - b;
+    }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        uint c = a + b;
+        require(c>=a && c>=b);
+        return c;
+    }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function safeDiv(uint a, uint b) internal returns (uint) {
+        require(b > 0);
+        uint c = a / b;
+        require(a == b * c + a % b);
+        return c;
+    }
 }
 
-    contract owned {
-        address public owner;
+contract Token {
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-        function owned() {
-            owner = msg.sender;
-        }
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
 
-        modifier onlyOwner {
-            require(msg.sender == owner);
-            _;
-        }
+/* ERC 20 token */
+contract ERC20Token is Token {
 
-        function transferOwnership(address newOwner) onlyOwner {
-            owner = newOwner;
-        }
-    }
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
-
-contract CarToken is owned {
-
-	using SafeMath for uint256;
-	
-    // Public variables of the token
-    string public name = "Car Token";
-    string public symbol = "CAR";
-    uint8 public decimals = 0;
-    uint256 public totalSupply = 19000000;
-
-    // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    // This generates a public event on the blockchain that will notify clients
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
-
-	
-    function CarToken(){
-     balanceOf[msg.sender] = totalSupply;
-    }
-
-	function mintToken(address target, uint256 mintedAmount) onlyOwner {
-		balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        Transfer(0, owner, mintedAmount);
-        Transfer(owner, target, mintedAmount);
-    }
-	
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
-        balanceOf[_from] -= _value;
-        // Add the same to the recipient
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
-
-    /**
-     * Transfer tokens from other address
-     *
-     * Send `_value` tokens to `_to` in behalf of `_from`
-     *
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens in your behalf
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
             return true;
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    mapping(address => uint256) balances;
+
+    mapping (address => mapping (address => uint256)) allowed;
+
+    uint256 public totalSupply;
+}
+
+
+/**
+ * CAR ICO contract.
+ *
+ */
+contract CARToken is ERC20Token, SafeMath {
+
+    string public name = "CAR SHARING";
+    string public symbol = "CAR";
+	uint public decimals = 9;
+
+    address public tokenIssuer = 0x0;
+	
+    // Unlock time
+	uint public month12Unlock = 1546387199;
+	uint public month24Unlock = 1577923199;
+	uint public month30Unlock = 1593647999;
+    uint public month48Unlock = 1641081599;
+	uint public month60Unlock = 1672617599;
+	
+	// End token sale
+	uint public endTokenSale = 1577836799;
+	
+	// Allocated
+    bool public month12Allocated = false;
+	bool public month24Allocated = false;
+	bool public month30Allocated = false;
+    bool public month48Allocated = false;
+	bool public month60Allocated = false;
+	
+
+    // Token count
+	uint totalTokenSaled = 0;
+    uint public totalTokensCrowdSale = 95000000 * 10**decimals;
+    uint public totalTokensReserve = 95000000 * 10**decimals;
+
+	event TokenMint(address newTokenHolder, uint amountOfTokens);
+    event AllocateTokens(address indexed sender);
+
+    function CARToken() {
+        tokenIssuer = msg.sender;
+    }
+	
+	/* Change issuer address */
+    function changeIssuer(address newIssuer) {
+        require(msg.sender==tokenIssuer);
+        tokenIssuer = newIssuer;
+    }
+
+    /* Allocate Tokens */
+    function allocateTokens()
+    {
+        require(msg.sender==tokenIssuer);
+        uint tokens = 0;
+     
+		if(block.timestamp > month12Unlock && !month12Allocated)
+        {
+			month12Allocated = true;
+			tokens = safeDiv(totalTokensReserve, 5);
+			balances[tokenIssuer] = safeAdd(balances[tokenIssuer], tokens);
+			totalSupply = safeAdd(totalSupply, tokens);
+            
         }
-    }
+        else if(block.timestamp > month24Unlock && !month24Allocated)
+        {
+			month24Allocated = true;
+			tokens = safeDiv(totalTokensReserve, 5);
+			balances[tokenIssuer] = safeAdd(balances[tokenIssuer], tokens);
+			totalSupply = safeAdd(totalSupply, tokens);
+			
+        }
+		if(block.timestamp > month30Unlock && !month30Allocated)
+        {
+			month30Allocated = true;
+			tokens = safeDiv(totalTokensReserve, 5);
+			balances[tokenIssuer] = safeAdd(balances[tokenIssuer], tokens);
+			totalSupply = safeAdd(totalSupply, tokens);
+            
+        }
+        else if(block.timestamp > month48Unlock && !month48Allocated)
+        {
+			month48Allocated = true;
+			tokens = safeDiv(totalTokensReserve, 5);
+			balances[tokenIssuer] = safeAdd(balances[tokenIssuer], tokens);
+			totalSupply = safeAdd(totalSupply, tokens);
+        }
+		else if(block.timestamp > month60Unlock && !month60Allocated)
+        {
+            month60Allocated = true;
+            tokens = safeDiv(totalTokensReserve, 5);
+            balances[tokenIssuer] = safeAdd(balances[tokenIssuer], tokens);
+            totalSupply = safeAdd(totalSupply, tokens);
+        }
+        else revert();
 
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
-        return true;
+        AllocateTokens(msg.sender);
     }
     
-    function distributeToken(address[] addresses, uint256 _value) onlyOwner {
-     for (uint i = 0; i < addresses.length; i++) {
-         balanceOf[owner] -= _value;
-         balanceOf[addresses[i]] += _value;
-         Transfer(owner, addresses[i], _value);
-     }
-}
-
+	/* Mint Token */
+    function mintTokens(address tokenHolder, uint256 amountToken) 
+    returns (bool success) 
+    {
+		require(msg.sender==tokenIssuer);
+		
+		if(totalTokenSaled + amountToken <= totalTokensCrowdSale && block.timestamp <= endTokenSale)
+		{
+			balances[tokenHolder] = safeAdd(balances[tokenHolder], amountToken);
+			totalTokenSaled = safeAdd(totalTokenSaled, amountToken);
+			totalSupply = safeAdd(totalSupply, amountToken);
+			TokenMint(tokenHolder, amountToken);
+			return true;
+		}
+		else
+		{
+		    return false;
+		}
+    }
 }
