fbb4861bb16b481928670cc16c690c1d816f84bc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:15 +0700

    add contract RoundToken

--- RoundToken.sol
+++ RoundToken.sol
@@ -1,40 +1,62 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RoundToken at 0x4993CB95c7443bdC06155c5f5688Be9D8f6999a5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RoundToken at 0xff95FBB77852C846724CE4Fa140Ca25f31A90bb0
 */
 pragma solidity ^0.4.0;
 
 contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 
 contract RoundToken {
-
+   
   string public constant name = "ROUND";
+
   string public constant symbol = "ROUND";
+
   uint8 public constant decimals = 18;
+
   string public constant version = '0.1';
+
   uint256 public constant totalSupply = 1000000000 * 1000000000000000000;
 
   address public owner;
 
+  uint256 public constant lockedUntilBlock = 0;
+
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  event BlockLockSet(uint256 _value);
   event NewOwner(address _newOwner);
+  
+  modifier onlyOwner {
+    if (msg.sender == owner) {
+      _;
+    }
+  }
+
+  function isLocked() constant returns (bool success) {
+    return lockedUntilBlock > block.number;
+  }
+
+  modifier blockLock(address _sender) {
+    if (!isLocked() || _sender == owner) {
+      _;
+    }
+  }
 
   modifier checkIfToContract(address _to) {
     if(_to != address(this))  {
       _;
     }
-  }
+  }    
 
   mapping (address => uint256) balances;
   mapping (address => mapping (address => uint256)) allowed;
 
   function RoundToken() {
     owner = msg.sender;
-    balances[owner] = totalSupply;
-  }
+    balances[owner] = totalSupply;        
+  }  
 
-  function replaceOwner(address _newOwner) returns (bool success) {
-    if (msg.sender != owner) throw;
+  function replaceOwner(address _newOwner) onlyOwner returns (bool success) {
     owner = _newOwner;
     NewOwner(_newOwner);
     return true;
@@ -44,7 +66,7 @@ contract RoundToken {
     return balances[_owner];
   }
 
-  function transfer(address _to, uint256 _value) checkIfToContract(_to) returns (bool success) {
+  function transfer(address _to, uint256 _value) blockLock(msg.sender) checkIfToContract(_to) returns (bool success) {
     if (balances[msg.sender] >= _value && _value > 0) {
       balances[msg.sender] -= _value;
       balances[_to] += _value;
@@ -55,7 +77,7 @@ contract RoundToken {
     }
   }
 
-  function transferFrom(address _from, address _to, uint256 _value) checkIfToContract(_to) returns (bool success) {
+  function transferFrom(address _from, address _to, uint256 _value) blockLock(_from) checkIfToContract(_to) returns (bool success) {
     if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
       balances[_to] += _value;
       balances[_from] -= _value;
@@ -65,7 +87,7 @@ contract RoundToken {
     } else {
       return false;
     }
-  }
+  }  
 
   function approve(address _spender, uint256 _value) returns (bool success) {
     allowed[msg.sender][_spender] = _value;
@@ -83,5 +105,5 @@ contract RoundToken {
 
   function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
     return allowed[_owner][_spender];
-  }
+  }      
 }
