c09a8974e3747d633ce1b58534c152a79b18e6d2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:46:46 +0700

    add contract DaoChallenge

--- DaoChallenge.sol
+++ DaoChallenge.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DaoChallenge at 0x131a76478D2eef5cEAA28e93030eB8a8894aD811
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DaoChallenge at 0x16163229926aad97318fac28c8d06101954ad732
 */
 contract AbstractDaoChallenge {
 	function isMember (DaoAccount account, address allegedOwnerAddress) returns (bool);
@@ -129,19 +129,20 @@ contract DaoAccount
 		suicide(challengeOwner);
 	}
 }
+
 contract DaoChallenge
 {
 	/**************************
 					Constants
 	***************************/
 
+	uint256 constant public tokenPrice = 1000000000000000; // 1 finney
 
 	/**************************
 					Events
 	***************************/
 
 	event notifyTerminate(uint256 finalBalance);
-	event notifyTokenIssued(uint256 n, uint256 price, uint deadline);
 
 	event notifyNewAccount(address owner, address account);
 	event notifyBuyToken(address owner, uint256 tokens, uint256 price);
@@ -151,12 +152,6 @@ contract DaoChallenge
 	/**************************
 	     Public variables
 	***************************/
-
-	// For the current token issue:
-	uint public tokenIssueDeadline = now;
-	uint256 public tokensIssued = 0;
-	uint256 public tokensToIssue = 0;
-	uint256 public tokenPrice = 1000000000000000; // 1 finney
 
 	mapping (address => DaoAccount) public daoAccounts;
 
@@ -226,40 +221,9 @@ contract DaoChallenge
 		return account.getTokenBalance();
 	}
 
-	// n: max number of tokens to be issued
-	// price: in szabo, e.g. 1 finney = 1,000 szabo = 0.001 ether
-	// deadline: unix timestamp in seconds
-	function issueTokens (uint256 n, uint256 price, uint deadline) noEther onlyChallengeOwner {
-		// Only allow one issuing at a time:
-		if (now < tokenIssueDeadline) throw;
-
-		// Deadline can't be in the past:
-		if (deadline < now) throw;
-
-		// Issue at least 1 token
-		if (n == 0) throw;
-
-		tokenPrice = price * 1000000000000;
-		tokenIssueDeadline = deadline;
-		tokensToIssue = n;
-		tokensIssued = 0;
-
-		notifyTokenIssued(n, price, deadline);
-	}
-
 	function buyTokens () returns (uint256 tokens) {
-		tokens = msg.value / tokenPrice;
-
-		if (now > tokenIssueDeadline) throw;
-		if (tokensIssued >= tokensToIssue) throw;
-
-		// This hopefully prevents issuing too many tokens
-		// if there's a race condition:
-		tokensIssued += tokens;
-		if (tokensIssued > tokensToIssue) throw;
-
 	  DaoAccount account = accountFor(msg.sender, true);
-		if (account.buyTokens.value(msg.value)() != tokens) throw;
+		tokens = account.buyTokens.value(msg.value)();
 
 		notifyBuyToken(msg.sender, tokens, msg.value);
 		return tokens;
