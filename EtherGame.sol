6429e166bc61f903017fcf4f1bf0ba4a7d7b7d41
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:08:24 +0700

    add contract EtherGame

--- EtherGame.sol
+++ EtherGame.sol
@@ -1,32 +1,24 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGame at 0xaa54201cab79e4de3072ededd32ea68d8cfa2375
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGame at 0x49d2374241a0a4b407a71d359a753fc630c5d8d4
 */
 pragma solidity ^0.4.10;
 
 contract EtherGame 
 {
     uint[] a;
-    function Test1(uint a) public constant returns(address)
+    function Test1(uint a) public returns(address)
     {
         return msg.sender;
     }
-    function Test2(uint a) constant returns(address)
+    function Test2(uint a) returns(address)
     {
         return msg.sender;
     }
-    function Test3(uint b) public constant returns(uint)
+    function Test3(uint b) public returns(uint)
     {
         return a.length;
     }
-    function Test4(uint b) constant returns(uint)
-    {
-        return a.length;
-    }
-    function Test5(uint b) external constant returns(uint)
-    {
-        return a.length;
-    }
-    function Test6() constant returns(uint)
+    function Test4(uint b) returns(uint)
     {
         return a.length;
     }
