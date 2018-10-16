71fb71b729916ee6530abb50e4f2ce7796459766
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:09:00 +0700

    add contract HireGoCrowdsale

--- HireGoCrowdsale.sol
+++ HireGoCrowdsale.sol
@@ -1,22 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HireGoCrowdsale at 0x3d3ce5c8baeeb7e6ded63afcb443272a9703527a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HireGoCrowdsale at 0x47349ebb01fa27bb3ad72995ff7182d949851a93
 */
-contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-
+pragma solidity ^0.4.18;
 
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -47,6 +32,21 @@ library SafeMath {
   }
 }
 
+
+contract ERC20Basic {
+    uint256 public totalSupply;
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 contract BasicToken is ERC20Basic {
     using SafeMath for uint256;
@@ -79,7 +79,6 @@ contract BasicToken is ERC20Basic {
     }
 
 }
-
 
 
 contract Ownable {
@@ -118,8 +117,6 @@ contract Ownable {
     }
 
 }
-
-
 
 contract StandardToken is ERC20, BasicToken {
 
@@ -195,6 +192,26 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
+contract BurnableToken is StandardToken {
+
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value > 0);
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
+    }
+}
 
 contract MintableToken is StandardToken, Ownable {
     event Mint(address indexed to, uint256 amount);
@@ -234,30 +251,6 @@ contract MintableToken is StandardToken, Ownable {
 }
 
 
-
-contract BurnableToken is StandardToken {
-
-    event Burn(address indexed burner, uint256 value);
-
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
-    function burn(uint256 _value) public {
-        require(_value > 0);
-        require(_value <= balances[msg.sender]);
-        // no need to require value <= totalSupply, since that would imply the
-        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
-
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        Burn(burner, _value);
-    }
-}
-
-
-
 contract HireGoToken is MintableToken, BurnableToken {
 
     string public constant name = "HireGo";
@@ -265,12 +258,11 @@ contract HireGoToken is MintableToken, BurnableToken {
     uint32 public constant decimals = 18;
 
     function HireGoToken() public {
-        totalSupply = 100000000E18;
+        totalSupply = 100000000E18;  //100m
         balances[owner] = totalSupply; // Add all tokens to issuer balance (crowdsale in this case)
     }
 
 }
-
 
 
 contract HireGoCrowdsale is Ownable {
@@ -281,8 +273,6 @@ contract HireGoCrowdsale is Ownable {
     uint totalSupply = token.totalSupply();
 
     bool public isRefundAllowed;
-    bool public newBonus_and_newPeriod;
-    bool public new_bonus_for_next_period;
 
     uint public icoStartTime;
     uint public icoEndTime;
@@ -290,21 +280,23 @@ contract HireGoCrowdsale is Ownable {
     uint public weiRaised;
     uint public hardCap; // amount of ETH collected, which marks end of crowd sale
     uint public tokensDistributed; // amount of bought tokens
-    uint public bonus_for_add_stage;
+    uint public foundersTokensUnlockTime;
 
     /*         Bonus variables          */
-    uint internal baseBonus1 = 160;
-    uint internal baseBonus2 = 140;
-    uint internal baseBonus3 = 130;
-    uint internal baseBonus4 = 120;
-    uint internal baseBonus5 = 110;
-	  uint internal baseBonus6 = 100;
+    uint internal baseBonus1 = 135;
+    uint internal baseBonus2 = 130;
+    uint internal baseBonus3 = 125;
+    uint internal baseBonus4 = 115;
     uint public manualBonus;
     /* * * * * * * * * * * * * * * * * * */
 
+    uint public waveCap1;
+    uint public waveCap2;
+    uint public waveCap3;
+    uint public waveCap4;
+
     uint public rate; // how many token units a buyer gets per wei
     uint private icoMinPurchase; // In ETH
-    uint private icoEndDateIncCount;
 
     address[] public investors_number;
     address private wallet; // address where funds are collected
@@ -324,6 +316,11 @@ contract HireGoCrowdsale is Ownable {
         _;
     }
 
+    modifier foundersTokensUnlocked() {
+        require(now > foundersTokensUnlockTime);
+        _;
+    }
+
     modifier crowdsaleInProgress() {
         bool withinPeriod = (now >= icoStartTime && now <= icoEndTime);
         require(withinPeriod);
@@ -338,14 +335,19 @@ contract HireGoCrowdsale is Ownable {
 
         icoStartTime = _icoStartTime;
         icoEndTime = _icoEndTime;
+        foundersTokensUnlockTime = icoEndTime.add(180 days);
         wallet = _wallet;
 
         rate = 250 szabo; // wei per 1 token (0.00025ETH)
 
-        hardCap = 11575 ether;
-        icoEndDateIncCount = 0;
+        hardCap = 11836 ether;
         icoMinPurchase = 50 finney; // 0.05 ETH
         isRefundAllowed = false;
+
+        waveCap1 = 2777 ether;
+        waveCap2 = waveCap1.add(2884 ether);
+        waveCap3 = waveCap2.add(4000 ether);
+        waveCap4 = waveCap3.add(2174 ether);
     }
 
     // fallback function can be used to buy tokens
@@ -372,18 +374,17 @@ contract HireGoCrowdsale is Ownable {
         isRefundAllowed = true;
     }
 
-    // Moves ICO ending date by one month. End date can be moved only 1 times.
-    // Returns true if ICO end date was successfully shifted
-    function moveIcoEndDateByOneMonth(uint bonus_percentage) public onlyOwner crowdsaleInProgress returns (bool) {
-        if (icoEndDateIncCount < 1) {
-            icoEndTime = icoEndTime.add(30 days);
-            icoEndDateIncCount++;
-            newBonus_and_newPeriod = true;
-            bonus_for_add_stage = bonus_percentage;
-            return true;
-        }
-        else {
-            return false;
+    // Sends ordered tokens to investors after ICO end if soft cap is reached
+    // tokens can be send only if ico has ended
+    function sendOrderedTokens() public onlyOwner crowdsaleEnded {
+        address investor;
+        uint tokensCount;
+        for(uint i = 0; i < investors_number.length; i++) {
+            investor = investors_number[i];
+            tokensCount = orderedTokens[investor];
+            assert(tokensCount > 0);
+            orderedTokens[investor] = 0;
+            token.transfer(investor, tokensCount);
         }
     }
 
@@ -416,21 +417,12 @@ contract HireGoCrowdsale is Ownable {
         }
     }
 
-    // Owner of contract can withdraw collected ETH, if soft cap is reached, by calling this function
+    // Owner of contract can withdraw collected ETH by calling this function
     function withdraw() public onlyOwner {
         uint to_send = weiRaised;
         weiRaised = 0;
         FundsWithdrawn(msg.sender, to_send);
         wallet.transfer(to_send);
-    }
-
-    // This function should be used to manually reserve some tokens for "big sharks" or bug-bounty program participants
-    function manualReserve(address _beneficiary, uint _amount) public onlyOwner crowdsaleInProgress {
-        require(_beneficiary != address(0));
-        require(_amount > 0);
-        checkAndMint(_amount);
-        tokensDistributed = tokensDistributed.add(_amount);
-        token.transfer(_beneficiary, _amount);
     }
 
     function burnUnsold() public onlyOwner crowdsaleEnded {
@@ -442,8 +434,8 @@ contract HireGoCrowdsale is Ownable {
         icoEndTime = now;
     }
 
-    function distribute_for_founders() public onlyOwner {
-        uint to_send = 40000000000000000000000000; //40m
+    function distribute_for_founders() public onlyOwner foundersTokensUnlocked {
+        uint to_send = 40000000E18; //40m
         checkAndMint(to_send);
         token.transfer(wallet, to_send);
     }
@@ -487,8 +479,6 @@ contract HireGoCrowdsale is Ownable {
         orderedTokens[_beneficiary] = orderedTokens[_beneficiary].add(_tokens);
 
         if (change > 0) _beneficiary.transfer(change);
-
-        token.transfer(_beneficiary,_tokens);
     }
 
     // Calculates bonuses based on current stage
@@ -504,28 +494,20 @@ contract HireGoCrowdsale is Ownable {
     // Calculates bonuses, specific for the ICO
     // Contains date and volume based bonuses
     function calculateBonusIco(uint _baseAmount) internal returns(uint) {
-        if(now >= icoStartTime && now < 1520726399) {//3:55-4
-            // 4-10 Mar - 60% bonus
+        if(totalWeiRaised < waveCap1) {
             return _baseAmount.mul(baseBonus1).div(100);
         }
-        else if(now >= 1520726400 && now < 1521331199) {
-            // 11-17 Mar - 40% bonus
+        else if(totalWeiRaised >= waveCap1 && totalWeiRaised < waveCap2) {
             return _baseAmount.mul(baseBonus2).div(100);
         }
-        else if(now >= 1521331200 && now < 1521935999) {
-            // 18-24 Mar - 30% bonus
+        else if(totalWeiRaised >= waveCap2 && totalWeiRaised < waveCap3) {
             return _baseAmount.mul(baseBonus3).div(100);
         }
-        else if(now >= 1521936000 && now < 1524959999) {
-            // 25 Mar-28 Apr - 20% bonus
+        else if(totalWeiRaised >= waveCap3 && totalWeiRaised < waveCap4) {
             return _baseAmount.mul(baseBonus4).div(100);
         }
-        else if(now >= 1524960000 && now < 1526169599) {
-            //29 Apr - 12 May - 10% bonus
-            return _baseAmount.mul(baseBonus5).div(100);
-        }
         else {
-            //13 May - 26 May - no bonus
+            // No bonus
             return _baseAmount;
         }
     }
