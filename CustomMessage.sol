da9efe159656f1666409662372752604e08ea10a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:42:50 +0700

    add contract CustomMessage

---
+++ CustomMessage.sol
@@ -0,0 +1,9 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomMessage at 0x12fb2394324124ca27ce0b6723b5cd9e9dafaaee
+*/
+pragma solidity ^0.4.2;
+
+contract CustomMessage
+{
+	string constant public message = "Hi nickpsecurity! -chadski";
+}
