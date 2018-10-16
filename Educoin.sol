6f31a1d038e4ff1c4c0702c2e08f72aa1c2703e8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:07:51 +0700

    add contract EduCoin

--- Educoin.sol
+++ Educoin.sol
@@ -1,167 +1,63 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Educoin at 0xb2723a0a68d9068d673a42ea314729a721251d02
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EduCoin at 0xc82ed6226c8fbbd1f7db529e81c1c36b82072576
 */
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.18;
+
 contract owned {
     address public owner;
 
-    function owned() {
+    function owned() public {
         owner = msg.sender;
     }
 
     modifier onlyOwner {
-        if (msg.sender != owner) revert();
+        require(msg.sender == owner);
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner {
+    function transferOwnership(address newOwner) onlyOwner public {
         owner = newOwner;
     }
 }
 
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+contract EduCoin is owned {
+    string public constant name = "EduCoin";
+    string public constant symbol = "EDU";
+    uint256 private constant _INITIAL_SUPPLY = 15000000000;  //???????150?
+    uint8 public decimals = 0;
 
-contract token {
-    /* Public variables of the token */
-    string public name;
-    string public symbol;
-    uint8 public decimals;
     uint256 public totalSupply;
 
-    /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function token(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-        ) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    event Burn(address indexed from, uint256 value);
+   
+    function EduCoin (
+        address genesis
+    ) public {
+        owner = msg.sender;
+        require(owner != 0x0);
+        require(genesis != 0x0);
+        totalSupply = _INITIAL_SUPPLY;
+        balanceOf[genesis] = totalSupply;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-    }
-
-    /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-
-    /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {    
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
+    function _transfer(address _from, address _to, uint _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
         Transfer(_from, _to, _value);
-        return true;
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function () {
-        revert();     // Prevents accidental sending of ether
-    }
-}
-
-contract Educoin is owned, token {
-
-    uint public buyRate = 4000; // price of one token
-    bool public isSelling = true;
-
-    mapping (address => bool) public frozenAccount;
-
-    /* This generates a public event on the blockchain that will notify clients */
-    event FrozenFunds(address target, bool frozen);
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function Educoin(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-    ) token (initialSupply, tokenName, decimalUnits, tokenSymbol) {}
-
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
-        if (frozenAccount[msg.sender]) revert();                // Check if frozen
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
     }
 
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (frozenAccount[_from]) revert();                        // Check if frozen            
-        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function mintToken(address target, uint256 mintedAmount) onlyOwner {
-        balanceOf[target] += mintedAmount;
-        Transfer(0, owner, mintedAmount);
-        Transfer(owner, target, mintedAmount);
-    }
-
-    function freezeAccount(address target, bool freeze) onlyOwner {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
-    }
-
-    function setBuyRate(uint newBuyRate) onlyOwner {
-        buyRate = newBuyRate;
-    }
-    
-    function setSelling(bool newStatus) onlyOwner {
-        isSelling = newStatus;
-    }
-
-    function buy() payable {
-        if(isSelling == false) revert();
-        uint amount = msg.value * buyRate;                  // calculates the amount
-        balanceOf[msg.sender] += amount;                   // adds the amount to buyer's balance
-        balanceOf[owner] -= amount;                         // subtracts amount from seller's balance
-        Transfer(owner, msg.sender, amount);                // execute an event reflecting the change
-    }
-    
-    function withdrawToOwner(uint256 amountWei) onlyOwner {
-        owner.transfer(amountWei);
-    }
 }
