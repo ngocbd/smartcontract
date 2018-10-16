5336f07ba9ea1a0ea9d355f57840bae2d40a78c0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:54:27 +0700

    add contract Bittle

--- BITTLE.sol
+++ BITTLE.sol
@@ -1,82 +1,398 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BITTLE at 0x1135feb23902949e44caecddf8ac42526c014aac
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bittle at 0x4e2504551edc1fefd574e0c946c1d9c8db250afd
 */
-pragma solidity ^ 0.4 .18;
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
+pragma solidity >=0.4.19;
+
+///from Zeppelin
+contract SafeMath {
+    function safeMul(uint a, uint b) internal returns (uint) {
+        uint c = a * b;
         assert(a == 0 || c / a == b);
         return c;
     }
 
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
+    function safeSub(uint a, uint b) internal returns (uint) {
         assert(b <= a);
         return a - b;
     }
 
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
         return c;
     }
+
+    function assert(bool assertion) internal {
+        if (!assertion) throw;
+    }
 }
-contract BITTLE {
-    using SafeMath
-    for uint256;
-    mapping(address => mapping(address => uint256)) allowed;
-    mapping(address => uint256) balances;
-    uint256 public totalSupply;
-    uint256 public decimals;
+
+contract Owned {
     address public owner;
-    bytes32 public symbol;
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed spender, uint256 value);
 
-    function BITTLE() {
-        totalSupply = 16000000000000000;
-        symbol = 'BTT';
-        owner = 0x2e2ab69143e18353d1b04658111f31fd88a8a239;
-        balances[owner] = totalSupply;
-        decimals = 8;
+    function Owned() {
+        owner = msg.sender;
     }
 
-    function balanceOf(address _owner) constant returns(uint256 balance) {
-        return balances[_owner];
+    modifier onlyOwner() {
+        if (msg.sender != owner) throw;
+        _;
     }
 
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
+    address newOwner;
+
+    function changeOwner(address _newOwner) onlyOwner {
+        newOwner = _newOwner;
     }
 
-    function transfer(address _to, uint256 _value) returns(bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
+    function acceptOwnership() {
+        if (msg.sender == newOwner) {
+            owner = newOwner;
+        }
+    }
+}
+
+contract Finalizable is Owned {
+    bool public finalized;
+
+    function finalize() onlyOwner {
+        finalized = true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
-        var _allowance = allowed[_from][msg.sender];
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
+    modifier notFinalized() {
+        if (finalized) throw;
+        _;
+    }
+}
+
+contract BittleSolutions  {
+    function transfer(address _to, uint _value) returns (bool);
+    function balanceOf(address owner) returns(uint);
+}
+
+contract TokenReceivable is Owned {
+    event logTokenTransfer(address token, address to, uint amount);
+
+    function claimTokens(address _token, address _to) onlyOwner returns (bool) {
+        BittleSolutions token = BittleSolutions (_token);
+        uint balance = token.balanceOf(this);
+        if (token.transfer(_to, balance)) {
+            logTokenTransfer(_token, _to, balance);
+            return true;
+        }
+        return false;
+    }
+}
+
+contract EventDefinitions {
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract Bittle is Finalizable, TokenReceivable, SafeMath, EventDefinitions {
+
+    string public name = "Bittle";
+    uint8 public decimals = 10;
+    string public symbol = "BTT";
+
+    Controller controller;
+    address owner;
+
+    modifier onlyController() {
+        assert(msg.sender == address(controller));
+        _;
+    }
+
+    function setController(address _c) onlyOwner notFinalized {
+        controller = Controller(_c);
+    }
+
+    function balanceOf(address a) constant returns (uint) {
+        return controller.balanceOf(a);
+    }
+
+    function totalSupply() constant returns (uint) {
+        return controller.totalSupply();
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint) {
+        return controller.allowance(_owner, _spender);
+    }
+
+    function transfer(address _to, uint _value)
+    onlyPayloadSize(2)
+    returns (bool success) {
+       success = controller.transfer(msg.sender, _to, _value);
+        if (success) {
+            Transfer(msg.sender, _to, _value);
+        }
+    }
+
+    function transferFrom(address _from, address _to, uint _value)
+    onlyPayloadSize(3)
+    returns (bool success) {
+       success = controller.transferFrom(msg.sender, _from, _to, _value);
+        if (success) {
+            Transfer(_from, _to, _value);
+        }
+    }
+
+    function approve(address _spender, uint _value)
+    onlyPayloadSize(2)
+    returns (bool success) {
+        //promote safe user behavior
+        if (controller.allowance(msg.sender, _spender) > 0) throw;
+
+        success = controller.approve(msg.sender, _spender, _value);
+        if (success) {
+            Approval(msg.sender, _spender, _value);
+        }
+    }
+
+    function increaseApproval (address _spender, uint _addedValue)
+    onlyPayloadSize(2)
+    returns (bool success) {
+        success = controller.increaseApproval(msg.sender, _spender, _addedValue);
+        if (success) {
+            uint newval = controller.allowance(msg.sender, _spender);
+            Approval(msg.sender, _spender, newval);
+        }
+    }
+
+    function decreaseApproval (address _spender, uint _subtractedValue)
+    onlyPayloadSize(2)
+    returns (bool success) {
+        success = controller.decreaseApproval(msg.sender, _spender, _subtractedValue);
+        if (success) {
+            uint newval = controller.allowance(msg.sender, _spender);
+            Approval(msg.sender, _spender, newval);
+        }
+    }
+
+    modifier onlyPayloadSize(uint numwords) {
+        assert(msg.data.length >= numwords * 32 + 4);
+        _;
+    }
+
+    function burn(uint _amount) {
+        controller.burn(msg.sender, _amount);
+        Transfer(msg.sender, 0x0, _amount);
+    }
+
+    function controllerTransfer(address _from, address _to, uint _value)
+    onlyController {
         Transfer(_from, _to, _value);
+    }
+
+    function controllerApprove(address _owner, address _spender, uint _value)
+    onlyController {
+        Approval(_owner, _spender, _value);
+    }
+
+    // multi-approve, multi-transfer
+
+    bool public multilocked;
+
+    modifier notMultilocked {
+        assert(!multilocked);
+        _;
+    }
+
+    //do we want lock permanent? I think so.
+    function lockMultis() onlyOwner {
+        multilocked = true;
+    }
+
+    /// multi functions just issue events, to fix initial event history
+
+    function multiTransfer(uint[] bits) onlyOwner notMultilocked {
+        if (bits.length % 3 != 0) throw;
+        for (uint i=0; i<bits.length; i += 3) {
+            address from = address(bits[i]);
+            address to = address(bits[i+1]);
+            uint amount = bits[i+2];
+            Transfer(from, to, amount);
+        }
+    }
+
+    function multiApprove(uint[] bits) onlyOwner notMultilocked {
+        if (bits.length % 3 != 0) throw;
+        for (uint i=0; i<bits.length; i += 3) {
+            address owner = address(bits[i]);
+            address spender = address(bits[i+1]);
+            uint amount = bits[i+2];
+            Approval(owner, spender, amount);
+        }
+    }
+
+    string public motd;
+    event Motd(string message);
+    function setMotd(string _m) onlyOwner {
+        motd = _m;
+        Motd(_m);
+    }
+}
+
+contract Controller is Owned, Finalizable {
+    Ledger public ledger;
+    address public token;
+
+    function setToken(address _token) onlyOwner {
+        token = _token;
+    }
+
+    function setLedger(address _ledger) onlyOwner {
+        ledger = Ledger(_ledger);
+    }
+
+    modifier onlyToken() {
+        if (msg.sender != token) throw;
+        _;
+    }
+
+    function totalSupply() constant returns (uint) {
+        return ledger.totalSupply();
+    }
+
+    function balanceOf(address _a) onlyToken constant returns (uint) {
+        return Ledger(ledger).balanceOf(_a);
+    }
+
+    function allowance(address _owner, address _spender)
+    onlyToken constant returns (uint) {
+        return ledger.allowance(_owner, _spender);
+    }
+
+    function transfer(address _from, address _to, uint _value)
+    onlyToken
+    returns (bool success) {
+        return ledger.transfer(_from, _to, _value);
+    }
+
+    function transferFrom(address _spender, address _from, address _to, uint _value)
+    onlyToken
+    returns (bool success) {
+        return ledger.transferFrom(_spender, _from, _to, _value);
+    }
+
+    function approve(address _owner, address _spender, uint _value)
+    onlyToken
+    returns (bool success) {
+        return ledger.approve(_owner, _spender, _value);
+    }
+
+    function increaseApproval (address _owner, address _spender, uint _addedValue)
+    onlyToken
+    returns (bool success) {
+        return ledger.increaseApproval(_owner, _spender, _addedValue);
+    }
+
+    function decreaseApproval (address _owner, address _spender, uint _subtractedValue)
+    onlyToken
+    returns (bool success) {
+        return ledger.decreaseApproval(_owner, _spender, _subtractedValue);
+    }
+
+
+    function burn(address _owner, uint _amount) onlyToken {
+        ledger.burn(_owner, _amount);
+    }
+}
+
+contract Ledger is Owned, SafeMath, Finalizable {
+    address public controller;
+    mapping(address => uint) public balanceOf;
+    mapping (address => mapping (address => uint)) public allowance;
+    uint public totalSupply;
+
+    function setController(address _controller) onlyOwner notFinalized {
+        controller = _controller;
+    }
+
+    modifier onlyController() {
+        if (msg.sender != controller) throw;
+        _;
+    }
+
+    function transfer(address _from, address _to, uint _value)
+    onlyController
+    returns (bool success) {
+        if (balanceOf[_from] < _value) return false;
+
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) returns(bool) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function transferFrom(address _spender, address _from, address _to, uint _value)
+    onlyController
+    returns (bool success) {
+        if (balanceOf[_from] < _value) return false;
+
+        var allowed = allowance[_from][_spender];
+        if (allowed < _value) return false;
+
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        allowance[_from][_spender] = safeSub(allowed, _value);
         return true;
     }
 
-    function() {
-        revert();
+    function approve(address _owner, address _spender, uint _value)
+    onlyController
+    returns (bool success) {
+        //require user to set to zero before resetting to nonzero
+        if ((_value != 0) && (allowance[_owner][_spender] != 0)) {
+            return false;
+        }
+
+        allowance[_owner][_spender] = _value;
+        return true;
+    }
+
+    function increaseApproval (address _owner, address _spender, uint _addedValue)
+    onlyController
+    returns (bool success) {
+        uint oldValue = allowance[_owner][_spender];
+        allowance[_owner][_spender] = safeAdd(oldValue, _addedValue);
+        return true;
+    }
+
+    function decreaseApproval (address _owner, address _spender, uint _subtractedValue)
+    onlyController
+    returns (bool success) {
+        uint oldValue = allowance[_owner][_spender];
+        if (_subtractedValue > oldValue) {
+            allowance[_owner][_spender] = 0;
+        } else {
+            allowance[_owner][_spender] = safeSub(oldValue, _subtractedValue);
+        }
+        return true;
+    }
+
+    event LogMint(address indexed owner, uint amount);
+    event LogMintingStopped();
+
+    function mint(address _a, uint _amount) onlyOwner mintingActive {
+        balanceOf[_a] += _amount;
+        totalSupply += _amount;
+        LogMint(_a, _amount);
+    }
+
+    bool public mintingStopped;
+
+    function stopMinting() onlyOwner {
+        mintingStopped = true;
+        LogMintingStopped();
+    }
+
+    modifier mintingActive() {
+        if (mintingStopped) throw;
+        _;
+    }
+
+    function burn(address _owner, uint _amount) onlyController {
+        balanceOf[_owner] = safeSub(balanceOf[_owner], _amount);
+        totalSupply = safeSub(totalSupply, _amount);
     }
 }
