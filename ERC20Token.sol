2a56123746b4064a78876d258fcdfe6b170b3426
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:42:28 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x5e93e6f1f19cd0977ceff3b5e6fa318f631c9c86
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xd37748a09dec934dacc27bcee6ab448d23c746c2
 */
 pragma solidity ^0.4.4;
 
@@ -106,9 +106,9 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
+    string public name = "LEX Coin";                   //fancy name: eg Simon Bucks
+    uint8 public decimals = 2;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol = "LEX";                 //An identifier: eg SBX
     string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
 //
@@ -119,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 55555555;            
-        totalSupply = 100000000000000000000;    
-        name = "voyeur coin";                            
-        decimals = 18;                            
-        symbol = "VYR";
+        balances[msg.sender] = 20000000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 20000000000000000000;                        // Update total supply (100000 for example)
+        name = "LEX Coin";                                   // Set the name for display purposes
+        decimals = 2;                            // Amount of decimals for display purposes
+        symbol = "LEX";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
