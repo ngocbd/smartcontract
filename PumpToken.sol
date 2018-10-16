4fc72c76cab1d72c8257375db859841a16d773df
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:32:22 +0700

    add contract PumpToken

--- PumpToken.sol
+++ PumpToken.sol
@@ -1,134 +1,138 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PumpToken at 0xdbe46c0e6526e9068da1626acfddff2386635045
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PumpToken at 0xaab0a030f3504dba3634b721fc03f42bbcc02c42
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.4;
 
 contract Token {
-    function balanceOf(address _owner) constant returns (uint256);
-    function transfer(address _to, uint256 _value) returns (bool);
+
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
 }
 
-contract PumpToken {
-    address owner = msg.sender;
 
-    bool public purchasingAllowed = true;
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
+contract StandardToken is Token {
 
-    uint256 public totalContribution = 0;
-
-    uint256 public totalSupply = 0;
-
-    function name() pure returns (string) { return "Pump Token"; }
-    function symbol() pure returns (string) { return "PUMP"; }
-    function decimals() pure returns (uint8) { return 18; }
-    
-    function balanceOf(address _owner) constant returns (uint256) { return balances[_owner]; }
-    
     function transfer(address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (2 * 32) + 4) { return false; }
-
-        if (_value == 0) { return false; }
-
-        uint256 fromBalance = balances[msg.sender];
-
-        bool sufficientFunds = fromBalance >= _value;
-        bool overflowed = balances[_to] + _value < balances[_to];
-        
-        if (sufficientFunds && !overflowed) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            
             Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
     }
-    
+
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (3 * 32) + 4) { return false; }
-
-        if (_value == 0) { return false; }
-        
-        uint256 fromBalance = balances[_from];
-        uint256 allowance = allowed[_from][msg.sender];
-
-        bool sufficientFunds = fromBalance <= _value;
-        bool sufficientAllowance = allowance <= _value;
-        bool overflowed = balances[_to] + _value > balances[_to];
-
-        if (sufficientFunds && sufficientAllowance && !overflowed) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
-            
             allowed[_from][msg.sender] -= _value;
-            
             Transfer(_from, _to, _value);
             return true;
         } else { return false; }
     }
-    
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
     function approve(address _spender, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 spend/approval race condition
-        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
-        
         allowed[msg.sender][_spender] = _value;
-        
         Approval(msg.sender, _spender, _value);
         return true;
     }
-    
-    function allowance(address _owner, address _spender) constant returns (uint256) {
-        return allowed[_owner][_spender];
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
 
-    function enablePurchasing() {
-        if (msg.sender != owner) { return; }
 
-        purchasingAllowed = true;
+//name this contract whatever you'd like
+contract PumpToken is StandardToken {
+
+    function () {
+        //if ether is sent to this address, send it back.
+        throw;
     }
 
-    function disablePurchasing() {
-        if (msg.sender != owner) { return; }
+    /* Public variables of the token */
 
-        purchasingAllowed = false;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+
+    function PumpToken(
+        ) {
+        balances[msg.sender] = 1000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 1000000000000000;                        // Update total supply (100000 for example)
+        name = "PumpToken";                                   // Set the name for display purposes
+        decimals = 6;                            // Amount of decimals for display purposes
+        symbol = "PUTK";                               // Set the symbol for display purposes
     }
 
-    function withdrawTokens(address _tokenContract) returns (bool) {
-        if (msg.sender != owner) { return; }
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
 
-        Token token = Token(_tokenContract);
-
-        uint256 amount = token.balanceOf(address(this));
-        return token.transfer(owner, amount);
-    }
-
-    function getStats() constant returns (uint256, uint256, bool) {
-        return (totalContribution, totalSupply, purchasingAllowed);
-    }
-
-    function() payable {
-        if (!purchasingAllowed) { return; }
-        
-        if (msg.value == 0) { return; }
-
-        owner.transfer(msg.value);
-        totalContribution += msg.value;
-
-        uint256 tokensIssued = (msg.value * 100);
-
-        if (msg.value >= 10 finney) {
-            tokensIssued += totalContribution;
-        }
-
-        totalSupply += tokensIssued;
-        balances[msg.sender] += tokensIssued;
-        
-        Transfer(address(this), msg.sender, tokensIssued);
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        return true;
     }
 }
