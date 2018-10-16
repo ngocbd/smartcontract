b6aa8951deee8c9d7fb6b4a25aa0b128f8c58924
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:15 +0700

    add contract ProofOfExistence

--- ProofOfExistence.sol
+++ ProofOfExistence.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0x4e7c5ae901c0d34a45ba83ba2ba2c2b9e5247625
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfExistence at 0x1d6671bad68e2ac15f72502f8a6c3bab42ccafd6
 */
 contract ProofOfExistence {
 
@@ -10,7 +10,7 @@ contract ProofOfExistence {
     bytes memory b_hash = bytes(sha256);
     
     if ( b_hash.length == 64 ){
-      if ( proofs[sha256] == 0 ){
+      if ( proofs[sha256] != 0 ){
         proofs[sha256] = block.timestamp;
       }
     }
