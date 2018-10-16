4990062eb6d9ea7c85dba1ec517dce4e470eb5c3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:57:05 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,287 +1,102 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xc901d93cc5c45b12802087b5610897fb6469ead7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xd25cd68fd05176d6188c55eed8e2f079158b6237
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-    
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+interface token {
+    function transfer(address receiver, uint amount);
+}
+
+contract Crowdsale {
+    address public beneficiary;
+    uint public fundingGoal;
+    uint public amountRaised;
+    uint public deadline;
+    uint public price;
+    token public tokenReward;
+    mapping(address => uint256) public balanceOf;
+    bool fundingGoalReached = false;
+    bool crowdsaleClosed = false;
+
+    event GoalReached(address recipient, uint totalAmountRaised);
+    event FundTransfer(address backer, uint amount, bool isContribution);
+
+    /**
+     * Constrctor function
+     *
+     * Setup the owner
+     */
+    function Crowdsale(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint durationInMinutes,
+        uint etherCostOfEachToken,
+        address addressOfTokenUsedAsReward
+    ) {
+        beneficiary = ifSuccessfulSendTo;
+        fundingGoal = fundingGoalInEthers * 1 ether;
+        deadline = now + durationInMinutes * 1 minutes;
+        price = etherCostOfEachToken * 1 ether;
+        tokenReward = token(addressOfTokenUsedAsReward);
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-contract DragonToken{
-  function transferFrom(address _from, address _to, uint256 _value) returns(bool success);
-}
-
-/**
- * @title Crowdsale
- * @dev Crowdsale is a base contract for managing a token crowdsale.
- * Crowdsales have a start and end timestamps, where investors can make
- * token purchases and the crowdsale will assign them tokens based
- * on a token per ETH rate. Funds collected are forwarded to a wallet
- * as they arrive. The contract requires a MintableToken that will be
- * minted as contributions arrive, note that the crowdsale contract
- * must be owner of the token in order to be able to mint it.
- */
-contract Crowdsale is Ownable{
-  using SafeMath for uint256;
-
-  // The token being sold
-  DragonToken public token;
-  
-  // The address of token reserves
-  address public tokenReserve;
-
-  // start and end timestamps where investments are allowed (both inclusive)
-  uint256 public startTime;
-  uint256 public endTime;
-
-  // address where funds are collected
-  address public wallet;
-
-  // token rate in wei
-  uint256 public rate;
-
-  // amount of raised money in wei
-  uint256 public weiRaised;
-  
-  uint256 public tokensSold;
-
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   * @param releaseTime tokens unlock time
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount, uint256 releaseTime);
-  
-  /**
-   * event upon endTime updated
-   */
-  event EndTimeUpdated();
-  
-  /**
-   * Dragon token price updated
-   */
-  event DragonPriceUpdated();
-  
-  /**
-   * event for token releasing
-   * @param holder who is releasing his tokens
-   */
-  event TokenReleased(address indexed holder, uint256 amount);
-
-
-  function Crowdsale() public {
-  
-    owner =  0xF615Ac471E066b5ae4BD211CC5044c7a31E89C4e; // overriding owner
-    startTime = now;
-    endTime = 1521187200;
-    rate = 5500000000000000; // price in wei
-    wallet =  0xF615Ac471E066b5ae4BD211CC5044c7a31E89C4e;
-    token = DragonToken(0x814F67fA286f7572B041D041b1D99b432c9155Ee);
-    tokenReserve =  0xF615Ac471E066b5ae4BD211CC5044c7a31E89C4e;
-  }
-
-  // fallback function can be used to buy tokens
-  function () external payable {
-    buyTokens(msg.sender);
-  }
-
-  // low level token purchase function
-  function buyTokens(address beneficiary) public payable {
-    require(beneficiary != address(0));
-    require(validPurchase());
-
-    uint256 weiAmount = msg.value;
-
-    // calculate token amount to be created
-    uint256 tokens = getTokenAmount(weiAmount);
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
-    tokensSold = tokensSold.add(tokens);
-
-    uint256 lockedFor = assignTokens(beneficiary, tokens);
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens, lockedFor);
-
-    forwardFunds();
-  }
-
-  // @return true if crowdsale event has ended
-  function hasEnded() public view returns (bool) {
-    return now > endTime;
-  }
-
-  function getTokenAmount(uint256 weiAmount) internal view returns(uint256) {
-    return (weiAmount.mul(100000000)).div(rate); // multiply with decimals
-  }
-
-  // send ether to the fund collection wallet
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
-  }
-
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal view returns (bool) {
-    bool withinPeriod = now >= startTime && now <= endTime;
-    bool nonZeroPurchase = msg.value != 0;
-    return withinPeriod && nonZeroPurchase;
-  }
-
-  function updateEndTime(uint256 newTime) onlyOwner external {
-    require(newTime > startTime);
-    endTime = newTime;
-    EndTimeUpdated();
-  }
-  
-  function updateDragonPrice(uint256 weiAmount) onlyOwner external {
-    require(weiAmount > 0);
-    rate = weiAmount;
-    DragonPriceUpdated();
-  }
-  
-  mapping(address => uint256) balances;
-  
-  struct account{
-      uint256[] releaseTime;
-      mapping(uint256 => uint256) balance;
-  }
-  mapping(address => account) ledger;
- 
- 
-  function assignTokens(address beneficiary, uint256 amount) private returns(uint256 lockedFor){
-      //lockedFor = now + 45 days;
-      
-      lockedFor = now + 6 minutes;
-      
-      balances[beneficiary] = balances[beneficiary].add(amount);
-      
-      ledger[beneficiary].releaseTime.push(lockedFor);
-      ledger[beneficiary].balance[lockedFor] = amount;
-  }
-  
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-  
-
-  function unlockedBalance(address _owner) public view returns (uint256 amount) {
-    for(uint256 i = 0 ; i < ledger[_owner].releaseTime.length; i++){
-        uint256 time = ledger[_owner].releaseTime[i];
-        if(now >= time) amount +=  ledger[_owner].balance[time];
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+    function () payable {
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+        balanceOf[msg.sender] += amount;
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
+        FundTransfer(msg.sender, amount, true);
     }
-  }
 
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function releaseDragonTokens() public {
-    require(balances[msg.sender] > 0);
-    
-    uint256 amount = 0;
-    for(uint8 i = 0 ; i < ledger[msg.sender].releaseTime.length; i++){
-        uint256 time = ledger[msg.sender].releaseTime[i];
-        if(now >= time && ledger[msg.sender].balance[time] > 0){
-            amount = ledger[msg.sender].balance[time];
-            ledger[msg.sender].balance[time] = 0;
-            continue;
+    modifier afterDeadline() { if (now >= deadline) _; }
+
+    /**
+     * Check if goal was reached
+     *
+     * Checks if the goal or time limit has been reached and ends the campaign
+     */
+    function checkGoalReached() afterDeadline {
+        if (amountRaised >= fundingGoal){
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+        }
+        crowdsaleClosed = true;
+    }
+
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal() afterDeadline {
+        if (!fundingGoalReached) {
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                if (msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+
+        if (fundingGoalReached && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised)) {
+                FundTransfer(beneficiary, amountRaised, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock funders balance
+                fundingGoalReached = false;
+            }
         }
     }
-      
-    if(amount <= 0 || balances[msg.sender] < amount){
-        revert();
-    }
-    
-    balances[msg.sender] = balances[msg.sender].sub(amount);
-    
-    if(!token.transferFrom(tokenReserve,msg.sender,amount)){
-        revert();
-    }
-
-    TokenReleased(msg.sender,amount);
-  }
-  
 }
