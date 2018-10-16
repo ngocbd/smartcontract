c79b6c056476b117b4546bf275a062f449e8b0f2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:17:59 +0700

    add contract Storage

--- Storage.sol
+++ Storage.sol
@@ -1,58 +1,44 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0xfd49214d24b241097f2193d3166ebcc60096c1a6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0x00539863217abd04b374a422c855e4c71ed1019a
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
-contract Storage{
-    address public founder;
-    bool public changeable;
-    mapping( address => bool) public adminStatus;
-    mapping( address => uint256) public slot;
-    
-    event Update(address whichAdmin, address whichUser, uint256 data);
-    event Set(address whichAdmin, address whichUser, uint256 data);
-    event Admin(address addr, bool yesno);
+contract Storage {
+  address public owner;
+  uint256 public storedAmount;
 
-    modifier onlyFounder() {
-        require(msg.sender==founder);
-        _;
-    }
-    
-    modifier onlyAdmin() {
-        assert (adminStatus[msg.sender]==true);
-        _;
-    }
-    
-    function Storage() public {
-        founder=msg.sender;
-        adminStatus[founder]=true;
-        changeable=true;
-    }
-    
-    function update(address userAddress,uint256 data) public onlyAdmin(){
-        assert(changeable==true);
-        assert(slot[userAddress]+data>slot[userAddress]);
-        slot[userAddress]+=data;
-        Update(msg.sender,userAddress,data);
-    }
-    
-    function set(address userAddress, uint256 data) public onlyAdmin() {
-        require(changeable==true || msg.sender==founder);
-        slot[userAddress]=data;
-        Set(msg.sender,userAddress,data);
-    }
-    
-    function admin(address addr) public onlyFounder(){
-        adminStatus[addr] = !adminStatus[addr];
-        Admin(addr, adminStatus[addr]);
-    }
-    
-    function halt() public onlyFounder(){
-        changeable=!changeable;
-    }
-    
-    function() public{
-        revert();
-    }
-    
+  function Storage() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner{
+    require(msg.sender == owner);
+    _;
+  }
+
+  function()
+  public
+  payable {
+    storeEth();
+  }
+
+  function storeEth()
+  public
+  payable {
+    storedAmount += msg.value;
+  }
+
+  function getEth()
+  public
+  onlyOwner{
+    storedAmount = 0;
+    owner.transfer(this.balance);
+  }
+
+  function sendEthTo(address to)
+  public
+  onlyOwner{
+    storedAmount = 0;
+    to.transfer(this.balance);
+  }
 }
