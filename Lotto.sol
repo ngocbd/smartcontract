5916700dba06b3de50c9599ac4ea47de8b67d780
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:04:14 +0700

    add contract Lotto

--- Lotto.sol
+++ Lotto.sol
@@ -1,149 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Lotto at 0xdcf14a9cba1d5ee6d4671c20c559373f331910ea
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Lotto at 0x80616F35Df2ef0CB42280a629761e0350FaFd679
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.8;
 
-contract Lotto {
+/**
+ * Very basic owned/mortal boilerplate.  Used for basically everything, for
+ * security/access control purposes.
+ */
+contract Owned {
+  address owner;
 
-    //	Play lottery.
-    //	Rules:
-    //	-Address plays between minPlay & mayPlay in order to increase stack amount.
-    //	-Address can play many times.
-    //	-A fee is directly withrawn from incoming amounts (see feesPerMillion).
-    //	-As soon as the jackpot goes beyond the target, an address is chosen randomly proportionally to its stack amount.
-    //	-The winner address get paid with the jackpot.
-    //	-gameNumber is increased and minPlay,maxPlay,target get the values from nextMinPlay,nextMaxPlay,nextTarget.
-    //	-nextMinPlay,nextMaxPlay,nextTarget can only be changed by the owner for the next game.
-
-
-    address public owner;
-    uint256 public gameNumber;
-    uint256 public feesPerMillion;
-    uint256 public nextFeesPerMillion;
-    uint256 public jackpot;
-    uint256 public target;
-    uint256 public nextTarget;
-    uint256 public minPlay;
-    uint256 public nextMinPlay;
-    uint256 public maxPlay;
-    uint256 public nextMaxPlay;
-    mapping(address => uint256) public playersAmounts;
-    address[] public playersList;
-    uint256 private vMillion;
-
-
-    event newGame(uint256 ts, uint256 game);
-    event newPlay(uint256 ts, uint256 game, address player, uint256 grossAmount, uint256 amount, uint256 stack, uint256 jackpot, uint256 target);
-    event newWinner(uint256 ts, uint256 game, address player, uint256 amount);
-    event forceGame(uint256 ts,  uint256 game);
-    event payment(uint256 ts, address addr, uint256 amount, bool status);
-
-
-
-    function Lotto(){
-        owner=msg.sender;
-        gameNumber=1;
-        vMillion=1000000;
-        feesPerMillion=5000;
-        nextFeesPerMillion=feesPerMillion;
-        minPlay=1 ether;
-        nextMinPlay=minPlay;
-        maxPlay=3 ether;
-        nextMaxPlay=maxPlay;
-        target=10 ether;
-        nextTarget=target;
-        jackpot=0;
-        newGame(block.timestamp, gameNumber);
+  modifier onlyOwner {
+    if (msg.sender != owner) {
+      throw;
     }
+    _;
+  }
 
+  /**
+   * Basic constructor.  The sender is the owner.
+   */
+  function Owned() {
+    owner = msg.sender;
+  }
 
+  /**
+   * Transfers ownership of the contract to a new owner.
+   * @param newOwner  Who gets to inherit this thing.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    owner = newOwner;
+  }
 
-    function() payable public{}
+  /**
+   * Shuts down the contract and removes it from the blockchain state.
+   * Only available to the owner.
+   */
+  function shutdown() onlyOwner {
+    selfdestruct(owner);
+  }
 
-
-
-    function play() payable public {
-        if(msg.sender==owner) throw;
-        if(msg.value<minPlay || msg.value>maxPlay) throw;
-
-        if(playersAmounts[msg.sender]==0){
-            playersList.push(msg.sender);
-        }
-        uint256 amount;
-        amount=(vMillion-feesPerMillion)*msg.value/vMillion;
-        playersAmounts[msg.sender]=playersAmounts[msg.sender]+amount;
-        jackpot=jackpot+amount;
-        newPlay(block.timestamp, gameNumber, msg.sender, msg.value, amount, playersAmounts[msg.sender], jackpot, target);
-        if(jackpot>=target){
-            _pickWinner();
-        }
+  /**
+   * Withdraw all the funds from this contract.
+   * Only available to the owner.
+   */
+  function withdraw() onlyOwner {
+    if (!owner.send(this.balance)) {
+      throw;
     }
+  }
+}
 
+/**
+ * The base interface is what the parent contract expects to be able to use.
+ * If rules change in the future, and new logic is introduced, it only has to
+ * implement these methods, wtih the role of the curator being used
+ * to execute the additional functionality (if any).
+ */
+contract LotteryGameLogicInterface {
+  address public currentRound;
+  function finalizeRound() returns(address);
+  function isUpgradeAllowed() constant returns(bool);
+  function transferOwnership(address newOwner);
+}
 
+/**
+ * This contract is pretty generic, as it really only serves to maintain a constant
+ * address on the blockchain (through upgrades to the game logic), and to maintain
+ * a history of previous rounds.  Note that the rounds will have had ownership
+ * transferred to the curator (most likely), so there's mostly just here for
+ * accounting purposes.
+ *
+ * A side effect of this is that finalizing a round has to happen from here.
+ */
+contract Lotto is Owned {
 
-    function _pickWinner() private{
-        uint256 random;
-        random=(uint256(sha256(block.blockhash(block.number-1), block.coinbase, block.difficulty, block.gaslimit, block.number, block.timestamp, msg.data, msg.gas, msg.sender, msg.sig, msg.value))%jackpot)+1;
-        uint256 playerNumber;
-        bool keepGoing;
-        playerNumber=0;
-        keepGoing=true;
-        while(keepGoing && playerNumber<playersList.length){
-            if(random<=playersAmounts[playersList[playerNumber]]){
-                keepGoing=false;
-            }else{
-                random=random-playersAmounts[playersList[playerNumber]];
-                playerNumber=playerNumber+1;
-            }
-        }
-        if(!keepGoing){
-            newWinner(block.timestamp, gameNumber, playersList[playerNumber], jackpot);
-            if(playersList[playerNumber].send(jackpot)){
-                payment(block.timestamp, playersList[playerNumber], jackpot, true);
-            }else{
-                payment(block.timestamp, playersList[playerNumber], jackpot, false);
-            }
-        }
-        target=nextTarget;
-        minPlay=nextMinPlay;
-        maxPlay=nextMaxPlay;
-        feesPerMillion=nextFeesPerMillion;
-        jackpot=0;
-        gameNumber=gameNumber+1;
-        for(uint256 i; i<playersList.length; i++){
-            playersAmounts[playersList[i]]=0;
-        }
-        delete playersList;
+  address[] public previousRounds;
+
+  LotteryGameLogicInterface public gameLogic;
+
+  modifier onlyWhenUpgradeable {
+    if (!gameLogic.isUpgradeAllowed()) {
+      throw;
     }
+    _;
+  }
 
-
-
-    function _admin(uint256 cmd, uint256 value) public{
-       if(msg.sender!=owner) throw;
-       if(cmd==1){
-           nextTarget=value;
-       }else if(cmd==2){
-           nextMinPlay=value;
-       }else if(cmd==3){
-           nextMaxPlay=value;
-       }else if(cmd==4){
-           nextFeesPerMillion=value;
-       }else if(cmd==5){
-           forceGame(block.timestamp, gameNumber);
-           _pickWinner();
-       }
+  modifier onlyGameLogic {
+    if (msg.sender != address(gameLogic)) {
+      throw;
     }
+    _;
+  }
 
+  /**
+   * Creates a new lottery contract.
+   * @param initialGameLogic   The starting game logic.
+   */
+  function Lotto(address initialGameLogic) {
+    gameLogic = LotteryGameLogicInterface(initialGameLogic);
+  }
 
+  /**
+   * Upgrade the game logic.  Only possible to do when the game logic
+   * has deemed it clear to do so.  Hands the old one over to the owner
+   * for cleanup.  Expects the new logic to already be configured.
+   * @param newLogic   New, already-configured game logic.
+   */
+  function setNewGameLogic(address newLogic) onlyOwner onlyWhenUpgradeable {
+    gameLogic.transferOwnership(owner);
+    gameLogic = LotteryGameLogicInterface(newLogic);
+  }
 
-    function _withdraw(address destination, uint256 value) public{
-       if(msg.sender!=owner) throw;
-       if(value>=(this.balance-jackpot)) throw;
-       if(destination.send(value)){
-           payment(block.timestamp, destination, value, true);
-       }else{
-           payment(block.timestamp, destination, value, false);
-       }
-    }
+  /**
+   * Returns the current round.
+   * @return address The current round (when applicable)
+   */
+  function currentRound() constant returns(address) {
+    return gameLogic.currentRound();
+  }
 
+  /**
+   * Used to finalize (e.g. pay winners) the current round, then log
+   * it in the history.
+   */
+  function finalizeRound() onlyOwner {
+    address roundAddress = gameLogic.finalizeRound();
+    previousRounds.push(roundAddress);
+  }
+
+  /**
+   * Tells how many previous rounds exist.
+   */
+  function previousRoundsCount() constant returns(uint) {
+    return previousRounds.length;
+  }
+
+  // You must think I'm a joke
+  // I ain't gonna be part of your system
+  // Man! Pump that garbage in another man's veins
+  function () {
+    throw;
+  }
 }
