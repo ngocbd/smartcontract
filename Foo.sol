f366663ac6ffd19919c4e10cfef640b450d2077c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:18:30 +0700

    add contract Foo

---
+++ Foo.sol
@@ -0,0 +1,13 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Foo at 0x1622aafed9a6f0b59ef50a778bcb0cdbf990bf81
+*/
+pragma solidity ^0.4.19;
+
+contract Foo
+{
+    string public phrase;
+    
+    function Foo(string _phrase) public {
+        phrase = _phrase;
+    }
+}
