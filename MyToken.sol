9e9321bb3f31c46bf7f8909ad1c36cd34502391c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:25:00 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,72 +1,119 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x760757e54A19412120e6d76F637bA27A28B244C8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x5ac8359a7d38afedf7ed0d4abb283cd2654d126a
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
-contract MyOwned {
+contract Token {
+    //????_owner??token??? 
+    function balanceOf(address _owner) constant returns (uint256 balance);
 
-    address public owner;
-    function MyOwned() public { owner = msg.sender; }
-    modifier onlyOwner { require(msg.sender == owner ); _; }
-    function transferOwnership (address newOwner) onlyOwner public { owner = newOwner; }
+    //??????????_to??????_value?token
+    function transfer(address _to, uint256 _value) returns (bool success);
+
+    //???_from????_to????_value?token??approve??????
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+    //??????????_spender????????????_value?token
+    function approve(address _spender, uint256 _value) returns (bool success);
+
+    //????_spender?????_owner???token???
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+    //????????????? 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    //???approve(address _spender, uint256 _value)????????????
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+    //token???????public??????getter????????totalSupply().
+    uint256 public totalSupply;
 }
 
-contract MyToken is MyOwned {   
-
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    uint256 public firstPublish;
-    
-    mapping (address => uint256) public balanceOf;
-    mapping (address => bool) public frozenAccount;
-    event Burn (address indexed from,uint256 value);
-    event FrozenFunds (address target,bool frozen);
-    event Transfer (address indexed from,address indexed to,uint256 value);
-    
-    function MyToken(
-
-        string _Name,
-        string _Symbol,
-        uint8 _decimals,
-        uint256 _totalSupply,
-        uint256 _firstPublish) public {
-
-        name = _Name;
-        symbol = _Symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
-        firstPublish = _firstPublish;
-        balanceOf[msg.sender] = _firstPublish;
-    }
-
-    function transfer (address _to, uint256 _value) public {
-
-        require(!frozenAccount[msg.sender]);
-        require (balanceOf[msg.sender] >= _value);
-        require (balanceOf[_to] + _value >= balanceOf[_to]);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        Transfer(msg.sender, _to, _value);
+contract StandardToken is Token {
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
     }
     
-    function freezeAccount (address target,bool freeze) public onlyOwner {
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //??totalSupply ??????? (2^256 - 1).
+        //??????????????token??????????????????
+        require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
+        balances[msg.sender] -= _value;//???????????token??_value
+        balances[_to] += _value;//???????token??_value
+        Transfer(msg.sender, _to, _value);//????????
+        return true;
+    }
 
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(balances[_from] >= _value && allowed[_from][msg.sender] >= 
+            _value && balances[_to] + _value > balances[_to]);
+        balances[_to] += _value;//??????token??_value
+        balances[_from] -= _value; //????_from??token??_value
+        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
+        Transfer(_from, _to, _value);//????????
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success)   
+    {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
     }
     
-    function burnFrom (address _from,uint256 _value) public onlyOwner {
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        require(_addedValue > 0);
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender] + _addedValue;
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+    
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        require(_subtractedValue > 0);
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue >= oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue - _subtractedValue;
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
 
-        require(balanceOf[_from] >= _value); 
-        balanceOf[_from] -= _value; 
-        Burn(_from, _value);
-    }    
-    function mintTo (address target, uint256 mintedAmount) public onlyOwner {
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];//??_spender?_owner????token?
+    }
+    
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+}
 
-        balanceOf[target] += mintedAmount;
-        Transfer(0, this, mintedAmount);
-        Transfer(this, target, mintedAmount);
+contract MyToken is StandardToken {
+    /* Public variables of the token */
+    string public name;             // token name
+    uint8  public decimals;
+    string public symbol;           // token symbol
+    
+    function(){
+        revert();
+    }
+
+    function MyToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
+        name = _tokenName;
+        decimals = _decimalUnits;
+        symbol = _tokenSymbol;
+        totalSupply = _initialAmount * (10 ** uint256(_decimalUnits));
+        balances[msg.sender] = totalSupply;
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        //??????????1STORE??????????????????? approveAndCall()???
+        //???????????1STORE????????????????????? receiveApproval()???
+        //???????????????????????????????????????? transferFrom()?? ??????)
+        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        return true;
     }
 }
