de2bdb8cbc331ddca465c7dcf7a2aabfcf0368ef
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:03:34 +0700

    add contract SynchroCoin

--- SynchroCoin.sol
+++ SynchroCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SynchroCoin at 0x77eC4fC3013eEA1598E449b09afaA3E1e0d85897
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SynchroCoin at 0xf149230261f14f75c78206b7358e0cc391d85c98
 */
 pragma solidity ^0.4.11;
 
@@ -40,8 +40,8 @@ library SafeMath {
  */
 contract ERC20Basic {
   uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
@@ -60,7 +60,7 @@ contract BasicToken is ERC20Basic {
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint256 _value) returns (bool) {
+  function transfer(address _to, uint256 _value) public returns (bool) {
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
@@ -72,22 +72,20 @@ contract BasicToken is ERC20Basic {
   * @param _owner The address to query the the balance of. 
   * @return An uint256 representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) constant returns (uint256 balance) {
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
     return balances[_owner];
   }
 
 }
-
-
 
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
@@ -110,7 +108,7 @@ contract StandardToken is ERC20, BasicToken {
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amout of tokens to be transfered
    */
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
     var _allowance = allowed[_from][msg.sender];
 
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
@@ -128,7 +126,7 @@ contract StandardToken is ERC20, BasicToken {
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
-  function approve(address _spender, uint256 _value) returns (bool) {
+  function approve(address _spender, uint256 _value) public returns (bool) {
 
     // To change the approve amount you first have to reduce the addresses`
     //  allowance to zero by calling `approve(_spender, 0)` if it is not
@@ -147,25 +145,10 @@ contract StandardToken is ERC20, BasicToken {
    * @param _spender address The address which will spend the funds.
    * @return A uint256 specifing the amount of tokens still avaible for the spender.
    */
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
     return allowed[_owner][_spender];
   }
 
-}
-
-
-contract AbstractStarbaseCrowdsale {
-    function workshop() constant returns (address) {}
-    function startDate() constant returns (uint256) {}
-    function endedAt() constant returns (uint256) {}
-    function isEnded() constant returns (bool);
-    function totalRaisedAmountInCny() constant returns (uint256);
-    function numOfPurchasedTokensOnCsBy(address purchaser) constant returns (uint256);
-    function numOfPurchasedTokensOnEpBy(address purchaser) constant returns (uint256);
-}
-
-contract AbstractStarbaseMarketingCampaign {
-    function workshop() constant returns (address) {}
 }
 
 /**
@@ -181,7 +164,7 @@ contract Ownable {
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() {
+  function Ownable() public {
     owner = msg.sender;
   }
 
@@ -199,7 +182,7 @@ contract Ownable {
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner {
+  function transferOwnership(address newOwner) public onlyOwner {
     if (newOwner != address(0)) {
       owner = newOwner;
     }
@@ -207,71 +190,149 @@ contract Ownable {
 
 }
 
+contract SYNVault {
+    // flag to determine if address is for a real contract or not
+    bool public isSYNVault = false;
+
+    SynchroCoin synchroCoin;
+    address businessAddress;
+    uint256 unlockedAtBlockNumber;
+    // Locked for 345days (1year after completion of the crowdsale)
+    // 2129143 blocks = 24 hours * 60 minutes * 60 seconds * 345days / 14seconds per block
+    uint256 public constant numBlocksLocked = 2129143;
+
+    /// @notice Constructor function sets the Multisig address and
+    /// total number of locked tokens to transfer
+    function SYNVault(address _businessAddress) public {
+        require(_businessAddress != 0x0);
+        synchroCoin = SynchroCoin(msg.sender);
+        businessAddress = _businessAddress;
+        isSYNVault = true;
+        unlockedAtBlockNumber = SafeMath.add(block.number, numBlocksLocked); // 345 days of blocks later
+    }
+
+    /// @notice Transfer locked tokens to Synchrolife's wallet
+    function unlock() external {
+        // Wait your turn!
+        require(block.number > unlockedAtBlockNumber);
+        // Will fail if allocation (and therefore toTransfer) is 0.
+        if (!synchroCoin.transfer(businessAddress, synchroCoin.balanceOf(this))) revert();
+    }
+
+    // disallow payment this is for SYN not ether
+    function () public { revert(); }
+}
 
 contract SynchroCoin is Ownable, StandardToken {
 
     string public constant symbol = "SYC";
-
     string public constant name = "SynchroCoin";
-
-    uint8 public constant decimals = 12;
+    uint8 public constant decimals = 18;
+    uint256 public constant initialSupply = 100000000e18;    //100000000000000000000000000
     
+    uint256 public constant startDate = 1506092400;
+    uint256 public constant endDate = 1508511599;
+    uint256 public constant firstPresaleStart = 1502884800;
+    uint256 public constant firstPresaleEnd = 1503835140;
+    uint256 public constant secondPresaleStart = 1504526400;
+    uint256 public constant secondPresaleEnd = 1504785540;
 
-    uint256 public STARTDATE;
+    //55% for CrowdSale distribution
+    uint256 public constant crowdSalePercentage = 5500;
+    //20% for Synchrolife pool for rewards
+    uint256 public constant rewardPoolPercentage = 2000;
+    //9.5% for Synchrolife business + 5% for early investors
+    uint256 public constant businessPercentage = 1450;
+    //9.5% for Synchrolife team, advisors and partners
+    uint256 public constant vaultPercentage = 950;
+    //1% for Bounty
+    uint256 public constant bountyPercentage = 100;
+    
+    //Denominator for percentage calculation.
+    uint256 public constant hundredPercent = 10000; 
+    
+    //First Presale: 268000000000000000000
+    //Second Presale: 70000000000000000000 
+    //Crowdsale:     417427897026000000400
+    uint256 public constant totalFundedEther = 755427897026000000400;
+    
+    //First Presale: 375200000000000000000
+    //Second Presale: 91000000000000000000
+    //Crowdsale:     438371225465900000400
+    uint256 public constant totalConsideredFundedEther = 904571225465900000400;
+    
+    SYNVault public vault;
+    address public businessAddress;
+    address public rewardPoolAddress;
+    
+    uint256 public crowdSaleTokens;
+    uint256 public bountyTokens;
+    uint256 public rewardPoolTokens;
 
-    uint256 public ENDDATE;
-
-    // 55% to distribute during CrowdSale
-    uint256 public crowdSale;
-
-    // 20% to pool to reward
-    // 25% to other business operations
-    address public multisig;
-
-    function SynchroCoin(
-    uint256 _initialSupply,
-    uint256 _start,
-    uint256 _end,
-    address _multisig) {
-        totalSupply = _initialSupply;
-        STARTDATE = _start;
-        ENDDATE = _end;
-        multisig = _multisig;
-        crowdSale = _initialSupply * 55 / 100;
-        balances[multisig] = _initialSupply;
+    function SynchroCoin(address _businessAddress, address _rewardPoolAddress) public {
+        totalSupply = initialSupply;
+        businessAddress = _businessAddress;
+        rewardPoolAddress = _rewardPoolAddress;
+        
+        vault = new SYNVault(businessAddress);
+        require(vault.isSYNVault());
+        
+        uint256 remainingSupply = initialSupply;
+        
+        // 55% of total to be distributed to presale and crowdsale participents
+        crowdSaleTokens = SafeMath.div(SafeMath.mul(totalSupply, crowdSalePercentage), hundredPercent);
+        remainingSupply = SafeMath.sub(remainingSupply, crowdSaleTokens);
+        
+        // 20% of total to be allocated for rewards
+        rewardPoolTokens = SafeMath.div(SafeMath.mul(totalSupply, rewardPoolPercentage), hundredPercent);
+        balances[rewardPoolAddress] = SafeMath.add(balances[rewardPoolAddress], rewardPoolTokens);
+        Transfer(0, rewardPoolAddress, rewardPoolTokens);
+        remainingSupply = SafeMath.sub(remainingSupply, rewardPoolTokens);
+        
+        // 9.5% of total goes to vault, timelocked for 1 year
+        uint256 vaultTokens = SafeMath.div(SafeMath.mul(totalSupply, vaultPercentage), hundredPercent);
+        balances[vault] = SafeMath.add(balances[vault], vaultTokens);
+        Transfer(0, vault, vaultTokens);
+        remainingSupply = SafeMath.sub(remainingSupply, vaultTokens);
+        
+        // 1% of total used for bounty. Remainder will be used for business.
+        bountyTokens = SafeMath.div(SafeMath.mul(totalSupply, bountyPercentage), hundredPercent);
+        remainingSupply = SafeMath.sub(remainingSupply, bountyTokens);
+        
+        balances[businessAddress] = SafeMath.add(balances[businessAddress], remainingSupply);
+        Transfer(0, businessAddress, remainingSupply);
     }
 
-    // crowdsale statuses
-    uint256 public totalFundedEther;
-
-    //This includes the Ether raised during the presale.
-    uint256 public totalConsideredFundedEther = 338;
-
-    mapping (address => uint256) consideredFundedEtherOf;
-
-    mapping (address => bool) withdrawalStatuses;
-
-    function calcBonus() public constant returns (uint256){
-        return calcBonusAt(now);
+    /* Send coins */
+    function transfer(address _to, uint256 _amount) public returns (bool success) {
+        return super.transfer(_to, _amount);
     }
 
-    function calcBonusAt(uint256 at) public constant returns (uint256){
-        if (at < STARTDATE) {
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
+        return super.transferFrom(_from, _to, _amount);
+    }
+    
+    function getBonusMultiplierAt(uint256 _timestamp) public constant returns (uint256) {
+        if (_timestamp >= firstPresaleStart && _timestamp < firstPresaleEnd) {
             return 140;
         }
-        else if (at < (STARTDATE + 1 days)) {
+        else if (_timestamp >= secondPresaleStart && _timestamp < secondPresaleEnd) {
+            return 130;
+        }
+        else if (_timestamp < (startDate + 1 days)) {
             return 120;
         }
-        else if (at < (STARTDATE + 7 days)) {
+        else if (_timestamp < (startDate + 7 days)) {
             return 115;
         }
-        else if (at < (STARTDATE + 14 days)) {
+        else if (_timestamp < (startDate + 14 days)) {
             return 110;
         }
-        else if (at < (STARTDATE + 21 days)) {
+        else if (_timestamp < (startDate + 21 days)) {
             return 105;
         }
-        else if (at <= ENDDATE) {
+        else if (_timestamp <= endDate) {
             return 100;
         }
         else {
@@ -279,70 +340,37 @@ contract SynchroCoin is Ownable, StandardToken {
         }
     }
 
-
-    function() public payable {
-        proxyPayment(msg.sender);
+    function distributeCrowdsaleTokens(address _to, uint256 _ether, uint256 _timestamp) public onlyOwner returns (uint256) {
+        require(_to != 0x0);
+        require(_ether >= 100 finney);
+        require(_timestamp >= firstPresaleStart);
+        require(_timestamp <= endDate);
+        
+        //Calculate participant's bonus
+        uint256 consideredFundedEther = SafeMath.div(SafeMath.mul(_ether, getBonusMultiplierAt(_timestamp)), 100);
+        //Calculate participant's token share
+        uint256 share = SafeMath.div(SafeMath.mul(consideredFundedEther, crowdSaleTokens), totalConsideredFundedEther);
+        balances[_to] = SafeMath.add(balances[_to], share);
+        Transfer(0, _to, share);
+        return share;
     }
-
-    function proxyPayment(address participant) public payable {
-        require(now >= STARTDATE);
-
-        require(now <= ENDDATE);
-
-        //require msg.value >= 0.1 ether
-        require(msg.value >= 100 finney);
-
-        totalFundedEther = totalFundedEther.add(msg.value);
-
-        uint256 _consideredEther = msg.value.mul(calcBonus()).div(100);
-        totalConsideredFundedEther = totalConsideredFundedEther.add(_consideredEther);
-        consideredFundedEtherOf[participant] = consideredFundedEtherOf[participant].add(_consideredEther);
-        withdrawalStatuses[participant] = true;
-
-        // Log events
-        Fund(
-        participant,
-        msg.value,
-        totalFundedEther
-        );
-
-        // Move the funds to a safe wallet
-        multisig.transfer(msg.value);
+    
+    function distributeBountyTokens(address[] _to, uint256[] _values) public onlyOwner {
+        require(_to.length == _values.length);
+        
+        uint256 i = 0;
+        while (i < _to.length) {
+            bountyTokens = SafeMath.sub(bountyTokens, _values[i]);
+            balances[_to[i]] = SafeMath.add(balances[_to[i]], _values[i]);
+            Transfer(0, _to[i], _values[i]);
+            i += 1;
+        }
     }
-
-    event Fund(
-    address indexed buyer,
-    uint256 ethers,
-    uint256 totalEther
-    );
-
-    function withdraw() public returns (bool success){
-        return proxyWithdraw(msg.sender);
+    
+    function completeBountyDistribution() public onlyOwner {
+        //After distribution of bounty tokens, transfer remaining tokens to Synchrolife business address
+        balances[businessAddress] = SafeMath.add(balances[businessAddress], bountyTokens);
+        Transfer(0, businessAddress, bountyTokens);
+        bountyTokens = 0;
     }
-
-    function proxyWithdraw(address participant) public returns (bool success){
-        require(now > ENDDATE);
-        require(withdrawalStatuses[participant]);
-        require(totalConsideredFundedEther > 1);
-
-        uint256 share = crowdSale.mul(consideredFundedEtherOf[participant]).div(totalConsideredFundedEther);
-        participant.transfer(share);
-        withdrawalStatuses[participant] = false;
-        return true;
-    }
-
-    /* Send coins */
-    function transfer(address _to, uint256 _amount) public returns (bool success) {
-        require(now > ENDDATE);
-        return super.transfer(_to, _amount);
-    }
-
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _amount) public
-    returns (bool success)
-    {
-        require(now > ENDDATE);
-        return super.transferFrom(_from, _to, _amount);
-    }
-
 }
