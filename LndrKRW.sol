68c6ad832addd20af95f76e3b11d7ea758778750
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:49:48 +0700

    add contract LndrKRW

---
+++ LndrKRW.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LndrKRW at 0xddc1d8d327978b00b526bd160e4b7212b6c55a26
+*/
+pragma solidity 0.4.15;
+
+contract LndrKRW {
+    function allowTransaction(address creditor, address debtor, uint256 amount) public returns (bool) {
+        return true;
+    }
+}
