b061410a93f07ebc496ea867a4d30daccc7210fe
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:55:21 +0700

    add contract Sale

--- Sale.sol
+++ Sale.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0xd75c3a825fed92a1aa75a0bbdacdebbc918931ec
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x070bb4f057c276fde15ed7f636603353c8f72864
 */
-pragma solidity 0.4.18;
+pragma solidity ^0.4.18;
 
 contract ERC20Basic {
     uint256 public totalSupply;
@@ -443,10 +443,6 @@ contract Sale is Configurable, Bonuses{
         return withinPeriod && nonZeroPurchase && minimalPriceChecked && activated && !closed;
     }
 
-    function isStarted() public constant returns (bool) {
-        return now > startTime;
-    }
-
     function isEnded() public constant returns (bool) {
         return now > endTime;
     }
@@ -535,8 +531,8 @@ contract DoubleLandICO_TEST is Ownable {
         require(!isDeployed);
         isDeployed = true;
 
-        softCap = 8000 ether;
-        hardCap = 40000 ether;
+        softCap = 6000 ether;
+        hardCap = 25000 ether;
         maxActivatedSalesTotalCount = 5;
 
         setGlobalMultisigWallet(0xcC6E23E740FBc50e242B6B90f0BcaF64b83BF813);
