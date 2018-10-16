e67b04b388f0b214d1cb131d87b5669b7e2ff8c2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:46:26 +0700

    add contract EnjinBuyer

--- EnjinBuyer.sol
+++ EnjinBuyer.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EnjinBuyer at 0x6c1bcb34142bffd35f57db626e0ac427af616a4d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EnjinBuyer at 0xfa21d56a4058e571da42c627e4f59826e78cf0b8
 */
 pragma solidity ^0.4.13;
 
@@ -14,11 +14,12 @@ contract ERC20 {
 
 contract EnjinBuyer {
   mapping (address => uint256) public balances;
-  mapping (address => uint256) public balances_after_buy;
+  mapping (address => uint256) public balances_for_refund;
   bool public bought_tokens;
   bool public token_set;
-  bool public refunded;
   uint256 public contract_eth_value;
+  uint256 public refund_contract_eth_value;
+  uint256 public refund_eth_value;
   bool public kill_switch;
   bytes32 password_hash = 0x8bf0720c6e610aace867eba51b03ab8ca908b665898b10faddc95a96e829539d;
   address public developer = 0x0639C169D9265Ca4B4DEce693764CdA8ea5F3882;
@@ -31,12 +32,6 @@ contract EnjinBuyer {
     token = ERC20(_token);
     token_set = true;
   }
-
-  // This function should only be called in the unfortunate case that Enjin should refund from a different address.
-  function set_refunded(bool _refunded) {
-    require(msg.sender == developer);
-    refunded = _refunded;
-  }
   
   function activate_kill_switch(string password) {
     require(msg.sender == developer || sha3(password) == password_hash);
@@ -44,14 +39,8 @@ contract EnjinBuyer {
   }
   
   function personal_withdraw(){
-    if (balances_after_buy[msg.sender]>0 && msg.sender != sale) {
-        uint256 eth_to_withdraw_after_buy = balances_after_buy[msg.sender];
-        balances_after_buy[msg.sender] = 0;
-        msg.sender.transfer(eth_to_withdraw_after_buy);
-    }
     if (balances[msg.sender] == 0) return;
-    require(msg.sender != sale);
-    if (!bought_tokens || refunded) {
+    if (!bought_tokens) {
       uint256 eth_to_withdraw = balances[msg.sender];
       balances[msg.sender] = 0;
       msg.sender.transfer(eth_to_withdraw);
@@ -69,58 +58,60 @@ contract EnjinBuyer {
     }
   }
 
-  function withdraw(address user){
-    require(bought_tokens || kill_switch);
-    // We don't allow the crowdsale to withdraw its funds back (or anyone to do that on their behalf).
-    require(user != sale);
-    if (balances_after_buy[user]>0 && user != sale) {
-        uint256 eth_to_withdraw_after_buy = balances_after_buy[user];
-        balances_after_buy[user] = 0;
-        user.transfer(eth_to_withdraw_after_buy);
-    }
-    if (balances[user] == 0) return;
-    if (!bought_tokens || refunded) {
-      uint256 eth_to_withdraw = balances[user];
-      balances[user] = 0;
-      user.transfer(eth_to_withdraw);
+
+  // Use with caution - use this withdraw function if you do not trust the
+  // contract's token setting. You can only use this once, so if you
+  // put in the wrong token address you will burn the Enjin on the contract.
+  function withdraw_token(address _token){
+    ERC20 myToken = ERC20(_token);
+    if (balances[msg.sender] == 0) return;
+    require(msg.sender != sale);
+    if (!bought_tokens) {
+      uint256 eth_to_withdraw = balances[msg.sender];
+      balances[msg.sender] = 0;
+      msg.sender.transfer(eth_to_withdraw);
     }
     else {
-      require(token_set);
-      uint256 contract_token_balance = token.balanceOf(address(this));
+      uint256 contract_token_balance = myToken.balanceOf(address(this));
       require(contract_token_balance != 0);
-      uint256 tokens_to_withdraw = (balances[user] * contract_token_balance) / contract_eth_value;
-      contract_eth_value -= balances[user];
-      balances[user] = 0;
+      uint256 tokens_to_withdraw = (balances[msg.sender] * contract_token_balance) / contract_eth_value;
+      contract_eth_value -= balances[msg.sender];
+      balances[msg.sender] = 0;
       uint256 fee = tokens_to_withdraw / 100;
-      require(token.transfer(developer, fee));
-      require(token.transfer(user, tokens_to_withdraw - fee));
+      require(myToken.transfer(developer, fee));
+      require(myToken.transfer(msg.sender, tokens_to_withdraw - fee));
     }
   }
 
-  function purchase_tokens() {
-    require(msg.sender == developer);
-    if (this.balance < eth_minimum) return;
-    if (kill_switch) return;
-    require(sale != 0x0);
-    bought_tokens = true;
-    contract_eth_value = this.balance;
-    require(sale.call.value(contract_eth_value)());
-    require(this.balance==0);
+  // This handles the withdrawal of refunds. Also works with partial refunds.
+  function withdraw_refund(){
+    require(refund_eth_value!=0);
+    require(balances_for_refund[msg.sender] != 0);
+    uint256 eth_to_withdraw = (balances_for_refund[msg.sender] * refund_eth_value) / refund_contract_eth_value;
+    refund_contract_eth_value -= balances_for_refund[msg.sender];
+    refund_eth_value -= eth_to_withdraw;
+    balances_for_refund[msg.sender] = 0;
+    msg.sender.transfer(eth_to_withdraw);
   }
-  
+
   function () payable {
     if (!bought_tokens) {
       balances[msg.sender] += msg.value;
+      balances_for_refund[msg.sender] += msg.value;
+      if (this.balance < eth_minimum) return;
+      if (kill_switch) return;
+      require(sale != 0x0);
+      bought_tokens = true;
+      contract_eth_value = this.balance;
+      refund_contract_eth_value = this.balance;
+      require(sale.call.value(contract_eth_value)());
+      require(this.balance==0);
     } else {
-      // We might be getting a refund from Enjin's multisig wallet.
-      // It could also be someone who has missed the buy, so we keep
-      // track of this as well so that he can safely withdraw.
-      // We might get the Enjin refund from another wallet, so this
-      // is why we allow this behavior.
-      balances_after_buy[msg.sender] += msg.value;
-      if (msg.sender == sale && this.balance >= contract_eth_value) {
-        refunded = true;
-      }
+      // We might be getting a full refund or partial refund if we go over the limit from Enjin's multisig wallet.
+      // We have been assured by the CTO that the refund would only
+      // come from the pre-sale wallet.
+      require(msg.sender == sale);
+      refund_eth_value += msg.value;
     }
   }
 }
