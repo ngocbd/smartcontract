3c60980d875a9382da2572a853efda4c00e2d094
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:20:41 +0700

    add contract Migrations

--- Migrations.sol
+++ Migrations.sol
@@ -1,26 +1,56 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0xe5f0323264ff7988e74974295c8bbc215fe58b26
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0xb0C3b59EA7A89F889475de65BC23b08fd94eAaC0
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.4;
 
-contract Migrations {
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
   address public owner;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
 
-  function Migrations() public {
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
+    }
   }
+
+}
+
+contract Migrations is Ownable {
+    uint public last_completed_migration;
+
+    function setCompleted(uint _completed) onlyOwner {
+        last_completed_migration = _completed;
+    }
+
+    function upgrade(address _newAddress) onlyOwner {
+        Migrations upgraded = Migrations(_newAddress);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
