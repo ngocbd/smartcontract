451363da0bb45c49892d74b2ad773a75f8201536
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:18:09 +0700

    add contract MonethaGateway

--- MonethaGateway.sol
+++ MonethaGateway.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MonethaGateway at 0x03dc9b46243703fa1c1f95c2693212635f742204
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MonethaGateway at 0x35a9f962096a1c5e960c8dc69872fc84893ac6f8
 */
 pragma solidity 0.4.18;
 
@@ -54,6 +54,12 @@ contract Ownable {
  */
 contract Restricted is Ownable {
 
+    //MonethaAddress set event
+    event MonethaAddressSet(
+        address _address,
+        bool _isMonethaAddress
+    );
+
     mapping (address => bool) public isMonethaAddress;
 
     /**
@@ -69,8 +75,9 @@ contract Restricted is Ownable {
      */
     function setMonethaAddress(address _address, bool _isMonethaAddress) onlyOwner public {
         isMonethaAddress[_address] = _isMonethaAddress;
-    }
 
+        MonethaAddressSet(_address, _isMonethaAddress);
+    }
 }
 
 // File: zeppelin-solidity/contracts/lifecycle/Destructible.sol
@@ -204,7 +211,7 @@ contract MonethaGateway is Pausable, Contactable, Destructible, Restricted {
 
     using SafeMath for uint256;
     
-    string constant VERSION = "0.3";
+    string constant VERSION = "0.4";
 
     /**
      *  Fee permille of Monetha fee.
@@ -239,17 +246,18 @@ contract MonethaGateway is Pausable, Contactable, Destructible, Restricted {
      *  acceptPayment accept payment from PaymentAcceptor, forwards it to merchant's wallet
      *      and collects Monetha fee.
      *  @param _merchantWallet address of merchant's wallet for fund transfer
+     *  @param _monethaFee is a fee collected by Monetha
      */
-    function acceptPayment(address _merchantWallet) external payable onlyMonetha whenNotPaused {
+    function acceptPayment(address _merchantWallet, uint _monethaFee) external payable onlyMonetha whenNotPaused {
         require(_merchantWallet != 0x0);
+        require(_monethaFee >= 0 && _monethaFee <= FEE_PERMILLE.mul(msg.value).div(1000)); // Monetha fee cannot be greater than 1.5% of payment
 
-        uint merchantIncome = msg.value.sub(FEE_PERMILLE.mul(msg.value).div(1000));
-        uint monethaIncome = msg.value.sub(merchantIncome);
+        uint merchantIncome = msg.value.sub(_monethaFee);
 
         _merchantWallet.transfer(merchantIncome);
-        monethaVault.transfer(monethaIncome);
+        monethaVault.transfer(_monethaFee);
 
-        PaymentProcessed(_merchantWallet, merchantIncome, monethaIncome);
+        PaymentProcessed(_merchantWallet, merchantIncome, _monethaFee);
     }
 
     /**
@@ -267,6 +275,8 @@ contract MonethaGateway is Pausable, Contactable, Destructible, Restricted {
         require(msg.sender == admin || msg.sender == owner);
 
         isMonethaAddress[_address] = _isMonethaAddress;
+
+        MonethaAddressSet(_address, _isMonethaAddress);
     }
 
     /**
