d7e412a387c8c5675adba82816aacc7a9b9e2732
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:13:29 +0700

    add contract PrivateSale

--- PrivateSale.sol
+++ PrivateSale.sol
@@ -1,99 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PrivateSale at 0xbcaf3d736343447464ee6acdf03ee84ef4c39f61
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PrivateSale at 0xb9df29b983802242281ae3409454703ab7805c8c
 */
-pragma solidity ^0.4.18;
-
-// File: zeppelin-solidity/contracts/ownership/Ownable.sol
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-// File: zeppelin-solidity/contracts/lifecycle/Pausable.sol
-
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
-
-  bool public paused = false;
-
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is not paused.
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
-  }
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is paused.
-   */
-  modifier whenPaused() {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused public {
-    paused = true;
-    Pause();
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused public {
-    paused = false;
-    Unpause();
-  }
-}
-
-// File: zeppelin-solidity/contracts/math/SafeMath.sol
+pragma solidity 0.4.19;
 
 /**
  * @title SafeMath
@@ -101,11 +9,8 @@ contract Pausable is Ownable {
  */
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
     uint256 c = a * b;
-    assert(c / a == b);
+    assert(a == 0 || c / a == b);
     return c;
   }
 
@@ -128,71 +33,158 @@ library SafeMath {
   }
 }
 
-// File: zeppelin-solidity/contracts/token/ERC20Basic.sol
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+contract PrivateSale {
+  using SafeMath for uint256;
 
-// File: zeppelin-solidity/contracts/token/ERC20.sol
+  // Address of owner
+  address public owner;
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+  // Address where funds are collected
+  address public wallet;
 
-// File: contracts/PrivateSale.sol
+  // Amount of wei raised
+  uint256 public weiRaised;
 
-contract PrivateSale is Pausable {
-    using SafeMath for uint256;
+  // Flag to accept or reject payments
+  bool public isAcceptingPayments;
 
-    ERC20 public token;
-    uint256 public rate = 1000;
+  // List of admins who can edit the whitelist
+  mapping (address => bool) public whitelistAdmins;
 
-    function PrivateSale(address _token) public {
-        token = ERC20(_token);
+  // List of addresses that are whitelisted for private sale
+  mapping (address => bool) public whitelist;
+  uint256 public whitelistCount;
+
+  // List of addresses that have made payments
+  mapping (address => uint256) public weiPaid;
+
+  uint256 public HARD_CAP = 6666 ether;
+
+  // modifier to check owner
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  // modifier to check whitelist admin status
+  modifier onlyWhitelistAdmin() {
+    require(whitelistAdmins[msg.sender]);
+    _;
+  }
+
+  // modifier to check if whitelisted address
+  modifier isWhitelisted() {
+    require(whitelist[msg.sender]);
+    _;
+  }
+
+  // modifier to check if payments being accepted
+  modifier acceptingPayments() {
+    require(isAcceptingPayments);
+    _;
+  }
+
+  /**
+   * Constructor
+   * @param _wallet Address where collected funds will be forwarded to
+   */
+  function PrivateSale(address _wallet) public {
+    require(_wallet != address(0));
+    owner = msg.sender;
+    wallet = _wallet;
+    whitelistAdmins[msg.sender] = true;
+  }
+
+  /**
+   * @dev fallback function
+   */
+  function () isWhitelisted acceptingPayments payable public {
+    require(msg.value >= 0.2 ether);
+    require(msg.value <= 500 ether);
+    require(msg.sender != address(0));
+    
+    uint256 contribution = msg.value;
+    // add to sender's weiPaid record
+    weiPaid[msg.sender] += msg.value;
+
+    // add to amount raised
+    weiRaised = weiRaised.add(msg.value);
+
+    if (weiRaised > HARD_CAP) {
+      uint256 refundAmount = weiRaised.sub(HARD_CAP);
+      msg.sender.transfer(refundAmount);
+      contribution = contribution.sub(refundAmount);
+      refundAmount = 0;
+      weiRaised = HARD_CAP;
+      isAcceptingPayments = false;
     }
 
-    function setToken(address _tokenAddr) public onlyOwner {
-        token = ERC20(_tokenAddr);
-    }
+    // transfer funds to external wallet
+    wallet.transfer(contribution);
+  }
 
-    function setRate(uint256 _rate) public onlyOwner {
-        rate = _rate;
-    }
+  /**
+   * @dev Start accepting payments
+   */
+  function acceptPayments() onlyOwner public  {
+    isAcceptingPayments = true;
+  }
 
-    function transferToken(address _to, uint256 _value) public onlyOwner {
-        token.transfer(_to, _value);
-    }
+  /**
+   * @dev Stop accepting payments
+   */
+  function rejectPayments() onlyOwner public  {
+    isAcceptingPayments = false;
+  }
 
-    function () public payable whenNotPaused {
-        require(token != address(0));
-        require(msg.value > 0);
+  /**
+   *  @dev Add a user to the whitelist admins
+   */
+  function addWhitelistAdmin(address _admin) onlyOwner public {
+    whitelistAdmins[_admin] = true;
+  }
 
-        uint256 amount = msg.value.mul(rate);
-        uint256 currentBal = token.balanceOf(this);
-        if (currentBal >= amount) {
-            owner.transfer(msg.value);
-            token.transfer(msg.sender, amount);
-        } else {
-            uint256 value = currentBal.div(rate);
-            owner.transfer(value);
-            token.transfer(msg.sender, currentBal);
-            msg.sender.transfer(msg.value.sub(value));
-        }
+  /**
+   *  @dev Remove a user from the whitelist admins
+   */
+  function removeWhitelistAdmin(address _admin) onlyOwner public {
+    whitelistAdmins[_admin] = false;
+  }
+
+  /**
+   * @dev Add an address to the whitelist
+   * @param _user The address of the contributor
+   */
+  function whitelistAddress(address _user) onlyWhitelistAdmin public  {
+    whitelist[_user] = true;
+  }
+
+  /**
+   * @dev Add multiple addresses to the whitelist
+   * @param _users The addresses of the contributor
+   */
+  function whitelistAddresses(address[] _users) onlyWhitelistAdmin public {
+    for (uint256 i = 0; i < _users.length; i++) {
+      whitelist[_users[i]] = true;
     }
+  }
+
+  /**
+   * @dev Remove an addresses from the whitelist
+   * @param _user The addresses of the contributor
+   */
+  function unWhitelistAddress(address _user) onlyWhitelistAdmin public  {
+    whitelist[_user] = false;
+  }
+
+  /**
+   * @dev Remove multiple addresses from the whitelist
+   * @param _users The addresses of the contributor
+   */
+  function unWhitelistAddresses(address[] _users) onlyWhitelistAdmin public {
+    for (uint256 i = 0; i < _users.length; i++) {
+      whitelist[_users[i]] = false;
+    }
+  }
 }
