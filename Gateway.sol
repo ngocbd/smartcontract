1bde00636e43279d701242bab75693da1576aff8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:16:24 +0700

    add contract Gateway

--- Gateway.sol
+++ Gateway.sol
@@ -1,153 +1,214 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gateway at 0xc89ce62e1f5bf285ad4427f2941fc887de649058
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gateway at 0xcf19cfee14e161d339a084993b2cb18b7eaec773
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
-// ----------------------------------------------------------------------------
-//
-// Funds Gateway contract
-//
-// ----------------------------------------------------------------------------
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
-contract Owned {
-
-  address public owner;
-  address public newOwner;
-
-
-  event OwnershipTransferProposed(address indexed _from, address indexed _to);
-  event OwnershipTransferConfirmed(address indexed _from, address indexed _to);
-
-
-  modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
   }
 
-
-  function Owned() public{
-    owner = msg.sender;
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
   }
 
-
-  function transferOwnership(address _newOwner) onlyOwner public{
-    require(_newOwner != owner);
-    OwnershipTransferProposed(owner, _newOwner);
-    newOwner = _newOwner;
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
   }
 
-
-  function confirmOwnership() public{
-    assert(msg.sender == newOwner);
-    OwnershipTransferConfirmed(owner, newOwner);
-    owner = newOwner;
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
   }
 
 }
 
+contract Token {
+  /// @return total amount of tokens
+  function totalSupply() constant returns (uint256 supply) {}
 
-contract Gateway is Owned{
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return The balance
+  function balanceOf(address _owner) constant returns (uint256 balance) {}
 
-  address public targetWallet;
-  address public whitelistWallet;
+  /// @notice send `_value` token to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value) returns (bool success) {}
 
+  /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 
-  bool public gatewayOpened = false;
+  /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @param _value The amount of wei to be approved for transfer
+  /// @return Whether the approval was successful or not
+  function approve(address _spender, uint256 _value) returns (bool success) {}
 
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return Amount of remaining tokens allowed to spent
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  uint public decimals;
+  string public name;
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
     
-  mapping(address => bool) public whitelist;
+  address public owner;
 
-  
-  event TargetWalletUpdated(address _newWallet);
-  event WhitelistWalletUpdated(address _newWhitelistWallet);
-  event GatewayStatusUpdated(bool _status);
-  event WhitelistUpdated(address indexed _participant, bool _status);
-  event PassedGateway(address _participant, uint _value);
-  
-
-  function Gateway() public{
-    targetWallet = owner;
-    whitelistWallet = owner;
-    newOwner = address(0x0);
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
   }
 
-  
-  function () payable public{
-    passGateway();
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
 
-  function addToWhitelist(address _participant) external{
-    require(msg.sender == whitelistWallet || msg.sender == owner);
-    whitelist[_participant] = true;
-    WhitelistUpdated(_participant, true);
-  }  
-
-
-  function addToWhitelistMultiple(address[] _participants) external{
-    require(msg.sender == whitelistWallet || msg.sender == owner);
-    for (uint i = 0; i < _participants.length; i++) {
-      whitelist[_participants[i]] = true;
-      WhitelistUpdated(_participants[i], true);
+}
+contract Gateway is Ownable{
+    using SafeMath for uint;
+    address public feeAccount1 = 0x703f9037088A93853163aEaaEd650f3e66aD7A4e; //the account1 that will receive fees
+    address public feeAccount2 = 0xc94cac4a4537865753ecdf2ad48F00112dC09ea8; //the account2 that will receive fees
+    address public feeAccountToken = 0x2EF9B82Ab8Bb8229B3D863A47B1188672274E1aC;//the account that will receive fees tokens
+    
+    struct BuyInfo {
+      address buyerAddress; 
+      address sellerAddress;
+      uint value;
+      address currency;
     }
-  }
-
-
-  function removeFromWhitelist(address _participant) external{
-    require(msg.sender == whitelistWallet || msg.sender == owner);
-    whitelist[_participant] = false;
-    WhitelistUpdated(_participant, false);
-  }  
-
-
-  function removeFromWhitelistMultiple(address[] _participants) external{
-    require(msg.sender == whitelistWallet || msg.sender == owner);
-    for (uint i = 0; i < _participants.length; i++) {
-      whitelist[_participants[i]] = false;
-      WhitelistUpdated(_participants[i], false);
+    
+    mapping(address => mapping(uint => BuyInfo)) public payment;
+   
+    mapping(address => uint) public balances;
+    uint balanceFee;
+    uint public feePercent;
+    uint public maxFee;
+    function Gateway() public{
+       feePercent = 1500000; // decimals 6. 1,5% fee by default
+       maxFee = 3000000; // fee can not exceed 3%
     }
-  }
-
-
-  function setTargetWallet(address _wallet) onlyOwner external{
-    require(_wallet != address(0x0));
-    targetWallet = _wallet;
-    TargetWalletUpdated(_wallet);
-  }
-  
-
-  function setWhitelistWallet(address _wallet) onlyOwner external{
-    whitelistWallet = _wallet;
-    WhitelistWalletUpdated(_wallet);
-  }
-
-
-  function openGateway() onlyOwner external{
-    require(!gatewayOpened);
-    gatewayOpened = true;
     
-    GatewayStatusUpdated(true);
-  }
-
-
-  function closeGateway() onlyOwner external{
-    require(gatewayOpened);
-    gatewayOpened = false;
     
-    GatewayStatusUpdated(false);
+    function getBuyerAddressPayment(address _sellerAddress, uint _orderId) public constant returns(address){
+      return  payment[_sellerAddress][_orderId].buyerAddress;
+    }    
+    function getSellerAddressPayment(address _sellerAddress, uint _orderId) public constant returns(address){
+      return  payment[_sellerAddress][_orderId].sellerAddress;
+    }    
+    
+    function getValuePayment(address _sellerAddress, uint _orderId) public constant returns(uint){
+      return  payment[_sellerAddress][_orderId].value;
+    }    
+    
+    function getCurrencyPayment(address _sellerAddress, uint _orderId) public constant returns(address){
+      return  payment[_sellerAddress][_orderId].currency;
+    }
+    
+    
+    function setFeeAccount1(address _feeAccount1) onlyOwner public{
+      feeAccount1 = _feeAccount1;  
+    }
+    function setFeeAccount2(address _feeAccount2) onlyOwner public{
+      feeAccount2 = _feeAccount2;  
+    }
+    function setFeeAccountToken(address _feeAccountToken) onlyOwner public{
+      feeAccountToken = _feeAccountToken;  
+    }    
+    function setFeePercent(uint _feePercent) onlyOwner public{
+      require(_feePercent <= maxFee);
+      feePercent = _feePercent;  
+    }    
+    function payToken(address _tokenAddress, address _sellerAddress, uint _orderId,  uint _value) public returns (bool success){
+      require(_tokenAddress != address(0));
+      require(_sellerAddress != address(0)); 
+      require(_value > 0);
+      Token token = Token(_tokenAddress);
+      require(token.allowance(msg.sender, this) >= _value);
+      token.transferFrom(msg.sender, feeAccountToken, _value.mul(feePercent).div(100000000));
+      token.transferFrom(msg.sender, _sellerAddress, _value.sub(_value.mul(feePercent).div(100000000)));
+      payment[_sellerAddress][_orderId] = BuyInfo(msg.sender, _sellerAddress, _value, _tokenAddress);
+      success = true;
+    }
+    function payEth(address _sellerAddress, uint _orderId, uint _value) public returns  (bool success){
+      require(_sellerAddress != address(0)); 
+      require(_value > 0);
+      require(balances[msg.sender] >= _value);
+      uint fee = _value.mul(feePercent).div(100000000);
+      balances[msg.sender] = balances[msg.sender].sub(_value);
+      _sellerAddress.transfer(_value.sub(fee));
+      balanceFee = balanceFee.add(fee);
+      payment[_sellerAddress][_orderId] = BuyInfo(msg.sender, _sellerAddress, _value, 0x0000000000000000000000000000000000000001);    
+      success = true;
+    }
+    function transferFee() onlyOwner public{
+      uint valfee1 = balanceFee.div(2);
+      feeAccount1.transfer(valfee1);
+      balanceFee = balanceFee.sub(valfee1);
+      feeAccount2.transfer(balanceFee);
+      balanceFee = 0;
+    }
+    function balanceOfToken(address _tokenAddress, address _Address) public constant returns (uint) {
+      Token token = Token(_tokenAddress);
+      return token.balanceOf(_Address);
+    }
+    function balanceOfEthFee() public constant returns (uint) {
+      return balanceFee;
+    }
+    function refund() public{
+      require(balances[msg.sender] > 0);
+      uint value = balances[msg.sender];
+      balances[msg.sender] = 0;
+      msg.sender.transfer(value);
+    }
+    function getBalanceEth() public constant returns(uint){
+      return balances[msg.sender];    
+    }
+    function() external payable {
+      balances[msg.sender] = balances[msg.sender].add(msg.value);    
   }
-
-
-  function passGateway() private{
-
-    require(gatewayOpened);
-    require(whitelist[msg.sender]);
-
-	  // sends Eth forward; covers edge case of mining/selfdestructing Eth to the contract address
-    targetWallet.transfer(this.balance);
-
-    // log event
-    PassedGateway(msg.sender, msg.value);
-  }
-  
 }
