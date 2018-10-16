94292d6f09f453df80d514b14ba95b1a6cc4dc40
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:09:56 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,109 +1,137 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x24f8752ae86cdd340bc40e606e6f89844b1cfe77
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xc8155f3fd57737b5855dc60b179bfc0e5404f167
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.18;
 
 interface token {
-    function transfer(address receiver, uint amount);
+    function transfer(address receiver, uint amount) external;
 }
 
-contract Crowdsale {
-    address public beneficiary;  // ?????????
-    uint public fundingGoal;   // ????
-    uint public amountRaised;   // ????
-    uint public deadline;      // ?????
 
-    uint public price;    //  token ??????? , token????
-    token public tokenReward;   // ???token
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
-    mapping(address => uint256) public balanceOf;
-
-    bool fundingGoalReached = false;  // ????????
-    bool crowdsaleClosed = false;   //  ??????
-
-    /**
-    * ??????????
-    **/
-    event GoalReached(address recipient, uint totalAmountRaised);
-    event FundTransfer(address backer, uint amount, bool isContribution);
-
-    /**
-     * ????, ??????
-     */
-    function Crowdsale(
-        address ifSuccessfulSendTo,
-        uint fundingGoalInEthers,
-        uint durationInMinutes,
-        uint finneyCostOfEachToken,
-        address addressOfTokenUsedAsReward) {
-            beneficiary = ifSuccessfulSendTo;
-            fundingGoal = fundingGoalInEthers * 1 ether;
-            deadline = now + durationInMinutes * 1 minutes;
-            price = finneyCostOfEachToken * 1 finney;
-            tokenReward = token(addressOfTokenUsedAsReward);   // ?????? token ??????????
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
     }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-    /**
-     * ?????Fallback???
-     * ????????????????
-     */
-    function () payable {
-      require(!crowdsaleClosed);
-      uint amount = msg.value;
-      balanceOf[msg.sender] += amount;
-      amountRaised += amount;
-      tokenReward.transfer(msg.sender, amount / price);
-      FundTransfer(msg.sender, amount, true);
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
     
-        // ???????????
-      beneficiary.send(amount);
+  address public owner;
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public{
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) private onlyOwner {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
+
+}
+
+contract Crowdsale is Ownable {
+    
+    using SafeMath for uint;
+    
+    address public escrow;
+    
+    token public tokenReward;
+    
+    uint start = 1525132800;
+    
+    uint period = 31;
+    
+    
+    
+    function Crowdsale (
+        
+        
+        ) public {
+        escrow = 0x8bB3E0e70Fa2944DBA0cf5a1AF6e230A9453c647;
+        tokenReward = token(0xACE380244861698DBa241C4e0d6F8fFc588A6F73);
     }
-
-    /**
-    *  ???????modifier????Python????????
-    * ?????????????????????????????????
-    * _ ???????????
-    **/
-    modifier afterDeadline() { if (now >= deadline) _; }
-
-    /**
-     * ????????????? ???????afterDeadline?????
-     *
-     */
-    function checkGoalReached() afterDeadline {
-        if (amountRaised >= fundingGoal) {
-            fundingGoalReached = true;
-            GoalReached(beneficiary, amountRaised);
-        }
-        crowdsaleClosed = true;
+    
+        modifier saleIsOn() {
+        require(now > start && now < start + period * 1 days);
+        _;
     }
-
-
-    /**
-     * ?????????????????
-     * ?????????????
-     *
-     */
-    function safeWithdrawal() afterDeadline {
-        if (!fundingGoalReached) {
-            uint amount = balanceOf[msg.sender];
-            balanceOf[msg.sender] = 0;
-            if (amount > 0) {
-                if (msg.sender.send(amount)) {
-                    FundTransfer(msg.sender, amount, false);
-                } else {
-                    balanceOf[msg.sender] = amount;
-                }
-            }
-        }
-
-        if (fundingGoalReached && beneficiary == msg.sender) {
-            if (beneficiary.send(amountRaised)) {
-                FundTransfer(beneficiary, amountRaised, false);
-            } else {
-                //If we fail to send the funds to beneficiary, unlock funders balance
-                fundingGoalReached = false;
-            }
-        }
+    
+    function sellTokens() public saleIsOn payable {
+        escrow.transfer(msg.value);
+        
+        uint price = 400;
+        
+    
+    uint tokens = msg.value.mul(price);
+    
+    tokenReward.transfer(msg.sender, tokens); 
+    
     }
+    
+    
+   function() external payable {
+        sellTokens();
+    }
+    
 }
