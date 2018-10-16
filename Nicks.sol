d24107e447f257a2b0daf1230426fc52ce8268e8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:10:09 +0700

    add contract Nicks

--- Nicks.sol
+++ Nicks.sol
@@ -1,49 +1,52 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Nicks at 0xb797e63afa90af26b6bc331d7242b3384441e0d3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Nicks at 0x25470ca21a09e56b4275eeafba1858a6f3375b28
 */
+pragma solidity ^0.4.18;
+
 contract Nicks {
-    mapping(address => string) private nickOfOwner;
-    mapping(string => address) private ownerOfNick;
+  mapping (address => string) private nickOfOwner;
+  mapping (string => address) private ownerOfNick;
 
-    event NickSet (string _nick, address _owner);
-    event NickUnset (string _nick, address _owner);
+  event Set (string indexed _nick, address indexed _owner);
+  event Unset (string indexed _nick, address indexed _owner);
 
-    function Nicks() public {
+  function Nicks () public {
+  }
+
+  function nickOf (address _owner) public view returns (string _nick) {
+    return nickOfOwner[_owner];
+  }
+
+  function ownerOf (string _nick) public view returns (address _owner) {
+    return ownerOfNick[_nick];
+  }
+
+  function set (string _nick) public {
+    require(bytes(_nick).length > 2);
+    require(ownerOf(_nick) == address(0));
+
+    address owner = msg.sender;
+    string storage oldNick = nickOfOwner[owner];
+
+    if (bytes(oldNick).length > 0) {
+      Unset(oldNick, owner);
+      delete ownerOfNick[oldNick];
     }
 
-    function nickOf(address _owner) public view returns (string _nick) {
-        return nickOfOwner[_owner];
-    }
+    nickOfOwner[owner] = _nick;
+    ownerOfNick[_nick] = owner;
+    Set(_nick, owner);
+  }
 
-    function ownerOf(string _nick) public view returns (address _owner) {
-        return ownerOfNick[_nick];
-    }
+  function unset () public {
+    require(bytes(nickOfOwner[msg.sender]).length > 0);
 
-    function saveNick(string _nick) public {
-        require(bytes(_nick).length > 2);
-        require(ownerOf(_nick) == address(0));
+    address owner = msg.sender;
+    string storage oldNick = nickOfOwner[owner];
 
-        address owner = msg.sender;
-        string storage oldNick = nickOfOwner[owner];
+    Unset(oldNick, owner);
 
-        if (bytes(oldNick).length > 0) {
-            delete ownerOfNick[oldNick];
-        }
-
-        nickOfOwner[owner] = _nick;
-        ownerOfNick[_nick] = owner;
-        NickSet(_nick, owner);
-    }
-
-    function deleteNick() public {
-        require(bytes(nickOfOwner[msg.sender]).length > 0);
-
-        address owner = msg.sender;
-        string storage oldNick = nickOfOwner[owner];
-
-        NickUnset(oldNick, owner);
-
-        delete ownerOfNick[oldNick];
-        delete nickOfOwner[owner];
-    }
+    delete ownerOfNick[oldNick];
+    delete nickOfOwner[owner];
+  }
 }
