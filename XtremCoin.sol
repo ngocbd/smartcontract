f1cebd096e0b03d89b4d1f7089fc738e581bbaa9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:26:13 +0700

    add contract Xtremcoin

--- XtremCoin.sol
+++ XtremCoin.sol
@@ -1,103 +1,148 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XtremCoin at 0xdf2a006ced0d5c33ef53f24c964e2354c75873bd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Xtremcoin at 0x619de4f05fe07522de2bdcf2daeb1a23da8c0bb4
 */
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.19;
 
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+contract SafeMath{
+  function safeMul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-contract XtremCoin {
-    /* Public variables of the token */
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
+  function safeDiv(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+	
+	function safeSub(uint a, uint b) internal returns (uint) {
+    	assert(b <= a);
+    	return a - b;
+  }
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+	function safeAdd(uint a, uint b) internal returns (uint) {
+    	uint c = a + b;
+    	assert(c >= a);
+    	return c;
+  }
+	function assert(bool assertion) internal {
+	    if (!assertion) {
+	      throw;
+	    }
+	}
+}
 
-    /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
+contract ERC20{
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function XtremCoin() {
-        balanceOf[msg.sender] = 20231453571;              // Give the creator all initial tokens
-        totalSupply = 20231453571;                        // Update total supply
-        name = "Xtrem Coin";                                   // Set the name for display purposes
-        symbol = "XTR";                               // Set the symbol for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-    }
+ 	function totalSupply() constant returns (uint256 totalSupply) {}
+	function balanceOf(address _owner) constant returns (uint256 balance) {}
+	function transfer(address _recipient, uint256 _value) returns (bool success) {}
+	function transferFrom(address _from, address _recipient, uint256 _value) returns (bool success) {}
+	function approve(address _spender, uint256 _value) returns (bool success) {}
+	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(_from, _to, _value);
-    }
+	event Transfer(address indexed _from, address indexed _recipient, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    /// @notice Send `_value` tokens to `_to` from your account
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
-    function transfer(address _to, uint256 _value) {
-        _transfer(msg.sender, _to, _value);
-    }
 
-    /// @notice Send `_value` tokens to `_to` in behalf of `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        require (_value < allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
+}
 
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
+contract Xtremcoin is ERC20, SafeMath{
 
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
-    /// @param _extraData some extra information to send to the approved contract
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
+	
+	mapping(address => uint256) balances;
+
+	string 	public name = "Xtremcoin";
+	string 	public symbol = "XTR";
+	uint 	public decimals = 8;
+	uint256 public CIR_SUPPLY;
+	uint256 public totalSupply;
+	uint256 public price;
+	address public owner;
+	uint256 public endTime;
+	uint256 public startTime;
+
+	function Xtremcoin(uint256 _initial_supply, uint256 _price, uint256 _cir_supply) {
+		totalSupply = _initial_supply;
+		balances[msg.sender] = _initial_supply;  // Give all of the initial tokens to the contract deployer.
+		CIR_SUPPLY = _cir_supply;
+		endTime = now + 17 weeks;
+		startTime = now + 15 days;
+		owner 	= msg.sender;
+		price 	= _price;
+	}
+
+	function balanceOf(address _owner) constant returns (uint256 balance) {
+	    return balances[_owner];
+	}
+    
+	function transfer(address _to, uint256 _value) returns (bool success){
+	    require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balances[msg.sender] > _value);                // Check if the sender has enough
+        require (safeAdd(balances[_to], _value) > balances[_to]); // Check for overflows
+	    balances[msg.sender] = safeSub(balances[msg.sender], _value);
+	    balances[_to] = safeAdd(balances[_to], _value);
+	    Transfer(msg.sender, _to, _value);
+	    return true;
+	}
+
+	mapping (address => mapping (address => uint256)) allowed;
+
+	function transferFrom(address _from, address _to, uint256 _value) returns (bool success){
+	    var _allowance = allowed[_from][msg.sender];
+	    require (_value < _allowance);
+	    
+	    require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balances[msg.sender] > _value);                // Check if the sender has enough
+        require (safeAdd(balances[_to], _value) > balances[_to]); // Check for overflows
+	    balances[_to] = safeAdd(balances[_to], _value);
+	    balances[_from] = safeSub(balances[_from], _value);
+	    allowed[_from][msg.sender] = safeSub(_allowance, _value);
+	    Transfer(_from, _to, _value);
+	    return true;
+	}
+
+	function approve(address _spender, uint256 _value) returns (bool success) {
+	    allowed[msg.sender][_spender] = _value;
+	    Approval(msg.sender, _spender, _value);
+	    return true;
+	}
+
+	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+	    return allowed[_owner][_spender];
+	}
+
+
+	modifier during_offering_time(){
+		if (now < startTime || now >= endTime){
+			throw;
+		}else{
+			_;
+		}
+	}
+
+	function () payable during_offering_time {
+		createTokens(msg.sender);
+	}
+
+	function createTokens(address recipient) payable {
+		if (msg.value == 0) {
+		  throw;
+		}
+		uint tokens = safeDiv(safeMul(msg.value, price), 1 ether);
+        if(safeSub(balances[owner],tokens)>safeSub(totalSupply, CIR_SUPPLY)){
+            balances[owner] = safeSub(balances[owner], tokens);
+		    balances[recipient] = safeAdd(balances[recipient], tokens);   
+        }else{
+            throw;
         }
-    }        
 
-    /// @notice Remove `_value` tokens from the system irreversibly
-    /// @param _value the amount of money to burn
-    function burn(uint256 _value) returns (bool success) {
-        require (balanceOf[msg.sender] > _value);            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
-        totalSupply -= _value;                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
+		if (!owner.send(msg.value)) {
+		  throw;
+		}
+	}
 
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
-        return true;
-    }
 }
