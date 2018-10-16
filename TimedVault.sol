d5662498fbce39cc0c5ac24299b85c6a99f0ea88
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:31:57 +0700

    add contract TimedVault

---
+++ TimedVault.sol
@@ -0,0 +1,54 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TimedVault at 0xaefe05643b613823dbaf6245afb819fd56fbdd22
+*/
+pragma solidity ^0.4.16;
+
+interface token {
+    function transfer(address receiver, uint amount) public;
+    function balanceOf(address addr) public returns (uint);
+}
+
+contract TimedVault {
+    address public beneficiary;
+    
+    // Lock the tokens till 03/01/2019 @ 3:00pm (UTC)
+    uint public releaseDate = 1551452400;
+    token public tokenReward;
+    
+    uint public amountOfTokens;
+
+    /**
+     * Constructor function
+     *
+     * Setup the owner
+     */
+    function TimedVault(
+        address ifSuccessfulSendTo,
+        address addressOfTokenUsedAsReward
+    ) public {
+        beneficiary = ifSuccessfulSendTo;
+        tokenReward = token(addressOfTokenUsedAsReward);
+    }
+
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+    function () payable public {
+    }
+
+    modifier afterDeadline() { if (now >= releaseDate) _; }
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal() afterDeadline public {
+        amountOfTokens = tokenReward.balanceOf(this);
+        tokenReward.transfer(beneficiary, amountOfTokens);
+    }
+}
