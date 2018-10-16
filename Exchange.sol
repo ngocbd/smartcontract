f6be5ef8348c502d0a733e3f27540c7d316fcefa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:31:52 +0700

    add contract Exchange

--- Exchange.sol
+++ Exchange.sol
@@ -1,256 +1,120 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Exchange at 0x465556b0b461a1ed5c9be3711dbd3b3d77ba1498
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Exchange at 0xe9c48b34e2498797b7f70d9fb5875805dd6bc8b4
 */
-pragma solidity ^0.4.19;
+contract SafeMath {
 
-contract Token {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-    
-    uint8 public decimals;
+  function safeMul(uint a, uint b) returns (uint) {
+    if (a == 0) {
+      return 0;
+    } else {
+      uint c = a * b;
+      require(c / a == b);
+      return c;
+    }
+  }
+
+  function safeDiv(uint a, uint b) returns (uint) {
+    require(b > 0);
+    uint c = a / b;
+    require(a == b * c + a % b);
+    return c;
+  }
+
 }
 
-contract Exchange {
-    struct Order {
-        address creator;
-        address token;
-        bool buy;
-        uint price;
-        uint amount;
-    }
-    
-    address public owner;
-    uint public feeDeposit = 500;
-    
-    mapping (uint => Order) orders;
-    uint currentOrderId = 0;
-    
-    /* Token address (0x0 - Ether) => User address => balance */
-    mapping (address => mapping (address => uint)) public balanceOf;
-    
-    event FundTransfer(address backer, uint amount, bool isContribution);
-    
-    event PlaceSell(address indexed token, address indexed user, uint price, uint amount, uint id);
-    event PlaceBuy(address indexed token, address indexed user, uint price, uint amount, uint id);
-    event FillOrder(uint indexed id, address indexed user, uint amount);
-    event CancelOrder(uint indexed id);
-    event Deposit(address indexed token, address indexed user, uint amount);
-    event Withdraw(address indexed token, address indexed user, uint amount);
-    event BalanceChanged(address indexed token, address indexed user, uint value);
 
-    modifier onlyOwner {
-        if (msg.sender != owner) revert();
+contract token {
+    function balanceOf( address who ) constant returns (uint value);
+    function transfer( address to, uint value) returns (bool ok);
+}
+
+
+contract Exchange is SafeMath {
+
+    uint public priceInWei;
+    address public creator;
+    token public tokenExchanged;
+    bool public exchangeState = false;
+    uint public multiplier = 1000000000000000000; //Token decimals
+
+    event TokenTransfer(address _sender, uint _tokenAmount);
+    event TokenExchangeFailed(address _sender, uint _tokenAmount);
+    event EthFundTransfer(uint _ethAmount);
+    event TokenFundTransfer(uint _tokenAmount);
+
+
+    function Exchange(
+        uint tokenPriceInWei,
+        address addressOfTokenExchanged
+    ) {
+        creator = msg.sender;
+        priceInWei = tokenPriceInWei;
+        tokenExchanged = token(addressOfTokenExchanged);
+    }
+
+
+    modifier isCreator() {
+        require(msg.sender == creator);
         _;
     }
-    
-    function transferOwnership(address newOwner) external onlyOwner {
-        owner = newOwner;
+
+
+    function setTokenPriceInWei(uint _price) isCreator() returns (bool result){
+      require(!exchangeState);
+      priceInWei = _price;
+      return true;
     }
-    
-    function Exchange() public {
-        owner = msg.sender;
+
+
+    function stopExchange() isCreator() returns (bool result){
+      exchangeState = false;
+      return true;
     }
-    
-    function safeAdd(uint a, uint b) private pure returns (uint) {
-        uint c = a + b;
-        assert(c >= a);
-        return c;
+
+
+    function startExchange() isCreator() returns (bool result){
+      exchangeState = true;
+      return true;
     }
-    
-    function safeSub(uint a, uint b) private pure returns (uint) {
-        assert(b <= a);
-        return a - b;
-    }
-    
-    function safeMul(uint a, uint b) private pure returns (uint) {
-        if (a == 0) {
-          return 0;
-        }
-        
-        uint c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-    
-    function decFeeDeposit(uint delta) external onlyOwner {
-        feeDeposit = safeSub(feeDeposit, delta);
-    }
-    
-    function calcAmountEther(address tokenAddr, uint price, uint amount) private view returns (uint) {
-        uint k = 10;
-        k = k ** Token(tokenAddr).decimals();
-        return safeMul(amount, price) / k;
-    }
-    
-    function balanceAdd(address tokenAddr, address user, uint amount) private {
-        balanceOf[tokenAddr][user] =
-            safeAdd(balanceOf[tokenAddr][user], amount);
-    }
-    
-    function balanceSub(address tokenAddr, address user, uint amount) private {
-        require(balanceOf[tokenAddr][user] >= amount);
-        balanceOf[tokenAddr][user] =
-            safeSub(balanceOf[tokenAddr][user], amount);
-    }
-    
-    function placeBuy(address tokenAddr, uint price, uint amount) external {
-        require(price > 0 && amount > 0);
-        uint amountEther = calcAmountEther(tokenAddr, price, amount);
-        require(amountEther > 0);
-        balanceSub(0x0, msg.sender, amountEther);
-        BalanceChanged(0x0, msg.sender, balanceOf[0x0][msg.sender]);
-        orders[currentOrderId] = Order({
-            creator: msg.sender,
-            token: tokenAddr,
-            buy: true,
-            price: price,
-            amount: amount
-        });
-        PlaceBuy(tokenAddr, msg.sender, price, amount, currentOrderId);
-        currentOrderId++;
-    }
-    
-    function placeSell(address tokenAddr, uint price, uint amount) external {
-        require(price > 0 && amount > 0);
-        uint amountEther = calcAmountEther(tokenAddr, price, amount);
-        require(amountEther > 0);
-        balanceSub(tokenAddr, msg.sender, amount);
-        BalanceChanged(tokenAddr, msg.sender, balanceOf[tokenAddr][msg.sender]);
-        orders[currentOrderId] = Order({
-            creator: msg.sender,
-            token: tokenAddr,
-            buy: false,
-            price: price,
-            amount: amount
-        });
-        PlaceSell(tokenAddr, msg.sender, price, amount, currentOrderId);
-        currentOrderId++;
-    }
-    
-    function fillOrder(uint id, uint amount) external {
-        require(id < currentOrderId);
-        require(amount > 0);
-        require(orders[id].creator != msg.sender);
-        require(orders[id].amount >= amount);
-        uint amountEther = calcAmountEther(orders[id].token, orders[id].price, amount);
-        if (orders[id].buy) {
-            /* send tokens from sender to creator */
-            // sub from sender
-            balanceSub(orders[id].token, msg.sender, amount);
-            BalanceChanged(
-                orders[id].token,
-                msg.sender,
-                balanceOf[orders[id].token][msg.sender]
-            );
-            
-            // add to creator
-            balanceAdd(orders[id].token, orders[id].creator, amount);
-            BalanceChanged(
-                orders[id].token,
-                orders[id].creator,
-                balanceOf[orders[id].token][orders[id].creator]
-            );
-            
-            /* send Ether to sender */
-            balanceAdd(0x0, msg.sender, amountEther);
-            BalanceChanged(
-                0x0,
-                msg.sender,
-                balanceOf[0x0][msg.sender]
-            );
+
+
+    function () payable {
+        require(exchangeState);
+        uint _etherAmountInWei = msg.value;
+        uint _tokenAmount = safeDiv(safeMul(_etherAmountInWei, multiplier), priceInWei);
+        if ( _tokenAmount <= tokenExchanged.balanceOf(this) ){
+          tokenExchanged.transfer(msg.sender, _tokenAmount);
+          TokenTransfer(msg.sender, _tokenAmount);
         } else {
-            /* send Ether from sender to creator */
-            // sub from sender
-            balanceSub(0x0, msg.sender, amountEther);
-            BalanceChanged(
-                0x0,
-                msg.sender,
-                balanceOf[0x0][msg.sender]
-            );
-            
-            // add to creator
-            balanceAdd(0x0, orders[id].creator, amountEther);
-            BalanceChanged(
-                0x0,
-                orders[id].creator,
-                balanceOf[0x0][orders[id].creator]
-            );
-            
-            /* send tokens to sender */
-            balanceAdd(orders[id].token, msg.sender, amount);
-            BalanceChanged(
-                orders[id].token,
-                msg.sender,
-                balanceOf[orders[id].token][msg.sender]
-            );
+          TokenExchangeFailed(msg.sender, _tokenAmount);
+          throw;
         }
-        orders[id].amount -= amount;
-        FillOrder(id, msg.sender, orders[id].amount);
     }
-    
-    function cancelOrder(uint id) external {
-        require(id < currentOrderId);
-        require(orders[id].creator == msg.sender);
-        require(orders[id].amount > 0);
-        if (orders[id].buy) {
-            uint amountEther = calcAmountEther(orders[id].token, orders[id].price, orders[id].amount);
-            balanceAdd(0x0, msg.sender, amountEther);
-            BalanceChanged(0x0, msg.sender, balanceOf[0x0][msg.sender]);
-        } else {
-            balanceAdd(orders[id].token, msg.sender, orders[id].amount);
-            BalanceChanged(orders[id].token, msg.sender, balanceOf[orders[id].token][msg.sender]);
-        }
-        orders[id].amount = 0;
-        CancelOrder(id);
+
+
+    function drainEther() isCreator() returns (bool success){
+      require(!exchangeState);
+      if ( creator.send(this.balance) ) {
+        EthFundTransfer(this.balance);
+        return true;
+      }
+      return false;
     }
-    
-    function () external payable {
-        require(msg.value > 0);
-        uint fee = msg.value * feeDeposit / 10000;
-        require(msg.value > fee);
-        balanceAdd(0x0, owner, fee);
-        
-        uint toAdd = msg.value - fee;
-        balanceAdd(0x0, msg.sender, toAdd);
-        
-        Deposit(0x0, msg.sender, toAdd);
-        BalanceChanged(0x0, msg.sender, balanceOf[0x0][msg.sender]);
-        
-        FundTransfer(msg.sender, toAdd, true);
+
+
+    function drainTokens() isCreator() returns (bool success){
+      require(!exchangeState);
+      if ( tokenExchanged.transfer(creator, tokenExchanged.balanceOf(this) ) ) {
+        TokenFundTransfer(this.balance);
+        return true;
+      }
+      return false;
     }
-    
-    function depositToken(address tokenAddr, uint amount) external {
-        require(tokenAddr != 0x0);
-        require(amount > 0);
-        Token(tokenAddr).transferFrom(msg.sender, this, amount);
-        balanceAdd(tokenAddr, msg.sender, amount);
-        
-        Deposit(tokenAddr, msg.sender, amount);
-        BalanceChanged(tokenAddr, msg.sender, balanceOf[tokenAddr][msg.sender]);
+
+
+    function removeContract() public isCreator() {
+        require(!exchangeState);
+        selfdestruct(msg.sender);
     }
-    
-    function withdrawEther(uint amount) external {
-        require(amount > 0);
-        balanceSub(0x0, msg.sender, amount);
-        msg.sender.transfer(amount);
-        
-        Withdraw(0x0, msg.sender, amount);
-        BalanceChanged(0x0, msg.sender, balanceOf[0x0][msg.sender]);
-        
-        FundTransfer(msg.sender, amount, false);
-    }
-    
-    function withdrawToken(address tokenAddr, uint amount) external {
-        require(tokenAddr != 0x0);
-        require(amount > 0);
-        balanceSub(tokenAddr, msg.sender, amount);
-        Token(tokenAddr).transfer(msg.sender, amount);
-        
-        Withdraw(tokenAddr, msg.sender, amount);
-        BalanceChanged(tokenAddr, msg.sender, balanceOf[tokenAddr][msg.sender]);
-    }
+
 }
