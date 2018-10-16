380e6e2b0ac7e0e42752578beba4dfe6b3875b16
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:16:25 +0700

    add contract CaruTokenSender

---
+++ CaruTokenSender.sol
@@ -0,0 +1,36 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CaruTokenSender at 0x02878a096847c755049abc937e84ae8f49b69285
+*/
+pragma solidity ^0.4.24;
+
+contract Ownable {
+    address public owner;
+
+    constructor () public {
+        owner = 0xCfBbef59AC2620271d8C8163a294A04f0b31Ef3f;
+    }
+
+     modifier onlyOwner() {
+    if (msg.sender != owner) {
+      revert();
+    }
+    _;
+}
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
+
+contract TokenERC20 {
+  function transfer(address _to, uint256 _value) public;
+}
+
+contract CaruTokenSender is Ownable {
+
+    function drop(TokenERC20 token, address[] to, uint256[] value) onlyOwner public {
+    for (uint256 i = 0; i < to.length; i++) {
+      token.transfer(to[i], value[i]);
+    }
+  }
+}
