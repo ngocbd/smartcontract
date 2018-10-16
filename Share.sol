1b0ef5546b458dd948d322bd235e46126a8c0790
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:57:11 +0700

    add contract Share

--- Share.sol
+++ Share.sol
@@ -1,371 +1,188 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Share at 0x6f69ef58ddec9cd6ee428253c607a0acd13da05f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Share at 0x1db45a09efcdd8955b1c3bb855b5a8d333446bff
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
 /**
- * @author OpenZeppelin
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
+ * this contract stands for the holds of WestIndia group
+ * all income will be split to holders according to their holds
+ * user can buy holds from shareholders at his will
  */
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+contract Share {
 
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
-pragma solidity ^0.4.11;
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
+    bool public pause;
+    /**
+     * owner can pause the contract so that no one can withdrawal
+     * he can't do anything else
+     */
     address public owner;
-
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
+    
+    /**
+     * the holds of every holder
+     * the total holds stick to 10000
+     */
+    mapping (address => uint) public holds;
 
     /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    function Ownable()
-        public
-    {
-        owner = msg.sender;
-    }
+     * since we don't record holders' address in a list
+     * and we don't want to loop holders list everytime when there is income
+     *
+     * we use a mechanism called 'watermark'
+     * 
+     * the watermark indicates the value that brought into each holds from the begining
+     * it only goes up when new income send to the contract
 
+     * fullfilled indicate the amount that the holder has withdrawaled from his share
+     * it goes up when user withdrawal bonus
+     * and it goes up when user sell holds, goes down when user buy holds, since the total bonus of him stays the same.
+     */
+    mapping (address => uint256) public fullfilled;
 
     /**
-     * @dev Throws if called by any account other than the owner.
+     * any one can setup a price to sell his holds
+     * if set to 0, means not on sell
      */
+    mapping (address => uint256) public sellPrice;
+    mapping (address => uint) public toSell;
+
+    uint256 public watermark;
+
+    event PAUSED();
+    event STARTED();
+
+    event SHARE_TRANSFER(address from, address to, uint amount);
+    event INCOME(uint256);
+    event PRICE_SET(address holder, uint shares, uint256 price, uint sell);
+    event WITHDRAWAL(address owner, uint256 amount);
+    event SELL_HOLDS(address from, address to, uint amount, uint256 price);
+    event SEND_HOLDS(address from, address to, uint amount);
+
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-
-
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function transferOwnership(
-        address newOwner
-    )
-        onlyOwner
-        public
-    {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-}
-
-pragma solidity ^0.4.8;
-contract Token {
-    /* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
-    uint256 public totalSupply;
-    This automatically creates a getter function for the totalSupply.
-    This is moved to the base contract since public getter functions are not
-    currently recognised as an implementation of the matching abstract
-    function by the compiler.
-    */
-    /// total amount of tokens
-    uint256 public totalSupply;
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-pragma solidity ^0.4.8;
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-}
-
-pragma solidity ^0.4.8;
-contract HumanStandardToken is StandardToken {
-
-    /* Public variables of the token */
-
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-    function HumanStandardToken(
-        uint256 _initialAmount,
-        string _tokenName,
-        uint8 _decimalUnits,
-        string _tokenSymbol
-        ) {
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-        totalSupply = _initialAmount;                        // Update total supply
-        name = _tokenName;                                   // Set the name for display purposes
-        decimals = _decimalUnits;                            // Amount of decimals for display purposes
-        symbol = _tokenSymbol;                               // Set the symbol for display purposes
-    }
-
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
-        return true;
-    }
-}
-
-pragma solidity ^0.4.17;
-/**
- * SHARE token is an ERC20 token.
- */
-contract Share is HumanStandardToken, Ownable {
-    using SafeMath for uint;
-
-    string public constant TOKEN_NAME = "Vyral Token";
-
-    string public constant TOKEN_SYMBOL = "SHARE";
-
-    uint8 public constant TOKEN_DECIMALS = 18;
-
-    uint public constant TOTAL_SUPPLY = 777777777 * (10 ** uint(TOKEN_DECIMALS));
-
-    mapping (address => uint256) lockedBalances;
-
-    mapping (address => bool) public transferrers;
-
-    /**
-     * Init this contract with the same params as a HST.
-     */
-    function Share() HumanStandardToken(TOTAL_SUPPLY, TOKEN_NAME, TOKEN_DECIMALS, TOKEN_SYMBOL)
-        public
-    {
-        transferrers[msg.sender] = true;
-    }
-
-    ///-----------------
-    /// Overrides
-    ///-----------------
-
-    /// Off on deployment.
-    bool isTransferable = false;
-
-    /// Bonus tokens are locked on deployment
-    bool isBonusLocked = true;
-
-    /// Allows the owner to transfer tokens whenever, but others to only transfer after owner says so.
-    modifier canBeTransferred {
-        require(transferrers[msg.sender] || isTransferable);
+    
+    modifier notPaused() {
+        require(!pause);
         _;
     }
-
-    function transferReward(
-        address _to,
-        uint _value
-    )
-        canBeTransferred
-        public
-        returns (bool)
-    {
-        require(balances[msg.sender] >= _value);
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-
-        lockedBalances[_to] = lockedBalances[_to].add(_value);
-
-        Transfer(msg.sender, _to, _value);
-        return true;
+    
+    function setState(bool _pause) onlyOwner public {
+        pause = _pause;
+        
+        if (_pause) {
+            emit PAUSED();
+        } else {
+            emit STARTED();
+        } 
     }
-
-    function transfer(
-        address _to,
-        uint _value
-    )
-        canBeTransferred
-        public
-        returns (bool)
-    {
-        require(balances[msg.sender] >= _value);
-
-        /// Only transfer unlocked balance
-        if(isBonusLocked) {
-            require(balances[msg.sender].sub(lockedBalances[msg.sender]) >= _value);
-        }
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint _value
-    )
-        canBeTransferred
-        public
-        returns (bool)
-    {
-        require(balances[_from] >= _value);
-        require(allowed[_from][msg.sender] >= _value);
-
-        /// Only transfer unlocked balance
-        if(isBonusLocked) {
-            require(balances[_from].sub(lockedBalances[_from]) >= _value);
-        }
-
-        allowed[_from][msg.sender] = allowed[_from][_to].sub(_value);
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function lockedBalanceOf(
-        address _owner
-    )
-        constant
-        returns (uint)
-    {
-        return lockedBalances[_owner];
-    }
-
-    ///-----------------
-    /// Admin
-    ///-----------------
-
-    function enableTransfers()
-        onlyOwner
-        external
-        returns (bool)
-    {
-        isTransferable = true;
-
-        return isTransferable;
-    }
-
-    function addTransferrer(
-        address _transferrer
-    )
-        public
-        onlyOwner
-    {
-        transferrers[_transferrer] = true;
-    }
-
 
     /**
-     * @dev Allow bonus tokens to be withdrawn
+     * at start the owner has 100% share, which is 10,000 holds
      */
-    function releaseBonus()
-        public
-        onlyOwner
-    {
-        isBonusLocked = false;
+    function Share() public {        
+        owner = msg.sender;
+        holds[owner] = 10000;
+        pause = false;
     }
 
+    /**
+     * when there's income, the water mark goes up
+     */
+    function onIncome() public payable {
+        if (msg.value > 0) {
+            watermark += (msg.value / 10000);
+            assert(watermark * 10000 > watermark);
+
+            emit INCOME(msg.value);
+        }
+    }
+
+    /**
+     * automatically split income
+     */
+    function() public payable {
+        onIncome();
+    }
+
+    function bonus() public view returns (uint256) {
+        return (watermark - fullfilled[msg.sender]) * holds[msg.sender];
+    }
+    
+    function setPrice(uint256 price, uint sell) public notPaused {
+        sellPrice[msg.sender] = price;
+        toSell[msg.sender] = sell;
+        emit PRICE_SET(msg.sender, holds[msg.sender], price, sell);
+    }
+
+    /**
+     * withdrawal the bonus
+     */
+    function withdrawal() public notPaused {
+        if (holds[msg.sender] == 0) {
+            //you don't have any, don't bother
+            return;
+        }
+        uint256 value = bonus();
+        fullfilled[msg.sender] = watermark;
+
+        msg.sender.transfer(value);
+
+        emit WITHDRAWAL(msg.sender, value);
+    }
+
+    /**
+     * transfer holds from => to (only holds, no bouns)
+     * this will withdrawal the holder bonus of these holds
+     * and the to's fullfilled will go up, since total bonus unchanged, but holds goes more
+     */
+    function transferHolds(address from, address to, uint amount) internal {
+        require(holds[from] >= amount);
+        require(amount > 0);
+
+        uint256 fromBonus = (watermark - fullfilled[from]) * amount;
+        uint256 toBonus = (watermark - fullfilled[to]) * holds[to];
+        
+
+        holds[from] -= amount;
+        holds[to] += amount;
+        fullfilled[to] = watermark - toBonus / holds[to];
+
+        from.transfer(fromBonus);
+
+        emit SHARE_TRANSFER(from, to, amount);
+        emit WITHDRAWAL(from, fromBonus);
+    }
+
+    /**
+     * one can buy holds from anyone who set up an price,
+     * and u can buy @ price higher than he setup
+     */
+    function buyFrom(address from) public payable notPaused {
+        require(sellPrice[from] > 0);
+        uint256 amount = msg.value / sellPrice[from];
+
+        if (amount >= holds[from]) {
+            amount = holds[from];
+        }
+
+        if (amount >= toSell[from]) {
+            amount = toSell[from];
+        }
+
+        require(amount > 0);
+
+        toSell[from] -= amount;
+        transferHolds(from, msg.sender, amount);
+        from.transfer(msg.value);
+        
+        emit SELL_HOLDS(from, msg.sender, amount, sellPrice[from]);
+    }
+    
+    function transfer(address to, uint amount) public notPaused {
+        require(holds[msg.sender] >= amount);
+        transferHolds(msg.sender, to, amount);
+        
+        emit SEND_HOLDS(msg.sender, to, amount);
+    }
 }
