615645557451f332bf29882cd8b497bda8362b58
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:03:33 +0700

    add contract TGE

--- TGE.sol
+++ TGE.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TGE at 0x2212efd06e557aea1bbbbcabe5ebb747194a9523
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TGE at 0x33ee4dbe891859811958c43e05c09cff084b0a03
 */
 pragma solidity ^0.4.18;
 
@@ -248,7 +248,7 @@ contract LightcashCryptoToken is StandardToken, Ownable {
 
   string public constant name = 'Lightcash crypto';
 
-  string public constant symbol = 'LCSH';
+  string public constant symbol = 'LCCT';
 
   uint32 public constant decimals = 18;
 
@@ -665,21 +665,21 @@ contract Deployer is Ownable {
     token = new LightcashCryptoToken();
 
     preTGE = new PreTGE();
-    preTGE.setPrice(7143000000000000000000);
-    preTGE.setMinPurchaseLimit(100000000000000000);
-    preTGE.setSoftcap(7142857000000000000000000);
-    preTGE.setHardcap(52500000000000000000000000);
-    preTGE.setStart(1517230800);
+    preTGE.setPrice(7143000000000000000000); // 1 ETH = 7143 LCCT
+    preTGE.setMinPurchaseLimit(100000000000000000); // 0.1 ETH
+    preTGE.setSoftcap(7000000000000000000000000); // 7 000 000 LCCT
+    preTGE.setHardcap(52500000000000000000000000); // 52 000 000 LCCT
+    preTGE.setStart(1519995600); // Fri Mar 02 2018 16:00:00 GMT+0300
     preTGE.setPeriod(11);
     preTGE.setWallet(0xDFDCAc0c9Eb45C63Bcff91220A48684882F1DAd0);
-    preTGE.setMaxReferrerTokens(10000000000000000000000);
+    preTGE.setMaxReferrerTokens(10000000000000000000000); // 10 000 LCCT
     preTGE.setReferrerPercent(10);
 
     tge = new TGE();
-    tge.setPrice(5000000000000000000000);
-    tge.setMinPurchaseLimit(10000000000000000);
-    tge.setHardcap(126000000000000000000000000);
-    tge.setStart(1517835600);
+    tge.setPrice(5000000000000000000000); // 5 000 LCCT
+    tge.setMinPurchaseLimit(10000000000000000); // 0.01 ETH
+    tge.setHardcap(126000000000000000000000000); // 126 000 000 LCCT
+    tge.setStart(1520859600); // Mar 12 2018 16:00:00 GMT+0300
     tge.setWallet(0x3aC45b49A4D3CB35022fd8122Fd865cd1B47932f);
     tge.setExtraTokensWallet(0xF0e830148F3d1C4656770DAa282Fda6FAAA0Fe0B);
     tge.setExtraTokensPercent(15);
@@ -687,7 +687,7 @@ contract Deployer is Ownable {
     tge.addStage(7, 15);
     tge.addStage(7, 10);
     tge.addStage(1000, 5);
-    tge.setMaxReferrerTokens(10000000000000000000000);
+    tge.setMaxReferrerTokens(10000000000000000000000); // 10 000 LCCT
     tge.setReferrerPercent(10);
 
     preTGE.setToken(token);
