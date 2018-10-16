1c53b92186f4d8b8bd526feecf4910f1838047e8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:54:25 +0700

    add contract store

--- store.sol
+++ store.sol
@@ -1,13 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract store at 0xd39fe1cffd8f070429169b416b7e07f486d553cf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract store at 0x1c94f41ab9cd8faa8af45ae7827bf5a8f891f4cd
 */
+/* A contract to store a list of messages. Obtainable as events. */
+
 contract store {
 
     address owner;
 
-    uint public contentCount = 0;
+    uint16 public contentCount = 0;
     
-    event content(string datainfo, uint indexed version, address indexed sender, uint indexed datatype, uint timespan, uint payment);
+    event content(string datainfo, address sender, uint payment);
     modifier onlyowner { if (msg.sender == owner) _ } 
     
     function store() public { owner = msg.sender; }
@@ -19,26 +21,8 @@ contract store {
         owner.send(this.balance);
     }
 
-    function add(string datainfo, uint version, uint datatype, uint timespan) {
-        //item listing
-        if(datatype == 1) {
-          //2 weeks listing costs 0,04 USD = 0,004 ether
-          if(timespan <= 1209600) {
-            if(msg.value < (4 finney)) return;
-          //4 weeks listing costs 0,06 USD = 0,006 ether
-          } else if(timespan <= 2419200) {
-            if(msg.value < (6 finney)) return;
-          //limit 4 weeks max
-          } else {
-            timespan = 2419200;
-            if(msg.value < (6 finney)) return;
-          }
-        }
-
-        //revert higher payment transactions
-        if(msg.value > (6 finney)) throw;
-
+    function add(string datainfo) {
         contentCount++;
-        content(datainfo, version, msg.sender, datatype, timespan, msg.value);
+        content(datainfo, msg.sender, msg.value);
     }
 }
