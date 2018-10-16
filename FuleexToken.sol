7b3e846048db9e4a9b60eb135b9e2c37be80aae4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:28:35 +0700

    add contract FuleexToken

--- FuleexToken.sol
+++ FuleexToken.sol
@@ -1,46 +1,94 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FuleexToken at 0xc77fd023641bd3060776b02000e56b598dfe40c1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FuleexToken at 0x4c983ae4e3f799dcaab554ed0414ec5a1f24cf45
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.4;
 
-contract FuleexToken {
-    /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public initialSupply;
+contract Token {
+
+    function totalSupply() constant returns (uint256 supply) {}
+    
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+}
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
     uint256 public totalSupply;
+}
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+contract FuleexToken is StandardToken {
 
-  
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function Token() public {
-
-         initialSupply = 300000000;
-         name ="FuleexToken";
-         decimals = 18;
-         symbol = "FEXT";
-        
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-                                   
+    function () {
+        //if ether is sent to this address, send it back.
+        throw;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) public {
-        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-      
+    /* Public variables of the token */
+
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+    function FuleexToken(
+        ) {
+        balances[msg.sender] = 300000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 300000000;                        // Update total supply (100000 for example)
+        name = "FuleexToken";                                   // Set the name for display purposes
+        decimals = 18;                            // Amount of decimals for display purposes
+        symbol = "FEXT";                               // Set the symbol for display purposes
     }
 
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function () public {
-        revert();     // Prevents accidental sending of ether
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        return true;
     }
 }
