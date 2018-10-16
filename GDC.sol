531a887ba81a7d10ec295a04c14ddc0a6a449fc8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:13:36 +0700

    add contract GDC

--- GDC.sol
+++ GDC.sol
@@ -1,153 +1,136 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GDC at 0x36aff6fe8ffe81844bba32427f622af6c47eab66
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GDC at 0x841b42c7ee6a15904d1926bf15ffdb6d30a77723
 */
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.16;
 
-contract SafeMath {
-    //SafeAdd ????????ERC20??function
-    function safeAdd(uint256 x, uint256 y) internal returns(uint256) {
-      uint256 z = x + y;
-      assert((z >= x) && (z >= y));
-      return z;
-    }
-
-    //SafeSubtract ????????ERC20??function
-    function safeSubtract(uint256 x, uint256 y) internal returns(uint256) {
-      assert(x >= y);
-      uint256 z = x - y;
-      return z;
-    }
-
-    //SafeMult ????????ERC20??function
-    function safeMult(uint256 x, uint256 y) internal returns(uint256) {
-      uint256 z = x * y;
-      assert((x == 0)||(z/x == y));
-      return z;
-    }
-
-}
-
-// Token?????????????????
-contract Token {
-    //totalSupply: ????
+contract GDC {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 8;
+    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
-    //balanceOf: ???????GDC??
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    //transfer: GDC???????
-    function transfer(address _to, uint256 _value) returns (bool success);
-    //transferFrom: ?GDC????????????
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    //approve: ???A???B????????approve, ????transferFrom
-    function approve(address _spender, uint256 _value) returns (bool success);
-    //allowance???Approve?????
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    //Transfer?????????????????????
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    //Approval??????approve?????????Approval??
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
 
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-/*  ERC 20 token */
-contract StandardToken is Token {
-    //?????????????????
-    function transfer(address _to, uint256 _value) returns (bool success) {
-      if (balances[msg.sender] >= _value && _value > 0) {
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    // ???A???B???????????A??????B?????????
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
-    //???????????
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-    //?????????????
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-    // ???????????????
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-    // ????????????????????
-    mapping (address => uint256) balances;
-    // ??????????A?????B?????????
-    mapping (address => mapping (address => uint256)) allowed;
-}
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
 
-// GDC??????ERC20?????
-contract GDC is StandardToken, SafeMath {
-    string public constant name = "GDC"; //?????GDC
-    string public constant symbol = "GDC"; //?????GDC
-    uint256 public constant decimals = 18; //??????18?
-    string public version = "1.0"; //???? 1.0
-
-    address  public GDCAcc01;  //GDC?????1
-    address  public GDCAcc02;  //GDC?????2
-    address  public GDCAcc03;  //GDC?????3
-    address  public GDCAcc04;  //GDC?????4
-    address  public GDCAcc05;  //GDC?????5
-
-    uint256 public constant factorial = 6; //??????????GDC?????
-    uint256 public constant GDCNumber1 = 200 * (10**factorial) * 10**decimals; //GDCAcc1?????200M??2???
-    uint256 public constant GDCNumber2 = 200 * (10**factorial) * 10**decimals; //GDCAcc2?????200M??2???
-    uint256 public constant GDCNumber3 = 200 * (10**factorial) * 10**decimals; //GDCAcc3?????200M??2???
-    uint256 public constant GDCNumber4 = 200 * (10**factorial) * 10**decimals; //GDCAcc4?????200M??2???
-    uint256 public constant GDCNumber5 = 200 * (10**factorial) * 10**decimals; //GDCAcc5?????200M??2???
-
-
-    // ?????????????????????????2?GDC??
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
     function GDC(
-      address _GDCAcc01,
-      address _GDCAcc02,
-      address _GDCAcc03,
-      address _GDCAcc04,
-      address _GDCAcc05
-    )
-    {
-      totalSupply = 1000 * (10**factorial) * 10**decimals; // ???????10?
-      GDCAcc01 = _GDCAcc01;
-      GDCAcc02 = _GDCAcc02;
-      GDCAcc03 = _GDCAcc03;
-      GDCAcc04 = _GDCAcc04;
-      GDCAcc05 = _GDCAcc05;
-
-      balances[GDCAcc01] = GDCNumber1;
-      balances[GDCAcc02] = GDCNumber2;
-      balances[GDCAcc03] = GDCNumber3;
-      balances[GDCAcc04] = GDCNumber4;
-      balances[GDCAcc05] = GDCNumber5;
-
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
-    // transferLock ????????flag?true??????????????????
-    function transferLock(address _to, uint256 _value, bool flag) returns (bool success) {
-      if (balances[msg.sender] >= _value && _value > 0 && flag) {
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
         return true;
-      } else {
-        return false;
-      }
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
     }
 }
