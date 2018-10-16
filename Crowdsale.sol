5ffb84995652a046894061fd0e9800d2a37704cb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:48:00 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x4420681bda548cddb90d0e2419b9c52ec2ca7c9f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x0fe61116f781f26a67d8618bd11abf2de00b6f0f
 */
 pragma solidity ^0.4.16;
 
@@ -209,20 +209,19 @@ contract BurnableToken is StandardToken {
 
 }
 
-contract Misscoin is BurnableToken {
+contract SimpleCoinToken is BurnableToken {
     
-  string public constant name = "Misscoin";
+  string public constant name = "Scam Coin token";
    
-  string public constant symbol = "MISC";
+  string public constant symbol = "SCC";
     
   uint32 public constant decimals = 18;
 
-  uint256 public INITIAL_SUPPLY = 1000000000 * 1 ether;
+  uint256 public INITIAL_SUPPLY = 380000 * 1 ether;
 
-  function Misscoin() {
+  function SimpleCoinToken() {
     totalSupply = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
-    balances[0x49B25aDDdd6503d275375C7c261A444862360396]=150000000 * 1 ether;
   }
     
 }
@@ -233,135 +232,51 @@ contract Crowdsale is Ownable {
     
   address multisig;
 
+  uint restrictedPercent;
+
   address restricted;
 
-  bool addtok=false;
-
-  Misscoin public token = new Misscoin();
+  SimpleCoinToken public token = new SimpleCoinToken();
 
   uint start;
     
   uint period;
 
-  uint128 constant WAD = 10 ** 18;
+  uint rate;
 
-  mapping (uint => mapping (address => uint))  public  userBuys;
-  mapping (uint => uint)                       public  dailyTotals;
-  mapping (uint => mapping (address => bool))  public  claimed;
-
-   function Crowdsale() {
-      multisig = 0x49B25aDDdd6503d275375C7c261A444862360396;
-      restricted  = 0x49B25aDDdd6503d275375C7c261A444862360396;
-      start = 1512741600;
-      period = 150;
-    }
+  function Crowdsale() {
+    multisig = 0x157A032C79557103b561996890e32fede87D469D;
+    restricted = 0x99BBCe354a39B72A0d07e47574B038B9096Ac441;
+    restrictedPercent = 20;
+    rate = 1250*1 ether;
+    start = 1513350915;
+    period = 20;
+  }
 
   modifier saleIsOn() {
     require(now > start && now < start + period * 1 days);
     _;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-  function wmul(uint128 x, uint128 y) constant internal returns (uint128 z) {
-        z = cast((uint256(x) * y + WAD / 2) / WAD);
+  function createTokens() saleIsOn payable {
+    multisig.transfer(msg.value);
+    uint tokens = rate.mul(msg.value).div(1 ether);
+    uint bonusTokens = 0;
+     if(now <= start + 1 days) {
+      bonusTokens = tokens;
+    } else if(now > start + 1 days && now < start + 3 days) {
+      bonusTokens = tokens.div(2);
+    } else if(now >= start + 3 days && now < start + 7 days) {
+      bonusTokens = tokens.div(10);
     }
-
-    function wdiv(uint128 x, uint128 y) constant internal returns (uint128 z) {
-        z = cast((uint256(x) * WAD + y / 2) / y);
-    }
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-   function cast(uint256 x) constant internal returns (uint128 z) {
-        assert((z = uint128(x)) == x);
+    uint tokensWithBonus = tokens.add(bonusTokens);
+    token.transfer(msg.sender, tokensWithBonus);
+    uint restrictedTokens = tokens.mul(restrictedPercent).div(100 - restrictedPercent);
+    token.transfer(restricted, restrictedTokens);
   }
 
-  function time() constant returns (uint) {
-        return block.timestamp;
+  function() external payable {
+    createTokens();
   }
-
-  function today() constant returns (uint) {
-        return dayFor(time());
-  }
-
-  function dayFor(uint timestamp) constant returns (uint) {
-
-        return timestamp < start
-            ? 0
-            : sub(timestamp, start) / 24 hours + 1;
-  }
-
-  function buyWithLimit(uint day, uint limit) payable saleIsOn {
-        assert(time() >= start && today() <= period);
-        assert(msg.value >= 0.001 ether);
-        
-        assert(day >= today());
-        assert(day <= period);
-
-        userBuys[day][msg.sender] += msg.value;
-        dailyTotals[day] += msg.value;
-
-        if (limit != 0) {
-            assert(dailyTotals[day] <= limit);
-        }
-
-  }
-
-    function addtokens() onlyOwner{
-      assert(today() >= 149 && !addtok);
-      token.transfer(0x49B25aDDdd6503d275375C7c261A444862360396, 100000000 * 1 ether);
-      addtok=true;
-    }
-
-    function buy() payable {
-       buyWithLimit(today(), 0);
-    }
-
-    function () payable {
-       buy();
-    }
-  
-  function claim(uint day) saleIsOn {
-        assert(today() > day);
-
-        if (claimed[day][msg.sender] || dailyTotals[day] == 0) {
-            return;
-        }
-
-       
-
-        var dailyTotal = cast(dailyTotals[day]);
-        var userTotal  = cast(userBuys[day][msg.sender]);
-        var price      = wdiv(cast(5000000), dailyTotal);
-        var reward     = wmul(price, userTotal);
-
-        claimed[day][msg.sender] = true;
-        token.transfer(msg.sender, reward * 1 ether);
-
-  } 
-
-  function claimAll() {
-        for (uint i = 0; i < today(); i++) {
-            claim(i);
-        }
-  }
-
-  function collect() onlyOwner{
-        assert(today() > 0); // Prevent recycling during window 0
-        multisig.transfer(this.balance);
-  }
-
     
 }
