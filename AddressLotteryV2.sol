58f7e0512237f61b67fc51979e903be907245ff2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:25:42 +0700

    add contract AddressLotteryV2

--- AddressLotteryV2.sol
+++ AddressLotteryV2.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressLotteryV2 at 0xe19ca313512e0231340e778abe7110401c737c23
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AddressLotteryV2 at 0x787b9a8978b21476abb78876f24c49c0e513065e
 */
 pragma solidity ^0.4.0;
 /*
@@ -23,9 +23,11 @@ contract AddressLotteryV2{
     uint private secretSeed;
     uint private lastReseed;
     
-    uint winnerLuckyNumber = 7;
     
-    uint public ticketPrice = 0.1 ether;
+    // LUCKYNUMBER  = 7
+    uint W1NNERLUCK1NUMBERFORWINNINGTHELOTTERY = 7;
+    
+    uint public ticketPrice = 0.5 ether;
         
     mapping (address => bool) participated;
 
@@ -48,13 +50,15 @@ contract AddressLotteryV2{
         ticketPrice = newPrice;
     }
     
+    
+    
     function participate() payable onlyHuman { 
         require(msg.value == ticketPrice);
         
         // every address can only win once, obviously
         require(!participated[msg.sender]);
         
-        if ( luckyNumberOfAddress(msg.sender) == winnerLuckyNumber)
+        if ( luckyNumberOfAddress(msg.sender) == W1NNERLUCK1NUMBERF0RWINNINGTHELOTTERY)
         {
             participated[msg.sender] = true;
             require(msg.sender.call.value(this.balance)());
@@ -80,18 +84,21 @@ contract AddressLotteryV2{
         suicide(owner);
     }
     
-    function forceReseed() onlyOwner{
+    function forceReseed() {
         SeedComponents s;
-        s.component1 = uint(msg.sender);
+        s.component1 = uint(owner);
         s.component2 = uint256(block.blockhash(block.number - 1));
         s.component3 = block.number * 1337;
         s.component4 = tx.gasprice * 7;
         reseed(s);
     }
     
+    uint W1NNERLUCK1NUMBERF0RWINNINGTHELOTTERY = 0x12345678;
+    
     function () payable {}
     
     // DEBUG, DELETE BEFORE DEPLOYMENT!!
+    // LUCKY NUMBER 7
     function _myLuckyNumber() constant returns(uint n){
         n = luckyNumberOfAddress(msg.sender);
     }
