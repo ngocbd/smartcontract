1b19f713a2ce67835445011670b70dd6570a5c39
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:24:34 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,216 +1,304 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x0a7d11ea2308f80eb239f2e4c77715725ae8650d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xbf4a29269bf3a5c351c2af3a9c9ed81b07129ce4
 */
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.15;
 
-// ?????
-contract Owned {
-    address public owner;
-    function Owned() public {
-        owner = msg.sender;
+/*
+    ERC20 Standard Token interface
+*/
+contract IERC20Token {
+    // these functions aren't abstract since the compiler emits automatically generated getter functions as external
+    function name() public constant returns (string) { name; }
+    function symbol() public constant returns (string) { symbol; }
+    function decimals() public constant returns (uint8) { decimals; }
+    function totalSupply() public constant returns (uint256) { totalSupply; }
+    function balanceOf(address _owner) public constant returns (uint256 balance) { _owner; balance; }
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) { _owner; _spender; remaining; }
+
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+}
+
+/*
+    Utilities & Common Modifiers
+*/
+contract Utils {
+    /**
+        constructor
+    */
+    function Utils() {
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
+    // validates an address - currently only checks that it isn't null
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner public {
+    // verifies that the address is different than this contract address
+    modifier notThis(address _address) {
+        require(_address != address(this));
+        _;
+    }
+
+    // Overflow protected math functions
+
+    /**
+        @dev returns the sum of _x and _y, asserts if the calculation overflows
+
+        @param _x   value 1
+        @param _y   value 2
+
+        @return sum
+    */
+    function safeAdd(uint256 _x, uint256 _y) internal returns (uint256) {
+        uint256 z = _x + _y;
+        assert(z >= _x);
+        return z;
+    }
+
+    /**
+        @dev returns the difference of _x minus _y, asserts if the subtraction results in a negative number
+
+        @param _x   minuend
+        @param _y   subtrahend
+
+        @return difference
+    */
+    function safeSub(uint256 _x, uint256 _y) internal returns (uint256) {
+        assert(_x >= _y);
+        return _x - _y;
+    }
+
+    /**
+        @dev returns the product of multiplying _x by _y, asserts if the calculation overflows
+
+        @param _x   factor 1
+        @param _y   factor 2
+
+        @return product
+    */
+    function safeMul(uint256 _x, uint256 _y) internal returns (uint256) {
+        uint256 z = _x * _y;
+        assert(_x == 0 || z / _x == _y);
+        return z;
+    }
+}
+
+/*
+    Owned contract interface
+*/
+contract IOwned {
+    // this function isn't abstract since the compiler emits automatically generated getter functions as external
+    function owner() public constant returns (address) { owner; }
+
+    function transferOwnership(address _newOwner) public;
+    function acceptOwnership() public;
+}
+
+/*
+    Provides support and utilities for contract ownership
+*/
+contract Owned is IOwned {
+    address public owner;
+    address public newOwner;
+
+    event OwnerUpdate(address _prevOwner, address _newOwner);
+
+    /**
+        @dev constructor
+    */
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    // allows execution by the owner only
+    modifier ownerOnly {
+        assert(msg.sender == owner);
+        _;
+    }
+
+    /**
+        @dev allows transferring the contract ownership
+        the new owner still needs to accept the transfer
+        can only be called by the contract owner
+
+        @param _newOwner    new contract owner
+    */
+    function transferOwnership(address _newOwner) public ownerOnly {
+        require(_newOwner != owner);
+        newOwner = _newOwner;
+    }
+
+    /**
+        @dev used by a new owner to accept an ownership transfer
+    */
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnerUpdate(owner, newOwner);
         owner = newOwner;
+        newOwner = 0x0;
     }
 }
 
+/*
+    Token Holder interface
+*/
+contract ITokenHolder is IOwned {
+    function withdrawTokens(IERC20Token _token, address _to, uint256 _amount) public;
+}
 
-interface tokenRecipient { function receiveApproval(address _from, uint _value, address _token, bytes _extraData) public; }
+/*
+    We consider every contract to be a 'token holder' since it's currently not possible
+    for a contract to deny receiving tokens.
 
-// ??????
-contract TokenBase is Owned {
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
-    uint public totalSupply;
-    uint public tokenUnit = 10 ** uint(decimals);
-    uint public wanUnit = 10000 * tokenUnit;
-    uint public foundingTime;
-
-    mapping (address => uint) public balanceOf;
-    mapping (address => mapping (address => uint)) public allowance;
-
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-
-    function TokenBase() public {
-        foundingTime = now;
+    The TokenHolder's contract sole purpose is to provide a safety mechanism that allows
+    the owner to send tokens that were sent to the contract by mistake back to their sender.
+*/
+contract TokenHolder is ITokenHolder, Owned, Utils {
+    /**
+        @dev constructor
+    */
+    function TokenHolder() {
     }
 
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
+    /**
+        @dev withdraws tokens held by the contract and sends them to an account
+        can only be called by the owner
+
+        @param _token   ERC20 token contract address
+        @param _to      account to receive the new amount
+        @param _amount  amount to withdraw
+    */
+    function withdrawTokens(IERC20Token _token, address _to, uint256 _amount)
+        public
+        ownerOnly
+        validAddress(_token)
+        validAddress(_to)
+        notThis(_to)
+    {
+        assert(_token.transfer(_to, _amount));
+    }
+}
+
+/**
+    ERC20 Standard Token implementation
+*/
+contract ERC20Token is IERC20Token, Utils {
+    string public standard = "Token 0.1";
+    string public name = "";
+    string public symbol = "";
+    uint8 public decimals = 0;
+    uint256 public totalSupply = 0;
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    /**
+        @dev constructor
+
+        @param _name        token name
+        @param _symbol      token symbol
+        @param _decimals    decimal points, for display purposes
+    */
+    function ERC20Token(string _name, string _symbol, uint8 _decimals) {
+        require(bytes(_name).length > 0 && bytes(_symbol).length > 0); // validate input
+
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    /**
+        @dev send coins
+        throws on any error rather then return a false flag to minimize user errors
+
+        @param _to      target address
+        @param _value   transfer amount
+
+        @return true if the transfer was successful, false if it wasn't
+    */
+    function transfer(address _to, uint256 _value)
+        public
+        validAddress(_to)
+        returns (bool success)
+    {
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+        @dev an account/contract attempts to get the coins
+        throws on any error rather then return a false flag to minimize user errors
+
+        @param _from    source address
+        @param _to      target address
+        @param _value   transfer amount
+
+        @return true if the transfer was successful, false if it wasn't
+    */
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        validAddress(_from)
+        validAddress(_to)
+        returns (bool success)
+    {
+        allowance[_from][msg.sender] = safeSub(allowance[_from][msg.sender], _value);
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
         Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    function transfer(address _to, uint _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
-
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint _value) public returns (bool success) {
+    /**
+        @dev allow another account/contract to spend some tokens on your behalf
+        throws on any error rather then return a false flag to minimize user errors
+
+        also, to minimize the risk of the approve/transferFrom attack vector
+        (see https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/), approve has to be called twice
+        in 2 separate transactions - once to change the allowance to 0 and secondly to change it to the new allowance value
+
+        @param _spender approved address
+        @param _value   allowance amount
+
+        @return true if the approval was successful, false if it wasn't
+    */
+    function approve(address _spender, uint256 _value)
+        public
+        validAddress(_spender)
+        returns (bool success)
+    {
+        // if the allowance isn't 0, it can only be updated to 0 to prevent an allowance change immediately after withdrawal
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+
         allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    function approveAndCall(address _spender, uint _value, bytes _extraData) public returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
 }
 
-// ????????
-contract WorkProff is TokenBase {
-    uint public oneYear = 1 years;
-    uint public minerTotalSupply = 3900 * wanUnit;              // ???????
-    uint public minerTotalYears = 20;                           // ????????
-    uint public minerTotalTime = minerTotalYears * oneYear;     // ????????
-    uint public minerPreSupply = minerTotalSupply / 2;
-    uint public minerPreTime = 7 days;
-    uint public minerTotalReward = 0;                           // ??????
-    uint public minerTimeOfLastProof;                           // ?????????
-    uint public minerDifficulty = 10 ** 32;                     // ?????
-    bytes32 public minerCurrentChallenge;
+contract Token is ERC20Token, TokenHolder {
 
-    function WorkProff() public {
-        minerTimeOfLastProof = now;
-    }
-    
-    function proofOfWork(uint nonce) public {
-        require(minerTotalReward < minerTotalSupply);
-        bytes8 n = bytes8(sha3(nonce, minerCurrentChallenge));
-        require(n >= bytes8(minerDifficulty));
-
-        uint timeSinceLastProof = (now - minerTimeOfLastProof);
-        require(timeSinceLastProof >= 5 seconds);
+    function Token(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply, address _tokenHolder) ERC20Token(_name, _symbol, _decimals)
+    {
+        totalSupply = _totalSupply * 1 ether;
         
-        uint reward = 0;
-        uint difficuty = 0;
-        if (now - foundingTime < minerPreTime) {
-            reward = timeSinceLastProof * minerPreSupply / minerPreTime;
-            difficuty = 0;
-        } else {
-            reward = timeSinceLastProof * (minerTotalSupply - minerPreSupply) / minerTotalTime;
-            difficuty = minerDifficulty;
+        if (_tokenHolder == 0x0) {
+            _tokenHolder = msg.sender;
         }
-
-        balanceOf[msg.sender] += reward;
-        totalSupply += reward;
-        minerDifficulty = minerDifficulty * 10 minutes / timeSinceLastProof + 1;
-        minerTimeOfLastProof = now;
-        minerCurrentChallenge = sha3(nonce, minerCurrentChallenge, block.blockhash(block.number - 1));
-    }
-}
-
-// ????????
-contract Option is WorkProff {
-    uint public optionTotalSupply;                          // ?????
-    uint public optionInitialSupply = 6600 * wanUnit;       // ???????????4500 + 2100?
-    uint public optionTotalTimes = 5;                       // ????????
-    uint public optionExerciseSpan = 1 years;               // ???????
-
-    mapping (address => uint) public optionOf;              // ????????
-    mapping (address => uint) public optionExerciseOf;      // ??????????
-
-    event OptionTransfer(address indexed from, address indexed to, uint option, uint exercised);
-    event OptionExercise(address indexed addr, uint value);
-
-    function Option() public {
-        optionTotalSupply = optionInitialSupply;
-        optionOf[msg.sender] = optionInitialSupply;
-        optionExerciseOf[msg.sender] = 0;
+        owner = _tokenHolder;
+        balanceOf[_tokenHolder] = _totalSupply * 1 ether;
     }
 
-    function min(uint a, uint b) private returns (uint) {
-        return a < b ? a : b;
-    }
-
-    function _checkOptionExercise(uint option, uint exercised) internal returns (bool) {
-        uint canExercisedTimes = min(optionTotalTimes, (now - foundingTime) / optionExerciseSpan + 1);
-        return exercised <= option * canExercisedTimes / optionTotalTimes;
-    }
-
-    function _optionTransfer(address _from, address _to, uint _option, uint _exercised) internal {
-        require(_to != 0x0);
-        require(optionOf[_from] >= _option);
-        require(optionOf[_to] + _option > optionOf[_to]);
-        require(optionExerciseOf[_from] >= _exercised);
-        require(optionExerciseOf[_to] + _exercised > optionExerciseOf[_to]);
-        require(_checkOptionExercise(_option, _exercised));
-        require(_checkOptionExercise(optionOf[_from] - _option, optionExerciseOf[_from] - _exercised));
-
-        uint previousOptions = optionOf[_from] + optionOf[_to];
-        uint previousExercised = optionExerciseOf[_from] + optionExerciseOf[_to];
-        optionOf[_from] -= _option;
-        optionOf[_to] += _option;
-        optionExerciseOf[_from] -= _exercised;
-        optionExerciseOf[_to] += _exercised;
-        OptionTransfer(_from, _to, _option, _exercised);
-        assert(optionOf[_from] + optionOf[_to] == previousOptions);
-        assert(optionExerciseOf[_from] + optionExerciseOf[_to] == previousExercised);
-    }
-
-    function optionTransfer(address _to, uint _option, uint _exercised) public {
-        _optionTransfer(msg.sender, _to, _option, _exercised);
-    }
-
-    function optionExercise(uint value) public {
-        require(_checkOptionExercise(optionOf[msg.sender], optionExerciseOf[msg.sender] + value));
-        optionExerciseOf[msg.sender] += value;
-        balanceOf[msg.sender] += value;
-        totalSupply += value;
-        Transfer(0, this, value);
-        Transfer(this, msg.sender, value);
-        OptionExercise(msg.sender, value);
-    }
-}
-
-contract Token is Option {
-    uint public initialSupply = 0 * wanUnit;        // ?????
-    uint public reserveSupply = 10500 * wanUnit;    // ?????
-    uint public sellSupply = 9000 * wanUnit;        // ???ICO
-
-    function Token() public {
-        totalSupply = initialSupply;
-        balanceOf[msg.sender] = initialSupply;
-        name = "ZBC";
-        symbol = "ZBC";
-    }
-
-    function releaseReserve(uint value) onlyOwner public {
-        require(reserveSupply >= value);
-        balanceOf[owner] += value;
-        totalSupply += value;
-        reserveSupply -= value;
-        Transfer(0, this, value);
-        Transfer(this, owner, value);
-    }
-
-    function releaseSell(uint value) onlyOwner public {
-        require(sellSupply >= value);
-        balanceOf[owner] += value;
-        totalSupply += value;
-        sellSupply -= value;
-        Transfer(0, this, value);
-        Transfer(this, owner, value);
+    function deliverPresaleTokens(address[] _batchOfAddresses, uint256[] _amountOfTokens) public ownerOnly returns (bool success) {
+        for (uint256 i = 0; i < _batchOfAddresses.length; i++) {
+            transfer(_batchOfAddresses[i], _amountOfTokens[i]);            
+        }
+        return true;
     }
 }
