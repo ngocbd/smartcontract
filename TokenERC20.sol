a69d85432cc7d8bf7c22f40cbd494441f8eeefe9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:54:20 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,25 +1,17 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0xb70735a91c889c02f606179593c7cd669a5b1a97
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0xad1ff4873633c9248130fbfe28d17e1bd5467cc4
 */
 pragma solidity ^0.4.16;
+
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+
 contract TokenERC20 {
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
-
+    uint8 public decimals = 0;
+    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
-    //AnimatedProject
-    address public owner;
-    uint public exrate; //exchange radio 
-    bool public ifEndGetting;
-    //AnimatedProject
-    uint256 public bonusPool;
-    mapping (address => uint8) public bonusTimes; //record the times address has taken
-    uint8 public bonusNum;  //times of bonus distribution
-    uint256[30] public bonusPer; //ETH bonus per token, max 30 times
-
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
@@ -28,25 +20,21 @@ contract TokenERC20 {
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
 
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
     /**
      * Constrctor function
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
     function TokenERC20(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol,
-        uint exchangeRate
+        
     ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        //AnimatedProject
-        owner =  msg.sender;
-        exrate = exchangeRate;
-
+        totalSupply = 14999999 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = 14999999;                // Give the creator all initial tokens
+        name = "Ethereum Diamond Association" ;                                   // Set the name for display purposes
+        symbol = "EDA";                               // Set the symbol for display purposes
     }
 
     /**
@@ -131,66 +119,36 @@ contract TokenERC20 {
         }
     }
 
-
-    //AnimatedProject: Getting Token From Owner by ETH
-    function getToken  () public payable{
-        uint256 exvalue = msg.value;
-
-        if (!ifEndGetting &&
-            msg.sender != owner && 
-            exvalue > 0 && 
-            balanceOf[owner] >= exvalue * exrate){
-
-            //send ETH to the owner, send token to the sender
-            if (owner.send(exvalue)) {
-                _transfer(owner, msg.sender, exvalue * exrate);
-            } 
-        }
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
     }
 
-    //AnimatedProject: Destroy Contract Before Publiced
-    function owner_testEnd  () public {
-        if (msg.sender == owner &&
-            balanceOf[owner] > totalSupply * 4/5){
-            selfdestruct(owner);
-        }
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
     }
-
-    //AnimatedProject: End Getting Token
-    function owner_endGetting () public {
-        ifEndGetting = true;
-    }
-
-    //AnimatedProject: Send Bonus To Contract
-    function owner_bonusSend () public payable {
-        if (msg.sender == owner &&
-            bonusNum < 30){
-
-            bonusPool += msg.value;
-            bonusNum ++;
-            bonusPer[bonusNum] = msg.value/totalSupply;
-        }
-    }
-
-    //AnimatedProject: Take Bonus By Token
-    function bonusTake () public {
-
-        if (bonusTimes[msg.sender] < bonusNum){
-
-            uint256 sendCount;
-            address addrs = msg.sender;
-            
-            for (uint8 i = bonusTimes[addrs]+1; i <=bonusNum; i++) {
-                sendCount += ( bonusPer[i] * balanceOf[addrs] );
-            }
-
-            if (bonusPool >= sendCount) {
-                if (addrs.send(sendCount)){
-                    bonusPool -= sendCount;
-                    bonusTimes[addrs] ++;
-                }
-            }
-        }
-    }
-
 }
