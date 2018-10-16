d01cf989761fbd75c5b7203a2cbd5f1e1b1c8fb0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:38:09 +0700

    add contract Kether

--- Kether.sol
+++ Kether.sol
@@ -1,124 +1,153 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Kether at 0xEcb1De8b1a874F42abD321E982137D1fABeA0F93
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Kether at 0xb1ee0d2b6968a18bd4af29f9bf9bfcd9d9889d7b
 */
 pragma solidity ^0.4.8;
-    
-   // ERC Token Standard #20 Interface
-   // https://github.com/ethereum/EIPs/issues/20
-  contract ERC20Interface {
-      // Get the total token supply
-      function totalSupply() constant returns (uint256 totalSupply);
-   
-      // Get the account balance of another account with address _owner
-      function balanceOf(address _owner) constant returns (uint256 balance);
-   
-      // Send _value amount of tokens to address _to
-      function transfer(address _to, uint256 _value) returns (bool success);
-   
-      // Send _value amount of tokens from address _from to address _to
-      function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-   
-      // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-      // If this function is called again it overwrites the current allowance with _value.
-      // this function is required for some DEX functionality
-      function approve(address _spender, uint256 _value) returns (bool success);
-   
-      // Returns the amount which _spender is still allowed to withdraw from _owner
-      function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-   
-      // Triggered when tokens are transferred.
-      event Transfer(address indexed _from, address indexed _to, uint256 _value);
-   
-      // Triggered whenever approve(address _spender, uint256 _value) is called.
-      event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+library SafeMath {
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
   }
-   
-  contract Kether is ERC20Interface {
-      string public constant symbol = "IDRK";
-      string public constant name = "Kether";
-      uint8 public constant decimals = 2;
-      uint256 _totalSupply = 10000000000000000;
-      
-      // Owner of this contract
-      address public owner;
-   
-      // Balances for each account
-      mapping(address => uint256) balances;
-   
-      // Owner of account approves the transfer of an amount to another account
-      mapping(address => mapping (address => uint256)) allowed;
-   
-      // Functions with this modifier can only be executed by the owner
-      modifier onlyOwner() {
-          if (msg.sender != owner) {
-              throw;
-          }
-          _;
-      }
-   
-      // Constructor
-      function Kether() {
-          owner = msg.sender;
-          balances[owner] = _totalSupply;
-      }
-   
-      function totalSupply() constant returns (uint256 totalSupply) {
-          totalSupply = _totalSupply;
-      }
-   
-      // What is the balance of a particular account?
-      function balanceOf(address _owner) constant returns (uint256 balance) {
-          return balances[_owner];
-      }
-   
-      // Transfer the balance from owner's account to another account
-      function transfer(address _to, uint256 _amount) returns (bool success) {
-          if (balances[msg.sender] >= _amount 
-              && _amount > 0
-              && balances[_to] + _amount > balances[_to]) {
-              balances[msg.sender] -= _amount;
-              balances[_to] += _amount;
-              Transfer(msg.sender, _to, _amount);
-              return true;
-          } else {
-              return false;
-          }
-      }
-   
-      // Send _value amount of tokens from address _from to address _to
-      // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-      // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-      // fees in sub-currencies; the command should fail unless the _from account has
-      // deliberately authorized the sender of the message via some mechanism; we propose
-      // these standardized APIs for approval:
-      function transferFrom(
-          address _from,
-          address _to,
-          uint256 _amount
-     ) returns (bool success) {
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(_from, _to, _amount);
-             return true;
-         } else {
-             return false;
-         }
-     }
-  
-     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-     // If this function is called again it overwrites the current allowance with _value.
-     function approve(address _spender, uint256 _amount) returns (bool success) {
-         allowed[msg.sender][_spender] = _amount;
-         Approval(msg.sender, _spender, _amount);
-         return true;
-     }
-  
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-         return allowed[_owner][_spender];
-     }
- }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_value > 0);
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_value > 0);
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    require(_value > 0);
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    require(_addedValue > 0);
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    require(_subtractedValue > 0);
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+}
+
+contract BurnableToken is BasicToken {
+
+  event Burn(address indexed burner, uint256 value);
+
+  function burn(uint256 _value) public {
+    require(_value > 0);
+    require(_value <= balances[msg.sender]);
+
+    address burner = msg.sender;
+    balances[burner] = balances[burner].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    Burn(burner, _value);
+  }
+}
+
+
+contract Kether is StandardToken, BurnableToken {
+
+  string public constant name = "Kether"; 
+  string public constant symbol = "SGDK"; 
+  uint8 public constant decimals = 2; 
+
+  uint256 public constant INITIAL_SUPPLY = 21000000000000 * (10 ** uint256(decimals));
+
+  function Kether() public {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+
+}
