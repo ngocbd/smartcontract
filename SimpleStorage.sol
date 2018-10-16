3649402de34b1054415630b68e0fc2e548439255
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:55:26 +0700

    add contract SimpleStorage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,16 +1,16 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0x9295ede03f6eab5c8f215d3ddcf7dca3a26ca087
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0xf423af010426bbdffc1fdd32a8e302c7b8623fb2
 */
 pragma solidity ^0.4.0;
 
 contract SimpleStorage {
     uint storedData;
 
-    function set(uint x) {
+    function set(uint x) public {
         storedData = x;
     }
 
-    function get() constant returns (uint storedData) {
+    function get() public constant returns (uint) {
         return storedData;
     }
 }
