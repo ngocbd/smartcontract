b0cc1c732c60fee4a24a68f0bdbf7e2b9e0d55f7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:29:17 +0700

    add contract SimplePonzi

--- SimplePonzi.sol
+++ SimplePonzi.sol
@@ -1,19 +1,22 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimplePonzi at 0x428472c5664d079eb4f035d872801241a4e8a4de
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimplePonzi at 0xd09180f956c97a165c23c7f932908600c2e3e0fb
 */
 contract SimplePonzi {
     address public currentInvestor;
     uint public currentInvestment = 0;
     
     function () payable public {
-        require(msg.value > currentInvestment);
-        
-        // payout previous investor
-        currentInvestor.send(currentInvestment);
+        // new investments must be 10% greater than current
+        uint minimumInvestment = currentInvestment * 11 / 10;
+        require(msg.value > minimumInvestment);
 
-        // document new investment
+        // document new investor
+        address previousInvestor = currentInvestor;
         currentInvestor = msg.sender;
         currentInvestment = msg.value;
 
+        
+        // payout previous investor
+        previousInvestor.send(msg.value);
     }
 }
