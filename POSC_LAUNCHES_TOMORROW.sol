a5c387739d02fc28d1b90c89d0ea30d33452ff31
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:03:19 +0700

    add contract POSC_LAUNCHES_TOMORROW

---
+++ POSC_LAUNCHES_TOMORROW.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract POSC_LAUNCHES_TOMORROW at 0x15db248c9237fd29d340119f7f907c4390c9a29a
+*/
+pragma solidity ^0.4.21;
+
+
+
+contract POSC_LAUNCHES_TOMORROW{
+
+}
