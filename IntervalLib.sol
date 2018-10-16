1f24e40102cd2cbe084278c1b54bec867c4ce4e9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:42:13 +0700

    add contract IntervalLib

---
+++ IntervalLib.sol
@@ -0,0 +1,12 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IntervalLib at 0xf5f2a533b065125c5edb3cc0119c7866020de4cf
+*/
+pragma solidity ^0.4.15;
+
+library IntervalLib {
+  struct Interval {
+    uint begin;
+    uint end;
+    bytes32 data;
+  }
+}
