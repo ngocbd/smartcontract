4c445a770f6199080dc102c6d4f80b2e7b6ccaef
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:59:26 +0700

    add contract DMToken

--- DMToken.sol
+++ DMToken.sol
@@ -1,35 +1,72 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DMToken at 0xbcd7db2ca9914d50b121fd1f5f950818f981d55b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DMToken at 0x2ccbff3a042c68716ed2a2cb0c544a9f1d1935e1
 */
-pragma solidity ^0.4.16;
+pragma solidity 0.4.18;
 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a * b;
         assert(a == 0 || c / a == b);
         return c;
     }
 
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
 
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
         return c;
+    }
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
     }
 }
 
@@ -56,73 +93,11 @@ contract ERC20 is ERC20Basic {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances.
- */
-contract BasicToken is ERC20Basic {
+
+contract ApprovalContract is ERC20 {
     using SafeMath for uint256;
 
-    mapping(address => uint256) balances;
-
-    /**
-    * @dev transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-
-        // SafeMath.sub will throw if there is not enough balance.
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-}
-
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-contract StandardToken is ERC20, BasicToken {
-
-    mapping (address => mapping (address => uint256)) allowed;
-
-
-    /**
-     * @dev Transfer tokens from one address to another
-     * @param _from address The address which you want to send tokens from
-     * @param _to address The address which you want to transfer to
-     * @param _value uint256 the amount of tokens to be transferred
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-
-        uint256 _allowance = allowed[_from][msg.sender];
-
-        // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-        // require (_value <= _allowance);
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
+    mapping (address => mapping (address => uint256)) public allowed;
 
     /**
      * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
@@ -156,14 +131,14 @@ contract StandardToken is ERC20, BasicToken {
      * the first transaction is mined)
      * From MonolithDAO Token.sol
      */
-    function increaseApproval (address _spender, uint _addedValue)
+    function increaseApproval (address _spender, uint _addedValue) public
     returns (bool success) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
         Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    function decreaseApproval (address _spender, uint _subtractedValue)
+    function decreaseApproval (address _spender, uint _subtractedValue) public
     returns (bool success) {
         uint oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
@@ -174,54 +149,18 @@ contract StandardToken is ERC20, BasicToken {
         Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
-
 }
 
 /**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
+ * @title Mintable token
+ * @dev Simple ERC20 Token example, with mintable token creation
+ * @dev Issue: * https://github.com/OpenZeppelin/zeppelin-solidity/issues/120
+ * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
  */
-contract Ownable {
-    address public owner;
-
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    function Ownable() {
-        owner = msg.sender;
-    }
-
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function transferOwnership(address newOwner) onlyOwner public {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-}
-
-contract MintableToken is StandardToken, Ownable {
+contract MintableToken is ApprovalContract, Ownable {
 
     uint256 public hardCap;
+    mapping(address => uint256) public balances;
 
     event Mint(address indexed to, uint256 amount);
 
@@ -244,27 +183,156 @@ contract MintableToken is StandardToken, Ownable {
         Transfer(0x0, _to, _amount);
         return true;
     }
-
-    /**
-     * @dev Function to stop minting new tokens.
-     * @return True if the operation was successful.
-     */
 }
 
-contract DMToken is MintableToken {
+/**
+ * @title Vesting token
+ */
+contract Vesting is MintableToken {
+
+    event VestingMemberAdded(address indexed _address, uint256 _amount, uint _start, uint _end);
+
+    struct _Vesting {
+        uint256 totalSum;     //total amount
+        uint256 start;        //start block
+        uint256 end;          //end block
+        uint256 usedAmount;   //the amount of paid payments
+    }
+
+    mapping (address => _Vesting ) public vestingMembers;
+
+    function addVestingMember(
+        address _address,
+        uint256 _amount,
+        uint256 _start,
+        uint256 _end
+    ) onlyOwner public returns (bool) {
+        require(
+            _address != address(0) &&
+            _amount > 0 &&
+            _start < _end &&
+            vestingMembers[_address].totalSum == 0 &&
+            balances[msg.sender] > _amount
+        );
+
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+
+        vestingMembers[_address].totalSum = _amount;    //total amount
+        vestingMembers[_address].start = _start;        //start block
+        vestingMembers[_address].end = _end;            //end block
+        vestingMembers[_address].usedAmount = 0;        //the amount of paid payments
+
+        VestingMemberAdded(_address, _amount, _start, _end);
+
+        return true;
+    }
+
+    function currentPart(address _address) private constant returns (uint256) {
+        if (vestingMembers[_address].totalSum == 0 || block.number <= vestingMembers[_address].start) {
+            return 0;
+        }
+        if (block.number >= vestingMembers[_address].end) {
+            return vestingMembers[_address].totalSum.sub(vestingMembers[_address].usedAmount);
+        }
+
+        return vestingMembers[_address].totalSum
+        .mul(block.number - vestingMembers[_address].start)
+        .div(vestingMembers[_address].end - vestingMembers[_address].start)
+        .sub(vestingMembers[_address].usedAmount);
+    }
+
+    function subFromBalance(address _address, uint256 _amount) private returns (uint256) {
+        require(_address != address(0));
+
+        if (vestingMembers[_address].totalSum == 0) {
+            balances[_address] = balances[_address].sub(_amount);
+            return balances[_address];
+        }
+        uint256 summary = balanceOf(_address);
+        require(summary >= _amount);
+
+        if (balances[_address] > _amount) {
+            balances[_address] = balances[_address].sub(_amount);
+        } else {
+            uint256 part = currentPart(_address);
+            if (block.number >= vestingMembers[_address].end) {
+                vestingMembers[_address].totalSum = 0;          //total amount
+                vestingMembers[_address].start = 0;             //start block
+                vestingMembers[_address].end = 0;               //end block
+                vestingMembers[_address].usedAmount = 0;        //the amount of paid payments
+            } else {
+                vestingMembers[_address].usedAmount = vestingMembers[_address].usedAmount.add(part);
+            }
+            balances[_address] = balances[_address].add(part).sub(_amount);
+        }
+
+        return balances[_address];
+    }
+
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        if (vestingMembers[_owner].totalSum == 0) {
+            return balances[_owner];
+        } else {
+            return balances[_owner].add(currentPart(_owner));
+        }
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balanceOf(msg.sender));
+
+        subFromBalance(msg.sender, _value);
+
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+
+        uint256 _allowance = allowed[_from][msg.sender];
+
+        subFromBalance(_from, _value);
+
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+}
+
+
+contract DMToken is Vesting {
 
     string public name = "DMarket Token";
-    string public symbol = "MARK";
+    string public symbol = "DMT";
     uint256 public decimals = 8;
 
-    function DMToken() {
-        
-        /**
-        *  1-Phase: 36037245 * 10^8 tokens
-        *  2-Phase: 100000000 * 10^8 tokens
-        *  Additionaly issued tokens: (36037245 + 100000000) % 15 =  20405587 * 10^8 tokens
-        *  HardCap will be equal to: 36037245 + 100000000 + 20405587 = 156442831 * 10^8 tokens
-        */
+    function DMToken() public {
         hardCap = 15644283100000000;
+    }
+
+    function multiTransfer(address[] recipients, uint256[] amounts) public {
+        require(recipients.length == amounts.length);
+        for (uint i = 0; i < recipients.length; i++) {
+            transfer(recipients[i], amounts[i]);
+        }
+    }
+
+    function multiVesting(
+        address[] _address,
+        uint256[] _amount,
+        uint256[] _start,
+        uint256[] _end
+    ) public onlyOwner {
+        require(
+            _address.length == _amount.length &&
+            _address.length == _start.length &&
+            _address.length == _end.length
+        );
+        for (uint i = 0; i < _address.length; i++) {
+            addVestingMember(_address[i], _amount[i], _start[i], _end[i]);
+        }
     }
 }
