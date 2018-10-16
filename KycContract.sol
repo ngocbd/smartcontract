e19f1e052ac228be3810a73a156c3979e49ce121
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:09:41 +0700

    add contract KycContract

--- KycContract.sol
+++ KycContract.sol
@@ -1,40 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KycContract at 0x6aD154bF6a76Ce3aF58fB0D229a0175612908445
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KycContract at 0x2f0a1bd676e786f9dd5543697341e717a4af61c7
 */
-contract SafeMath {
-    
-    uint256 constant MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+contract ERC20TokenInterface {
+  function totalSupply() public constant returns (uint256 _totalSupply);
+  function balanceOf(address _owner) public constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) public returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+  function approve(address _spender, uint256 _value) public returns (bool success);
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
 
-    function safeAdd(uint256 x, uint256 y) constant internal returns (uint256 z) {
-        require(x <= MAX_UINT256 - y);
-        return x + y;
-    }
-
-    function safeSub(uint256 x, uint256 y) constant internal returns (uint256 z) {
-        require(x >= y);
-        return x - y;
-    }
-
-    function safeMul(uint256 x, uint256 y) constant internal returns (uint256 z) {
-        if (y == 0) {
-            return 0;
-        }
-        require(x <= (MAX_UINT256 / y));
-        return x * y;
-    }
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract ReentrancyHandlingContract{
-
-    bool locked;
-
-    modifier noReentrancy() {
-        require(!locked);
-        locked = true;
-        _;
-        locked = false;
-    }
-}
 
 contract Owned {
     address public owner;
@@ -63,60 +41,6 @@ contract Owned {
 
     event OwnerUpdate(address _prevOwner, address _newOwner);
 }
-
-contract Lockable is Owned {
-
-    uint256 public lockedUntilBlock;
-
-    event ContractLocked(uint256 _untilBlock, string _reason);
-
-    modifier lockAffected {
-        require(block.number > lockedUntilBlock);
-        _;
-    }
-
-    function lockFromSelf(uint256 _untilBlock, string _reason) internal {
-        lockedUntilBlock = _untilBlock;
-        ContractLocked(_untilBlock, _reason);
-    }
-
-
-    function lockUntil(uint256 _untilBlock, string _reason) onlyOwner public {
-        lockedUntilBlock = _untilBlock;
-        ContractLocked(_untilBlock, _reason);
-    }
-}
-
-contract ERC20TokenInterface {
-  function totalSupply() public constant returns (uint256 _totalSupply);
-  function balanceOf(address _owner) public constant returns (uint256 balance);
-  function transfer(address _to, uint256 _value) public returns (bool success);
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-  function approve(address _spender, uint256 _value) public returns (bool success);
-  function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-contract InsurePalTokenInterface {
-    function mint(address _to, uint256 _amount) public;
-}
-
-contract tokenRecipientInterface {
-  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
-}
-
-contract KycContractInterface {
-    function isAddressVerified(address _address) public view returns (bool);
-}
-
-
-
-
-
-
-
 
 
 contract KycContract is Owned {
