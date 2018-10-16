6907cc1d33541b03ee36b2ee522ba272f1df1fc3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:21:30 +0700

    add contract SplitCoinFactory

--- SplitCoinFactory.sol
+++ SplitCoinFactory.sol
@@ -1,13 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SplitCoinFactory at 0x2b96efc474b9fb935951ca65f10d0cecdc480d57
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SplitCoinFactory at 0x50d998d0faf680b9cf44d6cfd9f7c3654056dcc5
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.17;
 
 library CSCLib {
-
-	uint constant MILLION = 1000000;
-	uint constant GASLIMIT = 65000;
-
 
 	struct Split {
 		address to;
@@ -28,23 +24,10 @@ library CSCLib {
 
 	event SplitTransfer(address to, uint amount, uint balance);
 
-	/*
-	self: a storage pointer
-
-	members: an array of addresses
-
-	ppms: an array of integers that should sum to 1 million.
-		Represents how much ether a user should get
-
-	refer: the address of a referral contract that referred this user.
-		Referral contract should be a claimable contract
-
-	*/
 	function init(CSCStorage storage self,  address[] members, uint[] ppms, address refer) internal {
 		uint shift_amt = self.dev_fee / members.length;
 		uint remainder = self.dev_fee % members.length * members.length / 10;
 		uint dev_total = self.dev_fee + remainder;
-		self.deposits.push(0);
 		if(refer != 0x0){
 			addSplit(self, Split({to: self.developer, ppm: dev_total - self.refer_fee}));
 			addSplit(self, Split({to: refer, ppm: self.refer_fee}));
@@ -52,12 +35,9 @@ library CSCLib {
 			addSplit(self, Split({to: self.developer, ppm: dev_total}));
 		}
 
-		uint sum = 0;
 		for(uint index = 0; index < members.length; index++) {
-			sum += ppms[index];
 			addSplit(self, Split({to: members[index], ppm: ppms[index] - shift_amt}));
 		}
-		require(sum >= MILLION - 1 && sum < MILLION + 1 );
 	}
 
 	function addSplit(CSCStorage storage self, Split newSplit) internal {
@@ -68,16 +48,15 @@ library CSCLib {
 			self.splits[index] = newSplit;
 		} else {
 			self.userSplit[newSplit.to] = self.splits.length;
-			self.lastUserClaim[newSplit.to] = self.deposits.length;
 			self.splits.push(newSplit);
 		}
 	}
 
 	function payAll(CSCStorage storage self) internal {
 		for(uint index = 0; index < self.splits.length; index++) {
-			uint value = (msg.value) * self.splits[index].ppm / MILLION;
+			uint value = (msg.value) * self.splits[index].ppm / 1000000.00;
 			if(value > 0 ) {
-				require(self.splits[index].to.call.gas(GASLIMIT).value(value)());
+				require(self.splits[index].to.call.gas(60000).value(value)());
 				SplitTransfer(self.splits[index].to, value, this.balance);
 			}
 		}
@@ -97,7 +76,7 @@ library CSCLib {
 		uint splitIndex = self.userSplit[user];
 		self.lastUserClaim[user] = self.deposits.length;
 		if(sum > 0) {
-			require(self.splits[splitIndex].to.call.gas(GASLIMIT).value(sum)());
+			require(self.splits[splitIndex].to.call.gas(60000).value(sum)());
 			SplitTransfer(self.splits[splitIndex].to, sum, this.balance);
 		}
 	}
@@ -112,7 +91,7 @@ library CSCLib {
 		uint unclaimed = 0;
 		if(self.splits[splitIndex].to == user) {
 			for(uint depositIndex = lastClaimIndex; depositIndex < self.deposits.length; depositIndex++) {
-				uint value = self.deposits[depositIndex] * self.splits[splitIndex].ppm / MILLION;
+				uint value = self.deposits[depositIndex] * self.splits[splitIndex].ppm / 1000000.00;
 				unclaimed += value;
 			}
 		}
@@ -130,6 +109,7 @@ library CSCLib {
 		// neither user can have a pending balance to use transfer
 		uint splitIndex = self.userSplit[msg.sender];
 		if(splitIndex > 0 && self.splits[splitIndex].to == msg.sender && self.splits[splitIndex].ppm >= ppm) {
+			self.lastUserClaim[to] = self.lastUserClaim[msg.sender];
 			self.splits[splitIndex].ppm -= ppm;
 			addSplit(self, Split({to: to, ppm: ppm}));
 		}
@@ -143,6 +123,7 @@ library CSCLib {
 		}
 	}
 }
+
 contract ClaimableSplitCoin {
 
 	using CSCLib for CSCLib.CSCStorage;
@@ -199,6 +180,7 @@ contract ClaimableSplitCoin {
 		csclib.transfer(to, ppm);
 	}
 }
+
 contract SplitCoinFactory {
   mapping(address => address[]) public contracts;
   mapping(address => uint) public referralContracts;
