0914f28e4a5c4e1b91a82288009757ec0e9c2a75
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:58:33 +0700

    add contract Counter

--- Counter.sol
+++ Counter.sol
@@ -1,8 +1,32 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Counter at 0xa82749c94ab7f921725624fb90e7600216169597
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Counter at 0xefc1b11ad4b183ad859ef220a9c498cb04716a31
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.20;
+
 contract Counter {
-    uint public counter;
-    function increment() public { counter++; }
+    
+    event Won(address winner, uint amount);
+    
+    uint public i;
+    address public owner;
+    
+    function Counter() public {
+        owner = msg.sender;
+    }
+    
+    function reset() public {
+        require(msg.sender==owner);
+        i=0;
+    }
+    
+    function inc() public payable {
+        require(msg.value >= 0.001 ether);
+        i++;
+        if (i==2) {
+            emit Won(msg.sender,address(this).balance);
+            msg.sender.transfer(address(this).balance);
+            i = 0;
+        }
+    }
+    
 }
