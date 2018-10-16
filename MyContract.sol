5c8c6d656d2a34a354cd9927abd96f2d42379a6e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:03:44 +0700

    add contract MyContract

--- MyContract.sol
+++ MyContract.sol
@@ -1,14 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyContract at 0x7f6d05523908a9688eab8ab8bbc7842e3d453bfb
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyContract at 0xD5c93B00516AF744933016179195ACCD3dEd981E
 */
+pragma solidity 0.4.8;
+
 contract MyContract {
-    /* Constructor */
-   
- function MyContract() {
-var co23actline1 = "2";
-var str2 = "3";
-
-var contractline2="1.1.   Both the Site and the Platform use cookie files. These Cookie Regulations supplement the Site Rules and Regulations and the Rules and Regulations of the Platform.";
-
+    function whatDidJasonSay() constant returns (string said){
+        return "ether is a scam";
     }
 }
