5228a6e6f245d545bff093bb6d5c82f29a4d9e1b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:53 +0700

    add contract ReplaySafeSplit

--- ReplaySafeSplit.sol
+++ ReplaySafeSplit.sol
@@ -1,44 +1,44 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0xaBbb6bEbFA05aA13e908EaA492Bd7a8343760477
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0xcd820a2f792f429bf95e6d0de909d06110b44123
 */
 contract RequiringFunds {
-    modifier NeedEth () {
-        if (msg.value <= 0 ) throw;
-        _
-    }
+   modifier NeedEth () {
+       if (msg.value <= 0 ) throw;
+       _
+   }
 }
 
 contract AmIOnTheFork {
-    function forked() constant returns(bool);
+   function forked() constant returns(bool);
 }
 
 contract ReplaySafeSplit is RequiringFunds {
-    //address private constant oracleAddress = 0x8128B12cABc6043d94BD3C4d9B9455077Eb18807;    // testnet
-    address private constant oracleAddress = 0x2bd2326c993dfaef84f696526064ff22eba5b362;   // mainnet
-    
-    // Fork oracle to use
-    AmIOnTheFork amIOnTheFork = AmIOnTheFork(oracleAddress);
+   // address private constant oracleAddress = 0x8128B12cABc6043d94BD3C4d9B9455077Eb18807;    // testnet
+   address private constant oracleAddress = 0x2bd2326c993dfaef84f696526064ff22eba5b362;   // mainnet
 
-    // Splits the funds into 2 addresses
-    function split(address targetFork, address targetNoFork) NeedEth returns(bool) {
-        // The 2 checks are to ensure that users provide BOTH addresses
-        // and prevent funds to be sent to 0x0 on one fork or the other.
-        if (targetFork == 0) throw;
-        if (targetNoFork == 0) throw;
+   // Fork oracle to use
+   AmIOnTheFork amIOnTheFork = AmIOnTheFork(oracleAddress);
 
-        if (amIOnTheFork.forked()                   // if we are on the fork 
-            && targetFork.send(msg.value)) {        // send the ETH to the targetFork address
-            return true;
-        } else if (!amIOnTheFork.forked()           // if we are NOT on the fork 
-            && targetNoFork.send(msg.value)) {      // send the ETH to the targetNoFork address 
-            return true;
-        }
+   // Splits the funds into 2 addresses
+   function split(address targetFork, address targetNoFork) NeedEth returns(bool) {
+       // The 2 checks are to ensure that users provide BOTH addresses
+       // and prevent funds to be sent to 0x0 on one fork or the other.
+       if (targetFork == 0) throw;
+       if (targetNoFork == 0) throw;
 
-        throw;                                      // don't accept value transfer, otherwise it would be trapped.
-    }
+       if (amIOnTheFork.forked()                   // if we are on the fork
+           && targetFork.send(msg.value)) {        // send the ETH to the targetFork address
+           return true;
+       } else if (!amIOnTheFork.forked()           // if we are NOT on the fork
+           && targetNoFork.send(msg.value)) {      // send the ETH to the targetNoFork address
+           return true;
+       }
 
-    // Reject value transfers.
-    function() {
-        throw;
-    }
+       throw;                                      // don't accept value transfer, otherwise it would be trapped.
+   }
+
+   // Reject value transfers.
+   function() {
+       throw;
+   }
 }
