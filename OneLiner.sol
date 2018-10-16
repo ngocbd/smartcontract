22e875d8135640ef3327abe14535c7aa29bf2179
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:51:43 +0700

    add contract OneLiner

---
+++ OneLiner.sol
@@ -0,0 +1,7 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OneLiner at 0x442EfA42459c1d13ed2Eb93A56c43A950296396f
+*/
+pragma solidity 0.4.17;
+
+
+contract OneLiner {}
