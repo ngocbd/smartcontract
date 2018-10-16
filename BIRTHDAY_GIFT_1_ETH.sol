afd21fdf2376740d160613fdb4b71065823a6f5c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:13:31 +0700

    add contract BIRTHDAY_GIFT_1_ETH

---
+++ BIRTHDAY_GIFT_1_ETH.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BIRTHDAY_GIFT_1_ETH at 0x2634Baad203cBA4AA4114C132b2E50A3a6027Ff9
+*/
+pragma solidity ^0.4.19;
+
+contract BIRTHDAY_GIFT_1_ETH
+{
+    address sender;
+    
+    address reciver;
+    
+    bool closed = false;
+    
+    uint unlockTime;
+ 
+    function PutGift(address _reciver)
+    public
+    payable
+    {
+        if( (!closed&&(msg.value > 1 ether)) || sender==0x00 )
+        {
+            sender = msg.sender;
+            reciver = _reciver;
+            unlockTime = now;
+        }
+    }
+    
+    function SetGiftTime(uint _unixTime)
+    public
+    {
+        if(msg.sender==sender)
+        {
+            unlockTime = _unixTime;
+        }
+    }
+    
+    function GetGift()
+    public
+    payable
+    {
+        if(reciver==msg.sender&&now>unlockTime)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function CloseGift()
+    public
+    {
+        if(sender == msg.sender && reciver != 0x0 )
+        {
+           closed=true;
+        }
+    }
+    
+    function() public payable{}
+}
