90d4923b76c0d76bab0aa194c6fc444b34fa5dd4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:58:48 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0xa0ff8b2ea199dbb477b375070c74cc6dbd28f2ba
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0xfae4ee59cdd86e3be9e8b90b53aa866327d7c090
 */
 pragma solidity ^0.4.16;
 
@@ -9,6 +9,7 @@ contract TokenERC20 {
     // Public variables of the token
     string public name;
     string public symbol;
+    //address public target;
     uint8 public decimals = 18;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
@@ -31,10 +32,12 @@ contract TokenERC20 {
     function TokenERC20(
         uint256 initialSupply,
         string tokenName,
-        string tokenSymbol
+        string tokenSymbol,
+        address target
     ) public {
         totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        //target = _target;
+        balanceOf[target] = totalSupply;                // Give the creator all initial tokens
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
