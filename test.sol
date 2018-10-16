3d1b3de786c63bb9e6b6d8e195781e1391a06944
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:41:06 +0700

    add contract test

--- test.sol
+++ test.sol
@@ -1,18 +1,41 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract test at 0x80c13052ea39bc9f78d6723bfcef273f5a7ff651
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract test at 0x4bf95b1b69f195041895aaf46c7d0cc325121e95
 */
 pragma solidity ^0.4.18;
 
-contract test {
-  uint256 public totalSupply;
-  function test(uint256 _totalSupply) {
-    totalSupply = _totalSupply;
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
   }
-  function add(uint256 _add) {
-    if (_add > 0) {
-      totalSupply += _add;
-    } else {
-      totalSupply++;
-    }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract test {
+  using SafeMath for uint256;
+  uint256 public num;
+  function test() {
+    num = 10;
+  }
+  function add(uint256 _num) {
+    num = num.add(_num);
   }
 }
