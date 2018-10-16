2d34d281da91159f86976133e2e7b94bc4f753b0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:06:57 +0700

    add contract ETH_VAULT

---
+++ ETH_VAULT.sol
@@ -0,0 +1,74 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETH_VAULT at 0xBaf51E761510C1a11Bf48dd87c0307aC8A8C8A4f
+*/
+pragma solidity ^0.4.19;
+
+contract ETH_VAULT
+{
+    mapping (address => uint) public balances;
+    
+    uint public MinDeposit = 1 ether;
+    
+    Log TransferLog;
+    
+    function ETH_VAULT(address _log)
+    public 
+    {
+        TransferLog = Log(_log);
+    }
+    
+    function Deposit()
+    public
+    payable
+    {
+        if(msg.value > MinDeposit)
+        {
+            balances[msg.sender]+=msg.value;
+            TransferLog.AddMessage(msg.sender,msg.value,"Deposit");
+        }
+    }
+    
+    function CashOut(uint _am)
+    public
+    payable
+    {
+        if(_am<=balances[msg.sender])
+        {
+            
+            if(msg.sender.call.value(_am)())
+            {
+                balances[msg.sender]-=_am;
+                TransferLog.AddMessage(msg.sender,_am,"CashOut");
+            }
+        }
+    }
+    
+    function() public payable{}    
+    
+}
+
+contract Log 
+{
+   
+    struct Message
+    {
+        address Sender;
+        string  Data;
+        uint Val;
+        uint  Time;
+    }
+    
+    Message[] public History;
+    
+    Message LastMsg;
+    
+    function AddMessage(address _adr,uint _val,string _data)
+    public
+    {
+        LastMsg.Sender = _adr;
+        LastMsg.Time = now;
+        LastMsg.Val = _val;
+        LastMsg.Data = _data;
+        History.push(LastMsg);
+    }
+}
