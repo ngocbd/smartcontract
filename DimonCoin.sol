49e3ed272668b008af50a1d57740641ee1360045
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:06:46 +0700

    add contract DimonCoin

--- DimonCoin.sol
+++ DimonCoin.sol
@@ -1,14 +1,35 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DimonCoin at 0xf084d5bc3e35e3d903260267ebd545c49c6013d0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DimonCoin at 0xde39e5e5a1b0eeb3afe717d6d011cae88d19451e
 */
 pragma solidity ^0.4.16;
 
+
 contract ForeignToken {
-    function balanceOf(address _owner) constant returns (uint256);
-    function transfer(address _to, uint256 _value) returns (bool);
+    function balanceOf(address _owner) public constant returns (uint256);
+    function transfer(address _to, uint256 _value) public returns (bool);
 }
 
-contract DimonCoin {
+contract ERC20Basic {
+
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+}
+
+
+
+contract ERC20 is ERC20Basic {
+
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
+
+contract DimonCoin is ERC20 {
     
     address owner = msg.sender;
 
@@ -17,14 +38,23 @@ contract DimonCoin {
     
     uint256 public totalSupply = 100000000 * 10**8;
 
-    function name() constant returns (string) { return "DimonCoin"; }
-    function symbol() constant returns (string) { return "FUD"; }
-    function decimals() constant returns (uint8) { return 8; }
+    function name() public constant returns (string) { return "DimonCoin"; }
+    function symbol() public constant returns (string) { return "FUDD"; }
+    function decimals() public constant returns (uint8) { return 8; }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function DimonCoin() {
+    event DistrFinished();
+
+    bool public distributionFinished = false;
+
+    modifier canDistr() {
+    require(!distributionFinished);
+    _;
+    }
+
+    function DimonCoin() public {
         owner = msg.sender;
         balances[msg.sender] = totalSupply;
     }
@@ -34,15 +64,15 @@ contract DimonCoin {
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner {
+    function transferOwnership(address newOwner) onlyOwner public {
         owner = newOwner;
     }
 
-    function getEthBalance(address _addr) constant returns(uint) {
+    function getEthBalance(address _addr) constant public returns(uint) {
     return _addr.balance;
     }
 
-    function distributeFUD(address[] addresses, uint256 _value, uint256 _ethbal) onlyOwner {
+    function distributeFUDD(address[] addresses, uint256 _value, uint256 _ethbal) onlyOwner canDistr public {
          for (uint i = 0; i < addresses.length; i++) {
 	     if (getEthBalance(addresses[i]) < _ethbal) {
  	         continue;
@@ -53,7 +83,7 @@ contract DimonCoin {
          }
     }
     
-    function balanceOf(address _owner) constant returns (uint256) {
+    function balanceOf(address _owner) constant public returns (uint256) {
 	 return balances[_owner];
     }
 
@@ -63,47 +93,37 @@ contract DimonCoin {
         _;
     }
     
-    function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) returns (bool success) {
+    function transfer(address _to, uint256 _amount) onlyPayloadSize(2 * 32) public returns (bool success) {
 
-        if (_value == 0) { return false; }
-
-        uint256 fromBalance = balances[msg.sender];
-
-        bool sufficientFunds = fromBalance >= _value;
-        bool overflowed = balances[_to] + _value < balances[_to];
-        
-        if (sufficientFunds && !overflowed) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+         if (balances[msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[msg.sender] -= _amount;
+             balances[_to] += _amount;
+             Transfer(msg.sender, _to, _amount);
+             return true;
+         } else {
+             return false;
+         }
     }
     
-    function transferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(2 * 32) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _amount) onlyPayloadSize(3 * 32) public returns (bool success) {
 
-        if (_value == 0) { return false; }
-        
-        uint256 fromBalance = balances[_from];
-        uint256 allowance = allowed[_from][msg.sender];
-
-        bool sufficientFunds = fromBalance <= _value;
-        bool sufficientAllowance = allowance <= _value;
-        bool overflowed = balances[_to] + _value > balances[_to];
-
-        if (sufficientFunds && sufficientAllowance && !overflowed) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            
-            allowed[_from][msg.sender] -= _value;
-            
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+         if (balances[_from] >= _amount
+             && allowed[_from][msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[_from] -= _amount;
+             allowed[_from][msg.sender] -= _amount;
+             balances[_to] += _amount;
+             Transfer(_from, _to, _amount);
+             return true;
+         } else {
+            return false;
+         }
     }
     
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         // mitigates the ERC20 spend/approval race condition
         if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
         
@@ -113,12 +133,17 @@ contract DimonCoin {
         return true;
     }
     
-    function allowance(address _owner, address _spender) constant returns (uint256) {
+    function allowance(address _owner, address _spender) constant public returns (uint256) {
         return allowed[_owner][_spender];
     }
 
+    function finishDistribution() onlyOwner public returns (bool) {
+    distributionFinished = true;
+    DistrFinished();
+    return true;
+    }
 
-    function withdrawForeignTokens(address _tokenContract) returns (bool) {
+    function withdrawForeignTokens(address _tokenContract) public returns (bool) {
         require(msg.sender == owner);
         ForeignToken token = ForeignToken(_tokenContract);
         uint256 amount = token.balanceOf(address(this));
