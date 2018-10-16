f6b7ea8f5aac07fd749a119bfc284a21d7352564
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:42:11 +0700

    add contract NotAnotherPonzi

---
+++ NotAnotherPonzi.sol
@@ -0,0 +1,77 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NotAnotherPonzi at 0x5a437D94843541d5cB83221a4A4B253dE30B97b7
+*/
+contract NotAnotherPonzi {
+
+  struct Person {
+      address etherAddress;
+      uint amount;
+  }
+
+  Person[] public persons;
+
+  uint public payoutIdx = 0;
+  uint public collectedFees;
+  uint public balance = 0;
+
+  address public owner;
+
+
+  modifier onlyowner { if (msg.sender == owner) _ }
+
+
+  function NotAnotherPonzi() {
+    owner = msg.sender;
+  }
+
+  function() {
+    enter();
+  }
+  
+  function enter() {
+    if (msg.value < 1/100 ether) {
+        msg.sender.send(msg.value);
+        return;
+    }
+	
+		uint amount;
+		if (msg.value > 10 ether) {
+			msg.sender.send(msg.value - 10 ether);	
+			amount = 10 ether;
+    }
+		else {
+			amount = msg.value;
+		}
+
+
+    uint idx = persons.length;
+    persons.length += 1;
+    persons[idx].etherAddress = msg.sender;
+    persons[idx].amount = amount;
+ 
+    
+    if (idx != 0) {
+      collectedFees += amount / 10;
+	  owner.send(collectedFees);
+	  collectedFees = 0;
+      balance += amount - amount / 10;
+    } 
+    else {
+      balance += amount;
+    }
+
+
+    while (balance > persons[payoutIdx].amount / 100 * 125) {
+      uint transactionAmount = persons[payoutIdx].amount / 100 * 125;
+      persons[payoutIdx].etherAddress.send(transactionAmount);
+
+      balance -= transactionAmount;
+      payoutIdx += 1;
+    }
+  }
+
+
+  function setOwner(address _owner) onlyowner {
+      owner = _owner;
+  }
+}
