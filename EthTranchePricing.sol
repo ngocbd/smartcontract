08a134239010f386d3a839205f92e47254939189
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:44:45 +0700

    add contract EthTranchePricing

--- EthTranchePricing.sol
+++ EthTranchePricing.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthTranchePricing at 0x8557c179e6663360b3dac175003047989a1735fa
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthTranchePricing at 0xbcc1aa3bfc13c45df272ef2b99ca87248b0da952
 */
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
