7cfd2a4aa9899dca95ed3c2d60d57eb242c22c6c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:24:53 +0700

    add contract WanToken

--- WanToken.sol
+++ WanToken.sol
@@ -1,431 +1,256 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WanToken at 0xdbe22a77660b1261fb6830e48e0fa84438f7940d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WanToken at 0x2d51481469e34ac9fbc42c1990c1f6d0b1a9b65e
 */
 pragma solidity ^0.4.11;
 
 
-   
-   
-   
-   
+/**
+ * Math operations with safety checks
+ */
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-function div(uint256 a, uint256 b) internal returns (uint256) {
-    
-    uint256 c = a / b;
-    
+  function mul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
     return c;
-    }
- 
-function sub(uint256 a, uint256 b) internal returns (uint256) {
+  }
+
+  function div(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+  function sub(uint a, uint b) internal returns (uint) {
     assert(b <= a);
     return a - b;
-    }
+  }
 
-function add(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a + b;
+  function add(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
     assert(c >= a);
     return c;
-    }
-} 
+  }
 
-contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) constant returns (uint256);
-    function transfer(address to, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
 }
 
 
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value);
-    function approve(address spender, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+contract ERC20Protocol {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint supply);
+    is replaced with:
+    uint public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint _value) returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint _value) returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint _value) returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
 
 
-contract BasicToken is ERC20Basic {
-    using SafeMath for uint256;
-    
-    mapping(address => uint256) balances;
+contract StandardToken is ERC20Protocol {
+    using SafeMath for uint;
 
-
-
-
-
-
-function transfer(address _to, uint256 _value) {
-    
-    require ( balances[msg.sender] >= _value);
-    require ( balances[_to] + _value >= balances[_to]);
-    
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-}
-
-
-function burn(uint256 _value) {
-    
-    require ( balances[msg.sender] >= _value);
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    totalSupply = totalSupply.sub(_value);
-    
-    Burn(msg.sender, _value);
-}
-
-
-
-
-
-
-
-function balanceOf(address _owner) constant returns (uint256 balance) {
-    return balances[_owner];
-}
-
-}
-
-
-contract StandardToken is ERC20, BasicToken {
-   
-    mapping (address => mapping (address => uint256)) allowed;
-   
-   
-   
-   
-   
-   
-   
-   
-   function transferFrom(address _from, address _to, uint256 _value) {
-       var _allowance = allowed[_from][msg.sender];
-       
-       
-       
-       
-       balances[_to] = balances[_to].add(_value);
-       balances[_from] = balances[_from].sub(_value);
-       allowed[_from][msg.sender] = _allowance.sub(_value);
-       Transfer(_from, _to, _value);
-   } 
-   
-
-
-
-
-
-   function approve(address _spender, uint256 _value) {
-    
-    
-    
-    
-    
-    require ( !((_value !=0) && (allowed[msg.sender][_spender] !=0)));
-    
-    
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-}
-
-
-
-
-
-
-
-function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-}
-
-}
-
-contract Ownable {
-    address public owner;
-    
-    
-    
-    
-    
-    
-    function Ownable() {
-        owner = msg.sender;
-    }
-
-
-
-
-
-modifier onlyOwner() {
-    require(msg.sender == owner);
-    
-    _;
-}
-
-
-
-
-
-
-function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-        owner = newOwner;
-    }
-}
-
-}
-
-contract WanToken is StandardToken, Ownable {
-    event Mint(address indexed to, uint256 amount);
-    event MintFinished();
-    string public name = "WanCoin";
-    string public symbol = "WAN";
-    uint256 public decimals = 18;
-    
-    bool public mintingFinished = false;
-    
-    
-    modifier canMint() {
-        require(!mintingFinished);
+    /**
+    * @dev Fix for the ERC20 short address attack.
+    */
+    modifier onlyPayloadSize(uint size) {
+        require(msg.data.length >= size + 4);
         _;
     }
-    
-    function MintableToken() {
-        mint(msg.sender, 210000000000000000000000000);
-     finishMinting();   
-    }
-    
-    
-    
-    
-    
-    
-    
-    function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
-        totalSupply = totalSupply.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
-        Mint(_to, _amount);
-        Transfer(0, _to, _amount);
-        return true;
-    }
-    
-    
-    
-    
-    
-    
-    function finishMinting() onlyOwner returns (bool) {
-        mintingFinished = true;
-        MintFinished();
-        return true;
-    }
-    
-     
-}
 
-contract SwapToken is WanToken {
-    WanToken BasicToken;
-    mapping(address => bool) migrated;
-    
-    function SwapToken (WanToken _basicToken) {
-        BasicToken = _basicToken;
-        totalSupply = 210000000000000000000000000;
+    function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
     }
-    
-    function migration (address _owner) internal {
-        if (!migrated[_owner]) {
-            balances[_owner] = balances[_owner].add(BasicToken.balanceOf(_owner));
-            migrated[_owner] = true;
-        }
-        
+
+    function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
     }
-    
-    function transfer(address _to, uint256 _value) {
-        migration (msg.sender);
-        require ( balances[msg.sender] >= _value);
-        require ( balances[_to] + _value >= balances[_to]);
-        
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-    }
-    
-    
-    function burn(uint256 _value) {
-        migration (msg.sender);
-        require ( balances[msg.sender] >= _value);
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        
-        Burn(msg.sender, _value);
-    }
-    
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        migration(_owner);
+
+    function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
-    
-    function transferFrom(address _from, address _to, uint256 _value) {
-        var _allowance = allowed[_from][msg.sender];
-        
-        migration (msg.sender);
-        
-        
-        
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-        Transfer(_from, _to, _value);
-    }
-    
-    
-    
-    
-    
-    
-    function approve(address _spender, uint256 _value) {
-        
-        migration (msg.sender);
-        
-        
-        
-        
-        require ( !((_value != 0) && (allowed[msg.sender][_spender] != 0)));
-        
-        
+
+    function approve(address _spender, uint _value) onlyPayloadSize(2 * 32) returns (bool success) {
+        // To change the approve amount you first have to reduce the addresses`
+        //  allowance to zero by calling `approve(_spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        assert((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
+        return true;
     }
 
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+      return allowed[_owner][_spender];
+    }
 
+    mapping (address => uint) balances;
+    mapping (address => mapping (address => uint)) allowed;
 }
 
-contract Crowdsale is Ownable {
-    using SafeMath for uint256;
-    
-    
-    WanToken public token;
-    
-    
-    uint256 public deadline;
-    
-    
-    address public wallet;
-    
-    
-    uint256 public rate;
-    
-    
-    uint256 public weiRaised;
-    
-    
-    uint256 public tokensSold;
-    
-    
-    
-    
-    
-    
-    
-    
-    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-    
-    
-    function Crowdsale(WanToken tokenContract, uint256 durationInWeeks, uint256 _rate, address _wallet) {
-        
-        require(_rate > 0);
-        require(_wallet != 0x0);
-        
-        
-        token = tokenContract;
-        
-        deadline = now + durationInWeeks * 1 weeks;
-        rate = _rate;
-        wallet = _wallet;
-        
-        
-        
-        
+/// @title Wanchain Token Contract
+/// For more information about this token sale, please visit https://wanchain.org
+/// @author Cathy - <cathy@wanchain.org>
+contract WanToken is StandardToken {
+    using SafeMath for uint;
+
+    /// Constant token specific fields
+    string public constant name = "WanCoin";
+    string public constant symbol = "WAN";
+    uint public constant decimals = 18;
+
+    /// Wanchain total tokens supply
+    uint public constant MAX_TOTAL_TOKEN_AMOUNT = 210000000 ether;
+
+    /// Fields that are only changed in constructor
+    /// Wanchain contribution contract
+    address public minter; 
+    /// ICO start time
+    uint public startTime;
+    /// ICO end time
+    uint public endTime;
+
+    /// Fields that can be changed by functions
+    mapping (address => uint) public lockedBalances;
+    /*
+     * MODIFIERS
+     */
+
+    modifier onlyMinter {
+        assert(msg.sender == minter);
+        _;
     }
-    
-    
-    function setNewTokenOwner(address newOwner) onlyOwner {
-        token.transferOwnership(newOwner);
+
+    modifier isLaterThan (uint x){
+        assert(now > x);
+        _;
     }
-    
-    
-    
-    function createTokenContract() internal returns (WanToken) {
-        return new WanToken();
+
+    modifier maxWanTokenAmountNotReached (uint amount){
+        assert(totalSupply.add(amount) <= MAX_TOTAL_TOKEN_AMOUNT);
+        _;
     }
-    
-    
-    
-    function () payable {
-        buyTokens(msg.sender);
+
+    /**
+     * CONSTRUCTOR 
+     * 
+     * @dev Initialize the Wanchain Token
+     * @param _minter The Wanchain Contribution Contract     
+     * @param _startTime ICO start time
+     * @param _endTime ICO End Time
+     */
+    function WanToken(address _minter, uint _startTime, uint _endTime){
+        minter = _minter;
+        startTime = _startTime;
+        endTime = _endTime;
     }
-    
-    
-    function buyTokens(address beneficiary) payable {
-        require(beneficiary != 0x0);
-        require(validPurchase());
-        
-        uint256 weiAmount = msg.value;
-        uint256 updatedweiRaised = weiRaised.add(weiAmount);
-        
-        
-        uint256 tokens = weiAmount.mul(rate);
-        
-        
-        require ( tokens <= token.balanceOf(this) );
-        
-        
-        weiRaised = updatedweiRaised;
-        
-        
-        token.transfer(beneficiary, tokens);
-        
-        tokensSold = tokensSold.add(tokens);
-        
-        TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-        
-        forwardFunds();
+
+    /**
+     * EXTERNAL FUNCTION 
+     * 
+     * @dev Contribution contract instance mint token
+     * @param receipent The destination account owned mint tokens    
+     * @param amount The amount of mint token
+     * be sent to this address.
+     */    
+    function mintToken(address receipent, uint amount)
+        external
+        onlyMinter
+        maxWanTokenAmountNotReached(amount)
+        returns (bool)
+    {
+        require(now <= endTime);
+        lockedBalances[receipent] = lockedBalances[receipent].add(amount);
+        totalSupply = totalSupply.add(amount);
+        return true;
     }
-    
-    
-    
-    function forwardFunds() internal {
-        wallet.transfer(msg.value);
+
+    /*
+     * PUBLIC FUNCTIONS
+     */
+
+    /// @dev Locking period has passed - Locked tokens have turned into tradeable
+    ///      All tokens owned by receipent will be tradeable
+    function claimTokens(address receipent)
+        public
+        onlyMinter
+    {
+        balances[receipent] = balances[receipent].add(lockedBalances[receipent]);
+        lockedBalances[receipent] = 0;
     }
-    
-    
-    function validPurchase() internal constant returns (bool) {
-        uint256 current = block.number;
-        bool withinPeriod = now <= deadline;
-        bool nonZeroPurchase = msg.value != 0;
-        
-        return withinPeriod && nonZeroPurchase;
+
+    /*
+     * CONSTANT METHODS
+     */
+    function lockedBalanceOf(address _owner) constant returns (uint balance) {
+        return lockedBalances[_owner];
     }
-    
-    
-    function hasEnded() public constant returns (bool) {
-        return ( now > deadline);
-        
-        
-    }
-    
-    function tokenResend() onlyOwner {
-        
-        
-        
-        token.transfer(owner, token.balanceOf(this));
-    }
-    
 }
