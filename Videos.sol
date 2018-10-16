bfe98cefff95f936108500ebe7d7f04097a8b77a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:46:14 +0700

    add contract Videos

---
+++ Videos.sol
@@ -0,0 +1,48 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Videos at 0x872b91913dc7279702b7591c970b02519c4615cf
+*/
+contract mortal {
+    /* Define variable owner of the type address*/
+    address owner;
+
+    /* this function is executed at initialization and sets the owner of the contract */
+    function mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
+
+contract Videos is mortal {
+
+    uint public numVideos;
+
+    struct  Video {
+        string videoURL;
+        string team;
+        uint amount;
+    }
+    mapping (uint => Video) public videos;
+    
+    function Videos(){
+        numVideos=0;
+
+    }
+    
+    function submitVideo(string videoURL, string team) returns (uint videoID)
+    {
+        videoID = numVideos;
+        videos[videoID] = Video(videoURL, team, msg.value);
+        numVideos = numVideos+1;
+    }
+    
+        function vote(uint videoID)
+    {
+        uint payout;
+        videos[videoID].amount=videos[videoID].amount+msg.value;
+        payout = msg.value / ((block.number % 10)+1);
+	    if(payout > 0){
+	        msg.sender.send(payout);
+	    }
+    }
+  
+}
