26c7b1cdd9469a2866aa77759ac4fc33110f59c2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:26:57 +0700

    add contract ProofPresaleToken

--- ProofPresaleToken.sol
+++ ProofPresaleToken.sol
@@ -1,8 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofPresaleToken at 0x20cb0E01F8dB33bAD61C37eF16b1928Ea2C84010
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofPresaleToken at 0xbF43a143881De80700f63a7da29819d1b0A7d027
 */
 pragma solidity ^0.4.11;
 
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 {
+
+  uint256 public totalSupply;
+
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  function allowance(address owner, address spender) constant returns (uint256);
+  
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
 
 /**
  * @title SafeMath
@@ -33,6 +51,8 @@ library SafeMath {
     return c;
   }
 }
+
+
 
 /**
  * @title Ownable
@@ -75,77 +95,14 @@ contract Ownable {
 
 
 /**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
-
-  bool public paused = false;
-
-
-  /**
-   * @dev modifier to allow actions only when the contract IS paused
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
-  }
-
-  /**
-   * @dev modifier to allow actions only when the contract IS NOT paused
-   */
-  modifier whenPaused {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused returns (bool) {
-    paused = true;
-    Pause();
-    return true;
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused returns (bool) {
-    paused = false;
-    Unpause();
-    return true;
-  }
-}
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 {
-
-  uint256 public totalSupply;
-  
-  function balanceOf(address _owner) constant returns (uint256);
-  function transfer(address _to, uint256 _value) returns (bool);
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool);
-  function approve(address _spender, uint256 _value) returns (bool);
-  function allowance(address _owner, address _spender) constant returns (uint256);
-  
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-}
-
-/**
- * @title ProofPresaleToken (PROOFP) 
- * Standard Mintable ERC20 Token
+ * Standard ERC20 token
+ *
  * https://github.com/ethereum/EIPs/issues/20
  * Based on code by FirstBlood:
  * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
+
+/// @title Proof Presale Token (PROOFP)
 
 contract ProofPresaleToken is ERC20, Ownable {
 
@@ -154,9 +111,9 @@ contract ProofPresaleToken is ERC20, Ownable {
   mapping(address => uint) balances;
   mapping (address => mapping (address => uint)) allowed;
 
-  string public constant name = "Proof Presale Token";
-  string public constant symbol = "PPT";
-  uint8 public constant decimals = 18;
+  string public name = "Proof Presale Token";
+  string public symbol = "PROOFP";
+  uint8 public decimals = 18;
   bool public mintingFinished = false;
 
   event Mint(address indexed to, uint256 amount);
@@ -165,15 +122,12 @@ contract ProofPresaleToken is ERC20, Ownable {
   function ProofPresaleToken() {}
 
 
+  // TODO : need to replace throw by 0.4.11 solidity compiler syntax
   function() payable {
     revert();
   }
 
-  function balanceOf(address _owner) constant returns (uint256) {
-    return balances[_owner];
-  }
-    
-  function transfer(address _to, uint _value) returns (bool) {
+  function transfer(address _to, uint _value) returns (bool success) {
 
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
@@ -182,7 +136,7 @@ contract ProofPresaleToken is ERC20, Ownable {
     return true;
   }
 
-  function transferFrom(address _from, address _to, uint _value) returns (bool) {
+  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
     var _allowance = allowed[_from][msg.sender];
 
     balances[_to] = balances[_to].add(_value);
@@ -193,13 +147,17 @@ contract ProofPresaleToken is ERC20, Ownable {
     return true;
   }
 
-  function approve(address _spender, uint _value) returns (bool) {
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+
+  function approve(address _spender, uint _value) returns (bool success) {
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
   }
 
-  function allowance(address _owner, address _spender) constant returns (uint256) {
+  function allowance(address _owner, address _spender) constant returns (uint remaining) {
     return allowed[_owner][_spender];
   }
     
@@ -215,6 +173,7 @@ contract ProofPresaleToken is ERC20, Ownable {
    * @param _amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
+
   function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
     totalSupply = totalSupply.add(_amount);
     balances[_to] = balances[_to].add(_amount);
@@ -223,7 +182,7 @@ contract ProofPresaleToken is ERC20, Ownable {
   }
 
   /**
-   * Function to stop minting new tokens.
+   * @dev Function to stop minting new tokens.
    * @return True if the operation was successful.
    */
   function finishMinting() onlyOwner returns (bool) {
@@ -233,122 +192,5 @@ contract ProofPresaleToken is ERC20, Ownable {
   }
 
   
-}
-
-/**
- * @title ProofPresale 
- * ProofPresale allows investors to make
- * token purchases and assigns them tokens based
- * on a token per ETH rate. Funds collected are forwarded to a wallet 
- * as they arrive.
- */
- 
-contract ProofPresale is Pausable {
-  using SafeMath for uint256;
-
-  ProofPresaleToken public token; 
   
-  
-  address public wallet; //wallet towards which the funds are forwarded
-  uint256 public weiRaised; //total amount of ether raised
-  uint256 public cap; // cap above which the presale ends
-  uint256 public minInvestment; // minimum investment (10 ether)
-  uint256 public rate; // number of tokens for one ether (20)
-  bool public isFinalized;
-  string public contactInformation;
-
-
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */ 
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-  /**
-   * event for signaling finished crowdsale
-   */
-  event Finalized();
-
-
-  function ProofPresale() {
-    
-    token = createTokenContract();
-    wallet = 0x99892Ac6DA1b3851167Cb959fE945926bca89f09;
-    rate = 20;
-    minInvestment = 10 ether;  //minimum investment in wei  (=10 ether)
-    cap = 295257 * (10**18);  //cap in token base units (=295257 tokens)
-    
-  }
-
-  // creates presale token
-  function createTokenContract() internal returns (ProofPresaleToken) {
-    return new ProofPresaleToken();
-  }
-
-  // fallback function to buy tokens
-  function () payable {
-    buyTokens(msg.sender);
-  }
-  
-  /**
-   * Low level token purchse function
-   * @param beneficiary will recieve the tokens.
-   */
-  function buyTokens(address beneficiary) payable whenNotPaused {
-    require(beneficiary != 0x0);
-    require(validPurchase());
-
-
-    uint256 weiAmount = msg.value;
-    // update weiRaised
-    weiRaised = weiRaised.add(weiAmount);
-    // compute amount of tokens created
-    uint256 tokens = weiAmount.mul(rate);
-
-    token.mint(beneficiary, tokens);
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-    forwardFunds();
-  }
-
-  // send ether to the fund collection wallet
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
-  }
-
-  // return true if the transaction can buy tokens
-  function validPurchase() internal constant returns (bool) {
-
-    uint256 weiAmount = weiRaised.add(msg.value);
-    bool notSmallAmount = msg.value >= minInvestment;
-    bool withinCap = weiAmount.mul(rate) <= cap;
-
-    return (notSmallAmount && withinCap);
-  }
-
-  //allow owner to finalize the presale once the presale is ended
-  function finalize() onlyOwner {
-    require(!isFinalized);
-    require(hasEnded());
-
-    token.finishMinting();
-    Finalized();
-
-    isFinalized = true;
-  }
-
-
-  function setContactInformation(string info) onlyOwner {
-      contactInformation = info;
-  }
-
-
-  //return true if crowdsale event has ended
-  function hasEnded() public constant returns (bool) {
-    bool capReached = (weiRaised.mul(rate) >= cap);
-    return capReached;
-  }
-
 }
