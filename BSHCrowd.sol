10315cbe9c56f4043e32df55b3c4045373fcf9c1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:13:08 +0700

    add contract BSHCrowd

---
+++ BSHCrowd.sol
@@ -0,0 +1,77 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BSHCrowd at 0x07b1C32132C7A51C9ae92E1C401BC14726Fe719e
+*/
+pragma solidity ^0.4.17;
+
+/**
+ * ????
+ */
+contract BSHCrowd {
+    address public beneficiary = 0x5b218A74aAc7BcCB5dF3C73c5e2c9d7cf8834334; //?????
+    uint256 public fundingGoal = 9600 ether;  //????????ether
+    uint256 public amountRaised = 0; //???????? ???wei
+    bool public fundingGoalReached = false;  //??????
+    bool public crowdsaleClosed = false; //????
+
+    mapping(address => uint256) public balance; 
+
+    event GoalReached(address _beneficiary, uint _amountRaised);
+    event FundTransfer(address _backer, uint _amount, bool _isContribution);
+    event ReceiveFund(address _addr, uint _amount);
+
+    function BSHCrowd() public {
+    }
+
+    /**
+     * ??????????????
+     */
+    function () payable public {
+        //????????
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+
+        //???????
+        balance[msg.sender] += amount;
+
+        //??????
+        amountRaised += amount;
+
+        ReceiveFund(msg.sender, amount);
+    }
+
+    /**
+     * ????????????
+     */
+    function checkGoalReached() public {
+        if (amountRaised >= fundingGoal) {
+            //??????
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+                
+            //????
+            crowdsaleClosed = true;
+        }
+    }
+
+    /**
+     * ????
+     */
+    function closeCrowd() public {
+        if (beneficiary == msg.sender) {
+            crowdsaleClosed = true;
+        }
+    }
+
+    /**
+     * ??
+     */
+    function safeWithdrawal(uint256 _value) public {
+        if (beneficiary == msg.sender && _value > 0) {
+            if (beneficiary.send(_value)) {
+                FundTransfer(beneficiary, _value, false);
+            } else {
+                revert();
+            }
+        }
+    }
+}
