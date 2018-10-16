e612277a350b85bb49a511f590a0a2e29dba2bd2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:05:45 +0700

    add contract NetkillerToken

--- NetkillerToken.sol
+++ NetkillerToken.sol
@@ -1,12 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NetkillerToken at 0x9abcf16f6685fe1f79168534b1d30056c90b8a8a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NetkillerToken at 0xb2486fc307bffaad3bfe4df43288c4c5869589e7
 */
 pragma solidity ^0.4.21;
 
 contract NetkillerToken {
+  address public owner;
   string public name;
   string public symbol;
   uint public decimals;
+  uint256 public totalSupply;
 
   event Transfer(address indexed from, address indexed to, uint256 value);
 
@@ -14,10 +16,12 @@ contract NetkillerToken {
   mapping (address => uint256) public balanceOf;
 
   function NetkillerToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint decimalUnits) public {
-    balanceOf[msg.sender] = initialSupply;
+    owner = msg.sender;
+    balanceOf[msg.sender] = totalSupply;
     name = tokenName;
     symbol = tokenSymbol;
     decimals = decimalUnits;
+    totalSupply = initialSupply * 10 ** uint256(decimals);
   }
 
   /* Send coins */
