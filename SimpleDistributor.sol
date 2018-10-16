f20c9e63c097e5f1f547a222187f637a0d7d1791
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:47:55 +0700

    add contract SimpleDistributor

--- SimpleDistributor.sol
+++ SimpleDistributor.sol
@@ -1,11 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleDistributor at 0xd1d4ff98ce2fb26964997f25b86978a1412cb2a3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleDistributor at 0xde08a96e974bad18f64013d5b59b3c8e3f4621f8
 */
 pragma solidity ^0.4.20;
 
 contract Mintable {
     
   function mint(address _to, uint256 _amount) public returns (bool);
+  
+  function transfer(address to, uint256 value) public returns (bool);
   
 }
 
@@ -23,7 +25,8 @@ contract SimpleDistributor {
   function addReceivers(address[] receivers, uint[] balances) public {
     require(msg.sender == owner);
     for(uint i = 0; i < receivers.length; i++) {
-      token.mint(receivers[i], balances[i]);
+      token.mint(this, balances[i]);  
+      token.transfer(receivers[i], balances[i]);
     }
   } 
   
