cf5f2c8b3269e67a8656ee2c8b3da6ac5b7ece1a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:52:41 +0700

    add contract MintableToken

--- MintableToken.sol
+++ MintableToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintableToken at 0x07ba910f747b472ba4840c184f6b6e92b0e7495e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintableToken at 0x1E7b7f330da785ef66e33503fD93380137a9be7B
 */
 pragma solidity ^0.4.11;
 
@@ -127,7 +127,7 @@ contract StandardToken is ERC20, BasicToken {
     //  allowance to zero by calling `approve(_spender, 0)` if it is not
     //  already 0 to mitigate the race condition described here:
     //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    require((_value == 0) && (allowed[msg.sender][_spender] == 0)) ;
+    require ( !((_value != 0) && (allowed[msg.sender][_spender] != 0)));
 
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
@@ -183,9 +183,9 @@ contract Ownable {
 contract MintableToken is StandardToken, Ownable {
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
-  string public name = "Crystal Clear Token";
-  string public symbol = "CCT";
-  uint256 public decimals = 18;
+  string public name = "ICScoin";
+  string public symbol = "ICS";
+  uint256 public decimals = 10;
 
   bool public mintingFinished = false;
 
@@ -196,7 +196,7 @@ contract MintableToken is StandardToken, Ownable {
   }
 
   function MintableToken(){
-    mint(msg.sender,10000000000000000000000000);
+    mint(msg.sender,5000000000000000);
     finishMinting();
   }
     
@@ -213,7 +213,6 @@ contract MintableToken is StandardToken, Ownable {
     return true;
   }
   
-
   /**
    * @dev Function to stop minting new tokens.
    * @return True if the operation was successful.
@@ -223,4 +222,142 @@ contract MintableToken is StandardToken, Ownable {
     MintFinished();
     return true;
   }
+
+}
+
+
+contract Tokensale {
+    address public beneficiary;
+    uint public fundingGoal;
+    uint public amountRaised;
+    uint public deadline;
+    uint public price;
+    uint public minimumEntryThreshold;
+    address public devAddr; // developers team address (for reward)
+    MintableToken public tokenReward;
+    mapping(address => uint256) public balanceOf;
+    bool fundingGoalReached = false;
+    bool crowdsaleClosed = false;
+    bool public devPaid = false;
+    
+    // start and end block where investments are allowed (both inclusive)
+    uint256 public startTime;
+    uint256 public endTime;
+
+
+    event GoalReached(address beneficiary, uint amountRaised);
+    event FundTransfer(address backer, uint amount, bool isContribution);
+
+    /**
+     * Constrctor function
+     *
+     * Setup the owner
+     */
+    function Tokensale(
+    //    address ifSuccessfulSendTo,
+    //    uint fundingGoalInEthers,
+    //    uint finneyCostOfEachToken,
+        address addressOfTokenUsedAsReward
+     //   uint minimumEntryThresholdInFinney
+     //   address _devAddr
+    ) {
+        beneficiary = address(0x7486516460CdDC841ca7293C2a01328D359eB609);
+        fundingGoal = 4166 ether;
+        startTime = 1506600000; // September 28, 2017 12:00 (UTC)
+        endTime =   1507809600; // October 12, 2017 12:00 (UTC)
+        price = 833 finney;
+        minimumEntryThreshold = 1 ether;
+        tokenReward = MintableToken(addressOfTokenUsedAsReward);
+        devAddr = address(0x45e044ED9Bf130EafafA8095115Eda69FC3b0D20);
+    }
+
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+    function () payable {
+
+        require(validPurchase());
+        require(msg.value >= minimumEntryThreshold);
+        uint amount = msg.value;
+        uint tokens = amount * 10000000000 / price;
+        require( tokenReward.balanceOf(this) >= tokens);
+        balanceOf[msg.sender] += amount;
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, tokens);
+        FundTransfer(msg.sender, amount, true);
+    }
+
+    modifier afterDeadline() { if (now >  endTime) _; }
+    
+      // @return true if the transaction can buy tokens
+    function validPurchase() internal constant returns (bool) {
+        bool withinPeriod = now >= startTime && now <= endTime;
+        bool nonZeroPurchase = msg.value != 0;
+        return withinPeriod && nonZeroPurchase;// && maxTokensSold;
+    }
+
+    // @return true if crowdsale event has ended
+    function hasEnded() public constant returns (bool) {
+        return ( now > endTime );// || (tokensSold >= token.balanceOf(this)) ;
+    }
+    
+    /**
+     * Check if goal was reached
+     *
+     * Checks if the goal or time limit has been reached and ends the campaign
+     */
+    function checkGoalReached() afterDeadline {
+        if (amountRaised >= fundingGoal){
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+        }
+        crowdsaleClosed = true;
+    }
+
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal() afterDeadline {
+        
+        checkGoalReached();
+        
+        // sending reward for developers team
+        if ( !devPaid)  {
+            uint devReward;
+            if ( amountRaised >= 10 ether ) devReward = 10 ether; else devReward = amountRaised;
+            devAddr.transfer(devReward);
+            FundTransfer(devAddr, devReward, true);
+            devPaid = true;
+        }
+        
+        
+        if (!fundingGoalReached) {
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                if (msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+
+        if (fundingGoalReached && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised - 10 ether)) {
+                FundTransfer(beneficiary, amountRaised - 10 ether, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock funders balance
+                fundingGoalReached = false;
+            }
+        }
+        
+    }
 }
