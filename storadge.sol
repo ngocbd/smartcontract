533c4982cdbed9d1db4207c983482f77c617b1e5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:33 +0700

    add contract storadge

--- storadge.sol
+++ storadge.sol
@@ -1,8 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract storadge at 0xce1555c4fc4ddc2d87183d1a36e0a410e565624d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract storadge at 0xeb3c1a7f816da958a0f8b9f4ef63a19b26d03739
 */
+pragma solidity ^0.4.2;
+
 contract storadge {
+    
     event log(string description);
+    
 	function save(
         string mdhash
     )
