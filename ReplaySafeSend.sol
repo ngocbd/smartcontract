e4ea9ac3d8af2cfdfce2ec8a19da799b2ca9f929
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:45:27 +0700

    add contract ReplaySafeSend

---
+++ ReplaySafeSend.sol
@@ -0,0 +1,23 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSend at 0x40ebf2d6e998a76a848c41908733b26e04adffe2
+*/
+contract AmIOnTheFork {
+    function forked() constant returns(bool);
+}
+
+contract ReplaySafeSend {
+    // Fork oracle to use
+    AmIOnTheFork amIOnTheFork = AmIOnTheFork(0x2bd2326c993dfaef84f696526064ff22eba5b362);
+
+    function safeSend(address etcAddress) returns(bool) {
+        if (!amIOnTheFork.forked() && etcAddress.send(msg.value)) {
+            return true;
+        }
+        throw; // don't accept value transfer, otherwise it would be trapped.
+    }
+
+    // Reject value transfers.
+    function() {
+        throw;
+    }
+}
