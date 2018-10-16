75688496e761ce7baade4a2774fe278ff7635346
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:40:09 +0700

    add contract RefundVault

--- RefundVault.sol
+++ RefundVault.sol
@@ -1,20 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RefundVault at 0x265a63cfdf9a0a041ff0d4d33c3fd504da8958f4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RefundVault at 0x47c7a69677e3ded71fd13b94766d15daa4b825dd
 */
-pragma solidity ^0.4.17;
-/**
- * @title ERC20
- * @dev ERC20 interface
- */
-contract ERC20 {
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+pragma solidity ^0.4.18;
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -22,7 +9,6 @@ contract ERC20 {
  */
 contract Ownable {
   address public owner;
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
@@ -41,9 +27,8 @@ contract Ownable {
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner public {
+  function transferOwnership(address newOwner) onlyOwner {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
 }
@@ -51,48 +36,39 @@ contract Ownable {
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+contract SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
   }
-}
-/**
- * @title claim accidentally sent tokens
- */
-contract HasNoTokens is Ownable {
-    event ExtractedTokens(address indexed _token, address indexed _claimer, uint _amount);
-    /// @notice This method can be used to extract mistakenly
-    ///  sent tokens to this contract.
-    /// @param _token The address of the token contract that you want to recover
-    ///  set to 0 in case you want to extract ether.
-    /// @param _claimer Address that tokens will be send to
-    function extractTokens(address _token, address _claimer) onlyOwner public {
-        if (_token == 0x0) {
-            _claimer.transfer(this.balance);
-            return;
-        }
-        ERC20 token = ERC20(_token);
-        uint balance = token.balanceOf(this);
-        token.transfer(_claimer, balance);
-        ExtractedTokens(_token, _claimer, balance);
-    }
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
 }
 /**
  * @title RefundVault
@@ -100,40 +76,72 @@ contract HasNoTokens is Ownable {
  * is in progress. Supports refunding the money if crowdsale fails,
  * and forwarding it if crowdsale is successful.
  */
-contract RefundVault is Ownable, HasNoTokens {
-  using SafeMath for uint256;
+contract RefundVault is Ownable, SafeMath{
   enum State { Active, Refunding, Closed }
   mapping (address => uint256) public deposited;
-  address public wallet;
+  mapping (address => uint256) public refunded;
   State public state;
+  address[] public reserveWallet;
   event Closed();
   event RefundsEnabled();
   event Refunded(address indexed beneficiary, uint256 weiAmount);
-  function RefundVault(address _wallet) {
-    require(_wallet != 0x0);
-    wallet = _wallet;
+  /**
+   * @dev This constructor sets the addresses of
+   * 10 reserve wallets.
+   * and forwarding it if crowdsale is successful.
+   * @param _reserveWallet address[5] The addresses of reserve wallet.
+   */
+  function RefundVault(address[] _reserveWallet) {
     state = State.Active;
+    reserveWallet = _reserveWallet;
   }
-  function deposit(address investor) onlyOwner public payable {
+  /**
+   * @dev This function is called when user buy tokens. Only RefundVault
+   * contract stores the Ether user sent which forwarded from crowdsale
+   * contract.
+   * @param investor address The address who buy the token from crowdsale.
+   */
+  function deposit(address investor) onlyOwner payable {
     require(state == State.Active);
-    deposited[investor] = deposited[investor].add(msg.value);
+    deposited[investor] = add(deposited[investor], msg.value);
   }
-  function close() onlyOwner public {
+  event Transferred(address _to, uint _value);
+  /**
+   * @dev This function is called when crowdsale is successfully finalized.
+   */
+  function close() onlyOwner {
     require(state == State.Active);
     state = State.Closed;
+    uint256 balance = this.balance;
+    uint256 reserveAmountForEach = div(balance, reserveWallet.length);
+    for(uint8 i = 0; i < reserveWallet.length; i++){
+      reserveWallet[i].transfer(reserveAmountForEach);
+      Transferred(reserveWallet[i], reserveAmountForEach);
+    }
     Closed();
-    wallet.transfer(this.balance);
   }
-  function enableRefunds() onlyOwner public {
+  /**
+   * @dev This function is called when crowdsale is unsuccessfully finalized
+   * and refund is required.
+   */
+  function enableRefunds() onlyOwner {
     require(state == State.Active);
     state = State.Refunding;
     RefundsEnabled();
   }
-  function refund(address investor) public {
+  /**
+   * @dev This function allows for user to refund Ether.
+   */
+  function refund(address investor) returns (bool) {
     require(state == State.Refunding);
+    if (refunded[investor] > 0) {
+      return false;
+    }
     uint256 depositedValue = deposited[investor];
     deposited[investor] = 0;
+    refunded[investor] = depositedValue;
     investor.transfer(depositedValue);
     Refunded(investor, depositedValue);
+    return true;
   }
 }
