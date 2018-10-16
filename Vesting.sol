ef2870e73eb84de5fac8a34840267dae6631e053
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:42:13 +0700

    add contract Vesting

--- Vesting.sol
+++ Vesting.sol
@@ -1,144 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vesting at 0xb6021534da2709bd5215ed007e4925393922315b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vesting at 0xef11dcd9f162207d4e751d9afbdea3cb78e6efc1
 */
-pragma solidity 0.4.15;
+pragma solidity 0.4.19;
+/// @title ERC223 interface
+interface ERC223 {
 
-library SafeMath {
+    function totalSupply() public view returns (uint);
+    function name() public view returns (string);
+    function symbol() public view returns (string);
+    function decimals() public view returns (uint8);
+    function balanceOf(address _owner) public view returns (uint);
+    function transfer(address _to, uint _value) public returns (bool);
+    function transfer(address _to, uint _value, bytes _data) public returns (bool);
 
-    function mul(uint256 a, uint256 b) internal returns (uint256) {
-        if (a == 0) {
-          return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
+    event Transfer(address indexed _from, address indexed _to, uint indexed _value, bytes _data);
+}
 
-    function div(uint256 a, uint256 b) internal returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
+/// @title Interface for the contract that will work with ERC223 tokens.
+interface ERC223ReceivingContract { 
+    /**
+     * @dev Standard ERC223 function that will handle incoming token transfers.
+     *
+     * @param _from  Token sender address.
+     * @param _value Amount of tokens.
+     * @param _data  Transaction data.
+     */
+    function tokenFallback(address _from, uint _value, bytes _data) public;
+}
 
-    function sub(uint256 a, uint256 b) internal returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
 
-    function add(uint256 a, uint256 b) internal returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
 
 }
 
-contract Owned {
-    address public contractOwner;
-    address public pendingContractOwner;
 
-    function Owned() {
-        contractOwner = msg.sender;
+
+/**
+ * @title Vesting of DGTX tokens
+ * @dev Vesting contract allows to lock in DGTX tokens and withdraw them according to the predefined scheme.
+ *   The planned amount to lock-in is 100,000,000 DGTX.
+ * @author SmartDec
+ */
+contract Vesting is Ownable, ERC223ReceivingContract {
+    address public token;
+    uint public totalTokens = 0;
+    uint public constant FIRST_UNLOCK = 1531612800; // 15 July 2018 00:00 GMT
+    uint public constant TOTAL_TOKENS = 100000000 * (uint(10) ** 18); // 100 000 000 DGTX tokens
+    bool public tokenReceived = false;
+
+    event Withdraw(address _to, uint _value);
+
+    /**
+     * @param _token token that will be received by vesting
+     */
+    function Vesting(address _token) public Ownable() {
+        token = _token;
     }
 
-    modifier onlyContractOwner() {
-        if (contractOwner == msg.sender) {
-            _;
+    /**
+     * @dev Function to receive ERC223 tokens. Receives tokens once.
+     *   Checks that transfered amount is exactly as planned (100 000 000 DGTX)
+     * @param _value Number of transfered tokens in 10**(decimal)th
+     */
+    function tokenFallback(address, uint _value, bytes) public {
+        require(!tokenReceived);
+        require(msg.sender == token);
+        require(_value == TOTAL_TOKENS);
+        tokenReceived = true;
+    }
+
+    /**
+     * @dev withdraw less or equals than available tokens. Throws if there are not enough tokens available.
+     * @param _amount amount of tokens to withdraw.
+     */
+    function withdraw(uint _amount) public onlyOwner {
+        uint availableTokens = ERC223(token).balanceOf(this) - lockedAmount();
+        require(_amount <= availableTokens);
+        ERC223(token).transfer(msg.sender, _amount);
+        Withdraw(msg.sender, _amount);
+    }
+
+    /**
+     * @dev withdraw all available tokens.
+     */
+    function withdrawAll() public onlyOwner {
+        uint availableTokens = ERC223(token).balanceOf(this) - lockedAmount();
+        ERC223(token).transfer(msg.sender, availableTokens);
+        Withdraw(msg.sender, availableTokens);
+    }
+    
+    /**
+     * @dev Internal function that tells how many tokens are locked at the moment.
+     * @return {
+     *    "lockedTokens": "amount of locked tokens"
+     * }
+     */
+    function lockedAmount() internal view returns (uint) {
+        if (now < FIRST_UNLOCK) {
+            return TOTAL_TOKENS;  
         }
-    }
 
-    function changeContractOwnership(address _to) onlyContractOwner() returns(bool) {
-        pendingContractOwner = _to;
-        return true;
-    }
+        uint quarters = (now - FIRST_UNLOCK) / 0.25 years; // quarters past
+        uint effectiveQuarters = quarters <= 12 ? quarters : 12; // all tokens unlocked in 3 years after FIRST_UNLOCK
+        uint locked = TOTAL_TOKENS * (7500 - effectiveQuarters * 625) / 10000; // unlocks 25% plus 6.25% per quarter
 
-    function claimContractOwnership() returns(bool) {
-        if (pendingContractOwner != msg.sender) {
-            return false;
-        }
-        contractOwner = pendingContractOwner;
-        delete pendingContractOwner;
-        return true;
-    }
-}
-
-contract ERC20Interface {
-    function balanceOf(address _address) returns(uint);
-    function transfer(address _receiver, uint _amount) returns(bool);
-    function transferFrom(address _from, address _to, uint _amount) returns(bool);
-}
-
-contract Vesting is Owned {
-    struct Vestings {
-        address receiver;
-        ERC20Interface ERC20;
-        uint amount;
-        uint parts;
-        uint paymentInterval;
-        uint schedule;
-        uint sendings;
-    }
-
-    mapping (address => uint) public vestingBalance;
-    mapping (address => mapping (address => uint)) public receiverVestings;
-
-    Vestings[] public vestings;
-
-    event VestingCreated(address sender, address receiver, address ERC20, uint amount, uint id, uint parts, uint paymentInterval, uint schedule);
-    event VestingSent(address receiver, address ERC20, uint amount, uint id, uint sendings);
-    event ReceiverChanged(uint id, address from, address to);
-
-    function createVesting(address _receiver, ERC20Interface _ERC20, uint _amount, uint _parts, uint _paymentInterval, uint _schedule) returns(bool) {
-        require(_receiver != 0x0);
-        require(_parts > 0 && _amount > 0 && _parts <= 10000);
-        require(SafeMath.add(_schedule, SafeMath.mul(_paymentInterval, _parts)) <= ((365 * 5 days) + now));
-
-        vestings.push(Vestings(_receiver, _ERC20, _amount, _parts, _paymentInterval, _schedule, 0));
-        require(_ERC20.transferFrom(msg.sender, address(this), SafeMath.mul(_amount, _parts)));
-        vestingBalance[_ERC20] = SafeMath.add(vestingBalance[_ERC20], (_amount * _parts));
-        receiverVestings[_receiver][_ERC20] = SafeMath.add(receiverVestings[_receiver][_ERC20], (_amount * _parts));
-        VestingCreated(msg.sender, _receiver, _ERC20, _amount, (vestings.length - 1), _parts, _paymentInterval, _schedule);
-        return true;
-    }
-
-    function sendVesting(uint _id) returns(bool) {
-        require(now >= (vestings[_id].schedule + vestings[_id].paymentInterval * (vestings[_id].sendings + 1)));
-
-        require(vestings[_id].ERC20.transfer(vestings[_id].receiver, vestings[_id].amount));
-        VestingSent(vestings[_id].receiver, vestings[_id].ERC20, vestings[_id].amount, _id, vestings[_id].sendings);
-        vestings[_id].sendings++;
-        vestingBalance[vestings[_id].ERC20] -= vestings[_id].amount;
-        receiverVestings[vestings[_id].receiver][vestings[_id].ERC20] -= vestings[_id].amount;
-        if (vestings[_id].sendings == vestings[_id].parts) {
-            delete vestings[_id];
-        }
-        return true;
-    }
-
-    function changeReceiver(uint _id, address _newReceiver) returns(bool) {
-        require(_newReceiver != 0x0);
-        require(msg.sender == vestings[_id].receiver);
-
-        vestings[_id].receiver = _newReceiver;
-        ReceiverChanged(_id, msg.sender, _newReceiver);
-        return true;
-    }
-
-    function withdrawExtraTokens(ERC20Interface _ERC20) onlyContractOwner() returns(bool) {
-        require(_ERC20.transfer(contractOwner, getExtraTokens(_ERC20)));
-        return true;
-    }
-
-    function getVesting(uint _id) constant returns(address, address, uint, uint, uint, uint, uint) {
-        return (vestings[_id].receiver, vestings[_id].ERC20, vestings[_id].amount, vestings[_id].parts, vestings[_id].paymentInterval, vestings[_id].schedule, vestings[_id].sendings);
-    }
-
-    function getExtraTokens(ERC20Interface _ERC20) constant returns(uint) {
-        return (_ERC20.balanceOf(this) - vestingBalance[_ERC20]);
-    }
-
-    function getReceiverVesting(address _receiver, address _ERC20) constant returns(uint) {
-        return receiverVestings[_receiver][_ERC20];
+        return locked;
     }
 }
