89aab26fb3dc7a29a823630ec1ac5c8e665a02ec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:56:13 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,199 +1,54 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x51fdc3827865af9bde006a2ad6d593d4aca5e591
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xc1df55d279122d93f4965a17e205c0e74ea0c63e
 */
-pragma solidity ^ 0.4.17;
-
-library SafeMath {
-    function mul(uint a, uint b) pure internal returns(uint) {
-        uint c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+pragma solidity ^0.4.0;
+   
 
 
-    function sub(uint a, uint b) pure internal returns(uint) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint a, uint b) pure internal returns(uint) {
-        uint c = a + b;
-        assert(c >= a && c >= b);
-        return c;
-    }
-}
-
-contract ERC20 {
-    uint public totalSupply;
-
-    function balanceOf(address who) public view returns(uint);
-
-    function allowance(address owner, address spender) public view returns(uint);
-
-    function transfer(address to, uint value) public returns(bool ok);
-
-    function transferFrom(address from, address to, uint value) public returns(bool ok);
-
-    function approve(address spender, uint value) public returns(bool ok);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-
-contract Ownable {
-    address public owner;
-
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        if (newOwner != address(0)) 
-            owner = newOwner;
-    }
-
-    function kill() public {
-        if (msg.sender == owner) 
-            selfdestruct(owner);
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender == owner)
-            _;
-    }
-}
-
-
-
-
-// The ETHD Token 
-contract Token is ERC20,  Ownable {
-
-    using SafeMath for uint;
-    // Public variables of the token
+contract Token {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
     string public name;
     string public symbol;
-    uint8 public decimals; // How many decimals to show.
-    string public version = "v0.1";       
-    uint public totalSupply;
-    bool public locked;
-    address public crowdSaleAddress;
-    
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
 
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Token() {
 
-    // tokens are locked during the ICO. Allow transfer of tokens after ICO. 
-    modifier onlyUnlocked() {
-        if (msg.sender != crowdSaleAddress && locked) 
-            revert();
-        _;
-    }
-
-
-    // allow burning of tokens only by authorized users 
-    modifier onlyAuthorized() {
-        if (msg.sender != owner && msg.sender != crowdSaleAddress ) 
-            revert();
-        _;
-    }
-
-
-    // The Token 
-    function Token(address _crowdSaleAddress) public {
+         initialSupply = 5000;
+         name ="testIn";
+        decimals = 2;
+         symbol = "@";
         
-        locked = true;  // Lock the transfCrowdsaleer function during the crowdsale
-        totalSupply = 21000000e18; 
-        name = "Lottery Token"; // Set the name for display purposes
-        symbol = "ETHD"; // Set the symbol for display purposes
-        decimals = 18; // Amount of decimals for display purposes
-        crowdSaleAddress = _crowdSaleAddress;                                  
-        balances[crowdSaleAddress] = totalSupply;
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+                                   
     }
 
-    function unlock() public onlyAuthorized {
-        locked = false;
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
     }
 
-    function lock() public onlyAuthorized {
-        locked = true;
-    }
+   
+
     
 
-    function burn( address _member, uint256 _value) public onlyAuthorized returns(bool) {
-        balances[_member] = balances[_member].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        Transfer(_member, 0x0, _value);
-        return true;
+   
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
     }
-
-    function transfer(address _to, uint _value) public onlyUnlocked returns(bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    
-    function transferFrom(address _from, address _to, uint256 _value) public onlyUnlocked returns(bool success) {
-        require (balances[_from] >= _value); // Check if the sender has enough                            
-        require (_value <= allowed[_from][msg.sender]); // Check if allowed is greater or equal        
-        balances[_from] = balances[_from].sub(_value); // Subtract from the sender
-        balances[_to] = balances[_to].add(_value); // Add the same to the recipient
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function balanceOf(address _owner) public view returns(uint balance) {
-        return balances[_owner];
-    }
-
-
-    /**
-    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-    *
-    * Beware that changing an allowance with this method brings the risk that someone may use both the old
-    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-    * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    * @param _spender The address which will spend the funds.
-    * @param _value The amount of tokens to be spent.
-    */
-    function approve(address _spender, uint _value) public returns(bool) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-
-    function allowance(address _owner, address _spender) public constant returns(uint remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    /**
-    * approve should be called when allowed[_spender] == 0. To increment
-    * allowed value is better to use this function to avoid 2 calls (and wait until
-    * the first transaction is mined)
-    * From MonolithDAO Token.sol
-    */
-    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-        allowed[msg.sender][_spender] = 0;
-        } else {
-        allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
 }
