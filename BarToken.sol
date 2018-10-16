3b2c0a9148f9ebb9c684cb6744b2a35a1759dee3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:50:56 +0700

    add contract BARToken

--- BarToken.sol
+++ BarToken.sol
@@ -1,49 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BARToken at 0xc7579bb99af590ec71c316e1ac4436c535039594
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BARToken at 0xee8bd1502c3e9f6c543781467c01592ac51cfbb8
 */
 pragma solidity ^0.4.15;
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
+pragma solidity ^0.4.11;
 
 
 /**
@@ -89,7 +49,6 @@ contract ERC20Basic {
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-
 /**
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances.
@@ -127,7 +86,6 @@ contract BasicToken is ERC20Basic {
 
 
 
-
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -138,7 +96,6 @@ contract ERC20 is ERC20Basic {
   function approve(address spender, uint256 value) public returns (bool);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
 
 /**
  * @title Standard ERC20 token
@@ -226,6 +183,49 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
 
 /**
  * @title Mintable token
@@ -270,206 +270,15 @@ contract MintableToken is StandardToken, Ownable {
     return true;
   }
 }
-
-
-
 /**
  * @title The Titanium BAR Token contract
  * @dev The Titanium BAR Token contract
  * @dev inherits from MintableToken and Ownable by Zeppelin
- * @author James Connolly
  */
 contract BARToken is MintableToken {
 
-  string public constant name = "Titanium BAR Token";
-  string public constant symbol = "BAR";
+  string public constant name = "Titanium BAR";
+  string public constant symbol = "TBAR";
   uint8 public constant decimals = 18;
-
-}
-
-/**
- * @title BARTokenSale
- * @dev 
- * We add new features to a base crowdsale using multiple inheritance.
- * We are using the following extensions:
- * CappedCrowdsale - sets a max boundary for raised funds
- 
- *
- * The code is based on the contracts of Open Zeppelin and we add our contracts: BARTokenSale, and the BAR Token
- *
- * @author James Connolly
- */
-
-
-
-
-/**
- * @title Crowdsale
- * @dev Crowdsale is a base contract for managing a token crowdsale.
- * Crowdsales have a start and end timestamps, where investors can make
- * token purchases and the crowdsale will assign them tokens based
- * on a token per ETH rate. Funds collected are forwarded to a wallet
- * as they arrive.
- */
-contract Crowdsale {
-  using SafeMath for uint256;
-
-  // The token being sold
-  MintableToken public token;
-
-  // start and end timestamps where investments are allowed (both inclusive)
-  uint256 public startTime;
-  uint256 public endTime;
-
-  // address where funds are collected
-  address public wallet;
-
-  // how many token units a buyer gets per wei
-  uint256 public rate;
-
-  // amount of raised money in wei
-  uint256 public weiRaised;
-
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-  function Crowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) {
-    require(_startTime >= now);
-    require(_endTime >= _startTime);
-    require(_rate > 0);
-    require(_wallet != 0x0);
-
-    token = createTokenContract();
-    startTime = _startTime;
-    endTime = _endTime;
-    rate = _rate;
-    wallet = _wallet;
-  }
-
-  // creates the token to be sold.
-  // override this method to have crowdsale of a specific mintable token.
-  function createTokenContract() internal returns (MintableToken) {
-    return new MintableToken();
-  }
-
-
-  // fallback function can be used to buy tokens
-  function () payable {
-    buyTokens(msg.sender);
-  }
-
-  // low level token purchase function
-  function buyTokens(address beneficiary) public payable {
-    require(beneficiary != 0x0);
-    require(validPurchase());
-
-    address team = 0x65dC43D9a882793B37d7bbe4f29566cD037e4474;
-    address bounty = 0x65dC43D9a882793B37d7bbe4f29566cD037e4474;
-    address reserve = 0x65dC43D9a882793B37d7bbe4f29566cD037e4474;
-
-    uint256 weiAmount = msg.value;
-
-    // calculate token amount to be created
-    uint256 tokens = weiAmount.mul(rate);
-    uint256 bountyAmount = weiAmount.mul(50);
-    uint256 teamAmount = weiAmount.mul(100);
-    uint256 reserveAmount = weiAmount.mul(50);
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
-
-    token.mint(beneficiary, tokens);
-    token.mint(bounty, bountyAmount);
-    token.mint(team, teamAmount);
-    token.mint(reserve, reserveAmount);
-
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-
-    forwardFunds();
-  }
-
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
-  }
-
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal constant returns (bool) {
-    bool withinPeriod = now >= startTime && now <= endTime;
-    bool nonZeroPurchase = msg.value != 0;
-    return withinPeriod && nonZeroPurchase;
-  }
-
-  // @return true if crowdsale event has ended
-  function hasEnded() public constant returns (bool) {
-    return now > endTime;
-  }
-
-
-}
-
-/**
- * @title CappedCrowdsale
- * @dev Extension of Crowdsale with a max amount of funds raised
- */
-contract CappedCrowdsale is Crowdsale {
-  using SafeMath for uint256;
-
-  uint256 public cap;
-
-  function CappedCrowdsale(uint256 _cap) {
-    require(_cap > 0);
-    cap = _cap;
-  }
-
-  // overriding Crowdsale#validPurchase to add extra cap logic
-  // @return true if investors can buy at the moment
-  function validPurchase() internal constant returns (bool) {
-    bool withinCap = weiRaised.add(msg.value) <= cap;
-    return super.validPurchase() && withinCap;
-  }
-
-  // overriding Crowdsale#hasEnded to add cap logic
-  // @return true if crowdsale event has ended
-  function hasEnded() public constant returns (bool) {
-    bool capReached = weiRaised >= cap;
-    return super.hasEnded() || capReached;
-  }
-
-}
-
-contract BARTokenSale is CappedCrowdsale {
-
-  uint256 public _startTime = 1511049540;
-  uint256 public _endTime = 1511059540;
-  uint256 public _rate = 300;
-  uint256 public _cap = 116666000000000000000000;
-  address public _wallet = 0x0693de1E3eb349f1B4345F244f9a527A6B7822A77;
-
-  function BARTokenSale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, address _wallet)
-    CappedCrowdsale(_cap)
-    //FinalizableCrowdsale()
-    Crowdsale(_startTime, _endTime, _rate, _wallet)
-  {
-    
-    
-    
-    //wallet = 0x1020793A74CA39FA4DC03613D9153DC7CBA59765;
-    //As goal needs to be met for a successful crowdsale
-    //the value needs to less or equal than a cap which is limit for accepted funds
-    //require(_goal <= _cap);
-  }
-
-  function createTokenContract() internal returns (MintableToken) {
-    return new BARToken();
-  }
 
 }
