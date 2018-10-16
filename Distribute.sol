724f784b180971d0034b252aa7d09f5bb9dab225
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:46:18 +0700

    add contract Distribute

--- Distribute.sol
+++ Distribute.sol
@@ -1,116 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0x46d66b51247f8ab0dbc25cfbf5131203ee454521
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0x84004ca3b679b94d5c27c59d710e8201068bb93b
 */
 pragma solidity ^0.4.0;
+contract Distribute {
 
-/**
+    address public owner;
 
- * @title Ownable
-
- * @dev The Ownable contract has an owner address, and provides basic authorization control
-
- * functions, this simplifies the implementation of "user permissions".
-
- */
-
-contract Ownable {
-
-  address public owner;
-
-
-
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-
-
-
-  /**
-
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-
-   * account.
-
-   */
-
-  function Ownable () public {owner = msg.sender;
-
-  }
-
-
-
-
-
-  /**
-
-   * @dev Throws if called by any account other than the owner.
-
-   */
-
-  modifier onlyOwner() {
-
-    require(msg.sender == owner);
-
-    _;
-
-  }
-
-
-
-
-
-  /**
-
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-
-   * @param newOwner The address to transfer ownership to.
-
-   */
-
-  function transferOwnership(address newOwner) onlyOwner public {
-
-    require(newOwner != address(0));
-
-    OwnershipTransferred(owner, newOwner);
-
-    owner = newOwner;
-
-  }
-
-}
-
-
-
-contract token {function transfer (address receiver, uint _amount) public { } }
-
-
-
-contract Distribute is Ownable{
-
-  
-
-    token tokenReward = token(0xdd007278B667F6bef52fD0a4c23604aA1f96039a);
-
-
-
-  function register(address[] _addrs) public onlyOwner{
-
-    for(uint i = 0; i < _addrs.length; ++i){
-
-      tokenReward.transfer(_addrs[i],500*10**8);
-
+    constructor() public {
+        owner = msg.sender;
     }
 
-  }
+    //????  
+    function transferETHS(address[] _tos) payable public returns(bool) {
+        require(_tos.length > 0);
+        uint val = this.balance / _tos.length;
+        for (uint32 i = 0; i < _tos.length; i++) {
+            _tos[i].transfer(val);
+        }
+        return true;
+    }
 
-
-
-  function withdraw(uint _amount) public onlyOwner {
-
-    tokenReward.transfer(owner,_amount);
-
-  }
-
+    function () payable public {
+        owner.transfer(this.balance);
+    }
 }
