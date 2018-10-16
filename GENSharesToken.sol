6f513031b3a605b98bc99e5427954e24939de39f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:02:41 +0700

    add contract GENSharesToken

--- GENSharesToken.sol
+++ GENSharesToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GENSharesToken at 0x4721bccae510c30a43aea825e1ab0a28c37d7916
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GENSharesToken at 0x29548baf82278c4207dce57d1a44c4ef07981b03
 */
 pragma solidity ^0.4.18;
 
@@ -588,7 +588,7 @@ contract Deployer is Ownable {
 
     ico.setHardcap(206000000000000000000000);
     ico.addBonus(7,25);
-    ico.addBonus(7,10);
+    ico.addBonus(14,10);
     ico.setStart(1514163600);
     ico.setEnd(1517356800);
     ico.setWallet(0x65954fb8f45b40c9A60dffF3c8f4F39839Bf3596);
