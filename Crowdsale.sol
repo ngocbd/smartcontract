f0c6f166dcc25caf0d9a5e7f12af09a3eaf2859c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:48:55 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,102 +1,178 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x6f7d68ae5ecc966ff4415197de4756211deda0ff
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x96Ee9B27f822D71aE9cbF06773A878b41308C396
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.21;
 
-interface token {
-    function transfer(address receiver, uint amount);
+contract SafeMath {
+     function safeMul(uint a, uint b) internal pure returns (uint) {
+          uint c = a * b;
+          assert(a == 0 || c / a == b);
+          return c;
+     }
+
+     function safeSub(uint a, uint b) internal pure returns (uint) {
+          assert(b <= a);
+          return a - b;
+     }
+
+     function safeAdd(uint a, uint b) internal pure returns (uint) {
+          uint c = a + b;
+          assert(c>=a && c>=b);
+          return c;
+     }
 }
 
-contract Crowdsale {
-    address public beneficiary;
-    uint public fundingGoal;
-    uint public amountRaised;
-    uint public deadline;
-    uint public price;
-    token public tokenReward;
-    mapping(address => uint256) public balanceOf;
-    bool fundingGoalReached = false;
-    bool crowdsaleClosed = false;
 
-    event GoalReached(address recipient, uint totalAmountRaised);
-    event FundTransfer(address backer, uint amount, bool isContribution);
+contract Token is SafeMath {
+
+     
+     function transfer(address _to, uint256 _value) public;
+     function transferFrom(address _from, address _to, uint256 _value) public returns(bool);
+     function approve(address _spender, uint256 _amount) public returns (bool success);
+
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract Crowdsale is Token {
+
+    // Public and other variables of the token
+    address public owner;
+    string public name = "crowdsalenetworkplatform";
+    string public symbol = "CSNP";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 50000000 * 10 ** uint256(decimals);
+    
+    address internal foundersAddress;
+    address internal bonusAddress;
+    uint internal dayStart = now;
+
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
 
     /**
-     * Constructor function
+     * Constrctor function
      *
-     * Setup the owner
+     * Initializes contract with initial supply tokens to the creator of the contract
      */
-    function Crowdsale(
-        address ifSuccessfulSendTo,
-        uint fundingGoalInEthers,
-        uint durationInMinutes,
-        uint etherCostOfEachToken,
-        address addressOfTokenUsedAsReward
-    ) {
-        beneficiary = ifSuccessfulSendTo;
-        fundingGoal = fundingGoalInEthers * 1 ether;
-        deadline = now + durationInMinutes * 1 minutes;
-        price = etherCostOfEachToken * 1 ether;
-        tokenReward = token(addressOfTokenUsedAsReward);
+    function Crowdsale(address enterFoundersAddress, address enterBonusAddress) public {
+        foundersAddress = enterFoundersAddress;
+        bonusAddress = enterBonusAddress;
+        balanceOf[foundersAddress] = 12500000 * 10 ** uint256(decimals);
+        balanceOf[bonusAddress] = 18750000 * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply - (12500000 * 10 ** uint256(decimals)) - (18750000 * 10 ** uint256(decimals));                
+        owner = msg.sender;
+
+    }
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
     }
 
     /**
-     * Fallback function
-     *
-     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     * Internal transfer, only can be called by this contract
      */
-    function () payable {
-        require(!crowdsaleClosed);
-        uint amount = msg.value;
-        balanceOf[msg.sender] += amount;
-        amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount / price);
-        FundTransfer(msg.sender, amount, true);
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Subtract from the sender
+        balanceOf[_from] = safeSub(balanceOf[_from],_value);
+        // Add the same to the recipient
+        balanceOf[_to] = safeAdd(balanceOf[_to],_value);
+        emit Transfer(_from, _to, _value);
+
     }
 
-    modifier afterDeadline() { if (now >= deadline) _; }
-
     /**
-     * Check if goal was reached
+     * Transfer tokens
      *
-     * Checks if the goal or time limit has been reached and ends the campaign
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
      */
-    function checkGoalReached() afterDeadline {
-        if (amountRaised >= fundingGoal){
-            fundingGoalReached = true;
-            GoalReached(beneficiary, amountRaised);
+    function transfer(address _to, uint256 _value) public  {
+        if(now < (dayStart + 365 days)){
+            require(msg.sender != foundersAddress && tx.origin != foundersAddress);
         }
-        crowdsaleClosed = true;
+        
+        if(now < (dayStart + 180 days)){
+            require(msg.sender != bonusAddress && tx.origin != bonusAddress);
+        }
+        
+
+        _transfer(msg.sender, _to, _value);
     }
 
 
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        
+        if(now < (dayStart + 365 days)){
+            require(_from != foundersAddress);
+        }
+        
+        if(now < (dayStart + 180 days)){
+            require(_from != bonusAddress);
+        }
+
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    
     /**
-     * Withdraw the funds
-     *
-     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
-     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
-     * the amount they contributed.
-     */
-    function safeWithdrawal() afterDeadline {
-        if (!fundingGoalReached) {
-            uint amount = balanceOf[msg.sender];
-            balanceOf[msg.sender] = 0;
-            if (amount > 0) {
-                if (msg.sender.send(amount)) {
-                    FundTransfer(msg.sender, amount, false);
-                } else {
-                    balanceOf[msg.sender] = amount;
-                }
-            }
+    *   Set allowance for other address
+    *
+    *   also, to minimize the risk of the approve/transferFrom attack vector
+    *   approve has to be called twice in 2 separate transactions - once to
+    *   change the allowance to 0 and secondly to change it to the new allowance
+    *   value
+    *
+    *   @param _spender      approved address
+    *   @param _amount       allowance amount
+    *
+    *   @return true if the approval was successful
+    */
+    function approve(address _spender, uint256 _amount) public returns(bool success) {
+        require((_amount == 0) || (allowance[msg.sender][_spender] == 0));
+        
+        if(now < (dayStart + 365 days)){
+            require(msg.sender != foundersAddress && tx.origin != foundersAddress);
         }
-
-        if (fundingGoalReached && beneficiary == msg.sender) {
-            if (beneficiary.send(amountRaised)) {
-                FundTransfer(beneficiary, amountRaised, false);
-            } else {
-                //If we fail to send the funds to beneficiary, unlock funders balance
-                fundingGoalReached = false;
-            }
+        
+        if(now < (dayStart + 180 days)){
+            require(msg.sender != bonusAddress && tx.origin != bonusAddress);
         }
+        
+        
+        allowance[msg.sender][_spender] = _amount;
+        return true;
     }
+    
+        
+     
+
 }
