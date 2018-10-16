5f6fdc818c3f91dfbac016b84d2fc0c63ad06982
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:21:37 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x0e5ef10130e558f33b0cec77deb010a6b5c4cd80
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x5540bffe3b88df2b4e039b82753a98d7ff595fb9
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT92318' token contract
+    // 'ACT97114' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT92318
-    // Name        : ADZbuzz Pscho.party Community Token
+    // Symbol      : ACT97114
+    // Name        : ADZbuzz Petology.stream Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT92318";
-            name = "ADZbuzz Pscho.party Community Token";
+            symbol = "ACT97114";
+            name = "ADZbuzz Petology.stream Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
