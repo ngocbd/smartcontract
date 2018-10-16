b956394a2cc71b91fbe378d1f1cd369797591ed3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:47:27 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,113 +1,367 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xD9d09AB4b1d155DC17a3F919Ba003F4cF491E222
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x4420681bda548cddb90d0e2419b9c52ec2ca7c9f
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
-contract Token {
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function balanceOf(address _owner) public constant returns (uint256 balance);
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract Crowdsale {
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    address public crowdsaleOwner;
-    address public crowdsaleBeneficiary;
-    address public crowdsaleWallet;
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    uint public amountRaised;
-    uint public deadline;
-    uint public period;
-    uint public etherCost = 470;
-    uint public started;
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-    Token public rewardToken;
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    mapping(address => uint256) public balanceOf;
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  
+}
 
-    bool public fundingGoalReached = false;
-    bool public crowdsaleClosed = false;
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances. 
+ */
+contract BasicToken is ERC20Basic {
+    
+  using SafeMath for uint256;
 
-    event GoalReached(address recipient, uint totalAmountRaised);
-    event FundTransfer(address backer, uint amount, bool isContribution);
+  mapping(address => uint256) balances;
 
-    function Crowdsale(
-        address _beneficiaryThatOwnsTokens,
-        uint _durationInDays,
-        address _addressOfTokenUsedAsReward,
-        address _crowdsaleWallet
-    ) public {
-        crowdsaleOwner = msg.sender;
-        crowdsaleBeneficiary = _beneficiaryThatOwnsTokens;
-        deadline = now + _durationInDays * 1 days;
-        period = _durationInDays * 1 days / 3;
-        rewardToken = Token(_addressOfTokenUsedAsReward);
-        crowdsaleWallet = _crowdsaleWallet;
-        started = now;
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of. 
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
+
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
+
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) returns (bool) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifing the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    
+  address public owner;
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
+
+}
+
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is StandardToken {
+
+  /**
+   * @dev Burns a specific amount of tokens.
+   * @param _value The amount of token to be burned.
+   */
+  function burn(uint _value) public {
+    require(_value > 0);
+    address burner = msg.sender;
+    balances[burner] = balances[burner].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    Burn(burner, _value);
+  }
+
+  event Burn(address indexed burner, uint indexed value);
+
+}
+
+contract Misscoin is BurnableToken {
+    
+  string public constant name = "Misscoin";
+   
+  string public constant symbol = "MISC";
+    
+  uint32 public constant decimals = 18;
+
+  uint256 public INITIAL_SUPPLY = 1000000000 * 1 ether;
+
+  function Misscoin() {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    balances[0x49B25aDDdd6503d275375C7c261A444862360396]=150000000 * 1 ether;
+  }
+    
+}
+
+contract Crowdsale is Ownable {
+    
+  using SafeMath for uint;
+    
+  address multisig;
+
+  address restricted;
+
+  bool addtok=false;
+
+  Misscoin public token = new Misscoin();
+
+  uint start;
+    
+  uint period;
+
+  uint128 constant WAD = 10 ** 18;
+
+  mapping (uint => mapping (address => uint))  public  userBuys;
+  mapping (uint => uint)                       public  dailyTotals;
+  mapping (uint => mapping (address => bool))  public  claimed;
+
+   function Crowdsale() {
+      multisig = 0x49B25aDDdd6503d275375C7c261A444862360396;
+      restricted  = 0x49B25aDDdd6503d275375C7c261A444862360396;
+      start = 1512741600;
+      period = 150;
     }
 
-    function stageNumber() public constant returns (uint stage) {
-        require(now >= started);
-        uint result = 1  + (now - started) / period;
-        if (result > 3) {
-            result = 3;
+  modifier saleIsOn() {
+    require(now > start && now < start + period * 1 days);
+    _;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function wmul(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * y + WAD / 2) / WAD);
+    }
+
+    function wdiv(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * WAD + y / 2) / y);
+    }
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+   function cast(uint256 x) constant internal returns (uint128 z) {
+        assert((z = uint128(x)) == x);
+  }
+
+  function time() constant returns (uint) {
+        return block.timestamp;
+  }
+
+  function today() constant returns (uint) {
+        return dayFor(time());
+  }
+
+  function dayFor(uint timestamp) constant returns (uint) {
+
+        return timestamp < start
+            ? 0
+            : sub(timestamp, start) / 24 hours + 1;
+  }
+
+  function buyWithLimit(uint day, uint limit) payable saleIsOn {
+        assert(time() >= start && today() <= period);
+        assert(msg.value >= 0.001 ether);
+        
+        assert(day >= today());
+        assert(day <= period);
+
+        userBuys[day][msg.sender] += msg.value;
+        dailyTotals[day] += msg.value;
+
+        if (limit != 0) {
+            assert(dailyTotals[day] <= limit);
         }
-        stage = result;
+
+  }
+
+    function addtokens() onlyOwner{
+      assert(today() >= 149 && !addtok);
+      token.transfer(0x49B25aDDdd6503d275375C7c261A444862360396, 100000000 * 1 ether);
+      addtok=true;
     }
 
-    function calcTokenCost() public constant returns (uint tokenCost) {
-        /* How many WEIs in half dollar */
-        uint halfDollar = 1 ether / etherCost / 2;
-        /* Get current stage for discount calculation */
-        uint stage = stageNumber();
-        /* For first stage price is 2 dollars, for second stage is 2.5 dollars & 3 dollars for others */
-        if (stage == 1) {
-            tokenCost = halfDollar * 4;
-        } else if (stage == 2) {
-            tokenCost = halfDollar * 5;
-        } else {
-            tokenCost = halfDollar * 6;
+    function buy() payable {
+       buyWithLimit(today(), 0);
+    }
+
+    function () payable {
+       buy();
+    }
+  
+  function claim(uint day) saleIsOn {
+        assert(today() > day);
+
+        if (claimed[day][msg.sender] || dailyTotals[day] == 0) {
+            return;
         }
-    }
 
-    function () public payable {
-        /* Crowdsale shouldn't be closed */
-        require(!crowdsaleClosed);
-        /* Calculate & check number of tokens for that amount */
-        uint amount = msg.value;
-        uint tokens = amount / calcTokenCost();
-        require(tokens > 0);
-        /* Increase user's amount of WEI in crowdsale */
-        balanceOf[msg.sender] += amount;
-        amountRaised += amount;
-        /* Transfer allowed tokens from crowdsale owner to sender */
-        rewardToken.transferFrom(crowdsaleWallet, msg.sender, tokens);
-        FundTransfer(msg.sender, amount, true);
-        /* Check has goal been reached */
-        checkGoalReached();
-    }
+       
 
-    function checkGoalReached() public {
-        uint256 tokensLeft = rewardToken.balanceOf(crowdsaleWallet);
-        if (tokensLeft == 0) {
-            fundingGoalReached = true;
-            crowdsaleClosed = true;
-            GoalReached(crowdsaleBeneficiary, amountRaised);
-        } else if (now >= deadline) {
-            crowdsaleClosed = true;
-            GoalReached(crowdsaleBeneficiary, amountRaised);
+        var dailyTotal = cast(dailyTotals[day]);
+        var userTotal  = cast(userBuys[day][msg.sender]);
+        var price      = wdiv(cast(5000000), dailyTotal);
+        var reward     = wmul(price, userTotal);
+
+        claimed[day][msg.sender] = true;
+        token.transfer(msg.sender, reward * 1 ether);
+
+  } 
+
+  function claimAll() {
+        for (uint i = 0; i < today(); i++) {
+            claim(i);
         }
-    }
+  }
 
-    function withdraw() public {
-        require(crowdsaleBeneficiary == msg.sender);
-        if (crowdsaleBeneficiary.send(amountRaised)) {
-            FundTransfer(crowdsaleBeneficiary, amountRaised, false);
-        }
-    }
+  function collect() onlyOwner{
+        assert(today() > 0); // Prevent recycling during window 0
+        multisig.transfer(this.balance);
+  }
 
-    function updateEtherCost(uint _etherCost) public {
-        require(msg.sender == crowdsaleOwner);
-        etherCost = _etherCost;
-    }
+    
 }
