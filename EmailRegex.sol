45195921146ad0c4fa99c6eea83c26a404b2cebf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:17:13 +0700

    add contract EmailRegex

--- EmailRegex.sol
+++ EmailRegex.sol
@@ -1,177 +1,146 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmailRegex at 0xed1c69227e2bccc22e2d0a3e61b45be032c70fb9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmailRegex at 0x537837d00047c874d19b68e94adba107674c21b8
 */
 pragma solidity ^0.4.11;
 
 contract EmailRegex {
   struct State {
     bool accepts;
-    function (byte) constant internal returns (uint) func;
+    function (byte) constant internal returns (State memory) func;
   }
 
-  string public constant regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}";
+  string public constant regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-_]+\\.[a-zA-Z]{2,}";
 
-  function state(uint id) constant internal returns (State) {
-    if (id == 1) {
-      return State(false, s1);
-    }
-    if (id == 2) {
+  function s0(byte c) constant internal returns (State memory) {
+    c = c;
+    return State(false, s0);
+  }
+
+  function s1(byte c) constant internal returns (State memory) {
+    if (c == 37 || c == 43 || c == 45 || c == 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c == 95 || c >= 97 && c <= 122) {
       return State(false, s2);
     }
-    if (id == 3) {
+
+    return State(false, s0);
+  }
+
+  function s2(byte c) constant internal returns (State memory) {
+    if (c == 37 || c == 43 || c == 45 || c == 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c == 95 || c >= 97 && c <= 122) {
       return State(false, s3);
     }
-    if (id == 4) {
+    if (c == 64) {
       return State(false, s4);
     }
-    if (id == 5) {
+
+    return State(false, s0);
+  }
+
+  function s3(byte c) constant internal returns (State memory) {
+    if (c == 37 || c == 43 || c == 45 || c == 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c == 95 || c >= 97 && c <= 122) {
+      return State(false, s3);
+    }
+    if (c == 64) {
+      return State(false, s4);
+    }
+
+    return State(false, s0);
+  }
+
+  function s4(byte c) constant internal returns (State memory) {
+    if (c >= 46 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
       return State(false, s5);
     }
-    if (id == 6) {
+
+    return State(false, s0);
+  }
+
+  function s5(byte c) constant internal returns (State memory) {
+    if (c == 46) {
       return State(false, s6);
     }
-    if (id == 7) {
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
       return State(false, s7);
     }
-    if (id == 8) {
+
+    return State(false, s0);
+  }
+
+  function s6(byte c) constant internal returns (State memory) {
+    if (c == 46) {
+      return State(false, s6);
+    }
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
+      return State(false, s7);
+    }
+    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
       return State(false, s8);
     }
-    if (id == 9) {
+
+    return State(false, s0);
+  }
+
+  function s7(byte c) constant internal returns (State memory) {
+    if (c == 46) {
+      return State(false, s6);
+    }
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
+      return State(false, s7);
+    }
+
+    return State(false, s0);
+  }
+
+  function s8(byte c) constant internal returns (State memory) {
+    if (c == 46) {
+      return State(false, s6);
+    }
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
+      return State(false, s7);
+    }
+    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
       return State(true, s9);
     }
-    if (id == 10) {
+
+    return State(false, s0);
+  }
+
+  function s9(byte c) constant internal returns (State memory) {
+    if (c == 46) {
+      return State(false, s6);
+    }
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
+      return State(false, s7);
+    }
+    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
       return State(true, s10);
     }
+
+    return State(false, s0);
+  }
+
+  function s10(byte c) constant internal returns (State memory) {
+    if (c == 46) {
+      return State(false, s6);
+    }
+    if (c == 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
+      return State(false, s7);
+    }
+    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
+      return State(true, s10);
+    }
+
+    return State(false, s0);
   }
 
   function matches(string input) constant returns (bool) {
-    uint cur = 1;
+    var cur = State(false, s1);
 
-    for (var i = 0; i < bytes(input).length; i++) {
+    for (uint i = 0; i < bytes(input).length; i++) {
       var c = bytes(input)[i];
 
-      cur = state(cur).func(c);
-      if (cur == 0) {
-        return false;
-      }
+      cur = cur.func(c);
     }
 
-    return state(cur).accepts;
-  }
-
-  function s1(byte c) constant internal returns (uint) {
-    if (c >= 37 && c <= 37 || c >= 43 && c <= 43 || c >= 45 && c <= 45 || c >= 46 && c <= 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c >= 95 && c <= 95 || c >= 97 && c <= 122) {
-      return 2;
-    }
-
-    return 0;
-  }
-
-  function s2(byte c) constant internal returns (uint) {
-    if (c >= 37 && c <= 37 || c >= 43 && c <= 43 || c >= 45 && c <= 45 || c >= 46 && c <= 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c >= 95 && c <= 95 || c >= 97 && c <= 122) {
-      return 3;
-    }
-    if (c >= 64 && c <= 64) {
-      return 4;
-    }
-
-    return 0;
-  }
-
-  function s3(byte c) constant internal returns (uint) {
-    if (c >= 37 && c <= 37 || c >= 43 && c <= 43 || c >= 45 && c <= 45 || c >= 46 && c <= 46 || c >= 48 && c <= 57 || c >= 65 && c <= 90 || c >= 95 && c <= 95 || c >= 97 && c <= 122) {
-      return 3;
-    }
-    if (c >= 64 && c <= 64) {
-      return 4;
-    }
-
-    return 0;
-  }
-
-  function s4(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
-      return 5;
-    }
-
-    return 0;
-  }
-
-  function s5(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
-      return 7;
-    }
-
-    return 0;
-  }
-
-  function s6(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
-      return 7;
-    }
-    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
-      return 8;
-    }
-
-    return 0;
-  }
-
-  function s7(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 65 && c <= 90 || c >= 91 && c <= 95 || c >= 97 && c <= 122) {
-      return 7;
-    }
-
-    return 0;
-  }
-
-  function s8(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
-      return 7;
-    }
-    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
-      return 9;
-    }
-
-    return 0;
-  }
-
-  function s9(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
-      return 7;
-    }
-    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
-      return 10;
-    }
-
-    return 0;
-  }
-
-  function s10(byte c) constant internal returns (uint) {
-    if (c >= 46 && c <= 46) {
-      return 6;
-    }
-    if (c >= 47 && c <= 47 || c >= 48 && c <= 57 || c >= 58 && c <= 64 || c >= 91 && c <= 95) {
-      return 7;
-    }
-    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
-      return 10;
-    }
-
-    return 0;
+    return cur.accepts;
   }
 }
