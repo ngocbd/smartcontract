08ab5ddb6e15d74665150ddae24802faadeaf5d7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:14:05 +0700

    add contract KittyFurnace

---
+++ KittyFurnace.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KittyFurnace at 0x6a5c06692b34e9e5f7af8bbc6463c4add1459f1c
+*/
+pragma solidity ^0.4.4;
+
+
+contract KittyFurnace {
+    //Nothing Fucking happens
+
+}
