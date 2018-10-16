5669d3ee8a762ff60122a8e2f229595fab4684cc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:16:39 +0700

    add contract Guestbook

--- Guestbook.sol
+++ Guestbook.sol
@@ -1,28 +1,62 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Guestbook at 0x20C3e6A68A5c7d6858142624ab4E993be8d288FE
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Guestbook at 0x322909bb3aa921f101d829c0edf57493468d9bd4
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.18;
 
-// A simple decentralized guestbook.
 contract Guestbook {
-  address creator;
+    struct Entry{
+        // structure for an guestbook entry
+        address owner;
+        string alias;
+        uint timestamp;
+        uint donation;
+        string message;
+    }
 
-  event Post(address indexed _from, string _name, string _body);
+    address public owner; // Observeth owner
+    address public donationWallet; // wallet to store donations
+    
+    uint public running_id = 0; // number of guestbook entries
+    mapping(uint=>Entry) public entries; // guestbook entries
+    uint public minimum_donation = 0; // to prevent spam in the guestbook
 
-  function Guestbook() {
-    creator = msg.sender;
-  }
+    function Guestbook() public { // called at creation of contract
+        owner = msg.sender;
+        donationWallet = msg.sender;
+    }
+    
+    function() payable public {} // fallback function
 
-  function post(string _name, string _body) {
-    require(bytes(_name).length > 0);
-    require(bytes(_body).length > 0);
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-    Post(msg.sender, _name, _body);
-  }
+    function changeDonationWallet(address _new_storage) public onlyOwner {
+        donationWallet = _new_storage; 
+    }
 
-  function destroy() {
-    require(msg.sender == creator);
+    function changeOwner(address _new_owner) public onlyOwner {
+        owner = _new_owner;
+    }
 
-    selfdestruct(creator);
-  }
+    function changeMinimumDonation(uint _minDonation) public onlyOwner {
+        minimum_donation = _minDonation;
+    }
+
+    function destroy() onlyOwner public {
+        selfdestruct(owner);
+    }
+
+    function createEntry(string _alias, string _message) payable public {
+        require(msg.value > minimum_donation); // entries only for those that donate something
+        entries[running_id] = Entry(msg.sender, _alias, block.timestamp, msg.value, _message);
+        running_id++;
+        donationWallet.transfer(msg.value);
+    }
+
+    function getEntry(uint entry_id) public constant returns (address, string, uint, uint, string) {
+        return (entries[entry_id].owner, entries[entry_id].alias, entries[entry_id].timestamp,
+                entries[entry_id].donation, entries[entry_id].message);
+    }
 }
