86ed233546af2a364d04657b7059541eeb07c8d3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:46:37 +0700

    add contract FloraFicToken

--- FloraFicToken.sol
+++ FloraFicToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FloraFicToken at 0x6063f73fd4701ae7ffe7e5d990f2804538a4c199
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FloraFicToken at 0x0dd83b5013b2ad7094b1a7783d96ae0168f82621
 */
 pragma solidity ^0.4.17;
 
@@ -272,161 +272,12 @@ contract Ownable {
 
 }
 
-/**
- * @title TokenVesting
- * @dev A token holder contract that can release its token balance gradually like a
- * typical vesting scheme, with a cliff and vesting period. Optionally revocable by the
- * owner.
- */
-contract TokenVesting is Ownable {
-    using SafeMath for uint256;
-    using SafeERC20 for ERC20Basic;
-
-    event Released(uint256 amount);
-    event Revoked();
-
-    // beneficiary of tokens after they are released
-    address public beneficiary;
-
-    uint256 public cliff;
-    uint256 public start;
-    uint256 public duration;
-
-    bool public revocable;
-
-    mapping (address => uint256) public released;
-    mapping (address => bool) public revoked;
-
-    /**
-     * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
-     * _beneficiary, gradually in a linear fashion until _start + _duration. By then all
-     * of the balance will have vested.
-     * @param _beneficiary address of the beneficiary to whom vested tokens are transferred
-     * @param _cliff duration in seconds of the cliff in which tokens will begin to vest
-     * @param _duration duration in seconds of the period in which the tokens will vest
-     * @param _revocable whether the vesting is revocable or not
-     */
-    function TokenVesting(address _beneficiary, uint256 _start, uint256 _cliff, uint256 _duration, bool _revocable) public {
-        require(_beneficiary != address(0));
-        require(_cliff <= _duration);
-
-        beneficiary = _beneficiary;
-        revocable = _revocable;
-        duration = _duration;
-        cliff = _start.add(_cliff);
-        start = _start;
-    }
-
-    /**
-     * @notice Transfers vested tokens to beneficiary.
-     * @param token ERC20 token which is being vested
-     */
-    function release(ERC20Basic token) public {
-        uint256 unreleased = releasableAmount(token);
-
-        require(unreleased > 0);
-
-        released[token] = released[token].add(unreleased);
-
-        token.safeTransfer(beneficiary, unreleased);
-
-        emit Released(unreleased);
-    }
-
-    /**
-     * @notice Allows the owner to revoke the vesting. Tokens already vested
-     * remain in the contract, the rest are returned to the owner.
-     * @param token ERC20 token which is being vested
-     */
-    function revoke(ERC20Basic token) public onlyOwner {
-        require(revocable);
-        require(!revoked[token]);
-
-        uint256 balance = token.balanceOf(this);
-
-        uint256 unreleased = releasableAmount(token);
-        uint256 refund = balance.sub(unreleased);
-
-        revoked[token] = true;
-
-        token.safeTransfer(owner, refund);
-
-        emit Revoked();
-    }
-
-    /**
-     * @dev Calculates the amount that has already vested but hasn't been released yet.
-     * @param token ERC20 token which is being vested
-     */
-    function releasableAmount(ERC20Basic token) public view returns (uint256) {
-        return vestedAmount(token).sub(released[token]);
-    }
-
-    /**
-     * @dev Calculates the amount that has already vested.
-     * @param token ERC20 token which is being vested
-     */
-    function vestedAmount(ERC20Basic token) public view returns (uint256) {
-        uint256 currentBalance = token.balanceOf(this);
-        uint256 totalBalance = currentBalance.add(released[token]);
-
-        if (now < cliff) {
-            return 0;
-        } else if (now >= start.add(duration) || revoked[token]) {
-            return totalBalance;
-        } else {
-            return totalBalance.mul(now.sub(start)).div(duration);
-        }
-    }
-}
-
-/**
- * @title TokenTimelock
- * @dev TokenTimelock is a token holder contract that will allow a
- * beneficiary to extract the tokens after a given release time
- */
-contract TokenTimelock {
-    using SafeERC20 for ERC20Basic;
-
-    // ERC20 basic token contract being held
-    ERC20Basic public token;
-
-    // beneficiary of tokens after they are released
-    address public beneficiary;
-
-    // timestamp when token release is enabled
-    uint256 public releaseTime;
-
-    function TokenTimelock(ERC20Basic _token, address _beneficiary, uint256 _releaseTime) public {
-        require(_releaseTime > now);
-        token = _token;
-        beneficiary = _beneficiary;
-        releaseTime = _releaseTime;
-    }
-
-    /**
-     * @notice Transfers tokens held by timelock to beneficiary.
-     */
-    function release() public {
-        require(now >= releaseTime);
-
-        uint256 amount = token.balanceOf(this);
-        require(amount > 0);
-
-        token.safeTransfer(beneficiary, amount);
-    }
-}
-
 contract FloraFicToken is StandardToken, Ownable {
 
-    string public name = 'Flora Fic';
+    string public name = 'FIC';
     string public symbol = 'FIC';
-    uint8 public decimals = 6;
-
-    uint256 public constant INITIAL_SUPPLY = 5400000000000000;
-
-    event Vesting(address _indexed);
-    event Timelock(address _indexed);
+    uint8 public decimals = 18;
+    uint256 public constant INITIAL_SUPPLY = 5400000000000000000000000000;
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
@@ -435,21 +286,6 @@ contract FloraFicToken is StandardToken, Ownable {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
         emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-    }
-
-    function transferVesting(address _beneficiary, uint256 _start, uint256 _cliff, uint256 _duration) public
-    onlyOwner returns (TokenVesting) {
-        TokenVesting tokenVesting = new TokenVesting(_beneficiary, _start, _cliff, _duration, true);
-        tokenVesting.transferOwnership(owner);
-        emit Vesting(tokenVesting);
-        return tokenVesting;
-    }
-
-    function transferTimelocked(address _to, uint256 _releaseTime) public
-    onlyOwner returns (TokenTimelock) {
-        TokenTimelock timelock = new TokenTimelock(this, _to, _releaseTime);
-        emit Timelock(timelock);
-        return timelock;
     }
 
 }
