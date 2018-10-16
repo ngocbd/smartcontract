0e85beac9cd28a5aa12cf512b806b524e1ead8a0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:17 +0700

    add contract Sweeper

---
+++ Sweeper.sol
@@ -0,0 +1,38 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sweeper at 0xa43ebd8939d8328f5858119a3fb65f65c864c6dd
+*/
+pragma solidity ^0.4.4;
+/*
+* This is a contract for debloating the state
+* @author mhswende
+**/
+contract Sweeper
+{
+    //Solidity implentation
+    function sol_clean(uint256 s, uint i){
+        uint x = s;
+        address b = 0;
+        for(uint c=0 ; c < i ; c++){
+            x = x+s;
+            b = address(x/0x1000000000000000000000000);
+            b.send(0);
+
+        }
+    }
+    //Asm implementation
+    function asm_clean(uint s, uint i)
+    {
+
+        assembly{
+            let seed := calldataload(4)//4 if we're using a named function
+            let iterations := calldataload(36)
+            let target :=seed
+        
+        loop:
+            target := add(target,seed)
+            pop(call(0,div(target,0x1000000000000000000000000),0,0,0,0,0))
+            iterations := sub(iterations,1) 
+            jumpi(loop, iterations)
+        }
+    }
+}
