1fec81bbc9f155fce7e041e1d30857b59a5ae3c4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:11:00 +0700

    add contract CrowdSale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,282 +1,213 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xa485cb306cef5e1871f15851a6d46c7fda951107
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdSale at 0xACbfB851D69EF8Eca1453d82f38B590268ca46B1
 */
 pragma solidity ^0.4.16;
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+contract token { function transfer(address receiver, uint amount) public ;
+                 function mintToken(address target, uint mintedAmount) public ;
+                }
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-    
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-  
-}
-
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances. 
- */
-contract BasicToken is ERC20Basic {
-    
-  using SafeMath for uint256;
-
-  mapping(address => uint256) balances;
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) returns (bool) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of. 
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) constant returns (uint256 balance) {
-    return balances[_owner];
-  }
-
-}
-
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-contract StandardToken is ERC20, BasicToken {
-
-  mapping (address => mapping (address => uint256)) allowed;
-
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amout of tokens to be transfered
-   */
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
-    var _allowance = allowed[_from][msg.sender];
-
-    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-    // require (_value <= _allowance);
-
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    Transfer(_from, _to, _value);
-    return true;
-  }
-
-  /**
-   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) returns (bool) {
-
-    // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender, 0)` if it is not
-    //  already 0 to mitigate the race condition described here:
-    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifing the amount of tokens still available for the spender.
-   */
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-  }
-
-}
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-    
-  address public owner;
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));      
-    owner = newOwner;
-  }
-
-}
-
-/**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
- */
-contract BurnableToken is StandardToken {
-
-  /**
-   * @dev Burns a specific amount of tokens.
-   * @param _value The amount of token to be burned.
-   */
-  function burn(uint _value) public {
-    require(_value > 0);
-    address burner = msg.sender;
-    balances[burner] = balances[burner].sub(_value);
-    totalSupply = totalSupply.sub(_value);
-    Burn(burner, _value);
-  }
-
-  event Burn(address indexed burner, uint indexed value);
-
-}
-
-contract SimpleCoinToken is BurnableToken {
-    
-  string public constant name = "Bitcoin Bank token";
-   
-  string public constant symbol = "MBB";
-    
-  uint32 public constant decimals = 18;
-
-  uint256 public INITIAL_SUPPLY = 50000000 * 1 ether;
-
-  function SimpleCoinToken() {
-    totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-  }
-    
-}
-
-contract Crowdsale is Ownable {
-    
-  using SafeMath for uint;
-    
-  address multisig;
-
-  uint restrictedPercent;
-
-  address restricted;
-
-  SimpleCoinToken public token = new SimpleCoinToken();
-
-  uint start;
-    
-  uint period;
-
-  uint rate;
-
-  function Crowdsale() {
-    multisig = 0xd347c4Ee7B3849E62E1211b6d144a699999C9b3c;
-    restricted = 0x085619ADf1FdC68b12aB7d2F2427a9a7aB2b4D37;
-    restrictedPercent = 40;
-    rate = 1000*1 ether;
-    start = 1517832000;
-    period = 70;
-  }
-
-  modifier saleIsOn() {
-    require(now > start && now < start + period * 1 days);
-    _;
-  }
-
-  function createTokens() saleIsOn payable {
-    multisig.transfer(msg.value);
-    uint tokens = rate.mul(msg.value).div(1 ether);
-    uint bonusTokens = 0;
-     if(now <= start + 20 days) {
-      bonusTokens = tokens.div(5);
-    } else if(now > start + 20 days && now < start + 35 days) {
-      bonusTokens = tokens.div(10);
-    } else if(now >= start + 35 days && now < start + 50 days) {
-      bonusTokens = tokens.div(20);
+contract CrowdSale {
+    enum State {
+        Fundraising,
+        Failed,
+        Successful,
+        Closed
     }
-    uint tokensWithBonus = tokens.add(bonusTokens);
-    token.transfer(msg.sender, tokensWithBonus);
-    uint restrictedTokens = tokens.mul(restrictedPercent).div(100 - restrictedPercent);
-    token.transfer(restricted, restrictedTokens);
-  }
+    State public state = State.Fundraising;
 
-  function() external payable {
-    createTokens();
-  }
+    struct Contribution {
+        uint amount;
+        address contributor;
+    }
+    Contribution[] contributions;
+
     
+    
+    uint public totalRaised;
+    uint public currentBalance;
+    uint public deadline;
+    uint public completedAt;
+    uint public priceInWei;
+    uint public fundingMinimumTargetInWei; 
+    uint public fundingMaximumTargetInWei; 
+    token public tokenReward;
+    address public creator;
+    address public beneficiary; 
+    string campaignUrl;
+    byte constant version = "1";
+
+    
+    event LogFundingReceived(address addr, uint amount, uint currentTotal);
+    event LogWinnerPaid(address winnerAddress);
+    event LogFundingSuccessful(uint totalRaised);
+    event LogFunderInitialized(
+        address creator,
+        address beneficiary,
+        string url,
+        uint _fundingMaximumTargetInEther, 
+        uint256 deadline);
+
+
+    modifier inState(State _state) {
+        require(state == _state) ;
+        _;
+    }
+
+     modifier isMinimum() {
+        require(msg.value > priceInWei) ;
+        _;
+    }
+
+    modifier inMultipleOfPrice() {
+        require(msg.value%priceInWei == 0) ;
+        _;
+    }
+
+    modifier isCreator() {
+        require(msg.sender == creator) ;
+        _;
+    }
+
+    
+    modifier atEndOfLifecycle() {
+        if(!((state == State.Failed || state == State.Successful) && completedAt + 1 hours < now)) {
+            revert();
+        }
+        _;
+    }
+
+    
+    function CrowdSale(
+        uint _timeInMinutesForFundraising,
+        string _campaignUrl,
+        address _ifSuccessfulSendTo,
+        uint _fundingMinimumTargetInEther,
+        uint _fundingMaximumTargetInEther,
+        token _addressOfTokenUsedAsReward,
+        uint _etherCostOfEachToken) public
+    {
+        creator = msg.sender;
+        beneficiary = _ifSuccessfulSendTo;
+        campaignUrl = _campaignUrl;
+        fundingMinimumTargetInWei = _fundingMinimumTargetInEther * 1 ether; 
+        fundingMaximumTargetInWei = _fundingMaximumTargetInEther * 1 ether; 
+        deadline = now + (_timeInMinutesForFundraising * 1 minutes);
+        currentBalance = 0;
+        tokenReward = token(_addressOfTokenUsedAsReward);
+        priceInWei = _etherCostOfEachToken * 1 ether;
+        LogFunderInitialized(
+            creator,
+            beneficiary,
+            campaignUrl,
+            fundingMaximumTargetInWei,
+            deadline);
+    }
+
+    function contribute()
+    public
+    inState(State.Fundraising) isMinimum() inMultipleOfPrice() payable returns (uint256)
+    {
+        uint256 amountInWei = msg.value;
+
+        
+        contributions.push(
+            Contribution({
+                amount: msg.value,
+                contributor: msg.sender
+                }) 
+            );
+
+        totalRaised += msg.value;
+        currentBalance = totalRaised;
+
+
+        if(fundingMaximumTargetInWei != 0){
+            
+            tokenReward.transfer(msg.sender, amountInWei / priceInWei);
+        }
+        else{
+            tokenReward.mintToken(msg.sender, amountInWei / priceInWei);
+        }
+
+        LogFundingReceived(msg.sender, msg.value, totalRaised);
+
+        
+
+        checkIfFundingCompleteOrExpired();
+        return contributions.length - 1; 
+    }
+
+    function checkIfFundingCompleteOrExpired() public {
+        
+       
+        if (fundingMaximumTargetInWei != 0 && totalRaised > fundingMaximumTargetInWei) {
+            state = State.Successful;
+            LogFundingSuccessful(totalRaised);
+            payOut();
+            completedAt = now;
+            
+            } else if ( now > deadline )  {
+                if(totalRaised >= fundingMinimumTargetInWei){
+                    state = State.Successful;
+                    LogFundingSuccessful(totalRaised);
+                    payOut();  
+                    completedAt = now;
+                }
+                else{
+                    state = State.Failed; 
+                    completedAt = now;
+                }
+            } 
+        
+    }
+
+        function payOut()
+        public
+        inState(State.Successful)
+        {
+            
+            if(!beneficiary.send(this.balance)) {
+                revert();
+            }
+
+            state = State.Closed;
+            currentBalance = 0;
+            LogWinnerPaid(beneficiary);
+        }
+
+        function getRefund()
+        public
+        inState(State.Failed) 
+        returns (bool)
+        {
+            for(uint i=0; i<=contributions.length; i++)
+            {
+                if(contributions[i].contributor == msg.sender){
+                    uint amountToRefund = contributions[i].amount;
+                    contributions[i].amount = 0;
+                    if(!contributions[i].contributor.send(amountToRefund)) {
+                        contributions[i].amount = amountToRefund;
+                        return false;
+                    }
+                    else{
+                        totalRaised -= amountToRefund;
+                        currentBalance = totalRaised;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function removeContract()
+        public
+        isCreator()
+        atEndOfLifecycle()
+        {
+            selfdestruct(msg.sender);
+            
+        }
+
+        function () public { revert(); }
 }
