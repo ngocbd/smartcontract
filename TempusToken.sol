8e0790989e4d03d88f265510ca447b1c4aef3334
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:52:35 +0700

    add contract TempusToken

--- TempusToken.sol
+++ TempusToken.sol
@@ -1,7 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TempusToken at 0x3544af5d53a29d2a8ac1898ad8c8ea18a71cf12f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TempusToken at 0x900b2a673b71e77db2c9ae91a5934d0475b26f11
 */
-pragma solidity ^0.4.18;
+pragma solidity 0.4.23;
+
 
 /**
  * @title SafeMath
@@ -9,8 +10,11 @@ pragma solidity ^0.4.18;
  */
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
         uint256 c = a * b;
-        assert(a == 0 || c / a == b);
+        assert(c / a == b);
         return c;
     }
 
@@ -41,22 +45,14 @@ library SafeMath {
  */
 contract ERC20Basic {
     uint256 public totalSupply;
-    function balanceOf(address who) public constant returns (uint256);
+
+    function balanceOf(address who) public view returns (uint256);
+
     function transfer(address to, uint256 value) public returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
 
 /**
  * @title Basic token
@@ -74,11 +70,12 @@ contract BasicToken is ERC20Basic {
     */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
+        require(_value <= balances[msg.sender]);
 
         // SafeMath.sub will throw if there is not enough balance.
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
@@ -87,11 +84,27 @@ contract BasicToken is ERC20Basic {
     * @param _owner The address to query the the balance of.
     * @return An uint256 representing the amount owned by the passed address.
     */
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
 }
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public view returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 
 /**
  * @title Standard ERC20 token
@@ -102,7 +115,7 @@ contract BasicToken is ERC20Basic {
  */
 contract StandardToken is ERC20, BasicToken {
 
-    mapping (address => mapping (address => uint256)) allowed;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
 
     /**
@@ -113,16 +126,13 @@ contract StandardToken is ERC20, BasicToken {
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
-
-        uint256 _allowance = allowed[_from][msg.sender];
-
-        // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-        // require (_value <= _allowance);
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
 
         balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-        Transfer(_from, _to, _value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
@@ -138,7 +148,7 @@ contract StandardToken is ERC20, BasicToken {
      */
     function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -148,157 +158,148 @@ contract StandardToken is ERC20, BasicToken {
      * @param _spender address The address which will spend the funds.
      * @return A uint256 specifying the amount of tokens still available for the spender.
      */
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view returns (uint256) {
         return allowed[_owner][_spender];
     }
 
     /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     *
      * approve should be called when allowed[_spender] == 0. To increment
      * allowed value is better to use this function to avoid 2 calls (and wait until
      * the first transaction is mined)
      * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _addedValue The amount of tokens to increase the allowance by.
      */
-    function increaseApproval (address _spender, uint _addedValue) public
-    returns (bool success) {
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    function decreaseApproval (address _spender, uint _subtractedValue) public
-    returns (bool success) {
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     *
+     * approve should be called when allowed[_spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
         uint oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
         } else {
             allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-    address public owner;
 
+contract TempusToken is StandardToken {
+    string public name = "Tempus";
+    string public symbol = "TPS";
+    uint8 public decimals = 8;
+    uint256 public cap = 100000000000000000;
+    mapping(address => bool) public owners;
+    mapping(address => bool) public minters;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event Mint(address indexed to, uint256 amount);
+    event OwnerAdded(address indexed newOwner);
+    event OwnerRemoved(address indexed removedOwner);
+    event MinterAdded(address indexed newMinter);
+    event MinterRemoved(address indexed removedMinter);
+    event Burn(address indexed burner, uint256 value);
 
-
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    function Ownable() public {
-        owner = msg.sender;
+    constructor() public {
+        owners[msg.sender] = true;
     }
 
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) public onlyMinter returns (bool) {
+        require(totalSupply.add(_amount) <= cap);
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(burner, _value);
+    }
+
+    /**
+     * @dev Adds administrative role to address
+     * @param _address The address that will get administrative privileges
+     */
+    function addOwner(address _address) public onlyOwner {
+        owners[_address] = true;
+        emit OwnerAdded(_address);
+    }
+
+    /**
+     * @dev Removes administrative role from address
+     * @param _address The address to remove administrative privileges from
+     */
+    function delOwner(address _address) public onlyOwner {
+        owners[_address] = false;
+        emit OwnerRemoved(_address);
+    }
 
     /**
      * @dev Throws if called by any account other than the owner.
      */
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(owners[msg.sender]);
         _;
     }
 
-
     /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
+     * @dev Adds minter role to address (able to create new tokens)
+     * @param _address The address that will get minter privileges
      */
-    function transferOwnership(address newOwner) onlyOwner public {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
+    function addMinter(address _address) public onlyOwner {
+        minters[_address] = true;
+        emit MinterAdded(_address);
     }
 
-}
+    /**
+     * @dev Removes minter role from address
+     * @param _address The address to remove minter privileges
+     */
+    function delMinter(address _address) public onlyOwner {
+        minters[_address] = false;
+        emit MinterRemoved(_address);
+    }
 
-contract TempusToken is StandardToken, Ownable {
-
-    string public constant name = "Tempus Token";
-    string public constant symbol = "TPS";
-    uint8 public constant decimals = 3;
-
-    uint256 public constant INITIAL_SUPPLY = 0;
-
-    event Burn(address indexed from, uint256 value);
-    event Mint(address indexed receiver, uint256 value);
-
-    mapping(address => bool) burners;
-    mapping(address => bool) minters;
-
-    modifier onlyMinters() {
+    /**
+     * @dev Throws if called by any account other than the minter.
+     */
+    modifier onlyMinter() {
         require(minters[msg.sender]);
         _;
     }
-
-    modifier onlyBurners() {
-        require(burners[msg.sender]);
-        _;
-    }
-
-    /**
-    * @dev Token constructor that sets intial values and gives initial supply to msg.sender
-    */
-    function TempusToken() public {
-        totalSupply = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
-        minters[msg.sender] = true;
-        burners[msg.sender] = true;
-    }
-
-    /**
-    * @dev Function to mint tokens
-    * @param receiver The address that will receive the minted tokens
-    * @param amount The amount of tokens to mint
-    * @return A boolean that indicates if operation was successful
-    */
-    function mint(address receiver, uint256 amount) public onlyMinters returns (bool success) {
-        balances[receiver] = balances[receiver].add(amount);
-        totalSupply = totalSupply.add(amount);
-        Mint(receiver, amount);
-        return true;
-    }
-
-    /**
-    * @dev Function to burn tokens
-    * @param from The address that tokens will be burned from
-    * @param value The amount of tokens to burn
-    * @return A boolean that indicates if operation was successful
-    */
-    function burn(address from, uint256 value) public onlyBurners returns (bool success) {
-        require(balances[from] >= value);
-
-        balances[from] = balances[from].sub(value);
-        totalSupply = totalSupply.sub(value);
-        Burn(from, value);
-        return true;
-    }
-
-    /**
-    * @dev Function to set addresses that will be able to mint tokens
-    * @param addr The address that will be set as minter or not
-    * @param isMinter A boolean that indicates whether address should be set as minter
-    */
-    function setAsMinter(address addr, bool isMinter) public onlyOwner {
-        minters[addr] = isMinter;
-    }
-
-    /**
-    * @dev Function to set addresses that will be able to burn tokens
-    * @param addr The address that will be set as burner or not
-    * @param isBurner A boolean that indicates whether address should be set as burner
-    */
-    function setAsBurner(address addr, bool isBurner) public onlyOwner {
-        burners[addr] = isBurner;
-    }
-
 }
