dedfea686921b21dc2dcc14fea550a042494dacc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:10:44 +0700

    add contract CrowdsaleTokenExt

--- CrowdsaleTokenExt.sol
+++ CrowdsaleTokenExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleTokenExt at 0x3ab7b695573017eeBD6377c433F9Cf3eF5B4cd48
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CrowdsaleTokenExt at 0xce3bd1bdc9bb0e381ec05c9d5df3dccbacbe8af7
 */
 // Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 pragma solidity ^0.4.11;
