f5815e44a29b4f7d495105d19a50ae6ab291dbdd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:18:38 +0700

    add contract eGoldchain

--- eGoldchain.sol
+++ eGoldchain.sol
@@ -1,9 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract eGoldchain at 0xf1be2effd5158b94129d9e5533e0070e87f00d60
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract eGoldchain at 0x07a64cf812844b2a3af03006ba9356c64da96ce2
 */
-pragma solidity ^0.4.11;
-
-
+pragma solidity ^0.4.4;
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
@@ -33,8 +31,6 @@ library SafeMath {
         return c;
     }
 }
-
-
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -60,8 +56,6 @@ contract Ownable {
         require(msg.sender == owner);
         _;
     }
-
-
     /**
      * @dev Allows the current owner to transfer control of the contract to a newOwner.
      * @param newOwner The address to transfer ownership to.
@@ -72,8 +66,6 @@ contract Ownable {
     }
 
 }
-
-
 /**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
@@ -85,8 +77,6 @@ contract ERC20Basic {
     function transfer(address to, uint256 value) returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
-
-
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -97,26 +87,24 @@ contract ERC20 is ERC20Basic {
     function approve(address spender, uint256 value) returns (bool);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
-
 /**
- * @title eGoldchainStandard
- * @dev the interface of eGoldchainStandard
+ * @title PoSTokenStandard
+ * @dev the interface of PoSTokenStandard
  */
-contract eGoldchainStandard {
+contract PoSTokenStandard {
     uint256 public stakeStartTime;
     uint256 public stakeMinAge;
     uint256 public stakeMaxAge;
-    function mint() returns (bool);
-    function coinAge() constant returns (uint256);
+   //eGoldchain - Modified the correct technical term "mint" to a well know term "mine" for marketing purposes
+    function mine() returns (bool);
+    function coinAge(address who) constant returns (uint256);
     function annualInterest() constant returns (uint256);
-    event Mint(address indexed _address, uint _reward);
+    event Mine(address indexed _address, uint _reward);
 }
-
-
-contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
+//eGoldchain - Changed name of contract
+contract eGoldchain is ERC20,PoSTokenStandard,Ownable {
     using SafeMath for uint256;
-
+//eGoldchain - Changed name of contract
     string public name = "eGoldchain";
     string public symbol = "EGC";
     uint public decimals = 18;
@@ -141,7 +129,8 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
     mapping(address => mapping (address => uint256)) allowed;
     mapping(address => transferInStruct[]) transferIns;
 
-    event Burn(address indexed burner, uint256 value);
+//eGoldchain - Removed burn system
+    //event Burn(address indexed burner, uint256 value);
 
     /**
      * @dev Fix for the ERC20 short address attack.
@@ -156,19 +145,27 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
         _;
     }
 
-    function eGoldchain() {
-        maxTotalSupply = 10**25; // 10 Mil.
-        totalInitialSupply = 10**24; // 1 Mil.
+    function eGoldchainStart() onlyOwner {
+        address recipient;
+        uint value;
+        uint64 _now = uint64(now);
+        //kill start if this has already been ran
+        require((maxTotalSupply <= 0));
+
+        maxTotalSupply = 30000000000000000000000000; // 30 Mil.
+        
+        //eGoldchain - Modified initial supply to 15mil
+        totalInitialSupply = 15*(10**23); // 15mil
 
         chainStartTime = now;
         chainStartBlockNumber = block.number;
 
-        balances[msg.sender] = totalInitialSupply;
         totalSupply = totalInitialSupply;
     }
 
     function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) returns (bool) {
-        if(msg.sender == _to) return mint();
+        //eGoldchain - Modified to mine
+        if(msg.sender == _to) return mine();
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         Transfer(msg.sender, _to, _value);
@@ -213,8 +210,8 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
-
-    function mint() canPoSMint returns (bool) {
+//eGoldchain - Modified the correct technical term "mint" to a well know term "mine" for marketing purposes.
+    function mine() canPoSMint returns (bool) {
         if(balances[msg.sender] <= 0) return false;
         if(transferIns[msg.sender].length <= 0) return false;
 
@@ -225,8 +222,8 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
         balances[msg.sender] = balances[msg.sender].add(reward);
         delete transferIns[msg.sender];
         transferIns[msg.sender].push(transferInStruct(uint128(balances[msg.sender]),uint64(now)));
-
-        Mint(msg.sender, reward);
+//eGoldchain - Change event to Mine
+        Mine(msg.sender, reward);
         return true;
     }
 
@@ -234,16 +231,19 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
         blockNumber = block.number.sub(chainStartBlockNumber);
     }
 
-    function coinAge() constant returns (uint myCoinAge) {
-        myCoinAge = getCoinAge(msg.sender,now);
+    function coinAge(address who) constant returns (uint myCoinAge) {
+        myCoinAge = getCoinAge(who,now);
     }
 
     function annualInterest() constant returns(uint interest) {
         uint _now = now;
         interest = maxMintProofOfStake;
+        //eGoldchain - Modified initial interest rate to 300%
         if((_now.sub(stakeStartTime)).div(1 years) == 0) {
+            interest = (1650 * maxMintProofOfStake).div(100);
+        } else if((_now.sub(stakeStartTime)).div(1 years) == 1) {
             interest = (770 * maxMintProofOfStake).div(100);
-        } else if((_now.sub(stakeStartTime)).div(1 years) == 1){
+        } else if((_now.sub(stakeStartTime)).div(1 years) == 2){
             interest = (435 * maxMintProofOfStake).div(100);
         }
     }
@@ -258,11 +258,15 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
         uint interest = maxMintProofOfStake;
         // Due to the high interest rate for the first two years, compounding should be taken into account.
         // Effective annual interest rate = (1 + (nominal rate / number of compounding periods)) ^ (number of compounding periods) - 1
+        //eGoldchain - Modified initial interest rate to 300%
         if((_now.sub(stakeStartTime)).div(1 years) == 0) {
-            // 1st year effective annual interest rate is 100% when we select the stakeMaxAge (90 days) as the compounding period.
+            // 1st year effective annual interest rate is 300% when we select the stakeMaxAge (90 days) as the compounding period.
+            interest = (1650 * maxMintProofOfStake).div(100);
+        } else if((_now.sub(stakeStartTime)).div(1 years) == 1) {
+            // 2nd year effective annual interest rate is 100% when we select the stakeMaxAge (90 days) as the compounding period.
             interest = (770 * maxMintProofOfStake).div(100);
-        } else if((_now.sub(stakeStartTime)).div(1 years) == 1){
-            // 2nd year effective annual interest rate is 50%
+        } else if((_now.sub(stakeStartTime)).div(1 years) == 2){
+            // 3nd year effective annual interest rate is 50%
             interest = (435 * maxMintProofOfStake).div(100);
         }
 
@@ -287,41 +291,4 @@ contract eGoldchain is ERC20,eGoldchainStandard,Ownable {
         stakeStartTime = timestamp;
     }
 
-    function ownerBurnToken(uint _value) onlyOwner {
-        require(_value > 0);
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        delete transferIns[msg.sender];
-        transferIns[msg.sender].push(transferInStruct(uint128(balances[msg.sender]),uint64(now)));
-
-        totalSupply = totalSupply.sub(_value);
-        totalInitialSupply = totalInitialSupply.sub(_value);
-        maxTotalSupply = maxTotalSupply.sub(_value*10);
-
-        Burn(msg.sender, _value);
-    }
-
-    /* Batch token transfer. Used by contract creator to distribute initial tokens to holders */
-    function batchTransfer(address[] _recipients, uint[] _values) onlyOwner returns (bool) {
-        require( _recipients.length > 0 && _recipients.length == _values.length);
-
-        uint total = 0;
-        for(uint i = 0; i < _values.length; i++){
-            total = total.add(_values[i]);
-        }
-        require(total <= balances[msg.sender]);
-
-        uint64 _now = uint64(now);
-        for(uint j = 0; j < _recipients.length; j++){
-            balances[_recipients[j]] = balances[_recipients[j]].add(_values[j]);
-            transferIns[_recipients[j]].push(transferInStruct(uint128(_values[j]),_now));
-            Transfer(msg.sender, _recipients[j], _values[j]);
-        }
-
-        balances[msg.sender] = balances[msg.sender].sub(total);
-        if(transferIns[msg.sender].length > 0) delete transferIns[msg.sender];
-        if(balances[msg.sender] > 0) transferIns[msg.sender].push(transferInStruct(uint128(balances[msg.sender]),_now));
-
-        return true;
-    }
 }
