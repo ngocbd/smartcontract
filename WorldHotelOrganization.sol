75b9d0e32a07a477f2a5296b383f37d3702de27b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:24:52 +0700

    add contract WorldHotelOrganization

--- WorldHotelOrganization.sol
+++ WorldHotelOrganization.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WorldHotelOrganization at 0x9ceC06600AC633819a448fa73e40FF6032151c3d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WorldHotelOrganization at 0x7B7BA265B15B562151724be322fB0F7135e2EB0d
 */
 pragma solidity ^0.4.10;
 
