6692737cb6dfd6a28b7087eee2c07317d4fb7d0e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:23:18 +0700

    add contract Dividends

--- Dividends.sol
+++ Dividends.sol
@@ -1,13 +1,17 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Dividends at 0xc1086fa97549cea7acf7c2a7fa7820fd06f3e440
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Dividends at 0xc103d95c326383a01a2fc3d5bd1c2514bb9a7ae1
 */
 pragma solidity ^0.4.21;
 
 // Dev fee payout contract + dividend options 
 // EtherGuy DApp fee will be stored here 
-// Buying any token gives right to claim 
+// Buying any token gives right to claim part of development dividends.
+// It is suggested you do withdraw once in a while. If someone still finds an attack after this fixed contrat 
+// they are unable the steal any of your withdrawn eth. Withdrawing does not sell your tokens!
 // UI: etherguy.surge.sh/dividend.html
 // Made by EtherGuy, etherguy@mail.com 
+// Version 2 of contract. Exploit(s) found by ccashwell in v1, thanks for reporting them!
+
 
 // IF THERE IS ANY BUG the data will be rerolled from here. See the discord https://discord.gg/R84hD6f if anything happens or mail me 
 
@@ -21,6 +25,8 @@ contract Dividends{
     uint16 public Tax = 1250; 
     
     address dev;
+    
+    bool public StopSell=false; // in case aonther bug is found, stop selling so it is easier to give everyone their tokens back. 
     
     mapping (address => uint256) public MyTokens;
     mapping (address => uint256) public DividendCollectSince;
@@ -37,6 +43,7 @@ contract Dividends{
     function ViewMyTokens(address who) public view returns (uint256){
         return MyTokens[who];
     }
+
     
     function ViewMyDivs(address who) public view returns (uint256){
         uint256 tkns = MyTokens[who];
@@ -54,16 +61,34 @@ contract Dividends{
     function Dividends() public {
         dev = msg.sender;
         // EG
-        MyTokens[msg.sender] = TokenSupply - 400000;
+        MyTokens[msg.sender] =  8000000;// was: TokenSupply - 400000;
         // HE
         MyTokens[address(0x83c0Efc6d8B16D87BFe1335AB6BcAb3Ed3960285)] = 200000;
         // PG
         MyTokens[address(0x26581d1983ced8955C170eB4d3222DCd3845a092)] = 200000;
         //MyTokens[address(0x0)] = 400000;
-        PlaceSellOrder(1600000, (0.5 szabo)); // 1 token per 0.5 szabo / 500 gwei or 1000 tokens per 0.5 finney / 0.0005 ether or 1M tokens per 0.5 ETH 
+        
+        // Since not a lot of addresses bought, we can instantly restore this 
+        // If this happens in the future, we will need users to do a single transaction to retrieve their tokens from 
+        // previous contract and withdraw all amount immediately 
+        // Below tokens are paid for - 0.5 szabo per token (cheap right? blame the develo --- oh wait :D )
+        
+        MyTokens[address(0x3130259deEdb3052E24FAD9d5E1f490CB8CCcaa0)] = 100000;
+        MyTokens[address(0x4f0d861281161f39c62B790995fb1e7a0B81B07b)] = 200000;
+        MyTokens[address(0x36E058332aE39efaD2315776B9c844E30d07388B)] =  20000;
+        MyTokens[address(0x1f2672E17fD7Ec4b52B7F40D41eC5C477fe85c0c)] =  40000;
+        MyTokens[address(0xedDaD54E9e1F8dd01e815d84b255998a0a901BbF)] =  20000;
+        MyTokens[address(0x0a3239799518E7F7F339867A4739282014b97Dcf)] = 500000;
+        MyTokens[address(0x29A9c76aD091c015C12081A1B201c3ea56884579)] = 600000;
+        MyTokens[address(0x0668deA6B5ec94D7Ce3C43Fe477888eee2FC1b2C)] = 100000;
+        MyTokens[address(0x0982a0bf061f3cec2a004b4d2c802F479099C971)] =  20000;
+        //                                                              ------+
+        //                                                             1600000 = 1.6M which corresponds to the sell volume. Nice.     +400k + 8M = 10M, which corresponds to token supply                                                          
+        
+        //PlaceSellOrder(1600000, (0.5 szabo)); // 1 token per 0.5 szabo / 500 gwei or 1000 tokens per 0.5 finney / 0.0005 ether or 1M tokens per 0.5 ETH 
     }
     
-    function GetDividends(address who, uint256 TokenAmount) internal view  returns(uint256){
+    function GetDividends(address who, uint256 TokenAmount ) internal view  returns(uint256){
         if (TokenAmount == 0){
             return 0;
         }
@@ -76,14 +101,27 @@ contract Dividends{
         return  ((MyBalance * TokenAmount) / (TokenSupply));
     }
     
+    // dev can stop selling 
+    // this does NOT DISABLE withdrawing 
+    function EmergencyStopSell(bool setting) public {
+        require(msg.sender==dev);
+        StopSell=setting;
+    }
+    
 
     event Sold(address Buyer, address Seller, uint256 price, uint256 tokens);
-    function Buy(address who) public payable {
+    // price_max anti-scam arg 
+    function Buy(address who, uint256 price_max) public payable {
        // require(msg.value >= (1 szabo)); // normal amounts pls 
         // lookup order by addr 
-        uint256[2] memory order = SellOrder[who];
+        require(!StopSell);
+        require(who!=msg.sender && who!=tx.origin);
+        uint256[2] storage order = SellOrder[who];
         uint256 amt_available = order[0];
         uint256 price = order[1];
+        
+        // only buy for certain price 
+        require(price <= price_max);
         
         uint256 excess = 0;
         
@@ -92,7 +130,8 @@ contract Dividends{
             revert();
         }
         
-        uint256 max = amt_available * price; 
+        // high price overflow prevent (ccashwell)
+        uint256 max = mul(amt_available, price); 
         uint256 currval = msg.value;
         // more than max buy value 
         if (currval > max){
@@ -108,58 +147,77 @@ contract Dividends{
         if (take == 0){
             revert(); // very high price apparently 
         }
-        excess = excess + sub(currval, take * price); 
-
-        
-        if (excess > 0){
-            msg.sender.transfer(excess);
-        }
-        
-        currval = sub(currval,excess);
+        excess = excess + sub(currval, mul(take, price)); 
+        // do not take max value off .
+        currval = sub(currval,sub(currval, mul(take, price)));
         
         // pay fees 
 
-        uint256 fee = (Tax * currval)/10000;
-        dev.transfer(fee);
-        who.transfer(currval-fee);
+        uint256 fee = (mul(Tax, currval))/10000;
+
         
         // the person with these tokens will also receive dividend over this buy order (this.balance)
         // however the excess is removed, see the excess transfer above 
      //   if (msg.value > (excess+currval+fee)){
       //      msg.sender.transfer(msg.value-excess-currval-fee);
      //   }
-        _withdraw(who, MyTokens[who]);
-        if (MyTokens[msg.sender] > 0){
-            
-            _withdraw(msg.sender, MyTokens[msg.sender]);
-        }
+
+
         MyTokens[who] = MyTokens[who] - take; 
         SellOrder[who][0] = SellOrder[who][0]-take; 
         MyTokens[msg.sender] = MyTokens[msg.sender] + take;
     //    MyPayouts[msg.sender] = MyPayouts[msg.sender] + GetDividends(msg.sender, take);
-        DividendCollectSince[msg.sender] = (address(this).balance) + TotalPaid;
+        //DividendCollectSince[msg.sender] = (address(this).balance) + TotalPaid;
         
         emit Sold(msg.sender, who, price, take);
        // push((excess + currval)/(1 finney), (msg.value)/(1 finney));
+       
+       // all transfers at end 
+       
+       
+        dev.transfer(fee);
+        who.transfer(currval-fee);
+        if ((excess) > 0){
+            msg.sender.transfer(excess);
+        }
+        // call withdraw with tokens before data change 
+        _withdraw(who, MyTokens[who]+take);
+        //DividendCollectSince[msg.sender] = (address(this).balance) + TotalPaid;
+        if (sub(MyTokens[msg.sender],take) > 0){
+            _withdraw(msg.sender,MyTokens[msg.sender]-take);    
+        }
+        else{
+            // withdraw zero tokens to set DividendCollectSince to the right place. 
+            // updates before this break the withdraw if user has any tokens .
+            _withdraw(msg.sender, 0);
+        }
+        
+        
+        
     }
     
     function Withdraw() public {
         _withdraw(msg.sender, MyTokens[msg.sender]);
     }
     
+    
+    event GiveETH(address who, uint256 yummy_eth);
     function _withdraw(address who, uint256 amt) internal{
         // withdraws from amt. 
         // (amt not used in current code, always same value)
-        if (MyTokens[who] < amt){
-            revert(); // ??? security check 
-        }
+       // if (MyTokens[who] < amt){
+        //    revert(); // ??? security check 
+       // }
         
         uint256 divs = GetDividends(who, amt);
+        TotalPaid = TotalPaid + divs;
+        DividendCollectSince[who] = sub(TotalPaid + address(this).balance, divs);
+        
+        // muh logs 
+        emit GiveETH(who, divs);
         
         who.transfer(divs);
-        TotalPaid = TotalPaid + divs;
-        
-        DividendCollectSince[who] = TotalPaid + address(this).balance;
+
     }
     
     event SellOrderPlaced(address who, uint256 amt, uint256 price);
@@ -178,6 +236,7 @@ contract Dividends{
         Tax=amt;
     }
     
+
     
     // dump divs in contract 
     function() public payable {
@@ -188,5 +247,14 @@ contract Dividends{
         assert(b <= a);
         return a - b;
     } 
+    
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
     
 }
