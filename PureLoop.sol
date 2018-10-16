945e17a648b0d15d50dc0e3cc1475bda08fbc230
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:22:28 +0700

    add contract PureLoop

--- PureLoop.sol
+++ PureLoop.sol
@@ -1,19 +1,11 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PureLoop at 0xf20d09c6368ed472123057e7c29460ef7a1ecda7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PureLoop at 0x55930261bfebdbe7acfd58e0ebb05a18a733f27f
 */
 pragma solidity ^0.4.19;
 
 contract PureLoop
 {
-    function executePure() public pure returns (uint output) {
-        uint num;
-        while(true) {
-            num++;
-        }
-        return num;
-    }
-    
-    function executeConstant() public constant returns (uint output) {
+    function execute() public pure returns (uint output) {
         uint num;
         while(true) {
             num++;
