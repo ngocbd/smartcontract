a6ef715b9ff67cd3eae6fc065252adb0d0d49bbc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:41:54 +0700

    add contract MultiWhitelist

--- MultiWhitelist.sol
+++ MultiWhitelist.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiWhitelist at 0x342F4057110bE50957d0f156F4939a929944C69e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiWhitelist at 0x03ba1ee3c82ac59cf4b86300d87f0dd091acc3af
 */
 pragma solidity ^0.4.18;
 
@@ -15,8 +15,13 @@ contract MultiWhitelist {
     function MultiWhitelist(address _owner) public {
         owner = _owner;
     }
+    function transferOwner(address _owner) public {
+        require(msg.sender == owner);
+        owner = _owner;
+    }
     function multisetUserCategory(address[] users, uint category, whitelist listContract) public {
         require(msg.sender == owner);
+        require(category == 4);
 
         for(uint i = 0 ; i < users.length ; i++ ) {
             listContract.setUserCategory(users[i],category);
