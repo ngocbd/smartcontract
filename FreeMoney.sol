f77e84ce863b6c1a88527bc7ce108d0e069980d4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:31:13 +0700

    add contract FreeMoney

--- FreeMoney.sol
+++ FreeMoney.sol
@@ -1,13 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FreeMoney at 0x31710812a91c9cc695750a8d47fad1bb1d3a8f7d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FreeMoney at 0xede79a28fe8e2edb3efb629e7396034f76c22cc3
 */
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.18;
 
+// Free money. No bamboozle.
+// By NR
 contract FreeMoney {
-    function take() public payable {
-        if (msg.value > 15 finney) {
-            selfdestruct(msg.sender);
-        }
+    
+    uint public remaining;
+    
+    function FreeMoney() public payable {
+        remaining += msg.value;
     }
-    function () public payable {}
+    
+    // Feel free to give money to whomever
+    function() payable {
+        remaining += msg.value;
+    }
+    
+    // You're welcome?!
+    function withdraw() public {
+        remaining = 0;
+        msg.sender.transfer(this.balance);
+    }
 }
