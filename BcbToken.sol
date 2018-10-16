c13f647138e329f580d34ec5bf09f67cf8b4240d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:20:28 +0700

    add contract BCBToken

--- BcbToken.sol
+++ BcbToken.sol
@@ -1,121 +1,140 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BcbToken at 0xcD0506fa31Ed0900B062E03D257E5e9b5063068a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BCBToken at 0xc519a8f2512f09951d44780b2f678862d9b5e839
 */
-pragma solidity ^0.4.13; 
+pragma solidity ^0.4.4;
 
-contract owned {
-    address public owner;
-    function owned() {
-        owner = msg.sender;
-    }
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
+contract Token {
 
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
-    }
-  }
-contract tokenRecipient {
-     function receiveApproval(address from, uint256 value, address token, bytes extraData); 
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
 }
-contract token {
-    /*Public variables of the token */
-    string public name; string public symbol; uint8 public decimals; uint256 public totalSupply;
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-    /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function token() {
-    balanceOf[msg.sender] = 10000000000000000; 
-    totalSupply = 10000000000000000; 
-    name = "BCB"; 
-    symbol =  "?";
-    decimals = 8; 
-    }
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0); 
-        require (balanceOf[_from] > _value); 
-        require (balanceOf[_to] + _value > balanceOf[_to]); 
-        balanceOf[_from] -= _value; 
-        balanceOf[_to] += _value; 
-        Transfer(_from, _to, _value);
-    }
 
-    function transfer(address _to, uint256 _value) {
-        _transfer(msg.sender, _to, _value);
+
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        require (_value < allowance[_from][msg.sender]); 
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+
+//name this contract whatever you'd like
+contract BCBToken is StandardToken {
+
+    function () {
+        //if ether is sent to this address, send it back.
+        throw;
+    }
+
+    /* Public variables of the token */
+
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+//
+// CHANGE THESE VALUES FOR YOUR TOKEN
+//
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of BCBToken
+
+    function BCBToken(
+        ) {
+        balances[msg.sender] = 100000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 100000;                        // Update total supply (100000 for example)
+        name = "Block Chain Bridge";                                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "BCB";                               // Set the symbol for display purposes
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
     }
-
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-        spender.receiveApproval(msg.sender, _value, this, _extraData);
-        return true;
-        }
-    }
-    /// @notice Remove `_value` tokens from the system irreversibly
-    /// @param _value the amount of money to burn
-    function burn(uint256 _value) returns (bool success) {
-        require (balanceOf[msg.sender] > _value); // Check if the sender has enough
-        balanceOf[msg.sender] -= _value; // Subtract from the sender
-        totalSupply -= _value; // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        require(balanceOf[_from] >= _value); // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]); // Check allowance
-        balanceOf[_from] -= _value; // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value; // Subtract from the sender's allowance
-        totalSupply -= _value; // Update totalSupply
-        Burn(_from, _value);
-        return true;
-      }
-   }
-
-contract BcbToken is owned, token {
-    mapping (address => bool) public frozenAccount;
-    /* This generates a public event on the blockchain that will notify clients */
-    event FrozenFunds(address target, bool frozen);
-  
-
-
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0); 
-     
-        require(msg.sender != _to);
-        require (balanceOf[_from] > _value); // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); 
-        require(!frozenAccount[_from]); // Check if sender is frozen
-        require(!frozenAccount[_to]); // Check if recipient is frozen
-        balanceOf[_from] -= _value; // Subtract from the sender
-        balanceOf[_to] += _value; // Add the same to the recipient
-        Transfer(_from, _to, _value);
-    }
-
-    function freezeAccount(address target, bool freeze) onlyOwner {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
-    }
-    }
+}
