c6ba4a7d7a4f092e7b4a0485d845552a2a438822
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:24:33 +0700

    add contract XBV

--- XBV.sol
+++ XBV.sol
@@ -1,122 +1,19 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XBV at 0xb2ff8511991d43138ac881bd4a04a658e00ad3c0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XBV at 0xf4467cae19c5a53b9fdcd4ded678826805aa1927
 */
 pragma solidity ^ 0.4 .19;
 
-
-contract Contract {function XBVHandler( address _from, uint256 _value );}
-
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
+contract ERC223ReceivingContract { 
+/**
+ * @dev Standard ERC223 function that will handle incoming token transfers.
+ *
+ * @param _from  Token sender address.
+ * @param _value Amount of tokens.
+ * @param _data  Transaction metadata.
+ */
+    function tokenFallback(address _from, uint _value, bytes _data);
 }
 
-contract Contracts is Ownable {
-     
-    Contract public contract_address;
-    XBV token;
-    mapping( address => bool ) public contracts;
-    mapping( address => bool ) public contractExists;
-    mapping( uint => address) public  contractIndex;
-    uint public contractCount;
-    //address public owner;
-    event ContractCall ( address _address, uint _value );
-    event Log ( address _address, uint value  );
-    event Message ( uint value  );
-
-   
-
-    function addContract ( address _contract ) public onlyOwner returns(bool)  {
-        
-            contracts[ _contract ] = true;
-        if  ( !contractExists[ _contract ]){
-            contractExists[ _contract ] = true;
-            contractIndex[ contractCount ] = _contract;
-            contractCount++;
-            return true;
-        }
-        return false;
-    }
-    
-    
-    function latchContract () public returns(bool)  {
-        
-            contracts[ msg.sender ] = true;
-        if  ( !contractExists[ msg.sender ]){
-            contractExists[ msg.sender ] = true;
-            contractIndex[ contractCount ] = msg.sender;
-            contractCount++;
-            return true;
-        }
-        return false;
-    }
-    
-    
-    function unlatchContract ( ) public returns(bool){
-       contracts[ msg.sender ] = false;
-    }
-    
-    
-    function removeContract ( address _contract )  public  onlyOwner returns(bool) {
-        contracts[ _contract ] =  false;
-        return true;
-    }
-    
-    
-    function getContractCount() public constant returns (uint256){
-        return contractCount;
-    }
-    
-    function getContractAddress( uint slot ) public constant returns (address){
-        return contractIndex[slot];
-    }
-    
-    function getContractStatus( address _address) public constant returns (bool) {
-        return contracts[ _address];
-    }
-
-
-    function contractCheck ( address _address, uint256 value ) internal  {
-        
-        if( contracts[ _address ] ) {
-            contract_address = Contract (  _address  );
-            contract_address.XBVHandler  ( msg.sender , value );
-         
-        }        
-        ContractCall ( _address , value  );
-    }
-    
-}
 
 contract tokenRecipient {
     function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
@@ -172,23 +69,25 @@ contract ERC20 {
 }
 
 
-contract XBV is ERC20, Contracts {
+contract XBV is ERC20  {
 
     using SafeMath
     for uint256;
     /* Public variables of the token */
-    string public standard = 'XBV 2.0';
+    string public standard = 'XBV 2.2';
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
     uint256 public initialSupply;
+    bool initialize;
 
     mapping( address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
     event Approval(address indexed owner, address indexed spender, uint value);
 
     /* This notifies clients about the amount burnt */
@@ -205,9 +104,11 @@ contract XBV is ERC20, Contracts {
         name = "BlockVentureCoin"; // Set the name for display purposes
         symbol = "XBV"; // Set the symbol for display purposes
         decimals = decimalUnits; // Amount of decimals for display purposes
-        owner   = msg.sender;
-        
     }
+
+   
+
+
 
     function balanceOf(address tokenHolder) constant returns(uint256) {
 
@@ -219,22 +120,63 @@ contract XBV is ERC20, Contracts {
         return totalSupply;
     }
 
-   /* Send coins */
+
     function transfer(address _to, uint256 _value) returns(bool ok) {
         
         if (_to == 0x0) throw; // Prevent transfer to 0x0 address. Use burn() instead
         if (balanceOf[msg.sender] < _value) throw; // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        //balanceOf[msg.sender] -= _value; // Subtract from the sender
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(  _value ); // Subtract from the sender
+        bytes memory empty;
         
-        //balanceOf[_to] += _value; // Add the same to the recipient
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(  _value ); // Subtract from the sender
         balanceOf[_to] = balanceOf[_to].add( _value ); // Add the same to the recipient
         
+         if(isContract( _to )) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, empty);
+        }
         
         Transfer(msg.sender, _to, _value); // Notify anyone listening that this transfer took place
-        contractCheck( _to , _value );
         return true;
+    }
+    
+     function transfer(address _to, uint256 _value, bytes _data ) returns(bool ok) {
+        
+        if (_to == 0x0) throw; // Prevent transfer to 0x0 address. Use burn() instead
+        if (balanceOf[msg.sender] < _value) throw; // Check if the sender has enough
+        bytes memory empty;
+        
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(  _value ); // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].add( _value ); // Add the same to the recipient
+        
+         if(isContract( _to )) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+        
+        Transfer(msg.sender, _to, _value, _data); // Notify anyone listening that this transfer took place
+        return true;
+    }
+    
+    
+    
+    function isContract( address _to ) internal returns ( bool ){
+        
+        
+        uint codeLength = 0;
+        
+        assembly {
+            // Retrieve the size of the code on target address, this needs assembly .
+            codeLength := extcodesize(_to)
+        }
+        
+         if(codeLength>0) {
+           
+           return true;
+           
+        }
+        
+        return false;
+        
     }
     
     
@@ -271,7 +213,6 @@ contract XBV is ERC20, Contracts {
         balanceOf[_to] = balanceOf[_to].add( _value ); // Add the same to the recipient
         allowance[_from][msg.sender] = allowance[_from][msg.sender].sub( _value ); 
         Transfer(_from, _to, _value);
-        contractCheck( _to , _value );
         return true;
     }
   
