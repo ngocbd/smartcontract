8b9375e2a38197a3e9e36d1433564f23918ffa94
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:49:16 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x478931066157328d1216c17fcbdf85c27b694cee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x004904cb627fe62d46486c41a3d79f3cdf6b0460
 */
 pragma solidity ^0.4.4;
 
@@ -108,21 +108,19 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function HashnodeTestCoin() {
-        balances[msg.sender] = 100000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 100000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Ada Classic";                                   // Set the name for display purposes (CHANGE THIS)
+        balances[msg.sender] = 1000000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 1000000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "EXotic Coin";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "ADCS";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
-        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
+        symbol = "EXC";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 4000;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        fundsWallet = msg.sender;//0x20Fc65fA01F871387d68FfCeD6795450B37b8355;                                    // The owner of the contract gets ETH
     }
 
     function() payable{
         totalEthInWei = totalEthInWei + msg.value;
         uint256 amount = msg.value * unitsOneEthCanBuy;
-        if (balances[fundsWallet] < amount) {
-            return;
-        }
+        require(balances[fundsWallet] >= amount);
 
         balances[fundsWallet] = balances[fundsWallet] - amount;
         balances[msg.sender] = balances[msg.sender] + amount;
