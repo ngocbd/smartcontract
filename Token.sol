96969cd1df955eb9932383a5585db0d714c1d22b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:40:12 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,166 +1,42 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x6e6850d379f7f02862ffe932c87dce7d69a96cd6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xfce1ebd75bef6db4e6951fb9ef7485902dbc5ae5
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
+contract Token {
+    
+	/* Public variables of the token */
+	string public name;
+	string public symbol;
+	uint8 public decimals;
+	uint256 public totalSupply;
+    
+	/* This creates an array with all balances */
+	mapping (address => uint256) public balanceOf;
 
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+	/* This generates a public event on the blockchain that will notify clients */
+	event Transfer(address indexed from, address indexed to, uint256 value);
 
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
+	function Token() {
+	    totalSupply = 168*(10**6)*(10**18);
+		balanceOf[msg.sender] = 168*(10**6)*(10**18);             // Give the creator all initial tokens
+		name = "Oceancoin";                                   // Set the name for display purposes
+		symbol = "OAC";                               // Set the symbol for display purposes
+		decimals = 18;                            // Amount of decimals for display purposes
+	}
 
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+	function transfer(address _to, uint256 _value) {
+	/* Check if sender has balance and for overflows */
+	if (balanceOf[msg.sender] < _value || balanceOf[_to] + _value < balanceOf[_to])
+		revert();
+	/* Add and subtract new balances */
+	balanceOf[msg.sender] -= _value;
+	balanceOf[_to] += _value;
+	/* Notifiy anyone listening that this transfer took place */
+	Transfer(msg.sender, _to, _value);
+	}
 
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
-contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() { require(msg.sender == owner); _; }
-
-    function Ownable() {
-        owner = msg.sender;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
-
-contract ERC20 {
-    uint256 public totalSupply;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function balanceOf(address who) constant returns (uint256);
-    function transfer(address to, uint256 value) returns (bool);
-    function transferFrom(address from, address to, uint256 value) returns (bool);
-    function allowance(address owner, address spender) constant returns (uint256);
-    function approve(address spender, uint256 value) returns (bool);
-}
-
-contract StandardToken is ERC20 {
-    using SafeMath for uint256;
-
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
-
-    function balanceOf(address _owner) constant returns(uint256 balance) {
-        return balances[_owner];
-    }
-
-    function transfer(address _to, uint256 _value) returns(bool success) {
-        require(_to != address(0));
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-
-        Transfer(msg.sender, _to, _value);
-
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool success) {
-        require(_to != address(0));
-
-        var _allowance = allowed[_from][msg.sender];
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-
-        Transfer(_from, _to, _value);
-
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    function approve(address _spender, uint256 _value) returns(bool success) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-        allowed[msg.sender][_spender] = _value;
-
-        Approval(msg.sender, _spender, _value);
-
-        return true;
-    }
-
-    function increaseApproval(address _spender, uint _addedValue) returns(bool success) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-
-        return true;
-    }
-
-    function decreaseApproval(address _spender, uint _subtractedValue) returns(bool success) {
-        uint oldValue = allowed[msg.sender][_spender];
-
-        if(_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        
-        return true;
-    }
-}
-
-contract Token is StandardToken,Ownable {
-    using SafeMath for uint;
-
-    string public name = "Dragonfly Token";
-    string public symbol = "DRF";
-    uint256 public decimals = 18;
-
-    bool public mintingFinished = false;
-	
-    event Mint(address indexed holder, uint256 tokenAmount);
-    event MintFinished();
-
-    function _mint(address _to, uint256 _amount) onlyOwner returns(bool) {
-        totalSupply = totalSupply.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
-
-        Mint(_to, _amount);
-        Transfer(address(0), _to, _amount);
-
-        return true;
-    }
-
-    function mint(address _to, uint256 _amount) onlyOwner returns(bool) {
-        require(!mintingFinished);
-        return _mint(_to, _amount);
-    }
-
-    function finishMinting() onlyOwner returns(bool) {
-        mintingFinished = true;
-        MintFinished();
-
-        return true;
-    }
+	/* This unnamed function is called whenever someone tries to send ether to it */
+	function () {
+	revert();     // Prevents accidental sending of ether
+	}
 }
