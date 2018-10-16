ca2365c653227fbdb74af1af64f013de15d27551
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:18 +0700

    add contract Presale

--- Presale.sol
+++ Presale.sol
@@ -1,126 +1,206 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x4f529990b7f3d1fb4152736155e431c96fd86294
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Presale at 0x022de2e28a61f9d197f8966dd0fc8f7ddf70f2dc
 */
-// Mysterium Network Presale Smart Contract
-
 pragma solidity ^0.4.6;
 
+// Presale Smart Contract
+//
+// **** START:  WORK IN PROGRESS DISCLAIMER ****
+// This is a work in progress and not intended for reuse.
+// So don't reuse unless you know exactly what are you doing! 
+// **** END:  WORK IN PROGRESS DISCLAIMER ****
+//
+// **** START:  PARANOIA DISCLAIMER ****
+// A careful reader will find here some unnecessary checks and excessive code consuming some extra valuable gas. It is intentionally. 
+// Even contract will works without these parts, they make the code more secure in production as well for future refactoring.
+// Additionally it shows more clearly what we have took care of.
+// You are welcome to discuss that places.
+// **** END OF: PARANOIA DISCLAIMER *****
+//
+//
+// @author ethernian
+//
+
 contract Presale {
-    mapping (address => uint) public balances;
-    uint public transfered_total = 0;
-    
-    uint public constant min_goal_amount = 2000 ether;
-    uint public constant max_goal_amount = 6000 ether;
-    
-    // Mysterium project wallet
-    address public project_wallet;
 
-    uint public presale_start_block;
-    uint public presale_end_block;
-    
-    // approximate blocks count in 2 months 
-    uint constant blocks_in_two_months = 351558;
-    
-    // block number of the end of refund window, 
-    // which will occur in the end of 2 months after presale
-    uint public refund_window_end_block;
-    
-    function Presale(/*uint _start_block, uint _end_block, address _project_wallet*/) {
-        
-        uint _start_block = 2818600;
-        uint _end_block = 3191000;
-        address _project_wallet = 0x002515a2fd5C9DDa1d4109aE8BBF9f73A707B72f;
-        
-        if (_start_block <= block.number) throw;
-        if (_end_block <= _start_block) throw;
-        if (_project_wallet == 0) throw;
-        
-        presale_start_block = _start_block;
-        presale_end_block = _end_block;
-        project_wallet = _project_wallet;
-		refund_window_end_block = presale_end_block + blocks_in_two_months;
-	}
+    string public constant VERSION = "0.1.3-beta";
+
+	/* ====== configuration START ====== */
+
+	uint public constant PRESALE_START  = 3071952; //	approx. 	27.01.2017 08:30
+	uint public constant PRESALE_END    = 3074472; //	approx. 	27.01.2017 19:00
+	uint public constant WITHDRAWAL_END = 3080232; //	approx. 	28.01.2017 19:00
+
+
+	address public constant OWNER = 0x45d5426471D12b21C3326dD0cF96f6656F7d14b1;
 	
-	function has_presale_started() private constant returns (bool) {
-	    return block.number >= presale_start_block;
-	}
-    
-    function has_presale_time_ended() private constant returns (bool) {
-        return block.number > presale_end_block;
-    }
-    
-    function is_min_goal_reached() private constant returns (bool) {
-        return transfered_total >= min_goal_amount;
-    }
-    
-    function is_max_goal_reached() private constant returns (bool) {
-        return transfered_total >= max_goal_amount;
-    }
-    
-    // Accept ETH while presale is active or until maximum goal is reached.
-	function () payable {
-	    // check if presale has started
-        if (!has_presale_started()) throw;
-	    
-	    // check if presale date is not over
-	    if (has_presale_time_ended()) throw;
-	    
-	    // don`t accept transactions with zero value
-	    if (msg.value == 0) throw;
+    uint public constant MIN_TOTAL_AMOUNT_TO_RECEIVE_ETH = 1;
+    uint public constant MAX_TOTAL_AMOUNT_TO_RECEIVE_ETH = 5;
+    uint public constant MIN_ACCEPTED_AMOUNT_FINNEY = 1;
 
-        // check if max goal is not reached
-	    if (is_max_goal_reached()) throw;
-        
-        if (transfered_total + msg.value > max_goal_amount) {
-            // return change
-	        var change_to_return = transfered_total + msg.value - max_goal_amount;
-	        if (!msg.sender.send(change_to_return)) throw;
-            
-            var to_add = max_goal_amount - transfered_total;
-            balances[msg.sender] += to_add;
-	        transfered_total += to_add;
+    /* ====== configuration END ====== */
+	
+    string[5] private stateNames = ["BEFORE_START",  "PRESALE_RUNNING", "WITHDRAWAL_RUNNING", "REFUND_RUNNING", "CLOSED" ];
+    enum State { BEFORE_START,  PRESALE_RUNNING, WITHDRAWAL_RUNNING, REFUND_RUNNING, CLOSED }
+
+    uint public total_received_amount;
+	mapping (address => uint) public balances;
+	
+    uint private constant MIN_TOTAL_AMOUNT_TO_RECEIVE = MIN_TOTAL_AMOUNT_TO_RECEIVE_ETH * 1 ether;
+    uint private constant MAX_TOTAL_AMOUNT_TO_RECEIVE = MAX_TOTAL_AMOUNT_TO_RECEIVE_ETH * 1 ether;
+    uint private constant MIN_ACCEPTED_AMOUNT = MIN_ACCEPTED_AMOUNT_FINNEY * 1 finney;
+	
+
+    //constructor
+    function Presale () validSetupOnly() { }
+
+    //
+    // ======= interface methods =======
+    //
+
+    //accept payments here
+    function ()
+    payable
+    noReentrancy
+    {
+        State state = currentState();
+        if (state == State.PRESALE_RUNNING) {
+            receiveFunds();
+        } else if (state == State.REFUND_RUNNING) {
+            // any entring call in Refund Phase will cause full refund
+            sendRefund();
         } else {
-            // set data
-	        balances[msg.sender] += msg.value;
-	        transfered_total += msg.value;
+            throw;
         }
     }
-    
-    // Transfer ETH to Mysterium project wallet, as soon as minimum goal is reached.
-    function transfer_funds_to_project() {
-        if (!is_min_goal_reached()) throw;
-        if (this.balance == 0) throw;
-        
-        // transfer ethers to Mysterium project wallet
-        if (!project_wallet.send(this.balance)) throw;
+
+    function refund() external
+    inState(State.REFUND_RUNNING)
+    noReentrancy
+    {
+        sendRefund();
     }
-    
-    // Refund ETH in case minimum goal was not reached during presale.
-    // Refund will be available for two months window after presale.
-    function refund() {
-        if (!has_presale_time_ended()) throw;
-        if (is_min_goal_reached()) throw;
-        if (block.number > refund_window_end_block) throw;
-        
-        var amount = balances[msg.sender];
-        // check if sender has balance
-        if (amount == 0) throw;
-        
+
+
+    function withdrawFunds() external
+    inState(State.WITHDRAWAL_RUNNING)
+    onlyOwner
+    noReentrancy
+    {
+        // transfer funds to owner if any
+        if (this.balance > 0) {
+            if (!OWNER.send(this.balance)) throw;
+        }
+    }
+
+
+    //displays current contract state in human readable form
+    function state()  external constant
+    returns (string)
+    {
+        return stateNames[ uint(currentState()) ];
+    }
+
+
+    //
+    // ======= implementation methods =======
+    //
+
+    function sendRefund() private tokenHoldersOnly {
+        // load balance to refund plus amount currently sent
+        var amount_to_refund = balances[msg.sender] + msg.value;
         // reset balance
         balances[msg.sender] = 0;
-        
-        // actual refund
-        if (!msg.sender.send(amount)) throw;
+        // send refund back to sender
+        if (!msg.sender.send(amount_to_refund)) throw;
     }
-    
-    // In case any ETH has left unclaimed after two months window, send them to Mysterium project wallet.
-    function transfer_left_funds_to_project() {
-        if (!has_presale_time_ended()) throw;
-        if (is_min_goal_reached()) throw;
-        if (block.number <= refund_window_end_block) throw;
-        
-        if (this.balance == 0) throw;
-        // transfer left ETH to Mysterium project wallet
-        if (!project_wallet.send(this.balance)) throw;
+
+
+    function receiveFunds() private notTooSmallAmountOnly {
+      // no overflow is possible here: nobody have soo much money to spend.
+      if (total_received_amount + msg.value > MAX_TOTAL_AMOUNT_TO_RECEIVE) {
+          // accept amount only and return change
+          var change_to_return = total_received_amount + msg.value - MAX_TOTAL_AMOUNT_TO_RECEIVE;
+          if (!msg.sender.send(change_to_return)) throw;
+
+          var acceptable_remainder = MAX_TOTAL_AMOUNT_TO_RECEIVE - total_received_amount;
+          balances[msg.sender] += acceptable_remainder;
+          total_received_amount += acceptable_remainder;
+      } else {
+          // accept full amount
+          balances[msg.sender] += msg.value;
+          total_received_amount += msg.value;
+      }
     }
-}
+
+
+    function currentState() private constant returns (State) {
+        if (block.number < PRESALE_START) {
+            return State.BEFORE_START;
+        } else if (block.number <= PRESALE_END && total_received_amount < MAX_TOTAL_AMOUNT_TO_RECEIVE) {
+            return State.PRESALE_RUNNING;
+        } else if (block.number <= WITHDRAWAL_END && total_received_amount >= MIN_TOTAL_AMOUNT_TO_RECEIVE) {
+            return State.WITHDRAWAL_RUNNING;
+        } else if (this.balance > 0){
+            return State.REFUND_RUNNING;
+        } else {
+            return State.CLOSED;		
+		} 
+    }
+
+    //
+    // ============ modifiers ============
+    //
+
+    //fails if state dosn't match
+    modifier inState(State state) {
+        if (state != currentState()) throw;
+        _;
+    }
+
+
+    //fails if something in setup is looking weird
+    modifier validSetupOnly() {
+        if ( OWNER == 0x0 
+            || PRESALE_START == 0 
+            || PRESALE_END == 0 
+            || WITHDRAWAL_END ==0
+            || PRESALE_START <= block.number
+            || PRESALE_START >= PRESALE_END
+            || PRESALE_END   >= WITHDRAWAL_END
+            || MIN_TOTAL_AMOUNT_TO_RECEIVE > MAX_TOTAL_AMOUNT_TO_RECEIVE )
+				throw;
+        _;
+    }
+
+
+    //accepts calls from owner only
+    modifier onlyOwner(){
+    	if (msg.sender != OWNER)  throw;
+    	_;
+    }
+
+
+    //accepts calls from token holders only
+    modifier tokenHoldersOnly(){
+        if (balances[msg.sender] == 0) throw;
+        _;
+    }
+
+
+    // don`t accept transactions with value less than allowed minimum
+    modifier notTooSmallAmountOnly(){	
+        if (msg.value < MIN_ACCEPTED_AMOUNT) throw;
+        _;
+    }
+
+
+    //prevents reentrancy attacs
+    bool private locked = false;
+    modifier noReentrancy() {
+        if (locked) throw;
+        locked = true;
+        _;
+        locked = false;
+    }
+}//contract
