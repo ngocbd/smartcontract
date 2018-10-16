af17f8719a1e21b992a275fbde4e5a5f9826fa3b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:09:14 +0700

    add contract GetEbola

---
+++ GetEbola.sol
@@ -0,0 +1,46 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GetEbola at 0xe16f391e860420e65c659111c9e1601c0f8e2818
+*/
+pragma solidity ^0.4.17;
+
+contract GetEbola {
+    
+    address private creator = msg.sender;
+    
+    function getInfo() constant returns (string, string)
+    {
+        string memory developer = "Saluton, mia nomo estas Zach!"; // Tio estas mia nomo :)
+        string memory genomeInfo = "Ebola virus - Zaire, cat.1976"; // Ebola virus name and date genome was cataloged
+        return (developer, genomeInfo);
+    }
+    
+    function getEbola() constant returns (string)
+    {
+        // Returns bit.ly URL to swarm file bzz:/0191e5bf83b4b172ac36921a4ba1ceab49ba6178fcc35404047c04e6e5e95771
+        string memory genomeURL = "URL: http://bit.ly/0x4554482b45626f6c61";
+        return (genomeURL);
+    }
+    
+    function tipCreator() constant returns (string, address)
+    {
+        string memory tipMsg = "If you like you can tip me at this address :)";
+        address tipJar = creator; // Address of creator tip jar
+        return (tipMsg, tipJar);
+    }
+    
+    /**********
+     Standard kill() function to terminate contract 
+     **********/
+    
+    function kill() public returns (string)
+    { 
+        if (msg.sender == creator)
+        {
+            suicide(creator);  // kills the contract and sends balance to creator
+        }
+        else {
+            string memory nope = "Vi ne havas povon ?i tie!";
+            return (nope);
+        }
+    }
+}
