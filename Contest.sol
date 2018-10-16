463d0de81788621ddbfabd6622e3d8e2ad509db6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:29 +0700

    add contract Contest

--- Contest.sol
+++ Contest.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Contest at 0xd06fa8d5ec5a5e99f0b007a26b0485478ebbeb62
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Contest at 0x6c0bd1b6a5b64b6ed4718b65723d4c47a3e09d8b
 */
 /** 
 * The contract defining the contest, allowing participation and voting.
@@ -64,14 +64,14 @@ c4c = 0x87b0de512502f3e86fd22654b72a640c8e0f59cc;
 c4cfee = 1000;
 owner = msg.sender;
 
-deadlineParticipation=1494696420;
-deadlineVoting=1495732860;
-participationFee=7000000000000000;
-votingFee=1000000000000000;
-prizeOwner=300;
+deadlineParticipation=1493915340;
+deadlineVoting=1495038540;
+participationFee=50000000000000000;
+votingFee=5000000000000000;
+prizeOwner=200;
 prizeReferee=0;
 prizeWinners.push(6045);
-nLuckyVoters=2;
+nLuckyVoters=1;
 
 
 uint16 sumPrizes = prizeOwner;
