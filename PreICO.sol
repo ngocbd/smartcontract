c278e6cbc149a2ed723111966cfbf15c4a44a636
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:14:26 +0700

    add contract PreICO

--- PreICO.sol
+++ PreICO.sol
@@ -1,160 +1,296 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PreICO at 0x468b840b64e2de2849cec1321fe8131070029770
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PreICO at 0x00bbe94541b9c62aa09e59e55af5a6a22c16f2cc
 */
-pragma solidity ^0.4.15;
+/*! sptc.sol | (c) 2018 Develop by BelovITLab LLC (smartcontract.ru), author @stupidlovejoy | License: MIT */
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
+pragma solidity 0.4.18;
+
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+    function mul(uint256 a, uint256 b) internal pure returns(uint256) {
+        if(a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function div(uint256 a, uint256 b) internal pure returns(uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns(uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns(uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
+    address public owner;
 
-  address public owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
+    modifier onlyOwner() { require(msg.sender == owner); _; }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));
-    owner = newOwner;
-  }
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 }
 
-/**
- * @title Token
- * @dev API interface for interacting with the WILD Token contract 
- */
-interface Token {
-  function transfer(address _to, uint256 _value) returns (bool);
-  function balanceOf(address _owner) constant returns (uint256 balance);
+contract Pausable is Ownable {
+    bool public paused = false;
+
+    event Pause();
+    event Unpause();
+
+    modifier whenNotPaused() { require(!paused); _; }
+    modifier whenPaused() { require(paused); _; }
+
+    function pause() onlyOwner whenNotPaused public {
+        paused = true;
+        Pause();
+    }
+
+    function unpause() onlyOwner whenPaused public {
+        paused = false;
+        Unpause();
+    }
 }
 
-contract PreICO is Ownable {
+contract ERC20 {
+    uint256 public totalSupply;
 
-  using SafeMath for uint256;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-  Token token;
+    function balanceOf(address who) public view returns(uint256);
+    function transfer(address to, uint256 value) public returns(bool);
+    function transferFrom(address from, address to, uint256 value) public returns(bool);
+    function allowance(address owner, address spender) public view returns(uint256);
+    function approve(address spender, uint256 value) public returns(bool);
+}
 
-  uint256 public constant RATE = 20000; // Number of tokens per Ether
-  uint256 public constant CAP = 7500; // Cap in Ether
-  uint256 public constant START = 1517961600; // 07-02-2018 12:00:00 AM
-  uint256 public constant DAYS = 21; // 21 Days
-  
-  uint256 public constant initialTokens = 150000000 * 10**18; // Initial number of tokens available
-  bool public initialized = false;
-  uint256 public raisedAmount = 0;
+contract StandardToken is ERC20 {
+    using SafeMath for uint256;
 
-  event BoughtTokens(address indexed to, uint256 value);
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
 
-  modifier whenSaleIsActive() {
-    // Check if sale is active
-    assert(isActive());
+    function balanceOf(address _owner) public view returns(uint256 balance) {
+        return balances[_owner];
+    }
 
-    _;
-  }
+    function transfer(address _to, uint256 _value) public returns(bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
 
-  function PreICO(address _tokenAddr) {
-      require(_tokenAddr != 0);
-      token = Token(_tokenAddr);
-  }
-  
-  function initialize() onlyOwner {
-      require(initialized == false); // Can only be initialized once
-      require(tokensAvailable() == initialTokens); // Must have enough tokens allocated
-      initialized = true;
-  }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
-  function isActive() constant returns (bool) {
-    return (
-        initialized == true &&
-        now >= START && // Must be after the START date
-        now <= START.add(DAYS * 1 days) && // Must be before the end date
-        goalReached() == false // Goal must not already be reached
-    );
-  }
+        Transfer(msg.sender, _to, _value);
 
-  function goalReached() constant returns (bool) {
-    return (raisedAmount >= CAP * 1 ether);
-  }
-
-  function () payable {
-    buyTokens();
-  }
-
-  /**
-  * @dev function that sells available tokens
-  */
-  function buyTokens() payable whenSaleIsActive {
-    // Calculate tokens to sell
-    uint256 weiAmount = msg.value;
-    uint256 tokens = weiAmount.mul(RATE);
-
-    BoughtTokens(msg.sender, tokens);
-
-    // Increment raised amount
-    raisedAmount = raisedAmount.add(msg.value);
+        return true;
+    }
     
-    // Send tokens to buyer
-    token.transfer(msg.sender, tokens);
+    function multiTransfer(address[] _to, uint256[] _value) public returns(bool) {
+        require(_to.length == _value.length);
+
+        for(uint i = 0; i < _to.length; i++) {
+            transfer(_to[i], _value[i]);
+        }
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns(uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function approve(address _spender, uint256 _value) public returns(bool) {
+        allowed[msg.sender][_spender] = _value;
+
+        Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns(bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns(bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+
+        if(_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+
+        return true;
+    }
+}
+
+contract MintableToken is StandardToken, Ownable {
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
+
+    bool public mintingFinished = false;
+
+    modifier canMint() { require(!mintingFinished); _; }
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
+
+        return true;
+    }
+
+    function finishMinting() onlyOwner canMint public returns(bool) {
+        mintingFinished = true;
+
+        MintFinished();
+
+        return true;
+    }
+}
+
+contract CappedToken is MintableToken {
+    uint256 public cap;
+
+    function CappedToken(uint256 _cap) public {
+        require(_cap > 0);
+        cap = _cap;
+    }
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
+        require(totalSupply.add(_amount) <= cap);
+
+        return super.mint(_to, _amount);
+    }
+}
+
+/*
+    ICO Sportcy
+*/
+contract Token is CappedToken {
+    string public name = "SPORTCY";
+    string public symbol = "SPTC";
+    uint256 public decimals = 18;
+
+    function Token() CappedToken(3000000000 * 1 ether) public {
     
-    // Send money to owner
-    owner.transfer(msg.value);
-  }
+    }
+}
 
-  /**
-   * @dev returns the number of tokens allocated to this contract
-   */
-  function tokensAvailable() constant returns (uint256) {
-    return token.balanceOf(this);
-  }
+contract PreICO is Pausable {
+    using SafeMath for uint;
 
-  /**
-   * @notice Terminate contract and refund to owner
-   */
-  function destroy() onlyOwner {
-    // Transfer tokens back to owner
-    uint256 balance = token.balanceOf(this);
-    assert(balance > 0);
-    token.transfer(owner, balance);
+    Token public token;
+    address public beneficiary = 0xd2F04a987C17EEd55e8AFF25CADccF3D2D2CE845;
 
-    // There should be no ether in the contract but just in case
-    selfdestruct(owner);
-  }
+    uint public collectedWei;
+    uint public tokensSold;
 
+    uint public tokensForSale = 108000000 * 1 ether;
+    uint public priceTokenWei = 1 ether;
+
+    bool public preicoClosed = false;
+
+    event NewRate(uint256 rate);
+    event Purchase(address indexed holder, uint256 tokenAmount, uint256 etherAmount);
+    event PreicoClose();
+
+    function PreICO() public {
+        token = new Token();
+        setTokenRate(8400);
+    }
+
+    function() payable public {
+        purchase();
+    }
+
+    function setTokenRate(uint _value) onlyOwner public {
+        require(!preicoClosed);
+        
+        priceTokenWei = 1 ether / _value;
+
+        NewRate(priceTokenWei);
+    }
+    
+    function purchase() whenNotPaused payable public {
+        require(!preicoClosed);
+        require(tokensSold < tokensForSale);
+        require(msg.value >= 0.001 ether);
+
+        uint sum = msg.value;
+        uint amount = sum.mul(1 ether).div(priceTokenWei);
+        uint retSum = 0;
+        
+        if(tokensSold.add(amount) > tokensForSale) {
+            uint retAmount = tokensSold.add(amount).sub(tokensForSale);
+            retSum = retAmount.mul(priceTokenWei).div(1 ether);
+
+            amount = amount.sub(retAmount);
+            sum = sum.sub(retSum);
+        }
+
+        tokensSold = tokensSold.add(amount);
+        collectedWei = collectedWei.add(sum);
+
+        beneficiary.transfer(sum);
+        token.mint(msg.sender, amount);
+
+        if(retSum > 0) {
+            msg.sender.transfer(retSum);
+        }
+
+        Purchase(msg.sender, amount, sum);
+    }
+
+    function closePreICO() onlyOwner public {
+        require(!preicoClosed);
+        
+        token.transferOwnership(beneficiary);
+
+        preicoClosed = true;
+
+        PreicoClose();
+    }
 }
