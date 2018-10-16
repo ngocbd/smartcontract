81ed7ec53a0a9afff80c5d7f257b80fad782e12b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:05:13 +0700

    add contract Vault

--- Vault.sol
+++ Vault.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0x579472a4230d5ef5dada4c55fa437bf6a1bac2e4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Vault at 0x866b1c78ce40d5fa1201283ad8df3baf31b141e7
 */
 pragma solidity ^0.4.19;
 
@@ -12,7 +12,7 @@ contract Vault {
     Token constant public token = Token(0xa645264C5603E96c3b0B078cdab68733794B0A71);
     address constant public recipient = 0x002AE208AD6064F75Fa78e7bbeF9B12DB850f559;
     // UNIX timestamp
-    uint constant public unlockedAt = 1517760000;
+    uint constant public unlockedAt = 1564704000;
     
     function unlock() public {
         require(now > unlockedAt);
