e0c67a9abbb0d10f07cac4ae8c7a80f0e1f7c19d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:06:33 +0700

    add contract Utils

--- Utils.sol
+++ Utils.sol
@@ -1,17 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Utils at 0x6fc61357f00b7735e5a75156522def9cfb20ee02
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Utils at 0xaec7fa67e32f9ac53ce0684f508510324c018dcc
 */
 pragma solidity ^0.4.18;
 
 
+contract makerDAO {
+    function read() view public returns(bytes32);
+}
+
 
 /// @title Kyber constants contract
 contract Utils {
-  function testrc(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns(address) {
-    return ecrecover(hash, v, r, s);
-  }
-
-  function hash(uint x, uint y, uint z, uint w) public pure returns(bytes32) {
-    return sha3(x,y,z,w);
-  }
+    function toUint(bytes32 x) view public returns(uint) {
+        return uint(x);
+    }
+    
+    function test1() view public returns(uint){
+        bytes32 y = bytes32(0x123456);
+        return toUint(y);
+    }
+    
+    function testDAO() view public returns(uint) {
+        return toUint(makerDAO(0x729D19f657BD0614b4985Cf1D82531c67569197B).read());
+    }
 }
