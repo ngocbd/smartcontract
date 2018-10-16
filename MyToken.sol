e42ed19825daafe6ca37c4e45d937d96001d9509
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:28:38 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,108 +1,126 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x563383b56367Ff2afFFE5c6BCF9187bBE52d40Ad
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x864def510D754DF9c1d43B925DD9887baa5Acb07
 */
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.11;
+contract owned {
+    address public owner;
+    address public authorisedContract;
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    modifier onlyAuthorisedAddress{
+        require(msg.sender == authorisedContract);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+    modifier onlyPayloadSize(uint size) {
+     assert(msg.data.length == size + 4);
+     _;
+    }
+}
+
 contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 
-contract MyToken {
+contract MyToken is owned {
     /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-    mapping (address => uint256) public frozenAccount;
-
+    string public name = "DankToken";
+    string public symbol = "DANK";
+    uint8 public decimals = 18;
+    uint256 _totalSupply;
+    uint256 public amountRaised = 0;
+    uint256 public amountOfTokensPerEther = 500;
+        /* this makes an array with all frozen accounts. This is needed so voters can not send their funds while the vote is going on and they have already voted      */
+    mapping (address => bool) public frozenAccounts;
+        /* This creates an array with all balances */ 
+    mapping (address => uint256) _balanceOf;
+    mapping (address => mapping (address => uint256)) _allowance;
+    bool public crowdsaleClosed = false;
     /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-    
-    /* For frozen premine notifications*/
-    event FrozenFunds(address target, uint256 frozen);
-
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event FrozenFunds(address target, bool frozen);
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyToken() {
-        balanceOf[msg.sender] = 3330000000000;              // Give the creator all initial tokens
-        totalSupply = 3330000000000;                        // Update total supply
-        name = 'Hubcoin';                                   // Set the name for display purposes
-        symbol = 'HUB';                                     // Set the symbol for display purposes
-        decimals = 6;                                       // Amount of decimals for display purposes
+        _balanceOf[msg.sender] = 4000000000000000000000;              
+        _totalSupply = 4000000000000000000000;                 
+        Transfer(this, msg.sender,4000000000000000000000);
     }
-
+    function changeAuthorisedContract(address target) onlyOwner
+    {
+        authorisedContract = target;
+    }
+    function() payable{
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+        amountRaised += amount;
+        uint256 totalTokens = amount * amountOfTokensPerEther;
+        _balanceOf[msg.sender] += totalTokens;
+        _totalSupply += totalTokens;
+        Transfer(this,msg.sender, totalTokens);
+    }
+     function totalSupply() constant returns (uint TotalSupply){
+        TotalSupply = _totalSupply;
+     }
+      function balanceOf(address _owner) constant returns (uint balance) {
+        return _balanceOf[_owner];
+     }
+     function closeCrowdsale() onlyOwner{
+         crowdsaleClosed = true;
+     }
+     function openCrowdsale() onlyOwner{
+         crowdsaleClosed = false;
+     }
+     function changePrice(uint newAmountOfTokensPerEther) onlyOwner{
+         require(newAmountOfTokensPerEther <= 500);
+         amountOfTokensPerEther = newAmountOfTokensPerEther;
+     }
+     function withdrawal(uint256 amountOfWei) onlyOwner{
+         if(owner.send(amountOfWei)){}
+     }
+     function freezeAccount(address target, bool freeze) onlyAuthorisedAddress
+     {
+         frozenAccounts[target] = freeze;
+         FrozenFunds(target, freeze);
+     } 
+     
     /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        uint forbiddenPremine =  1501545600 - block.timestamp + 86400*365;
-        if (forbiddenPremine < 0) forbiddenPremine = 0;
-        
-        
-        require(_to != 0x0);                                 // Prevent transfer to 0x0 address. Use burn() instead
-        require(balanceOf[msg.sender] > _value + frozenAccount[msg.sender] * forbiddenPremine / (86400*365) );    // Check if the sender has enough
-        require(balanceOf[_to] + _value > balanceOf[_to]);   // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2*32) {
+        require(!frozenAccounts[msg.sender]);
+        require(_balanceOf[msg.sender] > _value);          // Check if the sender has enough
+        require(_balanceOf[_to] + _value > _balanceOf[_to]); // Check for overflows
+        _balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        _balanceOf[_to] += _value;                            // Add the same to the recipient
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
-
     /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+    function approve(address _spender, uint256 _value)onlyPayloadSize(2*32)
+        returns (bool success)  {
+        _allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
         return true;
-    }
-
-    /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }        
+    } 
 
     /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        uint forbiddenPremine =  1501545600 - block.timestamp + 86400*365;        
-        if (forbiddenPremine < 0) forbiddenPremine = 0;   
-        
-        require(_to != 0x0);                                // Prevent transfer to 0x0 address. Use burn() instead
-        require(balanceOf[_from] > _value + frozenAccount[_from] * forbiddenPremine / (86400*365) );    // Check if the sender has enough
-        require(balanceOf[_to] + _value > balanceOf[_to]);  // Check for overflows
-        require(_value < allowance[_from][msg.sender]);     // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success)  {
+        require(!frozenAccounts[_from]);
+        require(_balanceOf[_from] > _value);                 // Check if the sender has enough
+        require(_balanceOf[_to] + _value > _balanceOf[_to]);  // Check for overflows
+        require(_allowance[_from][msg.sender] >= _value);     // Check allowance
+        _balanceOf[_from] -= _value;                           // Subtract from the sender
+        _balanceOf[_to] += _value;                             // Add the same to the recipient
+        _allowance[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
         return true;
     }
-
-    function burn(uint256 _value) returns (bool success) {
-        require(balanceOf[msg.sender] > _value);            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
-        totalSupply -= _value;                              // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        require(balanceOf[_from] > _value);                // Check if the sender has enough
-        require(_value < allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                        // Subtract from the sender
-        totalSupply -= _value;                             // Updates totalSupply
-        Burn(_from, _value);
-        return true;
-    }
-    
-    function freezeAccount(address target, uint256 freeze) {
-        require(msg.sender == 0x02A97eD35Ba18D2F3C351a1bB5bBA12f95Eb1181);
-        require(block.timestamp < 1502036759 + 3600*10);
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+        return _allowance[_owner][_spender];
     }
 }
