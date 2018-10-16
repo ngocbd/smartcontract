fae9b973e6b06402a384fee1a1704780972acdb9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:08:26 +0700

    add contract Shitpost

---
+++ Shitpost.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Shitpost at 0x7e96a18d7aeec64c9c99ed388ff3907d156030de
+*/
+pragma solidity ^0.4.2;
+
+contract Shitpost 
+{
+	string constant public poster = "PeenuttButler";
+	string constant public message = "On 2017/4/21, I shitposted on the Ethereum blockchain.";
+}
