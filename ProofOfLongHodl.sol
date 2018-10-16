370b475b497940bcc3896a12544515c040717c3d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:27:12 +0700

    add contract ProofOfLongHodl

--- ProofOfLongHodl.sol
+++ ProofOfLongHodl.sol
@@ -1,10 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfLongHodl at 0x425e9ec3181d9ddfbfc417f1b9b55dbbd78c3a05
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProofOfLongHodl at 0x61ea25cfd1509888211f9e58e82edde74acc59b1
 */
-pragma solidity ^0.4.20;
-
 /*
-* Team Proof of Long Hodl presents..
+* Team Proof of Long Hodl presents... v2
 */
 
 pragma solidity ^0.4.21;
@@ -21,9 +19,9 @@ contract ProofOfLongHodl {
     mapping(address => bool) preauthorized;
     bool gameStarted;
 
-    uint constant depositTaxDivisor = 29;		// 29% of  deposits goes to  divs
-    uint constant withdrawalTaxDivisor = 29;	// 29% of  withdrawals goes to  divs
-    uint constant lotteryFee = 25; 				// 4% of deposits and withdrawals goes to lotteryPool
+    uint constant depositTaxDivisor = 5;		// 20% of  deposits goes to  divs
+    uint constant withdrawalTaxDivisor = 5;	// 20% of  withdrawals goes to  divs
+    uint constant lotteryFee = 20; 				// 5% of deposits and withdrawals goes to dailyPool
 
     mapping(address => uint) public investment;
 
@@ -51,20 +49,60 @@ contract ProofOfLongHodl {
 
     function depositHelper(uint _amount) private {
     	require(_amount > 0);
-        uint _tax = _amount.mul(depositTaxDivisor).div(100);
-        uint _lotteryPool = _amount.div(lotteryFee); // add to lottery fee
+        uint _tax = _amount.div(depositTaxDivisor);
+        uint _lotteryPool = _amount.div(lotteryFee);
         uint _amountAfterTax = _amount.sub(_tax).sub(_lotteryPool);
 
-        lotteryPool = lotteryPool.add(_lotteryPool);
+        // weekly and daily pool
+        uint weeklyPoolFee = _lotteryPool.div(5);
+        uint dailyPoolFee = _lotteryPool.sub(weeklyPoolFee);
 
-        // check if first deposit, and greater than and make user eligable for lottery
-        if (isEligable[msg.sender] == false &&  _amount > 0.1 ether) {
-        	isEligable[msg.sender] = true;
-        	hasWithdrawed[msg.sender] = false;
+        uint tickets = _amount.div(TICKET_PRICE);
 
-        	lotteryAddresses.push(msg.sender);
-        	eligableIndex[msg.sender] = lotteryAddresses.length - 1;      	
+        weeklyPool = weeklyPool.add(weeklyPoolFee);
+        dailyPool = dailyPool.add(dailyPoolFee);
+
+        //********** ADD DAILY TICKETS
+        dailyTicketPurchases storage dailyPurchases = dailyTicketsBoughtByPlayer[msg.sender];
+
+        // If we need to reset tickets from a previous lotteryRound
+        if (dailyPurchases.lotteryId != dailyLotteryRound) {
+            dailyPurchases.numPurchases = 0;
+            dailyPurchases.ticketsPurchased = 0;
+            dailyPurchases.lotteryId = dailyLotteryRound;
+            dailyLotteryPlayers[dailyLotteryRound].push(msg.sender); // Add user to lottery round
         }
+
+        // Store new ticket purchase
+        if (dailyPurchases.numPurchases == dailyPurchases.ticketsBought.length) {
+            dailyPurchases.ticketsBought.length += 1;
+        }
+        dailyPurchases.ticketsBought[dailyPurchases.numPurchases++] = dailyTicketPurchase(dailyTicketsBought, dailyTicketsBought + (tickets - 1)); // (eg: buy 10, get id's 0-9)
+        
+        // Finally update ticket total
+        dailyPurchases.ticketsPurchased += tickets;
+        dailyTicketsBought += tickets;
+
+        //********** ADD WEEKLY TICKETS
+		weeklyTicketPurchases storage weeklyPurchases = weeklyTicketsBoughtByPlayer[msg.sender];
+
+		// If we need to reset tickets from a previous lotteryRound
+		if (weeklyPurchases.lotteryId != weeklyLotteryRound) {
+		    weeklyPurchases.numPurchases = 0;
+		    weeklyPurchases.ticketsPurchased = 0;
+		    weeklyPurchases.lotteryId = weeklyLotteryRound;
+		    weeklyLotteryPlayers[weeklyLotteryRound].push(msg.sender); // Add user to lottery round
+		}
+
+		// Store new ticket purchase
+		if (weeklyPurchases.numPurchases == weeklyPurchases.ticketsBought.length) {
+		    weeklyPurchases.ticketsBought.length += 1;
+		}
+		weeklyPurchases.ticketsBought[weeklyPurchases.numPurchases++] = weeklyTicketPurchase(weeklyTicketsBought, weeklyTicketsBought + (tickets - 1)); // (eg: buy 10, get id's 0-9)
+
+		// Finally update ticket total
+		weeklyPurchases.ticketsPurchased += tickets;
+		weeklyTicketsBought += tickets;
 
         if (totalStake > 0)
             stakeValue = stakeValue.add(_tax.div(totalStake));
@@ -84,41 +122,16 @@ contract ProofOfLongHodl {
     function withdraw(uint _amount) public {
         require(_amount > 0);
         require(_amount <= investment[msg.sender]);
-        uint _tax = _amount.mul(withdrawalTaxDivisor).div(100);
-        uint _lotteryPool = _amount.div(lotteryFee); // add to lottery fee
+        uint _tax = _amount.div(withdrawalTaxDivisor);
+        uint _lotteryPool = _amount.div(lotteryFee);
         uint _amountAfterTax = _amount.sub(_tax).sub(_lotteryPool);
 
-        lotteryPool = lotteryPool.add(_lotteryPool);
+        // weekly and daily pool
+        uint weeklyPoolFee = _lotteryPool.div(20);
+        uint dailyPoolFee = _lotteryPool.sub(weeklyPoolFee);
 
-        // removing user from lotteryAddresses if it is first withdraw
-        if (lotteryAddresses.length != 0 && !hasWithdrawed[msg.sender] ) {
-        	hasWithdrawed[msg.sender] = true;
-        	isEligable[msg.sender] = false;
-        	totalWithdrawals = totalWithdrawals.add(_amountAfterTax);
-        	withdrawalsCTR++;
-
-        	// delete user from lottery addresses index to delete
-        	uint indexToDelete = eligableIndex[msg.sender]; 
-        	address lastAddress = lotteryAddresses[lotteryAddresses.length - 1];
-        	lotteryAddresses[indexToDelete] = lastAddress;
-        	lotteryAddresses.length--;
-
-        	eligableIndex[lastAddress] = indexToDelete;
-        	eligableIndex[msg.sender] = 0;
-
-        	if (withdrawalsCTR > 9 && totalWithdrawals > 1 ether) {
-        		// pick lottery winner and sent reward
-			    uint256 winnerIndex = rand(lotteryAddresses.length);
-			    address winner = lotteryAddresses[winnerIndex];
-
-			    winner.transfer(lotteryPool);
-			    totalWithdrawals = 0;
-			    withdrawalsCTR = 0;
-			    lotteryPool = 0;
-			    lotteryRound++;
-			    lastWinner = winner;
-        	}
-        }
+        weeklyPool = weeklyPool.add(weeklyPoolFee);
+        dailyPool = dailyPool.add(dailyPoolFee);
 
         uint _stakeDecrement = stake[msg.sender].mul(_amount).div(investment[msg.sender]);
         uint _dividendCredit = _stakeDecrement.mul(stakeValue);
@@ -176,41 +189,260 @@ contract ProofOfLongHodl {
         }
     }
 
-    // LOTTERY MODULE
-    uint private lotteryPool = 0;
-    uint private lotteryRound = 1;
-    address private lastWinner;
+    // LOTTERY MODULE 
+    // DAILY
+    uint private dailyPool = 0;
+    uint private dailyLotteryRound = 1;
+    uint private dailyTicketsBought = 0;
+    uint private dailyTicketThatWon;
+    address[] public dailyWinners;
+    uint256[] public dailyPots;
 
-    uint public withdrawalsCTR = 0;
-    uint public totalWithdrawals = 0;
+    // WEEKLY
+    uint private weeklyPool = 0;
+    uint private weeklyLotteryRound = 1;
+    uint private weeklyTicketsBought = 0;
+    uint private weeklyTicketThatWon;
+    address[] public weeklyWinners;
+    uint256[] public weeklyPots;
 
-    mapping(address => uint256) internal eligableIndex; // 
-    mapping(address => bool) internal isEligable; // for first deposit check
-    mapping(address => bool) internal hasWithdrawed; // check if user already withdrawed
+    uint public TICKET_PRICE = 0.01 ether;
+    uint public DAILY_LIMIT = 0.15 ether;
+    bool private dailyTicketSelected;
+    bool private weeklyTicketSelected;
 
-    address[] public lotteryAddresses;
-
-    // Generate random number between 0 & max
-    uint256 constant private FACTOR =  1157920892373161954235709850086879078532699846656405640394575840079131296399;
-    function rand(uint max) constant public returns (uint256 result){
-        uint256 factor = FACTOR * 100 / max;
-        uint256 lastBlockNumber = block.number - 1;
-        uint256 hashVal = uint256(block.blockhash(lastBlockNumber));
-    
-        return uint256((uint256(hashVal) / factor)) % max;
+    // STRUCTS for LOTTERY
+    // DAILY
+    struct dailyTicketPurchases {
+        dailyTicketPurchase[] ticketsBought;
+        uint256 numPurchases; // Allows us to reset without clearing dailyTicketPurchase[] (avoids potential for gas limit)
+        uint256 lotteryId;
+        uint256 ticketsPurchased;
     }
 
-    // check if address is withdrawed
-    function checkIfEligable(address _address) public view returns (bool) {
-    	return (isEligable[_address] && !hasWithdrawed[_address]) ;
+    // Allows us to query winner without looping (avoiding potential for gas limit)
+    struct dailyTicketPurchase {
+        uint256 startId;
+        uint256 endId;
     }
 
-    function getLotteryData() public view returns( uint256, uint256, address) {
-    	return (lotteryPool, lotteryRound, lastWinner);
+    mapping(address => dailyTicketPurchases) private dailyTicketsBoughtByPlayer;
+    mapping(uint256 => address[]) private dailyLotteryPlayers;
+
+    // WEEKLY
+    struct weeklyTicketPurchases {
+        weeklyTicketPurchase[] ticketsBought;
+        uint256 numPurchases; // Allows us to reset without clearing weeklyTicketPurchase[] (avoids potential for gas limit)
+        uint256 lotteryId;
+        uint256 ticketsPurchased;
     }
 
-    function lotteryParticipants() public view returns(uint256) {
-    	return lotteryAddresses.length;
+    // Allows us to query winner without looping (avoiding potential for gas limit)
+    struct weeklyTicketPurchase {
+        uint256 startId;
+        uint256 endId;
+    }
+
+    mapping(address => weeklyTicketPurchases) private weeklyTicketsBoughtByPlayer;
+    mapping(uint256 => address[]) private weeklyLotteryPlayers;
+
+    // DRAWS
+    function drawDailyWinner() public {
+        require(msg.sender == owner);
+        require(!dailyTicketSelected);
+       
+        uint256 seed = dailyTicketsBought + block.timestamp;
+        dailyTicketThatWon = addmod(uint256(block.blockhash(block.number-1)), seed, dailyTicketsBought);
+        dailyTicketSelected = true;
+    }
+
+    function drawWeeklyWinner() public {
+        require(msg.sender == owner);
+        require(!weeklyTicketSelected);
+       
+        uint256 seed = weeklyTicketsBought + block.timestamp;
+        weeklyTicketThatWon = addmod(uint256(block.blockhash(block.number-1)), seed, weeklyTicketsBought);
+        weeklyTicketSelected = true;
+    }
+
+    function awardDailyLottery(address checkWinner, uint256 checkIndex) external {
+		require(msg.sender == owner);
+	    
+	    if (!dailyTicketSelected) {
+	    	drawDailyWinner(); // Ideally do it in one call (gas limit cautious)
+	    }
+	        
+	    // Reduce gas by (optionally) offering an address to _check_ for winner
+	    if (checkWinner != 0) {
+	        dailyTicketPurchases storage tickets = dailyTicketsBoughtByPlayer[checkWinner];
+	        if (tickets.numPurchases > 0 && checkIndex < tickets.numPurchases && tickets.lotteryId == dailyLotteryRound) {
+	            dailyTicketPurchase storage checkTicket = tickets.ticketsBought[checkIndex];
+	            if (dailyTicketThatWon >= checkTicket.startId && dailyTicketThatWon <= checkTicket.endId) {
+	                if ( dailyPool >= DAILY_LIMIT) {
+	            		checkWinner.transfer(DAILY_LIMIT);
+	            		dailyPots.push(DAILY_LIMIT);
+	            		dailyPool = dailyPool.sub(DAILY_LIMIT);		
+	        		} else {
+	        			checkWinner.transfer(dailyPool);
+	        			dailyPots.push(dailyPool);
+	        			dailyPool = 0;
+	        		}
+
+	        		dailyWinners.push(checkWinner);
+            		dailyLotteryRound = dailyLotteryRound.add(1);
+            		dailyTicketsBought = 0;
+            		dailyTicketSelected = false;
+	                return;
+	            }
+	        }
+	    }
+	    
+	    // Otherwise just naively try to find the winner (will work until mass amounts of players)
+	    for (uint256 i = 0; i < dailyLotteryPlayers[dailyLotteryRound].length; i++) {
+	        address player = dailyLotteryPlayers[dailyLotteryRound][i];
+	        dailyTicketPurchases storage playersTickets = dailyTicketsBoughtByPlayer[player];
+	        
+	        uint256 endIndex = playersTickets.numPurchases - 1;
+	        // Minor optimization to avoid checking every single player
+	        if (dailyTicketThatWon >= playersTickets.ticketsBought[0].startId && dailyTicketThatWon <= playersTickets.ticketsBought[endIndex].endId) {
+	            for (uint256 j = 0; j < playersTickets.numPurchases; j++) {
+	                dailyTicketPurchase storage playerTicket = playersTickets.ticketsBought[j];
+	                if (dailyTicketThatWon >= playerTicket.startId && dailyTicketThatWon <= playerTicket.endId) {
+	                	if ( dailyPool >= DAILY_LIMIT) {
+	                		player.transfer(DAILY_LIMIT);
+	                		dailyPots.push(DAILY_LIMIT);
+	                		dailyPool = dailyPool.sub(DAILY_LIMIT);
+	            		} else {
+	            			player.transfer(dailyPool);
+	            			dailyPots.push(dailyPool);
+	            			dailyPool = 0;
+	            		}
+
+	            		dailyWinners.push(player);
+	            		dailyLotteryRound = dailyLotteryRound.add(1);
+	            		dailyTicketsBought = 0;
+	            		dailyTicketSelected = false;
+
+	                    return;
+	                }
+	            }
+	        }
+	    }
+	}
+
+	function awardWeeklyLottery(address checkWinner, uint256 checkIndex) external {
+		require(msg.sender == owner);
+	    
+	    if (!weeklyTicketSelected) {
+	    	drawWeeklyWinner(); // Ideally do it in one call (gas limit cautious)
+	    }
+	       
+	    // Reduce gas by (optionally) offering an address to _check_ for winner
+	    if (checkWinner != 0) {
+	        weeklyTicketPurchases storage tickets = weeklyTicketsBoughtByPlayer[checkWinner];
+	        if (tickets.numPurchases > 0 && checkIndex < tickets.numPurchases && tickets.lotteryId == weeklyLotteryRound) {
+	            weeklyTicketPurchase storage checkTicket = tickets.ticketsBought[checkIndex];
+	            if (weeklyTicketThatWon >= checkTicket.startId && weeklyTicketThatWon <= checkTicket.endId) {
+	        		checkWinner.transfer(weeklyPool);
+
+	        		weeklyPots.push(weeklyPool);
+	        		weeklyPool = 0;
+	            	weeklyWinners.push(player);
+	            	weeklyLotteryRound = weeklyLotteryRound.add(1);
+	            	weeklyTicketsBought = 0;
+	            	weeklyTicketSelected = false;
+	                return;
+	            }
+	        }
+	    }
+	    
+	    // Otherwise just naively try to find the winner (will work until mass amounts of players)
+	    for (uint256 i = 0; i < weeklyLotteryPlayers[weeklyLotteryRound].length; i++) {
+	        address player = weeklyLotteryPlayers[weeklyLotteryRound][i];
+	        weeklyTicketPurchases storage playersTickets = weeklyTicketsBoughtByPlayer[player];
+	        
+	        uint256 endIndex = playersTickets.numPurchases - 1;
+	        // Minor optimization to avoid checking every single player
+	        if (weeklyTicketThatWon >= playersTickets.ticketsBought[0].startId && weeklyTicketThatWon <= playersTickets.ticketsBought[endIndex].endId) {
+	            for (uint256 j = 0; j < playersTickets.numPurchases; j++) {
+	                weeklyTicketPurchase storage playerTicket = playersTickets.ticketsBought[j];
+	                if (weeklyTicketThatWon >= playerTicket.startId && weeklyTicketThatWon <= playerTicket.endId) {
+	            		player.transfer(weeklyPool);  
+
+	            		weeklyPots.push(weeklyPool);
+	            		weeklyPool = 0;
+	            		weeklyWinners.push(player);
+	            		weeklyLotteryRound = weeklyLotteryRound.add(1);
+	            		weeklyTicketsBought = 0;  
+	            		weeklyTicketSelected = false;            
+	                    return;
+	                }
+	            }
+	        }
+	    }
+	}
+
+    function getLotteryData() public view returns( uint256, uint256, uint256, uint256, uint256, uint256) {
+    	return (dailyPool, weeklyPool, dailyLotteryRound, weeklyLotteryRound, dailyTicketsBought, weeklyTicketsBought);
+    }
+
+    function getDailyLotteryParticipants(uint256 _round) public view returns(address[]) {
+    	return dailyLotteryPlayers[_round];
+    }
+
+    function getWeeklyLotteryParticipants(uint256 _round) public view returns(address[]) {
+    	return weeklyLotteryPlayers[_round];
+    }
+
+    function getLotteryWinners() public view returns(uint256, uint256) {
+    	return (dailyWinners.length, weeklyWinners.length);
+    }
+
+    function editDailyLimit(uint _price) public payable {
+    	require(msg.sender == owner);
+    	DAILY_LIMIT = _price;
+    }
+
+    function editTicketPrice(uint _price) public payable {
+    	require(msg.sender == owner);
+    	TICKET_PRICE = _price;
+    }
+
+    function getDailyTickets(address _player) public view returns(uint256) {
+    	dailyTicketPurchases storage dailyPurchases = dailyTicketsBoughtByPlayer[_player];
+
+    	if (dailyPurchases.lotteryId != dailyLotteryRound) {
+    		return 0;
+    	}
+
+    	return dailyPurchases.ticketsPurchased;
+    }
+
+    function getWeeklyTickets(address _player) public view returns(uint256) {
+    	weeklyTicketPurchases storage weeklyPurchases = weeklyTicketsBoughtByPlayer[_player];
+
+    	if (weeklyPurchases.lotteryId != weeklyLotteryRound) {
+    		return 0;
+    	}
+
+    	return weeklyPurchases.ticketsPurchased;	
+    }
+
+    // If someone is generous and wants to add to pool
+    function addToPool() public payable {
+    	require(msg.value > 0);
+    	uint _lotteryPool = msg.value;
+
+    	// weekly and daily pool
+        uint weeklyPoolFee = _lotteryPool.div(5);
+        uint dailyPoolFee = _lotteryPool.sub(weeklyPoolFee);
+
+        weeklyPool = weeklyPool.add(weeklyPoolFee);
+        dailyPool = dailyPool.add(dailyPoolFee);
+    }
+
+    function winningTickets() public view returns(uint256, uint256) {
+    	return (dailyTicketThatWon, weeklyTicketThatWon);
     }
     
 }
