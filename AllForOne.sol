97fd0f51da6ef3e08778479314189c40c329dc03
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:05:45 +0700

    add contract AllForOne

--- AllForOne.sol
+++ AllForOne.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AllForOne at 0x5173d352751af4e7593e599febac1e2205f8cd27
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AllForOne at 0x51996c0a12526138815407bc61135fed50c0d136
 */
 pragma solidity ^0.4.21;
 // The Original All For 1 -  www.allfor1.io
@@ -9,23 +9,23 @@ pragma solidity ^0.4.21;
 contract AllForOne {
     
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    mapping (address => uint) private playerCount;
-    mapping (address => uint) private currentGame;
-    mapping (address => uint) private currentPlayersRequired;
     mapping (address => uint) private playerRegistrationStatus;
     mapping (address => uint) private confirmedWinners;
     mapping (uint => address) private numberToAddress;
+    uint private currentPlayersRequired;
     uint private currentBet;
+    uint private playerCount;
     uint private jackpot;
-    uint private ownerBalance;
+    uint private revealBlock;
+    uint private currentGame;
     address private contractAddress;
     address private owner;
     address private lastWinner;
     
     function AllForOne () {
         contractAddress = this;
-        currentGame[contractAddress]++;
-        currentPlayersRequired[contractAddress] = 25;
+        currentGame++;
+        currentPlayersRequired = 25;
         owner = msg.sender;
         currentBet = 0.005 ether;
         lastWinner = msg.sender;
@@ -35,27 +35,24 @@ contract AllForOne {
         _;
     }
     modifier changeBetConditions () {
-        require (playerCount[contractAddress] == 0);
+        require (playerCount == 0);
         require (contractAddress.balance == 0 ether);
         _;
     }
     modifier betConditions () {
-        require (playerRegistrationStatus[msg.sender] < currentGame[contractAddress]);
-        require (playerCount[contractAddress] < currentPlayersRequired[contractAddress]);
+        require (playerRegistrationStatus[msg.sender] < currentGame);
+        require (playerCount < currentPlayersRequired);
         require (msg.value == currentBet);
         require (confirmedWinners[msg.sender] == 0);
         _;
     }
     modifier revealConditions () {
-        require (playerCount[contractAddress] == currentPlayersRequired[contractAddress]);
+        require (playerCount == currentPlayersRequired);
+        require (block.blockhash(revealBlock) != 0);
         _;
     }
     modifier winnerWithdrawConditions () {
         require (confirmedWinners[msg.sender] == 1);
-        _;
-    }
-    modifier ownerWithdrawConditions () {
-        require (ownerBalance >= 1);
         _;
     }
     function transferOwnership (address newOwner) public onlyOwner {
@@ -68,35 +65,38 @@ contract AllForOne {
     }
     function canBet () view public returns (uint, uint, address) {
         uint _status = 0;
-        uint _playerCount = playerCount[contractAddress];
+        uint _playerCount = playerCount;
         address _lastWinner = lastWinner;
-        if (playerRegistrationStatus[msg.sender] < currentGame[contractAddress]) {
+        if (playerRegistrationStatus[msg.sender] < currentGame) {
         _status = 1;
         }
         return (_status, _playerCount, _lastWinner);
     }
     function placeBet () payable betConditions {
-        playerCount[contractAddress]++;
-        playerRegistrationStatus[msg.sender] = currentGame[contractAddress];
-        numberToAddress[playerCount[contractAddress]] = msg.sender;
+        playerCount++;
+        playerRegistrationStatus[msg.sender] = currentGame;
+        numberToAddress[playerCount] = msg.sender;
+        if (playerCount == currentPlayersRequired) {
+            revealBlock = block.number;
+        }
         }
     function revealWinner () external revealConditions {
-        playerCount[contractAddress] = 0;
-        currentGame[contractAddress]++;
-        uint _winningNumber = uint(keccak256(currentGame[contractAddress] + uint(numberToAddress[8]) + uint(numberToAddress[24]) - uint(numberToAddress[6]) * uint(numberToAddress[17]) + uint(numberToAddress[15]) - uint(numberToAddress[19]) * uint(numberToAddress[18]) + uint(numberToAddress[22]) - uint(numberToAddress[2]) + uint(numberToAddress[5]) + uint(numberToAddress[4]) - uint(numberToAddress[23]) - uint(numberToAddress[10]) + uint(numberToAddress[21]) - uint(numberToAddress[20]) + uint(numberToAddress[3]) + uint(numberToAddress[16]) - uint(numberToAddress[13]) - uint(numberToAddress[1]) + uint(numberToAddress[12]) - uint(numberToAddress[11]) - uint(numberToAddress[9]) + uint(numberToAddress[14]) - uint(numberToAddress[25]) + uint(numberToAddress[7]))) % currentPlayersRequired[contractAddress] + 1;
-        address _winningAddress = numberToAddress[_winningNumber];
-        confirmedWinners[_winningAddress] = 1;
-        ownerBalance++;
-        lastWinner = _winningAddress;
-        msg.sender.transfer(currentBet);
+        uint _thisBlock = block.number;
+        if (_thisBlock - revealBlock <= 255) {
+            playerCount = 0;
+            currentGame++;
+            uint _winningNumber = uint(block.blockhash(revealBlock)) % currentPlayersRequired + 1;
+            address _winningAddress = numberToAddress[_winningNumber];
+            confirmedWinners[_winningAddress] = 1;
+            lastWinner = _winningAddress;
+            msg.sender.transfer(currentBet);
+        } else {
+            revealBlock = block.number;       
+        }
     }
     function winnerWithdraw () external winnerWithdrawConditions {
         confirmedWinners[msg.sender] = 0;
-        jackpot = (currentBet * (currentPlayersRequired[contractAddress] - 2));
+        jackpot = (currentBet * (currentPlayersRequired - 1));
         msg.sender.transfer(jackpot);
-    }
-    function ownerWithdraw () external onlyOwner ownerWithdrawConditions {
-        msg.sender.transfer(ownerBalance * currentBet);
-        ownerBalance = 0;
     }
 }
