1cd169171ea5951dd0d6d3d4af43fa6ecca2cfa3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:03:41 +0700

    add contract Airdropper

--- Airdropper.sol
+++ Airdropper.sol
@@ -1,20 +1,16 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x8037a3e0d864b1f332e52cf60f999990a9780c54
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x92ae973f0549c7d51370a236fe7b1dfe9ea5aa56
 */
-pragma solidity ^0.4.11;
-
-// File: contracts/zeppelin/contracts/ownership/Ownable.sol
-
 /**
  * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
+ * @dev The Ownable contract has an owner address, and provides basic authorization control 
+ * functions, this simplifies the implementation of "user permissions". 
  */
 contract Ownable {
   address public owner;
 
 
-  /**
+  /** 
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
@@ -24,95 +20,50 @@ contract Ownable {
 
 
   /**
-   * @dev Throws if called by any account other than the owner.
+   * @dev Throws if called by any account other than the owner. 
    */
   modifier onlyOwner() {
-    require(msg.sender == owner);
+    if (msg.sender != owner) {
+      throw;
+    }
     _;
   }
 
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
+   * @param newOwner The address to transfer ownership to. 
    */
   function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));
-    owner = newOwner;
+    if (newOwner != address(0)) {
+      owner = newOwner;
+    }
   }
 
 }
-
-// File: contracts/zeppelin/contracts/token/ERC20Basic.sol
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
 }
 
-// File: contracts/zeppelin/contracts/token/ERC20.sol
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+  function allowance(address owner, address spender) constant returns (uint);
+  function transferFrom(address from, address to, uint value);
+  function approve(address spender, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
 }
-
-// File: contracts/Airdropper.sol
-
-/*
-Copyright 2018 CargiX.io Pte Ltd
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
-
-pragma solidity ^0.4.18;
-
-
-
 
 
 contract Airdropper is Ownable {
 
-    function multisend(address _tokenAddr, address[] dests, uint256[] values)
+    function multisend(address _tokenAddr, address[] dests, uint256 value)
     onlyOwner
     returns (uint256) {
         uint256 i = 0;
         while (i < dests.length) {
-           ERC20(_tokenAddr).transfer(dests[i], values[i]);
-           i += 1;
-        }
-        return(i);
-    }
-
-    function multisendFrom(address _tokenAddr, address _fromAddr, address[] dests, uint256[] values)
-    onlyOwner
-    returns (uint256) {
-        uint256 i = 0;
-        while (i < dests.length) {
-           ERC20(_tokenAddr).transferFrom(_fromAddr, dests[i], values[i]);
+           ERC20(_tokenAddr).transfer(dests[i], value);
            i += 1;
         }
         return(i);
