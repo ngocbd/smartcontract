aaa13b0c64cb9875c3a6d6c08768dc90b08ee1b5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:23:40 +0700

    add contract Angelscoin

--- Angelscoin.sol
+++ Angelscoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Angelscoin at 0x0dad75ceca455cdfba35fbd046aec037a371193c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Angelscoin at 0x8229f9a52d9cdd1475fa3e045a9a9d7be2e56946
 */
 pragma solidity ^0.4.4;
 
@@ -108,12 +108,12 @@ contract Angelscoin is StandardToken { // CHANGE THIS. Update the contract name.
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function Angelscoin() {
-        balances[msg.sender] = 10000000000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 10000000000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        balances[msg.sender] = 1000000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 1000000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "Angelscoin";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "NGL";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 100000;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        symbol = "AGL";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 1000000;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
 
