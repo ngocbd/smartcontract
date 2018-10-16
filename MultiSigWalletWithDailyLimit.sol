1f6fd28d22dd5e68c3f2960b7d31e7e56c0d521b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:40:17 +0700

    add contract MultiSigWalletWithDailyLimit

--- MultiSigWalletWithDailyLimit.sol
+++ MultiSigWalletWithDailyLimit.sol
@@ -1,7 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWalletWithDailyLimit at 0x175E3fe868622c40A829EE9E4A963402b68C6dEB
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSigWalletWithDailyLimit at 0x0cf1125c00b054044aa4f0fc8c4807ec98216fbb
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
+
 
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
@@ -34,65 +35,56 @@ contract MultiSigWallet {
     }
 
     modifier onlyWallet() {
-        if (msg.sender != address(this))
-            throw;
+        require(msg.sender == address(this));
         _;
     }
 
-    modifier ownerDoesNotExist(address owner) {
-        if (isOwner[owner])
-            throw;
+    modifier ownerDoesNotExist(address owner){
+        require(!isOwner[owner]);
         _;
     }
 
     modifier ownerExists(address owner) {
-        if (!isOwner[owner])
-            throw;
+        require(isOwner[owner]);
         _;
     }
 
     modifier transactionExists(uint transactionId) {
-        if (transactions[transactionId].destination == 0)
-            throw;
+        require(transactions[transactionId].destination != 0);
         _;
     }
 
     modifier confirmed(uint transactionId, address owner) {
-        if (!confirmations[transactionId][owner])
-            throw;
+        require(confirmations[transactionId][owner]);
         _;
     }
 
     modifier notConfirmed(uint transactionId, address owner) {
-        if (confirmations[transactionId][owner])
-            throw;
+        require(!confirmations[transactionId][owner]);
         _;
     }
 
     modifier notExecuted(uint transactionId) {
-        if (transactions[transactionId].executed)
-            throw;
+        require(!transactions[transactionId].executed);
         _;
     }
 
     modifier notNull(address _address) {
-        if (_address == 0)
-            throw;
+        require(_address != 0);
         _;
     }
 
     modifier validRequirement(uint ownerCount, uint _required) {
-        if (   ownerCount > MAX_OWNER_COUNT
-            || _required > ownerCount
-            || _required == 0
-            || ownerCount == 0)
-            throw;
+        require(ownerCount <= MAX_OWNER_COUNT);
+        require(_required <= ownerCount);
+        require(_required != 0);
+        require(ownerCount != 0);
         _;
     }
-
+        
     /// @dev Fallback function allows to deposit ether.
     function()
-        payable
+        payable public
     {
         if (msg.value > 0)
             Deposit(msg.sender, msg.value);
@@ -108,9 +100,11 @@ contract MultiSigWallet {
         public
         validRequirement(_owners.length, _required)
     {
-        for (uint i=0; i<_owners.length; i++) {
-            if (isOwner[_owners[i]] || _owners[i] == 0)
-                throw;
+        for (uint i=0; i<_owners.length; i++) 
+        {
+            require(isOwner[_owners[i]] == false);
+            require(_owners[i] != 0);
+            
             isOwner[_owners[i]] = true;
         }
         owners = _owners;
@@ -226,13 +220,13 @@ contract MultiSigWallet {
         notExecuted(transactionId)
     {
         if (isConfirmed(transactionId)) {
-            Transaction tx = transactions[transactionId];
-            tx.executed = true;
-            if (tx.destination.call.value(tx.value)(tx.data))
+            Transaction storage txi = transactions[transactionId];
+            txi.executed = true;
+            if (txi.destination.call.value(txi.value)(txi.data))
                 Execution(transactionId);
             else {
                 ExecutionFailure(transactionId);
-                tx.executed = false;
+                txi.executed = false;
             }
         }
     }
@@ -367,6 +361,7 @@ contract MultiSigWallet {
     }
 }
 
+
 /// @title Multisignature wallet with daily limit - Allows an owner to withdraw a daily limit without multisig.
 /// @author Stefan George - <stefan.george@consensys.net>
 contract MultiSigWalletWithDailyLimit is MultiSigWallet {
@@ -407,19 +402,19 @@ contract MultiSigWalletWithDailyLimit is MultiSigWallet {
         public
         notExecuted(transactionId)
     {
-        Transaction tx = transactions[transactionId];
+        Transaction storage txi = transactions[transactionId];
         bool confirmed = isConfirmed(transactionId);
-        if (confirmed || tx.data.length == 0 && isUnderLimit(tx.value)) {
-            tx.executed = true;
+        if (confirmed || txi.data.length == 0 && isUnderLimit(txi.value)) {
+            txi.executed = true;
             if (!confirmed)
-                spentToday += tx.value;
-            if (tx.destination.call.value(tx.value)(tx.data))
+                spentToday += txi.value;
+            if (txi.destination.call.value(txi.value)(txi.data))
                 Execution(transactionId);
             else {
                 ExecutionFailure(transactionId);
-                tx.executed = false;
+                txi.executed = false;
                 if (!confirmed)
-                    spentToday -= tx.value;
+                    spentToday -= txi.value;
             }
         }
     }
