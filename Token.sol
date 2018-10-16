9ee969a628debe4fb3dbda321c7ee5f5b7f8f53c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:24:50 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,86 +1,81 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xf2031a9636371b592e7bdb19ca054e918a45eb2e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xb4b7d0c65b3618bc8706ab7b3719519ead624067
 */
-pragma solidity ^0.4.11;
- 
+pragma solidity >=0.4.10;
+
 contract Token {
-    string public symbol = "";
-    string public name = "";
-    uint8 public constant decimals = 18;
-    uint256 _totalSupply = 0;
-    address owner = 0;
-    bool setupDone = false;
-	
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
- 
-    mapping(address => uint256) balances;
- 
-    mapping(address => mapping (address => uint256)) allowed;
- 
-    function Token(address adr) {
-		owner = adr;        
-    }
-	
-	function SetupToken(string tokenName, string tokenSymbol, uint256 tokenSupply)
-	{
-		if (msg.sender == owner && setupDone == false)
-		{
-			symbol = tokenSymbol;
-			name = tokenName;
-			_totalSupply = tokenSupply * 1000000000000000000;
-			balances[owner] = _totalSupply;
-			setupDone = true;
-		}
+	event Transfer(address indexed from, address indexed to, uint value);
+	event Approval(address indexed owner, address indexed spender, uint value);
+
+	string constant public name = "Hodl Token";
+	string constant public symbol = "HODL";
+	uint8 constant public decimals = 8;
+	mapping (address => uint) public balanceOf;
+	mapping (address => mapping (address => uint)) public allowance;
+	uint public totalSupply;
+	address public owner;
+
+	function Token() {
+		owner = msg.sender;
 	}
- 
-    function totalSupply() constant returns (uint256 totalSupply) {        
-		return _totalSupply;
-    }
- 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
- 
-    function transfer(address _to, uint256 _amount) returns (bool success) {
-        if (balances[msg.sender] >= _amount 
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
-            balances[msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(msg.sender, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
- 
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) returns (bool success) {
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
-            balances[_from] -= _amount;
-            allowed[_from][msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(_from, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
- 
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }
- 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
+
+	function transfer(address to, uint value) returns(bool) {
+		uint bal = balanceOf[msg.sender];
+		require(bal >= value);
+		balanceOf[msg.sender] = bal - value;
+		balanceOf[to] = balanceOf[to] + value;
+		Transfer(msg.sender, to, value);
+		return true;
+	}
+
+	function approve(address spender, uint value) returns(bool) {
+		require(value == 0 || (allowance[msg.sender][spender] == 0 && balanceOf[msg.sender] >= value));
+		allowance[msg.sender][spender] = value;
+		Approval(msg.sender, spender, value);
+		return true;
+	}
+
+	function transferFrom(address owner, address to, uint value) returns(bool) {
+		uint allowed = allowance[owner][msg.sender];
+		uint balance = balanceOf[owner];
+		require(allowed >= value && balance >= value);
+		allowance[owner][msg.sender] = allowed - value;
+		balanceOf[owner] = balance - value;
+		balanceOf[to] = balanceOf[to] + value;
+		Transfer(owner, to, value);
+		return true;
+	}
+
+	function approval(address owner, address spender) constant returns(uint) {
+		return allowance[owner][spender];
+	}
+
+	function burn(uint amount) {
+		uint bal = balanceOf[msg.sender];
+		require(bal >= amount);
+		balanceOf[msg.sender] = bal - amount;
+		totalSupply -= amount;
+		Transfer(msg.sender, 0, amount);
+	}
+
+	function mint(address to, uint value) {
+		require(msg.sender == owner);
+		balanceOf[to] = balanceOf[to] + value;
+		totalSupply += value;
+		Transfer(0, to, value);
+	}
+
+	function multiMint(uint256[] bits) {
+		require(msg.sender == owner);
+		uint256 lomask = (1 << 96) - 1;
+		uint created = 0;
+		for (uint i=0; i<bits.length; i++) {
+			address a = address(bits[i]>>96);
+			uint value = bits[i]&lomask;
+			balanceOf[a] = balanceOf[a] + value;
+			Transfer(0, a, value);
+			created += value;
+		}
+		totalSupply += created;
+	}
 }
