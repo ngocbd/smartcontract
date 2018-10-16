971cd9522503d009f4a77d6bc0d4bb45e330a4a2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:41:54 +0700

    add contract FuleexToken

--- FuleexToken.sol
+++ FuleexToken.sol
@@ -1,29 +1,30 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FuleexToken at 0x4c983ae4e3f799dcaab554ed0414ec5a1f24cf45
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FuleexToken at 0xe9716b0aa8c8fb0c3e57f2ba84a94fa1408c79b8
 */
 pragma solidity ^0.4.4;
 
 contract Token {
 
-    function totalSupply() constant returns (uint256 supply) {}
-    
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-    
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function totalSupply() public constant returns (uint256 supply) {}
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function balanceOf(address _owner) public constant returns (uint256 balance) {}
 
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function transfer(address _to, uint256 _value) public returns (bool success) {}
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {}
+
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
 }
 
 contract StandardToken is Token {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+  
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
@@ -32,7 +33,8 @@ contract StandardToken is Token {
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+  
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
@@ -42,17 +44,17 @@ contract StandardToken is Token {
         } else { return false; }
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
 
@@ -63,28 +65,25 @@ contract StandardToken is Token {
 
 contract FuleexToken is StandardToken {
 
-    function () {
-        //if ether is sent to this address, send it back.
+    function () public {
+
         throw;
     }
 
-    /* Public variables of the token */
+    string public name;                   
+    uint8 public decimals;                
+    string public symbol;                 
+    string public version = 'H1.0';       
 
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-    function FuleexToken(
-        ) {
-        balances[msg.sender] = 300000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 300000000;                        // Update total supply (100000 for example)
-        name = "FuleexToken";                                   // Set the name for display purposes
-        decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "FEXT";                               // Set the symbol for display purposes
+    function FuleexToken() public {
+        balances[msg.sender] = 300000000;               
+        totalSupply = 300000000;                        
+        name = "FuleexToken";                      
+        decimals = 0;                              
+        symbol = "FEXT";                           
     }
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
