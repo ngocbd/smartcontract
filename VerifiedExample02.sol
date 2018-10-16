7b004130274c687165210fa921ef8dafab00dc5f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:36 +0700

    add contract VerifiedExample02

---
+++ VerifiedExample02.sol
@@ -0,0 +1,4 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VerifiedExample02 at 0xbf54d498760fe8e8f4dff031b783362de7043cb4
+*/
+contract VerifiedExample02 {}
