d161a6a0b2c97558ed1b7554c92a2042baf26fa5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:50:15 +0700

    add contract A

--- A.sol
+++ A.sol
@@ -1,34 +1,40 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract A at 0x70E2475B7DA322D4a379e43cD7495C63C055192A
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract A at 0x2019763bd984cce011cd9b55b0e700abe42fa6c7
 */
+pragma solidity ^0.4.19;
+// ECE 398 SC - Smart Contracts and Blockchain Security 
+// http://soc1024.ece.illinois.edu/teaching/ece398sc/spring2018/
+
+contract ClassSize {
+    event VoteYes(string note);
+    event VoteNo(string note);
+
+    string constant proposalText = "Should the class size increase from 35 to 45?";
+    uint16 public votesYes = 0;
+    uint16 public votesNo = 0;
+    function isYesWinning() public view returns(uint8) {
+        if (votesYes >= votesNo) {
+            return 0; // yes
+        } else  {
+            return 1; // no 
+        }
+    }
+    function voteYes(string note) public {
+        votesYes += 1;
+        VoteYes(note);
+    }
+    function voteNo(string note) public {
+        votesNo += 1;
+        VoteNo(note);
+    }
+}
+
 contract A {
-
-  uint b = msg.value;
-
-  struct B {
-    address c;
-    uint yield;
-  }
-
-  B[] public p;
-  uint public i = 0;
-
-  function A() {
-  }
-
-  function() {
-    if ((b < 1 ether) || (b > 10 ether)) {
-      throw;
+    ClassSize cz = ClassSize(0x6faf33c051c0703ad2a6e86b373bb92bb30c8f5c);
+    string[] rik = ["never gonna", "give you", "up, never gonna", "let you down", "never gonna run", "around and desert", "youuuuuu"];
+    function whee(uint256 whee2) {
+        for (uint i = 0; i < whee2; i++) {
+            cz.voteYes(rik[i % 7]);
+        }
     }
-
-    uint u = p.length;
-    p.length += 1;
-    p[u].c = msg.sender;
-    p[u].yield = (b * 110) / 100;
-
-    while (p[i].yield < this.balance) {
-      p[i].c.send(p[i].yield);
-      i += 1;
-    }
-  }
 }
