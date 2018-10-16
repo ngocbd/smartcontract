3a093418bd5da0a422a0100e2057178e51708815
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:26:25 +0700

    add contract TokenTimelock

--- TokenTimelock.sol
+++ TokenTimelock.sol
@@ -1,104 +1,75 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0x0fc3509c13fe30b945f543322b234c9b82d5e3ab
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0x5d5b1f935a2d8789a1476bd61f3feb88c8eb42a7
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
+contract Clockmaking {
+	address public clockmaker;
+	address public newClockmaker;
 
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
+	event ClockmakingTransferred(address indexed oldClockmaker, address indexed newClockmaker);
 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+	constructor() public {
+		clockmaker = msg.sender;
+		newClockmaker = address(0);
+	}
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+	modifier onlyClockmaker() {
+		require(msg.sender == clockmaker, "msg.sender == clockmaker");
+		_;
+	}
+
+	function transferClockmaker(address _newClockmaker) public onlyClockmaker {
+		require(address(0) != _newClockmaker, "address(0) != _newClockmaker");
+		newClockmaker = _newClockmaker;
+	}
+
+	function acceptClockmaker() public {
+		require(msg.sender == newClockmaker, "msg.sender == newClockmaker");
+		emit ClockmakingTransferred(clockmaker, msg.sender);
+		clockmaker = msg.sender;
+		newClockmaker = address(0);
+	}
 }
-
 
 contract Ownable {
-    address public owner;
+	address public owner;
+	address public newOwner;
 
+	event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+	constructor() public {
+		owner = msg.sender;
+		newOwner = address(0);
+	}
 
+	modifier onlyOwner() {
+		require(msg.sender == owner, "msg.sender == owner");
+		_;
+	}
 
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    function Ownable() public {
-        owner = msg.sender;
-    }
+	function transferOwnership(address _newOwner) public onlyOwner {
+		require(address(0) != _newOwner, "address(0) != _newOwner");
+		newOwner = _newOwner;
+	}
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
+	function acceptOwnership() public {
+		require(msg.sender == newOwner, "msg.sender == newOwner");
+		emit OwnershipTransferred(owner, msg.sender);
+		owner = msg.sender;
+		newOwner = address(0);
+	}
 }
 
-contract ERC20 {
-    uint256 public totalSupply;
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+contract ERC20Basic {
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
 }
-
 
 library SafeERC20 {
-    function safeTransfer(ERC20 token, address to, uint256 value) internal {
-        assert(token.transfer(to, value));
-    }
-
-    function safeTransferFrom(
-        ERC20 token,
-        address from,
-        address to,
-        uint256 value
-    )
-    internal
-    {
-        assert(token.transferFrom(from, to, value));
-    }
-
-    function safeApprove(ERC20 token, address spender, uint256 value) internal {
-        assert(token.approve(spender, value));
-    }
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    assert(token.transfer(to, value));
+  }
 }
 
 /**
@@ -106,42 +77,33 @@ library SafeERC20 {
  * @dev TokenTimelock is a token holder contract that will allow a
  * beneficiary to extract the tokens after a given release time
  */
-contract TokenTimelock is Ownable {
-    using SafeERC20 for ERC20;
+contract TokenTimelock is Ownable, Clockmaking {
+  using SafeERC20 for ERC20Basic;
+  ERC20Basic public token;   // ERC20 basic token contract being held
+  uint64 public releaseTime; // timestamp when token claim is enabled
 
-    // ERC20 basic token contract being held
-    ERC20 public token;
+  constructor(ERC20Basic _token, uint64 _releaseTime) public {
+    require(_releaseTime > now);
+    token = _token;
+    owner = msg.sender;
+    clockmaker = msg.sender;
+    releaseTime = _releaseTime;
+  }
 
-    // beneficiary of tokens after they are released
-    address public beneficiary;
+  /**
+   * @notice Transfers tokens held by timelock to owner.
+   */
+  function claim() public onlyOwner {
+    require(now >= releaseTime, "now >= releaseTime");
 
-    // timestamp when token release is enabled
-    uint256 public releaseTime;
+    uint256 amount = token.balanceOf(this);
+    require(amount > 0, "amount > 0");
 
-    function TokenTimelock(ERC20 _token, address _beneficiary) public {
-        // solium-disable-next-line security/no-block-members
-        
-        token = _token;
-        beneficiary = _beneficiary;
-        releaseTime = now + 365 days;
-    }
-
-    /**
-     * @notice Transfers tokens held by timelock to beneficiary.
-     */
-    function release() public {
-        // solium-disable-next-line security/no-block-members
-        require(now >= releaseTime);
-
-        uint256 amount = token.balanceOf(this);
-        require(amount > 0);
-
-        token.safeTransfer(beneficiary, amount);
-    }
-
-
-    function setReleaseTime(uint256 _releaseTime) onlyOwner external {
-        require(_releaseTime > releaseTime);
-        releaseTime = _releaseTime;
-    }
+    token.safeTransfer(owner, amount);
+  }
+  
+  function updateTime(uint64 _releaseTime) public onlyClockmaker {
+      releaseTime = _releaseTime;
+  }
+  
 }
