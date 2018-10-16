1fc79c7210bfbeac11ce4355aac27b76cf77fd66
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:31:29 +0700

    add contract myToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,114 +1,114 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x16f4f09d780c18ceb9757546093c5139b3b73ace
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract myToken at 0xb0991d65980bd9af6109f9e6b38e862e91d873bf
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
-contract SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-       return 0;
+contract ERC20Interface {
+     function totalSupply() constant returns (uint256 supply);
+     function balanceOf(address _owner) constant returns (uint256 balance);
+     function transfer(address _to, uint256 _value) returns(bool);
+     function transferFrom(address _from, address _to, uint256 _value) returns(bool);
+     function approve(address _spender, uint256 _value) returns (bool success);
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract StdToken is ERC20Interface {
+     mapping(address => uint256) balances;
+     mapping (address => mapping (address => uint256)) allowed;
+     uint public supply = 0;
+
+     function transfer(address _to, uint256 _value) returns(bool) {
+          require(balances[msg.sender] >= _value);
+          require(balances[_to] + _value > balances[_to]);
+
+          balances[msg.sender] -= _value;
+          balances[_to] += _value;
+
+          Transfer(msg.sender, _to, _value);
+          return true;
      }
-     uint256 c = a * b;
-     assert(c / a == b);
-     return c;
-   }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-     uint256 c = a / b;
-     return c;
-  }
+     function transferFrom(address _from, address _to, uint256 _value) returns(bool){
+          require(balances[_from] >= _value);
+          require(allowed[_from][msg.sender] >= _value);
+          require(balances[_to] + _value > balances[_to]);
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-     return a - b;
-   }
+          balances[_to] += _value;
+          balances[_from] -= _value;
+          allowed[_from][msg.sender] -= _value;
 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-     uint256 c = a + b;
-    assert(c >= a);
-     return c;
-   }
+          Transfer(_from, _to, _value);
+          return true;
+     }
+
+     function totalSupply() constant returns (uint256) {
+          return supply;
+     }
+
+     function balanceOf(address _owner) constant returns (uint256) {
+          return balances[_owner];
+     }
+
+     function approve(address _spender, uint256 _value) returns (bool) {
+          require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+          allowed[msg.sender][_spender] = _value;
+          Approval(msg.sender, _spender, _value);
+
+          return true;
+     }
+
+     function allowance(address _owner, address _spender) constant returns (uint256) {
+          return allowed[_owner][_spender];
+     }
 }
 
-contract ERC20 {
-   uint256 public totalSupply;
-   function balanceOf(address who) public view returns (uint256);
-   function transfer(address to, uint256 value) public returns (bool);
-   event Transfer(address indexed from, address indexed to, uint256 value);
-   function allowance(address owner, address spender) public view returns (uint256);
-   function transferFrom(address from, address to, uint256 value) public returns (bool);
-   function approve(address spender, uint256 value) public returns (bool);
-   event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+contract myToken is StdToken
+{
+    string public constant name = "168 Token";
+    string public constant symbol = "168";
+    uint public constant decimals = 18;
+    uint public constant TOKEN_SUPPLY_LIMIT = 1000000 * (1 ether / 1 wei);
+    uint public constant MANAGER_SUPPLY = 650000 * (1 ether / 1 wei);
+    uint public constant ICO_PRICE = 1000;     // per 1 Ether
+    address public tokenManager = 0;
 
-
-contract StandardToken is ERC20, SafeMath {
-
-   mapping(address => uint256) balances;
-   mapping (address => mapping (address => uint256)) internal allowed;
-
-   function transfer(address _to, uint256 _value) public returns (bool) {
-     require(_to != address(0));
-     require(_value <= balances[msg.sender]);
-     balances[msg.sender] = sub(balances[msg.sender], _value);
-     balances[_to] = add(balances[_to], _value);
-     Transfer(msg.sender, _to, _value);
-     return true;
-   }
-
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-   }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-     require(_value <= balances[_from]);
-     require(_value <= allowed[_from][msg.sender]);
-
-    balances[_from] = sub(balances[_from], _value);
-     balances[_to] = add(balances[_to], _value);
-     allowed[_from][msg.sender] = sub(allowed[_from][msg.sender], _value);
-    Transfer(_from, _to, _value);
-     return true;
-   }
-
-   function approve(address _spender, uint256 _value) public returns (bool) {
-     allowed[msg.sender][_spender] = _value;
-     Approval(msg.sender, _spender, _value);
-     return true;
-   }
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-     return allowed[_owner][_spender];
-   }
-
-   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-     allowed[msg.sender][_spender] = add(allowed[msg.sender][_spender], _addedValue);
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-     uint oldValue = allowed[msg.sender][_spender];
-     if (_subtractedValue > oldValue) {
-       allowed[msg.sender][_spender] = 0;
-     } else {
-       allowed[msg.sender][_spender] = sub(oldValue, _subtractedValue);
+    modifier onlyTokenManager()
+    {
+        require(msg.sender==tokenManager);
+        _;
     }
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
 
-}
+    function myToken()
+    {
+        tokenManager = msg.sender;
+        balances[tokenManager] += MANAGER_SUPPLY;
+        supply += MANAGER_SUPPLY;
+    }
 
-contract MyToken is StandardToken {
-   string public name = 'Spectre';
-   string public symbol = 'SPTR';
-   uint public decimals = 18;
-   uint public INITIAL_SUPPLY = 10000000000000000000000000000000;
+    function buyTokens() public payable
+    {
+        require(msg.value >= ((1 ether / 1 wei) / 100));
 
-   function MyToken() {
-     totalSupply = INITIAL_SUPPLY;
-     balances[msg.sender] = INITIAL_SUPPLY;
-   }
+        uint newTokens = msg.value * ICO_PRICE;
+
+        require(supply + newTokens <= TOKEN_SUPPLY_LIMIT);
+
+        tokenManager.transfer(msg.value);
+
+        balances[msg.sender] += newTokens;
+        supply += newTokens;
+    }
+
+    function setTokenManager(address _mgr) public onlyTokenManager
+    {
+        tokenManager = _mgr;
+    }
+
+    function() payable
+    {
+        buyTokens();
+    }
 }
