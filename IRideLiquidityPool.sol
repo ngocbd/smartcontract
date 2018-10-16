aad56ba59c04419fed2986199965a667023ab615
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:58:15 +0700

    add contract IRideLiquidityPool

---
+++ IRideLiquidityPool.sol
@@ -0,0 +1,65 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IRideLiquidityPool at 0x92dee36a50f66c603e15b9ac07338ec0046f902f
+*/
+pragma solidity ^0.4.16;
+
+interface Token {
+    function transferFrom(address _from, address _to, uint256 _value) external;
+}
+
+contract IRideLiquidityPool {
+    
+    Token public tokenReward;
+    address public creator;
+    address public owner = 0xBeDF65990326Ed2236C5A17432d9a30dbA3aBFEe;
+
+    uint256 public price;
+    uint256 public startDate;
+
+    modifier isCreator() {
+        require(msg.sender == creator);
+        _;
+    }
+
+    event FundTransfer(address backer, uint amount, bool isContribution);
+
+    function IRideLiquidityPool() public {
+        creator = msg.sender;
+        startDate = 1519862400;
+        price = 17500;
+        tokenReward = Token(0x69D94dC74dcDcCbadEc877454a40341Ecac34A7c);
+    }
+
+    function setOwner(address _owner) isCreator public {
+        owner = _owner;      
+    }
+
+    function setCreator(address _creator) isCreator public {
+        creator = _creator;      
+    }
+
+    function setStartDate(uint256 _startDate) isCreator public {
+        startDate = _startDate;      
+    }
+    
+    function setPrice(uint256 _price) isCreator public {
+        price = _price;      
+    }
+
+    function setToken(address _token) isCreator public {
+        tokenReward = Token(_token);      
+    }
+
+    function kill() isCreator public {
+        selfdestruct(owner);
+    }
+
+    function () payable public {
+        require(msg.value > 0);
+        require(now > startDate);
+	    uint amount = msg.value * price;
+        tokenReward.transferFrom(owner, msg.sender, amount);
+        FundTransfer(msg.sender, amount, true);
+        owner.transfer(msg.value);
+    }
+}
