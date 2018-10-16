3dd7d47e1b772da1ec0d87121e92b53474538bcb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:01:23 +0700

    add contract Ethereumgold

--- EthereumGold.sol
+++ EthereumGold.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHEREUMGOLD at 0x25adc8d81e74264e756827485a056b905df28c32
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Ethereumgold at 0x04D0B0f86F6376d513D106fB0A417A18dD8d8674
 */
 pragma solidity ^ 0.4 .9;
 library SafeMath {
@@ -25,7 +25,7 @@ library SafeMath {
         return c;
     }
 }
-contract ETHEREUMGOLD {
+contract Ethereumgold {
     using SafeMath
     for uint256;
     mapping(address => mapping(address => uint256)) allowed;
@@ -37,10 +37,10 @@ contract ETHEREUMGOLD {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed _owner, address indexed spender, uint256 value);
 
-    function ETHEREUMGOLD() {
-        totalSupply = 210000000;
-        symbol = 'ETHG';
-        owner = 0x6be3c9122cdd8ee31a8d2640f9bf6fd41963eb97;
+    function Ethereumgold() {
+        totalSupply = 24000000;
+        symbol = 'ETG';
+        owner = 0x664eb62a2c07bfd3d5e9e81f835df28961d40550;
         balances[owner] = totalSupply;
         decimals = 0;
     }
