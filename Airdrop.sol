4feffec6dfb19e3468c0cd0a45017f0b3d5850d4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:21:11 +0700

    add contract AirDrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,21 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x22260d1f2cb61a371bff2625c4620fc5a67f0a8e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x3f8fc6758e0ad014ee426565a6383969b22d9e37
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.11;
 
-/**
- * split income between shareholders
- */
-contract Share {
+contract Ownable {
   address public owner;
-  address[] public shares;
-  bool public pause;
-  mapping (address => uint256) public holds;
 
-  function Share() public {
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
     owner = msg.sender;
-    shares.push(owner);
-    pause = false;
   }
 
   modifier onlyOwner() {
@@ -23,121 +17,47 @@ contract Share {
     _;
   }
 
-  modifier whenNotPaused() {
-    require(!pause);
-    _;
-  }
-
-  function pause() public onlyOwner {
-    pause = true;
-  }
-
-  function unpause() public onlyOwner {
-    pause = false;
-  }
-
-  function addShare(address _share) public onlyOwner {
-    for (uint i = 0; i < shares.length; i ++) {
-      if (shares[i] == _share) {
-        return;
-      }
-    }
-    shares.push(_share);
-  }
-
-  function removeShare(address _share) public onlyOwner {
-    uint i = 0;
-    for (; i < shares.length; i ++) {
-      if (shares[i] == _share) {
-        break;
-      }
-    }
-
-    if (i > shares.length - 1) {
-      //not found
-      return;
-    } else {
-      shares[i] = shares[shares.length - 1];
-      shares.length = shares.length - 1;
-      return;
-    }
-  }
-
-  function split(uint256 value) internal {
-    uint256 each = value / shares.length;
-
-    for (uint i = 0; i < shares.length; i ++) {
-      holds[shares[i]] += each;
-    }
-
-    holds[owner] += value - each * shares.length;
-    return;
-  }
-
-  function withdrawal() public whenNotPaused {
-    if (holds[msg.sender] > 0) {
-      uint256 v = holds[msg.sender];
-      holds[msg.sender] = 0;
-      msg.sender.transfer(v);
-    }
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 }
 
 contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) public constant returns (uint);
-  function transfer(address to, uint value) public;
-  event Transfer(address indexed from, address indexed to, uint value);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public constant returns (uint);
-  function transferFrom(address from, address to, uint value) public;
-  function approve(address spender, uint value) public;
-  event Approval(address indexed owner, address indexed spender, uint value);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract AirDrop is Share {
-  // owner => (token addr => token amount)  
-  mapping(address => mapping(address => uint256)) toDrop;
+contract AirDrop is Ownable {
 
-  uint256 public fee;
-
-  function AirDrop (uint256 _fee) public {
-      fee = _fee;
-  }
-   
-  function setFee(uint256 _fee) public onlyOwner {
-    fee = _fee;
-  }
-
-  function drop(address _token, address[] dsts, uint256 value) public payable whenNotPaused {
-    require(dsts.length > 0);
-    uint256 total = dsts.length * value;
-    assert(total / dsts.length == value);
-    require(msg.value >= fee);
-    
-    split(fee);
-    
-    uint256 i = 0;
-    if (_token == address(0)) {
-      //send ETH
-      require((fee + total) >= total);
-      require(msg.value >= (fee + total));
-      
-      while (i < dsts.length) {
-        dsts[i].transfer(value);        
-        i += 1;
-      }
-
-    } else {
-      ERC20 erc20 = ERC20(_token);
-      require(erc20.allowance(msg.sender, this) >= total);
-
-      while (i < dsts.length) {
-        erc20.transferFrom(msg.sender, dsts[i], value);
-        i += 1;
-      }
+    function contractTokenBalance(address _tokenAddr) onlyOwner public constant returns(uint256) {
+       return  ERC20(_tokenAddr).balanceOf(msg.sender);
     }
-  }
+    
+    function send(address _tokenAddr, address _to, uint256 amount) onlyOwner public returns(bool) {
+       return ERC20(_tokenAddr).transfer(_to, amount);
+    }
+    
+    function multisend(address _tokenAddr, address[] dests, uint256 amount) onlyOwner public returns(uint256) {
+      
+        uint256 i = 0;
+        while (i < dests.length) {
+          ERC20(_tokenAddr).transfer(dests[i], amount);
+          i += 1;
+        }
+        return(i);
+    }
+    
+    function () payable public {
+    }
 }
