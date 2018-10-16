9a1e0b03f90c608b80831c177d735fc0006f77da
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:06:30 +0700

    add contract MITtoken

--- MitToken.sol
+++ MitToken.sol
@@ -1,89 +1,86 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MitToken at 0x97B91b7Ff065e14E6372526B65216eD3f662F7d0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MITtoken at 0x593e2cd90b0d5acce70962d32259258c6566fc18
 */
-pragma solidity ^0.4.16;
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+pragma solidity ^0.4.13;
 
-contract MitToken {
-    /* Public variables of the MIT token */
-    string public standard = 'MIT 1.1';
-    string public name = 'Mit Token';
-    string public symbol = 'MIT';
-    uint8 public decimals = 18;
-    uint256 public totalSupply = 100000;
-
-    /* Creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    /* Generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /* Notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-
-    /* Initializes contract with initial supply tokens to me */
-    function MitToken() {
-        balanceOf[msg.sender] = totalSupply;                    // Give the creator all initial tokens
+contract Ownable {
+    
+    address owner;
+    
+    function Ownable() {
+        owner = msg.sender;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (_to == 0x0) revert();                               // Prevent transfer to 0x0 address. Use burn() instead
-        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
-        balanceOf[msg.sender] -= _value;                        // Subtract from the sender
-        balanceOf[_to] += _value;                               // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                      // Notify anyone listening that this transfer took place
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
 
-    /* Allow another contract to spend some tokens on my behalf */
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        if ((_value != 0) && (allowance[msg.sender][_spender] != 0)) revert();
-        allowance[msg.sender][_spender] = _value;
-        return true;
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+    
+}
+
+contract MITtoken is Ownable {
+    
+    string public constant name = "MIT Token";
+    
+    string public constant symbol = "MIT";
+    
+    uint32 public constant decimals = 1;
+    
+    uint public totalSupply = 0;
+    
+    mapping (address => uint) balances;
+    
+    mapping (address => mapping(address => uint)) allowed;
+    
+    function mint(address _to, uint _value) onlyOwner {
+        assert(totalSupply + _value >= totalSupply && balances[_to] + _value >= balances[_to]);
+        balances[_to] += _value;
+        totalSupply += _value;
+    }
+    
+    function balanceOf(address _owner) constant returns (uint balance) {
+        return balances[_owner];
     }
 
-    /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
+    function transfer(address _to, uint _value) returns (bool success) {
+        if(balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
+            balances[msg.sender] -= _value; 
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
             return true;
-        }
-    }        
-
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (_to == 0x0) revert();                                // Prevent transfer to 0x0 address. Use burn() instead
-        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) revert();     // Check allowance
-        balanceOf[_from] -= _value;                              // Subtract from the sender
-        balanceOf[_to] += _value;                                // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
+        } 
+        return false;
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+        if( allowed[_from][msg.sender] >= _value &&
+            balances[_from] >= _value 
+            && balances[_to] + _value >= balances[_to]) {
+            allowed[_from][msg.sender] -= _value;
+            balances[_from] -= _value; 
+            balances[_to] += _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } 
+        return false;
+    }
+    
+    function approve(address _spender, uint _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
         return true;
     }
-
-	/* Burn MITs by User */
-    function burn(uint256 _value) returns (bool success) {
-        if (balanceOf[msg.sender] < _value) revert();            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                         // Subtract from the sender
-        totalSupply -= _value;                                   // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
+    
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+        return allowed[_owner][_spender];
     }
-
-	/* Burn MITs from Users */
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) revert();                // Check if the sender has enough
-        if (_value > allowance[_from][msg.sender]) revert();    // Check allowance
-        balanceOf[_from] -= _value;                             // Subtract from the sender
-        totalSupply -= _value;                                  // Updates totalSupply
-        Burn(_from, _value);
-        return true;
-    }
+    
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    
 }
