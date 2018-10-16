a27398ffb202c879bb35da03493f73472680cdf2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:47:08 +0700

    add contract Goldmint

--- Goldmint.sol
+++ Goldmint.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Goldmint at 0x0cffb692aa8b94704bc36d03bbb967c11d6c18d6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Goldmint at 0x45daebec837dadba9705ae3348e66490481668c0
 */
 pragma solidity ^0.4.16;
 
@@ -289,7 +289,7 @@ contract Goldmint is SafeMath {
           0xe9fC1A57a5dC1CaA3DE22A940E9F09e640615f7E,
           0xD360433950DE9F6FA0e93C29425845EeD6BFA0d0,
           0xF0De97EAff5D6c998c80e07746c81a336e1BBd43,
-          0xF4Ce80097bf1E584822dBcA84f91D5d7d9df0846
+          0x80b365da1C18f4aa1ecFa0dFA07Ed4417B05Cc69
      ];
 
      // We count ETH invested by person, for refunds (see below)
