cf2d97f18c6fee61c70c398608f920bf85dd5980
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:38:09 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xab7b4ddd5936b72b5167a5cf3fc361016f434a06
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x092cc215a5018f0ee10ade9ebec4db6ce25264b0
 */
 pragma solidity ^0.4.4;
 
@@ -90,7 +90,7 @@ contract StandardToken is Token {
 }
 
 
-
+//name this contract whatever you'd like
 contract ERC20Token is StandardToken {
 
     function () {
@@ -100,24 +100,30 @@ contract ERC20Token is StandardToken {
 
     /* Public variables of the token */
 
-  
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;                
-    string public version = 'MILY1.0';       //MILY1 0.1 standard.
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
 //
-// ERC20 TOKEN Standard Applied
+// CHANGE THESE VALUES FOR YOUR TOKEN
 //
 
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 100000000000000000000000000000;               
-        totalSupply = 100000000000000000000000000000;                        
-        name = "Truck Chain";                                   
-        decimals = 18;                                  
-        symbol = "RIG";                               
+        balances[msg.sender] = 7777777777777777;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 7777777777777777;                        // Update total supply (100000 for example)
+        name = "Jeez-It";                                   // Set the name for display purposes
+        decimals = 7;                            // Amount of decimals for display purposes
+        symbol = "JEEZ";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
