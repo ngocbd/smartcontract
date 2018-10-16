82a41d2aff4d28a83ccd0bb2a16008e55a8f1488
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:16:59 +0700

    add contract ApprovedTokenDone

--- ApprovedTokenDone.sol
+++ ApprovedTokenDone.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ApprovedTokenDone at 0x45a5223b54910fa8d4a416a27e4ce683a0ceca84
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ApprovedTokenDone at 0x91c3ef460ac2136647961aef6246a609bdbd7b7c
 */
 pragma solidity ^0.4.13;
 
@@ -153,8 +153,8 @@ contract StandardToken is ERC20, BasicToken {
  * Created specifically for TokenDone.io
  */
 contract ApprovedTokenDone is StandardToken {
-	string public name = '2018';
-	string public symbol = '2018';
+	string public name = 'LAB';
+	string public symbol = 'LAB';
 	uint public decimals = 3;
 	uint public initialSupply = 100000000000;
 	string public publisher = 'TokenDone.io';
