88beb9a6c1dc6c8c066b6b6ae893dd3fa3ac0b7a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:23:09 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,194 +1,139 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xc1Ec39D8FB9D0f2e2B521d77573226eB0e77d1A1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x01C67791309c71aA4Ed373025a0C089696D7c9e4
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-interface IERC20 {
-    function totalSupply() constant returns (uint256 totalSupply);
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-
-
-contract MyToken is IERC20 {
-    using SafeMath for uint256;
-
-    string public symbol = 'KCFOB';
-    string public name = 'KC19700 OP TOKEN';
-
-    uint8 public constant decimals = 18;
-    uint256 public constant tokensPerEther = 3500;
-
-    uint256 public _totalSupply = 10000000000000000000000000;
-    uint256 public _maxSupply = 38000000000000000000000000;
-
-    uint256 public totalContribution = 0;
-
-    bool public purchasingAllowed = true;
-
+contract owned {
     address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
 
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
 
-    mapping(address => uint256) balances;
-    mapping (address => mapping (address => uint256)) public allowed;
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
 
+contract tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
+}
 
-    function MyToken() {
-        owner = msg.sender;
-        balances[msg.sender] = _totalSupply;
+contract MyToken is owned {
+    /* Public variables of the token */
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* This notifies clients about the amount burnt */
+    event Burn(address indexed from, uint256 value);
+
+    mapping (address => bool) public frozenAccount;
+    event FrozenFunds(address target, bool frozen);
+
+    function freezeAccount(address target, bool freeze) onlyOwner {
+        frozenAccount[target] = freeze;
+        FrozenFunds(target, freeze);
     }
 
-
-    function totalSupply() constant returns (uint256 totalSupply) {
-        return _totalSupply;
-    }
-    /*
-     * get some stats
-     *
-     */
-    function getStats() public constant returns (uint256, uint256,  bool) {
-        return (totalContribution, _totalSupply, purchasingAllowed);
-    }
-
-    function getStats2() public constant returns (bool) {
-        return (purchasingAllowed);
-    }
-
-
-    /*
-     * somehow unnecessery 
-     */
-    function withdraw() onlyOwner {
-        owner.transfer(this.balance);
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function MyToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol,
+        address centralMinter
+        ) {
+        if (centralMinter != 0) owner = centralMinter;      // ?????
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
     }
 
-
-    function () payable {
-        require(
-            msg.value > 0
-            && purchasingAllowed
-            && _totalSupply < _maxSupply 
-        );
-        /*  everything is in wei */
-        uint256 baseTokens  = msg.value.mul(tokensPerEther);
-
-        /* send tokens to buyer. Buyer gets baseTokens */
-        balances[msg.sender] = balances[msg.sender].add(baseTokens);
-
-        /* send eth to owner */
-        owner.transfer(msg.value);
-        
-        totalContribution = totalContribution.add(msg.value);
-        _totalSupply      = _totalSupply.add(baseTokens);
-
-        Transfer(address(this), msg.sender, baseTokens);
-    }
-
-    function enablePurchasing() public onlyOwner {
-        purchasingAllowed = true;
-    }
-
-    function disablePurchasing() public onlyOwner {
-        purchasingAllowed = false;
-    }
-
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        require(
-            (balances[msg.sender] >= _value)
-            && (_value > 0)
-            && (_to != address(0))
-            && (balances[_to].add(_value) >= balances[_to])
-            && (msg.data.length >= (2 * 32) + 4)
-        );
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        require(
-            (allowed[_from][msg.sender] >= _value) // Check allowance
-            && (balances[_from] >= _value) // Check if the sender has enough
-            && (_value > 0) // Don't allow 0value transfer
-            && (_to != address(0)) // Prevent transfer to 0x0 address
-            && (balances[_to].add(_value) >= balances[_to]) // Check for overflows
-            && (msg.data.length >= (2 * 32) + 4) //mitigates the ERC20 short address attack
-            //most of these things are not necesary
-        );
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] > _value);                // Check if the sender has enough
+        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        require(!frozenAccount[_from]);
+        require(!frozenAccount[_to]);
+        balanceOf[_from] -= _value;                         // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
         Transfer(_from, _to, _value);
+    }
+
+    /// @notice Send `_value` tokens to `_to` from your account
+    /// @param _to The address of the recipient
+    /// @param _value the amount to send
+    function transfer(address _to, uint256 _value) {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /// @notice Send `_value` tokens to `_to` in behalf of `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value the amount to send
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require (_value < allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        /* To change the approve amount you first have to reduce the addresses`
-         * allowance to zero by calling `approve(_spender, 0)` if it is not
-         * already 0 to mitigate the race condition described here:
-         * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729 */
-        require(
-            (_value == 0) 
-            || (allowed[msg.sender][_spender] == 0)
-        );
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf
+    /// @param _spender The address authorized to spend
+    /// @param _value the max amount they can spend
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
         return true;
     }
 
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
+    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
+    /// @param _spender The address authorized to spend
+    /// @param _value the max amount they can spend
+    /// @param _extraData some extra information to send to the approved contract
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
     }
 
-    /*
-     * events
-     */
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    /// @notice Remove `_value` tokens from the system irreversibly
+    /// @param _value the amount of money to burn
+    function burn(uint256 _value) returns (bool success) {
+        require (balanceOf[msg.sender] > _value);            // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
+        totalSupply -= _value;                                // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
 
+    function burnFrom(address _from, uint256 _value) returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
+    }
 }
