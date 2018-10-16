ab725a426d51cd438de5a0e2b47b5d57b0c258ee
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:54:44 +0700

    add contract Satoshi

--- SATOSHI.sol
+++ SATOSHI.sol
@@ -1,7 +1,47 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Satoshi at 0xcbbe85ecfad7c2e964d5d0208bd8020c732ebf52
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Satoshi at 0x5f25045b6860d9490aa7ea06e3102bccc561b593
 */
-pragma solidity ^0.4.18;
+contract Satoshi {
+  using strings for *;
+  enum MoodState {happy, sad, angry, thinking, stoked}
+  string public whatSatoshiSays;
+  string public name;
+  MoodState public satoshiMood;
+  uint public currentPrice;
+  address public currentOwner;
+  address private devAddress;  
+  
+  
+  function Satoshi() public {
+    whatSatoshiSays = "My name is Satoshi Nakamoto, nice to meet you!";    
+    name = "Satoshi Nakamoto";
+    satoshiMood = MoodState.happy;
+    currentPrice = 1000000000000000; 
+    currentOwner = msg.sender;
+    devAddress = msg.sender;
+    
+  }
+  
+  function changeWhatSatoshiSays(string _whatSatoshiSays, MoodState _satoshiMood, string _name) payable public {
+    require(msg.value >= currentPrice && _name.toSlice().len() <= 25 && _whatSatoshiSays.toSlice().len() <= 180);
+    uint sentAmount = msg.value; 
+    uint devFee = (sentAmount * 1) / 100; // 1 % fee sent to devs
+    uint amountToSendToCurrentOwner = sentAmount - devFee;
+    devAddress.transfer(devFee); 
+    currentOwner.transfer(amountToSendToCurrentOwner); // Transfer the rest to the last owner
+    currentPrice = (currentPrice * 106) / 100; // 6 % increase in price every time
+    currentOwner = msg.sender;
+    whatSatoshiSays = _whatSatoshiSays;    
+    satoshiMood = _satoshiMood;
+    name = _name;
+    
+  }
+
+  function fetchCurrentSatoshiState() public view returns (string, string, MoodState, address, uint) {
+    return (whatSatoshiSays, name, satoshiMood, currentOwner, currentPrice);
+  }  
+}
+
 library strings {
     struct slice {
         uint _len;
@@ -674,43 +714,4 @@ library strings {
 
         return ret;
     }
-}
-
-contract Satoshi {
-  using strings for *;
-  enum MoodState {happy, sad, angry, thinking, stoked}
-  string public whatSatoshiSays;
-  string public name;
-  MoodState public satoshiMood;
-  uint public currentPrice;
-  address public currentOwner;
-  address private devAddress;  
-  
-
-  function Satoshi() public {
-    whatSatoshiSays = "My name is Satoshi Nakamoto, creator of Bitcoin!";    
-    name = "Satoshi Nakamoto";
-    satoshiMood = MoodState.happy;
-    currentPrice = 1000000000000000; // includes the fee 
-    currentOwner = msg.sender;
-    devAddress = msg.sender;
-  }
-  
-  function changeWhatSatoshiSays(string _whatSatoshiSays, MoodState _satoshiMood, string _name) payable public {
-    require(msg.value >= currentPrice && _name.toSlice().len() <= 25 && _whatSatoshiSays.toSlice().len() <= 180);
-    uint sentAmount = msg.value;
-    uint devFee = (sentAmount * 1) / 100; // 1 % fee sent to devs
-    uint amountToSendToCurrentOwner = sentAmount - devFee;
-    devAddress.transfer(devFee);
-    currentOwner.transfer(amountToSendToCurrentOwner);
-    currentPrice = (currentPrice * 105) / 100;
-    currentOwner = msg.sender;
-    whatSatoshiSays = _whatSatoshiSays;    
-    satoshiMood = _satoshiMood;
-    name = _name;    
-  }
-
-  function fetchCurrentSatoshiState() public view returns (string, string, MoodState, address, uint) {
-    return (whatSatoshiSays, name, satoshiMood, currentOwner, currentPrice);
-  }  
 }
