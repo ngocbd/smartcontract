18b0d1aef347892f27ada2e24c571941770d3fe2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:56:43 +0700

    add contract Registrar

--- Registrar.sol
+++ Registrar.sol
@@ -1,159 +1,123 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Registrar at 0xb98fdd97f3da105e6b166461afac208981bc786b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Registrar at 0x5b2ff75d7eaa47db475707dae12a688102ef4290
 */
-// Copyright (c) 2016 Chronicled, Inc. All rights reserved.
-// http://explorer.chronicled.org
-// http://demo.chronicled.org
-// http://chronicled.org
-
-contract Registrar {
-    address public registrar;
-
-    /**
-
-    * Created event, gets triggered when a new registrant gets created
-    * event
-    * @param registrant - The registrant address.
-    * @param registrar - The registrar address.
-    * @param data - The data of the registrant.
-    */
-    event Created(address indexed registrant, address registrar, bytes data);
-
-    /**
-    * Updated event, gets triggered when a new registrant id Updated
-    * event
-    * @param registrant - The registrant address.
-    * @param registrar - The registrar address.
-    * @param data - The data of the registrant.
-    */
-    event Updated(address indexed registrant, address registrar, bytes data, bool active);
-
-    /**
-    * Error event.
-    * event
-    * @param code - The error code.
-    * 1: Permission denied.
-    * 2: Duplicate Registrant address.
-    * 3: No such Registrant.
-    */
-    event Error(uint code);
-
-    struct Registrant {
-        address addr;
-        bytes data;
-        bool active;
-    }
-
-    mapping(address => uint) public registrantIndex;
-    Registrant[] public registrants;
-
-    /**
-    * Function can't have ether.
-    * modifier
-    */
-    modifier noEther() {
-        if (msg.value > 0) throw;
-        _
-    }
-
-    modifier isRegistrar() {
-      if (msg.sender != registrar) {
-        Error(1);
-        return;
-      }
-      else {
-        _
-      }
-    }
-
-    /**
-    * Construct registry with and starting registrants lenght of one, and registrar as msg.sender
-    * constructor
-    */
-    function Registrar() {
-        registrar = msg.sender;
-        registrants.length++;
-    }
-
-    /**
-    * Add a registrant, only registrar allowed
-    * public_function
-    * @param _registrant - The registrant address.
-    * @param _data - The registrant data string.
-    */
-    function add(address _registrant, bytes _data) isRegistrar noEther returns (bool) {
-        if (registrantIndex[_registrant] > 0) {
-            Error(2); // Duplicate registrant
-            return false;
-        }
-        uint pos = registrants.length++;
-        registrants[pos] = Registrant(_registrant, _data, true);
-        registrantIndex[_registrant] = pos;
-        Created(_registrant, msg.sender, _data);
-        return true;
-    }
-
-    /**
-    * Edit a registrant, only registrar allowed
-    * public_function
-    * @param _registrant - The registrant address.
-    * @param _data - The registrant data string.
-    */
-    function edit(address _registrant, bytes _data, bool _active) isRegistrar noEther returns (bool) {
-        if (registrantIndex[_registrant] == 0) {
-            Error(3); // No such registrant
-            return false;
-        }
-        Registrant registrant = registrants[registrantIndex[_registrant]];
-        registrant.data = _data;
-        registrant.active = _active;
-        Updated(_registrant, msg.sender, _data, _active);
-        return true;
-    }
-
-    /**
-    * Set new registrar address, only registrar allowed
-    * public_function
-    * @param _registrar - The new registrar address.
-    */
-    function setNextRegistrar(address _registrar) isRegistrar noEther returns (bool) {
-        registrar = _registrar;
-        return true;
-    }
-
-    /**
-    * Get if a regsitrant is active or not.
-    * constant_function
-    * @param _registrant - The registrant address.
-    */
-    function isActiveRegistrant(address _registrant) constant returns (bool) {
-        uint pos = registrantIndex[_registrant];
-        return (pos > 0 && registrants[pos].active);
-    }
-
-    /**
-    * Get all the registrants.
-    * constant_function
-    */
-    function getRegistrants() constant returns (address[]) {
-        address[] memory result = new address[](registrants.length-1);
-        for (uint j = 1; j < registrants.length; j++) {
-            result[j-1] = registrants[j].addr;
-        }
-        return result;
-    }
-
-    /**
-    * Function to reject value sends to the contract.
-    * fallback_function
-    */
-    function () noEther {}
-
-    /**
-    * Desctruct the smart contract. Since this is first, alpha release of Open Registry for IoT, updated versions will follow.
-    * Registry's discontinue must be executed first.
-    */
-    function discontinue() isRegistrar noEther {
-      selfdestruct(msg.sender);
-    }
+//Simple Msg XChange Registrar (does not provide validateion!)
+contract Message {
+	address public registrar;	
+	address public from;
+	address public to;
+	string public hash_msg;
+	string public hash_ack;
+	uint256 public timestamp_msg;
+	uint256 public timestamp_ack;
+	
+	
+	function Message(address _registrar,address _from,address _to,string _hash_msg) {
+		registrar=_registrar;
+		from=_from;
+		to=_to;
+		hash_msg=_hash_msg;
+		timestamp_msg=now;
+	}
+	
+	function ack(string _hash) {
+		if(msg.sender!=to) throw;
+		if(timestamp_ack>0) throw;
+		hash_ack=_hash;
+		timestamp_ack=now;		
+	}
+	
+	function() {
+		if(msg.value>0) {
+			if(msg.sender==from) {			
+				to.send(msg.value);
+			} else {
+				from.send(msg.value);
+			}
+		}
+	}
+	
+}
+contract Registrar
+{
+	address public registrar;		
+	
+	uint256 public fee_registration;
+	uint256 public fee_msg;
+	uint256 public cnt_registrations;
+	
+	struct Registration {
+		address adr;
+		string hash;
+		string gnid;
+	}	
+	
+	mapping(address=>Registration) public regadr;	
+	mapping(address=>Message[]) public msgs;
+	mapping(address=>Message[]) public sent;
+	mapping(address=>bool) public preregister;	
+	
+	Registration[] public regs;
+	
+	function Registrar() {
+		registrar=msg.sender;
+	}
+	
+	function register(string hash) {
+		updateRegistration(hash,'');		
+	}
+	
+	function unregister() {
+		delete regadr[msg.sender];
+	}
+	
+	function updateRegistration(string hash,string gnid) {		
+		if((msg.value>=fee_registration)||(preregister[msg.sender])) {			
+			regadr[msg.sender]=Registration(msg.sender,hash,gnid);
+			regs.push(regadr[msg.sender]);
+			if(fee_registration>0) registrar.send(this.balance);
+			preregister[msg.sender]=false;
+			cnt_registrations++;
+		} else throw;
+	}
+	
+	function preRegister(address preReg) {
+		if(msg.sender!=registrar) throw;
+		preReg.send(msg.value);		
+		preregister[preReg]=true;
+	}
+	
+	function getMsgs() returns (Message[]) {
+		return msgs[msg.sender];	
+	}
+	
+	function setRegistrationPrice(uint256 price) {
+		if(msg.sender!=registrar) throw;
+		fee_registration=price;
+	}
+	
+	function setMsgPrice(uint256 price) {
+		if(msg.sender!=registrar) throw;
+		fee_msg=price;
+	}
+	
+	function sendMsg(address to,string hash) {
+		if(msg.value>=fee_msg) {	
+			    Message m = new  Message(this,msg.sender,to,hash);
+				msgs[to].push(m);	
+			    sent[msg.sender].push(m);
+			if(fee_msg>0) registrar.send(this.balance);
+		} else throw;		
+	}
+	
+	function ackMsg(uint256 msgid,string hash) {
+		Message message =Message(msgs[msg.sender][msgid]);
+		message.ack(hash);
+	}
+	
+	function() {
+		if(msg.value>0) {
+			registrar.send(msg.value);
+		}
+	}
 }
