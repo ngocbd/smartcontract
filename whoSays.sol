e8dd0b36aa03f9e4cbeb125e780f85d0422268f7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:04:17 +0700

    add contract whoSays

--- whoSays.sol
+++ whoSays.sol
@@ -1,12 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract whoSays at 0x53570f9db0d81af09d9ef418e37e025668eed05f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract whoSays at 0xaafac9fa34fac2bd7512c5b9acf755643dadcd74
 */
 pragma solidity ^0.4.0;
 contract whoSays {
 
     string public name = "whoSays";
 
-    mapping(address => bytes) public data;
+    mapping(address => bytes) data;
 
     event Said(address indexed person, bytes message);
 
@@ -14,5 +14,5 @@ contract whoSays {
         data[msg.sender] = _data;
         Said(msg.sender, _data);
     }
-
+    
 }
