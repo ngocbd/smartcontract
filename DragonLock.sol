19553166298cc53f8f8cebfcefee3de0b6d2aa8d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:27:07 +0700

    add contract DragonLock

--- DragonLock.sol
+++ DragonLock.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DragonLock at 0x512088b53c524fb0ae111ec8c2cb9f186f5cc0b0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DragonLock at 0x6d949b9101204fa79d38df0848819f5b94acc891
 */
 pragma solidity ^0.4.18;
 
@@ -12,11 +12,79 @@ contract Dragon {
     
 }
 
+contract Ownable {
+  address public owner;
 
 
-contract DragonLock {
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+
+
+contract DragonLock is Ownable {
     
-  
+    using SafeMath for uint;
+   
   
     Dragon public tokenreward; 
     
@@ -57,4 +125,9 @@ contract DragonLock {
     
     
 
+   
+  
+
+    
+   
 }
