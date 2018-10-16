82de1d425d3001ed5f2a9b310ff3560d864d8605
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:00:14 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x52c00d4d7c5e488bc4447f9ccb009a0a6ba446c9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x01d9895bd853dc50ba4229c098b86f122dec5ceb
 */
 pragma solidity ^0.4.4;
 
@@ -98,20 +98,32 @@ contract ERC20Token is StandardToken {
         throw;
     }
 
-    
-    string public name;              
-    uint8 public decimals;               
-    string public symbol;                 
-    string public version = 'H1.0';
+    /* Public variables of the token */
 
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+//
+// CHANGE THESE VALUES FOR YOUR TOKEN
+//
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 49000000000000000000000000;                
-        totalSupply = 49000000000000000000000000;                       
-        name = "Colombia Coin";                                    
-        decimals = 18;                            
-        symbol = "COLC";                              
+        balances[msg.sender] = 10000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 10000000;                        // Update total supply (100000 for example)
+        name = "UpRentingCoin";                                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "URC";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
