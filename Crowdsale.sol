f25ed943d5c22093100b766650caa60da0c963c8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:52:14 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x2a99ff217a1077facf743a2eecfa5c56f22383de
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x36bdfebc5c817bd1b1bc042410439170267b31e5
 */
 pragma solidity ^0.4.16;
 
@@ -8,40 +8,95 @@ interface token {
 }
 
 contract Crowdsale {
-    uint public createdTimestamp; uint public start; uint public deadline;
     address public beneficiary;
+    uint public fundingGoal;
     uint public amountRaised;
+    uint public deadline;
+    uint public price;
+    token public tokenReward;
     mapping(address => uint256) public balanceOf;
+    bool fundingGoalReached = false;
     bool crowdsaleClosed = false;
+
+    event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
+
     /**
      * Constructor function
      *
      * Setup the owner
      */
     function Crowdsale(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint durationInMinutes,
+        uint etherCostOfEachToken,
+        address addressOfTokenUsedAsReward
     ) {
-        createdTimestamp = block.timestamp;
-        start = 1526292000;//createdTimestamp + 0 * 1 days + 30 * 1 minutes;
-        deadline = 1529143200;//;createdTimestamp + 1 * 1 days + 0 * 1 minutes;
-        amountRaised=0;
-        beneficiary = 0xDfD0500541c6F14eb9eD2A6e61BB63bc78693925;
+        beneficiary = ifSuccessfulSendTo;
+        fundingGoal = fundingGoalInEthers * 1 wei;
+        deadline = now + durationInMinutes * 1 minutes;
+        price = etherCostOfEachToken * 1 wei;
+        tokenReward = token(addressOfTokenUsedAsReward);
     }
+
     /**
      * Fallback function
      *
      * The function without name is the default function that is called whenever anyone sends funds to a contract
      */
     function () payable {
-        require(block.timestamp >= start && block.timestamp <= deadline && amountRaised<(6000 ether) );
-
+        require(!crowdsaleClosed);
         uint amount = msg.value;
         balanceOf[msg.sender] += amount;
         amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
         FundTransfer(msg.sender, amount, true);
-        if (beneficiary.send(amount)) {
-            FundTransfer(beneficiary, amount, false);
-        }
     }
 
+    modifier afterDeadline() { if (now >= deadline) _; }
+
+    /**
+     * Check if goal was reached
+     *
+     * Checks if the goal or time limit has been reached and ends the campaign
+     */
+    function checkGoalReached() afterDeadline {
+        if (amountRaised >= fundingGoal){
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+        }
+        crowdsaleClosed = true;
+    }
+
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal() afterDeadline {
+        if (!fundingGoalReached) {
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                if (msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+
+        if (fundingGoalReached && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised)) {
+                FundTransfer(beneficiary, amountRaised, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock funders balance
+                fundingGoalReached = false;
+            }
+        }
+    }
 }
