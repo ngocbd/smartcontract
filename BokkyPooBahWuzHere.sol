1209e2b9109600ab9b20a8b7b6a0e5a073a95934
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:01:59 +0700

    add contract BokkyPooBahWuzHere

---
+++ BokkyPooBahWuzHere.sol
@@ -0,0 +1,17 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BokkyPooBahWuzHere at 0x952c9a569725d4cb710c69184e5eb8f56d6b8bd6
+*/
+pragma solidity ^0.4.6;
+
+contract BokkyPooBahWuzHere {
+    function BokkyPooBahWuzHere() {
+    }
+    
+    function helloWorld() constant returns (string) {
+        return "Hello, World!";
+    }
+    
+    function yaaay() constant returns (string) {
+        return "yaaay!";
+    }
+}
