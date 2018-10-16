395ad43e44d2a89fc863c3c831a16228d3729447
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:13:05 +0700

    add contract MinereumTokenCreationService

---
+++ MinereumTokenCreationService.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MinereumTokenCreationService at 0x41fafb65b1514ce1177287a567002f8978d98205
+*/
+pragma solidity 0.4.11;
+
+contract MinereumTokenCreationService {
+    /* Constructor */
+    function MinereumTokenCreationService() {
+
+    }
+}
