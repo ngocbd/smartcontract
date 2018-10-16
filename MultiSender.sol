f3a84a2583b9f73bdb2877613c827fca9f953afd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:23:38 +0700

    add contract MultiSender

--- MultiSender.sol
+++ MultiSender.sol
@@ -1,29 +1,30 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSender at 0xafee74b6b4a3fc8b399b4c12cabfd9acb53c4992
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSender at 0x0d3094d335e3986268d7d144de9106a01aaa313c
 */
-pragma solidity ^0.4.18;
+contract ERC20Basic {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
+}
 
-contract ERC20 {
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint);
+  function transferFrom(address from, address to, uint value);
+  function approve(address spender, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 
 contract MultiSender {
-  function tokenFallback(address /*_from*/, uint _value, bytes /*_data*/) public {
-    require(false);
-  }
 
-  function multisendToken(address tokAddress, address[] _dests, uint256[] _amounts) public {
-    ERC20 tok = ERC20(tokAddress);
-    for (uint i = 0; i < _dests.length; i++){
-        tok.transferFrom(msg.sender, _dests[i], _amounts[i]);
+    function multisend(address _tokenAddr, address[] dests, uint256[] values)
+    returns (uint256) {
+        uint256 i = 0;
+        while (i < dests.length) {
+           ERC20(_tokenAddr).transfer(dests[i], values[i]);
+           i += 1;
+        }
+        return(i);
     }
-  }
-
-  function multisendEth(address[] _dests, uint256[] _amounts) public payable {
-    for (uint i = 0; i < _dests.length; i++){
-        _dests[i].transfer(_amounts[i]);
-    }
-    require(this.balance == 0);
-  }
 }
