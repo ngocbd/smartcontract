bd848736e38dfd6519ee45c8a5678ac80e7b4cbb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:37:55 +0700

    add contract Teste

--- Teste.sol
+++ Teste.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Teste at 0x734a1c579bb5f053103b798cac815f6346c7d035
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Teste at 0x589d5be703c93fede7de18c24f6e82f0a6e18005
 */
 pragma solidity ^0.4.16;
 
@@ -12,29 +12,29 @@ contract Teste
         creationDate = now;
     }
     
-   function segundos() view public returns (uint)
-   {
-       return (now / 1 seconds) - (creationDate / 1 seconds);
-   }
-   
-   function minutos() view public returns (uint)
-   {
-       return (now / 1 minutes) - (creationDate / 1 minutes);
-   }
-   
-   function horas() view public returns (uint)
-   {
-       return (now / 1 hours) - (creationDate / 1 hours);
-   }
-   
-   function dias() view public returns (uint)
-   {
-       return today() - (creationDate / 1 days);
-   }
-   
-   function today() view public returns (uint)
-   {
-       return now / 1 days;
-   }
-   
+    function today() view public returns (uint)
+    {
+       return ( now / 1 days);
+    }
+    
+    function pastFiveMinutes() view public returns (bool)
+    {
+        return (now >= creationDate + 5 * 1 minutes);
+    }
+
+    function pastTenMinutes() view public returns (bool)
+    {
+        return (now >= creationDate + 10 * 1 minutes);
+    }
+
+    function pastOneHour() view public returns (bool)
+    {
+        return (now >= creationDate + 1 * 1 hours);
+    }
+    
+    function pastOneDay() view public returns (bool)
+    {
+        return (now >= creationDate + 1 * 1 days);
+    }
+
 }
