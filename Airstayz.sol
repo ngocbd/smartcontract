fb9fd42467f6a1fdc2f971dd604ac709f2f908ee
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:49:33 +0700

    add contract Airstayz

--- Airstayz.sol
+++ Airstayz.sol
@@ -1,172 +1,199 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airstayz at 0x86a80daf8d1a85c09f02c80c0f3e85ee41bbca0c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airstayz at 0x478a65345db441796b6fbc1e8bea22c4c376daf5
 */
 pragma solidity 0.4.21;
 
 contract Ownable {
-    address public owner;
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+	address public owner;
+	event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
 
-    function Ownable() public {
-        owner = msg.sender;
-    }
+	function Ownable() public {
+		owner = msg.sender;
+	}
 
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+	modifier onlyOwner() {
+		require(msg.sender == owner);
+		_;
+	}
+
 
 ///////////// NEW OWNER FUNCTIONALITY
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0) && newOwner != owner);
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
+	function transferOwnership(address newOwner) public onlyOwner {
+		require(newOwner != address(0) && newOwner != owner);
+		emit OwnershipTransferred(owner, newOwner);
+		owner = newOwner;
+	}
 }
+
+
+///////////// POST ICO DESTROY COMMAND
+
+contract Destructible is Ownable {
+
+	function Destructible() public payable { }
+
+	function destroy() onlyOwner public {
+		selfdestruct(owner);
+	}
+
+	function destroyAndSend(address _recipient) onlyOwner public {
+		selfdestruct(_recipient);
+	}
+}
+
 
 ///////////// SAFE MATH FUNCTIONS
 
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
 
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        return c;
-    }
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b > 0);
+		uint256 c = a / b;
+		return c;
+	}
 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
 }
-
-contract UserTokensControl is Ownable {
-    address contractReserve;
-}
-
 
 ///////////// DECLARE ERC223 BASIC INTERFACE
 
 contract ERC223ReceivingContract {
-    function tokenFallback(address _from, uint256 _value, bytes _data) public pure {
-        _from;
-        _value;
-        _data;
-    }
+	function tokenFallback(address _from, uint256 _value, bytes _data) public pure {
+		_from;
+		_value;
+		_data;
+	}
 }
 
 contract ERC223 {
-    event Transfer(address indexed _from, address indexed _to, uint256 _value, bytes _data);
+	event Transfer(address indexed _from, address indexed _to, uint256 _value, bytes _data);
 }
 
 contract ERC20 {
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
 
 
-contract BasicToken is ERC20, ERC223, UserTokensControl {
-    uint256 public totalSupply;
-    using SafeMath for uint256;
+contract BasicToken is ERC20, ERC223, Destructible {
+	uint256 public totalSupply;
+	using SafeMath for uint256;
 
-    mapping(address => uint256) balances;
+	mapping(address => uint256) balances;
+
+	address[] allParticipants;
+	mapping(address => bool) isParticipated;
 
 
   ///////////// TRANSFER ////////////////
 
-    function transferToAddress(address _to, uint256 _value, bytes _data) internal returns (bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
-        emit Transfer(msg.sender, _to, _value, _data);
-        return true;
-    }
+	function transferToAddress(address _to, uint256 _value, bytes _data) internal returns (bool) {
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		if(!isParticipated[_to]){
+			allParticipants.push(_to);
+			isParticipated[_to] = true;
+		}
+		emit Transfer(msg.sender, _to, _value);
+		emit Transfer(msg.sender, _to, _value, _data);
+		return true;
+	}
 
-    function transferToContract(address _to, uint256 _value, bytes _data) internal returns (bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-        receiver.tokenFallback(msg.sender, _value, _data);
-        emit Transfer(msg.sender, _to, _value);
-        emit Transfer(msg.sender, _to, _value, _data);
-        return true;
-    }
+	function transferToContract(address _to, uint256 _value, bytes _data) internal returns (bool) {
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+		receiver.tokenFallback(msg.sender, _value, _data);
+		emit Transfer(msg.sender, _to, _value);
+		emit Transfer(msg.sender, _to, _value, _data);
+		return true;
+	}
 
-    function transfer(address _to, uint256 _value, bytes _data) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
-        require(_value > 0);
+	function transfer(address _to, uint256 _value, bytes _data) public onlyOwner returns (bool) {
+		require(_to != address(0));
+		require(_value <= balances[msg.sender]);
+		require(_value > 0);
 
-        uint256 codeLength;
-        assembly {
-            codeLength := extcodesize(_to)
-        }
-    
-        if(codeLength > 0) {
-            return transferToContract(_to, _value, _data);
-        } else {
-            return transferToAddress(_to, _value, _data);
-        }
-    }
-
-
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
-        require(_value > 0);
-
-        uint256 codeLength;
-        bytes memory empty;
-        assembly {
-            codeLength := extcodesize(_to)
-        }
-
-        if(codeLength > 0) {
-            return transferToContract(_to, _value, empty);
-        } else {
-            return transferToAddress(_to, _value, empty);
-        }
-    }
+		uint256 codeLength;
+		assembly {
+			codeLength := extcodesize(_to)
+		}
+	
+		if(codeLength > 0) {
+			return transferToContract(_to, _value, _data);
+		} else {
+			return transferToAddress(_to, _value, _data);
+		}
+	}
 
 
-    function balanceOf(address _address) public constant returns (uint256 balance) {
-        return balances[_address];
-    }
+	function transfer(address _to, uint256 _value) public onlyOwner returns (bool) {
+		require(_to != address(0));
+		require(_value <= balances[msg.sender]);
+		require(_value > 0);
+
+		uint256 codeLength;
+		bytes memory empty;
+		assembly {
+			codeLength := extcodesize(_to)
+		}
+
+		if(codeLength > 0) {
+			return transferToContract(_to, _value, empty);
+		} else {
+			return transferToAddress(_to, _value, empty);
+		}
+	}
+
+
+	function balanceOf(address _address) public constant returns (uint256 balance) {
+		return balances[_address];
+	}
+
+
+	function getCountPartipants() public constant returns (uint count){
+		return allParticipants.length;
+	}
+
+	function getParticipantIndexAddress(uint index)public constant returns (address){
+		return allParticipants[index];
+	}
 }
 
 
 contract StandardToken is BasicToken {
 
-    mapping (address => mapping (address => uint256)) internal allowed;
+	mapping (address => mapping (address => uint256)) internal allowed;
 }
 
 contract Airstayz is StandardToken {
-    string public constant name = "AIRSTAYZ";
-    uint public constant decimals = 18;
-    string public constant symbol = "STAY";
+	string public constant name = "AIRSTAYZSAFT";
+	uint public constant decimals = 18;
+	string public constant symbol = "STAYUS";
 
-    function Airstayz() public {
-        totalSupply=155000000 *(10**decimals);
-        owner = msg.sender;
-        contractReserve = 0xb5AB0c087b9228D584CD4363E3d000187FE69C51;
-        balances[msg.sender] = 150350000 * (10**decimals);
-        balances[contractReserve] = 4650000 * (10**decimals);
-    }
+	function Airstayz() public {
+		totalSupply = 93000000 *(10**decimals);
+		owner = msg.sender;
+		balances[msg.sender] = 93000000 * (10**decimals);
+	}
 
-    function() public {
-        revert();
-    }
+	function() public {
+		revert();
+	}
 }
