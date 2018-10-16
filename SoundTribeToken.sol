ee2f3aeac32653366a0f9138e06b1718f2583bfb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:16:48 +0700

    add contract SoundTribeToken

--- SoundTribeToken.sol
+++ SoundTribeToken.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SoundTribeToken at 0x1d07f0704dbb88298c69e351c92a82272f0daf75
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SoundTribeToken at 0xab28ae41b286952563a3ba71f5bcfba6d3e85451
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
 contract owned {
     address public owner;
@@ -22,20 +22,20 @@ contract owned {
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract SoundTribeToken is owned{
-    // Public variables
+contract SoundTribeToken {
+    // Public variables of the token
     string public name;
     string public symbol;
     uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
-    // Balances array
+    // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    //ERC20 events
+    // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     /**
      * Constrctor function
@@ -43,28 +43,20 @@ contract SoundTribeToken is owned{
      * Initializes contract with initial supply tokens to the creator of the contract
      */
     function SoundTribeToken(
-        uint256 initialSupply
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
     ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
-        name = "Sound Tribe Token"; 
-        symbol = "STS9";
-        decimals = 18;
-    }
-
-    /**
-     * ERC20 balance function
-     */
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
-        return balanceOf[_owner];
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
     /**
      * Internal transfer, only can be called by this contract
      */
     function _transfer(address _from, address _to, uint _value) internal {
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
         // Check if the sender has enough
@@ -79,8 +71,6 @@ contract SoundTribeToken is owned{
         balanceOf[_to] += _value;
         Transfer(_from, _to, _value);
         // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-        // Failsafe logic that should never be false
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
@@ -112,53 +102,29 @@ contract SoundTribeToken is owned{
         return true;
     }
 
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf
-     *
-     * @param _spender the address authorized to spend
-     * @param _value the max amount they can spend
-     */
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        
-        allowance[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender the address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
 }
 
-contract AdvSoundTribeToken is owned, SoundTribeToken {
+contract MyAdvancedSoundTribeToken is owned, SoundTribeToken {
 
+    mapping (address => bool) public frozenAccount;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event FrozenFunds(address target, bool frozen);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function AdvSoundTribeToken(
-        uint256 initialSupply
-    ) SoundTribeToken(initialSupply) public {}
+    function MyAdvancedSoundTribeToken(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) SoundTribeToken(initialSupply, tokenName, tokenSymbol) public {}
 
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) internal {
         require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
         require (balanceOf[_from] >= _value);               // Check if the sender has enough
         require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        require(!frozenAccount[_from]);                     // Check if sender is frozen
+        require(!frozenAccount[_to]);                       // Check if recipient is frozen
         balanceOf[_from] -= _value;                         // Subtract from the sender
         balanceOf[_to] += _value;                           // Add the same to the recipient
         Transfer(_from, _to, _value);
@@ -173,5 +139,6 @@ contract AdvSoundTribeToken is owned, SoundTribeToken {
         Transfer(0, this, mintedAmount);
         Transfer(this, target, mintedAmount);
     }
+
 
 }
