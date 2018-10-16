ae9eb4658faf7689aa8c951cda360243deeecd66
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:27:26 +0700

    add contract NAGACOIN

--- NAGACoin.sol
+++ NAGACoin.sol
@@ -1,153 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NAGACoin at 0x818ebdad12d3decefabe6ccb16f33c9bbfe1b30a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NAGACOIN at 0x015a8a0163ad54c86012ff57d3558b6271a2d2bd
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.4;
 
+contract Token {
 
-/// @title  NagaCoin internal presale - https://www.nagaico.com (NGC) - crowdfunding code
-/// Whitepaper:
-///  https://docsend.com/view/srsg4zw
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
 
-contract NAGACoin {
-    string public name = "NAGA Coin";
-    string public symbol = "NGC";
-    uint8 public constant decimals = 18;  
-    address public owner;
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
 
-    uint256 public constant tokensPerEth = 1;
-    uint256 public constant howManyEtherInWeiToBecomeOwner = 1000 ether;
-    uint256 public constant howManyEtherInWeiToKillContract = 500 ether;
-    uint256 public constant howManyEtherInWeiToChangeSymbolName = 400 ether;
-    
-    bool public funding = true;
-
-    // The current total token supply.
-    uint256 totalTokens = 1000;
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Migrate(address indexed _from, address indexed _to, uint256 _value);
-    event Refund(address indexed _from, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    function NAGACoin() public {
-        owner = msg.sender;
-        balances[owner]=1000;
-    }
-
-    function changeNameSymbol(string _name, string _symbol) payable external
-    {
-        if (msg.sender==owner || msg.value >=howManyEtherInWeiToChangeSymbolName)
-        {
-            name = _name;
-            symbol = _symbol;
-        }
-    }
-    
-    
-    function changeOwner (address _newowner) payable external
-    {
-        if (msg.value>=howManyEtherInWeiToBecomeOwner)
-        {
-            owner.transfer(msg.value);
-            owner.transfer(this.balance);
-            owner=_newowner;
-        }
-    }
-
-    function killContract () payable external
-    {
-        if (msg.sender==owner || msg.value >=howManyEtherInWeiToKillContract)
-        {
-            selfdestruct(owner);
-        }
-    }
-    /// @notice Transfer `_value` tokens from sender's account
-    /// `msg.sender` to provided account address `_to`.
-    /// @notice This function is disabled during the funding.
-    /// @dev Required state: Operational
-    /// @param _to The address of the tokens recipient
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        // Abort if not in Operational state.
-        
-        var senderBalance = balances[msg.sender];
-        if (senderBalance >= _value && _value > 0) {
-            senderBalance -= _value;
-            balances[msg.sender] = senderBalance;
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
             balances[_to] += _value;
             Transfer(msg.sender, _to, _value);
             return true;
-        }
-        return false;
+        } else { return false; }
     }
-    
-    function mintTo(address _to, uint256 _value) public returns (bool) {
-        // Abort if not in Operational state.
-        
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
             return true;
-    }
-    
-
-    function totalSupply() external constant returns (uint256) {
-        return totalTokens;
+        } else { return false; }
     }
 
-    function balanceOf(address _owner) external constant returns (uint256) {
+    function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
 
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-    function transferFrom(
-         address _from,
-         address _to,
-         uint256 _amount
-     ) public returns (bool success) {
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             return true;
-         } else {
-             return false;
-         }
-  }
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
 
-    function approve(address _spender, uint256 _amount) public returns (bool success) {
-         allowed[msg.sender][_spender] = _amount;
-         Approval(msg.sender, _spender, _amount);
-         
-         return true;
-     }
-// Crowdfunding:
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
 
-    /// @notice Create tokens when funding is active.
-    /// @dev Required state: Funding Active
-    /// @dev State transition: -> Funding Success (only if cap reached)
-    function () payable external {
-        // Abort if not in Funding Active state.
-        // The checks are split (instead of using or operator) because it is
-        // cheaper this way.
-        if (!funding) revert();
-        
-        // Do not allow creating 0 or more than the cap tokens.
-        if (msg.value == 0) revert();
-        
-        var numTokens = msg.value * (1000.0/totalTokens);
-        totalTokens += numTokens;
+contract NAGACOIN is StandardToken { // CHANGE THIS. Update the contract name.
 
-        // Assign new tokens to the sender
-        balances[msg.sender] += numTokens;
+    /* Public variables of the token */
 
-        // Log token creation event
-        Transfer(0, msg.sender, numTokens);
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   // Token Name
+    uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
+    string public symbol;                 // An identifier: eg SBX, XPR etc..
+    string public version = 'H1.0'; 
+    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
+    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
+    address public fundsWallet;           // Where should the raised ETH go?
+
+    // This is a constructor function 
+    // which means the following function name has to match the contract name declared above
+    function NAGACOIN() {
+        balances[msg.sender] = 77910266000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 77910266000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "NAGA TOKEN";                                   // Set the name for display purposes (CHANGE THIS)
+        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
+        symbol = "NAGA";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 100000;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
+    }
+
+    function() payable{
+        totalEthInWei = totalEthInWei + msg.value;
+        uint256 amount = msg.value * unitsOneEthCanBuy;
+        require(balances[fundsWallet] >= amount);
+
+        balances[fundsWallet] = balances[fundsWallet] - amount;
+        balances[msg.sender] = balances[msg.sender] + amount;
+
+        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
+
+        //Transfer ether to fundsWallet
+        fundsWallet.transfer(msg.value);                               
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        return true;
     }
 }
