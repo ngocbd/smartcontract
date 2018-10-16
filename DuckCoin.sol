fd4a3c95c50c5bfbbd1164bb55d9545c7a7d3de0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:35:12 +0700

    add contract Duckcoin

--- DuckCoin.sol
+++ DuckCoin.sol
@@ -1,95 +1,410 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DuckCoin at 0x64d0043b6bb6ddd8ff2dfbcb463741db8949671d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Duckcoin at 0x6d68003c0452632a87381c42091fc94ceb047fd1
 */
-pragma solidity ^0.4.8;
-contract Token {
-    /// token???????public??????getter????????totalSupply().
-    uint256 public totalSupply;
+pragma solidity ^0.4.18;
 
-    /// ????_owner??token???
-    function balanceOf(address _owner) constant returns (uint256 balance);
 
-    //??????????_to??????_value?token
-    function transfer(address _to, uint256 _value) returns (bool success);
+contract Owned {
 
-    //???_from????_to????_value?token??approve??????
-    function transferFrom(address _from, address _to, uint256 _value) returns  (bool success);
+   address public owner;
+   address public proposedOwner;
 
-    //??????????_spender????????????_value?token
-    function approve(address _spender, uint256 _value) returns (bool success);
+   event OwnershipTransferInitiated(address indexed _proposedOwner);
+   event OwnershipTransferCompleted(address indexed _newOwner);
+   event OwnershipTransferCanceled();
 
-    //????_spender?????_owner???token???
-    function allowance(address _owner, address _spender) constant returns  (uint256 remaining);
 
-    //????????????? 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+   function Owned() public
+   {
+      owner = msg.sender;
+   }
 
-    //???approve(address _spender, uint256 _value)????????????
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+   modifier onlyOwner() {
+      require(isOwner(msg.sender) == true);
+      _;
+   }
+
+
+   function isOwner(address _address) public view returns (bool) {
+      return (_address == owner);
+   }
+
+
+   function initiateOwnershipTransfer(address _proposedOwner) public onlyOwner returns (bool) {
+      require(_proposedOwner != address(0));
+      require(_proposedOwner != address(this));
+      require(_proposedOwner != owner);
+
+      proposedOwner = _proposedOwner;
+
+      OwnershipTransferInitiated(proposedOwner);
+
+      return true;
+   }
+
+
+   function cancelOwnershipTransfer() public onlyOwner returns (bool) {
+      if (proposedOwner == address(0)) {
+         return true;
+      }
+
+      proposedOwner = address(0);
+
+      OwnershipTransferCanceled();
+
+      return true;
+   }
+
+
+   function completeOwnershipTransfer() public returns (bool) {
+      require(msg.sender == proposedOwner);
+
+      owner = msg.sender;
+      proposedOwner = address(0);
+
+      OwnershipTransferCompleted(owner);
+
+      return true;
+   }
 }
 
-contract StandardToken is Token {
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //??totalSupply ??????? (2^256 - 1).
-        //??????????????token??????????????????
-        //require(balances[msg.sender] >= _value && balances[_to] + _value >balances[_to]);
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;//???????????token??_value
-        balances[_to] += _value;//???????token??_value
-        Transfer(msg.sender, _to, _value);//????????
-        return true;
-    }
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >=  _value);
-        balances[_to] += _value;//??????token??_value
-        balances[_from] -= _value; //????_from??token??_value
-        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
-        Transfer(_from, _to, _value);//????????
-        return true;
-    }
-    //????
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-    //????_spender???????????????_value?token
-    function approve(address _spender, uint256 _value) returns (bool success)   
-    {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];//??_spender?_owner????token?
-    }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
+
+contract OpsManaged is Owned {
+
+   address public opsAddress;
+
+   event OpsAddressUpdated(address indexed _newAddress);
+
+
+   function OpsManaged() public
+      Owned()
+   {
+   }
+
+
+   modifier onlyOwnerOrOps() {
+      require(isOwnerOrOps(msg.sender));
+      _;
+   }
+
+
+   function isOps(address _address) public view returns (bool) {
+      return (opsAddress != address(0) && _address == opsAddress);
+   }
+
+
+   function isOwnerOrOps(address _address) public view returns (bool) {
+      return (isOwner(_address) || isOps(_address));
+   }
+
+
+   function setOpsAddress(address _newOpsAddress) public onlyOwner returns (bool) {
+      require(_newOpsAddress != owner);
+      require(_newOpsAddress != address(this));
+
+      opsAddress = _newOpsAddress;
+
+      OpsAddressUpdated(opsAddress);
+
+      return true;
+   }
 }
 
-contract DuckCoin is StandardToken {
-    /* Public variables of the token */
-    string public name;                   //??: eg Simon Bucks
-    uint8 public decimals;                //???????How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //token??: eg SBX
-    string public version = 'H0.1';       //??
 
-    function DuckCoin(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
-        balances[msg.sender] = _initialAmount; // ??token?????????
-        totalSupply = _initialAmount;         // ??????
-        name = _tokenName;                   // token??
-        decimals = _decimalUnits;           // ????
-        symbol = _tokenSymbol;             // token??
-    }
-    /* ????????????????????? */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        
-        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
-        return true;
-    }
+library Math {
+
+   function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 r = a + b;
+
+      require(r >= a);
+
+      return r;
+   }
+
+
+   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      require(a >= b);
+
+      return a - b;
+   }
+
+
+   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 r = a * b;
+
+      require(a == 0 || r / a == b);
+
+      return r;
+   }
+
+
+   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+      return a / b;
+   }
+}
+
+// ----------------------------------------------------------------------------
+// Based on the final ERC20 specification at:
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+
+   event Transfer(address indexed _from, address indexed _to, uint256 _value);
+   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+   function name() public view returns (string);
+   function symbol() public view returns (string);
+   function decimals() public view returns (uint8);
+   function totalSupply() public view returns (uint256);
+
+   function balanceOf(address _owner) public view returns (uint256 balance);
+   function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+   function transfer(address _to, uint256 _value) public returns (bool success);
+   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+   function approve(address _spender, uint256 _value) public returns (bool success);
+}
+
+// ----------------------------------------------------------------------------
+
+
+contract ERC20Token is ERC20Interface {
+
+   using Math for uint256;
+
+   string  private tokenName;
+   string  private tokenSymbol;
+   uint8   private tokenDecimals;
+   uint256 internal tokenTotalSupply;
+
+   mapping(address => uint256) internal balances;
+   mapping(address => mapping (address => uint256)) allowed;
+
+
+   function ERC20Token(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply, address _initialTokenHolder) public {
+      tokenName = _name;
+      tokenSymbol = _symbol;
+      tokenDecimals = _decimals;
+      tokenTotalSupply = _totalSupply;
+
+      // The initial balance of tokens is assigned to the given token holder address.
+      balances[_initialTokenHolder] = _totalSupply;
+
+      // Per EIP20, the constructor should fire a Transfer event if tokens are assigned to an account.
+      Transfer(0x0, _initialTokenHolder, _totalSupply);
+   }
+
+
+   function name() public view returns (string) {
+      return tokenName;
+   }
+
+
+   function symbol() public view returns (string) {
+      return tokenSymbol;
+   }
+
+
+   function decimals() public view returns (uint8) {
+      return tokenDecimals;
+   }
+
+
+   function totalSupply() public view returns (uint256) {
+      return tokenTotalSupply;
+   }
+
+
+   function balanceOf(address _owner) public view returns (uint256 balance) {
+      return balances[_owner];
+   }
+
+
+   function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+   }
+
+
+   function transfer(address _to, uint256 _value) public returns (bool success) {
+      balances[msg.sender] = balances[msg.sender].sub(_value);
+      balances[_to] = balances[_to].add(_value);
+
+      Transfer(msg.sender, _to, _value);
+
+      return true;
+   }
+
+
+   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      balances[_from] = balances[_from].sub(_value);
+      allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+      balances[_to] = balances[_to].add(_value);
+
+      Transfer(_from, _to, _value);
+
+      return true;
+   }
+
+
+   function approve(address _spender, uint256 _value) public returns (bool success) {
+      allowed[msg.sender][_spender] = _value;
+
+      Approval(msg.sender, _spender, _value);
+
+      return true;
+   }
+}
+
+// ----------------------------------------------------------------------------
+
+
+
+contract Finalizable is Owned {
+
+   bool public finalized;
+
+   event Finalized();
+
+
+   function Finalizable() public
+      Owned()
+   {
+      finalized = false;
+   }
+
+
+   function finalize() public onlyOwner returns (bool) {
+      require(!finalized);
+
+      finalized = true;
+
+      Finalized();
+
+      return true;
+   }
+}
+
+// -----------------------
+// ----------------------------------------------------------------------------
+
+
+
+//
+contract FinalizableToken is ERC20Token, OpsManaged, Finalizable {
+
+   using Math for uint256;
+
+
+   // The constructor will assign the initial token supply to the owner (msg.sender).
+   function FinalizableToken(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) public
+      ERC20Token(_name, _symbol, _decimals, _totalSupply, msg.sender)
+      OpsManaged()
+      Finalizable()
+   {
+   }
+
+
+   function transfer(address _to, uint256 _value) public returns (bool success) {
+      validateTransfer(msg.sender, _to);
+
+      return super.transfer(_to, _value);
+   }
+
+
+   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      validateTransfer(msg.sender, _to);
+
+      return super.transferFrom(_from, _to, _value);
+   }
+
+
+   function validateTransfer(address _sender, address _to) private view {
+      require(_to != address(0));
+
+      // Once the token is finalized, everybody can transfer tokens.
+      if (finalized) {
+         return;
+      }
+
+      if (isOwner(_to)) {
+         return;
+      }
+
+      // Before the token is finalized, only owner and ops are allowed to initiate transfers.
+      // This allows them to move tokens while the sale is still ongoing for example.
+      require(isOwnerOrOps(_sender));
+   }
+}
+
+
+// ----------------------------------------------------------------------------
+// Token Contract Configuration
+//
+// ----------------------------------------------------------------------------
+
+
+contract TokenConfig {
+
+    string  public constant TOKEN_SYMBOL      = "DUCK";
+    string  public constant TOKEN_NAME        = "Duckcoin";
+    uint8   public constant TOKEN_DECIMALS    = 18;
+
+    uint256 public constant DECIMALSFACTOR    = 10**uint256(TOKEN_DECIMALS);
+    uint256 public constant TOKEN_TOTALSUPPLY = 2000000000000 * DECIMALSFACTOR;
+}
+
+
+
+// ----------------------------------------------------------------------------
+//  - ERC20 Compatible Token
+//
+// ----------------------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------------------
+// The  token is a standard ERC20 token with the addition of a few
+// concepts such as:
+//
+// 1. Finalization
+// Tokens can only be transfered by contributors after the contract has
+// been finalized.
+//
+// 2. Ops Managed Model
+// In addition to owner, there is a ops role which is used during the sale,
+// by the sale contract, in order to transfer tokens.
+// ----------------------------------------------------------------------------
+contract Duckcoin is FinalizableToken, TokenConfig {
+
+
+   event TokensReclaimed(uint256 _amount);
+
+
+   function Duckcoin() public
+      FinalizableToken(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, TOKEN_TOTALSUPPLY)
+   {
+   }
+
+
+   // Allows the owner to reclaim tokens that have been sent to the token address itself.
+   function reclaimTokens() public onlyOwner returns (bool) {
+
+      address account = address(this);
+      uint256 amount  = balanceOf(account);
+
+      if (amount == 0) {
+         return false;
+      }
+
+      balances[account] = balances[account].sub(amount);
+      balances[owner] = balances[owner].add(amount);
+
+      Transfer(account, owner, amount);
+
+      TokensReclaimed(amount);
+
+      return true;
+   }
 }
