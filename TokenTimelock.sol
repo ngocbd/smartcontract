754784e10bc0531a8671c1f082586ae7fe69c56e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:13:16 +0700

    add contract TokenTimelock

--- TokenTimelock.sol
+++ TokenTimelock.sol
@@ -1,8 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0xE4aF17a0beDcB1BEB5e5fDb52BdBFe0A26D320b8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0xf7d93efa5616836d48573fdee9d6d92873ffe5d5
 */
 pragma solidity ^0.4.23;
-
 
 /**
  * @title ERC20Basic
@@ -10,32 +9,30 @@ pragma solidity ^0.4.23;
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
-
 
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender)
-    public view returns (uint256);
+    function allowance(address owner, address spender)
+        public view returns (uint256);
 
-  function transferFrom(address from, address to, uint256 value)
-    public returns (bool);
+    function transferFrom(address from, address to, uint256 value)
+        public returns (bool);
 
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(
+            address indexed owner,
+            address indexed spender,
+            uint256 value
+            );
 }
-
 
 /**
  * @title SafeERC20
@@ -44,26 +41,25 @@ contract ERC20 is ERC20Basic {
  * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
 library SafeERC20 {
-  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
-    require(token.transfer(to, value));
-  }
+    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+        require(token.transfer(to, value));
+    }
 
-  function safeTransferFrom(
-    ERC20 token,
-    address from,
-    address to,
-    uint256 value
-  )
-    internal
-  {
-    require(token.transferFrom(from, to, value));
-  }
+    function safeTransferFrom(
+            ERC20 token,
+            address from,
+            address to,
+            uint256 value
+            )
+        internal
+        {
+            require(token.transferFrom(from, to, value));
+        }
 
-  function safeApprove(ERC20 token, address spender, uint256 value) internal {
-    require(token.approve(spender, value));
-  }
+    function safeApprove(ERC20 token, address spender, uint256 value) internal {
+        require(token.approve(spender, value));
+    }
 }
-
 
 /**
  * @title TokenTimelock
@@ -71,41 +67,49 @@ library SafeERC20 {
  * beneficiary to extract the tokens after a given release time
  */
 contract TokenTimelock {
-  using SafeERC20 for ERC20Basic;
+    using SafeERC20 for ERC20Basic;
 
-  // ERC20 basic token contract being held
-  ERC20Basic public token;
+    // ERC20 basic token contract being held
+    ERC20Basic public token;
 
-  // beneficiary of tokens after they are released
-  address public beneficiary;
+    // beneficiary of tokens after they are released
+    address public beneficiary = 0x2F1C2Fb4cf9b46172D59d8878Fc795277b8a2c9a;
 
-  // timestamp when token release is enabled
-  uint256 public releaseTime;
+    // timestamp when token release is enabled
+    uint256 public firstTime = 1529942400;         //2018-06-26
+    uint256 public secondTime = 1532534400;        //2018-07-26
+    uint256 public thirdTime = 1535212800;         //2018-08-26
 
-  constructor(
-    ERC20Basic _token,
-    address _beneficiary,
-    uint256 _releaseTime
-  )
-    public
-  {
-    // solium-disable-next-line security/no-block-members
-    require(_releaseTime > block.timestamp);
-    token = _token;
-    beneficiary = _beneficiary;
-    releaseTime = _releaseTime;
-  }
+    uint256 public firstPay = 900000000000000000000000000;    //900 million  FTI
+    uint256 public secondPay = 900000000000000000000000000;    //900 million  FTI
+    uint256 public thirdPay = 600000000000000000000000000;    //900 million  FTI
 
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function release() public {
-    // solium-disable-next-line security/no-block-members
-    require(block.timestamp >= releaseTime);
+    constructor(
+            ERC20Basic _token
+            )
+        public
+        {
+            token = _token;
+        }
 
-    uint256 amount = token.balanceOf(this);
-    require(amount > 0);
-
-    token.safeTransfer(beneficiary, amount);
-  }
+    /**
+     * @notice Transfers tokens held by timelock to beneficiary.
+     */
+    function release() public {
+        uint256 tmpPay = 0;
+        if(block.timestamp >= firstTime && firstPay > 0){
+            tmpPay = firstPay;
+            firstPay = 0;
+        }else if(block.timestamp >= secondTime && secondPay > 0 ){
+            tmpPay = secondPay;
+            secondPay = 0;
+        }else if (block.timestamp >= thirdTime && thirdPay > 0) {
+            tmpPay = token.balanceOf(this);
+            thirdPay = 0;
+        }
+        require(tmpPay > 0);
+        uint256 amount = token.balanceOf(this);
+        require(amount >= tmpPay);
+        token.safeTransfer(beneficiary, tmpPay);
+    }
 }
