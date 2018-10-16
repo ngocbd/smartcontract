eac0647be0c9d9fe8f3eff50cca7f829136ccf24
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:09:58 +0700

    add contract IntermediateVault

---
+++ IntermediateVault.sol
@@ -0,0 +1,59 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IntermediateVault at 0x4b667a378c1d9b2134cc4fa02d9cfba2cc2b11d4
+*/
+/**
+ *
+ * Time-locked ETH vault of money being transferred into team multisig.
+ *
+ * Add 4 weeks delay to the transfer to the team multisig wallet.
+ *
+ * The owning party can reset the timer.
+ *
+ */
+contract IntermediateVault  {
+
+  /** Interface flag to determine if address is for a real contract or not */
+  bool public isIntermediateVault = true;
+
+  /** Address that can claim tokens */
+  address public teamMultisig;
+
+  /** UNIX timestamp when tokens can be claimed. */
+  uint256 public unlockedAt;
+
+  event Unlocked();
+  event Paid(address sender, uint amount);
+
+  function IntermediateVault(address _teamMultisig, uint _unlockedAt) {
+
+    teamMultisig = _teamMultisig;
+    unlockedAt = _unlockedAt;
+
+    // Sanity check
+    if(teamMultisig == 0x0)
+      throw;
+
+    // Do not check for the future time here, so that test is easier.
+    // Check for an empty value though
+    // Use value 1 for testing
+    if(_unlockedAt == 0)
+      throw;
+  }
+
+  /// @notice Transfer locked tokens to Lunyr's multisig wallet
+  function unlock() public {
+    // Wait your turn!
+    if(now < unlockedAt) throw;
+
+    // StandardToken will throw in the case of transaction fails
+    if(!teamMultisig.send(address(this).balance)) throw; // Should this forward gas, since we trust the wallet?
+
+    Unlocked();
+  }
+
+  function () public payable {
+    // Collect deposits from the crowdsale
+    Paid(msg.sender, msg.value);
+  }
+
+}
