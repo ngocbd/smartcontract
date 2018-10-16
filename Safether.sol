e800006fc232d1db70c57f8717bcb3f212e46b34
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:18:20 +0700

    add contract Safether

--- Safether.sol
+++ Safether.sol
@@ -1,13 +1,24 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Safether at 0x2322f82d952116d044edd05b0bdcd098335502c3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Safether at 0xc544e35347e3bd4e98498d5f6613262f4e672c8e
 */
 pragma solidity ^0.4.19;
 
 contract SafetherStorage {
+    
+    /*
+    * Depositor Struct is Storage for User.
+    * 
+    * _token is access key required for find assets.
+    * _data is storage for depositor.
+    *
+    * _data[0] : Register Block Number
+    * _data[1] : Period holding assets
+    * _data[2] : Amount of holding assets
+    *
+    */
     struct Depositor {
-        bytes8      _token;
-        uint256     _limit;
-        uint256     _deposit;
+        bytes8     _token;
+        uint256[3]  _data;
     }
     
     mapping (address=>Depositor) internal _depositor;
@@ -15,57 +26,60 @@ contract SafetherStorage {
 
 contract SafetherModifier is SafetherStorage {
     modifier isRegisterd {
-        require(_depositor[msg.sender]._token != 0x0000000000000000);
+        require(_depositor[msg.sender]._token != 0x0);
         _;
     }
     
     modifier isNotRegisterd {
-        require(_depositor[msg.sender]._token == 0x0000000000000000);
+        require(_depositor[msg.sender]._token == 0x0);
         _;
     }
     
     modifier isValidDepositor(address depositor, bytes8 token) {
-        require(_depositor[depositor]._token != 0x0000000000000000);
-        require(_depositor[depositor]._deposit > 0);
         require(_depositor[depositor]._token == token);
-        require(block.number >= _depositor[depositor]._limit);
+        require(_depositor[depositor]._data[2] > 0);
+        require(block.number >= _depositor[depositor]._data[1]);
         _;
     }
 }
 
 contract SafetherAbstract {
-    function getDepositor() public constant returns(bytes8, uint256, uint256);
+    function authentication(bytes8 token) public constant returns(bool);
+    function getDepositor() public constant returns(uint256[3]);
     
-    function register() public;
+    function register(bytes7 password) public;
     function deposit(uint256 period) public payable;
     function withdraw(address depositor, bytes8 token) public payable;
     function cancel() public payable;
 }
 
 contract Safether is SafetherModifier, SafetherAbstract {
-    function getDepositor() public constant returns(bytes8, uint256, uint256) {
-        return (_depositor[msg.sender]._token, 
-                _depositor[msg.sender]._limit,
-                _depositor[msg.sender]._deposit);
+    function authentication(bytes8 token) public constant returns(bool) {
+        return _depositor[msg.sender]._token == token;
     }
     
-    function register() public isNotRegisterd {
-        _depositor[msg.sender]._token = bytes8(keccak256(block.number, msg.sender));
+    function getDepositor() public constant returns (uint256[3]) {
+        return (_depositor[msg.sender]._data);
+    }
+    
+    function register(bytes7 password) public isNotRegisterd {
+        _depositor[msg.sender]._token = bytes8(keccak256(block.number, msg.sender, password));
+        _depositor[msg.sender]._data[0] = block.number;
     }
     
     function deposit(uint256 period) public payable isRegisterd {
-        _depositor[msg.sender]._deposit += msg.value;
-        _depositor[msg.sender]._limit = block.number + period;
+        _depositor[msg.sender]._data[1] = block.number + period;
+        _depositor[msg.sender]._data[2] += msg.value;
     }
     
     function withdraw(address depositor, bytes8 token) public payable isValidDepositor(depositor, token) {
-        uint256 tempDeposit = _depositor[depositor]._deposit;
-         _depositor[depositor]._deposit = 0;
+        uint256 tempDeposit = _depositor[depositor]._data[2];
+         _depositor[depositor]._data[2] = 0;
          msg.sender.transfer(tempDeposit + msg.value);
     }
     
     function cancel() public payable isRegisterd {
-        uint256 tempDeposit = _depositor[msg.sender]._deposit;
+        uint256 tempDeposit = _depositor[msg.sender]._data[2];
         delete _depositor[msg.sender];
         msg.sender.transfer(tempDeposit + msg.value);
     }
