abf249aa2a41e44b720fd91571232c575602741c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:22:12 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,161 +1,283 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xed4ae9985461e7f54a37dc8647cf0902576ee08b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x08CF2917773aE20d9A1c66bB552df7475840BAa0
 */
 pragma solidity ^0.4.15;
+
+contract Base {
+
+    modifier only(address allowed) {
+        require(msg.sender == allowed);
+        _;
+    }
+
+    // *************************************************
+    // *          reentrancy handling                  *
+    // *************************************************
+    uint private bitlocks = 0;
+
+    modifier noAnyReentrancy {
+        var _locks = bitlocks;
+        require(_locks == 0);
+        bitlocks = uint(-1);
+        _;
+        bitlocks = _locks;
+    }
+}
+
+contract IToken {
+    function mint(address _to, uint _amount) public;
+    function start() public;
+    function getTotalSupply()  public returns(uint);
+    function balanceOf(address _owner)  public returns(uint);
+    function transfer(address _to, uint _amount)  public returns (bool success);
+    function transferFrom(address _from, address _to, uint _value)  public returns (bool success);
+}
 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
+
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
   }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
+contract Owned is Base {
+    address public owner;
+    address newOwner;
 
-  address public owner;
+    function Owned() public {
+        owner = msg.sender;
+    }
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
+    function transferOwnership(address _newOwner) public only(owner) {
+        newOwner = _newOwner;
+    }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    function acceptOwnership() public only(newOwner) {
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));
-    owner = newOwner;
-  }
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 }
 
-/**
- * @title Token
- * @dev API interface for interacting with the WILD Token contract 
- */
-interface Token {
-  function transfer(address _to, uint256 _value) returns (bool);
-  function balanceOf(address _owner) constant returns (uint256 balance);
-}
+contract Crowdsale is Base, Owned {
+    using SafeMath for uint256;
 
-contract Crowdsale is Ownable {
+    enum State { INIT, BOUNTY, PREICO, PREICO_FINISHED, ICO, CLOSED, STOPPED }
+    enum SupplyType { BOUNTY, SALE }
 
-  using SafeMath for uint256;
+    uint public constant DECIMALS = 10**18;
+    uint public constant MAX_PREICO_SUPPLY = 20000000 * DECIMALS;
+    uint public constant MAX_ICO_SUPPLY = 70000000 * DECIMALS;
+    uint public constant MAX_BOUNTY_SUPPLY = 10000000 * DECIMALS;
 
-  Token public token;
+    State public currentState = State.INIT;
+    IToken public token;
 
-  uint256 public constant RATE = 2000; // Number of tokens per Ether
-  uint256 public constant CAP = 500; // Cap in Ether
-  uint256 public constant START = now; // Sep 5, 2017 @ 08:00 GMT+1
-  uint256 public constant DAYS = 7; // 7 Days
+    uint public totalPreICOSupply = 0;
+    uint public totalICOSupply = 0;
+    uint public totalBountySupply = 0;
 
-  uint256 public constant initialTokens = 1000000 * 10**18; // Initial number of tokens available
-  bool public initialized = false;
-  uint256 public raisedAmount = 0;
+    uint public totalFunds = 0;
+    uint public tokenPrice = 6000000000000; //wei
+    uint public bonus = 2000; //20%
+    uint public currentPrice;
+    address public beneficiary;
+    mapping(address => uint) balances;
+    uint public countMembers = 0;
 
-  event BoughtTokens(address indexed to, uint256 value);
+    uint private bonusBase = 10000; //100%;
 
-  modifier whenSaleIsActive() {
-    // Check if sale is active
-    assert(isActive());
+    event Transfer(address indexed _to, uint256 _value);
 
-    _;
-  }
+    modifier inState(State _state){
+        require(currentState == _state);
+        _;
+    }
 
-  function Crowdsale(address _tokenAddr) {
-      require(_tokenAddr != 0);
-      token = Token(_tokenAddr);
-  }
-  
-  function initialize() onlyOwner {
-      require(initialized == false); // Can only be initialized once
-      require(tokensAvailable() == initialTokens); // Must have some tokens allocated
-      initialized = true;
-  }
+    modifier salesRunning(){
+        require(currentState == State.PREICO || currentState == State.ICO);
+        _;
+    }
 
-  function isActive() constant returns (bool) {
-    return (
-        initialized == true &&
-        now >= START && // Must be after the START date
-        now <= START.add(DAYS * 1 days) && // Must be before the end date
-        goalReached() == false // Goal must not already be reached
-    );
-  }
+    modifier notStopped(){
+        require(currentState != State.STOPPED);
+        _;
+    }
 
-  function goalReached() constant returns (bool) {
-    return (raisedAmount >= CAP * 1 ether);
-  }
+    function Crowdsale(address _beneficiary) public {
+        beneficiary = _beneficiary;
+    }
 
-  function () payable {
-    buyTokens();
-  }
+    function ()
+        public
+        payable
+        salesRunning
+    {
+        _receiveFunds();
+    }
 
-  /**
-  * @dev function that sells available tokens
-  */
-  function buyTokens() payable whenSaleIsActive {
+    function initialize(address _token)
+        public
+        only(owner)
+        inState(State.INIT)
+    {
+        require(_token != address(0));
 
-    // Calculate tokens to sell
-    uint256 weiAmount = msg.value;
-    uint256 tokens = weiAmount.mul(RATE);
+        token = IToken(_token);
+        currentPrice = tokenPrice;
+    }
 
-    BoughtTokens(msg.sender, tokens);
+    function setBonus(uint _bonus) public
+        only(owner)
+        notStopped
+    {
+        bonus = _bonus;
+    }
 
-    // Increment raised amount
-    raisedAmount = raisedAmount.add(msg.value);
+    function getBonus()
+        public
+        constant
+        returns(uint)
+    {
+        return bonus.mul(100).div(bonusBase);
+    }
+
+    function setTokenPrice(uint _tokenPrice) public
+        only(owner)
+        notStopped
+    {
+        currentPrice = _tokenPrice;
+    }
+
+    function setState(State _newState)
+        public
+        only(owner)
+    {
+        require(
+            currentState != State.STOPPED && (_newState == State.STOPPED ||
+            (currentState == State.INIT && _newState == State.BOUNTY
+            || currentState == State.BOUNTY && _newState == State.PREICO
+            || currentState == State.PREICO && _newState == State.PREICO_FINISHED
+            || currentState == State.PREICO_FINISHED && _newState == State.ICO
+            || currentState == State.ICO && _newState == State.CLOSED))
+        );
+
+        if(_newState == State.CLOSED){
+            _finish();
+        }
+
+        currentState = _newState;
+    }
+
+    function investDirect(address _to, uint _amount)
+        public
+        only(owner)
+        salesRunning
+    {
+        uint bonusTokens = _amount.mul(bonus).div(bonusBase);
+        _amount = _amount.add(bonusTokens);
+
+        _checkMaxSaleSupply(_amount);
+
+        _mint(_to, _amount);
+    }
     
-    // Send tokens to buyer
-    token.transfer(msg.sender, tokens);
+    function investBounty(address _to, uint _amount)
+        public
+        only(owner)
+        inState(State.BOUNTY)
+    {
+        _mint(_to, _amount);
+    }
+
+
+    function getCountMembers()
+    public
+    constant
+    returns(uint)
+    {
+        return countMembers;
+    }
+
+    //==================== Internal Methods =================
+    function _mint(address _to, uint _amount)
+        noAnyReentrancy
+        internal
+    {
+        _increaseSupply(_amount);
+        IToken(token).mint(_to, _amount);
+        Transfer(_to, _amount);
+    }
+
+    function _finish()
+        noAnyReentrancy
+        internal
+    {
+        IToken(token).start();
+    }
+
+    function _receiveFunds()
+        internal
+    {
+        require(msg.value != 0);
+        uint weiAmount = msg.value;
+        uint transferTokens = weiAmount.mul(DECIMALS).div(currentPrice);
+
+        uint bonusTokens = transferTokens.mul(bonus).div(bonusBase);
+        transferTokens = transferTokens.add(bonusTokens);
+
+        _checkMaxSaleSupply(transferTokens);
+
+        if(balances[msg.sender] == 0){
+            countMembers = countMembers.add(1);
+        }
+
+        balances[msg.sender] = balances[msg.sender].add(weiAmount);
+        totalFunds = totalFunds.add(weiAmount);
+
+        _mint(msg.sender, transferTokens);
+        beneficiary.transfer(weiAmount);
+    }
+
+    function _checkMaxSaleSupply(uint transferTokens)
+        internal
+    {
+        if(currentState == State.PREICO) {
+            require(totalPreICOSupply.add(transferTokens) <= MAX_PREICO_SUPPLY);
+        } else if(currentState == State.ICO) {
+            require(totalICOSupply.add(transferTokens) <= MAX_ICO_SUPPLY);
+        }
+    }
     
-    // Send money to owner
-    owner.transfer(msg.value);
-  }
-
-  /**
-   * @dev returns the number of tokens allocated to this contract
-   */
-  function tokensAvailable() constant returns (uint256) {
-    return token.balanceOf(this);
-  }
-
-  /**
-   * @notice Terminate contract and refund to owner
-   */
-  function destroy() onlyOwner {
-    // Transfer tokens back to owner
-    uint256 balance = token.balanceOf(this);
-    assert(balance > 0);
-    token.transfer(owner, balance);
-
-    // There should be no ether in the contract but just in case
-    selfdestruct(owner);
-  }
-
+     function _increaseSupply(uint _amount)
+        internal
+    {
+        if(currentState == State.PREICO) {
+            totalPreICOSupply = totalPreICOSupply.add(_amount);
+        } else if(currentState == State.ICO) {
+            totalICOSupply = totalICOSupply.add(_amount);
+        }
+    }
 }
