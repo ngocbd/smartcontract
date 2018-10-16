02d0cc1b674bb8c6809bcc303d72576ad99b4563
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:07:24 +0700

    add contract Petro

--- Petro.sol
+++ Petro.sol
@@ -1,140 +1,125 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PETRO at 0x3341b14ea28a4627cd807d704fbb85ff51239775
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Petro at 0xe964de82b9b83915ab8591bbebf1bfc4c65bb1dc
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.19;
+ 
+contract SafeMath{
+  function safeMul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-contract Token {
+  function safeDiv(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+	
+	function safeSub(uint a, uint b) internal returns (uint) {
+    	assert(b <= a);
+    	return a - b;
+  }
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
+	function safeAdd(uint a, uint b) internal returns (uint) {
+    	uint c = a + b;
+    	assert(c >= a);
+    	return c;
+  }
+    function assert(bool assertion) internal {
+    if (!assertion) assert;
+  }
 }
 
 
+contract ERC20{
 
-contract StandardToken is Token {
+ 	function totalSupply() constant returns (uint256 totalSupply) {}
+	function balanceOf(address _owner) constant returns (uint256 balance) {}
+	function transfer(address _recipient, uint256 _value) returns (bool success) {}
+	function transferFrom(address _from, address _recipient, uint256 _value) returns (bool success) {}
+	function approve(address _spender, uint256 _value) returns (bool success) {}
+	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
+	event Transfer(address indexed _from, address indexed _recipient, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
 }
 
+contract Petro is ERC20, SafeMath{
+	
+	mapping(address => uint256) balances;
 
-//name this contract whatever you'd like
-contract PETRO is StandardToken {
+	uint256 public totalSupply;
 
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
-    }
 
-    /* Public variables of the token */
+	function balanceOf(address _owner) constant public returns (uint256 balance) {
+	    return balances[_owner];
+	}
 
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+	function transfer(address _to, uint256 _value) public returns (bool success){
+	    balances[msg.sender] = safeSub(balances[msg.sender], _value);
+	    balances[_to] = safeAdd(balances[_to], _value);
+	    Transfer(msg.sender, _to, _value);
+	    return true;
+	}
 
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
+	mapping (address => mapping (address => uint256)) allowed;
 
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+	    var _allowance = allowed[_from][msg.sender];
+	    
+	    balances[_to] = safeAdd(balances[_to], _value);
+	    balances[_from] = safeSub(balances[_from], _value);
+	    allowed[_from][msg.sender] = safeSub(_allowance, _value);
+	    Transfer(_from, _to, _value);
+	    return true;
+	}
 
-    function PETRO(
-        ) {
-        balances[msg.sender] = 100000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 100000000;                        // Update total supply (100000 for example)
-        name = "PETRO";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "PTR";                               // Set the symbol for display purposes
-    }
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+	    allowed[msg.sender][_spender] = _value;
+	    Approval(msg.sender, _spender, _value);
+	    return true;
+	}
 
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+	function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+	    return allowed[_owner][_spender];
 
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
-    }
+		}
+
+	function () public payable {
+		createTokens(msg.sender);
+	}
+
+	function createTokens(address recipient) public payable {
+		if (msg.value == 0) {
+		  assert;
+		}
+
+		uint tokens = safeDiv(safeMul(msg.value, price), 1 ether);
+		totalSupply = safeAdd(totalSupply, tokens);
+
+		balances[recipient] = safeAdd(balances[recipient], tokens);
+
+		if (!owner.send(msg.value)) {
+		  assert;
+		}
+
+	}
+	string 	public name = "Petro";
+	string 	public symbol = "PTR";
+	uint 	public decimals = 8;
+	uint 	public INITIAL_SUPPLY = 10000000000000000;
+	uint256 public price;
+	address public owner;
+
+	function Petro() public {
+	  totalSupply = INITIAL_SUPPLY;
+	  balances[msg.sender] = INITIAL_SUPPLY;  // Give all of the initial tokens to the contract deployer.
+		owner 	= msg.sender;
+		price 	= 11;
+
+	}
 }
