9f47f305c2601b7209fad38b5eb6ca8c018231b8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:45:13 +0700

    add contract OraclizeAddrResolver

--- OraclizeAddrResolver.sol
+++ OraclizeAddrResolver.sol
@@ -1,28 +1,33 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OraclizeAddrResolver at 0x1d11e5eaE3112dbD44f99266872FF1D07C77DCe8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OraclizeAddrResolver at 0x1d3B2638a7cC9f2CB3D298A3DA7a90B67E5506ed
 */
 /*
-Copyright (c) 2015-2016 Oraclize srl, Thomas Bertani
+  Copyright (c) 2015-2016 Oraclize SRL
+  Copyright (c) 2016 Oraclize LTD
 */
 
 contract OraclizeAddrResolver {
 
     address public addr;
-    
+
     address owner;
-    
+
     function OraclizeAddrResolver(){
         owner = msg.sender;
     }
-    
-    
+
+    function changeOwner(address newowner){
+        if (msg.sender != owner) throw;
+        owner = newowner;
+    }
+
     function getAddress() returns (address oaddr){
         return addr;
     }
-    
+
     function setAddr(address newaddr){
         if (msg.sender != owner) throw;
         addr = newaddr;
     }
-    
+
 }
