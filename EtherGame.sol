83ed3cd8c70c1a64ca694479a21a212fbc365860
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:08:23 +0700

    add contract EtherGame

--- EtherGame.sol
+++ EtherGame.sol
@@ -1,140 +1,37 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGame at 0x1f4ee7184e1944aff86fb5ee95e247d1ea05c721
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGame at 0xaa54201cab79e4de3072ededd32ea68d8cfa2375
 */
 pragma solidity ^0.4.10;
 
 contract EtherGame 
 {
-    address Owner;
-    uint public RegCost;
-    uint public FirstLevelCost;
-    uint public SecondLevelCost;
-    uint public ParentFee;
-    
-    struct user
+    uint[] a;
+    function Test1(uint a) public constant returns(address)
     {
-        address parent;
-        uint8 level;
+        return msg.sender;
     }
-    
-    address[] ListOfUsers;
-    mapping(address=>user) public Users;
-    
-    event newuser(address User, address Parent);
-    event levelup(address User, uint Level);
-    
-    modifier OnlyOwner() 
+    function Test2(uint a) constant returns(address)
     {
-        if(msg.sender == Owner) 
-        _;
+        return msg.sender;
     }
-    
-    function EtherGame()
+    function Test3(uint b) public constant returns(uint)
     {
-        Owner = msg.sender;
-        RegCost = 0 ether;
-        FirstLevelCost = 0 ether;
-        SecondLevelCost = 0 ether;
-        ParentFee = 250;
-        Users[address(this)].parent = address(this);
-        Users[address(this)].level = 200;
-        ListOfUsers.push(address(this));
+        return a.length;
     }
-
-    function() payable {}
-    
-    function NewUser() payable
+    function Test4(uint b) constant returns(uint)
     {
-        if(msg.value < RegCost || Users[msg.sender].parent != 0) 
-            throw;
-        Users[msg.sender].parent = address(this);
-        ListOfUsers.push(msg.sender);
-        newuser(msg.sender, address(this));
+        return a.length;
     }
-    
-    function NewUser(address addr) payable
+    function Test5(uint b) external constant returns(uint)
     {
-        if(msg.value < RegCost || Users[msg.sender].parent != 0 || Users[addr].parent == 0)
-            throw;
-        if(addr != address(this))
-            addr.transfer(RegCost);
-        Users[msg.sender].parent = addr;
-        ListOfUsers.push(msg.sender);
-        newuser(msg.sender, addr);
+        return a.length;
     }
-    
-    function BuyLevel() payable
+    function Test6() constant returns(uint)
     {
-        uint Price;
-        if(Users[msg.sender].level == 0)
-            Price = FirstLevelCost;
-        else
-            Price = uint(8)**Users[msg.sender].level*SecondLevelCost/uint(5)**Users[msg.sender].level*2;
-        if(msg.value < Price || Users[msg.sender].parent == 0)
-            throw;
-        address ToTransfer = Users[msg.sender].parent;
-        uint Level = Users[msg.sender].level + 1;
-        while(Users[ToTransfer].level < Level)
-            ToTransfer = Users[ToTransfer].parent;
-        if(ToTransfer != address(this))
-        {
-            ToTransfer.transfer(Price/1000*(1000-ParentFee));
-            ToTransfer = Users[ToTransfer].parent;
-            if(ToTransfer != address(this) && ParentFee != 0)
-                ToTransfer.transfer(Price/1000*ParentFee);
-        }
-        Users[msg.sender].level++;
-        levelup(msg.sender, Level);
+        return a.length;
     }
-    
-    function TakeMoney() OnlyOwner
+    function Kill(uint a)
     {
-        Owner.transfer(this.balance);
+        selfdestruct(msg.sender);
     }
-    
-    function ChangeOwner(address NewOwner) OnlyOwner
-    {
-        Owner = NewOwner;
-    }
-    
-    function ChangeRules(uint NewRegCost, uint NewFirsLevelCost, uint NewSecondLevelCost, uint NewParentFee) OnlyOwner
-    {
-        ParentFee = NewParentFee;
-        FirstLevelCost = NewFirsLevelCost;
-        SecondLevelCost = NewSecondLevelCost;
-        RegCost = NewRegCost;
-    }
-    
-    function Kill() OnlyOwner
-    {
-        selfdestruct(Owner);
-    }
-
-    function UsersNumber() constant returns(uint)
-    {
-        return ListOfUsers.length;
-    }
-    
-    function UsersList() constant returns(address[])
-    {
-        return ListOfUsers;
-    }
-
-    function MaxChildLevel(address addr) constant returns(uint)
-    {
-        uint MaxLevel = 0;
-        uint Level;
-        address child;
-        for(uint i=0;i<ListOfUsers.length;i++)
-        {
-            child = ListOfUsers[i];
-            Level = Users[child].level;
-            while(child != address(this) && Users[child].parent != addr)
-                child = Users[child].parent;
-            if(child != address(this) && Level > MaxLevel)
-                MaxLevel = Level;
-        }
-        return MaxLevel;
-    }
-    
 }
