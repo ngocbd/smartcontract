35fcfe56a96c0662a4bffa69be1487b4a74ff21b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:47 +0700

    add contract SimpleStorage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,14 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0x6467687d8aaacd7239ee639c8808624013997b92
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0x488690160a80702e6b8e47c7d62ec2779eb5f240
 */
 contract SimpleStorage {
-  uint storedData;
+    uint storedData;
+    address storedAddress;
+    
+    event flag(uint val, address addr);
 
-  function set(uint x) {
-    storedData = x;
-  }
+    function set(uint x, address y) {
+        storedData = x;
+        storedAddress = y;
+    }
 
-  function get() constant returns (uint retVal) {
-    return storedData;
-  }
+    function get() constant returns (uint retVal, address retAddr) {
+        return (storedData, storedAddress);
+        flag(storedData, storedAddress);
+
+    }
 }
