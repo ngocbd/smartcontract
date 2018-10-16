54921c24f16e8e3d3913acce807b1a02a81602a0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:38:33 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,166 +1,84 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xda65ad3be569b69febd3dd41e8feec9ba5f8dcf1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xad43e8ba48a974e6dc5a385d87fc8cd1fe94a408
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.19;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0 uint256 c = a / b;
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+interface token {
+    function transfer(address receiver, uint amount);
 }
 
-/**
- * @title Crowdsale
- * @dev Crowdsale is a base contract for managing a token crowdsale.
- * Crowdsales have a start and end timestamps, where investors can make
- * token purchases and the crowdsale will assign them tokens based
- * on a token per ETH rate. Funds collected are forwarded 
- to a wallet
- * as they arrive.
- */
-contract token { function transfer(address receiver, uint amount){  } }
 contract Crowdsale {
-  using SafeMath for uint256;
-
-  // uint256 durationInMinutes;
-  // address where funds are collected
-  address public wallet;
-  // token address
-  address public addressOfTokenUsedAsReward;
-
-  uint256 public price = 2000;// 1 ether = 8000 BLE, this is initial you can change in code and also can change from there that UI 
-
-  token tokenReward;
-
-  // mapping (address => uint) public contributions;
-  
+    address public beneficiary;
+    uint public amountRaised;
+    token public tokenReward;
+    uint256 public soldTokensCounter;
+    uint public price = 0.000142857 ether;
+    bool public crowdsaleClosed = false;
+    bool public adminVer = false;
+    mapping(address => uint256) public balanceOf;
 
 
-  // start and end timestamps where investments are allowed (both inclusive)
-  // uint256 public startTime;
-  // uint256 public endTime;
-  // amount of raised money in wei
-  uint256 public weiRaised;
+    event GoalReached(address recipient, uint totalAmountRaised);
+    event FundTransfer(address backer, uint amount, uint price, bool isContribution);
 
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-  function Crowdsale() {
-    //You will change this to your wallet where you need the ETH 
-    wallet = 0x46361C8b8Be6006F461F2CA924627De9277A0aA4;//this must be owner address where all the funds shall go
-    // durationInMinutes = _durationInMinutes;
-    //Here will come the checksum address we got
-    addressOfTokenUsedAsReward = 0x6228c94F5960562B77D373a338AA737a0DC8315F;//this is token contract address
-
-
-    tokenReward = token(addressOfTokenUsedAsReward);
-  }
-
-  bool public started = false;
-
-  function startSale(){
-    if (msg.sender != wallet) throw;
-    started = true;
-  }
-
-  function stopSale(){
-    if(msg.sender != wallet) throw;
-    started = false;
-  }
-
-  function setPrice(uint256 _price){
-    if(msg.sender != wallet) throw;
-    price = _price;
-  }
-  function changeWallet(address _wallet){
-  	if(msg.sender != wallet) throw;
-  	wallet = _wallet;
-  }
-
-  function changeTokenReward(address _token){
-    if(msg.sender!=wallet) throw;
-    tokenReward = token(_token);
-  }
-
-  // fallback function can be used to buy tokens
-  function () payable {
-    buyTokens(msg.sender);
-  }
-
-  // low level token `purchase function
-  function buyTokens(address beneficiary) payable {
-    require(beneficiary != 0x0);
-    require(validPurchase());
-
-    uint256 weiAmount = msg.value;
-
-    if(weiAmount < 10**17) throw;
-    // if(weiAmount > 50*10**18) throw;
-
-    // calculate token amount to be sent
-    uint256 tokens = (weiAmount) * price;//weiamount * price 
-    // uint256 tokens = (weiAmount/10**(18-decimals)) * price;//weiamount * price 
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
-    
-    // if(contributions[msg.sender].add(weiAmount)>10*10**18) throw;
-    // contributions[msg.sender] = contributions[msg.sender].add(weiAmount);
-
-    tokenReward.transfer(beneficiary, tokens);
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-    forwardFunds();
-  }
-
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    // wallet.transfer(msg.value);
-    if (!wallet.send(msg.value)) {
-      throw;
+    /**
+     * Constrctor function
+     *
+     * Setup the owner
+     */
+    function Crowdsale() {
+        beneficiary = 0xA4047af02a2Fd8e6BB43Cfe8Ab25292aC52c73f4;
+        tokenReward = token(0x12AC8d8F0F48b7954bcdA736AF0576a12Dc8C387);
     }
-  }
 
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal constant returns (bool) {
-    bool withinPeriod = started;
-    bool nonZeroPurchase = msg.value != 0;
-    return withinPeriod && nonZeroPurchase;
-  }
+    modifier onlyOwner {
+        require(msg.sender == beneficiary);
+        _;
+    }
 
-  function withdrawTokens(uint256 _amount) {
-    if(msg.sender!=wallet) throw;
-    tokenReward.transfer(wallet,_amount);
-  }
+    /**
+     * Check ownership
+     */
+    function checkAdmin() onlyOwner {
+        adminVer = true;
+    }
+
+    /**
+     * Return unsold tokens to beneficiary address
+     */
+    function getUnsoldTokens(uint val_) onlyOwner {
+        tokenReward.transfer(beneficiary, val_);
+    }
+
+    /**
+     * Return unsold tokens to beneficiary address with decimals
+     */
+    function getUnsoldTokensWithDecimals(uint val_, uint dec_) onlyOwner {
+        val_ = val_ * 10 ** dec_;
+        tokenReward.transfer(beneficiary, val_);
+    }
+
+    /**
+     * Close/Open crowdsale
+     */
+    function closeCrowdsale(bool closeType) onlyOwner {
+        crowdsaleClosed = closeType;
+    }
+
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+    function () payable {
+        require(!crowdsaleClosed && msg.value <= 2 ether);                                  //1 ether is minimum to contribute                                                                
+        uint amount = msg.value;                                                           //save users eth value
+        balanceOf[msg.sender] += amount;                                                   //save users eth value in balance list 
+        amountRaised += amount;                                                            //update total amount of crowdsale
+        uint sendTokens = (amount / price) * 10 ** uint256(18);                            //calculate user's tokens
+        tokenReward.transfer(msg.sender, sendTokens);                                      //send tokens to user
+        soldTokensCounter += sendTokens;                                                   //update total sold tokens counter
+        FundTransfer(msg.sender, amount, price, true);                                     //pin transaction data in blockchain
+        if (beneficiary.send(amount)) { FundTransfer(beneficiary, amount, price, false); } //send users amount to beneficiary
+    }
 }
