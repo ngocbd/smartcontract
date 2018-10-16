ebe7c373929f462e0b4b9168c7cb77a8a4488745
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:44:31 +0700

    add contract HelloWorld

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,11 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HelloWorld at 0x9604f4d13f95f7db2479b05eb60bac3bf2cd345b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HelloWorld at 0xDD9aE24a9E442Fd7af6Cd321400Cc8243458924F
 */
 pragma solidity ^0.4.13;
+
 contract HelloWorld {
     
+    string wellcomeString = "Hello, world!";
+    
     function getData() public constant returns (string) {
-        return "Hello, world!";
+        return wellcomeString;
     }
-   
+    
+    function setData(string newData) public {
+        wellcomeString = newData;
+    }
+    
 }
