27449887b7b0a1a7f36e6f44a3559a8eeacbb3ac
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:16:19 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xc1c0ce739495bab1359ee770254c30b5a30769f9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x07dca8c16450d3197eb3b03b96f32ba05fcd52bf
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT97113' token contract
+    // 'ACT82167' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT97113
-    // Name        : ADZbuzz Religiongraph.faith Community Token
+    // Symbol      : ACT82167
+    // Name        : ADZbuzz Insurancetake.com Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT97113";
-            name = "ADZbuzz Religiongraph.faith Community Token";
+            symbol = "ACT82167";
+            name = "ADZbuzz Insurancetake.com Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
