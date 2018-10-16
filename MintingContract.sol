4b5aa4bd88814236de4f17ecfc6ccf28550373f2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:03:08 +0700

    add contract MintingContract

--- MintingContract.sol
+++ MintingContract.sol
@@ -1,135 +1,167 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintingContract at 0x2d4bEEFa9083F316cef85Ff94B6789310c97588b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintingContract at 0x9532014dadb2c980e43fe4665c86c2c0b1b4603d
 */
-contract IXaurumToken {
-    function doCoinage(address[] _coinageAddresses, uint256[] _coinageAmounts, uint256 _usdAmount, uint256 _xaurCoined, uint256 _goldBought) returns (bool) {}
+contract SafeMath {
+    
+    uint256 constant public MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+    function safeAdd(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        require(x <= MAX_UINT256 - y);
+        return x + y;
+    }
+
+    function safeSub(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        require(x >= y);
+        return x - y;
+    }
+
+    function safeMul(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        if (y == 0) {
+            return 0;
+        }
+        require(x <= (MAX_UINT256 / y));
+        return x * y;
+    }
 }
 
-contract MintingContract{
-    
-    //
-    /* Variables */
-    //
-    
-    /* Public variables */
-    address public curator;
-    address public dev;
-    address public defaultMintingAddress;
-    
-    uint256 public usdAmount;
-    uint256 public xaurCoined; 
-    uint256 public goldBought;
-    
-    /* Private variables */
-    IXaurumToken tokenContract;
-    
-    /* Events */    
-    event MintMade(uint256 usdAmount, uint256 xaurAmount, uint256 goldAmount);
+contract Owned {
+    address public owner;
+    address public newOwner;
 
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        assert(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != owner);
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnerUpdate(owner, newOwner);
+        owner = newOwner;
+        newOwner = 0x0;
+    }
+
+    event OwnerUpdate(address _prevOwner, address _newOwner);
+}
+
+
+contract Lockable is Owned {
+
+    uint256 public lockedUntilBlock;
+
+    event ContractLocked(uint256 _untilBlock, string _reason);
+
+    modifier lockAffected {
+        require(block.number > lockedUntilBlock);
+        _;
+    }
+
+    function lockFromSelf(uint256 _untilBlock, string _reason) internal {
+        lockedUntilBlock = _untilBlock;
+        ContractLocked(_untilBlock, _reason);
+    }
+
+
+    function lockUntil(uint256 _untilBlock, string _reason) onlyOwner public {
+        lockedUntilBlock = _untilBlock;
+        ContractLocked(_untilBlock, _reason);
+    }
+}
+
+
+contract ERC20PrivateInterface {
+    uint256 supply;
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract tokenRecipientInterface {
+  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
+}
+
+contract OwnedInterface {
+    address public owner;
+    address public newOwner;
+
+    modifier onlyOwner {
+        _;
+    }
+}
+
+contract ERC20TokenInterface {
+  function totalSupply() public constant returns (uint256 _totalSupply);
+  function balanceOf(address _owner) public constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) public returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+  function approve(address _spender, uint256 _value) public returns (bool success);
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+
+contract MintableTokenInterface {
+    function mint(address _to, uint256 _amount) public;
+}
+
+contract MintingContract is Owned {
     
-    //
-    /* Constructor */
-    //
+    address public tokenAddress;
+    enum state { crowdsaleMinintg, teamMinting, finished}
+
+    state public mintingState; 
+    uint public crowdsaleMintingCap;
+    uint public tokensAlreadyMinted;
     
-    function MintingContract(){
-        dev = msg.sender;
+    uint public teamTokensPercent;
+    address public teamTokenAddress;
+    uint public communityTokens;
+    uint public communityTokens2;
+    address public communityAddress;
+    
+    constructor() public {
+        crowdsaleMintingCap = 570500000 * 10**18;
+        teamTokensPercent = 27;
+        teamTokenAddress = 0xc2180bC387B7944FabE5E5e25BFaC69Af2Dc888A;
+        communityTokens = 24450000 * 10**18;
+        communityTokens2 = 5705000 * 10**18;
+        communityAddress = 0x4FAAc921781122AA61cfE59841A7669840821b86;
     }
     
-    //
-    /* Contract features */
-    //
-    
-    function doCoinage() returns (bool){
-        if (msg.sender != curator){ return false; }
-        if (usdAmount == 0 || xaurCoined == 0 || goldBought == 0){ return false; }
-        
-        address[] memory tempAddressArray = new address[](1);
-        tempAddressArray[0] = defaultMintingAddress;
-        
-        uint256[] memory tempAmountArray = new uint256[](1);
-        tempAmountArray[0] = xaurCoined;
-        
-        tokenContract.doCoinage(tempAddressArray, tempAmountArray, usdAmount, xaurCoined, goldBought);
-        
-        MintMade(usdAmount, xaurCoined, goldBought);
-        usdAmount = 0;
-        xaurCoined  = 0; 
-        goldBought = 0;
-        
-        return true;
+    function doCrowdsaleMinting(address _destination, uint _tokensToMint) onlyOwner public {
+        require(mintingState == state.crowdsaleMinintg);
+        require(tokensAlreadyMinted + _tokensToMint <= crowdsaleMintingCap);
+        MintableTokenInterface(tokenAddress).mint(_destination, _tokensToMint);
+        tokensAlreadyMinted += _tokensToMint;
     }
     
-    function setDefaultMintingAddress(address _mintingAddress) returns (bool){
-        if (msg.sender != curator){ return false; }
-        defaultMintingAddress = _mintingAddress;
-        return true;
+    function finishCrowdsaleMinting() onlyOwner public {
+        mintingState = state.teamMinting;
     }
     
-    function setUsdAmount(uint256 _usdAmount) returns (bool){
-        if (msg.sender != curator){ return false; }
-        usdAmount = _usdAmount;
-        return true;
+    function doTeamMinting() public {
+        require(mintingState == state.teamMinting);
+        uint onePercent = tokensAlreadyMinted/70;
+        MintableTokenInterface(tokenAddress).mint(communityAddress, communityTokens2);
+        MintableTokenInterface(tokenAddress).mint(teamTokenAddress, communityTokens - communityTokens2);
+        MintableTokenInterface(tokenAddress).mint(teamTokenAddress, (teamTokensPercent * onePercent));
+        mintingState = state.finished;
     }
-    
-    function getRealUsdAmount() constant returns (uint256){
-        return usdAmount / 10**8;
-    }
-    
-    function setXaurCoined(uint256 _xaurCoined) returns (bool){
-        if (msg.sender != curator){ return false; }
-        xaurCoined = _xaurCoined;
-        return true;
-    }
-    
-    function getRealXaurCoined() constant returns (uint256){
-        return xaurCoined / 10**8;
-    }
-    
-    function setGoldBought(uint256 _goldBought) returns (bool){
-        if (msg.sender != curator){ return false; }
-        goldBought = _goldBought;
-        return true;
-    }
-    
-    function getRealGoldBought() constant returns (uint256){
-        return goldBought / 10**8;
-    }
-    
-    //
-    /* Administration features */
-    //
-    
-    function setMintingCurator(address _curatorAddress) returns (uint error){
-        if (msg.sender != dev){ return 1; }
-        curator = _curatorAddress;
-        return 0;
-    }
-    
-    function setTokenContract(address _contractAddress) returns (uint error){
-        if (msg.sender != curator){ return 1; }
-        tokenContract = IXaurumToken(_contractAddress);
-        return 0;
-    }
-    
-    function killContract() returns (uint error) {
-        if (msg.sender != dev) { return 1; }
-        selfdestruct(dev);
-        return 0;
-    }
-    
-    //
-    /* Getters */
-    //
-    
-    function tokenAddress() constant returns (address tokenAddress){
-        return address(tokenContract);
-    }
-    
-    //
-    /* Other */
-    //
-    
-    function () {
-        throw;
+
+    function setTokenAddress(address _tokenAddress) onlyOwner public {
+        tokenAddress = _tokenAddress;
     }
 }
