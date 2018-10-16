20efb3e7731e2d4c7f33285fcae11f86206eb2b0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:35:23 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xf728b0ABDd7F796a79c3DE6AF7B7819418BE20Be
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x941995615e760A95a684513f5fFb12f16a343AAE
 */
 pragma solidity ^0.4.13;
 
@@ -12,14 +12,12 @@ contract MyToken {
     uint8 public decimals;
     uint256 public totalSupply;
     string public votingDescription;
-    uint256 public sellPrice;
 
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
     mapping (address => uint256) public voted;  
     mapping (address => string) public votedFor;  
-    mapping (address => uint256) public restFinish; 
 
 
     /* This generates a public event on the blockchain that will notify clients */
@@ -34,11 +32,11 @@ contract MyToken {
     
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyToken() {
-        balanceOf[msg.sender] = 3000000;              // Give the creator all initial tokens
-        totalSupply = 3000000;                        // Update total supply
-        name = 'GamityTest2';                                   // Set the name for display purposes
-        symbol = 'GMTEST2';                                     // Set the symbol for display purposes
-        decimals = 0;                                       // Amount of decimals for display purposes
+        balanceOf[msg.sender] = 500000;              // Give the creator all initial tokens
+        totalSupply = 500000;                        // Update total supply
+        name = 'GamityTest';                                   // Set the name for display purposes
+        symbol = 'GMTEST';                                     // Set the symbol for display purposes
+        decimals = 1;                                       // Amount of decimals for display purposes
     }
 
     /* Internal transfer, only can be called by this contract */
@@ -138,53 +136,7 @@ contract MyToken {
     }
     
     
-    function rest()  returns (bool success){    
-        require(balanceOf[msg.sender] >= 5000);         // checks if the sender has enough to sell
-        balanceOf[this] += 5000;                        // adds the amount to owner's balance
-        balanceOf[msg.sender] -= 5000; 
-        restFinish[msg.sender] = block.timestamp + 3 days;
-        return true; 
-    }
     
-    
-    
-    
-    function setPrice(uint256 newSellPrice){
-        require(msg.sender == 0x02A97eD35Ba18D2F3C351a1bB5bBA12f95Eb1181);
-        sellPrice = newSellPrice;
-    }
-     
-
-    function sell(uint amount) returns (uint revenue){
-        require(balanceOf[msg.sender] >= amount);         // checks if the sender has enough to sell
-        balanceOf[this] += amount;                        // adds the amount to owner's balance
-        balanceOf[msg.sender] -= amount;                  // subtracts the amount from seller's balance
-        revenue = amount * sellPrice;
-        require(msg.sender.send(revenue));                // sends ether to the seller: it's important to do this last to prevent recursion attacks
-        Transfer(msg.sender, this, amount);               // executes an event reflecting on the change
-        return revenue;                                   // ends function and returns
-    }
-    
-    function getTokens() returns (uint amount){
-        require(msg.sender == 0x02A97eD35Ba18D2F3C351a1bB5bBA12f95Eb1181);
-        require(balanceOf[this] >= amount);               // checks if it has enough to sell
-        balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
-        balanceOf[this] -= amount;                        // subtracts amount from seller's balance
-        Transfer(this, msg.sender, amount);               // execute an event reflecting the change
-        return amount;                                    // ends function and returns
-    }
-    
-    function sendEther() payable returns (uint amount){
-        require(msg.sender == 0x02A97eD35Ba18D2F3C351a1bB5bBA12f95Eb1181);
-        return amount;                                    // ends function and returns
-    }
-
-    
-    function getEther()  returns (uint amount){
-        require(msg.sender == 0x02A97eD35Ba18D2F3C351a1bB5bBA12f95Eb1181);
-        require(msg.sender.send(amount));                 // sends ether to the seller: it's important to do this last to prevent recursion attacks
-        return amount;                                  // ends function and returns
-    }
     
     
     
