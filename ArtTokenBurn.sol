d4c8cceae67f4ebfa5e8d71814b31f5826cf4a7c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:33:18 +0700

    add contract ArtTokenBurn

---
+++ ArtTokenBurn.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ArtTokenBurn at 0x7f934b1bbf2aa9d7037eaa352e38bdb1019b3536
+*/
+pragma solidity ^0.4.6;
+
+contract ArtTokenBurn {
+    /* Constructor */
+    function ArtTokenBurn() public {
+        
+    }
+}
