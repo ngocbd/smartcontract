c925757d32d821b3d6248734f566f8755195b7e5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:53:11 +0700

    add contract closeEXORImports

---
+++ closeEXORImports.sol
@@ -0,0 +1,27 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract closeEXORImports at 0x893cfdb6bd875912518842089f3a1e2d49f23cf5
+*/
+pragma solidity ^0.4.10;
+
+// After NovaExchange balance recovery, all dev privileges revoked. Contract is now fully decentralized.
+
+contract closeEXORImports   {
+
+function closeEXORImports()    {
+
+elixor elixorContract=elixor(0x898bf39cd67658bd63577fb00a2a3571daecbc53);
+
+elixorContract.lockGenesisImports();
+elixorContract.lockBalanceChanges();
+elixorContract.prohibitFreeze();
+
+}
+}
+
+contract elixor  {
+
+function lockGenesisImports() public;
+function lockBalanceChanges();
+function prohibitFreeze();
+
+}
