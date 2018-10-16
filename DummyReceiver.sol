b663af59d592617ed1ecc581853cd143ee705173
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:53:29 +0700

    add contract DummyReceiver

---
+++ DummyReceiver.sol
@@ -0,0 +1,16 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DummyReceiver at 0xa7136d7c9439dbda7f20e7c3f9173efae79b8429
+*/
+pragma solidity >=0.4.10;
+
+// Dummy Receiver to satisfy Sales contract need for 3 receivers
+contract DummyReceiver {
+
+    // callback from sale contract when the sale begins
+    function start() {
+    }
+
+    // callback from sale contract when sale ends
+    function end() {
+    }
+}
