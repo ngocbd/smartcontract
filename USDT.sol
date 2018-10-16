e73657d330b856379ff7519a3ab5cf1e0251b4e5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:42:39 +0700

    add contract USDT

--- USDT.sol
+++ USDT.sol
@@ -1,70 +1,50 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract USDT at 0x3aeae69c196d8db9a35b39c51d7cac00643ec7f1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract USDT at 0x9535dadbdb7a7e8f2b5da5910080ce7c546a6aba
 */
-pragma solidity 0.4.18;
+contract USDT {
+    /* Public variables of the token */
+    string public standard = 'USDT 1.01';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
 
-contract Token {
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    function totalSupply() constant returns (uint supply) {}
-    function balanceOf(address _owner) constant returns (uint balance) {}
-    function transfer(address _to, uint _value) returns (bool success) {}
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
-    function approve(address _spender, uint _value) returns (bool success) {}
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {}
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function USDT() {
 
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
-
-contract StandardToken is Token {
-
-    function transfer(address _to, uint _value) public returns (bool) {
-        if (balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+         initialSupply = 100000000;
+         name ="USDT";
+        decimals = 2;
+         symbol = "USDT";
+        
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+                                   
     }
 
-    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
     }
 
-    function balanceOf(address _owner) constant public returns (uint) {
-        return balances[_owner];
-    }
+   
 
-    function approve(address _spender, uint _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
+    
 
-    function allowance(address _owner, address _spender) constant public returns (uint) {
-        return allowed[_owner][_spender];
-    }
+   
 
-    mapping (address => uint) balances;
-    mapping (address => mapping (address => uint)) allowed;
-    uint public totalSupply;
-}
-
-contract USDT is StandardToken {
-
-    uint8 constant public decimals = 0;
-    uint public totalSupply = 0;
-    string constant public name = "USD Token";
-    string constant public symbol = "USDT";
-
-    function USDT() public {
-        totalSupply = 1e6;
-        balances[msg.sender] = totalSupply;
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
     }
 }
