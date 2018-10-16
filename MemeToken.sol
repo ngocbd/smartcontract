87df90f7ba3bfa354133789d49d9709decc857c3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:09:04 +0700

    add contract MemeToken

--- MemeToken.sol
+++ MemeToken.sol
@@ -1,9 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MemeToken at 0x0d623823d2aa4540f335bb926447dc582dc5bd64
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MemeToken at 0x76eec498e1502993324f7da98f0c78380e071098
 */
 pragma solidity ^0.4.18; // solhint-disable-line
-
-
 
 /// @title Interface for contracts conforming to ERC-721: Non-Fungible Tokens
 /// @author Dieter Shirley <dete@axiomzen.co> (https://github.com/dete)
@@ -20,42 +18,32 @@ contract ERC721 {
 
   event Transfer(address indexed from, address indexed to, uint256 tokenId);
   event Approval(address indexed owner, address indexed approved, uint256 tokenId);
-
-  // Optional
-  // function name() public view returns (string name);
-  // function symbol() public view returns (string symbol);
-  // function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256 tokenId);
-  // function tokenMetadata(uint256 _tokenId) public view returns (string infoUrl);
 }
 
 
 contract MemeToken is ERC721 {
-
   /*** EVENTS ***/
-
   /// @dev The Birth event is fired whenever a new meme comes into existence.
-  event Birth(uint256 tokenId, string name, address owner);
+  event Birth(uint256 tokenId, uint256 metadata, string text, address owner);
 
-  /// @dev The TokenSold event is fired whenever a token is sold.
-  event TokenSold(uint256 tokenId, uint256 oldPrice, uint256 newPrice, address prevOwner, address winner, string name);
+  /// @dev The TokenSold event is fired whenever a meme is sold.
+  event TokenSold(uint256 tokenId, uint256 oldPrice, uint256 newPrice, address prevOwner, address newOwner, uint256 metadata, string text);
 
   /// @dev Transfer event as defined in current draft of ERC721. 
   ///  ownership is assigned, including births.
   event Transfer(address from, address to, uint256 tokenId);
 
   /*** CONSTANTS ***/
-
   /// @notice Name and symbol of the non fungible token, as defined in ERC721.
   string public constant NAME = "CryptoMemes"; // solhint-disable-line
-  string public constant SYMBOL = "MemeToken"; // solhint-disable-line
+  string public constant SYMBOL = "CM"; // solhint-disable-line
 
   uint256 private startingPrice = 0.001 ether;
-  uint256 private constant PROMO_CREATION_LIMIT = 5000;
-  uint256 private firstStepLimit =  0.053613 ether;
-  uint256 private secondStepLimit = 0.564957 ether;
+  uint256 private constant PROMO_CREATION_LIMIT = 50000;
+  uint256 private firstStepLimit =  0.05 ether;
+  uint256 private secondStepLimit = 0.5 ether;
 
   /*** STORAGE ***/
-
   /// @dev A mapping from meme IDs to the address that owns them. All memes have
   ///  some valid owner address.
   mapping (uint256 => address) public memeIndexToOwner;
@@ -64,60 +52,58 @@ contract MemeToken is ERC721 {
   //  Used internally inside balanceOf() to resolve ownership count.
   mapping (address => uint256) private ownershipTokenCount;
 
-  /// @dev A mapping from MemeIDs to an address that has been approved to call
-  ///  transferFrom(). Each Meme can only have one approved address for transfer
+  /// @dev A mapping from memeIDs to an address that has been approved to call
+  ///  transferFrom(). Each meme can only have one approved address for transfer
   ///  at any time. A zero value means no approval is outstanding.
   mapping (uint256 => address) public memeIndexToApproved;
 
-  // @dev A mapping from MemeIDs to the price of the token.
+  // @dev A mapping from memeIDs to the price of the token.
   mapping (uint256 => uint256) private memeIndexToPrice;
 
-  // The addresses of the accounts (or contracts) that can execute actions within each roles.
-  address public ceoAddress;
-  address public cooAddress;
+  // The address of the account that can execute special actions.
+  // Not related to Dogecoin, just a normal Doge.
+  address public dogeAddress;
+  // Robot9000 address for automation.
+  // Not related to r9k, just a normal robot.
+  address public r9kAddress;
 
   uint256 public promoCreatedCount;
 
   /*** DATATYPES ***/
   struct Meme {
-    string name;
+    uint256 metadata;
+    string text;
   }
 
-  struct Memes {
-    uint256 Id;
-    string memeName;
-    uint256 sellingPrice;
-    address owner;
-  }
-
+  // All your memes are belong to us.
   Meme[] private memes;
 
   /*** ACCESS MODIFIERS ***/
-  /// @dev Access modifier for CEO-only functionality
-  modifier onlyCEO() {
-    require(msg.sender == ceoAddress);
+  /// @dev Access modifier for Doge functionality
+  modifier onlyDoge() {
+    require(msg.sender == dogeAddress);
     _;
   }
 
-  /// @dev Access modifier for COO-only functionality
-  modifier onlyCOO() {
-    require(msg.sender == cooAddress);
+  /// @dev Access modifier for Robot functionality
+  modifier onlyr9k() {
+    require(msg.sender == r9kAddress);
     _;
   }
 
-  /// Access modifier for contract owner only functionality
-  modifier onlyCLevel() {
+  /// @dev Access modifier for Doge and Robot functionality
+  modifier onlyDogeAndr9k() {
     require(
-      msg.sender == ceoAddress ||
-      msg.sender == cooAddress
+      msg.sender == dogeAddress ||
+      msg.sender == r9kAddress
     );
     _;
   }
 
   /*** CONSTRUCTOR ***/
   function MemeToken() public {
-    ceoAddress = msg.sender;
-    cooAddress = msg.sender;
+    dogeAddress = msg.sender;
+    r9kAddress = msg.sender;
   }
 
   /*** PUBLIC FUNCTIONS ***/
@@ -129,7 +115,8 @@ contract MemeToken is ERC721 {
   function approve(
     address _to,
     uint256 _tokenId
-  ) public {
+  ) public
+  {
     // Caller must own token.
     require(_owns(msg.sender, _tokenId));
 
@@ -145,13 +132,14 @@ contract MemeToken is ERC721 {
     return ownershipTokenCount[_owner];
   }
 
-  /// @dev Creates a new promo Meme with the given name, with given _price and assignes it to an address.
-  function createPromoMeme(address _owner, string _name, uint256 _price) public onlyCOO {
+  /// @dev Creates a new promo meme with the given metadata and text, with given _price and
+  ///  assignes it to an address.
+  function createPromoMeme(address _owner, uint256 _metadata, string _text, uint256 _price) public onlyDogeAndr9k {
     require(promoCreatedCount < PROMO_CREATION_LIMIT);
 
     address memeOwner = _owner;
     if (memeOwner == address(0)) {
-      memeOwner = cooAddress;
+      memeOwner = dogeAddress;
     }
 
     if (_price <= 0) {
@@ -159,39 +147,42 @@ contract MemeToken is ERC721 {
     }
 
     promoCreatedCount++;
-    _createMeme(_name, memeOwner, _price);
+    _createMeme(_metadata, _text, memeOwner, _price);
   }
 
-  /// @dev Creates a new Meme with the given name.
-  function createContractMeme(string _name) public onlyCOO {
-    _createMeme(_name, address(this), startingPrice);
+  /// @dev Creates a new user-generated meme with the given metadata and text, with given _price and
+  ///  assignes it to an address.
+  function createUserMeme(address _owner, uint256 _metadata, string _text, uint256 _price) public onlyDogeAndr9k {
+    address memeOwner = _owner;
+    if (memeOwner == address(0)) {
+      memeOwner = dogeAddress;
+    }
+
+    if (_price <= 0) {
+      _price = startingPrice;
+    }
+
+    _createMeme(_metadata, _text, memeOwner, _price);
+  }
+
+  /// @dev Creates a new meme with the given name.
+  function createContractMeme(uint256 _metadata, string _text) public onlyDogeAndr9k {
+    _createMeme(_metadata, _text, address(this), startingPrice);
   }
 
   /// @notice Returns all the relevant information about a specific meme.
   /// @param _tokenId The tokenId of the meme of interest.
   function getMeme(uint256 _tokenId) public view returns (
-    uint256 Id,
-    string memeName,
+    uint256 metadata,
+    string text,
     uint256 sellingPrice,
     address owner
   ) {
     Meme storage meme = memes[_tokenId];
-    Id = _tokenId;
-    memeName = meme.name;
+    metadata = meme.metadata;
+    text = meme.text;
     sellingPrice = memeIndexToPrice[_tokenId];
     owner = memeIndexToOwner[_tokenId];
-  }
-
-  /// @notice Returns all the relevant information about a specific meme.
-  /// @param _tokenIds The tokenId of the meme of interest.
-  function getMemeSellingPrices(uint256[] _tokenIds) public view returns (
-    uint256[] sellingPrices
-  ) {
-    sellingPrices = new uint256[](_tokenIds.length);
-    for(uint i=0;i<_tokenIds.length;i++){
-      sellingPrices[i]=memeIndexToPrice[_tokenIds[i]];
-    }
-
   }
 
   function implementsERC721() public pure returns (bool) {
@@ -215,18 +206,18 @@ contract MemeToken is ERC721 {
     require(owner != address(0));
   }
 
-  function payout(address _to) public onlyCLevel {
+  function payout(address _to) public onlyDoge {
     _payout(_to);
   }
 
-  // Allows someone to send ether and obtain the token
+  // Allows someone to send ether and obtain the meme
   function purchase(uint256 _tokenId) public payable {
     address oldOwner = memeIndexToOwner[_tokenId];
     address newOwner = msg.sender;
 
     uint256 sellingPrice = memeIndexToPrice[_tokenId];
 
-    // Making sure token owner is not sending to self
+    // Making sure meme owner is not sending to self
     require(oldOwner != newOwner);
 
     // Safety check to prevent against an unexpected 0x0 default.
@@ -235,29 +226,29 @@ contract MemeToken is ERC721 {
     // Making sure sent amount is greater than or equal to the sellingPrice
     require(msg.value >= sellingPrice);
 
-    uint256 payment = uint256(SafeMath.div(SafeMath.mul(sellingPrice, 94), 100));
+    uint256 payment = uint256(SafeMath.div(SafeMath.mul(sellingPrice, 97), 100));
     uint256 purchaseExcess = SafeMath.sub(msg.value, sellingPrice);
 
     // Update prices
     if (sellingPrice < firstStepLimit) {
       // first stage
-      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 200), 94);
+      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 200), 100);
     } else if (sellingPrice < secondStepLimit) {
       // second stage
-      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 120), 94);
+      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 150), 100);
     } else {
       // third stage
-      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 115), 94);
+      memeIndexToPrice[_tokenId] = SafeMath.div(SafeMath.mul(sellingPrice, 125), 100);
     }
 
     _transfer(oldOwner, newOwner, _tokenId);
 
     // Pay previous tokenOwner if owner is not contract
     if (oldOwner != address(this)) {
-      oldOwner.transfer(payment); //(1-0.06)
+      oldOwner.transfer(payment); //(1 - 0.05)
     }
 
-    TokenSold(_tokenId, sellingPrice, memeIndexToPrice[_tokenId], oldOwner, newOwner, memes[_tokenId].name);
+    TokenSold(_tokenId, sellingPrice, memeIndexToPrice[_tokenId], oldOwner, newOwner, memes[_tokenId].metadata, memes[_tokenId].text);
 
     msg.sender.transfer(purchaseExcess);
   }
@@ -266,20 +257,20 @@ contract MemeToken is ERC721 {
     return memeIndexToPrice[_tokenId];
   }
 
-  /// @dev Assigns a new address to act as the CEO. Only available to the current CEO.
-  /// @param _newCEO The address of the new CEO
-  function setCEO(address _newCEO) public onlyCEO {
-    require(_newCEO != address(0));
+  /// @dev Assigns a new address to act as Doge. Only available to the current Doge.
+  /// @param _newDoge The address of the new Doge
+  function setDoge(address _newDoge) public onlyDoge {
+    require(_newDoge != address(0));
 
-    ceoAddress = _newCEO;
+    dogeAddress = _newDoge;
   }
 
-  /// @dev Assigns a new address to act as the COO. Only available to the current COO.
-  /// @param _newCOO The address of the new COO
-  function setCOO(address _newCOO) public onlyCEO {
-    require(_newCOO != address(0));
+  /// @dev Assigns a new address to act as Robot. Only available to the current Doge.
+  /// @param _newRobot The address of the new Robot
+  function setRobot(address _newRobot) public onlyDoge {
+    require(_newRobot != address(0));
 
-    cooAddress = _newCOO;
+    r9kAddress = _newRobot;
   }
 
   /// @dev Required for ERC-721 compliance.
@@ -287,8 +278,8 @@ contract MemeToken is ERC721 {
     return SYMBOL;
   }
 
-  /// @notice Allow pre-approved user to take ownership of a token
-  /// @param _tokenId The ID of the Token that can be transferred if this call succeeds.
+  /// @notice Allow pre-approved user to take ownership of a meme
+  /// @param _tokenId The ID of the meme that can be transferred if this call succeeds.
   /// @dev Required for ERC-721 compliance.
   function takeOwnership(uint256 _tokenId) public {
     address newOwner = msg.sender;
@@ -305,7 +296,7 @@ contract MemeToken is ERC721 {
 
   /// @param _owner The owner whose meme tokens we are interested in.
   /// @dev This method MUST NEVER be called by smart contract code. First, it's fairly
-  ///  expensive (it walks the entire Memes array looking for memes belonging to owner),
+  ///  expensive (it walks the entire memes array looking for memes belonging to owner),
   ///  but it also returns a dynamic array, which is only supported for web3 calls, and
   ///  not contract-to-contract calls.
   function tokensOfOwner(address _owner) public view returns(uint256[] ownerTokens) {
@@ -315,11 +306,11 @@ contract MemeToken is ERC721 {
       return new uint256[](0);
     } else {
       uint256[] memory result = new uint256[](tokenCount);
-      uint256 totalMemes = totalSupply();
+      uint256 memeCount = totalSupply();
       uint256 resultIndex = 0;
 
       uint256 memeId;
-      for (memeId = 0; memeId <= totalMemes; memeId++) {
+      for (memeId = 0; memeId <= memeCount; memeId++) {
         if (memeIndexToOwner[memeId] == _owner) {
           result[resultIndex] = memeId;
           resultIndex++;
@@ -335,14 +326,15 @@ contract MemeToken is ERC721 {
     return memes.length;
   }
 
-  /// Owner initates the transfer of the token to another account
-  /// @param _to The address for the token to be transferred to.
-  /// @param _tokenId The ID of the Token that can be transferred if this call succeeds.
+  /// Owner initates the transfer of the meme to another account
+  /// @param _to The address for the meme to be transferred to.
+  /// @param _tokenId The ID of the meme that can be transferred if this call succeeds.
   /// @dev Required for ERC-721 compliance.
   function transfer(
     address _to,
     uint256 _tokenId
-  ) public {
+  ) public
+  {
     require(_owns(msg.sender, _tokenId));
     require(_addressNotNull(_to));
 
@@ -358,7 +350,8 @@ contract MemeToken is ERC721 {
     address _from,
     address _to,
     uint256 _tokenId
-  ) public {
+  ) public
+  {
     require(_owns(_from, _tokenId));
     require(_approved(_to, _tokenId));
     require(_addressNotNull(_to));
@@ -377,18 +370,19 @@ contract MemeToken is ERC721 {
     return memeIndexToApproved[_tokenId] == _to;
   }
 
-  /// For creating Meme
-  function _createMeme(string _name, address _owner, uint256 _price) private {
+  /// For creating a new meme
+  function _createMeme(uint256 _metadata, string _text, address _owner, uint256 _price) private {
     Meme memory _meme = Meme({
-      name: _name
+      metadata: _metadata,
+      text: _text
     });
     uint256 newMemeId = memes.push(_meme) - 1;
 
-    // It's probably never going to happen, 4 billion tokens are A LOT, but
+    // It's probably never going to happen, 2^64 memes are A LOT, but
     // let's just be 100% sure we never let this happen.
-    require(newMemeId == uint256(uint32(newMemeId)));
+    require(newMemeId == uint256(uint64(newMemeId)));
 
-    Birth(newMemeId, _name, _owner);
+    Birth(newMemeId, _metadata, _text, _owner);
 
     memeIndexToPrice[newMemeId] = _price;
 
@@ -405,13 +399,13 @@ contract MemeToken is ERC721 {
   /// For paying out balance on contract
   function _payout(address _to) private {
     if (_to == address(0)) {
-      ceoAddress.transfer(this.balance);
+      dogeAddress.transfer(this.balance);
     } else {
       _to.transfer(this.balance);
     }
   }
 
-  /// @dev Assigns ownership of a specific Meme to an address.
+  /// @dev Assigns ownership of a specific meme to an address.
   function _transfer(address _from, address _to, uint256 _tokenId) private {
     // Since the number of memes is capped to 2^32 we can't overflow this
     ownershipTokenCount[_to]++;
@@ -428,7 +422,6 @@ contract MemeToken is ERC721 {
     // Emit the transfer event.
     Transfer(_from, _to, _tokenId);
   }
-
 }
 library SafeMath {
 
