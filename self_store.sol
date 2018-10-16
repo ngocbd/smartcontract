3b6aaad775228a216c07f8bb68b9bca15c2cd69e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:45:02 +0700

    add contract self_store

--- self_store.sol
+++ self_store.sol
@@ -1,27 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract self_store at 0xf9e03d5e3d86277574cfd5f7e842f213cf88945f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract self_store at 0x0318179601a70085aeb488f178b081295b65ecc9
 */
-/* A contract to store only messages approved by owner */
 contract self_store {
 
     address owner;
 
-    uint16 public contentCount = 0;
+    uint public contentCount = 0;
     
-    event content(string datainfo); 
+    event content(string datainfo, uint indexed version);
+    modifier onlyowner { if (msg.sender == owner) _ }
     
     function self_store() public { owner = msg.sender; }
     
     ///TODO: remove in release
-    function kill() { if (msg.sender == owner) suicide(owner); }
+    function kill() onlyowner { suicide(owner); }
 
-    function add(string datainfo) {
-        if (msg.sender != owner) return;
-        contentCount++;
-        content(datainfo);
+    function flush() onlyowner {
+        owner.send(this.balance);
     }
 
-    function flush() {
-        owner.send(this.balance);
+    function add(string datainfo, uint version) onlyowner {
+        contentCount++;
+        content(datainfo, version);
     }
 }
