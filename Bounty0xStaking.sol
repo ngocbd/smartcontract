975085aa6d6a0dc4b2b14909aab58634171899f5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:03:49 +0700

    add contract Bounty0xStaking

--- Bounty0xStaking.sol
+++ Bounty0xStaking.sol
@@ -1,7 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bounty0xStaking at 0x2227275cedc6aced7bcbfb96b8a9a15ec377c3ed
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bounty0xStaking at 0xeef0ee02d4ae5520e202038112ad7e5aa6187c2b
 */
 pragma solidity ^0.4.21;
+
+
 /**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
@@ -13,6 +15,9 @@ contract ERC20Basic {
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
+
+
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -20,7 +25,11 @@ contract ERC20Basic {
  */
 contract Ownable {
   address public owner;
+
+
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
@@ -28,6 +37,7 @@ contract Ownable {
   function Ownable() public {
     owner = msg.sender;
   }
+
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -35,6 +45,7 @@ contract Ownable {
     require(msg.sender == owner);
     _;
   }
+
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
@@ -44,12 +55,19 @@ contract Ownable {
     emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
+
 }
+
+
+
+
+
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
+
   /**
   * @dev Multiplies two numbers, throws on overflow.
   */
@@ -61,6 +79,7 @@ library SafeMath {
     assert(c / a == b);
     return c;
   }
+
   /**
   * @dev Integer division of two numbers, truncating the quotient.
   */
@@ -70,6 +89,7 @@ library SafeMath {
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return a / b;
   }
+
   /**
   * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
@@ -77,6 +97,7 @@ library SafeMath {
     assert(b <= a);
     return a - b;
   }
+
   /**
   * @dev Adds two numbers, throws on overflow.
   */
@@ -86,6 +107,14 @@ library SafeMath {
     return c;
   }
 }
+
+
+
+
+
+
+
+
 /**
  * @title Pausable
  * @dev Base contract which allows children to implement an emergency stop mechanism.
@@ -93,7 +122,10 @@ library SafeMath {
 contract Pausable is Ownable {
   event Pause();
   event Unpause();
+
   bool public paused = false;
+
+
   /**
    * @dev Modifier to make a function callable only when the contract is not paused.
    */
@@ -101,6 +133,7 @@ contract Pausable is Ownable {
     require(!paused);
     _;
   }
+
   /**
    * @dev Modifier to make a function callable only when the contract is paused.
    */
@@ -108,6 +141,7 @@ contract Pausable is Ownable {
     require(paused);
     _;
   }
+
   /**
    * @dev called by the owner to pause, triggers stopped state
    */
@@ -115,6 +149,7 @@ contract Pausable is Ownable {
     paused = true;
     emit Pause();
   }
+
   /**
    * @dev called by the owner to unpause, returns to normal state
    */
@@ -123,6 +158,12 @@ contract Pausable is Ownable {
     emit Unpause();
   }
 }
+
+
+
+
+
+
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -133,36 +174,58 @@ contract ERC20 is ERC20Basic {
   function approve(address spender, uint256 value) public returns (bool);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
+
+
 contract Bounty0xStaking is Ownable, Pausable {
+
     using SafeMath for uint256;
+
     address public Bounty0xToken;
+
     mapping (address => uint) public balances;
+
     mapping (uint => mapping (address => uint)) public stakes; // mapping of submission ids to mapping of addresses that staked an amount of bounty token
+
+
     event Deposit(address depositor, uint amount, uint balance);
     event Withdraw(address depositor, uint amount, uint balance);
+
     event Stake(uint submissionId, address hunter, uint amount);
+
     event StakeReleased(uint submissionId, address from, address to, uint amount);
+
+
     constructor(address _bounty0xToken) public {
         Bounty0xToken = _bounty0xToken;
     }
+
+
     function deposit(uint _amount) public whenNotPaused {
         //remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
         require(ERC20(Bounty0xToken).transferFrom(msg.sender, this, _amount));
         balances[msg.sender] = SafeMath.add(balances[msg.sender], _amount);
+
         emit Deposit(msg.sender, _amount, balances[msg.sender]);
     }
+
     function withdraw(uint _amount) public whenNotPaused {
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
         require(ERC20(Bounty0xToken).transfer(msg.sender, _amount));
+
         emit Withdraw(msg.sender, _amount, balances[msg.sender]);
     }
+
+
     function stake(uint _submissionId, uint _amount) public whenNotPaused {
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
         stakes[_submissionId][msg.sender] = SafeMath.add(stakes[_submissionId][msg.sender], _amount);
+
         emit Stake(_submissionId, msg.sender, _amount);
     }
+
     function stakeToMany(uint[] _submissionIds, uint[] _amounts) public whenNotPaused {
         uint totalAmount = 0;
         for (uint j = 0; j < _amounts.length; j++) {
@@ -170,26 +233,36 @@ contract Bounty0xStaking is Ownable, Pausable {
         }
         require(balances[msg.sender] >= totalAmount);
         balances[msg.sender] = SafeMath.sub(balances[msg.sender], totalAmount);
+
         for (uint i = 0; i < _submissionIds.length; i++) {
             stakes[_submissionIds[i]][msg.sender] = SafeMath.add(stakes[_submissionIds[i]][msg.sender], _amounts[i]);
+
             emit Stake(_submissionIds[i], msg.sender, _amounts[i]);
         }
     }
+
+
     function releaseStake(uint _submissionId, address _from, address _to, uint _amount) public onlyOwner {
         require(stakes[_submissionId][_from] >= _amount);
+
         stakes[_submissionId][_from] = SafeMath.sub(stakes[_submissionId][_from], _amount);
         balances[_to] = SafeMath.add(balances[_to], _amount);
+
         emit StakeReleased(_submissionId, _from, _to, _amount);
     }
+
     function releaseManyStakes(uint[] _submissionIds, address[] _from, address[] _to, uint[] _amounts) public onlyOwner {
         require(_submissionIds.length == _from.length &&
                 _submissionIds.length == _to.length &&
                 _submissionIds.length == _amounts.length);
+
         for (uint i = 0; i < _submissionIds.length; i++) {
             require(stakes[_submissionIds[i]][_from[i]] >= _amounts[i]);
             stakes[_submissionIds[i]][_from[i]] = SafeMath.sub(stakes[_submissionIds[i]][_from[i]], _amounts[i]);
             balances[_to[i]] = SafeMath.add(balances[_to[i]], _amounts[i]);
+
             emit StakeReleased(_submissionIds[i], _from[i], _to[i], _amounts[i]);
         }
     }
+
 }
