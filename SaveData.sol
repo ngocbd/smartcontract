6bbc4f86976d000722e122271794da6f7d0ab7bd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:58:16 +0700

    add contract SaveData

--- SaveData.sol
+++ SaveData.sol
@@ -1,21 +1,21 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SaveData at 0xbad5ec35e643462d9125e242cf68888b8a800d1c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SaveData at 0xf34cd2fd11233df8f90646ab658b03bfea98aa92
 */
 pragma solidity ^0.4.21;
   
 contract SaveData {
-    mapping (uint => string) sign;
+    mapping (string => string) sign;
     address public owner;
-    event SetString(uint key,string types);
+    event SetString(string key,string types);
     function SaveData() public {
         owner = msg.sender;
     }
-    function setstring(uint key,string md5) public returns(string){
+    function setstring(string key,string md5) public returns(string){
         sign[key]=md5;
         return sign[key];
     }
 
-    function getString(uint key) public view returns(string){
+    function getString(string key) public view returns(string){
         return sign[key];
     }
 }
