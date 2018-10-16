4106bdb66fe8821797ac0d20221644a1f118d148
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:49:41 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x55dd50f588e07f44c72f7a205ceaf37326cbbb93
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x9bf929dcabfaefb177e959fde44a748c49bf3b63
 */
 pragma solidity ^0.4.4;
 
@@ -108,11 +108,11 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function HashnodeTestCoin() {
-        balances[msg.sender] = 21000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 21000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Digi World";                                   // Set the name for display purposes (CHANGE THIS)
+        balances[msg.sender] = 51000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 51000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "Power Minex";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "DW";                                             // Set the symbol for display purposes (CHANGE THIS)
+        symbol = "XPM";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 100;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
