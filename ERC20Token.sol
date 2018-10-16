ca5cf40ac8d6a77f7858b92a99f25e0d62d50aff
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:51:57 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x4fce19caa366f69ed6cc2793b83050e24d2c82e2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x8040d35ed6c82f75b1078cf5eb93a2cfd34b2bd8
 */
 pragma solidity ^0.4.4;
 
@@ -106,24 +106,20 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
+    string public name;                   //fancy name.
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
+    string public version = 'BRASIL; ORDEM E PROGRESSO';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
 //make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 999999999000000000000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 999999999000000000000000000;                        // Update total supply (100000 for example)
-        name = "AGRONEUM";                                   // Set the name for display purposes
+        balances[msg.sender] = 15000000000000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 15000000000000000000000000;                        // Update total supply (100000 for example)
+        name = "Bitreal";                                   // Set the name for display purposes
         decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "AGRO";                               // Set the symbol for display purposes
+        symbol = "BTR";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
