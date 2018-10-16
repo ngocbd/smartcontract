6090b7b46c37b43bb1142985ef7ec8427bc1ffb1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:39:51 +0700

    add contract GoodTimeCoin

--- GoodTimeCoin.sol
+++ GoodTimeCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GoodTimeCoin at 0x88765d79c5d26be5544ee66ed32e85000cc96753
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GoodTimeCoin at 0xaa4832b9d8e784ccf8b2aae438e67267828912f7
 */
 pragma solidity ^0.4.16;
 
@@ -20,61 +20,67 @@ contract owned {
     }
 }
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+interface tokenRecipient 
+{ 
+function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; 
+}
 
-contract GoodTimeCoin {
-   
+contract StandardToken {
+    
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
-    
+    uint8 public decimals = 18;    
     uint256 public totalSupply;
-    
+
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     event Burn(address indexed from, uint256 value);
-    
-    function GoodTimeCoin(
+
+    function StandardToken(
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
     ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;               
+        totalSupply = initialSupply * 10 ** uint256(decimals);  
+        balanceOf[msg.sender] = totalSupply;                
         name = tokenName;                                   
-        symbol = tokenSymbol;                              
+        symbol = tokenSymbol;                               
     }
-    
-    function _transfer(address _from, address _to, uint _value) internal {        
+
+    function _transfer(address _from, address _to, uint _value) {
         require(_to != 0x0);
-        require(balanceOf[_from] >= _value);       
-        require(balanceOf[_to] + _value > balanceOf[_to]);       
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];        
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);       
+        Transfer(_from, _to, _value);
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
-    
-    function transfer(address _to, uint256 _value) public {
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) 
+	{
         _transfer(msg.sender, _to, _value);
+		return true;
     }
-    
+
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        require(_value <= allowance[_from][msg.sender]);     
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
     }
-   
+
     function approve(address _spender, uint256 _value) public
         returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
-    
+
     function approveAndCall(address _spender, uint256 _value, bytes _extraData)
         public
         returns (bool success) {
@@ -84,7 +90,7 @@ contract GoodTimeCoin {
             return true;
         }
     }
-   
+    
     function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);   
         balanceOf[msg.sender] -= _value;            
@@ -92,7 +98,7 @@ contract GoodTimeCoin {
         Burn(msg.sender, _value);
         return true;
     }
-   
+
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);                
         require(_value <= allowance[_from][msg.sender]);    
@@ -104,57 +110,46 @@ contract GoodTimeCoin {
     }
 }
 
-contract StandardToken is owned, GoodTimeCoin {
+contract GoodTimeCoin is owned, StandardToken {
 
     uint256 public sellPrice;
     uint256 public buyPrice;
 
     mapping (address => bool) public frozenAccount;
-    
+
     event FrozenFunds(address target, bool frozen);
-    
-    function StandardToken(
+
+    function GoodTimeCoin(
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
-    ) GoodTimeCoin(initialSupply, tokenName, tokenSymbol) public {}
-    
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               
-        require (balanceOf[_from] >= _value);               
-        require (balanceOf[_to] + _value > balanceOf[_to]); 
-        require(!frozenAccount[_from]);                     
-        require(!frozenAccount[_to]);                       
-        balanceOf[_from] -= _value;                         
-        balanceOf[_to] += _value;                           
-        Transfer(_from, _to, _value);
-    }
-    
+    ) StandardToken(initialSupply, tokenName, tokenSymbol) public {}
+
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
         balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
         Transfer(0, this, mintedAmount);
         Transfer(this, target, mintedAmount);
     }
-   
+
     function freezeAccount(address target, bool freeze) onlyOwner public {
         frozenAccount[target] = freeze;
         FrozenFunds(target, freeze);
     }
-   
+
     function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
         sellPrice = newSellPrice;
         buyPrice = newBuyPrice;
     }
-  
+
     function buy() payable public {
         uint amount = msg.value / buyPrice;               
         _transfer(this, msg.sender, amount);              
     }
-    
+
     function sell(uint256 amount) public {
         require(this.balance >= amount * sellPrice);      
         _transfer(msg.sender, this, amount);              
-        msg.sender.transfer(amount * sellPrice);
+        msg.sender.transfer(amount * sellPrice);          
     }
 }
