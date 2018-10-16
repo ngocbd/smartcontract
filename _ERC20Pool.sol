be51003e35c96f0df96c98a717a7681b5a42610c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:15:40 +0700

    add contract _ERC20Pool

--- _ERC20Pool.sol
+++ _ERC20Pool.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract _ERC20Pool at 0xf7e05d7183c4c9d1d69d0242a0dbdd2f4ac180a8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract _ERC20Pool at 0x77741699cfdf210dd1301a564ae602243c189fc2
 */
-pragma solidity ^0.4.19; 
+pragma solidity ^0.4.23; 
 /*
 Author: Vox / 0xPool.io
 Description: This smart contract is designed to store mining pool payouts for 
@@ -16,6 +16,8 @@ Description: This smart contract is designed to store mining pool payouts for
 This contract is the 0xBTC (0xBitcoin) payout account for: http://0xpool.io 
 
 Not heard of 0xBitcoin? Head over to http://0xbitcoin.org
+
+May the Qat be with you. 
 */
 
 contract ERC20Interface {
@@ -32,7 +34,6 @@ contract ERC20Interface {
 
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-
     event Transfer(address indexed from, address indexed to, uint tokens);
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -41,14 +42,18 @@ contract ERC20Interface {
 
 
 contract _ERC20Pool {
-  address constant public bitcoinContract = 0xB6eD7644C69416d67B522e20bC294A9a9B405B31;
     
-  ERC20Interface public tokenContract = ERC20Interface(bitcoinContract);
+  using SafeMath for uint64;
 
-  address public owner = msg.sender;
-  uint32 public totalTokenSupply;
-  mapping (address => uint32) public minerTokens;
-  mapping (address => uint32) public minerTokenPayouts;
+  // 0xB6eD7644C69416d67B522e20bC294A9a9B405B31 is the 0xBitcoin Smart Contract
+  ERC20Interface public tokenContract = ERC20Interface(0xB6eD7644C69416d67B522e20bC294A9a9B405B31);
+  
+  // 0x53CE57325C126145dE454719b4931600a0BD6Fc4 is the wallet for 0xpool.io
+  address public owner = 0x53CE57325C126145dE454719b4931600a0BD6Fc4;
+  
+  uint64 public totalTokenSupply;
+  mapping (address => uint64) public minerTokens;
+  mapping (address => uint64) public minerTokenPayouts;
 
   // Modifier for important owner only functions
   modifier onlyOwner() {
@@ -57,39 +62,28 @@ contract _ERC20Pool {
   }
 
   // Require that the caller actually has tokens to withdraw.
-  modifier hasTokens(address sentFrom) {
-    require(minerTokens[sentFrom] > 0);
+  modifier hasTokens() {
+    require(minerTokens[msg.sender] > 0);
     _;
   }
 
   // Pool software updates the contract when it finds a reward
-  function addMinerTokens(uint32 totalTokensInBatch, address[] minerAddress, uint32[] minerRewardTokens) public onlyOwner {
+  function addMinerTokens(uint64 totalTokensInBatch, address[] minerAddress, uint64[] minerRewardTokens) public onlyOwner {
     totalTokenSupply += totalTokensInBatch;
     for (uint i = 0; i < minerAddress.length; i ++) {
-      minerTokens[minerAddress[i]] += minerRewardTokens[i];
+      minerTokens[minerAddress[i]] = minerTokens[minerAddress[i]].add(minerRewardTokens[i]);
     }
   }
   
   // Allow miners to withdraw their earnings from the contract. Update internal accounting.
   function withdraw() public
-    hasTokens(msg.sender) 
+    hasTokens
   {
-    uint32 amount = minerTokens[msg.sender];
+    uint64 amount = minerTokens[msg.sender];
     minerTokens[msg.sender] = 0;
-    totalTokenSupply -= amount;
-    minerTokenPayouts[msg.sender] += amount;
+    totalTokenSupply = totalTokenSupply.sub(amount);
+    minerTokenPayouts[msg.sender] = minerTokenPayouts[msg.sender].add(amount);
     tokenContract.transfer(msg.sender, amount);
-  }
-
-  // Getter function for token balance mapping.
-  function getBalance(address acc) public returns (uint32) {
-      return minerTokens[acc];
-    }
-  
-  
-  // Getter function for token payouts mapping.
-  function getPayouts(address acc) public returns (uint32) {
-      return minerTokenPayouts[acc];
   }
   
   // Fallback function, It's kind of you to send Ether, but we prefer to handle the true currency of
@@ -99,16 +93,59 @@ contract _ERC20Pool {
   }
   
   // Allow the owner to retrieve accidentally sent Ethereum
-  function withdrawEther(uint32 amount) onlyOwner {
+  function withdrawEther(uint64 amount) public onlyOwner {
     owner.transfer(amount);
   }
   
   // Allows the owner to transfer any accidentally sent ERC20 Tokens, excluding 0xBitcoin.
   function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-    if(tokenAddress == bitcoinContract ){ 
+    if(tokenAddress == 0xB6eD7644C69416d67B522e20bC294A9a9B405B31 ){ 
         revert(); 
     }
     return ERC20Interface(tokenAddress).transfer(owner, tokens);
   }
+  
+}
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    /**
+     * @dev Multiplies two numbers, throws on overflow.
+     */
+     function mul(uint64 a, uint64 b) internal pure returns (uint64) {
+     if (a == 0) {
+     return 0;
+     }
+     uint64 c = a * b;
+     assert(c / a == b);
+     return c;
+     }
+    /**
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
+     function div(uint64 a, uint64 b) internal pure returns (uint64) {
+     // assert(b > 0); // Solidity automatically throws when dividing by 0
+     uint64 c = a / b;
+     // assert(a == b * c + a % b); // There is no case in which this doesn’t hold
+     return c;
+     }
+    /**
+     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+     */
+     function sub(uint64 a, uint64 b) internal pure returns (uint64) {
+     assert(b <= a);
+     uint64 c = a - b;
+     return c;
+     }
+    /**
+     * @dev Adds two numbers, throws on overflow.
+     */
+     function add(uint64 a, uint64 b) internal pure returns (uint64) {
+     uint64 c = a + b;
+     assert(c >= a);
+     return c;
+     }
 }
