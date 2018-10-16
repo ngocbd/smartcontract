1a8b71567a43e4fe7b903f4f5c8a7503dd8d6f64
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:35:15 +0700

    add contract SafeMathLibExt

--- SafeMathLibExt.sol
+++ SafeMathLibExt.sol
@@ -1,12 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0x2Bd9370e22498B3A08771d5eA5Bda84b2c45A43a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0x1eA02560Ac6F1391f6deCcc0c9B46C1Ee0fc931e
 */
+// Created using ICO Wizard https://github.com/oraclesorg/ico-wizard by Oracles Network 
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
  *
  * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
  */
+
 pragma solidity ^0.4.6;
+
 /**
  * Safe unsigned safe math.
  *
@@ -18,24 +21,29 @@ pragma solidity ^0.4.6;
  *
  */
 library SafeMathLibExt {
+
   function times(uint a, uint b) returns (uint) {
     uint c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
+
   function divides(uint a, uint b) returns (uint) {
     assert(b > 0);
     uint c = a / b;
     assert(a == b * c + a % b);
     return c;
   }
+
   function minus(uint a, uint b) returns (uint) {
     assert(b <= a);
     return a - b;
   }
+
   function plus(uint a, uint b) returns (uint) {
     uint c = a + b;
     assert(c>=a);
     return c;
   }
+
 }
