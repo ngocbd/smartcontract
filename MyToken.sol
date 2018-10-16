ef5a61b47e900685ba23e0658513e3a376254d3b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:20:37 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,23 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x938E6Bfb6B0030ca755ad9f8904200cFDF8da07d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0xbB66419855FA71e363ebbD51314156e4677c494b
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.8;
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
 contract MyToken {
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(
-        uint256 initialSupply
-        ) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-    }
-
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-    }
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
 }
