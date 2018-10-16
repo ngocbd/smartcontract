a9d8f4b707a54f9da5f213719fc5068e581f3e08
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:49:11 +0700

    add contract WrapperLock

--- WrapperLock.sol
+++ WrapperLock.sol
@@ -1,66 +1,39 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WrapperLock at 0xf4988362b291724d5084049686d96bac3f1efb10
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WrapperLock at 0x328b6e9d6dc8908a860623a037f80ec5f501f4dc
 */
+pragma solidity ^0.4.22;
+
+
+
 /**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public constant returns (uint256);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract ERC20Interface {
-    function totalSupply() constant returns (uint supply) {}
-    function balanceOf(address _owner) constant returns (uint balance) {}
-    function transfer(address _to, uint _value) returns (bool success) {}
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
-    function approve(address _spender, uint _value) returns (bool success) {}
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {}
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
-
-
-
 /**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
  */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-
 contract BasicToken is ERC20Basic {
   using SafeMath for uint256;
 
   mapping(address => uint256) balances;
+
+  uint256 totalSupply_;
+
+  /**
+  * @dev total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
 
   /**
   * @dev transfer token for a specified address
@@ -83,87 +56,221 @@ contract BasicToken is ERC20Basic {
   * @param _owner The address to query the the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) public constant returns (uint256 balance) {
+  function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
   }
 
 }
 
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 
-contract WrapperLock is BasicToken {
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
-  address ZEROEX_PROXY = 0x8da0d80f5007ef1e431dd2127178d224e32c2ef4;
-  address ETHFINEX;
-
-  string public name;
-  string public symbol;
-  uint public decimals;
-  address public originalToken;
-
-  mapping (address => uint) public depositLock;
-
-  function WrapperLock(address _originalToken, string _name, string _symbol, uint _decimals) {
-    originalToken = _originalToken;
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
-    ETHFINEX = 0x5A2143B894C9E8d8DFe2A0e8B80d7DB2689fC382;
-  }
-
-  function deposit(uint _value, uint _forTime) returns (bool success) {
-    require (_forTime >= 1);
-    require (now + _forTime * 1 hours >= depositLock[msg.sender]);
-    success = ERC20Interface(originalToken).transferFrom(msg.sender, this, _value);
-    if(success) {
-      balances[msg.sender] = balances[msg.sender].add(_value);
-      depositLock[msg.sender] = now + _forTime * 1 hours;
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
     }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
   }
 
-  function withdraw(uint8 v, bytes32 r, bytes32 s, uint _value, uint signatureValidUntilBlock) returns (bool success) {
-    require(balanceOf(msg.sender) >= _value);
-    if (now > depositLock[msg.sender]){
-      balances[msg.sender] = balances[msg.sender].sub(_value);
-      success = ERC20Interface(originalToken).transfer(msg.sender, _value);
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+/*
+
+Copyright Will Harborne (Ethfinex) 2017
+
+*/
+
+contract WrapperLock is BasicToken, Ownable {
+    using SafeMath for uint256;
+
+
+    address public TRANSFER_PROXY;
+    mapping (address => bool) private isSigner;
+
+    string public name;
+    string public symbol;
+    uint public decimals;
+    address public originalToken;
+
+    mapping (address => uint256) public depositLock;
+    mapping (address => uint256) public balances;
+
+    function WrapperLock(address _originalToken, string _name, string _symbol, uint _decimals, address _transferProxy) {
+        originalToken = _originalToken;
+        TRANSFER_PROXY = _transferProxy;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        isSigner[msg.sender] = true;
     }
-    else {
-      require(block.number < signatureValidUntilBlock);
-      require(isValidSignature(ETHFINEX, keccak256(msg.sender, _value, signatureValidUntilBlock), v, r, s));
-      balances[msg.sender] = balances[msg.sender].sub(_value);
-      success = ERC20Interface(originalToken).transfer(msg.sender, _value);
+
+    function deposit(uint _value, uint _forTime) public returns (bool success) {
+        require(_forTime >= 1);
+        require(now + _forTime * 1 hours >= depositLock[msg.sender]);
+        require(ERC20(originalToken).transferFrom(msg.sender, this, _value));
+        balances[msg.sender] = balances[msg.sender].add(_value);
+        depositLock[msg.sender] = now + _forTime * 1 hours;
+        return true;
     }
-  }
 
-  function transferFrom(address _from, address _to, uint _value) {
-    assert(msg.sender == ZEROEX_PROXY);
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    Transfer(_from, _to, _value);
-  }
-
-  function allowance(address owner, address spender) returns (uint) {
-    if(spender == ZEROEX_PROXY) {
-      return 2**256 - 1;
+    function withdraw(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        uint _value,
+        uint signatureValidUntilBlock
+    )
+        public
+        returns
+        (bool success)
+    {
+        require(balanceOf(msg.sender) >= _value);
+        if (now > depositLock[msg.sender]) {
+            balances[msg.sender] = balances[msg.sender].sub(_value);
+            success = ERC20(originalToken).transfer(msg.sender, _value);
+        } else {
+            require(block.number < signatureValidUntilBlock);
+            require(isValidSignature(keccak256(msg.sender, address(this), signatureValidUntilBlock), v, r, s));
+            balances[msg.sender] = balances[msg.sender].sub(_value);
+            success = ERC20(originalToken).transfer(msg.sender, _value);
+        }
+        require(success);
     }
-  }
 
-  function isValidSignature(
-        address signer,
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        return false;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public {
+        require(_to == owner || _from == owner);
+        assert(msg.sender == TRANSFER_PROXY);
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        Transfer(_from, _to, _value);
+    }
+
+    function allowance(address _owner, address _spender) public constant returns (uint) {
+        if (_spender == TRANSFER_PROXY) {
+            return 2**256 - 1;
+        }
+    }
+
+    function balanceOf(address _owner) public constant returns (uint256) {
+        return balances[_owner];
+    }
+
+    function isValidSignature(
         bytes32 hash,
         uint8 v,
         bytes32 r,
-        bytes32 s)
+        bytes32 s
+    )
         public
         constant
         returns (bool)
     {
-        return signer == ecrecover(
+        return isSigner[ecrecover(
             keccak256("\x19Ethereum Signed Message:\n32", hash),
             v,
             r,
             s
-        );
+        )];
+    }
+
+    function addSigner(address _newSigner) public {
+        require(isSigner[msg.sender]);
+        isSigner[_newSigner] = true;
+    }
+
+    function keccak(address _sender, address _wrapper, uint _validTill) public constant returns(bytes32) {
+        return keccak256(_sender, _wrapper, _validTill);
     }
 
 }
