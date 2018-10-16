bdda9b142411147de6fd592d14f45030dff849c2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:13:32 +0700

    add contract Whitelist

--- WhiteList.sol
+++ WhiteList.sol
@@ -1,184 +1,62 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WhiteList at 0xcb46b40def144e355064d729d0e72dfea80383a9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Whitelist at 0xf85f4bb59c5fe860485d9260aef64ed5d2a12fef
 */
-pragma solidity 0.4.18;
+// compiler: 0.4.19+commit.c4cbbb05.Emscripten.clang
+pragma solidity ^0.4.19;
 
-contract PermissionGroups {
-
-    address public admin;
-    address public pendingAdmin;
-    mapping(address=>bool) internal operators;
-    mapping(address=>bool) internal alerters;
-    address[] internal operatorsGroup;
-    address[] internal alertersGroup;
-
-    function PermissionGroups() public {
-        admin = msg.sender;
-    }
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin);
-        _;
-    }
-
-    modifier onlyOperator() {
-        require(operators[msg.sender]);
-        _;
-    }
-
-    modifier onlyAlerter() {
-        require(alerters[msg.sender]);
-        _;
-    }
-
-    function getOperators () external view returns(address[]) {
-        return operatorsGroup;
-    }
-
-    function getAlerters () external view returns(address[]) {
-        return alertersGroup;
-    }
-
-    event TransferAdminPending(address pendingAdmin);
-
-    /**
-     * @dev Allows the current admin to set the pendingAdmin address.
-     * @param newAdmin The address to transfer ownership to.
-     */
-    function transferAdmin(address newAdmin) public onlyAdmin {
-        require(newAdmin != address(0));
-        TransferAdminPending(pendingAdmin);
-        pendingAdmin = newAdmin;
-    }
-
-    event AdminClaimed( address newAdmin, address previousAdmin);
-
-    /**
-     * @dev Allows the pendingAdmin address to finalize the change admin process.
-     */
-    function claimAdmin() public {
-        require(pendingAdmin == msg.sender);
-        AdminClaimed(pendingAdmin, admin);
-        admin = pendingAdmin;
-        pendingAdmin = address(0);
-    }
-
-    event AlerterAdded (address newAlerter, bool isAdd);
-
-    function addAlerter(address newAlerter) public onlyAdmin {
-        require(!alerters[newAlerter]); // prevent duplicates.
-        AlerterAdded(newAlerter, true);
-        alerters[newAlerter] = true;
-        alertersGroup.push(newAlerter);
-    }
-
-    function removeAlerter (address alerter) public onlyAdmin {
-        require(alerters[alerter]);
-        alerters[alerter] = false;
-
-        for (uint i = 0; i < alertersGroup.length; ++i) {
-            if (alertersGroup[i] == alerter) {
-                alertersGroup[i] = alertersGroup[alertersGroup.length - 1];
-                alertersGroup.length--;
-                AlerterAdded(alerter, false);
-                break;
-            }
-        }
-    }
-
-    event OperatorAdded(address newOperator, bool isAdd);
-
-    function addOperator(address newOperator) public onlyAdmin {
-        require(!operators[newOperator]); // prevent duplicates.
-        OperatorAdded(newOperator, true);
-        operators[newOperator] = true;
-        operatorsGroup.push(newOperator);
-    }
-
-    function removeOperator (address operator) public onlyAdmin {
-        require(operators[operator]);
-        operators[operator] = false;
-
-        for (uint i = 0; i < operatorsGroup.length; ++i) {
-            if (operatorsGroup[i] == operator) {
-                operatorsGroup[i] = operatorsGroup[operatorsGroup.length - 1];
-                operatorsGroup.length -= 1;
-                OperatorAdded(operator, false);
-                break;
-            }
-        }
-    }
+contract owned {
+  address public owner;
+  function owned() public { owner = msg.sender; }
+  function changeOwner( address newowner ) public onlyOwner {owner = newowner;}
+  function closedown() public onlyOwner { selfdestruct(owner); }
+  modifier onlyOwner {
+    if (msg.sender != owner) { revert(); }
+    _;
+  }
 }
 
-interface ERC20 {
-    function totalSupply() public view returns (uint supply);
-    function balanceOf(address _owner) public view returns (uint balance);
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
-    function decimals() public view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
+contract Whitelist is owned {
 
-contract Withdrawable is PermissionGroups {
+  address[] members_;
 
-    event TokenWithdraw(ERC20 token, uint amount, address sendTo);
+  function Whitelist() public {}
+  function() public payable { revert(); }
 
-    /**
-     * @dev Withdraw all ERC20 compatible tokens
-     * @param token ERC20 The address of the token contract
-     */
-    function withdrawToken(ERC20 token, uint amount, address sendTo) external onlyAdmin {
-        require(token.transfer(sendTo, amount));
-        TokenWithdraw(token, amount, sendTo);
-    }
+  function count() public constant returns (uint) {
+    return members_.length;
+  }
 
-    event EtherWithdraw(uint amount, address sendTo);
+  function memberAt( uint ix ) public constant returns (address) {
+    return members_[ix];
+  }
 
-    /**
-     * @dev Withdraw Ethers
-     */
-    function withdrawEther(uint amount, address sendTo) external onlyAdmin {
-        sendTo.transfer(amount);
-        EtherWithdraw(amount, sendTo);
-    }
-}
+  function setMembers( address[] mbrs ) onlyOwner public {
+    members_ = mbrs;
+  }
 
-contract WhiteList is Withdrawable {
+  function add( address member ) onlyOwner public {
+    int ix = toIndex( member );
+    if (-1 == ix) members_.push( member );
+  }
 
-    uint public weiPerSgd; // amount of weis in 1 singapore dollar
-    mapping (address=>uint) public userCategory; // each user has a category defining cap on trade. 0 for standard.
-    mapping (uint=>uint)    public categoryCap;  // will define cap on trade amount per category in singapore Dollar.
+  function remove( address member ) onlyOwner public
+  {
+    int ix = toIndex( member );
+    require( int(-1) != ix );
 
-    function WhiteList(address _admin) public {
-        require(_admin != address(0));
-        admin = _admin;
-    }
+    // deletion leaves a gap - shuffle higher elements down one
+    for ( uint jx = uint(ix); jx < members_.length - 1; jx++)
+      members_[jx] = members_[jx+1];
 
-    function getUserCapInWei(address user) external view returns (uint userCapWei) {
-        uint category = userCategory[user];
-        return (categoryCap[category] * weiPerSgd);
-    }
+    delete members_[members_.length - 1];
+    members_.length--;
+  }
 
-    event UserCategorySet(address user, uint category);
+  function toIndex( address who ) public constant returns (int)
+  {
+    for( uint ix = 0; ix < members_.length; ix++ )
+      if (members_[ix] == who) return int(ix);
 
-    function setUserCategory(address user, uint category) public onlyOperator {
-        userCategory[user] = category;
-        UserCategorySet(user, category);
-    }
-
-    event CategoryCapSet (uint category, uint sgdCap);
-
-    function setCategoryCap(uint category, uint sgdCap) public onlyOperator {
-        categoryCap[category] = sgdCap;
-        CategoryCapSet(category, sgdCap);
-    }
-
-    event SgdToWeiRateSet (uint rate);
-
-    function setSgdToEthRate(uint _sgdToWeiRate) public onlyOperator {
-        weiPerSgd = _sgdToWeiRate;
-        SgdToWeiRateSet(_sgdToWeiRate);
-    }
+    return int(-1);
+  }
 }
