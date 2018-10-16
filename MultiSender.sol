ca30b2e3611c0dabfe41747d33f7a135a08c1037
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 04:48:25 +0700

    add contract MultiSender

--- MultiSender.sol
+++ MultiSender.sol
@@ -1,30 +1,16 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSender at 0x0d3094d335e3986268d7d144de9106a01aaa313c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiSender at 0x67af54d9e20fe6f9dd2dfe0449ed65f97a3638de
 */
-contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
-}
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
+pragma solidity ^0.4.24;
 
 contract MultiSender {
-
-    function multisend(address _tokenAddr, address[] dests, uint256[] values)
-    returns (uint256) {
-        uint256 i = 0;
-        while (i < dests.length) {
-           ERC20(_tokenAddr).transfer(dests[i], values[i]);
-           i += 1;
+    function multiSend(uint256 amount, address[] addresses) public returns (bool) {
+        for (uint i = 0; i < addresses.length; i++) {
+            addresses[i].transfer(amount);
         }
-        return(i);
+    }
+
+    function () public payable {
+        
     }
 }
