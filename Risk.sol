0cdc3d2d3424ae49685f910ad191805cc2de668a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:16:45 +0700

    add contract Risk

--- Risk.sol
+++ Risk.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Risk at 0x1327c3993c24de9a821b2e512817a2668185d878
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Risk at 0x608d00b5e4ad0b4108f74163606ea22c457833fe
 */
 pragma solidity ^0.4.11;
 
@@ -12,9 +12,13 @@ contract Risk
     uint256 totalmoney=0;
     uint256 lastR=3;
     address lastgameendWinner=address(0);   
-    uint8 winnerLimit=20;
+    uint8 winnerLimit=50;
     
-    address[10] winnerloser; // first 5 represents winner last 5 loser
+    address[15] winnerloser; // first 5 represents attacker last 5 defender
+    //uint[5] winnerloserscore; //  attaker wins 2 attacker loses
+    
+    event attackhappened (address attacker, address defender);
+    event buyhappened (address buyer, uint countryID);
 
     function Risk() 
     {
@@ -31,6 +35,8 @@ contract Risk
         ownerofCountry[countryID]=msg.sender;
         playerList.push(msg.sender);
         
+        buyhappened(msg.sender,countryID);
+        
         return true;
     }
     
@@ -45,8 +51,52 @@ contract Risk
         
         uint a=playerCountries[attacker].length;
         uint b=playerCountries[defender].length;
+        
+        if(a<=1)
+            a=1;
+        else if(a<=4)
+            a=2;
+        else if(a<=9)
+            a=3;
+        else if(a<=16)
+            a=4;
+        else if(a<=25)
+            a=5;
+        else if(a<=36)
+            a=6;
+        else if(a<=49)
+            a=7;
+        else if(a<=64)
+            a=8;
+        else if(a<=81)
+            a=9;
+        else
+            a=10;
+        
+        if(b<=1)
+            b=1;
+        else if(b<=4)
+            b=2;
+        else if(b<=9)
+            b=3;
+        else if(b<=16)
+            b=4;
+        else if(b<=25)
+            b=5;
+        else if(b<=36)
+            b=6;
+        else if(b<=49)
+            b=7;
+        else if(b<=64)
+            b=8;
+        else if(b<=81)
+            b=9;
+        else
+            b=10;
 
-        for(uint256 i=9;i>=6;i--)
+        for(uint256 i=14;i>=11;i--)
+            winnerloser[i]=winnerloser[i-1];
+        for(i=9;i>=6;i--)
             winnerloser[i]=winnerloser[i-1];
         for(i=4;i>=1;i--)
             winnerloser[i]=winnerloser[i-1];
@@ -64,6 +114,7 @@ contract Risk
             playerCountries[defender].length=0;
             winnerloser[0]=attacker;
             winnerloser[5]=defender;
+            winnerloser[10]=1; //attacker wins
         }
         else //defender win
         {
@@ -74,9 +125,11 @@ contract Risk
                 ownerofCountry[playerCountries[attacker][i]]=defender;
             }
             playerCountries[attacker].length=0;
-            winnerloser[0]=defender;
-            winnerloser[5]=attacker;
+            winnerloser[0]=attacker;
+            winnerloser[5]=defender;
+            winnerloser[10]=2; //attacker loses
         }
+        attackhappened(attacker,defender);
         isGameEnd();
     }
     function isGameEnd()
@@ -138,7 +191,7 @@ contract Risk
     {
         return lastR;
     }
-    function getwinnerloser() constant returns(address[10])
+    function getwinnerloser() constant returns(address[15])
     {
         return winnerloser;
     }
