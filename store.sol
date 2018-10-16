47e144b6b1d47c040706d1ac2d648fd3800e5ac3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:54:32 +0700

    add contract store

--- store.sol
+++ store.sol
@@ -1,28 +1,28 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract store at 0x1c94f41ab9cd8faa8af45ae7827bf5a8f891f4cd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract store at 0x627917e47cd111382ddf7fb1dc140d2daf8876b3
 */
-/* A contract to store a list of messages. Obtainable as events. */
-
-contract store {
-
+contract mortal {
+    /* Define variable owner of the type address*/
     address owner;
+
+    /* this function is executed at initialization and sets the owner of the contract */
+    function mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) suicide(owner); }
+}
+
+contract store is mortal {
 
     uint16 public contentCount = 0;
     
-    event content(string datainfo, address sender, uint payment);
-    modifier onlyowner { if (msg.sender == owner) _ } 
+    event content(string datainfo); 
     
-    function store() public { owner = msg.sender; }
-    
-    ///TODO: remove in release
-    function kill() onlyowner { suicide(owner); }
-
-    function flush() onlyowner {
-        owner.send(this.balance);
+    function store() public {
     }
 
     function add(string datainfo) {
         contentCount++;
-        content(datainfo, msg.sender, msg.value);
+        content(datainfo);
     }
 }
