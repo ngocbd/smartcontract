f761ddee9a62fd4fa3ff6a80e657f6fe23d2eb67
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:48:42 +0700

    add contract TheRichestMan

---
+++ TheRichestMan.sol
@@ -0,0 +1,46 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TheRichestMan at 0xe54420ef4ecda25e08b82a71f72e043f83d7d808
+*/
+contract TheRichestMan {
+    address owner;
+
+    address public theRichest;
+    uint public treasure=0;
+    uint public withdrawDate=0;
+
+    function TheRichestMan(address _owner)
+    {
+        owner=_owner;
+    }
+
+    function () public payable{
+        require(treasure < msg.value);
+        treasure = msg.value;
+        withdrawDate = now + 2 days;
+        theRichest = msg.sender;
+    }
+
+    function withdraw() public{
+        require(now >= withdrawDate);
+        require(msg.sender == theRichest);
+
+        //Reset game
+        theRichest = 0;
+        treasure = 0;
+
+        //taking my 1% from the total prize.
+        owner.transfer(this.balance/100);
+        
+        //reward
+        msg.sender.transfer(this.balance);
+    }
+
+	// in case of long idling
+	function kill() public
+	{
+		require(msg.sender==owner);
+	        require(now >= withdrawDate);
+		owner.transfer(this.balance/100);
+		suicide(theRichest);
+	}
+}
