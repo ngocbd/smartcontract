23a2c8cd4e7a16d1e1f281c6d6ce2d4f4d846b7e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:00:02 +0700

    add contract AiraEtherFunds

--- AiraEtherFunds.sol
+++ AiraEtherFunds.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AiraEtherFunds at 0xa3ce9fa0f6b6649e40bc5146082661d5f0ed5d7a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AiraEtherFunds at 0xaaab51de73631114ea24ba4c0b3929a60e5d2413
 */
 pragma solidity ^0.4.2;
 /**
@@ -29,20 +29,9 @@ contract Owned {
     modifier onlyOwner { if (msg.sender != owner) throw; _; }
 }
 /**
- * @title Contract for objects that can be morder
- */
-contract Mortal is Owned {
-    /**
-     * @dev Destroy contract and scrub a data
-     * @notice Only owner can kill me
-     */
-    function kill() onlyOwner
-    { suicide(owner); }
-}
-/**
  * @title Token contract represents any asset in digital economy
  */
-contract Token is Mortal {
+contract Token is Owned {
     event Transfer(address indexed _from,  address indexed _to,      uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
@@ -180,82 +169,20 @@ contract TokenEther is Token {
         balanceOf[msg.sender] += msg.value;
         totalSupply           += msg.value;
     }
-    
-    /**
-     * @dev By security issues token that holds ethers can not be killed
-     */
-    function kill() onlyOwner { throw; }
 }
-
-
-//sol Registrar
-// Simple global registrar.
-// @authors:
-//   Gav Wood <g@ethdev.com>
-
-contract Registrar {
-	event Changed(string indexed name);
-
-	function owner(string _name) constant returns (address o_owner);
-	function addr(string _name) constant returns (address o_address);
-	function subRegistrar(string _name) constant returns (address o_subRegistrar);
-	function content(string _name) constant returns (bytes32 o_content);
-}
-
-//sol OwnedRegistrar
-// Global registrar with single authoritative owner.
-// @authors:
-//   Gav Wood <g@ethdev.com>
-
-contract AiraRegistrarService is Registrar, Mortal {
-	struct Record {
-		address addr;
-		address subRegistrar;
-		bytes32 content;
-	}
-	
-    function owner(string _name) constant returns (address o_owner)
-    { return 0; }
-
-	function disown(string _name) onlyOwner {
-		delete m_toRecord[_name];
-		Changed(_name);
-	}
-
-	function setAddr(string _name, address _a) onlyOwner {
-		m_toRecord[_name].addr = _a;
-		Changed(_name);
-	}
-	function setSubRegistrar(string _name, address _registrar) onlyOwner {
-		m_toRecord[_name].subRegistrar = _registrar;
-		Changed(_name);
-	}
-	function setContent(string _name, bytes32 _content) onlyOwner {
-		m_toRecord[_name].content = _content;
-		Changed(_name);
-	}
-	function record(string _name) constant returns (address o_addr, address o_subRegistrar, bytes32 o_content) {
-		o_addr = m_toRecord[_name].addr;
-		o_subRegistrar = m_toRecord[_name].subRegistrar;
-		o_content = m_toRecord[_name].content;
-	}
-	function addr(string _name) constant returns (address) { return m_toRecord[_name].addr; }
-	function subRegistrar(string _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
-	function content(string _name) constant returns (bytes32) { return m_toRecord[_name].content; }
-
-	mapping (string => Record) m_toRecord;
-}
-
 contract AiraEtherFunds is TokenEther {
-    function AiraEtherFunds(address _bot_reg, string _name, string _symbol)
-            TokenEther(_name, _symbol) {
-        reg = AiraRegistrarService(_bot_reg);
-    }
+    function AiraEtherFunds(string _name, string _symbol) TokenEther(_name, _symbol) {}
 
     /**
      * @dev Event spawned when activation request received
      */
     event ActivationRequest(address indexed sender, bytes32 indexed code);
+
+    /**
+     * @dev String to bytes32 conversion helper
+     */
+    function stringToBytes32(string memory source) constant returns (bytes32 result)
+    { assembly { result := mload(add(source, 32)) } }
 
     // Balance limit
     uint public limit;
@@ -268,6 +195,14 @@ contract AiraEtherFunds is TokenEther {
     
     function setFee(uint _fee) onlyOwner
     { fee = _fee; }
+
+    // AiraEtherBot
+    address public bot;
+
+    function setBot(address _bot) onlyOwner
+    { bot = _bot; }
+
+    modifier onlyBot { if (msg.sender != bot) throw; _; }
 
     /**
      * @dev Refill balance and activate it by code
@@ -299,12 +234,6 @@ contract AiraEtherFunds is TokenEther {
     }
 
     /**
-     * @dev String to bytes32 conversion helper
-     */
-    function stringToBytes32(string memory source) constant returns (bytes32 result)
-    { assembly { result := mload(add(source, 32)) } }
-
-    /**
      * @dev This is the way to refill your token balance by ethers
      */
     function refill() payable returns (bool) {
@@ -314,20 +243,6 @@ contract AiraEtherFunds is TokenEther {
         // Refill
         balanceOf[msg.sender] += msg.value;
         totalSupply           += msg.value;
-        return true;
-    }
-
-    /**
-     * @dev This is the way to refill token balance by ethers
-     * @param _dest is destination address
-     */
-    function refill(address _dest) payable returns (bool) {
-        // Throw when over limit
-        if (balanceOf[_dest] + msg.value > limit) throw;
-
-        // Refill
-        balanceOf[_dest] += msg.value;
-        totalSupply      += msg.value;
         return true;
     }
 
@@ -345,43 +260,31 @@ contract AiraEtherFunds is TokenEther {
     }
 
     /**
-     * @dev Outgoing transfer (send) with allowance
+     * @dev Internal transfer for AIRA
      * @param _from source address
      * @param _to destination address
      * @param _value amount of token values to send 
      */
-    function sendFrom(address _from, address _to, uint _value) {
-        var avail = allowance[_from][msg.sender]
-                  > balanceOf[_from] ? balanceOf[_from]
-                                     : allowance[_from][msg.sender];
-        if (avail >= _value) {
-            allowance[_from][msg.sender] -= _value;
-            balanceOf[_from]             -= _value;
-            totalSupply                  -= _value;
-            if (!_to.send(_value)) throw;
+    function airaTransfer(address _from, address _to, uint _value) onlyBot {
+        if (balanceOf[_from] >= _value) {
+            balanceOf[_from] -= _value;
+            balanceOf[_to]   += _value;
+            Transfer(_from, _to, _value);
         }
     }
 
-    AiraRegistrarService public reg;
-    modifier onlySecure { if (msg.sender != reg.addr("AiraSecure")) throw; _; }
-
     /**
-     * @dev Increase approved token values for AiraEthBot
-     * @param _client is a client address
-     * @param _value is amount of tokens
+     * @dev Outgoing transfer for AIRA
+     * @param _from source address
+     * @param _to destination address
+     * @param _value amount of token values to send 
      */
-    function secureApprove(address _client, uint _value) onlySecure {
-        var ethBot = reg.addr("AiraEth");
-        if (ethBot != 0)
-            allowance[_client][ethBot] += _value;
-    }
-
-    /**
-     * @dev Close allowance for AiraEthBot
-     */
-    function secureUnapprove(address _client) onlySecure {
-        var ethBot = reg.addr("AiraEth");
-        if (ethBot != 0)
-            allowance[_client][ethBot] = 0;
+    function airaSend(address _from, address _to, uint _value) onlyBot {
+        if (balanceOf[_from] >= _value) {
+            balanceOf[_from] -= _value;
+            totalSupply      -= _value;
+            Transfer(_from, _to, _value);
+            if (!_to.send(_value)) throw;
+        }
     }
 }
