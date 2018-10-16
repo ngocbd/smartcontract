a94b837a5bf9553a5dfdd2823a0ef516138f4474
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:07:13 +0700

    add contract AcademiCon

--- AcademiCon.sol
+++ AcademiCon.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AcademiCon at 0x622528056287da561004f59d6095769e0077257b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AcademiCon at 0x1a1e05667733854c3d40708629d0cf9a72156083
 */
 pragma solidity ^0.4.4;
 
@@ -108,12 +108,12 @@ contract AcademiCon is StandardToken { // CHANGE THIS. Update the contract name.
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function AcademiCon() {
-        balances[msg.sender] = 100000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 100000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        balances[msg.sender] = 100000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 100000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "AcademiCon";                                   // Set the name for display purposes (CHANGE THIS)
-        decimals = 0;                                               // Amount of decimals for display purposes (CHANGE THIS)
+        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
         symbol = "ADC";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        unitsOneEthCanBuy = 100;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
 
