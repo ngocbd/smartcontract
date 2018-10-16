cffb3293e531677dcd9e97eba303a23ff7d472ec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:50:52 +0700

    add contract FlowchainToken

--- FlowchainToken.sol
+++ FlowchainToken.sol
@@ -1,140 +1,172 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlowchainToken at 0x6631a18915303d20678bc38af589a6391bba9fd8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlowchainToken at 0x5b53f9755f82439cba66007ec7073c59e0da4a7d
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
+
+/**
+ * Copyright 2018, Flowchain.co
+ *
+ * The Flowchain tokens smart contract
+ */
+ 
+contract Mintable {
+    function mintToken(address to, uint amount) external returns (bool success);  
+    function setupMintableAddress(address _mintable) public returns (bool success);
+}
+
+contract ApproveAndCallReceiver {
+    function receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData);    
+}
 
 contract Token {
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
+    /// The total amount of tokens
+    uint256 public totalSupply;
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function balanceOf(address _owner) public view returns (uint256 balance);
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) public returns (bool success);
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) public returns (bool success);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
 }
-
-
 
 contract StandardToken is Token {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        // Not overflow
+        require(balances[_to] + _value >= balances[_to]);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        // Not overflow
+        require(balances[_to] + _value >= balances[_to]);          
+        balances[_to] += _value;    
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
             allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+        }  
+
+        Transfer(_from, _to, _value);
+        return true; 
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
 }
 
 
 //name this contract whatever you'd like
-contract FlowchainToken is StandardToken {
-
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
-    }
+contract FlowchainToken is StandardToken, Mintable {
 
     /* Public variables of the token */
+    string public name = "FlowchainCoin";
+    string public symbol = "FLC";    
+    uint8 public decimals = 18;
+    string public version = "1.0";
+    address public mintableAddress;
+    address public multiSigWallet;    
+    address public creator;
 
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    function() payable { revert(); }
 
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
+    function FlowchainToken() public {
+        // 1 billion tokens + 18 decimals
+        totalSupply = 10**27;                   
+        creator = msg.sender;
+        mintableAddress = 0x9581973c54fce63d0f5c4c706020028af20ff723;
+        multiSigWallet = 0x9581973c54fce63d0f5c4c706020028af20ff723;        
+        // Give the multisig wallet all initial tokens
+        balances[multiSigWallet] = totalSupply;  
+        Transfer(0x0, multiSigWallet, totalSupply);
+    }
 
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+    function setupMintableAddress(address _mintable) public returns (bool success) {
+        require(msg.sender == creator);    
+        mintableAddress = _mintable;
+        return true;
+    }
 
-    function FlowchainToken(
-        ) {
-        balances[msg.sender] = 18375000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 18375000;                        // Update total supply (100000 for example)
-        name = "Flowchain";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "FLC";                               // Set the symbol for display purposes
+    /// @dev Mint an amount of tokens and transfer to the backer
+    /// @param to The address of the backer who will receive the tokens
+    /// @param amount The amount of rewarded tokens
+    /// @return The result of token transfer
+    function mintToken(address to, uint256 amount) external returns (bool success) {
+        require(msg.sender == mintableAddress);
+        require(balances[multiSigWallet] >= amount);
+        balances[multiSigWallet] -= amount;
+        balances[to] += amount;
+        Transfer(multiSigWallet, to, amount);
+        return true;
+    }
+
+    /// @dev This function makes it easy to get the creator of the tokens
+    /// @return The address of token creator
+    function getCreator() constant returns (address) {
+        return creator;
+    }
+
+    /// @dev This function makes it easy to get the mintableAddress
+    /// @return The address of token creator
+    function getMintableAddress() constant returns (address) {
+        return mintableAddress;
     }
 
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) external returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+
+        ApproveAndCallReceiver(_spender).receiveApproval(msg.sender, _value, this, _extraData);
+
         return true;
     }
 }
