1917a82c0928e58dcbde407b81ff30a94e9a355b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:05:01 +0700

    add contract Switch

--- Switch.sol
+++ Switch.sol
@@ -1,25 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0xE7aA95217EB415B2c400c81e2B65359E720aB936
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Switch at 0x69644dad199c1973ae660ddbd86bfec1109c06a7
 */
-contract Switch {
-    address constant theWithdraw = 0xbf4ed7b27f1d666546e30d74d50d173d20bca754;
-    function Switch() {
-        forked = theWithdraw.balance > 10000 ether;
-    }
-    
-    function transferringETC(address to) {
-        if (forked)
-            throw;
-        if (!to.send(msg.value))
-            throw;
-    }
+pragma solidity ^0.4.8;
+contract Switch 
+{
+    uint256 public blink_block;
+    uint256 public on_block;
+    address public owner;
 
-    function transferringETH(address to) {
-        if (!forked)
-            throw;
-        if (!to.send(msg.value))
-            throw;
+    function Switch(){
+        owner=msg.sender;
+        on_block=block.number;
+        blink_block=block.number;
     }
-    
-    bool public forked;
+    function blink() payable {
+        if(msg.value>0)blink_block=block.number;
+    }
+    function () payable {
+        if(msg.value>0)on_block=block.number;
+    }
+    function kill() {
+    if (msg.sender==owner) selfdestruct(owner);
+    }
 }
