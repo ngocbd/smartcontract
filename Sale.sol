aee4b9fb97f68a505707cca94fd0065454733bea
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:37:11 +0700

    add contract Sale

--- Sale.sol
+++ Sale.sol
@@ -1,144 +1,206 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x21caf0734835e5766f752763401d2cfd8c23416b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0xd8444e581e288aecb2df4b442d19c2fafb3fd11f
 */
-pragma solidity 0.4.19;
+pragma solidity ^0.4.20;
 
-contract ERC20 {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function allowance(address owner, address spender) constant returns (uint);
-  function transfer(address to, uint value) returns (bool ok);
-  function transferFrom(address from, address to, uint value) returns (bool ok);
-  function approve(address spender, uint value) returns (bool ok);
-  function mintToken(address to, uint256 value) returns (uint256);
-  function changeTransfer(bool allowed);
+
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 
-contract Sale {
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
-    uint256 public maxMintable;
-    uint256 public totalMinted;
-    uint public endBlock;
-    uint public startBlock;
-    uint public exchangeRate;
-    bool public isFunding;
-    ERC20 public Token;
-    address public ETHWallet;
-    uint256 public heldTotal;
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-    bool private configSet;
-    address public creator;
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-    mapping (address => uint256) public heldTokens;
-    mapping (address => uint) public heldTimeline;
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    event Contribution(address from, uint256 amount);
-    event ReleaseTokens(address from, uint256 amount);
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
-    function Sale() {
-        startBlock = block.number;
-        maxMintable = 10000000e18; 
-        ETHWallet = 0x56710010B234A104D7E67dA5765A081eF7f2B4C8; 
-        isFunding = true;
-        creator = 0x0E6EFB81B03ea30Fd7Eac2a416FB5ec943B5cdBA;
-        createHeldCoins();
-        exchangeRate = 2000; 
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+
+contract Sale is Ownable {
+  using SafeMath for uint256;
+    uint rate = 1;
+    uint mincap = 0;
+    address tokenAddr;
+    bool isActive = false;
+    bool onlyWhitelist = false;
+
+    mapping(address => uint) balance;
+    mapping(address => uint) balanceWithdrawn;
+    address[] investors;
+    mapping(address => bool) whitelist;
+
+
+    function Sale() public{
     }
 
-    
-    
-    
-    function setup(address TOKEN, uint endBlockTime) {
-        require(!configSet);
-        Token = ERC20(TOKEN);
-        endBlock = endBlockTime;
-        configSet = true;
-    }
+    function() payable public{ 
+      require(isActive);
+      require(tokenAddr != address(0));
+      require(msg.sender != address(0));
+      require(msg.value >=  mincap );
+      require(!onlyWhitelist || whitelist[msg.sender]);
+      uint256 amt = msg.value.mul(rate);
 
-    function closeSale() external {
-      require(msg.sender==creator);
-      isFunding = false;
+      ERC20 token = ERC20(tokenAddr);
+      require(token.balanceOf(this) >= amt);
+      
+      if(balance[msg.sender] == 0){
+        investors.push(msg.sender);
+      }
+      balance[msg.sender] += amt;
     }
-
-    
-    
-    function contribute() external payable {
-        require(msg.value>0);
-        require(isFunding);
-        require(block.number <= endBlock);
-        uint256 amount = msg.value * exchangeRate;
-        uint256 total = totalMinted + amount;
-        require(total<=maxMintable);
-        totalMinted = total; 
-        ETHWallet.transfer(msg.value);
-        Token.mintToken(msg.sender, amount);
-        Contribution(msg.sender, amount);
+    function getRate() public view returns(uint){
+        return rate;
     }
-    
-    
-    function() payable public {
-        require(msg.value>0);
-        require(isFunding);
-        require(block.number <= endBlock);
-        uint256 amount = msg.value * exchangeRate;
-        uint256 total = totalMinted + amount;
-        require(total<=maxMintable);
-        totalMinted = total; 
-        ETHWallet.transfer(msg.value);
-        Token.mintToken(msg.sender, amount);
-        Contribution(msg.sender, amount);
+    function getCap() public view returns(uint){
+        return mincap;
     }
-
-    
-    function updateRate(uint256 rate) external {
-        require(msg.sender==creator);
-        require(isFunding);
-        exchangeRate = rate;
+    function getBalance() public view returns(uint256){
+      ERC20 token = ERC20(tokenAddr);
+      return token.balanceOf(this);
     }
-
-    
-    function changeCreator(address _creator) external {
-        require(msg.sender==creator);
-        creator = _creator;
+    function changeRate(uint _rate) public onlyOwner{
+      rate = _rate;
     }
-
-    
-    function changeTransferStats(bool _allowed) external {
-        require(msg.sender==creator);
-        Token.changeTransfer(_allowed);
+    function changemincap(uint _mincap) public onlyOwner{
+      mincap = _mincap;
     }
-
-    
-    
-    function createHeldCoins() internal {
-        
-        createHoldToken(0x44Bb8D9036Db5453219189E0a7262BFe1a69AfEB, 4000000e18); 
-        
-        
+    function changeAddr(address _tokenAddr) public onlyOwner{
+      tokenAddr = _tokenAddr;
     }
-
-    
-    function createHoldToken(address _to, uint256 amount) internal {
-        
-        heldTokens[_to] = amount;
-        heldTimeline[_to] = block.number + 0;
-        heldTotal += amount;
-        totalMinted += heldTotal;
+    function addWhitelist(address[] addr_list) public onlyOwner{
+      for(uint i=0;i<addr_list.length;i++){
+        whitelist[addr_list[i]] = true;
+      }
     }
-
-    
-    function releaseHeldCoins() external {
-        uint256 held = heldTokens[msg.sender];
-        uint heldBlock = heldTimeline[msg.sender];
-        require(!isFunding);
-        require(held >= 0);
-        require(block.number >= heldBlock);
-        heldTokens[msg.sender] = 0;
-        heldTimeline[msg.sender] = 0;
-        Token.mintToken(msg.sender, held);
-        ReleaseTokens(msg.sender, held);
+    function setActive() public onlyOwner{
+        isActive = !isActive;
     }
-
-
+    function setActiveWhitelist() public onlyOwner{
+        onlyWhitelist = !onlyWhitelist;
+    }
+    function drainWei() public onlyOwner{
+        owner.transfer(this.balance);
+    }    
+    function drainToken() public onlyOwner{
+        uint cantWithdrawAmt = 0;
+        for(uint i = 0;i<investors.length;i++){
+          cantWithdrawAmt += balance[investors[i]];
+        }      
+        ERC20 token = ERC20(tokenAddr);
+        token.transfer(msg.sender, (token.balanceOf(this)).sub(cantWithdrawAmt) );
+    }
+    function giveTokens(uint percent) public onlyOwner{
+        ERC20 token = ERC20(tokenAddr);
+        for(uint i = 0;i<investors.length;i++){
+          uint bal = balance[investors[i]];
+          uint canWithdrawAmt = (bal.div(100)).mul(percent);
+          if(canWithdrawAmt > 0 && balanceWithdrawn[investors[i]] + canWithdrawAmt <= bal){
+            balanceWithdrawn[investors[i]] += canWithdrawAmt;
+            token.transfer(investors[i], canWithdrawAmt);
+          }
+        }      
+    }        
 }
