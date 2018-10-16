c6a93fcfb87a89ffaec984ee63bdf53935a514a8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:43:36 +0700

    add contract AlwaysFail

---
+++ AlwaysFail.sol
@@ -0,0 +1,16 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AlwaysFail at 0xdb22939e20A4C41cd7e2b9B325ce452B36c80dd9
+*/
+contract AlwaysFail {
+
+    function AlwaysFail() {
+    }
+    
+    function() {
+        enter();
+    }
+    
+    function enter() {
+        throw;
+    }
+}
