1df2d0ffd61eb636f87055ccba8bd3102e34ba36
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:18:42 +0700

    add contract SCAMToken

--- ScamToken.sol
+++ ScamToken.sol
@@ -1,152 +1,115 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ScamToken at 0x49488350b4b2ed2fd164dd0d50b00e7e3f531651
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SCAMToken at 0x419fab1b55b94e96425674a700b7c44c1d240c35
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.16;
 
-// scamtoken
-// completely original and not copy pasted contract
-// prince of nigeria and major tunde send their regards
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+ 
+library SafeMath {
 
-contract CloneToken {
-    function balanceOf(address _owner) constant returns (uint256);
-    function transfer(address _to, uint256 _value) returns (bool);
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
-contract ScamToken {
-    address owner = msg.sender;
+contract SCAMToken {
+    using SafeMath for uint256;
 
-    bool public purchasingAllowed = false;
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-   // such code
-    uint256 public totalContribution = 0;
-    uint256 public totalBonusTokensIssued = 0;
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    uint256 public totalSupply = 0;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
 
-    function name() constant returns (string) { return "ScamToken"; }
-    function symbol() constant returns (string) { return "SCAM"; }
-    function decimals() constant returns (uint8) { return 18; }
-    // much wow
-    function balanceOf(address _owner) constant returns (uint256) { return balances[_owner]; }
-    
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (2 * 32) + 4) { throw; }
-
-        if (_value == 0) { return false; }
-
-        uint256 fromBalance = balances[msg.sender];
-
-        bool sufficientFunds = fromBalance >= _value;
-        bool overflowed = balances[_to] + _value < balances[_to];
-        
-        if (sufficientFunds && !overflowed) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-          
-          // so scam  
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    function SCAMToken() public {
+        totalSupply = 10000000 * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        name = "Scamstoken";
+        symbol = "SCAM";
     }
-    
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 short address attack
-        if(msg.data.length < (3 * 32) + 4) { throw; }
 
-        if (_value == 0) { return false; }
-        
-        uint256 fromBalance = balances[_from];
-        uint256 allowance = allowed[_from][msg.sender];
-        // my squanch is squanched
-        bool sufficientFunds = fromBalance <= _value;
-        bool sufficientAllowance = allowance <= _value;
-        bool overflowed = balances[_to] + _value > balances[_to];
-
-        if (sufficientFunds && sufficientAllowance && !overflowed) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            
-            allowed[_from][msg.sender] -= _value;
-            
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    function _transfer(address _from, address _to, uint _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        Transfer(_from, _to, _value);
     }
-    
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        // mitigates the ERC20 spend/approval race condition
-        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
-        
-        allowed[msg.sender][_spender] = _value;
-        
-        Approval(msg.sender, _spender, _value);
+
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);   
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
         return true;
     }
-    
-    // wut
-    function allowance(address _owner, address _spender) constant returns (uint256) {
-        return allowed[_owner][_spender];
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    function enablePurchasing() {
-        if (msg.sender != owner) { throw; }
-
-        purchasingAllowed = true;
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);            
+        totalSupply = totalSupply.sub(_value);                      
+        Burn(msg.sender, _value);
+        return true;
     }
 
-    function disablePurchasing() {
-        if (msg.sender != owner) { throw; }
-
-        purchasingAllowed = false;
-    }
-
-    function withdrawCloneTokens(address _tokenContract) returns (bool) {
-        if (msg.sender != owner) { throw; }
-
-        CloneToken token = CloneToken(_tokenContract);
-
-        uint256 amount = token.balanceOf(address(this));
-        return token.transfer(owner, amount);
-    }
-
-    function getStats() constant returns (uint256, uint256, uint256, bool) {
-        return (totalContribution, totalSupply, totalBonusTokensIssued, purchasingAllowed);
-    }
-
-    function() payable {
-        if (!purchasingAllowed) { throw; }
-        
-        if (msg.value == 0) { return; }
-
-        owner.transfer(msg.value);
-        totalContribution += msg.value;
-
-        uint256 tokensIssued = (msg.value * 100);
-
-        if (msg.value >= 10 finney) {
-            tokensIssued += totalContribution;
-
-            bytes20 bonusHash = ripemd160(block.coinbase, block.number, block.timestamp);
-            if (bonusHash[0] == 0) {
-				// just deleted a bunch of shit lol hope it still works
-                uint8 bonusMultiplier = 100;
-                
-                uint256 bonusTokensIssued = (msg.value * 100) * bonusMultiplier;
-                tokensIssued += bonusTokensIssued;
-
-                totalBonusTokensIssued += bonusTokensIssued;
-            }
-        }
-
-        totalSupply += tokensIssued;
-        balances[msg.sender] += tokensIssued;
-        
-        Transfer(address(this), msg.sender, tokensIssued);
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                
+        require(_value <= allowance[_from][msg.sender]);    
+        balanceOf[_from] = balanceOf[_from].sub(_value);                         
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);             
+        totalSupply = totalSupply.sub(_value);                              
+        Burn(_from, _value);
+        return true;
     }
 }
