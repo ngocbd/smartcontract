9941a4f9a4153b9ee72a455b60c0b0bca592a392
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:12:21 +0700

    add contract passOnContract

--- passOnContract.sol
+++ passOnContract.sol
@@ -1,15 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract passOnContract at 0xb4d735c011a492d180edcdcfab25420a6bfbed96
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract passOnContract at 0x9edd3447ef75bf40aa9da993e4b5d0c7ae5a5a73
 */
 pragma solidity ^0.4.21;
 
   contract MBLToken {
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function transfer(address _to, uint256 _value) public;
     function balanceOf(address _tokenOwner) external view returns (uint balance);
 
  }
 
-   contract passOnContract {
+    contract passOnContract {
 
     address public owner;
   	address public tokenAddress = 0x8D7dDaD45789a64c2AF9b4Ce031C774e277F1Cd4;
@@ -19,11 +19,12 @@ pragma solidity ^0.4.21;
   		owner = msg.sender;
   	}
 
+
     function () public payable {
 
     	owner.transfer(msg.value);
     	MBLToken mblToken = MBLToken(tokenAddress);
-    	mblToken.transferFrom(owner, msg.sender, msg.value/250000000000000);
+    	mblToken.transfer(msg.sender, msg.value*40);
 
     }
 
