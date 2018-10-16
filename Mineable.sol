11b9f3f37b4e2aa13dddf96d10fd187c647346de
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:06:50 +0700

    add contract Mineable

--- Mineable.sol
+++ Mineable.sol
@@ -1,101 +1,71 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Mineable at 0xed37a1395e3c9db3e6543ea770b91ea0e8bd4f05
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Mineable at 0x51e42087549dd9282362fcbbf328f8e34b27cf66
 */
 pragma solidity ^0.4.11;
 
 contract Mineable {
-    address public owner;
-    uint public supply = 100000000000000;
-    string public name = 'MineableBonusEthereumToken';
-    string public symbol = 'MBET';
-    uint8 public decimals = 8;
-    uint public price = 1 finney;
-    uint public durationInBlocks = 157553; // 1 month
-    uint public miningReward = 100000000;
-    uint public amountRaised;
-    uint public deadline;
-    uint public tokensSold;
+    uint public supply = 1000000000000000000000000000;
+    string public name = 'MIT';
+    string public symbol = 'MIT';
+    uint8 public decimals = 18;
+    uint public miningReward = 1000000000000000000;
     uint private divider;
     
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => uint256) public successesOf;
     mapping (address => uint256) public failsOf;
+    mapping (address => mapping (address => uint256)) public allowance;
     
     /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
     
-    event FundTransfer(address backer, uint amount, bool isContribution);
-    
-    function isOwner() returns (bool isOwner) {
-        return msg.sender == owner;
-    }
-    
-    function addressIsOwner(address addr)  returns (bool isOwner) {
-        return addr == owner;
-    }
-
-    modifier onlyOwner {
-        if (msg.sender != owner) revert();
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
-    }
-    
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function Mineable() {
-        owner = msg.sender;
+        balanceOf[msg.sender] = supply;
         divider -= 1;
         divider /= 1048576;
-        balanceOf[msg.sender] = supply;
-        deadline = block.number + durationInBlocks;
     }
     
-    function isCrowdsale() returns (bool isCrowdsale) {
-        return block.number < deadline;
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
     
     /* Send coins */
     function transfer(address _to, uint256 _value) {
-        /* if the sender doesnt have enough balance then stop */
-        if (balanceOf[msg.sender] < _value) revert();
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();
-        
-        /* Add and subtract new balances */
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        
-        /* Notify anyone listening that this transfer took place */
-        Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
+    }
+    
+    /* Transfer tokens from other address */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+    
+    /* Set allowance for other address */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
     }
     
     function () payable {
-        if (isOwner()) {
-            owner.transfer(amountRaised);
-            FundTransfer(owner, amountRaised, false);
-            amountRaised = 0;
-        } else if (isCrowdsale()) {
-            uint amount = msg.value;
-            if (amount == 0) revert();
-            
-            uint tokensCount = amount * 100000000 / price;
-            if (tokensCount < 100000000) revert();
-            
-            balanceOf[msg.sender] += tokensCount;
-            supply += tokensCount;
-            tokensSold += tokensCount;
-            Transfer(0, this, tokensCount);
-            Transfer(this, msg.sender, tokensCount);
-            amountRaised += amount;
-        } else if (msg.value == 0) {
+        if (msg.value == 0) {
             uint minedAtBlock = uint(block.blockhash(block.number - 1));
             uint minedHashRel = uint(sha256(minedAtBlock + uint(msg.sender))) / divider;
             uint balanceRel = balanceOf[msg.sender] * 1048576 / supply;
             
             if (minedHashRel < balanceRel * 933233 / 1048576 + 10485) {
-                uint reward = miningReward + minedHashRel * 10000;
+                uint reward = miningReward + minedHashRel * 1000000000000000;
                 balanceOf[msg.sender] += reward;
                 supply += reward;
                 Transfer(0, this, reward);
