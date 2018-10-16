a8ae8ed913234be8fbca09ebcb9a1193729c1bf6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:11:29 +0700

    add contract Empty

---
+++ Empty.sol
@@ -0,0 +1,9 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Empty at 0xfbeeb0b43213a0abaa921f65873e09849ae5deec
+*/
+pragma solidity 0.4.18;
+
+
+contract Empty {
+ uint public value = 5;
+}
