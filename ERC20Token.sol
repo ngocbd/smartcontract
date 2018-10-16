ef01f4acd877908f8b2b961c42866762512e6ec8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:16:11 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x18f41fefd923b3f45ba532445c1ebd8aa8428efb
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x87785881a871126a83f8d61ebf3b3c7aed077141
 */
 pragma solidity ^0.4.4;
 
@@ -106,10 +106,10 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //0.02 - 
+    string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX40ETH
-    string public version = 'H1.0';       //human 0.1 standard. Token lockdown
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
 //
 // CHANGE THESE VALUES FOR YOUR TOKEN
@@ -119,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 2000000000000000000000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 2000000000000000000000000000;                        // Update total supply (100000 for example)
-        name = "TexoChat";                                   // Set the name for display purposes
+        balances[msg.sender] = 6050000000000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 6050000000000000000000000;                        // Update total supply (100000 for example)
+        name = "Mcafee Redemption Unit";                                   // Set the name for display purposes
         decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "TXO";                               // Set the symbol for display purposes
+        symbol = "MRU";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
