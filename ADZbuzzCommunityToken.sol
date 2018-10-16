39f956e0cfd08eaba48d374af5d06323095eda29
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:44:58 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xcaaaecc33bb97c489becb078206ad0129bbe658c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x038983e6330000482019e6c34a2e3c31c421e5e9
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT490863' token contract
+    // 'ACT490876' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT490863
-    // Name        : ADZbuzz Indiatoday.in Community Token
+    // Symbol      : ACT490876
+    // Name        : ADZbuzz Traveller.com.au Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT490863";
-            name = "ADZbuzz Indiatoday.in Community Token";
+            symbol = "ACT490876";
+            name = "ADZbuzz Traveller.com.au Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
