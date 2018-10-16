31cbc6fc4a463575a4e0e8c1d510da04afd5c755
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:53:00 +0700

    add contract ETHVault

--- ETHVault.sol
+++ ETHVault.sol
@@ -1,12 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHVault at 0xcacf9396a56e9ff1e3f6533be83a043c36ce0436
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHVault at 0xf1aAB4171Ceb49B6a276975347e3c1D4D5650e5A
 */
 pragma solidity ^0.4.10;
 
 contract Owned {
     address public Owner;
     function Owned() { Owner = msg.sender; }
-    modifier onlyOwner { if( msg.sender == Owner ) _; }
+    modifier onlyOwner { require( msg.sender == Owner ); _; }
 }
 
 contract ETHVault is Owned {
@@ -18,19 +18,19 @@ contract ETHVault is Owned {
     function() payable { deposit(); }
     
     function deposit() payable {
-        if( msg.value >= 100 finney )
+        if( msg.value >= 0.25 ether )
             deposits[msg.sender] += msg.value;
         else throw;
     }
     
     function withdraw(uint amount) onlyOwner {
-        uint max = deposits[msg.sender];
-        if( amount <= max && max > 0 )
+        uint depo = deposits[msg.sender];
+        if( amount <= depo && depo > 0 )
             msg.sender.send(amount);
     }
-    
+
     function kill() onlyOwner {
         require(this.balance == 0);
         suicide(msg.sender);
-    }
+	}
 }
