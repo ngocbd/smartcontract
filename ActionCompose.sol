6cfb5ab0031121454218dfb363f2aac49ef6768d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:23:53 +0700

    add contract ActionCompose

--- ActionCompose.sol
+++ ActionCompose.sol
@@ -1,15 +1,6 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ActionCompose at 0x0176d6746520dd73c2db032348530520d64e074e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ActionCompose at 0xbe8115cdf67fcbbca3efb2a9ff18bb98eac2308f
 */
-/* ==================================================================== */
-/* Copyright (c) 2018 The ether.online Project.  All rights reserved.
-/* 
-/* https://ether.online  The first RPG game of blockchain 
-/*  
-/* authors rickhunter.shen@gmail.com   
-/*         ssesunding@gmail.com            
-/* ==================================================================== */
-
 pragma solidity ^0.4.20;
 
 /// @title ERC-165 Standard Interface Detection
@@ -38,6 +29,20 @@ contract ERC721 is ERC165 {
 /// @title ERC-721 Non-Fungible Token Standard
 interface ERC721TokenReceiver {
 	function onERC721Received(address _from, uint256 _tokenId, bytes data) external returns(bytes4);
+}
+
+/// @title ERC-721 Non-Fungible Token Standard, optional metadata extension
+interface ERC721Metadata /* is ERC721 */ {
+    function name() external pure returns (string _name);
+    function symbol() external pure returns (string _symbol);
+    function tokenURI(uint256 _tokenId) external view returns (string);
+}
+
+/// @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
+interface ERC721Enumerable /* is ERC721 */ {
+    function totalSupply() external view returns (uint256);
+    function tokenByIndex(uint256 _index) external view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
 }
 
 contract AccessAdmin {
@@ -119,75 +124,6 @@ contract AccessService is AccessAdmin {
         } else {
             receiver.transfer(this.balance);
         }      
-    }
-}
-
-interface IDataMining {
-    function getRecommender(address _target) external view returns(address);
-    function subFreeMineral(address _target) external returns(bool);
-}
-
-interface IDataEquip {
-    function isEquiped(address _target, uint256 _tokenId) external view returns(bool);
-    function isEquipedAny2(address _target, uint256 _tokenId1, uint256 _tokenId2) external view returns(bool);
-    function isEquipedAny3(address _target, uint256 _tokenId1, uint256 _tokenId2, uint256 _tokenId3) external view returns(bool);
-}
-
-contract Random {
-    uint256 _seed;
-
-    function _rand() internal returns (uint256) {
-        _seed = uint256(keccak256(_seed, block.blockhash(block.number - 1), block.coinbase, block.difficulty));
-        return _seed;
-    }
-
-    function _randBySeed(uint256 _outSeed) internal view returns (uint256) {
-        return uint256(keccak256(_outSeed, block.blockhash(block.number - 1), block.coinbase, block.difficulty));
-    }
-}
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-    /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
     }
 }
 
@@ -657,6 +593,94 @@ contract WarToken is ERC721, AccessAdmin {
     }
 }
 
+/// This Random is inspired by https://github.com/axiomzen/eth-random
+contract Random {
+    uint256 _seed;
+
+    function _rand() internal returns (uint256) {
+        _seed = uint256(keccak256(_seed, block.blockhash(block.number - 1), block.coinbase, block.difficulty));
+        return _seed;
+    }
+
+    function _randBySeed(uint256 _outSeed) internal view returns (uint256) {
+        return uint256(keccak256(_outSeed, block.blockhash(block.number - 1), block.coinbase, block.difficulty));
+    }
+
+    /*
+    function _randByRange(uint256 _min, uint256 _max) internal returns (uint256) {
+        if (_min >= _max) {
+            return _min;
+        }
+        return (_rand() % (_max - _min)) + _min;
+    }
+
+    function _rankByNumber(uint256 _max) internal returns (uint256) {
+        return _rand() % _max;
+    }
+    */
+}
+
+interface IDataMining {
+    function getRecommender(address _target) external view returns(address);
+    function subFreeMineral(address _target) external returns(bool);
+}
+
+interface IDataEquip {
+    function isEquiped(address _target, uint256 _tokenId) external view returns(bool);
+    function isEquipedAny2(address _target, uint256 _tokenId1, uint256 _tokenId2) external view returns(bool);
+    function isEquipedAny3(address _target, uint256 _tokenId1, uint256 _tokenId2, uint256 _tokenId3) external view returns(bool);
+}
+
+interface IDataAuction {
+    function isOnSaleAny2(uint256 _tokenId1, uint256 _tokenId2) external view returns(bool);
+    function isOnSaleAny3(uint256 _tokenId1, uint256 _tokenId2, uint256 _tokenId3) external view returns(bool);
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    /**
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
 contract ActionCompose is Random, AccessService {
     using SafeMath for uint256;
 
@@ -664,12 +688,8 @@ contract ActionCompose is Random, AccessService {
     
     /// @dev If the recommender can get reward 
     bool isRecommendOpen;
-    /// @dev prizepool percent
-    uint256 constant prizePoolPercent = 50;
-    /// @dev prizepool contact address
-    address poolContract;
-    /// @dev DataMining contract address
-    IDataMining public dataContract;
+    /// @dev Auction contract address
+    IDataAuction public auctionContract;
     /// @dev DataEquip contract address
     IDataEquip public equipContract;
     /// @dev WarToken(NFT) contract address
@@ -692,14 +712,9 @@ contract ActionCompose is Random, AccessService {
         isRecommendOpen = _isOpen;
     }
 
-    function setDataMining(address _addr) external onlyAdmin {
+    function setDataAuction(address _addr) external onlyAdmin {
         require(_addr != address(0));
-        dataContract = IDataMining(_addr);
-    }
-
-    function setPrizePool(address _addr) external onlyAdmin {
-        require(_addr != address(0));
-        poolContract = _addr;
+        auctionContract = IDataAuction(_addr);
     }
 
     function setDataEquip(address _addr) external onlyAdmin {
@@ -722,94 +737,71 @@ contract ActionCompose is Random, AccessService {
             qtyParam = 6;
         }
 
+        // ????
         uint256 rdm = _protoId % 3;
 
         curSeed /= 10000;
         uint256 tmpVal = (curSeed % 10000) % 21 + 90;
 
+        // ??1/??2/??3/??4/??5
         if (rdm == 0) {
             if (_pos == 1) {
-                uint256 attr = (200 + qtyParam * 200) * tmpVal / 100;              // +atk
+                uint256 attr = (200 + qtyParam * 200) * tmpVal / 100;              // ??+atk
                 attrs[4] = uint16(attr * 40 / 100);
                 attrs[5] = uint16(attr * 160 / 100);
             } else if (_pos == 2) {
-                attrs[6] = uint16((40 + qtyParam * 40) * tmpVal / 100);            // +def
+                attrs[6] = uint16((40 + qtyParam * 40) * tmpVal / 100);            // ??+def
             } else if (_pos == 3) {
-                attrs[3] = uint16((600 + qtyParam * 600) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((600 + qtyParam * 600) * tmpVal / 100);          // ??+hp
             } else if (_pos == 4) {
-                attrs[6] = uint16((60 + qtyParam * 60) * tmpVal / 100);            // +def
+                attrs[6] = uint16((60 + qtyParam * 60) * tmpVal / 100);            // ??+def
             } else {
-                attrs[3] = uint16((400 + qtyParam * 400) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((400 + qtyParam * 400) * tmpVal / 100);          // ??+hp
             }
         } else if (rdm == 1) {
             if (_pos == 1) {
-                uint256 attr2 = (190 + qtyParam * 190) * tmpVal / 100;              // +atk
+                uint256 attr2 = (190 + qtyParam * 190) * tmpVal / 100;              // ??+atk
                 attrs[4] = uint16(attr2 * 50 / 100);
                 attrs[5] = uint16(attr2 * 150 / 100);
             } else if (_pos == 2) {
-                attrs[6] = uint16((42 + qtyParam * 42) * tmpVal / 100);            // +def
+                attrs[6] = uint16((42 + qtyParam * 42) * tmpVal / 100);            // ??+def
             } else if (_pos == 3) {
-                attrs[3] = uint16((630 + qtyParam * 630) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((630 + qtyParam * 630) * tmpVal / 100);          // ??+hp
             } else if (_pos == 4) {
-                attrs[6] = uint16((63 + qtyParam * 63) * tmpVal / 100);            // +def
+                attrs[6] = uint16((63 + qtyParam * 63) * tmpVal / 100);            // ??+def
             } else {
-                attrs[3] = uint16((420 + qtyParam * 420) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((420 + qtyParam * 420) * tmpVal / 100);          // ??+hp
             }
         } else {
             if (_pos == 1) {
-                uint256 attr3 = (210 + qtyParam * 210) * tmpVal / 100;             // +atk
+                uint256 attr3 = (210 + qtyParam * 210) * tmpVal / 100;             // ??+atk
                 attrs[4] = uint16(attr3 * 30 / 100);
                 attrs[5] = uint16(attr3 * 170 / 100);
             } else if (_pos == 2) {
-                attrs[6] = uint16((38 + qtyParam * 38) * tmpVal / 100);            // +def
+                attrs[6] = uint16((38 + qtyParam * 38) * tmpVal / 100);            // ??+def
             } else if (_pos == 3) {
-                attrs[3] = uint16((570 + qtyParam * 570) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((570 + qtyParam * 570) * tmpVal / 100);          // ??+hp
             } else if (_pos == 4) {
-                attrs[6] = uint16((57 + qtyParam * 57) * tmpVal / 100);            // +def
+                attrs[6] = uint16((57 + qtyParam * 57) * tmpVal / 100);            // ??+def
             } else {
-                attrs[3] = uint16((380 + qtyParam * 380) * tmpVal / 100);          // +hp
+                attrs[3] = uint16((380 + qtyParam * 380) * tmpVal / 100);          // ??+hp
             }
         }
         attrs[8] = 0;
     }
 
-    function _transferHelper(uint256 ethVal) private {
-        bool recommenderSended = false;
-        uint256 fVal;
-        uint256 pVal;
-        if (isRecommendOpen) {
-            address recommender = dataContract.getRecommender(msg.sender);
-            if (recommender != address(0)) {
-                uint256 rVal = ethVal.div(10);
-                fVal = ethVal.sub(rVal).mul(prizePoolPercent).div(100);
-                addrFinance.transfer(fVal);
-                recommenderSended = true;
-                recommender.transfer(rVal);
-                pVal = ethVal.sub(rVal).sub(fVal);
-                if (poolContract != address(0) && pVal > 0) {
-                    poolContract.transfer(pVal);
-                }
-            } 
-        } 
-        if (!recommenderSended) {
-            fVal = ethVal.mul(prizePoolPercent).div(100);
-            pVal = ethVal.sub(fVal);
-            addrFinance.transfer(fVal);
-            if (poolContract != address(0) && pVal > 0) {
-                poolContract.transfer(pVal);
-            }
-        }
-    }
-
+    // gas 210019
     function lowCompose(uint256 token1, uint256 token2) 
         external
-        payable
         whenNotPaused
     {
-        require(msg.value >= 0.003 ether);
         require(tokenContract.ownerOf(token1) == msg.sender);
         require(tokenContract.ownerOf(token2) == msg.sender);
         require(!equipContract.isEquipedAny2(msg.sender, token1, token2));
+
+        if (address(auctionContract) != address(0)) {
+            require(!auctionContract.isOnSaleAny2(token1, token2));
+        }
 
         tokenContract.ownerOf(token1);
 
@@ -835,25 +827,23 @@ contract ActionCompose is Random, AccessService {
         tokenContract.destroyFashion(token2, 1);
 
         uint256 newTokenId = tokenContract.createFashion(msg.sender, attrs, 3);
-        _transferHelper(0.003 ether);
-
-        if (msg.value > 0.003 ether) {
-            msg.sender.transfer(msg.value - 0.003 ether);
-        }
 
         ComposeSuccess(msg.sender, newTokenId, attrs[0], attrs[1], attrs[2]);
     }
 
+    // gas 198125
     function highCompose(uint256 token1, uint256 token2, uint256 token3) 
         external
-        payable
         whenNotPaused
     {
-        require(msg.value >= 0.005 ether);
         require(tokenContract.ownerOf(token1) == msg.sender);
         require(tokenContract.ownerOf(token2) == msg.sender);
         require(tokenContract.ownerOf(token3) == msg.sender);
         require(!equipContract.isEquipedAny3(msg.sender, token1, token2, token3));
+
+        if (address(auctionContract) != address(0)) {
+            require(!auctionContract.isOnSaleAny3(token1, token2, token3));
+        }
 
         uint16 protoId;
         uint16 quality;
@@ -883,11 +873,6 @@ contract ActionCompose is Random, AccessService {
         tokenContract.destroyFashion(token3, 1);
 
         uint256 newTokenId = tokenContract.createFashion(msg.sender, attrs, 4);
-        _transferHelper(0.005 ether);
-
-        if (msg.value > 0.005 ether) {
-            msg.sender.transfer(msg.value - 0.005 ether);
-        }
 
         ComposeSuccess(msg.sender, newTokenId, attrs[0], attrs[1], attrs[2]);
     }
