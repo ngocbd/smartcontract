379e4792abac19254591e1cc8a6794af837693b5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:22:36 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0xe40f742ebb964b23f6a4b2bb80d4bedfe047f3b4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x1aef94c9eb6ede04f59c6fe4f7eda2b874cccfcd
 */
 pragma solidity ^0.4.4;
 
@@ -7,7 +7,7 @@ contract Token {
 
     /// @return total amount of tokens
     function totalSupply() constant returns (uint256 supply) {}
-   
+
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance) {}
@@ -119,11 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 1000000000;               // Give the creator all initial tokens (1000000 for example)
-        totalSupply = 1000000000;                        // Update total supply (30000000 for example)
-        name = "EOS";                                   // Set the name for display purposes
+        balances[msg.sender] = 21000000000000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 21000000000000000000000000;                        // Update total supply (100000 for example)
+        name = "Quod Erat Demonstrandum";                                   // Set the name for display purposes
         decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "EOS";                               // Set the symbol for display purposes
+        symbol = "QED";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
