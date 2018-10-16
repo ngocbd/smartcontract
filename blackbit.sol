8e5a1bf5d69bb1434933189480e05a2db13c8b9f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:33:03 +0700

    add contract blackbit

---
+++ blackbit.sol
@@ -0,0 +1,52 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract blackbit at 0xfc7205c2a169cc9f8fb3cc5ef0c82c7cd39deefc
+*/
+pragma solidity ^0.4.2;
+
+contract blackbit {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public initialSupply;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+  
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function blackbit() {
+
+         initialSupply = 500000;
+         name ="blackbit";
+        decimals = 0;
+         symbol = "BBT";
+        
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+                                   
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+      
+    }
+
+   
+
+    
+
+   
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
