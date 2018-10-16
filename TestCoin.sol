6eb3a0edeed14c9a277a376c90c8b67692c0b0c0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:16:15 +0700

    add contract Testcoin

--- TestCoin.sol
+++ TestCoin.sol
@@ -1,122 +1,210 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestCoin at 0x13f80909e2f7913112e5a16f57cf274a8b33fa05
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Testcoin at 0x73317e79086f748a73bf5f32dc689d0be431283b
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.11;
 
-contract TokenRecipient {
-	function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract ERC20 {
-	uint256 public totalSupply;
-	function balanceOf(address _owner) public constant returns (uint256 balance);
-	function transfer(address _to, uint256 _value) public returns (bool ok);
-	function transferFrom(address _from, address _to, uint256 _value) public returns (bool ok);
-	function approve(address _spender, uint256 _value) public returns (bool ok);
-	function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract TestCoin is ERC20 {
-	mapping (address => uint256) balances;
-	mapping (address => mapping (address => uint256)) allowed;
-	uint8 public decimals;
-	string public name;
-	string public symbol;
-	
-	bool public running;
-	address public owner;
-	address public ownerTemp;
-	
-	
+library SafeMath {
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-	modifier isOwner {
-		require(owner == msg.sender);
-		_;
-	}
-	
-	modifier isRunning {
-		require(running);
-		_;
-	}
-	
-	
-	function TestCoin() public {
-		running = true;
-		owner = msg.sender;
-		decimals = 18;
-		totalSupply = 2 * uint(10)**(decimals + 9);
-		balances[owner] = totalSupply;
-		name = "TESTCOIN";
-		symbol = "TSC";
-		emit Transfer(address(0), owner, totalSupply);
-	}
-	
-	function transfer(address _to, uint256 _value) public isRunning returns (bool) {
-		require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-		balances[msg.sender] -= _value;
-		balances[_to] += _value;
-		emit Transfer(msg.sender, _to, _value);
-		return true;
-	}
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-	function transferFrom(address _from, address _to, uint256 _value) public isRunning returns (bool) {
-		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-		balances[_to] += _value;
-		balances[_from] -= _value;
-		allowed[_from][msg.sender] -= _value;
-		emit Transfer(_from, _to, _value);
-		return true;
-	}
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-	function balanceOf(address _owner) public constant returns (uint256) {
-		return balances[_owner];
-	}
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  
+}
 
-	function approve(address _spender, uint256 _value) public isRunning returns (bool) {
-		allowed[msg.sender][_spender] = _value;
-		emit Approval(msg.sender, _spender, _value);
-		return true;
-	}
+contract BasicToken is ERC20Basic {
+    
+  using SafeMath for uint256;
 
-	function allowance(address _owner, address _spender) public constant returns (uint256) {
-	  return allowed[_owner][_spender];
-	}
-	
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public isRunning returns (bool ok) {
-		TokenRecipient spender = TokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-			return true;
-		}
+  mapping(address => uint256) balances;
+
+  function transfer(address _to, uint256 _value) returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
+
+
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) returns (bool) {
+
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+}
+
+contract Ownable {
+    
+  address public owner;
+
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
+
+}
+
+
+contract MintableToken is StandardToken, Ownable {
+    
+  event Mint(address indexed to, uint256 amount);
+  
+  event MintFinished();
+
+  bool public mintingFinished = false;
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  }
+
+  function finishMinting() onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+  
+}
+
+contract Testcoin is MintableToken {
+    
+    string public constant name = "Testcoin Token";
+    
+    string public constant symbol = "TSTC";
+    
+    uint32 public constant decimals = 18;
+    
+}
+
+
+contract Crowdsale is Ownable {
+    
+    using SafeMath for uint;
+    
+    address multisig;
+
+    Testcoin public token = new Testcoin();
+
+    uint start;
+    
+    uint period;
+
+    uint hardcap;
+
+    uint rate;
+
+    function Crowdsale() {
+        multisig = 0x18A09596E20A84EC5915DC1EBdC0B13312C924cD;
+        rate = 500000000000000000000;
+        start = 1523998800;
+        period = 30;
+        hardcap = 250000000000000000000000;
+    }
+
+    modifier saleIsOn() {
+    	require(now > start && now < start + period * 1 days);
+    	_;
     }
 	
-	
-	
-	function setName(string _name) public isOwner {
-		name = _name;
-	}
-	
-	function setSymbol(string _symbol) public isOwner {
-		symbol = _symbol;
-	}
-	
-	function setRunning(bool _run) public isOwner {
-		running = _run;
-	}
-	
-	function transferOwnership(address _owner) public isOwner {
-		ownerTemp = _owner;
-	}
-	
-	function acceptOwnership() public {
-		require(msg.sender == ownerTemp);
-		owner = ownerTemp;
-		ownerTemp = 0x0;
-	}
-	
-	function collectERC20(address _token, uint _amount) public isRunning isOwner returns (bool success) {
-		return ERC20(_token).transfer(owner, _amount);
-	}
+    modifier isUnderHardCap() {
+        require(multisig.balance <= hardcap);
+        _;
+    }
+    
+    function finishMinting() public onlyOwner {
+    token.finishMinting();
+    }
+
+   function createTokens() isUnderHardCap saleIsOn payable {
+        multisig.transfer(msg.value);
+        uint tokens = rate.mul(msg.value).div(1 ether);
+        uint bonusTokens = 0;
+        if(now < start + (period * 1 days).div(3)) {
+          bonusTokens = tokens.div(5);
+        }
+        tokens += bonusTokens;
+        token.mint(msg.sender, tokens);
+    }
+
+    function() external payable {
+        createTokens();
+    }
+    
 }
