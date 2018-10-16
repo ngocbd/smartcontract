cc91b209f6962a8381ddba27701aa7ff09a39cb9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:15:57 +0700

    add contract CryptoRoulette

--- CryptoRoulette.sol
+++ CryptoRoulette.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CryptoRoulette at 0x652eB151869C2E8fA354F29321BA192D5d9F84DC
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CryptoRoulette at 0x94602b0E2512DdAd62a935763BF1277c973B2758
 */
 pragma solidity ^0.4.19;
 
@@ -8,7 +8,7 @@ pragma solidity ^0.4.19;
 // Guess the number secretly stored in the blockchain and win the whole contract balance!
 // A new number is randomly chosen after each try.
 //
-// To play, call the play() method with the guessed number (1-10).  Bet price: 0.1 ether
+// To play, call the play() method with the guessed number (1-20).  Bet price: 0.1 ether
 
 contract CryptoRoulette {
 
@@ -29,8 +29,8 @@ contract CryptoRoulette {
     }
 
     function shuffle() internal {
-        // randomly set secretNumber with a value between 1 and 10
-        secretNumber = uint8(sha3(now, block.blockhash(block.number-1))) % 10 + 1;
+        // randomly set secretNumber with a value between 1 and 20
+        secretNumber = uint8(sha3(now, block.blockhash(block.number-1))) % 20 + 1;
     }
 
     function play(uint256 number) payable public {
@@ -49,6 +49,7 @@ contract CryptoRoulette {
         shuffle();
         lastPlayed = now;
     }
+
     function kill() public {
         if (msg.sender == ownerAddr && now > lastPlayed + 1 days) {
             suicide(msg.sender);
