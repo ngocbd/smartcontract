915f52043bf22d60c002a0ddf4f7e47a1318a2dd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:04:08 +0700

    add contract ASCCrowdsale

--- ASCCrowdsale.sol
+++ ASCCrowdsale.sol
@@ -1,21 +1,21 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ASCCrowdsale at 0x3a5de2ba9b13f07d7624b9d24d7f42cd8fa97c06
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ASCCrowdsale at 0xbc4c35f48d95e76d05e65c0c2bb7a933c1ec9542
 */
 pragma solidity ^0.4.16;
 
 interface Token {
-    function transfer(address receiver, uint amount) public;
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 }
 
 contract ASCCrowdsale {
     
     Token public tokenReward;
-    address creator;
-    address owner = 0xb99776950E24a1D580d8c1622ab6C92002aEc169;
+    address public creator;
+    address public owner = 0xb99776950E24a1D580d8c1622ab6C92002aEc169;
 
+    uint256 public price;
     uint256 public startDate;
     uint256 public endDate;
-    uint256 public price;
 
     event FundTransfer(address backer, uint amount, bool isContribution);
 
@@ -24,7 +24,7 @@ contract ASCCrowdsale {
         startDate = 1452038400;     // 06/01/2018
         endDate = 1521586800;       // 21/03/2018
         price = 72000;
-        tokenReward = Token(0xE5b7301D818299487b744900923A40cF7d1f0242);
+        tokenReward = Token(0x9A7fF9c99ECa95Faea0d866a4cdaceaFf276D948);
     }
 
     function setOwner(address _owner) public {
@@ -35,58 +35,49 @@ contract ASCCrowdsale {
     function setCreator(address _creator) public {
         require(msg.sender == creator);
         creator = _creator;      
-    }    
-
-    function setStartDate(uint256 _startDate) public {
-        require(msg.sender == creator);
-        startDate = _startDate;      
-    }
-
-    function setEndDate(uint256 _endDate) public {
-        require(msg.sender == creator);
-        endDate = _endDate;      
     }
 
     function setPrice(uint256 _price) public {
         require(msg.sender == creator);
         price = _price;      
     }
-
-    function sendToken(address receiver, uint amount) public {
+    
+    function kill() public {
         require(msg.sender == creator);
-        tokenReward.transfer(receiver, amount);
-        FundTransfer(receiver, amount, true);    
+        selfdestruct(owner);
     }
 
     function () payable public {
         require(msg.value > 0);
         require(now > startDate);
         require(now < endDate);
-        uint amount = msg.value / 1 finney;
-	    amount *= price / 10;
-	    uint amount10 = amount / 10;
+	    uint256 _price = price / 10;
 
         // period 1 : 60%
         if(now > startDate && now < 1516230000) {
-            amount += amount10 * 6;
+            _price *= 6;
+            _price += price;
         }
 
         // Pperiod 2 : 40%
         if(now > 1516230000 && now < 1517439600) {
-            amount += amount10 * 4;
+            _price *= 4;
+            _price += price;
         }
 
         // period 3 : 20%
         if(now > 1517439600 && now < 1518649200) {
-            amount += amount10 * 2;
+            _price *= 2;
+            _price += price;
         }
 
         // period 4 : 10%
         if(now > 1518649200 && now < 1519858800) {
-            amount += amount10;
+            _price += price;
         }
-
-        tokenReward.transfer(msg.sender, amount);
+        uint amount = msg.value;
+        amount /= 10 finney;
+        tokenReward.transferFrom(owner, msg.sender, amount);
         FundTransfer(msg.sender, amount, true);
         owner.transfer(msg.value);
     }
