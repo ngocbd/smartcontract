88f6516ef9e927271f8b310c737c1203cc222d60
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:30:21 +0700

    add contract ProductItem

---
+++ ProductItem.sol
@@ -0,0 +1,40 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ProductItem at 0x68E2f9a46A802775d99D5421E466DD70e2bB4012
+*/
+pragma solidity ^0.4.19;
+
+contract ProductItem {
+
+    address[] public _owners;
+    address public _currentOwner;
+    address public _nextOwner;
+    string public _productDigest;
+
+    function ProductItem(string productDigest) public {
+        _currentOwner = msg.sender;
+        _productDigest = productDigest;
+        _owners.push(msg.sender);
+    }
+
+    function setNextOwner(address nextOwner) public returns(bool set) {
+        if (_currentOwner != msg.sender) {
+            return false;
+        }
+
+        _nextOwner = nextOwner;
+
+        return true;
+    }
+
+    function confirmOwnership() public returns(bool confirmed) {
+        if (_nextOwner != msg.sender) {
+            return false;
+        }
+
+        _owners.push(_nextOwner);
+        _currentOwner = _nextOwner;
+        _nextOwner = address(0);
+
+        return true;
+    }
+}
