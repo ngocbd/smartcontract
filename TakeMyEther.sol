12b7dfa656954db8fa74667d46488fcb2e4f6831
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:14:06 +0700

    add contract TakeMyEther

---
+++ TakeMyEther.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TakeMyEther at 0x9bf79d314da6371a36ede8efd0aad1ce617ff4b1
+*/
+contract TakeMyEther {
+    function() {
+        selfdestruct(msg.sender);
+    }
+}
