fcaa779a263f5e87deb89614fdfe3ec1cf93821e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:52:47 +0700

    add contract SimpleTest

---
+++ SimpleTest.sol
@@ -0,0 +1,13 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleTest at 0x6918bf274c2417b866a87c7659dfa91acebd51f4
+*/
+pragma solidity ^0.4.17;
+
+contract SimpleTest
+{
+    string public blablabla;
+    function SimpleTest() public
+    {
+        blablabla = "Hello world!";
+    }
+}
