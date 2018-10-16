f4b1c847f45a6e0d8a4ed48268184d32364f4c17
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:28:49 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x5df9b36adc69dfaeeff71efa2f5e1e44e4b3c9c4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x24ba8f48f225aed3abb6760fdce05c058e474c3e
 */
 pragma solidity ^0.4.4;
 
@@ -119,17 +119,17 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] = 3000000000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 3000000000000000;                        // Update total supply (100000 for example)
-        name = "hypnosis";                                   // Set the name for display purposes
+        balances[msg.sender] = 30000000000000000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 30000000000000000000;                        // Update total supply (100000 for example)
+        name = "cryptosocialtoken";                                   // Set the name for display purposes
         decimals = 8;                            // Amount of decimals for display purposes
-        symbol = "hyp";                               // Set the symbol for display purposes
+        symbol = "CST";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);  
+        Approval(msg.sender, _spender, _value);
 
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
