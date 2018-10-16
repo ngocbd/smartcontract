6817cd2073924a408b2511e0f098ba3d32f8ca56
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:56:08 +0700

    add contract ZRXToken

--- ZRXToken.sol
+++ ZRXToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ZRXToken at 0xe41d2489571d322189246dafa5ebde1f4699f498
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ZRXToken at 0xb70ae1b7a95c3443af3c7f18b7f4e08cda58bbe7
 */
 /*
 
@@ -19,7 +19,27 @@
 
 */
 
-pragma solidity 0.4.11;
+
+/*
+
+  Copyright 2017 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+
+
 
 contract Token {
 
@@ -57,6 +77,7 @@ contract Token {
     event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
+
 
 contract StandardToken is Token {
 
@@ -99,10 +120,11 @@ contract StandardToken is Token {
     uint public totalSupply;
 }
 
+
 contract UnlimitedAllowanceToken is StandardToken {
 
     uint constant MAX_UINT = 2**256 - 1;
-    
+
     /// @dev ERC20 transferFrom, modified such that an allowance of MAX_UINT represents an unlimited allowance.
     /// @param _from Address to transfer from.
     /// @param _to Address to transfer to.
@@ -129,6 +151,7 @@ contract UnlimitedAllowanceToken is StandardToken {
         }
     }
 }
+
 
 contract ZRXToken is UnlimitedAllowanceToken {
 
