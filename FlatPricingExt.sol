15c1a18d8fc5715ae4a255eb87ca3e8af77575bf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:58:42 +0700

    add contract FlatPricingExt

--- FlatPricingExt.sol
+++ FlatPricingExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricingExt at 0x15F3f05c75749Be657A757DC851261D9fb95464C
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlatPricingExt at 0xcDDAfAa86726DC4F650cb61090adD1c8f449905F
 */
 // Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 pragma solidity ^0.4.11;
