c785d29df44dff101245645ccb0805401c88a8a6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:09:34 +0700

    add contract contractSKbasic

--- contractSKbasic.sol
+++ contractSKbasic.sol
@@ -1,23 +1,23 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract contractSKbasic at 0xd2fB71FbD065157B70268eB13a6d7f8F11e10857
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract contractSKbasic at 0x5b3c2581171eec91d104caf4572a4806d8c8380a
 */
 pragma solidity 0.4.11;
 
 contract contractSKbasic{
-
+    
     string name1 = "Persona 1";
     string name2 = "Persona 2";
     uint date = now;
-
-    function setContractSK(string intervener1, string intervener2){
+    
+    function setContract(string intervener1, string intervener2){
         date = now;
         name1 = intervener1;
         name2 = intervener2;
-    }
-
-
-    function getContractDataSK() constant returns(string, string, uint){
+    } 
+    
+    
+    function getContractData() constant returns(string, string, uint){
         return (name1, name2, date) ;
     }
-
+    
 }
