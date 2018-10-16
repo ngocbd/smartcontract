66b7550ab51da3052019b2a3e124a3e704192360
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:31:56 +0700

    add contract HumanStandardToken

--- HumanStandardToken.sol
+++ HumanStandardToken.sol
@@ -1,58 +1,45 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HumanStandardToken at 0x9643545c3E3117dA7812d985A2c98F787585daf0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HumanStandardToken at 0xdecd5780a9d50a96f34ce7e9e636903f6f3b47f5
 */
 pragma solidity ^0.4.8;
 contract Token{
-    // token???????public??????getter????????totalSupply().
     uint256 public totalSupply;
 
-    /// ????_owner??token??? 
     function balanceOf(address _owner) constant returns (uint256 balance);
 
-    //??????????_to??????_value?token
     function transfer(address _to, uint256 _value) returns (bool success);
 
-    //???_from????_to????_value?token??approve??????
     function transferFrom(address _from, address _to, uint256 _value) returns   
     (bool success);
 
-    //??????????_spender????????????_value?token
     function approve(address _spender, uint256 _value) returns (bool success);
 
-    //????_spender?????_owner???token???
     function allowance(address _owner, address _spender) constant returns 
     (uint256 remaining);
 
-    //????????????? 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    //???approve(address _spender, uint256 _value)????????????
     event Approval(address indexed _owner, address indexed _spender, uint256 
     _value);
 }
 
 contract StandardToken is Token {
     function transfer(address _to, uint256 _value) returns (bool success) {
-        //??totalSupply ??????? (2^256 - 1).
-        //??????????????token??????????????????
-        //require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;//???????????token??_value
-        balances[_to] += _value;//???????token??_value
-        Transfer(msg.sender, _to, _value);//????????
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
         return true;
     }
 
 
     function transferFrom(address _from, address _to, uint256 _value) returns 
     (bool success) {
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= 
-        // _value && balances[_to] + _value > balances[_to]);
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;//??????token??_value
-        balances[_from] -= _value; //????_from??token??_value
-        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
-        Transfer(_from, _to, _value);//????????
+        balances[_to] += _value;
+        balances[_from] -= _value; 
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
         return true;
     }
     function balanceOf(address _owner) constant returns (uint256 balance) {
@@ -69,7 +56,7 @@ contract StandardToken is Token {
 
 
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];//??_spender?_owner????token?
+        return allowed[_owner][_spender];
     }
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
@@ -78,13 +65,13 @@ contract StandardToken is Token {
 contract HumanStandardToken is StandardToken { 
 
     /* Public variables of the token */
-    string public name;                   //??: eg Simon Bucks
+    string public name;                   
     uint8 public decimals;               //????????How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;               //token??: eg SBX
     string public version = 'H0.1';    //??
 
     function HumanStandardToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
-        balances[msg.sender] = _initialAmount; // ??token?????????
+        balances[msg.sender] = _initialAmount; 
         totalSupply = _initialAmount;         // ??????
         name = _tokenName;                   // token??
         decimals = _decimalUnits;           // ????
