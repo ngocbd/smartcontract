d93cb4b25eb0ee3a0663ece8f81de6969d364958
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:11:19 +0700

    add contract BitandPay

--- BitandPay.sol
+++ BitandPay.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitandPay at 0x4b2ca6b25e063d8dce5bd862f99669a67ec44c9d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitandPay at 0xf0ff1e6daeb276cee781fd6498f2eef5c66efc8e
 */
 pragma solidity 0.4.19;
 
@@ -68,8 +68,8 @@ contract BitandPay is ERC20Interface {
 
     function BitandPay() public {
         owner = msg.sender;
-        balances[owner] = 250000000;
-        Transfer(0x0, owner, 250000000);
+        balances[this] = 250000000;
+        Transfer(0x0, this, 250000000);
     }
 
     function totalSupply() constant public returns (uint256 total) {
@@ -153,21 +153,22 @@ contract BitandPay is ERC20Interface {
     }
 
     function buyTokens(address purchaser) payable whenNotPaused public {
+        require(purchaser != address(0));
         require(validPurchase());
 
         uint256 weiAmount = msg.value;
 
         // calculate token amount to be created
-        uint256 tokens = weiAmount.mul(price);
-        require(balances[this] > tokens);
+        uint256 tokens = weiAmount.div(price);
 
         // update state
         weiRaised = weiRaised.add(weiAmount);
 
-        balances[purchaser] = balances[purchaser].add(tokens);  // adds the amount to buyer's balance
         balances[this] = balances[this].sub(tokens);            // subtracts amount from seller's balance
+        balances[purchaser] = balances[purchaser].add(tokens);  // adds the amount to buyer's balance
+
         Transfer(this, purchaser, tokens);                      // execute an event reflecting the change
-        TokenPurchase(purchaser, weiAmount, tokens);
+        // TokenPurchase(purchaser, weiAmount, tokens);
     }
 
     function validPurchase() internal constant returns (bool) {
