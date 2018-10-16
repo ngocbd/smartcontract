216abf217150bde68f098f1bbda9c014bd5e7bfd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:12:49 +0700

    add contract TestRevert

--- TestRevert.sol
+++ TestRevert.sol
@@ -1,14 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestRevert at 0x91583c4cca21ee968adee187bdb94186da992b73
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestRevert at 0xa86e4aeddf4336940e17d524000f1a0cef7c1fe9
 */
-pragma solidity ^0.4.0;
-
 contract TestRevert {
-    function test_require() public {
-        require(now < 1000);
+    
+    function revertMe() {
+        require(false);
     }
-
-    function test_assert() public {
-        assert(now < 1000);
+    
+    function throwMe() {
+        throw;
     }
 }
