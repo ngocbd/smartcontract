c9ffe5cb0e84767bf8a6cb2eb2ac4c01f0b84b8c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:04:30 +0700

    add contract Conteract

---
+++ Conteract.sol
@@ -0,0 +1,46 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Conteract at 0xFECbDc28F7fd30f24170fDE058D36a29C21B3fad
+*/
+pragma solidity ^0.4.24;
+
+interface token {
+    function transfer(address receiver, uint amount) external returns (bool);
+}
+
+contract Conteract {
+
+    event eDonate(address donor, uint256 value);
+    event eSuggest(address person, string suggestion);
+
+    string public About;
+    address public Creator;
+    mapping (address => uint256) public Donors;
+    mapping (address => string) public Suggestions;
+
+    constructor(string about) public {
+        Creator = msg.sender;
+        About = about;
+    }
+
+    function Suggest(string suggestion) public {
+        Suggestions[msg.sender] = suggestion;
+        emit eSuggest(msg.sender, suggestion);
+    }
+
+    function Donate() payable public {
+        Creator.transfer(msg.value);
+        Donors[msg.sender] += msg.value;
+        emit eDonate(msg.sender, msg.value);
+    }
+
+    function CollectERC20(address tokenAddress, uint256 amount) public {
+        require(msg.sender == Creator);
+        token tokenTransfer = token(tokenAddress);
+        tokenTransfer.transfer(Creator, amount);
+    }
+
+    function () payable public {
+        Creator.transfer(msg.value);
+    }
+
+}
