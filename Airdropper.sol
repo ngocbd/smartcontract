aea134564091f41d185bc7958db02a62b1f03fd5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:24:18 +0700

    add contract Airdropper

--- Airdropper.sol
+++ Airdropper.sol
@@ -1,71 +1,69 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0xc8a1b7d0e86f2ed64e9c56f6922050f84029106c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdropper at 0x71f6b9cb2160454c9219007a948ae55853bfcd44
 */
-pragma solidity ^0.4.18;
- 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
+pragma solidity ^0.4.11;
+
 contract Ownable {
   address public owner;
- 
-  /**
+
+
+  /** 
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() public {
+  function Ownable() {
     owner = msg.sender;
   }
 
+
   /**
-   * @dev Throws if called by any account other than the owner.
+   * @dev Throws if called by any account other than the owner. 
    */
   modifier onlyOwner() {
-    require(msg.sender == owner);
+    if (msg.sender != owner) {
+      throw;
+    }
     _;
   }
- 
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to. 
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
+    }
+  }
+
 }
 
- 
-interface IERC20 {
-  function totalSupply() public constant returns (uint256 );
-  function balanceOf(address _owner) public constant returns (uint256 );
-  function transfer(address _to, uint256 _value) public returns (bool );
-  function decimals() public constant returns (uint8 decimals);
-  //function transferFrom(address _from, address _to, uint256 _value) public returns (bool );
-  //function approve(address _spender, uint256 _value) public returns (bool );
-  //function allowance(address _owner, address _spender) public constant returns (uint256 );
-  //event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  //event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+contract ERC20Basic {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
 }
- 
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint);
+  function transferFrom(address from, address to, uint value);
+  function approve(address spender, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+
 contract Airdropper is Ownable {
-    
-    function batchTransfer(address[] _recipients, uint[] _values, address _tokenAddress) onlyOwner public returns (bool) {
-        require( _recipients.length > 0 && _recipients.length == _values.length);
- 
-        IERC20 token = IERC20(_tokenAddress);
-        // uint8 decimals = token.decimals();
 
-        // uint total = 0;
-        // for(uint i = 0; i < _values.length; i++){
-        //     total += _values[i];
-        // }
-        // require(total <= token.balanceOf(this));
-        
-        for(uint j = 0; j < _recipients.length; j++){
-            token.transfer(_recipients[j], _values[j]  );
+    function multisend(address _tokenAddr, address[] dests, uint256[] values)
+    onlyOwner
+    returns (uint256) {
+        uint256 i = 0;
+        while (i < dests.length) {
+           ERC20(_tokenAddr).transfer(dests[i], values[i]);
+           i += 1;
         }
- 
-        return true;
+        return(i);
     }
- 
-     function withdrawalToken(address _tokenAddress) onlyOwner public { 
-        IERC20 token = IERC20(_tokenAddress);
-        require(token.transfer(owner, token.balanceOf(this)));
-    }
-
 }
