382bac51842aaf7128f27c6156f2f89a15b614c3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:14:16 +0700

    add contract Asset

--- Asset.sol
+++ Asset.sol
@@ -1,10 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Asset at 0x2313f908beddcb07565cdb48354523981d3a01c2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Asset at 0xfae9cd03c11e15cf611fd935bd66026fe6611d39
 */
-pragma solidity 0.4.20;
+pragma solidity 0.4.21;
 /**
-* @notice TOKEN CONTRACT
-* @dev ERC-20 Token Standard Compliant
+* TOKEN Contract
+* ERC-20 Token Standard Compliant
 * @author Fares A. Akel C. f.antonio.akel@gmail.com
 */
 
@@ -28,12 +28,11 @@ library SafeMath {
 }
 
 /**
- * @title ERC20TokenInterface
- * @dev Token contract interface for external use
+ * Token contract interface for external use
  */
 contract ERC20TokenInterface {
 
-    function balanceOf(address _owner) public constant returns (uint256 balance);
+    function balanceOf(address _owner) public constant returns (uint256 value);
     function transfer(address _to, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
     function approve(address _spender, uint256 _value) public returns (bool success);
@@ -43,74 +42,183 @@ contract ERC20TokenInterface {
 
 
 /**
-* @title ERC20Token
-* @notice Token definition contract
+* @title Admin parameters
+* @dev Define administration parameters for this contract
 */
-contract ERC20Token is ERC20TokenInterface { //Standard definition of an ERC20Token
-    using SafeMath for uint256; //SafeMath is used for uint256 operations
+contract admined { //This token contract is administered
+    address public admin; //Admin address is public
+    bool public lockSupply; //Mint and Burn Lock flag
+    bool public lockTransfer; //Transfer Lock flag
+    address public allowedAddress; //an address that can override lock condition
+
+    /**
+    * @dev Contract constructor
+    * define initial administrator
+    */
+    function admined() internal {
+        admin = msg.sender; //Set initial admin to contract creator
+        emit Admined(admin);
+    }
+
+   /**
+    * @dev Function to set an allowed address
+    * @param _to The address to give privileges.
+    */
+    function setAllowedAddress(address _to) onlyAdmin public {
+        allowedAddress = _to;
+        emit AllowedSet(_to);
+    }
+
+    modifier onlyAdmin() { //A modifier to define admin-only functions
+        require(msg.sender == admin);
+        _;
+    }
+
+    modifier supplyLock() { //A modifier to lock mint and burn transactions
+        require(lockSupply == false);
+        _;
+    }
+
+    modifier transferLock() { //A modifier to lock transactions
+        require(lockTransfer == false || allowedAddress == msg.sender);
+        _;
+    }
+
+   /**
+    * @dev Function to set new admin address
+    * @param _newAdmin The address to transfer administration to
+    */
+    function transferAdminship(address _newAdmin) onlyAdmin public { //Admin can be transfered
+        require(_newAdmin != 0);
+        admin = _newAdmin;
+        emit TransferAdminship(admin);
+    }
+
+   /**
+    * @dev Function to set mint and burn locks
+    * @param _set boolean flag (true | false)
+    */
+    function setSupplyLock(bool _set) onlyAdmin public { //Only the admin can set a lock on supply
+        lockSupply = _set;
+        emit SetSupplyLock(_set);
+    }
+
+   /**
+    * @dev Function to set transfer lock
+    * @param _set boolean flag (true | false)
+    */
+    function setTransferLock(bool _set) onlyAdmin public { //Only the admin can set a lock on transfers
+        lockTransfer = _set;
+        emit SetTransferLock(_set);
+    }
+
+    //All admin actions have a log for public review
+    event AllowedSet(address _to);
+    event SetSupplyLock(bool _set);
+    event SetTransferLock(bool _set);
+    event TransferAdminship(address newAdminister);
+    event Admined(address administer);
+
+}
+
+/**
+* @title Token definition
+* @dev Define token paramters including ERC20 ones
+*/
+contract ERC20Token is ERC20TokenInterface, admined { //Standard definition of a ERC20Token
+    using SafeMath for uint256;
+    uint256 public totalSupply;
     mapping (address => uint256) balances; //A mapping of all balances per address
     mapping (address => mapping (address => uint256)) allowed; //A mapping of all allowances
-    uint256 public totalSupply;
-    
+
     /**
-    * @notice Get the balance of an _owner address.
+    * @dev Get the balance of an specified address.
     * @param _owner The address to be query.
     */
-    function balanceOf(address _owner) public constant returns (uint256 bal) {
+    function balanceOf(address _owner) public constant returns (uint256 value) {
       return balances[_owner];
     }
 
     /**
-    * @notice transfer _value tokens to address _to
+    * @dev transfer token to a specified address
     * @param _to The address to transfer to.
     * @param _value The amount to be transferred.
-    * @return success with boolean value true if done
     */
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(address _to, uint256 _value) transferLock public returns (bool success) {
         require(_to != address(0)); //If you dont want that people destroy token
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     /**
-    * @notice Transfer _value tokens from address _from to address _to using allowance msg.sender allowance on _from
-    * @param _from The address where tokens comes.
+    * @dev transfer token from an address to another specified address using allowance
+    * @param _from The address where token comes.
     * @param _to The address to transfer to.
     * @param _value The amount to be transferred.
-    * @return success with boolean value true if done
     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) transferLock public returns (bool success) {
         require(_to != address(0)); //If you dont want that people destroy token
-        balances[_from] = balances[_from].sub(_value);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
     /**
-    * @notice Assign allowance _value to _spender address to use the msg.sender balance
+    * @dev Assign allowance to an specified address to use the owner balance
     * @param _spender The address to be allowed to spend.
     * @param _value The amount to be allowed.
-    * @return success with boolean value true
     */
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0)); //exploit mitigation
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
     /**
-    * @notice Get the allowance of an specified address to use another address balance.
+    * @dev Get the allowance of an specified address to use another address balance.
     * @param _owner The address of the owner of the tokens.
     * @param _spender The address of the allowed spender.
-    * @return remaining with the allowance value
     */
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
+    }
+
+    /**
+    * @dev Mint token to own address.
+    * @param _mintedAmount amount to mint.
+    */
+    function mintToken(uint256 _mintedAmount) onlyAdmin supplyLock public {
+        require(totalSupply.add(_mintedAmount) < 250000000 * (10**18)); //Max supply ever
+        balances[msg.sender] = SafeMath.add(balances[msg.sender], _mintedAmount);
+        totalSupply = SafeMath.add(totalSupply, _mintedAmount);
+        emit Transfer(0, this, _mintedAmount);
+        emit Transfer(this, msg.sender, _mintedAmount);
+    }
+
+    /**
+    * @dev Burn token from own address.
+    * @param _burnedAmount amount to burn.
+    */
+    function burnToken(uint256 _burnedAmount) onlyAdmin supplyLock public {
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _burnedAmount);
+        totalSupply = SafeMath.sub(totalSupply, _burnedAmount);
+        emit Burned(msg.sender, _burnedAmount);
+    }
+
+    /**
+    * @dev This is an especial function to make massive tokens assignments
+    * @param data array of addresses to transfer to
+    * @param amount array of amounts to tranfer to each address
+    */
+    function batch(address[] data,uint256[] amount) public { //It takes an array of addresses and an amount
+        require(data.length == amount.length);//same array sizes
+        for (uint i=0; i<data.length; i++) { //It moves over the array
+            transfer(data[i],amount[i]);
+        }
     }
 
     /**
@@ -118,38 +226,30 @@ contract ERC20Token is ERC20TokenInterface { //Standard definition of an ERC20To
     */
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
+    event Burned(address indexed _target, uint256 _value);
 }
 
 /**
 * @title Asset
-* @notice Token creation.
-* @dev ERC20 Token
+* @dev Initial supply creation
+* @notice Supply is initially unlocked for minting
 */
 contract Asset is ERC20Token {
-    string public name;
-    uint8 public decimals;
-    string public symbol;
+    string public name = 'Citereum';
+    uint8 public decimals = 18;
+    string public symbol = 'CTR';
     string public version = '1';
-    
-    /**
-    * @notice token constructor.
-    */
-    function Asset() public {
 
-        name = 'Big Balls Token';
-        decimals = 18;
-        symbol = 'BBT';
-        totalSupply = 150000000 * 10 ** uint256(decimals); //Tokens initial supply;
+    function Asset() public {
+        totalSupply = 12500000 * (10**uint256(decimals)); //initial token creation
         balances[msg.sender] = totalSupply;
-        Transfer(0, this, totalSupply);
-        Transfer(this, msg.sender, balances[msg.sender]);
-        
+
+        emit Transfer(0, this, totalSupply);
+        emit Transfer(this, msg.sender, balances[msg.sender]);
     }
     
     /**
-    * @notice this contract will revert on direct non-function calls
-    * @dev Function to handle callback calls
+    *@dev Function to handle callback calls
     */
     function() public {
         revert();
