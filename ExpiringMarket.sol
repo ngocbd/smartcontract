49d3987c749da8894f690883c778884e3c827f06
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:29 +0700

    add contract ExpiringMarket

--- ExpiringMarket.sol
+++ ExpiringMarket.sol
@@ -1,10 +1,36 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ExpiringMarket at 0x0651f66037c713cc5f0e015a054f47e2bfb6d565
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ExpiringMarket at 0x37d65ca0754f12c7eec1c86b0915fba9fcbda131
 */
+pragma solidity ^0.4.2;
+
+// Token standard API
+// https://github.com/ethereum/EIPs/issues/20
+contract ERC20Constant {
+    function totalSupply() constant returns (uint supply);
+    function balanceOf( address who ) constant returns (uint value);
+    function allowance(address owner, address spender) constant returns (uint _allowance);
+}
+contract ERC20Stateful {
+    function transfer( address to, uint value) returns (bool ok);
+    function transferFrom( address from, address to, uint value) returns (bool ok);
+    function approve(address spender, uint value) returns (bool ok);
+}
+contract ERC20Events {
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval( address indexed owner, address indexed spender, uint value);
+}
+contract ERC20 is ERC20Constant, ERC20Stateful, ERC20Events {}
+
 contract Assertive {
     function assert(bool assertion) internal {
         if (!assertion) throw;
     }
+}
+
+contract FallbackFailer {
+  function () {
+    throw;
+  }
 }
 
 contract MutexUser {
@@ -12,26 +38,9 @@ contract MutexUser {
     modifier exclusive {
         if (lock) throw;
         lock = true;
-        _
+        _;
         lock = false;
     }
-}
-contract ERC20 {
-    function totalSupply() constant returns (uint);
-    function balanceOf(address who) constant returns (uint);
-    function allowance(address owner, address spender) constant returns (uint);
-
-    function transfer(address to, uint value) returns (bool ok);
-    function transferFrom(address from, address to, uint value) returns (bool ok);
-    function approve(address spender, uint value) returns (bool ok);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-contract FallbackFailer {
-  function () {
-    throw;
-  }
 }
 
 // A simple direct exchange order manager.
@@ -41,6 +50,7 @@ contract EventfulMarket {
     event Trade( uint sell_how_much, address indexed sell_which_token,
                  uint buy_how_much, address indexed buy_which_token );
 }
+
 contract SimpleMarket is EventfulMarket
                        , Assertive
                        , FallbackFailer
@@ -63,16 +73,16 @@ contract SimpleMarket is EventfulMarket
     }
 
     modifier can_offer {
-        _
+        _;
     }
     modifier can_buy(uint id) {
         assert(isActive(id));
-        _
+        _;
     }
     modifier can_cancel(uint id) {
         assert(isActive(id));
         assert(getOwner(id) == msg.sender);
-        _
+        _;
     }
     function isActive(uint id) constant returns (bool active) {
         return offers[id].active;
@@ -214,81 +224,18 @@ contract ExpiringMarket is SimpleMarket {
     // after market lifetime has elapsed, no new offers are allowed
     modifier can_offer {
         assert(!isClosed());
-        _
+        _;
     }
     // after close, no new buys are allowed
     modifier can_buy(uint id) {
         assert(isActive(id));
         assert(!isClosed());
-        _
+        _;
     }
     // after close, anyone can cancel an offer
     modifier can_cancel(uint id) {
         assert(isActive(id));
         assert(isClosed() || (msg.sender == getOwner(id)));
-        _
-    }
-}
-
-// Flat file implementation of `dappsys/token/base.sol::DSTokenBase`
-
-// Everything throws instead of returning false on failure.
-
-contract ERC20Base is ERC20
-{
-    mapping( address => uint ) _balances;
-    mapping( address => mapping( address => uint ) ) _approvals;
-    uint _supply;
-    function ERC20Base( uint initial_balance ) {
-        _balances[msg.sender] = initial_balance;
-        _supply = initial_balance;
-    }
-    function totalSupply() constant returns (uint supply) {
-        return _supply;
-    }
-    function balanceOf( address who ) constant returns (uint value) {
-        return _balances[who];
-    }
-    function transfer( address to, uint value) returns (bool ok) {
-        if( _balances[msg.sender] < value ) {
-            throw;
-        }
-        if( !safeToAdd(_balances[to], value) ) {
-            throw;
-        }
-        _balances[msg.sender] -= value;
-        _balances[to] += value;
-        Transfer( msg.sender, to, value );
-        return true;
-    }
-    function transferFrom( address from, address to, uint value) returns (bool ok) {
-        // if you don't have enough balance, throw
-        if( _balances[from] < value ) {
-            throw;
-        }
-        // if you don't have approval, throw
-        if( _approvals[from][msg.sender] < value ) {
-            throw;
-        }
-        if( !safeToAdd(_balances[to], value) ) {
-            throw;
-        }
-        // transfer and return true
-        _approvals[from][msg.sender] -= value;
-        _balances[from] -= value;
-        _balances[to] += value;
-        Transfer( from, to, value );
-        return true;
-    }
-    function approve(address spender, uint value) returns (bool ok) {
-        _approvals[msg.sender][spender] = value;
-        Approval( msg.sender, spender, value );
-        return true;
-    }
-    function allowance(address owner, address spender) constant returns (uint _allowance) {
-        return _approvals[owner][spender];
-    }
-    function safeToAdd(uint a, uint b) internal returns (bool) {
-        return (a + b >= a);
+        _;
     }
 }
