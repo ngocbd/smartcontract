1d48256eceabe97f4736c003f35fb3ed2cb92cfa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:23:08 +0700

    add contract TinyProxyFactory

--- TinyProxyFactory.sol
+++ TinyProxyFactory.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TinyProxyFactory at 0xa35e72415bebed1aad8c0308ee1e0eda746773e7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TinyProxyFactory at 0xb94c06b81d11bf68cbafe3694272bfc1d4edd9f1
 */
 pragma solidity ^0.4.17;
 
@@ -18,24 +18,25 @@ contract TinyProxy {
 
   function release() public {
     uint balance = address(this).balance;
-    FundsReleased(receiver, balance);
     if(gasBudget > 0){
-      receiver.call.gas(gasBudget).value(balance);
+      require(receiver.call.gas(gasBudget).value(balance)());
     } else {
-      receiver.transfer(balance);
+      require(receiver.send(balance));
     }
+    FundsReleased(receiver, balance);
   }
 }
 
-
 contract TinyProxyFactory {
   mapping(address => mapping(address => address)) public proxyFor;
+  mapping(address => address[]) public userProxies;
 
-  function make(address to, uint gas,  bool track) public returns(address proxy){
+  function make(address to, uint gas, bool track) public returns(address proxy){
     proxy = new TinyProxy(to, gas);
-    if(track && proxyFor[to][msg.sender] == 0x0) {
-     proxyFor[msg.sender][to] = proxy; 
-    } 
+    if(track) {
+      proxyFor[msg.sender][to] = proxy;
+      userProxies[msg.sender].push(proxy);
+    }
     return proxy;
   }
 }
