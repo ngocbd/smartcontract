82f1af35809bc97817aabc045497669170c69ade
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:12:48 +0700

    add contract testContract

---
+++ testContract.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract testContract at 0xa3483b08c8a0f33eb07aff3a66fbcaf5c9018cdc
+*/
+contract testContract {    
+   function go() constant returns (string) {        
+      return "Hello Ethereum!";    
+   }
+}
