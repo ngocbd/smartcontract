86053d9b1c947e5428aefa14e8bf39a2ded20b2c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:15:36 +0700

    add contract SDT

--- SDT.sol
+++ SDT.sol
@@ -1,19 +1,149 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SDT at 0xb928ab57b3b81de54ea675eefec744f442c2dc8f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SDT at 0x3452282f3b9d93f1f79cc168631fe98bb9673f77
 */
 pragma solidity ^0.4.18;
 
+// File: contracts/IEscrow.sol
+
 /**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
+ * @title Escrow interface
+ *
+ * @dev https://send.sd/token
  */
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+interface IEscrow {
+
+  event Created(
+    address indexed sender,
+    address indexed recipient,
+    address indexed arbitrator,
+    uint256 transactionId
+  );
+  event Released(address indexed arbitrator, address indexed sentTo, uint256 transactionId);
+  event Dispute(address indexed arbitrator, uint256 transactionId);
+  event Paid(address indexed arbitrator, uint256 transactionId);
+
+  function create(
+      address _sender,
+      address _recipient,
+      address _arbitrator,
+      uint256 _transactionId,
+      uint256 _tokens,
+      uint256 _fee,
+      uint256 _expiration
+  ) public;
+
+  function fund(
+      address _sender,
+      address _arbitrator,
+      uint256 _transactionId,
+      uint256 _tokens,
+      uint256 _fee
+  ) public;
+
 }
+
+// File: contracts/ISendToken.sol
+
+/**
+ * @title ISendToken - Send Consensus Network Token interface
+ * @dev token interface built on top of ERC20 standard interface
+ * @dev see https://send.sd/token
+ */
+interface ISendToken {
+  function transfer(address to, uint256 value) public returns (bool);
+
+  function isVerified(address _address) public constant returns(bool);
+
+  function verify(address _address) public;
+
+  function unverify(address _address) public;
+
+  function verifiedTransferFrom(
+      address from,
+      address to,
+      uint256 value,
+      uint256 referenceId,
+      uint256 exchangeRate,
+      uint256 fee
+  ) public;
+
+  function issueExchangeRate(
+      address _from,
+      address _to,
+      address _verifiedAddress,
+      uint256 _value,
+      uint256 _referenceId,
+      uint256 _exchangeRate
+  ) public;
+
+  event VerifiedTransfer(
+      address indexed from,
+      address indexed to,
+      address indexed verifiedAddress,
+      uint256 value,
+      uint256 referenceId,
+      uint256 exchangeRate
+  );
+}
+
+// File: contracts/ISnapshotToken.sol
+
+/**
+ * @title Snapshot Token
+ *
+ * @dev Snapshot Token interface
+ * @dev https://send.sd/token
+ */
+interface ISnapshotToken {
+  function requestSnapshots(uint256 _blockNumber) public;
+  function takeSnapshot(address _owner) public returns(uint256);
+}
+
+// File: zeppelin-solidity/contracts/ownership/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+// File: zeppelin-solidity/contracts/math/SafeMath.sol
 
 /**
  * @title SafeMath
@@ -48,7 +178,21 @@ library SafeMath {
   }
 }
 
+// File: zeppelin-solidity/contracts/token/ERC20Basic.sol
 
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+// File: zeppelin-solidity/contracts/token/BasicToken.sol
 
 /**
  * @title Basic token
@@ -86,9 +230,7 @@ contract BasicToken is ERC20Basic {
 
 }
 
-
-
-
+// File: zeppelin-solidity/contracts/token/ERC20.sol
 
 /**
  * @title ERC20 interface
@@ -101,6 +243,7 @@ contract ERC20 is ERC20Basic {
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+// File: zeppelin-solidity/contracts/token/StandardToken.sol
 
 /**
  * @title Standard ERC20 token
@@ -159,10 +302,14 @@ contract StandardToken is ERC20, BasicToken {
   }
 
   /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   *
    * approve should be called when allowed[_spender] == 0. To increment
    * allowed value is better to use this function to avoid 2 calls (and wait until
    * the first transaction is mined)
    * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _addedValue The amount of tokens to increase the allowance by.
    */
   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
     allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
@@ -170,6 +317,16 @@ contract StandardToken is ERC20, BasicToken {
     return true;
   }
 
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _subtractedValue The amount of tokens to decrease the allowance by.
+   */
   function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
     uint oldValue = allowed[msg.sender][_spender];
     if (_subtractedValue > oldValue) {
@@ -183,20 +340,318 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
+// File: contracts/SnapshotToken.sol
 
-contract SDT is StandardToken {
-  string public name = 'SEND';
-  string public symbol = 'SDT';
-  uint256 public decimals = 18;
+/**
+ * @title Snapshot Token
+ *
+ * @dev Snapshot Token implementtion
+ * @dev https://send.sd/token
+ */
+contract SnapshotToken is ISnapshotToken, StandardToken, Ownable {
+  uint256 public snapshotBlock;
 
-  address public owner;
+  mapping (address => Snapshot) internal snapshots;
 
-  function SDT () public {
-
-    totalSupply = 700000000 * 10 ** decimals;
-    owner = msg.sender;
-    balances[owner] = totalSupply;
-
+  struct Snapshot {
+    uint256 block;
+    uint256 balance;
   }
 
+  address public polls;
+
+  modifier isPolls() {
+    require(msg.sender == address(polls));
+    _;
+  }
+
+  /**
+   * @dev Remove Verified status of a given address
+   * @notice Only contract owner
+   * @param _address Address to unverify
+   */
+  function setPolls(address _address) public onlyOwner {
+    polls = _address;
+  }
+
+  /**
+   * @dev Extend OpenZeppelin's BasicToken transfer function to store snapshot
+   * @param _to The address to transfer to.
+   * @param _value The amount to be transferred.
+   */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    takeSnapshot(msg.sender);
+    takeSnapshot(_to);
+    return BasicToken.transfer(_to, _value);
+  }
+
+  /**
+   * @dev Extend OpenZeppelin's StandardToken transferFrom function to store snapshot
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    takeSnapshot(_from);
+    takeSnapshot(_to);
+    return StandardToken.transferFrom(_from, _to, _value);
+  }
+
+  /**
+   * @dev Take snapshot
+   * @param _owner address The address to take snapshot from
+   */
+  function takeSnapshot(address _owner) public returns(uint256) {
+    if (snapshots[_owner].block < snapshotBlock) {
+      snapshots[_owner].block = snapshotBlock;
+      snapshots[_owner].balance = balanceOf(_owner);
+    }
+    return snapshots[_owner].balance;
+  }
+
+  /**
+   * @dev Set snacpshot block
+   * @param _blockNumber uint256 The new blocknumber for snapshots
+   */
+  function requestSnapshots(uint256 _blockNumber) public isPolls {
+    snapshotBlock = _blockNumber;
+  }
+}
+
+// File: zeppelin-solidity/contracts/token/BurnableToken.sol
+
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is BasicToken {
+
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
+    }
+}
+
+// File: contracts/SendToken.sol
+
+/**
+ * @title Send token
+ *
+ * @dev Implementation of Send Consensus network Standard
+ * @dev https://send.sd/token
+ */
+contract SendToken is ISendToken, SnapshotToken, BurnableToken {
+  IEscrow public escrow;
+
+  mapping (address => bool) internal verifiedAddresses;
+
+  modifier verifiedResticted() {
+    require(verifiedAddresses[msg.sender]);
+    _;
+  }
+
+  modifier escrowResticted() {
+    require(msg.sender == address(escrow));
+    _;
+  }
+
+  /**
+   * @dev Check if an address is whitelisted by SEND
+   * @param _address Address to check
+   * @return bool
+   */
+  function isVerified(address _address) public view returns(bool) {
+    return verifiedAddresses[_address];
+  }
+
+  /**
+   * @dev Verify an addres
+   * @notice Only contract owner
+   * @param _address Address to verify
+   */
+  function verify(address _address) public onlyOwner {
+    verifiedAddresses[_address] = true;
+  }
+
+  /**
+   * @dev Remove Verified status of a given address
+   * @notice Only contract owner
+   * @param _address Address to unverify
+   */
+  function unverify(address _address) public onlyOwner {
+    verifiedAddresses[_address] = false;
+  }
+
+  /**
+   * @dev Remove Verified status of a given address
+   * @notice Only contract owner
+   * @param _address Address to unverify
+   */
+  function setEscrow(address _address) public onlyOwner {
+    escrow = IEscrow(_address);
+  }
+
+  /**
+   * @dev Transfer from one address to another issuing ane xchange rate
+   * @notice Only verified addresses
+   * @notice Exchange rate has 18 decimal places
+   * @notice Value + fee <= allowance
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   * @param _referenceId internal app/user ID
+   * @param _exchangeRate Exchange rate to sign transaction
+   * @param _fee fee tot ake from sender
+   */
+  function verifiedTransferFrom(
+      address _from,
+      address _to,
+      uint256 _value,
+      uint256 _referenceId,
+      uint256 _exchangeRate,
+      uint256 _fee
+  ) public verifiedResticted {
+    require(_exchangeRate > 0);
+
+    transferFrom(_from, _to, _value);
+    transferFrom(_from, msg.sender, _fee);
+
+    VerifiedTransfer(
+      _from,
+      _to,
+      msg.sender,
+      _value,
+      _referenceId,
+      _exchangeRate
+    );
+  }
+
+  /**
+   * @dev create an escrow transfer being the arbitrator
+   * @param _sender Address to send tokens
+   * @param _recipient Address to receive tokens
+   * @param _transactionId internal ID for arbitrator
+   * @param _tokens Amount of tokens to lock
+   * @param _fee A fee to be paid to arbitrator (may be 0)
+   * @param _expiration After this timestamp, user can claim tokens back.
+   */
+  function createEscrow(
+      address _sender,
+      address _recipient,
+      uint256 _transactionId,
+      uint256 _tokens,
+      uint256 _fee,
+      uint256 _expiration
+  ) public {
+    escrow.create(
+      _sender,
+      _recipient,
+      msg.sender,
+      _transactionId,
+      _tokens,
+      _fee,
+      _expiration
+    );
+  }
+
+  /**
+   * @dev fund escrow
+   * @dev specified amount will be locked on escrow contract
+   * @param _arbitrator Address of escrow arbitrator
+   * @param _transactionId internal ID for arbitrator
+   * @param _tokens Amount of tokens to lock
+   * @param _fee A fee to be paid to arbitrator (may be 0)
+   */
+  function fundEscrow(
+      address _arbitrator,
+      uint256 _transactionId,
+      uint256 _tokens,
+      uint256 _fee
+  ) public {
+    uint256 total = _tokens.add(_fee);
+    transfer(escrow, total);
+
+    escrow.fund(
+      msg.sender,
+      _arbitrator,
+      _transactionId,
+      _tokens,
+      _fee
+    );
+  }
+
+  /**
+   * @dev Issue exchange rates from escrow contract
+   * @param _from Address to send tokens
+   * @param _to Address to receive tokens
+   * @param _verifiedAddress Address issuing the exchange rate
+   * @param _value amount
+   * @param _transactionId internal ID for issuer's reference
+   * @param _exchangeRate exchange rate
+   */
+  function issueExchangeRate(
+      address _from,
+      address _to,
+      address _verifiedAddress,
+      uint256 _value,
+      uint256 _transactionId,
+      uint256 _exchangeRate
+  ) public escrowResticted {
+    bool noRate = (_exchangeRate == 0);
+    if (isVerified(_verifiedAddress)) {
+      require(!noRate);
+      VerifiedTransfer(
+        _from,
+        _to,
+        _verifiedAddress,
+        _value,
+        _transactionId,
+        _exchangeRate
+      );
+    } else {
+      require(noRate);
+    }
+  }
+}
+
+// File: contracts/SDT.sol
+
+/**
+ * @title To instance SendToken for SEND foundation crowdasale
+ * @dev see https://send.sd/token
+ */
+contract SDT is SendToken {
+  string constant public name = "SEND Token";
+  string constant public symbol = "SDT";
+  uint256 constant public decimals = 18;
+
+  modifier validAddress(address _address) {
+    require(_address != address(0x0));
+    _;
+  }
+
+  /**
+  * @dev Constructor
+  * @param _sale Address that will hold all vesting allocated tokens
+  * @notice contract owner will have special powers in the contract
+  * @notice _sale should hold all tokens in production as all pool will be vested
+  * @return A uint256 representing the locked amount of tokens
+  */
+  function SDT(address _sale) public validAddress(_sale) {
+    verifiedAddresses[owner] = true;
+    totalSupply = 700000000 * 10 ** decimals;
+    balances[_sale] = totalSupply;
+  }
 }
