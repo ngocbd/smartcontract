7d933f801b763221f2b6263ca7f3f41cf734f662
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 11:39:56 +0700

    add contract SafeMath

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,33 +1,26 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0xf261B576AB167d1250F4301A50B616b3A26AdD90
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0x1ce9f9beb9da0a692fbc56fd3ea88220d352b7ac
 */
-pragma solidity ^0.4.11;
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
-  function mul(uint256 a, uint256 b)  constant returns (uint256) {
+  function mul(uint256 a, uint256 b) constant public returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b)  constant returns (uint256) {
+  function div(uint256 a, uint256 b) constant public returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b)  constant returns (uint256) {
+  function sub(uint256 a, uint256 b) constant public returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b)  constant returns (uint256) {
+  function add(uint256 a, uint256 b) constant public returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
