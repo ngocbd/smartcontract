eb554ffed2711655426321973ba00abfd91597be
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 20:15:47 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xbd1debfc65a729ae439463a91a571f89f0ebeedd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xe98c56cbdd15b8fa7c6cf18e8b34dbfb673c2a69
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT92954' token contract
+    // 'ACT97115' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT92954
-    // Name        : ADZbuzz Kelvinwoodonline.com Community Token
+    // Symbol      : ACT97115
+    // Name        : ADZbuzz Naturetreat.bid Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT92954";
-            name = "ADZbuzz Kelvinwoodonline.com Community Token";
+            symbol = "ACT97115";
+            name = "ADZbuzz Naturetreat.bid Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
