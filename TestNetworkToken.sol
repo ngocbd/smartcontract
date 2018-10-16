3a9a8b12637179a65629f0c309f25486c4d47622
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:31 +0700

    add contract TestNetworkToken

--- TestNetworkToken.sol
+++ TestNetworkToken.sol
@@ -1,39 +1,30 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestNetworkToken at 0x940e7d8fb34921c3c5bbb8b9792b1a0e4688fe9b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestNetworkToken at 0x131288b47c92409d0778d0aa2fbb8367e3399d33
 */
 pragma solidity ^0.4.4;
 
 
-contract MockTestNetworkToken {
+contract TestNetworkToken {
 
     // Token metadata
     string public constant name = "Test Network Token";
     string public constant symbol = "TNT";
     uint8 public constant decimals = 18;  // 18 decimal places, the same as ETH.
 
+    uint256 public constant tokenCreationRate = 1000;
+
     // The current total token supply
     uint256 totalTokens;
-
-    // Balances are stored here
+    
     mapping (address => uint256) balances;
 
-    // Always false, corresponds to ongoing crowdfunding
-    bool transferable;
-
-
-    // ERC20 interface - Transfer event used to track token transfers
-    
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Refund(address indexed _from, uint256 _value);
 
-    // ERC20 interface - minimal functional subset
+    // ERC20 interface implementation
 
+    // Empty implementation, so that no tokens can be moved
     function transfer(address _to, uint256 _value) returns (bool) {
-        if (transferable && balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        }
         return false;
     }
 
@@ -45,22 +36,7 @@ contract MockTestNetworkToken {
         return balances[_owner];
     }
 
-}
-
-contract TestNetworkToken is MockTestNetworkToken {
-
-    // Only this address is allowed to kill this contract
-    address owner;
-
-    uint256 public constant tokenCreationRate = 1000;
-
-    event Refund(address indexed _from, uint256 _value);
-
-    function TestNetworkToken() {
-        owner = msg.sender;
-    }
-
-    // Public, external API exposed to all users interested in taking part in the crowdfunding
+    // External interface similar to the crowdfunding one
 
     function create() payable external {
         // Do not allow creating 0 tokens.
@@ -93,8 +69,7 @@ contract TestNetworkToken is MockTestNetworkToken {
     // This is a test contract, so kill can be used once it is not needed
     
     function kill() {
-        if (msg.sender != owner) throw;
-        if (totalTokens > 0) throw;
+        if(totalTokens > 0) throw;
 
         selfdestruct(msg.sender);
     }
