f00b36a6b65daa5de2efbbb47317a14917c77cf5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 03:42:59 +0700

    add contract $$$$$$$$$$$$

---
+++ $$$$$$$$$$$$.sol
@@ -0,0 +1,12 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract $$$$$$$$$$$$ at 0x797415a3ecd8e1689161f3a7e4427e909501183e
+*/
+pragma solidity ^0.4.24;
+
+contract $$$$$$$$$$$${
+    
+    function printMoney() public pure returns(string){
+        return "???";
+    }
+    
+}
