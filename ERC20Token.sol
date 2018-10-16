549f0cc51082b5c1af1dbf785ee1ae939beac8ea
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:32:54 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x8Da7840A3F50d1b941ec0dA7e42fe3C479b66D29
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x63a9783e08bd93d301710367d3c2eb6e72cf6c71
 */
 pragma solidity ^0.4.4;
 
@@ -106,10 +106,10 @@ contract ERC20Token is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;
-    uint8 public decimals;
-    string public symbol;
-    string public version = 'H1.0';
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
 //
 // CHANGE THESE VALUES FOR YOUR TOKEN
@@ -119,10 +119,11 @@ contract ERC20Token is StandardToken {
 
     function ERC20Token(
         ) {
-        balances[msg.sender] =1000000000000000000000000000;
-        name = "PYRamid Coin";
-        decimals = 18;
-        symbol = "PYR";
+        balances[msg.sender] = 10000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 10000000;                        // Update total supply (100000 for example)
+        name = "Switch";                                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "SWTH";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
