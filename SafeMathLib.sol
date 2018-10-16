2fe7e7030d1c1e55b70d94b28090be6950bcd429
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:23:49 +0700

    add contract SafeMathLib

--- SafeMathLib.sol
+++ SafeMathLib.sol
@@ -1,7 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLib at 0xd07e6506e4f470392f73ac69d75b41df53d925d2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLib at 0x4dc39b8c473aadd9f0cb89ce5fd09687f54dca75
 */
+pragma solidity ^0.4.6;
+
+/**
+ * Safe unsigned safe math.
+ *
+ * https://blog.aragon.one/library-driven-development-in-solidity-2bebcaf88736#.750gwtwli
+ *
+ * Originally from https://raw.githubusercontent.com/AragonOne/zeppelin-solidity/master/contracts/SafeMathLib.sol
+ *
+ * Maintained here until merged to mainline zeppelin-solidity.
+ *
+ */
 library SafeMathLib {
+
   function times(uint a, uint b) returns (uint) {
     uint c = a * b;
     assert(a == 0 || c / a == b);
@@ -15,7 +28,7 @@ library SafeMathLib {
 
   function plus(uint a, uint b) returns (uint) {
     uint c = a + b;
-    assert(c>=a && c>=b);
+    assert(c>=a);
     return c;
   }
 
