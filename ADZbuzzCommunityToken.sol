03e07065b265757e0d510f3140ba08d5d42dd783
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:45:13 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x87902e786470c949ad74296f91b986bf7805f725
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x050ae9b1a6174e0a53d99106b5d8f3ac4e9cd90c
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT118302' token contract
+    // 'ACT485055' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT118302
-    // Name        : ADZbuzz Abc.net.au Community Token
+    // Symbol      : ACT485055
+    // Name        : ADZbuzz Bitzamp.com Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT118302";
-            name = "ADZbuzz Abc.net.au Community Token";
+            symbol = "ACT485055";
+            name = "ADZbuzz Bitzamp.com Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
