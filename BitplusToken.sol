bb43681a029db5d21f1453c564a46caf2cdfbdec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:18:30 +0700

    add contract BitplusToken

--- BitplusToken.sol
+++ BitplusToken.sol
@@ -1,255 +1,225 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitplusToken at 0xa4f546b43c6338e20f544b2827d8ee099b389f02
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitplusToken at 0x9BD1C760208BFb99A82cDf3adAc09F4800db78B1
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.11;
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 
-/// @title Bitplus Token (BPNT) - crowdfunding code for Bitplus Project
-contract BitplusToken {
-    string public constant name = "Bitplus Token";
-    string public constant symbol = "BPNT";
-    uint8 public constant decimals = 18;  // 18 decimal places, the same as ETH.
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
-    uint256 public constant tokenCreationRate = 1000;
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances. 
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
 
-    // The funding cap in weis.
-    uint256 public constant tokenCreationCap = 25000 ether * tokenCreationRate;
-    uint256 public constant tokenCreationMin = 2500 ether * tokenCreationRate;
+  mapping(address => uint256) balances;
 
-    uint256 public fundingStartBlock;
-    uint256 public fundingEndBlock;
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+  }
 
-    // The flag indicates if the contract is in Funding state.
-    bool public funding = true;
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of. 
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
 
-    // Receives ETH
-    address public bitplusAddress;
+}
 
-    // The current total token supply.
-    uint256 totalTokens;
 
-    mapping (address => uint256) balances;
-    
-    // Owner of account approves the transfer of an amount to another account
-    mapping(address => mapping (address => uint256)) allowed;
-    
-    struct EarlyBackerCondition {
-        address backerAddress;
-        uint256 deposited;
-        uint256 agreedPercentage;
-        uint256 agreedEthPrice;
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value);
+  function approve(address spender, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function transferFrom(address _from, address _to, uint256 _value) {
+    var _allowance = allowed[_from][msg.sender];
+
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // if (_value > _allowance) throw;
+
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+  }
+
+  /**
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifing the amount of tokens still avaible for the spender.
+   */
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control 
+ * functions, this simplifies the implementation of "user permissions". 
+ */
+contract Ownable {
+  address public owner;
+
+
+  /** 
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner. 
+   */
+  modifier onlyOwner() {
+    if (msg.sender != owner) {
+      throw;
     }
-    
-    EarlyBackerCondition[] public earlyBackers;
-    
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Refund(address indexed _from, uint256 _value);
-    event EarlyBackerDeposit(address indexed _from, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    _;
+  }
 
-    function BitplusToken(uint256 _fundingStartBlock,
-                          uint256 _fundingEndBlock) {
 
-        address _bitplusAddress = 0x286e0060d9DBEa0231389485D455A80f14648B3c;
-        if (_bitplusAddress == 0) throw;
-        if (_fundingStartBlock <= block.number) throw;
-        if (_fundingEndBlock   <= _fundingStartBlock) throw;
-        
-        // special conditions for the early backers
-        earlyBackers.push(EarlyBackerCondition({
-            backerAddress: 0xa1cfc9ebdffbffe9b27d741ae04cfc2e78af527a,
-            deposited: 0,
-            agreedPercentage: 1000,
-            agreedEthPrice: 250 ether
-        }));
-        
-        // conditions for the company / developers
-        earlyBackers.push(EarlyBackerCondition({
-            backerAddress: 0x37ef1168252f274D4cA5b558213d7294085BCA08,
-            deposited: 0,
-            agreedPercentage: 500,
-            agreedEthPrice: 0.1 ether
-        }));
-        
-        earlyBackers.push(EarlyBackerCondition({
-            backerAddress: 0x246604643ac38e96526b66ba91c1b2ec0c39d8de,
-            deposited: 0,
-            agreedPercentage: 500,
-            agreedEthPrice: 0.1 ether
-        }));        
-        
-        bitplusAddress = _bitplusAddress;
-        fundingStartBlock = _fundingStartBlock;
-        fundingEndBlock = _fundingEndBlock;
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to. 
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
     }
+  }
 
-    /// @notice Transfer `_value` BPNT tokens from sender's account
-    /// `msg.sender` to provided account address `_to`.
-    /// @notice This function is disabled during the funding.
-    /// @dev Required state: Operational
-    /// @param _to The address of the tokens recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool) {
-        // Abort if not in Operational state.
-        if (funding) throw;
+}
 
-        var senderBalance = balances[msg.sender];
-        if (senderBalance >= _value && _value > 0) {
-            senderBalance -= _value;
-            balances[msg.sender] = senderBalance;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        }
-        return false;
-    }
-    
-    function transferFrom(
-         address _from,
-         address _to,
-         uint256 _amount
-     ) returns (bool success) {
-        // Abort if not in Operational state.
-        if (funding) throw;         
-         
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             return true;
-         } else {
-             return false;
-         }
-    }    
+contract MintableToken is StandardToken, Ownable {
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
 
-    function totalSupply() external constant returns (uint256) {
-        return totalTokens;
-    }
+  bool public mintingFinished = false;
 
-    function balanceOf(address _owner) external constant returns (uint256) {
-        return balances[_owner];
-    }
 
-    /// @notice Create tokens when funding is active.
-    /// @dev Required state: Funding Active
-    /// @dev State transition: -> Funding Success (only if cap reached)
-    function create() payable external {
-        // Abort if not in Funding Active state.
-        // The checks are split (instead of using or operator) because it is
-        // cheaper this way.
-        if (!funding) throw;
-        if (block.number < fundingStartBlock) throw;
-        if (block.number > fundingEndBlock) throw;
+  modifier canMint() {
+    if(mintingFinished) throw;
+    _;
+  }
 
-        // Do not allow creating 0 tokens.
-        if (msg.value == 0) throw;
-        
-        bool isEarlyBacker = false;
-        
-        for (uint i = 0; i < earlyBackers.length; i++) {
-            if(earlyBackers[i].backerAddress == msg.sender) {
-                earlyBackers[i].deposited += msg.value;
-                isEarlyBacker = true;
-                EarlyBackerDeposit(msg.sender, msg.value);
-            }
-        }
-        
-        
-        if(!isEarlyBacker) {
-            // do not allow to create more then cap tokens
-            if (msg.value > (tokenCreationCap - totalTokens) / tokenCreationRate)
-                throw;
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will recieve the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  }
 
-            var numTokens = msg.value * tokenCreationRate;
-            totalTokens += numTokens;
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+}
 
-            // Assign new tokens to the sender
-            balances[msg.sender] += numTokens;
-            
-            // Log token creation event
-            Transfer(0, msg.sender, numTokens);            
-        }
-    }
+contract BitplusToken is MintableToken {
 
-    /// @notice Finalize crowdfunding
-    /// @dev If cap was reached or crowdfunding has ended then:
-    /// create BPNT for the early backers,
-    /// transfer ETH to the Bitplus address.
-    /// @dev Required state: Funding Success
-    /// @dev State transition: -> Operational Normal
-    function finalize() external {
-        // Abort if not in Funding Success state.
-        if (!funding) throw;
-        if ((block.number <= fundingEndBlock ||
-             totalTokens < tokenCreationMin) &&
-             totalTokens < tokenCreationCap) throw;
+  string public name = "BitplusToken";
+  string public symbol = "BPNT";
+  uint256 public decimals = 18;
 
-        // Switch to Operational state. This is the only place this can happen.
-        funding = false;
-        // Transfer ETH to the Bitplus address.
-        if (!bitplusAddress.send(this.balance)) throw;
-        
-        for (uint i = 0; i < earlyBackers.length; i++) {
-            if(earlyBackers[i].deposited != uint256(0)) {
-                uint256 percentage = (earlyBackers[i].deposited * earlyBackers[i].agreedPercentage / earlyBackers[i].agreedEthPrice);
-                uint256 additionalTokens = totalTokens * percentage / (10000 - percentage);
-                address backerAddr = earlyBackers[i].backerAddress;
-                balances[backerAddr] = additionalTokens;
-                totalTokens += additionalTokens;
-                Transfer(0, backerAddr, additionalTokens);
-			}
-        }
-    }
+  /**
+   * @dev Contructor.
+   */
+  function BitplusToken() {
+    totalSupply = 0;
+  }
 
-    /// @notice Get back the ether sent during the funding in case the funding
-    /// has not reached the minimum level.
-    /// @dev Required state: Funding Failure
-    function refund() external {
-        // Abort if not in Funding Failure state.
-        if (!funding) throw;
-        if (block.number <= fundingEndBlock) throw;
-        if (totalTokens >= tokenCreationMin) throw;
-        
-        bool isEarlyBacker = false;
-        uint256 ethValue;
-        for (uint i = 0; i < earlyBackers.length; i++) {
-            if(earlyBackers[i].backerAddress == msg.sender) {
-                isEarlyBacker = true;
-                ethValue = earlyBackers[i].deposited;
-                if (ethValue == 0) throw;
-            }
-        }
-
-        if(!isEarlyBacker) {
-            var bpntValue = balances[msg.sender];
-            if (bpntValue == 0) throw;
-            balances[msg.sender] = 0;
-            totalTokens -= bpntValue;
-            ethValue = bpntValue / tokenCreationRate;
-        }
-        
-        Refund(msg.sender, ethValue);
-        if (!msg.sender.send(ethValue)) throw;
-    }
-    
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-    
-    // Just a safeguard for people who might invest and then loose the key
-    // If 2 weeks after an unsuccessful end of the campaign there are unclaimed
-    // funds, transfer those to Bitplus address - the funds will be returned to 
-    // respective owners from it
-    function safeguard() {
-        if(block.number > (fundingEndBlock + 71000)) {
-            if (!bitplusAddress.send(this.balance)) throw;
-        }
-    }
 }
