a2d78cced98790216c37d85985afd72e33f19548
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:53:23 +0700

    add contract setNumRewardsForTMEUser

--- setNumRewardsForTMEUser.sol
+++ setNumRewardsForTMEUser.sol
@@ -1,16 +1,17 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract setNumRewardsForTMEUser at 0x387bcd322bec21d73d7ce1a8eb9a8bfd9c91cc01
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract setNumRewardsForTMEUser at 0xe5677ff57460abd4a926a4c3f3790ff02b2dff1e
 */
 pragma solidity ^0.4.10;
 
-// Requested by person who made all their TME batches after snapshot.
+// Last time we can help people who make this mistake.
 
 contract setNumRewardsForTMEUser    {
 
 function setNumRewardsForTMEUser()    {
 
 elixor elixorContract=elixor(0x898bf39cd67658bd63577fb00a2a3571daecbc53);
-elixorContract.manuallySetNumRewardsAvailableForChildAddress(0x59d2d2a10c1c86367a88e35f0ef4c2fbba918471,10);
+
+elixorContract.manuallySetNumRewardsAvailableForChildAddress(0x909C10Ba205181D6c7D9F14fD304E2f7773975bb,10);
 
 }
 }
