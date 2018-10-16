5e8b684666ed908b975cb3b44f545d190ef1c5e4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:30:05 +0700

    add contract EthereumSoft

---
+++ EthereumSoft.sol
@@ -0,0 +1,71 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthereumSoft at 0x4461778cc8bc26abb611ff9cdb2c8248ba128caf
+*/
+pragma solidity ^0.4.11;
+
+contract EthereumSoft {
+
+    string public name = "Ethereum Soft";      //  Soft name
+    string public symbol = "ESFT";           //  Soft symbol
+    uint256 public decimals = 1;            //  Soft digit
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    uint256 public totalSupply = 5000000 * (10**decimals);
+    address public owner;
+
+    modifier isOwner {
+        assert(owner == msg.sender);
+        _;
+    }
+    function EthereumSoft() {
+        owner = msg.sender;
+        balanceOf[owner] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_to] += _value;
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success)
+    {
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function setName(string _name) isOwner 
+    {
+        name = _name;
+    }
+    function burnSupply(uint256 _amount) isOwner
+    {
+        balanceOf[owner] -= _amount;
+        SupplyBurn(_amount);
+    }
+    function burnTotalSupply(uint256 _amount) isOwner
+    {
+        totalSupply-= _amount;
+    }
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event SupplyBurn(uint256 _amount);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
