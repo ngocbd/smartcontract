39345160080a6a50c407e6e99cfd2eef1df5dc76
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 19:13:42 +0700

    add contract HashBux

--- HashBux.sol
+++ HashBux.sol
@@ -1,231 +1,132 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashBux at 0xec6d49ebeb6d30cec13f8d07d3b266a59aacdf46
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashBux at 0x06f8d7043f77e20df94bc2fab39bf90d702cbd15
 */
-//
-// compiler: solcjs
-//  version: 0.4.19+commit.c4cbbb05.Emscripten.clang
-//
 pragma solidity ^0.4.19;
 
+interface IERC20 {
+    function totalSupply() constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 contract owned {
-  address public owner;
+        address public owner;
 
-  function owned() public { owner = msg.sender; }
+        function owned() {
+            owner = msg.sender;
+        }
 
-  modifier onlyOwner {
-    if (msg.sender != owner) { revert(); }
-    _;
-  }
+        modifier onlyOwner {
+            require(msg.sender == owner);
+            _;
+        }
 
-  function changeOwner( address newowner ) public onlyOwner {
-    owner = newowner;
-  }
+        function transferOwnership(address newOwner) onlyOwner {
+            owner = newOwner;
+        }
 }
 
-// see https://www.ethereum.org/token
-interface tokenRecipient {
-  function receiveApproval( address from, uint256 value, bytes data ) public;
-}
-
-// ERC223
-interface ContractReceiver {
-  function tokenFallback( address from, uint value, bytes data ) public;
-}
-
-// ERC223-compliant token with ERC20 back-compatibility
-//
-// Implements:
-// - https://theethereum.wiki/w/index.php/ERC20_Token_Standard
-// - https://www.ethereum.org/token (uncontrolled, non-standard)
-// - https://github.com/Dexaran/ERC23-tokens/blob/Recommended/ERC223_Token.sol
-
-contract HashBux is owned
-{
-  string  public name;        // ERC20
-  string  public symbol;      // ERC20
-  uint8   public decimals;    // ERC20
-  uint256 public totalSupply; // ERC20
-
-  mapping( address => uint256 ) balances_;
-  mapping( address => mapping(address => uint256) ) allowances_;
-
-  // ERC20
-  event Approval( address indexed owner,
-                  address indexed spender,
-                  uint value );
-
-  // ERC223, ERC20 plus last parameter
-  event Transfer( address indexed from,
-                  address indexed to,
-                  uint256 value,
-                  bytes   indexed data );
-
-  // Ethereum Token
-  event Burn( address indexed from, uint256 value );
-
-  function HashBux() public
-  {
-    balances_[msg.sender] = uint256(80000000);
-    totalSupply = uint256(80000000);
-    name = "HashBux";
-    decimals = uint8(0);
-    symbol = "HASH";
-  }
-
-  // HashBux-specific
-  function mine( uint256 newTokens ) public onlyOwner {
-    if (newTokens + totalSupply > 4e9)
-      revert();
-
-    totalSupply += newTokens;
-    balances_[owner] += newTokens;
-    bytes memory empty;
-    Transfer( address(this), owner, newTokens, empty );
-  }
-
-  function() public payable { revert(); } // does not accept money
-
-  // ERC20
-  function balanceOf( address owner ) public constant returns (uint) {
-    return balances_[owner];
-  }
-
-  // ERC20
-  function approve( address spender, uint256 value ) public
-  returns (bool success)
-  {
-    allowances_[msg.sender][spender] = value;
-    Approval( msg.sender, spender, value );
-    return true;
-  }
+contract HashBux is owned,IERC20{
+    
+    using SafeMath for uint256;
+    
+    uint256 public _totalSupply = 80000000;
  
-  // ERC20
-  function allowance( address owner, address spender ) public constant
-  returns (uint256 remaining)
-  {
-    return allowances_[owner][spender];
-  }
+    string public symbol = 'HASH';
 
-  // ERC20
-  function transfer(address to, uint256 value) public
-  {
-    bytes memory empty; // null
-    _transfer( msg.sender, to, value, empty );
-  }
+    string public name = 'HashBux';
+    
+    uint8 public decimals = 0;
+    
+    mapping(address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) allowed;
 
-  // ERC20
-  function transferFrom( address from, address to, uint256 value ) public
-  returns (bool success)
-  {
-    require( value <= allowances_[from][msg.sender] );
-
-    allowances_[from][msg.sender] -= value;
-    bytes memory empty;
-    _transfer( from, to, value, empty );
-
-    return true;
-  }
-
-  // Ethereum Token
-  function approveAndCall( address spender, uint256 value, bytes context )
-  public returns (bool success)
-  {
-    if ( approve(spender, value) )
-    {
-      tokenRecipient recip = tokenRecipient( spender );
-      recip.receiveApproval( msg.sender, value, context );
-      return true;
+    function HashBux() {
+        balances[msg.sender] = _totalSupply;
     }
-    return false;
-  }        
-
-  // Ethereum Token
-  function burn( uint256 value ) public returns (bool success)
-  {
-    require( balances_[msg.sender] >= value );
-    balances_[msg.sender] -= value;
-    totalSupply -= value;
-
-    Burn( msg.sender, value );
-    return true;
-  }
-
-  // Ethereum Token
-  function burnFrom( address from, uint256 value ) public returns (bool success)
-  {
-    require( balances_[from] >= value );
-    require( value <= allowances_[from][msg.sender] );
-
-    balances_[from] -= value;
-    allowances_[from][msg.sender] -= value;
-    totalSupply -= value;
-
-    Burn( from, value );
-    return true;
-  }
-
-  function _transfer( address from,
-                      address to,
-                      uint value,
-                      bytes data ) internal
-  {
-    require( to != 0x0 );
-    require( balances_[from] >= value );
-    require( balances_[to] + value > balances_[to] ); // catch overflow
-
-    balances_[from] -= value;
-    balances_[to] += value;
-
-    Transfer( from, to, value, data );
-  }
-
-  // ERC223 Transfer and invoke specified callback
-  function transfer( address to,
-                     uint value,
-                     bytes data,
-                     string custom_fallback ) public returns (bool success)
-  {
-    _transfer( msg.sender, to, value, data );
-
-    if ( isContract(to) )
-    {
-      ContractReceiver rx = ContractReceiver( to );
-      require( rx.call.value(0)
-               (bytes4(keccak256(custom_fallback)), msg.sender, value, data) );
+    
+    // HashBux-specific
+   function mine( uint256 newTokens ) public onlyOwner {
+        require(newTokens + _totalSupply <= 4000000000);
+    
+        _totalSupply = _totalSupply.add(newTokens);
+        balances[owner] = balances[owner].add(newTokens);
+        Transfer(this, owner, newTokens);
+   }
+    
+    function totalSupply() constant returns (uint256 totalSupply) {
+        return _totalSupply;
+    }
+   
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(
+            balances[msg.sender] >= _value
+            && _value > 0
+        );
+        
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    return true;
-  }
-
-  // ERC223 Transfer to a contract or externally-owned account
-  function transfer( address to, uint value, bytes data ) public
-  returns (bool success)
-  {
-    if (isContract(to)) {
-      return transferToContract( to, value, data );
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(
+            allowed[_from][msg.sender] >= _value
+            && balances[_from] >= _value
+            && _value > 0  
+        );
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
     }
 
-    _transfer( msg.sender, to, value, data );
-    return true;
-  }
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-  // ERC223 Transfer to contract and invoke tokenFallback() method
-  function transferToContract( address to, uint value, bytes data ) private
-  returns (bool success)
-  {
-    _transfer( msg.sender, to, value, data );
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    ContractReceiver rx = ContractReceiver(to);
-    rx.tokenFallback( msg.sender, value, data );
-
-    return true;
-  }
-
-  // ERC223 fetch contract size (must be nonzero to be a contract)
-  function isContract( address _addr ) private constant returns (bool)
-  {
-    uint length;
-    assembly { length := extcodesize(_addr) }
-    return (length > 0);
-  }
 }
