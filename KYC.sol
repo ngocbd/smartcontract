91b60a403a0f090563c7319430117fe20967d79e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 06:47:48 +0700

    add contract KYC

--- KYC.sol
+++ KYC.sol
@@ -1,152 +1,150 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KYC at 0x8aa088de726048f3e837e90534f769cf86345b75
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KYC at 0x456e92a0252e74bd0daf9a321ab1944e088cab87
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract KYC {
-    // Public variables of the token
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
 
-    // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
 
-    // This generates a public event on the blockchain that will notify clients
-    event Transfer(address indexed from, address indexed to, uint256 value);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
 
-    /**
-     * Constructor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
-    function KYC(
-    ) public {
-        totalSupply = 275000000000000000000000000;                       // Total supply with the decimal amount
-        balanceOf[msg.sender] = 275000000000000000000000000;             // All initial tokens
-        name = "KeyChain";                                               // The name for display purposes
-        symbol = "KYC";                                                  // The symbol for display purposes
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+/**
+ * @title KYC
+ * @dev KYC contract handles the white list for PLCCrowdsale contract
+ * Only accounts registered in KYC contract can buy PLC token.
+ * Admins can register account, and the reason why
+ */
+contract KYC is Ownable {
+  // check the address is registered for token sale
+  mapping (address => bool) public registeredAddress;
+
+  // check the address is admin of kyc contract
+  mapping (address => bool) public admin;
+
+  event Registered(address indexed _addr);
+  event Unregistered(address indexed _addr);
+  event SetAdmin(address indexed _addr, bool indexed _isAdmin);
+
+  /**
+   * @dev check whether the msg.sender is admin or not
+   */
+  modifier onlyAdmin() {
+    require(admin[msg.sender]);
+    _;
+  }
+
+  function KYC() public {
+    admin[msg.sender] = true;
+  }
+
+  /**
+   * @dev set new admin as admin of KYC contract
+   * @param _addr address The address to set as admin of KYC contract
+   */
+  function setAdmin(address _addr, bool _isAdmin)
+    public
+    onlyOwner
+  {
+    require(_addr != address(0));
+    admin[_addr] = _isAdmin;
+
+    emit SetAdmin(_addr, _isAdmin);
+  }
+
+  /**
+   * @dev register the address for token sale
+   * @param _addr address The address to register for token sale
+   */
+  function register(address _addr)
+    public
+    onlyAdmin
+  {
+    require(_addr != address(0));
+
+    registeredAddress[_addr] = true;
+
+    emit Registered(_addr);
+  }
+
+  /**
+   * @dev register the addresses for token sale
+   * @param _addrs address[] The addresses to register for token sale
+   */
+  function registerByList(address[] _addrs)
+    public
+    onlyAdmin
+  {
+    for(uint256 i = 0; i < _addrs.length; i++) {
+      require(_addrs[i] != address(0));
+
+      registeredAddress[_addrs[i]] = true;
+
+      emit Registered(_addrs[i]);
     }
+  }
 
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
-        balanceOf[_from] -= _value;
-        // Add the same to the recipient
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
+  /**
+   * @dev unregister the registered address
+   * @param _addr address The address to unregister for token sale
+   */
+  function unregister(address _addr)
+    public
+    onlyAdmin
+  {
+    registeredAddress[_addr] = false;
 
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
+    emit Unregistered(_addr);
+  }
 
-    /**
-     * Transfer tokens from other address
-     *
-     * Send `_value` tokens to `_to` on behalf of `_from`
-     *
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
+  /**
+   * @dev unregister the registered addresses
+   * @param _addrs address[] The addresses to unregister for token sale
+   */
+  function unregisterByList(address[] _addrs)
+    public
+    onlyAdmin
+  {
+    for(uint256 i = 0; i < _addrs.length; i++) {
+      registeredAddress[_addrs[i]] = false;
 
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
+      emit Unregistered(_addrs[i]);
     }
-
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
-        return true;
-    }
+  }
 }
