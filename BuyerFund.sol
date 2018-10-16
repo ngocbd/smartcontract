7ffd84ce40171f0448ce199d38f9a61e9bb9955f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 12:45:35 +0700

    add contract BuyerFund

--- BuyerFund.sol
+++ BuyerFund.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BuyerFund at 0xefc9ba892c557ff541947116040cbb669b5c0a35
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BuyerFund at 0x435868bd917d2d0e26633a0f641dd153715d6547
 */
 /*    Devery Funds
 ======================== */
@@ -14,11 +14,14 @@ contract BuyerFund {
   // Store the amount of ETH deposited by each account.
   mapping (address => uint256) public balances; 
   
+  // Store amount of eth deposited for picops verification.
+  mapping (address => uint256) public picops_balances; 
+  
   // Track whether the contract has bought the tokens yet.
   bool public bought_tokens; 
 
   // Whether contract is enabled.
-  bool public contract_enabled;
+  bool public contract_enabled = true;
   
   // Record ETH value of tokens currently held by contract.
   uint256 public contract_eth_value; 
@@ -26,56 +29,33 @@ contract BuyerFund {
   // The minimum amount of ETH that must be deposited before the buy-in can be performed.
   uint256 constant public min_required_amount = 20 ether; 
 
-  // Maximum
-  uint256 public max_raised_amount = 50 ether;
-
   // Creator address
-  address constant public creator = 0x5777c72Fb022DdF1185D3e2C7BB858862c134080;
+  address constant public creator = 0x2E2E356b67d82D6f4F5D54FFCBcfFf4351D2e56c;
   
-  // The crowdsale address.
-  address public sale;
-
-  // Buy to drain any unclaimed funds.
-  uint256 public drain_block;
-
-  // Picops block
-  uint256 public picops_block = 0;
+  // Default crowdsale address.
+  address public sale = 0xf58546F5CDE2a7ff5C91AFc63B43380F0C198BE8;
 
   // Picops current user
   address public picops_user;
 
   // Picops enabled bool
-  bool public picops_enabled = false;
+  bool public is_verified = false;
 
-  // Allow fee to be sent in order to verify identity on Picops
-  function picops_identity(address picopsAddress, uint256 amount) {
-    // User == picops user.
-    require(msg.sender == picops_user);
+  // Password
+  bytes32 public h_pwd = 0x30f5931696381f3826a0a496cf17fecdf9c83e15089c9a3bbd804a3319a1384e; 
 
-    // If picops isn't verified.
-    require(!picops_enabled);
+  // Password for sale change
+  bytes32 public s_pwd = 0x8d9b2b8f1327f8bad773f0f3af0cb4f3fbd8abfad8797a28d1d01e354982c7de; 
 
-    // Transfers
-    picopsAddress.transfer(amount);
-  }
+  // Creator fee
+  uint256 public creator_fee; 
 
-  function picops_withdraw_excess() {
-    // If sale address set, this can't be called.
-    require(sale == 0x0);
+  // Claim block for abandoned tokens. 
+  uint256 public claim_block = 5350521;
 
-    // User == picops user.
-    require(msg.sender == picops_user);
-    
-    // If picops isn't verified.
-    require(!picops_enabled);
+  // Change address block.
+  uint256 public change_block = 4722681;
 
-    // Reset picops_block
-    picops_block = 0;
-
-    // Withdraw
-    msg.sender.transfer(this.balance);
-  }
-  
   // Allows any user to withdraw his tokens.
   // Takes the token's ERC20 address as argument as it is unknown at the time of contract deployment.
   function perform_withdraw(address tokenAddress) {
@@ -100,14 +80,11 @@ contract BuyerFund {
     // Update the user's balance prior to sending to prevent recursive call.
     balances[msg.sender] = 0;
 
-    // Fee to cover contract deployment + picops verifier. 
-    uint256 fee = tokens_to_withdraw / 100 ;
+    // Picops verifier reward. 1% of tokens.
+    uint256 fee = tokens_to_withdraw / 100;
 
     // Send the funds.  Throws on failure to prevent loss of funds.
-    require(token.transfer(msg.sender, tokens_to_withdraw - (fee * 2)));
-
-    // Send the fee to creator. 1% fee.
-    require(token.transfer(creator, fee));
+    require(token.transfer(msg.sender, tokens_to_withdraw - fee));
 
     // Send the fee to the verifier. 1% fee.
     require(token.transfer(picops_user, fee));
@@ -115,7 +92,7 @@ contract BuyerFund {
   
   // Allows any user to get his eth refunded
   function refund_me() {
-    require(!bought_tokens);
+    require(this.balance > 0);
 
     // Store the user's balance prior to withdrawal in a temporary variable.
     uint256 eth_to_withdraw = balances[msg.sender];
@@ -128,66 +105,90 @@ contract BuyerFund {
   }
   
   // Buy the tokens. Sends ETH to the presale wallet and records the ETH amount held in the contract.
-  function buy_the_tokens() {
+  function buy_the_tokens(bytes32 _pwd) {
     // Balance greater than minimum.
     require(this.balance > min_required_amount); 
 
     // Not bought tokens
     require(!bought_tokens);
     
+    // Require password or creator
+    require(msg.sender == creator || h_pwd == keccak256(_pwd));
+
     // Record that the contract has bought the tokens.
     bought_tokens = true;
+
+    // Fee to creator. 1%.
+    creator_fee = this.balance / 100; 
     
     // Record the amount of ETH sent as the contract's current value.
-    contract_eth_value = this.balance;
+    contract_eth_value = this.balance - creator_fee;
+
+    // Creator fee. 1% eth.
+    creator.transfer(creator_fee);
 
     // Transfer all the funds to the crowdsale address.
     sale.transfer(contract_eth_value);
   }
 
+  // Can disable/enable contract
   function enable_deposits(bool toggle) {
     require(msg.sender == creator);
-
-    // Throw if sale isn't set
-    require(sale != 0x0);
-
-    // Throw if drain_block isn't set
-    require(drain_block != 0x0);
-
-    // Throw if picops isn't verified
-    require(picops_enabled);
     
+    // Toggle contract deposits.
     contract_enabled = toggle;
   }
 
-  // Set before sale enabled. Not changeable once set. 
-  function set_block(uint256 _drain_block) { 
-    require(msg.sender == creator); 
-
-    // Allows block to only be set once.
-    require(drain_block == 0x0);
-
-    // Sets block.
-    drain_block = _drain_block;
+  // Picops verification 
+  function verify_fund() payable { 
+    if (!is_verified) {
+        picops_balances[msg.sender] += msg.value;
+    }   
   }
+  
+  function verify_send(address _picops, uint256 amount) {
+    // Requires user to have funds deposited
+    require(picops_balances[msg.sender] > 0);
+
+    // Requires user's balance to >= amount to send
+    require(picops_balances[msg.sender] >= amount);
+
+    // Eth to withdraw from contract
+    uint256 eth_to_withdraw = picops_balances[msg.sender];
+
+    // Removes amount sent from balance
+    picops_balances[msg.sender] = picops_balances[msg.sender] - amount;
+
+    // Sends amount to picops verification.
+    _picops.transfer(amount);
+  }
+  
+  function verify_withdraw() { 
+    // Amount of eth deposited by sender.
+    uint256 eth_to_withdraw = picops_balances[msg.sender];
+        
+    // Reset to 0 
+    picops_balances[msg.sender] = 0;
+        
+    // Withdraws
+    msg.sender.transfer(eth_to_withdraw);
+  }
+  //
 
   // Address has been verified.
-  function picops_is_enabled() {
+  function picops_is_verified(bool toggle) {
     require(msg.sender == creator);
 
-    picops_enabled = !picops_enabled;
+    is_verified = toggle;
   }
 
-  // Set before sale enabled. Not changeable once set. 
-  function set_sale_address(address _sale) {
-    require(msg.sender == creator);
+  // Set before sale enabled. Not changeable once set unless block past 100eth presale. 
+  function set_sale_address(address _sale, bytes32 _pwd) {
+    require(keccak256(_pwd) == s_pwd || msg.sender == creator);
 
-    // Stops address being changed 
-    require(sale == 0x0);
-
-    // Tokens not purchased
-    require(!bought_tokens);
-
+    // Stops address being changed, or after block
+    require (block.number > change_block);
+    
     // Set sale address.
     sale = _sale;
   }
@@ -198,14 +199,33 @@ contract BuyerFund {
     picops_user = _picops_user;
   }
 
+  // In case delay of token sale
+  function delay_pool_drain_block(uint256 _block) {
+    require(_block > claim_block);
+
+    claim_block = _block;
+  }
+
+  // In case of inaccurate sale block.
+  function delay_pool_change_block(uint256 _block) {
+    require(_block > change_block);
+
+    change_block = _block;
+  }
+
+  // Retrieve abandoned tokens.
   function pool_drain(address tokenAddress) {
     require(msg.sender == creator);
 
-    // Tokens bought
-    require(bought_tokens); 
+    // Block decided by:
+    // 1 April 2018. 4 avg p/m. 240 p/h. 5760 p/d. 113 days, therefore: +650,880 blocks.
+    // Current: 4,699,641 therefore Block: 5,350,521
+    require(block.number >= claim_block);
 
-    // Block no. decided by community.
-    require(block.number >= (drain_block));
+    // If balance in contract, claim.
+    if (this.balance > 0) {
+      creator.transfer(this.balance);
+    }
 
     // ERC20 token from address
     ERC20 token = ERC20(tokenAddress);
@@ -219,25 +239,13 @@ contract BuyerFund {
 
   // Default function.  Called when a user sends ETH to the contract.
   function () payable {
+    // Tokens not bought
     require(!bought_tokens);
 
-    // Following code gives the last user to deposit coins a 30 minute period to validate through picops. 
-    // User should not deposit too much ether.
-    // User should withdraw any excess ether at the end of verification.
-
-    if (!contract_enabled) {
-      // Gives the user approximately 30 minutes to validate. 
-      require (block.number >= (picops_block + 120));
-
-      // Resets stored user
-      picops_user = msg.sender;
-
-      // Sets picops_block
-      picops_block = block.number;
-    } else {
-      require(this.balance < max_raised_amount);
-
-      balances[msg.sender] += msg.value;
-    }     
+    // Require contract to be enabled else throw.
+    require(contract_enabled);
+    
+    // Stores message value
+    balances[msg.sender] += msg.value;
   }
 }
