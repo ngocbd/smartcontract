2663444009ae20d719463314ff8e78179f8e80eb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:58:51 +0700

    add contract ReplaySafeSplit

--- ReplaySafeSplit.sol
+++ ReplaySafeSplit.sol
@@ -1,25 +1,40 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0x341e790174e3a4d35b65fdc067b6b5634a61caea
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ReplaySafeSplit at 0xaBbb6bEbFA05aA13e908EaA492Bd7a8343760477
 */
+contract RequiringFunds {
+    modifier NeedEth () {
+        if (msg.value <= 0 ) throw;
+        _
+    }
+}
+
 contract AmIOnTheFork {
     function forked() constant returns(bool);
 }
 
-/**
- * 
- */
-contract ReplaySafeSplit {
+contract ReplaySafeSplit is RequiringFunds {
+    //address private constant oracleAddress = 0x8128B12cABc6043d94BD3C4d9B9455077Eb18807;    // testnet
+    address private constant oracleAddress = 0x2bd2326c993dfaef84f696526064ff22eba5b362;   // mainnet
+    
     // Fork oracle to use
-    AmIOnTheFork amIOnTheFork = AmIOnTheFork(0x2bd2326c993dfaef84f696526064ff22eba5b362);
+    AmIOnTheFork amIOnTheFork = AmIOnTheFork(oracleAddress);
 
     // Splits the funds into 2 addresses
-    function split(address targetFork, address targetNoFork) returns(bool) {
-        if (amIOnTheFork.forked() && targetFork.send(msg.value)) {
+    function split(address targetFork, address targetNoFork) NeedEth returns(bool) {
+        // The 2 checks are to ensure that users provide BOTH addresses
+        // and prevent funds to be sent to 0x0 on one fork or the other.
+        if (targetFork == 0) throw;
+        if (targetNoFork == 0) throw;
+
+        if (amIOnTheFork.forked()                   // if we are on the fork 
+            && targetFork.send(msg.value)) {        // send the ETH to the targetFork address
             return true;
-        } else if (!amIOnTheFork.forked() && targetNoFork.send(msg.value)) {
+        } else if (!amIOnTheFork.forked()           // if we are NOT on the fork 
+            && targetNoFork.send(msg.value)) {      // send the ETH to the targetNoFork address 
             return true;
         }
-        return false;
+
+        throw;                                      // don't accept value transfer, otherwise it would be trapped.
     }
 
     // Reject value transfers.
