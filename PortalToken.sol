62fa74d96896c04981540a554a542fff3e2c68ed
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:38:22 +0700

    add contract PortalToken

--- PortalToken.sol
+++ PortalToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PortalToken at 0x28c3f9d254aa969c117ba828f9d96007bf41f7af
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PortalToken at 0x8DB90E3e7D04C875a51997092f9178FCac9DefdB
 */
 pragma solidity ^0.4.18;
 
@@ -38,19 +38,16 @@ contract ERC20Token {
 
 contract PortalToken is ERC20Token {
     address public initialOwner;
-    uint256 public supply   = 1000000000 * 10 ** 18;  // 100, 000, 000
+    uint256 public supply   = 1000000000 * 10 ** 18;  // 1,000,000,000
     string  public name     = 'PortalToken';
     uint8   public decimals = 18;
-    string  public symbol   = 'PTC';
-    string  public version  = 'v0.1';
-    bool    public transfersEnabled = true;
+    string  public symbol   = 'PORTAL';
+    string  public version  = 'v0.2';
     uint    public creationBlock;
     uint    public creationTime;
 
     mapping (address => uint256) balance;
     mapping (address => mapping (address => uint256)) m_allowance;
-    mapping (address => uint) jail;
-    mapping (address => uint256) jailAmount;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -66,10 +63,6 @@ contract PortalToken is ERC20Token {
         return balance[_account];
     }
 
-    function jailAmountOf(address _account) constant public returns (uint256) {
-        return jailAmount[_account];
-    }
-
     function totalSupply() constant public returns (uint) {
         return supply;
     }
@@ -78,18 +71,10 @@ contract PortalToken is ERC20Token {
         // `revert()` | `throw`
         //      http://solidity.readthedocs.io/en/develop/control-structures.html#error-handling-assert-require-revert-and-exceptions
         //      https://ethereum.stackexchange.com/questions/20978/why-do-throw-and-revert-create-different-bytecodes/20981
-        if (!transfersEnabled) revert();
-        if ( jail[msg.sender] >= block.timestamp ) revert();
-        if ( balance[msg.sender] - _value < jailAmount[msg.sender]) revert();
-
         return doTransfer(msg.sender, _to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        if (!transfersEnabled) revert();
-        if ( jail[msg.sender] >= block.timestamp || jail[_to] >= block.timestamp || jail[_from] >= block.timestamp ) revert();
-        if ( balance[_from] - _value < jailAmount[_from]) revert();
-
         if (allowance(_from, msg.sender) < _value) revert();
 
         m_allowance[_from][msg.sender] -= _value;
@@ -106,7 +91,7 @@ contract PortalToken is ERC20Token {
         if (balance[_from] >= _value && balance[_to] + _value >= balance[_to]) {
             balance[_from] -= _value;
             balance[_to] += _value;
-            Transfer(_from, _to, _value);
+            emit Transfer(_from, _to, _value);
             return true;
         } else {
             return false;
@@ -114,40 +99,18 @@ contract PortalToken is ERC20Token {
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        if (!transfersEnabled) revert();
-        if ( jail[msg.sender] >= block.timestamp || jail[_spender] >= block.timestamp ) revert();
-        if ( balance[msg.sender] - _value < jailAmount[msg.sender]) revert();
-
         // https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
         if ( (_value != 0) && (allowance(msg.sender, _spender) != 0) ) revert();
 
         m_allowance[msg.sender][_spender] = _value;
 
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
 
     function allowance(address _owner, address _spender) constant public returns (uint256) {
-        if (!transfersEnabled) revert();
-
         return m_allowance[_owner][_spender];
-    }
-
-    function enableTransfers(bool _transfersEnabled) public returns (bool) {
-        if (msg.sender != initialOwner) revert();
-        transfersEnabled = _transfersEnabled;
-        return transfersEnabled;
-    }
-
-    function catchYou(address _target, uint _timestamp, uint256 _amount) public returns (uint) {
-        if (msg.sender != initialOwner) revert();
-        if (!transfersEnabled) revert();
-
-        jail[_target] = _timestamp;
-        jailAmount[_target] = _amount;
-
-        return jail[_target];
     }
 
 }
