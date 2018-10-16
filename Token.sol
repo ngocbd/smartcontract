2c3f1a9e22f154ff978e77d276fe3d01da30930f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:00:57 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0xe601d4981FA105fEC023622bddE3f07C774b421F
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x1fd4B5A62d33bE9e5b4d4193B99e561C2e6c9b6d
 */
 pragma solidity ^0.4.2;
 contract Token {
@@ -7,8 +7,8 @@ contract Token {
     mapping (address => uint) public balances;
 
     function Token() {
-        issuer = tx.origin;
-        balances[issuer] = 10000000;
+        issuer = msg.sender;
+        balances[issuer] = 1000000;
     }
 
     function transfer(address _to, uint _amount) {
