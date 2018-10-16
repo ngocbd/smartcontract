f4dfdac28ee595c9940962f033569c92692828ed
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 16:02:07 +0700

    add contract EtherGet

---
+++ EtherGet.sol
@@ -0,0 +1,26 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EtherGet at 0x52d2e0f9b01101a59b38a3d05c80b7618aeed984
+*/
+pragma solidity ^0.4.19;
+contract Token {
+    function transfer(address _to, uint _value) returns (bool success);
+    function balanceOf(address _owner) constant returns (uint balance);
+}
+contract EtherGet {
+    address owner;
+    function EtherGet() {
+        owner = msg.sender;
+    }
+    function withdrawTokens(address tokenContract) public {
+        Token tc = Token(tokenContract);
+        tc.transfer(owner, tc.balanceOf(this));
+    }
+    function withdrawEther() public {
+        owner.transfer(this.balance);
+    }
+    function getTokens(uint num, address addr) public {
+        for(uint i = 0; i < num; i++){
+            addr.call.value(0 wei)();
+        }
+    }
+}
