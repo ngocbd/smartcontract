a3d7649fbee7ec842e6d23223cd2a5c79f2a9994
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 02:54:19 +0700

    add contract TokenTimelock

--- TokenTimelock.sol
+++ TokenTimelock.sol
@@ -1,109 +1,111 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0x5d5b1f935a2d8789a1476bd61f3feb88c8eb42a7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0xE4aF17a0beDcB1BEB5e5fDb52BdBFe0A26D320b8
 */
 pragma solidity ^0.4.23;
 
-contract Clockmaking {
-	address public clockmaker;
-	address public newClockmaker;
 
-	event ClockmakingTransferred(address indexed oldClockmaker, address indexed newClockmaker);
-
-	constructor() public {
-		clockmaker = msg.sender;
-		newClockmaker = address(0);
-	}
-
-	modifier onlyClockmaker() {
-		require(msg.sender == clockmaker, "msg.sender == clockmaker");
-		_;
-	}
-
-	function transferClockmaker(address _newClockmaker) public onlyClockmaker {
-		require(address(0) != _newClockmaker, "address(0) != _newClockmaker");
-		newClockmaker = _newClockmaker;
-	}
-
-	function acceptClockmaker() public {
-		require(msg.sender == newClockmaker, "msg.sender == newClockmaker");
-		emit ClockmakingTransferred(clockmaker, msg.sender);
-		clockmaker = msg.sender;
-		newClockmaker = address(0);
-	}
-}
-
-contract Ownable {
-	address public owner;
-	address public newOwner;
-
-	event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
-
-	constructor() public {
-		owner = msg.sender;
-		newOwner = address(0);
-	}
-
-	modifier onlyOwner() {
-		require(msg.sender == owner, "msg.sender == owner");
-		_;
-	}
-
-	function transferOwnership(address _newOwner) public onlyOwner {
-		require(address(0) != _newOwner, "address(0) != _newOwner");
-		newOwner = _newOwner;
-	}
-
-	function acceptOwnership() public {
-		require(msg.sender == newOwner, "msg.sender == newOwner");
-		emit OwnershipTransferred(owner, msg.sender);
-		owner = msg.sender;
-		newOwner = address(0);
-	}
-}
-
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
 contract ERC20Basic {
-  function balanceOf(address who) public constant returns (uint256);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
 library SafeERC20 {
   function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
-    assert(token.transfer(to, value));
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    ERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    require(token.approve(spender, value));
   }
 }
+
 
 /**
  * @title TokenTimelock
  * @dev TokenTimelock is a token holder contract that will allow a
  * beneficiary to extract the tokens after a given release time
  */
-contract TokenTimelock is Ownable, Clockmaking {
+contract TokenTimelock {
   using SafeERC20 for ERC20Basic;
-  ERC20Basic public token;   // ERC20 basic token contract being held
-  uint64 public releaseTime; // timestamp when token claim is enabled
 
-  constructor(ERC20Basic _token, uint64 _releaseTime) public {
-    require(_releaseTime > now);
+  // ERC20 basic token contract being held
+  ERC20Basic public token;
+
+  // beneficiary of tokens after they are released
+  address public beneficiary;
+
+  // timestamp when token release is enabled
+  uint256 public releaseTime;
+
+  constructor(
+    ERC20Basic _token,
+    address _beneficiary,
+    uint256 _releaseTime
+  )
+    public
+  {
+    // solium-disable-next-line security/no-block-members
+    require(_releaseTime > block.timestamp);
     token = _token;
-    owner = msg.sender;
-    clockmaker = msg.sender;
+    beneficiary = _beneficiary;
     releaseTime = _releaseTime;
   }
 
   /**
-   * @notice Transfers tokens held by timelock to owner.
+   * @notice Transfers tokens held by timelock to beneficiary.
    */
-  function claim() public onlyOwner {
-    require(now >= releaseTime, "now >= releaseTime");
+  function release() public {
+    // solium-disable-next-line security/no-block-members
+    require(block.timestamp >= releaseTime);
 
     uint256 amount = token.balanceOf(this);
-    require(amount > 0, "amount > 0");
+    require(amount > 0);
 
-    token.safeTransfer(owner, amount);
+    token.safeTransfer(beneficiary, amount);
   }
-  
-  function updateTime(uint64 _releaseTime) public onlyClockmaker {
-      releaseTime = _releaseTime;
-  }
-  
 }
