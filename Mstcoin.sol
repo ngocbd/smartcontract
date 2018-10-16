4fde2df1dce90f13a519a5a5bdbca93870fc2b0d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 00:27:13 +0700

    add contract MSTCOIN

--- Mstcoin.sol
+++ Mstcoin.sol
@@ -1,133 +1,283 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Mstcoin at 0x64e8799f3acee96e884c7213496dc7b549e68454
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MSTCOIN at 0x33c0de76b28bd47180c38695a09d72bd2b45cf9c
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.17;
 
-contract Token {
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
+contract Ownable {
+    
+    address public owner;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    /**
+     * The address whcih deploys this contrcat is automatically assgined ownership.
+     * */
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    /**
+     * Functions with this modifier can only be executed by the owner of the contract. 
+     * */
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    event OwnershipTransferred(address indexed from, address indexed to);
 
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
+    /**
+    * Transfers ownership to new Ethereum address. This function can only be called by the 
+    * owner.
+    * @param _newOwner the address to be granted ownership.
+    **/
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != 0x0);
+        OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
 }
 
-contract StandardToken is Token {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+
+
+library SafeMath {
+    
+    function mul(uint256 a, uint256 b) internal pure  returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    function div(uint256 a, uint256 b) internal pure  returns (uint256) {
+        uint256 c = a / b;
+        return c;
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
+}
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
+
+
+contract ERC20Basic {
     uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    function balanceOf(address who) constant public returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract Mstcoin is StandardToken { // CHANGE THIS. Update the contract name.
 
-    /* Public variables of the token */
 
-    /*
-    NOTE:*************************M S T C O I N  *****************************
-    ********M S T C O I N   **************M S T C O I N  *****
-********M S T C O I N**************M S T C O I N*************M S T C O I N**************M S T C O I N ****
-    ********M S T C O I N**************M S T C O I N*************M S T C O I N**************M S T C O I N****
-*************M S T C O I N****************M S T C O I N   
-   */
-    string public name;                   // Token 
-    uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
-    string public symbol;                 // An identifier: ..
-    string public version = 'H1.0'; 
-    uint256 public Mstcoin ;     // How many units of your coin can be bought by 1 ETH?
-    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
-    address  fundsWallet;           // Where should the raised ETH go?
 
-    // This is a constructor function 
-    // which means the following function name has to match the contract name declared above
-    function Mstcoin() {
-        balances[msg.sender] = 500000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 500000000;                        // Update total supply (1000 for example) (Mstcoin )
-        name = "Mstcoin";                                   // Set the name for display purposes (MST Trading Application)
-        decimals = 0;                                               // Amount of decimals for display purposes (MST Trading Application)
-        symbol = "MST";                                             // Set the symbol for display purposes (MST Trading Application )
-                                              // Set the price of your token for the ICO (Mstcoin)
-        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) constant public returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public  returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+
+
+contract BasicToken is ERC20Basic {
+    
+    using SafeMath for uint256;
+    
+    mapping (address => uint256) internal balances;
+    
+    /**
+    * Returns the balance of the qeuried address
+    *
+    * @param _who The address which is being qeuried
+    **/
+    function balanceOf(address _who) public view returns(uint256) {
+        return balances[_who];
     }
-                                
-
-/* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+    
+    /**
+    * Allows for the transfer of MSTCOIN tokens from peer to peer. 
+    *
+    * @param _to The address of the receiver
+    * @param _value The amount of tokens to send
+    **/
+    function transfer(address _to, uint256 _value) public returns(bool) {
+        require(balances[msg.sender] >= _value && _value > 0 && _to != 0x0);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
         return true;
+    }
+}
+
+
+
+
+contract StandardToken is BasicToken, ERC20 {
+    
+    mapping (address => mapping (address => uint256)) internal allowances;
+    
+    /**
+    * Returns the amount of tokens one has allowed another to spend on his or her behalf.
+    *
+    * @param _owner The address which is the owner of the tokens
+    * @param _spender The address which has been allowed to spend tokens on the owner's
+    * behalf
+    **/
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowances[_owner][_spender];
+    }
+    
+    /**
+    * Allows for the transfer of tokens on the behalf of the owner given that the owner has
+    * allowed it previously. 
+    *
+    * @param _from The address of the owner
+    * @param _to The address of the recipient 
+    * @param _value The amount of tokens to be sent
+    **/
+    function transferFrom(address _from, address _to, uint256 _value) public  returns (bool) {
+        require(allowances[_from][msg.sender] >= _value && _to != 0x0 && balances[_from] >= _value && _value > 0);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowances[_from][msg.sender] = allowances[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+    
+    /**
+    * Allows the owner of tokens to approve another to spend tokens on his or her behalf
+    *
+    * @param _spender The address which is being allowed to spend tokens on the owner' behalf
+    * @param _value The amount of tokens to be sent
+    **/
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require(_spender != 0x0 && _value > 0);
+        if(allowances[msg.sender][_spender] > 0 ) {
+            allowances[msg.sender][_spender] = 0;
+        }
+        allowances[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+}
+
+
+
+
+contract Pausable is Ownable {
+   
+    event Pause();
+    event Unpause();
+    event Freeze ();
+    event LogFreeze();
+
+    address public constant IcoAddress = 0xe9c5c1c7dA613Ef0749492dA01129DDDbA484857;  
+    address public constant founderAddress = 0xF748D2322ADfE0E9f9b262Df6A2aD6CBF79A541A;
+
+    bool public paused = true;
+    
+    /**
+    * @dev modifier to allow actions only when the contract IS paused or if the 
+    * owner or ICO contract is invoking the action
+    */
+    modifier whenNotPaused() {
+        require(!paused || msg.sender == IcoAddress || msg.sender == founderAddress);
+        _;
+    }
+
+    /**
+    * @dev modifier to allow actions only when the contract IS NOT paused
+    */
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+    /**
+    * @dev called by the owner to pause, triggers stopped state
+    */
+    function pause() public onlyOwner {
+        paused = true;
+        Pause();
+    }
+    
+
+    /**
+    * @dev called by the owner to unpause, returns to normal state
+    */
+    function unpause() public onlyOwner {
+        paused = false;
+        Unpause();
+    }
+    
+}
+
+
+
+
+contract PausableToken is StandardToken, Pausable {
+
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
+    return super.transfer(_to, _value);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
+    return super.transferFrom(_from, _to, _value);
+  }
+
+  function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
+    return super.approve(_spender, _value);
+  }
+}
+
+
+
+
+contract MSTCOIN is PausableToken {
+    
+    function MSTCOIN() public {
+        name = "MSTCOIN";
+        symbol = "MSTCOIN";
+        decimals = 6;
+        totalSupply = 500000000e6;
+        balances[founderAddress] = totalSupply;
+        Transfer(address(this), founderAddress, totalSupply);
+    }
+    
+    event Burn(address indexed burner, uint256 value);
+    
+    /**
+    * Allows the owner to burn his own tokens.
+    * 
+    * @param _value The amount of token to be burned
+    */
+    function burn(uint256 _value) public onlyOwner {
+        _burn(msg.sender, _value);
+    }
+    
+    /**
+    * Function is internally called by the burn function. 
+    *
+    * @param _who Will always be the owners address
+    * @param _value The amount of tokens to be burned
+    **/
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balances[_who]);
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(_who, _value);
+        Transfer(_who, address(0), _value);
     }
 }
