7f08ae8f79e971a9316791e2cc61cae65f8ff8a2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:18:34 +0700

    add contract IconomiBlackHole

---
+++ IconomiBlackHole.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IconomiBlackHole at 0x0006157838d5a6b33ab66588a6a693a57c869999
+*/
+pragma solidity ^0.4.11;
+
+contract IconomiBlackHole {
+
+}
