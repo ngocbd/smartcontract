5fb4d7d71d2ce30a36023901e6acbcd69c883ae2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:03:10 +0700

    add contract Indorser

--- Indorser.sol
+++ Indorser.sol
@@ -1,65 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Indorser at 0xbfe284799e6435fc137fb0ab1792b684bc0056fe
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Indorser at 0xed18ea2286368906d1c04bf020b3076962f263e7
 */
-pragma solidity ^0.4.11;
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control 
- * functions, this simplifies the implementation of "user permissions". 
- */
-contract Ownable {
-  address public owner;
-
-
-  /** 
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner. 
-   */
-  modifier onlyOwner() {
-    if (msg.sender != owner) {
-      throw;
-    }
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to. 
-   */
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-
-}
+pragma solidity ^0.4.23;
 
 contract Token{
   function transfer(address to, uint value) returns (bool);
 }
 
-contract Indorser is Ownable {
-    
-    address _tokenAddr = 0xc5594d84B996A68326d89FB35E4B89b3323ef37d;
-    
-    function multisend(address[] _to, uint256[] _value)
+contract Indorser {
+    function multisend(address _tokenAddr, address[] _to, uint256[] _value)
     returns (bool _success) {
         assert(_to.length == _value.length);
         assert(_to.length <= 150);
         // loop through to addresses and send value
-        
         for (uint8 i = 0; i < _to.length; i++) {
-                assert((Token(_tokenAddr).transfer(_to[i], _value[i]*10**18)) == true);
+                assert((Token(_tokenAddr).transfer(_to[i], _value[i])) == true);
             }
             return true;
         }
