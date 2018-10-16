fb0807f6ec1ac7d84da4a450f415a5a792e3a3e2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 14:21:35 +0700

    add contract MINDTokenCrowdSale

--- MINDTokenCrowdSale.sol
+++ MINDTokenCrowdSale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MINDTokenCrowdSale at 0x79335d54b738d29d2534f5b69671fa00bce50baf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MINDTokenCrowdSale at 0x76cbff8f07687bc952b1f7b7066b8ee46ff3e971
 */
 pragma solidity ^0.4.11;
 
@@ -243,7 +243,7 @@ contract StandardCrowdsale {
     using SafeMath for uint256;
 
     // The token being sold
-    StandardToken public token; 
+    StandardToken public token; // Request Modification : change to not mintable
 
     // start and end timestamps where investments are allowed (both inclusive)
     uint256 public startTime;
@@ -282,10 +282,11 @@ contract StandardCrowdsale {
         rate = _rate;
         wallet = _wallet;
         
-        token = createTokenContract(); 
+        token = createTokenContract(); // Request Modification : change to StandardToken + position
     }
 
     // creates the token to be sold.
+    // Request Modification : change to StandardToken
     // override this method to have crowdsale of a specific mintable token.
     function createTokenContract() 
         internal 
@@ -302,6 +303,7 @@ contract StandardCrowdsale {
     }
 
     // low level token purchase function
+    // Request Modification : change to not mint but transfer from this contract
     function buyTokens() 
        public 
        payable 
@@ -317,7 +319,7 @@ contract StandardCrowdsale {
         // update state
         weiRaised = weiRaised.add(weiAmount);
 
-        require(token.transfer(msg.sender, tokens)); 
+        require(token.transfer(msg.sender, tokens)); // Request Modification : changed here - tranfer instead of mintable
         TokenPurchase(msg.sender, weiAmount, tokens);
 
         forwardFunds();
@@ -363,6 +365,7 @@ contract StandardCrowdsale {
         _;
     }
 
+    // Request Modification : Add check 24hours before token sale
     modifier only24HBeforeSale() {
         require(now < startTime.sub(1 days));
         _;
@@ -390,6 +393,7 @@ contract CappedCrowdsale is StandardCrowdsale {
 
   // overriding Crowdsale#validPurchase to add extra cap logic
   // @return true if investors can buy at the moment
+  // Request Modification : delete constant because needed in son contract
   function validPurchase() internal returns (bool) {
     bool withinCap = weiRaised.add(msg.value) <= cap;
     return super.validPurchase() && withinCap;
@@ -529,7 +533,7 @@ contract MINDToken is StandardToken, Ownable {
     }
 
     /** 
-    * enable transfer earlier 
+    * enable transfer earlier (only presale contract can enable the sale earlier)
     */
     function enableTransferEarlier ()
         public
@@ -561,7 +565,7 @@ contract MINDToken is StandardToken, Ownable {
  * We are using the following extensions:
  * CappedCrowdsale - sets a max boundary for raised funds
  *
- * The code is based on the contracts of Open Zeppelin and we add our contracts : MINDTokenCrowdSale and the MIND Token
+ * The code is based on the contracts of Open Zeppelin and we add our contracts : MINDTokenPreSale and the Request Token
  *
  */
 contract MINDTokenCrowdSale is Ownable, CappedCrowdsale {
@@ -574,6 +578,10 @@ contract MINDTokenCrowdSale is Ownable, CappedCrowdsale {
 
     // wallet use also to gather the ether of the token sale
     address private constant MIND_CROWDSALE_WALLET = 0x942b56E5A6e92B39643dCB5F232EF583680F0B01;
+
+    // Token initialy distributed for the full token sale (20%)
+    address public constant FULL_TOKEN_WALLET = 0x8759A03B3BEB1aa1A7bA765792cF83CaAe4f28E9;
+    uint public constant FULL_TOKEN_AMOUNT = 20000000e18;
 
     event CrowdSaleTokenSoldout();
 
@@ -592,6 +600,7 @@ contract MINDTokenCrowdSale is Ownable, CappedCrowdsale {
       internal 
       returns(StandardToken) 
     {
+        // Point to the MINDToken Contract created in Presale
         return MINDToken(0x0); // No token is created
     }
 
