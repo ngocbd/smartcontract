5865b220a10bf04b2c71d6623a53f8ea3d33cf48
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:53:10 +0700

    add contract closeImports

---
+++ closeImports.sol
@@ -0,0 +1,20 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract closeImports at 0xa05dde51cf9a133db33beb613337e68e8d779b7e
+*/
+pragma solidity ^0.4.10;
+
+// All contract balance imports closed. Contract is now fully decentralized.
+
+contract closeImports   {
+
+function closeImports()    {
+
+elix elixirContract=elix(0xc8C6A31A4A806d3710A7B38b7B296D2fABCCDBA8);
+elixirContract.lockBalanceChanges();
+
+}
+}
+
+contract elix  {
+function lockBalanceChanges();
+}
