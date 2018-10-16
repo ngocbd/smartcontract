470dc01305e7c0673c30ea014964fd2b4ac47238
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 18:16:19 +0700

    add contract Airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,102 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x9383bdb2229c0e62404d4748e337d7ffda4a4446
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x1bfd8b5bc03f18324ca9d59252927132403a88c7
 */
-//  ?????????   ?????      ???  ????      ???      ?????               ?????      ???
-//  ????????   ???????     ???  ?????   ??????    ???????             ???????     ???
-//  ????????  ???? ????    ???  ?????? ???????   ????  ???           ????  ???    ???
-//  ????????  ??????????   ???  ????????? ????   ??????????         ???????????   ???
-//  ???      ????    ????  ???  ???  ???  ????  ????    ????  ??   ?????    ????  ???
-//
-//
-// Congratulations! Its your free airdrop token.
-// Get ready to participate in token sale www.faima.ai 
-// Pre-ICO starts on 1.03.2018
-// Private SALE IS OPEN now
-// 1BTC=20000$+bonus
-// 1ETH=2000$+bonus
-// for booking: ceo@faima.ai
-
-pragma solidity 0.4.18;
-
-contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() { require(msg.sender == owner); _; }
-
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        owner = newOwner;
-        OwnershipTransferred(owner, newOwner);
-    }
-}
-
-contract Withdrawable is Ownable {
-    function withdrawEther(address _to, uint _value) onlyOwner public returns(bool) {
-        require(_to != address(0));
-        require(this.balance >= _value);
-
-        _to.transfer(_value);
-
-        return true;
-    }
-
-    function withdrawTokens(ERC20 _token, address _to, uint _value) onlyOwner public returns(bool) {
-        require(_to != address(0));
-
-        return _token.transfer(_to, _value);
-    }
-}
-
 contract ERC20 {
-    uint256 public totalSupply;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function balanceOf(address who) public view returns(uint256);
-    function transfer(address to, uint256 value) public returns(bool);
-    function transferFrom(address from, address to, uint256 value) public returns(bool);
-    function allowance(address owner, address spender) public view returns(uint256);
-    function approve(address spender, uint256 value) public returns(bool);
+  function transfer(address _recipient, uint256 _value) public returns (bool success);
 }
 
-contract AirDrop is Withdrawable {
-    event TransferEther(address indexed to, uint256 value);
-
-    function tokenBalanceOf(ERC20 _token) public view returns(uint256) {
-        return _token.balanceOf(this);
+contract Airdrop {
+  function drop(ERC20 token, address[] recipients, uint256[] values) public {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      token.transfer(recipients[i], values[i]);
     }
-
-    function tokenAllowance(ERC20 _token, address spender) public view returns(uint256) {
-        return _token.allowance(this, spender);
-    }
-    
-    function tokenTransfer(ERC20 _token, uint _value, address[] _to) onlyOwner public {
-        require(_token != address(0));
-
-        for(uint i = 0; i < _to.length; i++) {
-            require(_token.transfer(_to[i], _value));
-        }
-    }
-    
-    function tokenTransferFrom(ERC20 _token, address spender, uint _value, address[] _to) onlyOwner public {
-        require(_token != address(0));
-
-        for(uint i = 0; i < _to.length; i++) {
-            require(_token.transferFrom(spender, _to[i], _value));
-        }
-    }
-
-    function etherTransfer(uint _value, address[] _to) onlyOwner payable public {
-        for(uint i = 0; i < _to.length; i++) {
-            _to[i].transfer(_value);
-            TransferEther(_to[i], _value);
-        }
-    }
+  }
 }
