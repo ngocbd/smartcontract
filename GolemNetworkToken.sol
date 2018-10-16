bf2ae600fb0894f6e9bed28f32067ae2c96f25f1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 09:59:29 +0700

    add contract GolemNetworkToken

--- GolemNetworkToken.sol
+++ GolemNetworkToken.sol
@@ -1,18 +1,91 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GolemNetworkToken at 0x28297291aebef85bed1b15b9d5fca3146c6ebb3f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GolemNetworkToken at 0xde88ec7b6ac36ed99865098bebbc7756b00aed6f
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.2;
+
+/// @title GNT Allocation - Time-locked vault of tokens allocated
+/// to developers and Golem Factory
+contract GNTAllocation {
+    // Total number of allocations to distribute additional tokens among
+    // developers and the Golem Factory. The Golem Factory has right to 20000
+    // allocations, developers to 10000 allocations, divides among individual
+    // developers by numbers specified in  `allocations` table.
+    uint256 constant totalAllocations = 30000;
+
+    // Addresses of developer and the Golem Factory to allocations mapping.
+    mapping (address => uint256) allocations;
+
+    GolemNetworkToken gnt;
+    uint256 unlockedAt;
+
+    uint256 tokensCreated = 0;
+
+    function GNTAllocation(address _golemFactory) internal {
+        gnt = GolemNetworkToken(msg.sender);
+        unlockedAt = now + 30 minutes;
+
+        // For the Golem Factory:
+        allocations[_golemFactory] = 20000; // 12/18 pp of 30000 allocations.
+
+        // For developers:
+        allocations[0x3F4e79023273E82EfcD8B204fF1778e09df1a597] = 2500; // 25.0% of developers' allocations (10000).
+        allocations[0x1A5218B6E5C49c290745552481bb0335be2fB0F4] =  730; //  7.3% of developers' allocations.
+        allocations[0x00eA32D8DAe74c01eBe293C74921DB27a6398D57] =  730;
+        allocations[0xde03] =  730;
+        allocations[0xde04] =  730;
+        allocations[0xde05] =  730;
+        allocations[0xde06] =  630; //  6.3% of developers' allocations.
+        allocations[0xde07] =  630;
+        allocations[0xde08] =  630;
+        allocations[0xde09] =  630;
+        allocations[0xde10] =  310; //  3.1% of developers' allocations.
+        allocations[0xde11] =  153; //  1.53% of developers' allocations.
+        allocations[0xde12] =  150; //  1.5% of developers' allocations.
+        allocations[0xde13] =  100; //  1.0% of developers' allocations.
+        allocations[0xde14] =  100;
+        allocations[0xde15] =  100;
+        allocations[0xde16] =   70; //  0.7% of developers' allocations.
+        allocations[0xde17] =   70;
+        allocations[0xde18] =   70;
+        allocations[0xde19] =   70;
+        allocations[0xde20] =   70;
+        allocations[0xde21] =   42; //  0.42% of developers' allocations.
+        allocations[0xde22] =   25; //  0.25% of developers' allocations.
+    }
+
+    /// @notice Allow developer to unlock allocated tokens by transferring them
+    /// from GNTAllocation to developer's address.
+    function unlock() external {
+        if (now < unlockedAt) throw;
+
+        // During first unlock attempt fetch total number of locked tokens.
+        if (tokensCreated == 0)
+            tokensCreated = gnt.balanceOf(this);
+
+        var allocation = allocations[msg.sender];
+        allocations[msg.sender] = 0;
+        var toTransfer = tokensCreated * allocation / totalAllocations;
+
+        // Will fail if allocation (and therefore toTransfer) is 0.
+        if (!gnt.transfer(msg.sender, toTransfer)) throw;
+    }
+}
+
+/// @title Migration Agent interface
+contract MigrationAgent {
+    function migrateFrom(address _from, uint256 _value);
+}
 
 /// @title Golem Network Token (GNT) - crowdfunding code for Golem Project
 contract GolemNetworkToken {
-    string public constant name = "Token Network Token";
+    string public constant name = "Test Network Token";
     string public constant symbol = "TNT";
     uint8 public constant decimals = 18;  // 18 decimal places, the same as ETH.
 
     uint256 public constant tokenCreationRate = 1000;
 
     // The funding cap in weis.
-    uint256 public constant tokenCreationCap = 2 ether * tokenCreationRate;
+    uint256 public constant tokenCreationCap = 3 ether * tokenCreationRate;
     uint256 public constant tokenCreationMin = 1 ether * tokenCreationRate;
 
     uint256 public fundingStartBlock;
@@ -205,77 +278,4 @@ contract GolemNetworkToken {
         Refund(msg.sender, ethValue);
         if (!msg.sender.send(ethValue)) throw;
     }
-}
-
-/// @title GNT Allocation - Time-locked vault of tokens allocated 
-/// to developers and Golem Factory
-contract GNTAllocation {
-    // Total number of allocations to distribute additional tokens among
-    // developers and the Golem Factory. The Golem Factory has right to 20000
-    // allocations, developers to 10000 allocations, divides among individual
-    // developers by numbers specified in  `allocations` table.
-    uint256 constant totalAllocations = 30000;
-
-    // Addresses of developer and the Golem Factory to allocations mapping.
-    mapping (address => uint256) allocations;
-
-    GolemNetworkToken gnt;
-    uint256 unlockedAt;
-
-    uint256 tokensCreated = 0;
-
-    function GNTAllocation(address _golemFactory) internal {
-        gnt = GolemNetworkToken(msg.sender);
-        unlockedAt = now + 6 * 30 days;
-
-        // For the Golem Factory:
-        allocations[_golemFactory] = 20000; // 12/18 pp of 30000 allocations.
-
-        // For developers:
-        allocations[0xde00] = 2500; // 25.0% of developers' allocations (10000).
-        allocations[0xde01] =  730; //  7.3% of developers' allocations.
-        allocations[0xde02] =  730;
-        allocations[0xde03] =  730;
-        allocations[0xde04] =  730;
-        allocations[0xde05] =  730;
-        allocations[0xde06] =  630; //  6.3% of developers' allocations.
-        allocations[0xde07] =  630;
-        allocations[0xde08] =  630;
-        allocations[0xde09] =  630;
-        allocations[0xde10] =  310; //  3.1% of developers' allocations.
-        allocations[0xde11] =  153; //  1.53% of developers' allocations.
-        allocations[0xde12] =  150; //  1.5% of developers' allocations.
-        allocations[0xde13] =  100; //  1.0% of developers' allocations.
-        allocations[0xde14] =  100;
-        allocations[0xde15] =  100;
-        allocations[0xde16] =   70; //  0.7% of developers' allocations.
-        allocations[0xde17] =   70;
-        allocations[0xde18] =   70;
-        allocations[0xde19] =   70;
-        allocations[0xde20] =   70;
-        allocations[0xde21] =   42; //  0.42% of developers' allocations.
-        allocations[0xde22] =   25; //  0.25% of developers' allocations.
-    }
-
-    /// @notice Allow developer to unlock allocated tokens by transferring them 
-    /// from GNTAllocation to developer's address.
-    function unlock() external {
-        if (now < unlockedAt) throw;
-
-        // During first unlock attempt fetch total number of locked tokens.
-        if (tokensCreated == 0)
-            tokensCreated = gnt.balanceOf(this);
-
-        var allocation = allocations[msg.sender];
-        allocations[msg.sender] = 0;
-        var toTransfer = tokensCreated * allocation / totalAllocations;
-
-        // Will fail if allocation (and therefore toTransfer) is 0.
-        if (!gnt.transfer(msg.sender, toTransfer)) throw;
-    }
-}
-
-/// @title Migration Agent interface
-contract MigrationAgent {
-    function migrateFrom(address _from, uint256 _value);
 }
