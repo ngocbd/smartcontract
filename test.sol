507e1def4ebef9e064b1a36a689b1e0cfd5ccf0a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:33:46 +0700

    add contract Test

--- test.sol
+++ test.sol
@@ -1,12 +1,37 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0x9622d66f1772531e04df2c3f83601d9b04d10db2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Test at 0xc07f78bd412c53af2fabc56bbda107b718c6cd9d
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
 contract Test {
-    uint public test;
-    
-    function Test() {
-        test = 1;
-    }
+
+	//address who runs test
+	address public owner;
+
+	//max integer result
+	uint8 public maxResult;
+
+	function Test() {
+		owner = msg.sender;
+		maxResult = 100;
+	}
+
+	function() {
+		revert();
+	}
+
+	//get result of random
+	function getResult(uint index) constant returns (uint8 a)
+	{
+		bytes32 blockHash = block.blockhash(index);
+		bytes32 shaPlayer = sha3(owner, blockHash);
+		a = uint8(uint256(shaPlayer) % maxResult);
+	}
+
+	//to check hash in js and in solidity
+	function getResultblockHash(bytes32 blockHash) constant returns (uint8 a)
+	{
+		bytes32 shaPlayer = sha3(owner, blockHash);
+		a = uint8(uint256(shaPlayer) % maxResult);
+	}
 }
