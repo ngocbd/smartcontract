30fae98cbfed5235b0909edd0aac3b56a0a6f6a4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:38:19 +0700

    add contract MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,7 +1,23 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x73a8de5bbeab01b0be663cea7b64a0c35de8df29
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MyToken at 0x6bce536e0a938dc19dbcbb045b46b93c3d13620b
 */
 pragma solidity ^0.4.13;
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) revert();
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
 
 contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 
@@ -19,9 +35,6 @@ contract MyToken {
     /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyToken(
         uint256 initialSupply,
@@ -36,73 +49,119 @@ contract MyToken {
         decimals = decimalUnits;                            // Amount of decimals for display purposes
     }
 
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(_from, _to, _value);
-    }
-
-    /// @notice Send `_value` tokens to `_to` from your account
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
+    /* Send coins */
     function transfer(address _to, uint256 _value) {
-        _transfer(msg.sender, _to, _value);
+        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
 
-    /// @notice Send `_value` tokens to `_to` in behalf of `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        require (_value < allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
+    /* Allow another contract to spend some tokens in your behalf */
     function approve(address _spender, uint256 _value)
         returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
 
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
-    /// @param _extraData some extra information to send to the approved contract
+    /* Approve and then communicate the approved contract in a single tx */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
+        returns (bool success) {    
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
             return true;
         }
-    }        
+    }
 
-    /// @notice Remove `_value` tokens from the system irreversibly
-    /// @param _value the amount of money to burn
-    function burn(uint256 _value) returns (bool success) {
-        require (balanceOf[msg.sender] > _value);            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
-        totalSupply -= _value;                                // Updates totalSupply
-        Burn(msg.sender, _value);
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
         return true;
     }
 
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        revert();     // Prevents accidental sending of ether
+    }
+}
+
+contract TemplateToken is owned, MyToken {
+
+    uint public buyRate = 10000;
+    bool public isSelling = true;
+
+    mapping (address => bool) public frozenAccount;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event FrozenFunds(address target, bool frozen);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function TemplateToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+    ) MyToken (initialSupply, tokenName, decimalUnits, tokenSymbol) {}
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
+        if (frozenAccount[msg.sender]) revert();                // Check if frozen
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (frozenAccount[_from]) revert();                        // Check if frozen            
+        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
         return true;
+    }
+
+    function mintToken(address target, uint256 mintedAmount) onlyOwner {
+        balanceOf[target] += mintedAmount;
+        Transfer(0, owner, mintedAmount);
+        Transfer(owner, target, mintedAmount);
+    }
+
+    function freezeAccount(address target, bool freeze) onlyOwner {
+        frozenAccount[target] = freeze;
+        FrozenFunds(target, freeze);
+    }
+
+    function setBuyRate(uint newBuyRate) onlyOwner {
+        buyRate = newBuyRate;
+    }
+    
+    function setSelling(bool newStatus) onlyOwner {
+        isSelling = newStatus;
+    }
+
+    function buy() payable {
+        if(isSelling == false) revert();
+        uint amount = msg.value * buyRate;                  // calculates the amount
+        balanceOf[msg.sender] += amount;                   // adds the amount to buyer's balance
+        balanceOf[owner] -= amount;                         // subtracts amount from seller's balance
+        Transfer(owner, msg.sender, amount);                // execute an event reflecting the change
+    }
+    
+    function withdrawToOwner(uint256 amountWei) onlyOwner {
+        owner.transfer(amountWei);
     }
 }
