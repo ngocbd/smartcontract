7e018dd3009098150ca9714425e552c874a6ad81
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:41:59 +0700

    add contract debug

---
+++ debug.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract debug at 0xe18b0279521631528c6ce72d7659133b33987c86
+*/
+pragma solidity 0.4.23;
+
+contract debug {
+    function () public  payable{
+        revert("GET OUT!");
+    }
+}
