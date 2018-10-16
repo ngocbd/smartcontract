0551fa78d5b2bf7422214b0f3f332745110ae319
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 23:22:20 +0700

    add contract ERC827Receiver

---
+++ ERC827Receiver.sol
@@ -0,0 +1,12 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC827Receiver at 0x77a3bf09ce6236d89cf79b12a2b755b3dbe3a80e
+*/
+contract ERC827Receiver {
+  function ERC827Receiver() {
+    
+  }
+
+  function receive() returns(bool){
+    return true;
+  }
+}
