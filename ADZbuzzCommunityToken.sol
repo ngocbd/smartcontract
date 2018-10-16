3e98b894c15d9438367442fdaca7fce7102ace34
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 22:32:25 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x187be645188bf025947725d6cf1b8618899b2c2a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x420c23af4d87dfc3bd48880d124d95113e693ab0
 */
 pragma solidity ^0.4.18;
     
     // ----------------------------------------------------------------------------
-    // 'ACT238324' token contract
+    // 'ACT780489' token contract
     //
     // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-    // Symbol      : ACT238324
-    // Name        : ADZbuzz Phandroid.com Community Token
+    // Symbol      : ACT780489
+    // Name        : ADZbuzz Petapixel.com Community Token
     // Total supply: 2000000
     // Decimals    : 8
     //
@@ -117,8 +117,8 @@ pragma solidity ^0.4.18;
         // Constructor
         // ------------------------------------------------------------------------
         function ADZbuzzCommunityToken() public {
-            symbol = "ACT238324";
-            name = "ADZbuzz Phandroid.com Community Token";
+            symbol = "ACT780489";
+            name = "ADZbuzz Petapixel.com Community Token";
             decimals = 8;
             _totalSupply = 200000000000000;
             balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
