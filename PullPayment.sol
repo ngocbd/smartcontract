8752ea02d5a41befd64f65dcaa90de43dd406abf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:44:56 +0700

    add contract PullPayment

--- PullPayment.sol
+++ PullPayment.sol
@@ -1,38 +1,47 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PullPayment at 0x36cda1038a9556ebe626bcd7bab4e46f38d896ec
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PullPayment at 0x69cca8df17623f18a001b96ba4d958ce72bc6bdc
 */
-pragma solidity 0.4.11;
+pragma solidity 0.4.16;
+
+contract ControllerInterface {
 
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+  // State Variables
+  bool public paused;
+  address public nutzAddr;
 
-  function div(uint256 a, uint256 b) internal returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+  // Nutz functions
+  function babzBalanceOf(address _owner) constant returns (uint256);
+  function activeSupply() constant returns (uint256);
+  function burnPool() constant returns (uint256);
+  function powerPool() constant returns (uint256);
+  function totalSupply() constant returns (uint256);
+  function allowance(address _owner, address _spender) constant returns (uint256);
 
-  function sub(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+  function approve(address _owner, address _spender, uint256 _amountBabz) public;
+  function transfer(address _from, address _to, uint256 _amountBabz, bytes _data) public;
+  function transferFrom(address _sender, address _from, address _to, uint256 _amountBabz, bytes _data) public;
 
-  function add(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+  // Market functions
+  function floor() constant returns (uint256);
+  function ceiling() constant returns (uint256);
+
+  function purchase(address _sender, uint256 _value, uint256 _price) public returns (uint256);
+  function sell(address _from, uint256 _price, uint256 _amountBabz);
+
+  // Power functions
+  function powerBalanceOf(address _owner) constant returns (uint256);
+  function outstandingPower() constant returns (uint256);
+  function authorizedPower() constant returns (uint256);
+  function powerTotalSupply() constant returns (uint256);
+
+  function powerUp(address _sender, address _from, uint256 _amountBabz) public;
+  function downTick(address _owner, uint256 _now) public;
+  function createDownRequest(address _owner, uint256 _amountPower) public;
+  function downs(address _owner) constant public returns(uint256, uint256, uint256);
+  function downtime() constant returns (uint256);
 }
+
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
@@ -70,40 +79,36 @@ contract Ownable {
   }
 
 }
-contract ControllerInterface {
 
 
-  // State Variables
-  bool public paused;
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-  // Nutz functions
-  function babzBalanceOf(address _owner) constant returns (uint256);
-  function activeSupply() constant returns (uint256);
-  function burnPool() constant returns (uint256);
-  function powerPool() constant returns (uint256);
-  function totalSupply() constant returns (uint256);
-  function allowance(address _owner, address _spender) constant returns (uint256);
+  function div(uint256 a, uint256 b) internal returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-  function approve(address _owner, address _spender, uint256 _amountBabz) public;
-  function transfer(address _from, address _to, uint256 _amountBabz, bytes _data) public returns (bool);
-  function transferFrom(address _sender, address _from, address _to, uint256 _amountBabz, bytes _data) public returns (bool);
+  function sub(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-  // Market functions
-  function floor() constant returns (uint256);
-  function ceiling() constant returns (uint256);
-
-  function purchase(address _sender, uint256 _price) public payable returns (uint256, bool);
-  function sell(address _from, uint256 _price, uint256 _amountBabz) public;
-
-  // Power functions
-  function powerBalanceOf(address _owner) constant returns (uint256);
-  function outstandingPower() constant returns (uint256);
-  function authorizedPower() constant returns (uint256);
-  function powerTotalSupply() constant returns (uint256);
-
-  function powerUp(address _sender, address _from, uint256 _amountBabz) public;
-  function downTick(uint256 _pos, uint256 _now) public;
-  function createDownRequest(address _owner, uint256 _amountPower) public;
+  function add(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
 /**
@@ -113,28 +118,31 @@ contract ControllerInterface {
 contract PullPayment is Ownable {
   using SafeMath for uint256;
 
-  struct Payment {
-    uint256 value;  // TODO: use compact storage
-    uint256 date;   //
-  }
 
   uint public dailyLimit = 1000000000000000000000;  // 1 ETH
   uint public lastDay;
   uint public spentToday;
 
-  mapping(address => Payment) internal payments;
+  // 8bytes date, 24 bytes value
+  mapping(address => uint256) internal payments;
+
+  modifier onlyNutz() {
+    require(msg.sender == ControllerInterface(owner).nutzAddr());
+    _;
+  }
 
   modifier whenNotPaused () {
     require(!ControllerInterface(owner).paused());
      _;
   }
+
   function balanceOf(address _owner) constant returns (uint256 value) {
-    return payments[_owner].value;
+    return uint192(payments[_owner]);
   }
 
   function paymentOf(address _owner) constant returns (uint256 value, uint256 date) {
-    value = payments[_owner].value;
-    date = payments[_owner].date;
+    value = uint192(payments[_owner]);
+    date = (payments[_owner] >> 192);
     return;
   }
 
@@ -147,32 +155,33 @@ contract PullPayment is Ownable {
   function changeWithdrawalDate(address _owner, uint256 _newDate)  public onlyOwner {
     // allow to withdraw immediately
     // move witdrawal date more days into future
-    payments[_owner].date = _newDate;
+    payments[_owner] = (_newDate << 192) + uint192(payments[_owner]);
   }
 
-  function asyncSend(address _dest) public payable onlyOwner {
+  function asyncSend(address _dest) public payable onlyNutz {
     require(msg.value > 0);
-    uint256 newValue = payments[_dest].value.add(msg.value);
+    uint256 newValue = msg.value.add(uint192(payments[_dest]));
     uint256 newDate;
     if (isUnderLimit(msg.value)) {
-      newDate = (payments[_dest].date > now) ? payments[_dest].date : now;
+      uint256 date = payments[_dest] >> 192;
+      newDate = (date > now) ? date : now;
     } else {
       newDate = now.add(3 days);
     }
     spentToday = spentToday.add(msg.value);
-    payments[_dest] = Payment(newValue, newDate);
+    payments[_dest] = (newDate << 192) + uint192(newValue);
   }
 
 
   function withdraw() public whenNotPaused {
     address untrustedRecipient = msg.sender;
-    uint256 amountWei = payments[untrustedRecipient].value;
+    uint256 amountWei = uint192(payments[untrustedRecipient]);
 
     require(amountWei != 0);
-    require(now >= payments[untrustedRecipient].date);
+    require(now >= (payments[untrustedRecipient] >> 192));
     require(this.balance >= amountWei);
 
-    payments[untrustedRecipient].value = 0;
+    payments[untrustedRecipient] = 0;
 
     untrustedRecipient.transfer(amountWei);
   }
