3eb37f77789c06e8a4613cc88f818656326731ab
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 05:46:13 +0700

    add contract Jackpot

--- JackPot.sol
+++ JackPot.sol
@@ -1,193 +1,101 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jackpot at 0x9e6D1709335101D17FD86367Bce7FbeD07680b2d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jackpot at 0x0e9a67c02b7de4482125cdd3afa28306e7259b30
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.23;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+contract Jackpot {
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0 uint256 c = a / b;
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    uint64 public nextJackpotTime;
+    bool public jackpotPaused;
+    address public owner;
+    uint public jackpotPersent = 100;
+    uint public  winnerLimit = 1;
+    uint public JackpotPeriods = 1;
+    address public diceRollAddress;
+    uint256 seed;
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    mapping (uint=>address) public winnerHistory;
+    address[] public tempPlayer;
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
+    event SendJackpotSuccesss(address indexed winner, uint amount, uint JackpotPeriods);
+    event OwnerTransfer(address SentToAddress, uint AmountTransferred);
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-}
-
-contract Jackpot is Ownable {
-
-  string public constant name = "Jackpot";
-
-  event newWinner(address winner, uint256 ticketNumber);
-  // event newRandomNumber_bytes(bytes);
-  // event newRandomNumber_uint(uint);
-  event newContribution(address contributor, uint value);
-
-  using SafeMath for uint256;
-  address[] public players = new address[](10);
-  uint256 public lastTicketNumber = 0;
-  uint8 public lastIndex = 0;
-
-  struct tickets {
-    uint256 startTicket;
-    uint256 endTicket;
-  }
-
-  mapping (address => tickets[]) public ticketsMap;
-  mapping (address => uint256) public contributions;
-
-
-  function executeLottery() { 
-      
-        if (lastIndex > 9) {
-          uint randomNumber = this.balance.mul(16807) % 2147483647;
-          randomNumber = randomNumber % lastTicketNumber;
-          address winner;
-          bool hasWon;
-          for (uint8 i = 0; i < lastIndex; i++) {
-            address player = players[i];
-            for (uint j = 0; j < ticketsMap[player].length; j++) {
-              uint256 start = ticketsMap[player][j].startTicket;
-              uint256 end = ticketsMap[player][j].endTicket;
-              if (randomNumber >= start && randomNumber < end) {
-                winner = player;
-                hasWon = true;
-                break;
-              }
-            }
-            if(hasWon) break;
-          }
-          require(winner!=address(0) && hasWon);
-
-          for (uint8 k = 0; k < lastIndex; k++) {
-            delete ticketsMap[players[k]];
-            delete contributions[players[k]];
-          }
-
-          lastIndex = 0;
-          lastTicketNumber = 0;
-
-          uint balance = this.balance;
-          if (!owner.send(balance/10)) throw;
-          //Both SafeMath.div and / throws on error
-          if (!winner.send(balance - balance/10)) throw;
-          newWinner(winner, randomNumber);
-          
-        }
-      
-  }
-
-  function getPlayers() constant returns (address[], uint256[]) {
-    address[] memory addrs = new address[](lastIndex);
-    uint256[] memory _contributions = new uint256[](lastIndex);
-    for (uint i = 0; i < lastIndex; i++) {
-      addrs[i] = players[i];
-      _contributions[i] = contributions[players[i]];
-    }
-    return (addrs, _contributions);
-  }
-
-  function getTickets(address _addr) constant returns (uint256[] _start, uint256[] _end) {
-    tickets[] tks = ticketsMap[_addr];
-    uint length = tks.length;
-    uint256[] memory startTickets = new uint256[](length);
-    uint256[] memory endTickets = new uint256[](length);
-    for (uint i = 0; i < length; i++) {
-      startTickets[i] = tks[i].startTicket;
-      endTickets[i] = tks[i].endTicket;
-    }
-    return (startTickets, endTickets);
-  }
-
-  function() payable {
-    uint256 weiAmount = msg.value;
-    require(weiAmount >= 1e16);
-
-    bool isSenderAdded = false;
-    for (uint8 i = 0; i < lastIndex; i++) {
-      if (players[i] == msg.sender) {
-        isSenderAdded = true;
-        break;
-      }
-    }
-    if (!isSenderAdded) {
-      players[lastIndex] = msg.sender;
-      lastIndex++;
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
 
-    tickets memory senderTickets;
-    senderTickets.startTicket = lastTicketNumber;
-    uint256 numberOfTickets = weiAmount/1e15;
-    senderTickets.endTicket = lastTicketNumber.add(numberOfTickets);
-    lastTicketNumber = lastTicketNumber.add(numberOfTickets);
-    ticketsMap[msg.sender].push(senderTickets);
-
-    contributions[msg.sender] = contributions[msg.sender].add(weiAmount);
-
-    newContribution(msg.sender, weiAmount);
-
-    if(lastIndex > 9) {
-      executeLottery();
+    modifier onlyDiceRoll {
+        require(msg.sender == diceRollAddress);
+        _;
     }
-  }
+
+    modifier jackpotAreActive {
+        require(!jackpotPaused);
+        _;
+    }
+
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+
+    function() external payable {
+
+    }
+
+    function getWinnerHistory(uint periods) external view returns(address){
+        return winnerHistory[periods];
+    }
+
+    function addPlayer(address add) public onlyDiceRoll jackpotAreActive{
+        tempPlayer.push(add);
+        
+    }
+
+    function createWinner() public onlyOwner jackpotAreActive {
+        require(tempPlayer.length > 0);
+        uint random = rand() % tempPlayer.length;
+        address winner = tempPlayer[random];
+        winnerHistory[JackpotPeriods] = winner;
+        uint64 tmNow = uint64(block.timestamp);
+        nextJackpotTime = tmNow + 72000;
+        tempPlayer.length = 0;
+        sendJackpot(winner, address(this).balance * jackpotPersent / 1000);
+        JackpotPeriods += 1;
+    }
+
+
+    function sendJackpot(address winner, uint256 amount) internal {
+        require(address(this).balance > amount);
+        emit SendJackpotSuccesss(winner, amount,JackpotPeriods);
+        winner.transfer(amount);
+        
+    }
+
+    function seTJackpotPersent(uint newPersent) external onlyOwner{
+        require(newPersent > 0 && newPersent < 1000);
+        jackpotPersent = newPersent;
+    }
+
+    function rand() internal returns (uint256) {
+        seed = uint256(keccak256(seed, blockhash(block.number - 1), block.coinbase, block.difficulty));
+        return seed;
+    }
+
+
+    function ownerPauseJackpot(bool newStatus) public onlyOwner{
+        jackpotPaused = newStatus;
+    }
+
+    function ownerSetdiceRollAddress(address add) public onlyOwner {
+        diceRollAddress = add;
+    }
+
+    function ownerTransferEther(address sendTo, uint amount) public onlyOwner{    
+        sendTo.transfer(amount);
+        emit OwnerTransfer(sendTo, amount);
+    }
+
 }
