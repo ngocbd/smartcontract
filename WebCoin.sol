be835cfb5aa11e200f385fbb4e476f422a9b682d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:35:48 +0700

    add contract WEBCOIN

--- WebCoin.sol
+++ WebCoin.sol
@@ -1,44 +1,70 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WebCoin at 0xed2c627ba90482bf01578957579366880c0c4ab4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WEBCOIN at 0x201916ad6b61df24a0982f885376d382e764ee5e
 */
-contract WebCoin{
-    /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public initialSupply;
-    uint256 public totalSupply;
+pragma solidity ^0.4.11;
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+contract ERC20Standard {
+	
+	mapping (address => uint256) balances;
+	mapping (address => mapping (address => uint)) allowed;
 
-  
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function WebCoin() {
+	//Fix for short address attack against ERC20
+	modifier onlyPayloadSize(uint size) {
+		assert(msg.data.length == size + 4);
+		_;
+	} 
 
-         initialSupply = 1000000000;
-         name ="WebCoin";
-        decimals = 8;
-         symbol = "Web";
-        
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-                                   
+	function balanceOf(address _owner) public constant returns (uint balance) {
+	    return balances[_owner];
+	}
+
+	function transfer(address _recipient, uint _value) onlyPayloadSize(2*32) public {
+		require(balances[msg.sender] >= _value && _value > 0);
+	    balances[msg.sender] -= _value;
+	    balances[_recipient] += _value;
+	    Transfer(msg.sender, _recipient, _value);        
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-      
+	function transferFrom(address _from, address _to, uint _value) public {
+		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
     }
 
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function () {
-        throw;     // Prevents accidental sending of ether
-    }
+	function approve(address _spender, uint _value) public {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+	}
+
+	function allowance(address _spender, address _owner) public constant returns (uint balance) {
+		return allowed[_owner][_spender];
+	}
+
+	//Event which is triggered to log all transfers to this contract's event log
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint _value
+		);
+		
+	//Event is triggered whenever an owner approves a new allowance for a spender.
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint _value
+		);
+
+}
+
+contract WEBCOIN is ERC20Standard {
+	string public name = "WEBCoin";
+	uint8 public decimals = 18;
+	string public symbol = "WEB";
+	uint public totalSupply = 21000000000000000000000000;
+	    
+	function WEBCOIN() {
+	    balances[msg.sender] = totalSupply;
+	}
 }
