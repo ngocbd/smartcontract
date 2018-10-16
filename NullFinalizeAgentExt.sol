d5221f6a5e36edd39d8c55551e80e8a33c80792d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:24:25 +0700

    add contract NullFinalizeAgentExt

--- NullFinalizeAgentExt.sol
+++ NullFinalizeAgentExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgentExt at 0x82C0bB373d8a19b7d770c01CadF5D948DCDc1A75
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NullFinalizeAgentExt at 0x62F0646eFB09c28518E787D0C7675D04013dB44f
 */
 pragma solidity ^0.4.11;
 
