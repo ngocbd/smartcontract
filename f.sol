0770d15350e14236a1e4dd3adb2d3a5f940bfa40
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 10:03:44 +0700

    add contract f

---
+++ f.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract f at 0x989d5889c93ABd6789aD8B4810060A255cb9a96A
+*/
+pragma solidity 0.4.8;
+
+contract f {
+    function a() constant returns (string b){
+        return "tickletickle vac ban in csgo march 10 2017";
+    }
+}
