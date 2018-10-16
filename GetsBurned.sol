7ab4e616f0aa1d51af7955cb0a24834be0acde75
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 17:54:23 +0700

    add contract GetsBurned

--- GetsBurned.sol
+++ GetsBurned.sol
@@ -1,14 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GetsBurned at 0x07d6c9b63dc0af6bdb32e924fc4e1a689391d230
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GetsBurned at 0x2f976a5382c0c6214b3d8fe6bdc5379edd35d9d4
 */
-pragma solidity ^0.4.17;
-
+pragma solidity ^0.4.0;
 contract GetsBurned {
-
-    function () payable {
+    function () payable public {
     }
 
-    function BurnMe () {
+    function BurnMe() public {
         // Selfdestruct and send eth to self, 
         selfdestruct(address(this));
     }
