6496f0fe418a3879adbcb8f85e7342d7fadcb9d6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 21:34:30 +0700

    add contract MikeyLovesMully

---
+++ MikeyLovesMully.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MikeyLovesMully at 0xb5194ff3f7d7a352c4cfb085e3fe16ae70243f3a
+*/
+pragma solidity ^0.4.20;
+
+contract MikeyLovesMully 
+{
+	string constant public poster = "Michael";
+	string constant public message = "Happy anniversary Mully! I love you forever.";
+}
