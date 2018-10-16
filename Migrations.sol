9511ad973110c3442c181e04da0cfd92021c3156
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 13:59:39 +0700

    add contract Migrations

--- Migrations.sol
+++ Migrations.sol
@@ -1,26 +1,61 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0xd983815f941df09dd815ed65276c20c969e47c59
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0x7a055da1ff6fc1570bfbdbb1fecff4fb4987ab3c
 */
-pragma solidity ^0.4.17;
+pragma solidity 0.4.18;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+/*
+ * https://github.com/OpenZeppelin/zeppelin-solidity
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Smart Contract Solutions, Inc.
+ */
+contract Ownable {
 
-  function Migrations() public {
-    owner = msg.sender;
-  }
+    address public owner;
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+
+contract Migrations is Ownable {
+    /* solhint-disable var-name-mixedcase */
+    /* solhint-disable func-param-name-mixedcase */
+
+    uint public last_completed_migration;
+
+    function setCompleted(uint completed) public onlyOwner {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public onlyOwner {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
