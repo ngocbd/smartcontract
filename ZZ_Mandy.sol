af8535f2aa4a40467abc659e403f74f455880f96
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-16 15:48:36 +0700

    add contract ZZ_Mandy

---
+++ ZZ_Mandy.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ZZ_Mandy at 0xb19c9e527fe2476cB26d7B92624b6fe806b70Ae1
+*/
+pragma solidity ^0.4.19;
+contract ZZ_Mandy
+{
+    function getMessage() public pure returns (bytes32) {
+        return "ZZ loves mandy.";
+    }
+}
