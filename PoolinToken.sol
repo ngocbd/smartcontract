9ca3b1412d22ab7c82ae6fcb37873062e2c3e94a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:50:24 +0700

    add contract PoolinToken

--- PoolinToken.sol
+++ PoolinToken.sol
@@ -1,13 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PoolinToken at 0xb01f85db1d8cd9961cb016354afd7a676f932fe3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PoolinToken at 0x005275450e77bfa6bcbd04d85175d5d0f2dfae43
 */
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
 // 'Poolin Miner Token' token contract
 //
-// Symbol      : POOLIN
+// Symbol      : PIN
 // Name        : Poolin Miner Token
+// Website     : https://www.poolin.com
 // Total supply: 2100000000
 // Decimals    : 18
 //
@@ -273,8 +274,6 @@ contract StandardToken is ERC20, BasicToken {
 contract Ownable {
   address public owner;
 
-  event OwnershipRenounced(address indexed previousOwner);
-
   event OwnershipTransferred(
     address indexed previousOwner,
     address indexed newOwner
@@ -294,17 +293,6 @@ contract Ownable {
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
-  }
-
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   * @notice Renouncing to ownership will leave the contract without an owner.
-   * It will not be possible to call the functions with the `onlyOwner`
-   * modifier anymore.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipRenounced(owner);
-    owner = address(0);
   }
 
   /**
@@ -438,11 +426,11 @@ contract PausableToken is StandardToken, Pausable {
 
 contract PoolinToken is PausableToken {
   string public constant name     = "Poolin Miner Token";
-  string public constant symbol   = "POOLIN";
+  string public constant symbol   = "PIN";
   uint8  public constant decimals = 18;
 
-  // total supply: 21*10^8
-  uint256 public constant K_INITIAL_SUPPLY = uint256(2100000000) * (uint256(10) ** decimals);
+  // total supply: 21 * 10^8 * 10^18
+  uint256 internal constant K_INITIAL_SUPPLY = 2100000000 * 10 ** uint256(decimals);
 
   /**
    * Token Constructor
