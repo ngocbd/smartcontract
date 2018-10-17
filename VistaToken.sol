5ded3abebe632c56d757f6a0243b1a1f25af7485
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:30:13 +0700

    add contract VistaToken

--- VistaToken.sol
+++ VistaToken.sol
@@ -1,227 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VistaToken at 0x18911e747108241e4afe4feeecc742e90c5177bd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VistaToken at 0x90871d626373b1c473e5d52dc45cfd99a3fa12da
 */
-pragma solidity ^0.4.21;
-  
-   // ----------------------------------------------------------------------------
-   // 'FIXED' 'Example Fixed Supply Token' token contract
-   //
-   // Symbol      : vista
-   // Name        : Vista Token
-   // Total supply: 1,000,000,000.000000000000000000
-   // Decimals    : 18
-  //
-  // Enjoy.
-  //
-  // Tony 2018-03-08
-  // ----------------------------------------------------------------------------
-  
-  
-  // ----------------------------------------------------------------------------
-  // Safe maths
-  // ----------------------------------------------------------------------------
-  library SafeMath {
-      function add(uint a, uint b) internal pure returns (uint c) {
-          c = a + b;
-          require(c >= a);
-      }
-      function sub(uint a, uint b) internal pure returns (uint c) {
-          require(b <= a);
-          c = a - b;
-      }
-      function mul(uint a, uint b) internal pure returns (uint c) {
-          c = a * b;
-          require(a == 0 || c / a == b);
-      }
-      function div(uint a, uint b) internal pure returns (uint c) {
-          require(b > 0);
-          c = a / b;
-      }
-  }
-  
-  
-  // ----------------------------------------------------------------------------
-  // ERC Token Standard #20 Interface
-  // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-  // ----------------------------------------------------------------------------
-  contract ERC20Interface {
-      function totalSupply() public constant returns (uint);
-      function balanceOf(address tokenOwner) public constant returns (uint balance);
-      function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-      function transfer(address to, uint tokens) public returns (bool success);
-      function approve(address spender, uint tokens) public returns (bool success);
-      function transferFrom(address from, address to, uint tokens) public returns (bool success);
-  
-      event Transfer(address indexed from, address indexed to, uint tokens);
-      event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-  }
-  
-  
-  // ----------------------------------------------------------------------------
-  // Contract function to receive approval and execute function in one call
-  //
-  // Borrowed from MiniMeToken
-  // ----------------------------------------------------------------------------
-  contract ApproveAndCallFallBack {
-      function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
-  }
-  
-  
-  // ----------------------------------------------------------------------------
-  // Owned contract
-  // ----------------------------------------------------------------------------
-  contract Owned {
-      address public owner;
-      address public newOwner;
-  
-      event OwnershipTransferred(address indexed _from, address indexed _to);
-  
-      function Owned() public {
-          owner = msg.sender;
-      }
-  
-      modifier onlyOwner {
-          require(msg.sender == owner);
-          _;
-      }
-  
-      function transferOwnership(address _newOwner) public onlyOwner {
-          newOwner = _newOwner;
-      }
-      function acceptOwnership() public {
-          require(msg.sender == newOwner);
-          OwnershipTransferred(owner, newOwner);
-          owner = newOwner;
-          newOwner = address(0);
-      }
-  }
-  
-  
-  // ----------------------------------------------------------------------------
-  // ERC20 Token, with the addition of symbol, name and decimals and an
-  // initial fixed supply
- // ----------------------------------------------------------------------------
- contract VistaToken is ERC20Interface, Owned {
-     using SafeMath for uint;
- 
-     string public symbol;
-     string public  name;
-     uint8 public decimals;
-     uint public _totalSupply;
- 
-     mapping(address => uint) balances;
-     mapping(address => mapping(address => uint)) allowed;
- 
- 
-     // ------------------------------------------------------------------------
-     // Constructor
-     // ------------------------------------------------------------------------
-     function VistaToken() public {
-         symbol = "vista";
-         name = "Vista Token";
-         decimals = 18;
-         _totalSupply = 1000000000 * 10**uint(decimals);
-         balances[owner] = _totalSupply;
-         Transfer(address(0), owner, _totalSupply);
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Total supply
-     // ------------------------------------------------------------------------
-     function totalSupply() public constant returns (uint) {
-         return _totalSupply  - balances[address(0)];
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Get the token balance for account `tokenOwner`
-     // ------------------------------------------------------------------------
-     function balanceOf(address tokenOwner) public constant returns (uint balance) {
-         return balances[tokenOwner];
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Transfer the balance from token owner's account to `to` account
-     // - Owner's account must have sufficient balance to transfer
-     // - 0 value transfers are allowed
-     // ------------------------------------------------------------------------
-     function transfer(address to, uint tokens) public returns (bool success) {
-         balances[msg.sender] = balances[msg.sender].sub(tokens);
-         balances[to] = balances[to].add(tokens);
-         Transfer(msg.sender, to, tokens);
-         return true;
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Token owner can approve for `spender` to transferFrom(...) `tokens`
-     // from the token owner's account
-     //
-     // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-     // recommends that there are no checks for the approval double-spend attack
-     // as this should be implemented in user interfaces 
-     // ------------------------------------------------------------------------
-     function approve(address spender, uint tokens) public returns (bool success) {
-         allowed[msg.sender][spender] = tokens;
-         Approval(msg.sender, spender, tokens);
-         return true;
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Transfer `tokens` from the `from` account to the `to` account
-     // 
-     // The calling account must already have sufficient tokens approve(...)-d
-     // for spending from the `from` account and
-     // - From account must have sufficient balance to transfer
-     // - Spender must have sufficient allowance to transfer
-     // - 0 value transfers are allowed
-     // ------------------------------------------------------------------------
-     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-         balances[from] = balances[from].sub(tokens);
-         allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-         balances[to] = balances[to].add(tokens);
-         Transfer(from, to, tokens);
-         return true;
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Returns the amount of tokens approved by the owner that can be
-     // transferred to the spender's account
-     // ------------------------------------------------------------------------
-     function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-         return allowed[tokenOwner][spender];
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Token owner can approve for `spender` to transferFrom(...) `tokens`
-     // from the token owner's account. The `spender` contract function
-     // `receiveApproval(...)` is then executed
-     // ------------------------------------------------------------------------
-     function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-         allowed[msg.sender][spender] = tokens;
-         Approval(msg.sender, spender, tokens);
-         ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-         return true;
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Don't accept ETH
-     // ------------------------------------------------------------------------
-     function () public payable {
-         revert();
-     }
- 
- 
-     // ------------------------------------------------------------------------
-     // Owner can transfer out any accidentally sent ERC20 tokens
-     // ------------------------------------------------------------------------
-     function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-         return ERC20Interface(tokenAddress).transfer(owner, tokens);
-     }
- }
+pragma solidity ^0.4.4;
+
+contract Token {
+
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+contract VistaToken is StandardToken { 
+
+    /* Public variables of the token */
+
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   // Token Name
+    uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
+    string public symbol;                 // An identifier: eg SBX, XPR etc..
+    string public version = 'H1.0'; 
+    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
+    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
+    address public fundsWallet;           // Where should the raised ETH go?
+
+    // This is a constructor function 
+    // which means the following function name has to match the contract name declared above
+    function VistaToken() {
+        balances[msg.sender] = 99110000000000000000000000;               // Give the creator all initial tokens.
+        totalSupply = 99110000000000000000000000;                       // Update total supply 
+        name = "VistaToken";                                           // Set the name for display purposes 
+        decimals = 18;                                                // Amount of decimals for display purposes
+        symbol = "AVGL";                                             // Set the symbol for display purposes 
+        unitsOneEthCanBuy = 5000;                                   // Set the price of your token for the ICO 
+        fundsWallet = msg.sender;                                  // The owner of the contract gets ETH
+    }
+
+    function() payable{
+        totalEthInWei = totalEthInWei + msg.value;
+        uint256 amount = msg.value * unitsOneEthCanBuy;
+        require(balances[fundsWallet] >= amount);
+
+        balances[fundsWallet] = balances[fundsWallet] - amount;
+        balances[msg.sender] = balances[msg.sender] + amount;
+
+        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
+
+        //Transfer ether to fundsWallet
+        fundsWallet.transfer(msg.value);                               
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        return true;
+    }
+}
