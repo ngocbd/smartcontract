537ff277fde1ba43d36fe3921a38d59c0699f216
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:59:37 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x8bdce3e2d608acdefda44b156d6fe16b9449eaee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x6B416561718E00aF145b8bd2873c72B3496bE617
 */
 pragma solidity ^0.4.16;
 
@@ -7,8 +7,8 @@ interface tokenRecipient { function receiveApproval(address _from, uint256 _valu
 
 contract TokenERC20 {
     // Public variables of the token
-    string public name = '??';
-    string public symbol = 'SALAD';
+    string public name;
+    string public symbol;
     uint8 public decimals = 18;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
@@ -28,11 +28,15 @@ contract TokenERC20 {
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    constructor() public {
-        totalSupply = 100000000 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+    constructor(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-       // name = tokenName;                                   // Set the name for display purposes
-       // symbol = tokenSymbol;                               // Set the symbol for display purposes
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
     /**
