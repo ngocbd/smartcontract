3cb3216454bebc3b6b07258d6874a568bd4d71a7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:43:13 +0700

    add contract ICOToken

--- IcoToken.sol
+++ IcoToken.sol
@@ -1,8 +1,193 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IcoToken at 0xa83ee28f8ca3b4bf0cd2a37c429e0afbe21994be
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ICOToken at 0xdfbd2889bdba743d3454911df61d200e79294b01
 */
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.21;
 
+// File: contracts/ERC20Interface.sol
+
+contract ERC20Interface {
+    uint256 public totalSupply;
+
+    function balanceOf(address _owner) public constant returns (uint256);
+    function transfer(address _to, uint256 _value) public returns (bool ok);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool ok);
+    function approve(address _spender, uint256 _value) public returns (bool ok);
+    function allowance(address _owner, address _spender) public constant returns (uint256);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+// File: contracts/SafeMath.sol
+
+/**
+ * Math operations with safety checks
+ */
+library SafeMath {
+    function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b > 0);
+        uint256 c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+
+    function subtract(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a && c >= b);
+        return c;
+    }
+}
+
+// File: contracts/StandardToken.sol
+
+contract StandardToken is ERC20Interface {
+    using SafeMath for uint256;
+
+    /* Actual balances of token holders */
+    mapping(address => uint) balances;
+    /* approve() allowances */
+    mapping (address => mapping (address => uint)) allowed;
+
+    /**
+     *
+     * Fix for the ERC20 short address attack
+     *
+     * http://vessenes.com/the-erc20-short-address-attack-explained/
+     */
+    modifier onlyPayloadSize(uint256 size) {
+        require(msg.data.length == size + 4);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) public returns (bool ok) {
+        require(_to != address(0));
+        require(_value > 0);
+        uint256 holderBalance = balances[msg.sender];
+        require(_value <= holderBalance);
+
+        balances[msg.sender] = holderBalance.subtract(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool ok) {
+        require(_to != address(0));
+        uint256 allowToTrans = allowed[_from][msg.sender];
+        uint256 balanceFrom = balances[_from];
+        require(_value <= balanceFrom);
+        require(_value <= allowToTrans);
+
+        balances[_from] = balanceFrom.subtract(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowToTrans.subtract(_value);
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    /**
+     * @dev Returns balance of the `_owner`.
+     *
+     * @param _owner   The address whose balance will be returned.
+     * @return balance Balance of the `_owner`.
+     */
+    function balanceOf(address _owner) public constant returns (uint256) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool ok) {
+        // To change the approve amount you first have to reduce the addresses`
+        //  allowance to zero by calling `approve(_spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        //    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+        //    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public constant returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    /**
+     * Atomic increment of approved spending
+     *
+     * Works around https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     */
+    function increaseApproval(address _spender, uint256 _addedValue) onlyPayloadSize(2 * 32) public returns (bool ok) {
+        uint256 oldValue = allowed[msg.sender][_spender];
+        allowed[msg.sender][_spender] = oldValue.add(_addedValue);
+
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+
+        return true;
+    }
+
+    /**
+     * Atomic decrement of approved spending.
+     *
+     * Works around https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     */
+    function decreaseApproval(address _spender, uint256 _subtractedValue) onlyPayloadSize(2 * 32) public returns (bool ok) {
+        uint256 oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.subtract(_subtractedValue);
+        }
+
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+
+        return true;
+    }
+
+}
+
+// File: contracts/BurnableToken.sol
+
+contract BurnableToken is StandardToken {
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        _burn(msg.sender, _value);
+    }
+
+    function _burn(address _holder, uint256 _value) internal {
+        require(_value <= balances[_holder]);
+
+        balances[_holder] = balances[_holder].subtract(_value);
+        totalSupply = totalSupply.subtract(_value);
+
+        emit Burn(_holder, _value);
+        emit Transfer(_holder, address(0), _value);
+    }
+
+    event Burn(address indexed _burner, uint256 _value);
+}
+
+// File: contracts/Ownable.sol
 
 /**
  * @title Ownable
@@ -10,262 +195,148 @@ pragma solidity ^0.4.23;
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-  address public owner;
+    address public owner;
+    address public newOwner;
 
-
-  event OwnershipRenounced(address indexed previousOwner);
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipRenounced(owner);
-    owner = address(0);
-  }
-}
-// ================= ERC20 Token Contract start =========================
-/*
- * ERC20 interface
- * see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 {
-  uint public totalSupply;
-  function balanceOf(address who) public constant returns (uint);
-  function allowance(address owner, address spender) public constant returns (uint);
-
-  function transfer(address to, uint value) public returns (bool status);
-  function transferFrom(address from, address to, uint value) public returns (bool status);
-  function approve(address spender, uint value) public returns (bool status);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-
-
-
-
-
-
-contract StandardToken is ERC20{
-  
-   /*Define SafeMath library here for uint256*/
-   
-   using SafeMath for uint256; 
-       
-  /**
-  * @dev Fix for the ERC20 short address attack.
-   */
-  modifier onlyPayloadSize(uint size) {
-    require(msg.data.length >= size + 4) ;
-    _;
-  }
-
-  mapping(address => uint) accountBalances;
-  mapping (address => mapping (address => uint)) allowed;
-
-  function transfer(address _to, uint _value) public onlyPayloadSize(2 * 32)  returns (bool success){
-    accountBalances[msg.sender] = accountBalances[msg.sender].sub(_value);
-    accountBalances[_to] = accountBalances[_to].add(_value);
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  function transferFrom(address _from, address _to, uint _value) public onlyPayloadSize(3 * 32) returns (bool success) {
-    uint _allowance = allowed[_from][msg.sender];
-
-    accountBalances[_to] = accountBalances[_to].add(_value);
-    accountBalances[_from] = accountBalances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function balanceOf(address _owner) public constant returns (uint balance) {
-    return accountBalances[_owner];
-  }
-
-  function approve(address _spender, uint _value) public returns (bool success) {
-    allowed[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) public constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-}
-
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        owner = msg.sender;
     }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param _newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+
+        owner = newOwner;
+        newOwner = address(0);
+
+        emit OwnershipTransferred(owner, newOwner);
+    }
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 }
 
+// File: contracts/ERC223Interface.sol
 
+contract ERC223Interface is ERC20Interface {
+    function transfer(address _to, uint256 _value, bytes _data) public returns (bool ok);
 
-
-
-
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
-
-  bool public paused = false;
-
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is not paused.
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
-  }
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is paused.
-   */
-  modifier whenPaused() {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused public {
-    paused = true;
-    emit Pause();
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused public {
-    paused = false;
-    emit Unpause();
-  }
+    event Transfer(address indexed _from, address indexed _to, uint256 _value, bytes indexed _data);
 }
 
-contract IcoToken is StandardToken, Pausable{
-    /*define SafeMath library for uint256*/
-    using SafeMath for uint256;
-    
+// File: contracts/ERC223ReceivingContract.sol
+
+contract ERC223ReceivingContract {
+    /**
+     * @dev Standard ERC223 function that will handle incoming token transfers.
+     *
+     * @param _from  Token sender address.
+     * @param _value Amount of tokens.
+     * @param _data  Transaction metadata.
+     */
+    function tokenFallback(address _from, uint256 _value, bytes _data) public;
+}
+
+// File: contracts/Standard223Token.sol
+
+contract Standard223Token is ERC223Interface, StandardToken {
+    function transfer(address _to, uint256 _value, bytes _data) public returns (bool ok) {
+        if (!super.transfer(_to, _value)) {
+            revert();
+        }
+        if (isContract(_to)) {
+            contractFallback(msg.sender, _to, _value, _data);
+        }
+
+        emit Transfer(msg.sender, _to, _value, _data);
+
+        return true;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool ok) {
+        return transfer(_to, _value, new bytes(0));
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value, bytes _data) public returns (bool ok) {
+        if (!super.transferFrom(_from, _to, _value)) {
+            revert();
+        }
+        if (isContract(_to)) {
+            contractFallback(_from, _to, _value, _data);
+        }
+
+        emit Transfer(_from, _to, _value, _data);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool ok) {
+        return transferFrom(_from, _to, _value, new bytes(0));
+    }
+
+    function contractFallback(address _origin, address _to, uint256 _value, bytes _data) private {
+        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+        receiver.tokenFallback(_origin, _value, _data);
+    }
+
+    function isContract(address _addr) private view returns (bool is_contract) {
+        uint256 length;
+        assembly {
+            length := extcodesize(_addr)
+        }
+
+        return (length > 0);
+    }
+}
+
+// File: contracts/ICOToken.sol
+
+// ----------------------------------------------------------------------------
+// ICO Token contract
+// ----------------------------------------------------------------------------
+contract ICOToken is BurnableToken, Ownable, Standard223Token {
     string public name;
     string public symbol;
-    string public version;
-    uint public decimals;
-    address public icoSaleDeposit;
-    address public icoContract;
-    
-    constructor(string _name, string _symbol, uint256 _decimals, string _version) public {
+    uint8 public decimals;
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    constructor(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) public {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        version = _version;
+        totalSupply = _totalSupply;
+
+        balances[owner] = totalSupply;
+
+        emit Mint(owner, totalSupply);
+        emit Transfer(address(0), owner, totalSupply);
+        emit MintFinished();
     }
-    
-    function transfer(address _to, uint _value) public whenNotPaused returns (bool success) {
-        return super.transfer(_to,_value);
+
+    function () public payable {
+        revert();
     }
-    
-    function approve(address _spender, uint _value) public whenNotPaused returns (bool success) {
-        return super.approve(_spender,_value);
-    }
-    
-    function balanceOf(address _owner) public view returns (uint balance){
-        return super.balanceOf(_owner);
-    }
-    
-    function setIcoContract(address _icoContract) public onlyOwner {
-        if(_icoContract != address(0)){
-            icoContract = _icoContract;           
-        }
-    }
-    
-    function sell(address _recipient, uint256 _value) public whenNotPaused returns (bool success){
-        assert(_value > 0);
-        require(msg.sender == icoContract);
-        
-        accountBalances[_recipient] = accountBalances[_recipient].add(_value);
-        totalSupply = totalSupply.add(_value);
-        
-        emit Transfer(0x0,owner,_value);
-        emit Transfer(owner,_recipient,_value);
-        return true;
-    }
-    
+
+    event Mint(address indexed _to, uint256 _amount);
+    event MintFinished();
 }
