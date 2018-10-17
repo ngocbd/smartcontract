be474bd648c38aabe97c0666a8d7afd330f85d99
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:40:13 +0700

    add contract MultiTransfer

--- Multitransfer.sol
+++ Multitransfer.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0x3c3a02efe0a39cec46b9cb6dd4df63cd7e20e3ba
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0x2153026c7f9059d4eb80060ac21cc1e0a544157d
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.16;
 
 
 
@@ -22,6 +22,9 @@ contract ERC20Basic {
 
 
 
+
+
+
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -36,19 +39,72 @@ contract ERC20 is ERC20Basic {
 
 
 
-contract MultiTransfer {
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+
+
+
+
+
+contract MultiTransfer is Ownable {
+    ERC20 public tkcAddress;
 
     function MultiTransfer() public {
 
     }
 
-    function transfer(address token, address owner,address[] to, uint[] value) public {
-        require(to.length == value.length);
-        require(token != address(0));
+    function setTKC(address tkc) public onlyOwner {
+        require(tkcAddress == address(0));
+        tkcAddress = ERC20(tkc);
+    }
 
-        ERC20 t = ERC20(token);
+    function transfer(address[] to, uint[] value) public onlyOwner {
+        require(to.length == value.length);
+
         for (uint i = 0; i < to.length; i++) {
-            t.transferFrom(owner, to[i], value[i]);
+            tkcAddress.transferFrom(owner, to[i], value[i]);
         }
     }
 }
