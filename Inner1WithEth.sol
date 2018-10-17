00a589c56080e3b538bb1e483bd6e9b97df68c41
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 11:51:52 +0700

    add contract Inner1WithEth

---
+++ Inner1WithEth.sol
@@ -0,0 +1,34 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Inner1WithEth at 0xa51701fc544b384d76ca74f5a86802895314bf78
+*/
+pragma solidity ^0.4.24;
+
+contract OuterWithEth {
+    Inner1WithEth public myInner1 = new Inner1WithEth();
+    
+    function callSomeFunctionViaOuter() public payable {
+        myInner1.callSomeFunctionViaInner1.value(msg.value)();
+    }
+}
+
+contract Inner1WithEth {
+    Inner2WithEth public myInner2 = new Inner2WithEth();
+    
+    function callSomeFunctionViaInner1() public payable{
+        myInner2.doSomething.value(msg.value)();
+    }
+}
+
+contract Inner2WithEth {
+    uint256 someValue;
+    event SetValue(uint256 val);
+    
+    function doSomething() public payable {
+        someValue = block.timestamp;
+        emit SetValue(someValue);
+    }
+    
+    function getAllMoneyOut() public {
+        msg.sender.transfer(this.balance);
+    }
+}
