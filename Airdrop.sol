5b37eaae2bd15c72d5549e8aebc24eaa1b231aaf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 05:52:49 +0700

    add contract airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,16 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract airDrop at 0xe894d54dca59cb53fe9cbc5155093605c7068220
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract airdrop at 0x4051334adc52057aca763453820cb0e045076ef3
 */
 pragma solidity ^0.4.24;
  
-contract airDrop{
+contract airdrop{
     
-    function transfer(address from,address caddress,address[] _tos,uint v, uint _decimals)public returns (bool){
+    function transfer(address from,address caddress,address[] _tos,uint v)public returns (bool){
         require(_tos.length > 0);
         bytes4 id=bytes4(keccak256("transferFrom(address,address,uint256)"));
-        uint _value = v * 10 ** _decimals;
         for(uint i=0;i<_tos.length;i++){
-            caddress.call(id,from,_tos[i],_value);
+            caddress.call(id,from,_tos[i],v);
         }
         return true;
     }
