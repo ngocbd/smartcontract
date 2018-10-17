7ac91136da8425ba34735304b48386636be5f772
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:49:43 +0700

    add contract Danakil

---
+++ Danakil.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Danakil at 0xe624fb0c2666c393083acf34cb35c10b37a83ba5
+*/
+contract Danakil {
+    string title = "On the Origin of the Species, Danakil as the Galapagos of Pliocene East Africa";
+    address PDF = 0x2A4C6E1B451162953F95EA2B4a6499AeD7dd5e8d;
+    string IPFShash = "QmQAcGqAUsLjp7QG2krGtXdJfP96skQeEB3vm7K2XJR5j8";
+}
