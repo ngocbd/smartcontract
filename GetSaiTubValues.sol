2c3818774dd0432ecd7b791fc7b525347b103a7d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:07:47 +0700

    add contract GetSaiTubValues

---
+++ GetSaiTubValues.sol
@@ -0,0 +1,35 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GetSaiTubValues at 0x2f5C4a85d7E133C4FeFa5a0C0ba1447A1AF86996
+*/
+pragma solidity ^0.4.24;
+
+contract SaiTub {
+    function tab(bytes32 cup) public returns (uint);
+    function rap(bytes32 cup) public returns (uint);
+    function din() public returns (uint);
+    function chi() public returns (uint);
+    function rhi() public returns (uint);
+}
+
+contract GetSaiTubValues {
+    SaiTub public saiTub = SaiTub(0x448a5065aeBB8E423F0896E6c5D525C040f59af3);
+
+    bytes32 public cup;
+    uint public tab;
+    uint public rap;
+    uint public din;
+    uint public chi;
+    uint public rhi;
+
+    function updateTabRap(bytes32 _cup) public {
+        cup = _cup;
+        tab = saiTub.tab(_cup);
+        rap = saiTub.rap(_cup);
+    }
+
+    function updateRest() public {
+        din = saiTub.din();
+        chi = saiTub.chi();
+        rhi = saiTub.rhi();
+    }
+}
