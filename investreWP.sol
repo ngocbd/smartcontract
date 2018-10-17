5c1feefcba2925f39bcebbbe272a2862c91d9e38
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 12:44:32 +0700

    add contract investreWP

---
+++ investreWP.sol
@@ -0,0 +1,13 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract investreWP at 0x8e817235d838339c5a0ef6dd44b338dd5da8582e
+*/
+pragma solidity ^0.4.17;
+
+contract investreWP {
+    string public url_white_paper;
+
+    constructor(string initialMessage) public {
+        url_white_paper = initialMessage;
+    }
+    
+}
