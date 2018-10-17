3351e858eac5e37b96e19a1c141a8b85ab38707f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:25:31 +0700

    add contract WhaleToken

--- WhaleToken.sol
+++ WhaleToken.sol
@@ -1,201 +1,256 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WhaleToken at 0x8f1c0056fcc966c2bf35c944bc0e3cb2b698b368
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract WhaleToken at 0xa8a14e8f85af67c580df121b1dc348dbb9794332
 */
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.24;
 
 library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        assert(c >= a);
     }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-
-  mapping(address => uint256) balances;
-
-  uint256 totalSupply_;
-
-  /**
-  * @dev total number of tokens in existence
-  */
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public view returns (uint256) {
-    return balances[_owner];
-  }
-
-}
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-contract StandardToken is ERC20, BasicToken {
-
-  mapping (address => mapping (address => uint256)) internal allowed;
-
-
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amount of tokens to be transferred
-   */
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   *
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
-  }
-
-  /**
-   * @dev Increase the amount of tokens that an owner allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To increment
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _addedValue The amount of tokens to increase the allowance by.
-   */
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-  /**
-   * @dev Decrease the amount of tokens that an owner allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To decrement
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _subtractedValue The amount of tokens to decrease the allowance by.
-   */
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    function sub(uint a, uint b) internal pure returns (uint c) {
+        assert(b <= a);
+        c = a - b;
     }
-    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        assert(a == 0 || c / a == b);
+    }
+    function div(uint a, uint b) internal pure returns (uint c) {
+        assert(b > 0);
+        c = a / b;
+        assert(a == b * c + a % b);
+    }
 }
 
-contract WhaleToken is StandardToken {
-    string public name = 'WHALE';
-    string public symbol = 'WHALE';
-    uint8 public decimals = 9;
-    uint public INITIAL_SUPPLY = 1000000000000000000;
+interface tokenRecipient { 
+    function receiveApproval(address _from, uint _value, address _token, bytes _extraData) external; 
+}
 
-    constructor() {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
+contract WhaleConfig {
+    using SafeMath for uint;
+
+    string internal constant TOKEN_NAME     = "Whale Chain";
+    string internal constant TOKEN_SYMBOL   = "WAT";
+    uint8  internal constant TOKEN_DECIMALS = 18;
+    uint   internal constant INITIAL_SUPPLY = 20*1e8 * 10 ** uint(TOKEN_DECIMALS);
+}
+
+contract Ownable is WhaleConfig {
+    address public ceo;
+    
+    event LogChangeCEO(address indexed oldOwner, address indexed newOwner);
+    
+    modifier onlyOwner {
+        require(msg.sender == ceo);
+        _;
+    }
+    
+    constructor() public {
+        ceo = msg.sender;
+    }
+    
+    function changeCEO(address _owner) onlyOwner public returns (bool success) {
+        require(_owner != address(0));
+        emit LogChangeCEO(ceo, _owner);
+        ceo = _owner;
+        return true;
     }
 
+    function isOwner(address _owner) internal view returns (bool) {
+        return ceo == _owner;
+    }
+}
+
+contract Pausable is Ownable {
+    bool public paused = false;
+    
+    event LogPause();
+    event LogUnpause();
+    
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+    
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+    
+    modifier whenConditionalPassing() {
+        if(!isOwner(msg.sender)) {
+            require(!paused);
+        }
+        _;
+    }
+    
+    function pause() onlyOwner whenNotPaused public returns (bool success) {
+        paused = true;
+        emit LogPause();
+        return true;
+    }
+  
+    function unpause() onlyOwner whenPaused public returns (bool success) {
+        paused = false;
+        emit LogUnpause();
+        return true;
+    }
+}
+
+contract Lockable is Pausable {
+    mapping (address => bool) public locked;
+    
+    event LogLockup(address indexed target);
+    
+    function lockup(address _target) onlyOwner public returns (bool success) {
+	    require( !isOwner(_target) );
+
+        locked[_target] = true;
+        emit LogLockup(_target);
+        return true;
+    }
+    
+    function isLockup(address _target) internal view returns (bool) {
+        if(true == locked[_target])
+            return true;
+    }
+}
+
+contract TokenERC20 {
+    using SafeMath for uint;
+    
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    uint public totalSupply;
+    mapping (address => uint) public balanceOf;
+    mapping (address => mapping (address => uint)) public allowance;
+
+    event ERC20Token(address indexed owner, string name, string symbol, uint8 decimals, uint supply);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event TransferFrom(address indexed from, address indexed to, address indexed spender, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor(
+        string _tokenName,
+        string _tokenSymbol,
+        uint8 _tokenDecimals,
+        uint _initialSupply
+    ) public {
+        name = _tokenName;
+        symbol = _tokenSymbol;
+        decimals = _tokenDecimals;
+        
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = totalSupply;
+        
+        emit ERC20Token(msg.sender, name, symbol, decimals, totalSupply);
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[_from] >= _value);
+        require(SafeMath.add(balanceOf[_to], _value) > balanceOf[_to]);
+        uint previousBalances = SafeMath.add(balanceOf[_from], balanceOf[_to]);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        assert(SafeMath.add(balanceOf[_from], balanceOf[_to]) == previousBalances);
+        return true;
+    }
+    
+    function transfer(address _to, uint _value) public returns (bool success) {
+        return _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        emit TransferFrom(_from, _to, msg.sender, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function approveAndCall(address _spender, uint _value, bytes _extraData) public returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+}
+
+contract WhaleToken is Lockable, TokenERC20 {
+    string public version = "v1.0";
+    
+    mapping (address => bool) public frozenAccount;
+
+    event LogFrozenAccount(address indexed target, bool frozen);
+    event LogBurn(address indexed owner, uint value);
+    event LogMining(address indexed recipient, uint value);
+    event LogWithdrawContractToken(address indexed owner, uint value);
+    
+    constructor() TokenERC20(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, INITIAL_SUPPLY) public {}
+
+    function _transfer(address _from, address _to, uint _value) whenConditionalPassing internal returns (bool success) {
+        require(!frozenAccount[_from]); 
+        require(!frozenAccount[_to]);
+        require(!isLockup(_from));
+        require(!isLockup(_to));
+
+        return super._transfer(_from, _to, _value);
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(!frozenAccount[msg.sender]);
+        require(!isLockup(msg.sender));
+        return super.transferFrom(_from, _to, _value);
+    }
+    
+    function freezeAccount(address _target) onlyOwner public returns (bool success) {
+        require(!isOwner(_target));
+        require(!frozenAccount[_target]);
+        frozenAccount[_target] = true;
+        emit LogFrozenAccount(_target, true);
+        return true;
+    }
+    
+    function unfreezeAccount(address _target) onlyOwner public returns (bool success) {
+        require(frozenAccount[_target]);
+        frozenAccount[_target] = false;
+        emit LogFrozenAccount(_target, false);
+        return true;
+    }
+    
+    function burn(uint _value) onlyOwner public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);            
+        totalSupply = totalSupply.sub(_value);                      
+        emit LogBurn(msg.sender, _value);
+        return true;
+    }
+    
+    function withdrawContractToken(uint _value) onlyOwner public returns (bool success) {
+        _transfer(this, msg.sender, _value);
+        emit LogWithdrawContractToken(msg.sender, _value);
+        return true;
+    }
+    
+    function getContractBalanceOf() public constant returns(uint blance) {
+        blance = balanceOf[this];
+    }
+    
+    function getBalance(address _owner) onlyOwner public constant returns(uint blance) {
+        blance = balanceOf[_owner];
+    }
+    
+    function () payable public { revert(); }
 }
