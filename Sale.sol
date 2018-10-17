f93b62f8c209dbf22cc59cd08247654325ed61c4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:57:44 +0700

    add contract Sale

--- Sale.sol
+++ Sale.sol
@@ -1,106 +1,120 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x41238f745046a3e6486003c3e43fe301542ff60f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x957b51309e3399f21842c02a023dfc829b334c41
 */
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.18;
 
+
+// Ownable
+contract Ownable {
+  address public owner;
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;}
+
+  modifier onlyOwner() {
+    require(msg.sender == owner); _; } }
+
+// Sale
+contract Sale is Ownable {
+  using SafeMath for uint256;
+
+  ERC20 public token;
+  address public wallet;
+  uint256 public rate;
+  uint256 public weiRaised;
+
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  function Sale (uint256 _rate, address _wallet, ERC20 _token) public {
+    require(_rate > 0);
+    require(_wallet != address(0));
+    require(_token != address(0));
+
+    rate = _rate;
+    wallet = _wallet;
+    token = _token; }
+
+  function () external payable {
+    buyTokens(msg.sender);}
+
+
+  function buyTokens(address _beneficiary) public payable {
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+    uint256 tokens = _getTokenAmount(weiAmount);
+    weiRaised = weiRaised.add(weiAmount);
+    _processPurchase(_beneficiary, tokens);
+    TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+    _updatePurchasingState(_beneficiary, weiAmount);
+    _forwardFunds();
+    _postValidatePurchase(_beneficiary, weiAmount); }
+
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0); }
+
+  function _postValidatePurchase(address _beneficiary, uint256 _weiAmount) internal { }
+
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    token.transfer(_beneficiary, _tokenAmount); }
+
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount); }
+
+  function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal { }
+
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount.mul(rate); }
+
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value); }
+   
+// Used to end the Presale
+  function TokenDestructible() public payable { }
+  function destroy(address[] tokens) onlyOwner public {
+
+// Transfer tokens to owner
+    for (uint256 i = 0; i < tokens.length; i++) {
+      ERC20Basic token = ERC20Basic(tokens[i]);
+      uint256 balance = token.balanceOf(this);
+      token.transfer(owner, balance);} 
+    selfdestruct(owner); }}
+    
+  
+  
+// SafeMath    
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
-      return 0;
-    }
-
-    c = a * b;
+      return 0; }
+    uint256 c = a * b;
     assert(c / a == b);
-    return c;
-  }
-
+    return c; }
+    
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    return a / b;
-  }
-
+    uint256 c = a / b;
+    return c; }
+    
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
+    return a - b; }
+    
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
     assert(c >= a);
-    return c;
-  }
-}
-
-interface Token {
-    function transfer(address _to, uint256 _value) external;
-}
-
-contract Sale{
-
-    using SafeMath for uint256;
+    return c;}}
     
-    Token public tokenReward;
-    address public creator;
-    address public owner = 0x99C4EF0a180C19e929452e94972AA60cAc1b0B7D;
+// ERC20Basic    
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);}
 
-    uint256 public price;
-    uint256 public startDate;
-    uint256 public endDate;
-
-    modifier isCreator() {
-        require(msg.sender == creator);
-        _;
-    }
-
-    event FundTransfer(address backer, uint amount, bool isContribution);
-
-    constructor() public {
-        creator = msg.sender;
-        startDate = 1532943398;
-        endDate = 1538351999;
-        price = 10000;
-        tokenReward = Token(0xecea1d051a5c3339983ecc2dbdc3f38a7f52c636);
-    }
-
-    function setOwner(address _owner) isCreator public {
-        owner = _owner;      
-    }
-
-    function setCreator(address _creator) isCreator public {
-        creator = _creator;      
-    }
-
-    function setStartDate(uint256 _startDate) isCreator public {
-        startDate = _startDate;      
-    }
-
-    function setEndtDate(uint256 _endDate) isCreator public {
-        endDate = _endDate;      
-    }
-    
-    function setPrice(uint256 _price) isCreator public {
-        price = _price;      
-    }
-
-    function setToken(address _token) isCreator public {
-        tokenReward = Token(_token);      
-    }
-
-    function sendToken(address _to, uint256 _value) isCreator public {
-        tokenReward.transfer(_to, _value);      
-    }
-
-    function kill() isCreator public {
-        selfdestruct(owner);
-    }
-
-    function () public payable {
-        require(msg.value > 70000000000000000);
-        require(now > startDate);
-        require(now < endDate);
-        uint256 amount = msg.value.mul(price); 
-       
-        tokenReward.transfer(msg.sender, amount);
-        emit FundTransfer(msg.sender, amount, true);
-        owner.transfer(msg.value);
-    }
-}
+// ERC20
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);}
