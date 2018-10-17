1e46c099e87a060ebc860a7be8af86d5533dbfab
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:06:08 +0700

    add contract PlayerBook

--- PlayerBook.sol
+++ PlayerBook.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PlayerBook at 0xee83e20C6AEab2284685Efe0B5ffb250bE5480bf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PlayerBook at 0x77abd49884c36193e7d1fccc6898fcdbd9d23ecc
 */
 pragma solidity ^0.4.24;
 /*
@@ -29,18 +29,36 @@ pragma solidity ^0.4.24;
  * ????????? ? ???? ???? ?   ???????????? ????????????    
  */
 
+interface ZethrBankroll {
+    function deposit() external payable;
+}
 
 interface PlayerBookReceiverInterface {
     function receivePlayerInfo(uint256 _pID, address _addr, bytes32 _name, uint256 _laff) external;
     function receivePlayerNameList(uint256 _pID, bytes32 _name) external;
 }
 
+interface TeamJustInterface {
+    function requiredSignatures() external view returns(uint256);
+    function requiredDevSignatures() external view returns(uint256);
+    function adminCount() external view returns(uint256);
+    function devCount() external view returns(uint256);
+    function adminName(address _who) external view returns(bytes32);
+    function isAdmin(address _who) external view returns(bool);
+    function isDev(address _who) external view returns(bool);
+}
 
 contract PlayerBook {
     using NameFilter for string;
     using SafeMath for uint256;
+    // CHANGEME
+    // this is now the Zethr Bankroll
+    address constant private Jekyll_Island_Inc = address(0xb51d0DF324c513Cf07efD075Cc5bccA1D0F211Ab);
+    //TeamJustInterface constant private TeamJust = TeamJustInterface(0x464904238b5CdBdCE12722A7E6014EC1C0B66928);
     
-    address private admin = msg.sender;
+
+
+
 //==============================================================================
 //     _| _ _|_ _    _ _ _|_    _   .
 //    (_|(_| | (_|  _\(/_ | |_||_)  .
@@ -56,6 +74,9 @@ contract PlayerBook {
     mapping (uint256 => Player) public plyr_;               // (pID => data) player data
     mapping (uint256 => mapping (bytes32 => bool)) public plyrNames_; // (pID => name => bool) list of names a player owns.  (used so you can change your display name amoungst any name you own)
     mapping (uint256 => mapping (uint256 => bytes32)) public plyrNameList_; // (pID => nameNum => name) list of names a player owns
+    
+    mapping(address => bool) devs;
+
     struct Player {
         address addr;
         bytes32 name;
@@ -69,42 +90,7 @@ contract PlayerBook {
     constructor()
         public
     {
-        // premine the dev names (sorry not sorry)
-            // No keys are purchased with this method, it's simply locking our addresses,
-            // PID's and names for referral codes.
-        plyr_[1].addr = 0x8e0d985f3Ec1857BEc39B76aAabDEa6B31B67d53;
-        plyr_[1].name = "justo";
-        plyr_[1].names = 1;
-        pIDxAddr_[0x8e0d985f3Ec1857BEc39B76aAabDEa6B31B67d53] = 1;
-        pIDxName_["justo"] = 1;
-        plyrNames_[1]["justo"] = true;
-        plyrNameList_[1][1] = "justo";
-        
-        plyr_[2].addr = 0x8b4DA1827932D71759687f925D17F81Fc94e3A9D;
-        plyr_[2].name = "mantso";
-        plyr_[2].names = 1;
-        pIDxAddr_[0x8b4DA1827932D71759687f925D17F81Fc94e3A9D] = 2;
-        pIDxName_["mantso"] = 2;
-        plyrNames_[2]["mantso"] = true;
-        plyrNameList_[2][1] = "mantso";
-        
-        plyr_[3].addr = 0x7ac74Fcc1a71b106F12c55ee8F802C9F672Ce40C;
-        plyr_[3].name = "sumpunk";
-        plyr_[3].names = 1;
-        pIDxAddr_[0x7ac74Fcc1a71b106F12c55ee8F802C9F672Ce40C] = 3;
-        pIDxName_["sumpunk"] = 3;
-        plyrNames_[3]["sumpunk"] = true;
-        plyrNameList_[3][1] = "sumpunk";
-        
-        plyr_[4].addr = 0x18E90Fc6F70344f53EBd4f6070bf6Aa23e2D748C;
-        plyr_[4].name = "inventor";
-        plyr_[4].names = 1;
-        pIDxAddr_[0x18E90Fc6F70344f53EBd4f6070bf6Aa23e2D748C] = 4;
-        pIDxName_["inventor"] = 4;
-        plyrNames_[4]["inventor"] = true;
-        plyrNameList_[4][1] = "inventor";
-        
-        pID_ = 4;
+        devs[msg.sender] = true;
     }
 //==============================================================================
 //     _ _  _  _|. |`. _  _ _  .
@@ -114,14 +100,15 @@ contract PlayerBook {
      * @dev prevents contracts from interacting with fomo3d 
      */
     modifier isHuman() {
-        address _addr = msg.sender;
-        uint256 _codeLength;
-        
-        assembly {_codeLength := extcodesize(_addr)}
-        require(_codeLength == 0, "sorry humans only");
+        require(msg.sender==tx.origin);
         _;
     }
-   
+    
+    modifier onlyDevs() 
+    {
+        require(devs[msg.sender]); //require(TeamJust.isDev(msg.sender) == true, "msg sender is not a dev");
+        _;
+    }
     
     modifier isRegisteredGame()
     {
@@ -396,7 +383,7 @@ contract PlayerBook {
         }
         
         // registration fee goes directly to community rewards
-        admin.transfer(address(this).balance);
+        Jekyll_Island_Inc.transfer(address(this).balance);
         
         // push player info to games
         if (_all == true)
@@ -576,9 +563,12 @@ contract PlayerBook {
 //  _\(/_ | |_||_)  .
 //=============|================================================================
     function addGame(address _gameAddress, string _gameNameStr)
+        onlyDevs()
         public
     {
         require(gameIDs_[_gameAddress] == 0, "derp, that games already been registered");
+        
+
             gID_++;
             bytes32 _name = _gameNameStr.nameFilter();
             gameIDs_[_gameAddress] = gID_;
@@ -589,12 +579,16 @@ contract PlayerBook {
             games_[gID_].receivePlayerInfo(2, plyr_[2].addr, plyr_[2].name, 0);
             games_[gID_].receivePlayerInfo(3, plyr_[3].addr, plyr_[3].name, 0);
             games_[gID_].receivePlayerInfo(4, plyr_[4].addr, plyr_[4].name, 0);
+        
     }
     
     function setRegistrationFee(uint256 _fee)
+        onlyDevs()
         public
     {
-      registrationFee_ = _fee;
+
+            registrationFee_ = _fee;
+        
     }
         
 } 
