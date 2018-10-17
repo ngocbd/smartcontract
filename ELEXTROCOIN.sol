4dfcf7987e11141ca4d67e3da8e5db125893685a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:03:19 +0700

    add contract ElextroCoin

--- ELEXTROCOIN.sol
+++ ELEXTROCOIN.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ELEXTROCOIN at 0x0171b8766acb725d84ff8f46e7a9f19853fd1c8b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ElextroCoin at 0x0494a29641ed7e5fad775f9bbc1f934944642552
 */
 pragma solidity ^0.4.4;
 
@@ -13,7 +13,7 @@ contract Token {
     function balanceOf(address _owner) constant returns (uint256 balance) {}
 
     /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The ADDRESS of the recipient
+    /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) returns (bool success) {}
@@ -31,19 +31,19 @@ contract Token {
     /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) returns (bool success) {}
 
-    /// @param _owner The ADDRESS of the account owning tokens
+    /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
-    event Transfer (address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 }
 
 contract StandardToken is Token {
 
-    function transfer (address _to, uint256 _value) returns (bool success) {
+    function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
@@ -87,7 +87,7 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract ELEXTROCOIN is StandardToken { // CHANGE THIS. UPDATE the contract name.
+contract ElextroCoin is StandardToken { // CHANGE THIS. Update the contract name.
 
     /* Public variables of the token */
 
@@ -101,19 +101,19 @@ contract ELEXTROCOIN is StandardToken { // CHANGE THIS. UPDATE the contract name
     uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
     string public symbol;                 // An identifier: eg SBX, XPR etc..
     string public version = 'H1.0'; 
-    uint256 public unitsOneEthCanBuy;     // How many UNITS of your coin can be bought by 1 ETH?
+    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
     uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
     address public fundsWallet;           // Where should the raised ETH go?
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function ELEXTROCOIN() {
-        balances[msg.sender] = 1000000000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000000000000000000000000000000;                        // UPDATE total supply (1000 for example) (CHANGE THIS)
-        name = "ELEXTROCOIN";                                   // Set the name for display purposes (CHANGE THIS)
+    function ElextroCoin() {
+        balances[msg.sender] = 1000000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 1000000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "ElextroCoin";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "ELX";                                             // Set the symbol for display purposes (CHANGE THIS)
-        unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
+        symbol = "ELEX";                                             // Set the symbol for display purposes (CHANGE THIS)
+        unitsOneEthCanBuy = 20000;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
 
@@ -139,7 +139,7 @@ contract ELEXTROCOIN is StandardToken { // CHANGE THIS. UPDATE the contract name
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
         //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval ADDRESS,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
     }
 }
