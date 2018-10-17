fe080508b3eb580129acf3406470d26419bcea4c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:56:33 +0700

    add contract SimpleStorage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,16 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0xf423af010426bbdffc1fdd32a8e302c7b8623fb2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleStorage at 0x9e5be319ffabb99cdf1db65e14846ae34d249043
 */
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.23;
 
 contract SimpleStorage {
-    uint storedData;
-
-    function set(uint x) public {
-        storedData = x;
-    }
-
-    function get() public constant returns (uint) {
-        return storedData;
+    string public welcomeMsg = "Hello block chain from InfPro IT Solutions!";
+    string[] public myStorage;
+    
+    function add(string _store) public {
+        myStorage.push(_store);
     }
 }
