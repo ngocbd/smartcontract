cd8d2728400cc33d4e5062cb2561ebea48afe6cb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:44:16 +0700

    add contract MintedTokenCappedCrowdsaleExt

--- MintedTokenCappedCrowdsaleExt.sol
+++ MintedTokenCappedCrowdsaleExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintedTokenCappedCrowdsaleExt at 0xe84c1E13Fa667F5495Fd5d6df37E8d3F4D2f6580
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MintedTokenCappedCrowdsaleExt at 0x1a69A1A80ddF51b555f4B19d2879e2994DCa3754
 */
 // Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 // Temporarily have SafeMath here until all contracts have been migrated to SafeMathLib version from OpenZeppelin
