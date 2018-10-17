6e64e76462cd2aff26b272886af5733c364b406f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:49:55 +0700

    add contract TokenReclaim

--- TokenReclaim.sol
+++ TokenReclaim.sol
@@ -1,51 +1,29 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenReclaim at 0x3c00a606544daf12be1747eadcc78654b1768e44
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenReclaim at 0x9ec2b41007a09a2bf8f06fe0faf2b80ba5480479
 */
 pragma solidity ^0.4.23;
 
+contract TokenRequestStub{
+    function balanceOf(address _owner) public view returns (uint256 balance);
+}
+
 contract TokenReclaim{
-    mapping (address=>string) internal _ethToSphtx;
-    mapping (string =>string) internal _accountToPubKey;
-    event AccountRegister (address ethAccount, string sphtxAccount, string pubKey);
+    TokenRequestStub tokenAddress;
+    mapping (address=>string) internal _ethToPubKey;
+    event AccountRegister (address ethAccount, string pubKey, uint holding);
 
-    function register(string name, string pubKey) public{
-        require(bytes(name).length >= 3 && bytes(name).length <= 16);
-        bytes memory b = bytes(name);
-        require( (b[0] >='a' && b[0] <='z') || (b[0] >='0' && b[0] <= '9'));
-        for(uint i=1;i< bytes(name).length; i++){
-            require( (b[i] >='a' && b[i] <='z') || (b[i] >='0' && b[i] <= '9') || b[i] == '-' || b[i] =='.'  );
-        }
-        require(bytes(pubKey).length <= 64 && bytes(pubKey).length >= 50 );
-
-        require(bytes(_ethToSphtx[msg.sender]).length == 0 || keccak256(bytes((_ethToSphtx[msg.sender]))) ==  keccak256(bytes(name)));//check that the address is not yet registered;
-
-        require(bytes(_accountToPubKey[name]).length == 0 || keccak256(bytes((_ethToSphtx[msg.sender]))) ==  keccak256(bytes(name))); //check that the name is not yet used
-        _accountToPubKey[name] = pubKey;
-        _ethToSphtx[msg.sender] = name;
-        emit AccountRegister(msg.sender, name, pubKey);
+    constructor() public{
+        tokenAddress = TokenRequestStub(0x3833ddA0AEB6947b98cE454d89366cBA8Cc55528);
     }
 
-    function account(address addr) constant public returns (string){
-        return _ethToSphtx[addr];
+    function register(string pubKey) public{
+        require(bytes(pubKey).length <= 64 && bytes(pubKey).length >= 50 );
+        uint holding = tokenAddress.balanceOf(msg.sender);
+        _ethToPubKey[msg.sender] = pubKey;
+        emit AccountRegister(msg.sender, pubKey, holding);
     }
 
     function keys(address addr) constant public returns (string){
-        return _accountToPubKey[_ethToSphtx[addr]];
+        return _ethToPubKey[addr];
     }
-
-    function nameAvailable(string name) constant public returns (bool){
-        if( bytes(_accountToPubKey[name]).length != 0 )
-           return false;
-        if(bytes(name).length < 3 && bytes(name).length > 16)
-           return false;
-        bytes memory b = bytes(name);
-        if( (b[0] < 'a' || b[0] > 'z') && ( b[0] < '0' || b[0] > '9' ) )
-           return false;
-        for(uint i=1;i< bytes(name).length; i++)
-           if( (b[0] < 'a' || b[0] > 'z') && ( b[0] < '0' || b[0] > '9' ) && b[i] != '-' && b[i] != '.' )
-              return false;
-        return true;
-    }
-
-
 }
