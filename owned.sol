68f7060a6eef712d8b757361817a4e707724423a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:07:51 +0700

    add contract Owned

--- owned.sol
+++ owned.sol
@@ -1,106 +1,42 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Owned at 0xeaf75dc0ee2e96ee633193cce9430e48b656af41
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Owned at 0xd112cc704a852698d42338e9a7f3d8f980de5841
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
+contract IOwned {
 
-contract Owned {
+    function owner() public pure returns (address) {
+        
+    }
+
+    function transferOwnership(address _newOwner) public;
+    function acceptOwnership() public;
+    }
+
+contract Owned is IOwned {
     address public owner;
+    address public newOwner;
+    event OwnerUpdate(address _prevOwner, address _newOwner);
 
     function Owned() public {
         owner = msg.sender;
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner);
+        assert(msg.sender == owner);
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner public {
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != owner);
+        newOwner = _newOwner;
+    }
+
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnerUpdate(owner, newOwner);
         owner = newOwner;
-    }
-}
-
-
-contract TNCN is Owned {
-
-    string public name="CHULIAN CHINA";
-    string public symbol="TNCN";
-    uint8  public decimals=18;
-    uint256 public totalSupply;
-    uint256 public sellPrice;
-    uint256 public buyPrice;
-    uint minBalanceForAccounts;
-
-    mapping (address => uint256) public balanceOf;
-    mapping (address => bool) public frozenAccount;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event FrozenFunds(address target, bool frozen);
-
-    function TNCN() public {
-        totalSupply = 1000000000000000000000000000;
-        balanceOf[msg.sender] = totalSupply;
-    }
-
-    function setMinBalance(uint minimumBalanceInFinney) onlyOwner public {
-        minBalanceForAccounts = minimumBalanceInFinney * 1 finney;
-    }
-
-    /* Internal transfer, can only be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] >= _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        require(!frozenAccount[_from]);                     // Check if sender is frozen
-        require(!frozenAccount[_to]);                       // Check if recipient is frozen
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
-        emit Transfer(_from, _to, _value);
-    }
-    function transfer(address _to, uint256 _value) public {
-        require(!frozenAccount[msg.sender]);
-        if (msg.sender.balance<minBalanceForAccounts) {
-            sell((minBalanceForAccounts-msg.sender.balance)/sellPrice);
-        }
-        _transfer(msg.sender, _to, _value);
-    }
-
-    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        emit Transfer(0, owner, mintedAmount);
-        emit Transfer(owner, target, mintedAmount);
-    }
-
-
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        emit FrozenFunds(target, freeze);
-    }
-
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
-    }
-
-
-    function buy() payable public returns (uint amount) {
-        amount = msg.value / buyPrice;
-        require(balanceOf[this] >= amount);
-        balanceOf[msg.sender] += amount;
-        balanceOf[this] -= amount;
-        emit Transfer(this, msg.sender, amount);
-        return amount;
-    }
-
-    function sell(uint amount) public returns (uint revenue) {
-        require(balanceOf[msg.sender] >= amount);
-        balanceOf[this] += amount;
-        balanceOf[msg.sender] -= amount;
-        revenue = amount * sellPrice;
-        msg.sender.transfer(revenue);
-        emit Transfer(msg.sender, this, amount);
-        return revenue;
+        newOwner = 0x0;
     }
 }
