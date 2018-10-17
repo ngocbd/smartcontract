ea372977217f68f560e05b947ee5a03b10a808b1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 14:18:29 +0700

    add contract KyberNetwork

--- KyberNetwork.sol
+++ KyberNetwork.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KyberNetwork at 0x44f854ea73eec7a10c9ff0e003d1ec076ac18197
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract KyberNetwork at 0x706abce058db29eb36578c463cf295f180a1fe9c
 */
 pragma solidity 0.4.18;
 
@@ -150,25 +150,6 @@ contract Utils2 is Utils {
         }
 
         return decimals[token];
-    }
-
-    /// @dev notice, overrides previous implementation.
-    function setDecimals(ERC20 token) internal {
-        uint decimal;
-
-        if (token == ETH_TOKEN_ADDRESS) {
-            decimal = ETH_DECIMALS;
-        } else {
-            if (!address(token).call(bytes4(keccak256("decimals()")))) {/* solhint-disable-line avoid-low-level-calls */
-                //above code can only be performed with low level call. otherwise all operation will revert.
-                // call failed
-                decimal = 18;
-            } else {
-                decimal = token.decimals();
-            }
-        }
-
-        decimals[token] = decimal;
     }
 
     function calcDestAmount(ERC20 src, ERC20 dest, uint srcAmount, uint rate) internal view returns(uint) {
