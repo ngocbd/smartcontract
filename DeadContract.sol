e606108eb4e5052a4f08079c164d6cd00d665e05
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:36:26 +0700

    add contract DeadContract

---
+++ DeadContract.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DeadContract at 0x4d8acc91877c78584231b59d2ac9c011ce68eb46
+*/
+pragma solidity ^0.4.19;
+
+//Created by Moozicore
+
+contract DeadContract {}
