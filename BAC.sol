143e54f6f36ff72f09e65ce76e36a857dbd8f16a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 15:04:50 +0700

    add contract BAC

--- BAC.sol
+++ BAC.sol
@@ -1,242 +1,143 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BAC at 0xef80158b7db893e74fac82fe32415d9420e60ffd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BAC at 0x452762b5b3ca3b2b2850b3782962d74cd13c33a1
 */
-pragma solidity ^0.4.11;
-
- library SafeMath {
-  function mul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint a, uint b) internal returns (uint) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c >= a);
-    return c;
-  }
-
-  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a >= b ? a : b;
-  }
-
-  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a < b ? a : b;
-  }
-
-  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a >= b ? a : b;
-  }
-
-  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a < b ? a : b;
-  }
-
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
+pragma solidity ^0.4.18;
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
     }
-  }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+    function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+        return a >= b ? a : b;
+    }
+
+    function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+        return a < b ? a : b;
+    }
+
+    function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
 }
+contract Ownable {
+    address public owner;
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert();
+        }
+        _;
+    }
 
- contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
+    function transferOwnership(address newOwner) public onlyOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
+    }
+
+    function destruct() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
-
+contract ERC20Basic {
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public;
+    function approve(address spender, uint256 value) public;
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
 contract BasicToken is ERC20Basic {
-  using SafeMath for uint;
+    using SafeMath for uint256;
 
-  mapping(address => uint) balances;
+    mapping(address => uint256) balances;
+    uint256 public totalSupply;
 
-  modifier onlyPayloadSize(uint size) {
-     if(msg.data.length < size + 4) {
-       throw;
-     }
-     _;
-  }
+    modifier onlyPayloadSize(uint256 size) {
+        if(msg.data.length < size + 4) {
+            revert();
+        }
+        _;
+    }
 
-  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-  }
+    function transfer(address _to, uint256 _value) public onlyPayloadSize(2 * 32) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+    }
 
-  function balanceOf(address _owner) constant returns (uint balance) {
-    return balances[_owner];
-  }
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        return balances[_owner];
+    }
 
 }
-
 contract StandardToken is BasicToken, ERC20 {
 
-  mapping (address => mapping (address => uint)) allowed;
+    mapping (address => mapping (address => uint256)) allowed;
 
-  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
-    var _allowance = allowed[_from][msg.sender];
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    Transfer(_from, _to, _value);
-  }
+    function transferFrom(address _from, address _to, uint256 _value) public onlyPayloadSize(3 * 32) {
+        var _allowance = allowed[_from][msg.sender];
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+    }
 
+    function approve(address _spender, uint256 _value) public {
+        if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) revert();
 
-  function approve(address _spender, uint _value) {
-    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-  }
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+    }
 
-
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
 }
+contract BAC is StandardToken, Ownable {
 
-contract BAC is StandardToken{
-    string public constant name = "BananaFundCoin";
+    string public constant name = "Hundred industry alliance chain???????";
     string public constant symbol = "BAC";
-    uint public constant decimals = 18;
-    string public version = "1.0";
-    
-    //1??????????
-    uint public price ;
-    uint public issueIndex = 0;
-    //????
-    uint public constant bacFund =500*(10**6)*10**decimals;
-    uint public constant MaxReleasedBac =1000*(10**6)*10**decimals;
-    //?????BAC???
-    bool public saleOrNot;
-    //??
-    event InvalidCaller(address caller);
-    event Issue(uint issueIndex, address addr, uint ethAmount, uint tokenAmount);
-    event StartOK();
-    event InvalidState(bytes msg);
-    event ShowMsg(bytes msg);
-    
-    //??????????????
-    address public target;
-    
-    //??????   ?????? 
-    function BAC(uint _price){
-        target = msg.sender;
-        price =_price;
-        totalSupply=bacFund;
-        balances[target] = bacFund;
-        saleOrNot = false;
-    }
-    
-    modifier onlyOwner {
-        if (target == msg.sender) {
-          _;
-        } else {
-            InvalidCaller(msg.sender);
-            throw;
-        }
+    uint256 public constant decimals = 8;
+
+    function BAC() public {
+        owner = msg.sender;
+        totalSupply=80000000000000000;
+        balances[owner]=totalSupply;
     }
 
-    modifier inProgress {
-        if (saleStarted()) {
-            _;
-        } else {
-            InvalidState("Sale is not in progress");
-            throw;
-        }
-    }
-  
-    //????????????
-    function () payable{
-        if(saleOrNot){
-            issueToken(msg.sender);
-        }else{
-            throw;
-        }
-    }
-    
-    function issueToken(address recipient) payable inProgress{
-        assert(msg.value >= 0.01 ether);
-        //???????????
-        uint  amount = computeAccount(msg.value);
-        if(totalSupply < bacFund+MaxReleasedBac){
-            balances[recipient] = balances[recipient].add(amount);
-            totalSupply = totalSupply.add(amount);
-            Issue(issueIndex++, recipient,msg.value, amount);
-        }else{
-            InvalidState("BAC is not enough");
-            throw;
-        }
-        //?????????????
-        if (!target.send(msg.value)) {
-            throw;
-        }
-    }
-    
-    //?????????
-    function computeAccount(uint ehtAccount) internal constant returns (uint tokens){
-        tokens=price.mul(ehtAccount);
-    }
-    
-    //?????????
-    function setPrice(uint _price) onlyOwner{
-        if(_price>0){
-            price= _price;
-        }else{
-            ShowMsg("Invalid price");
-        }
-    }
-    
-    //???????
-    function startSale() onlyOwner{
-        if(!saleOrNot){
-            saleOrNot = true;
-            StartOK();
-        }else{
-            ShowMsg("sale is ing ");
-        }
-    }   
-    
-    // ???????
-    function stopSale() onlyOwner{
-        if(saleOrNot) {
-            saleOrNot=false;
-            //???????????target
-            if(totalSupply< 1500*(10**6)*10**decimals){
-                balances[target] = balances[target].add(1500*(10**6)*10**decimals-totalSupply);
-            }
-        }else{
-            ShowMsg("sale has been over");
-        }
-    }
-    
-    function saleStarted() constant returns (bool) {
-        return saleOrNot;
-    }
-    
-    //??
-    function destroy() onlyOwner{
-        suicide(target);
+    function () public {
+        revert();
     }
 }
