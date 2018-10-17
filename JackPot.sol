757938d9ff03a5145be8eefad036792df1661628
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:16:03 +0700

    add contract Jackpot

--- JackPot.sol
+++ JackPot.sol
@@ -1,31 +1,27 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jackpot at 0x375ae5615279a524bd8cf092579cc738da9b0f38
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jackpot at 0x6ce0396631466d1b04f03136478ada38205715c6
 */
 pragma solidity ^0.4.20;
-
-/**''''''
- *  ====    ;
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0 uint256 c = a / b;
     uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
@@ -44,32 +40,38 @@ contract Ownable {
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
 
-
-  function Ownable() {
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor () public {
     owner = msg.sender;
   }
 
 
-
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
 
- 
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
   function transferOwnership(address newOwner) onlyOwner public {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+    emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
 }
 
 contract Jackpot is Ownable {
 
-  string public constant name = "Jackpot NOW";
-  uint public playersRequired = 29;
-  uint256 public priceOfTicket = 1e15 wei;
+  string public constant name = "Jackpot";
 
   event newWinner(address winner, uint256 ticketNumber);
   // event newRandomNumber_bytes(bytes);
@@ -77,37 +79,33 @@ contract Jackpot is Ownable {
   event newContribution(address contributor, uint value);
 
   using SafeMath for uint256;
-  address[] public players = new address[](399);
+  address[] public players = new address[](10);
   uint256 public lastTicketNumber = 0;
-  uint8 public playersSignedUp = 0;
-  uint public blockMinedAt;
-  uint public amountwon;
-  address public TheWinner;
-  uint public amounRefferalWon;
-  address public theWinningReferral;
-  uint public randomNumber;
-  uint public balanceOfPot = this.balance;
+  uint8 public lastIndex = 0;
+
+  uint256 public numberOfPlayers = 10;
 
   struct tickets {
     uint256 startTicket;
     uint256 endTicket;
   }
 
-  mapping (address => tickets[])  ticketsMap;
-  mapping(address => address) public referral;
+  mapping (address => tickets[]) public ticketsMap;
   mapping (address => uint256) public contributions;
-  function updateFileds(uint256 _playersRequired, uint256 _priceOfTicket) onlyOwner{
-      playersRequired = _playersRequired;
-      priceOfTicket = _priceOfTicket;
+
+  function setNumberOfPlayers(uint256 _noOfPlayers) public onlyOwner {
+    numberOfPlayers = _noOfPlayers;
   }
 
-  function executeLottery() { 
+
+  function executeLottery() public { 
       
-        if (playersSignedUp > playersRequired-1) {
-          randomNumber = uint(blockhash(block.number-1))%lastTicketNumber + 1;
-          address  winner;
+        if (lastIndex >= numberOfPlayers) {
+          uint randomNumber = address(this).balance.mul(16807) % 2147483647;
+          randomNumber = randomNumber % lastTicketNumber;
+          address winner;
           bool hasWon;
-          for (uint8 i = 0; i < playersSignedUp; i++) {
+          for (uint8 i = 0; i < lastIndex; i++) {
             address player = players[i];
             for (uint j = 0; j < ticketsMap[player].length; j++) {
               uint256 start = ticketsMap[player][j].startTicket;
@@ -122,47 +120,38 @@ contract Jackpot is Ownable {
           }
           require(winner!=address(0) && hasWon);
 
-          for (uint8 k = 0; k < playersSignedUp; k++) {
+          for (uint8 k = 0; k < lastIndex; k++) {
             delete ticketsMap[players[k]];
             delete contributions[players[k]];
           }
 
-          playersSignedUp = 0;
+          lastIndex = 0;
           lastTicketNumber = 0;
-          blockMinedAt = block.number;
 
-          uint balance = this.balance;
-          balanceOfPot = balance;
-          amountwon = (balance*80)/100;
-          TheWinner = winner;
-          if (!owner.send(balance/10)) throw;
-          if(referral[winner] != 0x0000000000000000000000000000000000000000){
-              amounRefferalWon = (amountwon*5)/100;
-              referral[winner].send(amounRefferalWon);
-              winner.send(amountwon*95/100);
-              theWinningReferral = referral[winner];
-          }
-          else{
-              if (!winner.send(amountwon)) throw;
-          }
-          newWinner(winner, randomNumber);
+          uint balance = address(this).balance;
+        //   if (!owner.send(balance/10)) throw;
+          owner.transfer(balance/10);
+          //Both SafeMath.div and / throws on error
+        //   if (!winner.send(balance - balance/10)) throw;
+        winner.transfer(balance.sub(balance/10));
+        emit  newWinner(winner, randomNumber);
           
         }
       
   }
 
-  function getPlayers() constant returns (address[], uint256[]) {
-    address[] memory addrs = new address[](playersSignedUp);
-    uint256[] memory _contributions = new uint256[](playersSignedUp);
-    for (uint i = 0; i < playersSignedUp; i++) {
+  function getPlayers() public constant returns (address[], uint256[]) {
+    address[] memory addrs = new address[](lastIndex);
+    uint256[] memory _contributions = new uint256[](lastIndex);
+    for (uint i = 0; i < lastIndex; i++) {
       addrs[i] = players[i];
       _contributions[i] = contributions[players[i]];
     }
     return (addrs, _contributions);
   }
 
-  function getTickets(address _addr) constant returns (uint256[] _start, uint256[] _end) {
-    tickets[] tks = ticketsMap[_addr];
+  function getTickets(address _addr) public constant returns (uint256[] _start, uint256[] _end) {
+    tickets[] storage tks = ticketsMap[_addr];
     uint length = tks.length;
     uint256[] memory startTickets = new uint256[](length);
     uint256[] memory endTickets = new uint256[](length);
@@ -173,67 +162,34 @@ contract Jackpot is Ownable {
     return (startTickets, endTickets);
   }
 
-  function join()  payable {
+  function () public payable {
     uint256 weiAmount = msg.value;
     require(weiAmount >= 1e16);
 
     bool isSenderAdded = false;
-    for (uint8 i = 0; i < playersSignedUp; i++) {
+    for (uint8 i = 0; i < lastIndex; i++) {
       if (players[i] == msg.sender) {
         isSenderAdded = true;
         break;
       }
     }
     if (!isSenderAdded) {
-      players[playersSignedUp] = msg.sender;
-      playersSignedUp++;
+      players[lastIndex] = msg.sender;
+      lastIndex++;
     }
 
     tickets memory senderTickets;
     senderTickets.startTicket = lastTicketNumber;
-    uint256 numberOfTickets = (weiAmount/priceOfTicket);
+    uint256 numberOfTickets = weiAmount/1e15;
     senderTickets.endTicket = lastTicketNumber.add(numberOfTickets);
     lastTicketNumber = lastTicketNumber.add(numberOfTickets);
     ticketsMap[msg.sender].push(senderTickets);
 
     contributions[msg.sender] = contributions[msg.sender].add(weiAmount);
 
-    newContribution(msg.sender, weiAmount);
+    emit newContribution(msg.sender, weiAmount);
 
-    if(playersSignedUp > playersRequired) {
-      executeLottery();
-    }
-  }
-  
-    function joinwithreferral(address refer)  payable {
-    uint256 weiAmount = msg.value;
-    require(weiAmount >= 1e16);
-
-    bool isSenderAdded = false;
-    for (uint8 i = 0; i < playersSignedUp; i++) {
-      if (players[i] == msg.sender) {
-        isSenderAdded = true;
-        break;
-      }
-    }
-    if (!isSenderAdded) {
-      players[playersSignedUp] = msg.sender;
-      referral[msg.sender] = refer;
-      playersSignedUp++;
-    }
-
-    tickets memory senderTickets;
-    senderTickets.startTicket = lastTicketNumber;
-    uint256 numberOfTickets = (weiAmount/priceOfTicket);
-    senderTickets.endTicket = lastTicketNumber.add(numberOfTickets);
-    lastTicketNumber = lastTicketNumber.add(numberOfTickets);
-    ticketsMap[msg.sender].push(senderTickets);
-
-    contributions[msg.sender] = contributions[msg.sender].add(weiAmount);
-
-    newContribution(msg.sender, weiAmount);
-
-    if(playersSignedUp > playersRequired) {
+    if(lastIndex >= numberOfPlayers) {
       executeLottery();
     }
   }
