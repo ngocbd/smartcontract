94579f4b149d8d1da4293850febb33956d826581
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:02:08 +0700

    add contract DMC

--- DMC.sol
+++ DMC.sol
@@ -1,161 +1,232 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DMC at 0x01b90ee8249afdf628f738af4a2a8ffd7b8434ed
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DMC at 0x07c9995acfe02968d71694a2a9edf9b8095d66b4
 */
-pragma solidity ^0.4.16;
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
-
-contract DMC {
-    // Public variables of the token
-    string public name;
-    string public symbol;
-    uint8 public decimals = 9;
-    // 18 decimals is the strongly suggested default, avoid changing it
-    uint256 public totalSupply;
-
-    // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    // This generates a public event on the blockchain that will notify clients
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    
-    // This generates a public event on the blockchain that will notify clients
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
-
-    /**
-     * Constructor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
-    function TokenERC20(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol
-    ) public {
-        totalSupply = 1000000000000000000;  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = "DMC";                                   // Set the name for display purposes
-        symbol = "DMC";                               // Set the symbol for display purposes
+/**
+  * SafeMath Libary
+  */
+pragma solidity ^0.4.24;
+contract SafeMath {
+    function safeAdd(uint256 a, uint256 b) internal pure returns(uint256)
+    {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
-
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
-        balanceOf[_from] -= _value;
-        // Add the same to the recipient
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    function safeSub(uint256 a, uint256 b) internal pure returns(uint256)
+    {
+        assert(b <= a);
+        return a - b;
     }
-
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    /**
-     * Transfer tokens from other address
-     *
-     * Send `_value` tokens to `_to` on behalf of `_from`
-     *
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
+    function safeMul(uint256 a, uint256 b) internal pure returns(uint256)
+    {
+        if (a == 0) {
+        return 0;
         }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+    function safeDiv(uint256 a, uint256 b) internal pure returns(uint256)
+    {
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+contract Ownable {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
     }
 
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
-        Burn(msg.sender, _value);
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
+
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    emit Pause();
+  }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    emit Unpause();
+  }
+}
+
+contract EIP20Interface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns(bool success);
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    // solhint-disable-next-line no-simple-event-func-name
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender,uint256 _value);
+}
+
+
+contract DMC is EIP20Interface,Ownable,SafeMath,Pausable{
+    //// Constant token specific fields
+    string public constant name ="DoMengCoin";
+    string public constant symbol = "DMC";
+    uint8 public constant decimals = 18;
+    string  public version  = 'v0.1';
+    uint256 public constant initialSupply = 100000000;
+    
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowances;
+
+    //set raise time
+    uint256 public finaliseTime;
+
+    /* function DMC() public{
+        totalSupply = initialSupply*10**uint256(decimals);                        //  total supply
+        balances[msg.sender] = totalSupply;             // Give the creator all initial tokens
+    } */
+
+    constructor() public{
+        totalSupply = initialSupply*10**uint256(decimals);                        //  total supply
+        balances[msg.sender] = totalSupply;             // Give the creator all initial tokens
+    }
+
+    modifier notFinalised() {
+        require(finaliseTime == 0);
+        _;
+    }
+
+    function balanceOf(address _account) public view returns (uint) {
+        return balances[_account];
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal whenNotPaused returns(bool) {
+        require(_to != address(0x0)&&_value>0);
+        require(balances[_from] >= _value);
+        require(safeAdd(balances[_to],_value) > balances[_to]);
+
+        uint previousBalances = safeAdd(balances[_from],balances[_to]);
+        balances[_from] = safeSub(balances[_from],_value);
+        balances[_to] = safeAdd(balances[_to],_value);
+        emit Transfer(_from, _to, _value);
+        assert(safeAdd(balances[_from],balances[_to]) == previousBalances);
         return true;
     }
 
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
+
+    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool success){
+        return _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
+        require(_value <= allowances[_from][msg.sender]);
+        allowances[_from][msg.sender] = safeSub(allowances[_from][msg.sender],_value);
+        return _transfer(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+     function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowances[msg.sender][_spender] = safeAdd(allowances[msg.sender][_spender],_addedValue);
+        emit Approval(msg.sender, _spender, allowances[msg.sender][_spender]);
+        return true;
+  }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+            uint oldValue = allowances[msg.sender][_spender];
+            if (_subtractedValue > oldValue) {
+              allowances[msg.sender][_spender] = 0;
+            } else {
+              allowances[msg.sender][_spender] = safeSub(oldValue,_subtractedValue);
+            }
+            emit Approval(msg.sender, _spender, allowances[msg.sender][_spender]);
+            return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowances[_owner][_spender];
+    }
+ 
+    //close the raise
+    function setFinaliseTime() onlyOwner notFinalised public returns(bool){
+        finaliseTime = now;
+        return true;
+    }
+     //close the raise
+    function Restart() onlyOwner public returns(bool){
+        finaliseTime = 0;
         return true;
     }
 }
