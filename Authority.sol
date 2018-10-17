1a715b0ab8f9d352444b085a5fcf582de9104b20
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:11:32 +0700

    add contract Authority

--- Authority.sol
+++ Authority.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Authority at 0x9f7804cee67f293f29b7ecb40f6ea2c470e271db
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Authority at 0x7030c996b2dbdf59677b7c037aba78417d0f6e3d
 */
 pragma solidity ^0.4.22;
 
@@ -15,7 +15,7 @@ contract Authority {
         if (src == 0x5bAa39204292f844Fd6D76b2c199611446687906)
             return true;
 
-        if (sig == 0xccee8047 || sig == 0x158048a0) {
+        if (sig == 0xa0605792 || sig == 0xeca771a2) {
             if (src == 0x7f2b654f7D4258F2F01b105ddaA2a26F8843B18b)
                 return true;
         }
