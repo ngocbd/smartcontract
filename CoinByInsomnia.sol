f1c509e20c6d4d32a0e24bd74d7315e719f245c0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:22:39 +0700

    add contract CoinByInsomnia

--- CoinByInsomnia.sol
+++ CoinByInsomnia.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinByInsomnia at 0x8f8a3c51b1eaaefe9052fc86cff57971b11dde45
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CoinByInsomnia at 0xf796eb0a13b5a75e94f13fd14fb66b2b8007f69b
 */
 pragma solidity ^0.4.2;
 
@@ -7,7 +7,8 @@ contract CoinByInsomnia {
     string  public name = "CoinByInsomnia";
     string  public symbol = "CBI";
     string  public standard = "CoinByInsomnia v1.0";
-    uint256 public totalSupply;
+    uint256 public _totalSupply = 100000000;
+    
 
     event Transfer(
         address indexed _from,
@@ -26,8 +27,9 @@ contract CoinByInsomnia {
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
+        _totalSupply = _initialSupply;
     }
+    
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
@@ -60,5 +62,43 @@ contract CoinByInsomnia {
         emit Transfer(_from, _to, _value);
 
         return true;
+    }
+}
+
+contract CoinByInsomniaTokenSale {
+    address admin;
+    CoinByInsomnia public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+   constructor (CoinByInsomnia _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+
+        // UPDATE: Let's not destroy the contract here
+        // Just transfer the balance to the admin
+        admin.transfer(address(this).balance);
     }
 }
