92a522289adcbeb04395a7eb0077e25870650da3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:15:01 +0700

    add contract BEthy

--- BEthy.sol
+++ BEthy.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BEthy at 0x248fda6a145642bd9bba9d5d349164e8ffe933cc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BEthy at 0xdb83c1130951bafd1d3716ef8baeeeea4cceb61e
 */
 pragma solidity ^0.4.24;
 
@@ -29,7 +29,7 @@ contract ExchangeAdmin {
     }
 }
 library StringYokes {
-        function zint_bytes32ToString(bytes32 x) public pure returns (string) {
+    function zint_bytes32ToString(bytes32 x) public pure returns (string) {
         bytes memory bytesString = new bytes(32);
         uint charCount = 0;
         for (uint j = 0; j < 32; j++) {
@@ -51,44 +51,49 @@ library StringYokes {
           ret := mload(add(key, 32))
         }
     }
-}    
+}
 contract Game is ExchangeAdmin {
-    
     bool expired;
-    
     struct Better {
         bool used;
         address betterAddress;
         uint contribAmount;
     }
-    
     struct Side {
+        bool isValidSide;
         uint totalPledged;
         bytes32 sideName;
         address[] usedAddresses;
         mapping (address => Better) contribDb;
     }
     string gName;
+    address gameMaker;
+    address mainContract;
     bytes32[] sides;
     uint allSidesPledged;
     uint expiry;
     mapping (bytes32 => Side) public sideData;
     mapping (bytes32 => uint) public idToNameRef;
-    
-    constructor (string gameName, uint gameExpiry, bytes32[] gameSides) public {
+    constructor (string gameName, uint gameExpiry, bytes32[] gameSides,address maker, address mainContractAdd) public {
+        require(now<gameExpiry);
         gName = gameName;
+        gameMaker = maker;
         expiry = gameExpiry;
+        mainContract = mainContractAdd;
         for (uint i = 0; i<gameSides.length; i++) {
             sideData[gameSides[i]].sideName=gameSides[i];
+            sideData[gameSides[i]].isValidSide=true;
             idToNameRef[gameSides[i]]=i;
             sides.push(gameSides[i]);
         }
         expired = false;
         allSidesPledged = 0;
     }
-    
     function getGameName() view public returns (string) {
         return gName;
+    }
+    function getGameMaker() view public returns(address){
+      return gameMaker;
     }
     function getGameSides() view public returns (bytes32[]) {
         return sides;
@@ -96,8 +101,14 @@ contract Game is ExchangeAdmin {
     function isNotExpired() view public returns (bool) {
         return ((now < expiry) && !expired);
     }
+    function isExpired() view public returns(bool){
+        return expired;
+    }
     function getNumSides() view public returns (uint) {
         return sides.length;
+    }
+    function getExpiryTime() view public returns(uint){
+      return expiry;
     }
     function getStrFromId(uint toConv) view public returns (string) {
         return StringYokes.zint_bytes32ToString(sides[toConv]);
@@ -105,31 +116,25 @@ contract Game is ExchangeAdmin {
     function getIdFromStr(string toConv) view public returns (uint) {
         return idToNameRef[StringYokes.zint_convert(toConv)];
     }
-    
     function placeBet(address a, uint value, string betSide) public payable {
-        require(isNotExpired());
+        require(isNotExpired() && value!=0 && msg.sender==mainContract && sideData[StringYokes.zint_convert(betSide)].isValidSide);
         bytes32 index = StringYokes.zint_convert(betSide);
         sideData[index].totalPledged+=value;
         allSidesPledged+=value;
-        if (sideData[index].contribDb[a].used) {
-            value+=sideData[index].contribDb[a].contribAmount;
-        }
-        else {
+        if (!sideData[index].contribDb[a].used) {
             sideData[index].usedAddresses.push(a);
             sideData[index].contribDb[a].used=true;
         }
         sideData[index].contribDb[a].contribAmount+=value;
     }
-    
     function allSidesPledgedAmount() public view returns (uint) {
         return allSidesPledged;
     }
-    
     function checkSidePledge(uint i) public view returns (uint) {
         return sideData[sides[i]].totalPledged;
     }
-    function dish(string winner, address profit) public onlyAdmin payable {
-        require(!expired);
+    function dish(string winner, address profit) public payable {
+        require((!expired) && (mainContract==msg.sender));
         expired = true;
         bytes32 winByte = StringYokes.zint_convert(winner);
         uint totalGameContrib = allSidesPledged;
@@ -137,95 +142,63 @@ contract Game is ExchangeAdmin {
         for (uint i = 0; i<sideData[winByte].usedAddresses.length; i++) {
             address recip = sideData[winByte].usedAddresses[i];
             uint contribAmount = sideData[winByte].contribDb[recip].contribAmount;
-            uint winAddition = (925*1000*contribAmount*(totalGameContrib-totalSideContrib))/(1000000*totalSideContrib);
+            uint winAddition = (950*1000*contribAmount*(totalGameContrib-totalSideContrib))/(1000000*totalSideContrib);
             recip.transfer(contribAmount+winAddition);
         }
-        profit.transfer(address(this).balance);
+        profit.transfer(2*(address(this).balance/5));
+        gameMaker.transfer(address(this).balance);
     }
-    function refund() public onlyAdmin payable {
+    function refund(address sentBy) public payable {
+        require(!expired && (mainContract==msg.sender) && ((sentBy==gameMaker) || now > getExpiryTime() + 259200));
         for (uint i = 0; i<sides.length; i++) {
             for (uint j = 0; j<sideData[sides[i]].usedAddresses.length; j++) {
-            address recip = sideData[sides[i]].usedAddresses[j];
-            uint contribAmount = sideData[sides[i]].contribDb[recip].contribAmount;
-            recip.transfer(contribAmount);
+                address recip = sideData[sides[i]].usedAddresses[j];
+                uint contribAmount = sideData[sides[i]].contribDb[recip].contribAmount;
+                recip.transfer(contribAmount);
             }
         }
     }
-    
 }
 
 contract BEthy is ExchangeAdmin {
     Game[] current;
-    uint etherBalance;
-    
-    mapping (bytes32 => uint) public references;
-    
-    constructor () public {
-    }
-    
-    function addGame(string gameName, uint gameExpiry, bytes32[] gameSides) onlyAdmin public {
-        current.push(new Game(gameName, gameExpiry, gameSides));
-        references[StringYokes.zint_convert(gameName)]=current.length-1;
-    }
-    
-    function numGames() view public returns (uint) {
+    constructor () public {    }
+
+    function numGames() view public returns (uint nGames) {
         return current.length;
     }
-    
-    function getName(uint i) view public returns (string, bool, uint) {
-       return (current[i].getGameName(), current[i].isNotExpired(), current[i].allSidesPledgedAmount());
+    function getName(uint i) view public returns (string gameName, bool isNotExpired, uint totalPledgedETH, bool wasFinalised, uint gameEndTime, address gameMakerAddress, uint gameNumSides, uint gameId) {
+       return (current[i].getGameName(), current[i].isNotExpired(), current[i].allSidesPledgedAmount(),current[i].isExpired(),current[i].getExpiryTime(), current[i].getGameMaker(), current[i].getNumSides(), i);
     }
-    
-    function getSidesById(uint i, uint j) view public returns (string, uint) {
+    function getSidesArray(uint i) view public returns (bytes32[] sideNameBytes) {
+        return current[i].getGameSides();
+    }
+    function getSidesById(uint i, uint j) view public returns (string sideName, uint sidePledgedETH) {
         return (StringYokes.zint_bytes32ToString(current[i].getGameSides()[j]), current[i].checkSidePledge(j));
     }
-    
-    function getSides(string str, uint j) view public returns (string, uint) {
-        return getSidesById(references[StringYokes.zint_convert(str)], j);
-    }
-    
-    function getGameNumSides(uint i) view public returns (uint) {
+    function getGameNumSides(uint i) view public returns (uint gameNumSides) {
         return current[i].getNumSides();
     }
-    function _byteToString(bytes32 x) public pure returns (string) {
-        return StringYokes.zint_bytes32ToString(x);
-    }
-    function _stringToByte(string x) public pure returns (bytes32) {
-        return StringYokes.zint_convert(x);
-    }
-    
-    function () public payable {
-        etherBalance+=msg.value;
-    }
-    
-    function getBalance() public view returns (uint) {
-        return etherBalance;
-    }
-    function getAddBal() public view returns (uint) {
+    function getContractBal() public view returns (uint invalidBalanceETH) {
         return address(this).balance;
     }
-    
+
+    function () public payable {    }
+    function emergency(uint amount, address recipient) public onlyAdmin payable {
+        recipient.transfer(amount);
+    }
+    function addGame(string gameName, uint gameExpiry, bytes32[] gameSides) public {
+        require(gameSides.length > 1);
+        current.push(new Game(gameName, gameExpiry, gameSides, msg.sender, address(this)));
+    }
+    function endGame(uint gameId, string winningSide) public  {
+        require(current[gameId].getGameMaker() == msg.sender);
+        current[gameId].dish(winningSide,owner);
+    }
+    function refund(uint gameId) public {
+        current[gameId].refund(msg.sender);
+    }
     function placeBet(uint gameId, string betSide) payable public {
-        require(msg.value!=0);
-        etherBalance+=msg.value;
         current[gameId].placeBet.value(msg.value)(msg.sender, msg.value, betSide);
-    }
-    
-    function placeBet(string gameId, string betSide) payable public {
-        placeBet(references[StringYokes.zint_convert(gameId)], betSide);
-    }
-    
-    function checkGameAmount(uint gameId) public view returns (uint) {
-        return current[gameId].allSidesPledgedAmount();
-    }
-    function checkGameSideAmount(uint gameId, uint sideNum) public view returns (uint) {
-        return current[gameId].checkSidePledge(sideNum);
-    }
-    
-    function endGame(uint gameId, string winningSide, address beneficiary) public onlyAdmin {//returns (address[10], uint[10]) {
-        current[gameId].dish(winningSide, beneficiary);
-    }
-    function endGame(uint gameId, uint winningId, address profit) public onlyAdmin {
-        endGame(gameId, current[gameId].getStrFromId(winningId), profit);
     }
 }
