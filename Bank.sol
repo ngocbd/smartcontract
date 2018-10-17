b78c6f09f3d0b83f0d43ad1cf04f82731c460575
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:46:09 +0700

    add contract Bank

--- Bank.sol
+++ Bank.sol
@@ -1,7 +1,139 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bank at 0xaa4250216f77a79fd9983642a987f708f1fe7504
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bank at 0xa04e5b78fbd31caec8f8af126d00a57f56c1f7ae
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.23;
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+pragma solidity 0.4.24;
+
+contract Transfer {
+
+    address constant public ETH = 0x0;
+
+    /**
+    * @dev Transfer tokens from this contract to an account.
+    * @param token Address of token to transfer. 0x0 for ETH
+    * @param to Address to send tokens to.
+    * @param amount Amount of token to send.
+    */
+    function transfer(address token, address to, uint256 amount) internal returns (bool) {
+        if (token == ETH) {
+            to.transfer(amount);
+        } else {
+            require(ERC20(token).transfer(to, amount));
+        }
+        return true;
+    }
+
+    /**
+    * @dev Transfer tokens from an account to this contract.
+    * @param token Address of token to transfer. 0x0 for ETH
+    * @param from Address to send tokens from.
+    * @param to Address to send tokens to.
+    * @param amount Amount of token to send.
+    */
+    function transferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 amount
+    ) 
+        internal
+        returns (bool)
+    {
+        require(token == ETH && msg.value == amount || msg.value == 0);
+
+        if (token != ETH) {
+            // Remember to approve first
+            require(ERC20(token).transferFrom(from, to, amount));
+        }
+        return true;
+    }
+
+}
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 
 /**
  * @title Ownable
@@ -12,14 +144,18 @@ contract Ownable {
   address public owner;
 
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
 
 
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() public {
+  constructor() public {
     owner = msg.sender;
   }
 
@@ -32,314 +168,139 @@ contract Ownable {
   }
 
   /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
+   * @param _newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
-
-  bool public paused = false;
-
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is not paused.
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
   }
 
   /**
-   * @dev Modifier to make a function callable only when the contract is paused.
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
    */
-  modifier whenPaused() {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused public {
-    paused = true;
-    emit Pause();
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused public {
-    paused = false;
-    emit Unpause();
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
   }
 }
 
 
+/*
 
-/// @author https://BlockChainArchitect.iocontract Bank is CutiePluginBase
-contract PluginInterface
-{
-    /// @dev simply a boolean to indicate this is the contract we expect to be
-    function isPluginInterface() public pure returns (bool);
+  Copyright 2018 Contra Labs Inc.
 
-    function onRemove() public;
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-    /// @dev Begins new feature.
-    /// @param _cutieId - ID of token to auction, sender must be owner.
-    /// @param _parameter - arbitrary parameter
-    /// @param _seller - Old owner, if not the message sender
-    function run(
-        uint40 _cutieId,
-        uint256 _parameter,
-        address _seller
-    ) 
-    public
-    payable;
+  http://www.apache.org/licenses/LICENSE-2.0
 
-    /// @dev Begins new feature, approved and signed by COO.
-    /// @param _cutieId - ID of token to auction, sender must be owner.
-    /// @param _parameter - arbitrary parameter
-    function runSigned(
-        uint40 _cutieId,
-        uint256 _parameter,
-        address _owner
-    )
-    external
-    payable;
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
-    function withdraw() public;
-}
+*/
+
+pragma solidity 0.4.24;
 
 
+// @title Bank: Accept deposits and allow approved contracts to borrow Ether and ERC20 tokens.
+// @author Rich McAteer <rich@marble.org>, Max Wolff <max@marble.org>
+contract Bank is Ownable, Transfer {
+    using SafeMath for uint256;
 
-contract CutieCoreInterface
-{
-    function isCutieCore() pure public returns (bool);
+    // Borrower => Approved
+    mapping (address => bool) public approved;
 
-    function transferFrom(address _from, address _to, uint256 _cutieId) external;
-    function transfer(address _to, uint256 _cutieId) external;
-
-    function ownerOf(uint256 _cutieId)
-        external
-        view
-        returns (address owner);
-
-    function getCutie(uint40 _id)
-        external
-        view
-        returns (
-        uint256 genes,
-        uint40 birthTime,
-        uint40 cooldownEndTime,
-        uint40 momId,
-        uint40 dadId,
-        uint16 cooldownIndex,
-        uint16 generation
-    );
-
-     function getGenes(uint40 _id)
-        public
-        view
-        returns (
-        uint256 genes
-    );
-
-
-    function getCooldownEndTime(uint40 _id)
-        public
-        view
-        returns (
-        uint40 cooldownEndTime
-    );
-
-    function getCooldownIndex(uint40 _id)
-        public
-        view
-        returns (
-        uint16 cooldownIndex
-    );
-
-
-    function getGeneration(uint40 _id)
-        public
-        view
-        returns (
-        uint16 generation
-    );
-
-    function getOptional(uint40 _id)
-        public
-        view
-        returns (
-        uint64 optional
-    );
-
-
-    function changeGenes(
-        uint40 _cutieId,
-        uint256 _genes)
-        public;
-
-    function changeCooldownEndTime(
-        uint40 _cutieId,
-        uint40 _cooldownEndTime)
-        public;
-
-    function changeCooldownIndex(
-        uint40 _cutieId,
-        uint16 _cooldownIndex)
-        public;
-
-    function changeOptional(
-        uint40 _cutieId,
-        uint64 _optional)
-        public;
-
-    function changeGeneration(
-        uint40 _cutieId,
-        uint16 _generation)
-        public;
-}
-
-
-/// @author https://BlockChainArchitect.iocontract Bank is CutiePluginBase
-contract CutiePluginBase is PluginInterface, Pausable
-{
-    function isPluginInterface() public pure returns (bool)
-    {
-        return true;
-    }
-
-    // Reference to contract tracking NFT ownership
-    CutieCoreInterface public coreContract;
-
-    // Cut owner takes on each auction, measured in basis points (1/100 of a percent).
-    // Values 0-10,000 map to 0%-100%
-    uint16 public ownerFee;
-
-    // @dev Throws if called by any account other than the owner.
-    modifier onlyCore() {
-        require(msg.sender == address(coreContract));
+    modifier onlyApproved() {
+        require(approved[msg.sender] == true);
         _;
     }
 
-    /// @dev Constructor creates a reference to the NFT ownership contract
-    ///  and verifies the owner cut is in the valid range.
-    /// @param _coreAddress - address of a deployed contract implementing
-    ///  the Nonfungible Interface.
-    /// @param _fee - percent cut the owner takes on each auction, must be
-    ///  between 0-10,000.
-    function setup(address _coreAddress, uint16 _fee) public {
-        require(_fee <= 10000);
-        require(msg.sender == owner);
-        ownerFee = _fee;
-        
-        CutieCoreInterface candidateContract = CutieCoreInterface(_coreAddress);
-        require(candidateContract.isCutieCore());
-        coreContract = candidateContract;
+    /**
+    * @dev Deposit tokens to the bank.
+    * @param token Address of token to deposit. 0x0 for ETH
+    * @param amount Amount of token to deposit.
+    */
+    function deposit(address token, uint256 amount) external onlyOwner payable {
+        transferFrom(token, msg.sender, this, amount);
     }
 
-    // @dev Set the owner's fee.
-    //  @param fee should be between 0-10,000.
-    function setFee(uint16 _fee) public
-    {
-        require(_fee <= 10000);
-        require(msg.sender == owner);
-
-        ownerFee = _fee;
+    /**
+    * @dev Withdraw tokens from the bank.
+    * @param token Address of token to withdraw. 0x0 for ETH
+    * @param amount Amount of token to withdraw.
+    */
+    function withdraw(address token, uint256 amount) external onlyOwner {
+        transfer(token, msg.sender, amount);
     }
 
-    /// @dev Returns true if the claimant owns the token.
-    /// @param _claimant - Address claiming to own the token.
-    /// @param _cutieId - ID of token whose ownership to verify.
-    function _isOwner(address _claimant, uint40 _cutieId) internal view returns (bool) {
-        return (coreContract.ownerOf(_cutieId) == _claimant);
+    /**
+    * @dev Borrow tokens from the bank.
+    * @param token Address of token to borrow. 0x0 for ETH
+    * @param amount Amount of token to borrow.
+    */
+    function borrow(address token, uint256 amount) external onlyApproved {
+        borrowFor(token, msg.sender, amount);
     }
 
-    /// @dev Escrows the NFT, assigning ownership to this contract.
-    /// Throws if the escrow fails.
-    /// @param _owner - Current owner address of token to escrow.
-    /// @param _cutieId - ID of token whose approval to verify.
-    function _escrow(address _owner, uint40 _cutieId) internal {
-        // it will throw if transfer fails
-        coreContract.transferFrom(_owner, this, _cutieId);
+    /**
+    * @dev Borrow tokens from the bank on behalf of another account.
+    * @param token Address of token to borrow. 0x0 for ETH
+    * @param who Address to send borrowed amount to.
+    * @param amount Amount of token to borrow.
+    */
+    function borrowFor(address token, address who, uint256 amount) public onlyApproved {
+        transfer(token, who, amount);        
     }
 
-    /// @dev Transfers an NFT owned by this contract to another address.
-    /// Returns true if the transfer succeeds.
-    /// @param _receiver - Address to transfer NFT to.
-    /// @param _cutieId - ID of token to transfer.
-    function _transfer(address _receiver, uint40 _cutieId) internal {
-        // it will throw if transfer fails
-        coreContract.transfer(_receiver, _cutieId);
+    /**
+    * @dev Repay tokens to the bank.
+    * @param token Address of token to repay. 0x0 for ETH
+    * @param amount Amount of token to repay.
+    */
+    function repay(address token, uint256 amount) external payable {
+        transferFrom(token, msg.sender, this, amount);
     }
 
-    /// @dev Computes owner's cut of a sale.
-    /// @param _price - Sale price of NFT.
-    function _computeFee(uint128 _price) internal view returns (uint128) {
-        // NOTE: We don't use SafeMath (or similar) in this function because
-        //  all of our entry functions carefully cap the maximum values for
-        //  currency (at 128-bits), and ownerFee <= 10000 (see the require()
-        //  statement in the ClockAuction constructor). The result of this
-        //  function is always guaranteed to be <= _price.
-        return _price * ownerFee / 10000;
+    /**
+    * @dev Approve a new borrower.
+    * @param borrower Address of new borrower.
+    */
+    function addBorrower(address borrower) external onlyOwner {
+        approved[borrower] = true;
     }
 
-    function withdraw() public
-    {
-        require(
-            msg.sender == owner ||
-            msg.sender == address(coreContract)
-        );
-        if (address(this).balance > 0)
-        {
-            address(coreContract).transfer(address(this).balance);
+    /**
+    * @dev Revoke approval of a borrower.
+    * @param borrower Address of borrower to revoke.
+    */
+    function removeBorrower(address borrower) external onlyOwner {
+        approved[borrower] = false;
+    }
+
+    /**
+    * @dev Gets balance of bank. 
+    * @param token Address of token to calculate total supply of.
+    */
+    function totalSupplyOf(address token) public view returns (uint256 balance) {
+        if (token == ETH) {
+            return address(this).balance; 
+        } else {
+            return ERC20(token).balanceOf(this); 
         }
     }
 
-    function onRemove() public onlyCore
-    {
-        withdraw();
-    }
-}
-
-
-/// @dev Receives payments for payd features from players for Blockchain Cuties
-/// @author https://BlockChainArchitect.io
-contract Bank is CutiePluginBase
-{
-    function run(
-        uint40,
-        uint256,
-        address
-    ) 
-        public
-        payable
-        onlyCore
-    {
-        revert();
-    }
-
-    function runSigned(uint40, uint256, address)
-        external
-        payable
-        onlyCore
-    {
-        // just accept payments
-    }
 }
