ecfb9c2863804025a4cc157c0f83f703553f403d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:55:56 +0700

    add contract FundsLocker

--- FundsLocker.sol
+++ FundsLocker.sol
@@ -1,8 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FundsLocker at 0x68211bde16a31e03be57999c71cb69109afe53e1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FundsLocker at 0xed6f156fde77efe9d1d58324a647f72b1f938bc2
 */
 pragma solidity ^0.4.24;
 
+// File: node_modules\openzeppelin-solidity\contracts\ownership\Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
 contract Ownable {
   address public owner;
 
@@ -60,6 +67,12 @@ contract Ownable {
   }
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\math\SafeMath.sol
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
 
   /**
@@ -106,6 +119,13 @@ library SafeMath {
   }
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\token\ERC20\ERC20Basic.sol
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * See https://github.com/ethereum/EIPs/issues/179
+ */
 contract ERC20Basic {
   function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
@@ -113,12 +133,18 @@ contract ERC20Basic {
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\token\ERC20\BasicToken.sol
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
 contract BasicToken is ERC20Basic {
   using SafeMath for uint256;
 
-  mapping(address => uint256) internal balances;
+  mapping(address => uint256) balances;
 
-  uint256 internal totalSupply_;
+  uint256 totalSupply_;
 
   /**
   * @dev Total number of tokens in existence
@@ -133,8 +159,8 @@ contract BasicToken is ERC20Basic {
   * @param _value The amount to be transferred.
   */
   function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_value <= balances[msg.sender]);
     require(_to != address(0));
+    require(_value <= balances[msg.sender]);
 
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
@@ -153,6 +179,12 @@ contract BasicToken is ERC20Basic {
 
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\token\ERC20\ERC20.sol
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
 contract ERC20 is ERC20Basic {
   function allowance(address owner, address spender)
     public view returns (uint256);
@@ -168,6 +200,15 @@ contract ERC20 is ERC20Basic {
   );
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\token\ERC20\StandardToken.sol
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/issues/20
+ * Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
 contract StandardToken is ERC20, BasicToken {
 
   mapping (address => mapping (address => uint256)) internal allowed;
@@ -187,9 +228,9 @@ contract StandardToken is ERC20, BasicToken {
     public
     returns (bool)
   {
+    require(_to != address(0));
     require(_value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
-    require(_to != address(0));
 
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
@@ -269,7 +310,7 @@ contract StandardToken is ERC20, BasicToken {
     returns (bool)
   {
     uint256 oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue >= oldValue) {
+    if (_subtractedValue > oldValue) {
       allowed[msg.sender][_spender] = 0;
     } else {
       allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
@@ -280,6 +321,13 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
+// File: node_modules\openzeppelin-solidity\contracts\token\ERC20\MintableToken.sol
+
+/**
+ * @title Mintable token
+ * @dev Simple ERC20 Token example, with mintable token creation
+ * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
+ */
 contract MintableToken is StandardToken, Ownable {
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
@@ -330,7 +378,48 @@ contract MintableToken is StandardToken, Ownable {
   }
 }
 
+// File: contracts\BuddyToken.sol
+
+contract BuddyToken is MintableToken {
+
+    string public symbol = "BUD";
+    string public name = "Buddy";
+    uint8 public decimals = 18;
+
+    constructor() public  {
+
+    }
+
+    function burn(uint256 _value) public {
+        _burn(msg.sender, _value);
+    }
+
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balances[_who]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        emit Burn(_who, _value);
+        emit Transfer(_who, address(0), _value);
+    }
+
+    //helps to recover stucked tokens
+    function withdrawTokens(ERC20 erc20, address reciver, uint amount) public onlyOwner {
+        require(reciver != address(0x0));
+        erc20.transfer(reciver, amount);
+    }
+
+
+    event Burn(address indexed burner, uint256 value);
+
+}
+
+
 contract FundsLocker {
+
+    using SafeMath for uint;
 
     uint public lockTimeout;
     uint public totalTokenBalance;
@@ -339,27 +428,28 @@ contract FundsLocker {
 
     uint public vestingPeriod;
 
-    MintableToken public token;
+    BuddyToken public token;
 
-    constructor(address _beneficiary, uint256 _lockPeriod, uint _vestingPeriod,  MintableToken _token) public {
+    constructor(address _beneficiary,uint256 _start, uint256 _lockPeriod, uint _vestingPeriod,  BuddyToken _token) public {
         require(_beneficiary!=address(0));
+        require(_start >= now);
 
         //TODO check parameters
         beneficiary = _beneficiary;
-        lockTimeout = now + _lockPeriod;
+        lockTimeout = _start.add(_lockPeriod);
         vestingPeriod = _vestingPeriod;
         token = _token;
     }
 
     function () public {
         uint256 currentBalance = token.balanceOf(this);
-        uint256 additionalTokens = currentBalance - latestTokenBalance;
+        uint256 additionalTokens = currentBalance.sub(latestTokenBalance);
 
         //all new tokens are under same rules as transfereed on the begining
-        totalTokenBalance = totalTokenBalance+additionalTokens;
+        totalTokenBalance = totalTokenBalance.add(additionalTokens);
         uint withdrawAmount = calculateSumToWithdraw();
         
-        token.transfer(beneficiary, withdrawAmount);
+        require(token.transfer(beneficiary, withdrawAmount));
         latestTokenBalance = token.balanceOf(this);
 
     }
@@ -370,43 +460,15 @@ contract FundsLocker {
         if(now<=lockTimeout) 
             return 0;
 
-        if(now>lockTimeout+vestingPeriod)
+        if(now>lockTimeout.add(vestingPeriod))
             return currentBalance;
         
-        uint256 minRequiredBalance = totalTokenBalance-totalTokenBalance*(now-lockTimeout)/vestingPeriod;
+        uint256 minRequiredBalance = totalTokenBalance.sub(totalTokenBalance.mul(now.sub(lockTimeout)).div(vestingPeriod));
 
         if(minRequiredBalance > currentBalance)
             return 0;
         else 
-            return  currentBalance-minRequiredBalance;
+            return currentBalance.sub(minRequiredBalance);
     }
 
-}
-
-contract KabutoCash  is MintableToken {
-    	string public constant name = "Kabuto Cash";
-	string public constant symbol = "KC";
-	uint256 public constant DECIMALS = 15;
-	uint256 public constant decimals = 15;
-	address public constant dev = address(0x59a5ac4033db403587e8beab8996ede2f170413a);
-	address public constant own = address(0x7704C758db402bB7B1c2BbadA8af43B6B758B794);
-	address public vestingContract ;
-	
-	bool public initialized = false;
-	bool public devPayed = false;
-	
-	function initialize() public{
-	    vestingContract = address(new FundsLocker(own,1 years,9 years,MintableToken(this)));
-	    require(initialized==false);
-	    owner = this;
-	    this.mint(own,7000000*10**decimals);
-	    this.mint(vestingContract,63000000*10**decimals);
-	}
-	
-	function () public{
-	    require(devPayed==false);
-	    require(msg.sender==dev);
-	    devPayed = true;
-	    this.mint(dev,1000 ether);
-	}
 }
