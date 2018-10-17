aa7d363bb3efc5bd9bc9207904b350d942bed1fd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 04:42:52 +0700

    add contract AutomatedExchange

--- AutomatedExchange.sol
+++ AutomatedExchange.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AutomatedExchange at 0x286a090b31462890cd9bf9f167b610ed8aa8bd1a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AutomatedExchange at 0xac1a2952942e2751772eb7c6c57d4bdf21090465
 */
 pragma solidity ^0.4.18; // solhint-disable-line
 
@@ -9,13 +9,14 @@ pragma solidity ^0.4.18; // solhint-disable-line
 // ERC Token Standard #20 Interface
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 // ----------------------------------------------------------------------------
-contract ERC20Interface {
+contract VerifyToken {
     function totalSupply() public constant returns (uint);
     function balanceOf(address tokenOwner) public constant returns (uint balance);
     function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
     function transfer(address to, uint tokens) public returns (bool success);
     function approve(address spender, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    bool public activated;
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -25,32 +26,30 @@ contract ApproveAndCallFallBack {
     function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
 }
 
-// ----------------------------------------------------------------------------
-// This is an automated token exchange. It lets you buy and sell a specific token for Ethereum.
-// The more eth in the contract the higher the token price, the more tokens in the contract
-// the lower the token price. The formula is the same as for Ether Shrimp Farm.
-// There are no fees except gas cost.
-// ----------------------------------------------------------------------------
-
 contract AutomatedExchange is ApproveAndCallFallBack{
 
     uint256 PSN=100000000000000;
     uint256 PSNH=50000000000000;
-    address tokenAddress=0x841D34aF2018D9487199678eDd47Dd46B140690B;
-    ERC20Interface tokenContract=ERC20Interface(tokenAddress);
-    function AutomatedExchange() public{
-    }
+    address vrfAddress=0x9E129e47213589C5Da4d92CC6Bb056425d60b0e1; //0xe0832c4f024D2427bBC6BD0C4931096d2ab5CCaF;//0x64F82571C326487cac31669F1e797EA0c9650879;
+    VerifyToken vrfcontract=VerifyToken(vrfAddress);
+    event BoughtToken(uint tokens,uint eth,address indexed to);
+    event SoldToken(uint tokens,uint eth,address indexed to);
+
     //Tokens are sold by sending them to this contract with ApproveAndCall
     function receiveApproval(address from, uint256 tokens, address token, bytes data) public{
-        //only allow this to be called from the token contract
-        require(msg.sender==tokenAddress);
+        //only allow this to be called from the token contract once activated
+        require(vrfcontract.activated());
+        require(msg.sender==vrfAddress);
         uint256 tokenValue=calculateTokenSell(tokens);
-        tokenContract.transferFrom(from,this,tokens);
+        vrfcontract.transferFrom(from,this,tokens);
         from.transfer(tokenValue);
+        emit SoldToken(tokens,tokenValue,from);
     }
     function buyTokens() public payable{
+        require(vrfcontract.activated());
         uint256 tokensBought=calculateTokenBuy(msg.value,SafeMath.sub(this.balance,msg.value));
-        tokenContract.transfer(msg.sender,tokensBought);
+        vrfcontract.transfer(msg.sender,tokensBought);
+        emit BoughtToken(tokensBought,msg.value,msg.sender);
     }
     //magic trade balancing algorithm
     function calculateTrade(uint256 rt,uint256 rs, uint256 bs) public view returns(uint256){
@@ -58,10 +57,10 @@ contract AutomatedExchange is ApproveAndCallFallBack{
         return SafeMath.div(SafeMath.mul(PSN,bs),SafeMath.add(PSNH,SafeMath.div(SafeMath.add(SafeMath.mul(PSN,rs),SafeMath.mul(PSNH,rt)),rt)));
     }
     function calculateTokenSell(uint256 tokens) public view returns(uint256){
-        return calculateTrade(tokens,tokenContract.balanceOf(this),this.balance);
+        return calculateTrade(tokens,vrfcontract.balanceOf(this),this.balance);
     }
     function calculateTokenBuy(uint256 eth,uint256 contractBalance) public view returns(uint256){
-        return calculateTrade(eth,contractBalance,tokenContract.balanceOf(this));
+        return calculateTrade(eth,contractBalance,vrfcontract.balanceOf(this));
     }
     function calculateTokenBuySimple(uint256 eth) public view returns(uint256){
         return calculateTokenBuy(eth,this.balance);
@@ -74,7 +73,7 @@ contract AutomatedExchange is ApproveAndCallFallBack{
         return this.balance;
     }
     function getTokenBalance() public view returns(uint256){
-        return tokenContract.balanceOf(this);
+        return vrfcontract.balanceOf(this);
     }
     function min(uint256 a, uint256 b) private pure returns (uint256) {
         return a < b ? a : b;
