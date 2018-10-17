9b029730a5855dc892e7cf84f9ef3d1b0bf8db22
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:11:31 +0700

    add contract DutchExchangeProxy

---
+++ DutchExchangeProxy.sol
@@ -0,0 +1,48 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DutchExchangeProxy at 0xe938e574afef9a75d8d6b6b5bd7dc44f3df1add9
+*/
+pragma solidity ^0.4.21;
+
+// File: @gnosis.pm/util-contracts/contracts/Proxy.sol
+
+/// @title Proxied - indicates that a contract will be proxied. Also defines storage requirements for Proxy.
+/// @author Alan Lu - <alan@gnosis.pm>
+contract Proxied {
+    address public masterCopy;
+}
+
+/// @title Proxy - Generic proxy contract allows to execute all transactions applying the code of a master contract.
+/// @author Stefan George - <stefan@gnosis.pm>
+contract Proxy is Proxied {
+    /// @dev Constructor function sets address of master copy contract.
+    /// @param _masterCopy Master copy address.
+    function Proxy(address _masterCopy)
+        public
+    {
+        require(_masterCopy != 0);
+        masterCopy = _masterCopy;
+    }
+
+    /// @dev Fallback function forwards all transactions and returns all received return data.
+    function ()
+        external
+        payable
+    {
+        address _masterCopy = masterCopy;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let success := delegatecall(not(0), _masterCopy, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch success
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+}
+
+// File: contracts/DutchExchangeProxy.sol
+
+contract DutchExchangeProxy is Proxy {
+  function DutchExchangeProxy(address _masterCopy) Proxy (_masterCopy) {
+  }
+}
