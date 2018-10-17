c793cbf1faa88936e2bfeec7e6d379b4479cae07
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:46:41 +0700

    add contract DigitalAdTrust2018Q2

---
+++ DigitalAdTrust2018Q2.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DigitalAdTrust2018Q2 at 0x08f58e6b4adedaf16e1f5d2566834cc14cada9b6
+*/
+pragma solidity 0.4.24;
+
+contract DigitalAdTrust2018Q2 {
+
+    string public constant DISPLAY_CLASSIQUE_ET_VIDEO = "01net.com,boursorama.com";
+
+    string public constant DISPLAY_CLASSIQUE = "20minutes.fr,cdiscount.com,courrier-picard.fr,dna.fr,estrepublicain.fr,lalsace.fr,lamontagne.fr,lanouvellerepublique.fr,larep.fr,leberry.fr,lechorepublicain.fr,ledauphine.com,lejdc.fr,lejsl.com,leparisien.fr,lepopulaire.fr,leprogres.fr,lesechos.fr,letelegramme.fr,leveil.fr,lindependant.fr,lunion.fr,lyonne.fr,midilibre.fr,monacomatin.mc,nicematin.com,nordlittoral.fr,passeportsante.net,varmatin.com,vosgesmatin.fr";
+
+    string public constant DISPLAY_VIDEO = "tf1.fr";
+
+}
