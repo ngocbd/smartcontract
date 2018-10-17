e5f95c98062c3b0403c158e4ce45833e176a0c26
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:04:23 +0700

    add contract NeoWorldCash

--- NeoWorldCash.sol
+++ NeoWorldCash.sol
@@ -1,84 +1,35 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NeoWorldCash at 0x0a255f700b16c0acf5673565c757b94fb38b27f2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NeoWorldCash at 0x4b94c8567763654101f690cf4d54957206383b75
 */
-pragma solidity ^0.4.21;
-library SafeMath {
+pragma solidity 0.4.21;
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
-    }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-/*
-* Contract that is working with ERC223 tokens
-*/
- 
-contract ContractReceiver {
-	function tokenFallback(address _from, uint _value, bytes _data) public pure {
-	}
-	function doTransfer(address _to, uint256 _index) public returns (uint256 price, address owner);
-}
-
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
 contract Ownable {
-	address public owner;
-	address public newOwner;
+    address public owner;
+    address public newOwner;
 
-	event OwnershipTransferred(address indexed _from, address indexed _to);
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 
-	function Ownable() public {
-		owner = msg.sender;
-	}
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
-	modifier onlyOwner {
-		require(msg.sender == owner);
-		_;
-	}
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-	function transferOwnership(address _newOwner) public onlyOwner {
-		newOwner = _newOwner;
-	}
-
-	function acceptOwnership() public {
-		require(msg.sender == newOwner);
-		emit OwnershipTransferred(owner, newOwner);
-		owner = newOwner;
-		newOwner = address(0);
-	}
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
 }
 
 contract Pausable is Ownable {
@@ -122,7 +73,71 @@ contract Pausable is Ownable {
 		return true;
 	}
 }
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
 
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+library ContractLib {
+	//assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+	function isContract(address _addr) internal view returns (bool) {
+		uint length;
+		assembly {
+			//retrieve the size of the code on target address, this needs assembly
+			length := extcodesize(_addr)
+		}
+		return (length>0);
+	}
+}
+
+/*
+* Contract that is working with ERC223 tokens
+*/
+ 
+contract ContractReceiver {
+	function tokenFallback(address _from, uint _value, bytes _data) public pure;
+}
 
 // ----------------------------------------------------------------------------
 // ERC Token Standard #20 Interface
@@ -165,6 +180,7 @@ contract ERC223 is ERC20Interface {
 contract NeoWorldCash is ERC223, Pausable {
 
 	using SafeMath for uint256;
+	using ContractLib for address;
 
 	mapping(address => uint) balances;
 	mapping(address => mapping(address => uint)) allowed;
@@ -208,7 +224,8 @@ contract NeoWorldCash is ERC223, Pausable {
 	
 	// Function that is called when a user or another contract wants to transfer funds .
 	function transfer(address _to, uint _value, bytes _data) public whenNotPaused returns (bool) {
-		if(isContract(_to)) {
+		require(_to != 0x0);
+		if(_to.isContract()) {
 			return transferToContract(_to, _value, _data);
 		}
 		else {
@@ -221,8 +238,10 @@ contract NeoWorldCash is ERC223, Pausable {
 	function transfer(address _to, uint _value) public whenNotPaused returns (bool) {
 		//standard function transfer similar to ERC20 transfer with no _data
 		//added due to backwards compatibility reasons
+		require(_to != 0x0);
+
 		bytes memory empty;
-		if(isContract(_to)) {
+		if(_to.isContract()) {
 			return transferToContract(_to, _value, empty);
 		}
 		else {
@@ -230,19 +249,10 @@ contract NeoWorldCash is ERC223, Pausable {
 		}
 	}
 
-	//assemble the given address bytecode. If bytecode exists then the _addr is a contract.
-	function isContract(address _addr) private view returns (bool) {
-		uint length;
-		assembly {
-			//retrieve the size of the code on target address, this needs assembly
-			length := extcodesize(_addr)
-		}
-		return (length>0);
-	}
+
 
 	//function that is called when transaction target is an address
 	function transferToAddress(address _to, uint _value, bytes _data) private returns (bool) {
-		if (balanceOf(msg.sender) < _value) revert();
 		balances[msg.sender] = balanceOf(msg.sender).sub(_value);
 		balances[_to] = balanceOf(_to).add(_value);
 		emit Transfer(msg.sender, _to, _value);
@@ -250,45 +260,28 @@ contract NeoWorldCash is ERC223, Pausable {
 		return true;
 	}
 	
-	//function that is called when transaction target is a contract
-	function transferToContract(address _to, uint _value, bytes _data) private returns (bool) {
-	
-		ContractReceiver receiver = ContractReceiver(_to);
-		uint256 price;
-		address owner;
-		(price, owner) = receiver.doTransfer(msg.sender, bytesToUint(_data));
-
-		if (balanceOf(msg.sender) < price) revert();
-		balances[msg.sender] = balanceOf(msg.sender).sub(price);
-		balances[owner] = balanceOf(owner).add(price);
-		receiver.tokenFallback(msg.sender, price, _data);
-		emit Transfer(msg.sender, _to, _value);
-		emit Transfer(msg.sender, _to, _value, _data);
-		return true;
+  //function that is called when transaction target is a contract
+  function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
+	    balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+	    balances[_to] = balanceOf(_to).add(_value);
+	    ContractReceiver receiver = ContractReceiver(_to);
+	    receiver.tokenFallback(msg.sender, _value, _data);
+	    emit Transfer(msg.sender, _to, _value);
+	    emit Transfer(msg.sender, _to, _value, _data);
+	    return true;
 	}
-
+	
 	function balanceOf(address _owner) public constant returns (uint) {
 		return balances[_owner];
 	}  
 
-	function burn(uint256 _value) public returns (bool) {
+	function burn(uint256 _value) public whenNotPaused returns (bool) {
 		require (_value > 0); 
 		require (balanceOf(msg.sender) >= _value);            // Check if the sender has enough
 		balances[msg.sender] = balanceOf(msg.sender).sub(_value);                      // Subtract from the sender
 		totalSupply = totalSupply.sub(_value);                                // Updates totalSupply
 		emit Burn(msg.sender, _value);
 		return true;
-	}
-
-	function bytesToUint(bytes b) private pure returns (uint result) {
-		uint i;
-		result = 0;
-		for (i = 0; i < b.length; i++) {
-			uint c = uint(b[i]);
-			if (c >= 48 && c <= 57) {
-				result = result * 10 + (c - 48);
-			}
-		}
 	}
 
 	// ------------------------------------------------------------------------
@@ -305,6 +298,24 @@ contract NeoWorldCash is ERC223, Pausable {
 		return true;
 	}
 
+	function increaseApproval (address _spender, uint _addedValue) public whenNotPaused
+	    returns (bool success) {
+	    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+	    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+	    return true;
+	}
+
+	function decreaseApproval (address _spender, uint _subtractedValue) public whenNotPaused
+	    returns (bool success) {
+	    uint oldValue = allowed[msg.sender][_spender];
+	    if (_subtractedValue > oldValue) {
+	      allowed[msg.sender][_spender] = 0;
+	    } else {
+	      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+	    }
+	    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+	    return true;
+	}	
 
 	// ------------------------------------------------------------------------
 	// Transfer `tokens` from the `from` account to the `to` account
@@ -343,5 +354,231 @@ contract NeoWorldCash is ERC223, Pausable {
 	// ------------------------------------------------------------------------
 	function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool) {
 		return ERC20Interface(tokenAddress).transfer(owner, tokens);
-	}	
+	}
+
+	/* RO Token ?? */
+	/*
+	????????
+	1. ???????ERC20 Token
+	2. ??Token????????????
+	3. ?????????
+	4. ?????? joinPreSale ????????????????????
+	5. ?????????Nash??
+	MISC?????????????MetaMask????????????????????
+	MISC????????????????
+	*/
+	address[] supportedERC20Token;
+	mapping (address => bool) tokenSupported;
+	mapping (address => uint256) prices;
+	mapping (address => uint256) starttime;
+	mapping (address => uint256) endtime;
+
+	uint256 maxTokenCountPerTrans = 10000;
+	uint256 nashInPool;
+
+	event AddSupportedToken(
+		address _address, 
+		uint256 _price, 
+		uint256 _startTime, 
+		uint256 _endTime);
+
+	event RemoveSupportedToken(
+		address _address
+	);
+
+	function addSupportedToken(
+		address _address, 
+		uint256 _price, 
+		uint256 _startTime, 
+		uint256 _endTime
+	) public onlyOwner returns (bool) {
+		
+		require(_address != 0x0);
+		require(_address.isContract());
+		require(_startTime < _endTime);
+		require(_price > 0);
+		require(_endTime > block.timestamp);
+
+		for (uint256 i = 0; i < supportedERC20Token.length; i++) {
+			require(supportedERC20Token[i] != _address);
+		}
+
+		supportedERC20Token.push(_address);
+		tokenSupported[_address] = true;
+		prices[_address] = _price;
+		starttime[_address] = _startTime;
+		endtime[_address] = _endTime;
+
+		emit AddSupportedToken(_address, _price, _startTime, _endTime);
+
+		return true;
+	}
+
+	function removeSupportedToken(address _address) public onlyOwner returns (bool) {
+		require(_address != 0x0);
+		uint256 length = supportedERC20Token.length;
+		for (uint256 i = 0; i < length; i++) {
+			if (supportedERC20Token[i] == _address) {
+				if (i != length - 1) {
+					supportedERC20Token[i] = supportedERC20Token[length - 1];
+				}
+                delete supportedERC20Token[length-1];
+				supportedERC20Token.length--;
+
+				prices[_address] = 0;
+				starttime[_address] = 0;
+				endtime[_address] = 0;
+				tokenSupported[_address] = false;
+
+				emit RemoveSupportedToken(_address);
+
+				break;
+			}
+		}
+		return true;
+	}
+
+	modifier canBuy(address _address) { 
+		require(tokenSupported[_address]);
+		require(block.timestamp > starttime[_address]);
+		require(block.timestamp < endtime[_address]);
+		_; 
+	}
+
+	function joinPreSale(address _tokenAddress, uint256 _tokenCount) public canBuy(_tokenAddress) returns (bool) {
+		require(prices[_tokenAddress] > 0);
+		uint256 total = _tokenCount.mul(prices[_tokenAddress]); // will not overflow here since the price will not be high
+		balances[msg.sender] = balances[msg.sender].sub(total);
+		nashInPool = nashInPool.add(total);
+
+		require(ERC20Interface(_tokenAddress).transfer(msg.sender, _tokenCount));
+		emit Transfer(msg.sender, this, total);
+
+		return true;
+	}
+
+	function transferNashOut(address _to, uint256 count) public onlyOwner returns(bool) {
+		require(_to != 0x0);
+		nashInPool = nashInPool.sub(count);
+		balances[_to] = balances[_to].add(count);
+
+		emit Transfer(this, _to, count);
+
+		return true;
+	}
+
+	function getSupportedTokens() public view returns (address[]) {
+		return supportedERC20Token;
+	}
+
+	function getTokenStatus(address _tokenAddress) public view returns (uint256 _starttime, uint256 _endtime, uint256 _price) {
+		_starttime = starttime[_tokenAddress];
+		_endtime = endtime[_tokenAddress];
+		_price = prices[_tokenAddress];
+	}
+
+	/* end of ???? */
+
+	/* ???? */ 
+	// ???????????????????????????
+
+	mapping(address => uint256) lockedBalanceTotal;
+	mapping(address => uint256) lockedStartTime;
+	mapping(address => uint256) unlockPeriod;
+	mapping(address => uint256) unlockNumberOfCycles;
+
+	mapping(address => uint256) lockedBalanceRemains;
+	mapping(address => uint256) cyclesUnlocked;
+
+	mapping(address => bool) addressAllowToLock;
+
+	event Locked (address _address, uint256 _count, uint256 _starttime, uint256 _unlockPeriodInSeconds, uint256 _unlockNumberOfCycles);
+	event Unlocked (address _address, uint256 _count);
+
+	function allowToLock(address _address) public onlyOwner {
+		require(_address != 0x0);
+		addressAllowToLock[_address] = true;
+	}
+
+	function disallowToLock(address _address) public onlyOwner {
+		require(_address != 0x0);
+		addressAllowToLock[_address] = false;
+	}
+
+	function lock(uint256 _count, uint256 _starttime, uint256 _unlockPeriodInSeconds, uint256 _unlockNumberOfCycles) public returns (bool) {
+		require(addressAllowToLock[msg.sender]);
+		require(lockedStartTime[msg.sender] == 0);
+		require(0 < _unlockNumberOfCycles && _unlockNumberOfCycles <= 10); 
+		require(_unlockPeriodInSeconds > 0); 
+		require(_count > 10000);
+		require(_starttime > 0);
+
+		balances[msg.sender] = balances[msg.sender].sub(_count);
+
+		lockedBalanceTotal[msg.sender] = _count;
+		lockedStartTime[msg.sender] = _starttime;
+		unlockPeriod[msg.sender] = _unlockPeriodInSeconds;
+		unlockNumberOfCycles[msg.sender] = _unlockNumberOfCycles;
+
+		lockedBalanceRemains[msg.sender] = lockedBalanceTotal[msg.sender];
+
+		emit Locked (msg.sender, _count, _starttime, _unlockPeriodInSeconds, _unlockNumberOfCycles);
+
+		return true;
+	}
+
+	function tryUnlock() public returns (bool) {
+		require(lockedBalanceRemains[msg.sender] > 0);
+		uint256 cycle = (block.timestamp.sub(lockedStartTime[msg.sender])) / unlockPeriod[msg.sender];
+		require(cycle > cyclesUnlocked[msg.sender]);
+
+		if (cycle > unlockNumberOfCycles[msg.sender]) {
+			cycle = unlockNumberOfCycles[msg.sender];
+		}
+
+		uint256 amount = lockedBalanceTotal[msg.sender].mul(cycle - cyclesUnlocked[msg.sender]) / unlockNumberOfCycles[msg.sender] ;
+		lockedBalanceRemains[msg.sender] = lockedBalanceRemains[msg.sender].sub(amount);
+		balances[msg.sender] = balances[msg.sender].add(amount);
+
+		if (cycle == unlockNumberOfCycles[msg.sender]) {
+			// cleanup
+			lockedBalanceTotal[msg.sender] = 0;
+			lockedStartTime[msg.sender] = 0;
+			unlockPeriod[msg.sender] = 0;
+			unlockNumberOfCycles[msg.sender] = 0;
+			cyclesUnlocked[msg.sender] = 0;
+
+			if (lockedBalanceRemains[msg.sender] > 0) {
+				balances[msg.sender] = balances[msg.sender].add(lockedBalanceRemains[msg.sender]);
+				lockedBalanceRemains[msg.sender] = 0;
+			}
+
+		}
+		else {
+			cyclesUnlocked[msg.sender] = cycle;
+		}
+
+		emit Unlocked(msg.sender, amount);
+
+		return true;
+	}
+
+	function getLockStatus(address _address) public view returns (
+		uint256 _lockTotal, 
+		uint256 _starttime, 
+		uint256 _unlockPeriodInSeconds,
+		uint256 _unlockNumberOfCycles,
+		uint256 _lockedBalanceRemains,
+		uint256 _cyclesUnlocked 
+		 ) {
+
+		_lockTotal = lockedBalanceTotal[_address];
+		_starttime = lockedStartTime[_address];
+		_unlockPeriodInSeconds = unlockPeriod[_address];
+		_unlockNumberOfCycles = unlockNumberOfCycles[_address];
+		_lockedBalanceRemains = lockedBalanceRemains[_address];
+		_cyclesUnlocked = cyclesUnlocked[_address];
+
+	}
+
 }
