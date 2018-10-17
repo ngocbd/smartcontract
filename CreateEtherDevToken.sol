58eb6f082f85bfa0a42e35cdbd9821e14891b106
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:45:06 +0700

    add contract CreateEtherDevToken

---
+++ CreateEtherDevToken.sol
@@ -0,0 +1,31 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CreateEtherDevToken at 0xd08b5ead3c6b9873d51c068e93988aa58d24a3aa
+*/
+pragma solidity ^0.4.24;
+
+    contract CreateEtherDevToken {
+        mapping (address => uint256) public balanceOf;
+        
+        string public name = "EtherDev";
+        string public symbol = "EDEV";
+        uint8 public decimals = 18;
+
+    uint256 public totalSupply = 10000 * (uint256(10) ** decimals);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function SendTokens() public {
+        // Initially assign all tokens to the contract's creator.
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+    
+	function transfer(address to, uint256 value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= value);
+
+        balanceOf[msg.sender] -= value;  // deduct from sender's balance
+        balanceOf[to] += value;          // add to recipient's balance
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+}
