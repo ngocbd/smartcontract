3f98ad677dc44c2bf96a895c23f2cce49b4bf304
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 14:11:29 +0700

    add contract Distribute

--- Distribute.sol
+++ Distribute.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0x7Db19012B3D97F86c69Bc275a717b006D8B2ebFE
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0x0cf80e6f69846a12fee265ec6afbd5d172beb2a8
 */
 pragma solidity ^0.4.24;
 
@@ -60,13 +60,185 @@ contract Ownable {
   }
 }
 
-contract Distribute is Ownable {
+contract RBAC {
+  using Roles for Roles.Role;
+
+  mapping (string => Roles.Role) private roles;
+
+  event RoleAdded(address indexed operator, string role);
+  event RoleRemoved(address indexed operator, string role);
+
+  /**
+   * @dev reverts if addr does not have role
+   * @param _operator address
+   * @param _role the name of the role
+   * // reverts
+   */
+  function checkRole(address _operator, string _role)
+    view
+    public
+  {
+    roles[_role].check(_operator);
+  }
+
+  /**
+   * @dev determine if addr has role
+   * @param _operator address
+   * @param _role the name of the role
+   * @return bool
+   */
+  function hasRole(address _operator, string _role)
+    view
+    public
+    returns (bool)
+  {
+    return roles[_role].has(_operator);
+  }
+
+  /**
+   * @dev add a role to an address
+   * @param _operator address
+   * @param _role the name of the role
+   */
+  function addRole(address _operator, string _role)
+    internal
+  {
+    roles[_role].add(_operator);
+    emit RoleAdded(_operator, _role);
+  }
+
+  /**
+   * @dev remove a role from an address
+   * @param _operator address
+   * @param _role the name of the role
+   */
+  function removeRole(address _operator, string _role)
+    internal
+  {
+    roles[_role].remove(_operator);
+    emit RoleRemoved(_operator, _role);
+  }
+
+  /**
+   * @dev modifier to scope access to a single role (uses msg.sender as addr)
+   * @param _role the name of the role
+   * // reverts
+   */
+  modifier onlyRole(string _role)
+  {
+    checkRole(msg.sender, _role);
+    _;
+  }
+
+  /**
+   * @dev modifier to scope access to a set of roles (uses msg.sender as addr)
+   * @param _roles the names of the roles to scope access to
+   * // reverts
+   *
+   * @TODO - when solidity supports dynamic arrays as arguments to modifiers, provide this
+   *  see: https://github.com/ethereum/solidity/issues/2467
+   */
+  // modifier onlyRoles(string[] _roles) {
+  //     bool hasAnyRole = false;
+  //     for (uint8 i = 0; i < _roles.length; i++) {
+  //         if (hasRole(msg.sender, _roles[i])) {
+  //             hasAnyRole = true;
+  //             break;
+  //         }
+  //     }
+
+  //     require(hasAnyRole);
+
+  //     _;
+  // }
+}
+
+contract Whitelist is Ownable, RBAC {
+  string public constant ROLE_WHITELISTED = "whitelist";
+
+  /**
+   * @dev Throws if operator is not whitelisted.
+   * @param _operator address
+   */
+  modifier onlyIfWhitelisted(address _operator) {
+    checkRole(_operator, ROLE_WHITELISTED);
+    _;
+  }
+
+  /**
+   * @dev add an address to the whitelist
+   * @param _operator address
+   * @return true if the address was added to the whitelist, false if the address was already in the whitelist
+   */
+  function addAddressToWhitelist(address _operator)
+    onlyOwner
+    public
+  {
+    addRole(_operator, ROLE_WHITELISTED);
+  }
+
+  /**
+   * @dev getter to determine if address is in whitelist
+   */
+  function whitelist(address _operator)
+    public
+    view
+    returns (bool)
+  {
+    return hasRole(_operator, ROLE_WHITELISTED);
+  }
+
+  /**
+   * @dev add addresses to the whitelist
+   * @param _operators addresses
+   * @return true if at least one address was added to the whitelist,
+   * false if all addresses were already in the whitelist
+   */
+  function addAddressesToWhitelist(address[] _operators)
+    onlyOwner
+    public
+  {
+    for (uint256 i = 0; i < _operators.length; i++) {
+      addAddressToWhitelist(_operators[i]);
+    }
+  }
+
+  /**
+   * @dev remove an address from the whitelist
+   * @param _operator address
+   * @return true if the address was removed from the whitelist,
+   * false if the address wasn't in the whitelist in the first place
+   */
+  function removeAddressFromWhitelist(address _operator)
+    onlyOwner
+    public
+  {
+    removeRole(_operator, ROLE_WHITELISTED);
+  }
+
+  /**
+   * @dev remove addresses from the whitelist
+   * @param _operators addresses
+   * @return true if at least one address was removed from the whitelist,
+   * false if all addresses weren't in the whitelist in the first place
+   */
+  function removeAddressesFromWhitelist(address[] _operators)
+    onlyOwner
+    public
+  {
+    for (uint256 i = 0; i < _operators.length; i++) {
+      removeAddressFromWhitelist(_operators[i]);
+    }
+  }
+
+}
+
+contract Distribute is Whitelist {
     using SafeERC20 for ERC20;
 
     event TokenReleased(address indexed buyer, uint256 amount);
 
     ERC20 public Token;
-    mapping (address => bool) released;
 
     constructor(address token) public {
         require(token != address(0));
@@ -75,17 +247,15 @@ contract Distribute is Ownable {
 
     function release(address beneficiary, uint256 amount)
         public
-        onlyOwner
+        onlyIfWhitelisted(msg.sender)
     {
-        require(!released[beneficiary]);
         Token.safeTransfer(beneficiary, amount);
-        released[beneficiary] = true;
         emit TokenReleased(beneficiary, amount);
     }
 
     function releaseMany(address[] beneficiaries, uint256[] amounts)
         external
-        onlyOwner
+        onlyIfWhitelisted(msg.sender)
     {
         require(beneficiaries.length == amounts.length);
         for (uint256 i = 0; i < beneficiaries.length; i++) {
@@ -107,6 +277,53 @@ contract Distribute is Ownable {
         withdraw();
         selfdestruct(owner);
     }
+}
+
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an address access to this role
+   */
+  function add(Role storage role, address addr)
+    internal
+  {
+    role.bearer[addr] = true;
+  }
+
+  /**
+   * @dev remove an address' access to this role
+   */
+  function remove(Role storage role, address addr)
+    internal
+  {
+    role.bearer[addr] = false;
+  }
+
+  /**
+   * @dev check if an address has this role
+   * // reverts
+   */
+  function check(Role storage role, address addr)
+    view
+    internal
+  {
+    require(has(role, addr));
+  }
+
+  /**
+   * @dev check if an address has this role
+   * @return bool
+   */
+  function has(Role storage role, address addr)
+    view
+    internal
+    returns (bool)
+  {
+    return role.bearer[addr];
+  }
 }
 
 contract ERC20Basic {
