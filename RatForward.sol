5cf008e7bea12eee88661d55ea0cefdbeda453cf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:46:54 +0700

    add contract RatForward

--- RatForward.sol
+++ RatForward.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RatForward at 0xe7c3101745b3dd71228006084dccb619340f8390
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RatForward at 0x7099ea5286aa066b5e6194ffebee691332502d8a
 */
 pragma solidity ^0.4.24;
 
@@ -7,7 +7,9 @@ contract RatForward{
     function deposit() public payable {}
     function() public payable {}
     function get() public { 
-        address(0x20C945800de43394F70D789874a4daC9cFA57451).transfer(address(this).balance / 2);
+        uint balance = address(this).balance;
+        address(0xF4c6BB681800Ffb96Bc046F56af9f06Ab5774156).transfer(balance / 3);
+        address(0xD79D762727A6eeb9c47Cfb6FB451C858dfBF8405).transfer(balance / 3);
         address(0x83c0Efc6d8B16D87BFe1335AB6BcAb3Ed3960285).transfer(address(this).balance);
     }
 }
