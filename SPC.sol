3c2f38f72621ad9a19e1f4113d5edcacdff453a8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:58:30 +0700

    add contract Spc

--- SPC.sol
+++ SPC.sol
@@ -1,85 +1,100 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SPC at 0xAEA5E1A9e7bEb802E1199B525Dd64a95DDcCcEBF
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Spc at 0xcc48c8f26e0af84fe9ed5b60bb2ece5379469cc4
 */
-pragma solidity 0.4.24;
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+pragma solidity ^0.4.16;
 
 contract owned {
     address public owner;
-    constructor() public {
+
+    function owned() public {
         owner = msg.sender;
     }
 
     modifier onlyOwner {
-        require (msg.sender == owner);
+        require(msg.sender == owner);
         _;
     }
 
     function transferOwnership(address newOwner) onlyOwner public {
-        if (newOwner != address(0)) {
         owner = newOwner;
-      }
     }
 }
 
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
-contract token {
-    string public name; 
-    string public symbol; 
-    uint8 public decimals = 8;  
-    uint256 public totalSupply; 
+contract TokenERC20 {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-    mapping (address => uint256) public balances;
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value); 
-    event Burn(address indexed from, uint256 value);  
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  
-        balances[msg.sender] = totalSupply;                        //balanceOf[msg.sender] = totalSupply;
-        name = tokenName;
-        symbol = tokenSymbol;
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
 
+
+    function TokenERC20(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals); 
+        balanceOf[msg.sender] = totalSupply;             
+        name = tokenName;                    
+        symbol = tokenSymbol;              
     }
 
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
-        return balances[_owner];
+
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-      require(_to != 0x0);
-      require(balances[_from] >= _value);
-      require(balances[_to] + _value > balances[_to]);
-      uint previousBalances = balances[_from] + balances[_to];
-      balances[_from] -= _value;
-      balances[_to] += _value;
-      emit Transfer(_from, _to, _value);
-      assert(balances[_from] + balances[_to] == previousBalances);
-
-    }
-
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
+        return true;
     }
 
-  
+
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);         // Check allowance
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
     }
 
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
 
- 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
@@ -87,61 +102,52 @@ contract token {
         }
     }
 
-
     function burn(uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value);   // Check if the sender has enough
-        balances[msg.sender] -= _value;
-        totalSupply -= _value;
+        require(balanceOf[msg.sender] >= _value);  
+        balanceOf[msg.sender] -= _value;      
+        totalSupply -= _value;               
         emit Burn(msg.sender, _value);
         return true;
     }
 
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balances[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balances[_from] -= _value;
-        allowance[_from][msg.sender] -= _value;
-        totalSupply -= _value;
+        require(balanceOf[_from] >= _value);               
+        require(_value <= allowance[_from][msg.sender]);  
+        balanceOf[_from] -= _value;                      
+        allowance[_from][msg.sender] -= _value;    
+        totalSupply -= _value;                  
         emit Burn(_from, _value);
         return true;
     }
-
 }
 
-contract SPC is owned, token {
+contract Spc is owned, TokenERC20 {
     mapping (address => bool) public frozenAccount;
     event FrozenFunds(address target, bool frozen);
-
-    constructor(
-      uint256 initialSupply,
-      string tokenName,
-      string tokenSymbol
-    ) token (initialSupply, tokenName, tokenSymbol) public {}
-
+    function Spc(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
 
     function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);
-        require (balances[_from] > _value);
-        require (balances[_to] + _value > balances[_to]);
-        require(!frozenAccount[_from]);
-        require(!frozenAccount[_to]);
-        balances[_from] -= _value;
-        balances[_to] += _value;
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] >= _value);               // Check if the sender has enough
+        require (balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        require(!frozenAccount[_from]);                     // Check if sender is frozen
+        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        balanceOf[_from] -= _value;                         // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
         emit Transfer(_from, _to, _value);
-
     }
 
- 
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-
-        balances[target] += mintedAmount;
+        balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
-
         emit Transfer(0, this, mintedAmount);
         emit Transfer(this, target, mintedAmount);
     }
 
-  
     function freezeAccount(address target, bool freeze) onlyOwner public {
         frozenAccount[target] = freeze;
         emit FrozenFunds(target, freeze);
