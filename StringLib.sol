160e7bed0eeed92f92ca45416af576ef80884b1a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:07:05 +0700

    add contract StringLib

--- StringLib.sol
+++ StringLib.sol
@@ -1,64 +1,22 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract StringLib at 0xcca8353a18e7ab7b3d094ee1f9ddc91bdf2ca6a4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract StringLib at 0x7a3471c362b1b3274d6b3f254e57185ffe94788a
 */
-// String Utils v0.1
+pragma solidity ^0.4.24;
 
-/// @title String Utils - String utility functions
-/// @author Piper Merriam - <pipermerriam@gmail.com>
 library StringLib {
-    /// @dev Converts an unsigned integert to its string representation.
-    /// @param v The number to be converted.
-    function uintToBytes(uint v) constant returns (bytes32 ret) {
-        if (v == 0) {
-            ret = '0';
-        }
-        else {
-            while (v > 0) {
-                ret = bytes32(uint(ret) / (2 ** 8));
-                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
-                v /= 10;
-            }
-        }
-        return ret;
+  function generateName(bytes16 _s, uint256 _len, uint256 _n)
+    public
+    pure
+    returns (bytes16 ret)
+  {
+    uint256 v = _n;
+    bytes16 num = 0;
+    while (v > 0) {
+      num = bytes16(uint(num) / (2 ** 8));
+      num |= bytes16(((v % 10) + 48) * 2 ** (8 * 15));
+      v /= 10;
     }
-
-    /// @dev Converts a numeric string to it's unsigned integer representation.
-    /// @param v The string to be converted.
-    function bytesToUInt(bytes32 v) constant returns (uint ret) {
-        if (v == 0x0) {
-            throw;
-        }
-
-        uint digit;
-
-        for (uint i = 0; i < 32; i++) {
-            digit = uint((uint(v) / (2 ** (8 * (31 - i)))) & 0xff);
-            if (digit == 0) {
-                break;
-            }
-            else if (digit < 48 || digit > 57) {
-                throw;
-            }
-            ret *= 10;
-            ret += (digit - 48);
-        }
-        return ret;
-    }
-}
-
-
-/// @title String Utils - String utility functions
-/// @author Piper Merriam - <pipermerriam@gmail.com>
-library StringUtils {
-    /// @dev Converts an unsigned integert to its string representation.
-    /// @param v The number to be converted.
-    function uintToBytes(uint v) constant returns (bytes32 ret) {
-            return StringLib.uintToBytes(v);
-    }
-
-    /// @dev Converts a numeric string to it's unsigned integer representation.
-    /// @param v The string to be converted.
-    function bytesToUInt(bytes32 v) constant returns (uint ret) {
-            return StringLib.bytesToUInt(v);
-    }
+    ret = _s | bytes16(uint(num) / (2 ** (8 * _len)));
+    return ret;
+  }
 }
