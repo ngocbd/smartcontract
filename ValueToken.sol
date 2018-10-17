440fe1ebbcd37a8559d6fd7749a0aace982dc771
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:41:56 +0700

    add contract ValueToken

--- ValueToken.sol
+++ ValueToken.sol
@@ -1,154 +1,129 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ValueToken at 0xfe81b2bb25c34dfb2a023d1378a5f41759973067
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ValueToken at 0xd7ec1b25fa39292acc12958ed6b08b611547bdb6
 */
-pragma solidity ^0.4.14;
+pragma solidity ^0.4.18;
 
-contract ValueToken {
-    string public symbol;
-    string public name;
-    uint8 public decimals;
-    uint256 _totalSupply;
-    uint256 _value;
+library SafeMath {
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
 
-    // Owner of this contract
-    address public owner;
-    address public centralBank;
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b > 0); // Solidity automatically throws when dividing by 0
+		uint256 c = a / b;
+		assert(a == b * c + a % b); // There is no case in which this doesn't hold
+		return c;
+	}
 
-    // Balances for each account
-    mapping(address => uint256) balances;
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
 
-    // Owner of account approves the transfer of an amount to another account
-    mapping(address => mapping (address => uint256)) allowed;
+	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
+}
 
-    // Constructor
-    function ValueToken() {
-        name = "Cyber Turtle Token";
-        symbol = "CTT";
-        decimals = 2;
-        _totalSupply = 164500;
-        _value = 1118;
-        centralBank = 0x77E370640B43a8A8Bf68C21fD068E312c89321eE;
+contract Ownable {
+	address public owner;
+
+	event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+	
+	function Ownable() public {
         owner = msg.sender;
-        balances[owner] = _totalSupply;
     }
 
-    function totalSupply() constant returns (uint256 supply) {
-        return _totalSupply;
-    }
+	modifier onlyOwner() {
+		require(msg.sender == owner);
+		_;
+	}
+	
+	function transferOwnership(address newOwner) public onlyOwner {
+		require(newOwner != address(0));
+		OwnershipTransferred(owner, newOwner);
+		owner = newOwner;
+	}
+}
 
-    function value() constant returns (uint256 returnValue) {
-        return _value;
-    }
+contract ERC20 {
+	uint public totalSupply;
+	function balanceOf(address _owner) public constant returns (uint balance);
+	function transfer(address _to,uint _value) public returns (bool success);
+	function transferFrom(address _from,address _to,uint _value) public returns (bool success);
+	function approve(address _spender,uint _value) public returns (bool success);
+	function allownce(address _owner,address _spender) public constant returns (uint remaining);
+	event Transfer(address indexed _from,address indexed _to,uint _value);
+	event Approval(address indexed _owner,address indexed _spender,uint _value);
+}
 
-    // What is the balance of a particular account?
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
+contract ValueToken is ERC20,Ownable {
+	using SafeMath for uint8;
+	using SafeMath for uint256;
 
-    // Transfer the balance from owner's account to another account
-    function transfer(address _to, uint256 _amount) returns (bool success) {
-        if (balances[msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
-            balances[msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(msg.sender, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	string public name;
+	string public symbol;
+	uint8 public decimals;
+	mapping (address => uint256) public balances;
+	mapping (address => mapping (address => uint256)) allowed;
+	
+	function ValueToken () public {
+		name = 'ValueToken';
+		symbol = 'VAT';
+		decimals = 18;
+		totalSupply = 10000000000 * (10 ** 18);
+		balances[msg.sender] = totalSupply;
+	}
+	
+	function balanceOf(address _owner) public constant returns (uint balance) {
+		return balances[_owner];
+	}
+	
+	function transfer(address _to,uint _value) public returns (bool success) {
+		if(balances[msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]){
+			balances[msg.sender] = balances[msg.sender].sub(_value);
+			balances[_to] = balances[_to].add(_value);
+			Transfer(msg.sender,_to,_value);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    // Send _value amount of tokens from address _from to address _to
-    // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-    // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-    // fees in sub-currencies; the command should fail unless the _from account has
-    // deliberately authorized the sender of the message via some mechanism; we propose
-    // these standardized APIs for approval:
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) returns (bool success) {
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
-            balances[_from] -= _amount;
-            allowed[_from][msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(_from, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	function transferFrom(address _from,address _to,uint _value) public returns (bool success) {
+		if(balances[_from] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+			if(_from != msg.sender) {
+				require(allowed[_from][msg.sender] > _value);
+				allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+			}
+			balances[_from] = balances[_from].sub(_value);
+			balances[_to] = balances[_to].add(_value);
+			Transfer(_from,_to,_value);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    // Ownership
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address _newOwner) onlyOwner {
-        owner = _newOwner;
-    }
-
-    function transferCentralBanking(address _newCentralBank) onlyOwner {
-        centralBank = _newCentralBank;
-    }
-
-    // Central bank functions
-    modifier onlyCentralBank {
-        require(msg.sender == centralBank);
-        _;
-    }
-
-    function mint(uint256 _amount) onlyCentralBank {
-        balances[owner] += _amount;
-        _totalSupply += _amount;
-        Transfer(0, this, _amount);
-        Transfer(this, owner, _amount);
-    }
-
-    function burn(uint256 _amount) onlyCentralBank {
-        require (balances[owner] >= _amount);
-        balances[owner] -= _amount;
-        _totalSupply -= _amount;
-        Transfer(owner, this, _amount);
-        Transfer(this, 0, _amount);
-    }
-
-    function updateValue(uint256 _newValue) onlyCentralBank {
-        require(_newValue >= 0);
-        _value = _newValue;
-    }
-
-    function updateValueAndMint(uint256 _newValue, uint256 _toMint) onlyCentralBank {
-        require(_newValue >= 0);
-        _value = _newValue;
-        mint(_toMint);
-    }
-
-    function updateValueAndBurn(uint256 _newValue, uint256 _toBurn) onlyCentralBank {
-        require(_newValue >= 0);
-        _value = _newValue;
-        burn(_toBurn);
-    }
-
-    // Events
-    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
+	function approve(address _spender, uint _value) public returns (bool success) {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender,_spender,_value);
+		return true;
+	}
+	
+	function allownce(address _owner,address _spender) public constant returns (uint remaining) {
+		return allowed[_owner][_spender];
+	}
+	
+	function multisend(address[] _dests, uint256[] _values) public returns (bool success) {
+		require(_dests.length == _values.length);
+		for(uint256 i = 0; i < _dests.length; i++) {
+			if( !transfer(_dests[i], _values[i]) ) return false;
+		}
+		return true;
+	}
 }
