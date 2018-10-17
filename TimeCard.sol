bad4c5e5fa5ac417a19484af9a6aa790c9ca2820
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:55:47 +0700

    add contract TimeCard

---
+++ TimeCard.sol
@@ -0,0 +1,30 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TimeCard at 0x536a32D5D8199D1BFd0737c277229f58F849b428
+*/
+pragma solidity ^0.4.23;
+
+contract TimeCard {
+    string public suicaId;
+    uint[] public timeList;
+    uint[] public workTimeList;
+    
+    constructor(string mySuicaId) public {
+        suicaId = mySuicaId;
+    }
+    
+    function setTimeStamp(string mySuicaId, uint timeStamp) public {
+        require(keccak256(abi.encodePacked(suicaId)) == keccak256(abi.encodePacked(mySuicaId)));
+        timeList.push(timeStamp);
+        if((timeList.length % 2 ) == 0 ) {
+            uint startTime = timeList[timeList.length -2];
+            uint endTime = timeList[timeList.length -1];
+            uint workTime = getWorkTime(startTime, endTime);
+            workTimeList.push(workTime);
+        }
+    }
+
+    function getWorkTime(uint startTime, uint endTime) internal pure returns(uint){
+        uint workTime = endTime - startTime;
+        return workTime;
+    }
+}
