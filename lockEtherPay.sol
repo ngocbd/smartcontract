53506ff12a3f2233937e3f35ca9b0485a6c608b8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:18:04 +0700

    add contract lockEtherPay

--- lockEtherPay.sol
+++ lockEtherPay.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract lockEtherPay at 0xcd669efd2d04fedd465be02dd1337dea9b0b3541
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract lockEtherPay at 0xa8f77c38c64a2694fcfbb936cc3931e6eeed8e51
 */
 pragma solidity ^0.4.18;
 
@@ -74,13 +74,13 @@ contract lockEtherPay is Ownable {
   bool public isReleased = false;
   uint256 public start_time;
   uint256 public end_time;
-  uint256 public fifty_two_weeks = 28166400;
+  uint256 public fifty_two_weeks = 27820800;
 
   event TokenReleased(address beneficiary, uint256 token_amount);
 
   constructor() public{
     token_reward = token(0xAa1ae5e57dc05981D83eC7FcA0b3c7ee2565B7D6);
-    beneficiary = 0xf285D6253eB0863430024EEbedF4C2a09D3c5EdB;
+    beneficiary = 0x1C252CFeB89Cf1e41Cb0B812c3abFb5bc31f8703;
   }
 
   function tokenBalance() constant public returns (uint256){
