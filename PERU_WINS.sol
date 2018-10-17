92d11f4eaee7313395c3f083a3d883c47f8ec6d4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:27:34 +0700

    add contract PERU_WINS

--- PERU_WINS.sol
+++ PERU_WINS.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PERU_WINS at 0xf03f87c82f30357099bde7d2059dad7fff94059f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PERU_WINS at 0x58006ec6efadceb1bbd806d0b9b6e6bc674df8c4
 */
 pragma solidity 		^0.4.21	;						
 									
@@ -8,10 +8,10 @@ contract	PERU_WINS				{
 	mapping (address => uint256) public balanceOf;								
 									
 	string	public		name =	"	PERU_WINS		"	;
-	string	public		symbol =	"	PERWI		"	;
+	string	public		symbol =	"	PERWII		"	;
 	uint8	public		decimals =		18			;
 									
-	uint256 public totalSupply =		1743148604647560000000000000					;	
+	uint256 public totalSupply =		1755731937980890000000000000					;	
 									
 	event Transfer(address indexed from, address indexed to, uint256 value);								
 									
