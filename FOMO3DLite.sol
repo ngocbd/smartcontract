bf80c21b7312e38a913672302ba1832f867d5a71
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:11:46 +0700

    add contract FOMO3DLite

--- FOMO3DLite.sol
+++ FOMO3DLite.sol
@@ -1,11 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FOMO3DLite at 0x1f97242c26c6dc70d031734cf2a14833946acae5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FOMO3DLite at 0x00ad1ae81accb071bdf654633727e6ecda5672b3
 */
-pragma solidity 0.4 .19;
+pragma solidity 0.4.19;
 
 contract FOMO3DLite {
-    using SafeMath
-    for uint256;
+    using SafeMath for uint256;
 
     event NewRound(
         uint _timestamp,
@@ -53,28 +52,29 @@ contract FOMO3DLite {
     uint public constant BASE_DURATION = 1 days;
 
     // Amount by which the countdown duration decreases per ether in the pot
-    uint public constant DURATION_DECREASE_PER_ETHER = 2 minutes;
+    uint public constant DURATION_DECREASE_PER_ETHER = 1 minutes;
 
     // Minimum countdown duration
     uint public constant MINIMUM_DURATION = 30 minutes;
 
+
+
     // Minimum fraction of the pot required by a bidder to become the new leader
     uint public constant MIN_LEADER_FRAC_TOP = 1;
-    uint public constant MIN_LEADER_FRAC_BOT = 100000;
+    uint public constant MIN_LEADER_FRAC_BOT = 1000000;
 
     // Fraction of each bid put into the dividend fund
-    uint public constant DIVIDEND_FUND_FRAC_TOP = 45;
+    uint public constant DIVIDEND_FUND_FRAC_TOP = 40;
     uint public constant DIVIDEND_FUND_FRAC_BOT = 100;
 
-    uint public constant FRAC_TOP = 15;
-    uint public constant FRAC_BOT = 100;
+    // Fraction of each bid taken for the developer fee
+    uint public constant DEVELOPER_FEE_FRAC_TOP = 15;
+    uint public constant DEVELOPER_FEE_FRAC_BOT = 100;
 
-    // Fraction of the previous pot used to seed the next pot
-    uint public constant NEXT_POT_FRAC_TOP = 1;
-    uint public constant NEXT_POT_FRAC_BOT = 2;
+    // Owner of the contract
+    address owner;
 
     // Mapping from addresses to amounts earned
-    address _null;
     mapping(address => uint) public earnings;
 
     // Mapping from addresses to dividend shares
@@ -82,8 +82,6 @@ contract FOMO3DLite {
 
     // Total number of dividend shares
     uint public totalDividendShares;
-
-    address owner;
 
     // Value of the dividend fund
     uint public dividendFund;
@@ -102,19 +100,16 @@ contract FOMO3DLite {
 
     function FOMO3DLite() public payable {
         require(msg.value > 0);
-        _null = msg.sender;
+        owner = msg.sender;
         round = 1;
         pot = msg.value;
-        leader = _null;
-        totalDividendShares = 200000;
-        dividendShares[_null] = 200000;
+        leader = owner;
         deadline = computeDeadline();
         NewRound(now, round, pot);
         NewLeader(now, leader, pot, deadline);
-        owner = msg.sender;
     }
 
-    function computeDeadline() internal view returns(uint) {
+    function computeDeadline() internal view returns (uint) {
         uint _durationDecrease = DURATION_DECREASE_PER_ETHER.mul(pot.div(1 ether));
         uint _duration;
         if (MINIMUM_DURATION.add(_durationDecrease) > BASE_DURATION) {
@@ -127,11 +122,12 @@ contract FOMO3DLite {
 
     modifier advanceRoundIfNeeded {
         if (now > deadline) {
-            uint _nextPot = pot.mul(NEXT_POT_FRAC_TOP).div(NEXT_POT_FRAC_BOT);
+            uint _nextPot = 0;
             uint _leaderEarnings = pot.sub(_nextPot);
             Winner(now, leader, _leaderEarnings, deadline);
             earnings[leader] = earnings[leader].add(_leaderEarnings);
-            pot = 0;
+            round++;
+            pot = _nextPot;
             leader = owner;
             deadline = computeDeadline();
             NewRound(now, round, pot);
@@ -140,18 +136,13 @@ contract FOMO3DLite {
         _;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
     function bid() public payable advanceRoundIfNeeded {
         uint _minLeaderAmount = pot.mul(MIN_LEADER_FRAC_TOP).div(MIN_LEADER_FRAC_BOT);
-        uint _bidAmountToCommunity = msg.value.mul(FRAC_TOP).div(FRAC_BOT);
+        uint _bidAmountToDeveloper = msg.value.mul(DEVELOPER_FEE_FRAC_TOP).div(DEVELOPER_FEE_FRAC_BOT);
         uint _bidAmountToDividendFund = msg.value.mul(DIVIDEND_FUND_FRAC_TOP).div(DIVIDEND_FUND_FRAC_BOT);
-        uint _bidAmountToPot = msg.value.sub(_bidAmountToCommunity).sub(_bidAmountToDividendFund);
+        uint _bidAmountToPot = msg.value.sub(_bidAmountToDeveloper).sub(_bidAmountToDividendFund);
 
-        earnings[_null] = earnings[_null].add(_bidAmountToCommunity);
+        earnings[owner] = earnings[owner].add(_bidAmountToDeveloper);
         dividendFund = dividendFund.add(_bidAmountToDividendFund);
         pot = pot.add(_bidAmountToPot);
         Bid(now, msg.sender, msg.value, pot);
@@ -187,10 +178,6 @@ contract FOMO3DLite {
         msg.sender.transfer(_amount);
         DividendsWithdrawal(now, msg.sender, _dividendShares, _amount, totalDividendShares, dividendFund);
     }
-
-    function start() public onlyOwner {
-        deadline = 0;
-    }
 }
 
 /**
@@ -199,9 +186,9 @@ contract FOMO3DLite {
  */
 library SafeMath {
     /**
-     * @dev Multiplies two numbers, throws on overflow.
-     */
-    function mul(uint256 a, uint256 b) internal pure returns(uint256) {
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
         }
@@ -211,9 +198,9 @@ library SafeMath {
     }
 
     /**
-     * @dev Integer division of two numbers, truncating the quotient.
-     */
-    function div(uint256 a, uint256 b) internal pure returns(uint256) {
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
@@ -221,17 +208,17 @@ library SafeMath {
     }
 
     /**
-     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-     */
-    function sub(uint256 a, uint256 b) internal pure returns(uint256) {
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
     /**
-     * @dev Adds two numbers, throws on overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns(uint256) {
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
         return c;
