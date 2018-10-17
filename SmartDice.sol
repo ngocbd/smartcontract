b51a452176727a2537fc259271e1b76320f08c8c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:43:29 +0700

    add contract SmartDice

--- SmartDice.sol
+++ SmartDice.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SmartDice at 0x7ed77f4dc8218db41789d75889394bcb752dd532
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SmartDice at 0xcc88d8ea235eb37d427a7040674d751a689ba4bc
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.17;
 
 /**
  * @title Ownable
@@ -1265,225 +1265,259 @@ contract usingOraclize {
 }
 // </ORACLIZE_API>
 
-/**
- * @title DiceGame
- * @dev Dice Betting contract using Oraclize, inherited Ownable and SafeMath
- */
 contract SmartDice is usingOraclize, Ownable, SafeMath {
+    
+    struct DiceBet {
+        address player;
+        address referee;
+        uint odd;
+        uint stake;
+        uint rng;
+        uint profit;
+        uint win;
+        bool paid;
+    }
+    
+    struct Ref {
+        uint refCnt;
+        uint balance;
+    }
+    
+    mapping (bytes32 => DiceBet) public bets;
+    mapping (address => Ref) public refWallet;
+    mapping (address => uint) pendingWallet;
+    mapping (uint => uint) maxBetAmounts;
+    
+    bytes32[] public queryIds;
+    uint public constant baseNumber = 1000;
+    uint public totalBets;
+    uint public totalUserProfit;
+    uint public totalUserLost;
+    uint public totalWins;
+    uint public totalLosts;
+    uint public houseEdge;
+    uint public refShare;
+    uint public balance;
+    uint public maxPendingBalances;
+    uint public minBetAmount;
+    uint public gasOraclize;
+    uint public gasPriceOraclize;
+    
+    event DiceRolled(address _address, bytes32 _queryId, uint _amount, uint _odd, address _referee);
+    event UserWin(address _address, bytes32 _queryId, uint _amount, uint _odd, uint _randomResult, uint _profit, address _referee);
+    event UserLose(address _address, bytes32 _queryId, uint _amount, uint _odd, uint _randomResult, uint _lost);
+    event HouseDeposited(uint _amount);
+    event HouseWithdrawed(address _withdraw, uint _amount);
+    event PaidPendingBalance(address _holder, uint _amount);
+    event PaidRefBalance(address _holder, uint _amount);
+    event ResetHouseEdge();
+    
+    modifier onlyOraclize() {
+        require (msg.sender == oraclize_cbAddress());
+        _;
+    }
 
-	struct DiceBet {
-		address player;
-		uint odd;
-		uint stake;
-		uint rng;
-		uint profit;
-		uint win;
-		bool paid;
-	}
+    function SmartDice() public {
+        oraclize_setNetwork(networkID_auto);
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        houseEdge = 96;
+        refShare = 1;
+        gasOraclize = 500000;
+        minBetAmount = 100 finney;
+        maxBetAmounts[10] = 500 finney;
+        maxBetAmounts[20] = 1 ether;
+        maxBetAmounts[30] = 2 ether;
+        maxBetAmounts[40] = 4 ether;
+        maxBetAmounts[50] = 3 ether;
+        maxBetAmounts[60] = 2 ether;
+        gasPriceOraclize = 20000000000 wei;
+        oraclize_setCustomGasPrice(gasPriceOraclize);
+    }
 
-	mapping (bytes32 => DiceBet) public bets;
-	mapping (address => uint) balances;
-	mapping (uint => uint) maxBetAmounts;
+    function __callback(bytes32 myId, string result, bytes proof) public onlyOraclize {
+        require(bets[myId].player != address(0x0));
+        require(bets[myId].win == 2);
+        
+        bets[myId].rng = uint(keccak256(parseInt(result), proof)) % baseNumber + 1;
+        maxPendingBalances = sub(maxPendingBalances, bets[myId].profit);
+        
+        if (bets[myId].rng < bets[myId].odd * 10) { 
+            /// player win
+            bets[myId].win = 1;
+            totalWins = totalWins + 1;
+            
+            uint refAmt = 0;
+            if (bets[myId].referee != address(0x0)) {
+                refAmt = bets[myId].profit * refShare / 100;
+                refWallet[bets[myId].referee].refCnt ++;
+                refWallet[bets[myId].referee].balance = add(refWallet[bets[myId].referee].balance, refAmt);
+            }
+            
+            balance = sub(balance, bets[myId].profit);
+            totalUserProfit = totalUserProfit + bets[myId].profit;
+            
+            uint amtToSend = add(bets[myId].profit, bets[myId].stake) - refAmt;
+            bets[myId].paid = true;
+            
+            if (!bets[myId].player.send(amtToSend)) {
+                bets[myId].paid = false;
+                pendingWallet[bets[myId].player] = add(pendingWallet[bets[myId].player], amtToSend);
+            }
+            
+            UserWin(bets[myId].player, myId, bets[myId].stake, bets[myId].odd, bets[myId].rng, bets[myId].profit - refAmt, bets[myId].referee);
+        } else {
+            /// player defeated
+            bets[myId].win = 0;
+            
+            balance = sub(balance, 1);
+            totalUserLost = totalUserLost + bets[myId].stake;
+            totalLosts = totalLosts + 1;
+            bets[myId].profit = 0;
+            bets[myId].paid = true;
+            
+            if (!bets[myId].player.send(1)) {
+                bets[myId].paid = false;
+                pendingWallet[bets[myId].player] = add(pendingWallet[bets[myId].player], 1);
+            }
+            
+            balance = add(balance, bets[myId].stake);
+            
+            UserLose(bets[myId].player, myId, bets[myId].stake, bets[myId].odd, bets[myId].rng, bets[myId].stake);
+        }
+    }
+    
+    function rollDice(uint _odd, address _referee) public payable returns (bytes32) {
+        require(_odd <= 60 && _odd > 0);
+        require(maxBetAmounts[_odd] > 0 && msg.value <= maxBetAmounts[_odd]);
+        require(msg.sender != _referee);
+        
+        uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
+        if (minBetAmount + oraclizeFee >= msg.value) revert();
+        
+        string memory payload = strConcat('\n{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"14a9ea18-183d-4f06-95ad-de43293dbe0c","n":1,"min":1,"max":', uint2str(baseNumber),  ',"replacement":true,"base":10},"id":"1"}');
+        bytes32 queryId = oraclize_query("URL", "json(https://api.random.org/json-rpc/1/invoke).result.random.data.0", payload, gasOraclize);
+        
+        uint stake = msg.value - oraclizeFee;
+        uint profit = stake * houseEdge / _odd - stake;
+        
+        bets[queryId] = DiceBet(msg.sender, _referee, _odd, stake, 0, profit, 2, false);
+        queryIds.push(queryId);
+        
+        maxPendingBalances = add(maxPendingBalances, profit);
+        if (maxPendingBalances > balance) revert();
+        
+        totalBets += 1;
+        
+        DiceRolled(msg.sender, queryId, stake, _odd, _referee);
+        return queryId;
+    }
+    
+    function getPendingBalance(address holder) public view returns (uint) {
+        return pendingWallet[holder];
+    }
+    
+    function setOraclizeGasLimit(uint amount) public onlyOwner {
+        gasOraclize = amount;
+    }
+    
+    function setOraclizeGasPrice(uint price) public onlyOwner {
+        gasPriceOraclize = price;
+        oraclize_setCustomGasPrice(price);
+    }
+    
+    function getMinBetAmount() public constant returns (uint) {
+        uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
+        return oraclizeFee + minBetAmount;
+    }
+    
+    function getMaxBetAmount(uint odd) public constant returns (uint) {
+        uint totalBalance = address(this).balance;
+        uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
+        return totalBalance * odd * 100 / (houseEdge * (100 - odd)) + oraclizeFee;
+    }
+    
+    function getBetCount() public constant returns (uint) {
+        return queryIds.length;
+    }
+    
+    function getBet(uint _id) public constant returns (address, uint, uint, uint, uint, uint, bool) {
+        require(_id < queryIds.length);
+        bytes32 qId = queryIds[_id];
+        if (bets[qId].stake > 0) {
+            DiceBet memory bet = bets[qId];
+            return (bet.player, bet.stake, bet.odd, bet.rng, bet.profit, bet.win, bet.paid);
+        }
+    }
+    
+    function getRefWallet() public constant returns (uint, uint) {
+        return (refWallet[msg.sender].refCnt, refWallet[msg.sender].balance);
+    }
+    
+    function getContractData() public constant returns (uint, uint, uint, uint, uint, uint, uint, uint) {
+        uint totalBalance = address(this).balance;
+        uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
+        return (totalBalance, oraclizeFee, totalBets, totalUserProfit, totalUserLost, totalWins, totalLosts, houseEdge);
+    }
+    
+    function setMinBetAmount(uint amount) public onlyOwner {
+        minBetAmount = amount;
+    }
+    
+    function setMaxBetAmount(uint odd, uint amount) public onlyOwner {
+        require(maxBetAmounts[odd] > 0);
+        maxBetAmounts[odd] = amount;
+    }
+    
+    function setHouseEdge(uint value) public onlyOwner {
+        houseEdge = value;
+        
+        ResetHouseEdge();
+    }
+    
+    function setRefShare(uint value) public onlyOwner {
+        refShare = value;
+    }
 
-	bytes32[] public queryIds;
-	uint public constant baseNumber = 1000;
-	uint public totalBets;
-	uint public totalUserProfit;
-	uint public totalUserLost;
-	uint public totalWins;
-	uint public totalLosts;
-	uint public houseEdge;
-	uint public balance;
-	uint public maxPendingBalances;
-	uint public minBetAmount;
-	uint public gasOraclize;
-	uint public gasPriceOraclize;
-
-	event DiceRolled(address _address, bytes32 _queryId, uint _amount, uint _odd);
-	event UserWin(address _address, bytes32 _queryId, uint _amount, uint _odd, uint _randomResult, uint _profit);
-	event UserLose(address _address, bytes32 _queryId, uint _amount, uint _odd, uint _randomResult, uint _lost);
-	event HouseDeposited(uint _amount);
-	event HouseWithdrawed(address _withdraw, uint _amount);
-	event PaidPendingBalance(address _holder, uint _amount);
-	event ResetHouseEdge();
-
-	modifier onlyOraclize() {
-		require (msg.sender == oraclize_cbAddress());
-		_;
-	}
-
-	function SmartDice() public {
-		oraclize_setNetwork(networkID_auto);
-		oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
-		houseEdge = 96;
-		gasOraclize = 500000;
-		minBetAmount = 100 finney;
-		maxBetAmounts[10] = 500 finney;
-		maxBetAmounts[20] = 1 ether;
-		maxBetAmounts[30] = 2 ether;
-		maxBetAmounts[40] = 4 ether;
-		maxBetAmounts[50] = 3 ether;
-		maxBetAmounts[60] = 2 ether;
-		gasPriceOraclize = 20000000000 wei;
-		oraclize_setCustomGasPrice(gasPriceOraclize);
-	}
-
-	function __callback(bytes32 myId, string result, bytes proof) public onlyOraclize {
-		require(bets[myId].player != address(0x0));
-		require(bets[myId].win == 2);
-
-		bets[myId].rng = uint(keccak256(parseInt(result), proof)) % baseNumber + 1;
-		maxPendingBalances = sub(maxPendingBalances, bets[myId].profit);
-
-		if (bets[myId].rng < bets[myId].odd * 10) {
-			/// player win
-			bets[myId].win = 1;
-
-			balance = sub(balance, bets[myId].profit);
-			totalUserProfit = totalUserProfit + bets[myId].profit;
-			totalWins = totalWins + 1;
-			uint amtToSend = add(bets[myId].profit, bets[myId].stake);
-			bets[myId].paid = true;
-
-			if (!bets[myId].player.send(amtToSend)) {
-				bets[myId].paid = false;
-				balances[bets[myId].player] = add(balances[bets[myId].player], amtToSend);
-			}
-
-			UserWin(bets[myId].player, myId, bets[myId].stake, bets[myId].odd, bets[myId].rng, bets[myId].profit);
-		} else {
-			/// player defeated
-			bets[myId].win = 0;
-
-			balance = sub(balance, 1);
-			totalUserLost = totalUserLost + bets[myId].stake;
-			totalLosts = totalLosts + 1;
-			bets[myId].profit = 0;
-			bets[myId].paid = true;
-
-			if (!bets[myId].player.send(1)) {
-				bets[myId].paid = false;
-				balances[bets[myId].player] = add(balances[bets[myId].player], 1);
-			}
-
-			balance = add(balance, bets[myId].stake);
-
-			UserLose(bets[myId].player, myId, bets[myId].stake, bets[myId].odd, bets[myId].rng, bets[myId].stake);
-		}
-	}
-
-	function rollDice(uint _odd) public payable returns (bytes32) {
-		require(_odd <= 60 && _odd > 0);
-		require(maxBetAmounts[_odd] > 0 && msg.value <= maxBetAmounts[_odd]);
-
-		uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
-		if (minBetAmount + oraclizeFee >= msg.value) revert();
-
-		string memory payload = strConcat('\n{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"14a9ea18-183d-4f06-95ad-de43293dbe0c","n":1,"min":1,"max":', uint2str(baseNumber),  ',"replacement":true,"base":10},"id":"1"}');
-		bytes32 queryId = oraclize_query("URL", "json(https://api.random.org/json-rpc/1/invoke).result.random.data.0", payload, gasOraclize);
-
-		uint stake = msg.value - oraclizeFee;
-		uint profit = stake * houseEdge / _odd - stake;
-
-		bets[queryId] = DiceBet(msg.sender, _odd, stake, 0, profit, 2, false);
-		queryIds.push(queryId);
-
-		maxPendingBalances = add(maxPendingBalances, profit);
-		if (maxPendingBalances > balance) revert();
-
-		totalBets += 1;
-
-		DiceRolled(msg.sender, queryId, stake, _odd);
-		return queryId;
-	}
-
-	function getPendingBalance(address holder) public view returns (uint) {
-		return balances[holder];
-	}
-
-	function setOraclizeGasLimit(uint amount) public onlyOwner {
-		gasOraclize = amount;
-	}
-
-	function setOraclizeGasPrice(uint price) public onlyOwner {
-		gasPriceOraclize = price;
-		oraclize_setCustomGasPrice(price);
-	}
-
-	function getMinBetAmount() public constant returns (uint) {
-		uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
-		return oraclizeFee + minBetAmount;
-	}
-
-	function getMaxBetAmount(uint odd) public constant returns (uint) {
-		uint totalBalance = address(this).balance;
-		uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
-		return totalBalance * odd * 100 / (houseEdge * (100 - odd)) + oraclizeFee;
-	}
-
-	function getBetCount() public constant returns (uint) {
-		return queryIds.length;
-	}
-
-	function getBet(uint _id) public constant returns (address, uint, uint, uint, uint, uint, bool) {
-		require(_id < queryIds.length);
-		bytes32 qId = queryIds[_id];
-		if (bets[qId].stake > 0) {
-			DiceBet memory bet = bets[qId];
-			return (bet.player, bet.stake, bet.odd, bet.rng, bet.profit, bet.win, bet.paid);
-		}
-	}
-
-	function getContractData() public constant returns (uint, uint, uint, uint, uint, uint, uint, uint) {
-		uint totalBalance = address(this).balance;
-		uint oraclizeFee = OraclizeI(OAR.getAddress()).getPrice("URL", gasOraclize);
-		return (totalBalance, oraclizeFee, totalBets, totalUserProfit, totalUserLost, totalWins, totalLosts, houseEdge);
-	}
-
-	function setMinBetAmount(uint amount) public onlyOwner {
-		minBetAmount = amount;
-	}
-
-	function setMaxBetAmount(uint odd, uint amount) public onlyOwner {
-		require(maxBetAmounts[odd] > 0);
-		maxBetAmounts[odd] = amount;
-	}
-
-	function setHouseEdge(uint value) public onlyOwner {
-		houseEdge = value;
-
-		ResetHouseEdge();
-	}
-
-	function depositBalance() public payable onlyOwner {
-		balance = add(balance, msg.value);
-
-		HouseDeposited(msg.value);
-	}
-
-	function resetBalance() public onlyOwner {
-		balance = address(this).balance;
-	}
-
-	function withdrawBalance(address withdraw, uint amount) public onlyOwner {
-		require(withdraw != address(0));
-		balance = sub(this.balance(), amount);
-		if (!withdraw.send(amount)) revert();
-
-		HouseWithdrawed(withdraw, amount);
-	}
-
-	function withdrawPendingBalance(address holder) public onlyOwner {
-		require(holder != address(0));
-		require(balances[holder] != 0);
-		uint amount = balances[holder];
-		balances[holder] = 0;
-		if (!holder.send(amount)) revert();
-
-		PaidPendingBalance(holder, amount);
-	}
-
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
+    function depositBalance() public payable onlyOwner {
+        balance = add(balance, msg.value);
+        
+        HouseDeposited(msg.value);
+    }
+    
+    function resetBalance() public onlyOwner {
+        balance = address(this).balance;
+    }
+    
+    function withdrawBalance(address withdraw, uint amount) public onlyOwner {
+        require(withdraw != address(0));
+        balance = sub(this.balance(), amount);
+        if (!withdraw.send(amount)) revert();
+        
+        HouseWithdrawed(withdraw, amount);
+    }
+    
+    function withdrawPendingBalance(address holder) public onlyOwner {
+        require(holder != address(0));
+        require(pendingWallet[holder] != 0);
+        uint amount = pendingWallet[holder];
+        pendingWallet[holder] = 0;
+        balance = sub(balance, amount);
+        if (!holder.send(amount)) revert();
+        
+        PaidPendingBalance(holder, amount);
+    }
+    
+    function withdrawRefBalance() public {
+        require(refWallet[msg.sender].balance > 0);
+        uint amount = refWallet[msg.sender].balance;
+        refWallet[msg.sender].balance = 0;
+        balance = sub(balance, amount);
+        if (!msg.sender.send(amount)) revert();
+        
+        PaidRefBalance(msg.sender, amount);
+    }
+    
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
