cf63a0f04f8d41a362d54dc7ca063217ae39a0ba
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:37:03 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,41 +1,87 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x9f543eb6a62b63fef48cbcfc12577eba4513ce85
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0xf30416167df0631d31a9be6412904049c6dc27f7
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.24;
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+interface tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external;
+}
 
-contract TokenERC20 {
-    string public name;         //??????? chinese vegetables supply chain
-    string public symbol;
-    uint8 public decimals = 18;  
-    uint256 public totalSupply;  
+contract owned {
+    address public owner;
+    address public newOwner;
 
-    mapping (address => uint256) public balanceOf; 
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Burn(address indexed from, uint256 value);
-
-
-    function TokenERC20(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
-        name = tokenName;
-        symbol = tokenSymbol;
+    constructor() owned() public {
+        owner = msg.sender;
     }
 
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) onlyOwner public {
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
+contract TokenERC20 is owned {
+    string public name = 'Telex';
+    string public symbol = 'TLX';
+    uint8 public decimals = 8;
+    uint256 public totalSupply = 2000000000000000;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+    mapping (address => bool) public frozenAccount;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event FrozenFunds(address target, bool frozen);
+
+    constructor() TokenERC20() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function _initialTransfers(address _from, address[] addresses, uint[] amounts) internal {
+        for (uint i=0; i<addresses.length; i++) {
+            address _to = addresses[i];
+            uint _value = amounts[i];
+            require(_from == owner);
+            _transfer(_from, _to, _value);
+        }
+    }
+
+    function initialTransfers(address[] addresses, uint256[] amounts) public {
+        _initialTransfers(msg.sender, addresses, amounts);
+    }
+
+    function _ownerTransfer(address _owner, address _from, address _to, uint _value) internal {
+        require(_owner == owner);
+        _transfer(_from, _to, _value);
+    }
+
+    function ownerTransfer(address _from, address _to, uint256 _value) public {
+        _ownerTransfer(msg.sender, _from, _to, _value);
+    }
 
     function _transfer(address _from, address _to, uint _value) internal {
         require(_to != 0x0);
         require(balanceOf[_from] >= _value);
         require(balanceOf[_to] + _value > balanceOf[_to]);
+        require(!frozenAccount[msg.sender]);
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
@@ -44,14 +90,13 @@ contract TokenERC20 {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        require(_value <= allowance[_from][msg.sender]);
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
@@ -64,21 +109,8 @@ contract TokenERC20 {
         }
     }
 
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
-        allowance[_from][msg.sender] -= _value;
-        totalSupply -= _value;
-        Burn(_from, _value);
-        return true;
+    function freezeAccount(address target, bool freeze) public onlyOwner {
+        frozenAccount[target] = freeze;
+        emit FrozenFunds(target, freeze);
     }
 }
