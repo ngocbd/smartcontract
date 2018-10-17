67b9a3edb313a4d1919cd943e6dc1647f76f8ea8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:33:56 +0700

    add contract HFCoin

--- HFCoin.sol
+++ HFCoin.sol
@@ -1,84 +1,223 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HFCoin at 0x191a70e9808c8d89be289cfe9001a7010dc3d78c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HFCoin at 0xa705d6fc756ae8e8800c0c2c12eed995ba50a6b4
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.22;
 
-contract HFCoin {
-    string public name;
-    string public symbol;
-    address public owner;
-    uint256 public prizeAmount = 0;
-    bool public gameStarted = false;
-    bool public prizeWon = false;
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    mapping (address => uint256) public balanceOf;
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract ForeignToken {
+    function balanceOf(address _owner) constant public returns (uint256);
+    function transfer(address _to, uint256 _value) public returns (bool);
+}
+
+contract ERC20Basic {
+    uint256 public totalSupply;
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface Token { 
+    function distr(address _to, uint256 _value) external returns (bool);
+    function totalSupply() constant external returns (uint256 supply);
+    function balanceOf(address _owner) constant external returns (uint256 balance);
+}
+
+contract HFCoin is ERC20 {
     
-    event Burn(address indexed from, uint256 value);
-    event Redemption(address indexed from, uint256 value);
-    event TokenRequest(address indexed from, uint256 value);
-    event Winner(address indexed from);
+    using SafeMath for uint256;
+    address owner = msg.sender;
 
-    modifier onlyOwner {
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    mapping (address => bool) public blacklist;
+
+    string public constant name = "HF Coin";
+    string public constant symbol = "HF";
+    uint public constant decimals = 18;
+    
+    uint256 public totalSupply = 600000000e18;
+    uint256 public totalDistributed = 100000000e18;
+    uint256 public totalRemaining = totalSupply.sub(totalDistributed);
+    uint256 public value = 7000e18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+    event Distr(address indexed to, uint256 amount);
+    event DistrFinished();
+    
+    event Burn(address indexed burner, uint256 value);
+
+    bool public distributionFinished = false;
+    
+    modifier canDistr() {
+        require(!distributionFinished);
+        _;
+    }
+    
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-
-    function HFCoin(
-        string tokenName,
-        string tokenSymbol
-    ) public 
-    {
-        balanceOf[msg.sender] = 0;
-        name = tokenName;
-        symbol = tokenSymbol;
+    
+    modifier onlyWhitelist() {
+        require(blacklist[msg.sender] == false);
+        _;
+    }
+    
+    function HFCoin() public {
         owner = msg.sender;
+        balances[owner] = totalDistributed;
     }
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(_value <= 10);
-        balanceOf[msg.sender] -= _value;
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function _redeem(address _from, uint256 _value) internal {
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[owner] + _value > balanceOf[owner]);
-        require(_value <= 1337);
-
-        balanceOf[_from] -= _value;
-        balanceOf[owner] += _value;
-        emit Redemption(_from, _value);
-
-        if (_value == 1337 && gameStarted && !prizeWon) {
-            prizeWon = true;
-            emit Winner(_from);
-            _from.transfer(prizeAmount);
+    
+    function transferOwnership(address newOwner) onlyOwner public {
+        if (newOwner != address(0)) {
+            owner = newOwner;
         }
     }
+    
+    function finishDistribution() onlyOwner canDistr public returns (bool) {
+        distributionFinished = true;
+        emit DistrFinished();
+        return true;
+    }
+    
+    function distr(address _to, uint256 _amount) canDistr private returns (bool) {
+        totalDistributed = totalDistributed.add(_amount);
+        totalRemaining = totalRemaining.sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Distr(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+        
+        if (totalDistributed >= totalSupply) {
+            distributionFinished = true;
+        }
+    }
+    
+    function () external payable {
+        getTokens();
+     }
+    
+    function getTokens() payable canDistr onlyWhitelist public {
+        if (value > totalRemaining) {
+            value = totalRemaining;
+        }
+        
+        require(value <= totalRemaining);
+        
+        address investor = msg.sender;
+        uint256 toGive = value;
+        
+        distr(investor, toGive);
+        
+        if (toGive > 0) {
+            blacklist[investor] = true;
+        }
 
-    function redeem(uint256 _value) public {
-        _redeem(msg.sender, _value);
+        if (totalDistributed >= totalSupply) {
+            distributionFinished = true;
+        }
+        
+        value = value.div(100000).mul(99999);
     }
 
-    function _requestTokens(address _to, uint256 _value) internal {
-        require(balanceOf[_to] + _value <= 10);
-        balanceOf[_to] += _value;
-        emit TokenRequest(_to, _value);  
+    function balanceOf(address _owner) constant public returns (uint256) {
+        return balances[_owner];
     }
 
-    function requestTokens(uint256 _value) public {
-        _requestTokens(msg.sender, _value);
+    modifier onlyPayloadSize(uint size) {
+        assert(msg.data.length >= size + 4);
+        _;
     }
-
-    function prizeDeposit() public onlyOwner payable {}
-
-    function startGame(uint256 _prizeAmount) public onlyOwner {
-        prizeAmount = _prizeAmount;
-        gameStarted = true;
+    
+    function transfer(address _to, uint256 _amount) onlyPayloadSize(2 * 32) public returns (bool success) {
+        require(_to != address(0));
+        require(_amount <= balances[msg.sender]);
+        
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
     }
+    
+    function transferFrom(address _from, address _to, uint256 _amount) onlyPayloadSize(3 * 32) public returns (bool success) {
+        require(_to != address(0));
+        require(_amount <= balances[_from]);
+        require(_amount <= allowed[_from][msg.sender]);
+        
+        balances[_from] = balances[_from].sub(_amount);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) constant public returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+    
+    function getTokenBalance(address tokenAddress, address who) constant public returns (uint){
+        ForeignToken t = ForeignToken(tokenAddress);
+        uint bal = t.balanceOf(who);
+        return bal;
+    }
+    
+    function withdraw() onlyOwner public {
+        uint256 etherBalance = address(this).balance;
+        owner.transfer(etherBalance);
+    }
+    
+    function burn(uint256 _value) onlyOwner public {
+        require(_value <= balances[msg.sender]);
 
-    function gameOver() public onlyOwner {
-        selfdestruct(owner);
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        totalDistributed = totalDistributed.sub(_value);
+        emit Burn(burner, _value);
+    }
+    
+    function withdrawForeignTokens(address _tokenContract) onlyOwner public returns (bool) {
+        ForeignToken token = ForeignToken(_tokenContract);
+        uint256 amount = token.balanceOf(address(this));
+        return token.transfer(owner, amount);
     }
 }
