f0cc6b147ef00e698ff81b408912327e1aa84ba6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:23:39 +0700

    add contract BCBCOIN

--- BcbCoin.sol
+++ BcbCoin.sol
@@ -1,129 +1,156 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BcbCoin at 0x9F138D5D9e24186eC96B35e5B5530C907860A78d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BCBCOIN at 0x61c0e30b2c8c93cd0970c1d64029037bfffa5954
 */
 pragma solidity ^0.4.16;
 
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
-contract Ownable {
-    address public owner;
-
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner()  {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        if (newOwner != address(0)) {
-            owner = newOwner;
-        }
-    }
-
-}
-
-contract SafeMath {
-    function safeSub(uint a, uint b) pure internal returns (uint) {
-        sAssert(b <= a);
-        return a - b;
-    }
-
-    function safeAdd(uint a, uint b) pure internal returns (uint) {
-        uint c = a + b;
-        sAssert(c>=a && c>=b);
-        return c;
-    }
-
-    function sAssert(bool assertion) internal pure {
-        if (!assertion) {
-            revert();
-        }
-    }
-}
-
-contract ERC20 {
-    uint public totalSupply;
-    function balanceOf(address who) public constant returns (uint);
-    function allowance(address owner, address spender) public constant returns (uint);
-
-    function transfer(address toAcct, uint value) public returns (bool ok);
-    function transferFrom(address fromAcct, address toAcct, uint value) public returns (bool ok);
-    function approve(address spender, uint value) public returns (bool ok);
-    event Transfer(address indexed fromAcct, address indexed toAcct, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-contract StandardToken is ERC20, SafeMath {
-
-    mapping(address => uint) balances;
-    mapping (address => mapping (address => uint)) allowed;
-
-    function transfer(address _toAcct, uint _value) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], _value);
-        balances[_toAcct] = safeAdd(balances[_toAcct], _value);
-        Transfer(msg.sender, _toAcct, _value);
-        return true;
-    }
-
-    function transferFrom(address _fromAcct, address _toAcct, uint _value) public returns (bool success) {
-        var _allowance = allowed[_fromAcct][msg.sender];
-        balances[_toAcct] = safeAdd(balances[_toAcct], _value);
-        balances[_fromAcct] = safeSub(balances[_fromAcct], _value);
-        allowed[_fromAcct][msg.sender] = safeSub(_allowance, _value);
-        Transfer(_fromAcct, _toAcct, _value);
-        return true;
-    }
-
-    function balanceOf(address _owner) public constant returns (uint balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint _value) public  returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
-        return allowed[_owner][_spender];
-    }
-
-}
-
-contract BcbCoin is Ownable, StandardToken {
-
+contract BCBCOIN {
+    // Public variables of the token
     string public name;
     string public symbol;
-    uint public decimals;                  
-    uint public totalSupply;  
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    /// @notice Initializes the contract and allocates all initial tokens to the owner and agreement account
-    function BcbCoin() public {
-        totalSupply = 2000 * (10**6) * (10**18); 
-        balances[msg.sender] = totalSupply;
-        name = "BCB";
-        symbol = "BCB";
-        decimals = 18;  
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function BCBCOIN(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
-    function () payable public{
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    /// @notice To transfer token contract ownership
-    /// @param _newOwner The address of the new owner of this contract
-    function transferOwnership(address _newOwner) public onlyOwner {
-        balances[_newOwner] = safeAdd(balances[owner], balances[_newOwner]);
-        balances[owner] = 0;
-        Ownable.transferOwnership(_newOwner);
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
     }
 
-    // Owner can transfer out any ERC20 tokens sent in by mistake
-    function transferAnyERC20Token(address tokenAddress, uint amount) public onlyOwner returns (bool success)
-    {
-        return ERC20(tokenAddress).transfer(owner, amount);
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
     }
 
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
 }
