faf679924f4e65feb40a205f4eb1c432f8482783
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:36:28 +0700

    add contract cyberEntry

---
+++ cyberEntry.sol
@@ -0,0 +1,34 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract cyberEntry at 0x9959ddf84f7d98d0a0597bf446bbf1428a089494
+*/
+pragma solidity ^0.4.19;
+
+contract Gatekeeper {
+    function enter(bytes32 _passcode, bytes8 _gateKey) public returns (bool);
+}
+
+contract cyberEntry {
+    address public gkAddress;
+
+    Gatekeeper gk; 
+
+    function cyberEntry(address _gkAddress) public {
+        gkAddress = _gkAddress;
+        gk = Gatekeeper(gkAddress);
+    }
+
+    function enter(bytes32 passphrase) public {
+        uint256 stipend = 483657;
+        
+        uint256 key;
+        uint256 upper;
+        uint256 lower;
+
+        upper = uint256(bytes4("cool")) << 32;
+        lower = uint256(uint16(msg.sender));
+
+        key = upper | lower;
+
+        gk.enter.gas(stipend)( passphrase, bytes8(key));
+    }
+}
