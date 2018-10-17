2cd9c24e5aa74224439cc5dade0d5680186f2923
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:35:16 +0700

    add contract JTCToken

--- JTCToken.sol
+++ JTCToken.sol
@@ -1,10 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract JTCToken at 0x6342a4744ea2fd9685610ab0c7d11f3d083bc7ba
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract JTCToken at 0x8582911374626d299985d3113b855f68706d857e
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.24;
 
 library SafeMath {
-
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
     function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
         if (a == 0) {
             return 0;
@@ -14,15 +16,27 @@ library SafeMath {
         return c;
     }
 
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return a / b;
     }
 
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
     function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
         c = a + b;
         assert(c >= a);
@@ -51,7 +65,6 @@ contract Ownable {
     }
 }
 
-
 interface tokenRecipient {function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external;}
 
 contract TokenERC20 {
@@ -66,8 +79,6 @@ contract TokenERC20 {
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Burn(address indexed from, uint256 value);
 
     constructor (
         uint256 initialSupply,
@@ -111,103 +122,24 @@ contract TokenERC20 {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public
-    returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-    public
-    returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
             return true;
         }
     }
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] = balanceOf[_from].sub(_value);
-        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        emit Burn(_from, _value);
-        return true;
-    }
 }
 
 contract JTCToken is Ownable, TokenERC20 {
-
-    uint256 public sellPrice;
-    uint256 public buyPrice;
-
-    uint minBalanceForAccounts;
-
-    mapping(address => bool) public frozenAccount;
-
-    event FrozenFunds(address target, bool frozen);
-
     constructor (
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
     ) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
-
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
-        require(!frozenAccount[_from]);
-        require(!frozenAccount[_to]);
-
-        if (msg.sender.balance < minBalanceForAccounts)
-            sell((minBalanceForAccounts - msg.sender.balance) / sellPrice);
-
-        balanceOf[_from] = balanceOf[_from].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(_from, _to, _value);
-    }
-
-    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-        balanceOf[target] = balanceOf[target].add(mintedAmount);
-        totalSupply = totalSupply.add(mintedAmount);
-        emit Transfer(0, this, mintedAmount);
-        emit Transfer(this, target, mintedAmount);
-    }
-
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        emit FrozenFunds(target, freeze);
-    }
-
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
-    }
-
-    function buy() payable public {
-        uint amount = msg.value.div(buyPrice);
-        _transfer(this, msg.sender, amount);
-    }
-
-    function sell(uint256 amount) public {
-        address myAddress = this;
-        require(myAddress.balance >= amount * sellPrice);
-        _transfer(msg.sender, this, amount);
-        msg.sender.transfer(amount.mul(sellPrice));
-    }
-
-    function setMinBalance(uint minimumBalanceInFinney) onlyOwner public {
-        minBalanceForAccounts = minimumBalanceInFinney * 1 finney;
-    }
 }
