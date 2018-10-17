7586d4a2f974b43d1345a19e443ece8cb949009e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:34:34 +0700

    add contract OnlyManager

--- OnlyManager.sol
+++ OnlyManager.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OnlyManager at 0xFb5978C7ca78074B2044034CbdbC3f2E03Dfe2bA
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OnlyManager at 0xa7c621d9fe8566585a6bb44a6eaa4e714e4d6496
 */
 pragma solidity ^0.4.13;
 
@@ -7,46 +7,33 @@ interface FundInterface {
 
     // EVENTS
 
-    event PortfolioContent(uint holdings, uint price, uint decimals);
+    event PortfolioContent(address[] assets, uint[] holdings, uint[] prices);
     event RequestUpdated(uint id);
-    event Invested(address indexed ofParticipant, uint atTimestamp, uint shareQuantity);
     event Redeemed(address indexed ofParticipant, uint atTimestamp, uint shareQuantity);
-    event SpendingApproved(address onConsigned, address ofAsset, uint amount);
     event FeesConverted(uint atTimestamp, uint shareQuantityConverted, uint unclaimed);
     event CalculationUpdate(uint atTimestamp, uint managementFee, uint performanceFee, uint nav, uint sharePrice, uint totalSupply);
-    event OrderUpdated(uint id);
-    event LogError(uint ERROR_CODE);
     event ErrorMessage(string errorMessage);
 
     // EXTERNAL METHODS
     // Compliance by Investor
-    function requestInvestment(uint giveQuantity, uint shareQuantity, bool isNativeAsset) external;
-    function requestRedemption(uint shareQuantity, uint receiveQuantity, bool isNativeAsset) external;
+    function requestInvestment(uint giveQuantity, uint shareQuantity, address investmentAsset) external;
     function executeRequest(uint requestId) external;
     function cancelRequest(uint requestId) external;
     function redeemAllOwnedAssets(uint shareQuantity) external returns (bool);
     // Administration by Manager
-    function enableInvestment() external;
-    function disableInvestment() external;
-    function enableRedemption() external;
-    function disableRedemption() external;
+    function enableInvestment(address[] ofAssets) external;
+    function disableInvestment(address[] ofAssets) external;
     function shutDown() external;
-    // Managing by Manager
-    function makeOrder(uint exchangeId, address sellAsset, address buyAsset, uint sellQuantity, uint buyQuantity) external;
-    function takeOrder(uint exchangeId, uint id, uint quantity) external;
-    function cancelOrder(uint exchangeId, uint id) external;
 
     // PUBLIC METHODS
     function emergencyRedeem(uint shareQuantity, address[] requestedAssets) public returns (bool success);
-    // Fees by Manager
-    function allocateUnclaimedFees();
+    function calcSharePriceAndAllocateFees() public returns (uint);
+
 
     // PUBLIC VIEW METHODS
     // Get general information
     function getModules() view returns (address, address, address);
-    function getLastOrderId() view returns (uint);
     function getLastRequestId() view returns (uint);
-    function getNameHash() view returns (bytes32);
     function getManager() view returns (address);
 
     // Get accounting information
