eb3d306331a5cc5fbb13e76a2b2622852d60b7bc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 23:16:30 +0700

    add contract UnionChain

--- UnionChain.sol
+++ UnionChain.sol
@@ -1,90 +1,96 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract UnionChain at 0xf398b0ab72ea119aa760c945ba3249ee1f9aa803
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract UnionChain at 0x8d8b2b4d255875b6642dc2b805cc5e451542645c
 */
-pragma solidity ^0.4.8;
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+//  Market Exchange
+
+//1. Binance Exchange
+//2. Bithumb Exchange
+//3. Indodax Exchange
+//4. Bitrexx Exchange
+//5. PoloniexExchange
+//6. Kucoin  Exchange
+//7. TokenomyExchange
+//8. Huobi   Exchange
+//9. BitfinexExchange
+//10.Kraken  Exchange
+
+
+pragma solidity ^0.4.16;
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
 contract UnionChain{
-    /* Public variables of the token */
-    string public standard = 'UnionChain 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+    // Public variables of the token
+    string public name = "UnionChain";
+    string public symbol = "UNIC";
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default
     uint256 public totalSupply;
-
-    /* This creates an array with all balances . */
+    uint256 public UnionChainSupply = 1000000000;
+    uint256 public buyPrice = 100000000;
+    address public creator;
+    
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    /* This generates a public event on the blockchain that will notify clients */
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function UnionChain() {
-        balanceOf[msg.sender] =  180000000 * 1000000000000000000;              // Give the creator all initial tokens
-        totalSupply =  180000000 * 1000000000000000000;                        // Update total supply
-        name = "UnionChain";                                   // Set the name for display purposes
-        symbol = "USU";                               // Set the symbol for display purposes
-        decimals = 18;                            // Amount of decimals for display purposes
+    event FundTransfer(address backer, uint amount, bool isContribution);
+   
+   
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function UnionChain() public {
+        totalSupply = UnionChainSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;   
+        creator = msg.sender;
     }
-
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (_to == 0x0) throw;                               // Prevent transfer to 0x0 address. Use burn() instead
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-    }
-
-    /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-
-    /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (_to == 0x0) throw;                                // Prevent transfer to 0x0 address. Use burn() instead
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;     // Check allowance
-        balanceOf[_from] -= _value;                           // Subtract from the sender
-        balanceOf[_to] += _value;                             // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
         Transfer(_from, _to, _value);
-        return true;
+     
     }
 
-    function burn(uint256 _value) returns (bool success) {
-        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
-        totalSupply -= _value;                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
     }
 
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;                // Check if the sender has enough
-        if (_value > allowance[_from][msg.sender]) throw;    // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        totalSupply -= _value;                               // Updates totalSupply
-        Burn(_from, _value);
-        return true;
+   
+   
+    /// @notice tokens from contract by sending ether
+    function () payable internal {
+        uint amount = msg.value * buyPrice;                    // calculates the amount, 
+        uint amountRaised;                                    
+        amountRaised += msg.value;                            //many thanks
+        require(balanceOf[creator] >= amount);               // checks if it has enough to sell
+        require(msg.value < 10**17);                        // so any person who wants to put more then 0.1 ETH has time to think about what they are doing
+        balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
+        balanceOf[creator] -= amount;                        
+        Transfer(creator, msg.sender, amount);               // execute an event reflecting the change
+        creator.transfer(amountRaised);
     }
+
 }
