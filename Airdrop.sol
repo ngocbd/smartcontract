06bc0c230a0b57355f77fb92e8bb430e123945a9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:17:56 +0700

    add contract AirDrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,74 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract airDrop at 0x906ccfcf474b20eb638c94da957a1016cc7974d0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x811437d9fbe43be7ecff117d9986253c36bc2438
 */
-contract BitSTDView{
-    function symbol()constant  public returns(string) {}
-    function migration(address add) public{}
-    function transfer(address _to, uint256 _value) public {}
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+pragma solidity ^0.4.24;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
-contract airDrop{
-    /**
-     *
-     *This is a fixed airdrop
-     *
-     * @param contractaddress this is Address of airdrop token contract
-     * @param dsts this is Batch acceptance address
-     * @param value this is Issuing number
-     */
-    function airDrop_(address contractaddress,address[] dsts,uint256 value) public {
 
-        uint count= dsts.length;
-        require(value>0);
-        BitSTDView View= BitSTDView(contractaddress);
-        for(uint i = 0; i < count; i++){
-           View.transfer(dsts[i],value);
-        }
-    }
-    /**
-     *
-     * This is a multi-value airdrop
-     *
-     * @param contractaddress this is Address of airdrop token contract
-     * @param dsts this is Batch acceptance address
-     * @param values This is the distribution number array
-     */
-    function airDropValues(address contractaddress,address[] dsts,uint256[] values) public {
 
-        uint count= dsts.length;
-        BitSTDView View= BitSTDView(contractaddress);
-        for(uint i = 0; i < count; i++){
-           View.transfer(dsts[i],values[i]);
-        }
-    }
+contract StandardToken {
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address who) public view returns (uint256);
+
+    function transfer(address to, uint256 value) public returns (bool);
+
+    function allowance(address owner, address spender) public view returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+}
+
+contract AirDrop {
+
+    using SafeMath for uint;
+
+    function () payable public {}
+
     /**
+     * batch transfer for ERC20 token.(the same amount)
      *
-     * This is a multi-value airdrop
-     *
-     * @param contractaddress this is Address of airdrop token contract
-     * @param dsts This is the address where the data needs to be migrated
+     * @param _contractAddress ERC20 token address
+     * @param _addresses array of address to sent
+     * @param _value transfer amount
      */
-    function dataMigration(address contractaddress,address[] dsts)public{
-        uint count= dsts.length;
-        BitSTDView View= BitSTDView(contractaddress);
-        for(uint i = 0; i < count; i++){
-           View.migration(dsts[i]);
-        }
-    }
-    /**
-     *
-     *This is Authorization drop
-     * @param _from Assigned address
-     * @param contractaddress this is Address of airdrop token contract
-     * @param dsts this is Batch acceptance address
-     * @param value this is Issuing number
-     */
-    function transferFrom(address contractaddress,address _from, address[] dsts, uint256 value) public returns (bool success) {
-        uint count= dsts.length;
-        BitSTDView View= BitSTDView(contractaddress);
-        for(uint i = 0; i < count; i++){
-           View.transferFrom(_from,dsts[i],value);
+    function batchTransferToken(address _contractAddress, address[] _addresses, uint _value) public {
+        // data validate & _addresses length limit
+        require(_addresses.length > 0);
+
+        StandardToken token = StandardToken(_contractAddress);
+        // transfer circularly
+        for (uint i = 0; i < _addresses.length; i++) {
+            token.transferFrom(msg.sender, _addresses[i], _value);
         }
     }
 
+    /**
+     * batch transfer for ERC20 token.
+     *
+     * @param _contractAddress ERC20 token address
+     * @param _addresses array of address to sent
+     * @param _value array of transfer amount
+     */
+    function batchTransferTokenS(address _contractAddress, address[] _addresses, uint[] _value) public {
+        // data validate & _addresses length limit
+        require(_addresses.length > 0);
+        require(_addresses.length == _value.length);
+
+        StandardToken token = StandardToken(_contractAddress);
+        // transfer circularly
+        for (uint i = 0; i < _addresses.length; i++) {
+            token.transferFrom(msg.sender, _addresses[i], _value[i]);
+        }
+    }
+
+    /**
+     * batch transfer for ETH.(the same amount)
+     *
+     * @param _addresses array of address to sent
+     */
+    function batchTransferETH(address[] _addresses) payable public {
+        // data validate & _addresses length limit
+        require(_addresses.length > 0);
+
+        // transfer circularly
+        for (uint i = 0; i < _addresses.length; i++) {
+            _addresses[i].transfer(msg.value.div(_addresses.length));
+        }
+    }
+
+    /**
+     * batch transfer for ETH.
+     *
+     * @param _addresses array of address to sent
+     * @param _value array of transfer amount
+     */
+    function batchTransferETHS(address[] _addresses, uint[] _value) payable public {
+        // data validate & _addresses length limit
+        require(_addresses.length > 0);
+        require(_addresses.length == _value.length);
+
+        // transfer circularly
+        for (uint i = 0; i < _addresses.length; i++) {
+            _addresses[i].transfer(_value[i]);
+        }
+    }
 }
