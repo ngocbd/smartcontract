ca237a06afa9717541e96a4ed8b65b3a3c3a573f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 15:36:08 +0700

    add contract Gateway

--- Gateway.sol
+++ Gateway.sol
@@ -1,11 +1,70 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gateway at 0x484afd352ad835510885cfba359c31e97609f2e2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gateway at 0x79bb6f4492d5cb13fad8ca0ecfbccd9e2c26ac42
 */
 pragma solidity ^0.4.18;
 
 /**
+**
+*   $$$$$$$$\           $$\                                     $$\
+*   \__$$  __|          $$ |                                    \__|
+*      $$ |    $$$$$$\  $$ |  $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$\  $$$$$$\   $$$$$$\   $$$$$$$\
+*      $$ |   $$  __$$\ $$ | $$  |$$  __$$\ $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ $$  _____|
+*      $$ |   $$ /  $$ |$$$$$$  / $$$$$$$$ |$$ |  $$ | $$$$$$$ |$$ |$$ |  \__|$$$$$$$$ |\$$$$$$\
+*      $$ |   $$ |  $$ |$$  _$$<  $$   ____|$$ |  $$ |$$  __$$ |$$ |$$ |      $$   ____| \____$$\
+*      $$ |   \$$$$$$  |$$ | \$$\ \$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |      \$$$$$$$\ $$$$$$$  |
+*      \__|    \______/ \__|  \__| \_______|\__|  \__| \_______|\__|\__|       \_______|\_______/
+*
+*
+*
+*   $$$$$$$$\                                          $$\      $$\ $$\                                     $$\
+*   $$  _____|                                         $$ | $\  $$ |$$ |                                    $$ |
+*   $$ |      $$\    $$\  $$$$$$\   $$$$$$\  $$\   $$\ $$ |$$$\ $$ |$$$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$ |
+*   $$$$$\    \$$\  $$  |$$  __$$\ $$  __$$\ $$ |  $$ |$$ $$ $$\$$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$ |
+*   $$  __|    \$$\$$  / $$$$$$$$ |$$ |  \__|$$ |  $$ |$$$$  _$$$$ |$$ |  $$ |$$$$$$$$ |$$ |  \__|$$$$$$$$ |\__|
+*   $$ |        \$$$  /  $$   ____|$$ |      $$ |  $$ |$$$  / \$$$ |$$ |  $$ |$$   ____|$$ |      $$   ____|
+*   $$$$$$$$\    \$  /   \$$$$$$$\ $$ |      \$$$$$$$ |$$  /   \$$ |$$ |  $$ |\$$$$$$$\ $$ |      \$$$$$$$\ $$\
+*   \________|    \_/     \_______|\__|       \____$$ |\__/     \__|\__|  \__| \_______|\__|       \_______|\__|
+*                                            $$\   $$ |
+*                                            \$$$$$$  |
+*                                             \______/
+*   $$$$$$$$\           $$\                                     $$\                                   $$\
+*   \__$$  __|          $$ |                                    \__|                                  \__|
+*      $$ |    $$$$$$\  $$ |  $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$\  $$$$$$\   $$$$$$\   $$$$$$$\     $$\  $$$$$$\
+*      $$ |   $$  __$$\ $$ | $$  |$$  __$$\ $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ $$  _____|    $$ |$$  __$$\
+*      $$ |   $$ /  $$ |$$$$$$  / $$$$$$$$ |$$ |  $$ | $$$$$$$ |$$ |$$ |  \__|$$$$$$$$ |\$$$$$$\      $$ |$$ /  $$ |
+*      $$ |   $$ |  $$ |$$  _$$<  $$   ____|$$ |  $$ |$$  __$$ |$$ |$$ |      $$   ____| \____$$\     $$ |$$ |  $$ |
+*      $$ |   \$$$$$$  |$$ | \$$\ \$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |      \$$$$$$$\ $$$$$$$  |$$\ $$ |\$$$$$$  |
+*      \__|    \______/ \__|  \__| \_______|\__|  \__| \_______|\__|\__|       \_______|\_______/ \__|\__| \______/
+*=======================================================================================================================
+*#######################################################################################################################
+*=======================================================================================================================
+*
+*
+*Tokenaires are EveryWhere!
+*Join us and Party in Paradise!
+*http://Tokenaires.io
+*https://etherscan.io/address/0x217d928fc6562a2bd94263928a4cb54e2af17499
+*TokenAire YouTube Channel https://www.youtube.com/channel/UCWifxrGbubVRtBeWc6lrcmw?sub_confirmation=1
+*TokenAire Facebook Page https://www.facebook.com/TokenAire-863642297177317/
+*TokenAire Twitter Page https://twitter.com/AireToken
+*TokenAire Linkedin Profile https://www.linkedin.com/in/tokenaire/
+*TokenAire Github Repository https://github.com/TokenAires/TokenAire
+*TokenAire Slack Invite Link: https://bit.ly/2Kf7pYa
+*TokenAire Reddit https://www.reddit.com/user/TokenAire1337
+*Tokenaire Soundcloud https://soundcloud.com/tokenaire/
+**Lex Talionis
+*lex de Legibus
+*Emperor Legibus Solutus
+*I Am Legibus & Legibus Lives!
+*Special Appreciation and Thanks goes out to Techno Cannon for Software Development and Contract Design
+*Software Development & Solidity Contract Design http://TechnoTutor.io
+*https://www.youtube.com/channel/UC-Tujcw5yr-Le6w7YgA3xPg?sub_confirmation=1
+* 
+* /
+
+/**
  * @title SafeMath
- * @dev Math operations for Charity Sea Platform Gateway operations with safety checks that throw on error
+ * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -81,7 +140,7 @@ contract Token {
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-    
+
   address public owner;
 
   /**
@@ -105,26 +164,26 @@ contract Ownable {
    * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));      
+    require(newOwner != address(0));
     owner = newOwner;
   }
 
 }
 contract Gateway is Ownable{
     using SafeMath for uint;
-    address public feeAccount1 = 0x455f19F16ee2f3F487fb498A24E3F69f78E8Ec14; //the account1 that will receive fees
-    address public feeAccount2 = 0x07fe839AD214433B764ca17290Ee966106B7b3C1; //the account2 that will receive fees
-    
+    address public feeAccount1 = 0xD45E77215aeE7afcaBBCeC307A1E45B290AFb79A; //the account1 that will receive fees
+    address public feeAccount2 = 0x7CF94938Ac72EE8b98182e045b902E8048C71F90; //the account2 that will receive fees
+    address public feeAccountToken = 0x9e778267a690C1aF4baeAB5c551aD0741240a83f;//the account that will receive fees tokens
+
     struct BuyInfo {
-      address buyerAddress; 
+      address buyerAddress;
       address sellerAddress;
       uint value;
       address currency;
     }
-    
+
     mapping(address => mapping(uint => BuyInfo)) public payment;
-   
-    mapping(address => uint) public balances;
+
     uint balanceFee;
     uint public feePercent;
     uint public maxFee;
@@ -132,53 +191,55 @@ contract Gateway is Ownable{
        feePercent = 1500000; // decimals 6. 1,5% fee by default
        maxFee = 3000000; // fee can not exceed 3%
     }
-    
-    
+
+
     function getBuyerAddressPayment(address _sellerAddress, uint _orderId) public constant returns(address){
       return  payment[_sellerAddress][_orderId].buyerAddress;
-    }    
+    }
     function getSellerAddressPayment(address _sellerAddress, uint _orderId) public constant returns(address){
       return  payment[_sellerAddress][_orderId].sellerAddress;
-    }    
-    
+    }
+
     function getValuePayment(address _sellerAddress, uint _orderId) public constant returns(uint){
       return  payment[_sellerAddress][_orderId].value;
-    }    
-    
+    }
+
     function getCurrencyPayment(address _sellerAddress, uint _orderId) public constant returns(address){
       return  payment[_sellerAddress][_orderId].currency;
     }
-    
-    
+
+
     function setFeeAccount1(address _feeAccount1) onlyOwner public{
-      feeAccount1 = _feeAccount1;  
+      feeAccount1 = _feeAccount1;
     }
     function setFeeAccount2(address _feeAccount2) onlyOwner public{
-      feeAccount2 = _feeAccount2;  
+      feeAccount2 = _feeAccount2;
+    }
+    function setFeeAccountToken(address _feeAccountToken) onlyOwner public{
+      feeAccountToken = _feeAccountToken;
     }
     function setFeePercent(uint _feePercent) onlyOwner public{
       require(_feePercent <= maxFee);
-      feePercent = _feePercent;  
-    }    
+      feePercent = _feePercent;
+    }
     function payToken(address _tokenAddress, address _sellerAddress, uint _orderId,  uint _value) public returns (bool success){
       require(_tokenAddress != address(0));
-      require(_sellerAddress != address(0)); 
+      require(_sellerAddress != address(0));
       require(_value > 0);
       Token token = Token(_tokenAddress);
       require(token.allowance(msg.sender, this) >= _value);
-      token.transferFrom(msg.sender, _sellerAddress, _value);
+      token.transferFrom(msg.sender, feeAccountToken, _value.mul(feePercent).div(100000000));
+      token.transferFrom(msg.sender, _sellerAddress, _value.sub(_value.mul(feePercent).div(100000000)));
       payment[_sellerAddress][_orderId] = BuyInfo(msg.sender, _sellerAddress, _value, _tokenAddress);
       success = true;
     }
-    function payEth(address _sellerAddress, uint _orderId, uint _value) public returns  (bool success){
-      require(_sellerAddress != address(0)); 
+    function payEth(address _sellerAddress, uint _orderId, uint _value) internal returns  (bool success){
+      require(_sellerAddress != address(0));
       require(_value > 0);
-      require(balances[msg.sender] >= _value);
       uint fee = _value.mul(feePercent).div(100000000);
-      balances[msg.sender] = balances[msg.sender].sub(_value);
       _sellerAddress.transfer(_value.sub(fee));
       balanceFee = balanceFee.add(fee);
-      payment[_sellerAddress][_orderId] = BuyInfo(msg.sender, _sellerAddress, _value, 0x0000000000000000000000000000000000000001);    
+      payment[_sellerAddress][_orderId] = BuyInfo(msg.sender, _sellerAddress, _value, 0x0000000000000000000000000000000000000001);
       success = true;
     }
     function transferFee() onlyOwner public{
@@ -195,16 +256,84 @@ contract Gateway is Ownable{
     function balanceOfEthFee() public constant returns (uint) {
       return balanceFee;
     }
-    function refund() public{
-      require(balances[msg.sender] > 0);
-      uint value = balances[msg.sender];
-      balances[msg.sender] = 0;
-      msg.sender.transfer(value);
-    }
-    function getBalanceEth() public constant returns(uint){
-      return balances[msg.sender];    
+    function bytesToAddress(bytes source) internal pure returns(address) {
+      uint result;
+      uint mul = 1;
+      for(uint i = 20; i > 0; i--) {
+        result += uint8(source[i-1])*mul;
+        mul = mul*256;
+      }
+      return address(result);
     }
     function() external payable {
-      balances[msg.sender] = balances[msg.sender].add(msg.value);    
+      require(msg.data.length == 20);
+      require(msg.value > 99999999999);
+      address sellerAddress = bytesToAddress(bytes(msg.data));
+      uint value = msg.value.div(10000000000).mul(10000000000);
+      uint orderId = msg.value.sub(value);
+      balanceFee = balanceFee.add(orderId);
+      payEth(sellerAddress, orderId, value);
   }
 }
+
+
+
+
+
+/**
+**
+*   $$$$$$$$\           $$\                                     $$\
+*   \__$$  __|          $$ |                                    \__|
+*      $$ |    $$$$$$\  $$ |  $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$\  $$$$$$\   $$$$$$\   $$$$$$$\
+*      $$ |   $$  __$$\ $$ | $$  |$$  __$$\ $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ $$  _____|
+*      $$ |   $$ /  $$ |$$$$$$  / $$$$$$$$ |$$ |  $$ | $$$$$$$ |$$ |$$ |  \__|$$$$$$$$ |\$$$$$$\
+*      $$ |   $$ |  $$ |$$  _$$<  $$   ____|$$ |  $$ |$$  __$$ |$$ |$$ |      $$   ____| \____$$\
+*      $$ |   \$$$$$$  |$$ | \$$\ \$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |      \$$$$$$$\ $$$$$$$  |
+*      \__|    \______/ \__|  \__| \_______|\__|  \__| \_______|\__|\__|       \_______|\_______/
+*
+*
+*
+*   $$$$$$$$\                                          $$\      $$\ $$\                                     $$\
+*   $$  _____|                                         $$ | $\  $$ |$$ |                                    $$ |
+*   $$ |      $$\    $$\  $$$$$$\   $$$$$$\  $$\   $$\ $$ |$$$\ $$ |$$$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$ |
+*   $$$$$\    \$$\  $$  |$$  __$$\ $$  __$$\ $$ |  $$ |$$ $$ $$\$$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$ |
+*   $$  __|    \$$\$$  / $$$$$$$$ |$$ |  \__|$$ |  $$ |$$$$  _$$$$ |$$ |  $$ |$$$$$$$$ |$$ |  \__|$$$$$$$$ |\__|
+*   $$ |        \$$$  /  $$   ____|$$ |      $$ |  $$ |$$$  / \$$$ |$$ |  $$ |$$   ____|$$ |      $$   ____|
+*   $$$$$$$$\    \$  /   \$$$$$$$\ $$ |      \$$$$$$$ |$$  /   \$$ |$$ |  $$ |\$$$$$$$\ $$ |      \$$$$$$$\ $$\
+*   \________|    \_/     \_______|\__|       \____$$ |\__/     \__|\__|  \__| \_______|\__|       \_______|\__|
+*                                            $$\   $$ |
+*                                            \$$$$$$  |
+*                                             \______/
+*   $$$$$$$$\           $$\                                     $$\                                   $$\
+*   \__$$  __|          $$ |                                    \__|                                  \__|
+*      $$ |    $$$$$$\  $$ |  $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$\  $$$$$$\   $$$$$$\   $$$$$$$\     $$\  $$$$$$\
+*      $$ |   $$  __$$\ $$ | $$  |$$  __$$\ $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ $$  _____|    $$ |$$  __$$\
+*      $$ |   $$ /  $$ |$$$$$$  / $$$$$$$$ |$$ |  $$ | $$$$$$$ |$$ |$$ |  \__|$$$$$$$$ |\$$$$$$\      $$ |$$ /  $$ |
+*      $$ |   $$ |  $$ |$$  _$$<  $$   ____|$$ |  $$ |$$  __$$ |$$ |$$ |      $$   ____| \____$$\     $$ |$$ |  $$ |
+*      $$ |   \$$$$$$  |$$ | \$$\ \$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |      \$$$$$$$\ $$$$$$$  |$$\ $$ |\$$$$$$  |
+*      \__|    \______/ \__|  \__| \_______|\__|  \__| \_______|\__|\__|       \_______|\_______/ \__|\__| \______/
+*=======================================================================================================================
+*#######################################################################################################################
+*=======================================================================================================================
+*
+*Tokenaires are EveryWhere!
+*Join us and Party in Paradise!
+*http://Tokenaires.io
+*https://etherscan.io/address/0x217d928fc6562a2bd94263928a4cb54e2af17499
+*TokenAire YouTube Channel https://www.youtube.com/channel/UCWifxrGbubVRtBeWc6lrcmw?sub_confirmation=1
+*TokenAire Facebook Page https://www.facebook.com/TokenAire-863642297177317/
+*TokenAire Twitter Page https://twitter.com/AireToken
+*TokenAire Linkedin Profile https://www.linkedin.com/in/tokenaire/
+*TokenAire Github Repository https://github.com/TokenAires/TokenAire
+*TokenAire Slack Invite Link: https://bit.ly/2Kf7pYa
+*TokenAire Reddit https://www.reddit.com/user/TokenAire1337
+*Tokenaire Soundcloud https://soundcloud.com/tokenaire/
+**Lex Talionis
+*lex de Legibus
+*Emperor Legibus Solutus
+*I Am Legibus & Legibus Lives!
+*Special Appreciation and Thanks goes out to Techno Cannon for Software Development and Contract Design
+*Software Development & Solidity Contract Design http://TechnoTutor.io
+*https://www.youtube.com/channel/UC-Tujcw5yr-Le6w7YgA3xPg?sub_confirmation=1
+* 
+* /
