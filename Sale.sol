1fc97d6c2486c782ee1f2df8f2d4976ffa459291
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:58:15 +0700

    add contract Sale

--- Sale.sol
+++ Sale.sol
@@ -1,52 +1,130 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x249049df6dad05cace6939cbfb7b8b46ec1fff79
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Sale at 0x3c5753b9de8c49101155a9b31116b965a5d82574
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.21;
 
-interface token {
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+/*
+  
+    ****************************************************************
+    AVALANCHE BLOCKCHAIN GENESIS BLOCK COIN ALLOCATION SALE CONTRACT
+    ****************************************************************
+
+    The Genesis Block in the Avalanche will deploy with pre-filled addresses
+    according to the results of this token sale.
+    
+    The Avalanche tokens will be sent to the Ethereum address that buys them.
+    
+    When the Avalanche blockchain deploys, all ethereum addresses that contains
+    Avalanche tokens will be credited with the equivalent AVALANCHE ICE (XAI) in the Genesis Block.
+
+    There will be no developer premine. There will be no private presale. This is it.
+
+    @author CHRIS DCOSTA For Meek Inc 2018.
+    
+    Reference Code by Hunter Long
+    @repo https://github.com/hunterlong/ethereum-ico-contract
+
+*/
+
+
+contract ERC20 {
+  uint public totalSupply;
+  function balanceOf(address who) public constant returns (uint);
+  function allowance(address owner, address spender) public constant returns (uint);
+  function transfer(address to, uint value) public returns (bool ok);
+  function transferFrom(address from, address to, uint value) public returns (bool ok);
+  function approve(address spender, uint value) public returns (bool ok);
+  function mintToken(address to, uint256 value) public returns (uint256);
+  function changeTransfer(bool allowed) public;
 }
 
+
 contract Sale {
-    address private maintoken = 0x2054a15c6822a722378d13c4e4ea85365e46e50b;
-    address private owner = 0xabc45921642cbe058555361490f49b6321ed6989;
-    address private owner8 = 0x98b3540c7D95950f44ED9774f6cAf04bF76c368F;
-    address private owner6 = 0x4e76f947fA07B655F5e3e2cDD645E590C5D0875e;    
-    address private owner4 = 0x966c0FD16a4f4292E6E0372e04fbB5c7013AD02e;            
-    uint256 private sendtoken;
-    uint256 public cost1token = 0.0004 ether;
-    uint256 private ethersum;
-    uint256 private ethersum8;
-    uint256 private ethersum6;    uint256 private ethersum4;            token public tokenReward;
-    
-    function Sale() public {
-        tokenReward = token(maintoken);
+
+    uint256 public maxMintable;
+    uint256 public totalMinted;
+    uint public endBlock;
+    uint public startBlock;
+    uint public exchangeRate;
+    bool public isFunding;
+    ERC20 public Token;
+    address public ETHWallet;
+    uint256 public heldTotal;
+
+    bool private configSet;
+    address public creator;
+
+    event Contribution(address from, uint256 amount);
+
+    constructor(address _wallet) public {
+        startBlock = block.number; // imediate start 
+        maxMintable = 4045084999529091000000000000; // max sellable (18 decimals)
+        ETHWallet = _wallet; // 0x696863b0099394384cd595468b8b6270ea77fC68
+        isFunding = true;
+        creator = msg.sender;
+        exchangeRate = 13483;
     }
-    
-    function() external payable {
-        sendtoken = (msg.value)/cost1token;
-        if (msg.value >= 5 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*125/100;
-        }
-        if (msg.value >= 10 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*150/100;
-        }
-        if (msg.value >= 15 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*175/100;
-        }
-        if (msg.value >= 20 ether) {
-            sendtoken = (msg.value)/cost1token;
-            sendtoken = sendtoken*200/100;
-        }
-        tokenReward.transferFrom(owner, msg.sender, sendtoken);
-        
-        ethersum8 = (msg.value)*8/100;
-        ethersum6 = (msg.value)*6/100;    	ethersum4 = (msg.value)*4/100;    	    	    	
-    	    	    	ethersum = (msg.value)-ethersum8-ethersum6-ethersum4;    	    	        
-        owner8.transfer(ethersum8);
-        owner6.transfer(ethersum6);    	owner4.transfer(ethersum4);    	    	        owner.transfer(ethersum);
+
+    // setup function to be ran only 1 time
+    // setup token address
+    // setup end Block number
+    function setup(address token_address, uint end_block) public {
+        require(!configSet);
+        Token = ERC20(token_address);
+        endBlock = end_block;
+        configSet = true;
     }
+
+    function closeSale() external {
+      require(msg.sender==creator);
+      isFunding = false;
+    }
+
+    function () payable public {
+        require(msg.value>0);
+        require(isFunding);
+        require(block.number <= endBlock);
+        uint256 amount = msg.value * exchangeRate;
+        uint256 total = totalMinted + amount;
+        require(total<=maxMintable);
+        totalMinted += total;
+        ETHWallet.transfer(msg.value);
+        Token.mintToken(msg.sender, amount);
+        emit Contribution(msg.sender, amount);
+    }
+
+    // CONTRIBUTE FUNCTION
+    // converts ETH to Avalanche Genesis Block TOKEN and sends new Avalanche TOKEN to the sender
+    function contribute() external payable {
+        require(msg.value>0);
+        require(isFunding);
+        require(block.number <= endBlock);
+        uint256 amount = msg.value * exchangeRate;
+        uint256 total = totalMinted + amount;
+        require(total<=maxMintable);
+        totalMinted += total;
+        ETHWallet.transfer(msg.value);
+        Token.mintToken(msg.sender, amount);
+        emit Contribution(msg.sender, amount);
+    }
+
+    // update the ETH/XAIT rate
+    function updateRate(uint256 rate) external {
+        require(msg.sender==creator);
+        require(isFunding);
+        exchangeRate = rate;
+    }
+
+    // change creator address
+    function changeCreator(address _creator) external {
+        require(msg.sender==creator);
+        creator = _creator;
+    }
+
+    // change transfer ability for ERC20 token (toggle on/off) 
+    function changeTransferStats(bool _allowed) external {
+        require(msg.sender==creator);
+        Token.changeTransfer(_allowed);
+    }
+
 }
