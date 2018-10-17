2f771db396e4437a6b4eda5f6803f7257a86bf25
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:44:04 +0700

    add contract CypherX

--- CYPHERX.sol
+++ CYPHERX.sol
@@ -1,198 +1,260 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CYPHERX at 0x865b2c18b7f0d371a447b896350e0914793b1b72
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CypherX at 0x833dd1582ad893729c71aa36c3ebd083393a2433
 */
 pragma solidity ^0.4.12;
 
-
-//Current version:0.4.24+commit.e67f0147
 /**
- * Math operations with safety checks
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
  */
-contract SafeMath {
-  function safeMul(uint256 a, uint256 b) internal returns (uint256) {
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b > 0);
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
-    assert(a == b * c + a % b);
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function safeSub(uint256 a, uint256 b) internal returns (uint256) {
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function safeAdd(uint256 a, uint256 b) internal returns (uint256) {
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a + b;
-    assert(c>=a && c>=b);
+    assert(c >= a);
     return c;
   }
+}
 
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
-    }
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 }
 
-contract ERC223Interface {
-      
-      event Transfer(address indexed _from, address indexed _to, uint256 _value);  
-      event TokenFallback(address _from, uint256 _value, bytes _data);
-
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract ERC223ReceivingContract { 
 
-    function tokenFallback(address _from, uint256 _value, bytes _data);
-    event TokenFallback(address _from, uint _value, bytes _data);
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
+    return balances[_owner];
+  }
 }
 
-contract owned {
-    address public owner;
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    function owned() public {
-        owner = msg.sender;
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+
+    uint256 _allowance = allowed[_from][msg.sender];
+
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  /**
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   */
+  function increaseApproval (address _spender, uint _addedValue)
+    returns (bool success) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval (address _spender, uint _subtractedValue)
+    returns (bool success) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
     }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+}
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
 
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is StandardToken {
+
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value > 0);
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
     }
 }
-contract CYPHERX is SafeMath,owned,ERC223Interface{
-    string  public name;
-    string  public symbol;
-    uint8   public decimals=8;
-    uint256 public totalSupply;
-    uint256 public soldToken;
 
+contract CypherX is BurnableToken, Ownable {
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-	mapping (address => uint256) public freezeOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+    string public constant name = "CypherX";
+    string public constant symbol = "CRX";
+    uint public constant decimals = 8;
+	uint256 public constant initialSupply = 1000000000 * (10 ** uint256(decimals));
 
-    /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed _from,address indexed _to,uint256 _value,bytes _data);
-
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-	
-	/* This notifies clients about the amount frozen */
-    event Freeze(address indexed from, uint256 value);
-	
-	/* This notifies clients about the amount unfrozen */
-    event Unfreeze(address indexed from, uint256 value);
-    
-   /* Initializes contract with initial supply tokens to the creator of the contract */
-    function CYPHERX() 
-    {
-        // Give the creator all initial tokens
-        
-        totalSupply = 1000000000 *10**uint256(decimals);    // Update total supply
-        balanceOf[msg.sender] = totalSupply; 
-        name = "CYPHERX";                                   // Set the name for display purposes
-        symbol = "CYX";                                  // Set the symbol for display purposes
-        soldToken=0;                                    // Amount of decimals for display purposes
-
-    }
-
-    /* Send tokens */
-    function transfer(address _to, uint256 _value) 
-    {
-        uint codeLength;
-        bytes memory empty;
-
-        assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_to)
-        }
-        if (_value <= 0) throw;
-        if(balanceOf[msg.sender] < _value) revert();
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();
-        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);
-        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);
-        if(codeLength>0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-	
-            receiver.tokenFallback(msg.sender, _value, empty);
-        }
-        soldToken+=_value;
-        Transfer(msg.sender, _to, _value);
-    }
-        
-     /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value) returns (bool success) 
-    {
-		if (_value <= 0) throw; 
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-       
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) 
-    {
-        if (_value <= 0) throw; 
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;     // Check allowance
-        balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);          // Subtract from the sender
-        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);           // Add the same to the recipient
-        allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
-        soldToken +=_value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function burn(uint256 _value) returns (bool success) 
-    {
-        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
-		if (_value <= 0) throw; 
-        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                      // Subtract from the sender
-        totalSupply = SafeMath.safeSub(totalSupply,_value);                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-	
-	function freeze(address _freeze,uint256 _value) onlyOwner returns (bool success) 
-	{
-        if (balanceOf[_freeze] < _value) throw;            // Check if the sender has enough
-	    if (_value <= 0) throw; 
-        balanceOf[_freeze] = SafeMath.safeSub(balanceOf[_freeze], _value);                      // Subtract from the sender
-        freezeOf[_freeze] = SafeMath.safeAdd(freezeOf[_freeze], _value);                                // Updates totalSupply
-        Freeze(_freeze, _value);
-        return true;
-    }
-    
-    function unfreeze(address _unfreeze,uint256 _value) onlyOwner returns (bool success)
-    {
-        if (freezeOf[_unfreeze] < _value) throw;            // Check if the sender has enough
-		if (_value <= 0) throw; 
-        freezeOf[_unfreeze] = SafeMath.safeSub(freezeOf[_unfreeze], _value);                      // Subtract from the sender
-		balanceOf[_unfreeze] = SafeMath.safeAdd(balanceOf[_unfreeze], _value);
-        Unfreeze(_unfreeze, _value);
-        return true;
-    }
-    
-    function mintToken(address target, uint256 mintedAmount) onlyOwner public 
-    {
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        Transfer(0, this, mintedAmount);
-        Transfer(this, target, mintedAmount);
-    }
-	
-	// can accept ether
-	function() payable {
+    // Constructor
+    function CypherX() {
+        totalSupply = initialSupply;
+        balances[msg.sender] = initialSupply; // Send all tokens to owner
     }
 }
