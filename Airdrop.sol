c5c312816147e1194b3ed811df4002d4c064aac9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 11:53:05 +0700

    add contract Airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,177 +1,56 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x9c4f63aa33c477774faf43299eda660664e7663d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x69bc8ce0ca03d6874f3a84732b5dea5ed5b8a6a3
 */
-// Congratulations! Its your free airdrop token! Get 6 USD in EToken FREE!
-// Promocode: 6forfree
-// More: tokensale.endo.im/promo/
-// Join us: https://t.me/endo_en
-// ENDO is a Protocol that solves the problem of certified information tracking and encrypted data storage. 
-// The ENDO ecosystem allows organisations and users to participate in information and service exchange through the EToken.
-
-// ?????????????????????????????????????EToken???6???????????????
-// ???????????6forfree?
-// ???????tokensale.endo.im/promo/
-// ??????Telegram?????????????https://t.me/endo_jp?
-// ENDO???????????????????????????????????????????? 
-// ENDO?????????????????EToken????????????????????????
-
-// ??? ??????????? ????6???EToken?
-// ?????6forfree
-// ???tokensale.endo.im/promo/
-// ?????https://t.me/endo_cn
-// ENDO????????????????????????
-// ENDO?????????????EToken??????????
-
-// ?????! ?? ???? ??! EToken?? 6 ????!
-// ???? ?? : 6forfree
-// ??? : tokensale.endo.im/promo/
-// ??? ?????? : https://t.me/endo_ko
-// ENDO? ??? ???? ???? ??? ? ??? ?? ???? ???.
-// ENDO ???? ??? ???? ??? ? ? ????.
-
-// ???????????! ??? ???????????? Airdrop ??? ?????! ???????? 6 USD ? ??????????? EToken ?????????!
-// ????????: 6forfree
-// ?????? ??????: tokensale.endo.im/promo/
-// ??????????????? ? ???: https://t.me/endo_ru
-// ENDO – ??? ????????, ???????? ???????? ???????????? ?????????????? ?????????? ? ???????? ????????????? ??????. 
-// ?????????? ENDO ????????? ???????????? ? ????????????? ????????? ??????? ? ???????? ?????? ??????????? ? ???????????? ???????? ? ??????? ?????? ENDO.
-
-
-pragma solidity ^0.4.11;
-
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a / b;
-    return c;
-  }
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+contract ERC20 {
+  function transfer(address _recipient, uint256 _value) public returns (bool success);
 }
 
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public returns (uint256);
-  //function transfer(address to, uint256 value) public returns(bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public returns (uint256);
-  //function transferFrom(address from, address to, uint256 value) public returns(bool);
-  //function approve(address spender, uint256 value) public returns(bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-  mapping(address => uint256) balances;
-  function transfer(address _to, uint256 _value) public {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-  }
-  function balanceOf(address _owner) public returns (uint256 balance) {
-    return balances[_owner];
-  }
-}
-contract StandardToken is ERC20, BasicToken {
-  mapping (address => mapping (address => uint256)) allowed;
-  function transferFrom(address _from, address _to, uint256 _value) public {
-    var _allowance = allowed[_from][msg.sender];
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    Transfer(_from, _to, _value);
-  }
-  function approve(address _spender, uint256 _value) public {
-    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) revert();
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-  }
-  function allowance(address _owner, address _spender) public returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-  }
-}
 contract Ownable {
   address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
   function Ownable() public {
     owner = msg.sender;
   }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
   modifier onlyOwner() {
-    if (msg.sender != owner) {
-      revert();
-    }
+    require(msg.sender == owner);
     _;
   }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
   function transferOwnership(address newOwner) public onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-}
-contract MintableToken is StandardToken, Ownable {
-  event Mint(address indexed to, uint256 amount);
-  event MintFinished();
-
-  string public name = "ENDO.network Promo Token";
-  string public symbol = "ETP";
-  uint256 public decimals = 18;
-
-  bool public mintingFinished = false;
-
-  modifier canMint() {
-    if(mintingFinished) revert();
-    _;
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
-    return true;
-  }
-
-  function finishMinting() onlyOwner public returns (bool) {
-    mintingFinished = true;
-    MintFinished();
-    return true;
-  }
 }
 
-contract Airdrop {
-  using SafeMath for uint256;
-
-  MintableToken public token;
-  
-  uint256 public currentTokenCount;
-  address public owner;
-  uint256 public maxTokenCount;
-
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-  function Airdrop() public {
-    token = createTokenContract();
-    owner = msg.sender;
-  }
-  
-  function sendToken(address[] recipients, uint256 values) public {
+contract Airdrop is Ownable {
+    
+  function distributeBulk(ERC20 token, address[] recipients, uint256[] values) onlyOwner public {
     for (uint256 i = 0; i < recipients.length; i++) {
-      token.mint(recipients[i], values);
+      token.transfer(recipients[i], values[i]);
     }
   }
-
-  function createTokenContract() internal returns (MintableToken) {
-    return new MintableToken();
+  
+  function distribute(ERC20 token, address recipient, uint256 value) onlyOwner public {
+      token.transfer(recipient, value);
   }
-
 }
