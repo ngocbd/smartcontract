98d4e839164c090b427a528a0e639dd20b332805
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:16:07 +0700

    add contract ipfsreg

--- ipfsreg.sol
+++ ipfsreg.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ipfsreg at 0x7f724a34c16e5c7bca2c3f792a6396cf8d7d7298
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ipfsreg at 0x8e717edb97a914915dc0b0fab9bc8fcb6fe1e658
 */
 pragma solidity ^0.4.24;
 
@@ -18,7 +18,7 @@ contract ipfsreg {
         return publicResolver.text(namehash(_name), "tinyipfs");
     }
     
-    function namehash(string _name) internal pure returns (bytes32 node) {
+    function namehash(string _name) public pure returns (bytes32 node) {
         node = 0x0000000000000000000000000000000000000000000000000000000000000000;
         node = keccak256(
         abi.encodePacked(node, keccak256(abi.encodePacked('eth')))
