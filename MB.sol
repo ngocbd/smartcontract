f05cf3a81e8ae58f901a0dd358e00c8cdc7d61fa
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 04:40:39 +0700

    add contract MB

--- MB.sol
+++ MB.sol
@@ -1,108 +1,88 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MB at 0x90b1b771d0814d607da104b988efa39288219d62
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MB at 0x7ab1fc79f319718690e9c883bac910f8e289ce8f
 */
-pragma solidity ^0.4.13;
-
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+pragma solidity ^0.4.20;
 
 contract MB {
-    /* Public variables of the token */
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+  string public name = "MicroChain";
+  string public symbol = "MB";
+  uint public decimals = 18;
+  uint public INITIAL_SUPPLY = 100000000000000000000000000000;
+    
+  mapping(address => uint) balances;
+  mapping (address => mapping (address => uint)) allowed;
+  uint256 public _totalSupply;
+  address public _creator;
+  bool bIsFreezeAll = false;
+  
+  event Transfer(address indexed from, address indexed to, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
+  
+  function safeSub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+  function safeAdd(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
+  
+  function totalSupply() public constant returns (uint256 total) {
+	total = _totalSupply;
+  }
 
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
+  function transfer(address _to, uint _value) public returns (bool success) {
+    require(bIsFreezeAll == false);
+    balances[msg.sender] = safeSub(balances[msg.sender], _value);
+    balances[_to] = safeAdd(balances[_to], _value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MB(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-        ) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
-    }
+  function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+    require(bIsFreezeAll == false);
+    var _allowance = allowed[_from][msg.sender];
+    balances[_to] = safeAdd(balances[_to], _value);
+    balances[_from] = safeSub(balances[_from], _value);
+    allowed[_from][msg.sender] = safeSub(_allowance, _value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
 
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(_from, _to, _value);
-    }
+  function balanceOf(address _owner) public constant returns (uint balance) {
+    return balances[_owner];
+  }
 
-    /// @notice Send `_value` tokens to `_to` from your account
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
-    function transfer(address _to, uint256 _value) {
-        _transfer(msg.sender, _to, _value);
-    }
+  function approve(address _spender, uint _value) public returns (bool success) {
+	require(bIsFreezeAll == false);
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
 
-    /// @notice Send `_value` tokens to `_to` in behalf of `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value the amount to send
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        require (_value < allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
+  function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+    return allowed[_owner][_spender];
+  }
 
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
+  function freezeAll() public 
+  {
+	require(msg.sender == _creator);
+	bIsFreezeAll = !bIsFreezeAll;
+  }
+  
+  function MB() public {
+    _totalSupply = INITIAL_SUPPLY;
+	_creator = 0xf2F91C1C681816eE275ce9b4366D5a906da6eBf5;
+	balances[_creator] = INITIAL_SUPPLY;
+	bIsFreezeAll = false;
+  }
+  
+  function destroy() public  {
+	require(msg.sender == _creator);
+	selfdestruct(_creator);
+  }
 
-    /// @notice Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-    /// @param _spender The address authorized to spend
-    /// @param _value the max amount they can spend
-    /// @param _extraData some extra information to send to the approved contract
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }        
-
-    /// @notice Remove `_value` tokens from the system irreversibly
-    /// @param _value the amount of money to burn
-    function burn(uint256 _value) returns (bool success) {
-        require (balanceOf[msg.sender] > _value);            // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
-        totalSupply -= _value;                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
-        return true;
-    }
 }
