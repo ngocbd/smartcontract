fcae35adde3470fd68e16e94e2ca7855b862a761
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:17:37 +0700

    add contract VotingToken

--- VotingToken.sol
+++ VotingToken.sol
@@ -1,51 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VotingToken at 0x272f022a374fc7e9213b35952e167ea217303e18
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract VotingToken at 0xea8b30b0bf4aefb965a8fdf0ccacafd17ad740ad
 */
-pragma solidity ^0.4.19;
+pragma solidity 0.4.24;
 
-
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
-
-
-
-contract Owned {
-    address public owner;
-
-    event OwnershipTransferred(address indexed from, address indexed to);
-
-    function Owned() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function setOwner(address _newOwner) public onlyOwner {
-        owner = _newOwner;
-        OwnershipTransferred(owner, _newOwner);
-    }
-}
-
-
-
-// ----------------------------------------------------------------------------
-// Safe maths
-// ----------------------------------------------------------------------------
 library SafeMath {
     function add(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
@@ -65,179 +22,223 @@ library SafeMath {
     }
 }
 
+contract Owned {
+    address public owner;
 
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-contract VotingToken is ERC20Interface, Owned {
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract ERC20 {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract StandardToken is ERC20 {
     using SafeMath for uint;
 
-
-    // ------------------------------------------------------------------------
-    // ERC 20 fields
-    // ------------------------------------------------------------------------
-    string public symbol;
     string public name;
+    string public symbol;
     uint8 public decimals;
     uint public totalSupply;
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+    mapping(address => uint) internal balances;
+    mapping (address => mapping (address => uint)) internal allowed;
 
-    // ------------------------------------------------------------------------
-    // Fields required for the referendum
-    // ------------------------------------------------------------------------
-    Description public description;
-    Props public props;
-    Reward public reward;
-    bool public open;
-    
-    struct Description {
-        string question;
-        string firstProp;
-        string secondProp;
-    }
-
-    struct Props {
-        address firstPropAddress;
-        address secondPropAddress;
-        address blankVoteAddress;
-    }
-
-    struct Reward {
-        address tokenAddress;
-        address refundWalletAddress; 
-    }
-
-    event VoteRewarded(address indexed to, uint amount);
-    event Finish(string question, 
-        string firstProp, uint firstPropCount, 
-        string secondProp, uint secondPropCount, uint blankVoteCount);
-
-
-    // ------------------------------------------------------------------------
-    // Constructor
-    // ------------------------------------------------------------------------
-    function VotingToken(
-        string _symbol, string _name, uint _totalSupply, 
-        string _question, string _firstProp, string _secondProp,
-        address _firstPropAddress, address _secondPropAddress, address _blankVoteAddress,
-        address _tokenAddress) public {
-
-        symbol = _symbol;
+    constructor(string _name, string _symbol, uint8 _decimals, uint _totalSupply) public {
         name = _name;
-        decimals = 8;
+        symbol = _symbol;
+        decimals = _decimals;
         totalSupply = _totalSupply;
-        balances[owner] = _totalSupply;
-        Transfer(address(0), owner, totalSupply);
-
-        description = Description(_question, _firstProp, _secondProp);
-        props = Props(_firstPropAddress, _secondPropAddress, _blankVoteAddress);
-        reward = Reward(_tokenAddress, owner);
-        open = true;
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
-    function close() public onlyOwner returns (bool success) {
-        require(open);
-        open = false;
-        Finish(description.question, 
-            description.firstProp, balanceOf(props.firstPropAddress), 
-            description.firstProp, balanceOf(props.secondPropAddress), 
-            balanceOf(props.blankVoteAddress));
+    function totalSupply() public view returns (uint) {
+        return totalSupply;
+    }
 
-        ERC20Interface rewardToken = ERC20Interface(reward.tokenAddress);
-        uint leftBalance = rewardToken.balanceOf(address(this));
-        rewardToken.transfer(reward.refundWalletAddress, leftBalance);
+    function balanceOf(address _owner) public view returns (uint) {
+        return balances[_owner];
+    }
 
+    function allowance(address _owner, address _spender) public view returns (uint) {
+        return allowed[_owner][_spender];
+    }
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function updateRefundWalletAddress(address _wallet) public onlyOwner returns (bool success) {
-        reward.refundWalletAddress = _wallet;
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
-    function getResults() public view returns (uint firstPropCount, uint secondPropCount, uint blankVoteCount) {
-        return (
-            balanceOf(props.firstPropAddress), 
-            balanceOf(props.secondPropAddress), 
-            balanceOf(props.blankVoteAddress));
-    }
-
-    function totalSupply() public constant returns (uint) {
-        return totalSupply - balances[address(0)];
-    }
-
-    function balanceOf(address _tokenOwner) public constant returns (uint balance) {
-        return balances[_tokenOwner];
-    }
-
-    function rewardVote(address _from, address _to, uint _tokens) private {
-        if(_to == props.firstPropAddress || 
-           _to == props.secondPropAddress || 
-           _to == props.blankVoteAddress) {
-            ERC20Interface rewardToken = ERC20Interface(reward.tokenAddress);
-            uint rewardTokens = _tokens.div(100);
-            rewardToken.transfer(_from, rewardTokens);
-            VoteRewarded(_from, _tokens);
-        }
-    }
-
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to `to` account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        return transferFrom(msg.sender, to, tokens);
-    }
-
-    // ------------------------------------------------------------------------
-    // Transfer `tokens` from the `from` account to the `to` account
-    // 
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the `from` account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        require(open);
-        balances[from] = balances[from].sub(tokens);
-        if(from != msg.sender) {
-            allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-        }
-        balances[to] = balances[to].add(tokens);
-        Transfer(from, to, tokens);
-        rewardVote(from, to, tokens);
+    function approve(address _spender, uint _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces 
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        require(open);
-        allowed[msg.sender][spender] = tokens;
-        Approval(msg.sender, spender, tokens);
+}
+
+/**
+ * @title SwissBorg Referendum 2
+ * @dev Hardcoded version with exactly 6 voting addresses.
+ */
+contract VotingToken is StandardToken, Owned {
+    using SafeMath for uint;
+
+    uint public constant numberOfAlternatives = 6;
+
+    event Reward(address indexed to, uint amount);
+    event Result(address indexed votingAddress, uint amount);
+
+    ERC20 private rewardToken;
+
+    bool public opened;
+    bool public closed;
+
+    address[numberOfAlternatives] public votingAddresses;
+
+    // ~~~~~ Constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    constructor(
+        string _name,
+        string _symbol,
+        uint8 _decimals,
+        ERC20 _rewardToken,
+        address[numberOfAlternatives] _votingAddresses
+    ) public StandardToken(_name, _symbol, _decimals, 0) {
+        require(_votingAddresses.length == numberOfAlternatives);
+        rewardToken = _rewardToken;
+        votingAddresses = _votingAddresses;
+    }
+
+    // ~~~~~ Public Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        require(super.transfer(_to, _value));
+        _rewardVote(msg.sender, _to, _value);
         return true;
     }
 
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        return allowed[tokenOwner][spender];
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        require(super.transferFrom(_from, _to, _value));
+        _rewardVote(_from, _to, _value);
+        return true;
     }
 
-    // ------------------------------------------------------------------------
-    // Don't accept ETH
-    // ------------------------------------------------------------------------
+    // Refuse ETH
     function () public payable {
         revert();
     }
+
+    // ~~~~~ Admin Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    function mint(address _to, uint _amount) onlyOwner external returns (bool) {
+        require(!opened);
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    function batchMint(address[] _tos, uint[] _amounts) onlyOwner external returns (bool) {
+        require(!opened);
+        require(_tos.length == _amounts.length);
+        uint sum = 0;
+        for (uint i = 0; i < _tos.length; i++) {
+            address to = _tos[i];
+            uint amount = _amounts[i];
+            sum = sum.add(amount);
+            balances[to] = balances[to].add(amount);
+            emit Transfer(address(0), to, amount);
+        }
+        totalSupply = totalSupply.add(sum);
+        return true;
+    }
+
+    function open() onlyOwner external {
+        require(!opened);
+        opened = true;
+    }
+
+    function close() onlyOwner external {
+        require(opened && !closed);
+        closed = true;
+    }
+
+    function destroy(address[] tokens) onlyOwner external {
+
+        // Transfer tokens to owner
+        for (uint i = 0; i < tokens.length; i++) {
+            ERC20 token = ERC20(tokens[i]);
+            uint balance = token.balanceOf(this);
+            token.transfer(owner, balance);
+        }
+
+        for (uint j = 0; j < numberOfAlternatives; j++) {
+            address votingAddress = votingAddresses[j];
+            uint votes = balances[votingAddress];
+            emit Result(votingAddress, votes);
+        }
+
+        // Transfer Eth to owner and terminate contract
+        selfdestruct(owner);
+    }
+
+    // ~~~~~ Private Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    function _rewardVote(address _from, address _to, uint _value) private {
+        if(_isVotingAddress(_to)) {
+            require(opened && !closed);
+            uint rewardTokens = _value.div(100);
+            require(rewardToken.transfer(_from, rewardTokens));
+            emit Reward(_from, _value);
+        }
+    }
+
+    function _isVotingAddress(address votingAddress) private view returns (bool) {
+        for (uint i = 0; i < numberOfAlternatives; i++) {
+            if (votingAddresses[i] == votingAddress) return true;
+        }
+        return false;
+    }
+
 }
