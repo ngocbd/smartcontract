5fecb82588e0aee99ff0d586d2ad6ff4ec9c169f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:59:02 +0700

    add contract TXToken

--- TXToken.sol
+++ TXToken.sol
@@ -1,132 +1,203 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TXToken at 0xc130aeeeb24a43f806da39cb0796bf37be9c7ebf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TXToken at 0x609bf627cd14d9af834ad75271d3d7ab1cfe2268
 */
-pragma solidity ^0.4.11;
-
-library SafeMath {
-	function mul(uint a, uint b) internal returns(uint) {
-		uint c = a * b;
-		assert(a == 0 || c / a == b);
-		return c;
-	}
-
-	function div(uint a, uint b) internal returns(uint) {
-		uint c = a / b;
-		return c; 
-	}
-
-	function sub(uint a, uint b) internal returns(uint) {
-		assert(b <= a);
-		return a - b;
-	}
-
-	function add(uint a, uint b) internal returns(uint) {
-		uint c = a + b;
-		assert(c >= a);
-		return c;
-	}
-	function max64(uint64 a, uint64 b) internal constant returns(uint64) {
-		return a >= b ? a : b;
-	}
-
-	function min64(uint64 a, uint64 b) internal constant returns(uint64) {
-		return a < b ? a : b;
-	}
-
-	function max256(uint256 a, uint256 b) internal constant returns(uint256) {
-		return a >= b ? a : b;
-	}
-
-	function min256(uint256 a, uint256 b) internal constant returns(uint256) {
-		return a < b ? a : b;
-	}
-
-	function assert(bool assertion) internal {
-		if(!assertion) {
-			throw;
-		}
-	}
-}
-
+pragma solidity ^0.4.18;
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
 contract ERC20Basic {
-	uint public totalSupply;
-	function balanceOf(address who) constant returns(uint);
-	function transfer(address to, uint value);
-	event Transfer(address indexed from, address indexed to, uint value);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
-
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
 contract BasicToken is ERC20Basic {
-	using SafeMath 	for uint;
-	mapping(address => uint) balances;
-
-	modifier onlyPayloadSize(uint size) {
-		if(msg.data.length < size + 4) {
-			throw;
-		}
-		_;
-	}
-
-	function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) {
-		balances[msg.sender] = balances[msg.sender].sub(_value);
-		balances[_to] = balances[_to].add(_value);
-		Transfer(msg.sender, _to, _value);
-	}
-
-	function balanceOf(address _owner) constant returns(uint balance) {
-		return balances[_owner];
-	}
-
+  using SafeMath for uint256;
+  mapping(address => uint256) balances;
+  uint256 totalSupply_;
+  /**
+  * @dev total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
 }
-
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
 contract ERC20 is ERC20Basic {
-	function allowance(address owner, address spender) constant returns(uint);
-	function transferFrom(address from, address to, uint value);
-	function approve(address spender, uint value);
-	event Approval(address indexed owner, address indexed spender, uint value);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
-contract StandardToken is BasicToken, ERC20 {
-	mapping(address => mapping(address => uint)) allowed;
-	function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
-		var _allowance = allowed[_from][msg.sender];
-		balances[_to] = balances[_to].add(_value);
-		balances[_from] = balances[_from].sub(_value);
-		allowed[_from][msg.sender] = _allowance.sub(_value);
-		Transfer(_from, _to, _value);
-	}
-
-	function approve(address _spender, uint _value) {
-		if((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
-		allowed[msg.sender][_spender] = _value;
-		Approval(msg.sender, _spender, _value);
-	}
-
-	function allowance(address _owner, address _spender) constant returns(uint remaining) {
-		return allowed[_owner][_spender];
-	}
-
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+  mapping (address => mapping (address => uint256)) internal allowed;
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
 }
-
 contract TXToken is StandardToken {
-	string public constant symbol = "TX";
-	string public constant name = "Tian Xiao";
-	uint8 public constant decimals = 18;
-	address public target;
-	
-	event InvalidCaller(address caller);
-
-	modifier onlyOwner {
-		if(target == msg.sender) {
-			_;
-		} else {
-			InvalidCaller(msg.sender);
-			throw;
-		}
-	}
-	function TXToken(address _target) {
-		target = _target;
-		totalSupply = 1000000 * 10 ** 18;
-		balances[target] = totalSupply;
-	}
-
+    string  public name = "TangXuToken";
+    string  public symbol = "TXT";
+    uint8   public decimals = 18;
+    uint    public totalSupply = 10 ** 27;
+    function ()
+        payable
+        public
+    {
+        revert();
+    }
+    function TXToken() public {
+        balances[msg.sender] = totalSupply;
+    }
 }
