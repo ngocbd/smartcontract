e5533ccc0c3f17ba26f944f7a897b12a3f47e375
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 05:32:07 +0700

    add contract AppCoinsIAB

--- AppCoinsIAB.sol
+++ AppCoinsIAB.sol
@@ -1,32 +1,78 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AppCoinsIAB at 0xb015d9bbabc472bbfc990ed6a0c961a90a482c57
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AppCoinsIAB at 0x1bd638b7e61db79bde3233c113ebd87e17e16de4
 */
 pragma solidity ^0.4.19;
 
 contract AppCoins {
     mapping (address => mapping (address => uint256)) public allowance;
-    function balanceOf (address _owner) public constant returns (uint256);
+    function balanceOf (address _owner) public view returns (uint256);
     function transferFrom(address _from, address _to, uint256 _value) public returns (uint);
 }
 
 contract AppCoinsIABInterface {
-    function division(uint numerator, uint denominator) public constant returns (uint);
-    function buy(uint _amount, string _sku, address _addr_appc, address _dev, address _appstore, address _oem) public constant returns (bool);
+    function division(uint numerator, uint denominator) public view returns (uint);
+    function buy(string _packageName, string _sku, uint256 _amount, address _addr_appc, address _dev, address _appstore, address _oem, bytes2 _countryCode) public view returns (bool);
 }
 
 contract AppCoinsIAB is AppCoinsIABInterface {
+
     uint public dev_share = 85;
     uint public appstore_share = 10;
     uint public oem_share = 5;
 
-    event Buy(uint _amount, string _sku, address _from, address _dev, address _appstore, address _oem);
+    mapping (address => bool) allowedAddresses;
+    address owner;
 
-    function division(uint numerator, uint denominator) public constant returns (uint) {
-        uint _quotient = numerator / denominator;
-        return _quotient;
+    modifier onlyAllowedAddress(string _funcName) {
+        if(!allowedAddresses[msg.sender]){
+            emit Error(_funcName, "Operation can only be performed by allowed Addresses");
+            return;
+        }
+        _;
     }
 
-    function buy(uint256 _amount, string _sku, address _addr_appc, address _dev, address _appstore, address _oem) public constant returns (bool) {
+    modifier onlyOwner(string _funcName) {
+        if(owner != msg.sender){
+            emit Error(_funcName, "Operation can only be performed by contract owner");
+            return;
+        }
+        _;
+    }
+
+
+    event Buy(string packageName, string _sku, uint _amount, address _from, address _dev, address _appstore, address _oem, bytes2 countryCode);
+    event Error(string func, string message);
+    event OffChainBuy(address _wallet, bytes32 _rootHash);
+
+    function AppCoinsIAB() public {
+        owner = msg.sender;
+    }
+
+    function addAllowedAddress(address _account) public onlyOwner("addAllowedAddress"){
+        allowedAddresses[_account] = true;
+    }
+
+    function removeAllowedAddress(address _account) public onlyOwner("removeAllowedAddress") {
+        allowedAddresses[_account] = false;
+    }
+
+    function informOffChainBuy(address[] _walletList, bytes32[] _rootHashList) public onlyAllowedAddress("informOffChainTransaction") {
+        if(_walletList.length != _rootHashList.length){
+            emit Error("informOffChainTransaction", "Wallet list and Roothash list must have the same lengths");
+            return;
+        }
+        for(uint i = 0; i < _walletList.length; i++){
+            emit OffChainBuy(_walletList[i],_rootHashList[i]);
+        }
+    }
+
+    function division(uint _numerator, uint _denominator) public view returns (uint) {
+        uint quotient = _numerator / _denominator;
+        return quotient;
+    }
+
+
+    function buy(string _packageName, string _sku, uint256 _amount, address _addr_appc, address _dev, address _appstore, address _oem, bytes2 _countryCode) public view returns (bool) {
         require(_addr_appc != 0x0);
         require(_dev != 0x0);
         require(_appstore != 0x0);
@@ -34,7 +80,10 @@ contract AppCoinsIAB is AppCoinsIABInterface {
 
         AppCoins appc = AppCoins(_addr_appc);
         uint256 aux = appc.allowance(msg.sender, address(this));
-        require(aux >= _amount);
+        if(aux < _amount){
+            emit Error("buy","Not enough allowance");
+            return false;
+        }
 
         uint[] memory amounts = new uint[](3);
         amounts[0] = division(_amount * dev_share, 100);
@@ -45,7 +94,7 @@ contract AppCoinsIAB is AppCoinsIABInterface {
         appc.transferFrom(msg.sender, _appstore, amounts[1]);
         appc.transferFrom(msg.sender, _oem, amounts[2]);
 
-        Buy(_amount, _sku, msg.sender, _dev, _appstore, _oem);
+        emit Buy(_packageName, _sku, _amount, msg.sender, _dev, _appstore, _oem, _countryCode);
 
         return true;
     }
