71e52d22d48eb5441c603ab7c1425df9800a8dfb
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:01:29 +0700

    add contract GROWCHAIN

--- GROWCHAIN.sol
+++ GROWCHAIN.sol
@@ -1,145 +1,156 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GROWCHAIN at 0xe87ff5509c89556b40774ff9eba1d518a39f7e26
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract GROWCHAIN at 0xcd28a4699b15186510acd5a36257b19e86c0940b
 */
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.16;
 
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint256 supply);
-    function balance() public constant returns (uint256);
-    function balanceOf(address _owner) public constant returns (uint256);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
+contract GROWCHAIN {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-// GROWCHAIN
-// YOU get a GROWCHAIN, and YOU get a GROWCHAIN, and YOU get a GROWCHAIN!
-contract GROWCHAIN is ERC20Interface {
-    string public constant symbol = "GROW";
-    string public constant name = "GROW CHAIN";
-    uint8 public constant decimals = 2;
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    uint256 _totalSupply = 0;
-    uint256 _airdropAmount = 188 * 10 ** uint256(decimals);
-    uint256 _cutoff = 1000000 * 10 ** uint256(decimals);
-    uint256 _outAmount = 0;
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    mapping(address => uint256) balances;
-    mapping(address => bool) initialized;
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
 
-    // GROWCHAIN accepts request to tip-touch another GROWCHAIN
-    mapping(address => mapping (address => uint256)) allowed;
-
-    function GROWCHAIN() {
-        initialized[msg.sender] = true;
-        balances[msg.sender] = 5000000000 * 10 ** uint256(decimals) - _cutoff;
-        _totalSupply = balances[msg.sender];
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function GROWCHAIN(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
-    function totalSupply() constant returns (uint256 supply) {
-        return _totalSupply;
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    // What's my girth?
-    function balance() constant returns (uint256) {
-        return getBalance(msg.sender);
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
     }
 
-    // What is the length of a particular GROWCHAIN?
-    function balanceOf(address _address) constant returns (uint256) {
-        return getBalance(_address);
-    }
-
-    // Tenderly remove hand from GROWCHAIN and place on another GROWCHAIN
-    function transfer(address _to, uint256 _amount) returns (bool success) {
-        initialize(msg.sender);
-
-        if (balances[msg.sender] >= _amount
-            && _amount > 0) {
-            initialize(_to);
-            if (balances[_to] + _amount > balances[_to]) {
-
-                balances[msg.sender] -= _amount;
-                balances[_to] += _amount;
-
-                Transfer(msg.sender, _to, _amount);
-
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    // Perform the inevitable actions which cause release of that which each GROWCHAIN
-    // is built to deliver. In EtherGROWCHAINLand there are only GROWCHAINes, so this 
-    // allows the transmission of one GROWCHAIN's payload (or partial payload but that
-    // is not as much fun) INTO another GROWCHAIN. This causes the GROWCHAINae to change 
-    // form such that all may see the glory they each represent. Erections.
-    function transferFrom(address _from, address _to, uint256 _amount) returns (bool success) {
-        initialize(_from);
-
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && _amount > 0) {
-            initialize(_to);
-            if (balances[_to] + _amount > balances[_to]) {
-
-                balances[_from] -= _amount;
-                allowed[_from][msg.sender] -= _amount;
-                balances[_to] += _amount;
-
-                Transfer(_from, _to, _amount);
-
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    // Allow splooger to cause a payload release from your GROWCHAIN, multiple times, up to 
-    // the point at which no further release is possible..
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
-    // internal privats
-    function initialize(address _address) internal returns (bool success) {
-        if (_outAmount < _cutoff && !initialized[_address]) {
-            initialized[_address] = true;
-            balances[_address] = _airdropAmount;
-            _outAmount += _airdropAmount;
-            _totalSupply += _airdropAmount;
-        }
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
         return true;
     }
 
-    function getBalance(address _address) internal returns (uint256) {
-        if (_outAmount < _cutoff && !initialized[_address]) {
-            return balances[_address] + _airdropAmount;
-        }
-        else {
-            return balances[_address];
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
         }
     }
-    
-    function getOutAmount()constant returns(uint256 amount){
-        return _outAmount;
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
     }
-    
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
 }
