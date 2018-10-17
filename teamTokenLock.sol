c7ca95b79d8199a18abfca714bb3b4a4471f28a7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:56:18 +0700

    add contract TeamTokenLock

--- teamTokenLock.sol
+++ teamTokenLock.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamTokenLock at 0x9c109e620c35475ab797706bd8a57d621d1a80d7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamTokenLock at 0xb88067f3d1e1564c844ecb875bb262cc9faed2c1
 */
 pragma solidity ^0.4.16;
 
@@ -25,82 +25,81 @@ contract TeamTokenLock is owned {
 
     address _tokenAddress = 0xAF815e887b039Fc06a8ddDcC7Ec4f57757616Cd2;
     uint256 _startTime = 1534723200;  // Aug 20, 2018
-    uint256 _teamTokenAmount = 1600000000e18;  // 1.6 Billion
     uint256 _totalWithdrawAmount = 0;
 
     function getAllowedAmountByTeam() public constant returns (uint256 amount) {
         if (now >= _startTime + (731 days)) {
             // Aug 20, 2020
-            return _teamTokenAmount;
+            return 160000000e18;
         } else if (now >= _startTime + (700 days)) {
             // July 20, 2020
-            return _teamTokenAmount / uint(24) * 23;
+            return 160000000e18 / uint(24) * 23;
         } else if (now >= _startTime + (670 days)) {
             // June 20, 2020
-            return _teamTokenAmount / uint(24) * 22;
+            return 160000000e18 / uint(24) * 22;
         } else if (now >= _startTime + (639 days)) {
             // May 20, 2020
-            return _teamTokenAmount / uint(24) * 21;
+            return 160000000e18 / uint(24) * 21;
         } else if (now >= _startTime + (609 days)) {
             // April 20, 2020
-            return _teamTokenAmount / uint(24) * 20;
+            return 160000000e18 / uint(24) * 20;
         } else if (now >= _startTime + (578 days)) {
             // March 20, 2020
-            return _teamTokenAmount / uint(24) * 19;
+            return 160000000e18 / uint(24) * 19;
         } else if (now >= _startTime + (549 days)) {
             // Febuary 20, 2020
-            return _teamTokenAmount / uint(24) * 18;
+            return 160000000e18 / uint(24) * 18;
         } else if (now >= _startTime + (518 days)) {
             // January 20, 2020
-            return _teamTokenAmount / uint(24) * 17;
+            return 160000000e18 / uint(24) * 17;
         } else if (now >= _startTime + (487 days)) {
             // December 20, 2019
-            return _teamTokenAmount / uint(24) * 16;
+            return 160000000e18 / uint(24) * 16;
         } else if (now >= _startTime + (457 days)) {
             // November 20, 2019
-            return _teamTokenAmount / uint(24) * 15;
+            return 160000000e18 / uint(24) * 15;
         } else if (now >= _startTime + (426 days)) {
             // October 20, 2019
-            return _teamTokenAmount / uint(24) * 14;
+            return 160000000e18 / uint(24) * 14;
         } else if (now >= _startTime + (396 days)) {
             // September 20, 2019
-            return _teamTokenAmount / uint(24) * 13;
+            return 160000000e18 / uint(24) * 13;
         } else if (now >= _startTime + (365 days)) {
             // August 20, 2019
-            return _teamTokenAmount / uint(24) * 12;
+            return 160000000e18 / uint(24) * 12;
         } else if (now >= _startTime + (334 days)) {
             // July 20, 2019
-            return _teamTokenAmount / uint(24) * 11;
+            return 160000000e18 / uint(24) * 11;
         } else if (now >= _startTime + (304 days)) {
             // June 20, 2019
-            return _teamTokenAmount / uint(24) * 10;
+            return 160000000e18 / uint(24) * 10;
         } else if (now >= _startTime + (273 days)) {
             // May 20, 2019
-            return _teamTokenAmount / uint(24) * 9;
+            return 160000000e18 / uint(24) * 9;
         } else if (now >= _startTime + (243 days)) {
             // April 20, 2019
-            return _teamTokenAmount / uint(24) * 8;
+            return 160000000e18 / uint(24) * 8;
         } else if (now >= _startTime + (212 days)) {
             // March 20, 2019
-            return _teamTokenAmount / uint(24) * 7;
+            return 160000000e18 / uint(24) * 7;
         } else if (now >= _startTime + (184 days)) {
             // Febuary 20, 2019
-            return _teamTokenAmount / uint(24) * 6;
+            return 160000000e18 / uint(24) * 6;
         } else if (now >= _startTime + (153 days)) {
             // January 20, 2019
-            return _teamTokenAmount / uint(24) * 5;
+            return 160000000e18 / uint(24) * 5;
         } else if (now >= _startTime + (122 days)) {
             // December 20, 2018
-            return _teamTokenAmount / uint(24) * 4;
+            return 160000000e18 / uint(24) * 4;
         } else if (now >= _startTime + (92 days)) {
             // Nobember 20, 2018
-            return _teamTokenAmount / uint(24) * 3;
+            return 160000000e18 / uint(24) * 3;
         } else if (now >= _startTime + (61 days)) {
             // October 20, 2018
-            return _teamTokenAmount / uint(24) * 2;
+            return 160000000e18 / uint(24) * 2;
         } else if (now >= _startTime + (31 days)) {
             // September 20, 2018
-            return _teamTokenAmount / uint(24);
+            return 160000000e18 / uint(24);
         } else {
             return 0;
         }
