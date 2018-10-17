c233136dbc1542d94fd3e2d15a6742381fc6b386
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:57:24 +0700

    add contract QPay

--- QPAY.sol
+++ QPAY.sol
@@ -1,106 +1,349 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract QPAY at 0x74d94fb1d80e69f29cace03e84d23fe37593ffbf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract QPay at 0x2fe0bc5ffb80a84739da913f0a393a4b0cce661b
 */
-pragma solidity ^0.4.8;
-contract Token{
-    // token???????public??????getter????????totalSupply().
-    uint256 public totalSupply;
+pragma solidity 0.4.18;
 
-    /// ????_owner??token??? 
-    function balanceOf(address _owner) constant returns (uint256 balance);
 
-    //??????????_to??????_value?token
-    function transfer(address _to, uint256 _value) returns (bool success);
+library SafeMath {
 
-    //???_from????_to????_value?token??approve??????
-    function transferFrom(address _from, address _to, uint256 _value) returns   
-    (bool success);
+    /**
+    * Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-    //??????????_spender????????????_value?token
-    function approve(address _spender, uint256 _value) returns (bool success);
+    /**
+    * Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
 
-    //????_spender?????_owner???token???
-    function allowance(address _owner, address _spender) constant returns 
-    (uint256 remaining);
+    /**
+    * Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-    //????????????? 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    //???approve(address _spender, uint256 _value)????????????
-    event Approval(address indexed _owner, address indexed _spender, uint256 
-    _value);
+    /**
+    * Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
-contract StandardToken is Token {
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //??totalSupply ??????? (2^256 - 1).
-        //??????????????token??????????????????
-        //require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;//???????????token??_value
-        balances[_to] += _value;//???????token??_value
-        Transfer(msg.sender, _to, _value);//????????
+contract QPay {
+
+    string public symbol="QPY";
+    string public name="QPay" ;
+    uint8 public constant decimals = 18;
+    uint256 _totalSupply = 0;	
+	uint256 _FreeQPY = 1230;
+    uint256 _ML1 = 2;
+    uint256 _ML2 = 3;
+	uint256 _ML3 = 4;
+    uint256 _LimitML1 = 3e15;
+    uint256 _LimitML2 = 6e15;
+	uint256 _LimitML3 = 9e15;
+	uint256 _MaxDistribPublicSupply = 950000000;
+    uint256 _OwnerDistribSupply = 0;
+    uint256 _CurrentDistribPublicSupply = 0;	
+    uint256 _ExtraTokensPerETHSended = 150000;
+    
+	address _DistribFundsReceiverAddress = 0;
+    address _remainingTokensReceiverAddress = 0;
+    address owner = 0;
+	
+	
+    bool setupDone = false;
+    bool IsDistribRunning = false;
+    bool DistribStarted = false;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Burn(address indexed _owner, uint256 _value);
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+    mapping(address => bool) public Claimed;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function QPay() public {
+        owner = msg.sender;
+    }
+
+    function() public payable {
+        if (IsDistribRunning) {
+            uint256 _amount;
+            if (((_CurrentDistribPublicSupply + _amount) > _MaxDistribPublicSupply) && _MaxDistribPublicSupply > 0) revert();
+            if (!_DistribFundsReceiverAddress.send(msg.value)) revert();
+            if (Claimed[msg.sender] == false) {
+                _amount = _FreeQPY * 1e18;
+                _CurrentDistribPublicSupply += _amount;
+                balances[msg.sender] += _amount;
+                _totalSupply += _amount;
+                Transfer(this, msg.sender, _amount);
+                Claimed[msg.sender] = true;
+            }
+
+           
+
+            if (msg.value >= 9e15) {
+            _amount = msg.value * _ExtraTokensPerETHSended * 4;
+            } else {
+                if (msg.value >= 6e15) {
+                    _amount = msg.value * _ExtraTokensPerETHSended * 3;
+                } else {
+                    if (msg.value >= 3e15) {
+                        _amount = msg.value * _ExtraTokensPerETHSended * 2;
+                    } else {
+
+                        _amount = msg.value * _ExtraTokensPerETHSended;
+
+                    }
+
+                }
+            }
+			 
+			 _CurrentDistribPublicSupply += _amount;
+                balances[msg.sender] += _amount;
+                _totalSupply += _amount;
+                Transfer(this, msg.sender, _amount);
+        
+
+
+
+        } else {
+            revert();
+        }
+    }
+
+    function SetupQPY(string tokenName, string tokenSymbol, uint256 ExtraTokensPerETHSended, uint256 MaxDistribPublicSupply, uint256 OwnerDistribSupply, address remainingTokensReceiverAddress, address DistribFundsReceiverAddress, uint256 FreeQPY) public {
+        if (msg.sender == owner && !setupDone) {
+            symbol = tokenSymbol;
+            name = tokenName;
+            _FreeQPY = FreeQPY;
+            _ExtraTokensPerETHSended = ExtraTokensPerETHSended;
+            _MaxDistribPublicSupply = MaxDistribPublicSupply * 1e18;
+            if (OwnerDistribSupply > 0) {
+                _OwnerDistribSupply = OwnerDistribSupply * 1e18;
+                _totalSupply = _OwnerDistribSupply;
+                balances[owner] = _totalSupply;
+                _CurrentDistribPublicSupply += _totalSupply;
+                Transfer(this, owner, _totalSupply);
+            }
+            _DistribFundsReceiverAddress = DistribFundsReceiverAddress;
+            if (_DistribFundsReceiverAddress == 0) _DistribFundsReceiverAddress = owner;
+            _remainingTokensReceiverAddress = remainingTokensReceiverAddress;
+
+            setupDone = true;
+        }
+    }
+
+    function SetupML(uint256 ML1inX, uint256 ML2inX, uint256 LimitML1inWei, uint256 LimitML2inWei) onlyOwner public {
+        _ML1 = ML1inX;
+        _ML2 = ML2inX;
+        _LimitML1 = LimitML1inWei;
+        _LimitML2 = LimitML2inWei;
+        
+    }
+
+    function SetExtra(uint256 ExtraTokensPerETHSended) onlyOwner public {
+        _ExtraTokensPerETHSended = ExtraTokensPerETHSended;
+    }
+
+    function SetFreeQPY(uint256 FreeQPY) onlyOwner public {
+        _FreeQPY = FreeQPY;
+    }
+
+    function StartDistrib() public returns(bool success) {
+        if (msg.sender == owner && !DistribStarted && setupDone) {
+            DistribStarted = true;
+            IsDistribRunning = true;
+        } else {
+            revert();
+        }
         return true;
     }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) returns 
-    (bool success) {
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= 
-        // _value && balances[_to] + _value > balances[_to]);
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;//??????token??_value
-        balances[_from] -= _value; //????_from??token??_value
-        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
-        Transfer(_from, _to, _value);//????????
+    function StopDistrib() public returns(bool success) {
+        if (msg.sender == owner && IsDistribRunning) {
+            if (_remainingTokensReceiverAddress != 0 && _MaxDistribPublicSupply > 0) {
+                uint256 _remainingAmount = _MaxDistribPublicSupply - _CurrentDistribPublicSupply;
+                if (_remainingAmount > 0) {
+                    balances[_remainingTokensReceiverAddress] += _remainingAmount;
+                    _totalSupply += _remainingAmount;
+                   Transfer(this, _remainingTokensReceiverAddress, _remainingAmount);
+                }
+            }
+            DistribStarted = false;
+            IsDistribRunning = false;
+        } else {
+            revert();
+        }
         return true;
     }
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+
+    function distribution(address[] addresses, uint256 _amount) onlyOwner public {
+
+        uint256 _remainingAmount = _MaxDistribPublicSupply - _CurrentDistribPublicSupply;
+        require(addresses.length <= 255);
+        require(_amount <= _remainingAmount);
+        _amount = _amount * 1e18;
+
+        for (uint i = 0; i < addresses.length; i++) {
+            require(_amount <= _remainingAmount);
+            _CurrentDistribPublicSupply += _amount;
+            balances[addresses[i]] += _amount;
+            _totalSupply += _amount;
+           Transfer(this, addresses[i], _amount);
+
+        }
+
+        if (_CurrentDistribPublicSupply >= _MaxDistribPublicSupply) {
+            DistribStarted = false;
+            IsDistribRunning = false;
+        }
+    }
+
+    function distributeAmounts(address[] addresses, uint256[] amounts) onlyOwner public {
+
+        uint256 _remainingAmount = _MaxDistribPublicSupply - _CurrentDistribPublicSupply;
+        uint256 _amount;
+
+        require(addresses.length <= 255);
+        require(addresses.length == amounts.length);
+
+        for (uint8 i = 0; i < addresses.length; i++) {
+            _amount = amounts[i] * 1e18;
+            require(_amount <= _remainingAmount);
+            _CurrentDistribPublicSupply += _amount;
+            balances[addresses[i]] += _amount;
+            _totalSupply += _amount;
+            Transfer(this, addresses[i], _amount);
+
+
+            if (_CurrentDistribPublicSupply >= _MaxDistribPublicSupply) {
+                DistribStarted = false;
+                IsDistribRunning = false;
+            }
+        }
+    }
+
+    function BurnTokens(uint256 amount) public returns(bool success) {
+        uint256 _amount = amount * 1e18;
+        if (balances[msg.sender] >= _amount) {
+            balances[msg.sender] -= _amount;
+            _totalSupply -= _amount;
+             Burn(msg.sender, _amount);
+            Transfer(msg.sender, 0, _amount);
+        } else {
+            revert();
+        }
+        return true;
+    }
+
+    function totalSupply() public constant returns(uint256 totalSupplyValue) {
+        return _totalSupply;
+    }
+
+    function MaxDistribPublicSupply_() public constant returns(uint256 MaxDistribPublicSupply) {
+        return _MaxDistribPublicSupply;
+    }
+
+    function OwnerDistribSupply_() public constant returns(uint256 OwnerDistribSupply) {
+        return _OwnerDistribSupply;
+    }
+
+    function CurrentDistribPublicSupply_() public constant returns(uint256 CurrentDistribPublicSupply) {
+        return _CurrentDistribPublicSupply;
+    }
+
+    function RemainingTokensReceiverAddress() public constant returns(address remainingTokensReceiverAddress) {
+        return _remainingTokensReceiverAddress;
+    }
+
+    function DistribFundsReceiverAddress() public constant returns(address DistribfundsReceiver) {
+        return _DistribFundsReceiverAddress;
+    }
+
+    function Owner() public constant returns(address ownerAddress) {
+        return owner;
+    }
+
+    function SetupDone() public constant returns(bool setupDoneFlag) {
+        return setupDone;
+    }
+
+    function IsDistribRunningFalg_() public constant returns(bool IsDistribRunningFalg) {
+        return IsDistribRunning;
+    }
+
+    function IsDistribStarted() public constant returns(bool IsDistribStartedFlag) {
+        return DistribStarted;
+    }
+
+    function balanceOf(address _owner) public constant returns(uint256 balance) {
         return balances[_owner];
     }
 
+    function transfer(address _to, uint256 _amount) public returns(bool success) {
+        if (balances[msg.sender] >= _amount &&
+            _amount > 0 &&
+            balances[_to] + _amount > balances[_to]) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    function approve(address _spender, uint256 _value) returns (bool success)   
-    {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public returns(bool success) {
+        if (balances[_from] >= _amount &&
+            allowed[_from][msg.sender] >= _amount &&
+            _amount > 0 &&
+            balances[_to] + _amount > balances[_to]) {
+            balances[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+         Transfer(_from, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function approve(address _spender, uint256 _amount) public returns(bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
         return true;
     }
 
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];//??_spender?_owner????token?
+    function allowance(address _owner, address _spender) public constant returns(uint256 remaining) {
+        return allowed[_owner][_spender];
     }
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-}
-
-contract QPAY is StandardToken { 
-
-    /* Public variables of the token */
-    string public name;                   //??: eg Simon Bucks
-    uint8 public decimals;               //????????How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;               //token??: eg SBX
-    string public version = 'H0.1';    //??
-
-    function QPAY(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
-        balances[msg.sender] = _initialAmount; // ??token?????????
-        totalSupply = _initialAmount;         // ??????
-        name = _tokenName;                   // token??
-        decimals = _decimalUnits;           // ????
-        symbol = _tokenSymbol;             // token??
-    }
-
-    /* Approves and then calls the receiving contract */
-    
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
-        return true;
-    }
-
 }
