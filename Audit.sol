a979564c75d6db99e8bd634bbf4f9a2b6e496a6b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:36:58 +0700

    add contract Audit

--- Audit.sol
+++ Audit.sol
@@ -1,41 +1,44 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Audit at 0xb5124ab26c97be4975b242bfc26de88e22e6d644
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Audit at 0x3512ea8e6a75d474dd7cefe1938df5ffd80118c5
 */
 pragma solidity ^0.4.24;
 
-library DS {
+contract Audit {
+
   struct Proof {
     uint level;         // Audit level
     uint insertedBlock; // Audit's block
     bytes32 ipfsHash;   // IPFS dag-cbor proof
     address auditedBy;  // Audited by address
   }
-}
 
-contract Audit {
   event AttachedEvidence(address indexed auditorAddr, bytes32 indexed codeHash, bytes32 ipfsHash);
   event NewAudit(address indexed auditorAddr, bytes32 indexed codeHash);
 
   // Maps auditor address and code's keccak256 to Audit
-  mapping (address => mapping (bytes32 => DS.Proof)) public auditedContracts;
+  mapping (address => mapping (bytes32 => Proof)) public auditedContracts;
   // Maps auditor address to a list of audit code hashes
   mapping (address => bytes32[]) public auditorContracts;
   
   // Returns code audit level, 0 if not present
   function isVerifiedAddress(address _auditorAddr, address _contractAddr) public view returns(uint) {
-    bytes32 codeHash = keccak256(codeAt(_contractAddr));
+    bytes32 codeHash = getCodeHash(_contractAddr);
     return auditedContracts[_auditorAddr][codeHash].level;
   }
 
   function isVerifiedCode(address _auditorAddr, bytes32 _codeHash) public view returns(uint) {
     return auditedContracts[_auditorAddr][_codeHash].level;
   }
+
+  function getCodeHash(address _contractAddr) public view returns(bytes32) {
+      return keccak256(codeAt(_contractAddr));
+  }
   
   // Add audit information
   function addAudit(bytes32 _codeHash, uint _level, bytes32 _ipfsHash) public {
     address auditor = msg.sender;
     require(auditedContracts[auditor][_codeHash].insertedBlock == 0);
-    auditedContracts[auditor][_codeHash] = DS.Proof({ 
+    auditedContracts[auditor][_codeHash] = Proof({ 
         level: _level,
         auditedBy: auditor,
         insertedBlock: block.number,
