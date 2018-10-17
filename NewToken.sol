c70f32df2337c48def3596b53d3b557c7421e3ac
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:10:47 +0700

    add contract NewToken

--- NewToken.sol
+++ NewToken.sol
@@ -1,103 +1,88 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NewToken at 0x76755c6d51989d2018dfe2b4eace95908ecb1c5e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NewToken at 0x2e3f4428026e83acd6c34279e5ed9e920499fd02
 */
-pragma solidity ^0.4.11;
-
-//------------------------------------------------------------------------------------------------
-// ERC20 Standard Token Implementation, based on ERC Standard:
-// https://github.com/ethereum/EIPs/issues/20
-// With some inspiration from ConsenSys HumanStandardToken as well
-// Copyright 2017 BattleDrome
-//------------------------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------------------------
-// LICENSE
-//
-// This file is part of BattleDrome.
-// 
-// BattleDrome is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// BattleDrome is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with BattleDrome.  If not, see <http://www.gnu.org/licenses/>.
-//------------------------------------------------------------------------------------------------
-
-contract ERC20Standard {
-	uint public totalSupply;
-	
-	string public name;
-	uint8 public decimals;
-	string public symbol;
-	string public version;
-	
-	mapping (address => uint256) balances;
-	mapping (address => mapping (address => uint)) allowed;
-
-	//Fix for short address attack against ERC20
-	modifier onlyPayloadSize(uint size) {
-		assert(msg.data.length == size + 4);
-		_;
-	} 
-
-	function balanceOf(address _owner) constant returns (uint balance) {
-		return balances[_owner];
-	}
-
-	function transfer(address _recipient, uint _value) onlyPayloadSize(2*32) {
-		require(balances[msg.sender] >= _value && _value > 0);
-	    balances[msg.sender] -= _value;
-	    balances[_recipient] += _value;
-	    Transfer(msg.sender, _recipient, _value);        
+pragma solidity ^0.4.18;
+/*
+ * Math operations with safety checks
+ */
+contract SafeMath {
+  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+    return 0;
     }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-	function transferFrom(address _from, address _to, uint _value) {
-		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-    }
+  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-	function approve(address _spender, uint _value) {
-		allowed[msg.sender][_spender] = _value;
-		Approval(msg.sender, _spender, _value);
-	}
+  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-	function allowance(address _spender, address _owner) constant returns (uint balance) {
-		return allowed[_owner][_spender];
-	}
-
-	//Event which is triggered to log all transfers to this contract's event log
-	event Transfer(
-		address indexed _from,
-		address indexed _to,
-		uint _value
-		);
-		
-	//Event which is triggered whenever an owner approves a new allowance for a spender.
-	event Approval(
-		address indexed _owner,
-		address indexed _spender,
-		uint _value
-		);
-
+  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
-contract NewToken is ERC20Standard {
-	function NewToken() {
-		totalSupply = 10000000000000000000000000;
-		name = "Crypto Chance Token";
-		decimals = 18;
-		symbol = "CCHT";
-		version = "1.0";
-		balances[msg.sender] = totalSupply;
-		Transfer(0x0, msg.sender, totalSupply);
-	}
+
+contract NewToken is SafeMath {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+	address public owner;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function NewToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) public  {
+        balanceOf[msg.sender] = initialSupply;              
+        totalSupply = initialSupply;                        
+        name = tokenName;                                   
+        symbol = tokenSymbol;                               
+        decimals = decimalUnits;                    
+		owner = msg.sender;
+    }
+
+
+    function transfer(address _to, uint256 _value) public {
+        if (_to == 0x0)  revert();                               
+		if (_value <= 0)  revert(); 
+        if (balanceOf[msg.sender] < _value)  revert();           
+        if (balanceOf[_to] + _value < balanceOf[_to])  revert(); 
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                    
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                           
+        Transfer(msg.sender, _to, _value);                  
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        if (_to == 0x0)  revert();                                
+		if (_value <= 0)  revert(); 
+        if (balanceOf[_from] < _value)  revert();                 
+        if (balanceOf[_to] + _value < balanceOf[_to])  revert();  
+        if (_value > allowance[_from][msg.sender])  revert();     
+        balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);                           
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                
+        allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
 }
