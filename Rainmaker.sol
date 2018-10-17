5d07111beba411145ce15905024802805bb1e8d5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:40:56 +0700

    add contract Rainmaker

--- Rainmaker.sol
+++ Rainmaker.sol
@@ -1,7 +1,87 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Rainmaker at 0x76e5cfba9633a467e77db2587a50c0bec4389ab9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Rainmaker at 0x1ca27f9f05953525f1cfaeada8673153c78ccdf3
 */
 pragma solidity ^0.4.24;
+
+/**
+ * @title Helps contracts guard agains reentrancy attacks.
+ * @author Remco Bloemen <remco@2?.com>
+ * @notice If you mark a function `nonReentrant`, you should also
+ * mark it `external`.
+ */
+contract ReentrancyGuard {
+
+  /**
+   * @dev We use a single lock for the whole contract.
+   */
+  bool private reentrancyLock = false;
+
+  /**
+   * @dev Prevents a contract from calling itself, directly or indirectly.
+   * @notice If you mark a function `nonReentrant`, you should also
+   * mark it `external`. Calling one nonReentrant function from
+   * another is not supported. Instead, you can implement a
+   * `private` function doing the actual work, and a `external`
+   * wrapper marked as `nonReentrant`.
+   */
+  modifier nonReentrant() {
+    require(!reentrancyLock);
+    reentrancyLock = true;
+    _;
+    reentrancyLock = false;
+  }
+
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 /**
  * @title Ownable
@@ -63,12 +143,41 @@ contract Ownable {
   }
 }
 
-contract Rainmaker is Ownable {
-    function letItRain(address[] _to, uint[] _value) onlyOwner public payable returns (bool _success) {
+interface ERC20 {
+    function totalSupply() external view returns (uint supply);
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint remaining);
+    function decimals() external view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+contract Rainmaker is Ownable, ReentrancyGuard {
+    function letItRain(address[] _to, uint[] _value) nonReentrant onlyOwner public payable returns (bool _success) {
         for (uint8 i = 0; i < _to.length; i++){
             uint amount = _value[i] * 1 finney;
             _to[i].transfer(amount);
         }
+        return true;
+    }
+    
+    //If accidentally tokens are transferred to this
+    //contract. They can be withdrawn by the following interface.
+    function withdrawERC20Token(ERC20 anyToken) public onlyOwner nonReentrant returns(bool){
+        if( anyToken != address(0x0) ) {
+            assert(anyToken.transfer(owner, anyToken.balanceOf(this)));
+        }
+        return true;
+    }
+
+    //ETH cannot get locked in this contract. If it does, this can be used to withdraw
+    //the locked ether.
+    function withdrawEther() public onlyOwner nonReentrant returns(bool){
+        if(address(this).balance > 0){
+            owner.transfer(address(this).balance);
+        }        
         return true;
     }
 }
