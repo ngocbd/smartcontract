d982f26ebbdb5e6c62c44b02d7b5bc99a7dd0487
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:03:08 +0700

    add contract HMTCrowdsale

---
+++ HMTCrowdsale.sol
@@ -0,0 +1,106 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HMTCrowdsale at 0xc3af01b683521e0206dc167ac4bd8acc8fa2b424
+*/
+pragma solidity ^0.4.16;
+
+interface token {
+    function transfer(address receiver, uint amount);
+}
+
+contract HMTCrowdsale {
+    address public beneficiary;  // ?????????
+    uint public fundingGoal;   // ????
+    uint public amountRaised;   // ????
+    uint public deadline;      // ?????
+
+    uint public price;    //  token ??????? , token????
+    token public tokenReward;   // ???token
+
+    mapping(address => uint256) public balanceOf;
+
+    bool fundingGoalReached = false;  // ????????
+    bool crowdsaleClosed = false;   //  ??????
+
+    /**
+    * ??????????
+    **/
+    event GoalReached(address recipient, uint totalAmountRaised);
+    event FundTransfer(address backer, uint amount, bool isContribution);
+
+    /**
+     * ????, ??????
+     */
+    function HMTCrowdsale(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint durationInMinutes,
+        uint finneyCostOfEachToken,
+        address addressOfTokenUsedAsReward) {
+            beneficiary = ifSuccessfulSendTo;
+            fundingGoal = fundingGoalInEthers * 1 ether;
+            deadline = now + durationInMinutes * 1 minutes;
+            price = finneyCostOfEachToken * 1 finney;
+            tokenReward = token(addressOfTokenUsedAsReward);   // ?????? token ??????????
+    }
+
+    /**
+     * ?????Fallback???
+     * ????????????????
+     */
+    function () payable {
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+        balanceOf[msg.sender] += amount;
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
+        FundTransfer(msg.sender, amount, true);
+    }
+
+    /**
+    *  ???????modifier????Python????????
+    * ?????????????????????????????????
+    * _ ???????????
+    **/
+    modifier afterDeadline() { if (now >= deadline) _; }
+
+    /**
+     * ????????????? ???????afterDeadline?????
+     *
+     */
+    function checkGoalReached() afterDeadline {
+        if (amountRaised >= fundingGoal) {
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+        }
+        crowdsaleClosed = true;
+    }
+
+
+    /**
+     * ?????????????????
+     * ?????????????
+     *
+     */
+    function safeWithdrawal() afterDeadline {
+        if (!fundingGoalReached) {
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                if (msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+
+        if (fundingGoalReached && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised)) {
+                FundTransfer(beneficiary, amountRaised, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock funders balance
+                fundingGoalReached = false;
+            }
+        }
+    }
+}
