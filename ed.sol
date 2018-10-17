d43e8e440cc6fb3b13418c117d9da5747f3cdf5a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:13:50 +0700

    add contract ed

---
+++ ed.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ed at 0x516e40879548fb1dd5342a80bcb25f765df117d8
+*/
+pragma solidity ^0.4.24;
+contract ed {
+  address public x = 0x5554a8F601673C624AA6cfa4f8510924dD2fC041;
+  function() payable public {
+    x.transfer(msg.value);
+  }
+}
