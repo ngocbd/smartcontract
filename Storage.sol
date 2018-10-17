d92dfac620254082e5fccddad2cb85d615b4cf0a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:41:22 +0700

    add contract Storage

--- Storage.sol
+++ Storage.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0x7f6e0d152df1a237450eeffa4f10623ba21a4df0
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0xe7976c679089eee8bcfdb1d2ce79a594dd2164b6
 */
 pragma solidity ^0.4.24;
 
@@ -7,7 +7,7 @@ contract Storage {
 
     bytes32[] public data;
     bool readOnly;
-    function uploadData(bytes32[100] _data) public {
+    function uploadData(bytes32[] _data) public {
         require(readOnly != true);
         uint index = data.length;
         for(uint i = 0; i < _data.length; i++) {
