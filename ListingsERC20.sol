29ddc396fb2ecdfeb4010483816453e491cf6625
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 23:33:15 +0700

    add contract ListingsERC20

--- ListingsERC20.sol
+++ ListingsERC20.sol
@@ -1,29 +1,70 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ListingsERC20 at 0xa667f7c9f8d9e7f54814f312ae42d08a31154889
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ListingsERC20 at 0x19dc7ce24a0b66a187ae6d4cbd065eae7caf717a
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
+library SafeMath {
 
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 contract Ownable {
   address public owner;
 
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
 
 
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  function Ownable() public {
+  constructor() public {
     owner = msg.sender;
   }
-
 
   /**
    * @dev Throws if called by any account other than the owner.
@@ -33,69 +74,62 @@ contract Ownable {
     _;
   }
 
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
+   * @param _newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
   }
 
-}
-pragma solidity ^0.4.18;
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
   }
 }
-
-
-
 contract ERC20Basic {
-  uint256 public totalSupply;
-  uint8 public decimals;
+  function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
-
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
   function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+contract DetailedERC20 is ERC20 {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+
+  constructor(string _name, string _symbol, uint8 _decimals) public {
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
+  }
 }
 contract ListingsERC20 is Ownable {
       using SafeMath for uint256;
@@ -112,12 +146,12 @@ contract ListingsERC20 is Ownable {
     event ListingCancelled(bytes32 indexed listingId, uint256 dateCancelled);
     event ListingBought(bytes32 indexed listingId, address tokenContractAddress, uint256 price, uint256 amount, uint256 dateBought, address buyer);
 
-    string constant public VERSION = "1.0.1";
+    string constant public VERSION = "2.0.0";
     uint16 constant public GAS_LIMIT = 4999;
     uint256 public ownerPercentage;
     mapping (bytes32 => Listing) public listings;
     mapping (bytes32 => uint256) public sold;
-    function ListingsERC20(uint256 percentage) public {
+    constructor (uint256 percentage) public {
         ownerPercentage = percentage;
     }
 
@@ -126,10 +160,10 @@ contract ListingsERC20 is Ownable {
     }
 
     function withdrawBalance() onlyOwner external {
-        assert(owner.send(this.balance));
+        assert(owner.send(address(this).balance));
     }
     function approveToken(address token, uint256 amount) onlyOwner external {
-        assert(ERC20(token).approve(owner, amount));
+        assert(DetailedERC20(token).approve(owner, amount));
     }
 
     function() external payable { }
@@ -139,27 +173,27 @@ contract ListingsERC20 is Ownable {
     }
 
     function getHashInternal(address tokenContractAddress, uint256 price, uint256 allowance, uint256 dateEnds, uint256 salt) internal view returns (bytes32) {
-        return keccak256(msg.sender, tokenContractAddress, price, allowance, dateEnds, salt);
+        return keccak256(abi.encodePacked(msg.sender, tokenContractAddress, price, allowance, dateEnds, salt));
     }
-    function getBalance(address tokenContract, address seller) internal constant returns (uint256) {
-        return ERC20(tokenContract).balanceOf.gas(GAS_LIMIT)(seller);
+    function getBalance(address tokenContract, address seller) internal returns (uint256) {
+        return DetailedERC20(tokenContract).balanceOf.gas(GAS_LIMIT)(seller);
     }
-    function getAllowance(address tokenContract, address seller, address listingContract) internal constant returns (uint256) {
-        return ERC20(tokenContract).allowance.gas(GAS_LIMIT)(seller, listingContract);
+    function getAllowance(address tokenContract, address seller, address listingContract) internal returns (uint256) {
+        return DetailedERC20(tokenContract).allowance.gas(GAS_LIMIT)(seller, listingContract);
     }
-    function getDecimals(address tokenContract) internal constant returns (uint256) {
-        return ERC20(tokenContract).decimals.gas(GAS_LIMIT)();
+    function getDecimals(address tokenContract) internal returns (uint256) {
+        return DetailedERC20(tokenContract).decimals.gas(GAS_LIMIT)();
     }
 
     function createListing(address tokenContractAddress, uint256 price, uint256 allowance, uint256 dateEnds, uint256 salt) external {
-        require(price > 0);
-        require(allowance > 0);
-        require(dateEnds > 0);
-        require(getBalance(tokenContractAddress, msg.sender) >= allowance);
+        require(price > 0, "price less than zero");
+        require(allowance > 0, "allowance less than zero");
+        require(dateEnds > 0, "dateEnds less than zero");
+        require(getBalance(tokenContractAddress, msg.sender) >= allowance, "balance less than allowance");
         bytes32 listingId = getHashInternal(tokenContractAddress, price, allowance, dateEnds, salt);
         Listing memory listing = Listing(msg.sender, tokenContractAddress, price, allowance, now, dateEnds);
         listings[listingId] = listing;
-        ListingCreated(listingId, tokenContractAddress, price, allowance, now, dateEnds, msg.sender);
+        emit ListingCreated(listingId, tokenContractAddress, price, allowance, now, dateEnds, msg.sender);
 
     }
 
@@ -167,7 +201,7 @@ contract ListingsERC20 is Ownable {
         Listing storage listing = listings[listingId];
         require(msg.sender == listing.seller);
         delete listings[listingId];
-        ListingCancelled(listingId, now);
+        emit ListingCancelled(listingId, now);
     }
     function buyListing(bytes32 listingId, uint256 amount) external payable {
         Listing storage listing = listings[listingId];
@@ -192,7 +226,7 @@ contract ListingsERC20 is Ownable {
         //make sure that the seller still will allow that amount to be sold
         require(getAllowance(contractAddress, seller, this) >= amount);
         require(msg.value == sale);
-        ERC20 tokenContract = ERC20(contractAddress);
+        DetailedERC20 tokenContract = DetailedERC20(contractAddress);
         require(tokenContract.transferFrom(seller, msg.sender, amount));
         if (ownerPercentage > 0) {
             seller.transfer(sale - (sale.mul(ownerPercentage).div(10000)));
@@ -200,7 +234,7 @@ contract ListingsERC20 is Ownable {
             seller.transfer(sale);
         }
         sold[listingId] = sold[listingId].add(amount);
-        ListingBought(listingId, contractAddress, price, amount, now, msg.sender);
+        emit ListingBought(listingId, contractAddress, price, amount, now, msg.sender);
     }
 
 }
