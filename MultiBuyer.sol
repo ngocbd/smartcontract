6ed1109146e86b948f9d9e713a7d5ea96bf65b51
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 06:55:27 +0700

    add contract MultiBuyer

--- MultiBuyer.sol
+++ MultiBuyer.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiBuyer at 0x98c1487b6da44ecec425672b219a3815f691e6ee
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiBuyer at 0x5e3e69e6ef89e9391b75715c96ff0688b557ad57
 */
 pragma solidity ^0.4.24;
 
@@ -243,6 +243,7 @@ contract MultiBuyer is CanReclaimToken {
     function buy(
         IMultiToken _mtkn,
         uint256 _minimumReturn,
+        ERC20 _throughToken,
         address[] _exchanges,
         bytes _datas,
         uint[] _datasIndexes, // including 0 and LENGTH values
@@ -259,13 +260,22 @@ contract MultiBuyer is CanReclaimToken {
             for (uint j = _datasIndexes[i]; j < _datasIndexes[i + 1]; j++) {
                 data[j - _datasIndexes[i]] = _datas[j];
             }
+
+            if (_throughToken != address(0) && i > 0) {
+                _throughToken.approve(_exchanges[i], 0);
+                _throughToken.approve(_exchanges[i], _throughToken.balanceOf(this));
+            }
             require(_exchanges[i].call.value(_values[i])(data), "buy: exchange arbitrary call failed");
+            if (_throughToken != address(0)) {
+                _throughToken.approve(_exchanges[i], 0);
+            }
         }
 
         j = _mtkn.totalSupply(); // optimization totalSupply
         uint256 bestAmount = uint256(-1);
         for (i = _mtkn.tokensCount(); i > 0; i--) {
             ERC20 token = _mtkn.tokens(i - 1);
+            token.approve(_mtkn, 0);
             token.approve(_mtkn, token.balanceOf(this));
 
             uint256 amount = j.mul(token.balanceOf(this)).div(token.balanceOf(_mtkn));
@@ -276,6 +286,51 @@ contract MultiBuyer is CanReclaimToken {
 
         require(bestAmount >= _minimumReturn, "buy: return value is too low");
         _mtkn.bundle(msg.sender, bestAmount);
+        if (address(this).balance > 0) {
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+
+    function buyFirstTokens(
+        IMultiToken _mtkn,
+        ERC20 _throughToken,
+        address[] _exchanges,
+        bytes _datas,
+        uint[] _datasIndexes, // including 0 and LENGTH values
+        uint256[] _values
+    )
+        public
+        payable
+    {
+        require(_datasIndexes.length == _exchanges.length + 1, "buy: _datasIndexes should start with 0 and end with LENGTH");
+        require(_values.length == _exchanges.length, "buy: _values should have the same length as _exchanges");
+
+        for (uint i = 0; i < _exchanges.length; i++) {
+            bytes memory data = new bytes(_datasIndexes[i + 1] - _datasIndexes[i]);
+            for (uint j = _datasIndexes[i]; j < _datasIndexes[i + 1]; j++) {
+                data[j - _datasIndexes[i]] = _datas[j];
+            }
+
+            if (_throughToken != address(0) && i > 0) {
+                _throughToken.approve(_exchanges[i], 0);
+                _throughToken.approve(_exchanges[i], _throughToken.balanceOf(this));
+            }
+            require(_exchanges[i].call.value(_values[i])(data), "buy: exchange arbitrary call failed");
+            if (_throughToken != address(0)) {
+                _throughToken.approve(_exchanges[i], 0);
+            }
+        }
+
+        uint tokensCount = _mtkn.tokensCount();
+        uint256[] memory amounts = new uint256[](tokensCount);
+        for (i = 0; i < tokensCount; i++) {
+            ERC20 token = _mtkn.tokens(i);
+            amounts[i] = token.balanceOf(this);
+            token.approve(_mtkn, 0);
+            token.approve(_mtkn, amounts[i]);
+        }
+
+        _mtkn.bundleFirstTokens(msg.sender, msg.value.mul(1000), amounts);
         if (address(this).balance > 0) {
             msg.sender.transfer(address(this).balance);
         }
