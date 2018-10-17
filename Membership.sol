2308e3e05d63e30d879867928632787d758835dd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:16:57 +0700

    add contract Membership

--- Membership.sol
+++ Membership.sol
@@ -1,314 +1,197 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Membership at 0x6f60c2fa9d71372be710681f13e4866cee38394c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Membership at 0x4286b9997df2af09e186c332e655e9cef71a40fa
 */
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
-// zeppelin-solidity: 1.9.0
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
+//Slightly modified SafeMath library - includes a min function
 library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
-    }
-    c = a * b;
-    assert(c / a == b);
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
     return c;
   }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
+    uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
+    return c;
   }
 
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
     assert(c >= a);
     return c;
   }
+
+  function min(uint a, uint b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
 }
 
-contract Membership is Ownable {
-  using SafeMath for uint;
 
-  mapping(address => bool) public isAdmin;
-  mapping(address => uint) public userToMemberIndex;
-  mapping(uint => uint[]) public tierToMemberIndexes;
+/**
+*This contract allows users to sign up for the DDA Cooperative Membership.
+*To complete membership DDA will provide instructions to complete KYC/AML verification
+*through a system external to this contract.
+*/
+contract Membership {
+    using SafeMath for uint256;
+    
+    /*Variables*/
+    address public owner;
+    //Memebership fees
+    uint public memberFee;
 
-  struct Member {
-    address addr;
-    uint tier;
-    uint tierIndex;
-    uint memberIndex;
-  }
-
-  Member[] private members;
-
-  event NewMember(address user, uint tier);
-  event UpdatedMemberTier(address user, uint oldTier, uint newTier);
-  event RemovedMember(address user, uint tier);
-
-  modifier onlyAdmin() {
-    require(isAdmin[msg.sender]);
-    _;
-  }
-
-  modifier isValidTier(uint _tier) {
-    require(_tier >= 1 && _tier <= 4);
-    _;
-  }
-
-  modifier notTryingToChangeFromTier1(address _user, uint _tier) {
-    require(members[userToMemberIndex[_user]].tier != _tier);
-    _;
-  }
-
-
-  modifier isMember(address _user) {
-    require(userToMemberIndex[_user] != 0);
-    _;
-  }
-
-  modifier isValidAddr(address _trgt) {
-    require(_trgt != address(0));
-    _;
-  }
-
-  constructor() public {
-    Member memory member = Member(address(0), 0, 0, 0);
-    members.push(member);
-  }
-
-  function addAdmin(address _user)
-    external
-    onlyOwner
-  {
-    isAdmin[_user] = true;
-  }
-
-  function removeMember(address _user)
-    external
-    onlyAdmin
-    isValidAddr(_user)
-    isMember(_user)
-  {
-    uint index = userToMemberIndex[_user];
-    require(index != 0);
-
-    Member memory removingMember = members[index];
-
-    uint tier = removingMember.tier;
-
-    uint lastTierIndex = tierToMemberIndexes[removingMember.tier].length - 1;
-    uint lastTierMemberIndex = tierToMemberIndexes[removingMember.tier][lastTierIndex];
-    Member storage lastTierMember = members[lastTierMemberIndex];
-    lastTierMember.tierIndex = removingMember.tierIndex;
-    tierToMemberIndexes[removingMember.tier][removingMember.tierIndex] = lastTierMember.memberIndex;
-    tierToMemberIndexes[removingMember.tier].length--;
-
-    Member storage lastMember = members[members.length - 1];
-    if (lastMember.addr != removingMember.addr) {
-      userToMemberIndex[lastMember.addr] = removingMember.memberIndex;
-      tierToMemberIndexes[lastMember.tier][lastMember.tierIndex] = removingMember.memberIndex;
-      lastMember.memberIndex = removingMember.memberIndex;
-      members[removingMember.memberIndex] = lastMember;
+    /*Structs*/
+    /**
+    *@dev Keeps member information 
+    */
+    struct Member {
+        uint memberId;
+        uint membershipType;
     }
-    userToMemberIndex[removingMember.addr] = 0;
-    members.length--;
+    
+    /*Mappings*/
+    //Members information
+    mapping(address => Member) public members;
+    address[] public membersAccts;
 
-    emit RemovedMember(_user, tier);
-  }
+    /*Events*/
+    event UpdateMemberAddress(address _from, address _to);
+    event NewMember(address _address, uint _memberId, uint _membershipType);
+    event Refund(address _address, uint _amount);
 
-  function addNewMember(address _user, uint _tier)
-    internal
-  {
-    // it's a new member
-    uint memberIndex = members.length; // + 1; // add 1 to keep index 0 unoccupied
-    uint tierIndex = tierToMemberIndexes[_tier].length;
-
-    Member memory newMember = Member(_user, _tier, tierIndex, memberIndex);
-
-    members.push(newMember);
-    userToMemberIndex[_user] = memberIndex;
-    tierToMemberIndexes[_tier].push(memberIndex);
-
-    emit NewMember(_user, _tier);
-  }
-
-  function updateExistingMember(address _user, uint _newTier)
-    internal
-  {
-    // this user is a member in another tier, remove him from that tier,
-    // and add him to the new tier
-    Member storage existingMember = members[userToMemberIndex[_user]];
-
-    uint oldTier = existingMember.tier;
-    uint tierIndex = existingMember.tierIndex;
-    uint lastTierIndex = tierToMemberIndexes[oldTier].length - 1;
-
-    if (tierToMemberIndexes[oldTier].length > 1 && tierIndex != lastTierIndex) {
-      Member storage lastMember = members[tierToMemberIndexes[oldTier][lastTierIndex]];
-      tierToMemberIndexes[oldTier][tierIndex] = lastMember.memberIndex;
-      lastMember.tierIndex = tierIndex;
+    /*Modifiers*/
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    /*Functions*/
+    /**
+    *@dev Constructor - Sets owner
+    */
+     constructor() public {
+        owner = msg.sender;
     }
 
-    tierToMemberIndexes[oldTier].length--;
-    tierToMemberIndexes[_newTier].push(existingMember.memberIndex);
-
-    existingMember.tier = _newTier;
-    existingMember.tierIndex = tierToMemberIndexes[_newTier].length - 1;
-
-    emit UpdatedMemberTier(_user, oldTier, _newTier);
-  }
-
-  function setMemberTier(address _user, uint _tier)
-    external
-    onlyAdmin
-    isValidAddr(_user)
-    isValidTier(_tier)
-  {
-    if (userToMemberIndex[_user] == 0) {
-      addNewMember(_user, _tier);
-    } else {
-      uint currentTier = members[userToMemberIndex[_user]].tier;
-      if (currentTier != _tier) {
-        // user's in tier 1 are lifetime tier 1 users
-        require(currentTier != 1);
-
-        updateExistingMember(_user, _tier);
-      }
+    /*
+    *@dev Updates the fee amount
+    *@param _memberFee fee amount for member
+    */
+    function setFee(uint _memberFee) public onlyOwner() {
+        //define fee structure for the three membership types
+        memberFee = _memberFee;
     }
-  }
-
-  function getTierOfMember(address _user)
-    external
-    view
-    returns (uint)
-  {
-    return members[userToMemberIndex[_user]].tier;
-  }
-
-  function getMembersOfTier(uint _tier)
-    external
-    view
-    returns (address[])
-  {
-    address[] memory addressesOfTier = new address[](tierToMemberIndexes[_tier].length);
-
-    for (uint i = 0; i < tierToMemberIndexes[_tier].length; i++) {
-      addressesOfTier[i] = members[tierToMemberIndexes[_tier][i]].addr;
+    
+    /**
+    *@notice Allows a user to become DDA members if they pay the fee. However, they still have to complete
+    *complete KYC/AML verification off line
+    *@dev This creates and transfers the token to the msg.sender
+    */
+    function requestMembership() public payable {
+        Member storage sender = members[msg.sender];
+        require(msg.value >= memberFee && sender.membershipType == 0 );
+        membersAccts.push(msg.sender);
+        sender.memberId = membersAccts.length;
+        sender.membershipType = 1;
+        emit NewMember(msg.sender, sender.memberId, sender.membershipType);
+    }
+    
+    /**
+    *@dev This updates/transfers the member address 
+    *@param _from is the current member address
+    *@param _to is the address the member would like to update their current address with
+    */
+    function updateMemberAddress(address _from, address _to) public onlyOwner {
+        require(_to != address(0));
+        Member storage currentAddress = members[_from];
+        Member storage newAddress = members[_to];
+        require(newAddress.memberId == 0);
+        newAddress.memberId = currentAddress.memberId;
+        newAddress.membershipType = currentAddress.membershipType;
+		membersAccts[currentAddress.memberId - 1] = _to;
+        currentAddress.memberId = 0;
+        currentAddress.membershipType = 0;
+        emit UpdateMemberAddress(_from, _to);
     }
 
-    return addressesOfTier;
-  }
-
-  function getMembersOfTierCount(uint _tier)
-    external
-    view
-    returns (uint)
-  {
-    return tierToMemberIndexes[_tier].length;
-  }
-
-  function getMembersCount()
-    external
-    view
-    returns (uint)
-  {
-    if (members.length == 0) {
-      return 0;
-    } else {
-      // remove sentinel at index zero from count
-      return members.length - 1;
+    /**
+    *@dev Use this function to set membershipType for the member
+    *@param _memberAddress address of member that we need to update membershipType
+    *@param _membershipType type of membership to assign to member
+    */
+    function setMembershipType(address _memberAddress,  uint _membershipType) public onlyOwner{
+        Member storage memberAddress = members[_memberAddress];
+        memberAddress.membershipType = _membershipType;
     }
-  }
 
-  function getMemberByIdx(uint _idx)
-    external
-    view
-    returns (address, uint)
-  {
-    Member memory member = members[_idx];
+    /**
+    *@dev getter function to get all membersAccts
+    */
+    function getMembers() view public returns (address[]){
+        return membersAccts;
+    }
+    
+    /**
+    *@dev Get member information
+    *@param _memberAddress address to pull the memberId, membershipType and membership
+    */
+    function getMember(address _memberAddress) view public returns(uint, uint) {
+        return(members[_memberAddress].memberId, members[_memberAddress].membershipType);
+    }
 
-    return (member.addr, member.tier);
-  }
+    /**
+    *@dev Gets length of array containing all member accounts or total supply
+    */
+    function countMembers() view public returns(uint) {
+        return membersAccts.length;
+    }
 
-  function isUserMember(address _user)
-    external
-    view
-    returns (bool)
-  {
-    return userToMemberIndex[_user] != 0;
-  }
+    /**
+    *@dev Gets membership type
+    *@param _memberAddress address to view the membershipType
+    */
+    function getMembershipType(address _memberAddress) public constant returns(uint){
+        return members[_memberAddress].membershipType;
+    }
+    
+    /**
+    *@dev Allows the owner to set a new owner address
+    *@param _new_owner the new owner address
+    */
+    function setOwner(address _new_owner) public onlyOwner() { 
+        owner = _new_owner; 
+    }
 
-  function getMemberIdxOfUser(address _user)
-    external
-    view
-    returns (uint)
-  {
-    return userToMemberIndex[_user];
-  }
+    /**
+    *@dev Refund money if KYC/AML fails
+    *@param _to address to send refund
+    *@param _amount to refund. If no amount  is specified the current memberFee is refunded
+    */
+    function refund(address _to, uint _amount) public onlyOwner {
+        require (_to != address(0));
+        if (_amount == 0) {_amount = memberFee;}
+        Member storage currentAddress = members[_to];
+        membersAccts[currentAddress.memberId-1] = 0;
+        currentAddress.memberId = 0;
+        currentAddress.membershipType = 0;
+        _to.transfer(_amount);
+        emit Refund(_to, _amount);
+    }
+
+    /**
+    *@dev Allow owner to withdraw funds
+    *@param _to address to send funds
+    *@param _amount to send
+    */
+    function withdraw(address _to, uint _amount) public onlyOwner {
+        _to.transfer(_amount);
+    }    
 }
