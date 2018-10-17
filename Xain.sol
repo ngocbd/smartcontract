bc6219f92b9eb491f4f4d013d0195c7d321850cc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:07:55 +0700

    add contract XAIN

--- Xain.sol
+++ Xain.sol
@@ -1,150 +1,224 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Xain at 0xC6cB0b17c9aCccD02e48d92151394721BC5Bf3fc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract XAIN at 0xa49cc7bb3437d67ae57979ddc9d1ef68a4f91461
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
+
+/*
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 
-contract ForeignToken {
-    function balanceOf(address _owner) public constant returns (uint256);
-    function transfer(address _to, uint256 _value) public returns (bool);
-}
 
-contract ERC20Basic {
-
-  uint256 public totalSupply;
-  function balanceOf(address who) public constant returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-
-}
-
-contract ERC20 is ERC20Basic {
-
-  function allowance(address owner, address spender) public constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-
-}
-
-contract Xain is ERC20 {
-    
-    address owner = msg.sender;
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    
-    uint256 public totalSupply = 283475912 * 10**8;
-
-    function name() public pure returns (string) { return "Xain"; }
-    function symbol() public pure returns (string) { return "XAIN"; }
-    function decimals() public pure returns (uint8) { return 8; }
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    event DistrFinished();
-
-    bool public distributionFinished = false;
-
-    modifier canDistr() {
-    require(!distributionFinished);
-    _;
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
 
-    function Xain() public {
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+
+
+contract Ownable {
+
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() internal {
         owner = msg.sender;
-        balances[msg.sender] = totalSupply;
     }
 
-    modifier onlyOwner { 
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner public {
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
+}
 
-    function getEthBalance(address _addr) constant public returns(uint) {
-    return _addr.balance;
+
+
+
+
+contract XAIN_ERC20 is Ownable {
+
+    using SafeMath for uint256;
+
+
+    mapping (address => uint256) public balances;
+
+
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+
+
+    // Public variables for the XAIN ERC20 XNP token contract
+    string public constant standard = "XAIN erc20 and Genesis";
+    uint256 public constant decimals = 18;   // hardcoded to be a constant
+    string public name = "XAIN";
+    string public symbol = "XNP";
+    uint256 public totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
     }
 
-    function distributeXAIN(address[] addresses, uint256 _value, uint256 _ethbal) onlyOwner canDistr public {
-         for (uint i = 0; i < addresses.length; i++) {
-	     if (getEthBalance(addresses[i]) < _ethbal) {
- 	         continue;
-             }
-             balances[owner] -= _value;
-             balances[addresses[i]] += _value;
-             Transfer(owner, addresses[i], _value);
-         }
-    }
-    
-    function balanceOf(address _owner) constant public returns (uint256) {
-	 return balances[_owner];
-    }
+    function transfer(address _to, uint256 _value) public returns (bool) {
 
-    // mitigates the ERC20 short address attack
-    modifier onlyPayloadSize(uint size) {
-        assert(msg.data.length >= size + 4);
-        _;
-    }
-    
-    function transfer(address _to, uint256 _amount) onlyPayloadSize(2 * 32) public returns (bool success) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
 
-         if (balances[msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(msg.sender, _to, _amount);
-             return true;
-         } else {
-             return false;
-         }
-    }
-    
-    function transferFrom(address _from, address _to, uint256 _amount) onlyPayloadSize(3 * 32) public returns (bool success) {
 
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(_from, _to, _amount);
-             return true;
-         } else {
-            return false;
-         }
-    }
-    
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        // mitigates the ERC20 spend/approval race condition
-        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
-        
-        allowed[msg.sender][_spender] = _value;
-        
-        Approval(msg.sender, _spender, _value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
-    
-    function allowance(address _owner, address _spender) constant public returns (uint256) {
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+
+
+        balances[_to] = balances[_to].add(_value);
+
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
         return allowed[_owner][_spender];
     }
 
-    function finishDistribution() onlyOwner public returns (bool) {
-    distributionFinished = true;
-    DistrFinished();
-    return true;
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
     }
 
-    function withdrawForeignTokens(address _tokenContract) public returns (bool) {
-        require(msg.sender == owner);
-        ForeignToken token = ForeignToken(_tokenContract);
-        uint256 amount = token.balanceOf(address(this));
-        return token.transfer(owner, amount);
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
     }
+
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+}
+
+
+
+interface tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external ;
+}
+
+
+//
+// This creates and adds six genesis pools of XNP tokens to the balance of the A,B,C,D,E and F Ethereum addresses
+//
+
+contract XAIN is XAIN_ERC20 {
+
+
+        address public A;
+        address public B;
+        address public C;
+        address public D;
+        address public E;
+        address public F;
+
+
+    constructor (
+
+        ) public {
+
+        A = 0xc3b60984Df1FeffBd884Da6C083EaB735563C641;
+        B = 0xD5b8D79dE753C98f165bD8d3eb896C1276c4B1FF;
+        C = 0x75B351AD3e51376C9a3373D724e16daA52C54cD5;
+        D = 0x908dA0Eb55C64Ea116A47a9bF62C6bfBd542FA81;
+        E = 0x48875C46796C14e3fDC27D7acfBbd4a0f2a39953;
+        F = 0xA010C083B38A9013d7E1Db8b4e5015BB7b280224;
+
+        balances[A]=balances[A].add(5000000*(uint256(10)**decimals));
+        balances[B]=balances[B].add(5000000*(uint256(10)**decimals));
+        balances[C]=balances[C].add(10000000*(uint256(10)**decimals));
+        balances[D]=balances[D].add(10000000*(uint256(10)**decimals));
+        balances[E]=balances[E].add(25000000*(uint256(10)**decimals));
+        balances[F]=balances[F].add(45000000*(uint256(10)**decimals));
+
+        totalSupply=balances[A]+balances[B]+balances[C]+balances[D]+balances[E]+balances[F];
+
+
+    }
+
+
 }
