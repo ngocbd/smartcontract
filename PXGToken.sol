7873fd2df683b1ea59b019aa47b95e36f971d81f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:16:12 +0700

    add contract PXGToken

--- PXGToken.sol
+++ PXGToken.sol
@@ -1,118 +1,109 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PXGToken at 0x47e67BA66b0699500f18A53F94E2b9dB3D47437e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PXGToken at 0x126bd4a9a438b0ad659fb1c7f646ee2ebe90c500
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.19;
 
-contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
+
+/**
+ * PIEXGO TEAM
+ */
+ 
+contract SafeMath {
+  function mul(uint256 a, uint256 b) internal returns (uint256) {
     uint256 c = a * b;
-    assert(c / a == b);
+    assert(a == 0 || c / a == b);
     return c;
   }
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+
+  function div(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b > 0);
     uint256 c = a / b;
+    assert(a == b * c + a % b);
     return c;
   }
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+
+  function sub(uint256 a, uint256 b) internal returns (uint256) {
     assert(b <= a);
     return a - b;
   }
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+
+  function add(uint256 a, uint256 b) internal returns (uint256) {
     uint256 c = a + b;
-    assert(c >= a);
+    assert(c>=a && c>=b);
     return c;
   }
-}
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-  mapping(address => uint256) balances;
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_value > 0);
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-}
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-contract StandardToken is ERC20, BasicToken {
-  mapping (address => mapping (address => uint256)) internal allowed;
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_value > 0);
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    Transfer(_from, _to, _value);
-    return true;
-  }
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    require(_value > 0);
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
-  }
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    require(_addedValue > 0);
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    require(_subtractedValue > 0);
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
     }
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
   }
 }
-contract BurnableToken is BasicToken {
-  event Burn(address indexed burner, uint256 value);
-  function burn(uint256 _value) public {
-    require(_value > 0);
-    require(_value <= balances[msg.sender]);
-    address burner = msg.sender;
-    balances[burner] = balances[burner].sub(_value);
-    totalSupply = totalSupply.sub(_value);
-    Burn(burner, _value);
-  }
+
+contract Token {
+
+    function totalSupply() constant returns (uint256 supply) {}
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
-contract PXGToken is StandardToken, BurnableToken {
-  string public constant name = "PlayGame"; 
-  string public constant symbol = "PXG"; 
-  uint8 public constant decimals = 18; 
-  uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
-  function PXGToken() public {
-    totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-  }
+
+contract RegularToken is Token, SafeMath {
+
+    function transfer(address _to, uint256 _value) returns (bool) {
+        require(balances[msg.sender] >= _value);
+        require(balances[_to] + _value >= balances[_to]);
+        balances[msg.sender] = sub(balances[msg.sender], _value);
+        balances[_to] = add(balances[_to], _value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+        require(balances[_from] >= _value);
+        require(balances[_to] + _value >= balances[_to]);
+        require(allowed[_from][msg.sender] >= _value);
+        balances[_from] = sub(balances[_from], _value);
+        balances[_to] = add(balances[_to], _value);
+        allowed[_from][msg.sender] = sub(allowed[_from][msg.sender], _value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant returns (uint256) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+
+contract PXGToken is RegularToken {
+
+    uint256 public totalSupply = 100*10**(18+8);
+    uint8 constant public decimals = 18;
+    string constant public name = "PIEXGO";
+    string constant public symbol = "PXG";
+
+    function PXGToken() {
+        balances[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
 }
