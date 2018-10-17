4099310e08aecdf0106bfe95d62ede48add1ff35
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:33:35 +0700

    add contract Shitcoin

--- Shitcoin.sol
+++ Shitcoin.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Shitcoin at 0x37bbeec2e1c7f1e53e72c32d333f28e4155ca5b1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Shitcoin at 0xa6590f2e04887977d3bab77ff906c83687ac5628
 */
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.16;
 
 contract Token {
 
@@ -90,40 +90,27 @@ contract StandardToken is Token {
 }
 
 
-//name this contract whatever you'd like
 contract Shitcoin is StandardToken {
 
     function () {
-        //if ether is sent to this address, send it back.
         throw;
     }
 
     /* Public variables of the token */
 
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name = "Shitcoin";                   //fancy name: eg Simon Bucks
-    uint8 public decimals = 1;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol = "SHT";                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    string public name;                   
+    uint8 public decimals;                
+    string public symbol;                 
+    string public version = 'H1.0';    
 
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
-
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function Shitcoin(
         ) {
-        balances[msg.sender] = 10000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 10000000000;                        // Update total supply (100000 for example)
-        name = "Shitcoin";                                   // Set the name for display purposes
-        decimals = 1;                            // Amount of decimals for display purposes
-        symbol = "SHT";                               // Set the symbol for display purposes
+        balances[msg.sender] = 1000000000000000000000000000;
+        totalSupply = 1000000000000000000000000000;
+        name = "Shitcoin";
+        decimals = 18;
+        symbol = "SHIT";
     }
 
     /* Approves and then calls the receiving contract */
