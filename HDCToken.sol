435c4eb94c0dc52c33294b8e29f431292ba95f2f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:07:49 +0700

    add contract HDCToken

--- HDCToken.sol
+++ HDCToken.sol
@@ -1,153 +1,118 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HDCToken at 0x5b4e0b60f6f3725628d831057c8a624422b2e9af
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HDCToken at 0x7A6910B15d929F20F85ecbfCBd89862062147D78
 */
 pragma solidity ^0.4.18;
 
-  // ----------------------------------------------------------------------------------------------
-  // Sample fixed supply token contract
-  // Enjoy. (c) BokkyPooBah 2017. The MIT Licence.
-  // ----------------------------------------------------------------------------------------------
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-   // ERC Token Standard #20 Interface
-  // https://github.com/ethereum/EIPs/issues/20
-  contract ERC20Interface {
-      // ???????
-      function totalSupply() constant public returns (uint256 _totalSupply);
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-      // ?????????
-      function balanceOf(address _owner) constant public returns (uint256 balance);
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-      // ???????token
-      function transfer(address _to, uint256 _value) public returns (bool success);
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
 
-      // ???????????????
-      function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+contract StandardToken {
+    using SafeMath for uint256;
 
-      //??_spender???????_value?????????????????DEX???????
-      function approve(address _spender, uint256 _value) public returns (bool success);
+    uint256 totalSupply_;
 
-      // ??_spender?????_owner???????
-      function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
 
-      // token???????
-      event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-      // approve(address _spender, uint256 _value)?????
-      event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-  }
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
 
-   //????????
-   contract HDCToken is ERC20Interface {
-      string public constant symbol = "HDCT"; //??
-      string public constant name = "Health Data Chain Token"; //??
-      uint8 public constant decimals = 18; //???????
-      uint256 _totalSupply = 10000000000000000000000000000; //????
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
 
-      // ????????
-      address public owner;
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
 
-      // ???????
-      mapping(address => uint256) balances;
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
-      // ???????????????????????????????allowed[??????][??????]
-      mapping(address => mapping (address => uint256)) allowed;
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
 
-      // ???????????????????
-      modifier onlyOwner() {
-          require (msg.sender != owner);
-          _;
-      }
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
 
-	  bool public paused = false;
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-      /**
-       * @dev Modifier to make a function callable only when the contract is not paused.
-       */
-      modifier whenNotPaused() {
-        require(!paused);
-        _;
-      }
-    
-      /**
-       * @dev Modifier to make a function callable only when the contract is paused.
-       */
-      modifier whenPaused() {
-        require(paused);
-        _;
-      }
-    
-      /**
-       * @dev called by the owner to pause, triggers stopped state
-       */
-      function pause() onlyOwner whenNotPaused public {
-        paused = true;
-      }
-    
-      /**
-       * @dev called by the owner to unpause, returns to normal state
-       */
-      function unpause() onlyOwner whenPaused public {
-        paused = false;
-      }
-  
-      // ????
-      constructor () public {
-          owner = msg.sender;
-          balances[owner] = _totalSupply;
-      }
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
 
-      function  totalSupply() public constant returns (uint256 totalSupplyRet) {
-          totalSupplyRet = _totalSupply;
-      }
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
 
-      // ???????
-      function balanceOf(address _owner) public constant returns (uint256 balance) {
-          return balances[_owner];
-      }
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+}
 
-      // ?????????
-      function transfer(address _to, uint256 _amount) public whenNotPaused returns (bool success) {
-          require(_to != address(0x0) );
 
-          require (balances[msg.sender] >= _amount 
-              && _amount > 0
-              && balances[_to] + _amount > balances[_to]); 
-              
-            balances[msg.sender] -= _amount;
-            balances[_to] += _amount;
-            emit Transfer(msg.sender, _to, _amount);
-            return true;
-      }
+contract HDCToken is StandardToken {
+    string public name    = "Hyper Dimension Chain";
+    string public symbol  = "HDCC";
+    uint8 public decimals = 18;
 
-      //???????????????????????????
-      function transferFrom(
-          address _from,
-          address _to,
-          uint256 _amount
-      ) public whenNotPaused returns (bool success) {
-          require(_to != address(0x0) );
-          
-          require (balances[_from] >= _amount
-              && allowed[_from][msg.sender] >= _amount
-              && _amount > 0
-              && balances[_to] + _amount > balances[_to]);
-              
-            balances[_from] -= _amount;
-            allowed[_from][msg.sender] -= _amount;
-            balances[_to] += _amount;
-            emit Transfer(_from, _to, _amount);
-            return true;
-      }
+    // 10 billion in initial supply
+    uint256 public totalSupply = 10000000000 * (10 ** uint256(decimals));
 
-      //??????????????????????????
-      function approve(address _spender, uint256 _amount) public whenNotPaused returns (bool success) {
-          allowed[msg.sender][_spender] = _amount;
-          emit Approval(msg.sender, _spender, _amount);
-          return true;
-      }
-
-      //????????????
-      function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-          return allowed[_owner][_spender];
-      }
-  }
+    function HDCToken() public {
+        balances[msg.sender] = totalSupply;
+        Transfer(address(0), msg.sender, totalSupply);
+    }
+}
