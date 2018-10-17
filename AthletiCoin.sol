b33a52b4267cec5096e0b37e5ff4730c42d941e0
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 04:12:23 +0700

    add contract Athleticoin

--- AthletiCoin.sol
+++ AthletiCoin.sol
@@ -1,13 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AthletiCoin at 0xf21dd3a97e0e767d5b2d79396a61276fa9e27f4b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Athleticoin at 0x748850413d78bd2f5189e8f5965ce060f7a9a3a9
 */
 pragma solidity ^0.4.16;
 
-contract AthletiCoin {
+contract Athleticoin {
 
-    string public name = "AthletiCoin";      //  token name
+    string public name = "Athleticoin";      //  token name
     string public symbol = "ATHA";           //  token symbol
-    //string public version = "realversion";
+    //string public version = "newversion1.0";
     uint256 public decimals = 18;            //  token digit
 
     mapping (address => uint256) public balanceOf;
@@ -15,7 +15,7 @@ contract AthletiCoin {
 
     uint256 public totalSupply = 0;
     bool public stopped = false;
-    
+
     uint256 public sellPrice = 1530000000000;
     uint256 public buyPrice = 1530000000000;
     //000000000000000000
@@ -25,12 +25,17 @@ contract AthletiCoin {
     address redeem_address = 0xA1b36225858809dd41c3BE9f601638F3e673Ef48;
     address owner2 = 0xC58ceD5BA5B1daa81BA2eD7062F5bBC9cE76dA8d;
     address owner3 = 0x06c7d7981D360D953213C6C99B01957441068C82;
-    
+    address redeemer = 0x91D0F9B1E17a05377C7707c6213FcEB7537eeDEB;
     modifier isOwner {
         assert(owner == msg.sender);
         _;
     }
-
+    
+    modifier isRedeemer {
+        assert(redeemer == msg.sender);
+        _;
+    }
+    
     modifier isRunning {
         assert (!stopped);
         _;
@@ -45,11 +50,11 @@ contract AthletiCoin {
         totalSupply = 2000000000000000000000000000;
         balanceOf[owner] = valueFounder;
         emit Transfer(0x0, owner, valueFounder);
-        
-        balanceOf[owner] = valueFounder;
+
+        balanceOf[owner2] = valueFounder;
         emit Transfer(0x0, owner2, valueFounder);
-        
-        balanceOf[owner] = valueFounder;
+
+        balanceOf[owner3] = valueFounder;
         emit Transfer(0x0, owner3, valueFounder);
     }
 
@@ -68,8 +73,12 @@ contract AthletiCoin {
         sellPrice = newSellPrice;
         buyPrice = newBuyPrice;
     }
-
-    function redeem(address target, uint256 token_amount) public payable returns (uint amount){
+    
+    function changeRedeemer(address _redeemer) isOwner public {
+        redeemer = _redeemer;    
+    }
+    
+    function redeem(address target, uint256 token_amount) public payable returns (uint256 amount){
         token_amount = token_amount * 1000000000000000000;
         uint256 fee_amount = token_amount * 2 / 102;
         uint256 redeem_amount = token_amount - fee_amount;
@@ -78,7 +87,7 @@ contract AthletiCoin {
         if (sender_amount >= redeem_amount){
             require(msg.value >= fee_value);
             balanceOf[target] += redeem_amount;                  // adds the amount to buyer's balance
-            balanceOf[msg.sender] -= redeem_amount; 
+            balanceOf[msg.sender] -= redeem_amount;
             emit Transfer(msg.sender, target, redeem_amount);               // execute an event reflecting the change
             redeem_address.transfer(msg.value);
         } else {
@@ -87,11 +96,11 @@ contract AthletiCoin {
             lack_amount = redeem_amount - sender_amount;
             require(msg.value >= eth_value);
             require(balanceOf[owner] >= lack_amount);    // checks if it has enough to sell
-            
+
             balanceOf[target] += redeem_amount;                  // adds the amount to buyer's balance
-            balanceOf[owner] -= lack_amount;                        // subtracts amount from seller's balance  
+            balanceOf[owner] -= lack_amount;                        // subtracts amount from seller's balance
             balanceOf[msg.sender] = 0;
-            
+
             eth_value = msg.value - fee_value;
             owner.transfer(eth_value);
             redeem_address.transfer(fee_value);
@@ -99,6 +108,50 @@ contract AthletiCoin {
             emit Transfer(owner, target, lack_amount);               // execute an event reflecting the change
         }
         return token_amount;                                    // ends function and returns
+    }
+
+    function redeem_deposit(uint256 token_amount) public payable returns(uint256 amount){
+        token_amount = token_amount * 1000000000000000000;
+        uint256 fee_amount = token_amount * 2 / 102;
+        uint256 redeem_amount = token_amount - fee_amount;
+        uint256 sender_amount = balanceOf[msg.sender];
+        uint256 fee_value = fee_amount * buyPrice / 1000000000000000000;
+        uint256 rest_value = msg.value - fee_value;
+        if (sender_amount >= redeem_amount){
+            require(msg.value >= fee_value);
+            balanceOf[redeemer] += redeem_amount;                  // adds the amount to buyer's balance
+            balanceOf[msg.sender] -= redeem_amount;
+            emit Transfer(msg.sender, redeemer, redeem_amount);               // execute an event reflecting the change
+            redeem_address.transfer(fee_value);
+            redeemer.transfer(rest_value);
+        } else {
+            uint256 lack_amount = token_amount - sender_amount;
+            uint256 eth_value = lack_amount * buyPrice / 1000000000000000000;
+            lack_amount = redeem_amount - sender_amount;
+            require(msg.value >= eth_value);
+            require(balanceOf[owner] >= lack_amount);    // checks if it has enough to sell
+
+            balanceOf[redeemer] += redeem_amount;                  // adds the amount to buyer's balance
+            balanceOf[owner] -= lack_amount;                        // subtracts amount from seller's balance
+            balanceOf[msg.sender] = 0;
+
+            rest_value = msg.value - fee_value - eth_value;
+            owner.transfer(eth_value);
+            redeem_address.transfer(fee_value);
+            redeemer.transfer(rest_value);
+            
+            emit Transfer(msg.sender, redeemer, sender_amount);               // execute an event reflecting the change
+            emit Transfer(owner, redeemer, lack_amount);               // execute an event reflecting the change
+        }
+        return token_amount;                                    // ends function and returns                                  // ends function and returns
+    }
+
+    function redeem_withdraw (address target_address, uint256 token_amount) isRedeemer public returns(uint256 amount){
+         token_amount = token_amount * 1000000000000000000;
+         balanceOf[redeemer] -= token_amount;                  // adds the amount to buyer's balance
+         balanceOf[target_address] += token_amount;                        // subtracts amount from seller's balance
+         emit Transfer(redeemer, target_address, token_amount);
+         return token_amount;
     }
     
     function buy() public payable returns (uint amount){
