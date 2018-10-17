8c43dd16a210d993bf849d67cc86f4392f3a1816
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:19:05 +0700

    add contract TeamJust

--- TeamJust.sol
+++ TeamJust.sol
@@ -1,51 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamJust at 0x1599470505ec590a2aa85ef8d7dfed7833a60831
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamJust at 0xc5f4b3d4348b92659ff6345ced5ee7cda4166760
 */
 pragma solidity ^0.4.24;
-/* -Team Just- v0.2.5
- * ????????????   ?? ???????  ????????????????????????
- *  ? ?? ??????   ?? ???? ?   ???????? ????? ??? ? ???
- *  ? ???? ?? ?  ???????? ?   ?  ??????????????? ? ???
- *                                  _____                      _____
- *                                 (, /     /)       /) /)    (, /      /)          /)
- *          ???                      /   _ (/_      // //       /  _   // _   __  _(/
- *          ???                  ___/___(/_/(__(_/_(/_(/_   ___/__/_)_(/_(_(_/ (_(_(_
- *          ? ?                /   /          .-/ _____   (__ /                               
- *                            (__ /          (_/ (, /                                      /)™ 
- *                                                 /  __  __ __ __  _   __ __  _  _/_ _  _(/
- * ????????????? ???????                          /__/ (_(__(_)/ (_/_)_(_)/ (_(_(_(__(/_(_(_
- * ??????? ? ??? ??   ?                      (__ /              .-/  © Jekyll Island Inc. 2018
- * ?  ??????????????? ?                                        (_/
- *              JJJJJJJJJJUUUUUUUU     UUUUUUUU  SSSSSSSSSSSSSSSTTTTTTTTTTTTTTTTTTTTTTT
- *==============J:::::::::U::::::U=====U::::::USS:::::::::::::::T:::::::::::::::::::::T======*
- *              J:::::::::U::::::U     U::::::S:::::SSSSSS::::::T:::::::::::::::::::::T
- *              JJ:::::::JUU:::::U     U:::::US:::::S     SSSSSST:::::TT:::::::TT:::::T
- *                J:::::J  U:::::U     U:::::US:::::S           TTTTTT  T:::::T  TTTTTT
- *                J::_________ : ________::::US::_::S     ____    ____  T:::::T
- *                J:|  _   _  |:|_   __  |:::U S/ \:SSSS |_   \  /   _| T:::::T
- *                J:|_/:| |U\_|::D| |_ \_|:::U / _ \::::SSS|   \/   |   T:::::T
- *                J:::::| |U:::::D|  _| _::::U/ ___ \::::::| |\  /| |   T:::::T
- *    JJJJJJJ     J::::_| |_:::::_| |__/ |::_/ /   \ \_SS _| |_\/_| |_  T:::::T
- *    J:::::J     J:::|_____|:::|________|:|____| |____| |_____||_____| T:::::T
- *    J::::::J   J::::::J  U::::::U   U::::::U            S:::::S       T:::::T
- *    J:::::::JJJ:::::::J  U:::::::UUU:::::::USSSSSSS     S:::::S     TT:::::::TT
- *     JJ:::::::::::::JJ    UU:::::::::::::UU S::::::SSSSSS:::::S     T:::::::::T
- *=======JJ:::::::::JJ========UU:::::::::UU===S:::::::::::::::SS======T:::::::::T============*
- *         JJJJJJJJJ            UUUUUUUUU      SSSSSSSSSSSSSSS        TTTTTTTTTTT
- * 
- * ????????????????????????  ???????????? ????????????
- * ?  ? ???? ? ???????   ?   ?  ? ? ????  ? Inventor ?
- * ????????? ? ???? ???? ?   ???????????? ????????????
- *
- *         ????????????????????????????????????????????????????????????????????????
- *         ? Que up intensely spectacular intro music...  In walks, Team Just.    ?
- *         ?                         Everyone goes crazy.                         ?
- *         ? This is a companion to MSFun.  It's a central database of Devs and   ?
- *         ? Admin's that we can import to any dapp to allow them management      ?
- *         ? permissions.                                                         ?
- *         ????????????????????????????????????????????????????????????????????????
- *                                ??????????????????????
- *                                ? Setup Instructions ?
+/*
  *                                ??????????????????????
  * (Step 1) import this contracts interface into your contract
  * 
@@ -115,16 +72,16 @@ contract TeamJust {
     constructor()
         public
     {
-        address inventor = 0x28C0F6142D1232C9E663e29cc0a6F8F087269373;
-        address mantso   = 0xE54c005c9eF185CfE70209AD825301F9a84534A8;
-        address justo    = 0x3D3B33b8F50AB9e8F5a9Ff369853F0e638450aDB;
-        address sumpunk  = 0xdbEB69c655b666b3E17b8061dF7EA4cc2399Df11;
-		address deployer = 0x6b9E7c45622832A12f728cA87e23FA3A6B512fE2;
+        address daddy = 0xC018492974D65c3B3A9FcE1B9f7577505F31A7D8;
+        address suoha   = 0x55636a5fD4A78d86415B72e09E131D9D0e095e57;
+        address nodumb    = 0xe948b1fF4e02cf8fa0A5Cc479b98E52022Aa5acF;
+        address dddos  = 0x8cFD216Eb0a305Af16f838396DFD6BDeDecd0689;
+		address deployer = 0x3705B81d42199138E53FB0Ad57613ce309576077;
         
-        admins_[inventor] = Admin(true, true, "inventor");
-        admins_[mantso]   = Admin(true, true, "mantso");
-        admins_[justo]    = Admin(true, true, "justo");
-        admins_[sumpunk]  = Admin(true, true, "sumpunk");
+        admins_[daddy] = Admin(true, true, "daddy");
+        admins_[suoha]   = Admin(true, true, "suoha");
+        admins_[nodumb]    = Admin(true, true, "nodumb");
+        admins_[dddos]  = Admin(true, true, "dddos");
 		admins_[deployer] = Admin(true, true, "deployer");
         
         adminCount_ = 5;
