de39bbaaa1e2016f5b2a28762ad797f58b422b19
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:35:46 +0700

    add contract Aeron

--- Aeron.sol
+++ Aeron.sol
@@ -1,139 +1,81 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Aeron at 0xad86ef88e72f87ca2b4476d9fc4b0b0cbf1f3d8d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Aeron at 0x4c3da77d8bce020d4128995d7f92c5bd0919fcc3
 */
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.16;
 
-/**
- * Math operations with safety checks
- */
-contract SafeMath {
-  function safeMul(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-  function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b > 0);
-    uint256 c = a / b;
-    assert(a == b * c + a % b);
-    return c;
-  }
-
-  function safeSub(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function safeAdd(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
-
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      revert();
-    }
-  }
-}
-
-contract Aeron is SafeMath {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+contract Aeron {
+    // Public variables of the token
+    string public name = "Aeron";
+    string public symbol = "ARN";
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default
     uint256 public totalSupply;
-    address public owner;
-
-    /* This creates an array with all balances */
+    uint256 public datboiSupply = 20000000;
+    uint256 public buyPrice = 1000;
+    address public creator;
+    // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
-    mapping (address => uint256) public freezeOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    /* This generates a public event on the blockchain that will notify clients */
+    // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
-
-    /* This notifies clients about the amount frozen */
-    event Freeze(address indexed from, uint256 value);
-
-    /* This notifies clients about the amount unfrozen */
-    event Unfreeze(address indexed from, uint256 value);
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function Aeron() {
-        balanceOf[msg.sender] = 100000000;       // Give the creator all initial tokens
-        totalSupply = 100000000;                 // Update total supply
-        name = 'Aeron';                          // Set the name for display purposes
-        symbol = 'ARN';                          // Set the symbol for display purposes
-        decimals = 8;                            // Amount of decimals for display purposes
-	owner = msg.sender;
+    event FundTransfer(address backer, uint amount, bool isContribution);
+    
+    
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function Aeron() public {
+        totalSupply = datboiSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;    // Give DatBoiCoin Mint the total created tokens
+        creator = msg.sender;
     }
-
-    /* Send tokens */
-    function transfer(address _to, uint256 _value) {
-        if (_to == 0x0) revert();                               // Prevent transfer to 0x0 address. Use burn() instead
-	if (_value <= 0) revert();
-        if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();   // Check for overflows
-        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);              // Subtract from the sender
-        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                      // Notify anyone listening that this transfer took place
-    }
-
-    /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        if (_value <= 0) revert();
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-
-    /* Transfer tokens */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (_to == 0x0) revert();                                // Prevent transfer to 0x0 address. Use burn() instead
-	if (_value <= 0) revert();
-        if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) revert();     // Check allowance
-        balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);                         // Subtract from the sender
-        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                             // Add the same to the recipient
-        allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
         Transfer(_from, _to, _value);
-        return true;
+      
     }
 
-    /* Destruction of the token */
-    function burn(uint256 _value) returns (bool success) {
-        if (balanceOf[msg.sender] < _value) revert();            // Check if the sender has enough
-	if (_value <= 0) revert();
-        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);           // Subtract from the sender
-        totalSupply = SafeMath.safeSub(totalSupply,_value);                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
     }
 
-    function freeze(uint256 _value) returns (bool success) {
-        if (balanceOf[msg.sender] < _value) revert();            // Check if the sender has enough
-	if (_value <= 0) revert();
-        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);             // Subtract from the sender
-        freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);               // Updates frozen tokens
-        Freeze(msg.sender, _value);
-        return true;
+    
+    
+    /// @notice Buy tokens from contract by sending ether
+    function () payable internal {
+        uint amount = msg.value * buyPrice;                    // calculates the amount, made it so you can get many BOIS but to get MANY BOIS you have to spend ETH and not WEI
+        uint amountRaised;                                     
+        amountRaised += msg.value;                            //many thanks bois, couldnt do it without r/me_irl
+        require(balanceOf[creator] >= amount);               // checks if it has enough to sell
+        balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
+        balanceOf[creator] -= amount;                        // sends ETH to DatBoiCoinMint
+        Transfer(creator, msg.sender, amount);               // execute an event reflecting the change
+        creator.transfer(amountRaised);
     }
 
-    function unfreeze(uint256 _value) returns (bool success) {
-        if (freezeOf[msg.sender] < _value) revert();            // Check if the sender has enough
-	if (_value <= 0) revert();
-        freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);              // Updates frozen tokens
-	balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);           // Add to the sender
-        Unfreeze(msg.sender, _value);
-        return true;
-    }
-
-  /* Prevents accidental sending of Ether */
-  function () {
-      revert();
-  }
-}
+ }
