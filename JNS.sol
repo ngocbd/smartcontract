ddc2835b5874b9614c9737b13bb7eb8fbf5a1d39
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:43:25 +0700

    add contract JNS

--- JNS.sol
+++ JNS.sol
@@ -1,19 +1,27 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract JNS at 0xfd77ba77e858703242c1d40491b5ddbe47ca4876
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract JNS at 0xb2c0aeaacfebbd7df7951a95d423a4e82cfb18eb
 */
 pragma solidity 0.4.24;
 
 contract JNS {
     mapping (string => address) strToAddr;
     mapping (address => string) addrToStr;
+    address private wallet;
+
+    constructor (address _wallet) public {
+        require(_wallet != address(0), "You must inform a valid address");
+        wallet = _wallet;
+    }
     
-    function registerAddress (string _nickname, address _address) public returns (bool) {
+    function registerAddress (string _nickname, address _address) public payable returns (bool) {
+        require (msg.value > 1000000000000000, "Send more money");
         require (strToAddr[_nickname] == address(0), "Name already registered");
         require (keccak256(addrToStr[_address]) == keccak256(""), "Address already registered");
         
         strToAddr[_nickname] = _address;
         addrToStr[_address] = _nickname;
-        
+
+        wallet.transfer(msg.value);
         return true;
     }
     
@@ -24,5 +32,4 @@ contract JNS {
     function getNickname (address _address) public view returns (string _nickname) {
         _nickname = addrToStr[_address];
     }
-    
 }
