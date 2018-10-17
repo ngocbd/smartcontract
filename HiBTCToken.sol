85417f8a5ad88903549b60a505e9fe7c735ae0d9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:45:11 +0700

    add contract HiBTCToken

--- HiBTCToken.sol
+++ HiBTCToken.sol
@@ -1,117 +1,179 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HiBTCToken at 0xda0732c300482d495e704e15a9961d9ad714661e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HiBTCToken at 0x9bb1db1445b83213a56d90d331894b3f26218e4e
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.24;
 
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    if (_a == 0) {
+      return 0;
+    }
+    uint256 c = _a * _b;
+    require(c / _a == _b);
     return c;
   }
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a / b;
+
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b > 0);
+    uint256 c = _a / _b;
     return c;
   }
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
+
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b <= _a);
+    uint256 c = _a - _b;
+    return c;
   }
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
+
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    uint256 c = _a + _b;
+    require(c >= _a);
     return c;
   }
 }
 
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public constant returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+contract ERC20 {
+  function totalSupply() public view returns (uint256);
+
+  function balanceOf(address _who) public view returns (uint256);
+
+  function allowance(address _owner, address _spender) public view returns (uint256);
+
+  function transfer(address _to, uint256 _value) public returns (bool);
+
+  function approve(address _spender, uint256 _value) public returns (bool);
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
+
   event Transfer(address indexed from, address indexed to, uint256 value);
-}
 
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-  mapping(address => uint256) balances;
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value > 0 && _value <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-  function balanceOf(address _owner) public constant returns (uint256 balance) {
-    return balances[_owner];
-  }
-}
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract StandardToken is ERC20, BasicToken {
-  mapping (address => mapping (address => uint256)) internal allowed;
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+contract StandardToken is ERC20 {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  mapping(address => mapping (address => uint256)) internal allowed;
+
+  uint256 totalSupply_;
+
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+
+  function balanceOf(address _owner) public view returns (uint256) {
+    return balances[_owner];
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances[msg.sender]);
     require(_to != address(0));
-    require(_value > 0 && _value <= balances[_from]);
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
+    require(_to != address(0));
+
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    Transfer(_from, _to, _value);
+    emit Transfer(_from, _to, _value);
     return true;
   }
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
+
+  function increaseApproval(address _spender, uint256 _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
+    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
     return true;
   }
-  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
+
+  function decreaseApproval(address _spender, uint256 _subtractedValue) public returns (bool) {
+    uint256 oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue >= oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
   }
 }
 
 contract Ownable {
   address public owner;
+
+  event OwnershipRenounced(address indexed previousOwner);
+
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-  function Ownable() {
+
+  constructor() public {
     owner = msg.sender;
   }
+
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
+
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
   }
 }
 
 contract Pausable is Ownable {
   event Pause();
+  
   event Unpause();
+
   bool public paused = false;
+
   modifier whenNotPaused() {
     require(!paused);
     _;
   }
+
   modifier whenPaused() {
     require(paused);
     _;
   }
-  function pause() onlyOwner whenNotPaused public {
+
+  function pause() public onlyOwner whenNotPaused {
     paused = true;
-    Pause();
+    emit Pause();
   }
-  function unpause() onlyOwner whenPaused public {
+
+  function unpause() public onlyOwner whenPaused {
     paused = false;
-    Unpause();
+    emit Unpause();
   }
 }
 
@@ -119,26 +181,33 @@ contract PausableToken is StandardToken, Pausable {
   function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
     return super.transfer(_to, _value);
   }
+
   function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
     return super.transferFrom(_from, _to, _value);
   }
+
   function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
     return super.approve(_spender, _value);
+  }
+
+  function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool success) {
+    return super.increaseApproval(_spender, _addedValue);
+  }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool success) {
+    return super.decreaseApproval(_spender, _subtractedValue);
   }
 }
 
 contract HiBTCToken is PausableToken {
-    string public name = "HiBTCToken";
-    string public symbol = "HIBT";
-    string public version = '1.0.0';
-    uint8 public decimals = 18;
+  string public constant name = "HiBTCToken";
+  string public constant symbol = "HIBT";
+  uint8 public constant decimals = 18;
+  uint256 public constant INITIAL_SUPPLY = 10000000000 * (10 ** uint256(decimals));
 
-    function HiBTCToken() {
-      totalSupply = 10000000000 * (10**(uint256(decimals)));
-      balances[msg.sender] = totalSupply;
-    }
-
-    function () {
-        revert();
-    }
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+  }
 }
