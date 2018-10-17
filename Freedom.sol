7c782a770a218825b67ec714958bca39c9895807
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 05:47:03 +0700

    add contract Freedom

--- Freedom.sol
+++ Freedom.sol
@@ -1,168 +1,159 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Freedom at 0xca232cd0170f7bc79a45eeff047192e6fc3adcef
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Freedom at 0x68fcc99a8cf6dee27dcbbf5ce8ab15f24c706da4
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.24;
 
-contract ERC20 {
-  uint public totalSupply;
-  function balanceOf(address who) constant public returns (uint);
-  function allowance(address owner, address spender) constant public returns (uint);
-  function transfer(address to, uint value) public returns (bool ok);
-  function transferFrom(address from, address to, uint value) public returns (bool ok);
-  function approve(address spender, uint value) public returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
+contract BaseContract {
+    bool public TokensAreFrozen = true;
+    address public owner;
 
-contract Ownable {
-  address public owner;
-
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    if (msg.sender == owner)
-      _;
-  }
-
-  function transferOwnership(address newOwner) onlyOwner public {
-    if (newOwner != address(0)) owner = newOwner;
-  }
-
-}
-
-contract TokenSpender {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
-}
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-contract SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract Freedom is ERC20, SafeMath, Ownable {
-
-  string public name;
-  string public symbol;
-  uint8 public decimals = 18;
-  string public version = 'v0.1'; 
-  uint public initialSupply;
-  uint public totalSupply;
-
-  mapping(address => uint) balances;
-  mapping (address => mapping (address => uint)) allowed;
-
-  function Freedom() public {
-    initialSupply = 150000000 * 10 ** uint256(decimals);
-    totalSupply = initialSupply;
-    balances[msg.sender] = totalSupply;
-    name = 'Freedom';   
-    symbol = 'FREE';
-  }
-
-  function burn(uint256 _value) public returns (bool){
-    balances[msg.sender] = safeSub(balances[msg.sender], _value) ;
-    totalSupply = safeSub(totalSupply, _value);
-    Transfer(msg.sender, 0x0, _value);
-    return true;
-  }
-  
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balances[_from] >= _value);
-        // Check for overflows
-        require(balances[_to] + _value > balances[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balances[_from] + balances[_to];
-        // Subtract from the sender
-        balances[_from] -= _value;
-        // Add the same to the recipient
-        balances[_to] += _value;
-        Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balances[_from] + balances[_to] == previousBalances);
+    constructor () public {
+        owner = msg.sender;
     }
 
-  function transfer(address _to, uint _value) public returns (bool) {
-    balances[msg.sender] = safeSub(balances[msg.sender], _value);
-    balances[_to] = safeAdd(balances[_to], _value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
+    modifier onlyByOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-  function transferFrom(address _from, address _to, uint _value) public returns (bool) {
-    var _allowance = allowed[_from][msg.sender];
+    function transferOwnership(address newOwner) external onlyByOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
+    }
+}
+
+contract ERC20Contract is BaseContract {
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event BurnTokens(address indexed from, uint256 value);
+    event FreezeTokensFrom(address indexed _owner);
+    event UnfreezeTokensFrom(address indexed _owner);
+}
+
+library SafeMath {
+    function Mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        return c;
+    }
+
+    function Sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    function Add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint c = a + b;
+        return c;
+    }
+
+    function Div(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+contract Freedom is ERC20Contract {
     
-    balances[_to] = safeAdd(balances[_to], _value);
-    balances[_from] = safeSub(balances[_from], _value);
-    allowed[_from][msg.sender] = safeSub(_allowance, _value);
-    Transfer(_from, _to, _value);
-    return true;
-  }
+    using SafeMath for uint256;
 
-  function balanceOf(address _owner) constant public returns (uint balance) {
-    return balances[_owner];
-  }
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowed;
+    
+    string public constant tokenName = "Freedom";
+    string public constant tokenSymbol = "FREE";
+    uint256 public totalSupply = 1000000000e8;
+    uint8 public decimals = 8;
 
-  function approve(address _spender, uint _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
+    constructor () public {
+        balanceOf[msg.sender] = totalSupply;
+        totalSupply = totalSupply;
+        decimals = decimals;
+    }
 
-  function approveAndCall(address _spender, uint256 _value, bytes _extraData) public{    
-      TokenSpender spender = TokenSpender(_spender);
-      if (approve(_spender, _value)) {
-          spender.receiveApproval(msg.sender, _value, this, _extraData);
-      }
-  }
+    modifier onlyPayloadSize(uint256 _size) {
+        require(msg.data.length >= _size + 4);
+        _;
+    }
 
-  function allowance(address _owner, address _spender) constant public returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-  
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) public returns (bool success) {
+        require(!TokensAreFrozen);
+        require(_to != 0x0);
+        require(_value > 0);
+        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        balanceOf[msg.sender] = balanceOf[msg.sender].Sub(_value);
+        balanceOf[_to] = balanceOf[_to].Add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(3 * 32) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(!TokensAreFrozen);
+        require(_to != 0x0);
+        require(_value > 0);
+        require(balanceOf[_from] >= _value && allowance >= _value);
+        balanceOf[_to]   = balanceOf[_to].Add(_value);
+        balanceOf[_from] = balanceOf[_from].Sub(_value);
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] = allowed[_from][msg.sender].Sub(_value);
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balanceOf[_owner];
+    }
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(!TokensAreFrozen);
+        require(_spender != address(0));
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    function freezeTokens(address _owner) external onlyByOwner {
+        require(TokensAreFrozen == false);
+        TokensAreFrozen = true;
+        emit FreezeTokensFrom(_owner);
+    }
+    
+    function unfreezeTokens(address _owner) external onlyByOwner {
+        require(TokensAreFrozen == true);
+        TokensAreFrozen = false;
+        emit UnfreezeTokensFrom(_owner);
+    }
+    
+    function burnTokens(address _owner, uint256 _value) external onlyByOwner {
+        require(!TokensAreFrozen);
+        require(balanceOf[_owner] >= _value);
+        balanceOf[_owner] -= _value;
+        totalSupply -= _value;
+        emit BurnTokens(_owner, _value);
+    }
+    
+    function withdraw() external onlyByOwner {
+        owner.transfer(address(this).balance);
+    }
+    
+    function() payable public {
+    }
 }
