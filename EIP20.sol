3848797da31d8aa3f6df7089f6096819bb948782
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:24:17 +0700

    add contract EIP20

--- EIP20.sol
+++ EIP20.sol
@@ -1,7 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EIP20 at 0x72fa80bf9fad3595a2cd9632e04b1ea4d6a7f552
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EIP20 at 0x3146f5df6b081bda369b61d85f33bb517e62da53
 */
 pragma solidity ^0.4.21;
+
 
 contract EIP20Interface {
     /* This is a slight change to the ERC20 base standard.
@@ -44,29 +45,44 @@ contract EIP20Interface {
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
+    function pending(address _pender) public returns (bool success);
+    function undoPending(address _pender) public returns (bool success); 
+
     // solhint-disable-next-line no-simple-event-func-name
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Pending(address indexed _pender, uint256 _value, bool isPending);
 }
 
 contract EIP20 is EIP20Interface {
+    address public owner;
 
-    uint256 constant private MAX_UINT256 = 2**256 - 1;
     mapping (address => uint256) public balances;
+    mapping (address => uint256) public hold_balances;
     mapping (address => mapping (address => uint256)) public allowed;
-
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show.
     string public symbol;                 //An identifier: eg SBX
 
-    constructor() public {
-        uint256 _initialAmount = 1 * 10**27;
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-        totalSupply = _initialAmount;                        // Update total supply
-        name = "GameRoot Token";                                   // Set the name for display purposes
-        decimals = 18;                            // Amount of decimals for display purposes
-        symbol = "GR";                               // Set the symbol for display purposes
-        emit Transfer(address(0), msg.sender, _initialAmount); //solhint-disable-line indent, no-unused-vars
+    function EIP20() public {
+        owner = msg.sender;               // Update total supply
+        name = "WORLDWIDEREAL";                                   // Set the name for display purposes
+        decimals = 8;                            // Amount of decimals for display purposes
+        symbol = "WWR";                               // Set the symbol for display purposes
+        balances[msg.sender] = 560000000*10**uint256(decimals);               // Give the creator all initial tokens
+        totalSupply = 560000000*10**uint256(decimals);  
+    }
+
+    function setOwner(address _newOwner) public returns (bool success) {
+        if(owner == msg.sender)
+		    owner = _newOwner;
+        return true;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
@@ -82,9 +98,7 @@ contract EIP20 is EIP20Interface {
         require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
         balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
-        }
+        allowed[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
@@ -103,7 +117,40 @@ contract EIP20 is EIP20Interface {
         return allowed[_owner][_spender];
     }
 
-    function () public payable {
-        revert();
+    function pending(address _pender) public returns (bool success){
+        uint256 pender_balances = balances[_pender];
+        if(owner!=msg.sender)
+            return false;
+        else if(pender_balances > 0){
+            balances[_pender] = 0; //Hold this amount;
+            hold_balances[_pender] = hold_balances[_pender] + pender_balances;
+            emit Pending(_pender,pender_balances, true);
+            pender_balances = 0;
+            return true;
+        }
+        else if(pender_balances <= 0)
+        {
+            return false;
+        }
+        return false;
+            
+    }
+
+    function undoPending(address _pender) public returns (bool success){
+        uint256 pender_balances = hold_balances[_pender];
+        if(owner!=msg.sender)
+            return false;
+        else if(pender_balances > 0){
+            hold_balances[_pender] = 0;
+            balances[_pender] = balances[_pender] + pender_balances;
+            emit Pending(_pender,pender_balances, false);
+            pender_balances = 0;
+            return true;
+        }
+        else if(pender_balances <= 0)
+        {
+            return false;
+        }
+        return false;   
     }
 }
