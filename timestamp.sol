dde907bd82a30f9fc83e111942e72a8ee8b66dae
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:08:31 +0700

    add contract timestamp

---
+++ timestamp.sol
@@ -0,0 +1,11 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract timestamp at 0x2ce8c401a222ae48494119c20d312ca8e13ffb8c
+*/
+pragma solidity ^0.4.7;
+
+contract timestamp {
+    string public hash = "F15EBC07782C7CBAC017803B739B6494A9848678D00244C8A702102BDA62BCDB";
+    constructor () public {
+        
+    }
+}
