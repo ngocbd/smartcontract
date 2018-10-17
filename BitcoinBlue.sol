a5799a9392142a731077733a4fd97b143f222383
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:42:38 +0700

    add contract BitcoinBlue

--- BitcoinBlue.sol
+++ BitcoinBlue.sol
@@ -1,20 +1,39 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BITCOINBLUE at 0xece4d2652f2b52241335649442c4f93911d27f3e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinBlue at 0x04123a57e3d3708dbdb9a5db801ea7a11bae0292
 */
 pragma solidity ^0.4.4;
 
 contract Token {
 
+    /// @return total amount of tokens
     function totalSupply() constant returns (uint256 supply) {}
 
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance) {}
 
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) returns (bool success) {}
 
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) returns (bool success) {}
 
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -25,7 +44,10 @@ contract Token {
 contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
-
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
@@ -35,7 +57,8 @@ contract StandardToken is Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
@@ -64,48 +87,51 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract BITCOINBLUE is StandardToken { 
+contract BitcoinBlue is StandardToken { 
 
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;                
-    string public version = 'C1.0'; 
-    uint256 public unitsOneEthCanBuy;     
-    uint256 public totalEthInWei;         
-    address public fundsWallet;  
+    /* Public variables of the token */
 
-    // This is a constructor function 
-    // which means the following function name has to match the contract name declared above
-    function BITCOINBLUE() {
-        balances[msg.sender] = 21000000e8;  
-        totalSupply = 21000000e8;  
-        name = "BITCOINBLUE";                                 
-        decimals = 8;                                              
-        symbol = "BTCBLUE";                                           
-        unitsOneEthCanBuy = 20000;                                
-        fundsWallet = msg.sender;                             
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'H1.0'; 
+    uint256 public unitsOneEthCanBuy;
+    uint256 public totalEthInWei; 
+    address public fundsWallet;
+
+    //constructor function 
+    function BitcoinBlue () {
+        balances[msg.sender] = 21000000000000000000000000;
+        totalSupply = 21000000000000000000000000;
+        name = "Bitcoin Blue";
+        decimals = 18;
+        symbol = "BTCBL";
+        unitsOneEthCanBuy = 22500;
+        fundsWallet = msg.sender;
     }
 
     function() payable{
         totalEthInWei = totalEthInWei + msg.value;
         uint256 amount = msg.value * unitsOneEthCanBuy;
-        if (balances[fundsWallet] < amount) {
-            return;
-        }
+        require(balances[fundsWallet] >= amount);
 
         balances[fundsWallet] = balances[fundsWallet] - amount;
         balances[msg.sender] = balances[msg.sender] + amount;
 
-        Transfer(fundsWallet, msg.sender, amount); 
+        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
 
+        //Transfer ether to fundsWallet
         fundsWallet.transfer(msg.value);                               
     }
 
- 
+    /* Approves and then calls the receiving contract */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
         if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
     }
