9b36bfab520c8ea0e1c37b35bec650840144abd9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:15:42 +0700

    add contract Lover

--- Lover.sol
+++ Lover.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Lover at 0x3561dd975b0daebe8d3991e0a59695278d215a23
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Lover at 0x3656b7e011d3f567089071de561a52a956273f79
 */
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 library SafeMath {
     function mul(uint a, uint b) internal pure returns (uint) {
@@ -58,31 +58,27 @@ contract ERC20 {
     function allowance(address _owner, address _spender) public view returns (uint remaining);
     event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
-	event FreezeAccount(address indexed _target, bool _frozen, uint _timestamp);
-	event CertifyAccount(address indexed _target, bool _certified, uint _timestamp);
 }
 
 contract ERC20Token is ERC20 {
     using SafeMath for uint;
     uint public totalToken;
 	bool public frozen;
-    mapping(address => uint) balances;
+    mapping (address => uint) balances;
     mapping (address => mapping (address => uint)) allowances;
 	mapping (address => bool) public frozenAccounts;
-	mapping (address => bool) public certifiedAccounts;
-
+	
     function _transfer(address _from, address _to, uint _value) internal returns (bool success) {
 		require(_from != 0x0 && _to != 0x0);
         require(balances[_from] >= _value && _value > 0);
-        require(balances[_to] + _value > balances[_to]);
 		require(!frozen);
-		require(!frozenAccounts[_from]);                     
-        require(!frozenAccounts[_to]);                       
+		require(!frozenAccounts[_from]);
+        require(!frozenAccounts[_to]);
         uint previousBalances = balances[_from] + balances[_to];
         balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        emit Transfer(_from, _to, _value);
 		assert(balances[_from] + balances[_to] == previousBalances);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
@@ -120,9 +116,10 @@ contract Lover is ERC20Token, Owned {
 	string public name = "Lover";
     string public symbol = "LOV";
     uint public constant decimals = 18;
-	string public note = "(C) loverchain.com all rights reserved";
+	string public note = "(C) Loverchain.com all rights reserved.";
     uint public burnedToken;
 	uint public fee;
+	mapping (address => bool) public certifiedAccounts;
 	mapping (address => string) public keys;
 	mapping (address => string) public signatures;
 	mapping (address => string) public identities;
@@ -141,19 +138,20 @@ contract Lover is ERC20Token, Owned {
 	event Fund(address indexed _user, string _fund, address indexed _certifier, uint _timestamp);
 	event Save(address indexed _user, uint _score, uint _level, uint _star, address indexed _certifier, uint _nonce, uint _timestamp);
 	event Burn(address indexed _from, uint _burntAmount, uint _timestamp);
+    event FreezeAccount(address indexed _target, bool _frozen, uint _timestamp);
+	event CertifyAccount(address indexed _target, bool _certified, uint _timestamp);
 
-    function Lover() public {
+    constructor() public {
 		totalToken = 1000000000000000000000000000;
 		balances[msg.sender] = totalToken;
 		owner = msg.sender;
 	    frozen = false;
-		fee = 100000000000000000000;
+		fee = 0;
 		certifiedAccounts[msg.sender] = true; 
     }
 
     function burn(uint _burntAmount) public returns (bool success) {
     	require(balances[msg.sender] >= _burntAmount && _burntAmount > 0);
-		require(totalToken - _burntAmount >= 100000000000000000000000000);
     	balances[msg.sender] = balances[msg.sender].sub(_burntAmount);
     	totalToken = totalToken.sub(_burntAmount);
     	burnedToken = burnedToken.add(_burntAmount);
@@ -163,66 +161,69 @@ contract Lover is ERC20Token, Owned {
 	}
 
     function setKey(string _key) public {
-        require(bytes(_key).length >= 32);
+        require(bytes(_key).length > 1);
         keys[msg.sender] = _key;
 		emit Key(msg.sender, _key, now);
     }
 
     function sign(string _data) public {
-        require(bytes(_data).length >= 32);
+        require(bytes(_data).length > 1);
         signatures[msg.sender] = _data;
 		emit Sign(msg.sender, _data, now);
     }
 
 	function register(address _user, string _identity) public {
-		require(bytes(_identity).length > 0);
+		require(bytes(_identity).length > 1);
 		require(certifiedAccounts[msg.sender]);
 		identities[_user] = _identity;
 		emit Register(_user, _identity, msg.sender, now);
     }
 
-    function _save(address _user, uint _score, uint _level, uint _star, string _profile, string _property, address _certifier, uint _nonce, uint _timestamp) internal {
-		require(_nonce == nonces[_user] + 1);  
-	    if(bytes(_profile).length > 16){
+    function _save(address _user, uint _score, uint _level, uint _star, string _profile, string _property, address _certifier, uint _nonce, uint _timestamp) internal returns (bool success){
+		require(_nonce > nonces[_user]);
+		require(!frozen);
+		require(!frozenAccounts[_user]); 
+	    if(bytes(_profile).length > 1){
 			profiles[_user] = _profile;
 		}
-	    if(bytes(_property).length > 16){
+	    if(bytes(_property).length > 1){
 		    properties[_user] = _property;
 		}
-		if(_level > levels[_user]){
-			levels[_user] = _level;
-		}
+		levels[_user] = _level;
 		scores[_user] = _score;
         stars[_user] = _star;
 		nonces[_user] = _nonce;
 		emit Save(_user, _score, _level, _star, _certifier, _nonce, _timestamp);
+		return true;
     }
 
-    function save(address _user, uint _score, uint _level, uint _star, string _profile, string _property, uint _nonce) public {
+    function save(address _user, uint _score, uint _level, uint _star, string _profile, string _property, uint _nonce) public returns (bool success){
         require(certifiedAccounts[msg.sender]);  
-		_save(_user, _score, _level, _star, _profile, _property, msg.sender, _nonce, now);
+		return _save(_user, _score, _level, _star, _profile, _property, msg.sender, _nonce, now);
     }
 
-	function _assign(address _from, address _to, address _certifier) internal {
+	function _assign(address _from, address _to, address _certifier) internal returns (bool success){
 		require(_from != 0x0 && _to != 0x0);
-		uint _timestamp = now;
-		uint _nonce = nonces[_from];
-		_save(_to, scores[_from], levels[_from], stars[_from], profiles[_from], properties[_from], _certifier, _nonce, _timestamp);
+		require(!frozen);
+		require(!frozenAccounts[_from]);
+        require(!frozenAccounts[_to]); 
+		_save(_to, scores[_from], levels[_from], stars[_from], profiles[_from], properties[_from], _certifier, nonces[_from], now);
         profiles[_from] = "";
         properties[_from] = "";
 		scores[_from] = 0; 
 		levels[_from] = 0;
 		stars[_from] = 0;
+		return true;
     }
 
-    function assign(address _to) public {
-		_transfer(msg.sender, owner, fee);
-		_assign(msg.sender, _to, owner);
+    function assign(address _to) public returns (bool success){
+        require(nonces[_to] == 0);
+		return _assign(msg.sender, _to, msg.sender);
 	}
 
-	function assignFrom(address _from, address _to) public {
+	function assignFrom(address _from, address _to) public returns (bool success){
         require(certifiedAccounts[msg.sender]);
-	    _assign(_from, _to, msg.sender);
+	    return _assign(_from, _to, msg.sender);
     }
 
     function setRule(address _user, string _rule) public {
@@ -250,7 +251,11 @@ contract Lover is ERC20Token, Owned {
         certifiedAccounts[_user] = _certified;
         emit CertifyAccount(_user, _certified, now);
     }
-    
+
+	function transferToken(address _tokenAddress, address _recipient, uint _value) public onlyOwner returns (bool success) {
+        return ERC20(_tokenAddress).transfer(_recipient, _value);
+    }
+
 	function setName(string _tokenName, string _tokenSymbol) public onlyOwner {
         name = _tokenName;
         symbol = _tokenSymbol; 
@@ -268,13 +273,13 @@ contract Lover is ERC20Token, Owned {
 	    if(_range == 0) {
 	       return 0;  
 	    }
-        uint ran = uint(keccak256(block.difficulty, now));
+        uint ran = uint(keccak256(abi.encodePacked(block.difficulty, now)));
         return ran % _range;
     }
     
-    function shuffle(uint[] _tiles) public view returns(uint[]) {
-        uint len = _tiles.length;
-        uint[] memory t = _tiles; 
+    function shuffle(uint[] _values) public view returns(uint[]) {
+        uint len = _values.length;
+        uint[] memory t = _values; 
         uint temp = 0;
         uint ran = 0;
         for (uint i = 0; i < len; i++) {
