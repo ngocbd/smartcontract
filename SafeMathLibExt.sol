2bd88c8527a10b0d44cf7b9378e8c74d2548e59d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:54:58 +0700

    add contract SafeMathLibExt

--- SafeMathLibExt.sol
+++ SafeMathLibExt.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0x3ac416C05E549314C4E7766678ddA5BB49fB287d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMathLibExt at 0xe6Fa157FF472dacF9d1cD8a0D04a4bfEf1ad2B27
 */
 // Created using Token Wizard https://github.com/poanetwork/token-wizard by POA Network 
 /**
