bf2d9205712374ae077cc1168ca78a4344e16839
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 12:37:52 +0700

    add contract JACK

--- Jack.sol
+++ Jack.sol
@@ -1,50 +1,179 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jack at 0xC9CFAce6c68D56E5775D3c0183E88F9b04ac1C03
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract JACK at 0x98caa64da8c7b723cadcdd00fe3f7338d36f7398
 */
-contract Jack {
-    /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public initialSupply;
-    uint256 public totalSupply;
+pragma solidity ^0.4.10;
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
 
-  
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function Jack() {
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-         initialSupply = 100000000;
-         name ="Jack";
-        decimals = 2;
-         symbol = "Jack";
-        
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-                                   
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+interface ERC20 {
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface ERC223 {
+    function transfer(address to, uint value, bytes data) public;
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
+
+contract ERC223ReceivingContract { 
+    function tokenFallback(address _from, uint _value, bytes _data) public;
+}
+
+contract JACK is ERC20, ERC223 {
+  using SafeMath for uint;
+     
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint256 internal _totalSupply;
+
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    function JACK(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
+        _symbol = symbol;
+        _name = name;
+        _decimals = decimals;
+        _totalSupply = totalSupply;
+        balances[msg.sender] = totalSupply;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-      
+    function name()
+        public
+        view
+        returns (string) {
+        return _name;
     }
 
+    function symbol()
+        public
+        view
+        returns (string) {
+        return _symbol;
+    }
+
+    function decimals()
+        public
+        view
+        returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply()
+        public
+        view
+        returns (uint256) {
+        return _totalSupply;
+    }
+
+   function transfer(address _to, uint256 _value) public returns (bool) {
+     require(_to != address(0));
+     require(_value <= balances[msg.sender]);
+     balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+     balances[_to] = SafeMath.add(balances[_to], _value);
+     Transfer(msg.sender, _to, _value);
+     return true;
+   }
+
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+   }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+     require(_value <= balances[_from]);
+     require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = SafeMath.sub(balances[_from], _value);
+     balances[_to] = SafeMath.add(balances[_to], _value);
+     allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+    Transfer(_from, _to, _value);
+     return true;
+   }
+
+   function approve(address _spender, uint256 _value) public returns (bool) {
+     allowed[msg.sender][_spender] = _value;
+     Approval(msg.sender, _spender, _value);
+     return true;
+   }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+     return allowed[_owner][_spender];
+   }
+
+   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+     allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
+     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+     return true;
+   }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+     uint oldValue = allowed[msg.sender][_spender];
+     if (_subtractedValue > oldValue) {
+       allowed[msg.sender][_spender] = 0;
+     } else {
+       allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
+    }
+     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+     return true;
+   }
    
-
+  function transfer(address _to, uint _value, bytes _data) public {
+    require(_value > 0 );
+    if(isContract(_to)) {
+        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+        receiver.tokenFallback(msg.sender, _value, _data);
+    }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value, _data);
+    }
     
-
-   
-
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function () {
-        throw;     // Prevents accidental sending of ether
+  function isContract(address _addr) private returns (bool is_contract) {
+      uint length;
+      assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+      }
+      return (length>0);
     }
+
+
 }
