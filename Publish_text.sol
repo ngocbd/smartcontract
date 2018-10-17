909f31c5d066b934f2932d32129be2e4ed609fb7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:21:17 +0700

    add contract Publish_text

---
+++ Publish_text.sol
@@ -0,0 +1,14 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Publish_text at 0xfa0cb1b68f5de256571f368c282b1d29567ba89b
+*/
+pragma solidity ^0.4.7;
+
+contract Publish_text {
+ 
+    string public my_message;
+    
+    constructor(string message) public {
+        my_message = message;
+    }
+    
+}
