3aaba6d9489193f7494cfec686419371b63cd6b1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 12:15:56 +0700

    add contract RabbitCoin

--- RabbitCoin.sol
+++ RabbitCoin.sol
@@ -1,254 +1,156 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RabbitCoin at 0x7b34be3ad5ed1b760bedb9657f9647eca14687a9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RabbitCoin at 0x784fb897b33bad8de5b848d1b8cecbb158d8226e
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
 
-/*
- * ERC223 interface
- * see https://github.com/ethereum/EIPs/issues/20
- * see https://github.com/ethereum/EIPs/issues/223
- */
-contract ERC223 {
-    function totalSupply() constant public returns (uint256 outTotalSupply);
-    function balanceOf( address _owner) constant public returns (uint256 balance);
-    function transfer( address _to, uint256 _value) public returns (bool success);
-    function transfer( address _to, uint256 _value, bytes _data) public returns (bool success);
-    function transferFrom( address _from, address _to, uint256 _value) public returns (bool success);
-    function approve( address _spender, uint256 _value) public returns (bool success);
-    function allowance( address _owner, address _spender) constant public returns (uint256 remaining);
-    event Transfer( address indexed _from, address indexed _to, uint _value, bytes _data);
-    event Approval( address indexed _owner, address indexed _spender, uint256 _value);
-}
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
+contract RabbitCoin {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-contract ERC223Receiver { 
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
     /**
-     * @dev Standard ERC223 function that will handle incoming token transfers.
+     * Constructor function
      *
-     * @param _from  Token sender address.
-     * @param _value Amount of tokens.
-     * @param _data  Transaction metadata.
+     * Initializes contract with initial supply tokens to the creator of the contract
      */
-    function tokenFallback(address _from, uint _value, bytes _data) public;
-}
-
-/**
- * Math operations with safety checks
- */
-contract SafeMath {
-    function safeMul(uint a, uint b) internal pure returns (uint) {
-        uint c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-    function safeDiv(uint a, uint b) internal pure returns (uint) {
-        assert(b > 0);
-        uint c = a / b;
-        assert(a == b * c + a % b);
-        return c;
-    }
-
-    function safeSub(uint a, uint b) internal pure returns (uint) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function safeAdd(uint a, uint b) internal pure returns (uint) {
-        uint c = a + b;
-        assert(c>=a && c>=b);
-        return c;
-    }
-
-    function max64(uint64 a, uint64 b) internal pure returns (uint64) {
-        return a >= b ? a : b;
-    }
-
-    function min64(uint64 a, uint64 b) internal pure returns (uint64) {
-        return a < b ? a : b;
-    }
-
-    function max256(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a >= b ? a : b;
-    }
-
-    function min256(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
- 
-}
-
-
-
-/**
- * Standard ERC223
- */
-contract StandardToken is ERC223, SafeMath {
-        
-    uint256 public supplyNum;
-    
-    uint256 public decimals;
-
-    /* Actual mapBalances of token holders */
-    mapping(address => uint) mapBalances;
-
-    /* approve() allowances */
-    mapping (address => mapping (address => uint)) mapApproved;
-
-    /* Interface declaration */
-    function isToken() public pure returns (bool weAre) {
-        return true;
-    }
-
-
-    function totalSupply() constant public returns (uint256 outTotalSupply) {
-        return supplyNum;
-    }
-
-    
-    function transfer(address _to, uint _value, bytes _data) public returns (bool) {
-        // Standard function transfer similar to ERC20 transfer with no _data .
-        // Added due to backwards compatibility reasons .
-        uint codeLength;
-
-        assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_to)
-        }
-
-        mapBalances[msg.sender] = safeSub(mapBalances[msg.sender], _value);
-        mapBalances[_to] = safeAdd(mapBalances[_to], _value);
-        
-        if (codeLength > 0) {
-            ERC223Receiver receiver = ERC223Receiver(_to);
-            receiver.tokenFallback(msg.sender, _value, _data);
-        }
-        emit Transfer(msg.sender, _to, _value, _data);
-        return true;
-    }
-    
-    
-    function transfer(address _to, uint _value) public returns (bool) {
-        uint codeLength;
-        bytes memory empty;
-
-        assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_to)
-        }
-
-        mapBalances[msg.sender] = safeSub(mapBalances[msg.sender], _value);
-        mapBalances[_to] = safeAdd(mapBalances[_to], _value);
-        
-        if (codeLength > 0) {
-            ERC223Receiver receiver = ERC223Receiver(_to);
-            receiver.tokenFallback(msg.sender, _value, empty);
-        }
-        emit Transfer(msg.sender, _to, _value, empty);
-        return true;
-    }
-    
-    
-
-    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
-        mapApproved[_from][msg.sender] = safeSub(mapApproved[_from][msg.sender], _value);
-        mapBalances[_from] = safeSub(mapBalances[_from], _value);
-        mapBalances[_to] = safeAdd(mapBalances[_to], _value);
-        
-        bytes memory empty;
-        emit Transfer(_from, _to, _value, empty);
-                
-        return true;
-    }
-
-    function balanceOf(address _owner) view public returns (uint balance)    {
-        return mapBalances[_owner];
-    }
-
-    function approve(address _spender, uint _value) public returns (bool success)    {
-
-        // To change the approve amount you first have to reduce the addresses`
-        //    allowance to zero by calling `approve(_spender, 0)` if it is not
-        //    already 0 to mitigate the race condition described here:
-        //    https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        require (_value != 0); 
-        require (mapApproved[msg.sender][_spender] == 0);
-
-        mapApproved[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) view public returns (uint remaining)    {
-        return mapApproved[_owner][_spender];
-    }
-
-}
-
-
-
-
-/**
- * Centrally issued Ethereum token.
- *
- * We mix in burnable and upgradeable traits.
- *
- * Token supply is created in the token contract creation and allocated to owner.
- * The owner can then transfer from its supply to crowdsale participants.
- * The owner, or anybody, can burn any excessive tokens they are holding.
- *
- */
-contract RabbitCoin is StandardToken {
-
-    string public name = "BetOnMatch";
-    string public symbol = "BOM";
-    
-    
-    address public coinMaster;
-    
-    
-    /** Name and symbol were updated. */
-    event UpdatedInformation(string newName, string newSymbol);
-
-    function RabbitCoin() public {
-        supplyNum = 10000000000 * (10 ** 18);
-        decimals = 18;
-        coinMaster = msg.sender;
-
-        // Allocate initial balance to the owner
-        mapBalances[coinMaster] = supplyNum;
+    function RabbitCoin(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
     /**
-     * Owner can update token information here.
-     *
-     * It is often useful to conceal the actual token association, until
-     * the token operations, like central issuance or reissuance have been completed.
-     * In this case the initial token can be supplied with empty name and symbol information.
-     *
-     * This function allows the token owner to rename the token after the operations
-     * have been completed and then point the audience to use the token contract.
+     * Internal transfer, only can be called by this contract
      */
-    function setTokenInformation(string _name, string _symbol) public {
-        require(msg.sender == coinMaster) ;
-
-        require(bytes(name).length > 0 && bytes(symbol).length > 0);
-
-        name = _name;
-        symbol = _symbol;
-        emit UpdatedInformation(name, symbol);
-    }
-    
-    
-    
-    /// transfer dead tokens to contract master
-    function withdrawTokens() external {
-        uint256 fundNow = balanceOf(this);
-        transfer(coinMaster, fundNow);//token
-        
-        uint256 balance = address(this).balance;
-        coinMaster.transfer(balance);//eth
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
 }
