05854ca094fb041078352fe8ca269d256c996ff3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:09:43 +0700

    add contract CustomToken

--- CustomToken.sol
+++ CustomToken.sol
@@ -1,7 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0x189b9c623570b30e178289740b9cdb5589094424
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0xD40441e477122AE3209742aA2bC6EDeFb62F920C
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.19;
 
 contract BaseToken {
     string public name;
@@ -16,18 +16,19 @@ contract BaseToken {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
     function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != address(0));
+        require(_to != 0x0);
         require(balanceOf[_from] >= _value);
         require(balanceOf[_to] + _value > balanceOf[_to]);
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-        emit Transfer(_from, _to, _value);
+        Transfer(_from, _to, _value);
     }
 
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
+        return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
@@ -39,76 +40,18 @@ contract BaseToken {
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
+        Approval(msg.sender, _spender, _value);
         return true;
     }
 }
 
-contract BurnToken is BaseToken {
-    event Burn(address indexed from, uint256 value);
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
-        allowance[_from][msg.sender] -= _value;
-        totalSupply -= _value;
-        emit Burn(_from, _value);
-        return true;
-    }
-}
-
-contract ICOToken is BaseToken {
-    // 1 ether = icoRatio token
-    uint256 public icoRatio;
-    uint256 public icoEndtime;
-    address public icoSender;
-    address public icoHolder;
-
-    event ICO(address indexed from, uint256 indexed value, uint256 tokenValue);
-    event Withdraw(address indexed from, address indexed holder, uint256 value);
-
-    modifier onlyBefore() {
-        if (now > icoEndtime) {
-            revert();
-        }
-        _;
-    }
-
-    function() public payable onlyBefore {
-        uint256 tokenValue = (msg.value * icoRatio * 10 ** uint256(decimals)) / (1 ether / 1 wei);
-        if (tokenValue == 0 || balanceOf[icoSender] < tokenValue) {
-            revert();
-        }
-        _transfer(icoSender, msg.sender, tokenValue);
-        emit ICO(msg.sender, msg.value, tokenValue);
-    }
-
-    function withdraw() public{
-        uint256 balance = address(this).balance;
-        icoHolder.transfer(balance);
-        emit Withdraw(msg.sender, icoHolder, balance);
-    }
-}
-
-contract CustomToken is BaseToken, BurnToken, ICOToken {
-    constructor(address icoAddress) public {
-        totalSupply = 210000000000000000;
-        balanceOf[msg.sender] = totalSupply;
-        name = 'DAP';
-        symbol = 'DAP';
-        decimals = 8;
-        icoRatio = 10000;
-        icoEndtime = 1590940800;
-        icoSender = icoAddress;
-        icoHolder = icoAddress;
+contract CustomToken is BaseToken {
+    function CustomToken() public {
+        totalSupply = 800000000000000000000000000;
+        name = 'EthereumLegend';
+        symbol = 'ETLG';
+        decimals = 18;
+        balanceOf[0xbcfe91ed0eaa53975b485384ac3b2f6825be2cf4] = totalSupply;
+        Transfer(address(0), 0xbcfe91ed0eaa53975b485384ac3b2f6825be2cf4, totalSupply);
     }
 }
