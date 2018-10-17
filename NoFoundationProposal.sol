77a83c9fe4c841fa2f1da98df5178a4ce32fa062
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:01:26 +0700

    add contract NoFoundationProposal

---
+++ NoFoundationProposal.sol
@@ -0,0 +1,4 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract NoFoundationProposal at 0xa9eb03f3a499e274170c378f958e4a0cbea2c7ef
+*/
+contract NoFoundationProposal {}
