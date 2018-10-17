bacf85ddd9bf6fb8d68110e61a9c97be986031b4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:44:42 +0700

    add contract BoltToken

--- BoltToken.sol
+++ BoltToken.sol
@@ -1,140 +1,163 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BoltToken at 0x7811bac10c1633d7bbf8488b0fbfbd4a6e24676a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BoltToken at 0x64d30c2E9715f0adD2815F862eAF22D9DBc1bd28
 */
 pragma solidity ^0.4.4;
 
-contract Token {
+contract ERC20 {
+  uint public totalSupply;
+  function balanceOf(address who) public constant returns (uint);
+  function allowance(address owner, address spender) public constant returns (uint);
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  function transfer(address to, uint value) public returns (bool ok);
+  function transferFrom(address from, address to, uint value) public returns (bool ok);
+  function approve(address spender, uint value) public returns (bool ok);
+  event Transfer(address indexed from, address indexed to, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-/*
-This implements ONLY the standard functions and NOTHING else.
-For a token like you would want to deploy in something like Mist, see HumanStandardToken.sol.
+contract BoltToken is ERC20{
+    
+    address owner = msg.sender;
+    
+    bool public canPurchase = false;
+    
+    mapping (address => uint) balances;
+    mapping (address => uint) roundContributions;
+    address[] roundContributionsIndexes;
+    mapping (address => mapping (address => uint)) allowed;
 
-If you deploy this, you won't have anything useful.
+    uint public currentSupply = 0;
+    uint public totalSupply = 32032000000000000000000000;
+    
+    uint public round = 0;
+    uint public roundFunds = 0;
+    uint public roundReward = 200200000000000000000000;
+    
+    string public name = "BOLT token";
+    string public symbol = "BOLT";
+    uint8 public decimals = 18;
+    
+    bool public isToken = true;
+    
+    string public tokenSaleAgreement = "https://bolt-project.net/tsa.pdf";
+    
+    uint contributionsDistribStep = 0;
+    
+    event Contribution(address indexed from, uint value);
+    event RoundEnd(uint roundNumber);
+    
+    function balanceOf(address _owner) public constant returns (uint balance) {
+        return balances[_owner];
+    }
+    
+    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    function transfer(address _to, uint _value) public returns (bool success) {
+        // mitigates the ERC20 short address attack
+        if(msg.data.length < (2 * 32) + 4) { return false; }
 
-Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
-.*/
-
-
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+            emit Transfer(msg.sender, _to, _value);
             return true;
-        } else { return false; }
+        }
+        return false;
     }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+    
+    function transferFrom(address _from, address _to, uint _value) public  returns (bool success){
+        // mitigates the ERC20 short address attack
+        if(msg.data.length < (3 * 32) + 4) { return false; }
+        
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
             allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
+            emit Transfer(_from, _to, _value);
             return true;
-        } else { return false; }
+        }
+        return false;
     }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    
+    function approve(address _spender, uint _value) public  returns (bool success){
+        // mitigates the ERC20 spend/approval race condition
+        if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
+        
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        
+        emit Approval(msg.sender, _spender, _value);
+        
         return true;
     }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+    
+    function enablePurchase() public {
+        if(msg.sender != owner && currentSupply>=totalSupply){ return; }
+        
+        canPurchase = true;
     }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
-
-contract BoltToken is StandardToken {
-
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
+    function disablePurchase() public {
+        if(msg.sender != owner){ return; }
+        
+        canPurchase = false;
+    }
+    
+    function changeTsaLink(string _link) public {
+        if(msg.sender != owner){ return; }
+        
+        tokenSaleAgreement = _link;
+    }
+    
+    function changeReward(uint _roundReward) public {
+        if(msg.sender != owner){ return; }
+        
+        roundReward = _roundReward;
+    }
+    
+    function nextRound() public {
+        if(msg.sender != owner){ return; }
+        uint i = contributionsDistribStep;
+        while(i < contributionsDistribStep+10 && i<roundContributionsIndexes.length){
+            address contributor = roundContributionsIndexes[i];
+            balances[contributor] += roundReward*roundContributions[contributor]/roundFunds;
+            roundContributions[contributor] = 0;
+            i++;
+        }
+        
+        contributionsDistribStep = i;
+        
+        if(i==roundContributionsIndexes.length){
+            delete roundContributionsIndexes;
+            
+            emit RoundEnd(round);
+            
+            roundFunds = 0;
+            currentSupply += roundReward;
+            round += 1;
+            contributionsDistribStep = 0;
+        }
     }
 
-    /* Public variables of the token */
-
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-    function BoltToken(
-         ) {
-        balances[msg.sender] = 6800000000000;               // Give the creator all initial tokens
-        totalSupply = 6800000000000;                        // Update total supply
-        name = "BOLTCOIN";                                   // Set the name for display purposes
-        decimals = 2;                            // Amount of decimals for display purposes
-        symbol = "BOLT";                               // Set the symbol for display purposes
-    }
-
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
+    function contribute(bool _acceptConditions) payable public {
+        
+        if(msg.value == 0){ return; }
+        
+        if(!canPurchase || !_acceptConditions || msg.value < 10 finney){
+            msg.sender.transfer(msg.value);
+            return;
+        }
+        
+        owner.transfer(msg.value);
+        
+        if(roundContributions[msg.sender] == 0){
+           roundContributionsIndexes.push(msg.sender); 
+        }
+        
+        roundContributions[msg.sender] += msg.value;
+        roundFunds += msg.value;
+        
+        emit Contribution(msg.sender, msg.value);
     }
 }
