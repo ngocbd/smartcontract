fe902d575457594a102faf79528eef20e211c156
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 03:41:47 +0700

    add contract Insurance

--- Insurance.sol
+++ Insurance.sol
@@ -1,243 +1,317 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Insurance at 0x1D6C596bB5022A4338B6B8651f3ACd4D836EF574
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Insurance at 0x71b7e3a3ff05224c1e38031b3d887657db565a20
 */
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.0;
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * See https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-  address public owner;
+    address public owner;
 
-
-  event OwnershipRenounced(address indexed previousOwner);
-  event OwnershipTransferred(
-    address indexed previousOwner,
-    address indexed newOwner
-  );
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   * @notice Renouncing to ownership will leave the contract without an owner.
-   * It will not be possible to call the functions with the `onlyOwner`
-   * modifier anymore.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipRenounced(owner);
-    owner = address(0);
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address _newOwner) public onlyOwner {
-    _transferOwnership(_newOwner);
-  }
-
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function _transferOwnership(address _newOwner) internal {
-    require(_newOwner != address(0));
-    emit OwnershipTransferred(owner, _newOwner);
-    owner = _newOwner;
-  }
-}
-
-contract BBODServiceRegistry is Ownable {
-
-  //1. Manager
-  //2. CustodyStorage
-  mapping(uint => address) public registry;
-
-    constructor(address _owner) {
-        owner = _owner;
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
     }
 
-  function setServiceRegistryEntry (uint key, address entry) external onlyOwner {
-    registry[key] = entry;
-  }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+}
+
+contract SafeMath {
+    function safeMul(uint a, uint b) internal returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function safeDiv(uint a, uint b) internal returns (uint) {
+        assert(b > 0);
+        uint c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+
+    function safeSub(uint a, uint b) internal returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
+
+    function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+        return a >= b ? a : b;
+    }
+
+    function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+        return a < b ? a : b;
+    }
+
+    function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+        return a < b ? a : b;
+    }
+
 }
 
 
 /**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
+ * Safe unsigned safe math.
+ *
+ * https://blog.aragon.one/library-driven-development-in-solidity-2bebcaf88736#.750gwtwli
+ *
+ * Originally from https://raw.githubusercontent.com/AragonOne/zeppelin-solidity/master/contracts/SafeMathLib.sol
+ *
+ * Maintained here until merged to mainline zeppelin-solidity.
+ *
  */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender)
-    public view returns (uint256);
+library SafeMathLibExt {
 
-  function transferFrom(address from, address to, uint256 value)
-    public returns (bool);
-
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
-}
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
+    function times(uint a, uint b) returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
     }
 
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-
-contract ManagerInterface {
-  function createCustody(address) external {}
-
-  function isExchangeAlive() public pure returns (bool) {}
-
-  function isDailySettlementOnGoing() public pure returns (bool) {}
-}
-
-contract Custody {
-
-  using SafeMath for uint;
-
-  BBODServiceRegistry public bbodServiceRegistry;
-  address public owner;
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-  constructor(address _serviceRegistryAddress, address _owner) public {
-    bbodServiceRegistry = BBODServiceRegistry(_serviceRegistryAddress);
-    owner = _owner;
-  }
-
-  function() public payable {}
-
-  modifier liveExchangeOrOwner(address _recipient) {
-    var manager = ManagerInterface(bbodServiceRegistry.registry(1));
-
-    if (manager.isExchangeAlive()) {
-
-      require(msg.sender == address(manager));
-
-      if (manager.isDailySettlementOnGoing()) {
-        require(_recipient == address(manager), "Only manager can do this when the settlement is ongoing");
-      } else {
-        require(_recipient == owner);
-      }
-
-    } else {
-      require(msg.sender == owner, "Only owner can do this when exchange is dead");
+    function divides(uint a, uint b) returns (uint) {
+        assert(b > 0);
+        uint c = a / b;
+        assert(a == b * c + a % b);
+        return c;
     }
-    _;
-  }
 
-  function withdraw(uint _amount, address _recipient) external liveExchangeOrOwner(_recipient) {
-    _recipient.transfer(_amount);
-  }
+    function minus(uint a, uint b) returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  function transferToken(address _erc20Address, address _recipient, uint _amount)
-    external liveExchangeOrOwner(_recipient) {
+    function plus(uint a, uint b) returns (uint) {
+        uint c = a + b;
+        assert(c>=a);
+        return c;
+    }
 
-    ERC20 token = ERC20(_erc20Address);
-
-    token.transfer(_recipient, _amount);
-  }
-
-  function transferOwnership(address newOwner) public {
-    require(msg.sender == owner, "Only the owner can transfer ownership");
-    require(newOwner != address(0));
-
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
 }
 
 
-contract Insurance is Custody {
+contract Destructable is Ownable {
 
-  constructor(address _serviceRegistryAddress, address _owner)
-  Custody(_serviceRegistryAddress, _owner) public {}
+    function burn() public onlyOwner {
+        selfdestruct(owner);
+    }
 
-  function useInsurance (uint _amount) external {
-    var manager = ManagerInterface(bbodServiceRegistry.registry(1));
-    //Only usable for manager during settlement
-    require(manager.isDailySettlementOnGoing() && msg.sender == address(manager));
+}
 
-    address(manager).transfer(_amount);
-  }
+
+contract TokensContract {
+    function balanceOf(address who) public constant returns (uint256);
+    function transferFrom(address _from, address _to, uint _value) returns (bool success);
+    function approve(address _spender, uint _value) returns (bool success);
+}
+
+contract Insurance is Destructable, SafeMath  {
+
+    uint startClaimDate;
+    uint endClaimDate;
+    uint rewardWeiCoefficient;
+    uint256 buyPrice;
+    address tokensContractAddress;
+    uint256 ichnDecimals;
+
+    mapping (address => uint256) buyersBalances;
+
+    struct ClientInsurance {
+        uint256 tokensCount;
+        bool isApplied;
+        bool exists;
+        bool isBlocked;
+    }
+
+
+    mapping(address => ClientInsurance) insurancesMap;
+
+
+    function Insurance() public {
+        /* I-CHAIN.NET (ICHN) ERC20 token */
+        tokensContractAddress = 0x3ab7b695573017eeBD6377c433F9Cf3eF5B4cd48;
+
+        /* UTC, 31 dec 2020 00:00 - 2 feb 2021 00:00 */
+        startClaimDate = 1609372800;
+        endClaimDate = 1612224000;
+
+
+        /* 0.1 ether */
+        rewardWeiCoefficient = 100000000000000000;
+
+        /* 0.05 ether */
+        buyPrice = 50000000000000000;
+
+        /* ICHN Token Decimals is 18 */
+        ichnDecimals = 1000000000000000000;
+    }
+
+    /**
+     * Don't expect to just send money by anyone except the owner
+     */
+    function () public payable {
+        throw;
+    }
+
+    /**
+     * Owner can add ETH to contract
+     */
+    function addEth() public payable onlyOwner {
+    }
+    
+    /**
+     * Owner can transfer ETH from contract to address
+     * Amount - 18 decimals
+     */
+    function transferEthTo(address to, uint256 amount) public payable onlyOwner {
+        require(address(this).balance > amount);
+        to.transfer(amount);
+    }
+
+    /**
+     * Basic entry point for buy insurance
+     */
+    function buy() public payable {
+        /* Can be called only once for address */
+        require(buyersBalances[msg.sender] == 0);
+
+        /* Checking price */
+        require(msg.value == buyPrice);
+
+        /* At least one token */
+        require(hasTokens(msg.sender));
+
+        /* Remember payment */
+        buyersBalances[msg.sender] = safeAdd(buyersBalances[msg.sender], msg.value);
+    }
+
+    function isClient(address clientAddress) public constant onlyOwner returns(bool) {
+        return insurancesMap[clientAddress].exists;
+    }
+
+    function addBuyer(address clientAddress, uint256 tokensCount) public onlyOwner {
+        require( (clientAddress != address(0)) && (tokensCount > 0) );
+
+        /* Checking payment */
+        require(buyersBalances[clientAddress] == buyPrice);
+
+        /* Can be called only once for address */
+        require(!insurancesMap[clientAddress].exists);
+
+        /* Checking the current number of tokens */
+        require(getTokensCount(clientAddress) >= tokensCount);
+
+        insurancesMap[clientAddress] = ClientInsurance(tokensCount, false, true, false);
+    }
+
+    function claim(address to, uint256 returnedTokensCount) public onlyOwner {
+        /* Can be called only on time range */
+        require(now > startClaimDate && now < endClaimDate);
+
+        /* Can be called once for address */
+        require( (to != address(0)) && (insurancesMap[to].exists) && (!insurancesMap[to].isApplied) && (!insurancesMap[to].isBlocked) );
+
+        /* Tokens returned */
+        require(returnedTokensCount >= insurancesMap[to].tokensCount);
+
+        /* Start transfer */
+        uint amount = getRewardWei(to);
+
+        require(address(this).balance > amount);
+        insurancesMap[to].isApplied = true;
+
+        to.transfer(amount);
+    }
+
+    function blockClient(address clientAddress) public onlyOwner {
+        insurancesMap[clientAddress].isBlocked = true;
+    }
+
+    function unblockClient(address clientAddress) public onlyOwner {
+        insurancesMap[clientAddress].isBlocked = false;
+    }
+
+    function isClientBlocked(address clientAddress) public constant onlyOwner returns(bool) {
+        return insurancesMap[clientAddress].isBlocked;
+    }
+
+    /**
+     * Sets buy price for insurance
+     */
+    function setBuyPrice(uint256 priceWei) public onlyOwner {
+        buyPrice = priceWei;
+    }
+
+    /**
+     * Sets tokens contract address from which check balance of tokens
+     */
+    function setTokensContractAddress(address contractAddress) public onlyOwner {
+        tokensContractAddress = contractAddress;
+    }
+
+    /**
+     * Returns address of tokens contract from which check balance of tokens
+     */
+    function getTokensContractAddress() public constant onlyOwner returns(address) {
+        return tokensContractAddress;
+    }
+
+    function getRewardWei(address clientAddress) private constant returns (uint256) {
+        uint tokensCount = insurancesMap[clientAddress].tokensCount;
+        return safeMul(tokensCount, rewardWeiCoefficient);
+    }
+
+    function hasTokens(address clientAddress) private constant returns (bool) {
+        return getTokensCount(clientAddress) > 0;
+    }
+
+    function getTokensCount(address clientAddress) private constant returns (uint256) {
+        TokensContract tokensContract = TokensContract(tokensContractAddress);
+
+        uint256 tcBalance = tokensContract.balanceOf(clientAddress);
+
+        return safeDiv(tcBalance, ichnDecimals);
+    }
+    
+    /**
+     * Transfer ERC20 tokens from contract to address
+     * tokensAmount - 18 decimals
+     */
+    function transferTokensTo(address to, uint256 tokensAmount) public onlyOwner {
+       TokensContract tokensContract = TokensContract(tokensContractAddress);
+       tokensContract.approve(address(this), tokensAmount);
+       tokensContract.transferFrom(address(this), to, tokensAmount);
+    }
+    
+    function getStartClaimDate() public constant onlyOwner returns(uint) {
+        return startClaimDate;
+    }
+    
+    function getEndClaimDate() public constant onlyOwner returns(uint) {
+        return endClaimDate;
+    }
 }
