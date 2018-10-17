890efa77c9910bcdc324431f363b65a7dcabaaf3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:04:07 +0700

    add contract getKittyInfo

---
+++ getKittyInfo.sol
@@ -0,0 +1,35 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract getKittyInfo at 0x3df8f2026d29d0bf9e315cdbe82b91d8df593a95
+*/
+pragma solidity ^0.4.24;
+
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract getKittyInfo {
+      address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+      KittyInterface kittyContract = KittyInterface(ckAddress);
+      function getInfo(uint256 _kittyId) public view returns (uint, uint, uint, uint, uint, uint) {
+        uint256 cooldownIndex;
+        uint256 birthTime;
+        uint256 matronId;
+        uint256 sireId;
+        uint256 generation;
+        uint256 genes;
+        (,,cooldownIndex,,,birthTime,matronId,sireId,generation,genes) = kittyContract.getKitty(_kittyId);
+        return (cooldownIndex, birthTime, matronId, sireId, generation, genes);
+      }
+      
+}
