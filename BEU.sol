6108968408e5c4bbff2c357a485ed1bc2163bbd8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:10:22 +0700

    add contract BEU

--- BEU.sol
+++ BEU.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BEU at 0x353cafd02131ca5b69211bbc3899f556bc62b7e3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BEU at 0x6bbb3f23c64e840a381e0615635475c56324d784
 */
 pragma solidity ^0.4.24;
 
@@ -7,29 +7,29 @@ pragma solidity ^0.4.24;
  * Math operations with safety checks
  */
 library SafeMath {
-  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a * b;
-    require(a == 0 || c / a == b);
-    return c;
-  }
+    function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        require(a == 0 || c / a == b);
+        return c;
+    }
 
-  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b > 0);
-    uint256 c = a / b;
-    require(a == b * c + a % b);
-    return c;
-  }
+    function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        require(a == b * c + a % b);
+        return c;
+    }
 
-  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
-    return a - b;
-  }
+    function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        return a - b;
+    }
 
-  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c>=a && c>=b);
-    return c;
-  }
+    function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c>=a && c>=b);
+        return c;
+    }
 }
 
 contract BEU {
@@ -37,15 +37,14 @@ contract BEU {
     string public name = "BitEDU";
     string public symbol = "BEU";
     uint8 public decimals = 18;
-    uint256 public totalSupply =   2000000000000000000000000000;
-	uint256 public totalLimit  = 100000000000000000000000000000;
+    uint256 public totalSupply = 2000000000000000000000000000;
     address public owner;
-	bool public lockAll = false;
+    bool public lockAll = false;
 
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
-	mapping (address => uint256) public freezeOf;
-	mapping (address => uint256) public lockOf;
+    mapping (address => uint256) public freezeOf;
+    mapping (address => uint256) public lockOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
     /* This generates a public event on the blockchain that will notify clients */
@@ -63,17 +62,18 @@ contract BEU {
     /* Initializes contract with initial supply tokens to the creator of the contract */
     constructor() public {
         owner = msg.sender;
-		balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        balanceOf[msg.sender] = totalSupply;                                        // Give the creator all initial tokens
     }
 
     /* Send coins */
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(!lockAll);                                                          // lock all transfor in critical situation
         require(_to != 0x0);                                                        // Prevent transfer to 0x0 address. Use burn() instead
-		require(_value > 0);                                                        // Check value
+        require(_value > 0);                                                        // Check value
         require(balanceOf[msg.sender] >= _value);                                   // Check if the sender has enough
         require(balanceOf[_to] + _value >= balanceOf[_to]);                         // Check for overflows
         require(balanceOf[_to] + _value >= _value);                                 // Check for overflows
+        require(balanceOf[msg.sender] >= lockOf[msg.sender]);                                 // Check for lock
         require(balanceOf[msg.sender] >= lockOf[msg.sender] + _value);              // Check for lock
         balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);    // Subtract from the sender
         balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                  // Add the same to the recipient
@@ -83,7 +83,7 @@ contract BEU {
 
     /* Allow another contract to spend some tokens in your behalf */
     function approve(address _spender, uint256 _value) public returns (bool success) {
-		require(_value >= 0);                                                        // Check Value
+        require((_value == 0) || (allowance[msg.sender][_spender] == 0));           // Only Reset, not allowed to modify
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
@@ -93,11 +93,12 @@ contract BEU {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(!lockAll);                                                          // lock all transfor in critical situation cases
         require(_to != 0x0);                                                        // Prevent transfer to 0x0 address. Use burn() instead
-		require(_value > 0);                                                        // Check Value
+        require(_value > 0);                                                        // Check Value
         require(balanceOf[_from] >= _value);                                        // Check if the sender has enough
         require(balanceOf[_to] + _value > balanceOf[_to]);                          // Check for overflows
         require(balanceOf[_to] + _value > _value);                                  // Check for overflows
         require(allowance[_from][msg.sender] >= _value);                            // Check allowance
+        require(balanceOf[_from] >= lockOf[_from]);                                 // Check for lock
         require(balanceOf[_from] >= lockOf[_from] + _value);                        // Check for lock
         balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);              // Subtract from the sender
         balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                  // Add the same to the recipient
@@ -109,8 +110,8 @@ contract BEU {
     function freeze(uint256 _value) public returns (bool success) {
         require(_value > 0);
         require(balanceOf[msg.sender] >= _value);                                   // Check if the sender has enough
-	    require(freezeOf[msg.sender] + _value >= freezeOf[msg.sender]);             // Check for Overflows
-	    require(freezeOf[msg.sender] + _value >= _value);                           // Check for Overflows
+        require(freezeOf[msg.sender] + _value >= freezeOf[msg.sender]);             // Check for Overflows
+        require(freezeOf[msg.sender] + _value >= _value);                           // Check for Overflows
         balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);    // Subtract from the sender
         freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);      // Updates totalSupply
         emit Freeze(msg.sender, _value);
@@ -121,9 +122,9 @@ contract BEU {
         require(_value > 0);                                                        // Check Value
         require(freezeOf[msg.sender] >= _value);                                    // Check if the sender has enough
         require(balanceOf[msg.sender] + _value > balanceOf[msg.sender]);            // Check for Overflows
-	    require(balanceOf[msg.sender] + _value > _value);                           // Check for overflows
+        require(balanceOf[msg.sender] + _value > _value);                           // Check for overflows
         freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);      // Subtract from the freeze
-	    balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);    // Add to balance
+        balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);    // Add to balance
         emit Unfreeze(msg.sender, _value);
         return true;
     }
@@ -133,34 +134,22 @@ contract BEU {
         require(_value > 0);                                                        // Check Value
         require(balanceOf[msg.sender] >= _value);                                   // Check if the sender has enough
         require(totalSupply >= _value);                                             // Check for overflows
-	    balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);    // Subtract from the sender
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);    // Subtract from the sender
         totalSupply = SafeMath.safeSub(totalSupply, _value);                        // Updates totalSupply
         return true;
     }
 
-    function mint(uint256 _value) public returns (bool success) {
-        require(msg.sender == owner);                                               // Only Owner
-        require(_value > 0);                                                        // Check Value
-        require(balanceOf[msg.sender] + _value > balanceOf[msg.sender]);            // Check if the sender has enough
-        require(balanceOf[msg.sender] + _value > _value);                           // Check if the sender has enough
-        require(totalSupply + _value > totalSupply);                                // Check for overflows
-		require(totalSupply + _value > _value);                                     // Check for overflows
-        require(totalSupply + _value <= totalLimit);                                // Check for limit
-        balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);    // Subtract from the sender
-        totalSupply = SafeMath.safeAdd(totalSupply, _value);                        // Updates totalSupply
-        return true;
-    }
-
     function lock(address _to, uint256 _value) public returns (bool success) {
-	    require(msg.sender == owner);                                                // Only Owner
+        require(msg.sender == owner);                                                // Only Owner
         require(_to != 0x0);                                                         // Prevent lock to 0x0 address
-	    require(_value >= 0);                                                        // Check Value
+        require((_value == 0) || (lockOf[_to] == 0));                                // Only Reset, not allowed to modify
+        require(balanceOf[_to] >= _value);                                           // Check for lock overflows
         lockOf[_to] = _value;
         return true;
     }
 
     function lockForAll(bool b) public returns (bool success) {
-	    require(msg.sender == owner);                                                // Only Owner
+        require(msg.sender == owner);                                                // Only Owner
         lockAll = b;
         return true;
     }
