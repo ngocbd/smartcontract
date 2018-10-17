ca33c074f61a222cac70ee475c6fee1e4d4e4c86
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:04:52 +0700

    add contract PChannel

--- PChannel.sol
+++ PChannel.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PChannel at 0xbf5eb45cb01824af3b47c656c1c8d1df630916e4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PChannel at 0x8700e5453718244cac9a5257aa82abc9d7bb13af
 */
 pragma solidity ^0.4.19;
 
@@ -702,10 +702,10 @@ contract PChannel is Ownable {
     Referral private refProgram;
 
     // fixed deposit amount in USD cents
-    uint private depositAmount = 5000;
+    uint private depositAmount = 10000000;
 
     // max deposit amount in USD cents
-    uint private maxDepositAmount = 6250;
+    uint private maxDepositAmount =12500000;
 
     // investor => number of deposits
     mapping (address => uint8) private deposits; 
@@ -717,7 +717,11 @@ contract PChannel is Ownable {
     function() payable public {
         uint8 depositsCount = deposits[msg.sender];
         // check if user has already exceeded 15 deposits limit
-        require(depositsCount < 15);
+        // if so, set deposit count to 0 and make first deposit
+        if (depositsCount == 15) {
+            depositsCount = 0;
+            deposits[msg.sender] = 0;
+        }
 
         uint amount = msg.value;
         uint usdAmount = amount * refProgram.ethUsdRate() / 10**18;
