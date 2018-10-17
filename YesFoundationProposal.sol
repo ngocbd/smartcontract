cfd6ca48d4e5c39e09471f953d89ffa778052576
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:01:14 +0700

    add contract YesFoundationProposal

---
+++ YesFoundationProposal.sol
@@ -0,0 +1,4 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract YesFoundationProposal at 0x305505c8c9d51602f3ebe0ce984ab2aeb0df3172
+*/
+contract YesFoundationProposal {}
