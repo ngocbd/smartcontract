70504179c1343ed40accf0c8853ce084773af3a7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:20:27 +0700

    add contract Distribution

--- Distribution.sol
+++ Distribution.sol
@@ -1,50 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribution at 0x7b8e03e39ea47263557606d621ff15c702cd8ddc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribution at 0x73d100cfb06c91ef649f442eda7ac6dc80138095
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.13;
 
-// File: zeppelin-solidity/contracts/math/SafeMath.sol
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-// File: zeppelin-solidity/contracts/ownership/Ownable.sol
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
   address public owner;
 
@@ -60,7 +18,6 @@ contract Ownable {
     owner = msg.sender;
   }
 
-
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -69,287 +26,219 @@ contract Ownable {
     _;
   }
 
-
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+    emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
 
 }
 
-// File: zeppelin-solidity/contracts/token/ERC20Basic.sol
+contract CanReclaimToken is Ownable {
+  using SafeERC20 for ERC20Basic;
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
+  /**
+   * @dev Reclaim all ERC20Basic compatible tokens
+   * @param token ERC20Basic The address of the token contract
+   */
+  function reclaimToken(ERC20Basic token) external onlyOwner {
+    uint256 balance = token.balanceOf(this);
+    token.safeTransfer(owner, balance);
+  }
+
+}
+
+contract Claimable is Ownable {
+  address public pendingOwner;
+
+  /**
+   * @dev Modifier throws if called by any account other than the pendingOwner.
+   */
+  modifier onlyPendingOwner() {
+    require(msg.sender == pendingOwner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to set the pendingOwner address.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    pendingOwner = newOwner;
+  }
+
+  /**
+   * @dev Allows the pendingOwner address to finalize the transfer.
+   */
+  function claimOwnership() onlyPendingOwner public {
+    emit OwnershipTransferred(owner, pendingOwner);
+    owner = pendingOwner;
+    pendingOwner = address(0);
+  }
+}
+
+contract Whitelist is Ownable {
+  mapping(address => bool) public whitelist;
+
+  event WhitelistedAddressAdded(address addr);
+  event WhitelistedAddressRemoved(address addr);
+
+  /**
+   * @dev Throws if called by any account that's not whitelisted.
+   */
+  modifier onlyWhitelisted() {
+    require(whitelist[msg.sender]);
+    _;
+  }
+
+  /**
+   * @dev add an address to the whitelist
+   * @param addr address
+   * @return true if the address was added to the whitelist, false if the address was already in the whitelist
+   */
+  function addAddressToWhitelist(address addr) onlyOwner public returns(bool success) {
+    if (!whitelist[addr]) {
+      whitelist[addr] = true;
+      emit WhitelistedAddressAdded(addr);
+      success = true;
+    }
+  }
+
+  /**
+   * @dev add addresses to the whitelist
+   * @param addrs addresses
+   * @return true if at least one address was added to the whitelist,
+   * false if all addresses were already in the whitelist
+   */
+  function addAddressesToWhitelist(address[] addrs) onlyOwner public returns(bool success) {
+    for (uint256 i = 0; i < addrs.length; i++) {
+      if (addAddressToWhitelist(addrs[i])) {
+        success = true;
+      }
+    }
+  }
+
+  /**
+   * @dev remove an address from the whitelist
+   * @param addr address
+   * @return true if the address was removed from the whitelist,
+   * false if the address wasn't in the whitelist in the first place
+   */
+  function removeAddressFromWhitelist(address addr) onlyOwner public returns(bool success) {
+    if (whitelist[addr]) {
+      whitelist[addr] = false;
+      emit WhitelistedAddressRemoved(addr);
+      success = true;
+    }
+  }
+
+  /**
+   * @dev remove addresses from the whitelist
+   * @param addrs addresses
+   * @return true if at least one address was removed from the whitelist,
+   * false if all addresses weren't in the whitelist in the first place
+   */
+  function removeAddressesFromWhitelist(address[] addrs) onlyOwner public returns(bool success) {
+    for (uint256 i = 0; i < addrs.length; i++) {
+      if (removeAddressFromWhitelist(addrs[i])) {
+        success = true;
+      }
+    }
+  }
+
+}
+
 contract ERC20Basic {
-  uint256 public totalSupply;
+  function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-// File: zeppelin-solidity/contracts/token/BasicToken.sol
-
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances.
- */
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-
-  mapping(address => uint256) balances;
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-
-    // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-// File: zeppelin-solidity/contracts/token/BurnableToken.sol
+library SafeERC20 {
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    assert(token.transfer(to, value));
+  }
 
-/**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
- */
-contract BurnableToken is BasicToken {
+  function safeTransferFrom(
+    ERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    assert(token.transferFrom(from, to, value));
+  }
 
-    event Burn(address indexed burner, uint256 value);
-
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
-    function burn(uint256 _value) public {
-        require(_value <= balances[msg.sender]);
-        // no need to require value <= totalSupply, since that would imply the
-        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
-
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        Burn(burner, _value);
-    }
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    assert(token.approve(spender, value));
+  }
 }
 
-// File: contracts/Distribution.sol
+contract Distribution is CanReclaimToken, Claimable, Whitelist {
 
-/**
- * @title Distribution contract
- * @dev see https://send.sd/distribution
- */
-contract Distribution is Ownable {
-  using SafeMath for uint256;
+    using SafeERC20 for ERC20Basic;
 
-  uint16 public stages;
-  uint256 public stageDuration;
-  uint256 public startTime;
+    event Distributed(address beneficiary, uint256 amount);
 
-  uint256 public soldTokens;
-  uint256 public bonusClaimedTokens;
-  uint256 public raisedETH;
-  uint256 public raisedUSD;
+    address[] public receivers;
+    // Also used to indicate the distribution state.
+    uint256 public amount = 0;
+    ERC20Basic public token;
 
-  uint256 public weiUsdRate;
-
-  BurnableToken public token;
-
-  bool public isActive;
-  uint256 public cap;
-  uint256 public stageCap;
-
-  mapping (address => mapping (uint16 => uint256)) public contributions;
-  mapping (uint16 => uint256) public sold;
-  mapping (uint16 => bool) public burned;
-  mapping (address => mapping (uint16 => bool)) public claimed;
-
-  event NewPurchase(
-    address indexed purchaser,
-    uint256 sdtAmount,
-    uint256 usdAmount,
-    uint256 ethAmount
-  );
-
-  event NewBonusClaim(
-    address indexed purchaser,
-    uint256 sdtAmount
-  );
-
-  function Distribution(
-      uint16 _stages,
-      uint256 _stageDuration,
-      address _token
-  ) public {
-    stages = _stages;
-    stageDuration = _stageDuration;
-    isActive = false;
-    token = BurnableToken(_token);
-  }
-
-  /**
-   * @dev contribution function
-   */
-  function () external payable {
-    require(isActive);
-    require(weiUsdRate > 0);
-    require(getStage() < stages);
-
-    uint256 usd = msg.value / weiUsdRate;
-    uint256 tokens = computeTokens(usd);
-    uint16 stage = getStage();
-
-    sold[stage] = sold[stage].add(tokens);
-    require(sold[stage] < stageCap);
-
-    contributions[msg.sender][stage] = contributions[msg.sender][stage].add(tokens);
-    soldTokens = soldTokens.add(tokens);
-    raisedETH = raisedETH.add(msg.value);
-    raisedUSD = raisedUSD.add(usd);
-
-    NewPurchase(msg.sender, tokens, usd, msg.value);
-    token.transfer(msg.sender, tokens);
-  }
-
-  /**
-   * @dev Initialize distribution
-   * @param _cap uint256 The amount of tokens for distribution
-   */
-  function init(uint256 _cap, uint256 _startTime) public onlyOwner {
-    require(!isActive);
-    require(token.balanceOf(this) == _cap);
-    require(_startTime > block.timestamp);
-
-    startTime = _startTime;
-    cap = _cap;
-    stageCap = cap / stages;
-    isActive = true;
-  }
-
-  /**
-   * @dev retrieve bonus from specified stage
-   * @param _stage uint16 The stage
-   */
-  function claimBonus(uint16 _stage) public {
-    require(!claimed[msg.sender][_stage]);
-    require(getStage() > _stage);
-
-    if (!burned[_stage]) {
-      token.burn(stageCap.sub(sold[_stage]).sub(sold[_stage].mul(computeBonus(_stage)).div(1 ether)));
-      burned[_stage] = true;
+    constructor(ERC20Basic _token) public {
+        token = _token;
     }
 
-    uint256 tokens = computeAddressBonus(_stage);
-    token.transfer(msg.sender, tokens);
-    bonusClaimedTokens = bonusClaimedTokens.add(tokens);
-    claimed[msg.sender][_stage] = true;
+    function setReceivers(address[] _receivers, uint256 _amount) onlyWhitelisted external {
+        // Be conservative about the size.
+        require(_receivers.length <= 80);
+        require(_amount > 0);
 
-    NewBonusClaim(msg.sender, tokens);
-  }
-
-  /**
-   * @dev set an exchange rate in wei
-   * @param _rate uint256 The new exchange rate
-   */
-  function setWeiUsdRate(uint256 _rate) public onlyOwner {
-    require(_rate > 0);
-    weiUsdRate = _rate;
-  }
-
-  /**
-   * @dev retrieve ETH
-   * @param _amount uint256 The new exchange rate
-   * @param _address address The address to receive ETH
-   */
-  function forwardFunds(uint256 _amount, address _address) public onlyOwner {
-    _address.transfer(_amount);
-  }
-
-  /**
-   * @dev compute tokens given a USD value
-   * @param _usd uint256 Value in USD
-   */
-  function computeTokens(uint256 _usd) public view returns(uint256) {
-    return _usd.mul(1000000000000000000 ether).div(
-      soldTokens.mul(19800000000000000000).div(cap).add(200000000000000000)
-    );
-  }
-
-  /**
-   * @dev current stage
-   */
-  function getStage() public view returns(uint16) {
-    require(block.timestamp >= startTime);
-    return uint16(uint256(block.timestamp).sub(startTime).div(stageDuration));
-  }
-
-  /**
-   * @dev compute bonus (%) for a specified stage
-   * @param _stage uint16 The stage
-   */
-  function computeBonus(uint16 _stage) public view returns(uint256) {
-    return uint256(100000000000000000).sub(sold[_stage].mul(100000).div(441095890411));
-  }
-
-  /**
-   * @dev compute for a specified stage
-   * @param _stage uint16 The stage
-   */
-  function computeAddressBonus(uint16 _stage) public view returns(uint256) {
-    return contributions[msg.sender][_stage].mul(computeBonus(_stage)).div(1 ether);
-  }
-
-  //////////
-  // Safety Methods
-  //////////
-  /// @notice This method can be used by the controller to extract mistakenly
-  ///  sent tokens to this contract.
-  /// @param _token The address of the token contract that you want to recover
-  ///  set to 0 in case you want to extract ether.
-  function claimTokens(address _token) public onlyOwner {
-    // owner can claim any token but SDT
-    require(_token != address(token));
-    if (_token == 0x0) {
-      owner.transfer(this.balance);
-      return;
+        receivers = _receivers;
+        amount = _amount;
     }
 
-    ERC20Basic erc20token = ERC20Basic(_token);
-    uint256 balance = erc20token.balanceOf(this);
-    erc20token.transfer(owner, balance);
-    ClaimedTokens(_token, owner, balance);
-  }
+    function distribute() onlyWhitelisted external {
+        require(receivers.length > 0);
+        require(amount > 0);
+        for (uint256 i = 0; i < receivers.length; ++i) {
+            address beneficiary = receivers[i];
+            token.safeTransfer(beneficiary, amount);
+            emit Distributed(beneficiary, amount);
+        }
+        // Clear.
+        amount = 0;
+        delete receivers;
+    }
 
-  event ClaimedTokens(
-    address indexed _token,
-    address indexed _controller,
-    uint256 _amount
-  );
+    function batchDistribute(
+        address[] batchReceivers,
+        uint256 batchAmount
+    ) onlyWhitelisted external
+    {
+        require(batchReceivers.length > 0);
+        require(batchAmount > 0);
+        for (uint256 i = 0; i < batchReceivers.length; ++i) {
+            address beneficiary = batchReceivers[i];
+            token.safeTransfer(beneficiary, batchAmount);
+            emit Distributed(beneficiary, batchAmount);
+        }
+    }
+
+    function finished() public view returns (bool) {
+        return amount == 0;
+    }
 }
