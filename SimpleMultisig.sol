95624f242f9ce6b01e34453930c06a1bd92c013d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:36:52 +0700

    add contract SimpleMultiSig

--- SimpleMultisig.sol
+++ SimpleMultisig.sol
@@ -1,44 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleMultiSig at 0xdcece8aadcbf7e3c6e8ec9e9452f9940b4790352
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleMultiSig at 0x4eb88afb7022249572eb6da58f07549a6f4b9d93
 */
-//
-// Simple MultiSig Wallet Contract by Christian Lundkvist
-// https://github.com/christianlundkvist/simple-multisig
-//
-//
-//  Copyright (c) 2017 Christian Lundkvist
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
-//
-pragma solidity 0.4.18;
+pragma solidity ^0.4.18;
+
 contract SimpleMultiSig {
 
-  uint public nonce;                // (only) mutable state
-  uint public threshold;            // immutable state
+  uint public nonce;                 // (only) mutable state
+  uint public threshold;             // immutable state
   mapping (address => bool) isOwner; // immutable state
   address[] public ownersArr;        // immutable state
 
   function SimpleMultiSig(uint threshold_, address[] owners_) public {
     require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
 
-    address lastAdd = address(0);
-    for (uint i=0; i<owners_.length; i++) {
+    address lastAdd = address(0); 
+    for (uint i = 0; i < owners_.length; i++) {
       require(owners_[i] > lastAdd);
       isOwner[owners_[i]] = true;
       lastAdd = owners_[i];
@@ -53,7 +29,7 @@ contract SimpleMultiSig {
     require(sigR.length == sigS.length && sigR.length == sigV.length);
 
     // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
-    bytes32 txHash = keccak256(byte(0x19), byte(0), address(this), destination, value, data, nonce);
+    bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
 
     address lastAdd = address(0); // cannot have address(0) as an owner
     for (uint i = 0; i < threshold; i++) {
@@ -64,8 +40,16 @@ contract SimpleMultiSig {
 
     // If we make it here all signatures are accounted for
     nonce = nonce + 1;
-    require(destination.call.value(value)(data));
+    require(executeCall(destination, value, data));
   }
 
-  function () public payable {}
+  // The address.call() syntax is no longer recommended, see:
+  // https://github.com/ethereum/solidity/issues/2884
+  function executeCall(address to, uint256 value, bytes data) internal returns (bool success) {
+    assembly {
+      success := call(gas, to, value, add(data, 0x20), mload(data), 0, 0)
+    }
+  }
+
+  function () payable public {}
 }
