2bf3826342493259889a94d598a0687032382427
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:09:14 +0700

    add contract AT

--- AT.sol
+++ AT.sol
@@ -1,143 +1,64 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AT at 0x259E8EADE77E15f5317A4e5A0Fb101C586C5707F
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AT at 0x22e087293e2ccd21f1667ee487f65800fcc02708
 */
-pragma solidity ^0.4.18;
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+pragma solidity ^0.4.16;
+contract AT{
 
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-
-    function max64(uint64 a, uint64 b) internal pure returns (uint64) {
-        return a >= b ? a : b;
-    }
-
-    function min64(uint64 a, uint64 b) internal pure returns (uint64) {
-        return a < b ? a : b;
-    }
-
-    function max256(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a >= b ? a : b;
-    }
-
-    function min256(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
-}
-contract Ownable {
-    address public owner;
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != owner) {
-            revert();
-        }
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        if (newOwner != address(0)) {
-            owner = newOwner;
-        }
-    }
-
-    function destruct() public onlyOwner {
-        selfdestruct(owner);
-    }
-}
-contract ERC20Basic {
-    function balanceOf(address who) public constant returns (uint256);
-    function transfer(address to, uint256 value) public;
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public;
-    function approve(address spender, uint256 value) public;
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-contract BasicToken is ERC20Basic {
-    using SafeMath for uint256;
-
-    mapping(address => uint256) balances;
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
     uint256 public totalSupply;
-
-    modifier onlyPayloadSize(uint256 size) {
-        if(msg.data.length < size + 4) {
-            revert();
-        }
-        _;
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg SBX
+     event Transfer(address indexed _from, address indexed _to, uint256 _value); 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+   
+    function AT() public {
+        balances[msg.sender] = 20000000000000;               // Give the creator all initial tokens
+        totalSupply = 20000000000000;                        // Update total supply
+        name = "agriculturatoken";                                   // Set the name for display purposes
+        decimals =4;                            // Amount of decimals for display purposes
+        symbol = "AT";                               // Set the symbol for display purposes
     }
 
-    function transfer(address _to, uint256 _value) public onlyPayloadSize(2 * 32) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
         Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-}
-contract StandardToken is BasicToken, ERC20 {
-
-    mapping (address => mapping (address => uint256)) allowed;
-
-    function transferFrom(address _from, address _to, uint256 _value) public onlyPayloadSize(3 * 32) {
-        var _allowance = allowed[_from][msg.sender];
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
-        Transfer(_from, _to, _value);
-    }
-
-    function approve(address _spender, uint256 _value) public {
-        if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) revert();
-
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
+        return true;
     }
 
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowed[_owner][_spender];
-    }
-}
-contract AT is StandardToken, Ownable {
-
-    string public constant name = "ATCHAIN(AT)";
-    string public constant symbol = "AT";
-    uint256 public constant decimals = 8;
-
-    function AT() public {
-        owner = msg.sender;
-        totalSupply=150000000000000000;
-        balances[owner]=totalSupply;
-    }
-
-    function () public {
-        revert();
-    }
+    }   
 }
