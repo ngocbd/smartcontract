0303ab5f562731bbd81c9205dfc2e57eefe7624d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:32:47 +0700

    add contract Distribute

--- Distribute.sol
+++ Distribute.sol
@@ -1,25 +1,153 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0xf3163e55e521bd49521976d43d82d21a9729dc9d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Distribute at 0x7Db19012B3D97F86c69Bc275a717b006D8B2ebFE
 */
-pragma solidity ^0.4.0;
-contract Distribute {
+pragma solidity ^0.4.24;
 
-    address public owner;
+contract Ownable {
+  address public owner;
 
-    constructor() public {
-        owner = msg.sender;
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+contract Distribute is Ownable {
+    using SafeERC20 for ERC20;
+
+    event TokenReleased(address indexed buyer, uint256 amount);
+
+    ERC20 public Token;
+    mapping (address => bool) released;
+
+    constructor(address token) public {
+        require(token != address(0));
+        Token = ERC20(token);
     }
 
-    function transferETHS(address[] _tos) payable public returns(bool) {
-        require(_tos.length > 0);
-        uint val = this.balance / _tos.length;
-        for (uint i = 0; i < _tos.length; i++) {
-            _tos[i].transfer(val);
+    function release(address beneficiary, uint256 amount)
+        public
+        onlyOwner
+    {
+        require(!released[beneficiary]);
+        Token.safeTransfer(beneficiary, amount);
+        released[beneficiary] = true;
+        emit TokenReleased(beneficiary, amount);
+    }
+
+    function releaseMany(address[] beneficiaries, uint256[] amounts)
+        external
+        onlyOwner
+    {
+        require(beneficiaries.length == amounts.length);
+        for (uint256 i = 0; i < beneficiaries.length; i++) {
+            release(beneficiaries[i], amounts[i]);
         }
-        return true;
     }
 
-    function () payable public {
-        owner.transfer(this.balance);
+    function withdraw()
+        public
+        onlyOwner
+    {
+        Token.safeTransfer(owner, Token.balanceOf(address(this)));
     }
+
+    function close()
+        external
+        onlyOwner
+    {
+        withdraw();
+        selfdestruct(owner);
+    }
+}
+
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+library SafeERC20 {
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    ERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    require(token.approve(spender, value));
+  }
 }
