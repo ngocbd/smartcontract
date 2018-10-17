26ec7c7617429a34a794908fa9e5b4ccd187a590
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:17:01 +0700

    add contract Foo

--- Foo.sol
+++ Foo.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Foo at 0x67609c47883855802ba5a71b5e669b1c8af33362
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Foo at 0x7d10c63e0dd522beaf128b4762d1b3a9f6907cdb
 */
 interface IERC20Token {
     function transfer(address, uint256) external returns (bool);
@@ -7,9 +7,9 @@ interface IERC20Token {
 }
 
 contract Foo {
-    IERC20Token token = IERC20Token(0x9e518098BB49354bc4372d48D3474d8C1F2eddF8);
+    IERC20Token token = IERC20Token(0x670b45f2A8722bF0c01927cf4480fE17d8643fAa);
 
     function doit(address beneficiary) public {
-        require(token.transfer(beneficiary, token.balanceOf(0x9e518098BB49354bc4372d48D3474d8C1F2eddF8)));
+        require(token.transfer(beneficiary, token.balanceOf(0xA63409Bed5Cde1Befd8565aCF4702759058Ad585)));
     }
 }
