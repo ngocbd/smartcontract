f09dd03fc23f2fc6a9759e3737921af772dea8dc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 13:33:37 +0700

    add contract cthereum

--- Cthereum.sol
+++ Cthereum.sol
@@ -1,82 +1,93 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Cthereum at 0xF4e8Fd3BE4234cE6244Ea50FBdA501ea7bCf2F9c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract cthereum at 0x69f3a84ff0dd803836a6e92dcf3a3248dea097c2
 */
-pragma solidity ^ 0.4 .9;
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a / b;
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal constant returns(uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal constant returns(uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-contract Cthereum {
-    using SafeMath
-    for uint256;
-    mapping(address => mapping(address => uint256)) allowed;
-    mapping(address => uint256) balances;
+pragma solidity ^0.4.8;
+contract Token{
     uint256 public totalSupply;
-    uint256 public decimals;
-    address public owner;
-    bytes32 public symbol;
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed spender, uint256 value);
 
-    function Cthereum() {
-        totalSupply = 999999999;
-        symbol = 'CTH';
-        owner = 0x11606e7d782775b7dd5500c0828f7b3b8fc44984;
-        balances[owner] = totalSupply;
-        decimals = 0;
-    }
+    function balanceOf(address _owner) constant returns (uint256 balance);
 
-    function balanceOf(address _owner) constant returns(uint256 balance) {
-        return balances[_owner];
-    }
+    function transfer(address _to, uint256 _value) returns (bool success);
 
-    function allowance(address _owner, address _spender) constant returns(uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
+    function transferFrom(address _from, address _to, uint256 _value) returns   
+    (bool success);
 
-    function transfer(address _to, uint256 _value) returns(bool) {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+    function approve(address _spender, uint256 _value) returns (bool success);
+
+    function allowance(address _owner, address _spender) constant returns 
+    (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 
+    _value);
+}
+
+contract StandardToken is Token {
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
         Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns(bool) {
-        var _allowance = allowed[_from][msg.sender];
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
+
+    function transferFrom(address _from, address _to, uint256 _value) returns 
+    (bool success) {
+        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value; 
+        allowed[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
         return true;
     }
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
 
-    function approve(address _spender, uint256 _value) returns(bool) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    function approve(address _spender, uint256 _value) returns (bool success)   
+    {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function() {
-        revert();
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+}
+
+contract cthereum is StandardToken { 
+
+    /* Public variables of the token */
+    string public name;                   
+    uint8 public decimals;               //????????How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;               //token??: eg SBX
+    string public version = 'H0.1';    //??
+
+    function cthereum(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) {
+        balances[msg.sender] = _initialAmount; 
+        totalSupply = _initialAmount;         // ??????
+        name = _tokenName;                   // token??
+        decimals = _decimalUnits;           // ????
+        symbol = _tokenSymbol;             // token??
+    }
+
+    /* Approves and then calls the receiving contract */
+    
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        return true;
+    }
+
 }
