201395f6c75c07063878a4d8c37b94f4453f1a19
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:29:16 +0700

    add contract publish_text

--- Publish_text.sol
+++ Publish_text.sol
@@ -1,14 +1,48 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Publish_text at 0xfa0cb1b68f5de256571f368c282b1d29567ba89b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract publish_text at 0x0dabb48a78e2216a1caa44839fb433699eb4700d
 */
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.17;
 
-contract Publish_text {
- 
-    string public my_message;
+contract publish_text {
     
-    constructor(string message) public {
-        my_message = message;
+    string public message;
+    address public owner;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string initialMessage) public {
+        message = initialMessage;
+        owner = msg.sender;
     }
     
+    function modifyMessage(string newMessage) onlyOwner public {
+        message = newMessage;
+    }
+    
+    function flushETH() public onlyOwner {
+        uint my_balance = address(this).balance;
+        if (my_balance > 0){
+            owner.transfer(address(this).balance);
+        }
+    }
+    
+    function flushERC20(address tokenContractAddress) public onlyOwner {
+        ERC20Interface instance = ERC20Interface(tokenContractAddress);
+        address forwarderAddress = address(this);
+        uint forwarderBalance = instance.balanceOf(forwarderAddress);
+        if (forwarderBalance == 0) {
+          return;
+        }
+        if (!instance.transfer(owner, forwarderBalance)) {
+          revert();
+        }
+    }
+}
+
+contract ERC20Interface {
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function balanceOf(address _owner) public constant returns (uint256 balance);
 }
