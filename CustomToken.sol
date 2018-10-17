776ff4fa9bb57e713b588f1b7d202cd20635ef96
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:36:30 +0700

    add contract CustomToken

--- CustomToken.sol
+++ CustomToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0xe196e2ff578c15f158c3a171715d2cc51199838d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0xf2c893cc574f1366a3a21a256951e01b75459254
 */
 pragma solidity ^0.4.19;
 
@@ -45,72 +45,13 @@ contract BaseToken {
     }
 }
 
-contract BurnToken is BaseToken {
-    event Burn(address indexed from, uint256 value);
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-        Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
-        allowance[_from][msg.sender] -= _value;
-        totalSupply -= _value;
-        Burn(_from, _value);
-        return true;
-    }
-}
-
-contract AirdropToken is BaseToken {
-    uint256 public airAmount;
-    uint256 public airBegintime;
-    uint256 public airEndtime;
-    address public airSender;
-    uint32 public airLimitCount;
-
-    mapping (address => uint32) public airCountOf;
-
-    event Airdrop(address indexed from, uint32 indexed count, uint256 tokenValue);
-
-    function airdrop() public payable {
-        require(now >= airBegintime && now <= airEndtime);
-        require(msg.value == 0);
-        if (airLimitCount > 0 && airCountOf[msg.sender] >= airLimitCount) {
-            revert();
-        }
-        _transfer(airSender, msg.sender, airAmount);
-        airCountOf[msg.sender] += 1;
-        Airdrop(msg.sender, airCountOf[msg.sender], airAmount);
-    }
-}
-
-
-
-contract CustomToken is BaseToken, BurnToken, AirdropToken {
+contract CustomToken is BaseToken {
     function CustomToken() public {
-        totalSupply = 11000000000000000000;
-        name = 'YCYR';
-        symbol = 'YCYR';
-        decimals = 10;
-        balanceOf[0x5ebc4B61A0E0187d9a72Da21bfb8b45F519cb530] = totalSupply;
-        Transfer(address(0), 0x5ebc4B61A0E0187d9a72Da21bfb8b45F519cb530, totalSupply);
-
-        airAmount = 11000000000000;
-        airBegintime = 1533042833;
-        airEndtime = 1564578833;
-        airSender = 0x34a13baBB85F0036FE7403Ab57DC9912a5596130;
-        airLimitCount = 1;
-
-        
-    }
-
-    function() public payable {
-        airdrop();
+        totalSupply = 100000000000000000000000000;
+        name = 'CYCCoin';
+        symbol = 'CYCC';
+        decimals = 18;
+        balanceOf[0x696423542f85B50fF3CA396A317De3abCb82f1c2] = totalSupply;
+        Transfer(address(0), 0x696423542f85B50fF3CA396A317De3abCb82f1c2, totalSupply);
     }
 }
