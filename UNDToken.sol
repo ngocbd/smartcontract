8bd102ecb9b422efb9cecfb0ae8aaeef7d5bbff6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 14:38:33 +0700

    add contract UNDToken

--- UNDToken.sol
+++ UNDToken.sol
@@ -1,251 +1,158 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract UNDToken at 0x66d41949efbbca83362b4e204299a729af650e2d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract UNDToken at 0x60519be559f5fddee5458aa85ccb87ec9830e78f
 */
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.23;
 
-/**
- * Welcome to the Telegram chat http://www.devsolidity.io/
- */
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
     uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+    assert(c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
   }
 }
 
-contract Token {
-    uint256 public totalSupply;
-    function balanceOf(address who) public constant returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    function allowance(address owner, address spender) public constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+contract ERC20Basic {
+  // events
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  // public functions
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address addr) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
 
+contract ERC20 is ERC20Basic {
+  // events
+  event Approval(address indexed owner, address indexed agent, uint256 value);
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    owner = newOwner;
-  }
+  // public functions
+  function allowance(address owner, address agent) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address agent, uint256 value) public returns (bool);
 
 }
 
 
-contract Pausable is Ownable {
-
-  uint public endDate;
-
-  /**
-   * @dev modifier to allow actions only when the contract IS not paused
-   */
-  modifier whenNotPaused() {
-    require(now >= endDate);
-    _;
-  }
-
-}
-
-contract StandardToken is Token, Pausable {
+contract BasicToken is ERC20Basic {
   using SafeMath for uint256;
-  mapping (address => mapping (address => uint256)) internal allowed;
 
-  mapping(address => uint256) balances;
+  // public variables
+  string public name;
+  string public symbol;
+  uint8 public decimals = 18;
 
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
-    require(_to != address(0));
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
+  // internal variables
+  uint256 _totalSupply;
+  mapping(address => uint256) _balances;
+
+  // public functions
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address addr) public view returns (uint256 balance) {
+    return _balances[addr];
+  }
+
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(to != address(0));
+    require(value <= _balances[msg.sender]);
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(msg.sender, to, value);
     return true;
   }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public constant returns (uint256 balance) {
-    return balances[_owner];
-  }
+
+}
 
 
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amount of tokens to be transferred
-   */
-  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
-    require(_to != address(0));
-    require(_value <= allowed[_from][msg.sender]);
+contract StandardToken is ERC20, BasicToken {
 
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    Transfer(_from, _to, _value);
+  mapping (address => mapping (address => uint256)) _allowances;
+
+  function transferFrom(address from, address to, uint256 value) public returns (bool) {
+    require(to != address(0));
+    require(value <= _balances[from]);
+    require(value <= _allowances[from][msg.sender]);
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowances[from][msg.sender] = _allowances[from][msg.sender].sub(value);
+    emit Transfer(from, to, value);
     return true;
   }
 
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   *
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
+  function approve(address agent, uint256 value) public returns (bool) {
+    _allowances[msg.sender][agent] = value;
+    emit Approval(msg.sender, agent, value);
     return true;
   }
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
+  function allowance(address owner, address agent) public view returns (uint256) {
+    return _allowances[owner][agent];
   }
 
-  /**
-   * approve should be called when allowed[_spender] == 0. To increment
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   */
-  function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+  function increaseApproval(address agent, uint value) public returns (bool) {
+    _allowances[msg.sender][agent] = _allowances[msg.sender][agent].add(value);
+    emit Approval(msg.sender, agent, _allowances[msg.sender][agent]);
     return true;
   }
 
-  function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
+  function decreaseApproval(address agent, uint value) public returns (bool) {
+    uint allowanceValue = _allowances[msg.sender][agent];
+    if (value > allowanceValue) {
+      _allowances[msg.sender][agent] = 0;
     } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+      _allowances[msg.sender][agent] = allowanceValue.sub(value);
     }
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    emit Approval(msg.sender, agent, _allowances[msg.sender][agent]);
     return true;
   }
-
+  // internal functions
 }
 
-/**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
- */
-contract BurnableToken is StandardToken {
 
-    event Burn(address indexed burner, uint256 value);
+contract UNDToken is StandardToken {
+  // public variables
+  string public name = "UND.SG";
+  string public symbol = "UND";
+  uint8 public decimals = 18;
 
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
-    function burn(uint256 _value) public {
-        require(_value > 0);
-        require(_value <= balances[msg.sender]);
+  // public functions
+  constructor() public {
+    //init _totalSupply
+    _totalSupply = 10000000000 * (10 ** uint256(decimals));
 
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        Burn(burner, _value);
-    }
-}
+    _balances[msg.sender] = _totalSupply;
+    emit Transfer(0x0, msg.sender, _totalSupply);
+  }
 
-contract UNDToken is BurnableToken {
 
-    string public constant name = "Undermillion Club";
-    string public constant symbol = "UND";
-    uint8 public constant decimals = 18;
-    address public tokenWallet;
-
-    uint256 public constant INITIAL_SUPPLY = 57.5 * 2.5 * 10**6 * 10**18;
-
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    function UNDToken(address tokenOwner, uint _endDate) {
-        totalSupply = INITIAL_SUPPLY;
-        balances[tokenOwner] = INITIAL_SUPPLY;
-        endDate = _endDate;
-        tokenWallet = tokenOwner;
-    }
-
-    function sendTokens(address _to, uint _amount) external onlyOwner {
-        require(_amount <= balances[tokenWallet]);
-        balances[tokenWallet] -= _amount;
-        balances[_to] += _amount;
-        Transfer(tokenWallet, _to, _amount);
-    }
-
+  // internal functions
 }
