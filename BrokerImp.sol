36c36560e4aecde97c6f01066eb6d1d77da3abf3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 14:44:08 +0700

    add contract BrokerImp

--- BrokerImp.sol
+++ BrokerImp.sol
@@ -1,9 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BrokerImp at 0x4129c45b929cdb787deaae3d0e27ba5b5cd3e3ca
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BrokerImp at 0xD5586EF251b2fbe32a03F226868E581C9f1aa261
 */
 pragma solidity ^0.4.21;
-
-
 /**
  * Changes by https://www.docademic.com/
  */
@@ -148,15 +146,18 @@ interface Token {
 	function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
 }
 
-contract BrokerImp is MultiOwnable, DestroyableMultiOwner {
+contract BrokerImp is DestroyableMultiOwner {
 	using SafeMath for uint256;
 	
 	Token public token;
 	uint256 public commission;
 	address public broker;
 	address public pool;
+	uint256 public ethReward;
+	mapping(address => bool) public ethSent;
 	
 	event CommissionChanged(uint256 _previousCommission, uint256 _commision);
+	event EthRewardChanged(uint256 _previousEthReward, uint256 _ethReward);
 	event BrokerChanged(address _previousBroker, address _broker);
 	event PoolChanged(address _previousPool, address _pool);
 	
@@ -166,13 +167,22 @@ contract BrokerImp is MultiOwnable, DestroyableMultiOwner {
 	 * @param _pool The pool of tokens address
 	 * @param _commission The percentage of the commission 0-100
 	 * @param _broker The broker address
+	 * @param _ethReward The eth to send to the beneficiary of the reward only once in wei
 	 */
-	function BrokerImp(address _token, address _pool, uint256 _commission, address _broker) public {
+	function BrokerImp(address _token, address _pool, uint256 _commission, address _broker, uint256 _ethReward) public {
 		require(_token != address(0));
 		token = Token(_token);
 		pool = _pool;
 		commission = _commission;
 		broker = _broker;
+		ethReward = _ethReward;
+	}
+	
+	/**
+	 * @dev Allows to fund the contract with ETH.
+	 */
+	function fund(uint256 amount) payable public {
+		require(msg.value == amount);
 	}
 	
 	/**
@@ -185,19 +195,32 @@ contract BrokerImp is MultiOwnable, DestroyableMultiOwner {
 		uint256 beneficiaryPart = hundred.sub(commission);
 		uint256 total = (_value.div(beneficiaryPart)).mul(hundred);
 		uint256 brokerCommission = total.sub(_value);
+		if (!ethSent[_beneficiary]) {
+			_beneficiary.transfer(ethReward);
+			ethSent[_beneficiary] = true;
+		}
 		return (
-		token.transferFrom(pool, _beneficiary, _value) &&
-		token.transferFrom(pool, broker, brokerCommission)
+		token.transferFrom(pool, broker, brokerCommission) &&
+		token.transferFrom(pool, _beneficiary, _value)
 		);
 	}
 	
 	/**
-	 * @dev Allows the owner to withdraw the balance of the tokens.
+	 * @dev Allows the owner to change the commission of the reward.
 	 * @param _commission The percentage of the commission 0-100
 	 */
 	function changeCommission(uint256 _commission) public onlyOwner {
 		emit CommissionChanged(commission, _commission);
 		commission = _commission;
+	}
+	
+	/**
+	 * @dev Allows the owner to withdraw the balance of the tokens.
+	 * @param _ethReward The eth reward to send to the beneficiary in wei
+	 */
+	function changeEthReward(uint256 _ethReward) public onlyOwner {
+		emit EthRewardChanged(ethReward, _ethReward);
+		ethReward = _ethReward;
 	}
 	
 	/**
