3715ae23984b19bb7fb4f1ec75735bda2613ce60
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 20:37:20 +0700

    add contract BitcoinPro

--- Bitcoinpro.sol
+++ Bitcoinpro.sol
@@ -1,102 +1,145 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinPro at 0xe57297258717ec66749fcfb4bf23869e252b03c3
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinPro at 0x931dc02a9819de68eafdac9767b38cf9d6ff3f28
 */
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.24;
 
-contract ERC20 {
-     function totalSupply() constant returns (uint256 totalSupply);
-     function balanceOf(address _owner) constant returns (uint256 balance);
-     function transfer(address _to, uint256 _value) returns (bool success);
-     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-     function approve(address _spender, uint256 _value) returns (bool success);
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
- }
-  
-  contract BitcoinPro is ERC20 {
-     string public constant symbol = "BTCP";
-     string public constant name = "BTCP";
-     uint8 public constant decimals = 8;
-     uint256 _totalSupply = 2000000 * 10**8;
-     
+contract Token {
 
-     address public owner;
-  
-     mapping(address => uint256) balances;
-  
-     mapping(address => mapping (address => uint256)) allowed;
-     
-  
-     function BitcoinPro() {
-         owner = 0xb4a36cc1971bd467d618ee5d7060f9d73e9bd12c;
-         balances[owner] = 2000000 * 10**8;
-     }
-     
-     modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-     
-     
-     function distributeBTR(address[] addresses) onlyOwner {
-         for (uint i = 0; i < addresses.length; i++) {
-             balances[owner] -= 2000 * 10**8;
-             balances[addresses[i]] += 2000 * 10**8;
-             Transfer(owner, addresses[i], 2000 * 10**8);
-         }
-     }
-     
-  
-     function totalSupply() constant returns (uint256 totalSupply) {
-         totalSupply = _totalSupply;
-     }
-  
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
 
-     function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-     }
- 
-     function transfer(address _to, uint256 _amount) returns (bool success) {
-         if (balances[msg.sender] >= _amount 
-            && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(msg.sender, _to, _amount);
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
             return true;
-         } else {
-             return false;
-         }
-     }
-     
-     
-     function transferFrom(
-         address _from,
-         address _to,
-         uint256 _amount
-     ) returns (bool success) {
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(_from, _to, _amount);
-             return true;
-         } else {
-            return false;
-         }
-     }
- 
-     function approve(address _spender, uint256 _amount) returns (bool success) {
-         allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-         return true;
-     }
-  
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-         return allowed[_owner][_spender];
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+contract BitcoinPro is StandardToken { // CHANGE THIS. Update the contract name.
+
+    /* Public variables of the token */
+
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   
+    uint8 public decimals;                
+    string public symbol;                 
+    string public version = 'H1.0';
+    uint256 public unitsOneEthCanBuy;     
+    uint256 public totalEthInWei;         
+    address public fundsWallet;           
+
+    // This is a constructor function
+    // which means the following function name has to match the contract name declared above
+    function BitcoinPro() {
+        balances[msg.sender] = 30000000000000000000000000000;      
+        totalSupply = 30000000000000000000000000000;
+        name = "BitcoinPro";                                   
+        decimals = 18;                                        
+        symbol = "BTCP";                                       
+        unitsOneEthCanBuy = 10000000;
+        fundsWallet = msg.sender;                             
+    }
+
+    function() public payable{
+        totalEthInWei = totalEthInWei + msg.value;
+        uint256 amount = msg.value * unitsOneEthCanBuy;
+        require(balances[fundsWallet] >= amount);
+
+        balances[fundsWallet] = balances[fundsWallet] - amount;
+        balances[msg.sender] = balances[msg.sender] + amount;
+
+        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
+
+        //Transfer ether to fundsWallet
+        fundsWallet.transfer(msg.value);                             
+    }
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        return true;
     }
 }
