b1187fca3537b6c34cf18623927ebee110942ce4
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 02:31:23 +0700

    add contract CpcToken

--- CPCToken.sol
+++ CPCToken.sol
@@ -1,73 +1,88 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CPCToken at 0xce156012cf03684b66c5ddf9471b458e40c07d68
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CpcToken at 0x8fdcc30eda7e94f1c12ce0280df6cd531e8365c5
 */
-contract EIP20Interface {    
+pragma solidity ^0.4.18;
+contract CpcToken{
+	mapping (address => uint256) balances;
+	address public owner;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+	// total amount of tokens
     uint256 public totalSupply;
-    function balanceOf(address _owner) public view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value); 
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-contract CPCToken is EIP20Interface {
-    uint256 constant private MAX_UINT256 = 2**256 - 1;
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;   
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;          
-    function CPCToken(
-        uint256 _initialAmount,
-        string _tokenName,
-        uint8 _decimalUnits,
-        string _tokenSymbol
-    ) public {
-        balances[msg.sender] = _initialAmount;               
-        totalSupply = _initialAmount;                        
-        name = _tokenName;                                   
-        decimals = _decimalUnits;                            
-        symbol = _tokenSymbol;                              
-}
+	// `allowed` tracks any extra transfer rights as in all ERC20 tokens
+    mapping (address => mapping (address => uint256)) allowed;
+    constructor () public { 
+        owner = msg.sender;                                       // Set owner of contract 
+        name = "CpcToken";                                        // Set the name for display purposes
+        symbol = "CPCT";                                          // Set the symbol for display purposes
+        decimals = 18;                                            // Amount of decimals for display purposes
+		totalSupply = 2000000000000000000000000000;               // Total supply
+		balances[owner] = totalSupply;                            // Set owner balance equal totalsupply 
+    }
+	
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+		 return balances[_owner];
+	}
 
-function rename(string newTokenName,string newSymbol) public returns(bool success)
-{
-  name = newTokenName;
-  symbol = newSymbol;  
-  return true;
-}
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
+	    require(_value > 0 );                                      // Check send token value > 0;
+		require(balances[msg.sender] >= _value);                   // Check if the sender has enough
+        require(balances[_to] + _value > balances[_to]);           // Check for overflows											
+		balances[msg.sender] -= _value;                            // Subtract from the sender
+		balances[_to] += _value;                                   // Add the same to the recipient                       
+		 
+		emit Transfer(msg.sender, _to, _value); 			       // Notify anyone listening that this transfer took place
+		return true;      
+	}
 
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from] >= _value && allowance >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
-        }
-        Transfer(_from, _to, _value);
+	  
+	    require(balances[_from] >= _value);                 // Check if the sender has enough
+        require(balances[_to] + _value >= balances[_to]);   // Check for overflows
+        require(_value <= allowed[_from][msg.sender]);      // Check allowance
+        balances[_from] -= _value;                         // Subtract from the sender
+        balances[_to] += _value;                           // Add the same to the recipient
+        allowed[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
-    }
+	}
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
+		require(balances[msg.sender] >= _value);
+		allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+		return true;
+	
+	}
+	
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+	}
+	
+	/* This unnamed function is called whenever someone tries to send ether to it */
+    function () private {
+        revert();     // Prevents accidental sending of ether
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }   
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
