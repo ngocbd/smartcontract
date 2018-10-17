92ad007e09e288ade3e2ac664fde6d4d0b2508df
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:56:11 +0700

    add contract TeamTokenLock

--- teamTokenLock.sol
+++ teamTokenLock.sol
@@ -1,115 +1,126 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamTokenLock at 0x73e86ee0adb893b77c932937883c78fd08f4af31
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TeamTokenLock at 0x9c109e620c35475ab797706bd8a57d621d1a80d7
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.16;
+
 
 contract ERC20 {
-
-    uint256 public totalSupply;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-
-    function allowance(address owner, address spender) public view returns (uint256);
-    function approve(address spender, uint256 value) public returns (bool);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-
+    function balanceOf(address tokenOwner) public constant returns (uint256 balance);
+    function transfer(address to, uint256 tokens) public returns (bool success);
 }
 
-contract Ownable {
-    address public owner;
 
-    event OwnerChanged(address oldOwner, address newOwner);
-
-    function Ownable() public {
-        owner = msg.sender;
-    }
+contract owned {
+    function owned() public { owner = msg.sender; }
+    address owner;
 
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        require(newOwner != owner && newOwner != address(0x0));
-        address oldOwner = owner;
-        owner = newOwner;
-        OwnerChanged(oldOwner, newOwner);
-    }
 }
 
-contract TeamTokenLock is Ownable {
 
-    ERC20 public token;
+contract TeamTokenLock is owned {
 
-    // address where receives funds when unlock period
-    address public beneficiary;
+    address _tokenAddress = 0xAF815e887b039Fc06a8ddDcC7Ec4f57757616Cd2;
+    uint256 _startTime = 1534723200;  // Aug 20, 2018
+    uint256 _teamTokenAmount = 1600000000e18;  // 1.6 Billion
+    uint256 _totalWithdrawAmount = 0;
 
-    uint public startTime = 1513728000;  // About time at 2018-1-1
-    uint public firstLockTime = 365 days;
-    uint public secondLockTime = 2 * 365 days;
-
-    uint public firstLockAmount = 120000000 * (10 ** 18);
-    uint public secondLockAmount = 120000000 * (10 ** 18);
-
-    modifier onlyOfficial {
-        require(msg.sender == owner || msg.sender == beneficiary);
-        _;
+    function getAllowedAmountByTeam() public constant returns (uint256 amount) {
+        if (now >= _startTime + (731 days)) {
+            // Aug 20, 2020
+            return _teamTokenAmount;
+        } else if (now >= _startTime + (700 days)) {
+            // July 20, 2020
+            return _teamTokenAmount / uint(24) * 23;
+        } else if (now >= _startTime + (670 days)) {
+            // June 20, 2020
+            return _teamTokenAmount / uint(24) * 22;
+        } else if (now >= _startTime + (639 days)) {
+            // May 20, 2020
+            return _teamTokenAmount / uint(24) * 21;
+        } else if (now >= _startTime + (609 days)) {
+            // April 20, 2020
+            return _teamTokenAmount / uint(24) * 20;
+        } else if (now >= _startTime + (578 days)) {
+            // March 20, 2020
+            return _teamTokenAmount / uint(24) * 19;
+        } else if (now >= _startTime + (549 days)) {
+            // Febuary 20, 2020
+            return _teamTokenAmount / uint(24) * 18;
+        } else if (now >= _startTime + (518 days)) {
+            // January 20, 2020
+            return _teamTokenAmount / uint(24) * 17;
+        } else if (now >= _startTime + (487 days)) {
+            // December 20, 2019
+            return _teamTokenAmount / uint(24) * 16;
+        } else if (now >= _startTime + (457 days)) {
+            // November 20, 2019
+            return _teamTokenAmount / uint(24) * 15;
+        } else if (now >= _startTime + (426 days)) {
+            // October 20, 2019
+            return _teamTokenAmount / uint(24) * 14;
+        } else if (now >= _startTime + (396 days)) {
+            // September 20, 2019
+            return _teamTokenAmount / uint(24) * 13;
+        } else if (now >= _startTime + (365 days)) {
+            // August 20, 2019
+            return _teamTokenAmount / uint(24) * 12;
+        } else if (now >= _startTime + (334 days)) {
+            // July 20, 2019
+            return _teamTokenAmount / uint(24) * 11;
+        } else if (now >= _startTime + (304 days)) {
+            // June 20, 2019
+            return _teamTokenAmount / uint(24) * 10;
+        } else if (now >= _startTime + (273 days)) {
+            // May 20, 2019
+            return _teamTokenAmount / uint(24) * 9;
+        } else if (now >= _startTime + (243 days)) {
+            // April 20, 2019
+            return _teamTokenAmount / uint(24) * 8;
+        } else if (now >= _startTime + (212 days)) {
+            // March 20, 2019
+            return _teamTokenAmount / uint(24) * 7;
+        } else if (now >= _startTime + (184 days)) {
+            // Febuary 20, 2019
+            return _teamTokenAmount / uint(24) * 6;
+        } else if (now >= _startTime + (153 days)) {
+            // January 20, 2019
+            return _teamTokenAmount / uint(24) * 5;
+        } else if (now >= _startTime + (122 days)) {
+            // December 20, 2018
+            return _teamTokenAmount / uint(24) * 4;
+        } else if (now >= _startTime + (92 days)) {
+            // Nobember 20, 2018
+            return _teamTokenAmount / uint(24) * 3;
+        } else if (now >= _startTime + (61 days)) {
+            // October 20, 2018
+            return _teamTokenAmount / uint(24) * 2;
+        } else if (now >= _startTime + (31 days)) {
+            // September 20, 2018
+            return _teamTokenAmount / uint(24);
+        } else {
+            return 0;
+        }
     }
 
-    modifier firstLockTimeEnd {
-        require(isFirstLockTimeEnd());
-        _;
+    function withdrawByTeam(address toAddress, uint256 amount) public onlyOwner {
+        require(now >= _startTime);
+
+        uint256 allowedAmount = getAllowedAmountByTeam();
+
+        require(amount + _totalWithdrawAmount <= allowedAmount);
+
+        _totalWithdrawAmount += amount;
+
+        ERC20(_tokenAddress).transfer(toAddress, amount);
     }
 
-    modifier secondLockTimeEnd {
-        require(isSecondLockTimeEnd());
-        _;
-    }
+    function withdrawByFoundation(address toAddress, uint256 amount) public onlyOwner {
+        require(now >= _startTime + (731 days));
 
-    function TeamTokenLock(address _beneficiary, address _token) public {
-        require(_beneficiary != address(0));
-        require(_token != address(0));
-
-        beneficiary = _beneficiary;
-        token = ERC20(_token);
-    }
-
-    function getTokenBalance() public view returns(uint) {
-        return token.balanceOf(address(this));
-    }
-
-    function isFirstLockTimeEnd() public view returns(bool) {
-        return now > startTime + firstLockTime;
-    }
-
-    function isSecondLockTimeEnd() public view returns(bool) {
-        return now > startTime + secondLockTime;
-    }
-
-    function unlockFirstTokens() public onlyOfficial firstLockTimeEnd {
-        require(firstLockAmount > 0);
-
-        uint unlockAmount = firstLockAmount < getTokenBalance() ? firstLockAmount : getTokenBalance();
-        require(unlockAmount <= firstLockAmount);
-        firstLockAmount = firstLockAmount - unlockAmount;
-        require(token.transfer(beneficiary, unlockAmount));
-    }
-
-    function unlockSecondTokens() public onlyOfficial secondLockTimeEnd {
-        require(secondLockAmount > 0);
-
-        uint unlockAmount = secondLockAmount < getTokenBalance() ? secondLockAmount : getTokenBalance();
-        require(unlockAmount <= secondLockAmount);
-        secondLockAmount = secondLockAmount - unlockAmount;
-        require(token.transfer(beneficiary, unlockAmount));
-    }
-
-    function changeBeneficiary(address _beneficiary) public onlyOwner {
-        require(_beneficiary != address(0));
-        beneficiary = _beneficiary;
+        ERC20(_tokenAddress).transfer(toAddress, amount);
     }
 }
