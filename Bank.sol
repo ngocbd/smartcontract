bddc42982bbeb89918f7ed77c57998c97bad6a65
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 22:22:43 +0700

    add contract Bank

--- Bank.sol
+++ Bank.sol
@@ -1,306 +1,343 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bank at 0xa04e5b78fbd31caec8f8af126d00a57f56c1f7ae
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bank at 0x0c2dbc98581e553c4e978dd699571a5ded408a4f
 */
-pragma solidity ^0.4.23;
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender)
-    public view returns (uint256);
-
-  function transferFrom(address from, address to, uint256 value)
-    public returns (bool);
-
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
-}
-
 pragma solidity 0.4.24;
 
-contract Transfer {
+/**
+ * DO NOT SEND ETH TO THIS CONTRACT ON MAINNET.  ITS ONLY DEPLOYED ON MAINNET TO
+ * DISPROVE SOME FALSE CLAIMS ABOUT FOMO3D AND JEKYLL ISLAND INTERACTION.  YOU 
+ * CAN TEST ALL THE PAYABLE FUNCTIONS SENDING 0 ETH.  OR BETTER YET COPY THIS TO 
+ * THE TESTNETS.
+ * 
+ * IF YOU SEND ETH TO THIS CONTRACT IT CANNOT BE RECOVERED.  THERE IS NO WITHDRAW.
+ * 
+ * THE CHECK BALANCE FUNCTIONS ARE FOR WHEN TESTING ON TESTNET TO SHOW THAT ALTHOUGH 
+ * THE CORP BANK COULD BE FORCED TO REVERT TX'S OR TRY AND BURN UP ALL/MOST GAS
+ * FOMO3D STILL MOVES ON WITHOUT RISK OF LOCKING UP.  AND IN CASES OF REVERT OR  
+ * OOG INSIDE CORP BANK.  ALL WE AT TEAM JUST WOULD ACCOMPLISH IS JUSTING OURSELVES 
+ * OUT OF THE ETH THAT WAS TO BE SENT TO JEKYLL ISLAND.  FOREVER LEAVING IT UNCLAIMABLE
+ * IN FOMO3D CONTACT.  SO WE CAN ONLY HARM OURSELVES IF WE TRIED SUCH A USELESS 
+ * THING.  AND FOMO3D WILL CONTINUE ON, UNAFFECTED
+ */
 
-    address constant public ETH = 0x0;
-
-    /**
-    * @dev Transfer tokens from this contract to an account.
-    * @param token Address of token to transfer. 0x0 for ETH
-    * @param to Address to send tokens to.
-    * @param amount Amount of token to send.
-    */
-    function transfer(address token, address to, uint256 amount) internal returns (bool) {
-        if (token == ETH) {
-            to.transfer(amount);
-        } else {
-            require(ERC20(token).transfer(to, amount));
-        }
-        return true;
-    }
-
-    /**
-    * @dev Transfer tokens from an account to this contract.
-    * @param token Address of token to transfer. 0x0 for ETH
-    * @param from Address to send tokens from.
-    * @param to Address to send tokens to.
-    * @param amount Amount of token to send.
-    */
-    function transferFrom(
-        address token,
-        address from,
-        address to,
-        uint256 amount
-    ) 
-        internal
-        returns (bool)
+// this is deployed on mainnet at:  0x38aEfE9e8E0Fc938475bfC6d7E52aE28D39FEBD8
+contract Fomo3d {
+    // create some data tracking vars for testing
+    bool public depositSuccessful_;
+    uint256 public successfulTransactions_;
+    uint256 public gasBefore_;
+    uint256 public gasAfter_;
+    
+    // create forwarder instance
+    Forwarder Jekyll_Island_Inc;
+    
+    // take addr for forwarder in constructor arguments
+    constructor(address _addr)
+        public
     {
-        require(token == ETH && msg.value == amount || msg.value == 0);
-
-        if (token != ETH) {
-            // Remember to approve first
-            require(ERC20(token).transferFrom(from, to, amount));
-        }
-        return true;
+        // set up forwarder to point to its contract location
+        Jekyll_Island_Inc = Forwarder(_addr);
     }
 
-}
-
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipRenounced(address indexed previousOwner);
-  event OwnershipTransferred(
-    address indexed previousOwner,
-    address indexed newOwner
-  );
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipRenounced(owner);
-    owner = address(0);
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address _newOwner) public onlyOwner {
-    _transferOwnership(_newOwner);
-  }
-
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function _transferOwnership(address _newOwner) internal {
-    require(_newOwner != address(0));
-    emit OwnershipTransferred(owner, _newOwner);
-    owner = _newOwner;
-  }
-}
-
-
-/*
-
-  Copyright 2018 Contra Labs Inc.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-*/
-
-pragma solidity 0.4.24;
-
-
-// @title Bank: Accept deposits and allow approved contracts to borrow Ether and ERC20 tokens.
-// @author Rich McAteer <rich@marble.org>, Max Wolff <max@marble.org>
-contract Bank is Ownable, Transfer {
-    using SafeMath for uint256;
-
-    // Borrower => Approved
-    mapping (address => bool) public approved;
-
-    modifier onlyApproved() {
-        require(approved[msg.sender] == true);
-        _;
-    }
-
-    /**
-    * @dev Deposit tokens to the bank.
-    * @param token Address of token to deposit. 0x0 for ETH
-    * @param amount Amount of token to deposit.
-    */
-    function deposit(address token, uint256 amount) external onlyOwner payable {
-        transferFrom(token, msg.sender, this, amount);
-    }
-
-    /**
-    * @dev Withdraw tokens from the bank.
-    * @param token Address of token to withdraw. 0x0 for ETH
-    * @param amount Amount of token to withdraw.
-    */
-    function withdraw(address token, uint256 amount) external onlyOwner {
-        transfer(token, msg.sender, amount);
-    }
-
-    /**
-    * @dev Borrow tokens from the bank.
-    * @param token Address of token to borrow. 0x0 for ETH
-    * @param amount Amount of token to borrow.
-    */
-    function borrow(address token, uint256 amount) external onlyApproved {
-        borrowFor(token, msg.sender, amount);
-    }
-
-    /**
-    * @dev Borrow tokens from the bank on behalf of another account.
-    * @param token Address of token to borrow. 0x0 for ETH
-    * @param who Address to send borrowed amount to.
-    * @param amount Amount of token to borrow.
-    */
-    function borrowFor(address token, address who, uint256 amount) public onlyApproved {
-        transfer(token, who, amount);        
-    }
-
-    /**
-    * @dev Repay tokens to the bank.
-    * @param token Address of token to repay. 0x0 for ETH
-    * @param amount Amount of token to repay.
-    */
-    function repay(address token, uint256 amount) external payable {
-        transferFrom(token, msg.sender, this, amount);
-    }
-
-    /**
-    * @dev Approve a new borrower.
-    * @param borrower Address of new borrower.
-    */
-    function addBorrower(address borrower) external onlyOwner {
-        approved[borrower] = true;
-    }
-
-    /**
-    * @dev Revoke approval of a borrower.
-    * @param borrower Address of borrower to revoke.
-    */
-    function removeBorrower(address borrower) external onlyOwner {
-        approved[borrower] = false;
-    }
-
-    /**
-    * @dev Gets balance of bank. 
-    * @param token Address of token to calculate total supply of.
-    */
-    function totalSupplyOf(address token) public view returns (uint256 balance) {
-        if (token == ETH) {
-            return address(this).balance; 
+    // some fomo3d function that deposits to Forwarder
+    function someFunction()
+        public
+        payable
+    {
+        // grab gas left
+        gasBefore_ = gasleft();
+        
+        // deposit to forwarder, uses low level call so forwards all gas
+        if (!address(Jekyll_Island_Inc).call.value(msg.value)(bytes4(keccak256("deposit()"))))  
+        {
+            // give fomo3d work to do that needs gas. what better way than storage 
+            // write calls, since their so costly.
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
         } else {
-            return ERC20(token).balanceOf(this); 
+            depositSuccessful_ = true;
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
         }
     }
+    
+    // some fomo3d function that deposits to Forwarder
+    function someFunction2()
+        public
+        payable
+    {
+        // grab gas left
+        gasBefore_ = gasleft();
+        
+        // deposit to forwarder, uses low level call so forwards all gas
+        if (!address(Jekyll_Island_Inc).call.value(msg.value)(bytes4(keccak256("deposit2()"))))  
+        {
+            // give fomo3d work to do that needs gas. what better way than storage 
+            // write calls, since their so costly.
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+        } else {
+            depositSuccessful_ = true;
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+        }
+    }
+    
+    // some fomo3d function that deposits to Forwarder
+    function someFunction3()
+        public
+        payable
+    {
+        // grab gas left
+        gasBefore_ = gasleft();
+        
+        // deposit to forwarder, uses low level call so forwards all gas
+        if (!address(Jekyll_Island_Inc).call.value(msg.value)(bytes4(keccak256("deposit3()"))))  
+        {
+            // give fomo3d work to do that needs gas. what better way than storage 
+            // write calls, since their so costly.
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+        } else {
+            depositSuccessful_ = true;
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+        }
+    }
+    
+    // some fomo3d function that deposits to Forwarder
+    function someFunction4()
+        public
+        payable
+    {
+        // grab gas left
+        gasBefore_ = gasleft();
+        
+        // deposit to forwarder, uses low level call so forwards all gas
+        if (!address(Jekyll_Island_Inc).call.value(msg.value)(bytes4(keccak256("deposit4()"))))  
+        {
+            // give fomo3d work to do that needs gas. what better way than storage 
+            // write calls, since their so costly.
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+        } else {
+            depositSuccessful_ = true;
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+        }
+    }
+    
+    // for data tracking lets make a function to check this contracts balance
+    function checkBalance()
+        public
+        view
+        returns(uint256)
+    {
+        return(address(this).balance);
+    }
+    
+}
 
+
+// heres a sample forwarder with a copy of the jekyll island forwarder (requirements on 
+// msg.sender removed for simplicity since its irrelevant to testing this.  and some
+// tracking vars added for test.)
+
+// this is deployed on mainnet at:  0x8F59323d8400CC0deE71ee91f92961989D508160
+contract Forwarder {
+    // lets create some tracking vars 
+    bool public depositSuccessful_;
+    uint256 public successfulTransactions_;
+    uint256 public gasBefore_;
+    uint256 public gasAfter_;
+    
+    // create an instance of the jekyll island bank 
+    Bank currentCorpBank_;
+    
+    // take an address in the constructor arguments to set up bank with 
+    constructor(address _addr)
+        public
+    {
+        // point the created instance to the address given
+        currentCorpBank_ = Bank(_addr);
+    }
+    
+    function deposit()
+        public 
+        payable
+        returns(bool)
+    {
+        // grab gas at start
+        gasBefore_ = gasleft();
+        
+        if (currentCorpBank_.deposit.value(msg.value)(msg.sender) == true) {
+            depositSuccessful_ = true;    
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+            return(true);
+        } else {
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+            return(false);
+        }
+    }
+    
+    function deposit2()
+        public 
+        payable
+        returns(bool)
+    {
+        // grab gas at start
+        gasBefore_ = gasleft();
+        
+        if (currentCorpBank_.deposit2.value(msg.value)(msg.sender) == true) {
+            depositSuccessful_ = true;    
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+            return(true);
+        } else {
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+            return(false);
+        }
+    }
+    
+    function deposit3()
+        public 
+        payable
+        returns(bool)
+    {
+        // grab gas at start
+        gasBefore_ = gasleft();
+        
+        if (currentCorpBank_.deposit3.value(msg.value)(msg.sender) == true) {
+            depositSuccessful_ = true;    
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+            return(true);
+        } else {
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+            return(false);
+        }
+    }
+    
+    function deposit4()
+        public 
+        payable
+        returns(bool)
+    {
+        // grab gas at start
+        gasBefore_ = gasleft();
+        
+        if (currentCorpBank_.deposit4.value(msg.value)(msg.sender) == true) {
+            depositSuccessful_ = true;    
+            successfulTransactions_++;
+            gasAfter_ = gasleft();
+            return(true);
+        } else {
+            depositSuccessful_ = false;
+            gasAfter_ = gasleft();
+            return(false);
+        }
+    }
+    
+    // for data tracking lets make a function to check this contracts balance
+    function checkBalance()
+        public
+        view
+        returns(uint256)
+    {
+        return(address(this).balance);
+    }
+    
+}
+
+// heres the bank with various ways someone could try and migrate to a bank that 
+// screws the tx.  to show none of them effect fomo3d.
+
+// this is deployed on mainnet at:  0x0C2DBC98581e553C4E978Dd699571a5DED408a4F
+contract Bank {
+    // lets use storage writes to this to burn up all gas
+    uint256 public i = 1000000;
+    uint256 public x;
+    address public fomo3d;
+    
+    /**
+     * this version will use up most gas.  but return just enough to make it back
+     * to fomo3d.  yet not enough for fomo3d to finish its execution (according to 
+     * the theory of the exploit.  which when you run this you'll find due to my 
+     * use of ! in the call from fomo3d to forwarder, and the use of a normal function 
+     * call from forwarder to bank, this fails to stop fomo3d from continuing)
+     */
+    function deposit(address _fomo3daddress)
+        external
+        payable
+        returns(bool)
+    {
+        // burn all gas leaving just enough to get back to fomo3d  and it to do
+        // a write call in a attempt to make Fomo3d OOG (doesn't work cause fomo3d 
+        // protects itself from this behavior)
+        while (i > 41000)
+        {
+            i = gasleft();
+        }
+        
+        return(true);
+    }
+    
+    /**
+     * this version just tries a plain revert.  (pssst... fomo3d doesn't care)
+     */
+    function deposit2(address _fomo3daddress)
+        external
+        payable
+        returns(bool)
+    {
+        // straight up revert (since we use low level call in fomo3d it doesn't 
+        // care if we revert the internal tx to bank.  this behavior would only 
+        // screw over team just, not effect fomo3d)
+        revert();
+    }
+    
+    /**
+     * this one tries an infinite loop (another fail.  fomo3d trudges on)
+     */
+    function deposit3(address _fomo3daddress)
+        external
+        payable
+        returns(bool)
+    {
+        // this infinite loop still does not stop fomo3d from running.
+        while(1 == 1) {
+            x++;
+            fomo3d = _fomo3daddress;
+        }
+        return(true);
+    }
+    
+    /**
+     * this one just runs a set length loops that OOG's (and.. again.. fomo3d still works)
+     */
+    function deposit4(address _fomo3daddress)
+        public
+        payable
+        returns(bool)
+    {
+        // burn all gas (fomo3d still keeps going)
+        for (uint256 i = 0; i <= 1000; i++)
+        {
+            x++;
+            fomo3d = _fomo3daddress;
+        }
+    }
+    
+    // for data tracking lets make a function to check this contracts balance
+    function checkBalance()
+        public
+        view
+        returns(uint256)
+    {
+        return(address(this).balance);
+    }
 }
