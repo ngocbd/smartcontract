8912db91a5740ba29ebbc5d4c0aedb8b0b66447b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 09:12:13 +0700

    add contract DetailedToken

--- DetailedToken.sol
+++ DetailedToken.sol
@@ -1,10 +1,117 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DetailedToken at 0x83042a265b420cdad43ffe4a74546d0f7dc3c99e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DetailedToken at 0x6629e2fd00b575199fbe656a514c6a1378eb3671
 */
-pragma solidity >0.4.23 <0.5.0;
+pragma solidity ^0.4.23;
+
+contract ERC20Basic {
+  // events
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  // public functions
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address addr) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+}
+
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  // public variables
+  string public name;
+  string public symbol;
+  uint8 public decimals = 18;
+
+  // internal variables
+  uint256 _totalSupply;
+  mapping(address => uint256) _balances;
+
+  // events
+
+  // public functions
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address addr) public view returns (uint256 balance) {
+    return _balances[addr];
+  }
+
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(to != address(0));
+    require(value <= _balances[msg.sender]);
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  // internal functions
+
+}
+
+contract ERC20 is ERC20Basic {
+  // events
+  event Approval(address indexed owner, address indexed agent, uint256 value);
+
+  // public functions
+  function allowance(address owner, address agent) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address agent, uint256 value) public returns (bool);
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+  // public variables
+
+  // internal variables
+  mapping (address => mapping (address => uint256)) _allowances;
+
+  // events
+
+  // public functions
+  function transferFrom(address from, address to, uint256 value) public returns (bool) {
+    require(to != address(0));
+    require(value <= _balances[from]);
+    require(value <= _allowances[from][msg.sender]);
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowances[from][msg.sender] = _allowances[from][msg.sender].sub(value);
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+  function approve(address agent, uint256 value) public returns (bool) {
+    _allowances[msg.sender][agent] = value;
+    emit Approval(msg.sender, agent, value);
+    return true;
+  }
+
+  function allowance(address owner, address agent) public view returns (uint256) {
+    return _allowances[owner][agent];
+  }
+
+  function increaseApproval(address agent, uint value) public returns (bool) {
+    _allowances[msg.sender][agent] = _allowances[msg.sender][agent].add(value);
+    emit Approval(msg.sender, agent, _allowances[msg.sender][agent]);
+    return true;
+  }
+
+  function decreaseApproval(address agent, uint value) public returns (bool) {
+    uint allowanceValue = _allowances[msg.sender][agent];
+    if (value > allowanceValue) {
+      _allowances[msg.sender][agent] = 0;
+    } else {
+      _allowances[msg.sender][agent] = allowanceValue.sub(value);
+    }
+    emit Approval(msg.sender, agent, _allowances[msg.sender][agent]);
+    return true;
+  }
+  // internal functions
+}
 
 library SafeMath {
-
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
       return 0;
@@ -15,7 +122,9 @@ library SafeMath {
   }
 
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
@@ -31,117 +140,26 @@ library SafeMath {
   }
 }
 
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
-  mapping(address => uint256) balances;
-  uint256 totalSupply_;
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
-
-
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-}
-
-contract StandardToken is ERC20, BasicToken {
-  mapping (address => mapping (address => uint256)) internal allowed;
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
-  }
-
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-    }
-    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-}
-
-/**
- * Detailed
- */
 contract DetailedToken is StandardToken {
-    string public constant  name = "zen and cosmos repair";
-    string public constant  symbol = "ZEN";
-    uint8 public constant decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
-    address public holder = 0x49A1c9bB3D7B2AcA989d12A964C842B529813841;
-    address public owner = 0x49A1c9bB3D7B2AcA989d12A964C842B529813841;
-    
-    string public statement = "zen and cosmos repair";
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can do this.");
-        _;
-    }
-    event TransferOwner(address newOwner, address lastOwner);
-    
-    function changeStatement(string newStatement) public onlyOwner returns (bool) {
-        statement = newStatement;
-    }
-    
-    function transferOwner(address newOwner) public onlyOwner returns (bool) {
-        owner = newOwner;
-        emit TransferOwner(newOwner, msg.sender);
-    }
-    
-    constructor() public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[holder] = INITIAL_SUPPLY;
-        emit Transfer(0x0, holder, INITIAL_SUPPLY);
-    }
+  // public variables
+  string public name = "Demo Coin Token";
+  string public symbol = "DEMO";
+  uint8 public decimals = 18;
+
+  // public functions
+  constructor (
+    string tokenName,
+    string tokenSymbol,
+    uint8 tokenDecimals,
+    uint256 tokenTotal,
+    address owner
+  ) public {
+    name = tokenName;
+    symbol = tokenSymbol;
+    decimals = tokenDecimals;
+
+    _totalSupply = tokenTotal * (10 ** uint256(decimals));
+    _balances[owner] = _totalSupply;
+    emit Transfer(0x0, owner, _totalSupply);
+  }
 }
