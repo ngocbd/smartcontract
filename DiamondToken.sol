19f2c61b85ffb4d268e15b10687f14c0bffd6848
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:14:05 +0700

    add contract DiamondToken

--- DiamondToken.sol
+++ DiamondToken.sol
@@ -1,15 +1,9 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DiamondToken at 0x62fe44f0911e87c16eb1eae49413afec058e78a2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DiamondToken at 0x5ec61057ca84828cb3e436dc692ad8ac75c4ed94
 */
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.10;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
-
- 
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
       return 0;
@@ -19,7 +13,6 @@ library SafeMath {
     return c;
   }
 
-  
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
@@ -27,7 +20,6 @@ library SafeMath {
     return c;
   }
 
-  
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
@@ -40,236 +32,141 @@ library SafeMath {
   }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = 0x222486a578d57CfFdD8b69E5b12BAC692c31a743;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
+interface ERC20 {
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
   function allowance(address owner, address spender) public view returns (uint256);
   function transferFrom(address from, address to, uint256 value) public returns (bool);
   function approve(address spender, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances. 
- */
-contract BasicToken is ERC20Basic, Ownable {
-  using SafeMath for uint256;
+interface ERC223 {
+    function transfer(address to, uint value, bytes data) public;
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
 
-  mapping(address => uint256) balances;
+contract ERC223ReceivingContract { 
+    function tokenFallback(address _from, uint _value, bytes _data) public;
+}
 
- uint256 totalSupply_;
+contract DiamondToken is ERC20, ERC223 {
+  using SafeMath for uint;
+     
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint256 internal _totalSupply;
 
-  /**
-  * @dev total number of tokens in existence
-  */
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
 
+    function DiamondToken(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
+        _symbol = symbol;
+        _name = name;
+        _decimals = decimals;
+        _totalSupply = totalSupply;
+        balances[msg.sender] = totalSupply;
+    }
 
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
- function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
+    function name()
+        public
+        view
+        returns (string) {
+        return _name;
+    }
 
-    // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
+    function symbol()
+        public
+        view
+        returns (string) {
+        return _symbol;
+    }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of. 
-  * @return An uint256 representing the amount owned by the passed address.
-  */
+    function decimals()
+        public
+        view
+        returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply()
+        public
+        view
+        returns (uint256) {
+        return _totalSupply;
+    }
+
+   function transfer(address _to, uint256 _value) public returns (bool) {
+     require(_to != address(0));
+     require(_value <= balances[msg.sender]);
+     balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+     balances[_to] = SafeMath.add(balances[_to], _value);
+     Transfer(msg.sender, _to, _value);
+     return true;
+   }
+
   function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
-  }
+   }
 
-}
-
-
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-contract StandardToken is ERC20, BasicToken {
-
-  mapping (address => mapping (address => uint256)) internal allowed;
-
-
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amount of tokens to be transfered
-   */
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
+     require(_value <= balances[_from]);
+     require(_value <= allowed[_from][msg.sender]);
 
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    balances[_from] = SafeMath.sub(balances[_from], _value);
+     balances[_to] = SafeMath.add(balances[_to], _value);
+     allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
     Transfer(_from, _to, _value);
-    return true;
-  }
+     return true;
+   }
 
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
- function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
+   function approve(address _spender, uint256 _value) public returns (bool) {
+     allowed[msg.sender][_spender] = _value;
+     Approval(msg.sender, _spender, _value);
+     return true;
+   }
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-   function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
-  }
-  
-  
-  /**
-   * @dev Function to prevent eth transfers to this contract
-    */
-    function() public payable {
-	    revert();
-	}
-	
-	
-   /**
-   * @dev  Owner can transfer out any accidentally sent ERC20 tokens
-   */
- function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-        return BasicToken(tokenAddress).transfer(owner, tokens);
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+     return allowed[_owner][_spender];
+   }
+
+   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+     allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
+     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+     return true;
+   }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+     uint oldValue = allowed[msg.sender][_spender];
+     if (_subtractedValue > oldValue) {
+       allowed[msg.sender][_spender] = 0;
+     } else {
+       allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
     }
-
-	
-  /**
-    * @dev Transfer the specified amounts of tokens to the specified addresses.
-    * @dev Be aware that there is no check for duplicate recipients.
-    *
-    * @param _toAddresses Receiver addresses.
-    * @param _amounts Amounts of tokens that will be transferred.
-    */
-    function multiSend(address[] _toAddresses, uint256[] _amounts) public {
-        /* Ensures _toAddresses array is less than or equal to 255 */
-        require(_toAddresses.length <= 255);
-        /* Ensures _toAddress and _amounts have the same number of entries. */
-        require(_toAddresses.length == _amounts.length);
-
-        for (uint8 i = 0; i < _toAddresses.length; i++) {
-            transfer(_toAddresses[i], _amounts[i]);
-        }
+     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+     return true;
+   }
+   
+  function transfer(address _to, uint _value, bytes _data) public {
+    require(_value > 0 );
+    if(isContract(_to)) {
+        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+        receiver.tokenFallback(msg.sender, _value, _data);
     }
-
-    /**
-    * @dev Transfer the specified amounts of tokens to the specified addresses from authorized balance of sender.
-    * @dev Be aware that there is no check for duplicate recipients.
-    *
-    * @param _from The address of the sender
-    * @param _toAddresses The addresses of the recipients (MAX 255)
-    * @param _amounts The amounts of tokens to be transferred
-    */
-    function multiSendFrom(address _from, address[] _toAddresses, uint256[] _amounts) public {
-        /* Ensures _toAddresses array is less than or equal to 255 */
-        require(_toAddresses.length <= 255);
-        /* Ensures _toAddress and _amounts have the same number of entries. */
-        require(_toAddresses.length == _amounts.length);
-
-        for (uint8 i = 0; i < _toAddresses.length; i++) {
-            transferFrom(_from, _toAddresses[i], _amounts[i]);
-        }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value, _data);
     }
-	
-}
-
- 
-contract DiamondToken is StandardToken {
-
-  string public constant name = "Diamond Token";
-  string public constant symbol = "DTO";
-  uint8 public constant decimals = 18; 
-
-  uint256 public constant INITIAL_SUPPLY = 90000000 * (10 ** uint256(decimals));
-
-  
-  function DiamondToken() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[0x222486a578d57CfFdD8b69E5b12BAC692c31a743] = INITIAL_SUPPLY;
-    Transfer(0x0, 0x222486a578d57CfFdD8b69E5b12BAC692c31a743, INITIAL_SUPPLY);
-  }
-
+    
+  function isContract(address _addr) private returns (bool is_contract) {
+      uint length;
+      assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+      }
+      return (length>0);
+    }
 }
