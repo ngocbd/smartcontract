53b59e98f9dd1a266e9143f34bcb8be2b8a1c8ad
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:41:41 +0700

    add contract Storage

--- Storage.sol
+++ Storage.sol
@@ -1,30 +1,32 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0xe7976c679089eee8bcfdb1d2ce79a594dd2164b6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Storage at 0xa98a18b786ebb5020552882b9a740ec69818a991
 */
 pragma solidity ^0.4.24;
 
 contract Storage {
-
-    bytes32[] public data;
-    bool readOnly;
-    function uploadData(bytes32[] _data) public {
-        require(readOnly != true);
-        uint index = data.length;
-        for(uint i = 0; i < _data.length; i++) {
-            data.length++;
-            data[index + i] = _data[i];
-        }
+    address owner;
+    bytes[6] public data;
+    uint counter;
+    constructor() public {
+        owner = msg.sender;
     }
-    function uploadFinish() {
-        readOnly = true;
+    function uploadData(bytes _data) public returns (uint){
+        require(msg.sender == owner);
+        data[counter] = _data;
+        counter++;
     }
     function getData() public view returns (bytes){
-        bytes memory result = new bytes(data.length*0x20);
-        for(uint i = 0; i < data.length; i++) {
-            bytes32 word = data[i];
-            assembly {
-                mstore(add(result, add(0x20, mul(i, 32))), word)
+        uint length;
+        for(uint i = 0; i<6; i++) {
+            length += data[i].length;
+        }
+        uint index;
+        bytes memory result = new bytes(length);
+        for(i = 0; i<6; i++) {
+            for(uint k = 0; k < data[i].length; k++) {
+                result[index + k] = data[i][k];
             }
+            index += data[i].length;
         }
         return result;
     }
