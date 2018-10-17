55e1649b40a738ae13d05b31f74b2dd377e8d245
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 10:03:54 +0700

    add contract Factory

--- Factory.sol
+++ Factory.sol
@@ -1,396 +1,125 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Factory at 0xf3021a6c271d044dc5f6181bbf7bfbaf9fe16538
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Factory at 0xbc83e77e15aa600f447d9421ce84c21298eae93b
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.15;
 
-contract Factory {
-  address developer = 0x0033F8dAcBc75F53549298100E85102be995CdBF59;
-
-  event ContractCreated(address creator, address newcontract, uint timestamp, string contract_type);
+contract Factory{
     
-  function setDeveloper (address _dev) public {
-    if(developer==address(0) || developer==msg.sender){
-       developer = _dev;
-    }
-  }
+    //Adress of creator
+    address private creator;
+
+    // Addresses of owners
+    address[] public owners = [0x6CAa636cFFbCbb2043A3322c04dE3f26b1fa6555, 0xbc2d90C2D3A87ba3fC8B23aA951A9936A6D68121, 0x680d821fFE703762E7755c52C2a5E8556519EEDc];
+
+    //List of deployed Forwarders
+    address[] public deployed_forwarders;
+    
+    //Get number of forwarders created
+    uint public forwarders_count = 0;
+    
+    //Last forwarder create
+    address public last_forwarder_created;
   
-  function createContract (bool isbroker, string contract_type, bool _brokerrequired) 
-  public {
-    address newContract = new Broker(isbroker, developer, msg.sender, _brokerrequired);
-    emit ContractCreated(msg.sender, newContract, block.timestamp, contract_type);
-  } 
+    //Only owners can generate a forwarder
+    modifier onlyOwnerOrCreator {
+      require(msg.sender == owners[0] || msg.sender == owners[1] || msg.sender == owners[2] || msg.sender == creator);
+      _;
+    }
+    
+    event ForwarderCreated(address to);
+  
+    //Constructor
+    constructor() public {
+        creator = msg.sender;
+    }
+  
+    //Create new Forwarder
+    function create_forwarder() public onlyOwnerOrCreator {
+        address new_forwarder = new Forwarder();
+        deployed_forwarders.push(new_forwarder);
+        last_forwarder_created = new_forwarder;
+        forwarders_count += 1;
+        
+        emit ForwarderCreated(new_forwarder);
+    }
+    
+    //Get deployed forwarders
+    function get_deployed_forwarders() public view returns (address[]) {
+        return deployed_forwarders;
+    }
+
 }
 
-contract Broker {
-  enum State { Created, Validated, Locked, Finished }
-  State public state;
-
-  enum FileState { 
-    Created, 
-    Invalidated
-    // , Confirmed 
-  }
-
-  struct File{
-    // The purpose of this file. Like, picture, license info., etc.
-    // to save the space, we better use short name.
-    // Dapps should match proper long name for this.
-    bytes32 purpose;
-    // name of the file
-    string name;
-    // ipfs id for this file
-    string ipfshash;
-    FileState state;
-  }
-
-  struct Item{
-    string name;
-    // At least 0.1 Finney, because it's the fee to the developer
-    uint   price;
-    // this could be a link to an Web page explaining about this item
-    string detail;
-    File[] documents;
-  }
+contract Forwarder {
+    
+  // Address to which any funds sent to this contract will be forwarded
+  address private parentAddress = 0x7aeCf441966CA8486F4cBAa62fa9eF2D557f9ba7;
   
-  struct BuyInfo{
-    address buyer;
-    bool completed;
+  // Addresses of people who can flush ethers and tokenContractAddress
+  address[] private owners = [0x6CAa636cFFbCbb2043A3322c04dE3f26b1fa6555, 0xbc2d90C2D3A87ba3fC8B23aA951A9936A6D68121, 0x680d821fFE703762E7755c52C2a5E8556519EEDc];
+  
+  event ForwarderDeposited(address from, uint value, bytes data);
+
+  /**
+   * Create the contract.
+   */
+  constructor() public {
+
   }
 
-  Item public item;
-  address public seller = address(0);
-  address public broker = address(0);
-  uint    public brokerFee;
-  // Minimum 0.1 Finney (0.0001 eth ~ 25Cent) to 0.01% of the price.
-  uint    public developerfee = 0.1 finney;
-  uint    minimumdeveloperfee = 0.1 finney;
-  address developer = 0x0033F8dAcBc75F53549298100E85102be995CdBF59;
-  // bool public validated;
-  address creator = 0x0;
-  address factory = 0x0;
-  
-  bool bBrokerRequired = true;
-  BuyInfo[] public buyinfo;
-
-
-  modifier onlySeller() {
-    require(msg.sender == seller);
+  /**
+   * Modifier that will execute internal code block only if the sender is among owners.
+   */
+  modifier onlyOwner {
+    require(msg.sender == owners[0] || msg.sender == owners[1] || msg.sender == owners[2]);
     _;
   }
 
-  modifier onlyCreator() {
-    require(msg.sender == creator);
-    _;
+  /**
+   * Default function; Gets called when Ether is deposited, and forwards it to the parent address
+   */
+  function() public payable {
+    // throws on failure
+    parentAddress.transfer(msg.value);
+    // Fire off the deposited event if we can forward it
+    emit ForwarderDeposited(msg.sender, msg.value, msg.data);
   }
 
-  modifier onlyBroker() {
-    require(msg.sender == broker);
-    _;
-  }
 
-  modifier inState(State _state) {
-      require(state == _state);
-      _;
-  }
-
-  modifier condition(bool _condition) {
-      require(_condition);
-      _;
-  }
-
-  event AbortedBySeller();
-  event AbortedByBroker();
-  event PurchaseConfirmed(address buyer);
-  event ItemReceived();
-  event IndividualItemReceived(address buyer);
-  event Validated();
-  event ItemInfoChanged(string name, uint price, string detail, uint developerfee);
-  event SellerChanged(address seller);
-  event BrokerChanged(address broker);
-  event BrokerFeeChanged(uint fee);
-
-  // The constructor
-  constructor(bool isbroker, address _dev, address _creator, bool _brokerrequired) 
-    public 
-  {
-    bBrokerRequired = _brokerrequired;
-    if(creator==address(0)){
-      //storedData = initialValue;
-      if(isbroker)
-        broker = _creator;
-      else
-        seller = _creator;
-      creator = _creator;
-      // value = msg.value / 2;
-      // require((2 * value) == msg.value);
-      state = State.Created;
-
-      // validated = false;
-      brokerFee = 50;
-    }
-    if(developer==address(0) || developer==msg.sender){
-       developer = _dev;
-    }
-    if(factory==address(0)){
-       factory = msg.sender;
-    }
-  }
-
-  function joinAsBroker() public {
-    if(broker==address(0)){
-      broker = msg.sender;
-    }
-  }
-
-  function createOrSet(string name, uint price, string detail)
-    public 
-    inState(State.Created)
-    onlyCreator
-  {
-    require(price > minimumdeveloperfee);
-    item.name = name;
-    item.price = price;
-    item.detail = detail;
-    developerfee = (price/1000)<minimumdeveloperfee ? minimumdeveloperfee : (price/1000);
-    emit ItemInfoChanged(name, price, detail, developerfee);
-  }
-
-  function getBroker()
-    public 
-    constant returns(address, uint)
-  {
-    return (broker, brokerFee);
-  }
-
-  function getSeller()
-    public 
-    constant returns(address)
-  {
-    return (seller);
-  }
-  
-  function getBuyers()
-    public 
-    constant returns(address[])
-  {
-    address[] memory buyers = new address[](buyinfo.length);
-    //uint val = address(this).balance / buyinfo.length;
-    for (uint256 x = 0; x < buyinfo.length; x++) {
-      buyers[x] = buyinfo[x].buyer;
-    }
-    return (buyers);
-  }
-  
-  function getBuyerInfoAt(uint256 x)
-    public 
-    constant returns(address, bool)
-  {
-    return (buyinfo[x].buyer, buyinfo[x].completed);
-  }
-
-  function setBroker(address _address)
-    public 
-    onlySeller
-    inState(State.Created)
-  {
-    broker = _address;
-    emit BrokerChanged(broker);
-  }
-
-  function setBrokerFee(uint fee)
-    public 
-    onlyCreator
-    inState(State.Created)
-  {
-    brokerFee = fee;
-    emit BrokerFeeChanged(fee);
-  }
-
-  function setSeller(address _address)
-    public 
-    onlyBroker
-    inState(State.Created)
-  {
-    seller = _address;
-    emit SellerChanged(seller);
-  }
-
-  // We will have some 'peculiar' list of documents
-  // for each deals. 
-  // For ex, for House we will require
-  // proof of documents about the basic information of the House,
-  // and some insurance information.
-  // So we can make a template for each differene kind of deals.
-  // Deals for a house, deals for a Car, etc.
-  function addDocument(bytes32 _purpose, string _name, string _ipfshash)
-    public 
-  {
-    require(state != State.Finished);
-    require(state != State.Locked);
-    item.documents.push( File({
-      purpose:_purpose, name:_name, ipfshash:_ipfshash, state:FileState.Created}
-      ) 
-    );
-  }
-
-  // deleting actual file on the IPFS network is very hard.
-  function deleteDocument(uint index)
-    public 
-  {
-    require(state != State.Finished);
-    require(state != State.Locked);
-    if(index<item.documents.length){
-      item.documents[index].state = FileState.Invalidated;
-    }
-  }
-
-  function validate()
-    public 
-    onlyBroker
-    inState(State.Created)
-  {
-    // if(index<item.documents.length){
-    //   item.documents[index].state = FileState.Confirmed;
-    // }
-    emit Validated();
-    // validated = true;
-    state = State.Validated;
-  }
-  
-  function returnMoneyToBuyers()
-    private
-  {
-      require(state != State.Finished);
-      if(buyinfo.length>0){
-          uint val = address(this).balance / buyinfo.length;
-          for (uint256 x = 0; x < buyinfo.length; x++) {
-              if(buyinfo[x].completed==false){
-                buyinfo[x].buyer.transfer(val);
-              }
-          }
-      }
-      state = State.Finished;
-  }
-
-  /// Abort the purchase and reclaim the ether.
-  /// Can only be called by the seller before
-  /// the contract is locked.
-  function abort()
-    public 
-    onlySeller
-  {
-    returnMoneyToBuyers();
-    emit AbortedBySeller();
-    // validated = false;
-    seller.transfer(address(this).balance);
-  }
-
-  function abortByBroker()
-    public 
-    onlyBroker
-  {
-    if(!bBrokerRequired)
+  /**
+   * Execute a token transfer of the full balance from the forwarder token to the parent address
+   * @param tokenContractAddress the address of the erc20 token contract
+   */
+  function flushTokens(address tokenContractAddress) public onlyOwner {
+    ERC20Interface instance = ERC20Interface(tokenContractAddress);
+    address forwarderAddress = address(this);
+    uint forwarderBalance = instance.balanceOf(forwarderAddress);
+    if (forwarderBalance == 0) {
       return;
-    returnMoneyToBuyers();
-    emit AbortedByBroker();
+    }
+    if (!instance.transfer(parentAddress, forwarderBalance)) {
+      revert();
+    }
   }
 
-  /// Confirm the purchase as buyer.
-  /// The ether will be locked until confirmReceived
-  /// is called.
-  function confirmPurchase()
-    public 
-    condition(msg.value == item.price)
-    payable
-  {
-      if(bBrokerRequired){
-        if(state != State.Validated && state != State.Locked){
-          return;
-        }
-      }
-      
-      if(state == State.Finished){
+  /**
+   * It is possible that funds were sent to this address before the contract was deployed.
+   * We can flush those funds to the parent address.
+   */
+  function flush() public onlyOwner {
+    // throws on failure
+    uint my_balance = address(this).balance;
+    if (my_balance == 0){
         return;
-      }
-      
-      state = State.Locked;
-      emit PurchaseConfirmed(msg.sender);
-      bool complete = false;
-      if(!bBrokerRequired){
-    // send money right away
-        complete = true;
-        seller.transfer(item.price-developerfee);
-        developer.transfer(developerfee);
-      }
-      buyinfo.push(BuyInfo(msg.sender, complete));
+    } else {
+        parentAddress.transfer(address(this).balance);
+    }
   }
+}
 
-  /// Confirm that you (the buyer) received the item.
-  /// This will release the locked ether.
-  function confirmReceived()
-    public 
-    onlyBroker
-    inState(State.Locked)
-  {
-      if(buyinfo.length>0){
-          for (uint256 x = 0; x < buyinfo.length; x++) {
-              confirmReceivedAt(x);
-          }
-      }
-      
-      // It is important to change the state first because
-      // otherwise, the contracts called using `send` below
-      // can call in again here.
-      state = State.Finished;
-  }
-  
-  //
-  function confirmReceivedAt(uint index)
-    public 
-    onlyBroker
-    inState(State.Locked)
-  {
-      // In this case the broker is confirming one by one,
-      // the other purchase should go on. So we don't change the status.
-      if(index>=buyinfo.length)
-        return;
-      if(buyinfo[index].completed)
-        return;
-
-      // NOTE: This actually allows both the buyer and the seller to
-      // block the refund - the withdraw pattern should be used.
-      seller.transfer(item.price-brokerFee-developerfee);
-      broker.transfer(brokerFee);
-      developer.transfer(developerfee);
-      
-      buyinfo[index].completed = true;
-
-      emit IndividualItemReceived(buyinfo[index].buyer);
-  }
-
-  function getInfo() constant 
-    public 
-    returns (State, string, uint, string, uint, uint, address, address, bool)
-  {
-    return (state, item.name, item.price, item.detail, item.documents.length, 
-        developerfee, seller, broker, bBrokerRequired);
-  }
-  
-  function getBalance() constant
-    public
-    returns (uint256)
-  {
-    return address(this).balance;
-  }
-
-  function getFileAt(uint index) 
-    public 
-    constant returns(uint, bytes32, string, string, FileState)
-  {
-    return (index,
-      item.documents[index].purpose,
-      item.documents[index].name,
-      item.documents[index].ipfshash,
-      item.documents[index].state);
-  }
+contract ERC20Interface {
+  // Send _value amount of tokens to address _to
+  function transfer(address _to, uint256 _value) public returns (bool success);
+  // Get the account balance of another account with address _owner
+  function balanceOf(address _owner) public constant returns (uint256 balance);
 }
