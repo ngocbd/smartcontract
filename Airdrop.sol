32490c754657d5dbff3009d20564a8e824d914ff
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 05:22:17 +0700

    add contract Airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,245 +1,77 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x02c11f38bec96eb38c4dc2bcc8d669ae676fd7a9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0xb1fdd533d35f406029625015cce4b2b7baf55b9d
 */
-pragma solidity ^0.4.18;
-
+pragma solidity ^0.4.24;
 
 /**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
+ * SmartEth.co
+ * ERC20 Token and ICO smart contracts development, smart contracts audit, ICO websites.
+ * contact@smarteth.co
  */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
 
 /**
  * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-    address public owner;
+  address public owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    /**
-    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-    * account.
-    */
-    function Ownable() public {
-        owner = msg.sender;
-    }
+  constructor() public {
+    owner = 0xbc0af2ca6846d835789e5c20282f15275af34fe4;
+  }
 
-    /**
-    * @dev Throws if called by any account other than the owner.
-    */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-    /**
-    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-    * @param newOwner The address to transfer ownership to.
-    */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
 }
-
 
 /**
  * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
  */
-contract ERC20BasicInterface {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    uint8 public decimals;
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-
 /**
- * @title AirDropContract
- * Simply do the airdrop.
+ * @title ERC20 interface
  */
-contract AirDrop is Ownable {
-    using SafeMath for uint256;
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    // the amount that owner wants to send each time
-    uint public airDropAmount;
+contract Airdrop is Ownable {
 
-    // the mapping to judge whether each address has already received airDropped
-    mapping ( address => bool ) public invalidAirDrop;
+  ERC20 public token = ERC20(0xb09830db5B21167A5a27969aC3Ab01B129cf41a1);
 
-    // the array of addresses which received airDrop
-    address[] public arrayAirDropReceivers;
-
-    // flag to stop airdrop
-    bool public stop = false;
-
-    ERC20BasicInterface public erc20;
-
-    uint256 public startTime;
-    uint256 public endTime;
-
-    // event
-    event LogAirDrop(address indexed receiver, uint amount);
-    event LogStop();
-    event LogStart();
-    event LogWithdrawal(address indexed receiver, uint amount);
-
-    /**
-    * @dev Constructor to set _airDropAmount and _tokenAddresss.
-    * @param _airDropAmount The amount of token that is sent for doing airDrop.
-    * @param _tokenAddress The address of token.
-    */
-    constructor(uint256 _startTime, uint256 _endTime, uint _airDropAmount, address _tokenAddress) public {
-        require(_startTime >= now &&
-            _endTime >= _startTime &&
-            _airDropAmount > 0 &&
-            _tokenAddress != address(0)
-        );
-        startTime = _startTime;
-        endTime = _endTime;
-        erc20 = ERC20BasicInterface(_tokenAddress);
-        uint tokenDecimals = erc20.decimals();
-        airDropAmount = _airDropAmount.mul(10 ** tokenDecimals);
-    }
-
-    /**
-    * @dev Confirm that airDrop is available.
-    * @return A bool to confirm that airDrop is available.
-    */
-    function isValidAirDropForAll() public view returns (bool) {
-        bool validNotStop = !stop;
-        bool validAmount = getRemainingToken() >= airDropAmount;
-        bool validPeriod = now >= startTime && now <= endTime;
-        return validNotStop && validAmount && validPeriod;
-    }
-
-    /**
-    * @dev Confirm that airDrop is available for msg.sender.
-    * @return A bool to confirm that airDrop is available for msg.sender.
-    */
-    function isValidAirDropForIndividual() public view returns (bool) {
-        bool validNotStop = !stop;
-        bool validAmount = getRemainingToken() >= airDropAmount;
-        bool validPeriod = now >= startTime && now <= endTime;
-        bool validReceiveAirDropForIndividual = !invalidAirDrop[msg.sender];
-        return validNotStop && validAmount && validPeriod && validReceiveAirDropForIndividual;
-    }
-
-    /**
-    * @dev Do the airDrop to msg.sender
-    */
-    function receiveAirDrop() public {
-        require(isValidAirDropForIndividual());
-
-        // set invalidAirDrop of msg.sender to true
-        invalidAirDrop[msg.sender] = true;
-
-        // set msg.sender to the array of the airDropReceiver
-        arrayAirDropReceivers.push(msg.sender);
-
-        // execute transferFrom
-        require(erc20.transfer(msg.sender, airDropAmount));
-
-        emit LogAirDrop(msg.sender, airDropAmount);
-    }
-
-    /**
-    * @dev Change the state of stop flag
-    */
-    function toggle() public onlyOwner {
-        stop = !stop;
-
-        if (stop) {
-            emit LogStop();
-        } else {
-            emit LogStart();
-        }
-    }
-
-    /**
-    * @dev Withdraw the amount of token that is remaining in this contract.
-    * @param _address The address of EOA that can receive token from this contract.
-    */
-    function withdraw(address _address) public onlyOwner {
-        require(stop || now > endTime);
-        require(_address != address(0));
-        uint tokenBalanceOfContract = getRemainingToken();
-        require(erc20.transfer(_address, tokenBalanceOfContract));
-        emit LogWithdrawal(_address, tokenBalanceOfContract);
-    }
-
-    /**
-    * @dev Get the total number of addresses which received airDrop.
-    * @return Uint256 the total number of addresses which received airDrop.
-    */
-    function getTotalNumberOfAddressesReceivedAirDrop() public view returns (uint256) {
-        return arrayAirDropReceivers.length;
-    }
-
-    /**
-    * @dev Get the remaining amount of token user can receive.
-    * @return Uint256 the amount of token that user can reveive.
-    */
-    function getRemainingToken() public view returns (uint256) {
-        return erc20.balanceOf(this);
-    }
-
-    /**
-    * @dev Return the total amount of token user received.
-    * @return Uint256 total amount of token user received.
-    */
-    function getTotalAirDroppedAmount() public view returns (uint256) {
-        return airDropAmount.mul(arrayAirDropReceivers.length);
-    }
+  function airdrop(address[] recipient, uint256[] amount) public onlyOwner returns (uint256) {
+    uint256 i = 0;
+      while (i < recipient.length) {
+        token.transfer(recipient[i], amount[i]);
+        i += 1;
+      }
+    return(i);
+  }
+  
+  function airdropSameAmount(address[] recipient, uint256 amount) public onlyOwner returns (uint256) {
+    uint256 i = 0;
+      while (i < recipient.length) {
+        token.transfer(recipient[i], amount);
+        i += 1;
+      }
+    return(i);
+  }
 }
