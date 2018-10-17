b468e8160cc16b67bace6435049b73561303fcbe
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 23:07:37 +0700

    add contract AirdropContract

--- AirDropContract.sol
+++ AirDropContract.sol
@@ -1,68 +1,36 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDropContract at 0xa719bec0625ed5bcb1df3f80c5c8d59a5d29b909
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirdropContract at 0xfee322eb8908519a852019b3b48ff632473f541f
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
-contract ERC20 {
-    uint public totalSupply;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function balanceOf(address who) view public returns (uint256);
-    function allowance(address owner, address spender) view public returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
+interface ERCToken {
+    
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
-contract AirDropContract {
 
-    event AirDropped(address addr, uint amount);
-    address public owner = 0xa107483c8a16a58871182a48d4ba1fbbb6a64c71;
-
-    function drop(
-        address tokenAddress,
-        address[] recipients,
-        uint256[] amounts) public {
-        require(msg.sender == owner);
-        require(tokenAddress != 0x0);
-        require(amounts.length == recipients.length);
-
-        ERC20 token = ERC20(tokenAddress);
-
-        uint balance = token.balanceOf(msg.sender);
-        uint allowance = token.allowance(msg.sender, address(this));
-        uint available = balance > allowance ? allowance : balance;
-
-        for (uint i = 0; i < recipients.length; i++) {
-            require(available >= amounts[i]);
-            if (isQualitifiedAddress(
-                recipients[i]
-            )) {
-                available -= amounts[i];
-                require(token.transferFrom(msg.sender, recipients[i], amounts[i]));
-
-                AirDropped(recipients[i], amounts[i]);
-            }
+contract AirdropContract {
+    
+    address public owner;
+    
+    ERCToken token;
+    
+    modifier onlyOwner() {
+    	require(msg.sender == owner);
+    	_;
+  	}
+    
+    constructor() public {
+      owner = msg.sender;
+    }
+    
+    function send(address _tokenAddr, address from, address[] dests, uint256[] values) public onlyOwner returns(uint256) {
+        uint256 i = 0;
+        token = ERCToken(_tokenAddr);
+        while (i < dests.length) {
+            token.transferFrom(from, dests[i], values[i]);
+            i += 1;
         }
-    }
-
-    function isQualitifiedAddress(address addr)
-        public
-        view
-        returns (bool result)
-    {
-        result = addr != 0x0 && addr != msg.sender && !isContract(addr);
-    }
-
-    function isContract(address addr) internal view returns (bool) {
-        uint size;
-        assembly { size := extcodesize(addr) }
-        return size > 0;
-    }
-
-    function () payable public {
-        revert();
+        return i;
     }
 }
