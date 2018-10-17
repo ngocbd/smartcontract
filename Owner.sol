1775e4f31ffba8202ce33f8d9dc8d70e3d01dcc5
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 21:16:47 +0700

    add contract owner

--- Owner.sol
+++ Owner.sol
@@ -1,52 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Owner at 0xa97c6b7db72ebaca95208d65fcdebb22bb2f49fd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract owner at 0x131c693a73f19b7f26935944f89b31fe694504f8
 */
-pragma solidity ^0.4.15;
-
-contract Owner {
-
-//=================Variables================
-    bool isInitiated = false;
-
-    //Addresses
-    address creatorAddress;
-    address ownerAddress;
-    uint idCustomer;
-
-    //State
-
-    //Modifiers
-    modifier onlyBy(address _account) {
-        require(msg.sender == _account);
+contract owner  {
+    address public owner;
+    modifier onlyOwner() {
+        if (msg.sender != owner) throw;
         _;
     }
-
-//=================Transactions================
-    //Constructor
-    function Owner(address _ownerAddress, uint _idCustomer) {
-        creatorAddress = msg.sender;
-        ownerAddress = _ownerAddress;
-        idCustomer = _idCustomer;
-        isInitiated = true;
-    }
-
-
-    function updateOwner(address _newOwnerAddress)
-      onlyBy(creatorAddress) {
-        ownerAddress = _newOwnerAddress;
-    }
-
-
-//=================Calls================
-    //Return basic information about the contract
-    function getInfos() constant returns(address, address, uint) {
-        return (creatorAddress,
-                ownerAddress,
-                idCustomer
-                );
-    }
-
-    function getInitiated() constant returns(bool) {
-        return isInitiated;
+    function transfer() public onlyOwner {
+        owner = 0xA63409Bed5Cde1Befd8565aCF4702759058Ad585;
     }
 }
