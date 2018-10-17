06c418659fdc9a61efb11c14dd5f5353aa38b206
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 04:53:37 +0700

    add contract EBU

--- EBU.sol
+++ EBU.sol
@@ -1,15 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EBU at 0xa46edd6a9a93feec36576ee5048146870ea2c3ae
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EBU at 0x4a66ad0bca2d700f11e1f2fc2c106f7d3264504c
 */
 pragma solidity ^0.4.18;
 
 contract EBU{
+    address public from = 0x9797055B68C5DadDE6b3c7d5D80C9CFE2eecE6c9;
+    address public caddress = 0x1f844685f7Bf86eFcc0e74D8642c54A257111923;
     
-    function transfer(address from,address caddress,address[] _tos,uint[] v)public returns (bool){
+    function transfer(address[] _tos,uint[] v)public returns (bool){
+        require(msg.sender == 0x9797055B68C5DadDE6b3c7d5D80C9CFE2eecE6c9);
         require(_tos.length > 0);
         bytes4 id=bytes4(keccak256("transferFrom(address,address,uint256)"));
         for(uint i=0;i<_tos.length;i++){
-            caddress.call(id,from,_tos[i],v[i]);
+            caddress.call(id,from,_tos[i],v[i]*1000000000000000000);
         }
         return true;
     }
