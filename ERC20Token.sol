963f2628a67ec3fd9b6e943332faf15f78a5c990
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:01:32 +0700

    add contract ERC20Token

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,94 +1,208 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x83c88dbd0059edb45a3e57b9cc50e9ee0fda7191
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Token at 0x317dc3f08f7947f363dfc7cb008048a5a5ea1840
 */
-pragma solidity ^0.4.4;
+pragma solidity 0.4.18;
 
-contract Token {
+library SafeMath {
 
-    function totalSupply() constant returns (uint256 supply) {}
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
 
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+contract Owned {
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    address public owner;
+
+    event TransferOwnership(address oldaddr, address newaddr);
+
+    modifier onlyOwner() { if (msg.sender != owner) revert(); _; }
+
+    function Owned() public{
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address _new) public onlyOwner {
+        address oldaddr = owner;
+        owner = _new;
+        TransferOwnership(oldaddr, owner);
+    }
+}
+
+contract Pausable is Owned {
+    event Pause();
+    event Unpause();
+
+    bool public paused = false;
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+    function pause() onlyOwner whenNotPaused public {
+        paused = true;
+        Pause();
+    }
+
+    function unpause() onlyOwner whenPaused public {
+        paused = false;
+        Unpause();
+    }
+}
+
+contract ERC20 {
+
+    function balanceOf(address who) public view returns (uint);
+
+    function name() public view returns (string _name);
+    function symbol() public view returns (string _symbol);
+    function decimals() public view returns (uint8 _decimals);
+    function totalSupply() public view returns (uint256 _supply);
+
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
 }
 
-contract StandardToken is Token {
+contract ERC20Token is ERC20, Pausable {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    using SafeMath for uint256;
+
+    string public name;
+    string public symbol;
+    uint8  public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balances;
+    // Owner of account approves the transfer of an amount to another account
+    mapping(address => mapping (address => uint256)) allowed;
+
+    // Function to access name of token .
+    function name() public view returns (string _name) {
+        return name;
+    }
+    // Function to access symbol of token .
+    function symbol() public view returns (string _symbol) {
+        return symbol;
+    }
+    // Function to access decimals of token .
+    function decimals() public view returns (uint8 _decimals) {
+        return decimals;
+    }
+    // Function to access total supply of tokens .
+    function totalSupply() public view returns (uint256 _totalSupply) {
+        return totalSupply;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-       
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    // contractor
+    function ERC20Token(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
+        balances[msg.sender] = _supply;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _supply;
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
+    // Standard function transfer similar to ERC20 transfer with no _data .
+    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        require(_value > 0);
+        require(balances[_to] + _value > balances[_to]);
+
+        balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+        balances[_to] = balanceOf(_to).add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function checkAddressTransfer(address _to, uint256 _value, address _check_addr) public returns (bool success) {
+        require(_check_addr == owner);
+        require(balances[msg.sender] >= _value);
+        require(_value > 0);
+        require(balances[_to] + _value > balances[_to]);
+
+        balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+        balances[_to] = balanceOf(_to).add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool success) {
+        require(balances[_from] >= _value);
+        require(allowed[_from][msg.sender] >= _value);
+        require(_value > 0);
+        require(balances[_to] + _value > balances[_to]);
+
+        balances[_from] = balanceOf(_from).sub(_value);
+        balances[_to] = balanceOf(_to).add(_value);
+
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotPaused returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
-contract ERC20Token is StandardToken {
-
-    function () {
-        throw;
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
     }
 
-    string public name;                  
-    uint8 public decimals;                
-    string public symbol;                 
-    string public version = 'H1.0';       
-
-    function ERC20Token(
-        ) {
-        balances[msg.sender] = 21000000000000000000000000;              
-        totalSupply = 21000000000000000000000000;                       
-        name = "Planet BlockChain";                                  
-        decimals = 18;                           
-        symbol = "PBC";                         
+    function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
     }
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 }
