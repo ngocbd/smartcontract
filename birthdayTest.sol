b3753a5e43b4b212c7cc66ddfdf372a73e199d31
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 01:04:53 +0700

    add contract birthdayTest

---
+++ birthdayTest.sol
@@ -0,0 +1,21 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract birthdayTest at 0xf076de430f45a177a010590378126657a803b953
+*/
+pragma solidity ^0.4.24;
+
+contract birthdayTest {
+    
+    uint originalTime;
+    
+    constructor() public{
+        originalTime = now;
+    }
+    
+    
+    //prints a birthday message and then becomes impossible to execute after 23 hours
+    function BirthdayBoyClickHere() public view returns(string) {
+        require(now < originalTime + 23 hours);
+        return "Happy Birthday Harrison! I know this contract is noobish but I will get better.";
+    }
+
+}
