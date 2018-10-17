0b73256a3855fc9182bfc98658f29bb838f336b9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 05:59:30 +0700

    add contract TokenBurner

--- TokenBurner.sol
+++ TokenBurner.sol
@@ -1,9 +1,8 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenBurner at 0x41e95553bf4dfca4ae323803b379b9e8cdedc565
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenBurner at 0xf9032eb0af032f86f39d9f985d27d2ef7858abc9
 */
 pragma solidity ^0.4.19;
 
-// File: contracts/TokenBurner.sol
 
 contract TokenBurner {
 
