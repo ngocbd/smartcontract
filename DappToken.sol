53d9524b20c65515515d4d386b4f2095559fba06
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 07:36:03 +0700

    add contract DappToken

--- DappToken.sol
+++ DappToken.sol
@@ -1,172 +1,67 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DAPPToken at 0x5d0fa08aeb173ade44b0cf7f31d506d8e04f0ac8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DappToken at 0xc3971619e272473286d7db7e7fb3fef8a0e9e8e8
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.19;
 
+contract DappToken {
+    string  public name = "Korean Unified Currency";
+    string  public symbol = "XWON";
+    string  public standard = "XWON Token v1.0";
+    uint8 public constant decimals = 0;
+    uint256 public totalSupply;
 
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function DappToken (uint256 _initialSupply) public {
+    //function DappToken () public {
+        _initialSupply = 100000000;
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
-    uint256 c = a * b;
-    require(c / a == b);
-    return c;
-  }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a / b;
-    return c;
-  }
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c >= a);
-    return c;
-  }
-}
-
-
-contract Ownable {
-  address public owner;
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-  function Ownable() public {
-    owner = msg.sender ;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-contract DAPPToken is Ownable{
-    
-    using SafeMath for uint256;
-    
-    string public constant name       = "360APP";
-    string public constant symbol     = "DAPP";
-    uint32 public constant decimals   = 18;
-    uint256 public totalSupply        = 900000000000 ether;
-    uint256 public currentTotalSupply = 0;
-    uint256 startBalance              = 100000 ether;
-    
-    mapping(address => bool) touched;
-    mapping(address => uint256) balances;
-    mapping (address => mapping (address => uint256)) internal allowed;
-    
-        function DAPPToken() public {
-        balances[msg.sender] = startBalance * 6000000;
-        currentTotalSupply = balances[msg.sender];
-    }
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    
-
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-
-        if( !touched[msg.sender] && currentTotalSupply < totalSupply ){
-            uint256 _nvalue = 5000 ether;
-            balances[msg.sender] = balances[msg.sender].add( startBalance );
-            touched[msg.sender] = true;
-            currentTotalSupply = currentTotalSupply.add( startBalance ).add(_nvalue).add(_nvalue);
-        }
-        
-        require(_value <= balances[msg.sender]);
-        
-        balances[msg.sender] = balances[msg.sender].sub(_value).add(_nvalue);
-        balances[_to] = balances[_to].add(_value).add(_nvalue);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
 
         Transfer(msg.sender, _to, _value);
-        startBalance = startBalance.div(1000000).mul(999999);
-        return true;
-    }
-  
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        
-        require(_value <= allowed[_from][msg.sender]);
-        
-        if( !touched[_from] && currentTotalSupply < totalSupply ){
-            touched[_from] = true;
-            balances[_from] = balances[_from].add( startBalance );
-            currentTotalSupply = currentTotalSupply.add( startBalance );
-        }
-        
-        require(_value <= balances[_from]);
-        
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
         return true;
     }
 
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
 
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
+
         return true;
     }
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
-    function allowance(address _owner, address _spender) public view returns (uint256) {
-        return allowed[_owner][_spender];
-     }
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
 
+        allowance[_from][msg.sender] -= _value;
 
-    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        Transfer(_from, _to, _value);
+
         return true;
     }
-
-
-    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-          allowed[msg.sender][_spender] = 0;
-        } else {
-          allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-     }
-    
-
-    function getBalance(address _a) internal constant returns(uint256)
-    {
-        if( currentTotalSupply < totalSupply ){
-            if( touched[_a] )
-                return balances[_a];
-            else
-                return balances[_a].add( startBalance );
-        } else {
-            return balances[_a];
-        }
-    }
-    
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return getBalance( _owner );
-    }
-
 }
