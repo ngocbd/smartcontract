a170c5660386bfa6383a9416c99aacbe085d0954
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:31:40 +0700

    add contract DOGEToken

--- Dogetoken.sol
+++ Dogetoken.sol
@@ -1,38 +1,35 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DOGEToken at 0x2a98c6dcac6d759695b09cdf6b26cf4a13370a57
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DOGEToken at 0xe12295065f44f0cd740612b4a95e1b9c1351fd7a
 */
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// 'DOGE' token contract
+// 'DOGE' CROWDSALE token contract
 //
-// Deployed to : 0x2a98c6dcac6d759695b09cdf6b26cf4a13370a57
+// Deployed to : 0xe12295065f44f0cd740612b4a95e1b9c1351fd7a
 // Symbol      : DOGE
 // Name        : DOGE Token
-// Total supply: 100000000
+// Total supply: Gazillion
 // Decimals    : 18
 //
-
-// ----------------------------------------------------------------------------
-
 
 // ----------------------------------------------------------------------------
 // Safe maths
 // ----------------------------------------------------------------------------
 contract SafeMath {
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
         require(c >= a);
     }
-    function safeSub(uint a, uint b) public pure returns (uint c) {
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
         require(b <= a);
         c = a - b;
     }
-    function safeMul(uint a, uint b) public pure returns (uint c) {
+    function safeMul(uint a, uint b) internal pure returns (uint c) {
         c = a * b;
         require(a == 0 || c / a == b);
     }
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
+    function safeDiv(uint a, uint b) internal pure returns (uint c) {
         require(b > 0);
         c = a / b;
     }
@@ -105,6 +102,9 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
     string public  name;
     uint8 public decimals;
     uint public _totalSupply;
+    uint public startDate;
+    uint public bonusEnds;
+    uint public endDate;
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
@@ -117,9 +117,9 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
         symbol = "DOGE";
         name = "DOGE Token";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0xdF74B93C1Fa5515E3D5199fce19b9DB57A98e8B8] = _totalSupply;
-        Transfer(address(0), 0xdF74B93C1Fa5515E3D5199fce19b9DB57A98e8B8, _totalSupply);
+        bonusEnds = now + 7 weeks;
+        endDate = now + 14 weeks;
+
     }
 
 
@@ -132,7 +132,7 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
 
 
     // ------------------------------------------------------------------------
-    // Get the token balance for account tokenOwner
+    // Get the token balance for account `tokenOwner`
     // ------------------------------------------------------------------------
     function balanceOf(address tokenOwner) public constant returns (uint balance) {
         return balances[tokenOwner];
@@ -140,7 +140,7 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
 
 
     // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to to account
+    // Transfer the balance from token owner's account to `to` account
     // - Owner's account must have sufficient balance to transfer
     // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
@@ -153,12 +153,12 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
 
 
     // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
     // from the token owner's account
     //
     // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
     // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces 
+    // as this should be implemented in user interfaces
     // ------------------------------------------------------------------------
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
@@ -168,10 +168,10 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
 
 
     // ------------------------------------------------------------------------
-    // Transfer tokens from the from account to the to account
-    // 
+    // Transfer `tokens` from the `from` account to the `to` account
+    //
     // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the from account and
+    // for spending from the `from` account and
     // - From account must have sufficient balance to transfer
     // - Spender must have sufficient allowance to transfer
     // - 0 value transfers are allowed
@@ -195,9 +195,9 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
 
 
     // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account. The spender contract function
-    // receiveApproval(...) is then executed
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
+    // from the token owner's account. The `spender` contract function
+    // `receiveApproval(...)` is then executed
     // ------------------------------------------------------------------------
     function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
@@ -206,6 +206,22 @@ contract DOGEToken is ERC20Interface, Owned, SafeMath {
         return true;
     }
 
+    // ------------------------------------------------------------------------
+    // 1,000,000 FWD Tokens per 1 ETH
+    // ------------------------------------------------------------------------
+    function () public payable {
+        require(now >= startDate && now <= endDate);
+        uint tokens;
+        if (now <= bonusEnds) {
+            tokens = msg.value * 1500000;
+        } else {
+            tokens = msg.value * 1000000;
+        }
+        balances[msg.sender] = safeAdd(balances[msg.sender], tokens);
+        _totalSupply = safeAdd(_totalSupply, tokens);
+        Transfer(address(0), msg.sender, tokens);
+        owner.transfer(msg.value);
+    }
 
 
 
