07a42e42ac839254b4bfa8a38f5e1b2bf1dfedb7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 08:34:07 +0700

    add contract ComplianceService

---
+++ ComplianceService.sol
@@ -0,0 +1,10 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ComplianceService at 0xff77264bb6899aaeae39bb41d710cdef189e2f3c
+*/
+pragma solidity ^0.4.22;
+
+contract ComplianceService {
+	function validate(address _from, address _to, uint256 _amount) public returns (bool allowed) {
+		return true;
+	}
+}
