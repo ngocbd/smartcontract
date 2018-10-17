2d19fcc3a6a034cd21a0b0cd5ea2f7db21f996ee
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 16:26:38 +0700

    add contract DigitalMediaBurn

---
+++ DigitalMediaBurn.sol
@@ -0,0 +1,15 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DigitalMediaBurn at 0x1f5060d82f872be9bc3c56d8cb52194aee85ae7f
+*/
+pragma solidity ^0.4.22;
+
+/**
+ * This is a contract used to burn older generation MakersTokens.
+ * When there's a mistake with a token and a creator wants to reissue
+ * a token, they can send the token to this contract knowing that it's
+ * lost forever.  This smart contract provides no capabilities to transfer out
+ * anything that's sent to it.
+ */
+contract DigitalMediaBurn {
+
+}
