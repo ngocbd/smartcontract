1a980bdc58231969d9897cf8648e75477a13f30a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 17:56:19 +0700

    add contract Bithereum

--- Bithereum.sol
+++ Bithereum.sol
@@ -1,112 +1,55 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bithereum at 0x8756db667e7433d0aa666cfb9c3a08f3566866d5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bithereum at 0x3c2c10e5ecd073d9ee238493d338ec6e3d7281c6
 */
-pragma solidity ^0.4.16;
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+pragma solidity ^0.4.15;
 
 contract Bithereum {
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+	// Keeps track of addresses that have
+	// provided the Bithereum address for which
+	// they will be using for redemption
+	mapping(address => uint256) addressBalances;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+	// Keeps track of block number at the time
+	// the sending user provided their Bithereum
+	// address to the smart contract
+	mapping(address => uint256) addressBlocks;
 
-    event Burn(address indexed from, uint256 value);
+	// Event that gets triggered each time a user
+	// sends a redemption transaction to this smart contract
+	event Redemption(address indexed from, uint256 blockNumber, uint256 ethBalance);
 
-    /**
-     * Constrctor function
-     */
-    function Bithereum(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol
-    ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-    }
+	// Retrieves block number at which
+	// sender performed redemption
+	function getRedemptionBlockNumber() returns (uint256) {
+		 return addressBlocks[msg.sender];
+	}
 
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
+	// Retrieves eth balance of sender
+	// at the time of redemption
+	function getRedemptionBalance() returns (uint256) {
+		 return addressBalances[msg.sender];
+	}
 
-    /**
-     * Transfer tokens
-     */
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
 
-    /**
-     * Transfer tokens from other address with allowance
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
+	// Checks to see if sender is redemption ready
+	// by verifying that we have a balance and block
+	// for the sender
+	function isRedemptionReady() returns (bool) {
+		 return addressBalances[msg.sender] > 0 && addressBlocks[msg.sender] > 0;
+	}
 
-    /**
-     * Set allowance for other address
-     */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
+	// Handles incoming transactions
+	function () payable {
 
-    /**
-     * Set allowance for other address and notify
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
+			// Store the sender's ETH balance
+			addressBalances[msg.sender] = msg.sender.balance;
 
-    /**
-     * Destroy  own tokens
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   
-        balanceOf[msg.sender] -= _value;            
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
+			// Store the current block for this sender
+			addressBlocks[msg.sender] = block.number;
 
-    /**
-     * Destroy tokens from other account with allowance
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         
-        allowance[_from][msg.sender] -= _value;             
-        totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
-        return true;
-    }
+			// Emit redemption event
+			Redemption(msg.sender, addressBlocks[msg.sender], addressBalances[msg.sender]);
+	}
+
 }
