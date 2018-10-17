b9c6ee4e363b0c4874c7c942b83ba1d7a298cb89
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 12:14:03 +0700

    add contract OuterWithEth

--- OuterWithEth.sol
+++ OuterWithEth.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OuterWithEth at 0xc37d670977715cac10801e2866dfd25cf00de2a5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OuterWithEth at 0xc1dc63ab0534112abeb75dce0b57178ab61a81cc
 */
 pragma solidity ^0.4.24;
 
@@ -15,17 +15,36 @@ contract Inner1WithEth {
     Inner2WithEth public myInner2 = new Inner2WithEth();
     
     function callSomeFunctionViaInner1() public payable{
-        myInner2.doSomething.value(msg.value)();
+        myInner2.callSomeFunctionViaInner2.value(msg.value)();
     }
 }
 
 contract Inner2WithEth {
+    Inner3WithEth public myInner3 = new Inner3WithEth();
+    
+    function callSomeFunctionViaInner2() public payable{
+        myInner3.callSomeFunctionViaInner3.value(msg.value)();
+    }
+}
+
+contract Inner3WithEth {
+    Inner4WithEth public myInner4 = new Inner4WithEth();
+    
+    function callSomeFunctionViaInner3() public payable{
+        myInner4.doSomething.value(msg.value)();
+    }
+}
+
+contract Inner4WithEth {
     uint256 someValue;
     event SetValue(uint256 val);
     
     function doSomething() public payable {
         someValue = block.timestamp;
         emit SetValue(someValue);
-        msg.sender.transfer(msg.value);
+    }
+    
+    function getAllMoneyOut() public {
+        msg.sender.transfer(this.balance);
     }
 }
