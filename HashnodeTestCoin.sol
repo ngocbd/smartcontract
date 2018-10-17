f9c9d6859886b2ad57ecbaeb61fb83ede14cc7e6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 00:27:16 +0700

    add contract HashnodeTestCoin

--- HashnodeTestCoin.sol
+++ HashnodeTestCoin.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0xb65c2cda44fba0378a742dc987109f54d9c1aabf
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract HashnodeTestCoin at 0x8067f84667544e7fb2085fd9dd847a15b6580e30
 */
 pragma solidity ^0.4.4;
 
@@ -108,11 +108,11 @@ contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
     function HashnodeTestCoin() {
-        balances[msg.sender] = 100000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 100000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Education";                                   // Set the name for display purposes (CHANGE THIS)
+        balances[msg.sender] = 50000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 50000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
+        name = "LGBT COMMUNITY";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "EDUN";                                             // Set the symbol for display purposes (CHANGE THIS)
+        symbol = "LGBT";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
