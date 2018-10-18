3fb45e5807b8a2f3bf9a9fd7231c601f71ab971e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:44:10 +0700

    add contract Gorgona

--- Gorgona.sol
+++ Gorgona.sol
@@ -1,21 +1,23 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gorgona at 0x20b3d3f8a3e4f7b23382d844f2ca1d7026d69479
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Gorgona at 0x020e13faf0955efef0ac9cd4d2c64c513ffcbdec
 */
 pragma solidity ^0.4.24;
 
 contract Gorgona {
     address public owner;
+    address public adminAddr;
+    uint constant public MASS_TRANSACTION_LIMIT = 150;
+    uint constant public MINIMUM_INVEST = 10000000000000000 wei;
+    uint constant public INTEREST = 3;
+    uint public depositAmount;
+    uint public round;
+    uint public lastPaymentDate;
+    GorgonaKiller public gorgonaKiller;
+    address[] public addresses;
+    mapping(address => Investor) public investors;
+    bool public pause;
 
-    uint constant PAYOUT_PER_INVESTOR_INTERVAL = 10 minutes;
-    uint constant INTEREST = 3;
-    uint private PAYOUT_CUMULATIVE_INTERVAL = 10 minutes;
-    uint private MINIMUM_INVEST = 10000000000000000 wei;
-
-    uint depositAmount;
-    uint investorCount;
-    uint public payoutDate;
-
-    struct investor
+    struct Investor
     {
         uint id;
         uint deposit;
@@ -24,161 +26,187 @@ contract Gorgona {
         address referrer;
     }
 
-    address[] public addresses;
+    struct GorgonaKiller
+    {
+        address addr;
+        uint deposit;
+    }
 
-    mapping(address => investor) public investors;
-
-    event Invest(address addr, uint amount);
-    event PayoutCumulative(uint amount, uint txs);
-    event PayoutSelf(address addr, uint amount);
-    event RefFee(address addr, uint amount);
-    event Cashback(address addr, uint amount);
+    event Invest(address addr, uint amount, address referrer);
+    event Payout(address addr, uint amount, string eventType, address from);
+    event NextRoundStarted(uint round, uint date, uint deposit);
+    event GorgonaKillerChanged(address addr, uint deposit);
 
     modifier onlyOwner {if (msg.sender == owner) _;}
 
     constructor() public {
         owner = msg.sender;
+        adminAddr = msg.sender;
         addresses.length = 1;
-        payoutDate = now;
+        round = 1;
+    }
+
+    function transferOwnership(address addr) onlyOwner public {
+        owner = addr;
+    }
+
+    function addInvestors(address[] _addr, uint[] _deposit, uint[] _date, address[] _referrer) onlyOwner public {
+        // add initiated investors
+        for (uint i = 0; i < _addr.length; i++) {
+            uint id = addresses.length;
+            if (investors[_addr[i]].deposit == 0) {
+                addresses.push(_addr[i]);
+                depositAmount += _deposit[i];
+            }
+
+            investors[_addr[i]] = Investor(id, _deposit[i], 1, _date[i], _referrer[i]);
+            emit Invest(_addr[i], _deposit  [i], _referrer[i]);
+
+            if (investors[_addr[i]].deposit > gorgonaKiller.deposit) {
+                gorgonaKiller = GorgonaKiller(_addr[i], investors[_addr[i]].deposit);
+            }
+        }
+        lastPaymentDate = now;
     }
 
     function() payable public {
-        if (msg.value == 0) {
+        if (owner == msg.sender) {
             return;
         }
 
-        require(msg.value >= MINIMUM_INVEST, "Too small amount, minimum 0.001 ether");
+        if (0 == msg.value) {
+            payoutSelf();
+            return;
+        }
 
-        investor storage user = investors[msg.sender];
+        require(false == pause, "Gorgona is restarting. Please wait.");
+        require(msg.value >= MINIMUM_INVEST, "Too small amount, minimum 0.01 ether");
+        Investor storage user = investors[msg.sender];
 
         if (user.id == 0) {
-            user.id = addresses.length;
+            // ensure that payment not from hacker contract
+            msg.sender.transfer(0 wei);
             addresses.push(msg.sender);
-            addresses.length++;
-            investorCount ++;
+            user.id = addresses.length;
+            user.date = now;
 
             // referrer
             address referrer = bytesToAddress(msg.data);
             if (investors[referrer].deposit > 0 && referrer != msg.sender) {
                 user.referrer = referrer;
             }
+        } else {
+            payoutSelf();
         }
 
         // save investor
         user.deposit += msg.value;
         user.deposits += 1;
-        user.date = now;
 
-        emit Invest(msg.sender, msg.value);
+        emit Invest(msg.sender, msg.value, user.referrer);
+
         depositAmount += msg.value;
+        lastPaymentDate = now;
 
-        // project fee
-        owner.transfer(msg.value / 5);
+        adminAddr.transfer(msg.value / 5); // project fee
+        uint bonusAmount = (msg.value / 100) * INTEREST; // referrer commission for all deposits
 
-        // referrer commission for all deposits
         if (user.referrer > 0x0) {
-            uint bonusAmount = (msg.value / 100) * INTEREST;
-            user.referrer.transfer(bonusAmount);
-            emit RefFee(user.referrer, bonusAmount);
-
-            // cashback only for first deposit
-            if (user.deposits == 1) {
-                msg.sender.transfer(bonusAmount);
-                emit Cashback(msg.sender, bonusAmount);
-            }
-        }
-    }
-
-    function payout(uint limit) public
-    {
-        require(now >= payoutDate + PAYOUT_CUMULATIVE_INTERVAL, "too fast payout request");
-
-        uint investorsPayout;
-        uint txs;
-        uint amount;
-
-        for (uint idx = addresses.length - 1; --idx >= 1;)
-        {
-            address addr = addresses[idx];
-            if (investors[addr].date + 24 hours > now) {
-                continue;
+            if (user.referrer.send(bonusAmount)) {
+                emit Payout(user.referrer, bonusAmount, "referral", msg.sender);
             }
 
-            amount = getInvestorUnPaidAmount(addr);
-            investors[addr].date = now;
-
-            if (address(this).balance < amount) {
-                selfdestruct(owner);
-                return;
+            if (user.deposits == 1) { // cashback only for the first deposit
+                if (msg.sender.send(bonusAmount)) {
+                    emit Payout(msg.sender, bonusAmount, "cash-back", 0);
+                }
             }
-
-            addr.transfer(amount);
-
-            investorsPayout += amount;
-            if (++txs >= limit) {
-                break;
+        } else if (gorgonaKiller.addr > 0x0) {
+            if (gorgonaKiller.addr.send(bonusAmount)) {
+                emit Payout(gorgonaKiller.addr, bonusAmount, "killer", msg.sender);
             }
         }
 
-        payoutDate = now;
-        emit PayoutCumulative(investorsPayout, txs);
+        if (user.deposit > gorgonaKiller.deposit) {
+            gorgonaKiller = GorgonaKiller(msg.sender, user.deposit);
+            emit GorgonaKillerChanged(msg.sender, user.deposit);
+        }
     }
 
-    function payoutSelf(address addr) public
+    function payout(uint offset) public
     {
-        require(addr == msg.sender, "You need specify your ETH address");
-
-        require(investors[addr].deposit > 0, "deposit not found");
-        require(now >= investors[addr].date + PAYOUT_PER_INVESTOR_INTERVAL, "too fast payment required");
-
-        uint amount = getInvestorUnPaidAmount(addr);
-        require(amount >= 1 finney, "too small unpaid amount");
-
-        investors[addr].date = now;
-        if (address(this).balance < amount) {
-            selfdestruct(owner);
+        if (pause == true) {
+            doRestart();
             return;
         }
 
-        addr.transfer(amount);
+        uint txs;
+        uint amount;
 
-        emit PayoutSelf(addr, amount);
+        for (uint idx = addresses.length - offset - 1; idx >= 1 && txs < MASS_TRANSACTION_LIMIT; idx--) {
+            address addr = addresses[idx];
+            if (investors[addr].date + 20 hours > now) {
+                continue;
+            }
+
+            amount = getInvestorDividendsAmount(addr);
+            investors[addr].date = now;
+
+            if (address(this).balance < amount) {
+                pause = true;
+                return;
+            }
+
+            if (addr.send(amount)) {
+                emit Payout(addr, amount, "bulk-payout", 0);
+            }
+
+            txs++;
+        }
     }
 
-    function getInvestorDeposit(address addr) public view returns (uint) {
-        return investors[addr].deposit;
+    function payoutSelf() private {
+        require(investors[msg.sender].id > 0, "Investor not found.");
+        uint amount = getInvestorDividendsAmount(msg.sender);
+
+        investors[msg.sender].date = now;
+        if (address(this).balance < amount) {
+            pause = true;
+            return;
+        }
+
+        msg.sender.transfer(amount);
+        emit Payout(msg.sender, amount, "self-payout", 0);
+    }
+
+    function doRestart() private {
+        uint txs;
+        address addr;
+
+        for (uint i = addresses.length - 1; i > 0; i--) {
+            addr = addresses[i];
+            addresses.length -= 1;
+            delete investors[addr];
+            if (txs++ == MASS_TRANSACTION_LIMIT) {
+                return;
+            }
+        }
+
+        emit NextRoundStarted(round, now, depositAmount);
+        pause = false;
+        round += 1;
+        depositAmount = 0;
+        lastPaymentDate = now;
+
+        delete gorgonaKiller;
     }
 
     function getInvestorCount() public view returns (uint) {
-        return investorCount;
+        return addresses.length - 1;
     }
 
-    function getDepositAmount() public view returns (uint) {
-        return depositAmount;
-    }
-
-    function getInvestorDatePayout(address addr) public view returns (uint) {
-        return investors[addr].date;
-    }
-
-    function getPayoutCumulativeInterval() public view returns (uint)
-    {
-        return PAYOUT_CUMULATIVE_INTERVAL;
-    }
-
-    function setDatePayout(address addr, uint date) onlyOwner public
-    {
-        investors[addr].date = date;
-    }
-
-    function setPayoutCumulativeInterval(uint interval) onlyOwner public
-    {
-        PAYOUT_CUMULATIVE_INTERVAL = interval;
-    }
-
-    function getInvestorUnPaidAmount(address addr) public view returns (uint)
-    {
-        return (((investors[addr].deposit / 100) * INTEREST) / 100) * ((now - investors[addr].date) * 100) / 1 days;
+    function getInvestorDividendsAmount(address addr) public view returns (uint) {
+        return investors[addr].deposit / 100 * INTEREST * (now - investors[addr].date) / 1 days;
     }
 
     function bytesToAddress(bytes bys) private pure returns (address addr) {
