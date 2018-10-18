5c30c7f7a2a56d4adaa681b90b130b49f0a261e9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:41:11 +0700

    add contract INFLIV

--- INFLIV.sol
+++ INFLIV.sol
@@ -1,265 +1,219 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract INFLIV at 0xe79d2818b0039052a77b0065948f0d94496bc775
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract INFLIV at 0x3656bd0f3f07623bb7f429b390d208f894e44ece
 */
-pragma solidity 0.4.24;
+pragma solidity 0.4.25;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+        return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
-contract ERC20 {
-  function totalSupply()public view returns (uint total_Supply);
-  function balanceOf(address who)public view returns (uint256);
-  function allowance(address owner, address spender)public view returns (uint);
-  function transferFrom(address from, address to, uint value)public returns (bool ok);
-  function approve(address spender, uint value)public returns (bool ok);
-  function transfer(address to, uint value)public returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+contract INFLIVERC20 {
+    function totalSupply() public view returns (uint total_Supply);
+    function balanceOf (address who) public view returns (uint256);
+    function allowance (address IFVOwner, address spender) public view returns (uint);
+    function transferFrom (address from, address to, uint value) public returns (bool ok);
+    function approve (address spender, uint value) public returns (bool ok);
+    function transfer (address to, uint value) public returns (bool ok);
+    event    Transfer (address indexed from, address indexed to, uint value);
+    event    Approval (address indexed IFVOwner, address indexed spender, uint value);
 }
 
 
-contract INFLIV is ERC20
-{ using SafeMath for uint256;
-    // Name of the token
-    string public constant name = "INFLIV";
-
-    // Symbol of token
-    string public constant symbol = "IFV";
-    uint8 public constant decimals = 18;
-    uint public _totalsupply = 70000000 * 10 ** 18; // 70 million total supply // muliplies dues to decimal precision
-    address public owner;                    // Owner of this contract
-    uint256 public _price_tokn_PRE = 6000;  // 1 Ether = 6000 coins
-    uint256 public _price_tokn_ICO_first = 3800;   // 1 Ether = 3800 coins
-    uint256 public _price_tokn_ICO_second = 2600;   // 1 Ether = 2600 coins
-    uint256 no_of_tokens;
+contract INFLIV is INFLIVERC20 { 
+    
+    using SafeMath for uint256;
+    
+    string  public constant name        = "INFLIV";                             // Name of the token
+    string  public constant symbol      = "IFV";                                // Symbol of token
+    uint8   public constant decimals    = 18;
+    
+    uint    public _totalsupply         = 70000000 * 10 ** 18;                  // 70 million Total Supply
+    uint256 maxPublicSale               = 22000000 * 10 ** 18;                  // 22 million Public Sale
+                                   
+    uint256 public PricePre             = 6000;                                 // 1 Ether = 6000 tokens in Pre-ICO
+    uint256 public PriceICO1            = 3800;                                 // 1 Ether = 3800 tokens in ICO Phase 1
+    uint256 public PriceICO2            = 2600;                                 // 1 Ether = 2600 tokens in ICO Phase 2
+    uint256 public PublicPrice          = 1800;                                 // 1 Ether = 1800 tokens in Public Sale
+    uint256 public PreStartTimeStamp;
+    uint256 public PreEndTimeStamp;
+    uint256 input_token;
     uint256 bonus_token;
     uint256 total_token;
-    bool stopped = false;
-    uint256 public pre_startdate;
-    uint256 public ico1_startdate;
-    uint256 ico_first;
-    uint256 ico_second;
-    uint256 pre_enddate;
-  
-    uint256 public eth_received; // total ether received in the contract
-    uint256 maxCap_public =  42000000 * 10 **18;  //  42 million in Public Sale
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
-
+    uint256 ICO1;
+    uint256 ICO2;
+    uint256 public ETHReceived;                                                 // Total ETH received in the contract
+    mapping (address => uint) balances;
+    mapping (address => mapping(address => uint)) allowed;
     
-     enum Stages {
+    address public IFVOwner;                                                    // Owner of this contract
+    bool stopped = false;
+
+    enum CurrentStages {
         NOTSTARTED,
-        PREICO,
+        PRE,
         ICO,
         PAUSED,
         ENDED
     }
-    Stages public stage;
     
-    modifier atStage(Stages _stage) {
+    CurrentStages public stage;
+    
+    modifier atStage(CurrentStages _stage) {
         if (stage != _stage)
             // Contract not in expected state
             revert();
         _;
     }
     
-     modifier onlyOwner() {
-        if (msg.sender != owner) {
+    modifier onlyOwner() {
+        if (msg.sender != IFVOwner) {
             revert();
         }
         _;
     }
 
-    function INFLIV() public
-    {
-        owner = msg.sender;
-        balances[owner] = 28000000 * 10 **18; // 28 million to owner
-        stage = Stages.NOTSTARTED;
-        Transfer(0, owner, balances[owner]);
+    function INFLIV() public {
+        IFVOwner            = msg.sender;
+        balances[IFVOwner]  = 48000000 * 10 ** 18;                              // 28 million to owner & 20 million to referral bonus
+        balances[address(this)] = maxPublicSale;
+        stage               = CurrentStages.NOTSTARTED;
+        Transfer (0, IFVOwner, balances[IFVOwner]);
+        Transfer (0, address(this), balances[address(this)]);
+    }
+  
+    function () public payable {
+        require(stage != CurrentStages.ENDED);
+        require(!stopped && msg.sender != IFVOwner);
+            if(stage == CurrentStages.PRE && now <= PreEndTimeStamp) { 
+                    require (ETHReceived <= 1500 ether);                        // Hardcap
+                    ETHReceived     = (ETHReceived).add(msg.value);
+                    input_token     = ((msg.value).mul(PricePre)); 
+                    bonus_token     = ((input_token).mul(50)).div(100);         // 50% bonus in Pre-ICO
+                    total_token     = input_token + bonus_token;
+                    transferTokens (msg.sender, total_token);
+            }
+            else if (now <= ICO2) {
+                    
+                if(now < ICO1)
+                {
+                    input_token     = (msg.value).mul(PriceICO1);
+                    bonus_token     = ((input_token).mul(25)).div(100);         // 25% bonus in ICO Phase 1
+                    total_token     = input_token + bonus_token;
+                    transferTokens (msg.sender, total_token);
+                }   
+                else if(now >= ICO1 && now < ICO2)
+                {
+                    input_token     = (msg.value).mul(PriceICO2);
+                    bonus_token     = ((input_token).mul(10)).div(100);         // 10% bonus in ICO Phase 2
+                    total_token     = input_token + bonus_token;
+                    transferTokens (msg.sender, total_token);
+                }
+            }
+            else
+            {
+                    input_token     = (msg.value).mul(PublicPrice);
+                    transferTokens (msg.sender, input_token);
+            }
     }
      
-      function () public payable 
-    
+    function start_ICO() public onlyOwner atStage(CurrentStages.NOTSTARTED)
     {
-              require(stage != Stages.ENDED);
-        require(!stopped && msg.sender != owner);
-            if( stage == Stages.PREICO && now <= pre_enddate )
-            { 
-                require (eth_received <= 100 ether);
-              eth_received = (eth_received).add(msg.value);
-              no_of_tokens =((msg.value).mul(_price_tokn_PRE));
-                bonus_token = ((no_of_tokens).mul(50)).div(100); // 50% bonus token
-                total_token = no_of_tokens + bonus_token;
-                transferTokens(msg.sender,total_token);
-               }
-               
-             else if(stage == Stages.ICO && now <= ico_second){
-                    
-                if( now < ico_first )
-            {
-              no_of_tokens =(msg.value).mul(_price_tokn_ICO_first);
-                bonus_token = ((no_of_tokens).mul(25)).div(100); // 25% bonus token
-                total_token = no_of_tokens + bonus_token;
-                transferTokens(msg.sender,total_token);
-                
-                
-            }   
-            
-              else if(now >= ico_first && now < ico_second)
-            {
-                
-                
-                  no_of_tokens =(msg.value).mul(_price_tokn_ICO_first);
-                bonus_token = ((no_of_tokens).mul(10)).div(100); // 10% bonus token
-                total_token = no_of_tokens + bonus_token;
-                transferTokens(msg.sender,total_token);
-                
-        }
-             }
-        else
-        {
-            revert();
-        }
-    
+        stage                   = CurrentStages.PRE;
+        stopped                 = false;
+        PreStartTimeStamp       = now;
+        PreEndTimeStamp         = now + 20 days;
+        ICO1                    = PreEndTimeStamp + 20 days;
+        ICO2                    = ICO1 + 20 days;
     }
-     function start_PREICO() public onlyOwner atStage(Stages.NOTSTARTED)
-      {
-          stage = Stages.PREICO;
-          stopped = false;
-           balances[address(this)] =  maxCap_public;
-          pre_startdate = now;
-          pre_enddate = now + 20 days;
-          Transfer(0, address(this), balances[address(this)]);
-          }
-      
-      function start_ICO() public onlyOwner atStage(Stages.PREICO)
-      {
-          require(now > pre_enddate || eth_received >= 100 ether);
-          stage = Stages.ICO;
-          stopped = false;
-          ico1_startdate = now;
-           ico_first = now + 20 days;
-          ico_second = ico_first + 20 days;
-          Transfer(0, address(this), balances[address(this)]);
-      }
     
-    // called by the owner, pause ICO
     function PauseICO() external onlyOwner
     {
         stopped = true;
-       }
+    }
 
-    // called by the owner , resumes ICO
     function ResumeICO() external onlyOwner
     {
         stopped = false;
-      }
+    }
    
-     
-     
-     function end_ICO() external onlyOwner atStage(Stages.ICO)
-     {
-         require(now > ico_second);
-         stage = Stages.ENDED;
-         _totalsupply = (_totalsupply).sub(balances[address(this)]);
-         balances[address(this)] = 0;
-         Transfer(address(this), 0 , balances[address(this)]);
-         
-     }
-
-    // what is the total supply of the ech tokens
-     function totalSupply() public view returns (uint256 total_Supply) {
-         total_Supply = _totalsupply;
-     }
-    
-    // What is the balance of a particular account?
-     function balanceOf(address _owner)public view returns (uint256 balance) {
-         return balances[_owner];
-     }
-    
-    // Send _value amount of tokens from address _from to address _to
-     // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-     // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-     // fees in sub-currencies; the command should fail unless the _from account has
-     // deliberately authorized the sender of the message via some mechanism; we propose
-     // these standardized APIs for approval:
-     function transferFrom( address _from, address _to, uint256 _amount )public returns (bool success) {
-     require( _to != 0x0);
-     require(balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
-     balances[_from] = (balances[_from]).sub(_amount);
-     allowed[_from][msg.sender] = (allowed[_from][msg.sender]).sub(_amount);
-     balances[_to] = (balances[_to]).add(_amount);
-     Transfer(_from, _to, _amount);
-     return true;
-         }
-    
-   // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-     // If this function is called again it overwrites the current allowance with _value.
-     function approve(address _spender, uint256 _amount)public returns (bool success) {
-         require( _spender != 0x0);
-         allowed[msg.sender][_spender] = _amount;
-         Approval(msg.sender, _spender, _amount);
-         return true;
-     }
-  
-     function allowance(address _owner, address _spender)public view returns (uint256 remaining) {
-         require( _owner != 0x0 && _spender !=0x0);
-         return allowed[_owner][_spender];
-   }
-
-     // Transfer the balance from owner's account to another account
-     function transfer(address _to, uint256 _amount)public returns (bool success) {
-        require( _to != 0x0);
-        require(balances[msg.sender] >= _amount && _amount >= 0);
-        balances[msg.sender] = (balances[msg.sender]).sub(_amount);
-        balances[_to] = (balances[_to]).add(_amount);
-        Transfer(msg.sender, _to, _amount);
-             return true;
-         }
-    
-          // Transfer the balance from owner's account to another account
-    function transferTokens(address _to, uint256 _amount) private returns(bool success) {
-        require( _to != 0x0);       
-        require(balances[address(this)] >= _amount && _amount > 0);
-        balances[address(this)] = (balances[address(this)]).sub(_amount);
-        balances[_to] = (balances[_to]).add(_amount);
-        Transfer(address(this), _to, _amount);
-        return true;
-        }
- 
-    
-    function drain() external onlyOwner {
-        owner.transfer(this.balance);
+    function end_ICO() external onlyOwner atStage(CurrentStages.PRE)
+    {
+        require (now > ICO2);
+        stage                       = CurrentStages.ENDED;
+        _totalsupply                = (_totalsupply).sub(balances[address(this)]);
+        balances[address(this)]     = 0;
+        Transfer (address(this), 0 , balances[address(this)]);
     }
     
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
+        require (_to != 0x0);
+        require (balances[_from]    >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
+        balances[_from]             = (balances[_from]).sub(_amount);
+        allowed[_from][msg.sender]  = (allowed[_from][msg.sender]).sub(_amount);
+        balances[_to]               = (balances[_to]).add(_amount);
+        Transfer (_from, _to, _amount);
+        return true;
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool success) {
+        require (_to != 0x0);
+        require (balances[msg.sender]       >= _amount && _amount >= 0);
+        balances[msg.sender]                = (balances[msg.sender]).sub(_amount);
+        balances[_to]                       = (balances[_to]).add(_amount);
+        Transfer (msg.sender, _to, _amount);
+        return true;
+    }
+    
+    function transferTokens(address _to, uint256 _amount) private returns (bool success) {
+        require (_to != 0x0);       
+        require (balances[address(this)]    >= _amount && _amount > 0);
+        balances[address(this)]             = (balances[address(this)]).sub(_amount);
+        balances[_to]                       = (balances[_to]).add(_amount);
+        Transfer (address(this), _to, _amount);
+        return true;
+    }
+ 
+    function withdrawETH() external onlyOwner {
+        IFVOwner.transfer(this.balance);
+    }
+    
+    function approve(address _spender, uint256 _amount) public returns (bool success) {
+        require (_spender != 0x0);
+        allowed[msg.sender][_spender] = _amount;
+        Approval (msg.sender, _spender, _amount);
+        return true;
+    }
+  
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        require (_owner != 0x0 && _spender !=0x0);
+        return allowed[_owner][_spender];
+    }
+
+    function totalSupply() public view returns (uint256 total_Supply) {
+        total_Supply                = _totalsupply;
+    }
+    
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 }
