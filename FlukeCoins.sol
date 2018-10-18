21adc502a18bcf1f619e185b111e6956ef963180
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:27:19 +0700

    add contract FlukeCoins

--- FlukeCoins.sol
+++ FlukeCoins.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlukeCoins at 0x7fd292eecae66ba59dff6bc8e819f01dd7cb42dd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlukeCoins at 0xebce4d7e04362ba0c820d76e964419e0bcfc0c80
 */
 pragma solidity ^0.4.4;
 
@@ -100,20 +100,20 @@ contract StandardToken is Token {
     string public name;                   // Token Name
     uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
     string public symbol;                 // An identifier: eg SBX, XPR etc..
-    string public version = 'FlukeCoins'; 
-    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
+    string public version = 'Contract V1.0'; 
+    uint256 public unitsOneEthCanBuy = 10000000;     // How many units of your coin can be bought by 1 ETH?
     uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
     address public fundsWallet;           // Where should the raised ETH go?
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function FlukeCoins() {
+    function Lol1CoinCoin() {
         balances[msg.sender] = 1000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 1000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "FlukeCoins";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 5;                                               // Amount of decimals for display purposes (CHANGE THIS)
         symbol = "FLUK";                                             // Set the symbol for display purposes (CHANGE THIS)
-        fundsWallet = 0x79F28af6933615D56A9C55A5cb09fD31Db8E6E98;                                    // The owner of the contract gets ETH
+        fundsWallet = 0x9C03D646D3A7F93a0869CaF922D4144c79920a37;                                    // The owner of the contract gets ETH
     }
 
     function() payable{
