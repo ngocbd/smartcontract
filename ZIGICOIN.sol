9c79e2019869e800ed523891ae1c2f45c2cdc1cf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:19:31 +0700

    add contract Zigicoin

--- ZIGICOIN.sol
+++ ZIGICOIN.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Zigicoin at 0x3ddeac1e7bd314a86d1510d7b11b47d16701939e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Zigicoin at 0x0d6e877899956f0936dbe36397fd98683bb2afee
 */
 pragma solidity ^0.4.4;
 
@@ -104,16 +104,16 @@ contract Zigicoin is StandardToken { // CHANGE THIS. Update the contract name.
     uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
     address  fundsWallet;           // Where should the raised ETH go?
 
-     // This is a constructor function 
+    // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function Zigicoin() {
+    function ZIGICOIN () {
         balances[msg.sender] = 1000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000000000000000000;                        // Update total supply (1000 for example) 
-        name = "Zigicoin";                                   // Set the name for display purposes 
-        decimals = 8;                                               // Amount of decimals for display purposes 
-        symbol = "zigi";                                             // Set the symbol for display purposes 
-                                              // Set the price of your token for the ICO 
-        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH 
+        totalSupply = 1000000000000000000;                        // Update total supply (1000 for example) (1000000000000000000)
+        name = "ZIGICOIN";                                   // Set the name for display purposes (ZIGICOIN)
+        decimals = 8;                                               // Amount of decimals for display purposes (ZIGICOIN)
+        symbol = "ZIGI";                                             // Set the symbol for display purposes (ZIGICOIN)
+                                           
+        fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
                              
 
