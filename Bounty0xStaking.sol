58edbe8ace7cce16703b1ce4dc54076935513b7d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:59:56 +0700

    add contract Bounty0xStaking

--- Bounty0xStaking.sol
+++ Bounty0xStaking.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bounty0xStaking at 0x130c640ae4916d20987781dbce83db7219eb1101
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Bounty0xStaking at 0xf7e299bfc9d4c510273f9e7ce6365d8be2b63ede
 */
 pragma solidity ^0.4.23;
 
@@ -220,41 +220,69 @@ contract Bounty0xStaking is Ownable, Pausable {
     using SafeMath for uint256;
 
     address public Bounty0xToken;
+    uint public lockTime;
 
     mapping (address => uint) public balances;
     mapping (uint => mapping (address => uint)) public stakes; // mapping of submission ids to mapping of addresses that staked an amount of bounty token
-
-
+    mapping (address => uint) public huntersLockDateTime;
+    mapping (address => uint) public huntersLockAmount;
+    
+    
     event Deposit(address indexed depositor, uint amount, uint balance);
     event Withdraw(address indexed depositor, uint amount, uint balance);
-
-    event Stake(uint indexed submissionId, address hunter, uint amount, uint balance);
-    event StakeReleased(uint indexed submissionId, address from, address to, uint amount);
+    event Stake(uint indexed submissionId, address indexed hunter, uint amount, uint balance);
+    event StakeReleased(uint indexed submissionId, address indexed from, address indexed to, uint amount);
+    event Lock(address indexed hunter, uint amount, uint endDateTime);
+    event Unlock(address indexed hunter, uint amount);
 
 
     constructor(address _bounty0xToken) public {
         Bounty0xToken = _bounty0xToken;
+        lockTime = 30 days;
     }
+    
 
-
-    function deposit(uint _amount) public whenNotPaused {
+    function deposit(uint _amount) external whenNotPaused {
         //remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
         require(ERC20(Bounty0xToken).transferFrom(msg.sender, this, _amount));
         balances[msg.sender] = SafeMath.add(balances[msg.sender], _amount);
 
         emit Deposit(msg.sender, _amount, balances[msg.sender]);
     }
-
-    function withdraw(uint _amount) public whenNotPaused {
+    
+    function withdraw(uint _amount) external whenNotPaused {
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
         require(ERC20(Bounty0xToken).transfer(msg.sender, _amount));
 
         emit Withdraw(msg.sender, _amount, balances[msg.sender]);
     }
+    
+    
+    function lock(uint _amount) external whenNotPaused {
+        require(_amount != 0);
+        require(balances[msg.sender] >= _amount);
+        
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
+        huntersLockAmount[msg.sender] = SafeMath.add(huntersLockAmount[msg.sender], _amount);
+        huntersLockDateTime[msg.sender] = SafeMath.add(now, lockTime);
+        
+        emit Lock(msg.sender, huntersLockAmount[msg.sender], huntersLockDateTime[msg.sender]);
+    }
+    
+    function unlock() external whenNotPaused {
+        require(huntersLockDateTime[msg.sender] <= now);
+        uint amountLocked = huntersLockAmount[msg.sender];
+        require(amountLocked != 0);
+        
+        huntersLockAmount[msg.sender] = SafeMath.sub(huntersLockAmount[msg.sender], amountLocked);
+        balances[msg.sender] = SafeMath.add(balances[msg.sender], amountLocked);
+        
+        emit Unlock(msg.sender, amountLocked);
+    }
 
 
-    function stake(uint _submissionId, uint _amount) public whenNotPaused {
+    function stake(uint _submissionId, uint _amount) external whenNotPaused {
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
         stakes[_submissionId][msg.sender] = SafeMath.add(stakes[_submissionId][msg.sender], _amount);
@@ -262,7 +290,7 @@ contract Bounty0xStaking is Ownable, Pausable {
         emit Stake(_submissionId, msg.sender, _amount, balances[msg.sender]);
     }
 
-    function stakeToMany(uint[] _submissionIds, uint[] _amounts) public whenNotPaused {
+    function stakeToMany(uint[] _submissionIds, uint[] _amounts) external whenNotPaused {
         uint totalAmount = 0;
         for (uint j = 0; j < _amounts.length; j++) {
             totalAmount = SafeMath.add(totalAmount, _amounts[j]);
@@ -278,41 +306,49 @@ contract Bounty0xStaking is Ownable, Pausable {
     }
 
 
-    function releaseStake(uint _submissionId, address _from, address _to, uint _amount) public onlyOwner {
-        require(stakes[_submissionId][_from] >= _amount);
+    function releaseStake(uint _submissionId, address _from, address _to) external onlyOwner {
+        require(stakes[_submissionId][_from] != 0);
 
-        stakes[_submissionId][_from] = SafeMath.sub(stakes[_submissionId][_from], _amount);
-        balances[_to] = SafeMath.add(balances[_to], _amount);
-
-        emit StakeReleased(_submissionId, _from, _to, _amount);
+        balances[_to] = SafeMath.add(balances[_to], stakes[_submissionId][_from]);
+        emit StakeReleased(_submissionId, _from, _to, stakes[_submissionId][_from]);
+        
+        stakes[_submissionId][_from] = 0;
     }
 
-    function releaseManyStakes(uint[] _submissionIds, address[] _from, address[] _to, uint[] _amounts) public onlyOwner {
+    function releaseManyStakes(uint[] _submissionIds, address[] _from, address[] _to) external onlyOwner {
         require(_submissionIds.length == _from.length &&
-                _submissionIds.length == _to.length &&
-                _submissionIds.length == _amounts.length);
+                _submissionIds.length == _to.length);
 
         for (uint i = 0; i < _submissionIds.length; i++) {
-            require(stakes[_submissionIds[i]][_from[i]] >= _amounts[i]);
-            stakes[_submissionIds[i]][_from[i]] = SafeMath.sub(stakes[_submissionIds[i]][_from[i]], _amounts[i]);
-            balances[_to[i]] = SafeMath.add(balances[_to[i]], _amounts[i]);
-
-            emit StakeReleased(_submissionIds[i], _from[i], _to[i], _amounts[i]);
+            require(_from[i] != address(0));
+            require(_to[i] != address(0));
+            require(stakes[_submissionIds[i]][_from[i]] != 0);
+            
+            balances[_to[i]] = SafeMath.add(balances[_to[i]], stakes[_submissionIds[i]][_from[i]]);
+            emit StakeReleased(_submissionIds[i], _from[i], _to[i], stakes[_submissionIds[i]][_from[i]]);
+            
+            stakes[_submissionIds[i]][_from[i]] = 0;
         }
     }
+    
 
+    function changeLockTime(uint _periodInSeconds) external onlyOwner {
+        lockTime = _periodInSeconds;
+    }
+    
+    
     // Burnable mechanism
 
     address public bntyController;
 
-    event Burn(uint indexed submissionId, address from, uint amount);
+    event Burn(uint indexed submissionId, address indexed from, uint amount);
 
-    function changeBntyController(address _bntyController) onlyOwner public {
+    function changeBntyController(address _bntyController) external onlyOwner {
         bntyController = _bntyController;
     }
 
 
-    function burnStake(uint _submissionId, address _from) public onlyOwner {
+    function burnStake(uint _submissionId, address _from) external onlyOwner {
         require(stakes[_submissionId][_from] > 0);
 
         uint amountToBurn = stakes[_submissionId][_from];
@@ -322,4 +358,10 @@ contract Bounty0xStaking is Ownable, Pausable {
         emit Burn(_submissionId, _from, amountToBurn);
     }
 
+
+    // in case of emergency
+    function emergentWithdraw() external onlyOwner {
+        require(ERC20(Bounty0xToken).transfer(msg.sender, this.balance));
+    }
+    
 }
