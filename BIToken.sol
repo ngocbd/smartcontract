b3237973d90325c13f567e2042bb78d01f5695a1
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 12:18:50 +0700

    add contract BIToken

--- BIToken.sol
+++ BIToken.sol
@@ -1,173 +1,284 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BIToken at 0xfbd11f51729d18168cd8af8219bfaa22549cda33
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BIToken at 0x403323ee9541ca541c412f9b9055de4619d2e7f8
 */
-pragma solidity ^0.4.16;
+/**
+ * @autor  Olga Solodina <olgasolidity@gmail.com>
+ */
 
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        require(c / a == b);
-        return c;
-    }
+pragma solidity ^0.4.24;
 
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        require(a == b * c + a % b); // There is no case in which this doesn’t hold
-        return c;
-    }
 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        return a-b;
-    }
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-        return c;
-    }
+/**
+ * @title ERC20 interface
+ */
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address _who) external view returns (uint256);
+
+  function allowance(address _owner, address _spender)
+    external view returns (uint256);
+
+  function transfer(address _to, uint256 _value) external returns (bool);
+
+  function approve(address _spender, uint256 _value)
+    external returns (bool);
+
+  function transferFrom(address _from, address _to, uint256 _value)
+    external returns (bool);
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
 }
 
-contract BIToken {
 
-    ///Public variables of the token
-    string public constant name = "Basic Integration Token";
-    uint8 public constant decimals = 18;
-    string public constant symbol="BINTO";
-    string public constant version = "1.0";
-    using SafeMath for uint256;
-    address public ownerAccount;
-    uint256 public totalSupply;
-    
-    //initialSupply of token without decimals
-    uint256 public constant initialSupply = 1000 * (10**6); //total tokens without decimals
-    
-    //initial rate
-    uint256 public purchaseRate = 10000;
-    
-    //to finish the sale
-    bool public isSaleEnded = false;
 
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
-    
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event Burn(address indexed burner, uint256 value);
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that revert on error
+ */
+library SafeMath {
 
-    constructor () public {
-        ownerAccount = msg.sender;
-        totalSupply = initialSupply*(10**uint256(decimals));
-        balances[msg.sender] = totalSupply; 
-    }
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
     
-    //setting Modifier
-    modifier onlyOwner() {
-        require (msg.sender == ownerAccount);
-        _;
-    }
-    
-    //purchase rate is changed by  owner in ico and post ico phases
-    function setPurchaseRate(uint newRate) public onlyOwner {
-        require(purchaseRate != newRate);
-        purchaseRate = newRate;
-    }
-    
-    //burn token held by  owner only in case he decides so to destroy
-    //any extra tokens in far future.
-    function burnToken(uint numberOfTokens) public onlyOwner {
-        require(numberOfTokens > 0);
-        require(balances[msg.sender] >= numberOfTokens);
-        balances[msg.sender] = balances[msg.sender].sub(numberOfTokens);
-        totalSupply = totalSupply.sub(numberOfTokens);
-        emit Burn(msg.sender,numberOfTokens);
-        emit Transfer(msg.sender, address(0), numberOfTokens);
+    if (_a == 0) {
+      return 0;
     }
 
-    function endSale() public onlyOwner {
-        isSaleEnded = true;
-    }
+    uint256 c = _a * _b;
+    require(c / _a == _b);
 
-    //sending Eather to Owner
-    function sendEtherToOwner() payable public {
-        uint256 owneramount = msg.value;
-        require(isSaleEnded == false);
-        require(owneramount > 0);
-        uint256 tokens = purchaseRate.mul(owneramount);
-        require(tokens > 0);
-        //increse allowances , those will be reduced in transferFrom method again
-        allowed[ownerAccount][msg.sender] = allowed[ownerAccount][msg.sender].add(tokens);
-        transferFrom(ownerAccount,msg.sender, tokens);
-        ownerAccount.transfer(owneramount);
-    }
-        
-    
-    //Transfer Token directly to Owner 
-    function () payable public {
-        sendEtherToOwner();
-    }
-    
-    
-    function transfer(address _to, uint256 _value) public  returns (bool){
-        
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        //check condition to avoid Overflows
-        require(_value > 0);
-        require(balances[msg.sender] >= _value);
-        require(balances[_to] + _value > balances[_to]);
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
-    
-     
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-      
-        require(_value > 0);
-        require(balances[_from] >= _value);
-        require(allowed[_from][msg.sender] >= _value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-    
-    function balanceOf(address _owner) public view returns (uint256) {
-        return balances[_owner];
-    }
+    return c;
+  }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = _a / _b;
 
-    function allowance(address _owner, address _spender) public view returns (uint256) {
-        return allowed[_owner][_spender];
-    }
-    
-    function increaseApproval(address _spender,uint _addedValue) public returns (bool){
-        allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
+    return c;
+  }
 
-    function decreaseApproval(address _spender,uint _subtractedValue) public returns (bool){
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }    
-    
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b <= _a);
+    uint256 c = _a - _b;
+
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    uint256 c = _a + _b;
+    require(c >= _a);
+
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ */
+
+contract ERC20 is IERC20 {
+
+  using SafeMath for uint256;
+
+  mapping (address => uint256) internal balances_;
+
+  mapping (address => mapping (address => uint256)) private allowed_;
+
+  uint256 internal totalSupply_;
+
+  /**
+  * @dev Total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256) {
+    return balances_[_owner];
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(
+    address _owner,
+    address _spender
+   )
+    public
+    view
+    returns (uint256)
+  {
+    return allowed_[_owner][_spender];
+  }
+
+  /**
+  * @dev Transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances_[msg.sender]);
+    require(_to != address(0));
+
+    balances_[msg.sender] = balances_[msg.sender].sub(_value);
+    balances_[_to] = balances_[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed_[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _value
+  )
+    public
+    returns (bool)
+  {
+    require(_value <= balances_[_from]);
+    require(_value <= allowed_[_from][msg.sender]);
+    require(_to != address(0));
+
+    balances_[_from] = balances_[_from].sub(_value);
+    balances_[_to] = balances_[_to].add(_value);
+    allowed_[_from][msg.sender] = allowed_[_from][msg.sender].sub(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * @param _spender The address which will spend the funds.
+   * @param _addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseApproval(
+    address _spender,
+    uint256 _addedValue
+  )
+    public
+    returns (bool)
+  {
+    allowed_[msg.sender][_spender] = (
+      allowed_[msg.sender][_spender].add(_addedValue));
+    emit Approval(msg.sender, _spender, allowed_[msg.sender][_spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * @param _spender The address which will spend the funds.
+   * @param _subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseApproval(
+    address _spender,
+    uint256 _subtractedValue
+  )
+    public
+    returns (bool)
+  {
+    uint256 oldValue = allowed_[msg.sender][_spender];
+    if (_subtractedValue >= oldValue) {
+      allowed_[msg.sender][_spender] = 0;
+    } else {
+      allowed_[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    emit Approval(msg.sender, _spender, allowed_[msg.sender][_spender]);
+    return true;
+  }
+
+}
+
+
+/**
+ * @title Token
+ * @dev All tokens are pre-assigned to the creator.
+ */
+
+contract BIToken is ERC20 {
+
+  string public constant name = "BIToken";
+  string public constant symbol = "BI";
+  uint8 public constant decimals = 12;
+
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  constructor() public {
+
+    totalSupply_ = 20480000 * (10 ** uint256(decimals));
+
+    balances_[msg.sender] = totalSupply_;
+    emit Transfer(address(0), msg.sender, totalSupply_);
+  }
+
 }
