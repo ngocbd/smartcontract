bb90e5d71de251b78243d75eda38caf058e34983
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:14:51 +0700

    add contract MultiTransfer

--- Multitransfer.sol
+++ Multitransfer.sol
@@ -1,94 +1,73 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0x77a3181fa59f63edf8dec0f75fa8707f3d1df719
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiTransfer at 0x1a3fddb782d87c9b7110aea514bd0a0ca97c1913
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
+// Professor Rui-Shan Lu Team
+// Rs Lu  <rslu2000@gmail.com>
+// Lursun <lursun914013@gmail.com>
 
-
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+contract ERC20{
+    function transfer(address _to, uint256 _value) public;
+    function transferFrom(address _from, address _to, uint256 _value) public;
+    function approve(address spender, uint tokens) public;
 }
 
 
+contract Owned {
+    address public owner;
 
-
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-contract MultiTransfer is Ownable {
-
-    function MultiTransfer() public {
-
+    constructor() public {
+        owner = msg.sender;
     }
 
-    function transfer(address token,address[] to, uint[] value) public onlyOwner {
-        require(to.length == value.length);
-        require(token != address(0));
+    modifier onlyOwner {
+        require(msg.sender == owner, "msg.sender != owner");
+        _;
+    }
 
-        ERC20 t = ERC20(token);
-        for (uint i = 0; i < to.length; i++) {
-            t.transfer(to[i], value[i]);
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+
+contract MultiTransfer is Owned {
+    constructor() Owned() public {}
+    event MultiTransferEvent(address tokenContract, uint amount);
+    function send(ERC20 ercContract,address[] memory addresses, uint[] memory values, function(ERC20, address, uint) func) private {
+        require(addresses.length == values.length, "addresses.length != values.length");
+        // require(addresses.length <= 255, "too many people");
+        uint amount;
+        
+        for (uint64 i = 0; i < addresses.length; i++) {
+            amount += values[i];
+            func(ercContract, addresses[i],values[i]);
         }
+        emit MultiTransferEvent(ercContract, amount);
+    }
+    function ethTransfer(ERC20 ercContract, address target, uint amount) private {
+        target.transfer(amount);
+    }
+    function transfer(ERC20 ercContract, address target, uint amount) private {
+        ercContract.transfer(target, amount);
+    }
+    function transferFrom(ERC20 ercContract, address target, uint amount) private {
+        ercContract.transferFrom(msg.sender, target, amount);
+    }
+    
+    function mutiEthTransfer(address[] addresses, uint[] values) external payable{
+        send(ERC20(0x0), addresses, values, ethTransfer);
+        if (address(this).balance > 0) {
+            ethTransfer(ERC20(0x0), msg.sender, address(this).balance);
+        }
+    }
+    
+    function mutiTransfer(ERC20 tokenContract, address[] addresses, uint[] values) external onlyOwner {
+        send(tokenContract, addresses, values, transfer);
+    }
+    
+    function mutiTransferFrom(ERC20 tokenContract, address[] addresses, uint[] values) external {
+        send(tokenContract, addresses, values, transferFrom);
     }
 }
