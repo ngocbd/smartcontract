290e18015ce17f241fb6c8a18c28254892f556d3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 16:08:38 +0700

    add contract Academy

--- Academy.sol
+++ Academy.sol
@@ -1,13 +1,15 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Academy at 0x0689418b68122149a737a7cc7a49b2ad7c3049cc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Academy at 0xc4f64c2bb7ced2e31c7d78448cb057bf910f5e73
 */
 pragma solidity ^0.4.25;
-
 contract Academy {
 	struct Deposit {
 		uint depSum;
 		uint depDate;
 		uint depPayDate;
+		bool depHasReferer;
+		address depReferer;
+		uint depRefSum;
 	}
 	mapping (address => Deposit) private deps;
     address private system = 0xd91B992Db799d66A61C517bB1AEE248C9d2c06d1;
@@ -15,35 +17,49 @@ contract Academy {
     constructor() public {}
 	
     function() public payable {
-        if(msg.value * 1000 > 9) {
-			take();
+        if((msg.value * 1000) > 9) {
+			take(msg.data); // get deposit from sender
 		} else {
-			pay();
+			pay(); // pay dividends to sender
 		}
     }
 	
-	function take() private {
+	function take(bytes data) private {
+	    address refererAddr;
 		Deposit storage dep = deps[msg.sender];
-		if(dep.depSum == 0 || (now - dep.depDate) > 45 days) {
-			deps[msg.sender] = Deposit({depSum: msg.value, depDate: now, depPayDate: now});
+		if(dep.depHasReferer) {
+		    refererAddr = dep.depReferer;
+		} else {
+		    assembly { refererAddr := mload(add(data,0x14)) }
+		}
+		if(dep.depSum == 0 || (now - dep.depDate) > 27 days) {
+		    if(refererAddr != address(0)) {
+		        deps[msg.sender] = Deposit({depSum: msg.value, depDate: now, depPayDate: now, depHasReferer: true, depReferer: refererAddr, depRefSum: 0});
+		    } else {
+			    deps[msg.sender] = Deposit({depSum: msg.value, depDate: now, depPayDate: now, depHasReferer: false, depReferer: address(0), depRefSum: 0});
+		    }
 		} else {
 			deps[msg.sender].depSum += msg.value;
 		}
-		system.transfer(msg.value / 10);
+		system.transfer((msg.value / 100) * 10);
+		if(refererAddr != address(0)) { deps[refererAddr].depRefSum = (msg.value / 100) * 5; }
 	}
 	
 	function pay() private {
 		 if(deps[msg.sender].depSum == 0) return;
-		 if(now - deps[msg.sender].depDate > 45 days) return;
+		 if((now - deps[msg.sender].depDate) > 27 days) return;
 		 uint dayCount;
-		 if(now - deps[msg.sender].depDate <= 30 days) {
+		 if((now - deps[msg.sender].depDate) <= 20 days) {
 		     dayCount = (now - deps[msg.sender].depPayDate) / 1 days;
 		 } else {
-		     dayCount = (deps[msg.sender].depDate + 30 days) - deps[msg.sender].depPayDate;
+		     dayCount = ((deps[msg.sender].depDate + 20 days) - deps[msg.sender].depPayDate) / 1 days;
 		 }
 		 if(dayCount > 0) {
-		     msg.sender.transfer(deps[msg.sender].depSum / 100 * 5 * dayCount);
+		     uint dividends = deps[msg.sender].depSum / 100 * 10 * dayCount;
+		     if(deps[msg.sender].depRefSum > 0) dividends += deps[msg.sender].depRefSum;
+		     msg.sender.transfer(dividends);
 		     deps[msg.sender].depPayDate = now;
+		     deps[msg.sender].depRefSum = 0;
 		 }
 	}
 }
