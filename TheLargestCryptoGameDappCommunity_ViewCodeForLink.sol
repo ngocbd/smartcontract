f1b337b16d78ec6fdaf6383ed40461f7a4604ff8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:48:34 +0700

    add contract TheLargestCryptoGameDappCommunity_ViewCodeForLink

---
+++ TheLargestCryptoGameDappCommunity_ViewCodeForLink.sol
@@ -0,0 +1,13 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TheLargestCryptoGameDappCommunity_ViewCodeForLink at 0x5adfd9befa72aa5c34929e6f70e7cad4fe322127
+*/
+pragma solidity ^0.4.25;
+
+// Hi, we are the largest crypto game / dapp community. We like talking about and sharing the latest dapps - CryptoKitties, POWH, FOMO, etc. We have a lot of strong developers and consumers and investors.
+// We welcome sharing and talking about Eth dapps, games, and more. Please join us!
+// Link is here: https://discord.gg/HeAUEp
+
+contract TheLargestCryptoGameDappCommunity_ViewCodeForLink {
+    
+    
+}
