5a3b78557c9e8b2ec5990a6aeee74d9400d7b15d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:31:05 +0700

    add contract LICToken

--- LICToken.sol
+++ LICToken.sol
@@ -1,126 +1,418 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LICToken at 0xbff7c92423569612422aa9c220acf1247571ac37
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LICToken at 0xe8aeaf2777aa2c6d3d4312b07ac2ead1cf92040e
 */
-pragma solidity 0.4.20;
+pragma solidity ^0.4.18;
+contract DSNote {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  guy,
+        bytes32  indexed  foo,
+        bytes32  indexed  bar,
+	    uint	 	      wad,
+        bytes             fax
+    ) anonymous;
 
-contract Token {
+    modifier note {
+        bytes32 foo;
+        bytes32 bar;
 
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint supply) {}
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint balance) {}
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint _value) returns (bool success) {}
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
+        assembly {
+            foo := calldataload(4)
+            bar := calldataload(36)
+        }
+        LogNote(msg.sig, msg.sender, foo, bar, msg.value, msg.data);
+        _;
+    }
 }
 
-contract RegularToken is Token {
-
-    function transfer(address _to, uint _value) returns (bool) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        if (balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function transferFrom(address _from, address _to, uint _value) returns (bool) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint _value) returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint) {
-        return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint) balances;
-    mapping (address => mapping (address => uint)) allowed;
-    uint public totalSupply;
+contract DSAuthority {
+    function canCall(
+        address src, address dst, bytes4 sig
+    ) public constant returns (bool);
 }
 
-contract UnboundedRegularToken is RegularToken {
+contract DSAuthEvents {
+    event LogSetAuthority (address indexed authority);
+    event LogSetOwner     (address indexed owner);
+}
 
-    uint constant MAX_UINT = 2**256 - 1;
-    
-    /// @dev ERC20 transferFrom, modified such that an allowance of MAX_UINT represents an unlimited amount.
-    /// @param _from Address to transfer from.
-    /// @param _to Address to transfer to.
-    /// @param _value Amount to transfer.
-    /// @return Success of transfer.
-    function transferFrom(address _from, address _to, uint _value)
-        public
-        returns (bool)
+contract DSAuth is DSAuthEvents {
+    DSAuthority  public  authority;
+    address      public  owner;
+
+    function DSAuth() public {
+        owner = msg.sender;
+        emit LogSetOwner(msg.sender);
+    }
+
+    function setOwner(address owner_)
+        public auth
     {
-        uint allowance = allowed[_from][msg.sender];
-        if (balances[_from] >= _value
-            && allowance >= _value
-            && balances[_to] + _value >= balances[_to]
-        ) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            if (allowance < MAX_UINT) {
-                allowed[_from][msg.sender] -= _value;
-            }
-            Transfer(_from, _to, _value);
+        owner = owner_;
+        LogSetOwner(owner);
+    }
+
+    function setAuthority(DSAuthority authority_)
+        public auth
+    {
+        authority = authority_;
+        LogSetAuthority(authority);
+    }
+
+    modifier auth {
+        assert(isAuthorized(msg.sender, msg.sig));
+        _;
+    }
+
+    modifier authorized(bytes4 sig) {
+        assert(isAuthorized(msg.sender, sig));
+        _;
+    }
+
+    function isAuthorized(address src, bytes4 sig) internal returns (bool) {
+        if (src == address(this)) {
             return true;
-        } else {
+        } else if (src == owner) {
+            return true;
+        } else if (authority == DSAuthority(0)) {
             return false;
+        } else {
+            return authority.canCall(src, this, sig);
         }
     }
 }
 
-contract LICToken is UnboundedRegularToken {
+contract DSStop is DSAuth, DSNote {
 
-    uint public totalSupply = 10*10**27;
-    uint8 constant public decimals = 18;
-    string constant public name = "LifeCoinToken";
-    string constant public symbol = "LIC";
+    bool public stopped;
 
-    function LICToken() {
-        balances[msg.sender] = totalSupply;
-        Transfer(address(0), msg.sender, totalSupply);
+    modifier stoppable {
+        assert (!stopped);
+        _;
+    }
+    function stop() public auth note {
+        stopped = true;
+    }
+    function start() public auth note {
+        stopped = false;
+    }
+
+}
+
+contract DSMath {
+    /*
+    standard uint256 functions
+     */
+    function add(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        assert((z = x + y) >= x);
+    }
+
+    function sub(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        assert((z = x - y) <= x);
+    }
+
+    function mul(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        assert((z = x * y) >= x);
+    }
+
+    function div(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        z = x / y;
+    }
+
+    function min(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        return x <= y ? x : y;
+    }
+    function max(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        return x >= y ? x : y;
+    }
+
+    /*
+    uint128 functions (h is for half)
+     */
+    function hadd(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        assert((z = x + y) >= x);
+    }
+
+    function hsub(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        assert((z = x - y) <= x);
+    }
+
+    function hmul(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        assert((z = x * y) >= x);
+    }
+
+    function hdiv(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = x / y;
+    }
+
+    function hmin(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        return x <= y ? x : y;
+    }
+    function hmax(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        return x >= y ? x : y;
+    }
+
+
+    /*
+    int256 functions
+     */
+    function imin(int256 x, int256 y) constant internal returns (int256 z) {
+        return x <= y ? x : y;
+    }
+    function imax(int256 x, int256 y) constant internal returns (int256 z) {
+        return x >= y ? x : y;
+    }
+
+    /*
+    WAD math
+     */
+    uint128 constant WAD = 10 ** 18;
+
+    function wadd(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hadd(x, y);
+    }
+
+    function wsub(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hsub(x, y);
+    }
+
+    function wmul(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * y + WAD / 2) / WAD);
+    }
+
+    function wdiv(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * WAD + y / 2) / y);
+    }
+
+    function wmin(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hmin(x, y);
+    }
+    function wmax(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hmax(x, y);
+    }
+
+    /*
+    RAY math
+     */
+    uint128 constant RAY = 10 ** 27;
+    function radd(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hadd(x, y);
+    }
+
+    function rsub(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hsub(x, y);
+    }
+
+    function rmul(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * y + RAY / 2) / RAY);
+    }
+
+    function rdiv(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        z = cast((uint256(x) * RAY + y / 2) / y);
+    }
+
+    function rpow(uint128 x, uint64 n) constant internal returns (uint128 z) {
+        // This famous algorithm is called "exponentiation by squaring"
+        // and calculates x^n with x as fixed-point and n as regular unsigned.
+        //
+        // It's O(log n), instead of O(n) for naive repeated multiplication.
+        //
+        // These facts are why it works:
+        //
+        //  If n is even, then x^n = (x^2)^(n/2).
+        //  If n is odd,  then x^n = x * x^(n-1),
+        //   and applying the equation for even x gives
+        //    x^n = x * (x^2)^((n-1) / 2).
+        //
+        //  Also, EVM division is flooring and
+        //    floor[(n-1) / 2] = floor[n / 2].
+
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
+
+    function rmin(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hmin(x, y);
+    }
+    function rmax(uint128 x, uint128 y) constant internal returns (uint128) {
+        return hmax(x, y);
+    }
+
+    function cast(uint256 x) constant internal returns (uint128 z) {
+        assert((z = uint128(x)) == x);
+    }
+
+}
+
+contract ERC20 {
+    function totalSupply() public constant returns (uint supply);
+    function balanceOf( address who ) public constant returns (uint value);
+    function allowance( address owner, address spender ) public constant returns (uint _allowance);
+
+    function transfer( address to, uint value) public returns (bool ok);
+    function transferFrom( address from, address to, uint value) public returns (bool ok);
+    function approve( address spender, uint value ) public returns (bool ok);
+
+    event Transfer( address indexed from, address indexed to, uint value);
+    event Approval( address indexed owner, address indexed spender, uint value);
+}
+
+contract DSTokenBase is ERC20, DSMath {
+    uint256                                            _supply;
+    mapping (address => uint256)                       _balances;
+    mapping (address => mapping (address => uint256))  _approvals;
+    
+    function DSTokenBase(uint256 supply) public {
+        _balances[msg.sender] = supply;
+        _supply = supply;
+    }
+    
+    function totalSupply() public constant returns (uint256) {
+        return _supply;
+    }
+    function balanceOf(address src) public constant returns (uint256) {
+        return _balances[src];
+    }
+    function allowance(address src, address guy) public constant returns (uint256) {
+        return _approvals[src][guy];
+    }
+    
+    function transfer(address dst, uint wad) public returns (bool) {
+        assert(_balances[msg.sender] >= wad);
+        
+        _balances[msg.sender] = sub(_balances[msg.sender], wad);
+        _balances[dst] = add(_balances[dst], wad);
+        
+        Transfer(msg.sender, dst, wad);
+        
+        return true;
+    }
+    
+    function transferFrom(address src, address dst, uint wad) public returns (bool) {
+        assert(_balances[src] >= wad);
+        assert(_approvals[src][msg.sender] >= wad);
+        
+        _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+        
+        Transfer(src, dst, wad);
+        
+        return true;
+    }
+    
+    function approve(address guy, uint256 wad) public returns (bool) {
+        _approvals[msg.sender][guy] = wad;
+        
+        Approval(msg.sender, guy, wad);
+        
+        return true;
+    }
+
+}
+
+contract LICToken is DSTokenBase(0), DSStop {
+
+    string  public  symbol = "LIC";
+    string  public  name = "LICToken";
+    uint256  public  decimals = 8; // standard token precision. override to customize
+
+    struct TokenData {
+        uint count;
+        uint unlockTime;
+    }
+    mapping(address => TokenData[]) accs;
+    
+    mapping(address => string) accsInfo;
+
+    function LICToken() public {
+    }
+
+    function transfer(address dst, uint wad) public stoppable note returns (bool) {
+        return super.transfer(dst, wad);
+    }
+    function transferFrom(
+        address src, address dst, uint wad
+    ) public stoppable note returns (bool) {
+        return super.transferFrom(src, dst, wad);
+    }
+    function approve(address guy, uint wad) public stoppable note returns (bool) {
+        return super.approve(guy, wad);
+    }
+
+    function push(address dst, uint128 wad) public returns (bool) {
+        return transfer(dst, wad);
+    }
+    function pull(address src, uint128 wad) public returns (bool) {
+        return transferFrom(src, msg.sender, wad);
+    }
+
+    function mint(uint128 wad) public auth stoppable note {
+        assert(owner == msg.sender);
+        _balances[msg.sender] = add(_balances[msg.sender], wad);
+        _supply = add(_supply, wad);
+    }
+    function burn(uint128 wad) public auth stoppable note {
+        assert(owner == msg.sender);
+        _balances[msg.sender] = sub(_balances[msg.sender], wad);
+        _supply = sub(_supply, wad);
+    }
+
+    function transferByLock(address to,uint c,uint256 lockTime,uint stages) public {
+        assert(owner == msg.sender);
+        assert(_balances[msg.sender] >= c);
+        assert(stages >= 1);
+        uint256 nowtime = now;
+        assert(lockTime > nowtime);
+
+        _balances[msg.sender] = sub(_balances[msg.sender], c);
+        uint256 waittime = div(sub(lockTime, nowtime), stages);
+        for(uint i = 1; i <= stages; i++){
+            accs[to].push(TokenData(div(c, stages),add(now, mul(i, waittime))));
+        }
+        //????
+        Transfer(msg.sender, to, c);
+    }
+
+    function unlock() public {
+        uint[] memory d_list = new uint[](accs[msg.sender].length);
+        uint dlen = 0;
+        for(uint i = 0 ; i < accs[msg.sender].length; i++ ) {
+            if ( accs[msg.sender][i].unlockTime < now){
+                _balances[msg.sender] = add(_balances[msg.sender], accs[msg.sender][i].count);
+                d_list[dlen] = i;
+                dlen++;
+            }
+        }
+        for(i = 0;i<dlen;i++){
+            delete (accs[msg.sender][d_list[i]]);
+        }
+    }
+
+    function getLockAccount(address addr, uint isAvailable) public constant returns (uint256) {
+        uint256 total = 0;
+        for(uint i = 0 ; i < accs[addr].length; i++ ) {
+            if ((isAvailable == 1) && (accs[addr][i].unlockTime > now)) {
+                continue;
+            }
+            total = add(total, accs[addr][i].count);
+        }
+        return total;
+    }
+    
+    function setLockAccInfo(address addr, string info) public {
+        assert(owner == msg.sender);
+        accsInfo[addr] = info;
+    }
+    
+    function getLockAccInfo(address addr) public constant returns (string) {
+        return accsInfo[addr];
     }
 }
