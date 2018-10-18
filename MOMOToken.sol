56abacdff5ddc8e06389a1ee12af3e8823c0ebe3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:53:30 +0700

    add contract MomoToken

--- MOMOToken.sol
+++ MOMOToken.sol
@@ -1,113 +1,250 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MOMOToken at 0xdcf8590df499eb04ee48c18676c07e2afa20d53d
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MomoToken at 0xbecba020927ad23848639d5b5fdd2d0ab3745056
 */
-/*
-You should inherit from StandardToken or, for a token like you would want to deploy
-Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
-.*/
-pragma solidity ^0.4.8;
-
-// Abstract contract for the full ERC 20 Token standard
-// https://github.com/ethereum/EIPs/issues/20
-pragma solidity ^0.4.8;
-
+pragma solidity ^0.4.12;
+ 
+contract IMigrationContract {
+    function migrate(address addr, uint256 nas) returns (bool success);
+}
+ 
+/* ?????NAS  coin*/
+contract SafeMath {
+ 
+ 
+    function safeAdd(uint256 x, uint256 y) internal returns(uint256) {
+        uint256 z = x + y;
+        assert((z >= x) && (z >= y));
+        return z;
+    }
+ 
+    function safeSubtract(uint256 x, uint256 y) internal returns(uint256) {
+        assert(x >= y);
+        uint256 z = x - y;
+        return z;
+    }
+ 
+    function safeMult(uint256 x, uint256 y) internal returns(uint256) {
+        uint256 z = x * y;
+        assert((x == 0)||(z/x == y));
+        return z;
+    }
+ 
+}
+ 
 contract Token {
-    /* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
     uint256 public totalSupply;
-    */
-    /// total amount of tokens
-    uint256 public totalSupply;
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) public view returns (uint256 balance);
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) public returns (bool success);
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) public returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
-
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
-
+ 
+ 
+/*  ERC 20 token */
 contract StandardToken is Token {
-
-    uint256 constant MAX_UINT256 = 2**256 - 1;
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from] >= _value && allowance >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
+ 
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
         }
-        Transfer(_from, _to, _value);
-        return true;
     }
-
-    function balanceOf(address _owner) view public returns (uint256 balance) {
+ 
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+ 
+    function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
-
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+ 
+    function approve(address _spender, uint256 _value) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    function allowance(address _owner, address _spender)
-    view public returns (uint256 remaining) {
+ 
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
-
+ 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
 }
-
-contract MOMOToken is StandardToken {
-
-    /* Public variables of the token */
-    string public constant name = "MOMO Token";
-    string public constant symbol = "MOMO";
-    uint8 public constant decimals = 18;
-    uint public constant tokenSupply = 28000000;
-    address public constant MOMO_FOUNDATION_WALLET = 0x3aad1ace46fc85e0c305e80d486ed77a8249e724;
-
-    function MOMOToken()
+ 
+contract MomoToken is StandardToken, SafeMath {
+ 
+    // metadataKKCoin
+    string  public constant name = "Momo";
+    string  public constant symbol = "MomoCoin";
+    uint256 public constant decimals = 18;
+    string  public version = "1.0";
+ 
+    // contracts
+    address public ethFundDeposit;          // ETH????
+    address public newContractAddr;         // token????
+ 
+    // crowdsale parameters
+    bool    public isFunding;                // ?????true
+    uint256 public fundingStartBlock;
+    uint256 public fundingStopBlock;
+ 
+    uint256 public currentSupply;           // ??????tokens??
+    uint256 public tokenRaised = 0;         // ??????token
+    uint256 public tokenMigrated = 0;     // ??????? token
+    uint256 public tokenExchangeRate = 500;             // 625 BILIBILI ?? 1 ETH
+ 
+    // events
+    event AllocateToken(address indexed _to, uint256 _value);   // ???????token;
+    event IssueToken(address indexed _to, uint256 _value);      // ???????token;
+    event IncreaseSupply(uint256 _value);
+    event DecreaseSupply(uint256 _value);
+    event Migrate(address indexed _to, uint256 _value);
+ 
+    // ??
+    function formatDecimals(uint256 _value) internal returns (uint256 ) {
+        return _value * 10 ** decimals;
+    }
+ 
+    // constructor
+    function MomoToken(
+        address _ethFundDeposit,
+        uint256 _currentSupply)
     {
-        totalSupply = tokenSupply * (10 ** uint256(decimals));
-        balances[MOMO_FOUNDATION_WALLET] = totalSupply;              // Give the creator all initial tokens
+        ethFundDeposit = _ethFundDeposit;
+ 
+        isFunding = false;                           //?????CrowdS ale??
+        fundingStartBlock = 0;
+        fundingStopBlock = 0;
+ 
+        currentSupply = formatDecimals(_currentSupply);
+        totalSupply = formatDecimals(100000000);
+        balances[msg.sender] = totalSupply;
+        if(currentSupply > totalSupply) throw;
+    }
+ 
+    modifier isOwner()  { require(msg.sender == ethFundDeposit); _; }
+ 
+    ///  ??token??
+    function setTokenExchangeRate(uint256 _tokenExchangeRate) isOwner external {
+        if (_tokenExchangeRate == 0) throw;
+        if (_tokenExchangeRate == tokenExchangeRate) throw;
+ 
+        tokenExchangeRate = _tokenExchangeRate;
+    }
+ 
+    /// @dev ??token??
+    function increaseSupply (uint256 _value) isOwner external {
+        uint256 value = formatDecimals(_value);
+        if (value + currentSupply > totalSupply) throw;
+        currentSupply = safeAdd(currentSupply, value);
+        IncreaseSupply(value);
+    }
+ 
+    /// @dev ??token??
+    function decreaseSupply (uint256 _value) isOwner external {
+        uint256 value = formatDecimals(_value);
+        if (value + tokenRaised > currentSupply) throw;
+ 
+        currentSupply = safeSubtract(currentSupply, value);
+        DecreaseSupply(value);
+    }
+ 
+    ///  ?????? ?????
+    function startFunding (uint256 _fundingStartBlock, uint256 _fundingStopBlock) isOwner external {
+        if (isFunding) throw;
+        if (_fundingStartBlock >= _fundingStopBlock) throw;
+        if (block.number >= _fundingStartBlock) throw;
+ 
+        fundingStartBlock = _fundingStartBlock;
+        fundingStopBlock = _fundingStopBlock;
+        isFunding = true;
+    }
+ 
+    ///  ????????
+    function stopFunding() isOwner external {
+        if (!isFunding) throw;
+        isFunding = false;
+    }
+ 
+    /// ????????????token?????token?
+    function setMigrateContract(address _newContractAddr) isOwner external {
+        if (_newContractAddr == newContractAddr) throw;
+        newContractAddr = _newContractAddr;
+    }
+ 
+    /// ?????????
+    function changeOwner(address _newFundDeposit) isOwner() external {
+        if (_newFundDeposit == address(0x0)) throw;
+        ethFundDeposit = _newFundDeposit;
+    }
+ 
+    ///??token?????
+    function migrate() external {
+        if(isFunding) throw;
+        if(newContractAddr == address(0x0)) throw;
+ 
+        uint256 tokens = balances[msg.sender];
+        if (tokens == 0) throw;
+ 
+        balances[msg.sender] = 0;
+        tokenMigrated = safeAdd(tokenMigrated, tokens);
+ 
+        IMigrationContract newContract = IMigrationContract(newContractAddr);
+        if (!newContract.migrate(msg.sender, tokens)) throw;
+ 
+        Migrate(msg.sender, tokens);               // log it
+    }
+ 
+    /// ??ETH ?BILIBILI??
+    function transferETH() isOwner external {
+        if (this.balance == 0) throw;
+        if (!ethFundDeposit.send(this.balance)) throw;
+    }
+ 
+    ///  ?BILIBILI token?????????
+    function allocateToken (address _addr, uint256 _eth) isOwner external {
+        if (_eth == 0) throw;
+        if (_addr == address(0x0)) throw;
+ 
+        uint256 tokens = safeMult(formatDecimals(_eth), tokenExchangeRate);
+        if (tokens + tokenRaised > currentSupply) throw;
+ 
+        tokenRaised = safeAdd(tokenRaised, tokens);
+        balances[_addr] += tokens;
+ 
+        AllocateToken(_addr, tokens);  // ??token??
+    }
+ 
+    /// ??token
+    function () payable {
+        if (!isFunding) throw;
+        if (msg.value == 0) throw;
+ 
+        if (block.number < fundingStartBlock) throw;
+        if (block.number > fundingStopBlock) throw;
+ 
+        uint256 tokens = safeMult(msg.value, tokenExchangeRate);
+        if (tokens + tokenRaised > currentSupply) throw;
+ 
+        tokenRaised = safeAdd(tokenRaised, tokens);
+        balances[msg.sender] += tokens;
+ 
+        IssueToken(msg.sender, tokens);  //????
     }
 }
