80820416957623acd5d5e9a27d5774f60280de03
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:59:46 +0700

    add contract Airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,15 +1,132 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x7f7b5fe07cbf3cda465af73559718ce940c3724a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x5584e6e31a01b2946fe56dc1b6af7fe092dcf5b4
 */
 pragma solidity ^0.4.24;
-contract ERC20 {
-  function transfer(address _recipient, uint256 _value) public returns (bool success);
+
+/**
+* POWERED BY
+* ?   ??? ??? ??? ?   ??? ??? ???      ??? ??? ??? ???
+* ?   ? ? ???  ?? ?   ??  ??? ???       ?  ??  ??? ???
+* ??? ??? ??? ??? ??? ??? ??? ???       ?  ??? ? ? ? ?
+* game at https://lordless.games
+* code at https://github.com/lordlessio
+*/
+
+// File: node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
 }
 
-contract Airdrop {
-  function multisend(ERC20 token, address[] recipients, uint256 value) public {
-    for (uint256 i = 0; i < recipients.length; i++) {
-      token.transfer(recipients[i], value * 100000);
+// File: node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * See https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address _who) public view returns (uint256);
+  function transfer(address _to, uint256 _value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+// File: node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address _owner, address _spender)
+    public view returns (uint256);
+
+  function transferFrom(address _from, address _to, uint256 _value)
+    public returns (bool);
+
+  function approve(address _spender, uint256 _value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+// File: contracts/Airdrop.sol
+
+contract Airdrop is Ownable {
+  /**
+   * @dev daAirdrop to address
+   * @param _tokenAddr address the erc20 token address
+   * @param dests address[] addresses to airdrop
+   * @param values uint256[] value(in wei) to airdrop
+   */
+  function doAirdrop(string desc, address _tokenAddr, address[] dests, uint256[] values) onlyOwner public
+    returns (uint256) {
+    uint256 i = 0;
+    while (i < dests.length) {
+      ERC20(_tokenAddr).transferFrom(msg.sender, dests[i], values[i]);
+      i += 1;
     }
+    return(i);
   }
 }
