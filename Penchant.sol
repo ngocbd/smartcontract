5af11e8f5605866202f9100a64916ecb67f5ef9b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:49:15 +0700

    add contract Penchant

--- Penchant.sol
+++ Penchant.sol
@@ -1,192 +1,87 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Penchant at 0x12605f121c88ebfdb62161eeb8317dfe8fc7e325
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Penchant at 0x70aa2ba250e71850b457e5dae83fe0e883a4c3a5
 */
 pragma solidity ^0.4.24;
 
-library SafeMath {
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        if (a == 0) {
-            return 0;
-        }
-        c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a / b;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
-contract AltcoinToken {
-    function balanceOf(address _owner) constant public returns (uint256);
+contract ForeignToken {
+    function balanceOf(address _owner) public constant returns (uint256);
     function transfer(address _to, uint256 _value) public returns (bool);
 }
 
 contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) public constant returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
+
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
 }
 
 contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
 }
 
 contract Penchant is ERC20 {
     
-    using SafeMath for uint256;
     address owner = msg.sender;
 
     mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;    
-
-    string public constant name = "Penchant Token";
-    string public constant symbol = "PCT";
-    uint public constant decimals = 18;
+    mapping (address => mapping (address => uint256)) allowed;
     
-    uint256 public totalSupply = 5000000000e18;
-    uint256 public totalDistributed = 0;        
-    uint256 public tokensPerEth = 3000000e18;
-    uint256 public bonus = 0;   
-    uint256 public constant minContribution = 1 ether / 100; // 0.01 Ether
-    uint256 public constant minBonus = 1 ether / 2; // 0.5 Ether
-    uint256 public constant maxBonus = 1 ether / 1; // 1 Ether
+    uint256 public totalSupply = 15000000000 * 10**8;
+
+    function name() public constant returns (string) { return "Penchant Token"; }
+    function symbol() public constant returns (string) { return "PENT"; }
+    function decimals() public constant returns (uint8) { return 8; }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
-    event Distr(address indexed to, uint256 amount);
+
     event DistrFinished();
 
-    event Airdrop(address indexed _owner, uint _amount, uint _balance);
-
-    event TokensPerEthUpdated(uint _tokensPerEth);
-    
-    event Burn(address indexed burner, uint256 value);
-
     bool public distributionFinished = false;
-    
+
     modifier canDistr() {
-        require(!distributionFinished);
-        _;
+    require(!distributionFinished);
+    _;
     }
-    
-    modifier onlyOwner() {
+
+    function Penchant() public {
+        owner = msg.sender;
+        balances[msg.sender] = totalSupply;
+    }
+
+    modifier onlyOwner { 
         require(msg.sender == owner);
         _;
     }
-    
-    
-    function Penchant () public {
-        owner = msg.sender;
-        uint256 devTokens = 2250000000e18;
-        distr(owner, devTokens);
-    }
-    
+
     function transferOwnership(address newOwner) onlyOwner public {
-        if (newOwner != address(0)) {
-            owner = newOwner;
-        }
+        owner = newOwner;
     }
 
-    function finishDistribution() onlyOwner canDistr public returns (bool) {
-        distributionFinished = true;
-        emit DistrFinished();
-        return true;
+    function getEthBalance(address _addr) constant public returns(uint) {
+    return _addr.balance;
+    }
+
+    function distributePENT(address[] addresses, uint256 _value, uint256 _ethbal) onlyOwner canDistr public {
+         for (uint i = 0; i < addresses.length; i++) {
+	     if (getEthBalance(addresses[i]) < _ethbal) {
+ 	         continue;
+             }
+             balances[owner] -= _value;
+             balances[addresses[i]] += _value;
+             emit Transfer(owner, addresses[i], _value);
+         }
     }
     
-    function distr(address _to, uint256 _amount) canDistr private returns (bool) {
-        totalDistributed = totalDistributed.add(_amount);        
-        balances[_to] = balances[_to].add(_amount);
-        emit Distr(_to, _amount);
-        emit Transfer(address(0), _to, _amount);
-
-        return true;
-    }
-
-    function doAirdrop(address _participant, uint _amount) internal {
-
-        require( _amount > 0 );      
-
-        require( totalDistributed < totalSupply );
-        
-        balances[_participant] = balances[_participant].add(_amount);
-        totalDistributed = totalDistributed.add(_amount);
-
-        if (totalDistributed >= totalSupply) {
-            distributionFinished = true;
-        }
-
-        emit Airdrop(_participant, _amount, balances[_participant]);
-        emit Transfer(address(0), _participant, _amount);
-    }
-
-    function adminClaimAirdrop(address _participant, uint _amount) public onlyOwner {        
-        doAirdrop(_participant, _amount);
-    }
-
-    function adminClaimAirdropMultiple(address[] _addresses, uint _amount) public onlyOwner {        
-        for (uint i = 0; i < _addresses.length; i++) doAirdrop(_addresses[i], _amount);
-    }
-
-    function updateTokensPerEth(uint _tokensPerEth) public onlyOwner {        
-        tokensPerEth = _tokensPerEth;
-        emit TokensPerEthUpdated(_tokensPerEth);
-    }
-           
-    function () external payable {
-        getTokens();
-     }
-    
-    function getTokens() payable canDistr  public {
-        uint256 tokens = 0;
-
-        require( msg.value >= minContribution );
-
-        require( msg.value > 0 );
-
-        tokens = tokensPerEth.mul(msg.value) / 1 ether;        
-        address investor = msg.sender;
-        bonus = 0;
-
-        if ( msg.value >= minBonus ) {
-            bonus = tokens / 5;
-        }
-
-        if ( msg.value >= maxBonus ) {
-            bonus = tokens / 2;
-        }
-
-        tokens = tokens + bonus;
-
-        if (tokens > 0) {
-            distr(investor, tokens);
-        }
-
-        if (totalDistributed >= totalSupply) {
-            distributionFinished = true;
-        }
-    }
-
     function balanceOf(address _owner) constant public returns (uint256) {
-        return balances[_owner];
+	 return balances[_owner];
     }
 
     // mitigates the ERC20 short address attack
@@ -197,32 +92,40 @@ contract Penchant is ERC20 {
     
     function transfer(address _to, uint256 _amount) onlyPayloadSize(2 * 32) public returns (bool success) {
 
-        require(_to != address(0));
-        require(_amount <= balances[msg.sender]);
-        
-        balances[msg.sender] = balances[msg.sender].sub(_amount);
-        balances[_to] = balances[_to].add(_amount);
-        emit Transfer(msg.sender, _to, _amount);
-        return true;
+         if (balances[msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[msg.sender] -= _amount;
+             balances[_to] += _amount;
+             emit Transfer(msg.sender, _to, _amount);
+             return true;
+         } else {
+             return false;
+         }
     }
     
     function transferFrom(address _from, address _to, uint256 _amount) onlyPayloadSize(3 * 32) public returns (bool success) {
 
-        require(_to != address(0));
-        require(_amount <= balances[_from]);
-        require(_amount <= allowed[_from][msg.sender]);
-        
-        balances[_from] = balances[_from].sub(_amount);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_amount);
-        balances[_to] = balances[_to].add(_amount);
-        emit Transfer(_from, _to, _amount);
-        return true;
+         if (balances[_from] >= _amount
+             && allowed[_from][msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[_from] -= _amount;
+             allowed[_from][msg.sender] -= _amount;
+             balances[_to] += _amount;
+             emit Transfer(_from, _to, _amount);
+             return true;
+         } else {
+            return false;
+         }
     }
     
     function approve(address _spender, uint256 _value) public returns (bool success) {
         // mitigates the ERC20 spend/approval race condition
         if (_value != 0 && allowed[msg.sender][_spender] != 0) { return false; }
+        
         allowed[msg.sender][_spender] = _value;
+        
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -230,32 +133,18 @@ contract Penchant is ERC20 {
     function allowance(address _owner, address _spender) constant public returns (uint256) {
         return allowed[_owner][_spender];
     }
-    
-    function getTokenBalance(address tokenAddress, address who) constant public returns (uint){
-        AltcoinToken t = AltcoinToken(tokenAddress);
-        uint bal = t.balanceOf(who);
-        return bal;
+
+    function finishDistribution() onlyOwner public returns (bool) {
+    distributionFinished = true;
+    emit DistrFinished();
+    return true;
     }
-    
-    function withdraw() onlyOwner public {
-        address myAddress = this;
-        uint256 etherBalance = myAddress.balance;
-        owner.transfer(etherBalance);
-    }
-    
-    function burn(uint256 _value) onlyOwner public {
-        require(_value <= balances[msg.sender]);
-        
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        totalDistributed = totalDistributed.sub(_value);
-        emit Burn(burner, _value);
-    }
-    
-    function withdrawAltcoinTokens(address _tokenContract) onlyOwner public returns (bool) {
-        AltcoinToken token = AltcoinToken(_tokenContract);
+
+    function withdrawForeignTokens(address _tokenContract) public returns (bool) {
+        require(msg.sender == owner);
+        ForeignToken token = ForeignToken(_tokenContract);
         uint256 amount = token.balanceOf(address(this));
         return token.transfer(owner, amount);
     }
+
 }
