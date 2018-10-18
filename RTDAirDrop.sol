5cb305b4784dd86f2441005f6786c14babd5b8d7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:30:41 +0700

    add contract RTDAirDrop

---
+++ RTDAirDrop.sol
@@ -0,0 +1,58 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RTDAirDrop at 0x234bcd869db7a7fcc02f92e988f17571621b5269
+*/
+pragma solidity ^0.4.16;
+
+contract owned {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
+
+
+
+contract RTDAirDrop is owned {
+    address public token_address;
+    string public detail;
+    string public website;
+
+    event AirDropCoin(address target, uint256 amount);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function setToken(address tokenAddress) onlyOwner public {
+        token_address = tokenAddress;
+    }
+
+    function setWebsite(string airdropWebsite) onlyOwner public {
+        website = airdropWebsite;
+    }
+
+    function setDetail(string airdropDetail) onlyOwner public {
+        detail = airdropDetail;
+    }
+
+    function() payable public {}
+
+    function withdrawEther() onlyOwner public {
+        owner.transfer(address(this).balance);
+    }
+
+    function airDrop(address _user, uint256 _value)  onlyOwner public {
+            _user.transfer(_value);
+            emit AirDropCoin(_user, _value);
+    }
+}
