477608f7f02d79201be9dc7f264f5d8e819e07d8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:05:08 +0700

    add contract Utils

--- Utils.sol
+++ Utils.sol
@@ -1,10 +1,72 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Utils at 0x7999163f9ab5c4c26194694a07b839c31f266ab2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Utils at 0xe190ec9b42c278c32056b5563e1892065efd2bf1
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.24;
+
+contract TokenInterface {
+    function name() public view returns (string);
+    function symbol() public view returns (string);
+    function decimals() public view returns (uint256);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address _owner) public view returns (uint256);
+}
 
 contract Utils {
-    function currentTime() public view returns (uint256){
-        return uint256(now);
+    function contractuallyOf(address _address) public view returns(bool) {
+        uint length;
+        assembly {
+            length := extcodesize(_address)
+        }
+        return (length > 0);
+    }
+
+    function tokenOf(address _contract, address _owner) public view returns(
+        bool contractually,
+        bool tokenized,
+        string name,
+        string symbol,
+        uint256 decimals,
+        uint256 totalSupply,
+        uint256 balance
+    ) {
+        contractually = contractuallyOf(_contract);
+        if (contractually) {
+            TokenInterface token = TokenInterface(_contract);
+            name = token.name();
+            symbol = token.symbol();
+            decimals = token.decimals();
+            totalSupply = token.totalSupply();
+            balance = token.balanceOf(_owner);
+            
+            if (bytes(name).length > 0 && bytes(symbol).length > 0 && totalSupply > 0) {
+                tokenized = true;
+            }
+        }
+    }
+
+    function recover(bytes32 _hash, bytes _sig) public pure returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        if (_sig.length != 65) {
+            return (address(0));
+        }
+
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        if (v != 27 && v != 28) {
+            return (address(0));
+        } else {
+            return ecrecover(_hash, v, r, s);
+        }
     }
 }
