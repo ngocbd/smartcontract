bf02709b3b56063baf6906a0590359f689ef8849
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:13:58 +0700

    add contract EthLong

--- EthLong.sol
+++ EthLong.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthLong at 0x7d60b8434b2ee40ca99774010a3a8de2375f61c5
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EthLong at 0x72d9513547252dbf5377e0ab9ad310fd88b52415
 */
 pragma solidity ^0.4.25;
  
@@ -7,12 +7,7 @@ pragma solidity ^0.4.25;
  *
  * Easy Investment 2 Contract
  *  - GAIN 2% PER 24 HOURS (every 5900 blocks)
- * How to use:
- *  1. Send any amount of ether to make an investment
- *  2a. Claim your profit by sending 0 ether transaction (every day, every week, i don't care unless you're spending too much on GAS)
- *  OR
- *  2b. Send more ether to reinvest AND get your profit at the same time
- *
+ * 
  * RECOMMENDED GAS LIMIT: 70000
  * RECOMMENDED GAS PRICE: https://ethgasstation.info/
  *
@@ -26,7 +21,6 @@ contract EthLong{
     mapping(address => uint256) investments;
     mapping(address => uint256) joined;
     mapping(address => uint256) withdrawals;
-    mapping(address => uint256) referrer;
  
     uint256 public minimum = 10000000000000000;
     uint256 public step = 33;
@@ -38,7 +32,6 @@ contract EthLong{
     event Invest(address investor, uint256 amount);
     event Withdraw(address investor, uint256 amount);
     event Bounty(address hunter, uint256 amount);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
    
     /**
      * @dev ?onstructor Sets the original roles of the contract
@@ -69,12 +62,6 @@ contract EthLong{
      * @param newOwner The address to transfer ownership to.
      * @param newOwnerWallet The address to transfer ownership to.
      */
-    function transferOwnership(address newOwner, address newOwnerWallet) public onlyOwner {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        ownerWallet = newOwnerWallet;
-    }
  
     /**
      * @dev Investments
@@ -124,19 +111,6 @@ contract EthLong{
         }
     }
    
-    /**
-    * @dev Bounty reward
-    */
-    function bounty() public {
-        uint256 refBalance = checkReferral(msg.sender);
-        if(refBalance >= minimum) {
-             if (address(this).balance > refBalance) {
-                referrer[msg.sender] = 0;
-                msg.sender.transfer(refBalance);
-                emit Bounty(msg.sender, refBalance);
-             }
-        }
-    }
  
     /**
     * @dev Gets balance of the sender address.
@@ -163,25 +137,7 @@ contract EthLong{
     function checkInvestments(address _investor) public view returns (uint256) {
         return investments[_investor];
     }
- 
-    /**
-    * @dev Gets referrer balance of the specified address.
-    * @param _hunter The address of the referrer
-    * @return An uint256 representing the referral earnings.
-    */
-    function checkReferral(address _hunter) public view returns (uint256) {
-        return referrer[_hunter];
-    }
-   
-    /**
-    * @dev Updates referrer balance
-    * @param _hunter The address of the referrer
-    * @param _amount An uint256 representing the referral earnings.
-    */
-    function updateReferral(address _hunter, uint256 _amount) onlyBountyManager public {
-        referrer[_hunter] = referrer[_hunter].add(_amount);
-    }
-   
+       
 }
  
 /**
