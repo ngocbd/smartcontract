e5f285cb5b21331857720cbd26136ffb95c595d6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:48:53 +0700

    add contract AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA.sol
@@ -0,0 +1,25 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA at 0x15e91e11f48c9ec88ecf2b145afca00274ac6ea9
+*/
+// CGT meme list (join us https://discord.gg/jbHKHTS )
+// goodbye motherfucker, get the fuck off this Scam.wish to your intestine affected on the sidewalk motherfucker.goodbye fucking asshole!
+// hellina, poorguy, papaprek, spydr 
+// AAAAA 
+// ALL MY LINK IS GONE 
+// can harj make a video about this ?
+// 69 wei 
+// CANT CASH OUT 
+// ETHERGUY IS A KNOWN SCAMMER AND CRIMINAL 
+//
+// Quotes 
+// Dostedt: seni na aparatusu
+// Fahrenheit: 9% AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+// Etherguy: kek 
+
+contract AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA{
+
+    function HelloWorld() returns (string){
+        return "LINK $1000 EOY!";
+    }    
+
+}
