ea6467562239e1d26a39dc62fe38680e8a7a4773
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:49:37 +0700

    add contract BPToken

--- BPToken.sol
+++ BPToken.sol
@@ -1,417 +1,127 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BPToken at 0x01db18f6a474840db3480a6a35227d4d0dfcca37
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BPToken at 0x6778fccf07547ae8ca00e12a87077a80130365ad
 */
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.18;
 
 
-/*
- * ERC20 interface
- * see https://github.com/ethereum/EIPs/issues/20
- */
+library SafeMath {
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 contract ERC20 {
-    uint public totalSupply;
-    function balanceOf(address who) constant returns (uint);
-    function allowance(address owner, address spender) constant returns (uint);
-
-    function transfer(address to, uint value) returns (bool ok);
-    function transferFrom(address from, address to, uint value) returns (bool ok);
-    function approve(address spender, uint value) returns (bool ok);
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 
-/**
- * Math operations with safety checks
- */
-contract SafeMath {
-    function safeMul(uint a, uint b) internal returns (uint) {
-        uint c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+contract StandardToken is ERC20 {
+  using SafeMath for uint256;
 
-    function safeDiv(uint a, uint b) internal returns (uint) {
-        assert(b > 0);
-        uint c = a / b;
-        assert(a == b * c + a % b);
-        return c;
-    }
+  mapping(address => uint256) balances;
+  mapping (address => mapping (address => uint256)) internal allowed;
 
-    function safeSub(uint a, uint b) internal returns (uint) {
-        assert(b <= a);
-        return a - b;
-    }
 
-    function safeAdd(uint a, uint b) internal returns (uint) {
-        uint c = a + b;
-        assert(c >= a && c >= b);
-        return c;
-    }
+  uint256 totalSupply_;
 
-    function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-        return a >= b ? a : b;
-    }
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
 
-    function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-        return a < b ? a : b;
-    }
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
 
-    function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-        return a >= b ? a : b;
-    }
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
 
-    function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-        return a < b ? a : b;
-    }
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
 
-    function assert(bool assertion) internal {
-        if (!assertion) {
-            throw;
-        }
-    }
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
 
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
 }
 
-/**
- * Owned contract
- */
-contract Owned {
-    address[] public pools;
-    address public owner;
+contract BPToken is StandardToken {
+    uint public totalSupply = 18*10**26;
+    uint8 constant public decimals = 18;
+    string constant public name = "BIT.EPOCH Token";
+    string constant public symbol = "BP";
 
-    function Owned() {
-        owner = msg.sender;
-        pools.push(msg.sender);
-    }
-
-    modifier onlyPool {
-        require(isPool(msg.sender));
-        _;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-    
-    /// add new pool address to pools
-    function addPool(address newPool) onlyOwner {
-        assert (newPool != 0);
-        if (isPool(newPool)) throw;
-        pools.push(newPool);
-    }
-    
-    /// remove a address from pools
-    function removePool(address pool) onlyOwner{
-        assert (pool != 0);
-        if (!isPool(pool)) throw;
-        
-        for (uint i=0; i<pools.length - 1; i++) {
-            if (pools[i] == pool) {
-                pools[i] = pools[pools.length - 1];
-                break;
-            }
-        }
-        pools.length -= 1;
-    }
-
-    function isPool(address pool) internal returns (bool ok){
-        for (uint i=0; i<pools.length; i++) {
-            if (pools[i] == pool)
-                return true;
-        }
-        return false;
-    }
-    
-    function transferOwnership(address newOwner) onlyOwner public {
-        removePool(owner);
-        addPool(newOwner);
-        owner = newOwner;
-    }
-}
-
-/**
- * BP crowdsale contract
-*/
-contract BPToken is SafeMath, Owned, ERC20 {
-    string public constant name = "Backpack Token";
-    string public constant symbol = "BP";
-    uint256 public constant decimals = 18;  
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-
-    function BPToken() {
-        totalSupply = 2000000000 * 10 ** uint256(decimals);
+    function BPToken() public {
         balances[msg.sender] = totalSupply;
-    }
-
-    /// asset pool map
-    mapping (address => address) addressPool;
-
-    /// address base amount
-    mapping (address => uint256) addressAmount;
-
-    /// per month seconds
-    uint perMonthSecond = 2592000;
-    
-    /// calc the balance that the user shuold hold
-    function shouldHadBalance(address who) constant returns (uint256){
-        if (isPool(who)) return 0;
-
-        address apAddress = getAssetPoolAddress(who);
-        uint256 baseAmount  = getBaseAmount(who);
-
-        /// Does not belong to AssetPool contract
-        if( (apAddress == address(0)) || (baseAmount == 0) ) return 0;
-
-        /// Instantiate ap contract
-        AssetPool ap = AssetPool(apAddress);
-
-        uint startLockTime = ap.getStartLockTime();
-        uint stopLockTime = ap.getStopLockTime();
-
-        if (block.timestamp > stopLockTime) {
-            return 0;
-        }
-
-        if (ap.getBaseLockPercent() == 0) {
-            return 0;
-        }
-
-        // base lock amount 
-        uint256 baseLockAmount = safeDiv(safeMul(baseAmount, ap.getBaseLockPercent()),100);
-        if (block.timestamp < startLockTime) {
-            return baseLockAmount;
-        }
-        
-        /// will not linear release
-        if (ap.getLinearRelease() == 0) {
-            if (block.timestamp < stopLockTime) {
-                return baseLockAmount;
-            } else {
-                return 0;
-            }
-        }
-        /// will linear release 
-
-        /// now timestamp before start lock time 
-        if (block.timestamp < startLockTime + perMonthSecond) {
-            return baseLockAmount;
-        }
-        // total lock months
-        uint lockMonth = safeDiv(safeSub(stopLockTime,startLockTime),perMonthSecond);
-        if (lockMonth <= 0) {
-            if (block.timestamp >= stopLockTime) {
-                return 0;
-            } else {
-                return baseLockAmount;
-            }
-        }
-
-        // unlock amount of every month
-        uint256 monthUnlockAmount = safeDiv(baseLockAmount,lockMonth);
-
-        // current timestamp passed month 
-        uint hadPassMonth = safeDiv(safeSub(block.timestamp,startLockTime),perMonthSecond);
-
-        return safeSub(baseLockAmount,safeMul(hadPassMonth,monthUnlockAmount));
-    }
-
-    function getAssetPoolAddress(address who) internal returns(address){
-        return addressPool[who];
-    }
-
-    function getBaseAmount(address who) internal returns(uint256){
-        return addressAmount[who];
-    }
-
-    function getBalance() constant returns(uint){
-        return balances[msg.sender];
-    }
-
-    function setPoolAndAmount(address who, uint256 amount) onlyPool returns (bool) {
-        assert(balances[msg.sender] >= amount);
-
-        if (owner == who) {
-            return true;
-        }
-        
-        address apAddress = getAssetPoolAddress(who);
-        uint256 baseAmount = getBaseAmount(who);
-
-        assert((apAddress == msg.sender) || (baseAmount == 0));
-
-        addressPool[who] = msg.sender;
-        addressAmount[who] += amount;
-
-        return true;
-    }
-
-    /// get balance of the special address
-    function balanceOf(address who) constant returns (uint) {
-        return balances[who];
-    }
-
-    /// @notice Transfer `value` BP tokens from sender's account
-    /// `msg.sender` to provided account address `to`.
-    /// @notice This function is disabled during the funding.
-    /// @dev Required state: Success
-    /// @param to The address of the recipient
-    /// @param value The number of BPs to transfer
-    /// @return Whether the transfer was successful or not
-    function transfer(address to, uint256 value) returns (bool) {
-        if (safeSub(balances[msg.sender],value) < shouldHadBalance(msg.sender)) throw;
-
-        uint256 senderBalance = balances[msg.sender];
-        if (senderBalance >= value && value > 0) {
-            senderBalance = safeSub(senderBalance, value);
-            balances[msg.sender] = senderBalance;
-            balances[to] = safeAdd(balances[to], value);
-            Transfer(msg.sender, to, value);
-            return true;
-        } else {
-            throw;
-        }
-    }
-
-    /// @notice Transfer `value` BP tokens from sender 'from'
-    /// to provided account address `to`.
-    /// @notice This function is disabled during the funding.
-    /// @dev Required state: Success
-    /// @param from The address of the sender
-    /// @param to The address of the recipient
-    /// @param value The number of BPs to transfer
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address from, address to, uint256 value) returns (bool) {
-        // Abort if not in Success state.
-        // protect against wrapping uints
-        if (balances[from] >= value &&
-        allowed[from][msg.sender] >= value &&
-        safeAdd(balances[to], value) > balances[to])
-        {
-            balances[to] = safeAdd(balances[to], value);
-            balances[from] = safeSub(balances[from], value);
-            allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], value);
-            Transfer(from, to, value);
-            return true;
-        } else {
-            throw;
-        }
-    }
-
-    /// @notice `msg.sender` approves `spender` to spend `value` tokens
-    /// @param spender The address of the account able to transfer the tokens
-    /// @param value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address spender, uint256 value) returns (bool) {
-        if (safeSub(balances[msg.sender],value) < shouldHadBalance(msg.sender)) throw;
-        
-        // Abort if not in Success state.
-        allowed[msg.sender][spender] = value;
-        Approval(msg.sender, spender, value);
-        return true;
-    }
-
-    /// @param owner The address of the account owning tokens
-    /// @param spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address owner, address spender) constant returns (uint) {
-        uint allow = allowed[owner][spender];
-        return allow;
-    }
-}
-
-
-
-contract ownedPool {
-    address public owner;
-
-    function ownedPool() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-}
-
-/**
- * Asset pool contract
-*/
-contract AssetPool is ownedPool {
-    uint  baseLockPercent;
-    uint  startLockTime;
-    uint  stopLockTime;
-    uint  linearRelease;
-    address public bpTokenAddress;
-
-    BPToken bp;
-
-    function AssetPool(address _bpTokenAddress, uint _baseLockPercent, uint _startLockTime, uint _stopLockTime, uint _linearRelease) {
-        assert(_stopLockTime > _startLockTime);
-        
-        baseLockPercent = _baseLockPercent;
-        startLockTime = _startLockTime;
-        stopLockTime = _stopLockTime;
-        linearRelease = _linearRelease;
-
-        bpTokenAddress = _bpTokenAddress;
-        bp = BPToken(bpTokenAddress);
-
-        owner = msg.sender;
-    }
-    
-    /// set role value
-    function setRule(uint _baseLockPercent, uint _startLockTime, uint _stopLockTime, uint _linearRelease) onlyOwner {
-        assert(_stopLockTime > _startLockTime);
-       
-        baseLockPercent = _baseLockPercent;
-        startLockTime = _startLockTime;
-        stopLockTime = _stopLockTime;
-        linearRelease = _linearRelease;
-    }
-
-    /// set bp token contract address
-    // function setBpToken(address _bpTokenAddress) onlyOwner {
-    //     bpTokenAddress = _bpTokenAddress;
-    //     bp = BPToken(bpTokenAddress);
-    // }
-    
-    /// assign BP token to another address
-    function assign(address to, uint256 amount) onlyOwner returns (bool) {
-        if (bp.setPoolAndAmount(to,amount)) {
-            if (bp.transfer(to,amount)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /// get the balance of current asset pool
-    function getPoolBalance() constant returns (uint) {
-        return bp.getBalance();
-    }
-    
-    function getStartLockTime() constant returns (uint) {
-        return startLockTime;
-    }
-    
-    function getStopLockTime() constant returns (uint) {
-        return stopLockTime;
-    }
-    
-    function getBaseLockPercent() constant returns (uint) {
-        return baseLockPercent;
-    }
-    
-    function getLinearRelease() constant returns (uint) {
-        return linearRelease;
     }
 }
