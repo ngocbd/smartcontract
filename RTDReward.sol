c2dffeca49f41978cb45f57a0bf93eff74429e0c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 12:24:59 +0700

    add contract RTDReward

---
+++ RTDReward.sol
@@ -0,0 +1,52 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RTDReward at 0xf54ea108bf27e1effa54ff61ba57577a1fc1c21e
+*/
+pragma solidity ^0.4.16;
+
+contract owned {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
+
+
+
+contract RTDReward is owned {
+    address public token;
+    string public detail;
+    string public website;
+
+    event Reward(address target, uint256 amount);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function setContract(address _token,string _website, string _detail) onlyOwner public {
+        token = _token;
+        website = _website;
+        detail = _detail;
+    }
+
+    function() payable public {}
+
+    function withdrawEther() onlyOwner public {
+        owner.transfer(address(this).balance);
+    }
+
+    function sendReward(address _user, uint256 _value)  onlyOwner public {
+            _user.transfer(_value);
+            emit Reward(_user, _value);
+    }
+}
