4ed69a44d9030716384c3346c7a8793346d0bf41
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:22:13 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,245 +1,189 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x7c4393ee129d7856b5bd765c2d20b66f464ccd0f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x2e94b11bab7b8c914d2993fee9119a56a3d8a15b
 */
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.21;
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    
+	function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
+
+	function div(uint256 a, uint256 b) internal constant returns (uint256) {
+		// assert(b > 0); // Solidity automatically throws when dividing by 0
+		uint256 c = a / b;
+		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+		return c;
+	}
+
+	function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+
+	function add(uint256 a, uint256 b) internal constant returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
+
+}
+
+contract ERC20 {
+	uint public totalSupply = 1653200;
+	function balanceOf(address who) constant returns (uint);
+	function transfer(address to, uint value);
+	function allowance(address owner, address spender) constant returns (uint);
+
+	function transferFrom(address from, address to, uint value);
+	function approve(address spender, uint value);
+
+	event Transfer(address indexed from, address indexed to, uint value);
+	event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract CNODStandart is ERC20 {
+    using SafeMath for uint;
+    
+	string  public name        = "Crypto Noda";
+    string  public symbol      = "CNOD";
+    uint8   public decimals    = 0;
+
+	mapping (address => mapping (address => uint)) allowed;
+	mapping (address => uint) balances;
+
+	function transferFrom(address _from, address _to, uint _value) {
+		balances[_from] = balances[_from].sub(_value);
+		allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		Transfer(_from, _to, _value);
+	}
+
+	function approve(address _spender, uint _value) {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+	}
+
+	function allowance(address _owner, address _spender) constant returns (uint remaining) {
+		return allowed[_owner][_spender];
+	}
+
+	function transfer(address _to, uint _value) {
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		Transfer(msg.sender, _to, _value);
+	}
+
+	function balanceOf(address _owner) constant returns (uint balance) {
+		return balances[_owner];
+	}
+}
+
+contract owned {
+    
+    address public owner;
+    address public newOwner;
+	
+    function owned() public payable {
+        owner = msg.sender;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+	
+    modifier onlyOwner {
+        require(owner == msg.sender);
+        _;
+    }
+	
+    function changeOwner(address _owner) onlyOwner public {
+        require(_owner != 0);
+        newOwner = _owner;
+    }
+    
+    function confirmOwner() public {
+        require(newOwner == msg.sender);
+        owner = newOwner;
+        delete newOwner;
+    }
 }
+ 
+contract Crowdsale is owned, CNODStandart {
+    
+	using SafeMath for uint;
+	uint public start;
+	uint public period;
+	uint public hardcap;
+	uint public softcap;
+	uint public min_contribution;
+	uint public totalEther;
+	uint public wei25;
+	uint public wei20;
+ 
+	function Crowdsale() {
+		start = 1535328000;
+		period = 45;
+		hardcap = 400000000000000000000;
+		softcap = 180000000000000000000;
+		min_contribution = 50000000000000000;
+		totalEther = 0;
 
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-contract Crowdsale is Ownable {
-  using SafeMath for uint256;
-
-  // start and end timestamps where investments are allowed (both inclusive)
-  uint256 public startTime;
-  uint256 public endTime;
-
-  // address where funds are collected
-  WhitelistedGateway public gateway;
-  PendingContributions public pending;
-
-	bool closedManually = false;
-	bool acceptWithoutWhitelist = true;
-  uint256 minContrib;
-
-	function setPending(bool newValue) public onlyOwner {
-		acceptWithoutWhitelist = newValue;
+		wei25 = 234410000000000;
+		wei20 = 250000000000000;
 	}
-
-	function setClosedManually(bool newValue) public onlyOwner {
-		closedManually = newValue;
-	}
-
-
-  function Crowdsale(uint256 _startTime, uint256 _endTime, address _vault, Whitelist _whitelist, uint256 _minContrib) public {
-    // require(_startTime >= now);
-    require(_endTime >= _startTime);
-    require(_vault != address(0));
-
-    startTime = _startTime;
-    endTime = _endTime;
-    minContrib = _minContrib;
-    gateway = new WhitelistedGateway(_whitelist, _vault);
-	pending = new PendingContributions(gateway);
-	// allow the pending container to fund the gateway
-	gateway.addOwner(pending);
-  }
-
-  // fallback function can be used to buy tokens
-  function () external payable {
-    require(validPurchase());
-    forwardFunds();  
-  }
-
-  // send ether either to the Gateway or to the PendingContributions
-  function forwardFunds() internal {
-	if(gateway.isWhitelisted(msg.sender)) {
-		gateway.fund.value(msg.value)(msg.sender);
-		return;
-	} 
-	pending.fund.value(msg.value)(msg.sender);
-  }
-
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal view returns (bool) {
-    bool withinPeriod = now >= startTime && now <= endTime;
-    bool sufficientPurchase = msg.value >= minContrib;
-    bool whitelisted = gateway.isWhitelisted(msg.sender);
-    return !closedManually && withinPeriod && sufficientPurchase && (acceptWithoutWhitelist || whitelisted);
-  }
-
-  // @return true if crowdsale event has ended
-  function hasEnded() public view returns (bool) {
-    return now > endTime;
-  }
-
-}
-
-contract PendingContributions is Ownable {
-	using SafeMath for uint256;
-
-	mapping(address=>uint256) public contributions;
-	WhitelistedGateway public gateway;
-
-	event PendingContributionReceived(address contributor, uint256 value, uint256 timestamp);
-	event PendingContributionAccepted(address contributor, uint256 value, uint256 timestamp);
-	event PendingContributionWithdrawn(address contributor, uint256 value, uint256 timestamp);
-
-	function PendingContributions(WhitelistedGateway _gateway) public {
-		gateway = _gateway;
-	}
-
-	modifier onlyWhitelisted(address contributor) {
-		require(gateway.isWhitelisted(contributor));
+ 
+	modifier saleIsOn() {
+		require(now > start && now < start + period * 1 days);
 		_;
 	}
 
-	function fund(address contributor) payable public onlyOwner {
-		contributions[contributor] += msg.value;
-		PendingContributionReceived(contributor, msg.value, now);
-	}
-
-	function withdraw() public {
-		uint256 toTransfer = contributions[msg.sender];
-		require(toTransfer > 0);
-		contributions[msg.sender] = 0;
-		msg.sender.transfer(toTransfer);
-		PendingContributionWithdrawn(msg.sender, toTransfer, now);
-	}
-
-	function retry(address contributor) public onlyWhitelisted(contributor) {
-		uint256 toTransfer = contributions[contributor];
-		require(toTransfer > 0);
-		gateway.fund.value(toTransfer)(contributor);
-		contributions[contributor] = 0;
-		PendingContributionAccepted(contributor, toTransfer, now);
-	}
-}
-
-contract Whitelist is Ownable {
-	using SafeMath for uint256;
-
-	mapping(address=>bool) public whitelist;
-	
-	event Authorized(address candidate, uint timestamp);
-	event Revoked(address candidate, uint timestamp);
-
-	function authorize(address candidate) public onlyOwner {
-	    whitelist[candidate] = true;
-	    Authorized(candidate, now);
-	}
-	
-	// also if not in the list..
-	function revoke(address candidate) public onlyOwner {
-	    whitelist[candidate] = false;
-	    Revoked(candidate, now);
-	}
-	
-	function authorizeMany(address[50] candidates) public onlyOwner {
-	    for(uint i = 0; i < candidates.length; i++) {
-	        authorize(candidates[i]);
-	    }
-	}
-
-	function isWhitelisted(address candidate) public view returns(bool) {
-		return whitelist[candidate];
-	}
-}
-
-contract WhitelistedGateway {
-	using SafeMath for uint256;
-
-	mapping(address=>bool) public owners;
-	mapping(address=>uint) public contributions;
-	address public vault;
-	Whitelist public whitelist;
-
-	event NewContribution(address contributor, uint256 amount, uint256 timestamp);
-
-	modifier onlyOwners() {
-		require(owners[msg.sender]);
+	modifier isUnderHardCap() {
+		require(totalEther <= hardcap);
 		_;
 	}
+	
+	function changeTotalSupply(uint _totalSupply) onlyOwner public {
+        totalSupply = _totalSupply;
+    }
+	
+	function minContribution(uint _min) onlyOwner public {
+        min_contribution = _min;
+    }
 
-	function addOwner(address newOwner) public onlyOwners {
-		owners[newOwner] = true;
+	function refund() {
+		require(this.balance < softcap && now > start + period * 1 days);
+		uint value = balances[msg.sender]; 
+		balances[msg.sender] = 0; 
+		msg.sender.transfer(value); 
 	}
 
-	function WhitelistedGateway(Whitelist _whitelist, address _vault) public {
-		whitelist = _whitelist;
-		vault = _vault;
-		owners[msg.sender] = true;
+	function createTokens() isUnderHardCap saleIsOn payable {
+		require(msg.value >= min_contribution);
+		uint tokens = 0;
+		if (totalEther < 200000000000000000000){
+			tokens = msg.value / wei25;
+		} else {
+			tokens = msg.value / wei20;
+		}
+		require((totalSupply - tokens) >= 0);
+		msg.sender.send(tokens);
+		balances[msg.sender] = tokens;
+        Transfer(owner, msg.sender, tokens);
+		
+		totalSupply -= tokens;
+		totalEther += msg.value;
 	}
+	
+	function sendToOwnerBalance(address _to, uint256 _valueWei) onlyOwner public {
+		require(totalEther >= softcap);
+        _to.send(_valueWei);
+    }
 
-	function isWhitelisted(address candidate) public view returns(bool) {
-		return whitelist.isWhitelisted(candidate);
+	function() external payable {
+		createTokens();
 	}
-
-	function fund(address contributor) public payable onlyOwners {
-		contributions[contributor] += msg.value;
-		vault.transfer(msg.value);
-		NewContribution(contributor, msg.value, now);
-	}
+    
 }
