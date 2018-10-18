4d47a3501e85c891a5f8ca9b99a78b0a0537ef7a
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 11:48:28 +0700

    add contract BitcoinStore

--- BitcoinStore.sol
+++ BitcoinStore.sol
@@ -1,36 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinStore at 0x711f388038efef6ca032ef9d9a0c79efc1579db6
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BitcoinStore at 0x3018f598fcc8aa5acc17c1f133e4679dfcd8274a
 */
 pragma solidity ^0.4.11;
-
-contract SafeMath {
-  function safeMul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function safeSub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function safeAdd(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
-
-  function assert(bool assertion) internal {
-    if (!assertion) throw;
-  }
-}
 
 contract Ownable {
   address public owner;
 
 
-  /** 
+  /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
@@ -40,7 +17,7 @@ contract Ownable {
 
 
   /**
-   * @dev Throws if called by any account other than the owner. 
+   * @dev Throws if called by any account other than the owner.
    */
   modifier onlyOwner() {
     if (msg.sender != owner) {
@@ -52,7 +29,7 @@ contract Ownable {
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to. 
+   * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) onlyOwner {
     if (newOwner != address(0)) {
@@ -77,29 +54,32 @@ contract ERC20 is ERC20Basic {
 }
 
 
-contract BitcoinStore is Ownable, SafeMath {
+contract BitcoinStore is Ownable {
 
-  address constant public Bitcoin_address =0xB6eD7644C69416d67B522e20bC294A9a9B405B31;// TESTNET CONTRACT: 0x9D2Cc383E677292ed87f63586086CfF62a009010;
-  uint public bitcoin_ratio = 400*1E8;
-  uint public eth_ratio = 1*1E18;
+  address constant public Bitcoin_address = 0xB6eD7644C69416d67B522e20bC294A9a9B405B31;
+  uint tokenPrice = 35e14; // 0.0035 eth starting price
+  uint tokenFactor = 1e8;
 
-  function update_eth_ratio( uint new_eth_ratio) 
-  onlyOwner
+  function getBalance()
+  public
+  view
+  returns (uint)
   {
-    eth_ratio = new_eth_ratio;
+      return ERC20Basic(Bitcoin_address).balanceOf(this);
   }
 
-  function update_bitcoin_ratio(uint new_bitcoin_ratio) 
-  onlyOwner
+  function getPrice()
+  public
+  view
+  returns (uint)
   {
-    bitcoin_ratio = new_bitcoin_ratio;
+      return tokenPrice;
   }
 
-  function update_ratio(uint new_bitcoin_ratio, uint new_eth_ratio) 
+  function updatePrice(uint newPrice)
   onlyOwner
   {
-    bitcoin_ratio = new_bitcoin_ratio;
-    eth_ratio = new_eth_ratio;
+      tokenPrice = newPrice;
   }
 
   function send(address _tokenAddr, address dest, uint value)
@@ -108,35 +88,20 @@ contract BitcoinStore is Ownable, SafeMath {
       ERC20(_tokenAddr).transfer(dest, value);
   }
 
-  function multisend(address _tokenAddr, address[] dests, uint[] values)
-  onlyOwner
-  returns (uint) {
-      uint i = 0;
-      while (i < dests.length) {
-         ERC20(_tokenAddr).transfer(dests[i], values[i]);
-         i += 1;
-      }
-      return(i);
-  }
-
   /* fallback function for when ether is sent to the contract */
   function () external payable {
-    require(eth_ratio > 0); // safe divide
-    uint buytokens = safeMul(bitcoin_ratio , msg.value)/eth_ratio;
-    ERC20(Bitcoin_address).transfer(msg.sender, buytokens);
-
-    owner.transfer(this.balance);
+      uint buytokens = msg.value * tokenFactor / tokenPrice;
+      require(getBalance() >= buytokens);
+      ERC20(Bitcoin_address).transfer(msg.sender, buytokens);
   }
 
   function buy() public payable {
-    require(eth_ratio > 0); // safe divide
-    uint buytokens = safeMul(bitcoin_ratio , msg.value)/eth_ratio;
-    ERC20(Bitcoin_address).transfer(msg.sender, buytokens);
-    
-    owner.transfer(this.balance);
+      uint buytokens = msg.value * tokenFactor / tokenPrice;
+      require(getBalance() >= buytokens);
+      ERC20(Bitcoin_address).transfer(msg.sender, buytokens);
   }
 
   function withdraw() onlyOwner {
-    msg.sender.transfer(this.balance);
+      msg.sender.transfer(this.balance);
   }
 }
