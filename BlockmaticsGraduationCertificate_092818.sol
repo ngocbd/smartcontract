ea61e95f5e4e11ca1a2cbe0bed4eee027d3e8fb8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 17:02:37 +0700

    add contract BlockmaticsGraduationCertificate_092818

---
+++ BlockmaticsGraduationCertificate_092818.sol
@@ -0,0 +1,15 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BlockmaticsGraduationCertificate_092818 at 0x3bab34ac9de2670b05b41df314a6c50d4ef3a05a
+*/
+contract BlockmaticsGraduationCertificate_092818 {
+    address public owner = msg.sender;
+    string public certificate;
+    bool public certIssued = false;
+
+    function publishGraduatingClass (string cert) public {
+        assert (msg.sender == owner && !certIssued);
+
+        certIssued = true;
+        certificate = cert;
+    }
+}
