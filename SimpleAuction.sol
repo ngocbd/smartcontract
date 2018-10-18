6044f5a63887906e4834254f19c7cb8030124008
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 17:38:06 +0700

    add contract SimpleAuction

--- SimpleAuction.sol
+++ SimpleAuction.sol
@@ -1,119 +1,212 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleAuction at 0x207ff240d2f5baaefce160785f38ebd5d7bbdce4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleAuction at 0x1950fbe83c6ec6ed873db41cc5d8a33f74d31c90
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.22;
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that revert on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+// ????
+library NameFilter {
+
+function filter(string _input)
+    internal
+    pure
+    returns(string)
+    {
+        bytes memory _temp = bytes(_input);
+        uint256 _length = _temp.length;
+
+        require (_length <= 256 && _length > 0, "string must be between 1 and 256 characters");
+        require(_temp[0] != 0x20 && _temp[_length-1] != 0x20, "string cannot start or end with space");
+        if (_temp[0] == 0x30)
+        {
+            require(_temp[1] != 0x78, "string cannot start with 0x");
+            require(_temp[1] != 0x58, "string cannot start with 0X");
+        }
+        return _input;
+    }
+}
+
+
 
 contract SimpleAuction {
-    // Parameters of the auction. Times are either
-    // absolute unix timestamps (seconds since 1970-01-01)
-    // or time periods in seconds.
-    address public beneficiary;
-    uint public auctionStart;
-    uint public biddingTime;
+    using NameFilter for string;
+    using SafeMath for *;
+    
+    
+    // ???
+    address private boss;
 
-    // Current state of the auction.
-    address public highestBidder;
-    uint public highestBid;
+    // fees
+    uint public fees;
 
-    // Allowed withdrawals of previous bids
+    address private top;
+
+    address private loser;
+
+    uint private topbid;
+
+    uint private loserbid;
+
+
+    //??????????
     mapping(address => uint) pendingReturns;
 
-    // Set to true at the end, disallows any change
-    bool ended;
+    mapping(address => string) giverNames;
 
-    // Events that will be fired on changes.
-    event HighestBidIncreased(address bidder, uint amount);
-    event AuctionEnded(address winner, uint amount);
+    mapping(address => string) giverMessages;
 
-    // The following is a so-called natspec comment,
-    // recognizable by the three slashes.
-    // It will be shown when the user is asked to
-    // confirm a transaction.
 
-    /// Create a simple auction with `_biddingTime`
-    /// seconds bidding time on behalf of the
-    /// beneficiary address `_beneficiary`.
-    function SimpleAuction() {
-        beneficiary = 0x7Ef6fA8683491521223Af5A69b923E771fF2e73A;
-        auctionStart = now;
-        biddingTime = 7 days;
+
+
+
+    constructor(
+        address _beneficiary
+    ) public {
+        boss = _beneficiary;
     }
 
-    /// Bid on the auction with the value sent
-    /// together with this transaction.
-    /// The value will only be refunded if the
-    /// auction is not won.
-    function bid() payable {
-        // No arguments are necessary, all
-        // information is already part of
-        // the transaction. The keyword payable
-        // is required for the function to
-        // be able to receive Ether.
 
-        // Revert the call if the bidding
-        // period is over.
-        require(now <= (auctionStart + biddingTime));
-
-        // If the bid is not higher, send the
-        // money back.
-        require(msg.value > highestBid);
-
-        if (highestBidder != 0) {
-            // Sending back the money by simply using
-            // highestBidder.send(highestBid) is a security risk
-            // because it could execute an untrusted contract.
-            // It is always safer to let the recipients
-            // withdraw their money themselves.
-            pendingReturns[highestBidder] += highestBid;
+    /// How much would you like to pay?
+    function bid() public payable {
+        // ??????0.0001ether
+        require(
+            msg.value > 0.0001 ether,
+            "?????"
+        );
+        // ?????????????
+        require(
+            msg.value > topbid,
+            "loser fuck off."
+        );
+        // ?????????????????????????
+        // ??????????????? payable ?????
+        pendingReturns[msg.sender] += (msg.value.div(10).mul(9));
+        fees+= msg.value.div(10);
+        
+        if(top != 0){
+            loser = top;
+            loserbid = topbid;
         }
-        highestBidder = msg.sender;
-        highestBid = msg.value;
-        HighestBidIncreased(msg.sender, msg.value);
+        top = msg.sender;
+        topbid = msg.value;
+
+        if(bytes(giverNames[msg.sender]).length== 0) {
+            giverNames[msg.sender] = "#Anonymous";
+            giverMessages[msg.sender] = "#Nothing";
+        }
     }
 
-    /// Withdraw a bid that was overbid.
-    function withdraw() returns (bool) {
+    function setInfo(string _name,string _message) public {
+        
+        giverNames[msg.sender] = _name.filter();
+        giverMessages[msg.sender] = _message.filter();
+    }
+
+    function getMyInfo() public view returns (string,string){
+        return getInfo(msg.sender);
+    }
+    
+    function getFess() public view returns (uint){
+        return fees;
+    }
+
+
+
+    function getWLInfo() public view returns (string,string,uint,string,string,uint){
+return (giverNames[top],giverMessages[top],topbid,giverNames[loser],giverMessages[loser],loserbid);
+    }
+
+
+
+    function getInfo(address _add) public view returns (string,string){
+        return (giverNames[_add],giverMessages[_add]);
+    }
+
+
+    /// ??
+    function withdraw() public returns (bool) {
+        require(pendingReturns[msg.sender]>0,"Nothing left for you");
         uint amount = pendingReturns[msg.sender];
-        if (amount > 0) {
-            // It is important to set this to zero because the recipient
-            // can call this function again as part of the receiving call
-            // before `send` returns.
-            pendingReturns[msg.sender] = 0;
-
-            if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
-        }
+        pendingReturns[msg.sender] = 0;
+        msg.sender.transfer(amount);
+        if(msg.sender==top){
+            loser = top;
+            loserbid =topbid;
+            top = 0;
+            topbid = 0;
+        }    
         return true;
     }
 
-    /// End the auction and send the highest bid
-    /// to the beneficiary.
-    function auctionEnd() {
-        // It is a good guideline to structure functions that interact
-        // with other contracts (i.e. they call functions or send Ether)
-        // into three phases:
-        // 1. checking conditions
-        // 2. performing actions (potentially changing conditions)
-        // 3. interacting with other contracts
-        // If these phases are mixed up, the other contract could call
-        // back into the current contract and modify the state or cause
-        // effects (ether payout) to be performed multiple times.
-        // If functions called internally include interaction with external
-        // contracts, they also have to be considered interaction with
-        // external contracts.
-
-        // 1. Conditions
-        require(now >= (auctionStart + biddingTime)); // auction did not yet end
-        require(!ended); // this function has already been called
-
-        // 2. Effects
-        ended = true;
-        AuctionEnded(highestBidder, highestBid);
-
-        // 3. Interaction
-        beneficiary.transfer(highestBid);
+    /// shouqian
+    function woyaoqianqian(uint fee) public {
+                require(
+            fee < fees,
+            "loser fuck off."
+        );
+        fees = fees.sub(fee);
+        // 3. ??
+        boss.transfer(fee);
     }
 }
