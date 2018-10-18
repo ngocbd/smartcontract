d410f40d95d5d1dce6714bf4719edf569e369c55
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:12:43 +0700

    add contract BatchTransfer

--- BatchTransfer.sol
+++ BatchTransfer.sol
@@ -1,20 +1,42 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BatchTransfer at 0xaa47a80f116cab5b4f03d37bcc06ac4dd4592056
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BatchTransfer at 0x58b4279f24455f78f604d1fce668f7ab2d23c658
 */
-pragma solidity ^0.4.24;
-
-contract ERC20Interface {
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-}
+pragma solidity ^0.4.23;
 
 contract BatchTransfer {
-    function transfer(address tokenAddress, address[] to, uint[] tokens) public returns (bool success) {
-        require(to.length > 0);
-        require(to.length <= 100);
-        require(to.length == tokens.length);
-        for (uint8 i = 0; i < to.length; i++) {
-            ERC20Interface(tokenAddress).transferFrom(msg.sender, to[i], tokens[i]);
-        }
-        return true;
+  address public owner;
+  uint256 public totalTransfer;
+  uint256 public totalAddresses;
+  uint256 public totalTransactions;
+
+  event Transfers(address indexed from, uint256 indexed value, uint256 indexed count);
+  
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  function batchTransfer(address[] _addresses) public payable {
+    require (msg.value > 0 && _addresses.length > 0);
+    totalTransfer += msg.value;
+    totalAddresses += _addresses.length;
+    totalTransactions++;
+    uint256 value = msg.value / _addresses.length;
+    for (uint i = 0; i < _addresses.length; i++) {
+      _addresses[i].transfer(value);
     }
+    emit Transfers(msg.sender,msg.value,_addresses.length);
+  }
+
+  function withdraw() public restricted {
+    address contractAddress = this;
+    owner.transfer(contractAddress.balance);
+  }
+
+  function () payable public {
+    msg.sender.transfer(msg.value);
+  }  
 }
