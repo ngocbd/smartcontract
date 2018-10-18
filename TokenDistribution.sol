ebe77086cf3d817fd5b69e5fafe7963e701762bd
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 20:09:37 +0700

    add contract TokenDistribution

--- TokenDistribution.sol
+++ TokenDistribution.sol
@@ -1,60 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenDistribution at 0x04d0811c922db2ce98ab7ac2599dac2d0353a394
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenDistribution at 0xc6d0ee710f619d8c6ef97ad9a830455b89774eb3
 */
-pragma solidity ^0.4.11;
-
-
-/// `Owned` is a base level contract that assigns an `owner` that can be later changed
-contract Owned {
-    /// @dev `owner` is the only address that can call a function with this
-    /// modifier
-    modifier onlyOwner { require (msg.sender == owner); _; }
-
-    address public owner;
-
-    /// @notice The Constructor assigns the message sender to be `owner`
-    function Owned() public { owner = msg.sender;}
-
-    /// @notice `owner` can step down and assign some other address to this role
-    /// @param _newOwner The address of the new owner. 0x0 can be used to create
-    ///  an unowned neutral vault, however that cannot be undone
-    function changeOwner(address _newOwner)  onlyOwner public {
-        owner = _newOwner;
-    }
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
-
-
-contract ERC20 {
-
-  function balanceOf(address who) constant public returns (uint);
-  function allowance(address owner, address spender) constant public returns (uint);
-
-  function transfer(address to, uint value) public returns (bool ok);
-  function transferFrom(address from, address to, uint value) public returns (bool ok);
-  function approve(address spender, uint value) public returns (bool ok);
-
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
-contract TokenDistribution is Owned {
-
-    ERC20 public tokenContract;
-    address public wallet;
-
-    function TokenDistribution ( address _tokenAddress, address _walletAddress ) public {
-        tokenContract = ERC20(_tokenAddress); // The Deployed Token Contract
-        wallet = _walletAddress;
-     }
-
-    function distributeTokens(address[] _owners, uint256[] _tokens) onlyOwner public {
-
-        require( _owners.length == _tokens.length );
-        for(uint i=0;i<_owners.length;i++){
-            require (tokenContract.transferFrom(wallet, _owners[i], _tokens[i]));
-        }
-
+contract TokenDistribution {
+    function distribute(ERC20 token, address[] destinations, uint[] amounts) public {
+        require(destinations.length == amounts.length);
+        uint total;
+        uint i;
+        for (i = 0; i < destinations.length; i++) total += amounts[i];
+        require(token.transferFrom(msg.sender, this, total));
+        for (i = 0; i < destinations.length; i++) require(token.transfer(destinations[i], amounts[i]));
     }
-
 }
