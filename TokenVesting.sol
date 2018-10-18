201530477bdefa6072ee0e098e7478478d91e0fc
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 20:50:04 +0700

    add contract TokenVesting

--- TokenVesting.sol
+++ TokenVesting.sol
@@ -1,301 +1,149 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenVesting at 0x627058c1d8438ae33edb07ef4c7d8c0de95ccba2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenVesting at 0x7884947f18a20d4b2b25e54b3a68e724f7a3eb80
 */
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;  
 
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
+library SafeMath {
+	function mul(uint a, uint b) internal pure returns(uint) {  
+		uint c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
+
+	function div(uint a, uint b) internal pure returns(uint) { 
+		uint c = a / b;
+		return c; 
+	}
+
+	function sub(uint a, uint b) internal pure returns(uint) {  
+		assert(b <= a);
+		return a - b;
+	}
+
+	function add(uint a, uint b) internal pure returns(uint) {  
+		uint c = a + b;
+		assert(c >= a);
+		return c;
+	}
+	function max64(uint64 a, uint64 b) internal pure  returns(uint64) { 
+		return a >= b ? a : b;
+	}
+
+	function min64(uint64 a, uint64 b) internal pure  returns(uint64) { 
+		return a < b ? a : b;
+	}
+
+	function max256(uint256 a, uint256 b) internal pure returns(uint256) { 
+		return a >= b ? a : b;
+	}
+
+	function min256(uint256 a, uint256 b) internal pure returns(uint256) {  
+		return a < b ? a : b;
+	}
+ 
+}
+
 contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
+	uint public totalSupply;
+	function balanceOf(address who) public constant returns(uint);  
+	function transfer(address to, uint value) public;  
+	event Transfer(address indexed from, address indexed to, uint value);
 }
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
+
 contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+	function allowance(address owner, address spender) public constant returns(uint);  
+	function transferFrom(address from, address to, uint value) public;  
+	function approve(address spender, uint value) public;  
+	event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 /**
- * Safe unsigned safe math.
- *
- * https://blog.aragon.one/library-driven-development-in-solidity-2bebcaf88736#.750gwtwli
- *
- * Originally from https://raw.githubusercontent.com/AragonOne/zeppelin-solidity/master/contracts/SafeMathLib.sol
- *
- * Maintained here until merged to mainline zeppelin-solidity.
- *
+ * @title TokenVesting
+ * @dev A contract can unlock token at designated time.
  */
-library SafeMathLibExt {
+contract TokenVesting  {
+  using SafeMath for uint256;
 
-    function times(uint a, uint b) public pure returns (uint) {
-        uint c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
+  event Released(uint256 amounts);
 
-    function divides(uint a, uint b) public pure returns (uint) {
-        assert(b > 0);
-        uint c = a / b;
-        assert(a == b * c + a % b);
-        return c;
-    }
+  address[] private _beneficiary ;
+  uint256[] private _unlocktime;  
+  uint256[] private _amount;
 
-    function minus(uint a, uint b) public pure returns (uint) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function plus(uint a, uint b) public pure returns (uint) {
-        uint c = a + b;
-        assert(c >= a);
-        return c;
-    }
-
-}
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-    * account.
-    */
-    constructor () public {
-        owner = msg.sender;
-    }
-
-    /**
-    * @dev Throws if called by any account other than the owner.
-    */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    /**
-    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-    * @param newOwner The address to transfer ownership to.
-    */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
-
-contract Allocatable is Ownable {
-
-  /** List of agents that are allowed to allocate new tokens */
-    mapping (address => bool) public allocateAgents;
-
-    event AllocateAgentChanged(address addr, bool state  );
+  constructor() public
+  {
+   	 _beneficiary = [0x9138D3b9d45cd8901aD1C2e670428Bc51f85c350,
+   	 0x2968d05dCF6e706F68ca8fC16F6e430fd822d742,
+   	 0xCD2C7D18325B7E09DA08DBA6f58D0E6F0e6BDf68,
+   	 0xA29459226F9aFa33b2b22093f5f9FCB9B16a9851,
+   	 0xD20D3CaC06BfC68f1d0e84855c3395D2D10CDb14,
+   	 0xd8B5C428E7F37e84d13a25C400a35fD97a2BfaBd,
+   	 0x2e5f02cb099c2b6ddc71694cafa6801eb30b60ce,
+   	 0x4e8b6b5b94ffc827b1ec2f6c172a93067248c4fa,
+   	 0xbe4c612de6221f557799b7ed456572f0c0a14bd1,
+   	 0xbe4c612de6221f557799b7ed456572f0c0a14bd1,
+   	 0x9c0A93e70143611fD5107eb865963b1E4670C852,
+   	 0x7A2D687BEDeb0B0C6e7Ef27db97Bcc5ab4d68c02,
+   	 0xf5991c3be1677F62Ac7A631108D56300634CFAcF];
+   	 
+     _unlocktime = [1546272000,1572969600,1572969600,1572969600,1569859200,1546185600,1556640000,1559318400,1551369600,1569859200,1545321600,1557244800,1564416000];
+     _amount=[227500000,773500000,136500000,91000000,1708182733,9614599,39173094,15054061,230700000,384500000,9000000,17500000,7500000];
+     
+  }
 
   /**
-   * Owner can allow a crowdsale contract to allocate new tokens.
+   * @return the beneficiary of the tokens.
    */
-    function setAllocateAgent(address addr, bool state) public onlyOwner  
-    {
-        allocateAgents[addr] = state;
-        emit AllocateAgentChanged(addr, state);
-    }
+  function beneficiary() public view returns(address[]) {
+    return _beneficiary;
+  }
 
-    modifier onlyAllocateAgent() {
-        //Only crowdsale contracts are allowed to allocate new tokens
-        require(allocateAgents[msg.sender]);
-        _;
-    }
-}
+  /**
+   * @return the unlocktime time of the token vesting.
+   */
+  function unlocktime() public view returns(uint256[]) {
+    return _unlocktime;
+  }
+   /**
+   * @return the amount of the tokens.
+   */
+  function amount() public view returns(uint256[]) {
+    return _amount;
+  }
+ 
+ 
 
-/**
- * Contract to enforce Token Vesting
- */
-contract TokenVesting is Allocatable {
+  /**
+   * @notice Transfers vested tokens to beneficiary.
+   * @param token ERC20 token which is being vested
+   */
+  function release(ERC20 token) public {
+       for(uint i = 0; i < _beneficiary.length; i++) {
+            if(block.timestamp >= _unlocktime[i] ){
+                   token.transfer(_beneficiary[i], _amount[i].mul(10**18));
+                    emit Released( _amount[i]);
+                    _amount[i]=0;
+            }
+       }
+  }
 
-    using SafeMathLibExt for uint;
-
-    address public crowdSaleTokenAddress;
-
-    /** keep track of total tokens yet to be released, 
-     * this should be less than or equal to UTIX tokens held by this contract. 
-     */
-    uint256 public totalUnreleasedTokens;
-
-    // default vesting parameters
-    uint256 private startAt = 0;
-    uint256 private cliff = 1;
-    uint256 private duration = 4; 
-    uint256 private step = 300; //15778463;  //2592000;
-    bool private changeFreezed = false;
-
-    struct VestingSchedule {
-        uint256 startAt;
-        uint256 cliff;
-        uint256 duration;
-        uint256 step;
-        uint256 amount;
-        uint256 amountReleased;
-        bool changeFreezed;
-    }
-
-    mapping (address => VestingSchedule) public vestingMap;
-
-    event VestedTokensReleased(address _adr, uint256 _amount);
-    
-    constructor(address _tokenAddress) public {
+  /**
+   * @notice Release the unexpected token.
+   * @param token ERC20 token which is being vested
+   */
+  
+    function checkRelease(ERC20 token) public {
         
-        crowdSaleTokenAddress = _tokenAddress;
-    }
-
-    /** Modifier to check if changes to vesting is freezed  */
-    modifier changesToVestingFreezed(address _adr) {
-        require(vestingMap[_adr].changeFreezed);
-        _;
-    }
-
-    /** Modifier to check if changes to vesting is not freezed yet  */
-    modifier changesToVestingNotFreezed(address adr) {
-        require(!vestingMap[adr].changeFreezed); // if vesting not set then also changeFreezed will be false
-        _;
-    }
-
-    /** Function to set default vesting schedule parameters. */
-    function setDefaultVestingParameters(
-        uint256 _startAt, uint256 _cliff, uint256 _duration,
-        uint256 _step, bool _changeFreezed) public onlyAllocateAgent {
-
-        // data validation
-        require(_step != 0);
-        require(_duration != 0);
-        require(_cliff <= _duration);
-
-        startAt = _startAt;
-        cliff = _cliff;
-        duration = _duration; 
-        step = _step;
-        changeFreezed = _changeFreezed;
-
-    }
-
-    /** Function to set vesting with default schedule. */
-    function setVestingWithDefaultSchedule(address _adr, uint256 _amount) 
-    public 
-    changesToVestingNotFreezed(_adr) onlyAllocateAgent {
-       
-        setVesting(_adr, startAt, cliff, duration, step, _amount, changeFreezed);
-    }    
-
-    /** Function to set/update vesting schedule. PS - Amount cannot be changed once set */
-    function setVesting(
-        address _adr,
-        uint256 _startAt,
-        uint256 _cliff,
-        uint256 _duration,
-        uint256 _step,
-        uint256 _amount,
-        bool _changeFreezed) 
-    public changesToVestingNotFreezed(_adr) onlyAllocateAgent {
-
-        VestingSchedule storage vestingSchedule = vestingMap[_adr];
-
-        // data validation
-        require(_step != 0);
-        require(_amount != 0 || vestingSchedule.amount > 0);
-        require(_duration != 0);
-        require(_cliff <= _duration);
-
-        //if startAt is zero, set current time as start time.
-        if (_startAt == 0) 
-            _startAt = block.timestamp;
-
-        vestingSchedule.startAt = _startAt;
-        vestingSchedule.cliff = _cliff;
-        vestingSchedule.duration = _duration;
-        vestingSchedule.step = _step;
-
-        // special processing for first time vesting setting
-        if (vestingSchedule.amount == 0) {
-            // check if enough tokens are held by this contract
-            ERC20 token = ERC20(crowdSaleTokenAddress);
-            require(token.balanceOf(this) >= totalUnreleasedTokens.plus(_amount));
-            totalUnreleasedTokens = totalUnreleasedTokens.plus(_amount);
-            vestingSchedule.amount = _amount; 
+       uint num = 0;
+        for(uint i = 0; i < _amount.length; i++) {
+            num = num.add(_amount[i]); 
         }
-
-        vestingSchedule.amountReleased = 0;
-        vestingSchedule.changeFreezed = _changeFreezed;
-    }
-
-    function isVestingSet(address adr) public view returns (bool isSet) {
-        return vestingMap[adr].amount != 0;
-    }
-
-    function freezeChangesToVesting(address _adr) public changesToVestingNotFreezed(_adr) onlyAllocateAgent {
-        require(isVestingSet(_adr)); // first check if vesting is set
-        vestingMap[_adr].changeFreezed = true;
-    }
-
-    /** Release tokens as per vesting schedule, called by contributor  */
-    function releaseMyVestedTokens() public changesToVestingFreezed(msg.sender) {
-        releaseVestedTokens(msg.sender);
-    }
-
-    /** Release tokens as per vesting schedule, called by anyone  */
-    function releaseVestedTokens(address _adr) public changesToVestingFreezed(_adr) {
-        VestingSchedule storage vestingSchedule = vestingMap[_adr];
+        if(num==0){
+             token.transfer(_beneficiary[0],token.balanceOf(this));
+        }
         
-        // check if all tokens are not vested
-        require(vestingSchedule.amount.minus(vestingSchedule.amountReleased) > 0);
-        
-        // calculate total vested tokens till now
-        uint256 totalTime = block.timestamp - vestingSchedule.startAt;
-        uint256 totalSteps = totalTime / vestingSchedule.step;
+  }
 
-        // check if cliff is passed
-        require(vestingSchedule.cliff <= totalSteps);
-
-        uint256 tokensPerStep = vestingSchedule.amount / vestingSchedule.duration;
-        // check if amount is divisble by duration
-        if (tokensPerStep * vestingSchedule.duration != vestingSchedule.amount) tokensPerStep++;
-
-        uint256 totalReleasableAmount = tokensPerStep.times(totalSteps);
-
-        // handle the case if user has not claimed even after vesting period is over or amount was not divisible
-        if (totalReleasableAmount > vestingSchedule.amount) totalReleasableAmount = vestingSchedule.amount;
-
-        uint256 amountToRelease = totalReleasableAmount.minus(vestingSchedule.amountReleased);
-        vestingSchedule.amountReleased = vestingSchedule.amountReleased.plus(amountToRelease);
-
-        // transfer vested tokens
-        ERC20 token = ERC20(crowdSaleTokenAddress);
-        token.transfer(_adr, amountToRelease);
-        // decrement overall unreleased token count
-        totalUnreleasedTokens = totalUnreleasedTokens.minus(amountToRelease);
-        emit VestedTokensReleased(_adr, amountToRelease);
-    }
-
-    /**
-    * Allow to (re)set Token.
-    */
-    function setCrowdsaleTokenExtv1(address _token) public onlyAllocateAgent {       
-        crowdSaleTokenAddress = _token;
-    }
 }
