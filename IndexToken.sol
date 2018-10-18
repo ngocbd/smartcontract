63d7fc9de330dc20d41e882e8c455a6469edd848
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:56:19 +0700

    add contract IndexToken

--- IndexToken.sol
+++ IndexToken.sol
@@ -1,15 +1,10 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IndexToken at 0xa56a4e82d89db2d2a433dfdc94404a7dfe8d7b44
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract IndexToken at 0x9e54a55d27cb8c730d95770d9a8972fd36868701
 */
 pragma solidity 0.4.24;
 
 // File: contracts\misc\Ownable.sol
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable 
 {
     address public owner;
@@ -27,32 +22,26 @@ contract Ownable
 
     modifier onlyOwner() 
     {
-        require(msg.sender == owner, "Incorrect Owner");
+        require(msg.sender == owner);
         _;
     }
 
-    function transferOwnership(address _newOwner) public
+    function transferOwnership(address _newOwner) public 
     onlyOwner 
     {
-        require(_newOwner != address(0), "Address should not be 0x0");
-        emit OwnershipTransferred(owner, _newOwner);
-        owner = _newOwner;
+        _transferOwnership(_newOwner);
     }
 
-    function renounceOwnership() public 
-    onlyOwner 
+    function _transferOwnership(address _newOwner) internal 
     {
-        emit OwnershipRenounced(owner);
-        owner = address(0);
+        require(_newOwner != address(0));
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
     }
 }
 
 // File: contracts\misc\SafeMath.sol
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that revert on error
- */
 library SafeMath 
 {
     function mul(uint256 a, uint256 b) internal pure 
@@ -92,10 +81,6 @@ library SafeMath
 
 // File: contracts\token\ERC20Basic.sol
 
-/**
- * @title ERC20 Basic interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20Basic 
 {
     function totalSupply() public view returns (uint256);
@@ -109,9 +94,9 @@ contract ERC20Basic
 contract BasicToken is ERC20Basic 
 {
     using SafeMath for uint256;
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) balances;
     
-    uint256 public totalSupply_;
+    uint256 totalSupply_;
 
     function totalSupply() public view 
     returns (uint256) 
@@ -140,37 +125,30 @@ contract BasicToken is ERC20Basic
 
 // File: contracts\token\BurnableToken.sol
 
-/**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
- */
 contract BurnableToken is BasicToken, Ownable
 {
     event Burn(address indexed burner, uint256 value);
 
-    function burn(uint256 value) public
+    function burn(address burnAddress, uint256 value) public 
     onlyOwner
     {
-        address burnAddress = msg.sender;
         require(value <= balances[burnAddress]);
 
         balances[burnAddress] = balances[burnAddress].sub(value);
         totalSupply_ = totalSupply_.sub(value);
-
         emit Burn(burnAddress, value);
+        emit Transfer(burnAddress, address(0), value);
     }
 }
 
 // File: contracts\token\ERC20.sol
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20 is ERC20Basic 
 {
     function allowance(address owner, address spender) public view returns (uint256);
+
     function transferFrom(address from, address to, uint256 value) public returns (bool);
+
     function approve(address spender, uint256 value) public returns (bool); 
     
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -178,13 +156,6 @@ contract ERC20 is ERC20Basic
 
 // File: contracts\token\StandardToken.sol
 
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
- * Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
 contract StandardToken is ERC20, BasicToken 
 {
     mapping (address => mapping (address => uint256)) internal allowed;
@@ -202,6 +173,7 @@ contract StandardToken is ERC20, BasicToken
         emit Transfer(_from, _to, _value);
         return true;
     }
+
 
     function approve(address _spender, uint256 _value) public 
     returns (bool) 
@@ -248,11 +220,6 @@ contract StandardToken is ERC20, BasicToken
 
 // File: contracts\token\MintableToken.sol
 
-/**
- * @title Mintable token
- * @dev Simple ERC20 Token example, with mintable token creation
- * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
- */
 contract MintableToken is StandardToken, Ownable 
 {
     event Mint(address indexed to, uint256 amount);
@@ -269,12 +236,8 @@ contract MintableToken is StandardToken, Ownable
     }
 }
 
-// File: contracts\misc\Pausable.sol
+// File: contracts\token\Pausable.sol
 
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
 contract Pausable is Ownable 
 {
     event Pause();
@@ -309,56 +272,12 @@ contract Pausable is Ownable
     }
 }
 
-// File: contracts\token\PausableToken.sol
-
-/**
- * @title Pausable token
- * @dev StandardToken modified with pausable transfers.
- **/
-contract PausableToken is StandardToken, Pausable 
-{
-    function transfer(address _to, uint256 _value) public
-    whenNotPaused
-    returns (bool)
-    {
-        return super.transfer(_to, _value);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public
-    whenNotPaused
-    returns (bool)
-    {
-        return super.transferFrom(_from, _to, _value);
-    }
-
-    function approve(address _spender, uint256 _value) public
-    whenNotPaused
-    returns (bool)
-    {
-        return super.approve(_spender, _value);
-    }
-
-    function increaseApproval(address _spender, uint _addedValue) public
-    whenNotPaused
-    returns (bool success)
-    {
-        return super.increaseApproval(_spender, _addedValue);
-    }
-
-    function decreaseApproval(address _spender, uint _subtractedValue) public
-    whenNotPaused
-    returns (bool success)
-    {
-        return super.decreaseApproval(_spender, _subtractedValue);
-    }
-}
-
 // File: contracts\IndexToken.sol
 
-contract IndexToken is BurnableToken, MintableToken, PausableToken
+contract IndexToken is BurnableToken, MintableToken, Pausable
 {
-    string constant public name = "DQR 30";
-    string constant public symbol = "dqr30";
+    string constant public name = "Coffee Token";
+    string constant public symbol = "dqr";
 
     uint public decimals = 18;
 }
