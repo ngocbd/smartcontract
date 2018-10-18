4eae5dc1a876dfa711b194e82c7fee689e417cb9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 11:20:24 +0700

    add contract Example

---
+++ Example.sol
@@ -0,0 +1,12 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Example at 0x777775f29694805112cebe0f1c8996a8f14c1777
+*/
+pragma solidity ^0.4.24;
+
+
+contract Example {
+    uint256 public value;
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+}
