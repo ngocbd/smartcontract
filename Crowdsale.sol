6c088078d50746de1494334bd04588f4d357e349
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:11:33 +0700

    add contract Crowdsale

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0x15b9a572f6fc95271afcb9c245379f2fb13d7524
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Crowdsale at 0xa298d0e1da04b6f62c9ae3f23406f37d8079648c
 */
 pragma solidity ^0.4.21;
 interface token {
@@ -12,15 +12,15 @@ contract Crowdsale {
     token public tokenReward = token(0xE15B50c6C5fDF84504024Db11b8Ae37979270d51);
     address BDCPadd = 0xb2416061B9AA21CEAd5fB5cF081c4eeD7BCBf508;
     address BDadd   = 0x881435DCBBaA9E9a642c15ce3b2D33Af57961184;
-    address AGadd   = 0x5DcbA0d785cA47ae4134Cda411e7431699CD9625;
-    //address DTadd   = 0x191b50D5FEc03ccE9d6b2A98BB2a8243b82De85C;
+    address AGadd   = 0x623Ce54F4d70682566a387f8eA32452ff2b23480;
+    address DTadd   = 0x9b3383d9393312336547C795aDE8AeeB6f7C126f;
  
     function () payable public{
         tokenReward.sell(msg.sender,msg.value);
         BDCPadd.transfer(msg.value * 60 / 100);
-        BDadd.transfer(msg.value * 35 / 100);
-        AGadd.transfer(msg.value * 5 / 100);
-       // DTadd.transfer(msg.value * 5 / 100);
+        BDadd.transfer(msg.value * 25 / 100);
+        AGadd.transfer(msg.value * 10 / 100);
+        DTadd.transfer(msg.value * 5 / 100);
     }
 
 }
