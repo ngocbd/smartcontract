fd29fa79d62733837994ff953705186f7397e901
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 12:29:30 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,16 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x4e632d85c1b34955c69ac7b6e7be8dc7297a5efe
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x1be34bec23ef52fca1b5bb9ce27da1277ca4eebd
 */
 pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
+contract DateTime {
+		function toTimestamp(uint16 year, uint8 month, uint8 day) public constant returns (uint timestamp);
+        function getYear(uint timestamp) public constant returns (uint16);
+        function getMonth(uint timestamp) public constant returns (uint8);
+        function getDay(uint timestamp) public constant returns (uint8);
+}
+
 contract TokenERC20 {
     // Public variables of the token
-    string public name;
-    string public symbol;
+    string public name = "mHealthCoin";
+    string public symbol = "MHEC";
     uint8 public decimals = 18;
-    uint256 public totalSupply;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply = 4000000000 * 10 ** uint256(decimals);
+	address public owner;
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
@@ -24,21 +33,21 @@ contract TokenERC20 {
 
     // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
+	
+	//Date related code
+	address public dateTimeAddr = 0x1a6184CD4C5Bea62B0116de7962EE7315B7bcBce;
+	DateTime dateTime = DateTime(dateTimeAddr);
+	uint[] lockupTime = [dateTime.toTimestamp(2018,11,13),dateTime.toTimestamp(2019,1,13),dateTime.toTimestamp(2019,3,13),dateTime.toTimestamp(2019,5,13)];
+	uint8[] lockupPercent = [0,25,50,75];
 
     /**
      * Constructor function
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    function TokenERC20(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol
-    ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+    constructor() public {
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
+		owner = msg.sender;
     }
 
     /**
@@ -51,6 +60,15 @@ contract TokenERC20 {
         require(balanceOf[_from] >= _value);
         // Check for overflows
         require(balanceOf[_to] + _value >= balanceOf[_to]);
+		
+		//Check for lockup period and lockup percentage
+		uint256 i=0;
+		for (uint256 l = lockupTime.length; i < l; i++) {
+			if(now < lockupTime[i]) break;
+		}
+		uint256 maxAmount = (i<1)? 0 : 
+			( (i>=lockupPercent.length)? balanceOf[_from] : (lockupPercent[i] * balanceOf[_from] / 100) );
+		if(_from != owner) require(_value <= maxAmount);
         // Save this for an assertion in the future
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
         // Subtract from the sender
@@ -137,6 +155,24 @@ contract TokenERC20 {
         balanceOf[msg.sender] -= _value;            // Subtract from the sender
         totalSupply -= _value;                      // Updates totalSupply
         emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
         return true;
     }
 }
