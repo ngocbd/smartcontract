7c40ceac9bad9b5a7923be269bd16810758e5569
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:00:46 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xac34fe6eda53f58cfd4743299fcfd9b40bf4d08a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xcd49432c28047d6319ff962c60ee0f4820058c41
 */
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// 'ACT111074' token contract
+// 'ACT255670' token contract
 //
 // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-// Symbol      : ACT111074
-// Name        : ADZbuzz Socialmediatoday.com Community Token
+// Symbol      : ACT255670
+// Name        : ADZbuzz Wunderground.com Community Token
 // Total supply: 2000000
 // Decimals    : 8
 //
@@ -117,8 +117,8 @@ contract ADZbuzzCommunityToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     function ADZbuzzCommunityToken() public {
-        symbol = "ACT111074";
-        name = "ADZbuzz Socialmediatoday.com Community Token";
+        symbol = "ACT255670";
+        name = "ADZbuzz Wunderground.com Community Token";
         decimals = 8;
         _totalSupply = 200000000000000;
         balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
