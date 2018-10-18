9ecbf76b0e74ce933368c7ffb55fe4890d6626f7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:52:09 +0700

    add contract FlyDropTokenMgr

--- FlyDropTokenMgr.sol
+++ FlyDropTokenMgr.sol
@@ -1,10 +1,146 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlyDropTokenMgr at 0x5e314712181d62c143d2d5f782103647d2c407f4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract FlyDropTokenMgr at 0x5c3d0012ff81bb9a19d13f8e512d3e72843f0848
 */
 pragma solidity ^0.4.24;
 
-interface itoken {
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) external returns (bool);
+contract RBAC {
+  using Roles for Roles.Role;
+
+  mapping (string => Roles.Role) private roles;
+
+  event RoleAdded(address indexed operator, string role);
+  event RoleRemoved(address indexed operator, string role);
+
+  /**
+   * @dev reverts if addr does not have role
+   * @param _operator address
+   * @param _role the name of the role
+   * // reverts
+   */
+  function checkRole(address _operator, string _role)
+    public
+    view
+  {
+    roles[_role].check(_operator);
+  }
+
+  /**
+   * @dev determine if addr has role
+   * @param _operator address
+   * @param _role the name of the role
+   * @return bool
+   */
+  function hasRole(address _operator, string _role)
+    public
+    view
+    returns (bool)
+  {
+    return roles[_role].has(_operator);
+  }
+
+  /**
+   * @dev add a role to an address
+   * @param _operator address
+   * @param _role the name of the role
+   */
+  function addRole(address _operator, string _role)
+    internal
+  {
+    roles[_role].add(_operator);
+    emit RoleAdded(_operator, _role);
+  }
+
+  /**
+   * @dev remove a role from an address
+   * @param _operator address
+   * @param _role the name of the role
+   */
+  function removeRole(address _operator, string _role)
+    internal
+  {
+    roles[_role].remove(_operator);
+    emit RoleRemoved(_operator, _role);
+  }
+
+  /**
+   * @dev modifier to scope access to a single role (uses msg.sender as addr)
+   * @param _role the name of the role
+   * // reverts
+   */
+  modifier onlyRole(string _role)
+  {
+    checkRole(msg.sender, _role);
+    _;
+  }
+
+  /**
+   * @dev modifier to scope access to a set of roles (uses msg.sender as addr)
+   * @param _roles the names of the roles to scope access to
+   * // reverts
+   *
+   * @TODO - when solidity supports dynamic arrays as arguments to modifiers, provide this
+   *  see: https://github.com/ethereum/solidity/issues/2467
+   */
+  // modifier onlyRoles(string[] _roles) {
+  //     bool hasAnyRole = false;
+  //     for (uint8 i = 0; i < _roles.length; i++) {
+  //         if (hasRole(msg.sender, _roles[i])) {
+  //             hasAnyRole = true;
+  //             break;
+  //         }
+  //     }
+
+  //     require(hasAnyRole);
+
+  //     _;
+  // }
+}
+
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an address access to this role
+   */
+  function add(Role storage _role, address _addr)
+    internal
+  {
+    _role.bearer[_addr] = true;
+  }
+
+  /**
+   * @dev remove an address' access to this role
+   */
+  function remove(Role storage _role, address _addr)
+    internal
+  {
+    _role.bearer[_addr] = false;
+  }
+
+  /**
+   * @dev check if an address has this role
+   * // reverts
+   */
+  function check(Role storage _role, address _addr)
+    internal
+    view
+  {
+    require(has(_role, _addr));
+  }
+
+  /**
+   * @dev check if an address has this role
+   * @return bool
+   */
+  function has(Role storage _role, address _addr)
+    internal
+    view
+    returns (bool)
+  {
+    return _role.bearer[_addr];
+  }
 }
 
 library SafeMath {
@@ -12,43 +148,43 @@ library SafeMath {
   /**
   * @dev Multiplies two numbers, throws on overflow.
   */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
     // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
     // benefit is lost if 'b' is also tested.
     // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
+    if (_a == 0) {
       return 0;
     }
 
-    c = a * b;
-    assert(c / a == b);
+    c = _a * _b;
+    assert(c / _a == _b);
     return c;
   }
 
   /**
   * @dev Integer division of two numbers, truncating the quotient.
   */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    // assert(_b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = _a / _b;
+    // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+    return _a / _b;
   }
 
   /**
   * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    assert(_b <= _a);
+    return _a - _b;
   }
 
   /**
   * @dev Adds two numbers, throws on overflow.
   */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
+    c = _a + _b;
+    assert(c >= _a);
     return c;
   }
 }
@@ -82,6 +218,9 @@ contract Ownable {
 
   /**
    * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
    */
   function renounceOwnership() public onlyOwner {
     emit OwnershipRenounced(owner);
@@ -122,41 +261,28 @@ contract Claimable is Ownable {
    * @dev Allows the current owner to set the pendingOwner address.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner public {
+  function transferOwnership(address newOwner) public onlyOwner {
     pendingOwner = newOwner;
   }
 
   /**
    * @dev Allows the pendingOwner address to finalize the transfer.
    */
-  function claimOwnership() onlyPendingOwner public {
+  function claimOwnership() public onlyPendingOwner {
     emit OwnershipTransferred(owner, pendingOwner);
     owner = pendingOwner;
     pendingOwner = address(0);
   }
 }
 
-contract FlyDropToken is Claimable {
+contract SimpleFlyDropToken is Claimable {
     using SafeMath for uint256;
 
     ERC20 internal erc20tk;
-    bytes[] internal approveRecords;
 
-    event ReceiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
-
-    /**
-     * @dev receive approval from an ERC20 token contract, take a record
-     *
-     * @param _from address The address which you want to send tokens from
-     * @param _value uint256 the amounts of tokens to be sent
-     * @param _token address the ERC20 token address
-     * @param _extraData bytes the extra data for the record
-     */
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public {
+    function setToken(address _token) onlyOwner public {
+        require(_token != address(0));
         erc20tk = ERC20(_token);
-        require(erc20tk.transferFrom(_from, this, _value)); // transfer tokens to this contract
-        approveRecords.push(_extraData);
-        emit ReceiveApproval(_from, _value, _token, _extraData);
     }
 
     /**
@@ -177,37 +303,6 @@ contract FlyDropToken is Claimable {
 
         return (i);
     }
-
-    /**
-     * @dev Send tokens to other multi addresses in one function
-     *
-     * @param _from address The address which you want to send tokens from
-     * @param _destAddrs address The addresses which you want to send tokens to
-     * @param _values uint256 the amounts of tokens to be sent
-     */
-    function multiSendFrom(address _from, address[] _destAddrs, uint256[] _values) onlyOwner public returns (uint256) {
-        require(_destAddrs.length == _values.length);
-
-        uint256 i = 0;
-        for (; i < _destAddrs.length; i = i.add(1)) {
-            if (!erc20tk.transferFrom(_from, _destAddrs[i], _values[i])) {
-                break;
-            }
-        }
-
-        return (i);
-    }
-
-    /**
-     * @dev get records about approval
-     *
-     * @param _ind uint the index of record
-     */
-    function getApproveRecord(uint _ind) onlyOwner public view returns (bytes) {
-        require(_ind < approveRecords.length);
-
-        return approveRecords[_ind];
-    }
 }
 
 contract DelayedClaimable is Claimable {
@@ -221,7 +316,7 @@ contract DelayedClaimable is Claimable {
    * @param _start The earliest time ownership can be claimed.
    * @param _end The latest time ownership can be claimed.
    */
-  function setLimits(uint256 _start, uint256 _end) onlyOwner public {
+  function setLimits(uint256 _start, uint256 _end) public onlyOwner {
     require(_start <= _end);
     end = _end;
     start = _start;
@@ -231,7 +326,7 @@ contract DelayedClaimable is Claimable {
    * @dev Allows the pendingOwner address to finalize the transfer, as long as it is called within
    * the specified start and end time.
    */
-  function claimOwnership() onlyPendingOwner public {
+  function claimOwnership() public onlyPendingOwner {
     require((block.number <= end) && (block.number >= start));
     emit OwnershipTransferred(owner, pendingOwner);
     owner = pendingOwner;
@@ -241,11 +336,61 @@ contract DelayedClaimable is Claimable {
 
 }
 
-contract FlyDropTokenMgr is DelayedClaimable {
+contract Poweruser is DelayedClaimable, RBAC {
+  string public constant ROLE_POWERUSER = "poweruser";
+
+  constructor () public {
+    addRole(msg.sender, ROLE_POWERUSER);
+  }
+
+  /**
+   * @dev Throws if called by any account that's not a superuser.
+   */
+  modifier onlyPoweruser() {
+    checkRole(msg.sender, ROLE_POWERUSER);
+    _;
+  }
+
+  modifier onlyOwnerOrPoweruser() {
+    require(msg.sender == owner || isPoweruser(msg.sender));
+    _;
+  }
+
+  /**
+   * @dev getter to determine if address has poweruser role
+   */
+  function isPoweruser(address _addr)
+    public
+    view
+    returns (bool)
+  {
+    return hasRole(_addr, ROLE_POWERUSER);
+  }
+
+  /**
+   * @dev Add a new account address as power user.
+   * @param _newPoweruser The address to be as a power user.
+   */
+  function addPoweruser(address _newPoweruser) public onlyOwner {
+    require(_newPoweruser != address(0));
+    addRole(_newPoweruser, ROLE_POWERUSER);
+  }
+
+  /**
+   * @dev Remove a new account address from power user list.
+   * @param _oldPoweruser The address to be as a power user.
+   */
+  function removePoweruser(address _oldPoweruser) public onlyOwner {
+    require(_oldPoweruser != address(0));
+    removeRole(_oldPoweruser, ROLE_POWERUSER);
+  }
+}
+
+contract FlyDropTokenMgr is Poweruser {
     using SafeMath for uint256;
 
     address[] dropTokenAddrs;
-    FlyDropToken currentDropTokenContract;
+    SimpleFlyDropToken currentDropTokenContract;
     // mapping(address => mapping (address => uint256)) budgets;
 
     /**
@@ -255,13 +400,11 @@ contract FlyDropTokenMgr is DelayedClaimable {
      * @param _from address The address which you want to send tokens from
      * @param _value uint256 the amounts of tokens to be sent
      * @param _token address the ERC20 token address
-     * @param _extraData bytes the extra data for the record
      */
     function prepare(uint256 _rand,
                      address _from,
                      address _token,
-                     uint256 _value,
-                     bytes _extraData) onlyOwner public returns (bool) {
+                     uint256 _value) onlyOwnerOrPoweruser public returns (bool) {
         require(_token != address(0));
         require(_from != address(0));
         require(_rand > 0);
@@ -271,16 +414,17 @@ contract FlyDropTokenMgr is DelayedClaimable {
         }
 
         if (_rand > dropTokenAddrs.length) {
-            FlyDropToken dropTokenContract = new FlyDropToken();
+            SimpleFlyDropToken dropTokenContract = new SimpleFlyDropToken();
             dropTokenAddrs.push(address(dropTokenContract));
             currentDropTokenContract = dropTokenContract;
         } else {
-            currentDropTokenContract = FlyDropToken(dropTokenAddrs[_rand.sub(1)]);
+            currentDropTokenContract = SimpleFlyDropToken(dropTokenAddrs[_rand.sub(1)]);
         }
 
-        ERC20(_token).transferFrom(_from, this, _value);
+        currentDropTokenContract.setToken(_token);
+        return ERC20(_token).transferFrom(_from, currentDropTokenContract, _value);
         // budgets[_token][_from] = budgets[_token][_from].sub(_value);
-        return itoken(_token).approveAndCall(currentDropTokenContract, _value, _extraData);
+        // return itoken(_token).approveAndCall(currentDropTokenContract, _value, _extraData);
         // return true;
     }
 
@@ -297,7 +441,7 @@ contract FlyDropTokenMgr is DelayedClaimable {
      * @param _destAddrs address The addresses which you want to send tokens to
      * @param _values uint256 the amounts of tokens to be sent
      */
-    function flyDrop(address[] _destAddrs, uint256[] _values) onlyOwner public returns (uint256) {
+    function flyDrop(address[] _destAddrs, uint256[] _values) onlyOwnerOrPoweruser public returns (uint256) {
         require(address(currentDropTokenContract) != address(0));
         return currentDropTokenContract.multiSend(_destAddrs, _values);
     }
@@ -306,19 +450,19 @@ contract FlyDropTokenMgr is DelayedClaimable {
 
 contract ERC20Basic {
   function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+  function balanceOf(address _who) public view returns (uint256);
+  function transfer(address _to, uint256 _value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender)
+  function allowance(address _owner, address _spender)
     public view returns (uint256);
 
-  function transferFrom(address from, address to, uint256 value)
+  function transferFrom(address _from, address _to, uint256 _value)
     public returns (bool);
 
-  function approve(address spender, uint256 value) public returns (bool);
+  function approve(address _spender, uint256 _value) public returns (bool);
   event Approval(
     address indexed owner,
     address indexed spender,
