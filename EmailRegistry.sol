1ca66379015f0ab727ccd124ad73456d01788a74
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:18:20 +0700

    add contract EmailRegistry

--- EmailRegistry.sol
+++ EmailRegistry.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmailRegistry at 0x6c6fc67bc549e31343297a5f892c5eb52f2e665b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmailRegistry at 0x20c576c6a2a706c8d15639d58038d94f9010971d
 */
 pragma solidity ^0.4.18; // solhint-disable-line
 
@@ -7,16 +7,7 @@ pragma solidity ^0.4.18; // solhint-disable-line
 
 contract EmailRegistry {
     mapping (address => string) public emails;
-    address [] public registeredAddresses;
-    function registerEmail(string email) public{
-        require(bytes(email).length>0);
-        //if previously unregistered, add to list
-        if(bytes(emails[msg.sender]).length==0){
-            registeredAddresses.push(msg.sender);
-        }
+    function registerEmail(string email){
         emails[msg.sender]=email;    
-    }
-    function numRegistered() public constant returns(uint count) {
-        return registeredAddresses.length;
     }
 }
