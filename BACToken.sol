1eba6cfba85d5f6eb125b830146e38a22ea46708
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 11:48:18 +0700

    add contract BACToken

--- BACToken.sol
+++ BACToken.sol
@@ -1,177 +1,224 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BACToken at 0x8247637b5154287fd1ce157bd5d6768e117afb81
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BACToken at 0x42f80b0371d44813acc22641e9e9cca0569b0bb8
 */
-// Abstract contract for the full ERC 20 Token standard
-// https://github.com/ethereum/EIPs/issues/20
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.24;
 
-contract Token {
-    
-/* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
-    uint256 public totalSupply;
-    This automatically creates a getter function for the totalSupply.
-    This is moved to the base contract since public getter functions are not
-    currently recognised as an implementation of the matching abstract
-    function by the compiler.
-    */
-    /// total amount of tokens
-    uint256 public totalSupply;
+library SafeMath {
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
-/*
-You should inherit from StandardToken or, for a token like you would want to
-deploy in something like Mist, see HumanStandardToken.sol.
-(This implements ONLY the standard functions and NOTHING else.
-If you deploy this, you won't have anything useful.)
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
-Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
-.*/
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-contract StandardToken is Token {
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+  mapping(address => uint256) balances;
+
+  uint256 totalSupply_;
+
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint256) {
+    return balances[_owner];
+  }
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
     }
+    emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
 }
 
 contract BACToken is StandardToken {
 
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
+  address public administror;
+  string public name = "BACToken";
+  string public symbol = "BAC";
+  uint8 public decimals = 8;
+  uint256 public INITIAL_SUPPLY = 98000000*10**8;
+  mapping (address => uint256) public frozenAccount;
+
+  // ??
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Burn(address indexed target, uint256 value);
+
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    administror = msg.sender;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+
+  // ??
+  function SEOS(uint256 _amount) public returns (bool) {
+    require(msg.sender == administror);
+    balances[msg.sender] = balances[msg.sender].add(_amount);
+    totalSupply_ = totalSupply_.add(_amount);
+    INITIAL_SUPPLY = totalSupply_;
+    return true;
+  }
+
+  // ????
+  function freezeAccount(address _target, uint _timestamp) public returns (bool) {
+    require(msg.sender == administror);
+    require(_target != address(0));
+    frozenAccount[_target] = _timestamp;
+    return true;
+  }
+
+  // ??????
+  function multiFreezeAccount(address[] _targets, uint _timestamp) public returns (bool) {
+    require(msg.sender == administror);
+    uint256 len = _targets.length;
+    require(len > 0);
+    for (uint256 i = 0; i < len; i = i.add(1)) {
+      address _target = _targets[i];
+      require(_target != address(0));
+      frozenAccount[_target] = _timestamp;
     }
+    return true;
+  }
 
-    /* Public variables of the token */
+  // ??
+  function transfer(address _target, uint256 _amount) public returns (bool) {
+    require(now > frozenAccount[msg.sender]);
+    require(_target != address(0));
+    require(balances[msg.sender] >= _amount);
+    balances[_target] = balances[_target].add(_amount);
+    balances[msg.sender] = balances[msg.sender].sub(_amount);
 
-    
-/*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'B0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    emit Transfer(msg.sender, _target, _amount);
 
-    
-function BACToken
-(
-        uint256 _initialAmount,
-        string _tokenName,
-        uint8 _decimalUnits,
-        string _tokenSymbol
-        )
- 
-{
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-        totalSupply = _initialAmount;                        // Update total supply
-        name = _tokenName;                                   // Set the name for display purposes
-        decimals = _decimalUnits;                            // Amount of decimals for display purposes
-        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+    return true;
+  }
+
+  // ????
+  function multiTransfer(address[] _targets, uint256[] _amounts) public returns (bool) {
+    require(now > frozenAccount[msg.sender]);
+    uint256 len = _targets.length;
+    require(len > 0);
+    uint256 totalAmount = 0;
+    for (uint256 i = 0; i < len; i = i.add(1)) {
+      totalAmount = totalAmount.add(_amounts[i]);
     }
+    require(balances[msg.sender] >= totalAmount);
+    for (uint256 j = 0; j < len; j = j.add(1)) {
+      address _target = _targets[j];
+      uint256 _amount = _amounts[j];
+      require(_target != address(0));
+      balances[_target] = balances[_target].add(_amount);
+      balances[msg.sender] = balances[msg.sender].sub(_amount);
 
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
+      emit Transfer(msg.sender, _target, _amount);
     }
+  }
 
-    /* This notifies clients about the amount burnt */
-    event Burn(address indexed from, uint256 value);
+  // ??
+  function burn(address _target, uint256 _amount) public returns (bool) {
+    require(msg.sender == administror);
+    require(_target != address(0));
+    require(balances[_target] >= _amount);
+    balances[_target] = balances[_target].sub(_amount);
+    totalSupply_ = totalSupply_.sub(_amount);
+    INITIAL_SUPPLY = totalSupply_;
 
-    function burn(uint256 _value) returns (bool success) {
-        if (balances[msg.sender] < _value) throw;            // Check if the sender has enough
-        balances[msg.sender] -= _value;                      // Subtract from the sender
-        totalSupply -= _value;                                // Updates totalSupply
-        Burn(msg.sender, _value);
-        return true;
-    }
+    emit Burn(_target, _amount);
 
-    function burnFrom(address _from, uint256 _value) returns (bool success) {
-        if (balances[_from] < _value) throw;                // Check if the sender has enough
-        if (_value > allowed[_from][msg.sender]) throw;    // Check allowance
-        balances[_from] -= _value;                          // Subtract from the sender
-        totalSupply -= _value;                               // Updates totalSupply
-        Burn(_from, _value);
-        return true;
-    }
+    return true;
+  }
+
+  // ?????????
+  function frozenOf(address _target) public view returns (uint256) {
+    require(_target != address(0));
+    return frozenAccount[_target];
+  }
 }
