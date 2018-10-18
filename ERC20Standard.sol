7fca632cd54943925c11c8c7810658a7f6a0b536
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 08:48:03 +0700

    add contract ERC20Standard

--- ERC20Standard.sol
+++ ERC20Standard.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Standard at 0x26fe03c9af82f0ae1ce2497c16ed4f6f3d7c32bd
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ERC20Standard at 0xE3d424dFFecC3759Fcc997F5EBa2A01a58585405
 */
 pragma solidity ^0.4.11;
 
@@ -30,34 +30,43 @@ pragma solidity ^0.4.11;
 //------------------------------------------------------------------------------------------------
 
 contract ERC20Standard {
-	uint public totalSupply;
-	
+	uint256 public totalSupply;
+	bool public mintable;
 	string public name;
-	uint8 public decimals;
+	uint256 public decimals;
 	string public symbol;
-	string public version;
-	
-	mapping (address => uint256) balances;
-	mapping (address => mapping (address => uint)) allowed;
+	address public owner;
 
+	mapping (address => uint256) balances;
+	mapping (address => mapping (address => uint256)) allowed;
+
+  function ERC20Standard() public {
+		decimals = 18;
+		symbol = "IPAY";
+		name = "infinite pay";
+		mintable = true;
+		owner = msg.sender;
+        totalSupply = 300000000 * (10 ** decimals);
+        balances[msg.sender] = totalSupply;
+  }
 	//Fix for short address attack against ERC20
 	modifier onlyPayloadSize(uint size) {
 		assert(msg.data.length == size + 4);
 		_;
 	} 
 
-	function balanceOf(address _owner) constant returns (uint balance) {
+	function balanceOf(address _owner) constant public returns (uint256) {
 		return balances[_owner];
 	}
 
-	function transfer(address _recipient, uint _value) onlyPayloadSize(2*32) {
+	function transfer(address _recipient, uint256 _value) onlyPayloadSize(2*32) public {
 		require(balances[msg.sender] >= _value && _value > 0);
 	    balances[msg.sender] -= _value;
 	    balances[_recipient] += _value;
 	    Transfer(msg.sender, _recipient, _value);        
     }
 
-	function transferFrom(address _from, address _to, uint _value) {
+	function transferFrom(address _from, address _to, uint256 _value) public {
 		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
         balances[_to] += _value;
         balances[_from] -= _value;
@@ -65,38 +74,34 @@ contract ERC20Standard {
         Transfer(_from, _to, _value);
     }
 
-	function approve(address _spender, uint _value) {
+	function approve(address _spender, uint256 _value) public {
 		allowed[msg.sender][_spender] = _value;
 		Approval(msg.sender, _spender, _value);
 	}
 
-	function allowance(address _spender, address _owner) constant returns (uint balance) {
+	function allowance(address _owner, address _spender) constant public returns (uint256) {
 		return allowed[_owner][_spender];
+	}
+
+	function mint(uint256 amount) public {
+		assert(amount >= 0);
+		require(msg.sender == owner);
+		balances[msg.sender] += amount;
+		totalSupply += amount;
 	}
 
 	//Event which is triggered to log all transfers to this contract's event log
 	event Transfer(
 		address indexed _from,
 		address indexed _to,
-		uint _value
+		uint256 _value
 		);
 		
 	//Event which is triggered whenever an owner approves a new allowance for a spender.
 	event Approval(
 		address indexed _owner,
 		address indexed _spender,
-		uint _value
+		uint256 _value
 		);
 
-}
-
-contract NewToken is ERC20Standard {
-	function NewToken() {
-		totalSupply = 100000000;
-		name = "BUZcoin";
-		decimals = 2;
-		symbol = "BUZ";
-		version = "1.0";
-		balances[msg.sender] = totalSupply;
-	}
 }
