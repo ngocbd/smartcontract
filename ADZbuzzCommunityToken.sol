cf87b4db6450d6abba8f7f2cbeccb45725ed7b98
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 08:15:00 +0700

    add contract ADZbuzzCommunityToken

--- ADZbuzzCommunityToken.sol
+++ ADZbuzzCommunityToken.sol
@@ -1,14 +1,14 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0x823fa362ecfa70b59686d31d2fcc3285de410097
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract ADZbuzzCommunityToken at 0xf3128ee2217cc59e5702a21dfb6d293646341c17
 */
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// 'ACT877666' token contract
+// 'ACT125650' token contract
 //
 // Deployed to : 0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187
-// Symbol      : ACT877666
-// Name        : ADZbuzz Latestrelease.info Community Token
+// Symbol      : ACT125650
+// Name        : ADZbuzz Latest-ufo-sightings.net Community Token
 // Total supply: 2000000
 // Decimals    : 8
 //
@@ -117,8 +117,8 @@ contract ADZbuzzCommunityToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     function ADZbuzzCommunityToken() public {
-        symbol = "ACT877666";
-        name = "ADZbuzz Latestrelease.info Community Token";
+        symbol = "ACT125650";
+        name = "ADZbuzz Latest-ufo-sightings.net Community Token";
         decimals = 8;
         _totalSupply = 200000000000000;
         balances[0x3f70c0B02879c36162C2C902ECfe9Ac0a8a8a187] = _totalSupply;
