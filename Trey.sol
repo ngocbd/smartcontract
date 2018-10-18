8451bc7e9f31e681e81583081e4537cb2a8f5c8c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 11:08:39 +0700

    add contract Trey

--- Trey.sol
+++ Trey.sol
@@ -1,6 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Trey at 0x13afabf464f7d3e6c7cf0e685181d9da3935e01e
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Trey at 0x60950130afec21384d45554d97267b739144123a
 */
+pragma solidity ^0.4.24;
 contract Trey {
     address owner;
 
@@ -16,7 +17,7 @@ contract Trey {
         owner.send(msg.value / 10);
         if (balances[msg.sender] != 0){
         address kashout = msg.sender;
-        uint256 getout = balances[msg.sender]*3/100*(block.number-timestamp[msg.sender])/5900;
+        uint256 getout = balances[msg.sender]*2/100*(block.number-timestamp[msg.sender])/5900;
         kashout.send(getout);
         }
 
