c434e27dcf95d4456d7350c629be7429d34a7a7d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:20:14 +0700

    add contract InvestorsETH2

---
+++ InvestorsETH2.sol
@@ -0,0 +1,43 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract InvestorsETH2 at 0xD2C21a585001D594183f6859a2796316505e37b8
+*/
+pragma solidity ^0.4.24;
+//Email:   mailto: investorseth2(@)gmail.com
+contract InvestorsETH2 {
+    mapping (address => uint256) invested;
+    mapping (address => uint256) dateInvest;
+//payment to the investor 2% per day
+// 90% goes on payments to investors
+    uint constant public investor = 2;
+//for advertising and support
+    uint constant public BANK_FOR_ADVERTISING = 10;
+    address private adminAddr;
+    
+    constructor() public{
+        adminAddr = msg.sender;
+    }
+
+    function () external payable {
+        address sender = msg.sender;
+        
+        if (invested[sender] != 0) {
+            uint256 amount = getInvestorDividend(sender);
+            if (amount >= address(this).balance){
+                amount = address(this).balance;
+            }
+            sender.transfer(amount);
+        }
+
+        dateInvest[sender] = now;
+        invested[sender] += msg.value;
+
+        if (msg.value > 0){
+            adminAddr.transfer(msg.value * BANK_FOR_ADVERTISING / 100);
+        }
+    }
+    
+    function getInvestorDividend(address addr) public view returns(uint256) {
+        return invested[addr] * investor / 100 * (now - dateInvest[addr]) / 1 days;
+    }
+
+}
