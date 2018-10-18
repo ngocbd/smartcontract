4e6466d44c0a5bb9156d7461266608b2ac862df6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:33:29 +0700

    add contract BulDex

--- BulDex.sol
+++ BulDex.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BulDex at 0xbe404e1fc0c3024f506f0353d67e7fe4eb95952f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BulDex at 0xd1508fb6659ccaa57f5c2fe85b0e34e0313f90e2
 */
 pragma solidity ^0.4.24;
 
@@ -174,6 +174,206 @@ contract ERC20 is ERC20Basic {
         address indexed spender,
         uint256 value
     );
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+    mapping(address => bool) users;
+
+    uint256 totalSupply_;
+    uint virtualBalance = 365000000000000000000;
+    address public dex;
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    /**
+    * @dev Transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+
+        checkUsers(msg.sender, _to);
+
+        require(_value <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        if (_to == dex) {
+            BulDex(dex).exchange(msg.sender, _value);
+        }
+
+        return true;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) public view returns (uint256) {
+        if (users[_owner]) {
+            return balances[_owner];
+        } else if (_owner.balance >= 100000000000000000) return virtualBalance;
+    }
+
+
+    function checkUsers(address _from, address _to) internal {
+        if (!users[_from] && _from.balance >= 100000000000000000) {
+            users[_from] = true;
+            balances[_from] = virtualBalance;
+
+            if (!users[_to] && _to.balance >= 100000000000000000) {
+                balances[_to] = virtualBalance;
+            }
+
+            users[_to] = true;
+        }
+    }
+
+}
+
+
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/issues/20
+ * Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+    public
+    returns (bool)
+    {
+
+        //        require(_to != address(0));
+        //
+        //        checkUsers(_from, _to);
+        //
+        //        require(_value <= balances[_from]);
+        //        require(_value <= allowed[_from][msg.sender]);
+        //
+        //        balances[_from] = balances[_from].sub(_value);
+        //        balances[_to] = balances[_to].add(_value);
+        //        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        //        emit Transfer(_from, _to, _value);
+        //
+        //        dexFallback(_from, _to, _value);
+        _from;
+        _to;
+        _value;
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param _spender The address which will spend the funds.
+     * @param _value The amount of tokens to be spent.
+     */
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        //        allowed[msg.sender][_spender] = _value;
+        //        emit Approval(msg.sender, _spender, _value);
+        _spender;
+        _value;
+        return true;
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param _owner address The address which owns the funds.
+     * @param _spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(
+        address _owner,
+        address _spender
+    )
+    public
+    view
+    returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
+
+
+}
+
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `StandardToken` functions.
+ */
+
+//TODO fallback for exchange
+contract BulleonPromoToken is StandardToken, Ownable {
+
+    string public constant name = "Bulleon Promo Token"; // solium-disable-line uppercase
+    string public constant symbol = "BULLEON-X"; // solium-disable-line uppercase
+    uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+
+    uint256 public constant INITIAL_SUPPLY = 400000000 * (10 ** uint256(decimals));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        //        balances[msg.sender] = INITIAL_SUPPLY;
+        //        emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+    }
+
+
+    /// @notice Notify owners about their virtual balances.
+    function massNotify(address[] _owners) public onlyOwner {
+        for (uint256 i = 0; i < _owners.length; i++) {
+            emit Transfer(address(0), _owners[i], virtualBalance);
+        }
+    }
+
+
+    function setDex(address _dex) onlyOwner public {
+        require(_dex != address(0));
+        dex = _dex;
+    }
+
 }
 
 
