ee9f77de9a780424d13e2ba4e729a705c4447477
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:49:02 +0700

    add contract PriceFeed

--- PriceFeed.sol
+++ PriceFeed.sol
@@ -1,12 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PriceFeed at 0x695bd54a75fa8e28183f9af30063ad444ca0ebfc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PriceFeed at 0xd649c9b68bb78e8fd25c0b7a9c22c42f57768c91
 */
 pragma solidity ^0.4.25;
 
 // ----------------------------------------------------------------------------
 // BokkyPooBah's Pricefeed from a single source
 //
-// Deployed to: 0x695Bd54a75FA8e28183F9aF30063AD444ca0EBFc
+// Deployed to: 0xD649c9b68BB78e8fd25c0B7a9c22c42f57768c91
 //
 // Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2018. The MIT Licence.
 // ----------------------------------------------------------------------------
@@ -83,6 +83,7 @@ contract Operated is Owned {
 // PriceFeed Interface - _live is true if the rate is valid, false if invalid
 // ----------------------------------------------------------------------------
 contract PriceFeedInterface {
+    function name() public view returns (string);
     function getRate() public view returns (uint _rate, bool _live);
 }
 
@@ -91,25 +92,28 @@ contract PriceFeedInterface {
 // Pricefeed from a single source
 // ----------------------------------------------------------------------------
 contract PriceFeed is PriceFeedInterface, Operated {
-    string public name;
-    uint public rate;
-    bool public live;
+    string private _name;
+    uint private _rate;
+    bool private _live;
 
     event SetRate(uint oldRate, bool oldLive, uint newRate, bool newLive);
 
-    constructor(string _name, uint _rate, bool _live) public {
+    constructor(string name, uint rate, bool live) public {
         initOperated(msg.sender);
-        name = _name;
-        rate = _rate;
-        live = _live;
-        emit SetRate(0, false, rate, live);
+        _name = name;
+        _rate = rate;
+        _live = live;
+        emit SetRate(0, false, _rate, _live);
     }
-    function setRate(uint _rate, bool _live) public onlyOperator {
-        rate = _rate;
-        live = _live;
-        emit SetRate(rate, live, _rate, _live);
+    function name() public view returns (string) {
+        return _name;
     }
-    function getRate() public view returns (uint _rate, bool _live) {
-        return (rate, live);
+    function setRate(uint rate, bool live) public onlyOperator {
+        emit SetRate(_rate, _live, rate, live);
+        _rate = rate;
+        _live = live;
+    }
+    function getRate() public view returns (uint rate, bool live) {
+        return (_rate, _live);
     }
 }
