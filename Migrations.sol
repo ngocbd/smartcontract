b25cfabc70f31b97f80fa6396a8b3035aeb12630
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:01:40 +0700

    add contract Migrations

--- Migrations.sol
+++ Migrations.sol
@@ -1,18 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0xc85ad52c1acb7c8e6478039f3238164e5f367640
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Migrations at 0xaec7c74796cc50da6642dd6fc3a4f8dece94e652
 */
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.23;
+
+// File: contracts/Migrations.sol
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
   constructor() public {
     owner = msg.sender;
+  }
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
   }
 
   function setCompleted(uint completed) public restricted {
