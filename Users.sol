4709e63ed8c6faebf652bf5e7f5bb87b5412d321
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 16:44:59 +0700

    add contract Users

---
+++ Users.sol
@@ -0,0 +1,88 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Users at 0x9a23e9215b31b380237f1d5844d0430fc42a9284
+*/
+pragma solidity 0.4.24;
+
+// File: contracts/Users.sol
+
+contract Users {
+    struct Entry {
+        uint keyIndex;
+        string value;
+    }
+
+    struct AddressStringMap {
+        mapping(address => Entry) data;
+        address[] keys;
+    }
+
+    AddressStringMap internal usernames;
+
+    function putUsername(string _username)
+        public
+        returns (bool)
+    {
+        address senderAddress = msg.sender;
+        Entry storage entry = usernames.data[senderAddress];
+        entry.value = _username;
+        if (entry.keyIndex > 0) {
+            return true;
+        } else {
+            entry.keyIndex = ++usernames.keys.length;
+            usernames.keys[entry.keyIndex - 1] = senderAddress;
+            return false;
+        }
+    }
+
+    function removeUsername()
+        public
+        returns (bool)
+    {
+        address senderAddress = msg.sender;
+        Entry storage entry = usernames.data[senderAddress];
+        if (entry.keyIndex == 0) {
+            return false;
+        }
+
+        if (entry.keyIndex <= usernames.keys.length) {
+            // Move an existing element into the vacated key slot.
+            usernames.data[usernames.keys[usernames.keys.length - 1]].keyIndex = entry.keyIndex;
+            usernames.keys[entry.keyIndex - 1] = usernames.keys[usernames.keys.length - 1];
+            usernames.keys.length -= 1;
+            delete usernames.data[senderAddress];
+            return true;
+        }
+    }
+
+    function size()
+        public
+        view
+        returns (uint)
+    {
+        return usernames.keys.length;
+    }
+
+    function getUsernameByAddress(address _address)
+        public
+        constant
+        returns (string)
+    {
+        return usernames.data[_address].value;
+    }
+
+    function getAddressByIndex(uint idx)
+        public
+        constant
+        returns (address)
+    {
+        return usernames.keys[idx];
+    }
+
+    function getUsernameByIndex(uint idx)
+        public
+        constant
+        returns (string)
+    {
+        return usernames.data[usernames.keys[idx]].value;
+    }
+}
