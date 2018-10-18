63b7fc64f69a836ceb1ae19b5deea17734c08667
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 10:38:49 +0700

    add contract Ownable

--- Ownable.sol
+++ Ownable.sol
@@ -1,45 +1,110 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Ownable at 0xb15fdc05fd34480e865d1999a6264ef7ecd67249
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Ownable at 0xB41a0dBCBD7726E21994F956F3103B2cF7bBB3D1
 */
-pragma solidity ^0.4.17;
-
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
+pragma solidity ^0.4.24;
 contract Ownable {
   address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
+  function Ownable() {
     owner = msg.sender;
   }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+  function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));      
     owner = newOwner;
   }
+}
+
+contract Token is Ownable{
+    uint256 public totalSupply;
+
+    function balanceOf(address _owner) constant returns (uint256 balance);
+
+    function transfer(address _to, uint256 _value) returns (bool success);
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+    function approve(address _spender, uint256 _value) returns (bool success);
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract StandardToken is Token {
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value; 
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value); 
+        return true;
+    }
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+}
+
+contract WanChainToken is StandardToken { 
+
+    string public name;                   
+    uint8 public decimals;               
+    string public symbol; 
+    uint256 public createTime;
+    uint public releaseAmount = 350000000;
+    mapping(uint => bool) public releaseMapping; 
+     
+
+    function WanChainToken() {
+        balances[msg.sender] = 3000000000; 
+        totalSupply = 10000000000;         
+        name = "Wan Chain Token";                   
+        decimals = 1;           
+        symbol = "WL";  
+        createTime = now;
+    }
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        require(_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        return true;
+    }
+    function release(uint month) onlyOwner{
+        require(month >=1 && month <= 20 );
+        require(now >= (month * 30 days) + createTime);
+        require(!releaseMapping[month]);
+        balances[owner] = balances[owner] + releaseAmount;
+        releaseMapping[month] = true;
+    }
 
 }
