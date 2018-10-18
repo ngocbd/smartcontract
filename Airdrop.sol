91ab8536cdb7d9f393783e78e3731b1626034e8e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 12:26:04 +0700

    add contract AirDrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,16 +1,56 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x7038c15f76ad2667837c438c8819fd969899cdbe
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract AirDrop at 0x4d932c0bf042f0fd93c5430e7a789b309c9ade5e
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.17;
 
-contract ERC20 {
-  function transfer(address _recipient, uint256 _value) public returns (bool success);
+contract Ownable {
+    
+    address public owner;
+
+    function Ownable() public {
+        owner = 0x83De551B3d7B77f2B6304D1836C17B42bDeCFC65;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    event OwnershipTransferred(address indexed from, address indexed to);
+    
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
 }
 
-contract Airdrop {
-  function drop(ERC20 token, address[] recipients, uint256[] values) public {
-    for (uint256 i = 0; i < recipients.length; i++) {
-      token.transfer(recipients[i], values[i]);
+
+contract TokenTransferInterface {
+    function transfer(address _to, uint256 _value) public;
+}
+
+
+contract AirDrop is Ownable {
+
+    TokenTransferInterface public constant token = TokenTransferInterface(0x96c2848f32E91C5d53796e37aFFB7f1331aA0635);
+
+    function multiValueAirDrop(address[] _addrs, uint256[] _values) public onlyOwner {
+	    require(_addrs.length == _values.length && _addrs.length <= 100);
+        for (uint i = 0; i < _addrs.length; i++) {
+            if (_addrs[i] != 0x0 && _values[i] > 0) {
+                token.transfer(_addrs[i], _values[i] * (10 ** 18));  
+            }
+        }
     }
-  }
+
+    function singleValueAirDrop(address[] _addrs, uint256 _value) public onlyOwner {
+	    require(_addrs.length <= 100 && _value > 0);
+        for (uint i = 0; i < _addrs.length; i++) {
+            if (_addrs[i] != 0x0) {
+                token.transfer(_addrs[i], _value * (10 ** 18));
+            }
+        }
+    }
 }
