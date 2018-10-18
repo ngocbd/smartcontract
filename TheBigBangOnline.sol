34508762cd3a4e9e17e775351bbc3a30ec47f4b8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:31:18 +0700

    add contract TheBigBangOnline

--- TheBigBangOnline.sol
+++ TheBigBangOnline.sol
@@ -1,18 +1,18 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TheBigBangOnline at 0xd84d7b4e43256aa8898fda41cccb36eea081ef3a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TheBigBangOnline at 0xe501aceae5a49228c170cd26fc6b678599c7f217
 */
 /*
 This software code is prohibited for copying and distribution. 
 The violation of this requirement will be punished by law. 
 
-Contact e-mail: maridendier@openmailbox.org
+Contact e-mail: thebigbangonline@protonmail.com
 
 Project site: http://thebigbang.online/
 
-Developed by "Naumov Lab" http://smart-contracts.ru/
+Calling the methods of this smart contract you accept the rules of the "The Big Bang" game, described by this program code.
 */
 
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 
 library SafeMath {
@@ -45,24 +45,62 @@ library SafeMath {
 contract Ownable {
 
   address public owner;
+  address public manager;
 
   constructor() public {
     owner = msg.sender;
+    manager = msg.sender;
   }
+  
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }  
+  
+  modifier onlyOwnerOrManager() {
+     require((msg.sender == owner)||(msg.sender == manager));
+      _;
+  }  
+  
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    owner = newOwner;
+  }
+  
+  function setManager(address _manager) public onlyOwner {
+      require(_manager != address(0));
+      manager = _manager;
+  }  
 
 }
+
+
 
 
 contract TheBigBangOnline is Ownable {
         
     using SafeMath for uint256;
     
+    bool contractProtection = true;
+    
+    modifier notFromContract() {
+      uint32 size;
+      address investor = msg.sender;
+      assembly {
+        size := extcodesize(investor)
+      }
+      if ((size > 0) && (contractProtection == true)){
+          revert("call from contract");
+      }        
+        _;
+    }     
+    
     event payEventLog(address indexed _address, uint value, uint periodCount, uint percent, uint time);
     event payRefEventLog(address indexed _addressFrom, address indexed _addressTo, uint value, uint percent, uint time);
     event payJackpotLog(address indexed _address, uint value, uint totalValue, uint userValue, uint time);    
     
     uint public period = 24 hours;
-    uint public startTime = 1536537600; //  Mon, 10 Sep 2018 00:00:00 GMT
+    uint public startTime = 1537142400; //  Mon, 17 Sep 2018 00:00:00 UTC
     
     uint public basicDayPercent = 300; //3%
     uint public bonusDayPercent = 330; //3.3%
@@ -74,7 +112,9 @@ contract TheBigBangOnline is Ownable {
     uint public referrerLevel2Ether = 1 ether;
     uint public referrerLevel3Ether = 10 ether;
     
-    uint public minBet = 0.01  ether;
+    uint public minBetLevel1_2 = 0.01  ether;
+    uint public minBetLevel3 = 0.02  ether;
+    uint public minBetLevel4 = 0.05  ether;  //If more than 100 ETH in Jackpot Bank  
     
     uint public referrerAndOwnerPercent = 2000; //20%    
     
@@ -97,11 +137,16 @@ contract TheBigBangOnline is Ownable {
     mapping (uint => address) public addressList;
     
     // Jackpot
+    uint public jackpotLevel2Amount = 1 ether;
+    uint public jackpotLevel3Amount = 10 ether;
+    uint public jackpotLevel4Amount = 100 ether;    
     uint public jackpotPercent = 1000; //10%
     uint public jackpotBank = 0;
     uint public jackpotMaxTime = 24 hours;
     uint public jackpotTime = startTime + jackpotMaxTime;  
-    uint public increaseJackpotTimeAfterBet = 10 minutes;
+    uint public increaseJackpotTimeAfterBetLevel1 = 5 minutes; 
+    uint public increaseJackpotTimeAfterBetLevel2_3 = 1 minutes;  
+    uint public increaseJackpotTimeAfterBetLevel4 = 30 seconds;  //If more than 100 ETH in Jackpot Bank 
     
     uint public gameRound = 1;   
     uint public currJackpotBetID = 0;
@@ -113,38 +158,54 @@ contract TheBigBangOnline is Ownable {
     mapping (uint => BetStructForJackpot) public betForJackpot;    
     
     
-    
+     
     
     constructor() public {
     
     }
 
+
+ function setContractProtection(bool _contractProtection) public onlyOwner {
+      contractProtection = _contractProtection;
+ }
+ 
+ function bytesToAddress(bytes bys) private pure returns (address addr) {
+    assembly {
+        addr := mload(add(bys, 20))
+    }
+ } 
+
+ function allBalance() public constant returns (uint) {
+     return address(this).balance;
+ }    
+  
+ function addToJackpot() public payable onlyOwnerOrManager {
+     jackpotBank += msg.value;
+ }
+
     
- function createBet(uint _referrerID) public payable {
+ function createBet(uint _referrerID) public payable notFromContract {
      
-        if( (_referrerID >= currBetID)&&(_referrerID!=0)){
+        if( (_referrerID >= currBetID)){  
             revert("Incorrect _referrerID");
         }
 
-        if( msg.value < minBet){
-            revert("Amount beyond acceptable limits");
-        }
-        
-            BetStruct memory betStruct;
+        if(  (msg.value < minBetLevel1_2)||(msg.value < minBetLevel3 && jackpotBank >= jackpotLevel3Amount)||(msg.value < minBetLevel4 && jackpotBank >= jackpotLevel4Amount)  ){
             
-            if(betsDatabase[msg.sender].isExist){
-                betStruct = betsDatabase[msg.sender];
+                revert("Amount beyond acceptable limits");
+        }
+            
+            if(betsDatabase[msg.sender].isExist){ 
                 
-                if( (betStruct.nextPayAfterTime < now) && (gameRound==1) ){
-                    getRewardForAddress(msg.sender);    
+                if( (betsDatabase[msg.sender].nextPayAfterTime < now) && (gameRound==1) ){
+                    payRewardForAddress(msg.sender);    
                 }            
+                betsDatabase[msg.sender].value += msg.value;
+                betsDatabase[msg.sender].lastBetTime = now;
                 
-                betStruct.value += msg.value;
-                betStruct.lastBetTime = now;
-                
-                betsDatabase[msg.sender] = betStruct;
                 
             } else {
+                BetStruct memory betStruct;
                 
                 uint nextPayAfterTime = startTime+((now.sub(startTime)).div(period)).mul(period)+period;
     
@@ -178,10 +239,18 @@ contract TheBigBangOnline is Ownable {
             
             betForJackpot[currJackpotBetID] = betStructForJackpot;
             
-            jackpotTime += increaseJackpotTimeAfterBet;
+            if(jackpotBank >= jackpotLevel4Amount){
+                jackpotTime += increaseJackpotTimeAfterBetLevel4;
+            }else if(jackpotBank >= jackpotLevel2Amount){
+                jackpotTime += increaseJackpotTimeAfterBetLevel2_3;
+            }else {
+                jackpotTime += increaseJackpotTimeAfterBetLevel1;
+            }
+            
+            
             if( jackpotTime > now + jackpotMaxTime ) {
                 jackpotTime = now + jackpotMaxTime;
-            }
+            } 
             
             if(gameRound==1){
                 jackpotBank += msg.value.mul(jackpotPercent).div(10000);
@@ -190,11 +259,11 @@ contract TheBigBangOnline is Ownable {
                 jackpotBank += msg.value.mul(10000-referrerAndOwnerPercent).div(10000);
             }
     
-            if(betStruct.referrerID!=0){
-                betsDatabase[addressList[betStruct.referrerID]].refValue += msg.value;
+            if(betsDatabase[msg.sender].referrerID!=0){
+                betsDatabase[addressList[betsDatabase[msg.sender].referrerID]].refValue += msg.value;
                 
                 uint currReferrerPercent;
-                uint currReferrerValue = betsDatabase[addressList[betStruct.referrerID]].value.add(betsDatabase[addressList[betStruct.referrerID]].refValue);
+                uint currReferrerValue = betsDatabase[addressList[betsDatabase[msg.sender].referrerID]].value.add(betsDatabase[addressList[betsDatabase[msg.sender].referrerID]].refValue);
                 
                 if (currReferrerValue >= referrerLevel3Ether){
                     currReferrerPercent = referrerLevel3Percent;
@@ -206,25 +275,43 @@ contract TheBigBangOnline is Ownable {
                 
                 uint refToPay = msg.value.mul(currReferrerPercent).div(10000);
                 
-                addressList[betStruct.referrerID].transfer( refToPay );
+                addressList[betsDatabase[msg.sender].referrerID].transfer( refToPay );
                 owner.transfer(msg.value.mul(referrerAndOwnerPercent - currReferrerPercent).div(10000));
                 
-                emit payRefEventLog(msg.sender, addressList[betStruct.referrerID], refToPay, currReferrerPercent, now);
+                emit payRefEventLog(msg.sender, addressList[betsDatabase[msg.sender].referrerID], refToPay, currReferrerPercent, now);
             } else {
                 owner.transfer(msg.value.mul(referrerAndOwnerPercent).div(10000));
             }
   }
     
-  function () public payable {
-        createBet(0);
+  function () public payable notFromContract {
+      
+      if(msg.value == 0){
+            payRewardForAddress(msg.sender);         
+      }else{
+      
+            uint refId = 1;
+            address referrer = bytesToAddress(msg.data);
+            
+            if (betsDatabase[referrer].isExist){
+                refId = betsDatabase[referrer].id;
+            }
+    
+          
+            createBet(refId);
+      }
   } 
   
   
-  function getReward() public {
-        getRewardForAddress(msg.sender);
+  function getReward() public notFromContract {
+        payRewardForAddress(msg.sender);
   }
   
-  function getRewardForAddress(address _address) public {
+  function getRewardForAddress(address _address) public onlyOwnerOrManager {
+        payRewardForAddress(_address);
+  }  
+  
+  function payRewardForAddress(address _address) internal  {
         if(gameRound!=1){
              revert("The first round end");    
         }        
@@ -249,8 +336,8 @@ contract TheBigBangOnline is Ownable {
         betsDatabase[_address].lastPaymentTime = now;
         betsDatabase[_address].nextPayAfterTime += periodCount.mul(period); 
         
-        if(toPay.add(jackpotBank) >= address(this).balance ){
-            toPay = address(this).balance.sub(jackpotBank);
+        if(toPay.add(jackpotBank) >= address(this).balance.sub(msg.value) ){
+            toPay = address(this).balance.sub(jackpotBank).sub(msg.value);
             gameRound = 2;
         }
         
@@ -259,7 +346,7 @@ contract TheBigBangOnline is Ownable {
         emit payEventLog(_address, toPay, periodCount, percent, now);
   }
   
-  function getJackpot() public {
+  function getJackpot() public notFromContract {
         if(now <= jackpotTime){
             revert("Jackpot did not come");  
         }
@@ -269,6 +356,10 @@ contract TheBigBangOnline is Ownable {
         if(currJackpotBetID > 5){
             uint toPay = jackpotBank;
             jackpotBank = 0;            
+            
+            if(toPay>address(this).balance){
+               toPay = address(this).balance; 
+            }
             
             uint totalValue = betForJackpot[currJackpotBetID].value + betForJackpot[currJackpotBetID - 1].value + betForJackpot[currJackpotBetID - 2].value + betForJackpot[currJackpotBetID - 3].value + betForJackpot[currJackpotBetID - 4].value;
             
@@ -287,7 +378,5 @@ contract TheBigBangOnline is Ownable {
             betForJackpot[currJackpotBetID-4].user.transfer(toPay.mul(betForJackpot[currJackpotBetID-4].value).div(totalValue) );
             emit payJackpotLog(betForJackpot[currJackpotBetID-4].user, toPay.mul(betForJackpot[currJackpotBetID-4].value).div(totalValue), totalValue, betForJackpot[currJackpotBetID-4].value, now);
         }
-        
-  }
-    
+  }  
 }
