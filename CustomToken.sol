50276c221ba9de42bfa0a857cb4a3b448140dc7b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:17:09 +0700

    add contract CustomToken

--- CustomToken.sol
+++ CustomToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0x00c8decc1ca373a99f1c2a341be50f03d35e03e7
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CustomToken at 0x31ead6a10b5215321afa8bef2f05aa271508fa6c
 */
 pragma solidity ^0.4.19;
 
@@ -67,29 +67,6 @@ contract BurnToken is BaseToken {
     }
 }
 
-contract AirdropToken is BaseToken {
-    uint256 public airAmount;
-    uint256 public airBegintime;
-    uint256 public airEndtime;
-    address public airSender;
-    uint32 public airLimitCount;
-
-    mapping (address => uint32) public airCountOf;
-
-    event Airdrop(address indexed from, uint32 indexed count, uint256 tokenValue);
-
-    function airdrop() public payable {
-        require(now >= airBegintime && now <= airEndtime);
-        require(msg.value == 0);
-        if (airLimitCount > 0 && airCountOf[msg.sender] >= airLimitCount) {
-            revert();
-        }
-        _transfer(airSender, msg.sender, airAmount);
-        airCountOf[msg.sender] += 1;
-        Airdrop(msg.sender, airCountOf[msg.sender], airAmount);
-    }
-}
-
 contract LockToken is BaseToken {
     struct LockMeta {
         uint256 amount;
@@ -106,34 +83,15 @@ contract LockToken is BaseToken {
     }
 }
 
-contract CustomToken is BaseToken, BurnToken, AirdropToken, LockToken {
+contract CustomToken is BaseToken, BurnToken, LockToken {
     function CustomToken() public {
-        totalSupply = 100000000000000000;
-        name = 'BitValue';
-        symbol = 'BV';
-        decimals = 8;
-        balanceOf[0xf35e3344cbb5ab8da4294b741f7e378212dc0e89] = totalSupply;
-        Transfer(address(0), 0xf35e3344cbb5ab8da4294b741f7e378212dc0e89, totalSupply);
+        totalSupply = 520000000000000000000000000;
+        name = 'DragonCoin';
+        symbol = 'DC';
+        decimals = 18;
+        balanceOf[0x837b5cc69e11e89b5fdb266a3ff0a71ba37aa3a6] = totalSupply;
+        Transfer(address(0), 0x837b5cc69e11e89b5fdb266a3ff0a71ba37aa3a6, totalSupply);
 
-        airAmount = 1000000000;
-        airBegintime = 1546149600;
-        airEndtime = 2177388000;
-        airSender = 0x8efc62dbf088c556503470ddbea0b797d66cf45d;
-        airLimitCount = 1;
-
-        lockedAddresses[0x51d013c61026c2819ee7880164c5226654b2092d] = LockMeta({amount: 9000000000000000, endtime: 1551402000});
-        lockedAddresses[0x8efc62dbf088c556503470ddbea0b797d66cf45d] = LockMeta({amount: 9000000000000000, endtime: 1559350800});
-        lockedAddresses[0x061aa72cbe0e4c02bc53cd7b4edd789f9465344e] = LockMeta({amount: 9000000000000000, endtime: 1575162000});
-        lockedAddresses[0x0eb9f24e3b5a0684ee04a23cc90adfce067c4cf5] = LockMeta({amount: 9000000000000000, endtime: 1590973200});
-        lockedAddresses[0x92c5c5d223607028e519e694a16999b004e17d49] = LockMeta({amount: 9000000000000000, endtime: 1606784400});
-        lockedAddresses[0xcb9ee43e4e2096be331c5be13d0a9a38cac955dc] = LockMeta({amount: 9000000000000000, endtime: 1622509200});
-        lockedAddresses[0xba14daefca3575d5b2f0238bf04d4ba2e0bef7ac] = LockMeta({amount: 9000000000000000, endtime: 1638320400});
-        lockedAddresses[0x79e8086c0345448b6613e5700b9cae8e05d748a8] = LockMeta({amount: 9000000000000000, endtime: 1654045200});
-        lockedAddresses[0x7e1061345337f8cb320d2e08ca6de757d2382c17] = LockMeta({amount: 9000000000000000, endtime: 1669856400});
-        lockedAddresses[0xc83ae4bbd5186fcedee714cc841889a835cb97c5] = LockMeta({amount: 9000000000000000, endtime: 1685581200});
-    }
-
-    function() public payable {
-        airdrop();
+        lockedAddresses[0x3a6aeca5666d4af4161e48609f83e80f124170d9] = LockMeta({amount: 400000000000000000000000000, endtime: 1549000800});
     }
 }
