4db0a35b1d3a98049f3a6608ba14c25066fb1c7f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:16:27 +0700

    add contract RevertReason

--- RevertReason.sol
+++ RevertReason.sol
@@ -1,20 +1,41 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RevertReason at 0x376c128eb3d734912de09f6ad9a8609e3f87e13f
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract RevertReason at 0x8799c361b00fee1a5e6c67877b172b9dd9292c3e
 */
 pragma solidity 0.4.25;
 
 contract RevertReason {
+    
+    ErrorReporter public error;
+    
+    constructor(address _error) public {
+        require(_error != address(0x0));
+        error = ErrorReporter(_error);
+    }
+    
     function shouldRevert(bool yes) public {
-        require(!yes, "Shit it reverted!");
+        if (yes) {
+            error.report("Shit it reverted!");
+        }
     }
     
     function shouldRevertWithReturn(bool yes) public returns (uint256) {
-        require(!yes, "Shit it reverted!");
+        if (yes) {
+            error.report("Shit it reverted!");
+        }
         return 42;
     }
     
-    function shouldRevertPure(bool yes) public pure returns (uint256) {
-        require(!yes, "Shit it reverted!");
+    function shouldRevertPure(bool yes) public view returns (uint256) {
+        if (yes) {
+            error.report("Shit it reverted!");
+        }
         return 42;
+    }
+}
+
+
+contract ErrorReporter {
+    function report(string reason) public pure {
+        revert(reason);
     }
 }
