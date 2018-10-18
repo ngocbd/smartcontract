2431bd62162fd6e853906896388e0c393b80fad6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:11:56 +0700

    add contract EmptyLib

--- EmptyLib.sol
+++ EmptyLib.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmptyLib at 0x369f03853a30e18e22c810664699b30bf74eb70b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract EmptyLib at 0xea6327dc6e7ce98358f6a3a2a1e80c8b1bf43f2d
 */
 pragma solidity ^0.4.25;
 
