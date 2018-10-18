7f51558cd47df1a2114dbd3b227bc619bd9298b3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:22:30 +0700

    add contract TokenERC20

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,32 +1,32 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x554d039f20c161298c018b9375637c05d89eb364
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenERC20 at 0x1f24727a39ec4e7d523f53757b3d565814207a5a
 */
 pragma solidity ^0.4.16;
- 
+
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
- 
+
 contract TokenERC20 {
     string public name;
     string public symbol;
-    uint8 public decimals = 18;  // 18 ???????
-    uint256 public totalSupply;
- 
-    mapping (address => uint256) public balanceOf;  // 
-    mapping (address => mapping (address => uint256)) public allowance;
- 
+    uint8 public decimals = 6; 
+    uint256 public totalSupply;  //??
+
+    mapping (address => uint256) public balanceOf;  //??????? 
+    mapping (address => mapping (address => uint256)) public allowance;//??
+
     event Transfer(address indexed from, address indexed to, uint256 value);
- 
+
     event Burn(address indexed from, uint256 value);
- 
- 
+
+
     function TokenERC20(uint256 initialSupply, string tokenName, string tokenSymbol) public {
         totalSupply = initialSupply * 10 ** uint256(decimals);
         balanceOf[msg.sender] = totalSupply;
         name = tokenName;
         symbol = tokenSymbol;
     }
- 
- 
+
+
     function _transfer(address _from, address _to, uint _value) internal {
         require(_to != 0x0);
         require(balanceOf[_from] >= _value);
@@ -37,24 +37,24 @@ contract TokenERC20 {
         Transfer(_from, _to, _value);
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
- 
+
     function transfer(address _to, uint256 _value) public {
         _transfer(msg.sender, _to, _value);
     }
- 
+
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
     }
- 
+
     function approve(address _spender, uint256 _value) public
         returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
- 
+
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
@@ -62,7 +62,7 @@ contract TokenERC20 {
             return true;
         }
     }
- 
+
     function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
@@ -70,7 +70,7 @@ contract TokenERC20 {
         Burn(msg.sender, _value);
         return true;
     }
- 
+
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);
         require(_value <= allowance[_from][msg.sender]);
