91ffbeb65207ed6a52289fea85e7602d1b9f4304
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:40:40 +0700

    add contract TTT

--- TTT.sol
+++ TTT.sol
@@ -1,179 +1,220 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TTT at 0x317572aabc73d59fc55f923750d1c51680fd28b4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TTT at 0x4e1bb58a40F34d8843f61030fe4257C11d09A2c5
 */
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.23;
 
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+library SafeMath
+{
+    function add(uint256 a, uint256 b) internal pure returns (uint256)
+    {
+        uint256 c = a+b;
+        assert (c>=a);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256)
+    {
+        assert(a>=b);
+        return (a-b);
+    }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    function mul(uint256 a,uint256 b)internal pure returns (uint256)
+    {
+        if (a==0)
+        {
+        return 0;
+        }
+        uint256 c = a*b;
+        assert ((c/a)==b);
+        return c;
+    }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function div(uint256 a,uint256 b)internal pure returns (uint256)
+    {
+        return a/b;
+    }
 }
 
-interface ERC20 {
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+contract ERC20
+{
   function allowance(address owner, address spender) public view returns (uint256);
   function transferFrom(address from, address to, uint256 value) public returns (bool);
   function approve(address spender, uint256 value) public returns (bool);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-interface ERC223 {
-    function transfer(address to, uint value, bytes data) public;
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+contract Owned
+{
+    address public owner;
+
+    constructor() internal
+     {
+         owner = msg.sender;
+     }
+
+     modifier onlyowner()
+     {
+         require(msg.sender==owner);
+         _;
+     }
+
 }
 
-contract ERC223ReceivingContract { 
-    function tokenFallback(address _from, uint _value, bytes _data) public;
+contract pausable is Owned
+{
+    event Pause();
+    event Unpause();
+    bool public pause = false;
+
+    modifier whenNotPaused()
+    {
+        require(!pause);
+        _;
+    }
+    modifier whenPaused()
+    {
+        require(pause);
+        _;
+    }
+
+    function pause() onlyowner whenNotPaused public
+    {
+        pause = true;
+        emit Pause();
+    }
+    function unpause() onlyowner whenPaused public
+    {
+        pause = false;
+        emit Unpause();
+    }
+
 }
 
-contract TTT is ERC20, ERC223 {
-  using SafeMath for uint;
-     
-    string internal _name;
-    string internal _symbol;
-    uint8 internal _decimals;
-    uint256 internal _totalSupply;
+contract TokenControl is ERC20,pausable
+{
+    using SafeMath for uint256;
+    mapping (address =>uint256) internal balances;
+    mapping (address => mapping(address =>uint256)) internal allowed;
+    uint256 totaltoken;
 
-    mapping (address => uint256) internal balances;
-    mapping (address => mapping (address => uint256)) internal allowed;
 
-    function TTT(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
-        _symbol = symbol;
-        _name = name;
-        _decimals = decimals;
-        _totalSupply = totalSupply;
-        balances[msg.sender] = totalSupply;
+    function totalSupply() public view returns (uint256)
+    {
+        return totaltoken;
     }
 
-    function name()
-        public
-        view
-        returns (string) {
-        return _name;
-    }
+    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool)
+    {
+        require(_to!=address(0));
+        require(_value <= balances[msg.sender]);
 
-    function symbol()
-        public
-        view
-        returns (string) {
-        return _symbol;
-    }
-
-    function decimals()
-        public
-        view
-        returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply()
-        public
-        view
-        returns (uint256) {
-        return _totalSupply;
-    }
-
-   function transfer(address _to, uint256 _value) public returns (bool) {
-     require(_to != address(0));
-     require(_value <= balances[msg.sender]);
-     balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
-     balances[_to] = SafeMath.add(balances[_to], _value);
-     Transfer(msg.sender, _to, _value);
-     return true;
-   }
-
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-   }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-     require(_value <= balances[_from]);
-     require(_value <= allowed[_from][msg.sender]);
-
-    balances[_from] = SafeMath.sub(balances[_from], _value);
-     balances[_to] = SafeMath.add(balances[_to], _value);
-     allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
-    Transfer(_from, _to, _value);
-     return true;
-   }
-
-   function approve(address _spender, uint256 _value) public returns (bool) {
-     allowed[msg.sender][_spender] = _value;
-     Approval(msg.sender, _spender, _value);
-     return true;
-   }
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-     return allowed[_owner][_spender];
-   }
-
-   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-     allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-     uint oldValue = allowed[msg.sender][_spender];
-     if (_subtractedValue > oldValue) {
-       allowed[msg.sender][_spender] = 0;
-     } else {
-       allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
-    }
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-   
-  function transfer(address _to, uint _value, bytes _data) public {
-    require(_value > 0 );
-    if(isContract(_to)) {
-        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-        receiver.tokenFallback(msg.sender, _value, _data);
-    }
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value, _data);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
-    
-  function isContract(address _addr) private returns (bool is_contract) {
-      uint length;
-      assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-      }
-      return (length>0);
+    function balanceOf(address _owner) public view returns (uint256 balance)
+    {
+        return balances[_owner];
     }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused  returns (bool)
+    {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotPaused returns (bool)
+    {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool)
+    {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool)
+    {
+        uint256 oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue)
+        {
+            allowed[msg.sender][_spender] = 0;
+        }
+        else
+        {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+}
+
+contract claimable is Owned
+{
+    address public pendingOwner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyPendingOwner()
+    {
+        require(msg.sender == pendingOwner);
+        _;
+    }
+
+     function transferOwnership(address newOwner) onlyowner public
+    {
+        pendingOwner = newOwner;
+    }
+
+    function claimOwnership() onlyPendingOwner public
+    {
+        emit OwnershipTransferred(owner, pendingOwner);
+        owner = pendingOwner;
+        pendingOwner = address(0);
+    }
+
+}
+
+
+contract TTT is TokenControl,claimable
+{
+    using SafeMath for uint256;
+    string public constant name    = "Tui Tui Token";
+    string public constant symbol  = "TTT";
+    uint256 public decimals = 18;
+    uint256 totalsupply =  3000000000*(10**decimals);
+
+
+
+    //contract initial
+    constructor () public
+    {
+        balances[0xEAc1b04cBdd484244fC0dB0A8BEdA6212fFb32b1] = totalsupply;
+        totaltoken = totalsupply;
+
+    }
+
+
 
 
 }
