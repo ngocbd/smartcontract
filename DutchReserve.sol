9acbdf701349b395f064688b72af84b2178c2145
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 08:42:54 +0700

    add contract DutchReserve

--- DutchReserve.sol
+++ DutchReserve.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DutchReserve at 0xa58c6d029f0ee738c653ea6cca5417ce5532ea1a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract DutchReserve at 0x6253025e2d4c2e89915844ef9854eb7498f497db
 */
 pragma solidity ^0.4.18;
 
@@ -24,37 +24,15 @@ interface DutchExchange {
 
 interface ERC20 {
   function transfer(address recipient, uint amount) public returns(bool);
-  function approve(address spender, uint amount) public returns(bool);  
-}
-
-interface KyberNetwork {
-    function trade(
-        ERC20 src,
-        uint srcAmount,
-        ERC20 dest,
-        address destAddress,
-        uint maxDestAmount,
-        uint minConversionRate,
-        address walletId
-    )
-        public
-        payable
-        returns(uint);    
 }
 
 
 contract DutchReserve {
   DutchExchange constant DUTCH_EXCHANGE = DutchExchange(0xaf1745c0f8117384Dfa5FFf40f824057c70F2ed3);
   WETH9 constant WETH = WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-  KyberNetwork constant KYBER = KyberNetwork(0x818E6FECD516Ecc3849DAf6845e3EC868087B755);
-  ERC20 constant ETH = ERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 
   function DutchReserve() public {
     require(WETH.approve(DUTCH_EXCHANGE,2**255));
-  }
-  
-  function enableToken(ERC20 token) public {
-      require(token.approve(KYBER,2**255));
   }
 
   function buyToken(ERC20 token) payable public {
@@ -65,8 +43,7 @@ contract DutchReserve {
     uint amount; uint first;
     (amount,first) = DUTCH_EXCHANGE.claimBuyerFunds(token,WETH,this,auctionIndex);
     DUTCH_EXCHANGE.withdraw(token,amount);
-    require(KYBER.trade(token,amount,ETH,msg.sender,2**255,1,this) > 0) ;
-    //token.transfer(msg.sender,amount);
+    token.transfer(msg.sender,amount);
   }
 
 }
