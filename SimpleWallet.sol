ce8e27221e55ca858c853e75ad72e6b7cc3944ea
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:06:09 +0700

    add contract SimpleWallet

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -1,61 +1,31 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleWallet at 0xc2f21e3d752834a38a0f8a68c4cef84df1eb4541
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SimpleWallet at 0xb37f18af15bafb869a065b61fc83cfc44ed9cc27
 */
-pragma solidity ^0.4.11;
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
+pragma solidity ^0.4.24;
 
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-contract SimpleWallet is Ownable {
-
-    function () public payable {
+contract SimpleWallet {
+    address public owner = msg.sender;
+    uint public depositsCount;
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
-
-    function weiBalance() public constant returns(uint256) {
-        return this.balance;
+    
+    function() public payable {
+        depositsCount++;
     }
-
-    function claim(address destination) public onlyOwner {
-        destination.transfer(this.balance);
+    
+    function withdrawAll() public onlyOwner {
+        withdraw(address(this).balance);
     }
-
+    
+    function withdraw(uint _value) public onlyOwner {
+        msg.sender.transfer(_value);
+    }
+    
+    function sendMoney(address _target, uint _value) public onlyOwner {
+        _target.call.value(_value)();
+    }
 }
