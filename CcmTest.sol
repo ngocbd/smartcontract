40f62657d67424a3135f9350839bbc2f5a602ad9
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:50:20 +0700

    add contract CcmTest

---
+++ CcmTest.sol
@@ -0,0 +1,20 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract CcmTest at 0x1b854A50aAf03BaBc48bC010fb301E1E2DABeAbA
+*/
+pragma solidity ^0.4.25;
+/*
+ * line1
+ * line2
+ * line3
+ * line4
+ * line5
+ * line6
+ * line7
+ * line8
+ * line9
+ * linea
+ * lineb
+ * linec
+ */
+contract CcmTest {
+}
