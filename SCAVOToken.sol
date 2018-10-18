9214736fd36c27a78e516b3a268a7e6edfe5dd60
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 10:21:11 +0700

    add contract SCAVOToken

--- SCAVOToken.sol
+++ SCAVOToken.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SCAVOToken at 0x0d30598f68e75574a6285f62eb5d7658bd62bd8b
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SCAVOToken at 0xa62ce5f4175ba550440171ef809197ee21002d64
 */
 pragma solidity ^0.4.24;
 
@@ -513,33 +513,353 @@ contract PausableToken is StandardToken, Pausable {
   }
 }
 
+/**
+ * @title ERC1132 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/1132
+ */
+
+contract ERC1132 {
+    /**
+     * @dev Reasons why a user's tokens have been locked
+     */
+    mapping(address => bytes32[]) public lockReason;
+
+    /**
+     * @dev locked token structure
+     */
+    struct lockToken {
+        uint256 amount;
+        uint256 validity;
+        bool claimed;
+    }
+
+    /**
+     * @dev Holds number & validity of tokens locked for a given reason for
+     *      a specified address
+     */
+    mapping(address => mapping(bytes32 => lockToken)) public locked;
+
+    /**
+     * @dev Records data of all the tokens Locked
+     */
+    event Lock(
+        address indexed _of,
+        bytes32 indexed _reason,
+        uint256 _amount,
+        uint256 _validity
+    );
+
+    /**
+     * @dev Records data of all the tokens unlocked
+     */
+    event Unlock(
+        address indexed _of,
+        bytes32 indexed _reason,
+        uint256 _amount
+    );
+    
+    /**
+     * @dev Locks a specified amount of tokens against an address,
+     *      for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be locked
+     * @param _time Lock time in seconds
+     */
+    function lock(bytes32 _reason, uint256 _amount, uint256 _time)
+        public returns (bool);
+  
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     */
+    function tokensLocked(address _of, bytes32 _reason)
+        public view returns (uint256 amount);
+    
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason at a specific time
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     * @param _time The timestamp to query the lock tokens for
+     */
+    function tokensLockedAtTime(address _of, bytes32 _reason, uint256 _time)
+        public view returns (uint256 amount);
+    
+    /**
+     * @dev Returns total tokens held by an address (locked + transferable)
+     * @param _of The address to query the total balance of
+     */
+    function totalBalanceOf(address _of)
+        public view returns (uint256 amount);
+    
+    /**
+     * @dev Extends lock for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _time Lock extension time in seconds
+     */
+    function extendLock(bytes32 _reason, uint256 _time)
+        public returns (bool);
+    
+    /**
+     * @dev Increase number of tokens locked for a specified reason
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be increased
+     */
+    function increaseLockAmount(bytes32 _reason, uint256 _amount)
+        public returns (bool);
+
+    /**
+     * @dev Returns unlockable tokens for a specified address for a specified reason
+     * @param _of The address to query the the unlockable token count of
+     * @param _reason The reason to query the unlockable tokens for
+     */
+    function tokensUnlockable(address _of, bytes32 _reason)
+        public view returns (uint256 amount);
+ 
+    /**
+     * @dev Unlocks the unlockable tokens of a specified address
+     * @param _of Address of user, claiming back unlockable tokens
+     */
+    function unlock(address _of)
+        public returns (uint256 unlockableTokens);
+
+    /**
+     * @dev Gets the unlockable tokens of a specified address
+     * @param _of The address to query the the unlockable token count of
+     */
+    function getUnlockableTokens(address _of)
+        public view returns (uint256 unlockableTokens);
+
+}
 
 
 /**
  * @title SCAVOToken
- * @dev ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * @dev ERC20 Token, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
- * Version: 1.1
+ * Version: 1.3
  */
-contract SCAVOToken is StandardToken, MintableToken, PausableToken, BurnableToken {
+contract SCAVOToken is ERC1132, StandardToken, MintableToken, PausableToken, BurnableToken {
 
-  string public constant name = "SCAVO Token";
-  string public constant symbol = "SCAVO";
-  string public constant version = "1.1";
-  uint8 public constant decimals = 18;
+	string public constant name = "SCAVO Token";
+	string public constant symbol = "SCAVO";
+	string public constant version = "1.3";
+	uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 200000000 * (10 ** uint256(decimals));
+	uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  constructor() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
-  }
+	/**
+	 * @dev Error messages for require statements
+	 */
+	string constant alreadyLocked = 'Tokens already locked';
+	string constant notLocked = 'No tokens locked';
+	string constant amountZero = 'Amount can not be 0';
+	string constant transferFailed = 'Transfer Failed';
   
+	/**
+	 * @dev Constructor that gives msg.sender all of existing tokens.
+	 */
+	constructor() public {
+		totalSupply_ = INITIAL_SUPPLY;
+		balances[msg.sender] = INITIAL_SUPPLY;
+		emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+	}
   
+    /**
+     * @dev Locks a specified amount of tokens against an address,
+     *      for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be locked
+     * @param _time Lock time in seconds
+     */
+    function lock(bytes32 _reason, uint256 _amount, uint256 _time)
+        public
+        returns (bool)
+    {
+        uint256 validUntil = block.timestamp.add(_time);
+
+        // If tokens are already locked, then functions extendLock or
+        // increaseLockAmount should be used to make any changes
+        require(tokensLocked(msg.sender, _reason) == 0, alreadyLocked);
+        require(_amount != 0, amountZero);
+
+        if (locked[msg.sender][_reason].amount == 0)
+            lockReason[msg.sender].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason] = lockToken(_amount, validUntil, false);
+
+        emit Lock(msg.sender, _reason, _amount, validUntil);
+        return true;
+    }
+    
+    /**
+     * @dev Transfers and Locks a specified amount of tokens,
+     *      for a specified reason and time
+     * @param _to adress to which tokens are to be transfered
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be transfered and locked
+     * @param _time Lock time in seconds
+     */
+    function transferWithLock(address _to, bytes32 _reason, uint256 _amount, uint256 _time)
+        public
+        returns (bool)
+    {
+        uint256 validUntil = block.timestamp.add(_time);
+
+        require(tokensLocked(_to, _reason) == 0, alreadyLocked);
+        require(_amount != 0, amountZero);
+
+        if (locked[_to][_reason].amount == 0)
+            lockReason[_to].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[_to][_reason] = lockToken(_amount, validUntil, false);
+        
+        emit Lock(_to, _reason, _amount, validUntil);
+        return true;
+    }
+
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     */
+    function tokensLocked(address _of, bytes32 _reason)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (!locked[_of][_reason].claimed)
+            amount = locked[_of][_reason].amount;
+    }
+    
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason at a specific time
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     * @param _time The timestamp to query the lock tokens for
+     */
+    function tokensLockedAtTime(address _of, bytes32 _reason, uint256 _time)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity > _time)
+            amount = locked[_of][_reason].amount;
+    }
+
+    /**
+     * @dev Returns total tokens held by an address (locked + transferable)
+     * @param _of The address to query the total balance of
+     */
+    function totalBalanceOf(address _of)
+        public
+        view
+        returns (uint256 amount)
+    {
+    	amount = balanceOf(_of);
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            amount = amount.add(tokensLocked(_of, lockReason[_of][i]));
+        }   
+    }    
+    
+    /**
+     * @dev Extends lock for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _time Lock extension time in seconds
+     */
+    function extendLock(bytes32 _reason, uint256 _time)
+        public
+        returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, notLocked);
+
+        locked[msg.sender][_reason].validity = locked[msg.sender][_reason].validity.add(_time);
+
+        emit Lock(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+    
+    /**
+     * @dev Increase number of tokens locked for a specified reason
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be increased
+     */
+    function increaseLockAmount(bytes32 _reason, uint256 _amount)
+        public
+        returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, notLocked);
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason].amount = locked[msg.sender][_reason].amount.add(_amount);
+
+        emit Lock(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+
+    /**
+     * @dev Returns unlockable tokens for a specified address for a specified reason
+     * @param _of The address to query the the unlockable token count of
+     * @param _reason The reason to query the unlockable tokens for
+     */
+    function tokensUnlockable(address _of, bytes32 _reason)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity <= now && !locked[_of][_reason].claimed)
+            amount = locked[_of][_reason].amount;
+    }
+
+    /**
+     * @dev Unlocks the unlockable tokens of a specified address
+     * @param _of Address of user, claiming back unlockable tokens
+     */
+    function unlock(address _of)
+        public
+        returns (uint256 unlockableTokens)
+    {
+        uint256 lockedTokens;
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            lockedTokens = tokensUnlockable(_of, lockReason[_of][i]);
+            if (lockedTokens > 0) {
+                unlockableTokens = unlockableTokens.add(lockedTokens);
+                locked[_of][lockReason[_of][i]].claimed = true;
+                emit Unlock(_of, lockReason[_of][i], lockedTokens);
+            }
+        }  
+
+        if(unlockableTokens > 0)
+        	this.transfer(_of, unlockableTokens);
+    }
+
+    /**
+     * @dev Gets the unlockable tokens of a specified address
+     * @param _of The address to query the the unlockable token count of
+     */
+    function getUnlockableTokens(address _of)
+        public
+        view
+        returns (uint256 unlockableTokens)
+    {
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            unlockableTokens = unlockableTokens.add(tokensUnlockable(_of, lockReason[_of][i]));
+        }  
+    }  
 
 }
