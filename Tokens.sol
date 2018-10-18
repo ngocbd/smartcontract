d7bc119ac5b3f2c727944b7945a935842ac71e0d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 12:11:30 +0700

    add contract Tokens

--- Tokens.sol
+++ Tokens.sol
@@ -1,69 +1,32 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Tokens at 0x4f587c74e0064379ff20c1144079f6169a312c94
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Tokens at 0xa1d838f7c0b30cfad66d47a144454bfd7ec0f9ab
 */
 pragma solidity ^0.4.24;
 
 contract Tokens {
+    address[] public tokens;
     address public owner;
-    mapping(string => uint) supply; // Total supply of token named
-    mapping(string => mapping(address => uint)) balances;
-    uint public fee; // For creation
 
-    constructor(uint _fee) public {
-        owner = msg.sender;
-        fee = _fee;
+    constructor() public {
+        owner = 0x5fa344f3B7AfD345377A37B62Ce87DDE01c1D414;
     }
 
-    modifier onlyOwner() {
+    function changeOwner(address _owner) public {
         require(msg.sender == owner);
-        _;
+        owner = _owner;      
     }
 
-    // Safe math functions
-    function subtr(uint a, uint b) internal pure returns (uint) {
-        assert(b <= a);
-        return a - b;
+    function addToken(address _address) public {
+        require(msg.sender == owner);
+        tokens.push(_address);
     }
 
-    function addit(uint a, uint b) internal pure returns (uint) {
-        uint c = a + b;
-        assert(c >= a);
-        return c;
+    function deleteToken(uint _tokenId) public {
+        require(msg.sender == owner);
+        delete tokens[_tokenId];      
     }
 
-    // Create new token, supply is hard, be serious on internal
-    function mint(string _name, uint _supply) public payable {
-        require(msg.value >= fee); // Fee is prevents over create
-        require(supply[_name] == 0); // Protect from remint
-        supply[_name] = _supply;
-        balances[_name][msg.sender] = _supply;
-        emit Mint(_name, _supply);
+    function getTokensCount() public view returns(uint) {
+        return tokens.length;
     }
-
-    function transfer(string _name, address _to, uint _amount) external {
-        require(_amount <= balances[_name][msg.sender]);
-        balances[_name][msg.sender] = subtr(balances[_name][msg.sender], _amount);
-        balances[_name][_to] = addit(balances[_name][_to], _amount);
-        emit Transfer(_name, msg.sender, _to, _amount);
-    }
-
-    function balanceOf(string _name, address _address) external view returns(uint) {
-        return balances[_name][_address];
-    }
-
-    function supplyOf(string _name) external view returns(uint) {
-        return supply[_name];
-    }
-
-    function setFee(uint _fee) external onlyOwner {
-        fee = _fee;
-    }
-
-    // Withdraw fee to owner
-    function withdraw() external onlyOwner {
-        owner.transfer(address(this).balance);
-    }
-
-    event Transfer(string indexed name, address indexed from, address indexed to, uint amount);
-    event Mint(string indexed name, uint supply);
 }
