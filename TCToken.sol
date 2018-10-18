85e5df995689fdd486cfe3f4a995afa0cc6468a6
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 08:57:22 +0700

    add contract TCToken

--- TCToken.sol
+++ TCToken.sol
@@ -1,185 +1,149 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TCToken at 0x2d4ca7d079415595f9e5b62602f7ae7d88b91e53
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TCToken at 0x862723e22dd5aa4c0875eb0c4ba656f40c750f02
 */
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.8;
 
+/**
+ * Math operations with safety checks
+ */
 contract SafeMath {
-  function mulSafe(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-       return 0;
-     }
-     uint256 c = a * b;
-     assert(c / a == b);
-     return c;
-   }
-
-  function divSafe(uint256 a, uint256 b) internal pure returns (uint256) {
-     uint256 c = a / b;
-     return c;
+  function safeMul(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
   }
 
-  function subSafe(uint256 a, uint256 b) internal pure returns (uint256) {
+  function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b > 0);
+    uint256 c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+  function safeSub(uint256 a, uint256 b) internal returns (uint256) {
     assert(b <= a);
-     return a - b;
-   }
-
-  function addSafe(uint256 a, uint256 b) internal pure returns (uint256) {
-     uint256 c = a + b;
-    assert(c >= a);
-     return c;
-   }
-}
-
-contract Owned {
-    address public owner;
-    address public newOwner;
-    event OwnershipTransferred(address indexed _from, address indexed _to);
-    function Constructor() public { owner = msg.sender; }
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address _newOwner) public onlyOwner {
-        newOwner = _newOwner;
-    }
-    function acceptOwnership() public {
-        require(msg.sender == newOwner);
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
-    }
-}
-
-contract ERC20 {
-   uint256 public totalSupply;
-   function balanceOf(address who) public view returns (uint256);
-   function transfer(address to, uint256 value) public returns (bool);
-   event Transfer(address indexed from, address indexed to, uint256 value);
-   function allowance(address owner, address spender) public view returns (uint256);
-   function transferFrom(address from, address to, uint256 value) public returns (bool);
-   function approve(address spender, uint256 value) public returns (bool);
-   event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-contract ERC223 {
-    function transfer(address to, uint value, bytes data) public;
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
-}
-
-contract ERC223ReceivingContract { 
-    function tokenFallback(address _from, uint _value, bytes _data) public;
-}
-
-contract StandardToken is ERC20, ERC223, SafeMath, Owned {
-  event ReleaseSupply(address indexed receiver, uint256 value, uint256 releaseTime);
-  mapping(address => uint256) balances;
-  mapping (address => mapping (address => uint256)) internal allowed;
-
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-    balances[msg.sender] = subSafe(balances[msg.sender], _value);
-    balances[_to] = addSafe(balances[_to], _value);
-    Transfer(msg.sender, _to, _value);
-    return true;
+    return a - b;
   }
 
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-   }
+  function safeAdd(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
 
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-
-    balances[_from] = subSafe(balances[_from], _value);
-     balances[_to] = addSafe(balances[_to], _value);
-     allowed[_from][msg.sender] = subSafe(allowed[_from][msg.sender], _value);
-    Transfer(_from, _to, _value);
-     return true;
-   }
-
-   function approve(address _spender, uint256 _value) public returns (bool) {
-     allowed[msg.sender][_spender] = _value;
-     Approval(msg.sender, _spender, _value);
-     return true;
-   }
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-     return allowed[_owner][_spender];
-   }
-
-   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-     allowed[msg.sender][_spender] = addSafe(allowed[msg.sender][_spender], _addedValue);
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-     uint oldValue = allowed[msg.sender][_spender];
-     if (_subtractedValue > oldValue) {
-       allowed[msg.sender][_spender] = 0;
-     } else {
-       allowed[msg.sender][_spender] = subSafe(oldValue, _subtractedValue);
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
     }
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-
-    function transfer(address _to, uint _value, bytes _data) public {
-        require(_value > 0 );
-        if(isContract(_to)) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-            receiver.tokenFallback(msg.sender, _value, _data);
-        }
-        balances[msg.sender] = subSafe(balances[msg.sender], _value);
-        balances[_to] = addSafe(balances[_to], _value);
-        Transfer(msg.sender, _to, _value, _data);
-    }
-
-    function isContract(address _addr) private view returns (bool is_contract) {
-      uint length;
-      assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-      }
-      return (length>0);
-    }
-
+  }
 }
+contract TCToken is SafeMath{
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+	address public owner;
 
-contract TCToken is StandardToken {
-  string public name = 'Toxbtc Token';
-  string public symbol = 'TC';
-  uint public decimals = 18;
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+	mapping (address => uint256) public freezeOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-  uint256 createTime         = 1527436800;  //20180528 //1528128000;  //20180605 00:00:00
-  uint256 bonusEnds          = 1528646400;  //20180611 00:00:00
-  uint256 endDate            = 1529078400;  //20180616 00:00:00
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-  uint256 public frozenForever ;
+    /* This notifies clients about the amount burnt */
+    event Burn(address indexed from, uint256 value);
+	
+	/* This notifies clients about the amount frozen */
+    event Freeze(address indexed from, uint256 value);
+	
+	/* This notifies clients about the amount unfrozen */
+    event Unfreeze(address indexed from, uint256 value);
 
-  function TCToken() public {
-    totalSupply  = 1000000000 * 10 ** uint256(decimals); 
-    frozenForever = 400000000 * 10 ** uint256(decimals); 
-    balances[msg.sender] = subSafe(totalSupply,frozenForever) ;
-    owner = msg.sender;
-  }
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function TCToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+		owner = msg.sender;
+    }
 
-  function () public payable {
-      require(now >= createTime && now <= endDate);
-      uint tokens;
-      if (now <= bonusEnds) {
-          tokens = msg.value * 2480;
-      } else {
-          tokens = msg.value * 2000;
-      }
-      require(tokens <= balances[owner]);
-      balances[msg.sender] = addSafe(balances[msg.sender], tokens);
-      balances[owner] = subSafe(balances[owner], tokens);
-      Transfer(address(0), msg.sender, tokens);
-      owner.transfer(msg.value);
-  }
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (_to == 0x0) throw;                               // Prevent transfer to 0x0 address. Use burn() instead
+		if (_value <= 0) throw; 
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                     // Subtract from the sender
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+		if (_value <= 0) throw; 
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+       
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (_to == 0x0) throw;                                // Prevent transfer to 0x0 address. Use burn() instead
+		if (_value <= 0) throw; 
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;     // Check allowance
+        balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);                           // Subtract from the sender
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                             // Add the same to the recipient
+        allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function burn(uint256 _value) returns (bool success) {
+        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
+		if (_value <= 0) throw; 
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                      // Subtract from the sender
+        totalSupply = SafeMath.safeSub(totalSupply,_value);                                // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+	
+	function freeze(uint256 _value) returns (bool success) {
+        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
+		if (_value <= 0) throw; 
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                      // Subtract from the sender
+        freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);                                // Updates totalSupply
+        Freeze(msg.sender, _value);
+        return true;
+    }
+	
+	function unfreeze(uint256 _value) returns (bool success) {
+        if (freezeOf[msg.sender] < _value) throw;            // Check if the sender has enough
+		if (_value <= 0) throw; 
+        freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);                      // Subtract from the sender
+		balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);
+        Unfreeze(msg.sender, _value);
+        return true;
+    }
+	
+	// transfer balance to owner
+	function withdrawEther(uint256 amount) {
+		if(msg.sender != owner)throw;
+		owner.transfer(amount);
+	}
+	
+	// can accept ether
+	function() payable {
+    }
 }
