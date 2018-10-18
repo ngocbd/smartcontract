b365ace6da2c3a6cd38f0d5396941e18367e0258
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 15:07:33 +0700

    add contract SLoader

---
+++ SLoader.sol
@@ -0,0 +1,45 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SLoader at 0x99af965b51312c8869fac5f527f47af92fccf83c
+*/
+pragma solidity 0.4.25;
+
+contract SLoader {
+  uint8 public releaseCount;
+  Version[] releases;
+  address owner;
+
+  modifier ifOwner {
+    require(owner == msg.sender);
+    _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function addRelease(bytes32 checksum, string url) ifOwner public {
+    releases.push(Version(checksum, url));
+    releaseCount++;
+  }
+
+  function latestReleaseChecksum() constant public returns (bytes32) {
+    return releases[releaseCount - 1].checksum;
+  }
+
+  function latestReleaseUrl() constant public returns (string) {
+    return releases[releaseCount - 1].url;
+  }
+
+  function releaseChecksum(uint8 index) constant public returns (bytes32) {
+    return releases[index].checksum;
+  }
+
+  function releaseUrl(uint8 index) constant public returns (string) {
+    return releases[index].url;
+  }
+
+  struct Version {
+    bytes32 checksum;
+    string url;
+  }
+}
