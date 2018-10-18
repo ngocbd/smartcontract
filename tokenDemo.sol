67a69d7c6a1eab46e27ab2f97da44d9404ad74b7
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:54:09 +0700

    add contract TokenDemo

--- tokenDemo.sol
+++ tokenDemo.sol
@@ -1,75 +1,74 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenDemo at 0xba45cc120a57e0951f59ab1f4e648871b730539c
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenDemo at 0x409a40058e7f70592d627fcc4a03a65b8423964e
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.16;
 contract Token{
     uint256 public totalSupply;
-
+ 
     function balanceOf(address _owner) public constant returns (uint256 balance);
     function transfer(address _to, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns   
     (bool success);
-
+ 
     function approve(address _spender, uint256 _value) public returns (bool success);
-
+ 
     function allowance(address _owner, address _spender) public constant returns 
     (uint256 remaining);
-
+ 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 
     _value);
 }
-
+ 
 contract TokenDemo is Token {
-
-    string public name;                   //?????"My test token"
-    uint8 public decimals;               //??token?????????????????3?????0.001??.
-    string public symbol;               //token??,like MTT
-
+ 
+    string public name;                   
+    uint8 public decimals;               
+    string public symbol;               
+ 
     function TokenDemo(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public {
-        totalSupply = _initialAmount * 10 ** uint256(_decimalUnits);         // ??????
-        balances[msg.sender] = totalSupply; // ??token??????????????????????????????
-
+        totalSupply = _initialAmount * 10 ** uint256(_decimalUnits);        
+        balances[msg.sender] = totalSupply; 
+ 
         name = _tokenName;                   
         decimals = _decimalUnits;          
         symbol = _tokenSymbol;
     }
-
+ 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        //??totalSupply ??????? (2^256 - 1).
-        //??????????????token??????????????????
+        
         require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         require(_to != 0x0);
-        balances[msg.sender] -= _value;//???????????token??_value
-        balances[_to] += _value;//???????token??_value
-        Transfer(msg.sender, _to, _value);//????????
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
         return true;
     }
-
-
+ 
+ 
     function transferFrom(address _from, address _to, uint256 _value) public returns 
     (bool success) {
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;//??????token??_value
-        balances[_from] -= _value; //????_from??token??_value
-        allowed[_from][msg.sender] -= _value;//??????????_from????????_value
-        Transfer(_from, _to, _value);//????????
+        balances[_to] += _value;
+        balances[_from] -= _value; 
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
         return true;
     }
     function balanceOf(address _owner) public constant returns (uint256 balance) {
         return balances[_owner];
     }
-
-
+ 
+ 
     function approve(address _spender, uint256 _value) public returns (bool success)   
     { 
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-
+ 
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];//??_spender?_owner????token?
+        return allowed[_owner][_spender];
     }
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
