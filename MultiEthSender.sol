8e7f56cc385c644a17de3c4eb983212550120fa8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 13:15:07 +0700

    add contract MultiEthSender

--- MultiEthSender.sol
+++ MultiEthSender.sol
@@ -1,33 +1,25 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiEthSender at 0xb52323c98e75974d2139e65d3cba06c43a53cae4
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiEthSender at 0x4BE2983B8C77316aaB2A73f7be66Eb7B15EC2ae1
 */
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.0;
 
 contract MultiEthSender {
     
-    event Send(uint256 _amount, address indexed _receiver);
-    
-    constructor() payable public {
-        
-    }
-    
-    function multiSendEth(uint256 amount, address[] list) public payable returns (bool) {
-        uint balanceBeforeTransfer = address(this).balance;
-        
-        for(uint i=0;i<list.length;i++){
+    event Send (
+        uint256 _amount,
+        address indexed_receiver
+    );
+
+    function multiSendEth(uint256 amount, address[] list) public returns (bool) {
+        uint listLength = list.length;
+        for (uint i=0; i<listLength; i++) {
             list[i].transfer(amount);
             emit Send(amount, list[i]);
         }
-        assert(address(this).balance == balanceBeforeTransfer - amount*list.length);
+        
         return true;
     }
     
-    function getBalance() constant public returns(uint){
-        return address(this).balance;
-    }
-    
-    function() public payable{
-        
-    }
-    
+    function () external payable {}
+
 }
