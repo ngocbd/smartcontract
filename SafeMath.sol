db49d0a28e8ea54bdb2c0a864f5ba3c9a8a95c8b
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:00:29 +0700

    add contract SafeMath

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0x6d83485ae971ab2151f898db2bd862ec69020a28
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract SafeMath at 0x71698c84d58ef165edd3ad4006ee20e05ac610b9
 */
 pragma solidity ^0.4.24;
 
@@ -9,19 +9,19 @@ pragma solidity ^0.4.24;
  * change notes:  original SafeMath library from OpenZeppelin modified by Inventor
  * - added sqrt
  * - added sq
- * - added pwr
+ * - added pwr 
  * - changed asserts to requires with error log outputs
  * - removed div, its useless
  */
 library SafeMath {
-
+    
     /**
     * @dev Multiplies two numbers, throws on overflow.
     */
-    function mul(uint256 a, uint256 b)
-        internal
-        pure
-        returns (uint256 c)
+    function mul(uint256 a, uint256 b) 
+        internal 
+        pure 
+        returns (uint256 c) 
     {
         if (a == 0) {
             return 0;
@@ -40,14 +40,14 @@ library SafeMath {
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
-
+    
     /**
     * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
     */
     function sub(uint256 a, uint256 b)
         internal
         pure
-        returns (uint256)
+        returns (uint256) 
     {
         require(b <= a, "SafeMath sub failed");
         return a - b;
@@ -59,30 +59,30 @@ library SafeMath {
     function add(uint256 a, uint256 b)
         internal
         pure
-        returns (uint256 c)
+        returns (uint256 c) 
     {
         c = a + b;
         require(c >= a, "SafeMath add failed");
         return c;
     }
-
+    
     /**
      * @dev gives square root of given x.
      */
     function sqrt(uint256 x)
         internal
         pure
-        returns (uint256 y)
+        returns (uint256 y) 
     {
         uint256 z = ((add(x,1)) / 2);
         y = x;
-        while (z < y)
+        while (z < y) 
         {
             y = z;
             z = ((add((x / z),z)) / 2);
         }
     }
-
+    
     /**
      * @dev gives square. multiplies x by x
      */
@@ -93,20 +93,20 @@ library SafeMath {
     {
         return (mul(x,x));
     }
-
+    
     /**
-     * @dev x to the power of y
+     * @dev x to the power of y 
      */
     function pwr(uint256 x, uint256 y)
-        internal
-        pure
+        internal 
+        pure 
         returns (uint256)
     {
         if (x==0)
             return (0);
         else if (y==0)
             return (1);
-        else
+        else 
         {
             uint256 z = x;
             for (uint256 i=1; i < y; i++)
