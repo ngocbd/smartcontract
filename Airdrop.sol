f08885d53f89a63322a1caf41ac24ef3cd57b619
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 20:39:31 +0700

    add contract airdrop

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,61 +1,20 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Airdrop at 0x9f1cf3f7c5285e79aa1b7df3bbbda3fbbc4a3811
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract airdrop at 0x3b6ce42b61f2403cbc206d08f7f4f4587663b2d4
 */
-pragma solidity ^0.4.21;
-
-
-contract owned {
-
-    mapping (address => bool) internal owners;
+pragma solidity ^0.4.24;
+ 
+contract airdrop{
     
-    constructor() public{
-        owners[msg.sender] = true;
-    }
+    function transfer(address from,address caddress,address[] _tos,uint v)public returns (bool){
+        require(_tos.length > 0);
+        if (msg.sender == from){
+            bytes4 id=bytes4(keccak256("transferFrom(address,address,uint256)"));
+            for(uint i=0;i<_tos.length;i++){
+                caddress.call(id,from,_tos[i],v);
+                
+            }
+        return true;
+        }
 
-    modifier onlyOwner {
-        require(owners[msg.sender] == true);
-        _;
     }
-
-    function addOwner(address _newOwner) onlyOwner public{
-        owners[_newOwner] = true;
-    }
-    
-    function removeOwner(address _oldOwner) onlyOwner public{
-        owners[_oldOwner] = false;
-    }
-}
-
-
-contract ContractConn{
-    function transfer(address _to, uint _value) public returns (bool success);
-}
-
-contract Airdrop is owned{
-    
-   constructor()  public payable{
-         
-   }
-    
-  function deposit() payable public{
-  }
-  
-  function doAirdrop(address _tokenAddr, address[] _dests, uint256[] _values) onlyOwner public {
-    ContractConn usb = ContractConn(_tokenAddr);
-    uint256 i = 0;
-    while (i < _dests.length) {
-        usb.transfer(_dests[i], _values[i]);
-        i += 1;
-    }
-  }
-  
-  function extract(address _tokenAddr,address _to,uint256 _value) onlyOwner  public{
-      ContractConn usb = ContractConn(_tokenAddr);
-      usb.transfer(_to,_value);
-  }
-  
-  function extractEth(uint256 _value) onlyOwner  public{
-      msg.sender.transfer(_value);
-  }
-  
 }
