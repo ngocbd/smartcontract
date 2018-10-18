caa9ca9cefe3c2698a4e414e6baa641aa854655c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:42:43 +0700

    add contract TwelveHourTrains

--- TwelveHourTrains.sol
+++ TwelveHourTrains.sol
@@ -1,12 +1,13 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TwelveHourTrains at 0xf00e5a59fde1cb2ead96a8be7711abc3f981e0f1
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TwelveHourTrains at 0xdf719da6ee495547b43737fc5ba56d139a16da5b
 */
 pragma solidity ^0.4.25;
 
 /**
 *
-12HourTrains - 100% each 12 hours
+12HourTrains - 3% every 12 hours. Want to get quick ETH? Try our new Dice game.
 https://12hourtrains.github.io/
+Version 3
 */
 contract TwelveHourTrains {
 
@@ -22,50 +23,65 @@ contract TwelveHourTrains {
     uint256 public stakingRequirement = 2 ether;
     address public ownerWallet;
     address public owner;
+    uint256 private randNonce = 0;
 
+    /**
+    * @dev Modifiers
+    */
+
+    modifier onlyOwner() 
+    {
+        require(msg.sender == owner);
+        _;
+    }
+    modifier disableContract()
+    {
+        require(tx.origin == msg.sender);
+        _;
+    }
+    /**
+    * @dev Event
+    */
     event Invest(address investor, uint256 amount);
     event Withdraw(address investor, uint256 amount);
     event Bounty(address hunter, uint256 amount);
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
+    event Lottery(address player, uint256 lotteryNumber, uint256 amount, uint256 result,bool isWin);
     /**
-     * @dev ?onstructor Sets the original roles of the contract
-     */
+    * @dev ?onstructor Sets the original roles of the contract
+    */
 
-    constructor() public {
+    constructor() public 
+    {
         owner = msg.sender;
         ownerWallet = msg.sender;
     }
 
     /**
-     * @dev Modifiers
-     */
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    /**
-     * @dev Allows current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     * @param newOwnerWallet The address to transfer ownership to.
-     */
-    function transferOwnership(address newOwner, address newOwnerWallet) public onlyOwner {
+    * @dev Allows current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    * @param newOwnerWallet The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner, address newOwnerWallet) public onlyOwner 
+    {
         require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
+
         owner = newOwner;
         ownerWallet = newOwnerWallet;
+
+        emit OwnershipTransferred(owner, newOwner);
     }
 
     /**
-     * @dev Investments
-     */
-    function () public payable {
+    * @dev Investments
+    */
+    function () public payable 
+    {
         buy(0x0);
     }
 
-    function buy(address _referredBy) public payable {
+    function buy(address _referredBy) public payable 
+    {
         require(msg.value >= minimum);
 
         address _customerAddress = msg.sender;
@@ -93,7 +109,29 @@ contract TwelveHourTrains {
        investments[msg.sender] = investments[msg.sender].add(msg.value);
        joined[msg.sender] = block.timestamp;
        ownerWallet.transfer(msg.value.mul(5).div(100));
+
        emit Invest(msg.sender, msg.value);
+    }
+    //--------------------------------------------------------------------------------------------
+    // LOTTERY
+    //--------------------------------------------------------------------------------------------
+    /**
+    * @param _value number in array [1,2,3]
+    */
+    function lottery(uint256 _value) public payable disableContract
+    {
+        uint256 random = getRandomNumber(msg.sender) + 1;
+        bool isWin = false;
+        if (random == _value) {
+            isWin = true;
+            uint256 prize = msg.value.mul(249).div(100);
+            if (prize <= address(this).balance) {
+                msg.sender.transfer(prize);
+            }
+        }
+        ownerWallet.transfer(msg.value.mul(5).div(100));
+        
+        emit Lottery(msg.sender, _value, msg.value, random, isWin);
     }
 
     /**
@@ -103,7 +141,7 @@ contract TwelveHourTrains {
     function getBalance(address _address) view public returns (uint256) {
         uint256 minutesCount = now.sub(joined[_address]).div(1 minutes);
         uint256 percent = investments[_address].mul(step).div(100);
-        uint256 different = percent.mul(minutesCount).div(720);
+        uint256 different = percent.mul(minutesCount).div(24000);
         uint256 balance = different.sub(withdrawals[_address]);
 
         return balance;
@@ -126,7 +164,6 @@ contract TwelveHourTrains {
             return false;
         }
     }
-
     /**
     * @dev Bounty reward
     */
@@ -154,16 +191,17 @@ contract TwelveHourTrains {
     * @param _investor The address to query the the balance of.
     * @return An uint256 representing the amount owned by the passed address.
     */
-    function checkWithdrawals(address _investor) public view returns (uint256) {
+    function checkWithdrawals(address _investor) public view returns (uint256) 
+    {
         return withdrawals[_investor];
     }
-
     /**
     * @dev Gets investments of the specified address.
     * @param _investor The address to query the the balance of.
     * @return An uint256 representing the amount owned by the passed address.
     */
-    function checkInvestments(address _investor) public view returns (uint256) {
+    function checkInvestments(address _investor) public view returns (uint256) 
+    {
         return investments[_investor];
     }
 
@@ -172,9 +210,23 @@ contract TwelveHourTrains {
     * @param _hunter The address of the referrer
     * @return An uint256 representing the referral earnings.
     */
-    function checkReferral(address _hunter) public view returns (uint256) {
+    function checkReferral(address _hunter) public view returns (uint256) 
+    {
         return referrer[_hunter];
     }
+    function checkContractBalance() public view returns (uint256) 
+    {
+        return address(this).balance;
+    }
+    //----------------------------------------------------------------------------------
+    // INTERNAL FUNCTION
+    //----------------------------------------------------------------------------------
+    function getRandomNumber(address _addr) private returns(uint256 randomNumber) 
+    {
+        randNonce++;
+        randomNumber = uint256(keccak256(abi.encodePacked(now, _addr, randNonce, block.coinbase, block.number))) % 3;
+    }
+
 }
 
 /**
