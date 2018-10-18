291f9dcd6c987e5ae1e24bb5426c91c84db95356
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 19:00:07 +0700

    add contract Whitelist

--- WhiteList.sol
+++ WhiteList.sol
@@ -1,34 +1,109 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Whitelist at 0x02d1987F90F7A31570E6111C5d506B2e48ee30A2
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Whitelist at 0x96422fa3f9cc8ecbf1e092881c74dd3cc8e70b36
 */
-contract Whitelist {
+pragma solidity 0.4.24;
 
-    mapping(address => bool) public whitelist;
-    mapping(address => bool) operators;
-    address authority;
+// File: contracts/lib/Ownable.sol
 
-    constructor(address _authority) {
-        authority = _authority;
-        operators[_authority] = true;
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner, "only owner is able to call this function");
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+// File: contracts/Whitelist.sol
+
+/**
+ * @title Whitelist - crowdsale whitelist contract
+ * @author Gustavo Guimaraes - <gustavo@starbase.co>
+ */
+contract Whitelist is Ownable {
+    mapping(address => bool) public allowedAddresses;
+
+    event WhitelistUpdated(uint256 timestamp, string operation, address indexed member);
+
+    /**
+    * @dev Adds single address to whitelist.
+    * @param _address Address to be added to the whitelist
+    */
+    function addToWhitelist(address _address) external onlyOwner {
+        allowedAddresses[_address] = true;
+        emit WhitelistUpdated(now, "Added", _address);
     }
-    
-    function add(address _address) public {
-        require(operators[msg.sender]);
-        whitelist[_address] = true;
+
+    /**
+     * @dev add various whitelist addresses
+     * @param _addresses Array of ethereum addresses
+     */
+    function addManyToWhitelist(address[] _addresses) external onlyOwner {
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            allowedAddresses[_addresses[i]] = true;
+            emit WhitelistUpdated(now, "Added", _addresses[i]);
+        }
     }
 
-    function remove(address _address) public {
-        require(operators[msg.sender]);
-        whitelist[_address] = false;
-    }
-
-    function addOperator(address _address) public {
-        require(authority == msg.sender);
-        operators[_address] = true;
-    }
-
-    function removeOperator(address _address) public {
-        require(authority == msg.sender);
-        operators[_address] = false;
+    /**
+     * @dev remove whitelist addresses
+     * @param _addresses Array of ethereum addresses
+     */
+    function removeManyFromWhitelist(address[] _addresses) public onlyOwner {
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            allowedAddresses[_addresses[i]] = false;
+            emit WhitelistUpdated(now, "Removed", _addresses[i]);
+        }
     }
 }
