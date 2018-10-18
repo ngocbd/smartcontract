5fa367b6881abf227947a29d2b5ff2c388ef6c38
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 18:28:29 +0700

    add contract LockingContract

--- LockingContract.sol
+++ LockingContract.sol
@@ -1,72 +1,7 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LockingContract at 0xe42600D58F17a6D1cf0Ba4f65969083907a4DEC9
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract LockingContract at 0x4eD0bCaF522b4845eeC9b0A23899eE4f15d09EF7
 */
-pragma solidity ^0.4.19;
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+pragma solidity ^0.4.21;
 
 
 /**
@@ -115,66 +50,138 @@ library SafeMath {
   }
 }
 
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
 contract LockingContract is Ownable {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  event NotedTokens(address indexed _beneficiary, uint256 _tokenAmount);
-  event ReleasedTokens(address indexed _beneficiary);
-  event ReducedLockingTime(uint256 _newUnlockTime);
+    event NotedTokens(address indexed _beneficiary, uint256 _tokenAmount);
+    event ReleasedTokens(address indexed _beneficiary);
+    event ReducedLockingTime(uint256 _newUnlockTime);
 
-  ERC20 public tokenContract;
-  mapping(address => uint256) public tokens;
-  uint256 public totalTokens;
-  uint256 public unlockTime;
+    ERC20 public tokenContract;
+    mapping(address => uint256) public tokens;
+    uint256 public totalTokens;
+    uint256 public unlockTime;
 
-  function isLocked() public view returns(bool) {
-    return now < unlockTime;
-  }
+    function isLocked() public view returns(bool) {
+        return now < unlockTime;
+    }
 
-  modifier onlyWhenUnlocked() {
-    require(!isLocked());
-    _;
-  }
+    modifier onlyWhenUnlocked() {
+        require(!isLocked());
+        _;
+    }
 
-  modifier onlyWhenLocked() {
-    require(isLocked());
-    _;
-  }
+    modifier onlyWhenLocked() {
+        require(isLocked());
+        _;
+    }
 
-  function LockingContract(ERC20 _tokenContract, uint256 _unlockTime) public {
-    require(_unlockTime > 0);
-    unlockTime = _unlockTime;
-    tokenContract = _tokenContract;
-  }
+    function LockingContract(ERC20 _tokenContract, uint256 _unlockTime) public {
+        require(_unlockTime > now);
+        require(address(_tokenContract) != 0x0);
+        unlockTime = _unlockTime;
+        tokenContract = _tokenContract;
+    }
 
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return tokens[_owner];
-  }
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return tokens[_owner];
+    }
 
-  // Should only be done from another contract.
-  // To ensure that the LockingContract can release all noted tokens later,
-  // one should mint/transfer tokens to the LockingContract's account prior to noting
-  function noteTokens(address _beneficiary, uint256 _tokenAmount) external onlyOwner onlyWhenLocked {
-    uint256 tokenBalance = tokenContract.balanceOf(this);
-    require(tokenBalance == totalTokens.add(_tokenAmount));
+    // Should only be done from another contract.
+    // To ensure that the LockingContract can release all noted tokens later,
+    // one should mint/transfer tokens to the LockingContract's account prior to noting
+    function noteTokens(address _beneficiary, uint256 _tokenAmount) external onlyOwner onlyWhenLocked {
+        uint256 tokenBalance = tokenContract.balanceOf(this);
+        require(tokenBalance >= totalTokens.add(_tokenAmount));
 
-    tokens[_beneficiary] = tokens[_beneficiary].add(_tokenAmount);
-    totalTokens = totalTokens.add(_tokenAmount);
-    NotedTokens(_beneficiary, _tokenAmount);
-  }
+        tokens[_beneficiary] = tokens[_beneficiary].add(_tokenAmount);
+        totalTokens = totalTokens.add(_tokenAmount);
+        emit NotedTokens(_beneficiary, _tokenAmount);
+    }
 
-  function releaseTokens(address _beneficiary) public onlyWhenUnlocked {
-    uint256 amount = tokens[_beneficiary];
-    tokens[_beneficiary] = 0;
-    require(tokenContract.transfer(_beneficiary, amount)); 
-    totalTokens = totalTokens.sub(amount);
-    ReleasedTokens(_beneficiary);
-  }
+    function releaseTokens(address _beneficiary) public onlyWhenUnlocked {
+        require(msg.sender == owner || msg.sender == _beneficiary);
+        uint256 amount = tokens[_beneficiary];
+        tokens[_beneficiary] = 0;
+        require(tokenContract.transfer(_beneficiary, amount)); 
+        totalTokens = totalTokens.sub(amount);
+        emit ReleasedTokens(_beneficiary);
+    }
 
-  function reduceLockingTime(uint256 _newUnlockTime) public onlyOwner onlyWhenLocked {
-    require(_newUnlockTime >= now);
-    require(_newUnlockTime < unlockTime);
-    unlockTime = _newUnlockTime;
-    ReducedLockingTime(_newUnlockTime);
-  }
+    function reduceLockingTime(uint256 _newUnlockTime) public onlyOwner onlyWhenLocked {
+        require(_newUnlockTime >= now);
+        require(_newUnlockTime < unlockTime);
+        unlockTime = _newUnlockTime;
+        emit ReducedLockingTime(_newUnlockTime);
+    }
 }
