dceb453e0fec79c6d2e676ca478d660d5f172cec
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:26:01 +0700

    add contract BuckySalary

--- BuckySalary.sol
+++ BuckySalary.sol
@@ -1,5 +1,5 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BuckySalary at 0x500287a823f413524835e99049c52bea8eb58f97
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BuckySalary at 0x766f28c22028b55b0bc6d7c56ad006a10403847b
 */
 pragma solidity ^0.4.18;
 
@@ -55,79 +55,19 @@ contract Ownable {
 
 contract BuckySalary is Ownable {
 
-    string[] public staff = [ 
-        "0xE385917ACF8251fca45595b7919f38bab581749c", 
-        "0xC4Bed66d88F39C0D18cE601b408464d554A38771", 
-        "0xc07ED3e252d8C6819F763d904D1692D1242ec7ee", 
-        "0x2CD147bb1d347a6A887887B569AAa8A262cF8346", 
-        "0x6A1eBbff7714dfcE36756d09440ac979Bbf87b10", 
-        "0x729501BE221C534d9C090a8Ee4e8B5B16d6b356C", 
-        "0xad82A5fb394a525835A3a6DC34C1843e19160CFA", 
-        "0x5DD309a882c2BB49B5e5Ed1b49D209363B0f2a37", 
-        "0x490f72f8DfB81859fe61ecfe1fEB9F6C61a1aa89", 
-        "0xBd0b6cdf81B282C0401bc67d0d523D00Fc59c55c"  
-    ];
+    address[] public staff;
+    mapping(address => uint) public eth;
 
-    uint[] public staffETH = [
-        1 ether,
-        1 ether,
-        1 ether,
-        1 ether,
-        1 ether,
-        1 ether,
-        1 ether,
-        1 ether,
-        0.5 ether,
-        0.5 ether
-    ];
-
-    
     
     function BuckySalary() public {
 
     }
 
-    function bytesToAddress (bytes b) internal constant returns (address) {
-        uint result = 0;
-        for (uint i = 0; i < b.length; i++) {
-            uint c = uint(b[i]);
-            if (c >= 48 && c <= 57) {
-                result = result * 16 + (c - 48);
-            }
-            if(c >= 65 && c<= 90) {
-                result = result * 16 + (c - 55);
-            }
-            if(c >= 97 && c<= 122) {
-                result = result * 16 + (c - 87);
-            }
-        }
-        return address(result);
-    }
-      
-    
-    function strCompare(string _a, string _b) internal returns (int) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-        uint minLength = a.length;
-        if (b.length < minLength) minLength = b.length;
-        for (uint i = 0; i < minLength; i ++) {
-            if (a[i] < b[i])
-                return -1;
-            else if (a[i] > b[i])
-                return 1;
-        }
-        if (a.length < b.length)
-            return -1;
-        else if (a.length > b.length)
-            return 1;
-        else
-            return 0;
-   } 
 
     function getTotal() internal view returns (uint) {
         uint total = 0;
         for (uint i = 0; i < staff.length; i++) {
-            total += staffETH[i];    
+            total += eth[staff[i]];    
         }
 
         return total;
@@ -140,9 +80,9 @@ contract BuckySalary is Ownable {
         require(msg.value >= total);
 
         for (uint i = 0; i < staff.length; i++) {
-            bytes b = bytes(staff[i]);
-            address s = bytesToAddress(b);
-            uint value = staffETH[i];
+            
+            address s = staff[i];
+            uint value = eth[s];
             if (value > 0) {
                 s.transfer(value);
                 Transfer(s, value);
@@ -154,46 +94,20 @@ contract BuckySalary is Ownable {
         }
     }
 
-    function addStaff(string addr, uint value) public onlyOwner {
-        for (uint i = 0; i < staff.length; i++) {
-            if (strCompare(staff[i], addr) == 0) {
-                staffETH[i] = value;
-                return;
-            }
 
-            if (strCompare(staff[i], "") == 0) {
-                staff[i] = addr;
-                staffETH[i] = value;
-                return;
-            }
+    function setETH(address addr, uint value) public onlyOwner {
+        if (eth[addr] == 0) {
+            staff.push(addr);
         }
 
-        staff.push(addr);
-        staffETH.push(value);
+        eth[addr] = value;
     }
 
-    function removeStaff(string addr) public onlyOwner {
-        for (uint i = 0; i < staff.length; i++) {
-            if (strCompare(staff[i], addr) == 0) {
-                staff[i] = "";
-                staffETH[i] = 0;
-            }
+    function setMultiETH(address[] addr, uint[] value) public onlyOwner {
+        require(addr.length == value.length);
+        for (uint i = 0; i < addr.length; i++) {
+            setETH(addr[i], value[i]);
         }
     }
 
-    function setETH(string addr, uint value) public onlyOwner {
-        for (uint i = 0; i < staff.length; i++) {
-            if (strCompare(staff[i], addr) == 0) {
-                staffETH[i] = value;
-                return;
-            }
-        }
-    }
-
-    
-    
-    
-    
-    
-    
 }
