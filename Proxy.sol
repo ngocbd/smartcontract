931c2d378d251c86979aa01f22bd2fd7be78fe71
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 20:17:08 +0700

    add contract Proxy

--- Proxy.sol
+++ Proxy.sol
@@ -1,47 +1,43 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Proxy at 0x47d4cc3d331525976553baff7c9e4d410087804a
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Proxy at 0x0073e5e52e2b4fe218d75d994ee2b3c82f9c87ea
 */
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.21;
 
-contract Contract {
-  mapping (address => uint256) public balances_bonus;
-  uint256 public contract_eth_value_bonus;
-}
+contract Proxy{
+  address public owner;
+  address public target;
+  event ProxyTargetSet(address target);
+  constructor () public{
+    owner = msg.sender;
+    }
 
-contract ERC20 {
-  function transfer(address _to, uint256 _value) public returns (bool success);
-  function balanceOf(address _owner) public constant returns (uint256 balance);
-}
-
-contract Proxy {
-
-  Contract contr;
-  uint256 public eth_balance;
-  ERC20 public token;
-  mapping (address => bool) public withdrew;
-  address owner;
-
-  constructor(address _contract, address _token) {
-      owner = msg.sender;
-      contr = Contract(_contract);
-      token = ERC20(_token);
-      eth_balance = contr.contract_eth_value_bonus();
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
-  function withdraw()  {
-      require(withdrew[msg.sender] == false);
-      withdrew[msg.sender] = true;
-      uint256 balance = contr.balances_bonus(msg.sender);
-      uint256 contract_token_balance = token.balanceOf(address(this));
-      uint256 tokens_to_withdraw = (balance*contract_token_balance)/eth_balance;
-      eth_balance -= balance;
-      require(token.transfer(msg.sender, tokens_to_withdraw));
-
+  function setTarget(address _target) public onlyOwner {
+    target = _target;
+    emit ProxyTargetSet(_target);
   }
 
-  function emergency_withdraw(address _token) {
-      require(msg.sender == owner);
-      require(ERC20(_token).transfer(owner, ERC20(_token).balanceOf(this)));
-  }
+  function () payable public {
+    address _impl = target;
+    require(_impl != address(0));
 
+    assembly {
+      let ptr := mload(0x40)
+      calldatacopy(ptr, 0, calldatasize)
+      let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+      let size := returndatasize
+      returndatacopy(ptr, 0, size)
+
+      switch result
+      case 0 { revert(ptr, size) }
+      default { return(ptr, size) }
+    }
+  }
 }
