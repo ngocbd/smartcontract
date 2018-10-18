af09464e78dabd4ad69d202ff0c8661889b145a8
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 14:50:33 +0700

    add contract BlocktradeBlackHole

---
+++ BlocktradeBlackHole.sol
@@ -0,0 +1,8 @@
+/* 
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BlocktradeBlackHole at 0x24171bcfb9d600ff8392387f5cc3171d07ebb266
+*/
+pragma solidity ^0.4.25;
+
+contract BlocktradeBlackHole{
+  
+}
