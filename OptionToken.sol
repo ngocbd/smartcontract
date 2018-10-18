d1be8edac701ddf2e575b66182db4df4d54c61cf
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:20:03 +0700

    add contract OptionToken

--- OptionToken.sol
+++ OptionToken.sol
@@ -1,266 +1,239 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OptionToken at 0x8f199d74d89b977d3f0684f0572bc66b16120ed8
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract OptionToken at 0x8a06ee20974db941226c874211bb0e5c1c5aabdc
 */
-/**
- *  The Option token contract complies with the ERC20 standard.
- *  This contract implements american option.
- *  Holders of the Option tokens can make a purchase of the underlying asset
- *  at the price of Strike until the Expiration time.
- *  The Strike price and Expiration date are set once and can't be changed.
- *  Author: Alexey Bukhteyev
- **/
+pragma solidity ^0.4.20;
 
-pragma solidity ^0.4.4;
-
-contract ERC20 {
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed from, address indexed spender, uint256 value);
-
-    function name() public constant returns(string);
-    function symbol() public constant returns(string);
-
-    function totalSupply() public constant returns(uint256 supply);
-    function balanceOf(address _owner) public constant returns(uint256 balance);
-    function transfer(address _to, uint256 _value) public returns(bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success);
-    function approve(address _spender, uint256 _value) public returns(bool success);
-    function allowance(address _owner, address _spender) public constant returns(uint256 remaining);
-    function decimals() public constant returns(uint8);
-}
-
-/*
-    Allows to recreate OptionToken contract on the same address.
-    Just create new TokenHolders(OptionToken) and reinitiallize OptionToken using it's address
-*/
-contract TokenHolders {
-    address public owner;
-
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-
-    /*
-        TokenHolders contract is being connected to OptionToken on creation.
-        Nobody can modify balanceOf and allowance except OptionToken
-    */
-
-    function validate() external constant returns (bool);
-
-    function setBalance(address _to, uint256 _value) external;
-
-    /* Send some of your tokens to a given address */
-    function transfer(address _from, address _to, uint256 _value) public returns(bool success);
-
-    /* Allow another contract or person to spend some tokens in your behalf */
-    function approve(address _sender, address _spender, uint256 _value) public returns(bool success);
-
-    /* A contract or  person attempts to get the tokens of somebody else.
-     *  This is only allowed if the token holder approved. */
-    function transferWithAllowance(address _origin, address _from, address _to, uint256 _value)
-    public returns(bool success);
-}
-
-/*
-    This ERC20 contract is a basic option contract that implements a token which
-    allows to token holder to buy some asset for the fixed strike price before expiration date.
-*/
 contract OptionToken {
-    string public standard = 'ERC20';
+
+    address public owner;
+// ?????ERC20??????
     string public name;
     string public symbol;
     uint8 public decimals;
-    address public owner;
-
-    // Option characteristics
-    uint256 public expiration = 1512172800; //02.12.2017 Use unix timespamp
-    uint256 public strike = 20000000000;
-
-    ERC20 public baseToken;
-    TokenHolders public tokenHolders;
-
-    bool _initialized = false;
-
-
-    /* This generates a public event on the blockchain that will notify clients */
-    // ERC20 events
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed from, address indexed spender, uint256 value);
-
-    // OptionToken events
-    event Deposit(address indexed from, uint256 value);
-    event Redeem(address indexed from, uint256 value, uint256 ethvalue);
-    event Issue(address indexed issuer, uint256 value);
-
-    // Only set owner on the constructor
-    function OptionToken() public {
+    uint public totalSupply;
+    
+    mapping (address => uint) public balanceOf;
+    mapping (address => mapping (address => uint)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    
+    event Burn(address indexed from, uint value);
+    
+    constructor (
+        uint initialSupply,
+        string tokenName,
+        string tokenSymbol,
+        uint8 tokenDecimals
+    ) public {
+        totalSupply = initialSupply * 10 ** uint(tokenDecimals);
+        balanceOf[msg.sender] = totalSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
         owner = msg.sender;
+        decimals = tokenDecimals;
     }
 
-    // ERC20 functions
-    function balanceOf(address _owner) public constant returns(uint256 balance) {
-        return tokenHolders.balanceOf(_owner);
-    }
-
-    function totalSupply() public constant returns(uint256 supply) {
-        // total supply is a balance of this contract in base tokens
-        return baseToken.balanceOf(this);
-    }
-
-    /* Send some of your tokens to a given address */
-    function transfer(address _to, uint256 _value) public returns(bool success) {
-        if(now > expiration)
-            return false;
-
-        if(!tokenHolders.transfer(msg.sender, _to, _value))
-            return false;
-
-        Transfer(msg.sender, _to, _value); // Notify anyone listening that this transfer took place
+    function _transfer(address _from, address _to, uint _value) internal returns (bool) {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(add(balanceOf[_to],_value) >= balanceOf[_to]);
+        uint previousBalances = add(balanceOf[_from],balanceOf[_to]);
+        balanceOf[_from] = sub(balanceOf[_from],_value);
+        balanceOf[_to] = add(balanceOf[_to],_value);
+        emit Transfer(_from, _to, _value);
+        assert(add(balanceOf[_from],balanceOf[_to]) == previousBalances);
         return true;
     }
 
-    /* Allow another contract or person to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value) public returns(bool success) {
-        if(now > expiration)
-            return false;
+    function transfer(address _to, uint _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
 
-        if(!tokenHolders.approve(msg.sender, _spender, _value))
-            return false;
-
-        Approval(msg.sender, _spender, _value);
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] = sub(allowance[_from][msg.sender],_value);
+        _transfer(_from, _to, _value);
         return true;
     }
 
-
-    /* A contract or  person attempts to get the tokens of somebody else.
-     *  This is only allowed if the token holder approved. */
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
-        if(now > expiration)
-            return false;
-
-        if(!tokenHolders.transferWithAllowance(msg.sender, _from, _to, _value))
-            return false;
-
-        Transfer(_from, _to, _value);
+    function burn(uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = sub(balanceOf[msg.sender],_value);
+        totalSupply = sub(totalSupply,_value);
+        emit Burn(msg.sender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) public constant returns(uint256 remaining) {
-        return tokenHolders.allowance(_owner, _spender);
-    }
-
-    // OptionToken functions
-
-    /*
-        Then we should pass base token contract address to init() function.
-        Only contract creator can call init() and only once
-    */
-    function init(ERC20 _baseToken, TokenHolders _tokenHolders, string _name, string _symbol,
-                uint256 _exp, uint256 _strike) public returns(bool success) {
-        require(msg.sender == owner && !_initialized);
-
-        baseToken = _baseToken;
-        tokenHolders = _tokenHolders;
-
-        // if baseToken.totalSupply() is zero - something is wrong
-        assert(baseToken.totalSupply() != 0);
-        // validate tokenHolders contract owner - it should be OptionToken
-        assert(tokenHolders.validate());
-
-        name = _name;
-        symbol = _symbol;
-        expiration = _exp;
-        strike = _strike;
-
-        decimals = baseToken.decimals();
-
-        _initialized = true;
+    function burnFrom(address _from, uint _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] = sub(balanceOf[_from],_value);
+        allowance[_from][msg.sender] = sub(allowance[_from][msg.sender],_value);
+        totalSupply = sub(totalSupply,_value);
+        emit Burn(_from, _value);
         return true;
     }
 
-    /*
-        Allows to increase totalSupply and get OptionTokens to their balance.
-        Before calling depositTokens the caller should approve the transfer for this contract address
-        using ERC20.approve().
-        Actually should be called by contract owner, because no ETH payout will be done for token transfer.
-    */
-    function issue(uint256 _value) public returns(bool success) {
-        require(now <= expiration && _initialized);
+// ?????ERC20??????
 
-        uint256 receiver_balance = balanceOf(msg.sender) + _value;
-        assert(receiver_balance >= _value);
 
-        // check if transfer failed
-        if(!baseToken.transferFrom(msg.sender, this, _value))
-            revert();
-
-        tokenHolders.setBalance(msg.sender, receiver_balance);
-        Issue(msg.sender, receiver_balance);
-
-        return true;
-    }
-
-    /*
-        Buy base tokens for the strike price
-    */
-    function() public payable {
-        require(now <= expiration && _initialized); // the contract should be initialized!
-        uint256 available = balanceOf(msg.sender); // balance of option holder
-
-        // check if there are tokens for sale
-        require(available > 0);
-
-        uint256 tokens = msg.value / (strike);
-        assert(tokens > 0 && tokens <= msg.value);
-
-        uint256 change = 0;
-        uint256 eth_to_transfer = 0;
-
-        if(tokens > available) {
-            tokens = available; // send all available tokens
-        }
-
-        // calculate the change for the operation
-        eth_to_transfer = tokens * strike;
-        assert(eth_to_transfer >= tokens);
-        change = msg.value - eth_to_transfer;
-        assert(change < msg.value);
-
-        if(!baseToken.transfer(msg.sender, tokens)) {
-            revert(); // error, revert transaction
-        }
-
-        uint256 new_balance = balanceOf(msg.sender) - tokens;
-        tokenHolders.setBalance(msg.sender, new_balance);
-
-        // new balance should be less then old balance
-        assert(balanceOf(msg.sender) < available);
-
-        if(change > 0) {
-            msg.sender.transfer(change); // return the change
-        }
-
-        if(eth_to_transfer > 0) {
-            owner.transfer(eth_to_transfer); // transfer eth for tokens to the contract owner
-        }
-
-        Redeem(msg.sender, tokens, eth_to_transfer);
-    }
-
-    /*
-        Allows the the contract owner to withdraw all unsold base tokens,
-        also deinitializes the token
-    */
-    function withdraw() public returns(bool success) {
+// ????????????
+    // ?????owner??
+    modifier onlyOwner() {
         require(msg.sender == owner);
-        if(now <= expiration || !_initialized)
-            return false;
+        _;
+    }
+    // ??owner??
+    function transferOwnership(address newOwner) external onlyOwner {
+        if (newOwner != address(0))
+            owner = newOwner;
+    }
+    // ???????
+    function selfdestruct() external onlyOwner {
+        selfdestruct(owner);
+    }
 
-        // transfer all tokens
-        baseToken.transfer(owner, totalSupply());
+    bool public status = true;// ???????
 
-        // perform deinitialization
-        baseToken = ERC20(0);
-        tokenHolders = TokenHolders(0);
-        _initialized = false;
+    modifier checkStatus() {
+        require(status == true);
+        _;
+    }
+
+    function unlockContract() external onlyOwner {
+        require(!status);
+        status = true;
+    }
+
+    function lockContract() external onlyOwner {
+        require(status);
+        status = false;
+    }
+
+    mapping (address => uint) whitelist;// ?????
+
+    function addWhiteList (address _user, uint _amount) public onlyOwner checkStatus {
+        whitelist[_user] = _amount;
+    }
+
+    function removeWhiteList (address _user) public onlyOwner checkStatus {
+        delete whitelist[_user];
+    }
+
+    function isAllowTransfer(address _user) public view returns (bool) {
+        return whitelist[_user] == 0 ? false : true;
+    }
+
+    function getAllowAmount(address _user) public view returns (uint) {
+        return whitelist[_user];
+    }
+
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        if (a == 0) {
+            return 0;
+        }
+
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint a, uint b) internal pure returns (uint) {
+        return a / b;
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+// ????????????
+
+// ?????????
+
+    event issueEvent(bytes32 issueKey);
+
+    struct IssueStruct {
+        // ????
+        uint issueAmount;
+        // ????
+        uint32 issueDate;
+        // ?????
+        uint32 vestingStartDate;
+    }
+    
+    mapping (address => mapping (bytes32 => IssueStruct)) public issueList;
+
+
+    // ??
+    function issue ( 
+        address _issueAddress, uint _issueAmount,
+        uint32 _issueDate,uint32 _vestingStartDate 
+    ) 
+        external 
+        checkStatus 
+        onlyOwner 
+        returns (bool)
+    {
+        require(_issueAddress != 0x0);
+        require(_issueDate != 0);
+        require(_vestingStartDate != 0);
+        
+        uint nowTime = block.timestamp;
+        bytes32 issueKey = sha256(_issueAddress, _issueAmount, _issueDate, _vestingStartDate, nowTime);
+        // ??
+        issueList[_issueAddress][issueKey] = IssueStruct({
+            issueAmount: _issueAmount,
+            issueDate: _issueDate,
+            vestingStartDate: _vestingStartDate
+        });
+
+        emit issueEvent(issueKey);
         return true;
     }
+
+    // ??address?key ??????
+    function showIssueDetail ( address _issueAddress, bytes32 _issueKey ) 
+        public 
+        view 
+        returns ( uint, uint32, uint32 ) 
+    {
+        require(hasIssue(_issueAddress, _issueKey));
+        IssueStruct storage issueDetail = issueList[_issueAddress][_issueKey];
+        return ( 
+            issueDetail.issueAmount, issueDetail.issueDate, 
+            issueDetail.vestingStartDate
+        );
+    }
+
+    // ??address ? key ??????????
+    function hasIssue ( address _issueAddress, bytes32 _issueKey )
+        internal 
+        view 
+        returns (bool)
+    {
+        if (issueList[_issueAddress][_issueKey].issueAmount != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+// ???????
+
+// ???????????
+    function reveiveToken ( address _issueAddress, uint amount ) 
+        external
+        onlyOwner
+        checkStatus
+    {
+        _transfer(owner, _issueAddress, amount);
+    }
+// ???????????
 }
