39ff3204cc4bf6b35d3f3e2eabeb602e20c5c80e
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 10:44:18 +0700

    add contract PFC

--- PFC.sol
+++ PFC.sol
@@ -1,238 +1,119 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PFC at 0x3a773baa30d09f04da0bd869b142ea5503a31022
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract PFC at 0xa37d94e80eab7a5bcb6d2e76b7666e341e4b58f6
 */
-pragma solidity ^0.4.11;
+// Copyright (C) PFC Team
+contract ERC20 {
+    function totalSupply() constant returns (uint supply);
+    function balanceOf( address who ) constant returns (uint value);
+    function allowance( address owner, address spender ) constant returns (uint _allowance);
 
-contract ERC20Token {
-    /* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
-    uint256 public totalSupply;
-    This automatically creates a getter function for the totalSupply.
-    This is moved to the base contract since public getter functions are not
-    currently recognised as an implementation of the matching abstract
-    function by the compiler.
-    */
-    /// total amount of tokens
-    uint256 public totalSupply;
+    function transfer( address to, uint value) returns (bool ok);
+    function transferFrom( address from, address to, uint value) returns (bool ok);
+    function approve( address spender, uint value ) returns (bool ok);
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer( address indexed from, address indexed to, uint value);
+    event Approval( address indexed owner, address indexed spender, uint value);
 }
 
+contract DSMath {
 
-contract Controlled {
-    /// @notice The address of the controller is the only address that can call
-    ///  a function with this modifier
-    modifier onlyController { if (msg.sender != controller) throw; _; }
-
-    address public controller;
-
-    function Controlled() { controller = msg.sender;}
-
-    /// @notice Changes the controller of the contract
-    /// @param _newController The new controller of the contract
-    function changeController(address _newController) onlyController {
-        controller = _newController;
+    function add(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        assert((z = x + y) >= x);
     }
+
+    function sub(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        assert((z = x - y) <= x);
+    }
+
 }
 
-contract StandardToken is ERC20Token ,Controlled{
+contract PFC is ERC20,DSMath {
+    uint256                                            _supply;
+    mapping (address => uint256)                       _balances;
+    mapping (address => mapping (address => uint256))  _approvals;
 
-    bool public showValue=true;
+    string   public  symbol;
+    string   public  name;
+    uint256  public  decimals = 18;
+    address  public  owner;
+    bool     public  stopped;
 
-    // Flag that determines if the token is transferable or not.
-    bool public transfersEnabled;
+    uint256  public maxSupply=1000000000 ether;
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
 
-        if(!transfersEnabled) throw;
-
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else {
-            return false;
-        }
+    function PFC() {
+        symbol="PFC";
+        name="Power Fans ERC20 Token";
+        owner=msg.sender;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-
-        if(!transfersEnabled) throw;
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else {
-            return false;
-        }
+    modifier auth {
+        assert (msg.sender==owner);
+        _;
+    }
+    modifier stoppable {
+        assert (!stopped);
+        _;
+    }
+    function stop() auth  {
+        stopped = true;
+    }
+    function start() auth  {
+        stopped = false;
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        if(!showValue)
-        return 0;
-        return balances[_owner];
+    function totalSupply() constant returns (uint256) {
+        return _supply;
+    }
+    function balanceOf(address src) constant returns (uint256) {
+        return _balances[src];
+    }
+    function allowance(address src, address guy) constant returns (uint256) {
+        return _approvals[src][guy];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        if(!transfersEnabled) throw;
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function transfer(address dst, uint wad) stoppable returns (bool) {
+        assert(_balances[msg.sender] >= wad);
+
+        _balances[msg.sender] = sub(_balances[msg.sender], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        Transfer(msg.sender, dst, wad);
+
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        if(!transfersEnabled) throw;
-        return allowed[_owner][_spender];
-    }
+    function transferFrom(address src, address dst, uint wad)stoppable returns (bool) {
+        assert(_balances[src] >= wad);
+        assert(_approvals[src][msg.sender] >= wad);
 
-    /// @notice Enables token holders to transfer their tokens freely if true
-    /// @param _transfersEnabled True if transfers are allowed in the clone
-    function enableTransfers(bool _transfersEnabled) onlyController {
-        transfersEnabled = _transfersEnabled;
-    }
-    function enableShowValue(bool _showValue) onlyController {
-        showValue = _showValue;
-    }
+        _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
 
-    function generateTokens(address _owner, uint _amount
-    ) onlyController returns (bool) {
-        uint curTotalSupply = totalSupply;
-        if (curTotalSupply + _amount < curTotalSupply) throw; // Check for overflow
-        totalSupply=curTotalSupply + _amount;
+        Transfer(src, dst, wad);
 
-        balances[_owner]+=_amount;
-
-        Transfer(0, _owner, _amount);
         return true;
     }
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-}
 
-contract MiniMeTokenSimple is StandardToken {
+    function approve(address guy, uint256 wad) stoppable returns (bool) {
+        _approvals[msg.sender][guy] = wad;
 
-    string public name;                //The Token's name: e.g. DigixDAO Tokens
-    uint8 public decimals;             //Number of decimals of the smallest unit
-    string public symbol;              //An identifier: e.g. REP
-    string public version = 'MMT_0.1'; //An arbitrary versioning scheme
+        Approval(msg.sender, guy, wad);
 
-
-    // `parentToken` is the Token address that was cloned to produce this token;
-    //  it will be 0x0 for a token that was not cloned
-    address public parentToken;
-
-    // `parentSnapShotBlock` is the block number from the Parent Token that was
-    //  used to determine the initial distribution of the Clone Token
-    uint public parentSnapShotBlock;
-
-    // `creationBlock` is the block number that the Clone Token was created
-    uint public creationBlock;
-
-    // The factory used to create new clone tokens
-    address public tokenFactory;
-
-    ////////////////
-    // Constructor
-    ////////////////
-
-    /// @notice Constructor to create a MiniMeTokenSimple
-    /// @param _tokenFactory The address of the MiniMeTokenFactory contract that
-    ///  will create the Clone token contracts, the token factory needs to be
-    ///  deployed first
-    /// @param _parentToken Address of the parent token, set to 0x0 if it is a
-    ///  new token
-    /// @param _parentSnapShotBlock Block of the parent token that will
-    ///  determine the initial distribution of the clone token, set to 0 if it
-    ///  is a new token
-    /// @param _tokenName Name of the new token
-    /// @param _decimalUnits Number of decimals of the new token
-    /// @param _tokenSymbol Token Symbol for the new token
-    /// @param _transfersEnabled If true, tokens will be able to be transferred
-    function MiniMeTokenSimple(
-    address _tokenFactory,
-    address _parentToken,
-    uint _parentSnapShotBlock,
-    string _tokenName,
-    uint8 _decimalUnits,
-    string _tokenSymbol,
-    bool _transfersEnabled
-    ) {
-        tokenFactory = _tokenFactory;
-        name = _tokenName;                                 // Set the name
-        decimals = _decimalUnits;                          // Set the decimals
-        symbol = _tokenSymbol;                             // Set the symbol
-        parentToken = _parentToken;
-        parentSnapShotBlock = _parentSnapShotBlock;
-        transfersEnabled = _transfersEnabled;
-        creationBlock = block.number;
+        return true;
     }
-    //////////
-    // Safety Methods
-    //////////
-
-    /// @notice This method can be used by the controller to extract mistakenly
-    ///  sent tokens to this contract.
-    /// @param _token The address of the token contract that you want to recover
-    ///  set to 0 in case you want to extract ether.
-    function claimTokens(address _token) onlyController {
-        if (_token == 0x0) {
-            controller.transfer(this.balance);
-            return;
-        }
-
-        ERC20Token token = ERC20Token(_token);
-        uint balance = token.balanceOf(this);
-        token.transfer(controller, balance);
-        ClaimedTokens(_token, controller, balance);
+    function mint(address dst,uint128 wad) auth stoppable {
+        assert(add(_supply,wad)<=maxSupply);
+        _balances[dst] = add(_balances[dst], wad);
+        _supply = add(_supply, wad);
     }
 
-    event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
+    event LogSetOwner     (address indexed owner);
 
-}
-
-contract PFC is MiniMeTokenSimple {
-
-    function PFC(address _tokenFactory)
-    MiniMeTokenSimple(
-    _tokenFactory,
-    0x0,                     // no parent token
-    0,                       // no snapshot block number from parent
-    "Power Fans Token",      // Token name
-    18,                      // Decimals
-    "PFC",                   // Symbol
-    false                    // Enable transfers
-    ) {}
+    function setOwner(address owner_) auth
+    {
+        owner = owner_;
+        LogSetOwner(owner);
+    }
 }
