409da9580c589b53aca3ac1e5ef0be98fbe27342
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:37:46 +0700

    add contract Token

--- Token.sol
+++ Token.sol
@@ -1,12 +1,12 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x8abfe51e121504cd163d28713d85d3404d7a0adc
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract Token at 0x03b3ab9bcbb05779d30a7c38e5cbe6489e7ebac5
 */
 pragma solidity ^0.4.23;
 
 contract Token {
 	uint8 public decimals;
 
-  	constructor () public {
-		decimals = 24;
+  	constructor (uint8 _decimals) public {
+		decimals = _decimals;
   	}
 }
