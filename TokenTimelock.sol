8ef827920b40e40dde720aa2b35d0a6ed36b5a46
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 08:47:28 +0700

    add contract TokenTimelock

--- TokenTimelock.sol
+++ TokenTimelock.sol
@@ -1,92 +1,47 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0xece61c4390dad8f7e9e1d6e3fa52e0a974b51868
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract TokenTimelock at 0x4c1544441ab650c85bc1f0de237ec13fe47dd99d
 */
 pragma solidity ^0.4.18;
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-// File: node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
-// File: node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-// File: node_modules/zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol
-
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure.
- * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
-library SafeERC20 {
-  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
-    assert(token.transfer(to, value));
-  }
-
-  function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
-    assert(token.transferFrom(from, to, value));
-  }
-
-  function safeApprove(ERC20 token, address spender, uint256 value) internal {
-    assert(token.approve(spender, value));
-  }
-}
-
-// File: node_modules/zeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol
-
-/**
- * @title TokenTimelock
- * @dev TokenTimelock is a token holder contract that will allow a
- * beneficiary to extract the tokens after a given release time
- */
 contract TokenTimelock {
-  using SafeERC20 for ERC20Basic;
+    ERC20Interface public token;
+    // beneficiary of tokens after they are released
+    address public beneficiary;
 
-  // ERC20 basic token contract being held
-  ERC20Basic public token;
+    // timestamp when token release is enabled
+    uint256 public releaseTime;
 
-  // beneficiary of tokens after they are released
-  address public beneficiary;
+    constructor(ERC20Interface _token, address _beneficiary, uint256 _releaseTime) public
+    {
+        // solium-disable-next-line security/no-block-members
+        require(_releaseTime > block.timestamp);
+        token = _token;
+        beneficiary = _beneficiary;
+        releaseTime = _releaseTime;
+    }
 
-  // timestamp when token release is enabled
-  uint256 public releaseTime;
+    /**
+    * @notice Transfers tokens held by timelock to beneficiary.
+    */
+    function release() public {
+        // solium-disable-next-line security/no-block-members
+        require(block.timestamp >= releaseTime);
 
-  function TokenTimelock(ERC20Basic _token, address _beneficiary, uint256 _releaseTime) public {
-    require(_releaseTime > now);
-    token = _token;
-    beneficiary = _beneficiary;
-    releaseTime = _releaseTime;
-  }
+        uint256 amount = token.balanceOf(address(this));
+        require(amount > 0);
 
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function release() public {
-    require(now >= releaseTime);
-
-    uint256 amount = token.balanceOf(this);
-    require(amount > 0);
-
-    token.safeTransfer(beneficiary, amount);
-  }
+        token.transfer(beneficiary, amount);
+    }
 }
