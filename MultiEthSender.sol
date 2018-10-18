33cc17468c613f59db8752b8c0e44add0aad435c
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 07:32:45 +0700

    add contract MultiEthSender

--- MultiEthSender.sol
+++ MultiEthSender.sol
@@ -1,41 +1,33 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiEthSender at 0xd2ad3ea94140860f6c9cec4fdbd373603b5031ec
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract MultiEthSender at 0xb52323c98e75974d2139e65d3cba06c43a53cae4
 */
-pragma solidity ^0.4.22;
+pragma solidity ^0.4.24;
 
 contract MultiEthSender {
-
-  uint256 constant private ethInWei = 10**18;
-  mapping(address => uint256) private balance;
-  address public owner;
-
-  event Send(uint256 _amount, address indexed receiver);
-
-  constructor() public payable {
-    owner = msg.sender;
-    balance[msg.sender] = msg.value;
-  }
-
-  function multiSendEth(uint256 amount, address[] list) public returns (bool) {
-    uint256 amountInWei = amount * ethInWei;
-    require(amountInWei * list.length <= balance[msg.sender], "the contract balance is not enough");
-    for (uint256 i = 0; i < list.length; i++) {
-      emit Send(amount, list[i]);
-      uint256 res = balance[msg.sender];
-      balance[msg.sender] = res - amountInWei;
-      list[i].transfer(amountInWei);
+    
+    event Send(uint256 _amount, address indexed _receiver);
+    
+    constructor() payable public {
+        
     }
-    return true;
-  }
-
-  function deposit() public payable returns (uint256) {
-    balance[msg.sender] += msg.value;
-    return balance[msg.sender];
-  }
-
-  function getBalance() public constant returns (uint256) {
-      return balance[msg.sender];
-  }
-
-  function() public payable { }
+    
+    function multiSendEth(uint256 amount, address[] list) public payable returns (bool) {
+        uint balanceBeforeTransfer = address(this).balance;
+        
+        for(uint i=0;i<list.length;i++){
+            list[i].transfer(amount);
+            emit Send(amount, list[i]);
+        }
+        assert(address(this).balance == balanceBeforeTransfer - amount*list.length);
+        return true;
+    }
+    
+    function getBalance() constant public returns(uint){
+        return address(this).balance;
+    }
+    
+    function() public payable{
+        
+    }
+    
 }
