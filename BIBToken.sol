609698b485f0b893a7e6f55848b76e0be861716d
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-18 09:18:47 +0700

    add contract BIBToken

--- BIBToken.sol
+++ BIBToken.sol
@@ -1,70 +1,224 @@
 /* 
- source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BIBToken at 0xd082e9e4f9b39bad6103c330292a7b219bf3b740
+ source code generate by Bui Dinh Ngoc aka ngocbd<buidinhngoc.aiti@gmail.com> for smartcontract BIBToken at 0x049158da08568d1973dc951ec758873bf7125478
 */
-contract EIP20Interface {    
-    uint256 public totalSupply;
-    function balanceOf(address _owner) public view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value); 
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+pragma solidity ^0.4.24;
+
+
+contract ERC223 {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+
+  function name() constant returns (string _name);
+  function symbol() constant returns (string _symbol);
+  function decimals() constant returns (uint8 _decimals);
+  function totalSupply() constant returns (uint256 _supply);
+
+  function transfer(address to, uint value) returns (bool ok);
+  function transfer(address to, uint value, bytes data) returns (bool ok);
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event ERC223Transfer(address indexed _from, address indexed _to, uint256 _value, bytes _data);
 }
-contract BIBToken is EIP20Interface {
 
-    uint256 constant private MAX_UINT256 = 2**256 - 1;
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
-   
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;                 
+contract ContractReceiver {
+  function tokenFallback(address _from, uint _value, bytes _data);
+}
 
-    function BIBToken(
-        uint256 _initialAmount,
-        string _tokenName,
-        uint8 _decimalUnits,
-        string _tokenSymbol
-    ) public {
-        balances[msg.sender] = _initialAmount;               
-        totalSupply = _initialAmount;                        
-        name = _tokenName;                                   
-        decimals = _decimalUnits;                            
-        symbol = _tokenSymbol;                              
+contract ERC223Token is ERC223 {
+  using SafeMath for uint;
+
+  mapping(address => uint) balances;
+
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+  uint256 public totalSupply;
+
+
+  // Function to access name of token .
+  function name() constant returns (string _name) {
+      return name;
+  }
+  // Function to access symbol of token .
+  function symbol() constant returns (string _symbol) {
+      return symbol;
+  }
+  // Function to access decimals of token .
+  function decimals() constant returns (uint8 _decimals) {
+      return decimals;
+  }
+  // Function to access total supply of tokens .
+  function totalSupply() constant returns (uint256 _totalSupply) {
+      return totalSupply;
+  }
+
+  // Function that is called when a user or another contract wants to transfer funds .
+  function transfer(address _to, uint _value, bytes _data) returns (bool success) {
+    if(isContract(_to)) {
+        return transferToContract(_to, _value, _data);
     }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
+    else {
+        return transferToAddress(_to, _value, _data);
     }
+}
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from] >= _value && allowance >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
+  // Standard function transfer similar to ERC20 transfer with no _data .
+  // Added due to backwards compatibility reasons .
+  function transfer(address _to, uint _value) returns (bool success) {
+
+    //standard function transfer similar to ERC20 transfer with no _data
+    //added due to backwards compatibility reasons
+    bytes memory empty;
+    if(isContract(_to)) {
+        return transferToContract(_to, _value, empty);
+    }
+    else {
+        return transferToAddress(_to, _value, empty);
+    }
+}
+
+//assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+  function isContract(address _addr) private returns (bool is_contract) {
+      uint length;
+      assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
         }
-        Transfer(_from, _to, _value);
-        return true;
+        if(length>0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
+  //function that is called when transaction target is an address
+  function transferToAddress(address _to, uint _value, bytes _data) private returns (bool success) {
+    if (balanceOf(msg.sender) < _value) revert();
+    balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+    balances[_to] = balanceOf(_to).add(_value);
+    Transfer(msg.sender, _to, _value);
+    ERC223Transfer(msg.sender, _to, _value, _data);
+    return true;
+  }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
+  //function that is called when transaction target is a contract
+  function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
+    if (balanceOf(msg.sender) < _value) revert();
+    balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+    balances[_to] = balanceOf(_to).add(_value);
+    ContractReceiver reciever = ContractReceiver(_to);
+    reciever.tokenFallback(msg.sender, _value, _data);
+    Transfer(msg.sender, _to, _value);
+    ERC223Transfer(msg.sender, _to, _value, _data);
+    return true;
+  }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }   
+
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+
+
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+
+
+// your contract's name
+
+contract BIBToken is ERC223Token {
+  using SafeMath for uint256;
+
+  // name
+  string public name = "Blockchain Investor Bearer";
+  // ticker
+  string public symbol = "BIB";
+  // set token's precision
+  //
+  // for example, 4 decimal points means that
+  // smallest token using will be 0.0001 BIB
+  uint public decimals = 4;
+  // total supply of the token
+  // 1 billion
+  uint public totalSupply = 1000000000 * (10**decimals);
+  //
+  address private treasury = 0x3805D6c12b3d53351Ad128EE78f4ca53Fb52dA77;
+  //
+  // given 4 decimals, this setting means "1 ETC = 4000 BIB"
+  uint256 private priceDiv = 25000000000;
+
+  event Purchase(address indexed purchaser, uint256 amount);
+
+  constructor() public {
+    // Tokens allocation. 150 million tokens distributed to current shareholders
+    // and 850 millions for future issuing (capital increase) against digital
+    // assets / currencies.
+    balances[msg.sender] = 1000000000 * (10**decimals);
+    // This is how many tokens you want to allocate for ICO
+    balances[0x0] = 0 * (10**decimals);
+  }
+
+  function () public payable {
+    bytes memory empty;
+    if (msg.value == 0) { revert(); }
+    uint256 purchasedAmount = msg.value.div(priceDiv);
+    if (purchasedAmount == 0) { revert(); } // not enough ETC sent
+    if (purchasedAmount > balances[0x0]) { revert(); } // too much ETC sent
+
+    treasury.transfer(msg.value);
+    balances[0x0] = balances[0x0].sub(purchasedAmount);
+    balances[msg.sender] = balances[msg.sender].add(purchasedAmount);
+
+    emit Transfer(0x0, msg.sender, purchasedAmount);
+    emit ERC223Transfer(0x0, msg.sender, purchasedAmount, empty);
+    emit Purchase(msg.sender, purchasedAmount);
+  }
 }
